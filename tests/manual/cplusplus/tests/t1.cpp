beee08de5a9630eb88cb088b9f07e3a74306445e
Roberto Raggi <qtc-committer@nokia.com>
2009-01-07 10:06:10 +0100

    Test the pretty printer.

--- tests/manual/cplusplus/tests/t1.cpp
+++ tests/manual/cplusplus/tests/t1.cpp
@@ -1,5 +1,5 @@
 
-class foo {
+class Class {
   int a, b;
 
   enum zoo { a, b };
@@ -8,5 +8,9 @@ class foo {
 
   void foo() {}
   inline void bar() {}
+
+  void another_foo() {
+    int a = static_cast<int>(1+2/3*4-5%6+(7&8));
+  }
 };
 
