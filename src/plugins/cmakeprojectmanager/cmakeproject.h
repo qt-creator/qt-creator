a4624993b96972ffd04bb1614667861867c9cc6f
Fred Emmott <mail@fredemmott.co.uk>
2009-01-15 14:48:28 +0000

    fill in project name for CMake projects

--- src/plugins/cmakeprojectmanager/cmakeproject.h
+++ src/plugins/cmakeprojectmanager/cmakeproject.h
@@ -118,6 +118,7 @@ private:
     CMakeManager *m_manager;
     QString m_fileName;
     CMakeFile *m_file;
+    QString m_projectName;
 
     // TODO probably need a CMake specific node structure
     CMakeProjectNode* m_rootNode;
@@ -137,10 +138,12 @@ public:
     QList<ProjectExplorer::FileNode *> fileList();
     QStringList includeFiles();
     QList<CMakeTarget> targets();
+    QString projectName() const;
 private:
     void parseCodeBlocks_project_file();
     void parseProject();
     void parseBuild();
+    void parseOption();
     void parseTarget();
     void parseTargetOption();
     void parseMakeCommand();
@@ -157,6 +160,7 @@ private:
     CMakeTarget m_target;
     bool m_targetType;
     QList<CMakeTarget> m_targets;
+    QString m_projectName;
 };
 
 class CMakeFile : public Core::IFile
