8d1edf7252160846a45926475d6a3a76af14e88f
Thorbjørn Lindeijer <thorbjorn.lindeijer@nokia.com>
2009-01-19 11:03:23 +0100

    Have the editor list combo adjust size to contents
    Now the box will grow larger when files with long filenames are opened.

--- src/plugins/coreplugin/editormanager/stackededitorgroup.cpp
+++ src/plugins/coreplugin/editormanager/stackededitorgroup.cpp
@@ -79,6 +79,7 @@ StackedEditorGroup::StackedEditorGroup(QWidget *parent) :
     tl->setMargin(0);
     {
         m_editorList->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+        m_editorList->setSizeAdjustPolicy(QComboBox::AdjustToContents);
         m_editorList->setMinimumContentsLength(20);
         m_proxyModel.setSourceModel(model());
         m_proxyModel.sort(0);
@@ -296,9 +297,9 @@ void StackedEditorGroup::setCurrentEditor(IEditor *editor)
 
 void StackedEditorGroup::checkEditorStatus()
 {
-        IEditor *editor = qobject_cast<IEditor *>(sender());
-        if (editor == currentEditor())
-            updateEditorStatus(editor);
+    IEditor *editor = qobject_cast<IEditor *>(sender());
+    if (editor == currentEditor())
+        updateEditorStatus(editor);
 }
 
 void StackedEditorGroup::updateEditorStatus(IEditor *editor)
