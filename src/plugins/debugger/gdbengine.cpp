e4bd6c55c4b09de69ab91d6e396d8228b111c82c
hjk <qtc-committer@nokia.com>
2009-04-01 15:44:34 +0200

    debugger: don't consider archer's "library-loaded" as a reason for a warning

--- src/plugins/debugger/gdbengine.cpp
+++ src/plugins/debugger/gdbengine.cpp
@@ -389,6 +389,11 @@ void GdbEngine::handleResponse(const QByteArray &buff)
                 handleAsyncOutput(record);
             } else if (asyncClass == "running") {
                 // Archer has 'thread-id="all"' here
+            } else if (asyncClass == "library-loaded") {
+                // Archer has 'id="/usr/lib/libdrm.so.2",
+                // target-name="/usr/lib/libdrm.so.2",
+                // host-name="/usr/lib/libdrm.so.2",
+                // symbols-loaded="0"'
             #ifdef Q_OS_MAC
             } else if (asyncClass == "shlibs-updated") {
                 // MAC announces updated libs
