13c699e2407c07ff6a50725c74be2dd5ea34879c
con <qtc-committer@nokia.com>
2009-04-24 16:47:39 +0200

    Missing files.

---
+++ src/plugins/coreplugin/dialogs/ioptionspage.cpp
@@ -0,0 +1,46 @@
+/**************************************************************************
+**
+** This file is part of Qt Creator
+**
+** Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).
+**
+** Contact:  Qt Software Information (qt-info@nokia.com)
+**
+** Commercial Usage
+**
+** Licensees holding valid Qt Commercial licenses may use this file in
+** accordance with the Qt Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Nokia.
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** If you are unsure which license is appropriate for your use, please
+** contact the sales department at qt-sales@nokia.com.
+**
+**************************************************************************/
+
+#include "ioptionspage.h"
+
+/*!
+  \class Core::IOptionsPage
+  \mainclass
+  \brief The IOptionsPage is an interface for providing options pages.
+
+  Guidelines for implementing:
+  \list
+  \o id() is an id used for filtering when calling ICore:: showOptionsDialog()
+  \o trName() is the (translated) name for display.
+  \o category() is the category used for filtering when calling ICore:: showOptionsDialog()
+  \o trCategory() is the translated category
+  \o apply() is called to store the settings. It should detect if any changes have been
+         made and store those.
+  \endlist
+*/
---
+++ src/plugins/coreplugin/editormanager/ieditor.cpp
@@ -0,0 +1,55 @@
+/**************************************************************************
+**
+** This file is part of Qt Creator
+**
+** Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).
+**
+** Contact:  Qt Software Information (qt-info@nokia.com)
+**
+** Commercial Usage
+**
+** Licensees holding valid Qt Commercial licenses may use this file in
+** accordance with the Qt Commercial License Agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Nokia.
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** If you are unsure which license is appropriate for your use, please
+** contact the sales department at qt-sales@nokia.com.
+**
+**************************************************************************/
+
+#include "ieditor.h"
+
+/*!
+  \class Core::IEditor
+  \brief The IEditor is an interface for providing different editors for different file types.
+
+  Classes that implement this interface are for example the editors for
+  C++ files, ui-files and resource files.
+
+  Whenever a user wants to edit or create a file, the EditorManager scans all
+  EditorFactoryInterfaces for suitable editors. The selected EditorFactory
+  is then asked to create an editor, which must implement this interface.
+
+  Guidelines for implementing:
+  \list
+  \o displayName() is used as a user visible description of the document (usually filename w/o path).
+  \o kind() must be the same value as the kind() of the corresponding EditorFactory.
+  \o The changed() signal should be emitted when the modified state of the document changes
+     (so /bold{not} every time the document changes, but /bold{only once}).
+  \o If duplication is supported, you need to ensure that all duplicates
+        return the same file().
+  \endlist
+
+  \sa Core::EditorFactoryInterface Core::IContext
+
+*/
