28303e861dfa53fb5b7388e2b0269a1abd5c0100
hjk <qtc-committer@nokia.com>
2009-05-27 11:45:47 +0200

    qstringbuilder: a version without QLatin1Literal

--- src/libs/utils/qstringbuilder.h
+++ src/libs/utils/qstringbuilder.h
@@ -57,22 +57,9 @@ QT_MODULE(Core)
 // constructor to allocated an unitialized string of the given size.
 //#define USE_CHANGED_QSTRING 1
 
-class QLatin1Literal
-{
-public:
-    template <int N>
-    QLatin1Literal(const char (&str)[N]) : m_size(N - 1), m_data(str) {}
-
-    inline int size() const { return m_size; }
-    inline const char *data() const { return m_data; }
-
-private:
-    const int m_size;
-    const char *m_data;
-};
-
-
 namespace {
+
+template <typename T> struct QConcatenable {};
 
 template <typename A, typename B>
 class QStringBuilder
@@ -85,10 +72,6 @@ public:
     const A &a;
     const B &b;
 };
-
-// make sure the operator% defined below acts only on types we want to handle.
-template <typename T> struct QConcatenable {};
-
 
 template <typename A, typename B>
 QStringBuilder<A, B>::operator QString() const 
@@ -107,7 +90,7 @@ QStringBuilder<A, B>::operator QString() const
 template <> struct QConcatenable<char>
 {
     typedef char type;
-    static int size(const char) { return 1; }
+    static inline int size(const char) { return 1; }
     static inline void appendTo(const char c, QChar *&out)
     {
         *out++ = QLatin1Char(c);
@@ -117,7 +100,7 @@ template <> struct QConcatenable<char>
 template <> struct QConcatenable<QLatin1Char>
 {
     typedef QLatin1Char type;
-    static int size(const QLatin1Char) { return 1; }
+    static inline int size(const QLatin1Char) { return 1; }
     static inline void appendTo(const QLatin1Char c, QChar *&out)
     {
         *out++ = c;
@@ -127,7 +110,7 @@ template <> struct QConcatenable<QLatin1Char>
 template <> struct QConcatenable<QLatin1String>
 {
     typedef QLatin1String type;
-    static int size(const QLatin1String &a) { return qstrlen(a.latin1()); }
+    static inline int size(const QLatin1String &a) { return qstrlen(a.latin1()); }
     static inline void appendTo(const QLatin1String &a, QChar *&out)
     {
         for (const char *s = a.latin1(); *s; )
@@ -136,21 +119,10 @@ template <> struct QConcatenable<QLatin1String>
 
 };
 
-template <> struct QConcatenable<QLatin1Literal>
-{
-    typedef QLatin1Literal type;
-    static int size(const QLatin1Literal &a) { return a.size(); }
-    static inline void appendTo(const QLatin1Literal &a, QChar *&out)
-    {
-        for (const char *s = a.data(); *s; )
-            *out++ = QLatin1Char(*s++);
-    }
-};
-
 template <> struct QConcatenable<QString>
 {
     typedef QString type;
-    static int size(const QString &a) { return a.size(); }
+    static inline int size(const QString &a) { return a.size(); }
     static inline void appendTo(const QString &a, QChar *&out)
     {
         const int n = a.size();
@@ -162,7 +134,7 @@ template <> struct QConcatenable<QString>
 template <> struct QConcatenable<QStringRef>
 {
     typedef QStringRef type;
-    static int size(const QStringRef &a) { return a.size(); }
+    static inline int size(const QStringRef &a) { return a.size(); }
     static inline void appendTo(QStringRef a, QChar *&out)
     {
         const int n = a.size();
@@ -171,12 +143,36 @@ template <> struct QConcatenable<QStringRef>
     }
 };
 
+template <int N>
+struct QConcatenable<const char[N]>
+{
+    typedef const char type[N];
+    static inline int size(const char *) { return N - 1; }
+    static inline void appendTo(const char *a, QChar *&out)
+    {
+        for (int i = 0; i < N - 1; ++i)
+            *out++ = QLatin1Char(*a++);
+    }
+};
+
+template <int N>
+struct QConcatenable<char[N]>
+{
+    typedef char type[N];
+    static inline int size(const char *) { return N - 1; }
+    static inline void appendTo(const char *a, QChar *&out)
+    {
+        for (int i = 0; i < N - 1; ++i)
+            *out++ = QLatin1Char(*a++);
+    }
+};
+
 
 template <typename A, typename B>
 struct QConcatenable< QStringBuilder<A, B> >
 {
     typedef QStringBuilder<A, B> type;
-    static int size(const type &p) 
+    static inline int size(const type &p) 
     {
         return QConcatenable<A>::size(p.a) + QConcatenable<B>::size(p.b);
     }
@@ -197,7 +193,6 @@ QStringBuilder<A, B> operator%(const A &a, const B &b)
     typedef typename QConcatenable<B>::type B1;
     return QStringBuilder<A1, B1>(a, b);
 }
-
 
 QT_END_NAMESPACE
 
