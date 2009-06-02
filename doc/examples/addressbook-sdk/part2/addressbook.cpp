330b6e2748992862bc56a2dc046d90e45e687ef9
Kavindra Devi Palaraja <kavindra.palaraja@nokia.com>
2009-06-02 17:42:27 +0200

    Fixes:    Doc - more of the tutorial
    RevBy:    TrustMe

--- doc/examples/addressbook-sdk/part2/addressbook.cpp
+++ doc/examples/addressbook-sdk/part2/addressbook.cpp
@@ -1,4 +1,3 @@
-//! [class implementation]
 #include "addressbook.h"
 #include "ui_addressbook.h"
 
@@ -6,10 +5,30 @@ AddressBook::AddressBook(QWidget *parent)
     : QWidget(parent), ui(new Ui::AddressBookClass)
 {
     ui->setupUi(this);
+
+    addButton = new QPushButton();
+    addButton = ui->addButton;
+
+    submitButton = new QPushButton();
+    submitButton = ui->submitButton;
+
+    cancelButton = new QPushButton();
+    cancelButton = ui->cancelButton;
 }
 
 AddressBook::~AddressBook()
 {
     delete ui;
 }
-//! [class implementation]
+
+void AddressBook::addContact()
+{
+}
+
+void AddressBook::submitContact()
+{
+}
+
+void AddressBook::cancel()
+{
+}
--- doc/examples/addressbook-sdk/part2/addressbook.h
+++ doc/examples/addressbook-sdk/part2/addressbook.h
@@ -3,6 +3,9 @@
 #define ADDRESSBOOK_H
 
 #include <QtGui/QWidget>
+#include <QtGui/QPushButton>
+#include <QtGui/QLineEdit>
+#include <QtGui/QTextEdit>
 
 namespace Ui
 {
@@ -17,8 +20,29 @@ public:
     AddressBook(QWidget *parent = 0);
     ~AddressBook();
 
+//! [slot definition]
+public slots:
+    void addContact();
+    void submitContact();
+    void cancel();
+//! [slot definition]
+
 private:
     Ui::AddressBookClass *ui;
+
+//! [members1]
+    QPushButton *addButton;
+    QPushButton *submitButton;
+    QPushButton *cancelButton;
+    QLineEdit *nameLine;
+    QTextEdit *addressText;
+//! [members1]
+
+//! [members2]
+    QMap<QString, QString> contacts;
+    QString oldName;
+    QString oldAddress;
+//! [members2]
 };
 
 #endif // ADDRESSBOOK_H
