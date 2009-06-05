d9e98f343c0edbf1b2048f92263a00e1ee94772c
Jens Bache-Wiig <jens.bache-wiig@nokia.com>
2009-06-05 14:05:24 +0200

    Fixes:    Fix red color on error for progressbars

--- src/plugins/coreplugin/progressmanager/progresspie.cpp
+++ src/plugins/coreplugin/progressmanager/progresspie.cpp
@@ -149,9 +149,8 @@ void ProgressBar::paintEvent(QPaintEvent *)
     QRect inner = rect.adjusted(2, 2, -1, -1);
     inner.adjust(0, 0, qRound((percent - 1) * inner.width()), 0);
     if (m_error) {
-        // TODO this is not fancy enough
-        QColor red(255, 0, 0, 180);
-        p.setBrush(red);
+        QColor red(255, 60, 0, 210);
+        c = red;
         // avoid too small red bar
         if (inner.width() < 10)
             inner.adjust(0, 0, 10 - inner.width(), 0);
