0912e70469bb63699702cfc25d7f855ed914cfa4
Kavindra Devi Palaraja <kavindra.palaraja@nokia.com>
2009-06-05 16:25:31 +0200

    Doc - Starting with Part 4
    Reviewed-by: TrustMe

---
+++ doc/examples/addressbook-sdk/part4/addressbook.h
@@ -0,0 +1,58 @@
+//! [class definition]
+#ifndef ADDRESSBOOK_H
+#define ADDRESSBOOK_H
+
+#include <QtGui/QWidget>
+#include <QtGui/QPushButton>
+#include <QtGui/QLineEdit>
+#include <QtGui/QTextEdit>
+#include <QtGui/QMessageBox>
+
+
+namespace Ui
+{
+    class AddressBook;
+}
+
+class AddressBook : public QWidget
+{
+    Q_OBJECT
+
+public:
+    AddressBook(QWidget *parent = 0);
+//! [enum]
+    enum Mode { NavigationMode, AddingMode, EditingMode };
+//! [enum]
+    ~AddressBook();
+
+public slots:
+    void addContact();
+    void submitContact();
+    void cancel();
+//! [slot definition]
+    void editContact();
+    void removeContact();
+//! [slot definition]
+    void next();
+    void previous();
+
+private:
+    Ui::AddressBook *ui;
+
+    QPushButton *addButton;
+    QPushButton *submitButton;
+    QPushButton *cancelButton;
+//! [members]
+    QPushButton *nextButton;
+    QPushButton *previousButton;
+//! [members]
+    QLineEdit *nameLine;
+    QTextEdit *addressText;
+
+    QMap<QString, QString> contacts;
+    QString oldName;
+    QString oldAddress;
+};
+
+#endif // ADDRESSBOOK_H
+//! [class definition]
