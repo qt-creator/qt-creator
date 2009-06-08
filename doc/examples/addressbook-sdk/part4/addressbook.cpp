1a07f2caeffadb2ce27ca4a727165a43390ae1a0
Kavindra Devi Palaraja <kavindra.palaraja@nokia.com>
2009-06-08 16:02:34 +0200

    Doc - finishing up Part 4 and setting up Parts 5, 6, and 7
    RevBy:    TrustMe

--- doc/examples/addressbook-sdk/part4/addressbook.cpp
+++ doc/examples/addressbook-sdk/part4/addressbook.cpp
@@ -33,6 +33,7 @@ AddressBook::AddressBook(QWidget *parent)
     previousButton = ui->previousButton;
     previousButton->setEnabled(false);
 
+//! [extract objects]
     editButton = new QPushButton;
     editButton = ui->editButton;
     editButton->setEnabled(false);
@@ -40,6 +41,7 @@ AddressBook::AddressBook(QWidget *parent)
     removeButton = new QPushButton;
     removeButton = ui->removeButton;
     removeButton->setEnabled(false);
+//! [extract objects]
 
     connect(addButton, SIGNAL(clicked()), this,
                 SLOT(addContact()));
@@ -51,6 +53,12 @@ AddressBook::AddressBook(QWidget *parent)
                 SLOT(next()));
     connect(previousButton, SIGNAL(clicked()), this,
                 SLOT(previous()));
+//! [signal slot]
+    connect(editButton, SIGNAL(clicked()), this,
+                SLOT(editContact()));
+    connect(removeButton, SIGNAL(clicked()), this,
+                SLOT(removeContact()));
+//! [signal slot]
 
     setWindowTitle(tr("Simple Address Book"));
 }
@@ -79,43 +87,54 @@ void AddressBook::addContact()
     cancelButton->show();
 }
 
+//! [submitContact part1]
 void AddressBook::submitContact()
 {
+//! [submitContact part1]
     QString name = nameLine->text();
     QString address = addressText->toPlainText();
 
     if (name == "" || address == "") {
         QMessageBox::information(this, tr("Empty Field"),
             tr("Please enter a name and address."));
-        return;
     }
 
-    if (!contacts.contains(name)) {
-        contacts.insert(name, address);
-        QMessageBox::information(this, tr("Add Successful"),
-            tr("\"%1\" has been added to your address book.").arg(name));
-        return;
-    } else {
-        QMessageBox::information(this, tr("Add Unsuccessful"),
-            tr("Sorry, \"%1\" is already in your address book.").arg(name));
-        return;
+//! [submitContact part2]
+    if (currentMode == AddingMode) {
+
+        if (!contacts.contains(name)) {
+            contacts.insert(name, address);
+            QMessageBox::information(this, tr("Add Successful"),
+                tr("\"%1\" has been added to your address book.").arg(name));
+        } else {
+            QMessageBox::information(this, tr("Add Unsuccessful"),
+                tr("Sorry, \"%1\" is already in your address book.").arg(name));
+        }
+//! [submitContact part2]
+
+//! [submitContact part3]
+    } else if (currentMode == EditingMode) {
+
+        if (oldName != name) {
+            if (!contacts.contains(name)) {
+                QMessageBox::information(this, tr("Edit Successful"),
+                    tr("\"%1\" has been edited in your address book.").arg(oldName));
+                contacts.remove(oldName);
+                contacts.insert(name, address);
+            } else  {
+                QMessageBox::information(this, tr("Edit Unsuccessful"),
+                    tr("Sorry, \"%1\" is already in your address book.").arg(name));
+            }
+        } else if (oldAddress != address) {
+            QMessageBox::information(this, tr("Edit Successful"),
+                tr("\"%1\" has been edited in your address book.").arg(name));
+            contacts[name] = address;
+        }
+
+        updateInterface(NavigationMode);
     }
-
-    if (contacts.isEmpty()) {
-        nameLine->clear();
-        addressText->clear();
-    }
-
-    nameLine->setReadOnly(true);
-    addressText->setReadOnly(true);
-    addButton->setEnabled(true);
-
-    int number = contacts.size();
-    nextButton->setEnabled(number > 1);
-    previousButton->setEnabled(number > 1);
-    submitButton->hide();
-    cancelButton->hide();
 }
+//! [submitContact part3]
 
 void AddressBook::cancel()
 {
@@ -167,3 +186,88 @@ void AddressBook::previous()
     addressText->setText(i.value());
 }
 
+//! [editContact]
+void AddressBook::editContact()
+{
+    oldName = nameLine->text();
+    oldAddress = addressText->toPlainText();
+
+    updateInterface(EditingMode);
+}
+//! [editContact]
+
+//! [removeContact]
+void AddressBook::removeContact()
+{
+    QString name = nameLine->text();
+    QString address = addressText->toPlainText();
+
+    if (contacts.contains(name)) {
+        int button = QMessageBox::question(this,
+            tr("Confirm Remove"),
+            tr("Are you sure you want to remove \"%1\"?").arg(name),
+            QMessageBox::Yes | QMessageBox::No);
+
+        if (button == QMessageBox::Yes) {
+            previous();
+            contacts.remove(name);
+
+            QMessageBox::information(this, tr("Remove Successful"),
+                tr("\"%1\" has been removed from your address book.").arg(name));
+        }
+    }
+
+    updateInterface(NavigationMode);
+}
+//! [removeContact]
+
+//! [updateInterface part1]
+void AddressBook::updateInterface(Mode mode)
+{
+    currentMode = mode;
+
+    switch (currentMode) {
+
+    case AddingMode:
+    case EditingMode:
+
+        nameLine->setReadOnly(false);
+        nameLine->setFocus(Qt::OtherFocusReason);
+        addressTExt->setReadOnly(false);
+
+        addButton->setEnabled(false);
+        editButton->setEnabled(false);
+        removeButton->setEnabled(false);
+
+        nextButton->setEnabled(false);
+        previousButton->setEnabled(false);
+
+        submitButton->show();
+        cancelButton->show();
+        break;
+//! [updateInterface part1]
+
+//! [updateInterface part2]
+    case NavigationMode:
+
+        if (contacts.isEmpty()) {
+            nameLine->clear();
+            addressText->clear();
+        }
+
+        nameLine->setReadOnly(true);
+        addressText->setReadOnly(true);
+        addButton->setEnabled(true);
+
+        int number = contacts.size();
+        editButton->setEnabled(number >= 1);
+        removeButton->setEnabled(number >= 1);
+        nextButton->setEnabled(number > 1);
+        previousButton->setEnabled(number >1);
+
+        submitButton->hide();
+        cancelButton->hide();
+        break;
+    }
+}
+//! [updateInterface part2]
