90b6d1f598fa04de29fec8b3e5c6643d320e78e9
Daniel Molkentin <daniel.molkentin@nokia.com>
2009-06-10 17:50:24 +0200

    German translation: Werkzeuge -> Extras to conform to 'standards'
    Reviewed-By: con

--- share/qtcreator/translations/qtcreator_de.ts
+++ share/qtcreator/translations/qtcreator_de.ts
@@ -1359,7 +1359,7 @@ Sollen sie überschrieben werden?</translation>
     <message>
         <location line="+11"/>
         <source>&amp;Tools</source>
-        <translation>&amp;Werkzeuge</translation>
+        <translation>E&amp;xtras</translation>
     </message>
     <message>
         <location line="+5"/>
@@ -10283,7 +10283,7 @@ p, li { white-space: pre-wrap; }
 &lt;font color=&quot;#ff0000&quot;&gt;&lt;b&gt;No valid Qt version set. Set one in Tools/Options &lt;/b&gt;&lt;/font&gt;
 </source>
         <translation>
-&lt;font color=&quot;#ff0000&quot;&gt;&lt;b&gt;Es ist keine gültige Qt-Version gesetzt. Setzen Sie sie unter Werkzeuge/Einstellungen&lt;/b&gt;&lt;/font&gt;
+&lt;font color=&quot;#ff0000&quot;&gt;&lt;b&gt;Es ist keine gültige Qt-Version gesetzt. Setzen Sie sie unter Extras/Einstellungen&lt;/b&gt;&lt;/font&gt;
 </translation>
     </message>
     <message>
