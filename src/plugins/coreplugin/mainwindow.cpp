02a157f528077d774eb82a1f9adbe1caedd3125e
Norwegian Rock Cat <qt-info@nokia.com>
2009-07-22 11:02:49 +0200

    Kill the slash.
    If we are going to be uncertain, then let's try to
    be somewhat better stylistically about it.

--- src/plugins/coreplugin/mainwindow.cpp
+++ src/plugins/coreplugin/mainwindow.cpp
@@ -557,14 +557,14 @@ void MainWindow::registerDefaultActions()
     connect(m_focusToEditor, SIGNAL(activated()), this, SLOT(setFocusToEditor()));
 
     // New File Action
-    m_newAction = new QAction(QIcon(Constants::ICON_NEWFILE), tr("&New File/Project..."), this);
+    m_newAction = new QAction(QIcon(Constants::ICON_NEWFILE), tr("&New File or Project..."), this);
     cmd = am->registerAction(m_newAction, Constants::NEW, m_globalContext);
     cmd->setDefaultKeySequence(QKeySequence::New);
     mfile->addAction(cmd, Constants::G_FILE_NEW);
     connect(m_newAction, SIGNAL(triggered()), this, SLOT(newFile()));
 
     // Open Action
-    m_openAction = new QAction(QIcon(Constants::ICON_OPENFILE), tr("&Open File/Project..."), this);
+    m_openAction = new QAction(QIcon(Constants::ICON_OPENFILE), tr("&Open File or Project..."), this);
     cmd = am->registerAction(m_openAction, Constants::OPEN, m_globalContext);
     cmd->setDefaultKeySequence(QKeySequence::Open);
     mfile->addAction(cmd, Constants::G_FILE_OPEN);
