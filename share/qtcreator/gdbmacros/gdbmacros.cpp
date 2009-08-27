b901b5013d66212535de5fec88387e9a6f60bad6
hjk <qtc-committer@nokia.com>
2009-08-27 18:21:22 +0200

    debugger: fix type field in QHash dumper

--- share/qtcreator/gdbmacros/gdbmacros.cpp
+++ share/qtcreator/gdbmacros/gdbmacros.cpp
@@ -1464,8 +1464,8 @@ static void qDumpQHash(QDumper &d)
                 } else {
                     d.putItem("addr", node);
                     d.beginItem("type");
-                        d.put("'"NS"QHashNode<").put(keyType).put(",")
-                            .put(valueType).put(MAP_NODE_TYPE_END"'");
+                        d.put(NS"QHashNode<").put(keyType).put(",")
+                            .put(valueType).put(MAP_NODE_TYPE_END);
                     d.endItem();
                 }
             d.endHash();
