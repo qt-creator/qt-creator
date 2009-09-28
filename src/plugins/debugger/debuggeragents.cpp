qt-creator/qt-creator

commit 37c98f89d6ee27d014f3d6ac29ccc0879f7ee856

debugger: fix crash in memory view when data arrives after view has been closed

src/plugins/debugger/debuggeragents.cpp
@@ -81,7 +81,8 @@ MemoryViewAgent::MemoryViewAgent(DebuggerManager *manager, const QString &addr)
 
 MemoryViewAgent::~MemoryViewAgent()
 {
-    m_editor->deleteLater();
+    if (m_editor)
+        m_editor->deleteLater();
 }
 
 void MemoryViewAgent::init(quint64 addr)
@@ -101,13 +102,15 @@ void MemoryViewAgent::init(quint64 addr)
 void MemoryViewAgent::fetchLazyData(quint64 block, bool sync)
 {
     Q_UNUSED(sync); // FIXME: needed support for incremental searching
-    m_engine->fetchMemory(this, BinBlockSize * block, BinBlockSize);
+    if (m_engine)
+        m_engine->fetchMemory(this, BinBlockSize * block, BinBlockSize);
 }
 
 void MemoryViewAgent::addLazyData(quint64 addr, const QByteArray &ba)
 {
-    QMetaObject::invokeMethod(m_editor->widget(), "addLazyData",
-        Q_ARG(quint64, addr / BinBlockSize), Q_ARG(QByteArray, ba));
+    if (m_editor && m_editor->widget())
+        QMetaObject::invokeMethod(m_editor->widget(), "addLazyData",
+            Q_ARG(quint64, addr / BinBlockSize), Q_ARG(QByteArray, ba));
 }
 
 
@@ -205,6 +208,9 @@ void DisassemblerViewAgent::setContents(const QString &contents)
 {
     using namespace Core;
     using namespace TextEditor;
+
+    if (!d->editor)
+        return;
 
     QPlainTextEdit *plainTextEdit = 0;
     EditorManager *editorManager = EditorManager::instance();

commit 0155f11cd8d9148b32efdc14d269b19c52c072dd

debugger: fix regression in disassembler listing introduced in 37c98f89

src/plugins/debugger/debuggeragents.cpp
@@ -194,6 +194,7 @@ DisassemblerViewAgent::~DisassemblerViewAgent()
     if (d->editor)
         d->editor->deleteLater();
     delete d;
+    d = 0;
 }
 
 void DisassemblerViewAgent::setFrame(const StackFrame &frame)
@@ -207,11 +208,9 @@ void DisassemblerViewAgent::setFrame(const StackFrame &frame)
 
 void DisassemblerViewAgent::setContents(const QString &contents)
 {
+    QTC_ASSERT(d, return);
     using namespace Core;
     using namespace TextEditor;
-
-    if (!d->editor)
-        return;
 
     QPlainTextEdit *plainTextEdit = 0;
     EditorManager *editorManager = EditorManager::instance();

src/plugins/debugger/gdb/gdbengine.cpp
@@ -3827,7 +3827,7 @@ struct MemoryAgentCookie
     MemoryAgentCookie(MemoryViewAgent *agent_, quint64 address_)
         : agent(agent_), address(address_)
     {}
-    MemoryViewAgent *agent;
+    QPointer<MemoryViewAgent> agent;
     quint64 address;
 };
 
@@ -3870,7 +3870,7 @@ struct DisassemblerAgentCookie
     DisassemblerAgentCookie(DisassemblerViewAgent *agent_)
         : agent(agent_)
     {}
-    DisassemblerViewAgent *agent;
+    QPointer<DisassemblerViewAgent> agent;
 };
 
 void GdbEngine::fetchDisassembler(DisassemblerViewAgent *agent,