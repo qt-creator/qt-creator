3407aab9a590d8b88a87b429fafc922fa0cc3a4d
hjk <qtc-committer@nokia.com>
2009-10-02 10:02:34 +0200

    debugger: keep track of library loads as reported by trk

--- src/plugins/debugger/gdb/trkgdbadapter.cpp
+++ src/plugins/debugger/gdb/trkgdbadapter.cpp
@@ -724,6 +724,17 @@ void TrkGdbAdapter::handleGdbServerCommand(const QByteArray &cmd)
         sendGdbServerMessage("l<target><architecture>symbianelf</architecture></target>");
     }
 
+    else if (cmd.startsWith("qXfer:libraries:read")) {
+        sendGdbServerAck();
+        /*
+            <library-list>
+              <library name="/lib/libc.so.6">
+                <segment address="0x10000000"/>
+              </library>
+            </library-list>
+i        */
+    }
+
     else if (cmd == "QStartNoAckMode") {
         //$qSupported#37
         //logMessage("Handling 'QStartNoAckMode'");
@@ -952,9 +963,14 @@ void TrkGdbAdapter::handleTrkResult(const TrkResult &result)
             str << " CODE: " << hexxNumber(codeseg);
             str << " DATA: " << hexxNumber(dataseg);
             str << " NAME: '" << name << '\'';
+            Library lib;
+            lib.name = name;
+            lib.codeseg = codeseg;
+            lib.dataseg = dataseg;
+            m_session.libraries.append(lib);
             logMessage(logMsg);
             // This lets gdb trigger a register update etc
-            //sendGdbServerMessage("T05library:r;");
+            sendGdbServerMessage("T05library:;");
             sendTrkMessage(0x18, TrkCallback(), trkContinueMessage(), "CONTINUE");
             break;
         }
