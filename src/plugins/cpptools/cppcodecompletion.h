996866814aee66a0cc7a5f61af9306630bf96232
Roberto Raggi <roberto.raggi@nokia.com>
2009-10-21 16:51:54 +0200

    Initialize all the members of CppCodeCompletion

--- src/plugins/cpptools/cppcodecompletion.h
+++ src/plugins/cpptools/cppcodecompletion.h
@@ -146,25 +146,22 @@ private:
 
     int findStartOfName(int pos = -1) const;
 
-    QList<TextEditor::CompletionItem> m_completions;
-
+private:
+    CppModelManager *m_manager;
     TextEditor::ITextEditable *m_editor;
     int m_startPosition;     // Position of the cursor from which completion started
 
-    CppModelManager *m_manager;
     Qt::CaseSensitivity m_caseSensitivity;
     bool m_autoInsertBrackets;
     bool m_partialCompletionEnabled;
-
     bool m_forcedCompletion;
+    unsigned m_completionOperator;
 
     CPlusPlus::Icons m_icons;
     CPlusPlus::Overview overview;
     CPlusPlus::TypeOfExpression typeOfExpression;
-
-    unsigned m_completionOperator;
-
     QPointer<FunctionArgumentWidget> m_functionArgumentWidget;
+    QList<TextEditor::CompletionItem> m_completions;
 };
 
 } // namespace Internal
