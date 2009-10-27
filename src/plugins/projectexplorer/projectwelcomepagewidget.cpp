a461c6e1e110f8868edd1a3478a7b6e30982cb45
Oswald Buddenhagen <oswald.buddenhagen@nokia.com>
2009-10-27 19:52:17 +0100

    reflect current session in the welcome page's list

--- src/plugins/projectexplorer/projectwelcomepagewidget.cpp
+++ src/plugins/projectexplorer/projectwelcomepagewidget.cpp
@@ -106,8 +106,13 @@ void ProjectWelcomePageWidget::updateWelcomePage(const WelcomePageData &welcomeP
     if (welcomePageData.sessionList.count() > 0) {
         foreach (const QString &s, welcomePageData.sessionList) {
             QString str = s;
-            if (s == welcomePageData.previousSession)
-                str = tr("%1 (last session)").arg(s);
+            if (welcomePageData.activeSession.isEmpty()) {
+                if (s == welcomePageData.previousSession)
+                    str = tr("%1 (last session)").arg(s);
+            } else {
+                if (s == welcomePageData.activeSession)
+                    str = tr("%1 (current session)").arg(s);
+            }
             ui->sessTreeWidget->addItem(str, s);
         }
         ui->sessTreeWidget->updateGeometry();
