00298ab24823a8cf922b693492929eac4ea71180
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2009-11-04 15:19:02 +0100

    WinGuiProcess: Fix hang if binary cannot be found.
    ..occurring in conjunction with custom run configuration
    and S60 emulator. Ensure all handles are closed even
    if start attempt failed.
    Reviewed-by: con <qtc-committer@nokia.com>

--- src/plugins/projectexplorer/winguiprocess.h
+++ src/plugins/projectexplorer/winguiprocess.h
@@ -42,6 +42,9 @@ using namespace Utils;
 namespace ProjectExplorer {
 namespace Internal {
 
+/* Captures the debug output of a Windows GUI application (which
+ * would otherwise not be visible) using the debug interface and
+ * emits via a signal. */
 class WinGuiProcess : public QThread, public AbstractProcess
 {
     Q_OBJECT
