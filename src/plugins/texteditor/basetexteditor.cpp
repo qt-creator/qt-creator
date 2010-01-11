ee53ebbf08bc285bc2861ee7b1724c9e2304790f
mae <qt-info@nokia.com>
2010-01-11 16:17:22 +0100

    Fix inserting printable text with AltGt
    Task: QTCREATORBUG-543
    Reviewed-by: Tobias Hunger

--- src/plugins/texteditor/basetexteditor.cpp
+++ src/plugins/texteditor/basetexteditor.cpp
@@ -1173,7 +1173,7 @@ void BaseTextEditor::keyPressEvent(QKeyEvent *e)
 
     if (ro || e->text().isEmpty() || !e->text().at(0).isPrint()) {
         QPlainTextEdit::keyPressEvent(e);
-    } else if ((e->modifiers() & Qt::ControlModifier) != Qt::ControlModifier){
+    } else if ((e->modifiers() & (Qt::ControlModifier|Qt::AltModifier)) != Qt::ControlModifier){
         QTextCursor cursor = textCursor();
         QString text = e->text();
         QString autoText = autoComplete(cursor, text);
