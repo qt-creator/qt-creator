1f3720a14de65303b57f884d5dc44f8aa9e76618
con <qtc-committer@nokia.com>
2010-01-13 11:02:10 +0100

    Fixes: Not able to set breakpoints in running application.
    Regression introduced by the gdb startup speedup.
    
    Reviewed-by: hjk
    (cherry picked from commit 232db42069cc6da3c8e89d0a37c155ee43888a27)

--- src/plugins/debugger/gdb/gdbengine.cpp
+++ src/plugins/debugger/gdb/gdbengine.cpp
@@ -1948,7 +1948,7 @@ void GdbEngine::sendInsertBreakpoint(int index)
 void GdbEngine::reloadBreakListInternal()
 {
     m_breakListUpdating = true; 
-    postCommand(_("-break-list"), CB(handleBreakList));
+    postCommand(_("-break-list"), NeedsStop, CB(handleBreakList));
 }
 
 void GdbEngine::handleBreakList(const GdbResponse &response)
