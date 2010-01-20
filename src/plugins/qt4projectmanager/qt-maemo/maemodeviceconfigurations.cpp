ed5a0fb0b2176ec11f0dea74c4ac38bca4c0c547
ck <qt-info@nokia.com>
2010-01-20 12:13:08 +0100

    Maemo: Remove special case for "developer" user.
    This user now also resides under /home on the device.

--- src/plugins/qt4projectmanager/qt-maemo/maemodeviceconfigurations.cpp
+++ src/plugins/qt4projectmanager/qt-maemo/maemodeviceconfigurations.cpp
@@ -49,9 +49,7 @@ QString homeDirOnDevice(const QString &uname)
 {
     const QString &dir = uname == QLatin1String("root")
         ? QLatin1String("/root")
-            : uname == QLatin1String("developer")
-            ? QLatin1String("/var/local/mad-developer-home")
-                : QLatin1String("/home/") + uname;
+        : QLatin1String("/home/") + uname;
     qDebug("%s: user name %s is mapped to home dir %s",
            Q_FUNC_INFO, qPrintable(uname), qPrintable(dir));
     return dir;
