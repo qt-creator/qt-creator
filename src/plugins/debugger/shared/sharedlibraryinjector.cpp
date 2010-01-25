b37dededbc866ef6aa1f9ebae4a024c7da05e467
Kai Koehne <kai.koehne@nokia.com>
2010-01-25 14:59:19 +0100

    Fix duplicated copyright notice

--- src/plugins/debugger/shared/sharedlibraryinjector.cpp
+++ src/plugins/debugger/shared/sharedlibraryinjector.cpp
@@ -2,7 +2,7 @@
 **
 ** This file is part of Qt Creator
 **
-** Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).
+** Copyright (c) 2010 Marius Storm-Olsen
 **
 ** Contact: Nokia Corporation (qt-info@nokia.com)
 **
@@ -26,30 +26,6 @@
 ** contact the sales department at http://qt.nokia.com/contact.
 **
 **************************************************************************/
-
-//
-// WinANSI - An ANSI implementation for the modern Windows
-//
-// Copyright (C) 2008- Marius Storm-Olsen <mstormo@gmail.com>
-//
-// Based on work by Robert Kuster in article
-//   http://software.rkuster.com/articles/winspy.htm
-//
-//  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU General Public License as published by
-//  the Free Software Foundation; either version 2 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-//
-// -------------------------------------------------------------------------------------------------
 
 #include "sharedlibraryinjector.h"
 #include <utils/winutils.h>
