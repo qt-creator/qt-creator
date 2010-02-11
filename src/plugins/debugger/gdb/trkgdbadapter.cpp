620fcd9199908df2b1e54de2e83847b37c8edf0b
hjk <qtc-committer@nokia.com>
2010-02-11 13:11:56 +0100

    debugger: comment out remote log file

--- src/plugins/debugger/gdb/trkgdbadapter.cpp
+++ src/plugins/debugger/gdb/trkgdbadapter.cpp
@@ -1868,7 +1868,7 @@ void TrkGdbAdapter::handleCreateProcess(const TrkResult &result)
     m_engine->postCommand("set breakpoint auto-hw on");
     m_engine->postCommand("set trust-readonly-sections on"); // No difference?
     m_engine->postCommand("set displaced-stepping on"); // No difference?
-    m_engine->postCommand("set remotelogfile /tmp/gdb-remotelog");
+    //m_engine->postCommand("set remotelogfile /tmp/gdb-remotelog");
     m_engine->postCommand("set mem inaccessible-by-default");
     m_engine->postCommand("mem 0x00400000 0x70000000 cache");
     m_engine->postCommand("mem 0x70000000 0x80000000 cache ro");
