d7a403f31dbe35ef9bba09e802b165dd946a5cfc
Thomas Hartmann <Thomas.Hartmann@nokia.com>
2010-03-18 10:18:14 +0100

    QmlDesigner.propertyEditor: mouseRelease instead of mousePress

--- src/plugins/qmldesigner/components/propertyeditor/propertyeditor.h
+++ src/plugins/qmldesigner/components/propertyeditor/propertyeditor.h
@@ -175,7 +175,7 @@ signals:
 protected:
     void paintEvent(QPaintEvent *event);
 
-    void mousePressEvent(QMouseEvent *)
+    void mouseReleaseEvent(QMouseEvent *)
     {
         if (m_collapsed) {
             m_collapsed = false;
