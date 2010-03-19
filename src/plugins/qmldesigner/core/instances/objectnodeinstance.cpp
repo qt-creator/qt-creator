qt-creator/qt-creator

commit ed31fdd2ae23a1836f74847482ed5813137e6e3b

Fix compilation against latest qt/master
Adapt to latest QtDeclarative API changes.

src/plugins/qmldesigner/core/instances/objectnodeinstance.cpp
@@ -57,6 +57,8 @@
 #include <QSharedPointer>
 
 #include <private/qdeclarativebinding_p.h>
+#include <private/qdeclarativemetatype_p.h>
+#include <private/qdeclarativevaluetype_p.h>
 
 namespace QmlDesigner {
 namespace Internal {
@@ -406,7 +408,7 @@ void ObjectNodeInstance::setPropertyBinding(const QString &name, const QString &
         binding->setTarget(metaProperty);
         binding->setNotifyOnValueChanged(true);
         QDeclarativeAbstractBinding *oldBinding = QDeclarativePropertyPrivate::setBinding(metaProperty, binding);
-        delete oldBinding;
+        oldBinding->destroy();
         binding->update();
     } else {
         qWarning() << "Cannot set binding for property" << name << ": property is unknown for type"
@@ -458,7 +460,7 @@ void ObjectNodeInstance::resetProperty(QObject *object, const QString &propertyN
     QDeclarativeAbstractBinding *binding = QDeclarativePropertyPrivate::binding(qmlProperty);
     if (binding) {
         binding->setEnabled(false, 0);
-        delete binding;
+        binding->destroy();
     }
 
     if (metaProperty.isResettable()) {
@@ -687,7 +689,7 @@ void ObjectNodeInstance::createDynamicProperty(const QString &name, const QStrin
 void ObjectNodeInstance::refreshBindings(QDeclarativeContext *context)
 {
     // TODO: Maybe do this via a timer to prevent update flooding
-    QDeclarativeContextPrivate::get(context)->refreshExpressions();
+    QDeclarativeContextPrivate::get(context)->data->refreshExpressions();
 }
 
 }

src/plugins/qmldesigner/components/propertyeditor/basicwidgets.cpp
@@ -976,7 +976,7 @@ void WidgetLoader::setSource(const QUrl &source)
             }
 
             QDeclarativeContext *ctxt = new QDeclarativeContext(qmlContext(this));
-	    ctxt->addDefaultObject(this);
+            ctxt->setContextObject(this);
             QObject *obj = m_component->create(ctxt);
             if (obj) {
                 QWidget *widget = qobject_cast<QWidget *>(obj);

src/plugins/qmldesigner/core/instances/nodeinstancesignalspy.cpp
@@ -1,12 +1,11 @@
 #include "nodeinstancesignalspy.h"
-
+#include "objectnodeinstance.h"
 
 #include <QMetaProperty>
 #include <QMetaObject>
 #include <QtDebug>
-
-#include "objectnodeinstance.h"
 #include <QSharedPointer>
+#include <private/qdeclarativemetatype_p.h>
 
 namespace QmlDesigner {
 namespace Internal {

src/plugins/qmldesigner/core/instances/nodeinstanceview.cpp
@@ -31,6 +31,7 @@
 
 #include <QDeclarativeEngine>
 #include <QDeclarativeContext>
+#include <private/qdeclarativeengine_p.h>
 
 #include <QtDebug>
 #include <QUrl>

src/plugins/qmldesigner/core/instances/proxywidgetnodeinstance.cpp
@@ -30,6 +30,7 @@
 #include "proxywidgetnodeinstance.h"
 
 #include <QGraphicsProxyWidget>
+#include <private/qdeclarativemetatype_p.h>
 
 namespace QmlDesigner {
 namespace Internal {

src/plugins/qmldesigner/core/instances/qmlgraphicsitemnodeinstance.cpp
@@ -42,6 +42,7 @@
 
 #include <private/qdeclarativeanchors_p.h>
 #include <private/qdeclarativeanchors_p_p.h>
+#include <private/qdeclarativeproperty_p.h>
 #include <private/qdeclarativerectangle_p.h>
 
 #include <cmath>

commit bffeb830f5e9a5bccb0fcf1074ac4535a8ff1529

QmlDesigner: Fix crash when changing a binding
Regression introduced by ed31fdd2ae23a1836f74847482ed5813137e6e3b

src/plugins/qmldesigner/core/instances/objectnodeinstance.cpp
@@ -408,7 +408,8 @@ void ObjectNodeInstance::setPropertyBinding(const QString &name, const QString &
         binding->setTarget(metaProperty);
         binding->setNotifyOnValueChanged(true);
         QDeclarativeAbstractBinding *oldBinding = QDeclarativePropertyPrivate::setBinding(metaProperty, binding);
-        oldBinding->destroy();
+        if (oldBinding)
+            oldBinding->destroy();
         binding->update();
     } else {
         qWarning() << "Cannot set binding for property" << name << ": property is unknown for type"