1eceed92f1bd33904963fc812860e4642f5dede3
Thorbjørn Lindeijer <thorbjorn.lindeijer@nokia.com>
2010-03-23 17:21:17 +0100

    Remove special showMaximized case for Maemo 5
    The Maemo 5 window manager already makes sure applications always run
    maximized.
    
    Reviewed-by: Robert Griebl

--- share/qtcreator/templates/qt4project/main.cpp
+++ share/qtcreator/templates/qt4project/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     %CLASS% w;
-#if defined(Q_WS_S60) || defined(Q_WS_MAEMO_5)
+#if defined(Q_WS_S60)
     w.showMaximized();
 #else
     w.show();
