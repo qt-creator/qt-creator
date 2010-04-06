639ae491c10e11321426999ed9b1a34f0a9d781b
Kai Koehne <kai.koehne@nokia.com>
2010-04-06 10:34:05 +0200

    QmlDesigner: Store result of bauhaustests in $CREATORDIR/../bauhaustests

--- tests/auto/qml/qmldesigner/bauhaustests/run_test.pl
+++ tests/auto/qml/qmldesigner/bauhaustests/run_test.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 ($second, $minute, $hour, $dayOfMonth, $month, $yearOffset, $dayOfWeek, $dayOfYear, $daylightSavings) = localtime();
-$filename = "$yearOffset-$month-$dayOfMonth.log";
+$filename = "../../../../../../bauhaustests/$yearOffset-$month-$dayOfMonth.log";
 
 system("git rev-parse HEAD >$filename");
-system("./tst_bauhaus >$filename");
+system("./tst_bauhaus >>$filename");
