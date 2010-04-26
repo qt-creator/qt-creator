qt-creator/qt-creator

commit 5923733889075800a0979c73476bf3bbae204e89

VCS: Speed up
Do not listen on ICore::contextChanged to update actions
(became slow after additional contexts).
git: Make code to find top level faster, avoid
constructing file info lists.
Acked-by: con

src/plugins/vcsbase/vcsbaseplugin.cpp
@@ -173,8 +173,6 @@ StateListener::StateListener(QObject *parent) :
         QObject(parent)
 {
     Core::ICore *core = Core::ICore::instance();
-    connect(core, SIGNAL(contextChanged(Core::IContext*,QList<int>)),
-            this, SLOT(slotStateChanged()));
     connect(core->fileManager(), SIGNAL(currentFileChanged(QString)),
             this, SLOT(slotStateChanged()));
 

src/plugins/git/gitclient.cpp
@@ -153,28 +153,14 @@ GitClient::~GitClient()
 
 const char *GitClient::noColorOption = "--no-color";
 
-QString GitClient::findRepositoryForFile(const QString &fileName)
-{
-    const QString gitDirectory = QLatin1String(kGitDirectoryC);
-    const QFileInfo info(fileName);
-    QDir dir = info.absoluteDir();
-    do {
-        if (dir.entryList(QDir::AllDirs|QDir::Hidden).contains(gitDirectory))
-            return dir.absolutePath();
-    } while (dir.cdUp());
-
-    return QString();
-}
-
 QString GitClient::findRepositoryForDirectory(const QString &dir)
 {
     const QString gitDirectory = QLatin1String(kGitDirectoryC);
     QDir directory(dir);
     do {
-        if (directory.entryList(QDir::AllDirs|QDir::Hidden).contains(gitDirectory))
+        if (QFileInfo(directory, gitDirectory).isDir())
             return directory.absolutePath();
     } while (directory.cdUp());
-
     return QString();
 }
 

src/plugins/git/gitclient.h
@@ -75,7 +75,6 @@ public:
     bool managesDirectory(const QString &) const { return false; }
     QString findTopLevelForDirectory(const QString &) const { return QString(); }
 
-    static QString findRepositoryForFile(const QString &fileName);
     static QString findRepositoryForDirectory(const QString &dir);
 
     void diff(const QString &workingDirectory, const QStringList &diffArgs, const QString &fileName);

commit aa0926901b1070c5f636e3fb11dea9d1660657d7

VCS: Initially enable menus.
Fix breakage introduced by:
5923733889075800a0979c73476bf3bbae204e89

src/plugins/cvs/cvsplugin.cpp
@@ -398,10 +398,6 @@ bool CVSPlugin::initialize(const QStringList & /*arguments */, QString *errorMes
     return true;
 }
 
-void CVSPlugin::extensionsInitialized()
-{
-}
-
 bool CVSPlugin::submitEditorAboutToClose(VCSBase::VCSBaseSubmitEditor *submitEditor)
 {
     if (!isCommitEditorOpen())

src/plugins/cvs/cvsplugin.h
@@ -84,7 +84,6 @@ public:
     ~CVSPlugin();
 
     virtual bool initialize(const QStringList &arguments, QString *error_message);
-    virtual void extensionsInitialized();
 
     void cvsDiff(const QString &workingDir, const QStringList &files);
 

src/plugins/git/gitplugin.cpp
@@ -507,10 +507,6 @@ bool GitPlugin::initialize(const QStringList &arguments, QString *errorMessage)
     return true;
 }
 
-void GitPlugin::extensionsInitialized()
-{
-}
-
 GitVersionControl *GitPlugin::gitVersionControl() const
 {
     return static_cast<GitVersionControl *>(versionControl());

src/plugins/git/gitplugin.h
@@ -91,7 +91,6 @@ public:
     static GitPlugin *instance();
 
     virtual bool initialize(const QStringList &arguments, QString *error_message);
-    virtual void extensionsInitialized();
 
     GitVersionControl *gitVersionControl() const;
 

src/plugins/mercurial/mercurialplugin.cpp
@@ -188,10 +188,6 @@ bool MercurialPlugin::initialize(const QStringList & /* arguments */, QString * 
     return true;
 }
 
-void MercurialPlugin::extensionsInitialized()
-{
-}
-
 const MercurialSettings &MercurialPlugin::settings() const
 {
     return mercurialSettings;

src/plugins/mercurial/mercurialplugin.h
@@ -81,7 +81,7 @@ public:
     MercurialPlugin();
     virtual ~MercurialPlugin();
     bool initialize(const QStringList &arguments, QString *error_message);
-    void extensionsInitialized();
+
     static MercurialPlugin *instance() { return m_instance; }
     MercurialClient *client() const { return m_client; }
 

src/plugins/perforce/perforceplugin.cpp
@@ -456,6 +456,7 @@ bool PerforcePlugin::initialize(const QStringList & /* arguments */, QString * e
 
 void PerforcePlugin::extensionsInitialized()
 {
+    VCSBase::VCSBasePlugin::extensionsInitialized();
     getTopLevel();
 }
 

src/plugins/subversion/subversionplugin.cpp
@@ -440,10 +440,6 @@ bool SubversionPlugin::initialize(const QStringList & /*arguments */, QString *e
     return true;
 }
 
-void SubversionPlugin::extensionsInitialized()
-{
-}
-
 bool SubversionPlugin::submitEditorAboutToClose(VCSBase::VCSBaseSubmitEditor *submitEditor)
 {
     if (!isCommitEditorOpen())

src/plugins/subversion/subversionplugin.h
@@ -81,7 +81,6 @@ public:
     ~SubversionPlugin();
 
     bool initialize(const QStringList &arguments, QString *error_message);
-    void extensionsInitialized();
 
     void svnDiff(const QString  &workingDir, const QStringList &files, QString diffname = QString());
 

src/plugins/vcsbase/vcsbaseplugin.cpp
@@ -165,7 +165,7 @@ public:
 signals:
     void stateChanged(const VCSBase::Internal::State &s, Core::IVersionControl *vc);
 
-private slots:
+public slots:
     void slotStateChanged();
 };
 
@@ -463,6 +463,12 @@ void VCSBasePlugin::initialize(Core::IVersionControl *vc)
             SIGNAL(stateChanged(VCSBase::Internal::State, Core::IVersionControl*)),
             this,
             SLOT(slotStateChanged(VCSBase::Internal::State,Core::IVersionControl*)));
+}
+
+void VCSBasePlugin::extensionsInitialized()
+{
+    // Initialize enable menus.
+    VCSBasePluginPrivate::m_listener->slotStateChanged();
 }
 
 void VCSBasePlugin::slotSubmitEditorAboutToClose(VCSBaseSubmitEditor *submitEditor, bool *result)

src/plugins/vcsbase/vcsbaseplugin.h
@@ -151,7 +151,8 @@ class VCSBASE_EXPORT VCSBasePlugin : public ExtensionSystem::IPlugin
 protected:
     explicit VCSBasePlugin(const QString &submitEditorId);
 
-    void initialize(Core::IVersionControl *vc);
+    virtual void initialize(Core::IVersionControl *vc);
+    virtual void extensionsInitialized();
 
 public:
     virtual ~VCSBasePlugin();