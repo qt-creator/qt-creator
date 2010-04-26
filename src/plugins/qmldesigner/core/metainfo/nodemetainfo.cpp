75d1fa5d93721a6370631e78e043afef77ce5bdd
Marco Bubke <marco.bubke@nokia.com>
2010-04-26 13:52:00 +0200

    Set the rootObject of a file component as the parent of the context of the file component
    Reviewed-by: kkoehne

--- src/plugins/qmldesigner/core/metainfo/nodemetainfo.cpp
+++ src/plugins/qmldesigner/core/metainfo/nodemetainfo.cpp
@@ -176,7 +176,9 @@ QObject *NodeMetaInfo::createInstance(QDeclarativeContext *context) const
         // qml component
         // TODO: This is maybe expensive ...
         QDeclarativeComponent component(context->engine(), QUrl::fromLocalFile(m_data->qmlFile));
-        object = component.create(new QDeclarativeContext(context));
+        QDeclarativeContext *newContext =  new QDeclarativeContext(context);
+        object = component.create(newContext);
+        newContext->setParent(object);
     } else {
         // primitive
         object = QDeclarativeMetaType::qmlType(typeName().toAscii(), minorVersion(), majorVersion())->create();
