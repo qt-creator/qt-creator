441db7c946a2bb4e3d6c52cee94ed5887c1e57ec
Thomas Hartmann <Thomas.Hartmann@nokia.com>
2010-04-29 15:23:27 +0200

    QmlDesigner.propertyEditor: initialize members

--- src/plugins/qmldesigner/components/propertyeditor/propertyeditorcontextobject.cpp
+++ src/plugins/qmldesigner/components/propertyeditor/propertyeditorcontextobject.cpp
@@ -3,7 +3,7 @@
 namespace QmlDesigner {
 
 PropertyEditorContextObject::PropertyEditorContextObject(QObject *parent) :
-    QObject(parent), m_backendValues(0)
+    QObject(parent), m_backendValues(0), m_isBaseState(false), m_selectionChanged(false)
 {
 
 }
