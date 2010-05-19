qt-creator/qt-creator

commit 8c9a97479a58447f5f94526390d14641181ed2dd

Small fixes for vertical block selection
The find scope was sometimes painted incorrectly, and the last
line was occassionally ommited from the standard selection paint.

Reviewed-by: thorbjorn

src/plugins/texteditor/basetexteditor.cpp
@@ -2361,11 +2361,7 @@ void BaseTextEditor::paintEvent(QPaintEvent *e)
 
         TextEditorOverlay *overlay = new TextEditorOverlay(this);
         overlay->addOverlaySelection(d->m_findScopeStart.position(),
-                                     d->m_findScopeVerticalBlockSelection ?
-                                     d->m_findScopeEnd.block().position()
-                                     + d->m_findScopeVerticalBlockSelection
-                                     + d->m_findScopeStart.positionInBlock() + 1
-                                         : d->m_findScopeEnd.position(),
+                                     d->m_findScopeEnd.position(),
                                      d->m_searchScopeFormat.background().color().darker(120),
                                      d->m_searchScopeFormat.background().color(),
                                      TextEditorOverlay::ExpandBegin,
@@ -2470,8 +2466,9 @@ void BaseTextEditor::paintEvent(QPaintEvent *e)
                 const QAbstractTextDocumentLayout::Selection &range = context.selections.at(i);
                 const int selStart = range.cursor.selectionStart() - blpos;
                 const int selEnd = range.cursor.selectionEnd() - blpos;
-                if (selStart < bllen && selEnd > 0
-                    && selEnd > selStart) {
+                if (selStart <= bllen && selEnd >= 0
+                    && selEnd >= selStart
+                    && !range.format.hasProperty(QTextFormat::FullWidthSelection)) {
                     QTextLayout::FormatRange o;
                     o.start = selStart;
                     o.length = selEnd - selStart;

commit 04840b0ea7f0fe0b87f4589d378947a395e9c5d3

Fix text highlighting on Mac.
Broke with 8c9a97479a58447f5f94526390d14641181ed2dd

Reviewed-by: Thorbjørn Lindeijer

src/plugins/texteditor/basetexteditor.cpp
@@ -2467,8 +2467,7 @@ void BaseTextEditor::paintEvent(QPaintEvent *e)
                 const int selStart = range.cursor.selectionStart() - blpos;
                 const int selEnd = range.cursor.selectionEnd() - blpos;
                 if (selStart <= bllen && selEnd >= 0
-                    && selEnd >= selStart
-                    && !range.format.hasProperty(QTextFormat::FullWidthSelection)) {
+                    && selEnd >= selStart) {
                     QTextLayout::FormatRange o;
                     o.start = selStart;
                     o.length = selEnd - selStart;