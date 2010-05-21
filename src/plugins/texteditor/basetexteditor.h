53b8df81554c3a83d181de4c9934d4206dc776ff
Roberto Raggi <roberto.raggi@nokia.com>
2010-05-21 15:42:05 +0200

    Introduced a new kind of extra selection that we can use to mark undefined symbols.

--- src/plugins/texteditor/basetexteditor.h
+++ src/plugins/texteditor/basetexteditor.h
@@ -335,6 +335,7 @@ public:
         ParenthesesMatchingSelection,
         CodeWarningsSelection,
         CodeSemanticsSelection,
+        UndefinedSymbolSelection,
         UnusedSymbolSelection,
         FakeVimSelection,
         OtherSelection,
