661308c96e4d622fc20cb895a85786261e4f6275
hjk <qtc-committer@nokia.com>
2010-06-01 14:35:51 +0200

    debugger: always switch to frame 0 before trying to leave frame

--- src/plugins/debugger/gdb/gdbengine.cpp
+++ src/plugins/debugger/gdb/gdbengine.cpp
@@ -1851,6 +1851,7 @@ void GdbEngine::executeStepI()
 void GdbEngine::executeStepOut()
 {
     QTC_ASSERT(state() == InferiorStopped, qDebug() << state());
+    postCommand("-stack-select-frame 0");
     setTokenBarrier();
     setState(InferiorRunningRequested);
     showStatusMessage(tr("Finish function requested..."), 5000);
