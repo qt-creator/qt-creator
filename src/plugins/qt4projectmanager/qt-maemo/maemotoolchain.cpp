0017103863bc79f7f12eb60bb9e64812f9ffa22a
kh1 <qt-info@nokia.com>
2010-06-01 17:38:15 +0200

    Fix MADDE and pkg-config with QtCreator.
    We missed some environment vars that had to be set to make the script
    work. NOTE: still does not work on windows, since madbin\pkg-config.cmd
    needs to be too.
    
    Reviewed-by: kh

--- src/plugins/qt4projectmanager/qt-maemo/maemotoolchain.cpp
+++ src/plugins/qt4projectmanager/qt-maemo/maemotoolchain.cpp
@@ -60,9 +60,14 @@ void MaemoToolChain::addToEnvironment(ProjectExplorer::Environment &env)
         .arg(maddeRoot())));
     env.prependOrSetPath(QDir::toNativeSeparators(QString("%1/bin")
         .arg(targetRoot())));
-#ifdef Q_OS_WIN
+#if defined(Q_OS_WIN) || defined(Q_OS_MAC)
+    env.prependOrSet(QLatin1String("SYSROOT_DIR"), sysrootRoot());
     env.prependOrSetPath(QDir::toNativeSeparators(QString("%1/madbin")
         .arg(maddeRoot())));
+    env.prependOrSetPath(QDir::toNativeSeparators(QString("%1/madlib")
+        .arg(maddeRoot())));
+    env.prependOrSet(QLatin1String("PERL5LIB"),
+        QDir::toNativeSeparators(QString("%1/madlib/perl5").arg(maddeRoot())));
 #endif
 }
 
