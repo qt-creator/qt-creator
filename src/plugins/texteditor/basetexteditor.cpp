502e86b7373c24411e79cb684eb454530f45f5b5
Thorbjørn Lindeijer <thorbjorn.lindeijer@nokia.com>
2010-06-02 11:20:56 +0200

    Only do line selection in the gutter when the line numbers are visible
    While 2641496a997 removed the marks check entirely, a fix for the same
    issue made on the master branch (1263babfc6c96) changed it to check
    whether the line numbers are visible. This is more correct.
    
    Reviewed-by: mae

--- src/plugins/texteditor/basetexteditor.cpp
+++ src/plugins/texteditor/basetexteditor.cpp
@@ -3557,7 +3557,7 @@ void BaseTextEditor::extraAreaMouseEvent(QMouseEvent *e)
                     toggleBlockVisible(c);
                     d->moveCursorVisible(false);
                 }
-            } else if (e->pos().x() > markWidth) {
+            } else if (d->m_lineNumbersVisible && e->pos().x() > markWidth) {
                 QTextCursor selection = cursor;
                 selection.setVisualNavigation(true);
                 d->extraAreaSelectionAnchorBlockNumber = selection.blockNumber();
