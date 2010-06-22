3c915709c78f1cc0284e7176beec8145ba90de00
dt <qtc-committer@nokia.com>
2010-06-22 13:11:51 +0200

    Revert "Fix crash for newly added project nodes"
    Actually that's not correct, the projectNode() of ProjectNodes is set in
    the constructor to "this".
    This reverts commit 123e2b6cce37baaafe15a877c6e3aedb9d1d6e95.

--- src/plugins/projectexplorer/projectnodes.cpp
+++ src/plugins/projectexplorer/projectnodes.cpp
@@ -316,8 +316,6 @@ void ProjectNode::addProjectNodes(const QList<ProjectNode*> &subProjects)
             QTC_ASSERT(!project->parentFolderNode(),
                 qDebug("Project node has already a parent"));
             project->setParentFolderNode(this);
-            project->setProjectNode(this);
-
             foreach (NodesWatcher *watcher, m_watchers)
                 project->registerWatcher(watcher);
             m_subFolderNodes.append(project);
