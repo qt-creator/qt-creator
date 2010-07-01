9ab9f9d35ffa0362643e615dd01c54d4e9ff5ca7
con <qtc-committer@nokia.com>
2010-07-01 11:25:38 +0200

    Don't clear the filter widget's focus when clearing it.
    Often the reason for clicking the clear button is to type something into
    it afterwards.
    See also QTCREATORBUG-705

--- src/plugins/qmldesigner/components/itemlibrary/itemlibrary.cpp
+++ src/plugins/qmldesigner/components/itemlibrary/itemlibrary.cpp
@@ -185,7 +185,6 @@ ItemLibrary::ItemLibrary(QWidget *parent) :
     lineEditLayout->addWidget(m_d->m_lineEdit, 1, 1, 1, 1);
     lineEditLayout->addItem(new QSpacerItem(5, 5, QSizePolicy::Fixed, QSizePolicy::Fixed), 1, 2);
     connect(m_d->m_lineEdit, SIGNAL(filterChanged(QString)), this, SLOT(setSearchFilter(QString)));
-    connect(m_d->m_lineEdit, SIGNAL(buttonClicked()), this, SLOT(clearLineEditFocus()));
 
     m_d->m_stackedWidget = new QStackedWidget(this);
     m_d->m_stackedWidget->addWidget(m_d->m_itemsView);
@@ -284,11 +283,6 @@ void ItemLibrary::updateModel()
 void ItemLibrary::updateSearch()
 {
     setSearchFilter(m_d->m_lineEdit->text());
-}
-
-void ItemLibrary::clearLineEditFocus()
-{
-    m_d->m_lineEdit->clearFocus();
 }
 
 void ItemLibrary::setResourcePath(const QString &resourcePath)
