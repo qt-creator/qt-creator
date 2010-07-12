qt-creator/qt-creator

commit 9d803829ebf186dcf8c78205ac3582fb472996e9

CppEditor: Don't calculate current index twice

src/plugins/cppeditor/cppeditor.cpp
@@ -1095,11 +1095,11 @@ void CPPEditor::updateMethodBoxIndexNow()
     int line = 0, column = 0;
     convertPosition(position(), &line, &column);
 
-    QModelIndex m_overviewModelIndex = indexForPosition(line, column);
+    m_overviewModelIndex = indexForPosition(line, column);
     emit overviewModelIndexChanged(m_overviewModelIndex);
 
     // ComboBox only let's you select top level indexes!
-    QModelIndex comboIndex = indexForPosition(line, column);
+    QModelIndex comboIndex = m_overviewModelIndex;
     while (comboIndex.parent().isValid())
         comboIndex = comboIndex.parent();
 

commit ed38d62c9e42b6bcfaf217f754a9460cb6eeaadc

Fix crash in cpp text editor
Crashes were introduced in commit 9d803829ebf186dcf8c78205ac3582fb472996e9.
Reverting this, to have a second look.

src/plugins/cppeditor/cppeditor.cpp
@@ -1095,11 +1095,11 @@ void CPPEditor::updateMethodBoxIndexNow()
     int line = 0, column = 0;
     convertPosition(position(), &line, &column);
 
-    m_overviewModelIndex = indexForPosition(line, column);
-    emit overviewModelIndexChanged(m_overviewModelIndex);
+    QModelIndex overviewModelIndex = indexForPosition(line, column);
+    emit overviewModelIndexChanged(overviewModelIndex);
 
     // ComboBox only let's you select top level indexes!
-    QModelIndex comboIndex = m_overviewModelIndex;
+    QModelIndex comboIndex = overviewModelIndex;
     while (comboIndex.parent().isValid())
         comboIndex = comboIndex.parent();
 