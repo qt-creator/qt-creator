qt-creator/qt-creator

commit 7862e3125628df7f761ec2850abb5d56f296cb75

Refactor OutputFormatter
Move link handling code to outputwindow from OutputFormatter

Move createOutputFormatter to the RunConfiguration

That makes it easier for Qt4RunConfiguration et all.
This also fixes that each time a runcontrol was rerun a new
OutputFormatter was created without deleting the old one, thus
increasing the memory usage.

src/plugins/qt4projectmanager/qtoutputformatter.cpp
@@ -27,24 +27,28 @@
 **
 **************************************************************************/
 
-#include "qmloutputformatter.h"
+#include "qtoutputformatter.h"
 
 #include <texteditor/basetexteditor.h>
+#include <qt4projectmanager/qt4project.h>
 
+#include <QtCore/QFileInfo>
 #include <QtGui/QPlainTextEdit>
 
 using namespace ProjectExplorer;
-using namespace QmlProjectManager::Internal;
+using namespace Qt4ProjectManager;
 
-QmlOutputFormatter::QmlOutputFormatter(QObject *parent)
-    : OutputFormatter(parent)
+QtOutputFormatter::QtOutputFormatter(Qt4Project *project)
+    : OutputFormatter()
     , m_qmlError(QLatin1String("(file:///[^:]+:\\d+:\\d+):"))
-    , m_linksActive(true)
-    , m_mousePressed(false)
+    , m_qtError(QLatin1String("Object::.*in (.*:\\d+)"))
+    , m_project(project)
+
 {
+
 }
 
-void QmlOutputFormatter::appendApplicationOutput(const QString &text, bool onStdErr)
+void QtOutputFormatter::appendApplicationOutput(const QString &text, bool onStdErr)
 {
     QTextCharFormat linkFormat;
     linkFormat.setForeground(plainTextEdit()->palette().link().color());
@@ -52,37 +56,33 @@ void QmlOutputFormatter::appendApplicationOutput(const QString &text, bool onStd
     linkFormat.setAnchor(true);
 
     // Create links from QML errors (anything of the form "file:///...:[line]:[column]:")
-    int index = 0;
-    while (m_qmlError.indexIn(text, index) != -1) {
+    if (m_qmlError.indexIn(text) != -1) {
         const int matchPos = m_qmlError.pos(1);
-        const QString leader = text.mid(index, matchPos - index);
+        const QString leader = text.left(matchPos);
         append(leader, onStdErr ? StdErrFormat : StdOutFormat);
 
         const QString matched = m_qmlError.cap(1);
         linkFormat.setAnchorHref(matched);
         append(matched, linkFormat);
 
-        index = matchPos + m_qmlError.matchedLength() - 1;
+        int index = matchPos + m_qmlError.matchedLength() - 1;
+        append(text.mid(index), onStdErr ? StdErrFormat : StdOutFormat);
+    } else if (m_qtError.indexIn(text) != -1) {
+        const int matchPos = m_qtError.pos(1);
+        const QString leader = text.left(matchPos);
+        append(leader, onStdErr ? StdErrFormat : StdOutFormat);
+
+        const QString matched = m_qtError.cap(1);
+        linkFormat.setAnchorHref(m_qtError.cap(1));
+        append(matched, linkFormat);
+
+        int index = matchPos + m_qtError.matchedLength() - 1;
+        append(text.mid(index), onStdErr ? StdErrFormat : StdOutFormat);
     }
-    append(text.mid(index), onStdErr ? StdErrFormat : StdOutFormat);
 }
 
-void QmlOutputFormatter::mousePressEvent(QMouseEvent * /*e*/)
+void QtOutputFormatter::handleLink(const QString &href)
 {
-    m_mousePressed = true;
-}
-
-void QmlOutputFormatter::mouseReleaseEvent(QMouseEvent *e)
-{
-    m_mousePressed = false;
-
-    if (!m_linksActive) {
-        // Mouse was released, activate links again
-        m_linksActive = true;
-        return;
-    }
-
-    const QString href = plainTextEdit()->anchorAt(e->pos());
     if (!href.isEmpty()) {
         QRegExp qmlErrorLink(QLatin1String("^file://(/[^:]+):(\\d+):(\\d+)"));
 
@@ -91,18 +91,30 @@ void QmlOutputFormatter::mouseReleaseEvent(QMouseEvent *e)
             const int line = qmlErrorLink.cap(2).toInt();
             const int column = qmlErrorLink.cap(3).toInt();
             TextEditor::BaseTextEditor::openEditorAt(fileName, line, column - 1);
+            return;
+        }
+
+        QRegExp qtErrorLink(QLatin1String("^(.*):(\\d+)$"));
+        if (qtErrorLink.indexIn(href) != 1) {
+            QString fileName = qtErrorLink.cap(1);
+            const int line = qtErrorLink.cap(2).toInt();
+            QFileInfo fi(fileName);
+            if (fi.isRelative()) {
+                // Yeah fileName is relative, no suprise
+                Qt4Project *pro = m_project.data();
+                if (pro) {
+                    QString baseName = fi.fileName();
+                    foreach (const QString &file, pro->files(Project::AllFiles)) {
+                        if (file.endsWith(baseName)) {
+                            // pick the first one...
+                            fileName = file;
+                            break;
+                        }
+                    }
+                }
+            }
+            TextEditor::BaseTextEditor::openEditorAt(fileName, line, 0);
+            return;
         }
     }
-}
-
-void QmlOutputFormatter::mouseMoveEvent(QMouseEvent *e)
-{
-    // Cursor was dragged to make a selection, deactivate links
-    if (m_mousePressed && plainTextEdit()->textCursor().hasSelection())
-        m_linksActive = false;
-
-    if (!m_linksActive || plainTextEdit()->anchorAt(e->pos()).isEmpty())
-        plainTextEdit()->viewport()->setCursor(Qt::IBeamCursor);
-    else
-        plainTextEdit()->viewport()->setCursor(Qt::PointingHandCursor);
 }

src/plugins/projectexplorer/outputformatter.cpp
@@ -37,8 +37,8 @@
 using namespace ProjectExplorer;
 using namespace TextEditor;
 
-OutputFormatter::OutputFormatter(QObject *parent)
-    : QObject(parent)
+OutputFormatter::OutputFormatter()
+    : QObject()
     , m_formats(0)
 {
     initFormats();
@@ -58,7 +58,6 @@ QPlainTextEdit *OutputFormatter::plainTextEdit() const
 void OutputFormatter::setPlainTextEdit(QPlainTextEdit *plainText)
 {
     m_plainTextEdit = plainText;
-    setParent(m_plainTextEdit);
 }
 
 void OutputFormatter::appendApplicationOutput(const QString &text, bool onStdErr)
@@ -82,15 +81,6 @@ void OutputFormatter::append(const QString &text, const QTextCharFormat &format)
     cursor.movePosition(QTextCursor::End);
     cursor.insertText(text, format);
 }
-
-void OutputFormatter::mousePressEvent(QMouseEvent * /*e*/)
-{}
-
-void OutputFormatter::mouseReleaseEvent(QMouseEvent * /*e*/)
-{}
-
-void OutputFormatter::mouseMoveEvent(QMouseEvent * /*e*/)
-{}
 
 void OutputFormatter::initFormats()
 {
@@ -116,4 +106,9 @@ void OutputFormatter::initFormats()
     // StdErrFormat
     m_formats[StdErrFormat].setFont(font);
     m_formats[StdErrFormat].setForeground(QColor(200, 0, 0));
+}
+
+void OutputFormatter::handleLink(const QString &href)
+{
+    Q_UNUSED(href);
 }

src/plugins/projectexplorer/outputformatter.h
@@ -46,7 +46,7 @@ class PROJECTEXPLORER_EXPORT OutputFormatter: public QObject
     Q_OBJECT
 
 public:
-    OutputFormatter(QObject *parent = 0);
+    OutputFormatter();
     virtual ~OutputFormatter();
 
     QPlainTextEdit *plainTextEdit() const;
@@ -55,9 +55,7 @@ public:
     virtual void appendApplicationOutput(const QString &text, bool onStdErr);
     virtual void appendMessage(const QString &text, bool isError);
 
-    virtual void mousePressEvent(QMouseEvent *e);
-    virtual void mouseReleaseEvent(QMouseEvent *e);
-    virtual void mouseMoveEvent(QMouseEvent *e);
+    virtual void handleLink(const QString &href);
 
 protected:
     enum Format {

src/plugins/projectexplorer/outputwindow.cpp
@@ -44,6 +44,8 @@
 #include <coreplugin/icontext.h>
 #include <find/basetextfind.h>
 #include <aggregation/aggregate.h>
+#include <texteditor/basetexteditor.h>
+#include <projectexplorer/project.h>
 
 #include <QtGui/QIcon>
 #include <QtGui/QScrollBar>
@@ -211,7 +213,7 @@ void OutputPane::createNewOutputWindow(RunControl *rc)
             OutputWindow *ow = static_cast<OutputWindow *>(m_tabWidget->widget(i));
             ow->grayOutOldContent();
             ow->verticalScrollBar()->setValue(ow->verticalScrollBar()->maximum());
-            ow->setFormatter(rc->createOutputFormatter(ow));
+            ow->setFormatter(rc->outputFormatter());
             m_outputWindows.insert(rc, ow);
             found = true;
             break;
@@ -219,7 +221,7 @@ void OutputPane::createNewOutputWindow(RunControl *rc)
     }
     if (!found) {
         OutputWindow *ow = new OutputWindow(m_tabWidget);
-        ow->setFormatter(rc->createOutputFormatter(ow));
+        ow->setFormatter(rc->outputFormatter());
         Aggregation::Aggregate *agg = new Aggregation::Aggregate;
         agg->add(ow);
         agg->add(new Find::BaseTextFind(ow));
@@ -376,6 +378,8 @@ OutputWindow::OutputWindow(QWidget *parent)
     : QPlainTextEdit(parent)
     , m_enforceNewline(false)
     , m_scrollToBottom(false)
+    , m_linksActive(true)
+    , m_mousePressed(false)
 {
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
     //setCenterOnScroll(false);
@@ -427,6 +431,42 @@ OutputWindow::~OutputWindow()
 {
     Core::ICore::instance()->removeContextObject(m_outputWindowContext);
     delete m_outputWindowContext;
+}
+
+void OutputWindow::mousePressEvent(QMouseEvent *e)
+{
+    m_mousePressed = true;
+    QPlainTextEdit::mousePressEvent(e);
+}
+
+
+void OutputWindow::mouseReleaseEvent(QMouseEvent *e)
+{
+    m_mousePressed = false;
+
+    if (!m_linksActive) {
+        // Mouse was released, activate links again
+        m_linksActive = true;
+        return;
+    }
+
+    const QString href = anchorAt(e->pos());
+    if (m_formatter)
+        m_formatter->handleLink(href);
+    QPlainTextEdit::mouseReleaseEvent(e);
+}
+
+void OutputWindow::mouseMoveEvent(QMouseEvent *e)
+{
+    // Cursor was dragged to make a selection, deactivate links
+    if (m_mousePressed && textCursor().hasSelection())
+        m_linksActive = false;
+
+    if (!m_linksActive || anchorAt(e->pos()).isEmpty())
+        viewport()->setCursor(Qt::IBeamCursor);
+    else
+        viewport()->setCursor(Qt::PointingHandCursor);
+    QPlainTextEdit::mouseMoveEvent(e);
 }
 
 OutputFormatter *OutputWindow::formatter() const
@@ -553,22 +593,4 @@ void OutputWindow::enableUndoRedo()
 {
     setMaximumBlockCount(0);
     setUndoRedoEnabled(true);
-}
-
-void OutputWindow::mousePressEvent(QMouseEvent *e)
-{
-    QPlainTextEdit::mousePressEvent(e);
-    m_formatter->mousePressEvent(e);
-}
-
-void OutputWindow::mouseReleaseEvent(QMouseEvent *e)
-{
-    QPlainTextEdit::mouseReleaseEvent(e);
-    m_formatter->mouseReleaseEvent(e);
-}
-
-void OutputWindow::mouseMoveEvent(QMouseEvent *e)
-{
-    QPlainTextEdit::mouseMoveEvent(e);
-    m_formatter->mouseMoveEvent(e);
 }

src/plugins/projectexplorer/outputwindow.h
@@ -49,6 +49,7 @@ namespace Core {
 namespace ProjectExplorer {
 class OutputFormatter;
 class RunControl;
+class Project;
 
 namespace Constants {
     const char * const C_APP_OUTPUT = "Application Output";
@@ -140,23 +141,24 @@ public:
     void showEvent(QShowEvent *);
 
 protected:
-    void mousePressEvent(QMouseEvent *e);
-    void mouseReleaseEvent(QMouseEvent *e);
-    void mouseMoveEvent(QMouseEvent *e);
-
     bool isScrollbarAtBottom() const;
     void scrollToBottom();
+
+    virtual void mousePressEvent(QMouseEvent *e);
+    virtual void mouseReleaseEvent(QMouseEvent *e);
+    virtual void mouseMoveEvent(QMouseEvent *e);
 
 private:
     void enableUndoRedo();
     QString doNewlineEnfocement(const QString &out);
 
-private:
     Core::BaseContext *m_outputWindowContext;
     OutputFormatter *m_formatter;
 
     bool m_enforceNewline;
     bool m_scrollToBottom;
+    bool m_linksActive;
+    bool m_mousePressed;
 };
 
 } // namespace Internal

src/plugins/projectexplorer/runconfiguration.cpp
@@ -179,6 +179,11 @@ Target *RunConfiguration::target() const
     return m_target;
 }
 
+ProjectExplorer::OutputFormatter *RunConfiguration::createOutputFormatter() const
+{
+    return new OutputFormatter();
+}
+
 IRunConfigurationFactory::IRunConfigurationFactory(QObject *parent) :
     QObject(parent)
 {
@@ -218,13 +223,23 @@ IRunControlFactory::~IRunControlFactory()
 RunControl::RunControl(RunConfiguration *runConfiguration, QString mode)
     : m_runMode(mode), m_runConfiguration(runConfiguration)
 {
-    if (runConfiguration)
+    if (runConfiguration) {
         m_displayName  = runConfiguration->displayName();
+        m_outputFormatter = runConfiguration->createOutputFormatter();
+    }
+    // We need to ensure that there's always a OutputFormatter
+    if (!m_outputFormatter)
+        m_outputFormatter = new OutputFormatter();
 }
 
 RunControl::~RunControl()
 {
+    delete m_outputFormatter;
+}
 
+OutputFormatter *RunControl::outputFormatter()
+{
+    return m_outputFormatter;
 }
 
 QString RunControl::runMode() const
@@ -240,11 +255,6 @@ QString RunControl::displayName() const
 bool RunControl::sameRunConfiguration(RunControl *other)
 {
     return other->m_runConfiguration.data() == m_runConfiguration.data();
-}
-
-OutputFormatter *RunControl::createOutputFormatter(QObject *parent)
-{
-    return new OutputFormatter(parent);
 }
 
 void RunControl::bringApplicationToForeground(qint64 pid)

src/plugins/projectexplorer/runconfiguration.h
@@ -85,6 +85,8 @@ public:
 
     Target *target() const;
 
+    virtual ProjectExplorer::OutputFormatter *createOutputFormatter() const;
+
 signals:
     void isEnabledChanged(bool value);
 
@@ -169,7 +171,7 @@ public:
 
     bool sameRunConfiguration(RunControl *other);
 
-    virtual OutputFormatter *createOutputFormatter(QObject *parent = 0);
+    OutputFormatter *outputFormatter();
     QString runMode() const;
 
 signals:
@@ -189,6 +191,7 @@ private:
     QString m_displayName;
     QString m_runMode;
     const QWeakPointer<RunConfiguration> m_runConfiguration;
+    OutputFormatter *m_outputFormatter;
 
 #ifdef Q_OS_MAC
     //these two are used to bring apps in the foreground on Mac

src/plugins/qmlprojectmanager/qmlprojectmanager.pro
@@ -22,8 +22,7 @@ HEADERS += qmlproject.h \
     qmltaskmanager.h \
     qmlprojectmanager_global.h \
     qmlprojectmanagerconstants.h \
-    qmlprojecttarget.h \
-    qmloutputformatter.h
+    qmlprojecttarget.h
 SOURCES += qmlproject.cpp \
     qmlprojectplugin.cpp \
     qmlprojectmanager.cpp \
@@ -35,8 +34,7 @@ SOURCES += qmlproject.cpp \
     qmlprojectrunconfigurationfactory.cpp \
     qmlprojectapplicationwizard.cpp \
     qmltaskmanager.cpp \
-    qmlprojecttarget.cpp \
-    qmloutputformatter.cpp
+    qmlprojecttarget.cpp
 RESOURCES += qmlproject.qrc
 
 OTHER_FILES += QmlProjectManager.pluginspec \

commit bc3878b7002a1c059111cb991e3b2b129abe1a97

Show again all application output in 'Application Output'
Fixes a regression introduced in 7862e31256.

Reviewed-by: dt

src/plugins/qt4projectmanager/qtoutputformatter.cpp
@@ -78,6 +78,8 @@ void QtOutputFormatter::appendApplicationOutput(const QString &text, bool onStdE
 
         int index = matchPos + m_qtError.matchedLength() - 1;
         append(text.mid(index), onStdErr ? StdErrFormat : StdOutFormat);
+    } else {
+        append(text, onStdErr ? StdErrFormat : StdOutFormat);
     }
 }
 