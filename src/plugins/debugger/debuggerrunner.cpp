f23c2286c9c943d6a4d9a3b8283f9e590755d13d
Lasse Holmstedt <lasse.holmstedt@nokia.com>
2010-08-13 14:56:05 +0200

    QML Debugger: OS X support for detecting qml viewer/observer

--- src/plugins/debugger/debuggerrunner.cpp
+++ src/plugins/debugger/debuggerrunner.cpp
@@ -331,7 +331,11 @@ void DebuggerRunControl::createEngine(const DebuggerStartParameters &sp)
     QString errorMessage;
     QString settingsIdHint;
 
-    if (sp.executable.endsWith(_("qmlviewer")) || sp.executable.endsWith(_("qmlobserver")))
+    bool isQmlExecutable = sp.executable.endsWith(_("qmlviewer")) || sp.executable.endsWith(_("qmlobserver"));
+#ifdef Q_OS_MAC
+    isQmlExecutable = sp.executable.endsWith(_("QMLViewer.app")) || sp.executable.endsWith(_("QMLObserver.app"));
+#endif
+    if (isQmlExecutable)
         engineType = QmlEngineType;
     else if (sp.executable.endsWith(_(".js")))
         engineType = ScriptEngineType;
