9cdc70fd7b8288df4db2f57619da7fc43ed35127
dt <qtc-committer@nokia.com>
2010-09-17 14:32:25 +0200

    Process Step: Expect a command, not a file

--- src/plugins/projectexplorer/processstep.cpp
+++ src/plugins/projectexplorer/processstep.cpp
@@ -249,7 +249,7 @@ ProcessStepConfigWidget::ProcessStepConfigWidget(ProcessStep *step)
         : m_step(step)
 {
     m_ui.setupUi(this);
-    m_ui.command->setExpectedKind(Utils::PathChooser::File);
+    m_ui.command->setExpectedKind(Utils::PathChooser::Command);
     connect(m_ui.command, SIGNAL(changed(QString)),
             this, SLOT(commandLineEditTextEdited()));
     connect(m_ui.workingDirectory, SIGNAL(changed(QString)),
