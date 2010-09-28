0cb9227615972384aed1ac489fd952ee817698b1
Erik Verbruggen <erik.verbruggen@nokia.com>
2010-09-28 10:57:38 +0200

    QML: added validation rules to the extension wizard.

--- share/qtcreator/templates/wizards/qml-extension/object.cpp
+++ share/qtcreator/templates/wizards/qml-extension/object.cpp
@@ -6,29 +6,6 @@
 %ObjectName%::%ObjectName%(QObject *parent):
         QObject(parent)
 {
-    timer = new QTimer(this);
-    timer->setInterval(750);
-    connect(timer, SIGNAL(timeout()), this, SLOT(timerFired()));
-    timer->start();
-}
-
-QString %ObjectName%::text() const
-{
-    return theText;
-}
-
-void %ObjectName%::setText(const QString &text)
-{
-    if (theText != text) {
-        theText = text;
-        emit textChanged(theText);
-    }
-}
-
-void %ObjectName%::timerFired()
-{
-    QTime t = QTime::currentTime();
-    setText(t.toString(QLatin1String("HH:mm:ss")));
 }
 
 QML_DECLARE_TYPE(%ObjectName%);
--- share/qtcreator/templates/wizards/qml-extension/object.h
+++ share/qtcreator/templates/wizards/qml-extension/object.h
@@ -8,26 +8,10 @@
 class %ObjectName% : public QObject
 {
     Q_OBJECT
-
-    Q_PROPERTY(QString text READ text WRITE setText NOTIFY textChanged)
+    Q_DISABLE_COPY(%ObjectName%)
 
 public:
     %ObjectName%(QObject *parent = 0);
-
-    QString text() const;
-    void setText(const QString &text);
-
-signals:
-    void textChanged(const QString &newText);
-
-private slots:
-    void timerFired();
-
-private:
-    QString theText;
-    QTimer *timer;
-
-    Q_DISABLE_COPY(%ObjectName%)
 };
 
 #endif // %ObjectName:u%_H
