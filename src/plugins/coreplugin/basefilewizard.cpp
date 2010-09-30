qt-creator/qt-creator

commit 65e95c3aba69123bf3286ea0c8ff60da0eece479

Added few new functions to IVersionControl, VCSManager and GeneratedFile to support the funcationality needed by the trac plugin. All the version control plugins updated to have implementations according to the interface changes.
Merge-request: 2178
Reviewed-by: Tobias Hunger <tobias.hunger@nokia.com>

Merge-request: 2178
Reviewed-by: Tobias Hunger <tobias.hunger@nokia.com>

src/plugins/coreplugin/basefilewizard.cpp
@@ -69,12 +69,14 @@ public:
     QString editorId;
     bool binary;
     GeneratedFile::Attributes attributes;
+    bool writeAccess;
 };
 
 GeneratedFilePrivate::GeneratedFilePrivate(const QString &p) :
     path(p),
     binary(false),
-    attributes(0)
+    attributes(0),
+    writeAccess(true)
 {
 }
 
@@ -102,6 +104,11 @@ GeneratedFile &GeneratedFile::operator=(const GeneratedFile &rhs)
 
 GeneratedFile::~GeneratedFile()
 {
+}
+
+void GeneratedFile::writeAccess(bool access)
+{
+   m_d->writeAccess = access;
 }
 
 QString GeneratedFile::path() const
@@ -156,6 +163,9 @@ void GeneratedFile::setEditorId(const QString &k)
 
 bool GeneratedFile::write(QString *errorMessage) const
 {
+    if (!m_d->writeAccess)
+      return true;
+
     // Ensure the directory
     const QFileInfo info(m_d->path);
     const QDir dir = info.absoluteDir();

src/plugins/coreplugin/basefilewizard.h
@@ -101,6 +101,7 @@ public:
     void setEditorId(const QString &k);
 
     bool write(QString *errorMessage) const;
+    void writeAccess(bool access = true);
 
     Attributes attributes() const;
     void setAttributes(Attributes a);

src/plugins/coreplugin/iversioncontrol.h
@@ -52,7 +52,9 @@ public:
         AddOperation, DeleteOperation, OpenOperation, MoveOperation,
         CreateRepositoryOperation,
         SnapshotOperations,
-        AnnotateOperation
+        AnnotateOperation,
+        CheckoutOperation,
+        GetRepositoryRootOperation
         };
 
     explicit IVersionControl(QObject *parent = 0) : QObject(parent) {}
@@ -115,6 +117,16 @@ public:
      * Called to initialize the version control system in a directory.
      */
     virtual bool vcsCreateRepository(const QString &directory) = 0;
+
+    /*!
+     * Called to clone/checkout the version control system in a directory.
+     */
+    virtual bool vcsCheckout(const QString &directory,const QByteArray &url) = 0;
+
+    /*!
+     * Called to get the version control repository root.
+     */
+    virtual QString vcsGetRepositoryURL(const QString &director) = 0;
 
     /*!
      * Create a snapshot of the current state and return an identifier or

src/plugins/coreplugin/vcsmanager.cpp
@@ -167,6 +167,42 @@ bool VCSManager::promptToDelete(const QString &fileName)
     return true;
 }
 
+IVersionControl* VCSManager::checkout(const QString &versionControlType,
+                                      const QString &directory,
+                                      const QByteArray &url)
+{
+    foreach (IVersionControl * versionControl, allVersionControls()) {
+        if (versionControl->displayName() == versionControlType &&
+            versionControl->supportsOperation(Core::IVersionControl::CheckoutOperation)) {
+            if (versionControl->vcsCheckout(directory,url)) {
+                m_d->m_cachedMatches.insert(directory, versionControl);
+                return versionControl;
+            }
+            return 0;
+        }
+    }
+    return 0;
+}
+
+bool VCSManager::findVersionControl(const QString &versionControlType)
+{
+    foreach (IVersionControl * versionControl, allVersionControls()) {
+        if (versionControl->displayName() == versionControlType) {
+            return true;
+        }
+    }
+    return false;
+}
+
+QString VCSManager::getRepositoryURL(const QString &directory)
+{
+    IVersionControl *vc = findVersionControlForDirectory(directory);
+
+    if (vc && vc->supportsOperation(Core::IVersionControl::GetRepositoryRootOperation))
+       return vc->vcsGetRepositoryURL(directory);
+    return QString();
+}
+
 bool VCSManager::promptToDelete(IVersionControl *vc, const QString &fileName)
 {
     QTC_ASSERT(vc, return true)

src/plugins/coreplugin/vcsmanager.h
@@ -68,6 +68,11 @@ public:
 
     IVersionControl *findVersionControlForDirectory(const QString &directory,
                                                     QString *topLevelDirectory = 0);
+    IVersionControl *checkout(const QString &versionControlType,
+                              const QString &directory,
+                              const QByteArray &url);
+    bool findVersionControl(const QString &versionControl);
+    QString getRepositoryURL(const QString &directory);
 
     // Shows a confirmation dialog, whether the file should also be deleted
     // from revision control Calls sccDelete on the file. Returns false

src/plugins/cvs/cvscontrol.cpp
@@ -58,6 +58,8 @@ bool CVSControl::supportsOperation(Operation operation) const
     case MoveOperation:
     case CreateRepositoryOperation:
     case SnapshotOperations:
+    case CheckoutOperation:
+    case GetRepositoryRootOperation:
         rc = false;
         break;
     }
@@ -90,6 +92,16 @@ bool CVSControl::vcsMove(const QString &from, const QString &to)
 }
 
 bool CVSControl::vcsCreateRepository(const QString &)
+{
+    return false;
+}
+
+QString CVSControl::vcsGetRepositoryURL(const QString &)
+{
+    return QString();
+}
+
+bool CVSControl::vcsCheckout(const QString &/*directory*/, const QByteArray &/*url*/)
 {
     return false;
 }

src/plugins/cvs/cvscontrol.h
@@ -53,6 +53,8 @@ public:
     virtual bool vcsDelete(const QString &filename);
     virtual bool vcsMove(const QString &from, const QString &to);
     virtual bool vcsCreateRepository(const QString &directory);
+    virtual bool vcsCheckout(const QString &directory, const QByteArray &url);
+    virtual QString vcsGetRepositoryURL(const QString &directory);
     virtual QString vcsCreateSnapshot(const QString &topLevel);
     virtual QStringList vcsSnapshots(const QString &topLevel);
     virtual bool vcsRestoreSnapshot(const QString &topLevel, const QString &name);

src/plugins/git/gitclient.cpp
@@ -50,6 +50,7 @@
 #include <texteditor/itexteditor.h>
 #include <utils/qtcassert.h>
 #include <utils/synchronousprocess.h>
+#include <utils/environment.h>
 #include <vcsbase/vcsbaseeditor.h>
 #include <vcsbase/vcsbaseoutputwindow.h>
 #include <vcsbase/vcsbaseplugin.h>
@@ -1920,6 +1921,61 @@ QString GitClient::readConfig(const QString &workingDirectory, const QStringList
 QString GitClient::readConfigValue(const QString &workingDirectory, const QString &configVar)
 {
     return readConfig(workingDirectory, QStringList(configVar)).remove(QLatin1Char('\n'));
+}
+
+bool GitClient::cloneRepository(const QString &directory,const QByteArray &url)
+{
+    QDir workingDirectory(directory);
+    const unsigned flags =  VCSBase::VCSBasePlugin::SshPasswordPrompt
+                           |VCSBase::VCSBasePlugin::ShowStdOutInLogWindow
+                           |VCSBase::VCSBasePlugin::ShowSuccessMessage;
+
+    if (workingDirectory.exists()) {
+        if (!synchronousInit(workingDirectory.path()))
+            return false;
+
+        QStringList arguments(QLatin1String("remote"));
+        arguments << QLatin1String("add") << QLatin1String("origin") << url;
+        if (!fullySynchronousGit(workingDirectory.path(),arguments,0,0,true))
+            return false;        
+
+        arguments.clear();
+        arguments << QLatin1String("fetch");
+        const Utils::SynchronousProcessResponse resp = synchronousGit(workingDirectory.path(),arguments,flags);
+        if (resp.result != Utils::SynchronousProcessResponse::Finished)
+            return false;
+
+        arguments.clear();
+        arguments << QLatin1String("config") <<  QLatin1String("branch.master.remote") <<  QLatin1String("origin");
+        if (!fullySynchronousGit(workingDirectory.path(),arguments,0,0,true))
+            return false;
+
+        arguments.clear();
+        arguments << QLatin1String("config") <<  QLatin1String("branch.master.merge") <<  QLatin1String("refs/heads/master");
+        if (!fullySynchronousGit(workingDirectory.path(),arguments,0,0,true))
+            return false;
+
+        return true;
+    } else {
+        QStringList arguments(QLatin1String("clone"));
+        arguments << url << workingDirectory.dirName();
+        workingDirectory.cdUp();
+        const Utils::SynchronousProcessResponse resp = synchronousGit(workingDirectory.path(),arguments,flags);
+        return resp.result == Utils::SynchronousProcessResponse::Finished;
+    }
+}
+
+QString GitClient::vcsGetRepositoryURL(const QString &directory)
+{
+    QStringList arguments(QLatin1String("config"));
+    QByteArray outputText;
+
+    arguments << QLatin1String("remote.origin.url");
+
+    if (fullySynchronousGit(directory,arguments,&outputText,0,false)) {
+        return commandOutputFromLocal8Bit(outputText);
+    }
+    return QString();
 }
 
 GitSettings GitClient::settings() const

commit ac80d924c77e4811738ce62b0a4a6ad4430ada16

Custom Wizards: No files generated.
Fix breakage introduced by 65e95c3aba69123bf3286ea0c8ff60da0eece479,
introducing a writeAccess attribute not properly initialized
in the default CT. The GeneratedFile::CustomGeneratorAttribute
exists for that purpose.

Reviewed-by: Tobias Hunger <tobias.hunger@nokia.com>
Task-number: QTCREATORBUG-2518

src/plugins/coreplugin/basefilewizard.cpp
@@ -69,14 +69,12 @@ public:
     QString editorId;
     bool binary;
     GeneratedFile::Attributes attributes;
-    bool writeAccess;
 };
 
 GeneratedFilePrivate::GeneratedFilePrivate(const QString &p) :
     path(p),
     binary(false),
-    attributes(0),
-    writeAccess(true)
+    attributes(0)
 {
 }
 
@@ -104,11 +102,6 @@ GeneratedFile &GeneratedFile::operator=(const GeneratedFile &rhs)
 
 GeneratedFile::~GeneratedFile()
 {
-}
-
-void GeneratedFile::setWriteAccess(bool access)
-{
-   m_d->writeAccess = access;
 }
 
 QString GeneratedFile::path() const
@@ -163,9 +156,6 @@ void GeneratedFile::setEditorId(const QString &k)
 
 bool GeneratedFile::write(QString *errorMessage) const
 {
-    if (!m_d->writeAccess)
-      return true;
-
     // Ensure the directory
     const QFileInfo info(m_d->path);
     const QDir dir = info.absoluteDir();

src/plugins/coreplugin/basefilewizard.h
@@ -101,7 +101,6 @@ public:
     void setEditorId(const QString &k);
 
     bool write(QString *errorMessage) const;
-    void setWriteAccess(bool access = true);
 
     Attributes attributes() const;
     void setAttributes(Attributes a);