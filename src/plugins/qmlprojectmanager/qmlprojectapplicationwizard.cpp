defb9881b161174d0163a4cd9c0b56b5e49bb4eb
Thomas Hartmann <Thomas.Hartmann@nokia.com>
2010-10-04 16:21:08 +0200

    QmlWizards: use QtQuick 1.0 instead of Qt 4.7
    Reviewed-by: Kai Koehne

--- src/plugins/qmlprojectmanager/qmlprojectapplicationwizard.cpp
+++ src/plugins/qmlprojectmanager/qmlprojectapplicationwizard.cpp
@@ -114,7 +114,7 @@ Core::GeneratedFiles QmlProjectApplicationWizard::generateFiles(const QWizard *w
         QTextStream out(&contents);
 
         out
-            << "import Qt 4.7" << endl
+            << "import QtQuick 1.0" << endl
             << endl
             << "Rectangle {" << endl
             << "    width: 200" << endl
