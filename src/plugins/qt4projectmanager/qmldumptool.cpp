f9938a97386411a5fbb8fe217698cf0a9621825f
Kai Koehne <kai.koehne@nokia.com>
2010-10-05 09:32:45 +0200

    qmldump: Always check for private headers being available
    4.7.1 won't come with private headers always being installed.
    
    Reviewed-by: ckamm

--- src/plugins/qt4projectmanager/qmldumptool.cpp
+++ src/plugins/qt4projectmanager/qmldumptool.cpp
@@ -131,7 +131,7 @@ bool QmlDumpTool::canBuild(const QtVersion *qtVersion)
     const QString installHeaders = qtVersion->versionInfo().value("QT_INSTALL_HEADERS");
     const QString header = installHeaders + QLatin1String("/QtDeclarative/private/qdeclarativemetatype_p.h");
     return qtVersion->supportsTargetId(Constants::DESKTOP_TARGET_ID)
-            && (checkMinimumQtVersion(qtVersion->qtVersionString(), 4, 7, 1) || QFile::exists(header));
+            && QFile::exists(header);
 }
 
 static QtVersion *qtVersionForProject(ProjectExplorer::Project *project)
