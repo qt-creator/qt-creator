qt-creator/qt-creator

commit 29b073e92e61972846b743e52787b56a0a4d58d0

Refactor block selection
Block selection was "broken" when using tabs, or rather
incomplete: It treated tabs as normal characters, which
has shown to be unexpected by people using tabs in code.

The new implementation has a vastly improved find scope
as well. In addition, creating a blog selection with
mouse or keyboard feels a lot more solid now, as the
actual selection is detached from possible valid cursor
positions.

Task-number: QTCREATORBUG-1541

src/plugins/projectexplorer/outputwindow.cpp
@@ -591,7 +591,11 @@ void OutputWindow::appendText(const QString &textIn, const QTextCharFormat &form
 
 bool OutputWindow::isScrollbarAtBottom() const
 {
-    return verticalScrollBar()->value() == verticalScrollBar()->maximum();
+    return isVisible()
+            && (blockBoundingRect(document()->lastBlock()).bottom()
+                + contentOffset().y() <= viewport()->rect().bottom());
+
+    // return verticalScrollBar()->value() == verticalScrollBar()->maximum();
 }
 
 void OutputWindow::scrollToBottom()

src/plugins/find/basetextfind.cpp
@@ -39,14 +39,16 @@ using namespace Find;
 
 BaseTextFind::BaseTextFind(QTextEdit *editor)
     : m_editor(editor)
-    , m_findScopeVerticalBlockSelection(0)
+    , m_findScopeVerticalBlockSelectionFirstColumn(-1)
+    , m_findScopeVerticalBlockSelectionLastColumn(-1)
     , m_incrementalStartPos(-1)
 {
 }
 
 BaseTextFind::BaseTextFind(QPlainTextEdit *editor)
     : m_plaineditor(editor)
-    , m_findScopeVerticalBlockSelection(0)
+    , m_findScopeVerticalBlockSelectionFirstColumn(-1)
+    , m_findScopeVerticalBlockSelectionLastColumn(-1)
     , m_incrementalStartPos(-1)
 {
 }
@@ -268,17 +270,16 @@ QTextCursor BaseTextFind::findOne(const QRegExp &expr, const QTextCursor &from, 
     if (candidate.isNull())
         return candidate;
 
-    if (!m_findScopeVerticalBlockSelection)
+    if (m_findScopeVerticalBlockSelectionFirstColumn < 0)
         return candidate;
     forever {
         if (!inScope(candidate.selectionStart(), candidate.selectionEnd()))
             return candidate;
-        QTextCursor b = candidate;
-        b.setPosition(candidate.selectionStart());
-        QTextCursor e = candidate;
-        e.setPosition(candidate.selectionEnd());
-        if (b.positionInBlock() >= m_findScopeStart.positionInBlock() + 1
-            && e.positionInBlock() <= m_findScopeStart.positionInBlock() + 1 + m_findScopeVerticalBlockSelection)
+        bool inVerticalFindScope = false;
+        QMetaObject::invokeMethod(m_plaineditor, "inFindScope", Qt::DirectConnection,
+                                  Q_RETURN_ARG(bool, inVerticalFindScope),
+                                  Q_ARG(QTextCursor, candidate));
+        if (inVerticalFindScope)
             return candidate;
         candidate = document()->find(expr, candidate, options);
     }
@@ -299,23 +300,19 @@ void BaseTextFind::defineFindScope()
     if (cursor.hasSelection() && cursor.block() != cursor.document()->findBlock(cursor.anchor())) {
         m_findScopeStart = QTextCursor(document()->docHandle(), qMax(0, cursor.selectionStart()-1));
         m_findScopeEnd = QTextCursor(document()->docHandle(), cursor.selectionEnd());
-        m_findScopeVerticalBlockSelection = 0;
+        m_findScopeVerticalBlockSelectionFirstColumn = -1;
+        m_findScopeVerticalBlockSelectionLastColumn = -1;
 
-        int verticalBlockSelection = 0;
-        if (m_plaineditor && m_plaineditor->metaObject()->indexOfProperty("verticalBlockSelection") >= 0)
-            verticalBlockSelection = m_plaineditor->property("verticalBlockSelection").toInt();
-
-        if (verticalBlockSelection) {
-            QTextCursor findScopeVisualStart(document()->docHandle(), cursor.selectionStart());
-            int findScopeFromColumn = qMin(findScopeVisualStart.positionInBlock(),
-                                         m_findScopeEnd.positionInBlock());
-            int findScopeToColumn = findScopeFromColumn + verticalBlockSelection;
-            m_findScopeStart.setPosition(findScopeVisualStart.block().position() + findScopeFromColumn - 1);
-            m_findScopeEnd.setPosition(m_findScopeEnd.block().position()
-                                       + qMin(m_findScopeEnd.block().length()-1, findScopeToColumn));
-            m_findScopeVerticalBlockSelection = verticalBlockSelection;
+        if (m_plaineditor && m_plaineditor->metaObject()->indexOfProperty("verticalBlockSelectionFirstColumn") >= 0) {
+            m_findScopeVerticalBlockSelectionFirstColumn
+                    = m_plaineditor->property("verticalBlockSelectionFirstColumn").toInt();
+            m_findScopeVerticalBlockSelectionLastColumn
+                    = m_plaineditor->property("verticalBlockSelectionLastColumn").toInt();
         }
-        emit findScopeChanged(m_findScopeStart, m_findScopeEnd, m_findScopeVerticalBlockSelection);
+
+        emit findScopeChanged(m_findScopeStart, m_findScopeEnd,
+                              m_findScopeVerticalBlockSelectionFirstColumn,
+                              m_findScopeVerticalBlockSelectionLastColumn);
         cursor.setPosition(m_findScopeStart.position()+1);
         setTextCursor(cursor);
     } else {
@@ -327,6 +324,9 @@ void BaseTextFind::clearFindScope()
 {
     m_findScopeStart = QTextCursor();
     m_findScopeEnd = QTextCursor();
-    m_findScopeVerticalBlockSelection = 0;
-    emit findScopeChanged(m_findScopeStart, m_findScopeEnd, m_findScopeVerticalBlockSelection);
+    m_findScopeVerticalBlockSelectionFirstColumn = -1;
+    m_findScopeVerticalBlockSelectionLastColumn = -1;
+    emit findScopeChanged(m_findScopeStart, m_findScopeEnd,
+                          m_findScopeVerticalBlockSelectionFirstColumn,
+                          m_findScopeVerticalBlockSelectionLastColumn);
 }

src/plugins/find/basetextfind.h
@@ -72,7 +72,9 @@ public:
 
 signals:
     void highlightAll(const QString &txt, Find::FindFlags findFlags);
-    void findScopeChanged(const QTextCursor &start, const QTextCursor &end, int verticalBlockSelection);
+    void findScopeChanged(const QTextCursor &start, const QTextCursor &end,
+                          int verticalBlockSelectionFirstColumn,
+                          int verticalBlockSelectionLastColumn);
 
 private:
     bool find(const QString &txt,
@@ -89,7 +91,8 @@ private:
     QPointer<QPlainTextEdit> m_plaineditor;
     QTextCursor m_findScopeStart;
     QTextCursor m_findScopeEnd;
-    int m_findScopeVerticalBlockSelection;
+    int m_findScopeVerticalBlockSelectionFirstColumn;
+    int m_findScopeVerticalBlockSelectionLastColumn;
     bool inScope(int startPosition, int endPosition) const;
     QTextCursor findOne(const QRegExp &expr, const QTextCursor &from, QTextDocument::FindFlags options) const;
     int m_incrementalStartPos;

src/plugins/texteditor/basetexteditor.h
@@ -47,6 +47,7 @@ namespace Utils {
 }
 
 namespace TextEditor {
+struct TabSettings;
 
 namespace Internal {
     class BaseTextEditorPrivate;
@@ -54,6 +55,28 @@ namespace Internal {
     class RefactorOverlay;
     struct RefactorMarker;
     typedef QList<RefactorMarker> RefactorMarkers;
+
+    class TEXTEDITOR_EXPORT BaseTextBlockSelection
+    {
+    public:
+
+        bool isValid() const{ return !firstBlock.isNull() && !lastBlock.isNull(); }
+        void clear() { firstBlock = lastBlock = QTextCursor(); }
+
+        QTextCursor firstBlock; // defines the first block
+        QTextCursor lastBlock; // defines the last block
+        int firstVisualColumn; // defines the first visual column of the selection
+        int lastVisualColumn; // defines the last visual column of the selection
+        enum Anchor {TopLeft = 0, TopRight, BottomLeft, BottomRight} anchor;
+        BaseTextBlockSelection():firstVisualColumn(0), lastVisualColumn(0), anchor(BottomRight){}
+        void moveAnchor(int blockNumber, int visualColumn);
+        inline int anchorColumnNumber() const { return (anchor % 2) ? lastVisualColumn : firstVisualColumn; }
+        inline int anchorBlockNumber() const {
+            return (anchor <= TopRight ? firstBlock.blockNumber() : lastBlock.blockNumber()); }
+        QTextCursor selection(const TabSettings &ts) const;
+        void fromSelection(const TabSettings &ts, const QTextCursor &selection);
+    };
+
 
 }
 
@@ -66,8 +89,6 @@ struct BehaviorSettings;
 struct CompletionSettings;
 struct DisplaySettings;
 struct StorageSettings;
-struct TabSettings;
-
 
 class TEXTEDITOR_EXPORT BaseTextEditorAnimator : public QObject
 {
@@ -113,8 +134,8 @@ private:
 class TEXTEDITOR_EXPORT BaseTextEditor : public QPlainTextEdit
 {
     Q_OBJECT
-    Q_PROPERTY(int verticalBlockSelection READ verticalBlockSelection)
-
+    Q_PROPERTY(int verticalBlockSelectionFirstColumn READ verticalBlockSelectionFirstColumn)
+    Q_PROPERTY(int verticalBlockSelectionLastColumn READ verticalBlockSelectionLastColumn)
 public:
     BaseTextEditor(QWidget *parent);
     ~BaseTextEditor();
@@ -214,7 +235,8 @@ public:
     void insertCodeSnippet(const QTextCursor &cursor, const QString &snippet);
 
 
-    int verticalBlockSelection() const;
+    int verticalBlockSelectionFirstColumn() const;
+    int verticalBlockSelectionLastColumn() const;
 
     QRegion translatedLineRegion(int lineStart, int lineEnd) const;
 
@@ -315,7 +337,9 @@ private slots:
     void documentAboutToBeReloaded();
     void documentReloaded();
     void highlightSearchResults(const QString &txt, Find::FindFlags findFlags);
-    void setFindScope(const QTextCursor &start, const QTextCursor &end, int);
+    void setFindScope(const QTextCursor &start, const QTextCursor &end, int, int);
+    bool inFindScope(const QTextCursor &cursor);
+    bool inFindScope(int selectionStart, int selectionEnd);
     void currentEditorChanged(Core::IEditor *editor);
     void maybeEmitContentsChangedBecauseOfUndo();
 
@@ -531,6 +555,8 @@ private:
 private slots:
     // auto completion
     void _q_requestAutoCompletion();
+
+    void handleBlockSelection(int diff_row, int diff_col);
 
     // parentheses matcher
     void _q_matchParentheses();

src/plugins/texteditor/basetexteditor_p.h
@@ -245,8 +245,6 @@ public:
 
     // block selection mode
     bool m_inBlockSelectionMode;
-    bool m_lastEventWasBlockSelectionEvent;
-    int m_blockSelectionExtraX;
     void clearBlockSelection();
     QString copyBlockSelection();
     void removeBlockSelection(const QString &text = QString());
@@ -254,8 +252,12 @@ public:
 
     QTextCursor m_findScopeStart;
     QTextCursor m_findScopeEnd;
-    int m_findScopeVerticalBlockSelection;
+    int m_findScopeVerticalBlockSelectionFirstColumn;
+    int m_findScopeVerticalBlockSelectionLastColumn;
+
     QTextCursor m_selectBlockAnchor;
+
+    Internal::BaseTextBlockSelection m_blockSelection;
 
     void moveCursorVisible(bool ensureVisible = true);
 

src/plugins/texteditor/tabsettings.cpp
@@ -226,6 +226,22 @@ int TabSettings::columnAt(const QString &text, int position) const
     return column;
 }
 
+int TabSettings::positionAtColumn(const QString &text, int column, int *offset) const
+{
+    int col = 0;
+    int i = 0;
+    while (i < text.size() && col < column) {
+        if (text.at(i) == QLatin1Char('\t'))
+            col = col - (col % m_tabSize) + m_tabSize;
+        else
+            ++col;
+        ++i;
+    }
+    if (offset)
+        *offset = column - col;
+    return i;
+}
+
 int TabSettings::spacesLeftFromPosition(const QString &text, int position) const
 {
     int i = position;

src/plugins/texteditor/tabsettings.h
@@ -61,6 +61,7 @@ struct TEXTEDITOR_EXPORT TabSettings
     int firstNonSpace(const QString &text) const;
     inline bool onlySpace(const QString &text) const { return firstNonSpace(text) == text.length(); }
     int columnAt(const QString &text, int position) const;
+    int positionAtColumn(const QString &text, int column, int *offset = 0) const;
     int spacesLeftFromPosition(const QString &text, int position) const;
     int indentedColumn(int column, bool doIndent = true) const;
     QString indentationString(int startColumn, int targetColumn, const QTextBlock &currentBlock = QTextBlock()) const;

commit 5d2d31279bd7df7b69b9713114d3225fd16f774c

Fix scrolling of compile output windwow yet again Broke with 29b073e92e61972846b743e52787b56a0a4d58d0
Reviewed-by: mae

src/plugins/projectexplorer/outputwindow.cpp
@@ -688,11 +688,7 @@ void OutputWindow::appendText(const QString &textIn, const QTextCharFormat &form
 
 bool OutputWindow::isScrollbarAtBottom() const
 {
-    return isVisible()
-            && (blockBoundingRect(document()->lastBlock()).bottom()
-                + contentOffset().y() <= viewport()->rect().bottom());
-
-    // return verticalScrollBar()->value() == verticalScrollBar()->maximum();
+    return verticalScrollBar()->value() == verticalScrollBar()->maximum();
 }
 
 void OutputWindow::clear()