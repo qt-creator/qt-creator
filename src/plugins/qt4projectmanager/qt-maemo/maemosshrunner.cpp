2c35a6417a790a4a518e01eebcaa0d26a9e725bf
Christian Kandeler <christian.kandeler@nokia.com>
2010-10-06 10:53:33 +0200

    Maemo: Fix SSH signal connections.

--- src/plugins/qt4projectmanager/qt-maemo/maemosshrunner.cpp
+++ src/plugins/qt4projectmanager/qt-maemo/maemosshrunner.cpp
@@ -93,7 +93,7 @@ void MaemoSshRunner::start()
         m_connection = SshConnection::create();
     connect(m_connection.data(), SIGNAL(connected()), this,
         SLOT(handleConnected()));
-    connect(m_connection.data(), SIGNAL(error(SshError)), this,
+    connect(m_connection.data(), SIGNAL(error(Core::SshError)), this,
         SLOT(handleConnectionFailure()));
     if (reUse) {        
         handleConnected();
