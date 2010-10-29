9c9a630da90997f607e1a75045549bcd49c0405f
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2010-10-29 13:48:14 +0200

    Debugger: Avoid warnings from debugger when using -client.
    Do not parse options if empty (when a file was passed to
    the core).
    
    Reviewed-by: Thorbjørn <thorbjorn.lindeijer@nokia.com>

--- src/plugins/debugger/debuggerplugin.cpp
+++ src/plugins/debugger/debuggerplugin.cpp
@@ -2868,6 +2868,9 @@ bool DebuggerPlugin::hasSnapsnots() const
 
 void DebuggerPlugin::remoteCommand(const QStringList &options, const QStringList &)
 {
+    if (options.isEmpty())
+        return;
+
     unsigned enabledEngines = 0;
     QString errorMessage;
     bool success = false;
