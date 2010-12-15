qt-creator/qt-creator

commit 77e3bfe8451343be5478027154a027cbcfe4b778

debugger: structural cleanup remote debugging
Use virtual function instead of if/qobject_cast cascades.

src/plugins/debugger/gdb/remotegdbserveradapter.cpp
@@ -96,7 +96,7 @@ void RemoteGdbServerAdapter::startAdapter()
     }
     if (startParameters().serverStartScript.isEmpty()) {
         showMessage(_("No server start script given. "), StatusBar);
-        emit requestSetup();
+        m_engine->requestRemoteSetup();
     } else {
         m_uploadProc.start(_("/bin/sh ") + startParameters().serverStartScript);
         m_uploadProc.waitForStarted();

src/plugins/debugger/gdb/remoteplaingdbadapter.cpp
@@ -50,8 +50,7 @@ void RemotePlainGdbAdapter::startAdapter()
 {
     QTC_ASSERT(state() == EngineSetupRequested, qDebug() << state());
     showMessage(QLatin1String("TRYING TO START ADAPTER"));
-
-    emit requestSetup();
+    m_engine->requestRemoteSetup();
 }
 
 void RemotePlainGdbAdapter::setupInferior()
@@ -101,8 +100,9 @@ void RemotePlainGdbAdapter::shutdownAdapter()
     m_engine->notifyAdapterShutdownOk();
 }
 
-void RemotePlainGdbAdapter::handleSetupDone(int qmlPort)
+void RemotePlainGdbAdapter::handleSetupDone(int gdbServerPort, int qmlPort)
 {
+    Q_UNUSED(gdbServerPort);
     QTC_ASSERT(state() == EngineSetupRequested, qDebug() << state());
 
     if (qmlPort != -1)

src/plugins/debugger/gdb/remoteplaingdbadapter.h
@@ -43,11 +43,8 @@ class RemotePlainGdbAdapter : public AbstractPlainGdbAdapter
 public:
     friend class RemoteGdbProcess;
     explicit RemotePlainGdbAdapter(GdbEngine *engine, QObject *parent = 0);
-    void handleSetupDone(int qmlPort);
+    void handleSetupDone(int gdbServerPort, int qmlPort);
     void handleSetupFailed(const QString &reason);
-
-signals:
-    void requestSetup();
 
 private slots:
     void handleGdbStarted();

src/plugins/debugger/debuggerengine.cpp
@@ -1430,6 +1430,17 @@ void DebuggerEngine::openDisassemblerView(const StackFrame &frame)
     agent->setFrame(frame, true, false);
 }
 
+void DebuggerEngine::handleRemoteSetupDone(int gdbServerPort, int qmlPort)
+{
+    Q_UNUSED(gdbServerPort);
+    Q_UNUSED(qmlPort);
+}
+
+void DebuggerEngine::handleRemoteSetupFailed(const QString &message)
+{
+    Q_UNUSED(message);
+}
+
 } // namespace Debugger
 
 #include "debuggerengine.moc"

src/plugins/debugger/debuggerengine.h
@@ -35,7 +35,7 @@
 #include "moduleshandler.h" // For 'Symbols'
 #include "breakpoint.h" // For 'BreakpointId'
 
-#include <coreplugin/ssh/sshconnection.h> 
+#include <coreplugin/ssh/sshconnection.h>
 
 #include <utils/environment.h>
 
@@ -203,6 +203,9 @@ public:
     virtual void removeTooltip();
     virtual void selectThread(int index);
 
+    virtual void handleRemoteSetupDone(int gdbServerPort, int qmlPort);
+    virtual void handleRemoteSetupFailed(const QString &message);
+
 protected:
     friend class Internal::DebuggerPluginPrivate;
     virtual void detachDebugger();
@@ -255,7 +258,7 @@ public:
 
     void breakByFunction(const QString &functionName);
     void breakByFunctionMain();
-    
+
     DebuggerState state() const;
     DebuggerState lastGoodState() const;
     DebuggerState targetState() const;
@@ -289,6 +292,15 @@ public:
 signals:
     void stateChanged(const DebuggerState &state);
     void updateViewsRequested();
+    /*
+     * For "external" clients of a debugger run control that need to do
+     * further setup before the debugger is started (e.g. Maemo).
+     * Afterwards, handleSetupDone() or handleSetupFailed() must be called
+     * to continue or abort debugging, respectively.
+     * This signal is only emitted if the start parameters indicate that
+     * a server start script should be used, but none is given.
+     */
+    void requestRemoteSetup();
 
 protected:
     // The base notify*() function implementation should be sufficient

src/plugins/debugger/debuggerrunner.cpp
@@ -35,12 +35,7 @@
 #include "debuggermainwindow.h"
 #include "debuggerplugin.h"
 #include "debuggerstringutils.h"
-#include "gdb/gdbengine.h"
-#include "gdb/remotegdbserveradapter.h"
-#include "gdb/remoteplaingdbadapter.h"
 #include "gdb/gdboptionspage.h"
-#include "qml/qmlengine.h"
-#include "qml/qmlcppengine.h"
 #include "lldb/lldbenginehost.h"
 
 #ifdef Q_OS_WIN
@@ -269,10 +264,6 @@ public:
     DebuggerEngineType engineForMode(unsigned enabledEngineTypes,
         DebuggerStartMode mode);
 
-    void initGdbEngine(GdbEngine *engine);
-    GdbEngine *gdbEngine() const;
-    AbstractGdbAdapter *gdbAdapter() const;
-
 public:
     DebuggerRunControl *q;
     DebuggerEngine *m_engine;
@@ -383,38 +374,6 @@ DebuggerEngineType DebuggerRunControlPrivate::engineForMode
 #endif
 }
 
-void DebuggerRunControlPrivate::initGdbEngine(GdbEngine *engine)
-{
-    QTC_ASSERT(engine, return)
-
-    // Forward adapter signals.
-    AbstractGdbAdapter *adapter = engine->gdbAdapter();
-    RemotePlainGdbAdapter *rpga = qobject_cast<RemotePlainGdbAdapter *>(adapter);
-    RemoteGdbServerAdapter *rgsa = qobject_cast<RemoteGdbServerAdapter *>(adapter);
-    if (rpga)
-        q->connect(rpga, SIGNAL(requestSetup()), SIGNAL(engineRequestSetup()));
-    else if (rgsa)
-        q->connect(rgsa, SIGNAL(requestSetup()), SIGNAL(engineRequestSetup()));
-}
-
-GdbEngine *DebuggerRunControlPrivate::gdbEngine() const
-{
-    QTC_ASSERT(m_engine, return 0);
-    if (GdbEngine *gdbEngine = qobject_cast<GdbEngine *>(m_engine))
-        return gdbEngine;
-    if (QmlCppEngine *qmlEngine = qobject_cast<QmlCppEngine *>(m_engine))
-        if (GdbEngine *embeddedGdbEngine = qobject_cast<GdbEngine *>(qmlEngine->cppEngine()))
-            return embeddedGdbEngine;
-    return 0;
-}
-
-AbstractGdbAdapter *DebuggerRunControlPrivate::gdbAdapter() const
-{
-    GdbEngine *engine = gdbEngine();
-    QTC_ASSERT(engine, return 0)
-    return engine->gdbAdapter();
-}
-
 } // namespace Internal
 
 
@@ -514,7 +473,6 @@ DebuggerRunControl::DebuggerRunControl(RunConfiguration *runConfiguration,
     switch (engineType) {
         case GdbEngineType:
             d->m_engine = createGdbEngine(sp);
-            d->initGdbEngine(qobject_cast<GdbEngine *>(d->m_engine));
             break;
         case ScriptEngineType:
             d->m_engine = createScriptEngine(sp);
@@ -534,14 +492,9 @@ DebuggerRunControl::DebuggerRunControl(RunConfiguration *runConfiguration,
             break;
         case QmlEngineType:
             d->m_engine = createQmlEngine(sp);
-            connect(qobject_cast<QmlEngine *>(d->m_engine),
-                SIGNAL(remoteStartupRequested()),
-                SIGNAL(engineRequestSetup()));
             break;
         case QmlCppEngineType:
             d->m_engine = createQmlCppEngine(sp);
-            if (GdbEngine *embeddedGdbEngine = d->gdbEngine())
-                d->initGdbEngine(embeddedGdbEngine);
             break;
         case LldbEngineType:
             d->m_engine = createLldbEngine(sp);
@@ -740,56 +693,6 @@ DebuggerEngine *DebuggerRunControl::engine()
 {
     QTC_ASSERT(d->m_engine, /**/);
     return d->m_engine;
-}
-
-void DebuggerRunControl::handleRemoteSetupDone(int gdbServerPort, int qmlPort)
-{
-    // FIXME: Use virtual functions?
-    if (QmlEngine *qmlEngine = qobject_cast<QmlEngine *>(d->m_engine)) {
-        qmlEngine->handleRemoteSetupDone(qmlPort);
-        return;
-    }
-
-    AbstractGdbAdapter *adapter = d->gdbAdapter();
-    QTC_ASSERT(adapter, return);
-
-    RemotePlainGdbAdapter *rpga = qobject_cast<RemotePlainGdbAdapter *>(adapter);
-    if (rpga) {
-        rpga->handleSetupDone(qmlPort);
-        return;
-    }
-
-    RemoteGdbServerAdapter *rgsa = qobject_cast<RemoteGdbServerAdapter *>(adapter);
-    if (rgsa) {
-        rgsa->handleSetupDone(gdbServerPort, qmlPort);
-        return;
-    }
-    QTC_ASSERT(false, /**/);
-}
-
-void DebuggerRunControl::handleRemoteSetupFailed(const QString &message)
-{
-    // FIXME: Use virtual functions?
-    if (QmlEngine *qmlEngine = qobject_cast<QmlEngine *>(d->m_engine)) {
-        qmlEngine->handleRemoteSetupFailed(message);
-        return;
-    }
-
-    AbstractGdbAdapter *adapter = d->gdbAdapter();
-    QTC_ASSERT(adapter, return);
-
-    RemotePlainGdbAdapter *rpga = qobject_cast<RemotePlainGdbAdapter *>(adapter);
-    if (rpga) {
-        rpga->handleSetupFailed(message);
-        return;
-    }
-
-    RemoteGdbServerAdapter *rgsa = qobject_cast<RemoteGdbServerAdapter *>(adapter);
-    if (rgsa) {
-        rgsa->handleSetupFailed(message);
-        return;
-    }
-    QTC_ASSERT(false, /**/);
 }
 
 RunConfiguration *DebuggerRunControl::runConfiguration() const

src/plugins/debugger/debuggerrunner.h
@@ -106,9 +106,6 @@ public:
 
     void showMessage(const QString &msg, int channel);
 
-    void handleRemoteSetupDone(int gdbServerPort, int qmlPort);
-    void handleRemoteSetupFailed(const QString &message);
-
     static bool checkDebugConfiguration(int toolChain,
                                  QString *errorMessage,
                                  QString *settingsCategory = 0,

src/plugins/debugger/gdb/abstractgdbadapter.cpp
@@ -145,5 +145,16 @@ void AbstractGdbAdapter::showMessage(const QString &msg, int channel, int timeou
     m_engine->showMessage(msg, channel, timeout);
 }
 
+void AbstractGdbAdapter::handleRemoteSetupDone(int gdbServerPort, int qmlPort)
+{
+    Q_UNUSED(gdbServerPort);
+    Q_UNUSED(qmlPort);
+}
+
+void AbstractGdbAdapter::handleRemoteSetupFailed(const QString &reason)
+{
+    Q_UNUSED(reason);
+}
+
 } // namespace Internal
 } // namespace Debugger

commit ee6bf38ea21e3b644b87fed489be5f14758a818a

Maemo: Fix remote debugging.
Was broken by 77e3bfe8451343be5478027154a027cbcfe4b778.

src/plugins/debugger/gdb/remotegdbserveradapter.cpp
@@ -159,7 +159,7 @@ void RemoteGdbServerAdapter::uploadProcFinished()
         && m_uploadProc.exitCode() == 0)
         handleSetupDone();
     else
-        handleSetupFailed(m_uploadProc.errorString());
+        handleRemoteSetupFailed(m_uploadProc.errorString());
 }
 
 void RemoteGdbServerAdapter::setupInferior()
@@ -300,7 +300,7 @@ void RemoteGdbServerAdapter::shutdownAdapter()
     m_engine->notifyAdapterShutdownOk();
 }
 
-void RemoteGdbServerAdapter::handleSetupDone(int gdbServerPort, int qmlPort)
+void RemoteGdbServerAdapter::handleRemoteSetupDone(int gdbServerPort, int qmlPort)
 {
     QTC_ASSERT(state() == EngineSetupRequested, qDebug() << state());
 
@@ -323,7 +323,7 @@ void RemoteGdbServerAdapter::handleSetupDone()
         m_engine->handleAdapterStarted();
 }
 
-void RemoteGdbServerAdapter::handleSetupFailed(const QString &reason)
+void RemoteGdbServerAdapter::handleRemoteSetupFailed(const QString &reason)
 {
     QTC_ASSERT(state() == EngineSetupRequested, qDebug() << state());
 

src/plugins/debugger/gdb/remotegdbserveradapter.h
@@ -48,8 +48,6 @@ class RemoteGdbServerAdapter : public AbstractGdbAdapter
 
 public:
     RemoteGdbServerAdapter(GdbEngine *engine, int toolChainType, QObject *parent = 0);
-    void handleSetupDone(int gdbServerPort, int qmlPort);
-    void handleSetupFailed(const QString &reason);
 
 private:
     DumperHandling dumperHandling() const;
@@ -81,6 +79,9 @@ private:
     Q_SLOT void readUploadStandardError();
     Q_SLOT void uploadProcError(QProcess::ProcessError error);
     Q_SLOT void uploadProcFinished();
+
+    virtual void handleRemoteSetupDone(int gdbServerPort, int qmlPort);
+    virtual void handleRemoteSetupFailed(const QString &reason);
 
     void handleSetTargetAsync(const GdbResponse &response);
     void handleFileExecAndSymbols(const GdbResponse &response);

src/plugins/debugger/gdb/remoteplaingdbadapter.cpp
@@ -100,7 +100,7 @@ void RemotePlainGdbAdapter::shutdownAdapter()
     m_engine->notifyAdapterShutdownOk();
 }
 
-void RemotePlainGdbAdapter::handleSetupDone(int gdbServerPort, int qmlPort)
+void RemotePlainGdbAdapter::handleRemoteSetupDone(int gdbServerPort, int qmlPort)
 {
     Q_UNUSED(gdbServerPort);
     QTC_ASSERT(state() == EngineSetupRequested, qDebug() << state());
@@ -128,7 +128,7 @@ void RemotePlainGdbAdapter::handleGdbStartFailed()
     m_engine->handleAdapterStartFailed(m_gdbProc.errorString());
 }
 
-void RemotePlainGdbAdapter::handleSetupFailed(const QString &reason)
+void RemotePlainGdbAdapter::handleRemoteSetupFailed(const QString &reason)
 {
     QTC_ASSERT(state() == EngineSetupRequested, qDebug() << state());
 

src/plugins/debugger/gdb/remoteplaingdbadapter.h
@@ -43,8 +43,6 @@ class RemotePlainGdbAdapter : public AbstractPlainGdbAdapter
 public:
     friend class RemoteGdbProcess;
     explicit RemotePlainGdbAdapter(GdbEngine *engine, QObject *parent = 0);
-    void handleSetupDone(int gdbServerPort, int qmlPort);
-    void handleSetupFailed(const QString &reason);
 
 private slots:
     void handleGdbStarted();
@@ -56,6 +54,8 @@ private:
     void interruptInferior();
     void shutdownInferior();
     void shutdownAdapter();
+    void handleRemoteSetupDone(int gdbServerPort, int qmlPort);
+    void handleRemoteSetupFailed(const QString &reason);
     AbstractGdbProcess *gdbProc() { return &m_gdbProc; }
     DumperHandling dumperHandling() const { return DumperLoadedByGdbPreload; }
 