f6c862db4905050f70abf6ebf9af46416ad6f873
Kai Koehne <kai.koehne@nokia.com>
2011-02-04 16:41:16 +0100

    Fix linkification of Asserts and QML errors for Symbian Output
    On Symbian Qt errors are prefixed with '[Qt Message] '
    
    Task-number: QTCREATORBUG-3694

--- src/plugins/qt4projectmanager/qtoutputformatter.cpp
+++ src/plugins/qt4projectmanager/qtoutputformatter.cpp
@@ -47,12 +47,14 @@ using namespace Qt4ProjectManager;
 
 QtOutputFormatter::QtOutputFormatter(ProjectExplorer::Project *project)
     : OutputFormatter()
-    , m_qmlError(QLatin1String("^(file:///.+"    // file url
+    , m_qmlError(QLatin1String("^(?:\\[Qt Message\\] )?" // '[Qt Message] ' prefix (optional, on Symbian)
+                               "(file:///.+"      // file url
                                ":\\d+"           // colon, line
                                "(?::\\d+)?)"     // colon, column (optional)
                                ":"))             // colon
     , m_qtError(QLatin1String("Object::.*in (.*:\\d+)"))
-    , m_qtAssert(QLatin1String("^ASSERT: .* in file (.+, line \\d+)$"))
+    , m_qtAssert(QLatin1String("^(?:\\[Qt Message\\] )?" // '[Qt Message] ' prefix (optional, on Symbian)
+                               "ASSERT: .* in file (.+, line \\d+)$"))
     , m_qtTestFail(QLatin1String("^   Loc: \\[(.*)\\]$"))
     , m_project(project)
 {
