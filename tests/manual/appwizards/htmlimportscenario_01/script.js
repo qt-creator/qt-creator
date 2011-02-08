d2cfe6cb05bf9f4134103e9d501359dd77b61156
Alessandro Portale <alessandro.portale@nokia.com>
2011-02-08 18:07:07 +0100

    Test data for html5 import

---
+++ tests/manual/appwizards/htmlimportscenario_01/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $("div").mousedown(function () {
+        $(this).hide(400);
+    });
+    $(".div").click(function () {
+        $(this).hide(400);
+    });
+});
