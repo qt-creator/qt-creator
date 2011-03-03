qt-creator/qt-creator

commit d28dc2d98929cc3ba9e6b80b10751bb4534775da

coreplugin: Make RightPaneWidget more widely usable.
RightPaneWidget now has setWidget() and is no longer intimately tied
to the help plugin. Help plugin now uses setWidget().

Reviewed-By: dt

src/plugins/coreplugin/rightpane.cpp
@@ -34,7 +34,6 @@
 #include "rightpane.h"
 
 #include <coreplugin/modemanager.h>
-#include <extensionsystem/pluginmanager.h>
 
 #include <QtCore/QSettings>
 
@@ -129,51 +128,36 @@ void RightPanePlaceHolder::currentModeChanged(Core::IMode *mode)
 RightPaneWidget *RightPaneWidget::m_instance = 0;
 
 RightPaneWidget::RightPaneWidget()
-    : m_shown(true), m_width(0)
+    : m_shown(true), m_width(0), m_widget(0)
 {
     m_instance = this;
 
     QVBoxLayout *layout = new QVBoxLayout;
     layout->setMargin(0);
     setLayout(layout);
-
-    ExtensionSystem::PluginManager *pm = ExtensionSystem::PluginManager::instance();
-
-    BaseRightPaneWidget *rpw = pm->getObject<BaseRightPaneWidget>();
-    if (rpw) {
-        layout->addWidget(rpw->widget());
-    }
-    connect(pm, SIGNAL(objectAdded(QObject *)),
-            this, SLOT(objectAdded(QObject *)));
-    connect(pm, SIGNAL(aboutToRemoveObject(QObject *)),
-            this, SLOT(aboutToRemoveObject(QObject *)));
 }
 
 RightPaneWidget::~RightPaneWidget()
 {
+    clearWidget();
     m_instance = 0;
-}
-
-void RightPaneWidget::objectAdded(QObject *obj)
-{
-    BaseRightPaneWidget *rpw = qobject_cast<BaseRightPaneWidget *>(obj);
-    if (rpw) {
-        layout()->addWidget(rpw->widget());
-        setFocusProxy(rpw->widget());
-    }
-}
-
-void RightPaneWidget::aboutToRemoveObject(QObject *obj)
-{
-    BaseRightPaneWidget *rpw = qobject_cast<BaseRightPaneWidget *>(obj);
-    if (rpw) {
-        delete rpw->widget();
-    }
 }
 
 RightPaneWidget *RightPaneWidget::instance()
 {
     return m_instance;
+}
+
+void RightPaneWidget::setWidget(QWidget *widget)
+{
+    clearWidget();
+    m_widget = widget;
+    if (m_widget) {
+        m_widget->setParent(this);
+        layout()->addWidget(m_widget);
+        setFocusProxy(m_widget);
+        m_widget->show();
+    }
 }
 
 int RightPaneWidget::storedWidth()
@@ -227,21 +211,11 @@ bool RightPaneWidget::isShown()
     return m_shown;
 }
 
-/////
-// BaseRightPaneWidget
-/////
-
-BaseRightPaneWidget::BaseRightPaneWidget(QWidget *widget)
+void RightPaneWidget::clearWidget()
 {
-    m_widget = widget;
-}
-
-BaseRightPaneWidget::~BaseRightPaneWidget()
-{
-
-}
-
-QWidget *BaseRightPaneWidget::widget() const
-{
-    return m_widget;
+    if (m_widget) {
+        m_widget->hide();
+        m_widget->setParent(0);
+        m_widget = 0;
+    }
 }

src/plugins/coreplugin/rightpane.h
@@ -47,13 +47,6 @@ namespace Core {
 class IMode;
 class RightPaneWidget;
 
-// TODO: The right pane works only for the help plugin atm.  It can't cope
-// with more than one plugin publishing objects they want in the right pane
-// For that the API would need to be different. (Might be that instead of
-// adding objects to the pool, there should be a method
-// RightPaneWidget::setWidget(QWidget *w) Anyway if a second plugin wants to
-// show something there, redesign this API
-
 class CORE_EXPORT RightPanePlaceHolder : public QWidget
 {
     friend class Core::RightPaneWidget;
@@ -73,21 +66,6 @@ private:
     static RightPanePlaceHolder* m_current;
 };
 
-
-class CORE_EXPORT BaseRightPaneWidget : public QObject
-{
-    Q_OBJECT
-
-public:
-    BaseRightPaneWidget(QWidget *widget);
-    ~BaseRightPaneWidget();
-    QWidget *widget() const;
-
-private:
-    QWidget *m_widget;
-};
-
-
 class CORE_EXPORT RightPaneWidget : public QWidget
 {
     Q_OBJECT
@@ -104,18 +82,18 @@ public:
 
     static RightPaneWidget *instance();
 
+    void setWidget(QWidget *widget);
+
     int storedWidth();
 
 protected:
     void resizeEvent(QResizeEvent *);
 
-private slots:
-    void objectAdded(QObject *obj);
-    void aboutToRemoveObject(QObject *obj);
-
 private:
+    void clearWidget();
     bool m_shown;
     int m_width;
+    QWidget *m_widget;
     static RightPaneWidget *m_instance;
 };
 

src/plugins/help/helpplugin.cpp
@@ -554,8 +554,6 @@ void HelpPlugin::createRightPaneContextViewer()
     layout->addWidget(toolButton(close));
 
     QWidget *rightPaneSideBar = new QWidget;
-    addAutoReleasedObject(new Core::BaseRightPaneWidget(rightPaneSideBar));
-
     m_helpViewerForSideBar = new HelpViewer(qreal(0.0), rightPaneSideBar);
     connect(m_helpViewerForSideBar, SIGNAL(openFindToolBar()), this,
         SLOT(openFindToolBar()));
@@ -834,6 +832,7 @@ HelpViewer* HelpPlugin::viewerForContextMode()
     }
 
     if (placeHolder && showSideBySide) {
+        RightPaneWidget::instance()->setWidget(m_helpViewerForSideBar->parentWidget());
         RightPaneWidget::instance()->setShown(true);
         createRightPaneContextViewer();
         return m_helpViewerForSideBar;

commit aa5b5e237c4d8219a8cf9ce8c682bc4a0be4f483

Help: Fix leaking side bar widget and find placeholder.
Breakage introduced by d28dc2d98929cc3ba9e6b80b10751bb4534775da.

Reviewed-by: con

src/plugins/coreplugin/rightpane.cpp
@@ -128,7 +128,7 @@ void RightPanePlaceHolder::currentModeChanged(Core::IMode *mode)
 RightPaneWidget *RightPaneWidget::m_instance = 0;
 
 RightPaneWidget::RightPaneWidget()
-    : m_shown(true), m_width(0), m_widget(0)
+    : m_shown(true), m_width(0)
 {
     m_instance = this;
 

src/plugins/coreplugin/rightpane.h
@@ -37,6 +37,7 @@
 #include "core_global.h"
 
 #include <QtGui/QWidget>
+#include <QtCore/QPointer>
 
 QT_BEGIN_NAMESPACE
 class QSettings;
@@ -93,7 +94,7 @@ private:
     void clearWidget();
     bool m_shown;
     int m_width;
-    QWidget *m_widget;
+    QPointer<QWidget> m_widget;
     static RightPaneWidget *m_instance;
 };
 

src/plugins/help/helpplugin.cpp
@@ -128,6 +128,7 @@ HelpPlugin::HelpPlugin()
     : m_mode(0),
     m_core(0),
     m_centralWidget(0),
+    m_rightPaneSideBarWidget(0),
     m_helpViewerForSideBar(0),
     m_contentItem(0),
     m_indexItem(0),
@@ -145,6 +146,7 @@ HelpPlugin::HelpPlugin()
 
 HelpPlugin::~HelpPlugin()
 {
+    delete m_rightPaneSideBarWidget;
 }
 
 bool HelpPlugin::initialize(const QStringList &arguments, QString *error)
@@ -328,7 +330,9 @@ bool HelpPlugin::initialize(const QStringList &arguments, QString *error)
 
     if (QLayout *layout = m_centralWidget->layout()) {
         layout->setSpacing(0);
-        layout->addWidget(new Core::FindToolBarPlaceHolder(m_centralWidget));
+        Core::FindToolBarPlaceHolder *fth = new Core::FindToolBarPlaceHolder(m_centralWidget);
+        fth->setObjectName(QLatin1String("HelpFindToolBarPlaceHolder"));
+        layout->addWidget(fth);
     }
 
     HelpIndexFilter *helpIndexFilter = new HelpIndexFilter();
@@ -526,7 +530,7 @@ void HelpPlugin::resetFilter()
 
 void HelpPlugin::createRightPaneContextViewer()
 {
-    if (m_helpViewerForSideBar)
+    if (m_rightPaneSideBarWidget)
         return;
 
     Utils::StyledBar *toolBar = new Utils::StyledBar();
@@ -553,21 +557,23 @@ void HelpPlugin::createRightPaneContextViewer()
     layout->addStretch();
     layout->addWidget(toolButton(close));
 
-    QWidget *rightPaneSideBar = new QWidget;
-    m_helpViewerForSideBar = new HelpViewer(qreal(0.0), rightPaneSideBar);
+    m_rightPaneSideBarWidget = new QWidget;
+    m_helpViewerForSideBar = new HelpViewer(qreal(0.0));
     connect(m_helpViewerForSideBar, SIGNAL(openFindToolBar()), this,
         SLOT(openFindToolBar()));
 #if !defined(QT_NO_WEBKIT)
     m_helpViewerForSideBar->pageAction(QWebPage::OpenLinkInNewWindow)->setVisible(false);
 #endif
 
-    QVBoxLayout *rightPaneLayout = new QVBoxLayout(rightPaneSideBar);
+    QVBoxLayout *rightPaneLayout = new QVBoxLayout(m_rightPaneSideBarWidget);
     rightPaneLayout->setMargin(0);
     rightPaneLayout->setSpacing(0);
     rightPaneLayout->addWidget(toolBar);
     rightPaneLayout->addWidget(m_helpViewerForSideBar);
-    rightPaneLayout->addWidget(new Core::FindToolBarPlaceHolder(rightPaneSideBar));
-    rightPaneSideBar->setFocusProxy(m_helpViewerForSideBar);
+    Core::FindToolBarPlaceHolder *fth = new Core::FindToolBarPlaceHolder(m_rightPaneSideBarWidget);
+    fth->setObjectName(QLatin1String("HelpRightPaneFindToolBarPlaceHolder"));
+    rightPaneLayout->addWidget(fth);
+    m_rightPaneSideBarWidget->setFocusProxy(m_helpViewerForSideBar);
 
     Aggregation::Aggregate *agg = new Aggregation::Aggregate();
     agg->add(m_helpViewerForSideBar);
@@ -832,7 +838,7 @@ HelpViewer* HelpPlugin::viewerForContextMode()
     }
 
     if (placeHolder && showSideBySide) {
-        RightPaneWidget::instance()->setWidget(m_helpViewerForSideBar->parentWidget());
+        RightPaneWidget::instance()->setWidget(m_rightPaneSideBarWidget);
         RightPaneWidget::instance()->setShown(true);
         createRightPaneContextViewer();
         return m_helpViewerForSideBar;

src/plugins/help/helpplugin.h
@@ -141,6 +141,7 @@ private:
     HelpMode *m_mode;
     Core::ICore *m_core;
     CentralWidget *m_centralWidget;
+    QWidget *m_rightPaneSideBarWidget;
     HelpViewer *m_helpViewerForSideBar;
 
     Core::SideBarItem *m_contentItem;