d8f83e2b1b3730731b2bf781587fd2a65fb281b5
Kai Koehne <kai.koehne@nokia.com>
2011-03-25 09:25:17 +0100

    QmlProfiler: Add license headers

--- src/plugins/qmlprofiler/qmlprofilerconstants.h
+++ src/plugins/qmlprofiler/qmlprofilerconstants.h
@@ -1,5 +1,38 @@
-#ifndef QMLPROFILERPLUGINCONSTANTS_H
-#define QMLPROFILERPLUGINCONSTANTS_H
+/**************************************************************************
+**
+** This file is part of Qt Creator
+**
+** Copyright (c) 2011 Nokia Corporation and/or its subsidiary(-ies).
+**
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+** No Commercial Usage
+**
+** This file contains pre-release code and may not be distributed.
+** You may use this file in accordance with the terms and conditions
+** contained in the Technology Preview License Agreement accompanying
+** this package.
+**
+** GNU Lesser General Public License Usage
+**
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights.  These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**************************************************************************/
+
+#ifndef QMLPROFILERCONSTANTS_H
+#define QMLPROFILERCONSTANTS_H
 
 namespace QmlProfiler {
 namespace Constants {
@@ -10,5 +43,5 @@ namespace Constants {
 } // namespace QmlProfiler
 } // namespace Constants
 
-#endif // QMLPROFILERPLUGINCONSTANTS_H
+#endif // QMLPROFILERCONSTANTS_H
 
