82e73ac4233247aef74c5466d3ed3b4ad64112e4
Christian Kandeler <christian.kandeler@nokia.com>
2011-03-29 14:11:08 +0200

    Meego: We still needed an additional option for rpm...
    Replacing files is dangerous, but otherwise we'd have to e.g. change
    the spec file's "Release" field on every deployment, which would
    be annoying for the user.

--- src/plugins/qt4projectmanager/qt-maemo/maemodeploystep.cpp
+++ src/plugins/qt4projectmanager/qt-maemo/maemodeploystep.cpp
@@ -729,7 +729,8 @@ void MaemoDeployStep::runPackageInstaller(const QString &packageFilePath)
 
     writeOutput(tr("Installing package to device..."));
     const QByteArray installCommand = packagingStep()->debBasedMaemoTarget()
-        ? "dpkg -i --no-force-downgrade" : "rpm -Uhv --replacepkgs";
+        ? "dpkg -i --no-force-downgrade"
+        : "rpm -Uhv --replacepkgs --replacefiles";
     QByteArray cmd = MaemoGlobal::remoteSudo().toUtf8() + ' '
         + installCommand + ' ' + packageFilePath.toUtf8();
     if (removeAfterInstall)
