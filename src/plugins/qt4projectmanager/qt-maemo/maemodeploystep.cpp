0166c18c8e59831eb958917f3f3c1d13a2ab3044
Christian Kandeler <christian.kandeler@nokia.com>
2011-03-29 12:16:46 +0200

    Meego: Allow re-installation of RPM package.
    This is a backport of 34030efbc45bbdc2300c724219d01c1a024d4cff.

--- src/plugins/qt4projectmanager/qt-maemo/maemodeploystep.cpp
+++ src/plugins/qt4projectmanager/qt-maemo/maemodeploystep.cpp
@@ -729,7 +729,7 @@ void MaemoDeployStep::runPackageInstaller(const QString &packageFilePath)
 
     writeOutput(tr("Installing package to device..."));
     const QByteArray installCommand = packagingStep()->debBasedMaemoTarget()
-        ? "dpkg -i --no-force-downgrade" : "rpm -Uhv";
+        ? "dpkg -i --no-force-downgrade" : "rpm -Uhv --replacepkgs";
     QByteArray cmd = MaemoGlobal::remoteSudo().toUtf8() + ' '
         + installCommand + ' ' + packageFilePath.toUtf8();
     if (removeAfterInstall)
