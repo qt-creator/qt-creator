qt-creator/qt-creator

commit f023b1ddbd3ee9ccb04b6b1263df565e18db5246

debugger: remove lldb specific option from debugger menu
Reviewed-By: hjk

src/plugins/debugger/debuggerplugin.cpp
@@ -2708,9 +2708,11 @@ void DebuggerPluginPrivate::extensionsInitialized()
     act->setText(tr("Start and Debug External Application..."));
     connect(act, SIGNAL(triggered()), SLOT(startExternalApplication()));
 
+#ifdef WITH_LLDB
     act = m_startRemoteLldbAction = new QAction(this);
     act->setText(tr("Start and Debug External Application with External Engine..."));
     connect(act, SIGNAL(triggered()), SLOT(startRemoteEngine()));
+#endif
 
     act = m_attachExternalAction = new QAction(this);
     act->setText(tr("Attach to Running External Application..."));

commit a0925af7d1007d5ca42e20c9dc18df348fdf9518

Fix startup crash on Mac
Introduced by f023b1ddbd3ee9ccb04b6b1263df565e18db5246

Reviewed-by: Kai Koehne

src/plugins/debugger/debuggerplugin.cpp
@@ -2774,10 +2774,12 @@ void DebuggerPluginPrivate::extensionsInitialized()
     cmd->setAttribute(Command::CA_Hide);
     mstart->addAction(cmd, CC::G_DEFAULT_ONE);
 
+#ifdef WITH_LLDB
     cmd = am->registerAction(m_startRemoteLldbAction,
         Constants::STARTREMOTELLDB, globalcontext);
     cmd->setAttribute(Command::CA_Hide);
     mstart->addAction(cmd, CC::G_DEFAULT_ONE);
+#endif
 
     if (m_startRemoteCdbAction) {
         cmd = am->registerAction(m_startRemoteCdbAction,