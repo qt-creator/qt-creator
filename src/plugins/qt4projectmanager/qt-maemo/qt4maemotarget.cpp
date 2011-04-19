3fcc88fc84a7ad667b38394eb1f97b805dd41aa7
Christian Kandeler <christian.kandeler@nokia.com>
2011-04-19 15:32:34 +0200

    Maemo: Get rid of some Debian control file entries.
    According to the Harmattan people, support for them has been dropped
    and they are afraid users could get confused.

--- src/plugins/qt4projectmanager/qt-maemo/qt4maemotarget.cpp
+++ src/plugins/qt4projectmanager/qt-maemo/qt4maemotarget.cpp
@@ -1146,8 +1146,7 @@ QString Qt4HarmattanTarget::defaultDisplayName()
 
 void Qt4HarmattanTarget::addAdditionalControlFileFields(QByteArray &controlContents)
 {
-    adaptControlFileField(controlContents, "XB-Meego-Desktop-Entry", "");
-    adaptControlFileField(controlContents, "XB-MeeGo-Desktop-Entry-Filename", "");
+    Q_UNUSED(controlContents);
 }
 
 QString Qt4HarmattanTarget::debianDirName() const
