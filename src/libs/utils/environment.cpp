fdb3b884ac3a9a74c432af5d10103a821eaa8a08
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2011-04-29 15:14:37 +0200

    Compilation.

--- src/libs/utils/environment.cpp
+++ src/libs/utils/environment.cpp
@@ -178,11 +178,11 @@ void Environment::prependOrSetLibrarySearchPath(const QString &value)
 {
 #ifdef Q_OS_MAC
     // we could set DYLD_LIBRARY_PATH on Mac but it is unnecessary in practice
-#elif Q_OS_WIN
+#elif defined(Q_OS_WIN)
     const QChar sep = QLatin1Char(';');
     const QLatin1String path("PATH");
     prependOrSet(path, QDir::toNativeSeparators(value), QString(sep));
-#elif Q_OS_UNIX
+#elif defined(Q_OS_UNIX)
     const QChar sep = QLatin1Char(':');
     const QLatin1String path("LD_LIBRARY_PATH");
     prependOrSet(path, QDir::toNativeSeparators(value), QString(sep));
