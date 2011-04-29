8714d3ae43f23abe1ecfe83d77df214347944d46
con <qtc-committer@nokia.com>
2011-04-29 16:08:53 +0200

    Uploading the doc is now handled differently.

--- scripts/createSrcAndDocPackage.sh
+++ scripts/createSrcAndDocPackage.sh
@@ -9,7 +9,7 @@ usage:
   Creates tar and zip source package from <refspec> and documentation-zip from current checkout.
   Files and directories are named after <version>.
   example:
-    $0 origin/2.0.0 2.0.0-rc1
+    $0 origin/2.2 2.2.0
 USAGE
     exit 1
 fi
@@ -21,10 +21,10 @@ echo "Creating tar archive..."
 git archive --format=tar --prefix=qt-creator-${VERSION}-src/ ${BRANCH} | gzip > qt-creator-${VERSION}-src.tar.gz || exit 1
 echo "Creating zip archive..."
 git archive --format=zip --prefix=qt-creator-${VERSION}-src/ ${BRANCH} > qt-creator-${VERSION}-src.zip || exit 1
-echo "Creating documentation..."
-rm -r doc/html
-qmake -r && make docs_online || exit 1
-cd doc
-cp -r html qt-creator-${VERSION}
-zip -r ../qt-creator-${VERSION}-doc.zip qt-creator-${VERSION}
-rm -r qt-creator-${VERSION}
+#echo "Creating documentation..."
+#rm -r doc/html
+#qmake -r && make docs_online || exit 1
+#cd doc
+#cp -r html qt-creator-${VERSION}
+#zip -r ../qt-creator-${VERSION}-doc.zip qt-creator-${VERSION}
+#rm -r qt-creator-${VERSION}
