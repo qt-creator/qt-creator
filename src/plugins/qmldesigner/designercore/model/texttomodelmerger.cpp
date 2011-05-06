qt-creator/qt-creator

commit d05992d0fa36fdba45a159491abf8fccf3622a0e

QmlJS: Stop doing significant work in constructors.
To avoid order-of-initialization problems.

Reviewed-by: Thomas Hartmann

src/plugins/qmldesigner/designercore/model/texttomodelmerger.cpp
@@ -298,10 +298,12 @@ public:
         : m_snapshot(snapshot)
         , m_doc(doc)
         , m_context(new Interpreter::Context(snapshot))
-        , m_link(m_context, snapshot, importPaths, doc, &m_diagnosticLinkMessages)
-        , m_lookupContext(LookupContext::create(doc, snapshot, *m_context, QList<AST::Node*>()))
+        , m_link(m_context, snapshot, importPaths)
         , m_scopeBuilder(m_context, doc)
     {
+        m_lookupContext = LookupContext::create(doc, *m_context, QList<AST::Node*>());
+        // cheaper than calling m_scopeBuilder.initializeRootScope()
+        *m_context = *m_lookupContext->context();
     }
 
     ~ReadingContext()

src/libs/qmljs/qmljsbind.h
@@ -43,7 +43,6 @@
 
 namespace QmlJS {
 
-class Link;
 class Document;
 
 class QMLJS_EXPORT Bind: protected AST::Visitor

src/libs/qmljs/qmljscheck.cpp
@@ -375,6 +375,7 @@ Check::Check(Document::Ptr doc, const Context *linkedContextNoScope)
           | WarnCaseWithoutFlowControlEnd | ErrCheckTypeErrors)
     , _lastValue(0)
 {
+    _scopeBuilder.initializeRootScope();
 }
 
 Check::~Check()

src/libs/qmljs/qmljsinterpreter.cpp
@@ -3286,6 +3286,7 @@ const Value *ASTPropertyReference::value(const Context *context) const
         QmlJS::Document::Ptr doc = _doc->ptr();
         Context localContext(*context);
         QmlJS::ScopeBuilder builder(&localContext, doc);
+        builder.initializeRootScope();
 
         int offset = _ast->expression->firstSourceLocation().begin();
         builder.push(ScopeAstPath(doc)(offset));

src/libs/qmljs/qmljslink.cpp
@@ -110,8 +110,7 @@ public:
     \l{Context} with \l{Link}.
 */
 
-Link::Link(Context *context, const Snapshot &snapshot, const QStringList &importPaths,
-           QHash<QString, QList<DiagnosticMessage> > *messages)
+Link::Link(Context *context, const Snapshot &snapshot, const QStringList &importPaths)
     : d_ptr(new LinkPrivate)
 {
     Q_D(Link);
@@ -120,30 +119,6 @@ Link::Link(Context *context, const Snapshot &snapshot, const QStringList &import
     d->importPaths = importPaths;
 
     d->diagnosticMessages = 0;
-    d->allDiagnosticMessages = messages;
-
-    // populate engine with types from C++
-    ModelManagerInterface *modelManager = ModelManagerInterface::instance();
-    if (modelManager) {
-        foreach (const QList<FakeMetaObject::ConstPtr> &cppTypes, modelManager->cppQmlTypes()) {
-            engine()->cppQmlTypes().load(engine(), cppTypes);
-        }
-    }
-
-    linkImports();
-}
-
-Link::Link(Context *context, const Snapshot &snapshot, const QStringList &importPaths,
-           const Document::Ptr &doc, QList<DiagnosticMessage> *messages)
-    : d_ptr(new LinkPrivate)
-{
-    Q_D(Link);
-    d->context = context;
-    d->snapshot = snapshot;
-    d->importPaths = importPaths;
-
-    d->doc = doc;
-    d->diagnosticMessages = messages;
     d->allDiagnosticMessages = 0;
 
     // populate engine with types from C++
@@ -153,7 +128,20 @@ Link::Link(Context *context, const Snapshot &snapshot, const QStringList &import
             engine()->cppQmlTypes().load(engine(), cppTypes);
         }
     }
+}
 
+void Link::operator()(QHash<QString, QList<DiagnosticMessage> > *messages)
+{
+    Q_D(Link);
+    d->allDiagnosticMessages = messages;
+    linkImports();
+}
+
+void Link::operator()(const Document::Ptr &doc, QList<DiagnosticMessage> *messages)
+{
+    Q_D(Link);
+    d->doc = doc;
+    d->diagnosticMessages = messages;
     linkImports();
 }
 

src/libs/qmljs/qmljslink.h
@@ -54,14 +54,15 @@ class QMLJS_EXPORT Link
     Q_DECLARE_TR_FUNCTIONS(QmlJS::Link)
 
 public:
-    // Link all documents in snapshot, collecting all diagnostic messages
     Link(Interpreter::Context *context, const Snapshot &snapshot,
-         const QStringList &importPaths, QHash<QString, QList<DiagnosticMessage> > *messages = 0);
+         const QStringList &importPaths);
+
+    // Link all documents in snapshot, collecting all diagnostic messages (if messages != 0)
+    void operator()(QHash<QString, QList<DiagnosticMessage> > *messages = 0);
 
     // Link all documents in snapshot, appending the diagnostic messages
     // for 'doc' in 'messages'
-    Link(Interpreter::Context *context, const Snapshot &snapshot,
-         const QStringList &importPaths, const Document::Ptr &doc, QList<DiagnosticMessage> *messages);
+    void operator()(const Document::Ptr &doc, QList<DiagnosticMessage> *messages);
 
     ~Link();
 

src/libs/qmljs/qmljslookupcontext.cpp
@@ -48,18 +48,21 @@ public:
     {
         // since we keep the document and snapshot around, we don't need to keep the Link instance
         Link link(&context, snapshot, ModelManagerInterface::instance()->importPaths());
+        link();
 
         ScopeBuilder scopeBuilder(&context, doc);
+        scopeBuilder.initializeRootScope();
         scopeBuilder.push(path);
     }
 
-    LookupContextData(Document::Ptr doc, const Snapshot &snapshot,
+    LookupContextData(Document::Ptr doc,
                       const Interpreter::Context &linkedContextWithoutScope,
                       const QList<AST::Node *> &path)
         : context(linkedContextWithoutScope),
           doc(doc)
     {
         ScopeBuilder scopeBuilder(&context, doc);
+        scopeBuilder.initializeRootScope();
         scopeBuilder.push(path);
     }
 
@@ -73,10 +76,10 @@ LookupContext::LookupContext(Document::Ptr doc, const Snapshot &snapshot, const 
 {
 }
 
-LookupContext::LookupContext(const Document::Ptr doc, const Snapshot &snapshot,
+LookupContext::LookupContext(const Document::Ptr doc,
                              const Interpreter::Context &linkedContextWithoutScope,
                              const QList<AST::Node *> &path)
-    : d(new LookupContextData(doc, snapshot, linkedContextWithoutScope, path))
+    : d(new LookupContextData(doc, linkedContextWithoutScope, path))
 {
 }
 
@@ -90,11 +93,11 @@ LookupContext::Ptr LookupContext::create(Document::Ptr doc, const Snapshot &snap
     return ptr;
 }
 
-LookupContext::Ptr LookupContext::create(const Document::Ptr doc, const Snapshot &snapshot,
+LookupContext::Ptr LookupContext::create(const Document::Ptr doc,
                                          const Interpreter::Context &linkedContextWithoutScope,
                                          const QList<AST::Node *> &path)
 {
-    Ptr ptr(new LookupContext(doc, snapshot, linkedContextWithoutScope, path));
+    Ptr ptr(new LookupContext(doc, linkedContextWithoutScope, path));
     return ptr;
 }
 

src/libs/qmljs/qmljslookupcontext.h
@@ -54,7 +54,7 @@ class QMLJS_EXPORT LookupContext
     Q_DISABLE_COPY(LookupContext)
 
     LookupContext(const Document::Ptr doc, const Snapshot &snapshot, const QList<AST::Node *> &path);
-    LookupContext(const Document::Ptr doc, const Snapshot &snapshot,
+    LookupContext(const Document::Ptr doc,
                   const Interpreter::Context &linkedContextWithoutScope,
                   const QList<AST::Node *> &path);
 
@@ -66,7 +66,7 @@ public:
     // consider using SemanticInfo::lookupContext instead, it's faster
     static Ptr create(const Document::Ptr doc, const Snapshot &snapshot,
                       const QList<AST::Node *> &path);
-    static Ptr create(const Document::Ptr doc, const Snapshot &snapshot,
+    static Ptr create(const Document::Ptr doc,
                       const Interpreter::Context &linkedContextWithoutScope,
                       const QList<AST::Node *> &path);
 

commit ed1fbc940ebeaa2230290f122af66377470b1366

QmlDesigner.textToModelMerger: fix for link
Fixes: d05992d0fa36fdba45a159491abf8fccf3622a0e

Reviewed-by: Christian Kamm

src/plugins/qmldesigner/designercore/model/texttomodelmerger.cpp
@@ -301,6 +301,7 @@ public:
         , m_link(m_context, snapshot, importPaths)
         , m_scopeBuilder(m_context, doc)
     {
+        m_link(doc, &m_diagnosticLinkMessages);
         m_lookupContext = LookupContext::create(doc, *m_context, QList<AST::Node*>());
         // cheaper than calling m_scopeBuilder.initializeRootScope()
         *m_context = *m_lookupContext->context();