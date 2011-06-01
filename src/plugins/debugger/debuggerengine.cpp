362aef67e005a93172f4de2a61a80e433ac11463
hjk <qtc-committer@nokia.com>
2011-06-01 18:58:55 +0200

    debugger: don't crash when telling a dead runcontrol about a pid
    Change-Id: I1cf33f3587de2e8e35a74a15aa797539f2f58014
    Reviewed-on: http://codereview.qt.nokia.com/318
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: hjk <qthjk@ovi.com>

--- src/plugins/debugger/debuggerengine.cpp
+++ src/plugins/debugger/debuggerengine.cpp
@@ -1176,7 +1176,8 @@ void DebuggerEngine::notifyInferiorPid(qint64 pid)
         return;
     d->m_inferiorPid = pid;
     if (pid) {
-        d->m_runControl->setApplicationProcessHandle(ProcessHandle(pid));
+        if (d->m_runControl)
+            d->m_runControl->setApplicationProcessHandle(ProcessHandle(pid));
         showMessage(tr("Taking notice of pid %1").arg(pid));
         if (d->m_startParameters.startMode == StartInternal
             || d->m_startParameters.startMode == StartExternal
