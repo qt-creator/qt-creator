b9742018ee81803062551f6ef4869769cdf620f3
Thomas Hartmann <Thomas.Hartmann@nokia.com>
2011-06-08 18:40:35 +0200

    QmlDesigner.nodeinstances: adding missing files
    Change-Id: Ied53326c2f8c92b9af28591496e118eef69790b8
    Reviewed-on: http://codereview.qt.nokia.com/410
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Thomas Hartmann <Thomas.Hartmann@nokia.com>

---
+++ src/plugins/qmldesigner/designercore/instances/changeauxiliarycommand.cpp
@@ -0,0 +1,65 @@
+/**************************************************************************
+**
+** This file is part of Qt Creator
+**
+** Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).
+**
+** Contact: Nokia Corporation (info@qt.nokia.com)
+**
+**
+** GNU Lesser General Public License Usage
+**
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this file.
+** Please review the following information to ensure the GNU Lesser General
+** Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** Other Usage
+**
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at info@qt.nokia.com.
+**
+**************************************************************************/
+
+#include "changeauxiliarycommand.h"
+
+namespace QmlDesigner {
+
+ChangeAuxiliaryCommand::ChangeAuxiliaryCommand()
+{
+}
+
+ChangeAuxiliaryCommand::ChangeAuxiliaryCommand(const QVector<PropertyValueContainer> &auxiliaryChangeVector)
+    : m_auxiliaryChangeVector (auxiliaryChangeVector)
+{
+}
+
+QVector<PropertyValueContainer> ChangeAuxiliaryCommand::auxiliaryChanges() const
+{
+    return m_auxiliaryChangeVector;
+}
+
+QDataStream &operator<<(QDataStream &out, const ChangeAuxiliaryCommand &command)
+{
+    out << command.auxiliaryChanges();
+
+    return out;
+}
+
+QDataStream &operator>>(QDataStream &in, ChangeAuxiliaryCommand &command)
+{
+    in >> command.m_auxiliaryChangeVector;
+
+    return in;
+}
+
+} // namespace QmlDesigner
---
+++ src/plugins/qmldesigner/designercore/instances/changeauxiliarycommand.h
@@ -0,0 +1,64 @@
+/**************************************************************************
+**
+** This file is part of Qt Creator
+**
+** Copyright (c) 2009 Nokia Corporation and/or its subsidiary(-ies).
+**
+** Contact: Nokia Corporation (info@qt.nokia.com)
+**
+**
+** GNU Lesser General Public License Usage
+**
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this file.
+** Please review the following information to ensure the GNU Lesser General
+** Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** Other Usage
+**
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at info@qt.nokia.com.
+**
+**************************************************************************/
+
+#ifndef CHANGEAUXILIARYCOMMAND_H
+#define CHANGEAUXILIARYCOMMAND_H
+
+#include <QMetaType>
+#include <QVector>
+
+#include "propertyvaluecontainer.h"
+
+namespace QmlDesigner {
+
+class ChangeAuxiliaryCommand
+{
+    friend QDataStream &operator>>(QDataStream &in, ChangeAuxiliaryCommand &command);
+
+public:
+    ChangeAuxiliaryCommand();
+    ChangeAuxiliaryCommand(const QVector<PropertyValueContainer> &auxiliaryChangeVector);
+
+    QVector<PropertyValueContainer> auxiliaryChanges() const;
+
+private:
+    QVector<PropertyValueContainer> m_auxiliaryChangeVector;
+};
+
+QDataStream &operator<<(QDataStream &out, const ChangeAuxiliaryCommand &command);
+QDataStream &operator>>(QDataStream &in, ChangeAuxiliaryCommand &command);
+
+} // namespace QmlDesigner
+
+Q_DECLARE_METATYPE(QmlDesigner::ChangeAuxiliaryCommand)
+
+#endif // CHANGEAUXILIARYCOMMAND_H
