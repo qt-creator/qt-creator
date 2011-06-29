83c5f81cc3e9582c41895824d0583571b6dfffa8
Christian Kamm <christian.d.kamm@nokia.com>
2011-06-29 12:03:53 +0200

    QmlJS: Merge QML and QML/JS tools menu.
    Change-Id: I0b68cbec1c44b82e4ef84b7305bfa29d660194c7
    Reviewed-on: http://codereview.qt.nokia.com/888
    Reviewed-by: Leandro T. C. Melo <leandro.melo@nokia.com>

--- src/plugins/qmljseditor/qmljseditorconstants.h
+++ src/plugins/qmljseditor/qmljseditorconstants.h
@@ -40,10 +40,10 @@ namespace Constants {
 
 // menus
 const char * const M_CONTEXT = "QML JS Editor.ContextMenu";
-const char * const M_TOOLS_QML = "QmlJSEditor.Tools.Menu";
 
 const char * const SEPARATOR1 = "QmlJSEditor.Separator1";
 const char * const SEPARATOR2 = "QmlJSEditor.Separator2";
+const char * const SEPARATOR3 = "QmlJSEditor.Separator3";
 const char * const M_REFACTORING_MENU_INSERTION_POINT = "QmlJSEditor.RefactorGroup";
 
 const char * const C_QMLJSEDITOR_ID = "QMLProjectManager.QMLJSEditor";
