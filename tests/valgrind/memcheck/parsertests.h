a4c4d67772cf243e9f694e78129bf9a24d0304b0
Thomas Murach <thomas.murach@ovi.com>
2011-07-06 15:48:52 +0000

    corrected license headers
    Change-Id: I56fa50745a80abf52488bebe6a5a72cf26932a8b
    Merge-request: 352
    Reviewed-by: Tobias Hunger <tobias.hunger@nokia.com>
    Reviewed-on: http://codereview.qt.nokia.com/1251

--- tests/valgrind/memcheck/parsertests.h
+++ tests/valgrind/memcheck/parsertests.h
@@ -1,19 +1,13 @@
 /**************************************************************************
 **
-** This file is part of Qt Creator Analyzer Tools
+** This file is part of Qt Creator
 **
-** Copyright (c) 2010 Nokia Corporation and/or its subsidiary(-ies).
+** Copyright (c) 2011 Nokia Corporation and/or its subsidiary(-ies).
 **
 ** Author: Frank Osterfeld, KDAB (frank.osterfeld@kdab.com)
 **
 ** Contact: Nokia Corporation (info@qt.nokia.com)
 **
-** Commercial Usage
-**
-** Licensees holding valid Qt Commercial licenses may use this file in
-** accordance with the Qt Commercial License Agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and Nokia.
 **
 ** GNU Lesser General Public License Usage
 **
@@ -24,8 +18,17 @@
 ** Public License version 2.1 requirements will be met:
 ** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
 **
-** If you are unsure which license is appropriate for your use, please
-** contact the sales department at http://qt.nokia.com/contact.
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** Other Usage
+**
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at info@qt.nokia.com.
 **
 **************************************************************************/
 
