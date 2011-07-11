b7d453b0c86db334a2a0f3a8391aff899a6fc6c1
Marco Bubke <marco.bubke@nokia.com>
2011-07-11 13:48:34 +0200

    QmlDesinger: Fix AnchorChanges are not exported
    This reverts commit 6f161c99f7a3a7c1389341c59aab9a787f11f072.
    
    Task-number: QTCREATORBUG-5026
    Change-Id: I51cb29887a35f8a9d0aee7d9b974aa037dc446bd
    Reviewed-on: http://codereview.qt.nokia.com/1443
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Thomas Hartmann <Thomas.Hartmann@nokia.com>

---
+++ share/qtcreator/qml/qmlpuppet/instances/anchorchangesnodeinstance.cpp
@@ -0,0 +1,114 @@
+/**************************************************************************
+
+**
+
+**  This  file  is  part  of  Qt  Creator
+
+**
+
+**  Copyright  (c)  2011  Nokia  Corporation  and/or  its  subsidiary(-ies).
+
+**
+
+**  Contact:  Nokia  Corporation  (info@nokia.com)
+
+**
+
+**  No  Commercial  Usage
+
+**
+
+**  This  file  contains  pre-release  code  and  may  not  be  distributed.
+
+**  You  may  use  this  file  in  accordance  with  the  terms  and  conditions
+
+**  contained  in  the  Technology  Preview  License  Agreement  accompanying
+
+**  this  package.
+
+**
+
+**  GNU  Lesser  General  Public  License  Usage
+
+**
+
+**  Alternatively,  this  file  may  be  used  under  the  terms  of  the  GNU  Lesser
+
+**  General  Public  License  version  2.1  as  published  by  the  Free  Software
+
+**  Foundation  and  appearing  in  the  file  LICENSE.LGPL  included  in  the
+
+**  packaging  of  this  file.   Please  review  the  following  information  to
+
+**  ensure  the  GNU  Lesser  General  Public  License  version  2.1  requirements
+
+**  will  be  met:  http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+
+**
+
+**  In  addition,  as  a  special  exception,  Nokia  gives  you  certain  additional
+
+**  rights.   These  rights  are  described  in  the  Nokia  Qt  LGPL  Exception
+
+**  version  1.1,  included  in  the  file  LGPL_EXCEPTION.txt  in  this  package.
+
+**
+
+**  If  you  have  questions  regarding  the  use  of  this  file,  please  contact
+
+**  Nokia  at  info@nokia.com.
+
+**
+
+**************************************************************************/
+
+#include "anchorchangesnodeinstance.h"
+
+namespace QmlDesigner {
+
+namespace Internal {
+
+AnchorChangesNodeInstance::AnchorChangesNodeInstance(QObject *object) :
+        ObjectNodeInstance(object)
+{
+}
+
+AnchorChangesNodeInstance::Pointer AnchorChangesNodeInstance::create(QObject *object)
+{
+    Q_ASSERT(object);
+
+    Pointer instance(new AnchorChangesNodeInstance(object));
+
+    return instance;
+}
+
+void AnchorChangesNodeInstance::setPropertyVariant(const QString &/*name*/, const QVariant &/*value*/)
+{
+}
+
+void AnchorChangesNodeInstance::setPropertyBinding(const QString &/*name*/, const QString &/*expression*/)
+{
+}
+
+QVariant AnchorChangesNodeInstance::property(const QString &/*name*/) const
+{
+    return QVariant();
+}
+
+void AnchorChangesNodeInstance::resetProperty(const QString &/*name*/)
+{
+}
+
+
+void AnchorChangesNodeInstance::reparent(const ServerNodeInstance &/*oldParentInstance*/, const QString &/*oldParentProperty*/, const ServerNodeInstance &/*newParentInstance*/, const QString &/*newParentProperty*/)
+{
+}
+
+QObject *AnchorChangesNodeInstance::changesObject() const
+{
+    return object();
+}
+
+} // namespace Internal
+
+} // namespace QmlDesigner
