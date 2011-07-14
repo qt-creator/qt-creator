6d2ac4e964f7a70fa716f3428c222ecb586d4b8d
Daniel Teske <daniel.teske@nokia.com>
2011-07-14 15:19:16 +0200

    QmlProjectRunConfiguration: Copy enabled state on cloning
    Change-Id: I9f6dcac96e4b448f008498707571a0f80a134921
    Reviewed-on: http://codereview.qt.nokia.com/1668
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Kai Koehne <kai.koehne@nokia.com>

--- src/plugins/qmlprojectmanager/qmlprojectrunconfiguration.cpp
+++ src/plugins/qmlprojectmanager/qmlprojectrunconfiguration.cpp
@@ -80,7 +80,8 @@ QmlProjectRunConfiguration::QmlProjectRunConfiguration(QmlProjectTarget *parent,
     m_qmlViewerArgs(source->m_qmlViewerArgs),
     m_projectTarget(parent),
     m_usingCurrentFile(source->m_usingCurrentFile),
-    m_userEnvironmentChanges(source->m_userEnvironmentChanges)
+    m_userEnvironmentChanges(source->m_userEnvironmentChanges),
+    m_isEnabled(source->m_isEnabled)
 {
     ctor();
     updateQtVersions();
