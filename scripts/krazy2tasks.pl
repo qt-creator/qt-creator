94d034b803fa68027695f760c9c26b76cc12659a
Friedemann Kleint <Friedemann.Kleint@nokia.com>
2011-08-29 11:57:48 +0200

    Quote arguments in krazy2tasks, add usage.
    Change-Id: I992c0e72c41d1c82788ef096c7d0b4433b6ce148
    Reviewed-on: http://codereview.qt.nokia.com/3726
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Tobias Hunger <tobias.hunger@nokia.com>

--- scripts/krazy2tasks.pl
+++ scripts/krazy2tasks.pl
@@ -3,11 +3,23 @@
 use strict;
 
 my $file = shift;
-die "No .tasks file given to save data into." unless ($file);
 
-open(PIPE, "find . -name \*.cpp -o -name \*.h | grep -v /tests/ | grep -v /3rdparty/ | xargs krazy2 --check-sets qt4 --exclude captruefalse --export textedit |") or
-    die "Could not start krazy2all, please make sure it is in your PATH.";  
-open(FILE, ">$file") or die "Failed to open \"$file\" for writing.";
+if (!defined $file)
+{
+   my $usage=<<EOF;
+Usage: krazy2tasks.pl outputfile
+
+Runs KDE's Krazy2 code scanning tool on .cpp/.h files it finds below the
+the working directory, filters and converts the output into a .tasks file
+that can be loaded into Qt Creator's build issues pane via 'Open'.
+EOF
+   print $usage;
+   exit(0);
+}
+
+open(PIPE, 'find . -name "*.cpp" -o -name "*.h" | grep -v /tests/ | grep -v /3rdparty/ | xargs krazy2 --check-sets qt4 --exclude captruefalse --export textedit |') or
+    die 'Could not start krazy2, please make sure it is in your PATH.';
+open(FILE, ">$file") or die ('Failed to open ' . $file . ' for writing.');
 
 while (<PIPE>) {
     my $line = $_;
@@ -22,4 +34,3 @@ while (<PIPE>) {
 
     print FILE "$file\t$lineno\tWARN\tKrazy: $description\n";
 }
-
