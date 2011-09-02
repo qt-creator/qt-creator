5a6c76a118689533ebc550acb202e7992c1fbace
hjk <qtc-committer@nokia.com>
2011-09-02 13:19:50 +0200

    projectexplorer: convert the string literal constants
    Change-Id: I58997c7fbe64002b4b3f38c9c85b98d2bd75dcbf
    Reviewed-on: http://codereview.qt.nokia.com/4135
    Reviewed-by: hjk <qthjk@ovi.com>

--- src/plugins/projectexplorer/projectexplorerconstants.h
+++ src/plugins/projectexplorer/projectexplorerconstants.h
@@ -38,185 +38,184 @@
 namespace ProjectExplorer {
 namespace Constants {
 
-// modes and their priorities
-const char * const MODE_SESSION         = "Project";
-const int          P_MODE_SESSION       = 85;
+// Modes and their priorities
+const char MODE_SESSION[]         = "Project";
+const int  P_MODE_SESSION         = 85;
 
-// actions
-const char * const NEWSESSION           = "ProjectExplorer.NewSession";
-const char * const NEWPROJECT           = "ProjectExplorer.NewProject";
-const char * const LOAD                 = "ProjectExplorer.Load";
-const char * const UNLOAD               = "ProjectExplorer.Unload";
-const char * const CLEARSESSION         = "ProjectExplorer.ClearSession";
-const char * const BUILDPROJECTONLY     = "ProjectExplorer.BuildProjectOnly";
-const char * const BUILD                = "ProjectExplorer.Build";
-const char * const BUILDCM              = "ProjectExplorer.BuildCM";
-const char * const BUILDSESSION         = "ProjectExplorer.BuildSession";
-const char * const REBUILDPROJECTONLY   = "ProjectExplorer.RebuildProjectOnly";
-const char * const REBUILD              = "ProjectExplorer.Rebuild";
-const char * const REBUILDCM            = "ProjectExplorer.RebuildCM";
-const char * const REBUILDSESSION       = "ProjectExplorer.RebuildSession";
-const char * const DEPLOYPROJECTONLY    = "ProjectExplorer.DeployProjectOnly";
-const char * const DEPLOY               = "ProjectExplorer.Deploy";
-const char * const DEPLOYCM             = "ProjectExplorer.DeployCM";
-const char * const DEPLOYSESSION        = "ProjectExplorer.DeploySession";
-const char * const PUBLISH              = "ProjectExplorer.Publish";
-const char * const CLEANPROJECTONLY     = "ProjectExplorer.CleanProjectOnly";
-const char * const CLEAN                = "ProjectExplorer.Clean";
-const char * const CLEANCM              = "ProjectExplorer.CleanCM";
-const char * const CLEANSESSION         = "ProjectExplorer.CleanSession";
-const char * const CANCELBUILD          = "ProjectExplorer.CancelBuild";
-const char * const RUN                  = "ProjectExplorer.Run";
-const char * const RUNCONTEXTMENU       = "ProjectExplorer.RunContextMenu";
-const char * const STOP                 = "ProjectExplorer.Stop";
-const char * const ADDNEWFILE           = "ProjectExplorer.AddNewFile";
-const char * const ADDEXISTINGFILES     = "ProjectExplorer.AddExistingFiles";
-const char * const ADDNEWSUBPROJECT     = "ProjectExplorer.AddNewSubproject";
-const char * const REMOVEPROJECT        = "ProjectExplorer.RemoveProject";
-const char * const OPENFILE             = "ProjectExplorer.OpenFile";
-const char * const SEARCHONFILESYSTEM   = "ProjectExplorer.SearchOnFileSystem";
-const char * const SHOWINGRAPHICALSHELL = "ProjectExplorer.ShowInGraphicalShell";
-const char * const OPENTERMIANLHERE     = "ProjectExplorer.OpenTerminalHere";
-const char * const REMOVEFILE           = "ProjectExplorer.RemoveFile";
-const char * const DELETEFILE           = "ProjectExplorer.DeleteFile";
-const char * const RENAMEFILE           = "ProjectExplorer.RenameFile";
-const char * const SETSTARTUP           = "ProjectExplorer.SetStartup";
-const char * const PROJECTTREE_COLLAPSE_ALL = "ProjectExplorer.CollapseAll";
+// Actions
+const char NEWSESSION[]           = "ProjectExplorer.NewSession";
+const char NEWPROJECT[]           = "ProjectExplorer.NewProject";
+const char LOAD[]                 = "ProjectExplorer.Load";
+const char UNLOAD[]               = "ProjectExplorer.Unload";
+const char CLEARSESSION[]         = "ProjectExplorer.ClearSession";
+const char BUILDPROJECTONLY[]     = "ProjectExplorer.BuildProjectOnly";
+const char BUILD[]                = "ProjectExplorer.Build";
+const char BUILDCM[]              = "ProjectExplorer.BuildCM";
+const char BUILDSESSION[]         = "ProjectExplorer.BuildSession";
+const char REBUILDPROJECTONLY[]   = "ProjectExplorer.RebuildProjectOnly";
+const char REBUILD[]              = "ProjectExplorer.Rebuild";
+const char REBUILDCM[]            = "ProjectExplorer.RebuildCM";
+const char REBUILDSESSION[]       = "ProjectExplorer.RebuildSession";
+const char DEPLOYPROJECTONLY[]    = "ProjectExplorer.DeployProjectOnly";
+const char DEPLOY[]               = "ProjectExplorer.Deploy";
+const char DEPLOYCM[]             = "ProjectExplorer.DeployCM";
+const char DEPLOYSESSION[]        = "ProjectExplorer.DeploySession";
+const char PUBLISH[]              = "ProjectExplorer.Publish";
+const char CLEANPROJECTONLY[]     = "ProjectExplorer.CleanProjectOnly";
+const char CLEAN[]                = "ProjectExplorer.Clean";
+const char CLEANCM[]              = "ProjectExplorer.CleanCM";
+const char CLEANSESSION[]         = "ProjectExplorer.CleanSession";
+const char CANCELBUILD[]          = "ProjectExplorer.CancelBuild";
+const char RUN[]                  = "ProjectExplorer.Run";
+const char RUNCONTEXTMENU[]       = "ProjectExplorer.RunContextMenu";
+const char STOP[]                 = "ProjectExplorer.Stop";
+const char ADDNEWFILE[]           = "ProjectExplorer.AddNewFile";
+const char ADDEXISTINGFILES[]     = "ProjectExplorer.AddExistingFiles";
+const char ADDNEWSUBPROJECT[]     = "ProjectExplorer.AddNewSubproject";
+const char REMOVEPROJECT[]        = "ProjectExplorer.RemoveProject";
+const char OPENFILE[]             = "ProjectExplorer.OpenFile";
+const char SEARCHONFILESYSTEM[]   = "ProjectExplorer.SearchOnFileSystem";
+const char SHOWINGRAPHICALSHELL[] = "ProjectExplorer.ShowInGraphicalShell";
+const char OPENTERMIANLHERE[]     = "ProjectExplorer.OpenTerminalHere";
+const char REMOVEFILE[]           = "ProjectExplorer.RemoveFile";
+const char DELETEFILE[]           = "ProjectExplorer.DeleteFile";
+const char RENAMEFILE[]           = "ProjectExplorer.RenameFile";
+const char SETSTARTUP[]           = "ProjectExplorer.SetStartup";
+const char PROJECTTREE_COLLAPSE_ALL[] = "ProjectExplorer.CollapseAll";
 
-const char * const SHOW_TASK_IN_EDITOR  = "ProjectExplorer.ShowTaskInEditor";
-const char * const VCS_ANNOTATE_TASK    = "ProjectExplorer.VcsAnnotateTask";
-const char * const SHOW_TASK_OUTPUT     = "ProjectExplorer.ShowTaskOutput";
+const char SHOW_TASK_IN_EDITOR[]  = "ProjectExplorer.ShowTaskInEditor";
+const char VCS_ANNOTATE_TASK[]    = "ProjectExplorer.VcsAnnotateTask";
+const char SHOW_TASK_OUTPUT[]     = "ProjectExplorer.ShowTaskOutput";
 
 // Run modes
-const char * const RUNMODE              = "ProjectExplorer.RunMode";
+const char RUNMODE[]              = "ProjectExplorer.RunMode";
+const char SELECTTARGET[]         = "ProjectExplorer.SelectTarget";
 
-const char * const SELECTTARGET         = "ProjectExplorer.SelectTarget";
+// Action priorities
+const int  P_ACTION_RUN            = 100;
+const int  P_ACTION_BUILDPROJECT   = 80;
 
+// Context
+const char C_PROJECTEXPLORER[]    = "Project Explorer";
+const char C_PROJECT_TREE[]       = "ProjectExplorer.ProjectTreeContext";
+const char C_APP_OUTPUT[]         = "ProjectExplorer.ApplicationOutput";
+const char C_COMPILE_OUTPUT[]     = "ProjectExplorer.CompileOutput";
 
-// action priorities
-const int          P_ACTION_RUN            = 100;
-const int          P_ACTION_BUILDPROJECT   = 80;
+// Languages
+const char LANG_CXX[]             = "CXX";
+const char LANG_QMLJS[]           = "QMLJS";
 
-// context
-const char * const C_PROJECTEXPLORER    = "Project Explorer";
-const char * const C_PROJECT_TREE       = "ProjectExplorer.ProjectTreeContext";
-const char * const C_APP_OUTPUT         = "ProjectExplorer.ApplicationOutput";
-const char * const C_COMPILE_OUTPUT     = "ProjectExplorer.CompileOutput";
+// Menus
+const char M_RECENTPROJECTS[]     = "ProjectExplorer.Menu.Recent";
+const char M_BUILDPROJECT[]       = "ProjectExplorer.Menu.Build";
+const char M_DEBUG[]              = "ProjectExplorer.Menu.Debug";
+const char M_DEBUG_STARTDEBUGGING[] = "ProjectExplorer.Menu.Debug.StartDebugging";
+const char M_SESSION[]            = "ProjectExplorer.Menu.Session";
 
-// languages
-const char * const LANG_CXX             = "CXX";
-const char * const LANG_QMLJS           = "QMLJS";
+// Menu groups
+const char G_BUILD_SESSION[]      = "ProjectExplorer.Group.BuildSession";
+const char G_BUILD_PROJECT[]      = "ProjectExplorer.Group.Build";
+const char G_BUILD_OTHER[]        = "ProjectExplorer.Group.Other";
+const char G_BUILD_RUN[]          = "ProjectExplorer.Group.Run";
+const char G_BUILD_CANCEL[]       = "ProjectExplorer.Group.BuildCancel";
 
-// menus
-const char * const M_RECENTPROJECTS     = "ProjectExplorer.Menu.Recent";
-const char * const M_BUILDPROJECT       = "ProjectExplorer.Menu.Build";
-const char * const M_DEBUG              = "ProjectExplorer.Menu.Debug";
-const char * const M_DEBUG_STARTDEBUGGING = "ProjectExplorer.Menu.Debug.StartDebugging";
-const char * const M_SESSION            = "ProjectExplorer.Menu.Session";
+// Context menus
+const char M_SESSIONCONTEXT[]     = "Project.Menu.Session";
+const char M_PROJECTCONTEXT[]     = "Project.Menu.Project";
+const char M_SUBPROJECTCONTEXT[]  = "Project.Menu.SubProject";
+const char M_FOLDERCONTEXT[]      = "Project.Menu.Folder";
+const char M_FILECONTEXT[]        = "Project.Menu.File";
+const char M_OPENFILEWITHCONTEXT[] = "Project.Menu.File.OpenWith";
 
-// menu groups
-const char * const G_BUILD_SESSION      = "ProjectExplorer.Group.BuildSession";
-const char * const G_BUILD_PROJECT      = "ProjectExplorer.Group.Build";
-const char * const G_BUILD_OTHER        = "ProjectExplorer.Group.Other";
-const char * const G_BUILD_RUN          = "ProjectExplorer.Group.Run";
-const char * const G_BUILD_CANCEL       = "ProjectExplorer.Group.BuildCancel";
+// Context menu groups
+const char G_SESSION_BUILD[]      = "Session.Group.Build";
+const char G_SESSION_FILES[]      = "Session.Group.Files";
+const char G_SESSION_OTHER[]      = "Session.Group.Other";
+const char G_SESSION_CONFIG[]     = "Session.Group.Config";
 
-// context menus
-const char * const M_SESSIONCONTEXT     = "Project.Menu.Session";
-const char * const M_PROJECTCONTEXT     = "Project.Menu.Project";
-const char * const M_SUBPROJECTCONTEXT  = "Project.Menu.SubProject";
-const char * const M_FOLDERCONTEXT      = "Project.Menu.Folder";
-const char * const M_FILECONTEXT        = "Project.Menu.File";
-const char * const M_OPENFILEWITHCONTEXT = "Project.Menu.File.OpenWith";
+const char G_PROJECT_FIRST[]      = "Project.Group.Open";
+const char G_PROJECT_BUILD[]      = "Project.Group.Build";
+const char G_PROJECT_RUN[]        = "Project.Group.Run";
+const char G_PROJECT_FILES[]      = "Project.Group.Files";
+const char G_PROJECT_TREE[]       = "Project.Group.Tree";
+const char G_PROJECT_LAST[]       = "Project.Group.Last";
 
-// context menu groups
-const char * const G_SESSION_BUILD      = "Session.Group.Build";
-const char * const G_SESSION_FILES      = "Session.Group.Files";
-const char * const G_SESSION_OTHER      = "Session.Group.Other";
-const char * const G_SESSION_CONFIG     = "Session.Group.Config";
+const char G_FOLDER_FILES[]       = "ProjectFolder.Group.Files";
+const char G_FOLDER_OTHER[]       = "ProjectFolder.Group.Other";
+const char G_FOLDER_CONFIG[]      = "ProjectFolder.Group.Config";
 
-const char * const G_PROJECT_FIRST      = "Project.Group.Open";
-const char * const G_PROJECT_BUILD      = "Project.Group.Build";
-const char * const G_PROJECT_RUN        = "Project.Group.Run";
-const char * const G_PROJECT_FILES      = "Project.Group.Files";
-const char * const G_PROJECT_TREE       = "Project.Group.Tree";
-const char * const G_PROJECT_LAST       = "Project.Group.Last";
+const char G_FILE_OPEN[]          = "ProjectFile.Group.Open";
+const char G_FILE_OTHER[]         = "ProjectFile.Group.Other";
+const char G_FILE_CONFIG[]        = "ProjectFile.Group.Config";
 
-const char * const G_FOLDER_FILES       = "ProjectFolder.Group.Files";
-const char * const G_FOLDER_OTHER       = "ProjectFolder.Group.Other";
-const char * const G_FOLDER_CONFIG      = "ProjectFolder.Group.Config";
+const char RUNMENUCONTEXTMENU[]   = "Project.RunMenu";
 
-const char * const G_FILE_OPEN          = "ProjectFile.Group.Open";
-const char * const G_FILE_OTHER         = "ProjectFile.Group.Other";
-const char * const G_FILE_CONFIG        = "ProjectFile.Group.Config";
+// File factory
+const char FILE_FACTORY_ID[]      = "ProjectExplorer.FileFactoryId";
 
-const char * const RUNMENUCONTEXTMENU   = "Project.RunMenu";
+// Icons
+const char ICON_BUILD[]           = ":/projectexplorer/images/build.png";
+const char ICON_BUILD_SMALL[]     = ":/projectexplorer/images/build_small.png";
+const char ICON_CLEAN[]           = ":/projectexplorer/images/clean.png";
+const char ICON_CLEAN_SMALL[]     = ":/projectexplorer/images/clean_small.png";
+const char ICON_REBUILD[]         = ":/projectexplorer/images/rebuild.png";
+const char ICON_REBUILD_SMALL[]   = ":/projectexplorer/images/rebuild_small.png";
+const char ICON_RUN[]             = ":/projectexplorer/images/run.png";
+const char ICON_RUN_SMALL[]       = ":/projectexplorer/images/run_small.png";
+const char ICON_DEBUG_SMALL[]     = ":/projectexplorer/images/debugger_start_small.png";
+const char ICON_STOP[]            = ":/projectexplorer/images/stop.png";
+const char ICON_STOP_SMALL[]      = ":/projectexplorer/images/stop_small.png";
+const char ICON_TOOLCHAIN_SETTINGS_CATEGORY[] = ":projectexplorer/images/build.png"; // FIXME: Need an icon!
+const char ICON_WINDOW[]          = ":/projectexplorer/images/window.png";
 
-// file id
-const char * const FILE_FACTORY_ID      = "ProjectExplorer.FileFactoryId";
+const char TASK_BUILD[]           = "ProjectExplorer.Task.Build";
 
-// icons
-const char * const ICON_BUILD           = ":/projectexplorer/images/build.png";
-const char * const ICON_BUILD_SMALL     = ":/projectexplorer/images/build_small.png";
-const char * const ICON_CLEAN           = ":/projectexplorer/images/clean.png";
-const char * const ICON_CLEAN_SMALL     = ":/projectexplorer/images/clean_small.png";
-const char * const ICON_REBUILD         = ":/projectexplorer/images/rebuild.png";
-const char * const ICON_REBUILD_SMALL   = ":/projectexplorer/images/rebuild_small.png";
-const char * const ICON_RUN             = ":/projectexplorer/images/run.png";
-const char * const ICON_RUN_SMALL       = ":/projectexplorer/images/run_small.png";
-const char * const ICON_DEBUG_SMALL     = ":/projectexplorer/images/debugger_start_small.png";
-const char * const ICON_STOP            = ":/projectexplorer/images/stop.png";
-const char * const ICON_STOP_SMALL      = ":/projectexplorer/images/stop_small.png";
-const char * const ICON_TOOLCHAIN_SETTINGS_CATEGORY = ":projectexplorer/images/build.png"; // FIXME: Need an icon!
-const char * const ICON_WINDOW          = ":/projectexplorer/images/window.png";
+// Mime types
+const char C_SOURCE_MIMETYPE[]    = "text/x-csrc";
+const char C_HEADER_MIMETYPE[]    = "text/x-chdr";
+const char CPP_SOURCE_MIMETYPE[]  = "text/x-c++src";
+const char CPP_HEADER_MIMETYPE[]  = "text/x-c++hdr";
+const char FORM_MIMETYPE[]        = "application/x-designer";
+const char QML_MIMETYPE[]         = "application/x-qml";
+const char RESOURCE_MIMETYPE[]    = "application/vnd.nokia.xml.qt.resource";
 
-const char * const TASK_BUILD           = "ProjectExplorer.Task.Build";
+// Settings page
+const char PROJECTEXPLORER_SETTINGS_CATEGORY[]  = "K.ProjectExplorer";
+const char PROJECTEXPLORER_SETTINGS_TR_CATEGORY[] = QT_TRANSLATE_NOOP("ProjectExplorer", "Projects");
+const char PROJECTEXPLORER_SETTINGS_CATEGORY_ICON[]  = ":/core/images/category_project.png";
+const char PROJECTEXPLORER_SETTINGS_ID[] = "ProjectExplorer.ProjectExplorer";
+const char TOOLCHAIN_SETTINGS_CATEGORY[] = "ProjectExplorer.Settings.ToolChains";
+const char TOOLCHAIN_SETTINGS_PAGE_ID[] = "M.ProjectExplorer.ToolChainOptions";
 
-const char * const C_SOURCE_MIMETYPE = "text/x-csrc";
-const char * const C_HEADER_MIMETYPE = "text/x-chdr";
-const char * const CPP_SOURCE_MIMETYPE = "text/x-c++src";
-const char * const CPP_HEADER_MIMETYPE = "text/x-c++hdr";
-const char * const FORM_MIMETYPE = "application/x-designer";
-const char * const QML_MIMETYPE = "application/x-qml";
-const char * const RESOURCE_MIMETYPE = "application/vnd.nokia.xml.qt.resource";
-
-// settings page
-const char * const PROJECTEXPLORER_SETTINGS_CATEGORY  = "K.ProjectExplorer";
-const char * const PROJECTEXPLORER_SETTINGS_TR_CATEGORY = QT_TRANSLATE_NOOP("ProjectExplorer", "Projects");
-const char * const PROJECTEXPLORER_SETTINGS_CATEGORY_ICON  = ":/core/images/category_project.png";
-const char * const PROJECTEXPLORER_SETTINGS_ID = "ProjectExplorer.ProjectExplorer";
-const char * const TOOLCHAIN_SETTINGS_CATEGORY = "ProjectExplorer.Settings.ToolChains";
-const char * const TOOLCHAIN_SETTINGS_PAGE_ID = "M.ProjectExplorer.ToolChainOptions";
-
-// task categories
-const char * const TASK_CATEGORY_COMPILE = "Task.Category.Compile";
-const char * const TASK_CATEGORY_BUILDSYSTEM = "Task.Category.Buildsystem";
+// Task categories
+const char TASK_CATEGORY_COMPILE[] = "Task.Category.Compile";
+const char TASK_CATEGORY_BUILDSYSTEM[] = "Task.Category.Buildsystem";
 
 // Wizard category
-const char * const PROJECT_WIZARD_CATEGORY = "I.Projects"; // (after Qt)
-const char * const PROJECT_WIZARD_TR_SCOPE = "ProjectExplorer";
-const char * const PROJECT_WIZARD_TR_CATEGORY = QT_TRANSLATE_NOOP("ProjectExplorer", "Other Project");
+const char PROJECT_WIZARD_CATEGORY[] = "I.Projects"; // (after Qt)
+const char PROJECT_WIZARD_TR_SCOPE[] = "ProjectExplorer";
+const char PROJECT_WIZARD_TR_CATEGORY[] = QT_TRANSLATE_NOOP("ProjectExplorer", "Other Project");
 
 // Build step lists ids:
-const char * const BUILDSTEPS_CLEAN = "ProjectExplorer.BuildSteps.Clean";
-const char * const BUILDSTEPS_BUILD = "ProjectExplorer.BuildSteps.Build";
-const char * const BUILDSTEPS_DEPLOY = "ProjectExplorer.BuildSteps.Deploy";
+const char BUILDSTEPS_CLEAN[] = "ProjectExplorer.BuildSteps.Clean";
+const char BUILDSTEPS_BUILD[] = "ProjectExplorer.BuildSteps.Build";
+const char BUILDSTEPS_DEPLOY[] = "ProjectExplorer.BuildSteps.Deploy";
 
 // Deploy Configuration id:
-const char * const DEFAULT_DEPLOYCONFIGURATION_ID = "ProjectExplorer.DefaultDeployConfiguration";
+const char DEFAULT_DEPLOYCONFIGURATION_ID[] = "ProjectExplorer.DefaultDeployConfiguration";
 
 // ToolChain Ids
-const char * const CLANG_TOOLCHAIN_ID = "ProjectExplorer.ToolChain.Clang";
-const char * const GCC_TOOLCHAIN_ID = "ProjectExplorer.ToolChain.Gcc";
-const char * const LINUXICC_TOOLCHAIN_ID = "ProjectExplorer.ToolChain.LinuxIcc";
-const char * const MINGW_TOOLCHAIN_ID = "ProjectExplorer.ToolChain.Mingw";
-const char * const MSVC_TOOLCHAIN_ID = "ProjectExplorer.ToolChain.Msvc";
+const char CLANG_TOOLCHAIN_ID[] = "ProjectExplorer.ToolChain.Clang";
+const char GCC_TOOLCHAIN_ID[] = "ProjectExplorer.ToolChain.Gcc";
+const char LINUXICC_TOOLCHAIN_ID[] = "ProjectExplorer.ToolChain.LinuxIcc";
+const char MINGW_TOOLCHAIN_ID[] = "ProjectExplorer.ToolChain.Mingw";
+const char MSVC_TOOLCHAIN_ID[] = "ProjectExplorer.ToolChain.Msvc";
 
 // Run Configuration defaults:
 const int QML_DEFAULT_DEBUG_SERVER_PORT = 3768;
 
 // Default directory to run custom (build) commands in.
-const char * const DEFAULT_WORKING_DIR = "%{buildDir}";
+const char DEFAULT_WORKING_DIR[] = "%{buildDir}";
 
 } // namespace Constants
 } // namespace ProjectExplorer
