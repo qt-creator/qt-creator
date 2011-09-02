9fd44abcf1a815e93f4bd69a4cc31535859c11ac
hjk <qtc-committer@nokia.com>
2011-09-02 19:11:45 +0200

    actioncontainer: style
    Change-Id: I8f83b11250817425a06c063532e5662cb72fb7dc
    Reviewed-on: http://codereview.qt.nokia.com/4204
    Reviewed-by: hjk <qthjk@ovi.com>

--- src/plugins/coreplugin/actionmanager/actioncontainer.h
+++ src/plugins/coreplugin/actionmanager/actioncontainer.h
@@ -66,9 +66,9 @@ public:
 
     virtual QAction *insertLocation(const QString &group) const = 0;
     virtual void appendGroup(const QString &group) = 0;
-    virtual void addAction(Core::Command *action, const QString &group = QString()) = 0;
-    virtual void addMenu(Core::ActionContainer *menu, const QString &group = QString()) = 0;
-    virtual void addMenu(Core::ActionContainer *before, Core::ActionContainer *menu, const QString &group = QString()) = 0;
+    virtual void addAction(Command *action, const QString &group = QString()) = 0;
+    virtual void addMenu(ActionContainer *menu, const QString &group = QString()) = 0;
+    virtual void addMenu(ActionContainer *before, ActionContainer *menu, const QString &group = QString()) = 0;
 
     // clears this menu and submenus from all actions and submenus
     // doesn't destroy the submenus and commands, just removes them from their parents
