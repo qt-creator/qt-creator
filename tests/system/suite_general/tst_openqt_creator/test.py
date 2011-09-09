81689c9f56f87a64085b8e18c866f14dbf42e9e5
Bill King <bill.king@nokia.com>
2011-09-09 12:25:43 +0200

    Speed up this testcase
    While qt is large, it also takes a fair amount of time to process.
    This should still be a sufficient testcase for basic project opening.
    
    Change-Id: I319d009d22ad1607926d2cd751953a6268266f35
    Reviewed-on: http://codereview.qt-project.org/4523
    Reviewed-by: Christian Stenger <christian.stenger@nokia.com>

--- tests/system/suite_general/tst_openqt_creator/test.py
+++ tests/system/suite_general/tst_openqt_creator/test.py
@@ -7,23 +7,23 @@ def handleRefreshFinished(object, fileList):
     refreshFinishedCount += 1
 
 def main():
-    test.verify(os.path.exists(SDKPath + "/qt/projects.pro"))
     test.verify(os.path.exists(SDKPath + "/creator/qtcreator.pro"))
+    test.verify(os.path.exists(SDKPath + "/creator-test-data/speedcrunch/src/speedcrunch.pro"))
 
     startApplication("qtcreator" + SettingsPath)
 
     installLazySignalHandler("{type='CppTools::Internal::CppModelManager'}", "sourceFilesRefreshed(QStringList)", "handleRefreshFinished")
 
-    openQmakeProject(SDKPath + "/qt/projects.pro")
     openQmakeProject(SDKPath + "/creator/qtcreator.pro")
+    openQmakeProject(SDKPath + "/creator-test-data/speedcrunch/src/speedcrunch.pro")
 
     # Test that some of the expected items are in the navigation tree
-    for row, record in enumerate(testData.dataset("qt_tree.tsv")):
+    for row, record in enumerate(testData.dataset("creator_tree.tsv")):
         node = testData.field(record, "node")
         value = testData.field(record, "value")
         test.compare(waitForObject(node).text, value)
 
-    for row, record in enumerate(testData.dataset("creator_tree.tsv")):
+    for row, record in enumerate(testData.dataset("speedcrunch_tree.tsv")):
         node = testData.field(record, "node")
         value = testData.field(record, "value")
         test.compare(waitForObject(node).text, value)
@@ -35,11 +35,10 @@ def main():
     # Now check some basic lookups in the search box
 
     mouseClick(waitForObject(":*Qt Creator_Utils::FilterLineEdit", 20000), 5, 5, 0, Qt.LeftButton)
-    type(waitForObject(":*Qt Creator_Utils::FilterLineEdit", 20000), ": Qlist::QList")
-    snooze(1)
+    replaceLineEditorContent(waitForObject(":*Qt Creator_Utils::FilterLineEdit", 20000), ": Qlist::QList")
     type(waitForObject(":*Qt Creator_Utils::FilterLineEdit", 20000), "<Return>")
 
-    test.compare(wordUnderCursor(findObject(":Qt Creator_CppEditor::Internal::CPPEditorWidget")), "QList")
+    test.compare(wordUnderCursor(waitForObject(":Qt Creator_CppEditor::Internal::CPPEditorWidget")), "QList")
 
     invokeMenuItem("File", "Exit")
 
@@ -50,14 +49,7 @@ def init():
 def cleanup():
     # Make sure the .user files are gone
 
-    if os.access(SDKPath + "/qt/projects.pro.user", os.F_OK):
-        os.remove(SDKPath + "/qt/projects.pro.user")
+    if os.access(SDKPath + "/creator-test-data/speedcrunch/src/speedcrunch.pro.user", os.F_OK):
+        os.remove(SDKPath + "/creator-test-data/speedcrunch/src/speedcrunch.pro.user")
     if os.access(SDKPath + "/creator/qtcreator.pro.user", os.F_OK):
         os.remove(SDKPath + "/creator/qtcreator.pro.user")
-
-    BuildPath = glob.glob(SDKPath + "/qtcreator-build-*")
-    BuildPath += glob.glob(SDKPath + "/src/projects-build-*")
-
-    for dir in BuildPath:
-        if os.access(dir, os.F_OK):
-            shutil.rmtree(dir)
