98f44af8a22125dc54c5f21911411941a5fe5123
Christian Kamm <christian.d.kamm@nokia.com>
2011-09-21 10:29:21 +0200

    QmlJS: Lexer apply fix from Qt5.
    Change-Id: Ic11e6bad2868f56b5d7c2c063b72fb3d25c5417f
    Reviewed-on: http://codereview.qt-project.org/5294
    Reviewed-by: Roberto Raggi <roberto.raggi@nokia.com>

--- src/libs/qmljs/parser/qmljslexer.cpp
+++ src/libs/qmljs/parser/qmljslexer.cpp
@@ -465,7 +465,7 @@ again:
             scanChar();
 
             if (_terminator && !_delimited && !_prohibitAutomaticSemicolon) {
-                _stackToken = T_PLUS_PLUS;
+                _stackToken = T_MINUS_MINUS;
                 return T_SEMICOLON;
             }
 
