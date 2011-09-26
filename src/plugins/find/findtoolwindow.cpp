b2cc1a9d4ee604adc4bd259b69c1603efe4374bc
Eike Ziller <eike.ziller@nokia.com>
2011-09-26 14:49:27 +0200

    Allow usage of keypad 'enter' key for initiating searches.
    Change-Id: Ida418646a35c51cc848abfeb8fd598f13d56b7dc
    Reviewed-on: http://codereview.qt-project.org/5541
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Eike Ziller <eike.ziller@nokia.com>

--- src/plugins/find/findtoolwindow.cpp
+++ src/plugins/find/findtoolwindow.cpp
@@ -90,7 +90,7 @@ bool FindToolWindow::event(QEvent *event)
     if (event->type() == QEvent::KeyPress) {
         QKeyEvent *ke = static_cast<QKeyEvent *>(event);
         if ((ke->key() == Qt::Key_Return || ke->key() == Qt::Key_Enter)
-                && ke->modifiers() == Qt::NoModifier) {
+                && (ke->modifiers() == Qt::NoModifier || ke->modifiers() == Qt::KeypadModifier)) {
             ke->accept();
             search();
             return true;
