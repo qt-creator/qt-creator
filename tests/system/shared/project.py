368726d6f22520abe0162a3d50cee71bbe474f75
Robert Loehning <robert.loehning@nokia.com>
2011-10-06 20:16:15 +0200

    Squish: Moved contents of mainwin.py to other files
    Change-Id: I998b1304dc9ad8832990da5e90ab426c38eb329e
    Reviewed-on: http://codereview.qt-project.org/6182
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Christian Stenger <christian.stenger@nokia.com>

--- tests/system/shared/project.py
+++ tests/system/shared/project.py
@@ -1,3 +1,47 @@
+def openQmakeProject(projectPath):
+    invokeMenuItem("File", "Open File or Project...")
+    if platform.system()=="Darwin":
+        snooze(1)
+        nativeType("<Command+Shift+g>")
+        snooze(1)
+        nativeType(projectPath)
+        snooze(1)
+        nativeType("<Return>")
+        snooze(2)
+        nativeType("<Return>")
+    else:
+        waitForObject("{name='QFileDialog' type='QFileDialog' visible='1' windowTitle='Open File'}")
+        type(findObject("{name='fileNameEdit' type='QLineEdit'}"), projectPath)
+        clickButton(findObject("{text='Open' type='QPushButton'}"))
+    waitForObject("{type='Qt4ProjectManager::Internal::ProjectLoadWizard' visible='1' windowTitle='Project Setup'}")
+    selectFromCombo(":scrollArea.Create Build Configurations:_QComboBox", "For Each Qt Version One Debug And One Release")
+    clickButton(findObject("{text~='(Finish|Done)' type='QPushButton'}"))
+
+def openCmakeProject(projectPath):
+    invokeMenuItem("File", "Open File or Project...")
+    if platform.system()=="Darwin":
+        snooze(1)
+        nativeType("<Command+Shift+g>")
+        snooze(1)
+        nativeType(projectPath)
+        snooze(1)
+        nativeType("<Return>")
+        snooze(2)
+        nativeType("<Return>")
+    else:
+        waitForObject("{name='QFileDialog' type='QFileDialog' visible='1' windowTitle='Open File'}")
+        type(findObject("{name='fileNameEdit' type='QLineEdit'}"), projectPath)
+        clickButton(findObject("{text='Open' type='QPushButton'}"))
+    clickButton(waitForObject(":CMake Wizard.Next_QPushButton", 20000))
+    generatorCombo = waitForObject(":Generator:_QComboBox")
+    index = generatorCombo.findText("MinGW Generator (MinGW from SDK)")
+    if index == -1:
+        index = generatorCombo.findText("NMake Generator (Microsoft Visual C++ Compiler 9.0 (x86))")
+    if index != -1:
+        generatorCombo.setCurrentIndex(index)
+    clickButton(waitForObject(":CMake Wizard.Run CMake_QPushButton", 20000))
+    clickButton(waitForObject(":CMake Wizard.Finish_QPushButton", 60000))
+
 def shadowBuildDir(path, project, qtVersion, debugVersion):
     qtVersion = qtVersion.replace(" ", "_")
     qtVersion = qtVersion.replace(".", "_")
