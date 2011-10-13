584a8f016ebc61b584b3ca696328b03586cb231e
Robert Loehning <robert.loehning@nokia.com>
2011-10-13 14:29:51 +0200

    Squish: Moved remaining functions for creating projects to project.py
    Change-Id: I92b56ed9401be41d0a54dd32c623b55b7d1e4f71
    Reviewed-by: Christian Stenger <christian.stenger@nokia.com>

--- tests/system/suite_qtquick/tst_qtquick_creation3/test.py
+++ tests/system/suite_qtquick/tst_qtquick_creation3/test.py
@@ -7,31 +7,11 @@ def main():
     startApplication("qtcreator" + SettingsPath)
     # using a temporary directory won't mess up an eventually exisiting
     workingDir = tempDir()
-    createNewQtQuickUI()
+    createNewQtQuickUI(workingDir)
     test.log("Running project")
     if runAndCloseQtQuickUI():
         logApplicationOutput()
     invokeMenuItem("File", "Exit")
-
-def createNewQtQuickUI():
-    global workingDir
-    invokeMenuItem("File", "New File or Project...")
-    clickItem(waitForObject("{type='QTreeView' name='templateCategoryView'}", 20000), "Projects.Qt Quick Project", 5, 5, 0, Qt.LeftButton)
-    clickItem(waitForObject("{name='templatesView' type='QListView'}", 20000), "Qt Quick UI", 5, 5, 0, Qt.LeftButton)
-    clickButton(waitForObject("{text='Choose...' type='QPushButton' unnamed='1' visible='1'}", 20000))
-    baseLineEd = waitForObject("{type='Utils::BaseValidatingLineEdit' unnamed='1' visible='1'}", 20000)
-    replaceEditorContent(baseLineEd, workingDir)
-    stateLabel = findObject("{type='QLabel' name='stateLabel'}")
-    labelCheck = stateLabel.text=="" and stateLabel.styleSheet == ""
-    test.verify(labelCheck, "Project name and base directory without warning or error")
-    # make sure this is not set as default location
-    cbDefaultLocation = waitForObject("{type='QCheckBox' name='projectsDirectoryCheckBox' visible='1'}", 20000)
-    if cbDefaultLocation.checked:
-        clickButton(cbDefaultLocation)
-    # now there's the 'untitled' project inside a temporary directory - step forward...!
-    clickButton(waitForObject("{text~='(Next.*|Continue)' type='QPushButton' visible='1'}", 20000))
-    selectFromCombo(":addToVersionControlComboBox_QComboBox", "<None>")
-    clickButton(waitForObject("{type='QPushButton' text~='(Finish|Done)' visible='1'}", 20000))
 
 def cleanup():
     global workingDir
