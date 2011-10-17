705472d3d92ab0ca99759a2f2216ec364e21722c
Aurindam Jana <aurindam.jana@nokai.com>
2011-10-17 15:23:42 +0200

    QmlEngine: Activate selected frame only in specific states
    Selecting a row in stack window activates the frame only if the
    state is InferiorStopOk or InferiorUnrunnable, as followed by
    gdbengine.
    
    Change-Id: Iad6a16816c0ffd634572f26e1dcf8c3c577502db
    Reviewed-by: Kai Koehne <kai.koehne@nokia.com>

--- src/plugins/debugger/qml/qmlengine.cpp
+++ src/plugins/debugger/qml/qmlengine.cpp
@@ -518,6 +518,9 @@ void QmlEngine::executeJumpToLine(const ContextData &data)
 
 void QmlEngine::activateFrame(int index)
 {
+    if (state() != InferiorStopOk && state() != InferiorUnrunnable)
+        return;
+
     if (d->m_adapter.activeDebuggerClient()) {
         logMessage(LogSend, QString("%1 %2").arg(QString("ACTIVATE_FRAME"), QString::number(index)));
         d->m_adapter.activeDebuggerClient()->activateFrame(index);
