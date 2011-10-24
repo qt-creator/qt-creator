5a38e9b9cc1eaa9d868746bf83e22b140ac122d6
Christian Kandeler <christian.kandeler@nokia.com>
2011-10-24 17:29:58 +0200

    Fremantle: Don't use unsupported "mad" option.
    Our SDK uses an outdated MADDE that does not support
    the "--no-force-downgrade" option when deploying into the sysroot.
    
    Task-number: QTCREATORBUG-6328
    
    Change-Id: Icb0c9774daee9b9840403ca8a14c9f6604cf3624
    Reviewed-by: Christian Kandeler <christian.kandeler@nokia.com>

--- src/plugins/madde/maemoinstalltosysrootstep.cpp
+++ src/plugins/madde/maemoinstalltosysrootstep.cpp
@@ -35,6 +35,7 @@
 #include "maemoglobal.h"
 #include "maemopackagecreationstep.h"
 #include "maemoqtversion.h"
+#include "qt4maemotarget.h"
 
 #include <utils/fileutils.h>
 
@@ -248,8 +249,12 @@ BuildStepConfigWidget *MaemoInstallDebianPackageToSysrootStep::createConfigWidge
 
 QStringList MaemoInstallDebianPackageToSysrootStep::madArguments() const
 {
-    return QStringList() << QLatin1String("xdpkg") << QLatin1String("--no-force-downgrade")
-        << QLatin1String("-i");
+    QStringList args;
+    args << QLatin1String("xdpkg");
+    if (qobject_cast<Qt4HarmattanTarget *>(target()))
+        args << QLatin1String("--no-force-downgrade");
+    args << QLatin1String("-i");
+    return args;
 }
 
 const QString MaemoInstallDebianPackageToSysrootStep::Id
