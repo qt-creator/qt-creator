qt-creator/qt-creator

commit 44928237af12ec16eeb13b5eb86c6da2c483a6c1

welcome: less artifacts in top bar
Change-Id: I38af2be3bffccee8b3fa2f9094c21f2b06800371
Reviewed-by: Eike Ziller <eike.ziller@nokia.com>

share/qtcreator/welcomescreen/widgets/LinksBar.qml
@@ -36,58 +36,100 @@ import qtcomponents 1.0 as Components
 Item {
     id: tabBar
 
-    height: 62
+    height: 60
     width: parent.width
+
     property alias model: tabs.model
-    property int tabWidth: Math.floor(tabBar.width/tabs.count)
-    property url inactiveSource: "qrc:welcome/images/tab_inactive.png"
-    property url activeSource: "qrc:welcome/images/tab_active.png"
 
-    Row {
+    Rectangle {
         id: row
-        height: 24
-        width: parent.width
+        width: 100
+        height: 26
+        anchors.top: parent.top
+        anchors.left: parent.left
+        gradient: Gradient {
+            GradientStop { position: 0; color: "#f7f7f7" }
+            GradientStop { position: 1; color: "#e4e4e4" }
+        }
+        Text {
+            id: text
+            horizontalAlignment: Qt.AlignHCenter; verticalAlignment: Qt.AlignVCenter
+            anchors.fill: parent
+            text: qsTr("Qt Creator")
+        }
+    }
 
-        BorderImage {
-            id: active
-            width: 100
-            height: parent.height
-            border { top: 2; bottom: 2; left: 2; right: 2}
-            source: activeSource
-            Text {
-                id: text
-                horizontalAlignment: Qt.AlignHCenter; verticalAlignment: Qt.AlignVCenter
-                anchors.fill: parent
-                text: qsTr("Qt Creator")
-                elide: Text.ElideRight
-                color: "black"
+    Item {
+        anchors.top: parent.top
+        anchors.left: row.right
+        anchors.right: parent.right
+        anchors.bottom: row.bottom
+
+        Rectangle {
+            id: left1
+            anchors.top: parent.top
+            anchors.left: parent.left
+            anchors.bottom: parent.bottom
+            width: 1
+            gradient: Gradient {
+                GradientStop { position: 0; color: "#fcfcfc" }
+                GradientStop { position: 1; color: "#f7f7f7" }
             }
         }
 
-        BorderImage {
-            anchors.left: active.right
+        Rectangle {
+            id: left2
+            anchors.top: parent.top
+            anchors.left: left1.right
+            anchors.bottom: parent.bottom
+            width: 1
+            color: "#313131"
+        }
+
+        Rectangle {
+            id: bottom1
+            height: 1
+            anchors.left: left1.right
             anchors.right: parent.right
-            height: parent.height
-            border { top: 2; bottom: 2; left: 2; right: 2}
-            source: inactiveSource
+            anchors.bottom: parent.bottom
+            color: "#fbfbfb"
+        }
+
+        Rectangle {
+            id: bottom2
+            height: 1
+            anchors.left: left2.right
+            anchors.right: parent.right
+            anchors.bottom: bottom1.top
+            width: 1
+            color: "#313131"
+        }
+
+        Rectangle {
+            anchors.top: parent.top
+            anchors.left: left2.right
+            anchors.right: parent.right
+            anchors.bottom: bottom2.top
+            gradient: Gradient {
+                GradientStop { position: 0.00; color: "#8e8e8e" }
+                GradientStop { position: 0.07; color: "#8e8e8e" }
+                GradientStop { position: 0.08; color: "#757575" }
+                GradientStop { position: 0.40; color: "#666666" }
+                GradientStop { position: 0.41; color: "#585858" }
+                GradientStop { position: 1.00; color: "#404040" }
+             }
         }
     }
+
     Rectangle {
         id: background
         height: 38
-        gradient: Gradient {
-            GradientStop {
-                position: 0
-                color: "#e4e4e4"
-            }
-
-            GradientStop {
-                position: 1
-                color: "#cecece"
-            }
-        }
         width: parent.width
         anchors.top: row.bottom
+        gradient: Gradient {
+            GradientStop { position: 0; color: "#e4e4e4" }
+            GradientStop { position: 1; color: "#cecece" }
+        }
         Rectangle {
             color: "black"
             height: 1

commit 0c3f3c009ae2aba4105d32efc71a916a708cbe80

WelcomePage: layout fix
Fixes a regression introduced in
44928237af12ec16eeb13b5eb86c6da2c483a6c1

Task-number: QTCREATORBUG-6444
Change-Id: Ie92ba0be9e888e065aaa52aac41bf3338bf8aeb6
Reviewed-by: Eike Ziller <eike.ziller@nokia.com>

share/qtcreator/welcomescreen/welcomescreen.qml
@@ -84,7 +84,6 @@ Rectangle {
         anchors.top: navigationAndDevLinks.bottom
         anchors.bottom: parent.bottom
         width: 220
-        anchors.topMargin: -1
 
         FeaturedAndNewsListing {
             anchors.fill: parent
@@ -93,14 +92,6 @@ Rectangle {
             anchors.left: parent.left
             anchors.right: parent.right
             anchors.top: parent.top
-            height: 1
-            color: "black"
-        }
-        Rectangle {
-            anchors.left: parent.left
-            anchors.right: parent.right
-            anchors.top: parent.top
-            anchors.topMargin: 1
             height: 1
             color: "#ccc"
         }

share/qtcreator/welcomescreen/widgets/LinksBar.qml
@@ -123,7 +123,7 @@ Item {
 
     Rectangle {
         id: background
-        height: 38
+        anchors.bottom: parent.bottom
         width: parent.width
         anchors.top: row.bottom
         gradient: Gradient {