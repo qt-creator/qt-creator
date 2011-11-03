qt-creator/qt-creator

commit 97b4bcfa6c98a0a3a4dcc6dc6315513f27b7a976

ProjectExplorer: On shutdown, delete runcontrols before singnaling done.
Change-Id: I6860465e575b6751d804304377c16a513c24f20d
Reviewed-on: http://codereview.qt-project.org/5248
Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
Reviewed-by: Kai Koehne <kai.koehne@nokia.com>

src/plugins/projectexplorer/appoutputpane.cpp
@@ -301,7 +301,7 @@ void AppOutputPane::createNewOutputWindow(RunControl *rc)
     connect(rc, SIGNAL(started()),
             this, SLOT(runControlStarted()));
     connect(rc, SIGNAL(finished()),
-            this, SLOT(runControlFinished()));
+            this, SLOT(runControlFinished()), Qt::QueuedConnection);
     connect(rc, SIGNAL(applicationProcessHandleChanged()),
             this, SLOT(enableButtons()));
     connect(rc, SIGNAL(appendMessage(ProjectExplorer::RunControl*,QString,Utils::OutputFormat)),
@@ -452,11 +452,7 @@ bool AppOutputPane::closeTab(int tabIndex, CloseTabMode closeTabMode)
     }
 
     m_tabWidget->removeTab(tabIndex);
-    if (tab.asyncClosing) { // We were invoked from its finished() signal.
-        tab.runControl->deleteLater();
-    } else {
-        delete tab.runControl;
-    }
+    delete tab.runControl;
     delete tab.window;
     m_runControlTabs.removeAt(index);
     updateCloseActions();

commit 46e912ad24f472178563deedb97840d11d78c6b0

Appoutputpane sender() can't be used in a queued connection
The bug was introduced in 97b4bcfa6c98a0a3a4dcc6dc6315513f27b7a976

Change-Id: Iec364a40ff748cd8f94f65e8fd22d4d71a5de5f4
Reviewed-by: hjk <qthjk@ovi.com>

src/plugins/projectexplorer/appoutputpane.cpp
@@ -301,7 +301,7 @@ void AppOutputPane::createNewOutputWindow(RunControl *rc)
     connect(rc, SIGNAL(started()),
             this, SLOT(slotRunControlStarted()));
     connect(rc, SIGNAL(finished()),
-            this, SLOT(slotRunControlFinished()), Qt::QueuedConnection);
+            this, SLOT(slotRunControlFinished()));
     connect(rc, SIGNAL(applicationProcessHandleChanged()),
             this, SLOT(enableButtons()));
     connect(rc, SIGNAL(appendMessage(ProjectExplorer::RunControl*,QString,Utils::OutputFormat)),
@@ -541,8 +541,14 @@ void AppOutputPane::slotRunControlStarted()
 
 void AppOutputPane::slotRunControlFinished()
 {
-    RunControl *senderRunControl = qobject_cast<RunControl *>(sender());
-    const int senderIndex = indexOf(senderRunControl);
+    ProjectExplorer::RunControl *rc = qobject_cast<RunControl *>(sender());
+    QMetaObject::invokeMethod(this, "slotRunControlFinished2", Qt::QueuedConnection,
+                              Q_ARG(ProjectExplorer::RunControl *, rc));
+}
+
+void AppOutputPane::slotRunControlFinished2(RunControl *sender)
+{
+    const int senderIndex = indexOf(sender);
 
     QTC_ASSERT(senderIndex != -1, return; )
 
@@ -550,17 +556,17 @@ void AppOutputPane::slotRunControlFinished()
     RunControl *current = currentRunControl();
 
     if (debug)
-        qDebug() << "OutputPane::runControlFinished"  << senderRunControl << senderIndex
+        qDebug() << "OutputPane::runControlFinished"  << sender << senderIndex
                     << " current " << current << m_runControlTabs.size();
 
-    if (current && current == sender())
+    if (current && current == sender)
         enableButtons(current, false); // RunControl::isRunning() cannot be trusted in signal handler.
 
     // Check for asynchronous close. Close the tab.
     if (m_runControlTabs.at(senderIndex).asyncClosing)
         closeTab(tabWidgetIndexOf(senderIndex), CloseTabNoPrompt);
 
-    emit runControlFinished(senderRunControl);
+    emit runControlFinished(sender);
 
     if (!isRunning())
         emit allRunControlsFinished();

src/plugins/projectexplorer/appoutputpane.h
@@ -107,6 +107,7 @@ private slots:
     void contextMenuRequested(const QPoint &pos, int index);
     void slotRunControlStarted();
     void slotRunControlFinished();
+    void slotRunControlFinished2(RunControl *sender);
 
     void aboutToUnloadSession();
     void updateFromSettings();