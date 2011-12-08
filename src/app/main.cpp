qt-creator/qt-creator

commit 4edec9f92ad0cf2d00a374a1edcfc2bd543d5952

Workaround for Mac users with automatic proxy configuration
Qt's implementation for the Mac system configuration doesn't handle
automatic proxy configurations. Qt Creator now also looks at the
http_proxy environment variable on Mac as a workaround.

Task-number: QTCREATORBUG-4190
Reviewed-by: Daniel Molkentin

src/app/main.cpp
@@ -251,15 +251,18 @@ int main(int argc, char **argv)
     }
 
     // Make sure we honor the system's proxy settings
-#if defined(Q_OS_UNIX) && !defined(Q_OS_MAC)
+#if defined(Q_OS_UNIX)
     QUrl proxyUrl(QString::fromLatin1(qgetenv("http_proxy")));
     if (proxyUrl.isValid()) {
         QNetworkProxy proxy(QNetworkProxy::HttpProxy, proxyUrl.host(),
                             proxyUrl.port(), proxyUrl.userName(), proxyUrl.password());
         QNetworkProxy::setApplicationProxy(proxy);
     }
-#else
-    QNetworkProxyFactory::setUseSystemConfiguration(true);
+#if defined(Q_OS_MAC)
+    else {
+        QNetworkProxyFactory::setUseSystemConfiguration(true);
+    }
+#endif
 #endif
     // Load
     const QStringList pluginPaths = getPluginPaths();

commit b2c73f6e18a18d143445a163b358d1700eef5d32

Fix windows proxy setting again.
Broke with 4edec9f9.

Change-Id: I1dfad5964f7d36566de0874d19e7ca94e3b92054
Reviewed-by: Daniel Teske <daniel.teske@nokia.com>

src/app/main.cpp
@@ -284,11 +284,13 @@ int main(int argc, char **argv)
                             proxyUrl.port(), proxyUrl.userName(), proxyUrl.password());
         QNetworkProxy::setApplicationProxy(proxy);
     }
-#if defined(Q_OS_MAC)
+# if defined(Q_OS_MAC) // unix and mac
     else {
         QNetworkProxyFactory::setUseSystemConfiguration(true);
     }
-#endif
+# endif
+#else // windows
+    QNetworkProxyFactory::setUseSystemConfiguration(true);
 #endif
     // Load
     const QStringList pluginPaths = getPluginPaths();