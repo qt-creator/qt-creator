6571e01725718a6a7edea18b60e1c29a7c885b54
Christian Stenger <christian.stenger@nokia.com>
2012-01-10 16:55:57 +0100

    Squish: Only log compile if failed or debug mode
    Change-Id: I266fb9b7796d810c98e642d3c7a3f682eb05bf6b
    Reviewed-by: Bill King <bill.king@nokia.com>

--- tests/system/shared/build_utils.py
+++ tests/system/shared/build_utils.py
@@ -89,7 +89,10 @@ def checkCompile():
     waitFor("len(str(output.plainText))>0",5000)
     success = str(output.plainText).endswith("exited normally.")
     if success:
-        test.log("Compile Output:\n%s" % output.plainText)
+        if os.getenv("SYSTEST_DEBUG") == "1":
+            test.log("Compile Output:\n%s" % output.plainText)
+        else:
+            test.passes("Compile successful")
     else:
         test.fail("Compile Output:\n%s" % output.plainText)
     return success
