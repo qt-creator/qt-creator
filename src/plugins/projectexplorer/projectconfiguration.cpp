983a6ef5066da3ee2b2a471eab50204023dd663b
hjk <qtc-committer@nokia.com>
2012-01-24 16:27:05 +0100

    projectexplorer: string literal style
    Change-Id: Id64ab30f4a69f03e941bde507cba56806bb50a7e
    Reviewed-by: hjk <qthjk@ovi.com>

--- src/plugins/projectexplorer/projectconfiguration.cpp
+++ src/plugins/projectexplorer/projectconfiguration.cpp
@@ -34,11 +34,9 @@
 
 using namespace ProjectExplorer;
 
-namespace {
-const char * const CONFIGURATION_ID_KEY("ProjectExplorer.ProjectConfiguration.Id");
-const char * const DISPLAY_NAME_KEY("ProjectExplorer.ProjectConfiguration.DisplayName");
-const char * const DEFAULT_DISPLAY_NAME_KEY("ProjectExplorer.ProjectConfiguration.DefaultDisplayName");
-}
+const char CONFIGURATION_ID_KEY[] = "ProjectExplorer.ProjectConfiguration.Id";
+const char DISPLAY_NAME_KEY[] = "ProjectExplorer.ProjectConfiguration.DisplayName";
+const char DEFAULT_DISPLAY_NAME_KEY[] = "ProjectExplorer.ProjectConfiguration.DefaultDisplayName";
 
 ProjectConfiguration::ProjectConfiguration(QObject *parent, const QString &id) :
     QObject(parent),
