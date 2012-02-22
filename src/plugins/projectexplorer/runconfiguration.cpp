qt-creator/qt-creator

commit 503c6e2684e85cbb8363d885d473aeac0f51cbe5

projectexplorer: Make the debugger language chooser a (hardcoded) "aspect"
This is the first step of a larger refactoring. The plan is to make
debuggersettings more orthogonal to individual runconfiguration
implementation. This patch alone already pushes the settings handling
to the debugger plugin and removes code duplication in the
runconfiguration implementation.

Change-Id: I4c78d1658ea462d3df14b873f8f41cc918a23f1a
Reviewed-by: Daniel Teske <daniel.teske@nokia.com>
Reviewed-by: hjk <qthjk@ovi.com>

src/plugins/projectexplorer/runconfiguration.cpp
@@ -194,6 +194,103 @@ bool ProcessHandle::equals(const ProcessHandle &rhs) const
 }
 
 /*!
+    \class ProjectExplorer::DebuggerProjectSettings
+*/
+
+DebuggerProjectSettings::DebuggerProjectSettings() :
+    m_useCppDebugger(true),
+    m_useQmlDebugger(AutoEnableQmlDebugger),
+    m_qmlDebugServerPort(Constants::QML_DEFAULT_DEBUG_SERVER_PORT)
+{}
+
+DebuggerProjectSettings::DebuggerProjectSettings(DebuggerProjectSettings *other) :
+    m_useCppDebugger(other->m_useCppDebugger),
+    m_useQmlDebugger(other->m_useQmlDebugger),
+    m_qmlDebugServerPort(other->m_qmlDebugServerPort)
+{}
+
+void DebuggerProjectSettings::setUseQmlDebugger(bool value)
+{
+    m_useQmlDebugger = value ? EnableQmlDebugger : DisableQmlDebugger;
+    emit debuggersChanged();
+}
+
+void DebuggerProjectSettings::setUseCppDebugger(bool value)
+{
+    m_useCppDebugger = value;
+    emit debuggersChanged();
+}
+
+bool DebuggerProjectSettings::useCppDebugger() const
+{
+    return m_useCppDebugger;
+}
+
+static bool isQtQuickAppProject(Project *project)
+{
+    const QString filePath = project->projectDirectory()
+            + QLatin1String("/qmlapplicationviewer/qmlapplicationviewer.pri");
+    return project->files(Project::ExcludeGeneratedFiles).contains(filePath);
+}
+
+DebuggerProjectSettings::QmlDebuggerStatus DebuggerProjectSettings::useQmlDebugger() const
+{
+    return m_useQmlDebugger;
+}
+
+bool RunConfiguration::useQmlDebugger() const
+{
+    DebuggerProjectSettings::QmlDebuggerStatus s = m_debuggerAspect->useQmlDebugger();
+    if (s == DebuggerProjectSettings::AutoEnableQmlDebugger)
+        return isQtQuickAppProject(target()->project());
+    return s == DebuggerProjectSettings::EnableQmlDebugger;
+}
+
+uint DebuggerProjectSettings::qmlDebugServerPort() const
+{
+    return m_qmlDebugServerPort;
+}
+
+void DebuggerProjectSettings::setQmlDebugServerPort(uint port)
+{
+    m_qmlDebugServerPort = port;
+    emit qmlDebugServerPortChanged(port);
+}
+
+void DebuggerProjectSettings::suppressQmlDebuggingOptions()
+{
+    m_useQmlDebugger = SuppressQmlDebugger;
+}
+
+QString DebuggerProjectSettings::displayName() const
+{
+    return tr("Debugger settings");
+}
+
+QVariantMap DebuggerProjectSettings::toMap() const
+{
+    QVariantMap map;
+    map.insert(QLatin1String(USE_CPP_DEBUGGER_KEY), m_useCppDebugger);
+    map.insert(QLatin1String(USE_QML_DEBUGGER_KEY), m_useQmlDebugger == EnableQmlDebugger);
+    map.insert(QLatin1String(USE_QML_DEBUGGER_AUTO_KEY), m_useQmlDebugger == AutoEnableQmlDebugger);
+    map.insert(QLatin1String(QML_DEBUG_SERVER_PORT_KEY), m_qmlDebugServerPort);
+    return map;
+}
+
+bool DebuggerProjectSettings::fromMap(const QVariantMap &map)
+{
+    m_useCppDebugger = map.value(QLatin1String(USE_CPP_DEBUGGER_KEY), true).toBool();
+    if (map.value(QLatin1String(USE_QML_DEBUGGER_AUTO_KEY), false).toBool()) {
+        m_useQmlDebugger = AutoEnableQmlDebugger;
+    } else {
+        bool useQml = map.value(QLatin1String(USE_QML_DEBUGGER_KEY), false).toBool();
+        m_useQmlDebugger = useQml ? EnableQmlDebugger : DisableQmlDebugger;
+    }
+    return true;
+}
+
+
+/*!
     \class ProjectExplorer::RunConfiguration
     \brief  Base class for a run configuration. A run configuration specifies how a
     target should be run, while the runner (see below) does the actual running.
@@ -209,9 +306,7 @@ bool ProcessHandle::equals(const ProcessHandle &rhs) const
 
 RunConfiguration::RunConfiguration(Target *target, const QString &id) :
     ProjectConfiguration(target, id),
-    m_useCppDebugger(true),
-    m_useQmlDebugger(AutoEnableQmlDebugger),
-    m_qmlDebugServerPort(Constants::QML_DEFAULT_DEBUG_SERVER_PORT)
+    m_debuggerAspect(new DebuggerProjectSettings)
 {
     Q_ASSERT(target);
     addExtraAspects();
@@ -219,9 +314,7 @@ RunConfiguration::RunConfiguration(Target *target, const QString &id) :
 
 RunConfiguration::RunConfiguration(Target *target, RunConfiguration *source) :
     ProjectConfiguration(target, source),
-    m_useCppDebugger(source->m_useCppDebugger),
-    m_useQmlDebugger(source->m_useQmlDebugger),
-    m_qmlDebugServerPort(source->m_qmlDebugServerPort)
+    m_debuggerAspect(new DebuggerProjectSettings(source->debuggerAspect()))
 {
     Q_ASSERT(target);
     addExtraAspects();
@@ -229,6 +322,7 @@ RunConfiguration::RunConfiguration(Target *target, RunConfiguration *source) :
 
 RunConfiguration::~RunConfiguration()
 {
+    delete m_debuggerAspect;
     qDeleteAll(m_aspects);
 }
 
@@ -272,65 +366,10 @@ Target *RunConfiguration::target() const
     return static_cast<Target *>(parent());
 }
 
-void RunConfiguration::setUseQmlDebugger(bool value)
-{
-    if (value) {
-        m_useQmlDebugger = EnableQmlDebugger;
-    } else {
-        m_useQmlDebugger = DisableQmlDebugger;
-    }
-    emit debuggersChanged();
-}
-
-void RunConfiguration::setUseCppDebugger(bool value)
-{
-    m_useCppDebugger = value;
-    emit debuggersChanged();
-}
-
-bool RunConfiguration::useCppDebugger() const
-{
-    return m_useCppDebugger;
-}
-
-static bool isQtQuickAppProject(Project *project)
-{
-    const QString &filePath = project->projectDirectory()
-            +QLatin1String("/qmlapplicationviewer/qmlapplicationviewer.pri");
-    return project->files(Project::ExcludeGeneratedFiles).contains(filePath);
-}
-
-bool RunConfiguration::useQmlDebugger() const
-{
-    if (m_useQmlDebugger == AutoEnableQmlDebugger) {
-        if (isQtQuickAppProject(target()->project())) {
-            m_useQmlDebugger = EnableQmlDebugger;
-        } else {
-            m_useQmlDebugger = DisableQmlDebugger;
-        }
-    }
-
-    return (m_useQmlDebugger == EnableQmlDebugger);
-}
-
-uint RunConfiguration::qmlDebugServerPort() const
-{
-    return m_qmlDebugServerPort;
-}
-
-void RunConfiguration::setQmlDebugServerPort(uint port)
-{
-    m_qmlDebugServerPort = port;
-    emit qmlDebugServerPortChanged(port);
-}
-
 QVariantMap RunConfiguration::toMap() const
 {
-    QVariantMap map = ProjectConfiguration::toMap();
-    map.insert(QLatin1String(USE_CPP_DEBUGGER_KEY), m_useCppDebugger);
-    map.insert(QLatin1String(USE_QML_DEBUGGER_KEY), m_useQmlDebugger == EnableQmlDebugger);
-    map.insert(QLatin1String(USE_QML_DEBUGGER_AUTO_KEY), m_useQmlDebugger == AutoEnableQmlDebugger);
-    map.insert(QLatin1String(QML_DEBUG_SERVER_PORT_KEY), m_qmlDebugServerPort);
+    QVariantMap map = m_debuggerAspect->toMap();
+
     foreach (IRunConfigurationAspect *aspect, m_aspects)
         map.unite(aspect->toMap());
 
@@ -350,16 +389,8 @@ ProjectExplorer::Abi RunConfiguration::abi() const
 
 bool RunConfiguration::fromMap(const QVariantMap &map)
 {
-    m_useCppDebugger = map.value(QLatin1String(USE_CPP_DEBUGGER_KEY), true).toBool();
-    if (map.value(QLatin1String(USE_QML_DEBUGGER_AUTO_KEY), false).toBool()) {
-        m_useQmlDebugger = AutoEnableQmlDebugger;
-    } else {
-        if (map.value(QLatin1String(USE_QML_DEBUGGER_KEY), false).toBool()) {
-            m_useQmlDebugger = EnableQmlDebugger;
-        } else {
-            m_useQmlDebugger = DisableQmlDebugger;
-        }
-    }
+    if (!m_debuggerAspect->fromMap(map))
+        return false;
 
     foreach (IRunConfigurationAspect *aspect, m_aspects)
         if (!aspect->fromMap(map))

src/libs/utils/debuggerlanguagechooser.cpp
@@ -1,136 +1,0 @@
-/**************************************************************************
-**
-** This file is part of Qt Creator
-**
-** Copyright (c) 2012 Nokia Corporation and/or its subsidiary(-ies).
-**
-** Contact: Nokia Corporation (qt-info@nokia.com)
-**
-**
-** GNU Lesser General Public License Usage
-**
-** This file may be used under the terms of the GNU Lesser General Public
-** License version 2.1 as published by the Free Software Foundation and
-** appearing in the file LICENSE.LGPL included in the packaging of this file.
-** Please review the following information to ensure the GNU Lesser General
-** Public License version 2.1 requirements will be met:
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** In addition, as a special exception, Nokia gives you certain additional
-** rights. These rights are described in the Nokia Qt LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-** Other Usage
-**
-** Alternatively, this file may be used in accordance with the terms and
-** conditions contained in a signed written agreement between you and Nokia.
-**
-** If you have questions regarding the use of this file, please contact
-** Nokia at qt-info@nokia.com.
-**
-**************************************************************************/
-
-#include "debuggerlanguagechooser.h"
-
-#include <QHBoxLayout>
-#include <QCheckBox>
-#include <QSpinBox>
-#include <QLabel>
-
-namespace Utils {
-
-DebuggerLanguageChooser::DebuggerLanguageChooser(QWidget *parent) :
-    QWidget(parent)
-{
-    m_useCppDebugger = new QCheckBox(tr("C++"), this);
-    m_useQmlDebugger = new QCheckBox(tr("QML"), this);
-
-    connect(m_useCppDebugger, SIGNAL(toggled(bool)),
-            this, SLOT(useCppDebuggerToggled(bool)));
-    connect(m_useQmlDebugger, SIGNAL(toggled(bool)),
-            this, SLOT(useQmlDebuggerToggled(bool)));
-
-    m_debugServerPortLabel = new QLabel(tr("Debug port:"), this);
-    m_debugServerPort = new QSpinBox(this);
-    m_debugServerPort->setMinimum(1);
-    m_debugServerPort->setMaximum(65535);
-
-    m_debugServerPortLabel->setBuddy(m_debugServerPort);
-
-    m_qmlDebuggerInfoLabel = new QLabel(tr("<a href=\"qthelp://com.nokia.qtcreator/doc/creator-debugging-qml.html\">What are the prerequisites?</a>"));
-    connect(m_qmlDebuggerInfoLabel, SIGNAL(linkActivated(QString)),
-            this, SIGNAL(openHelpUrl(QString)));
-
-    connect(m_useQmlDebugger, SIGNAL(toggled(bool)), m_debugServerPort, SLOT(setEnabled(bool)));
-    connect(m_useQmlDebugger, SIGNAL(toggled(bool)), m_debugServerPortLabel, SLOT(setEnabled(bool)));
-    connect(m_debugServerPort, SIGNAL(valueChanged(int)), this, SLOT(onDebugServerPortChanged(int)));
-
-    QHBoxLayout *qmlLayout = new QHBoxLayout;
-    qmlLayout->setMargin(0);
-    qmlLayout->addWidget(m_useQmlDebugger);
-    qmlLayout->addWidget(m_debugServerPortLabel);
-    qmlLayout->addWidget(m_debugServerPort);
-    qmlLayout->addWidget(m_qmlDebuggerInfoLabel);
-    qmlLayout->addStretch();
-
-    QVBoxLayout *layout = new QVBoxLayout;
-    layout->setMargin(0);
-    layout->addWidget(m_useCppDebugger);
-    layout->addLayout(qmlLayout);
-
-    setLayout(layout);
-}
-
-bool DebuggerLanguageChooser::cppChecked() const
-{
-    return m_useCppDebugger->isChecked();
-}
-
-bool DebuggerLanguageChooser::qmlChecked() const
-{
-    return m_useQmlDebugger->isChecked();
-}
-
-uint DebuggerLanguageChooser::qmlDebugServerPort() const
-{
-    return m_debugServerPort->value();
-}
-
-void DebuggerLanguageChooser::setCppChecked(bool value)
-{
-    m_useCppDebugger->setChecked(value);
-}
-
-void DebuggerLanguageChooser::setQmlChecked(bool value)
-{
-    m_useQmlDebugger->setChecked(value);
-    m_debugServerPortLabel->setEnabled(value);
-    m_debugServerPort->setEnabled(value);
-}
-
-void DebuggerLanguageChooser::setQmlDebugServerPort(uint port)
-{
-    m_debugServerPort->setValue(port);
-}
-
-void DebuggerLanguageChooser::useCppDebuggerToggled(bool toggled)
-{
-    emit cppLanguageToggled(toggled);
-    if (!toggled && !m_useQmlDebugger->isChecked())
-        m_useQmlDebugger->setChecked(true);
-}
-
-void DebuggerLanguageChooser::useQmlDebuggerToggled(bool toggled)
-{
-    emit qmlLanguageToggled(toggled);
-    if (!toggled && !m_useCppDebugger->isChecked())
-        m_useCppDebugger->setChecked(true);
-}
-
-void DebuggerLanguageChooser::onDebugServerPortChanged(int port)
-{
-    emit qmlDebugServerPortChanged((uint)port);
-}
-
-
-} // namespace Utils

src/libs/utils/debuggerlanguagechooser.h
@@ -1,81 +1,0 @@
-/**************************************************************************
-**
-** This file is part of Qt Creator
-**
-** Copyright (c) 2012 Nokia Corporation and/or its subsidiary(-ies).
-**
-** Contact: Nokia Corporation (qt-info@nokia.com)
-**
-**
-** GNU Lesser General Public License Usage
-**
-** This file may be used under the terms of the GNU Lesser General Public
-** License version 2.1 as published by the Free Software Foundation and
-** appearing in the file LICENSE.LGPL included in the packaging of this file.
-** Please review the following information to ensure the GNU Lesser General
-** Public License version 2.1 requirements will be met:
-** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
-**
-** In addition, as a special exception, Nokia gives you certain additional
-** rights. These rights are described in the Nokia Qt LGPL Exception
-** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
-**
-** Other Usage
-**
-** Alternatively, this file may be used in accordance with the terms and
-** conditions contained in a signed written agreement between you and Nokia.
-**
-** If you have questions regarding the use of this file, please contact
-** Nokia at qt-info@nokia.com.
-**
-**************************************************************************/
-
-#ifndef DEBUGGERLANGUAGECHOOSER_H
-#define DEBUGGERLANGUAGECHOOSER_H
-
-#include "utils_global.h"
-
-#include <QWidget>
-
-QT_FORWARD_DECLARE_CLASS(QCheckBox)
-QT_FORWARD_DECLARE_CLASS(QLabel)
-QT_FORWARD_DECLARE_CLASS(QSpinBox)
-
-namespace Utils {
-
-class QTCREATOR_UTILS_EXPORT DebuggerLanguageChooser : public QWidget
-{
-    Q_OBJECT
-public:
-    explicit DebuggerLanguageChooser(QWidget *parent = 0);
-
-    bool cppChecked() const;
-    bool qmlChecked() const;
-    uint qmlDebugServerPort() const;
-
-    void setCppChecked(bool value);
-    void setQmlChecked(bool value);
-    void setQmlDebugServerPort(uint port);
-
-signals:
-    void cppLanguageToggled(bool value);
-    void qmlLanguageToggled(bool value);
-    void qmlDebugServerPortChanged(uint port);
-    void openHelpUrl(const QString &url);
-
-private slots:
-    void useCppDebuggerToggled(bool toggled);
-    void useQmlDebuggerToggled(bool toggled);
-    void onDebugServerPortChanged(int port);
-
-private:
-    QCheckBox *m_useCppDebugger;
-    QCheckBox *m_useQmlDebugger;
-    QSpinBox *m_debugServerPort;
-    QLabel *m_debugServerPortLabel;
-    QLabel *m_qmlDebuggerInfoLabel;
-};
-
-} // namespace Utils
-
-#endif // DEBUGGERLANGUAGECHOOSER_H

src/libs/utils/utils-lib.pri
@@ -59,7 +59,6 @@ SOURCES += $$PWD/environment.cpp \
     $$PWD/htmldocextractor.cpp \
     $$PWD/navigationtreeview.cpp \
     $$PWD/crumblepath.cpp \
-    $$PWD/debuggerlanguagechooser.cpp \
     $$PWD/historycompleter.cpp \
     $$PWD/buildablehelperlibrary.cpp \
     $$PWD/annotateditemdelegate.cpp \
@@ -160,7 +159,6 @@ HEADERS += \
     $$PWD/htmldocextractor.h \
     $$PWD/navigationtreeview.h \
     $$PWD/crumblepath.h \
-    $$PWD/debuggerlanguagechooser.h \
     $$PWD/historycompleter.h \
     $$PWD/buildablehelperlibrary.h \
     $$PWD/annotateditemdelegate.h \

src/libs/utils/utils.qbs
@@ -44,8 +44,6 @@ DynamicLibrary {
         "consoleprocess.h",
         "consoleprocess_p.h",
         "crumblepath.h",
-        "debuggerlanguagechooser.cpp",
-        "debuggerlanguagechooser.h",
         "detailsbutton.cpp",
         "detailsbutton.h",
         "detailswidget.cpp",

src/plugins/cmakeprojectmanager/cmakerunconfiguration.cpp
@@ -45,7 +45,6 @@
 #include <utils/pathchooser.h>
 #include <utils/detailswidget.h>
 #include <utils/qtcassert.h>
-#include <utils/debuggerlanguagechooser.h>
 #include <utils/qtcprocess.h>
 #include <utils/stringutils.h>
 
@@ -365,16 +364,6 @@ CMakeRunConfigurationWidget::CMakeRunConfigurationWidget(CMakeRunConfiguration *
     QCheckBox *runInTerminal = new QCheckBox;
     fl->addRow(tr("Run in Terminal"), runInTerminal);
 
-    QLabel *debuggerLabel = new QLabel(tr("Debugger:"), this);
-    debuggerLabel->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::MinimumExpanding);
-
-    m_debuggerLanguageChooser = new Utils::DebuggerLanguageChooser(this);
-    fl->addRow(debuggerLabel, m_debuggerLanguageChooser);
-
-    m_debuggerLanguageChooser->setCppChecked(m_cmakeRunConfiguration->useCppDebugger());
-    m_debuggerLanguageChooser->setQmlChecked(m_cmakeRunConfiguration->useQmlDebugger());
-    m_debuggerLanguageChooser->setQmlDebugServerPort(m_cmakeRunConfiguration->qmlDebugServerPort());
-
     m_detailsContainer = new Utils::DetailsWidget(this);
     m_detailsContainer->setState(Utils::DetailsWidget::NoSummary);
 
@@ -426,15 +415,6 @@ CMakeRunConfigurationWidget::CMakeRunConfigurationWidget(CMakeRunConfiguration *
     connect(runInTerminal, SIGNAL(toggled(bool)),
             this, SLOT(runInTerminalToggled(bool)));
 
-    connect(m_debuggerLanguageChooser, SIGNAL(cppLanguageToggled(bool)),
-            this, SLOT(useCppDebuggerToggled(bool)));
-    connect(m_debuggerLanguageChooser, SIGNAL(qmlLanguageToggled(bool)),
-            this, SLOT(useQmlDebuggerToggled(bool)));
-    connect(m_debuggerLanguageChooser, SIGNAL(qmlDebugServerPortChanged(uint)),
-            this, SLOT(qmlDebugServerPortChanged(uint)));
-    connect(m_debuggerLanguageChooser, SIGNAL(openHelpUrl(QString)),
-            Core::HelpManager::instance(), SLOT(handleHelpRequest(QString)));
-
     connect(m_environmentWidget, SIGNAL(userChangesChanged()),
             this, SLOT(userChangesChanged()));
 
@@ -474,21 +454,6 @@ void CMakeRunConfigurationWidget::runInTerminalToggled(bool toggled)
 {
     m_cmakeRunConfiguration->setRunMode(toggled ? ProjectExplorer::LocalApplicationRunConfiguration::Console
                                                 : ProjectExplorer::LocalApplicationRunConfiguration::Gui);
-}
-
-void CMakeRunConfigurationWidget::useCppDebuggerToggled(bool toggled)
-{
-    m_cmakeRunConfiguration->setUseCppDebugger(toggled);
-}
-
-void CMakeRunConfigurationWidget::useQmlDebuggerToggled(bool toggled)
-{
-    m_cmakeRunConfiguration->setUseQmlDebugger(toggled);
-}
-
-void CMakeRunConfigurationWidget::qmlDebugServerPortChanged(uint port)
-{
-    m_cmakeRunConfiguration->setQmlDebugServerPort(port);
 }
 
 void CMakeRunConfigurationWidget::userChangesChanged()

src/plugins/cmakeprojectmanager/cmakerunconfiguration.h
@@ -41,7 +41,6 @@ class QComboBox;
 QT_END_NAMESPACE
 
 namespace Utils {
-class DebuggerLanguageChooser;
 class PathChooser;
 class DetailsWidget;
 }
@@ -147,9 +146,6 @@ private slots:
     void setWorkingDirectory();
     void resetWorkingDirectory();
     void runInTerminalToggled(bool toggled);
-    void useCppDebuggerToggled(bool toggled);
-    void useQmlDebuggerToggled(bool toggled);
-    void qmlDebugServerPortChanged(uint port);
 
     void baseEnvironmentComboBoxChanged(int index);
     void workingDirectoryChanged(const QString &workingDirectory);
@@ -160,7 +156,6 @@ private:
     CMakeRunConfiguration *m_cmakeRunConfiguration;
     Utils::PathChooser *m_workingDirectoryEdit;
     QComboBox *m_baseEnvironmentComboBox;
-    Utils::DebuggerLanguageChooser *m_debuggerLanguageChooser;
     ProjectExplorer::EnvironmentWidget *m_environmentWidget;
     Utils::DetailsWidget *m_detailsContainer;
 };

src/plugins/debugger/debuggermainwindow.cpp
@@ -229,9 +229,9 @@ void DebuggerMainWindowPrivate::updateActiveLanguages()
         newLanguages = m_engineDebugLanguages;
     else {
         if (m_previousRunConfiguration) {
-            if (m_previousRunConfiguration->useCppDebugger())
+            if (m_previousRunConfiguration->debuggerAspect()->useCppDebugger())
                 newLanguages |= CppLanguage;
-            if (m_previousRunConfiguration->useQmlDebugger())
+            if (m_previousRunConfiguration->debuggerAspect()->useQmlDebugger())
                 newLanguages |= QmlLanguage;
         }
     }

commit 872353a9f77b2bafbc1792444473eecdef10ab64

projectexplorer: fix writing of project settings.
This was broken after 503c6e2684e85cbb8363d885d473aeac0f51cbe5.

Task-number: QTCREATORBUG-6989
Change-Id: I4419c7efb541e7a2f6d089c279d66c9a52baf46b
Reviewed-by: Daniel Teske <daniel.teske@nokia.com>

src/plugins/projectexplorer/runconfiguration.cpp
@@ -368,7 +368,9 @@ Target *RunConfiguration::target() const
 
 QVariantMap RunConfiguration::toMap() const
 {
-    QVariantMap map = m_debuggerAspect->toMap();
+    QVariantMap map = ProjectConfiguration::toMap();
+
+    map.unite(m_debuggerAspect->toMap());
 
     foreach (IRunConfigurationAspect *aspect, m_aspects)
         map.unite(aspect->toMap());