abd62116f8263cf4f8ecfb1ad4f62f7ef4d000ee
Robert Loehning <robert.loehning@nokia.com>
2012-03-08 13:27:00 +0100

    Squish: Removed unneeded workaround
    Change-Id: I38fc9742fa4b26a694d8bcaddabaf0ceddb48d7c
    Reviewed-by: Christian Stenger <christian.stenger@nokia.com>

--- tests/system/shared/project.py
+++ tests/system/shared/project.py
@@ -204,10 +204,6 @@ def createNewQtQuickApplication(workingDir, projectName = None, templateFile = N
         available = __createProjectSelectType__("  Applications", "Qt Quick Application (from existing \.qml file)")
     else:
         available = __createProjectSelectType__("  Applications", "Qt Quick Application (Built-in elements)")
-    # This needs Qt 4.7.4 - a version we don't have for Maemo
-    # it's a hack to keep the test passing, proper version checking would be better
-    if not templateFile and QtQuickConstants.Targets.MAEMO5 in available:
-        available.remove(QtQuickConstants.Targets.MAEMO5)
     projectName = __createProjectSetNameAndPath__(workingDir, projectName)
     if templateFile:
         baseLineEd = waitForObject("{type='Utils::BaseValidatingLineEdit' unnamed='1' visible='1'}", 20000)
