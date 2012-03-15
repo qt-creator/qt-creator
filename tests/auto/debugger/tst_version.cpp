be4adfac23a8ca73fa7290a529cad50a08b1a5d5
hjk <qtc-committer@nokia.com>
2012-03-15 10:25:57 +0100

    debugger: adapt autotest to recognize qnx
    Change-Id: I5fa9c0b1aa900361b346d35e4f5c4c26658f8b14
    Reviewed-by: hjk <qthjk@ovi.com>

--- tests/auto/debugger/tst_version.cpp
+++ tests/auto/debugger/tst_version.cpp
@@ -54,13 +54,16 @@ void tst_Version::version()
     QFETCH(int, gdbVersion);
     QFETCH(int, gdbBuildVersion);
     QFETCH(bool, isMacGdb);
+    QFETCH(bool, isQnxGdb);
     int v = 0, bv = 0;
     bool mac = true;
-    Debugger::Internal::extractGdbVersion(msg, &v, &bv, &mac);
-    qDebug() << msg << " -> " << v << bv << mac;
+    bool qnx = true;
+    Debugger::Internal::extractGdbVersion(msg, &v, &bv, &mac, &qnx);
+    qDebug() << msg << " -> " << v << bv << mac << qnx;
     QCOMPARE(v, gdbVersion);
     QCOMPARE(bv, gdbBuildVersion);
     QCOMPARE(mac, isMacGdb);
+    QCOMPARE(qnx, isQnxGdb);
 }
 
 void tst_Version::version_data()
@@ -69,42 +72,47 @@ void tst_Version::version_data()
     QTest::addColumn<int>("gdbVersion");
     QTest::addColumn<int>("gdbBuildVersion");
     QTest::addColumn<bool>("isMacGdb");
+    QTest::addColumn<bool>("isQnxGdb");
 
     QTest::newRow("Debian")
         << "GNU gdb (GDB) 7.0.1-debian"
-        << 70001 << 0 << false;
+        << 70001 << 0 << false << false;
 
     QTest::newRow("CVS 7.0.90")
         << "GNU gdb (GDB) 7.0.90.20100226-cvs"
-        << 70090 << 20100226 << false;
+        << 70090 << 20100226 << false << false;
 
     QTest::newRow("Ubuntu Lucid")
         << "GNU gdb (GDB) 7.1-ubuntu"
-        << 70100 << 0 << false;
+        << 70100 << 0 << false << false;
 
     QTest::newRow("Fedora 13")
         << "GNU gdb (GDB) Fedora (7.1-22.fc13)"
-        << 70100 << 22 << false;
+        << 70100 << 22 << false << false;
 
     QTest::newRow("Gentoo")
         << "GNU gdb (Gentoo 7.1 p1) 7.1"
-        << 70100 << 1 << false;
+        << 70100 << 1 << false << false;
 
     QTest::newRow("Fedora EL5")
         << "GNU gdb Fedora (6.8-37.el5)"
-        << 60800 << 37 << false;
+        << 60800 << 37 << false << false;
 
     QTest::newRow("SUSE")
         << "GNU gdb (GDB) SUSE (6.8.91.20090930-2.4)"
-        << 60891 << 20090930 << false;
+        << 60891 << 20090930 << false << false;
 
     QTest::newRow("Apple")
         << "GNU gdb 6.3.50-20050815 (Apple version gdb-1461.2)"
-        << 60350 << 1461 << true;
+        << 60350 << 1461 << true << false;
 
     QTest::newRow("Apple")
         << "GNU gdb 6.3.50-20050815 (Apple version gdb-960)"
-        << 60350 << 960 << true;
+        << 60350 << 960 << true << false;
+
+    QTest::newRow("QNX")
+        << "GNU gdb (GDB) 7.3 qnx (rev. 613)"
+        << 70300 << 613 << false << true;
 }
 
 
