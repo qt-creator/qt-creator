77b78a77f8c79d99d900d9f1f3db7ab5fe420bf6
Daniel Teske <daniel.teske@nokia.com>
2012-04-23 13:03:42 +0200

    Revert "Fix Options/Qt Versions adding versions on apply"
    The fix is only needed in master, this was wrongly commited to 2.5
    
    This reverts commit e636c09163cbebc1921f97628ee0354a29d545d4
    
    Change-Id: I2d99e0c06b3ed5b054021aea906ecd59055d659e
    Reviewed-by: Tobias Hunger <tobias.hunger@nokia.com>

--- src/plugins/qtsupport/qtoptionspage.cpp
+++ src/plugins/qtsupport/qtoptionspage.cpp
@@ -114,7 +114,8 @@ void QtOptionsPage::apply()
         return;
     m_widget->finish();
 
-    m_widget->apply();
+    QtVersionManager *vm = QtVersionManager::instance();
+    vm->setNewQtVersions(m_widget->versions());
 }
 
 bool QtOptionsPage::matches(const QString &s) const
@@ -982,18 +983,6 @@ void QtOptionsPageWidget::finish()
 {
     if (QTreeWidgetItem *item = m_ui->qtdirList->currentItem())
         fixQtVersionName(indexForTreeItem(item));
-}
-
-void QtOptionsPageWidget::apply()
-{
-    disconnect(QtVersionManager::instance(), SIGNAL(qtVersionsChanged(QList<int>,QList<int>,QList<int>)),
-            this, SLOT(updateQtVersions(QList<int>,QList<int>,QList<int>)));
-
-    QtVersionManager *vm = QtVersionManager::instance();
-    vm->setNewQtVersions(versions());
-
-    connect(QtVersionManager::instance(), SIGNAL(qtVersionsChanged(QList<int>,QList<int>,QList<int>)),
-            this, SLOT(updateQtVersions(QList<int>,QList<int>,QList<int>)));
 }
 
 /* Checks that the qt version name is unique
