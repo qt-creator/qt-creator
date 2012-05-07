ac430542ae24bfe5d19cdff0a0d44e6f8406527e
Jan Kerekes <jan.kerekes@ixonos.com>
2012-05-07 10:58:35 +0200

    Added new test cases (QMLS06, QMLS07, QMLS08) to QMLS suite
    initial commit
    
    Change-Id: I3be474d8def1161e60421030a799e297e21fc4fe
    Reviewed-by: Robert Löhning <robert.loehning@nokia.com>
    Reviewed-by: Christian Stenger <christian.stenger@nokia.com>

---
+++ tests/system/suite_QMLS/tst_QMLS07/test.py
@@ -0,0 +1,25 @@
+source("../shared/qmls.py")
+
+def main():
+    editorArea = startQtCreatorWithNewAppAtQMLEditor(tempDir(), "SampleApp", "Rectangle {")
+    if not editorArea:
+        return
+    type(editorArea, "<Return>")
+    type(editorArea, "Color")
+    for i in range(3):
+        type(editorArea, "<Left>")
+    invokeMenuItem("File", "Save All")
+    # invoke Refactoring - Add a message suppression comment.
+    numLinesExpected = len(str(editorArea.plainText).splitlines()) + 1
+    ctxtMenu = openContextMenuOnTextCursorPosition(editorArea)
+    activateItem(waitForObjectItem(objectMap.realName(ctxtMenu), "Refactoring"))
+    activateItem(waitForObjectItem(objectMap.realName(ctxtMenu), "Add a comment to suppress this message"))
+    # wait until refactoring ended
+    waitFor("len(str(editorArea.plainText).splitlines()) >= numLinesExpected", 5000)
+    # verify if refactoring was properly applied
+    type(editorArea, "<Up>")
+    test.compare(str(lineUnderCursor(editorArea)).strip(), "// @disable-check M16",
+                 "Verifying 'Add comment to suppress message' refactoring")
+    # save and exit
+    invokeMenuItem("File", "Save All")
+    invokeMenuItem("File", "Exit")
