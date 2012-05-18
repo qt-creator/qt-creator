28d358b01699b1ce4c24a26cc2764d7164cf4fc1
Marc Mutz <marc.mutz@kdab.com>
2012-05-18 20:00:23 +0200

    Use ProStringList::join(QChar) overload where applicable
    Change-Id: I9c9964703dedfdab6e7bfac80be22bd5570e2e49
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>
    (cherry picked from qtbase/f4d47945ba17db276e94046473816014ed0342e9)
    Reviewed-by: Daniel Teske <daniel.teske@digia.com>

--- src/shared/proparser/qmakeevaluator.cpp
+++ src/shared/proparser/qmakeevaluator.cpp
@@ -1364,7 +1364,7 @@ QMakeEvaluator::VisitReturn QMakeEvaluator::visitProFile(
 
         // After user configs, to override them
         if (!m_extraConfigs.isEmpty())
-            evaluateCommand(fL1S("CONFIG += ") + m_extraConfigs.join(fL1S(" ")), fL1S("(extra configs)"));
+            evaluateCommand(fL1S("CONFIG += ") + m_extraConfigs.join(QLatin1Char(' ')), fL1S("(extra configs)"));
     }
 
     debugMsg(1, "visiting file %s", qPrintable(pro->fileName()));
@@ -1379,7 +1379,7 @@ QMakeEvaluator::VisitReturn QMakeEvaluator::visitProFile(
         // Specifically, do not allow a project to override debug/release within a
         // debug_and_release build pass - it's too late for that at this point anyway.
         if (!m_extraConfigs.isEmpty())
-            evaluateCommand(fL1S("CONFIG += ") + m_extraConfigs.join(fL1S(" ")), fL1S("(extra configs)"));
+            evaluateCommand(fL1S("CONFIG += ") + m_extraConfigs.join(QLatin1Char(' ')), fL1S("(extra configs)"));
 
         if ((vr = evaluateFeatureFile(QLatin1String("default_post.prf"))) == ReturnError)
             goto failed;
