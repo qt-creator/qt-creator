qt-creator/qt-creator

commit f77cd29e54f832cbb72533842acb4dda655540f1

IOptionsPage: replace virtual fuctions by data members
Change-Id: I8297df85109b67b79f90c33263a5dc7b89efbb60
Reviewed-by: Eike Ziller <eike.ziller@nokia.com>

src/plugins/debugger/cdb/cdboptionspage.cpp
@@ -230,36 +230,18 @@ CdbOptionsPage::CdbOptionsPage() :
 {
     CdbOptionsPage::m_instance = this;
     m_options->fromSettings(Core::ICore::settings());
+
+    setId(QLatin1String("F.Cda")); // before old CDB
+    setDisplayName(tr("CDB"));
+    setCategory(QLatin1String(Debugger::Constants::DEBUGGER_SETTINGS_CATEGORY));
+    setDisplayCategory(QCoreApplication::translate("Debugger",
+        Constants::DEBUGGER_SETTINGS_TR_CATEGORY));
+    setCategoryIcon(QLatin1String(Constants::DEBUGGER_COMMON_SETTINGS_CATEGORY_ICON));
 }
 
 CdbOptionsPage::~CdbOptionsPage()
 {
     CdbOptionsPage::m_instance = 0;
-}
-
-QString CdbOptionsPage::settingsId()
-{
-    return QLatin1String("F.Cda"); // before old CDB
-}
-
-QString CdbOptionsPage::displayName() const
-{
-    return tr("CDB");
-}
-
-QString CdbOptionsPage::category() const
-{
-    return QLatin1String(Debugger::Constants::DEBUGGER_SETTINGS_CATEGORY);
-}
-
-QString CdbOptionsPage::displayCategory() const
-{
-    return QCoreApplication::translate("Debugger", Debugger::Constants::DEBUGGER_SETTINGS_TR_CATEGORY);
-}
-
-QIcon CdbOptionsPage::categoryIcon() const
-{
-    return QIcon(QLatin1String(Debugger::Constants::DEBUGGER_COMMON_SETTINGS_CATEGORY_ICON));
 }
 
 QWidget *CdbOptionsPage::createPage(QWidget *parent)

src/plugins/debugger/cdb/cdboptionspage.h
@@ -102,18 +102,10 @@ public:
     static CdbOptionsPage *instance();
 
     // IOptionsPage
-    virtual QString id() const { return settingsId(); }
-    virtual QString displayName() const;
-    virtual QString category() const;
-    virtual QString displayCategory() const;
-    QIcon categoryIcon() const;
-
-    virtual QWidget *createPage(QWidget *parent);
-    virtual void apply();
-    virtual void finish();
-    virtual bool matches(const QString &) const;
-
-    static QString settingsId();
+    QWidget *createPage(QWidget *parent);
+    void apply();
+    void finish();
+    bool matches(const QString &) const;
 
     QSharedPointer<CdbOptions> options() const { return m_options; }
 

commit 1a81c33e2f4b061610b0169e819250e2124c41c8

Debugger: Fix compilation on Windows.
Broken by f77cd29e54f832cbb72533842acb4dda655540f1.

Change-Id: I3cbb4e90ffae45ca1475b0de1a00cb5ee58f966a
Reviewed-by: hjk <qthjk@ovi.com>

src/plugins/debugger/cdb/cdboptionspage.cpp
@@ -231,12 +231,18 @@ CdbOptionsPage::CdbOptionsPage() :
     CdbOptionsPage::m_instance = this;
     m_options->fromSettings(Core::ICore::settings());
 
-    setId(QLatin1String("F.Cda")); // before old CDB
+    setId(CdbOptionsPage::settingsId());
+
     setDisplayName(tr("CDB"));
     setCategory(QLatin1String(Debugger::Constants::DEBUGGER_SETTINGS_CATEGORY));
     setDisplayCategory(QCoreApplication::translate("Debugger",
         Constants::DEBUGGER_SETTINGS_TR_CATEGORY));
     setCategoryIcon(QLatin1String(Constants::DEBUGGER_COMMON_SETTINGS_CATEGORY_ICON));
+}
+
+QString CdbOptionsPage::settingsId()
+{
+    return QLatin1String("F.Cda");
 }
 
 CdbOptionsPage::~CdbOptionsPage()

src/plugins/debugger/cdb/cdboptionspage.h
@@ -100,6 +100,7 @@ public:
     virtual ~CdbOptionsPage();
 
     static CdbOptionsPage *instance();
+    static QString settingsId();
 
     // IOptionsPage
     QWidget *createPage(QWidget *parent);