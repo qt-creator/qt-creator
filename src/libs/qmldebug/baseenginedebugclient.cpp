qt-creator/qt-creator

commit 7f09d0b756ff3f9bb52737d4aaf65ed84d751316

Merge QML inspector into debugger plugin
Merge QmlJSInspector plugin into the debugger. Also merge the
extra Inspector window with the Locals & Watchers: It now shows
the QML object tree in the running state.

Change-Id: I59ae0c1b970a48ba10ecda92ed3ba765d94b1d9c
Reviewed-by: Aurindam Jana <aurindam.jana@nokia.com>

src/libs/qmldebug/baseenginedebugclient.cpp
@@ -348,14 +348,16 @@ quint32 BaseEngineDebugClient::queryObjectRecursive(const QmlDebugObjectReferenc
 }
 
 quint32 BaseEngineDebugClient::queryExpressionResult(int objectDebugId,
-                                                    const QString &expr)
+                                                     const QString &expr,
+                                                     int engineId)
 {
     quint32 id = 0;
     if (status() == QmlDebugClient::Enabled && objectDebugId != -1) {
         id = getId();
         QByteArray message;
         QDataStream ds(&message, QIODevice::WriteOnly);
-        ds << QByteArray("EVAL_EXPRESSION") << id << objectDebugId << expr;
+        ds << QByteArray("EVAL_EXPRESSION") << id << objectDebugId << expr
+           << engineId;
         sendMessage(message);
     }
     return id;

src/libs/qmldebug/baseenginedebugclient.h
@@ -66,7 +66,7 @@ public:
     quint32 queryObject(const QmlDebugObjectReference &object);
     quint32 queryObjectRecursive(const QmlDebugObjectReference &object);
     quint32 queryExpressionResult(int objectDebugId,
-                                  const QString &expr);
+                                  const QString &expr, int engineId = -1);
     virtual quint32 setBindingForObject(int objectDebugId, const QString &propertyName,
                                 const QVariant &bindingExpression,
                                 bool isLiteralValue,
@@ -116,7 +116,7 @@ class QmlDebugEngineReference
 {
 public:
     QmlDebugEngineReference() : m_debugId(-1) {}
-    QmlDebugEngineReference(int id) : m_debugId(id) {}
+    explicit QmlDebugEngineReference(int id) : m_debugId(id) {}
 
     int debugId() const { return m_debugId; }
     QString name() const { return m_name; }
@@ -133,7 +133,7 @@ class QmlDebugObjectReference
 {
 public:
     QmlDebugObjectReference() : m_debugId(-1), m_parentId(-1), m_contextDebugId(-1), m_needsMoreData(false) {}
-    QmlDebugObjectReference(int id) : m_debugId(id), m_parentId(-1), m_contextDebugId(-1), m_needsMoreData(false) {}
+    explicit QmlDebugObjectReference(int id) : m_debugId(id), m_parentId(-1), m_contextDebugId(-1), m_needsMoreData(false) {}
 
     int debugId() const { return m_debugId; }
     int parentId() const { return m_parentId; }
@@ -148,18 +148,18 @@ public:
     QList<QmlDebugPropertyReference> properties() const { return m_properties; }
     QList<QmlDebugObjectReference> children() const { return m_children; }
 
-    bool insertObjectInTree(const QmlDebugObjectReference &obj)
+    int insertObjectInTree(const QmlDebugObjectReference &obj)
     {
         for (int i = 0; i < m_children.count(); i++) {
             if (m_children[i].debugId() == obj.debugId()) {
                 m_children.replace(i, obj);
-                return true;
+                return debugId();
             } else {
                 if (m_children[i].insertObjectInTree(obj))
-                    return true;
+                    return debugId();
             }
         }
-        return false;
+        return -1;
     }
 
     bool operator ==(const QmlDebugObjectReference &obj)
@@ -228,5 +228,21 @@ Q_DECLARE_METATYPE(QmlDebug::QmlDebugObjectReference)
 Q_DECLARE_METATYPE(QmlDebug::QmlDebugEngineReference)
 Q_DECLARE_METATYPE(QmlDebug::QmlDebugEngineReferenceList)
 Q_DECLARE_METATYPE(QmlDebug::QmlDebugContextReference)
+
+inline QDebug operator<<(QDebug dbg, const QmlDebug::QmlDebugEngineReference &ref) {
+    dbg.nospace() << "(Engine " << ref.debugId() << "/" << ref.name() <<  ")";
+    return dbg.space();
+}
+
+inline QDebug operator<<(QDebug dbg, const QmlDebug::QmlDebugContextReference &ref) {
+    dbg.nospace() << "(Context " << ref.debugId() << "/" << ref.name() <<  ")";
+    return dbg.space();
+}
+
+inline QDebug operator<<(QDebug dbg, const QmlDebug::QmlDebugObjectReference &ref) {
+    dbg.nospace() << "(Object " << ref.debugId() << "/"
+                  << (ref.idString().isEmpty() ? ref.idString() : ref.className()) <<  ")";
+    return dbg.space();
+}
 
 #endif // BASEENGINEDEBUGCLIENT_H

src/plugins/debugger/qml/qmlengine.cpp
@@ -32,8 +32,10 @@
 
 #include "qmlengine.h"
 #include "qmladapter.h"
+#include "qmlinspectoradapter.h"
 #include "interactiveinterpreter.h"
 #include "baseqmldebuggerclient.h"
+#include "qmlinspectoragent.h"
 
 #include "debuggerstartparameters.h"
 #include "debuggeractions.h"
@@ -113,13 +115,13 @@ public:
 private:
     friend class QmlEngine;
     QmlAdapter m_adapter;
+    QmlInspectorAdapter m_inspectorAdapter;
     ApplicationLauncher m_applicationLauncher;
     QTimer m_noDebugOutputTimer;
     QmlOutputParser m_outputParser;
     QHash<QString, QTextDocument*> m_sourceDocuments;
     QHash<QString, QWeakPointer<TextEditor::ITextEditor> > m_sourceEditors;
     InteractiveInterpreter m_interpreter;
-    bool m_validContext;
     QHash<QString,BreakpointModelId> pendingBreakpoints;
     QList<quint32> queryIds;
     bool m_retryOnConnectFail;
@@ -128,10 +130,11 @@ private:
 
 QmlEnginePrivate::QmlEnginePrivate(QmlEngine *q)
     : m_adapter(q),
-      m_validContext(false),
+      m_inspectorAdapter(&m_adapter, q),
       m_retryOnConnectFail(false),
       m_automaticConnect(false)
-{}
+{
+}
 
 class ASTWalker: public Visitor
 {
@@ -326,13 +329,17 @@ QmlEngine::QmlEngine(const DebuggerStartParameters &startParameters,
             SLOT(updateCurrentContext()));
     connect(this->stackHandler(), SIGNAL(currentIndexChanged()),
             SLOT(updateCurrentContext()));
-    connect(&d->m_adapter, SIGNAL(selectionChanged()),
+    connect(&d->m_inspectorAdapter, SIGNAL(selectionChanged()),
             SLOT(updateCurrentContext()));
+    connect(d->m_inspectorAdapter.agent(), SIGNAL(
+                expressionResult(quint32,QVariant)),
+            SLOT(expressionEvaluated(quint32,QVariant)));
     connect(d->m_adapter.messageClient(),
             SIGNAL(message(QtMsgType,QString,
                            QmlDebug::QDebugContextInfo)),
             SLOT(appendDebugOutput(QtMsgType,QString,
                                    QmlDebug::QDebugContextInfo)));
+
 
     connect(&d->m_applicationLauncher,
         SIGNAL(processExited(int)),
@@ -1027,19 +1034,23 @@ void QmlEngine::updateWatchData(const WatchData &data,
 {
 //    qDebug() << "UPDATE WATCH DATA" << data.toString();
     //watchHandler()->rebuildModel();
-    showStatusMessage(tr("Stopped."), 5000);
+    //showStatusMessage(tr("Stopped."), 5000);
 
-    if (!data.name.isEmpty() && d->m_adapter.activeDebuggerClient()) {
-        if (data.isValueNeeded()) {
-            d->m_adapter.activeDebuggerClient()->updateWatchData(data);
+    if (data.iname.startsWith("inspect.")) {
+        d->m_inspectorAdapter.agent()->updateWatchData(data);
+    } else {
+        if (!data.name.isEmpty() && d->m_adapter.activeDebuggerClient()) {
+            if (data.isValueNeeded()) {
+                d->m_adapter.activeDebuggerClient()->updateWatchData(data);
+            }
+            if (data.isChildrenNeeded()
+                    && watchHandler()->isExpandedIName(data.iname)) {
+                d->m_adapter.activeDebuggerClient()->expandObject(data.iname, data.id);
+            }
         }
-        if (data.isChildrenNeeded()
-                && watchHandler()->isExpandedIName(data.iname)) {
-            d->m_adapter.activeDebuggerClient()->expandObject(data.iname, data.id);
-        }
+        synchronizeWatchers();
     }
 
-    synchronizeWatchers();
 
     if (!data.isSomethingNeeded())
         watchHandler()->insertData(data);
@@ -1111,8 +1122,7 @@ void QmlEngine::updateCurrentContext()
 {
     const QString context = state() == InferiorStopOk ?
                 stackHandler()->currentFrame().function :
-                d->m_adapter.currentSelectedDisplayName();
-    d->m_validContext = !context.isEmpty();
+                d->m_inspectorAdapter.currentSelectedDisplayName();
     showMessage(tr("Context: ").append(context), QtMessageLogStatus);
 }
 
@@ -1149,55 +1159,39 @@ void QmlEngine::executeDebuggerCommand(const QString &command, DebuggerLanguages
     }
 }
 
-bool QmlEngine::evaluateScriptExpression(const QString& expression)
+bool QmlEngine::evaluateScriptExpression(const QString &expression)
 {
     bool didEvaluate = true;
     //Check if string is only white spaces
     if (!expression.trimmed().isEmpty()) {
-        //Check for a valid context
-        if (d->m_validContext) {
-            //check if it can be evaluated
-            if (canEvaluateScript(expression)) {
-                //Evaluate expression based on engine state
-                //When engine->state() == InferiorStopOk, the expression
-                //is sent to V8DebugService. In all other cases, the
-                //expression is evaluated by QDeclarativeEngine.
-                if (state() != InferiorStopOk) {
-                    BaseEngineDebugClient *engineDebug =
-                            d->m_adapter.engineDebugClient();
-
-                    int id = d->m_adapter.currentSelectedDebugId();
-                    if (engineDebug && id != -1) {
-                        quint32 queryId =
-                                engineDebug->queryExpressionResult(
-                                    id, expression);
-                        if (queryId) {
-                            d->queryIds << queryId;
-                        } else {
-                            didEvaluate = false;
-                            qtMessageLogHandler()->
-                                    appendItem(
-                                        new QtMessageLogItem(
-                                            qtMessageLogHandler()->root(),
-                                            QtMessageLogHandler::ErrorType,
-                                            _("Error evaluating expression.")));
-                        }
-                    }
+        //check if it can be evaluated
+        if (canEvaluateScript(expression)) {
+            //Evaluate expression based on engine state
+            //When engine->state() == InferiorStopOk, the expression
+            //is sent to V8DebugService. In all other cases, the
+            //expression is evaluated by QDeclarativeEngine.
+            if (state() != InferiorStopOk) {
+                QmlInspectorAgent *agent = d->m_inspectorAdapter.agent();
+                quint32 queryId
+                        = agent->queryExpressionResult(
+                            d->m_inspectorAdapter.currentSelectedDebugId(),
+                            expression);
+                if (queryId) {
+                    d->queryIds << queryId;
                 } else {
-                    executeDebuggerCommand(expression, QmlLanguage);
+                    didEvaluate = false;
+                    qtMessageLogHandler()->
+                            appendItem(
+                                new QtMessageLogItem(
+                                    qtMessageLogHandler()->root(),
+                                    QtMessageLogHandler::ErrorType,
+                                    _("Error evaluating expression.")));
                 }
             } else {
-                didEvaluate = false;
+                executeDebuggerCommand(expression, QmlLanguage);
             }
         } else {
-            //Incase of invalid context, show Error message
-            qtMessageLogHandler()->
-                            appendItem(new QtMessageLogItem(
-                                           qtMessageLogHandler()->root(),
-                                           QtMessageLogHandler::ErrorType,
-                                           _("Cannot evaluate without "
-                                             "a valid QML/JS Context.")),
-                                       qtMessageLogHandler()->rowCount());
+            didEvaluate = false;
         }
     }
     return didEvaluate;

src/plugins/debugger/qml/qmlinspectoragent.h
@@ -1,0 +1,160 @@
+/**************************************************************************
+**
+** This file is part of Qt Creator
+**
+** Copyright (c) 2012 Nokia Corporation and/or its subsidiary(-ies).
+**
+** Contact: Nokia Corporation (qt-info@nokia.com)
+**
+**
+** GNU Lesser General Public License Usage
+**
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this file.
+** Please review the following information to ensure the GNU Lesser General
+** Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** Other Usage
+**
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at qt-info@nokia.com.
+**
+**************************************************************************/
+
+#ifndef QMLINSPECTORAGENT_H
+#define QMLINSPECTORAGENT_H
+
+#include <QObject>
+
+#include <qmldebug/baseenginedebugclient.h>
+#include <watchdata.h>
+
+using namespace QmlDebug;
+
+namespace Debugger {
+
+class DebuggerEngine;
+
+namespace Internal {
+
+class WatchData;
+
+//map <filename, editorRevision> -> <lineNumber, columnNumber> -> debugId
+typedef
+QHash<QPair<QString, int>, QHash<QPair<int, int>, QList<int> > > DebugIdHash;
+
+class QmlInspectorAgent : public QObject
+{
+    Q_OBJECT
+public:
+    explicit QmlInspectorAgent(DebuggerEngine *engine, QObject *parent = 0);
+
+
+    void refreshObjectTree();
+    void fetchObject(int debugId);
+    quint32 queryExpressionResult(int debugId, const QString &expression);
+
+    void updateWatchData(const WatchData &data);
+    void selectObjectInTree(int debugId);
+
+    quint32 setBindingForObject(int objectDebugId,
+                                const QString &propertyName,
+                                const QVariant &value,
+                                bool isLiteralValue,
+                                QString source,
+                                int line);
+    quint32 setMethodBodyForObject(int objectDebugId, const QString &methodName,
+                                   const QString &methodBody);
+    quint32 resetBindingForObject(int objectDebugId,
+                                  const QString &propertyName);
+
+    QList<QmlDebugObjectReference> objects() const;
+    QmlDebugObjectReference objectForId(int debugId) const;
+    QmlDebugObjectReference objectForId(const QString &objectId) const;
+    QmlDebugObjectReference objectForLocation(int line, int column) const;
+    QList<QmlDebugObjectReference> rootObjects() const { return m_rootObjects; }
+    DebugIdHash debugIdHash() const { return m_debugIdHash; }
+
+    bool addObjectWatch(int objectDebugId);
+    bool isObjectBeingWatched(int objectDebugId);
+    bool removeObjectWatch(int objectDebugId);
+    void removeAllObjectWatches();
+
+    void setEngineClient(BaseEngineDebugClient *client);
+
+signals:
+    void objectTreeUpdated();
+    void objectFetched(const QmlDebugObjectReference &ref);
+    void expressionResult(quint32 queryId, const QVariant &value);
+    void propertyChanged(int debugId, const QByteArray &propertyName,
+                         const QVariant &propertyValue);
+
+private slots:
+    void updateStatus();
+    void onResult(quint32 queryId, const QVariant &value, const QByteArray &type);
+    void newObjects();
+
+private:
+    void reloadEngines();
+    void queryEngineContext(int id);
+    quint32 fetchContextObject(const QmlDebugObjectReference &obj);
+    void fetchRootObjects(const QmlDebugContextReference &context, bool clear);
+
+    void updateEngineList(const QmlDebugEngineReferenceList &engines);
+    void rootContextChanged(const QmlDebugContextReference &context);
+    void objectTreeFetched(const QmlDebugObjectReference &result);
+    void onCurrentObjectsFetched(const QmlDebugObjectReference &result);
+    bool getObjectHierarchy(const QmlDebugObjectReference &object);
+
+
+    void buildDebugIdHashRecursive(const QmlDebugObjectReference &ref);
+    QList<WatchData> buildWatchData(const QmlDebugObjectReference &obj,
+                                           const WatchData &parent);
+    void addObjectToTree(const QmlDebugObjectReference &obj, bool notify);
+
+    QmlDebugObjectReference objectForId(
+            int debugId,
+            const QmlDebugObjectReference &ref) const;
+    QList<QmlDebugObjectReference> objects(
+            const QmlDebugObjectReference &objectRef) const;
+
+
+    enum LogDirection {
+        LogSend,
+        LogReceive
+    };
+    void log(LogDirection direction, const QString &message);
+
+    bool isConnected();
+
+private:
+    DebuggerEngine *m_engine;
+    QmlDebug::BaseEngineDebugClient *m_engineClient;
+
+    quint32 m_engineQueryId;
+    quint32 m_rootContextQueryId;
+    int m_objectToSelect;
+    QList<quint32> m_objectTreeQueryIds;
+    QList<QmlDebugObjectReference> m_rootObjects;
+    QList<quint32> m_fetchCurrentObjectsQueryIds;
+    QList<QmlDebugObjectReference> m_fetchCurrentObjects;
+    QmlDebugEngineReferenceList m_engines;
+    QHash<int, QByteArray> m_debugIdToIname;
+    DebugIdHash m_debugIdHash;
+
+    QList<int> m_objectWatches;
+};
+
+} // Internal
+} // Debugger
+
+#endif // QMLINSPECTORAGENT_H

commit a79e38ac6f59bf3a8398e807dac64b2265e466be

QmlJSInspector: Set property values
QML property values could be set in the inspector.
Regression introduced by 7f09d0b756ff3f9bb52737d4aaf65ed84d751316.
This patch is a partial fix. Only values can be assigned currently.
For expressions, use the console.

Change-Id: I945b11109cd7788dd0f5277af206bf9658844aee
Reviewed-by: Christiaan Janssen <christiaan.janssen@nokia.com>
Reviewed-by: hjk <qthjk@ovi.com>

src/libs/qmldebug/baseenginedebugclient.cpp
@@ -286,14 +286,14 @@ quint32 BaseEngineDebugClient::addWatch(const ObjectReference &object,
     return id;
 }
 
-quint32 BaseEngineDebugClient::addWatch(int objectId)
+quint32 BaseEngineDebugClient::addWatch(int objectDebugId)
 {
     quint32 id = 0;
     if (status() == Enabled) {
         id = getId();
         QByteArray message;
         QDataStream ds(&message, QIODevice::WriteOnly);
-        ds << QByteArray("WATCH_OBJECT") << id << objectId;
+        ds << QByteArray("WATCH_OBJECT") << id << objectDebugId;
         sendMessage(message);
     }
     return id;

src/libs/qmldebug/baseenginedebugclient.h
@@ -56,7 +56,7 @@ public:
     quint32 addWatch(const PropertyReference &property);
     quint32 addWatch(const ContextReference &context, const QString &id);
     quint32 addWatch(const ObjectReference &object, const QString &expr);
-    quint32 addWatch(int objectId);
+    quint32 addWatch(int objectDebugId);
     quint32 addWatch(const FileReference &file);
 
     void removeWatch(quint32 watch);

src/plugins/debugger/qml/qmlengine.cpp
@@ -989,10 +989,11 @@ bool QmlEngine::setToolTipExpression(const QPoint &mousePos,
 void QmlEngine::assignValueInDebugger(const WatchData *data,
     const QString &expression, const QVariant &valueV)
 {
-    if (!expression.isEmpty() && m_adapter.activeDebuggerClient()) {
-        m_adapter.activeDebuggerClient()->assignValueInDebugger(data,
-                                                                   expression,
-                                                                   valueV);
+    if (!expression.isEmpty()) {
+        if (data->isInspect() && m_inspectorAdapter.agent())
+            m_inspectorAdapter.agent()->assignValue(data, expression, valueV);
+        else if (m_adapter.activeDebuggerClient())
+            m_adapter.activeDebuggerClient()->assignValueInDebugger(data, expression, valueV);
     }
 }
 

src/plugins/debugger/qml/qmlinspectoragent.cpp
@@ -81,6 +81,23 @@ quint32 QmlInspectorAgent::queryExpressionResult(int debugId,
                                                  m_engine.debugId());
 }
 
+void QmlInspectorAgent::assignValue(const WatchData *data,
+                                    const QString &expr, const QVariant &valueV)
+{
+    if (debug)
+        qDebug() << __FUNCTION__ << "(" << data->id << ")" << data->iname;
+
+    if (data->id) {
+        QString val(valueV.toString());
+        if (valueV.type() == QVariant::String) {
+            val = val.replace(QLatin1Char('\"'), QLatin1String("\\\""));
+            val = QLatin1Char('\"') + val + QLatin1Char('\"');
+        }
+        QString expression = QString(_("%1 = %2;")).arg(expr).arg(val);
+        queryExpressionResult(data->id, expression);
+    }
+}
+
 void QmlInspectorAgent::updateWatchData(const WatchData &data)
 {
     if (debug)
@@ -269,7 +286,7 @@ bool QmlInspectorAgent::addObjectWatch(int objectDebugId)
     if (m_engineClient->addWatch(objectDebugId))
         m_objectWatches.append(objectDebugId);
 
-    return false;
+    return true;
 }
 
 bool QmlInspectorAgent::isObjectBeingWatched(int objectDebugId)
@@ -316,6 +333,8 @@ void QmlInspectorAgent::setEngineClient(BaseEngineDebugClient *client)
                    this, SLOT(onResult(quint32,QVariant,QByteArray)));
         disconnect(m_engineClient, SIGNAL(newObject(int,int,int)),
                    this, SLOT(newObject(int,int,int)));
+        disconnect(m_engineClient, SIGNAL(valueChanged(int,QByteArray,QVariant)),
+                   this, SLOT(onValueChanged(int,QByteArray,QVariant)));
     }
 
     m_engineClient = client;
@@ -327,6 +346,8 @@ void QmlInspectorAgent::setEngineClient(BaseEngineDebugClient *client)
                 this, SLOT(onResult(quint32,QVariant,QByteArray)));
         connect(m_engineClient, SIGNAL(newObject(int,int,int)),
                 this, SLOT(newObject(int,int,int)));
+        connect(m_engineClient, SIGNAL(valueChanged(int,QByteArray,QVariant)),
+                this, SLOT(onValueChanged(int,QByteArray,QVariant)));
     }
 
     updateStatus();
@@ -416,6 +437,21 @@ void QmlInspectorAgent::newObject(int engineId, int objectId, int /*parentId*/)
         fetchObject(objectId);
     else
         m_delayQueryTimer.start();
+}
+
+void QmlInspectorAgent::onValueChanged(int debugId, const QByteArray &propertyName,
+                                       const QVariant &value)
+{
+    const QByteArray iname = m_debugIdToIname.value(debugId) +
+            ".[properties]." + propertyName;
+    const WatchData *data = m_debuggerEngine->watchHandler()->findData(iname);
+    if (debug)
+        qDebug() << __FUNCTION__ << "(" << debugId << ")" << iname <<value.toString();
+    if (data) {
+        WatchData d(*data);
+        d.value = value.toString();
+        m_debuggerEngine->watchHandler()->insertData(d);
+    }
 }
 
 void QmlInspectorAgent::reloadEngines()
@@ -660,6 +696,7 @@ QList<WatchData> QmlInspectorAgent::buildWatchData(const ObjectReference &obj,
 
     list.append(objWatch);
     m_debugIdToIname.insert(objWatch.id, objWatch.iname);
+    addObjectWatch(objWatch.id);
 
     if (m_engineClient->objectName() != QmlDebug::Constants::QML_DEBUGGER) {
         QList<int> childIds;
@@ -694,6 +731,7 @@ QList<WatchData> QmlInspectorAgent::buildWatchData(const ObjectReference &obj,
 
         foreach (const PropertyReference &property, obj.properties()) {
             WatchData propertyWatch;
+            propertyWatch.id = objWatch.id;
             propertyWatch.exp = property.name().toLatin1();
             propertyWatch.name = property.name();
             propertyWatch.iname = buildIName(propertiesWatch.iname, property.name());
@@ -744,6 +782,9 @@ void QmlInspectorAgent::clearObjectTree()
     m_debugIdToIname.clear();
     m_debugIdChildIds.clear();
     m_objectStack.clear();
+
+    removeAllObjectWatches();
 }
 } // Internal
 } // Debugger
+

src/plugins/debugger/qml/qmlinspectoragent.h
@@ -62,6 +62,7 @@ public:
     void fetchObject(int debugId);
     quint32 queryExpressionResult(int debugId, const QString &expression);
 
+    void assignValue(const WatchData *data, const QString &expression, const QVariant &valueV);
     void updateWatchData(const WatchData &data);
     void selectObjectInTree(int debugId);
 
@@ -105,6 +106,7 @@ private slots:
     void updateStatus();
     void onResult(quint32 queryId, const QVariant &value, const QByteArray &type);
     void newObject(int engineId, int objectId, int parentId);
+    void onValueChanged(int debugId, const QByteArray &propertyName, const QVariant &value);
 
 private:
     void reloadEngines();

src/plugins/debugger/watchhandler.cpp
@@ -1139,12 +1139,13 @@ Qt::ItemFlags WatchModel::flags(const QModelIndex &idx) const
         = Qt::ItemIsSelectable | Qt::ItemIsEnabled;
     static const Qt::ItemFlags editable = notEditable | Qt::ItemIsEditable;
 
-    // Disable editing if debuggee is positively running.
+    // Disable editing if debuggee is positively running except for Inspector data
+    const WatchData &data = *watchItem(idx);
     const bool isRunning = engine() && engine()->state() == InferiorRunOk;
-    if (isRunning && engine() && !engine()->hasCapability(AddWatcherWhileRunningCapability))
+    if (isRunning && engine() && !engine()->hasCapability(AddWatcherWhileRunningCapability) &&
+            !data.isInspect())
         return notEditable;
 
-    const WatchData &data = *watchItem(idx);
     if (data.isWatcher()) {
         if (idx.column() == 0 && data.iname.count('.') == 1)
             return editable; // Watcher names are editable.
@@ -1159,6 +1160,9 @@ Qt::ItemFlags WatchModel::flags(const QModelIndex &idx) const
     } else if (data.isLocal()) {
         if (idx.column() == 1 && data.valueEditable)
             return editable; // Locals values are sometimes editable.
+    } else if (data.isInspect()) {
+        if (idx.column() == 1 && data.valueEditable)
+            return editable; // Inspector values are sometimes editable.
     }
     return notEditable;
 }