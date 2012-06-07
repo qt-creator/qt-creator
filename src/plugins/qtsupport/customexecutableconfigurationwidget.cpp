d10b52741a05a55eb90f16bf5afeacd05c6d94f4
Tobias Hunger <tobias.hunger@nokia.com>
2012-06-07 13:05:47 +0200

    Make custom process step and custom exectuable step UI more similar
    Change-Id: I7d3c5109ac56f5d4627ad590d8e13e235433fe59
    Reviewed-by: Eike Ziller <eike.ziller@nokia.com>

--- src/plugins/qtsupport/customexecutableconfigurationwidget.cpp
+++ src/plugins/qtsupport/customexecutableconfigurationwidget.cpp
@@ -62,7 +62,7 @@ CustomExecutableConfigurationWidget::CustomExecutableConfigurationWidget(CustomE
     m_executableChooser = new Utils::PathChooser(this);
     m_executableChooser->setExpectedKind(Utils::PathChooser::Command);
     m_executableChooser->setEnvironment(rc->environment());
-    layout->addRow(tr("Executable:"), m_executableChooser);
+    layout->addRow(tr("Command:"), m_executableChooser);
 
     m_commandLineArgumentsLineEdit = new QLineEdit(this);
     m_commandLineArgumentsLineEdit->setMinimumWidth(200); // this shouldn't be fixed here...
