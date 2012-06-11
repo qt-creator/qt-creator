qt-creator/qt-creator

commit 2574a5846983a3016e551432d24f8d8d4e3b97b5

Debugger:Clearing the object tree corrected.
newObjectsCreated reset while clearing the object tree.

Task-number: QTCREATORBUG-7519
Change-Id: Ie5ee9bfd0faafb60f968a0c219b372eea6679761
Reviewed-by: Aurindam Jana <aurindam.jana@nokia.com>

src/plugins/debugger/qml/qmlinspectoragent.cpp
@@ -877,6 +877,7 @@ void QmlInspectorAgent::clearObjectTree()
     m_debugIdToIname.clear();
     m_debugIdChildIds.clear();
     m_objectStack.clear();
+    m_newObjectsCreated = false;
 
     removeAllObjectWatches();
 }

commit fdabb6d6f934ab55c6ba59a0b504d24148d7e1ef

Debugger: resetting of newObjectsCreated done only for Qt5.x
done to correct the regression introduced in
2574a5846983a3016e551432d24f8d8d4e3b97b5

Change-Id: I0d9fb8fabcd134f53f1a1c9d0fa04ca7b542a88c
Reviewed-by: Aurindam Jana <aurindam.jana@nokia.com>

src/plugins/debugger/qml/qmlinspectoragent.cpp
@@ -879,7 +879,9 @@ void QmlInspectorAgent::clearObjectTree()
     m_debugIdToIname.clear();
     m_debugIdChildIds.clear();
     m_objectStack.clear();
-    m_newObjectsCreated = false;
+    // reset only for 5.x.
+    if (m_engineClient->objectName() == QmlDebug::Constants::QML_DEBUGGER)
+        m_newObjectsCreated = false;
 
     removeAllObjectWatches();
 }