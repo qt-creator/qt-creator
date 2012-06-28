qt-creator/qt-creator

commit ded2dd12b84e67712e2bc5e0c8c0f392fa2479a6

debugger: adjust to profile changes
This replaces the debugger command, sysroot and target abi fields
with a profile id.

Change-Id: I831c42ff8624fcfa520c2f28f6f06d73191b2680
Reviewed-by: Tobias Hunger <tobias.hunger@nokia.com>

src/plugins/debugger/cdb/cdbengine.cpp
@@ -368,24 +368,13 @@ static inline bool isMsvcFlavor(Abi::OSFlavor osf)
       || osf == Abi::WindowsMsvc2010Flavor;
 }
 
-static QString cdbBinary(const DebuggerStartParameters &sp)
-{
-    if (!sp.debuggerCommand.isEmpty()) {
-        // Do not use a GDB binary if we got started for a project with MinGW runtime.
-        const bool abiMatch = sp.toolChainAbi.os() == Abi::WindowsOS
-                   && isMsvcFlavor(sp.toolChainAbi.osFlavor());
-        if (abiMatch)
-            return sp.debuggerCommand;
-    }
-    return debuggerCore()->debuggerForAbi(sp.toolChainAbi, CdbEngineType);
-}
-
 bool checkCdbConfiguration(const DebuggerStartParameters &sp, ConfigurationCheck *check)
 {
 #ifdef Q_OS_WIN
+    const Abi abi = toolChainAbi(sp);
     if (!isCdbEngineEnabled()) {
         check->errorDetails.push_back(CdbEngine::tr("The CDB debug engine required for %1 is currently disabled.").
-                           arg(sp.toolChainAbi.toString()));
+                           arg(abi.toString()));
         check->settingsCategory = QLatin1String(Debugger::Constants::DEBUGGER_SETTINGS_CATEGORY);
         check->settingsPage = CdbOptionsPage::settingsId();
         return false;
@@ -396,19 +385,19 @@ bool checkCdbConfiguration(const DebuggerStartParameters &sp, ConfigurationCheck
         return false;
     }
 
-    if (sp.toolChainAbi.binaryFormat() != Abi::PEFormat || sp.toolChainAbi.os() != Abi::WindowsOS) {
+    if (abi.binaryFormat() != Abi::PEFormat || abi.os() != Abi::WindowsOS) {
         check->errorDetails.push_back(CdbEngine::tr("The CDB debug engine does not support the %1 ABI.").
-                                      arg(sp.toolChainAbi.toString()));
+                                      arg(abi.toString()));
         return false;
     }
 
-    if (sp.startMode == AttachCore && !isMsvcFlavor(sp.toolChainAbi.osFlavor())) {
+    if (sp.startMode == AttachCore && !isMsvcFlavor(abi.osFlavor())) {
         check->errorDetails.push_back(CdbEngine::tr("The CDB debug engine cannot debug gdb core files."));
         return false;
     }
 
-    if (cdbBinary(sp).isEmpty()) {
-        check->errorDetails.push_back(msgNoCdbBinaryForToolChain(sp.toolChainAbi));
+    if (debuggerCommand(sp).isEmpty()) {
+        check->errorDetails.push_back(msgNoCdbBinaryForToolChain(abi));
         check->settingsCategory = QLatin1String(ProjectExplorer::Constants::PROJECTEXPLORER_SETTINGS_CATEGORY);
         check->settingsPage = QLatin1String(ProjectExplorer::Constants::PROJECTEXPLORER_SETTINGS_CATEGORY);
         return false;
@@ -722,7 +711,7 @@ bool CdbEngine::launchCDB(const DebuggerStartParameters &sp, QString *errorMessa
     // Determine binary (force MSVC), extension lib name and path to use
     // The extension is passed as relative name with the path variable set
     //(does not work with absolute path names)
-    const QString executable = cdbBinary(sp);
+    const QString executable = sp.debuggerCommand;
     if (executable.isEmpty()) {
         *errorMessage = tr("There is no CDB executable specified.");
         return false;

src/plugins/android/androiddebugsupport.cpp
@@ -37,9 +37,10 @@
 #include "androidrunner.h"
 #include "androidmanager.h"
 
-#include <debugger/debuggerplugin.h>
-#include <debugger/debuggerrunner.h>
 #include <debugger/debuggerengine.h>
+#include <debugger/debuggerplugin.h>
+#include <debugger/debuggerprofileinformation.h>
+#include <debugger/debuggerrunner.h>
 #include <debugger/debuggerstartparameters.h>
 
 #include <projectexplorer/target.h>
@@ -69,11 +70,14 @@ static Qt4Project *project(AndroidRunConfiguration *rc)
 RunControl *AndroidDebugSupport::createDebugRunControl(AndroidRunConfiguration *runConfig)
 {
     DebuggerStartParameters params;
-    params.toolChainAbi = runConfig->abi();
+    Profile *profile = runConfig->target()->profile();
+    params.sysRoot = SysRootProfileInformation::sysRoot(profile).toString();
+    params.debuggerCommand = DebuggerProfileInformation::debuggerCommand(profile).toString();
+    if (ToolChain *tc = ToolChainProfileInformation::toolChain(profile))
+        params.toolChainAbi = tc->targetAbi();
     params.dumperLibrary = runConfig->dumperLib();
     params.startMode = AttachToRemoteServer;
     params.executable = project(runConfig)->rootQt4ProjectNode()->buildDir() + QLatin1String("/app_process");
-    params.debuggerCommand = runConfig->gdbCmd().toString();
     params.remoteChannel = runConfig->remoteChannel();
     params.displayName = AndroidManager::packageName(runConfig->target());
 

src/plugins/debugger/attachcoredialog.ui
@@ -60,7 +60,7 @@
      <item row="2" column="0">
       <widget class="QLabel" name="toolchainLabel">
        <property name="text">
-        <string>&amp;Tool chain:</string>
+        <string>&amp;Target:</string>
        </property>
        <property name="buddy">
         <cstring>toolchainComboBox</cstring>
@@ -68,7 +68,7 @@
       </widget>
      </item>
      <item row="2" column="1">
-      <widget class="Debugger::Internal::DebuggerToolChainComboBox" name="toolchainComboBox"/>
+      <widget class="Debugger::ProfileChooser" name="toolchainComboBox"/>
      </item>
      <item row="3" column="1">
       <widget class="Utils::PathChooser" name="overrideStartScriptFileName" native="true"/>
@@ -129,7 +129,7 @@
    </slots>
   </customwidget>
   <customwidget>
-   <class>Debugger::Internal::DebuggerToolChainComboBox</class>
+   <class>Debugger::ProfileChooser</class>
    <extends>QComboBox</extends>
    <header>debuggertoolchaincombobox.h</header>
   </customwidget>

src/plugins/debugger/attachexternaldialog.ui
@@ -44,7 +44,7 @@
      <item row="1" column="0">
       <widget class="QLabel" name="toolchainLabel">
        <property name="text">
-        <string>&amp;Tool chain:</string>
+        <string>&amp;Target:</string>
        </property>
        <property name="buddy">
         <cstring>toolchainComboBox</cstring>
@@ -52,7 +52,7 @@
       </widget>
      </item>
      <item row="1" column="1">
-      <widget class="Debugger::Internal::DebuggerToolChainComboBox" name="toolchainComboBox"/>
+      <widget class="Debugger::ProfileChooser" name="toolchainComboBox"/>
      </item>
     </layout>
    </item>
@@ -94,7 +94,7 @@
    <header location="global">utils/filterlineedit.h</header>
   </customwidget>
   <customwidget>
-   <class>Debugger::Internal::DebuggerToolChainComboBox</class>
+   <class>Debugger::ProfileChooser</class>
    <extends>QComboBox</extends>
    <header>debuggertoolchaincombobox.h</header>
   </customwidget>

src/plugins/debugger/attachtoqmlportdialog.ui
@@ -1,97 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>Debugger::Internal::AttachToQmlPortDialog</class>
- <widget class="QDialog" name="Debugger::Internal::AttachToQmlPortDialog">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>212</width>
-    <height>136</height>
-   </rect>
-  </property>
-  <property name="windowTitle">
-   <string>Start Debugger</string>
-  </property>
-  <layout class="QVBoxLayout" name="verticalLayout">
-   <item>
-    <layout class="QFormLayout" name="formLayout">
-     <item row="0" column="0">
-      <widget class="QLabel" name="hostLabel">
-       <property name="text">
-        <string>&amp;Host:</string>
-       </property>
-       <property name="buddy">
-        <cstring>hostLineEdit</cstring>
-       </property>
-      </widget>
-     </item>
-     <item row="0" column="1">
-      <widget class="QLineEdit" name="hostLineEdit">
-       <property name="text">
-        <string notr="true">localhost</string>
-       </property>
-      </widget>
-     </item>
-     <item row="1" column="0">
-      <widget class="QLabel" name="portLabel">
-       <property name="text">
-        <string>&amp;Port:</string>
-       </property>
-       <property name="buddy">
-        <cstring>portSpinBox</cstring>
-       </property>
-      </widget>
-     </item>
-     <item row="1" column="1">
-      <widget class="QSpinBox" name="portSpinBox">
-       <property name="maximum">
-        <number>65535</number>
-       </property>
-       <property name="value">
-        <number>3768</number>
-       </property>
-      </widget>
-     </item>
-     <item row="2" column="0">
-      <widget class="QLabel" name="sysrootLabel">
-       <property name="text">
-        <string>Sys&amp;root:</string>
-       </property>
-       <property name="buddy">
-        <cstring>sysRootChooser</cstring>
-       </property>
-      </widget>
-     </item>
-     <item row="2" column="1">
-      <widget class="Utils::PathChooser" name="sysRootChooser" native="true"/>
-     </item>
-    </layout>
-   </item>
-   <item>
-    <widget class="QDialogButtonBox" name="buttonBox">
-     <property name="orientation">
-      <enum>Qt::Horizontal</enum>
-     </property>
-     <property name="standardButtons">
-      <set>QDialogButtonBox::Cancel|QDialogButtonBox::Ok</set>
-     </property>
-    </widget>
-   </item>
-  </layout>
- </widget>
- <customwidgets>
-  <customwidget>
-   <class>Utils::PathChooser</class>
-   <extends>QWidget</extends>
-   <header location="global">utils/pathchooser.h</header>
-   <container>1</container>
-   <slots>
-    <signal>editingFinished()</signal>
-    <signal>browsingFinished()</signal>
-   </slots>
-  </customwidget>
- </customwidgets>
- <resources/>
- <connections/>
-</ui>

src/plugins/debugger/debugger.pro
@@ -143,8 +143,7 @@ FORMS += attachexternaldialog.ui \
     commonoptionspage.ui \
     startexternaldialog.ui \
     startremotedialog.ui \
-    startremoteenginedialog.ui \
-    attachtoqmlportdialog.ui
+    startremoteenginedialog.ui
 
 RESOURCES += debugger.qrc
 

src/plugins/debugger/debugger.qbs
@@ -42,7 +42,6 @@ QtcPlugin {
         "debuggerprofileinformation.h",
         "attachcoredialog.ui",
         "attachexternaldialog.ui",
-        "attachtoqmlportdialog.ui",
         "basewindow.cpp",
         "breakhandler.cpp",
         "breakhandler.h",

src/plugins/debugger/debuggercore.h
@@ -110,8 +110,8 @@ public:
     virtual bool initialize(const QStringList &arguments, QString *errorMessage) = 0;
     virtual QWidget *mainWindow() const = 0;
     virtual bool isDockVisible(const QString &objectName) const = 0;
-    virtual QString debuggerForAbi(const ProjectExplorer::Abi &abi,
-        DebuggerEngineType et = NoEngineType) const = 0;
+//    virtual QString debuggerForAbi(const ProjectExplorer::Abi &abi,
+//        DebuggerEngineType et = NoEngineType) const = 0;
     virtual void showModuleSymbols(const QString &moduleName,
         const QVector<Symbol> &symbols) = 0;
     virtual void openMemoryEditor() = 0;

commit 7c7e6bd6c44da5b3509d9c00f64c8172fea2e4c2

Debugger: Fix compilation.
Breakage introduced by ded2dd12b84e67712e2bc5e0c8c0f392fa2479a6.

Change-Id: I2293024c42c0b237960ad0a1dee06a01dd777ba8
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@nokia.com>

src/plugins/debugger/cdb/cdbengine.cpp
@@ -371,7 +371,7 @@ static inline bool isMsvcFlavor(Abi::OSFlavor osf)
 bool checkCdbConfiguration(const DebuggerStartParameters &sp, ConfigurationCheck *check)
 {
 #ifdef Q_OS_WIN
-    const Abi abi = toolChainAbi(sp);
+    const Abi abi = sp.toolChainAbi;
     if (!isCdbEngineEnabled()) {
         check->errorDetails.push_back(CdbEngine::tr("The CDB debug engine required for %1 is currently disabled.").
                            arg(abi.toString()));
@@ -396,7 +396,7 @@ bool checkCdbConfiguration(const DebuggerStartParameters &sp, ConfigurationCheck
         return false;
     }
 
-    if (debuggerCommand(sp).isEmpty()) {
+    if (sp.debuggerCommand.isEmpty()) {
         check->errorDetails.push_back(msgNoCdbBinaryForToolChain(abi));
         check->settingsCategory = QLatin1String(ProjectExplorer::Constants::PROJECTEXPLORER_SETTINGS_CATEGORY);
         check->settingsPage = QLatin1String(ProjectExplorer::Constants::PROJECTEXPLORER_SETTINGS_CATEGORY);