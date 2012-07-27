qt-creator/qt-creator

commit 24314562165588b56a318b3b8a846bf5deda7c41

Profile introduction
Introduce Profiles to store sets of values that describe a system/device.

These profiles are held by a target, getting rid of much of the information
stored in the Build-/Run-/DeployConfigurations, greatly simplifying those.

This is a squash of the wip/profile branch which has been on gerrit for a
while, rebased to current master.

Change-Id: I25956c8dd4d1962b2134bfaa8a8076ae3909460f
Reviewed-by: Daniel Teske <daniel.teske@nokia.com>

src/plugins/madde/maemoremotemounter.cpp
@@ -33,8 +33,10 @@
 #include "maemoremotemounter.h"
 
 #include "maemoglobal.h"
-#include "qt4maemotarget.h"
+#include "maddedevice.h"
 
+#include <projectexplorer/profileinformation.h>
+#include <projectexplorer/target.h>
 #include <ssh/sshconnection.h>
 #include <ssh/sshremoteprocess.h>
 #include <qt4projectmanager/qt4buildconfiguration.h>
@@ -79,11 +81,9 @@ void MaemoRemoteMounter::setBuildConfiguration(const Qt4BuildConfiguration *bc)
 {
     QTC_ASSERT(m_state == Inactive, return);
 
-    const QtSupport::BaseQtVersion * const qtVersion = bc->qtVersion();
-    const AbstractQt4MaemoTarget * const maemoTarget
-        = qobject_cast<AbstractQt4MaemoTarget *>(bc->target());
-    m_remoteMountsAllowed = maemoTarget && maemoTarget->allowsRemoteMounts();
-    m_maddeRoot = qtVersion ? MaemoGlobal::maddeRoot(qtVersion->qmakeCommand().toString()) : QString();
+    Core::Id typeId = ProjectExplorer::DeviceTypeProfileInformation::deviceTypeId(bc->target()->profile());
+    m_remoteMountsAllowed = MaddeDevice::allowsRemoteMounts(typeId);
+    m_maddeRoot = ProjectExplorer::SysRootProfileInformation::sysRoot(bc->target()->profile());
 }
 
 void MaemoRemoteMounter::addMountSpecification(const MaemoMountSpecification &mountSpec,
@@ -297,7 +297,7 @@ void MaemoRemoteMounter::startUtfsServers()
         connect(utfsServerProc.data(), SIGNAL(readyReadStandardError()), this,
             SLOT(handleUtfsServerStderr()));
         m_utfsServers << utfsServerProc;
-        utfsServerProc->start(utfsServer(), utfsServerArgs);
+        utfsServerProc->start(utfsServer().toString(), utfsServerArgs);
     }
 
     setState(UtfsServersStarted);
@@ -342,9 +342,10 @@ QString MaemoRemoteMounter::utfsClientOnDevice() const
     return QLatin1String("/usr/lib/mad-developer/utfs-client");
 }
 
-QString MaemoRemoteMounter::utfsServer() const
+Utils::FileName MaemoRemoteMounter::utfsServer() const
 {
-    return m_maddeRoot + QLatin1String("/madlib/utfs-server");
+    Utils::FileName result = m_maddeRoot;
+    return result.appendPath(QLatin1String("madlib/utfs-server"));
 }
 
 void MaemoRemoteMounter::killAllUtfsServers()

commit e9a81cad455da862bb3cef966698481c10969bc8

Madde: Fix madde root setting.
Was broken by 24314562165588b56a318b3b8a846bf5deda7c41.

Change-Id: I7eebad12dd23974412660d457ebd6e99b158c92a
Reviewed-by: Christian Kandeler <christian.kandeler@nokia.com>

src/plugins/madde/maemoremotemounter.cpp
@@ -78,7 +78,7 @@ void MaemoRemoteMounter::setProfile(const Profile *profile)
 
     Core::Id typeId = DeviceTypeProfileInformation::deviceTypeId(profile);
     m_remoteMountsAllowed = MaddeDevice::allowsRemoteMounts(typeId);
-    m_maddeRoot = SysRootProfileInformation::sysRoot(profile);
+    m_maddeRoot = SysRootProfileInformation::sysRoot(profile).parentDir().parentDir();
 }
 
 void MaemoRemoteMounter::addMountSpecification(const MaemoMountSpecification &mountSpec,