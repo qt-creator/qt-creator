362b8d989dcc54833684962bda8540b103f867ee
Thomas Hartmann <Thomas.Hartmann@digia.com>
2012-09-26 13:52:50 +0200

    QmlDesigner: fix for qmlpuppet and Qt 5
    Change-Id: Ie199a0e3d8342fb62ff51ec2475134cdf4f9dcee
    Reviewed-by: Marco Bubke <marco.bubke@digia.com>

--- share/qtcreator/qml/qmlpuppet/qmlpuppet/instances/nodeinstanceserver.cpp
+++ share/qtcreator/qml/qmlpuppet/qmlpuppet/instances/nodeinstanceserver.cpp
@@ -966,7 +966,7 @@ InformationChangedCommand NodeInstanceServer::createAllInformationChangedCommand
 
 static bool supportedVariantType(QVariant::Type type)
 {
-    return type < QVariant::UserType && type != QVariant::Icon;
+    return type < int(QVariant::UserType) && type != QMetaType::QObjectStar && type != QVariant::Icon;
 }
 
 ValuesChangedCommand NodeInstanceServer::createValuesChangedCommand(const QList<ServerNodeInstance> &instanceList) const
