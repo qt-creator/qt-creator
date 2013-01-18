1edce87173acbef69df733fe1aef348afbeae4ba
Daniel Teske <daniel.teske@digia.com>
2013-01-18 14:35:43 +0100

    Readd Android specific code for Qt Quick 2 template
    I tested that it works.
    
    Change-Id: I01387c302c48f3ba9244ba40099f1dead970fb33
    Reviewed-by: Kai Koehne <kai.koehne@digia.com>
    Reviewed-by: Alessandro Portale <alessandro.portale@digia.com>
    Reviewed-by: BogDan Vatra <bogdan@kde.org>

--- share/qtcreator/templates/qtquick2app/qtquick2applicationviewer/qtquick2applicationviewer.cpp
+++ share/qtcreator/templates/qtquick2app/qtquick2applicationviewer/qtquick2applicationviewer.cpp
@@ -26,7 +26,7 @@ QString QtQuick2ApplicationViewerPrivate::adjustPath(const QString &path)
     if (!QDir::isAbsolutePath(path))
         return QString::fromLatin1("%1/../Resources/%2")
                 .arg(QCoreApplication::applicationDirPath(), path);
-#elif defined(Q_OS_UNIX)
+#elif defined(Q_OS_UNIX) && !defined(Q_OS_ANDROID)
     const QString pathInInstallDir =
             QString::fromLatin1("%1/../%2").arg(QCoreApplication::applicationDirPath(), path);
     if (QFileInfo(pathInInstallDir).exists())
@@ -51,7 +51,11 @@ QtQuick2ApplicationViewer::~QtQuick2ApplicationViewer()
 void QtQuick2ApplicationViewer::setMainQmlFile(const QString &file)
 {
     d->mainQmlFile = QtQuick2ApplicationViewerPrivate::adjustPath(file);
+#ifdef Q_OS_ANDROID
+    setSource(QUrl(QLatin1String("assets:/")+d->mainQmlFile));
+#else
     setSource(QUrl::fromLocalFile(d->mainQmlFile));
+#endif
 }
 
 void QtQuick2ApplicationViewer::addImportPath(const QString &path)
