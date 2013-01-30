4e9a8232554a877b82b1a52144e83cf6a0a411c5
Oswald Buddenhagen <oswald.buddenhagen@digia.com>
2013-01-30 18:37:11 +0100

    don't flush qmake command line twice
    otherwise variable assignments (including -config options) from
    $QMAKEFLAGS are lost.
    
    Change-Id: I818e9372d2b0ff44333dc3eb8fc3420f84ab01c5
    Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>
    (cherry picked from qtbase/864e1ceaf628e823103ae2d28d2c983400ba38c7)

--- src/shared/proparser/qmakeglobals.h
+++ src/shared/proparser/qmakeglobals.h
@@ -87,6 +87,8 @@ public:
     QString pwd;
     QStringList precmds, preconfigs, postcmds, postconfigs;
     bool after;
+
+    void flush() { after = false; }
 };
 
 class QMAKE_EXPORT QMakeGlobals
