59216c270fa273bc15827d5873154112916f3a9d
Robert Loehning <robert.loehning@digia.com>
2013-02-08 18:28:07 +0100

    HTML improvement
    Change-Id: I95e2628e28a48ad05bb2230aa7089dd45f615a74
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>

--- src/plugins/help/helpviewer_qtb.cpp
+++ src/plugins/help/helpviewer_qtb.cpp
@@ -172,8 +172,8 @@ void HelpViewer::setSource(const QUrl &url)
         ? HelpViewer::tr("<title>about:blank</title>")
         : HelpViewer::tr("<html><head><meta http-equiv=\""
             "content-type\" content=\"text/html; charset=UTF-8\"><title>Error 404...</title>"
-            "</head><body><div align=\"center\"><br><br><h1>The page could not be found</h1>"
-            "<br><h3>'%1'</h3></div></body>")
+            "</head><body><div align=\"center\"><br/><br/><h1>The page could not be found</h1>"
+            "<br/><h3>'%1'</h3></div></body>")
           .arg(url.toString()));
 
     emit loadFinished(true);
