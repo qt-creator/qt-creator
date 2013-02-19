ef6a9d06f4b835265305b1178e96526ab62e7656
El Mehdi Fekari <mfekari@rim.com>
2013-02-19 19:32:33 +0100

    Qnx: Add BB10 style to the BB QWidget-based app templates
    Change-Id: Iff25302043b4f6078f9a147e52ebde7624aa7585
    Reviewed-by: Nicolas Arnaud-Cormos <nicolas@kdab.com>

--- share/qtcreator/templates/wizards/bb-guiapp/main.cpp
+++ share/qtcreator/templates/wizards/bb-guiapp/main.cpp
@@ -4,6 +4,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    // Set default BB10 style. You can also use bb10dark style
+    a.setStyle(QLatin1String("bb10bright"));
 
     MainWidget w;
     w.showMaximized();
