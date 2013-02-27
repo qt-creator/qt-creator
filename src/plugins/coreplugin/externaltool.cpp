0e0ce962bf7c79205dc7c421397c8384bc2092c5
Eike Ziller <eike.ziller@digia.com>
2013-02-27 17:44:48 +0100

    Don't destroy non-Latin1 characters when reading external tools.
    Task-number: QTCREATORBUG-8603
    
    Change-Id: Ie8a0265165cf1fc3e806335738185de97a062c4f
    Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>

--- src/plugins/coreplugin/externaltool.cpp
+++ src/plugins/coreplugin/externaltool.cpp
@@ -319,7 +319,7 @@ static void localizedText(const QStringList &locales, QXmlStreamReader *reader, 
     } else {
         if (*currentLocale < 0 && currentText->isEmpty()) {
             *currentText = QCoreApplication::translate("Core::Internal::ExternalTool",
-                                                       reader->readElementText().toLatin1().constData());
+                                                       reader->readElementText().toUtf8().constData());
         } else {
             reader->skipCurrentElement();
         }
