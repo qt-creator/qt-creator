d67bf48408b1417620b4a6ce177c76bd5da22f09
Tobias Hunger <tobias.hunger@digia.com>
2013-03-21 13:42:58 +0100

    Fix debugger auto-detection
    Do not change the debugger when autodetecting it if the current
    one is ok already.
    
    Task-number: QTCREATORBUG-8954
    Change-Id: I258da68abb121f5ce9a7618269adf783ab8de487
    Reviewed-by: hjk <hjk121@nokiamail.com>

--- src/plugins/debugger/debuggerkitinformation.cpp
+++ src/plugins/debugger/debuggerkitinformation.cpp
@@ -158,6 +158,9 @@ unsigned int DebuggerKitInformation::priority() const
 
 DebuggerKitInformation::DebuggerItem DebuggerKitInformation::autoDetectItem(const Kit *k)
 {
+    if (DebuggerKitInformation::isValidDebugger(k))
+        return DebuggerKitInformation::debuggerItem(k);
+
     DebuggerItem result;
     const ToolChain *tc = ToolChainKitInformation::toolChain(k);
     Abi abi = Abi::hostAbi();
@@ -203,8 +206,7 @@ DebuggerKitInformation::DebuggerItem DebuggerKitInformation::autoDetectItem(cons
 void DebuggerKitInformation::setup(Kit *k)
 {
     QTC_ASSERT(ToolChainManager::instance()->isLoaded(), return);
-    if (!isValidDebugger(k))
-        setDebuggerItem(k, autoDetectItem(k));
+    setDebuggerItem(k, autoDetectItem(k));
 }
 
 // Check the configuration errors and return a flag mask. Provide a quick check and
