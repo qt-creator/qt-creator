qt-creator/qt-creator

commit bb13b8d65e8a0748750ba03a04e411c1474cfe91

QmlDesigner: Fix memory leak because of template magic
Be careful if you use a private smart pointer.

Change-Id: Ia78583b080c8d936d98517c55d3294d3040f8cdb
Reviewed-by: Thomas Hartmann <Thomas.Hartmann@digia.com>

src/plugins/qmldesigner/components/formeditor/resizecontroller.cpp
@@ -152,6 +152,21 @@ ResizeController::ResizeController(LayerItem *layerItem, FormEditorItem *formEdi
     updatePosition();
 }
 
+ResizeController::ResizeController(const ResizeController &other)
+    : m_data(other.m_data)
+{
+
+}
+
+ResizeController::~ResizeController()
+{
+}
+
+ResizeController &ResizeController::operator =(const ResizeController &other)
+{
+    m_data = other.m_data;
+}
+
 
 bool ResizeController::isValid() const
 {

src/plugins/qmldesigner/components/formeditor/resizecontroller.h
@@ -49,6 +49,10 @@ public:
 
     ResizeController();
     ResizeController(LayerItem *layerItem, FormEditorItem *formEditorItem);
+    ResizeController(const ResizeController &resizeController);
+    ~ResizeController();
+
+    ResizeController& operator=(const ResizeController &other);
 
     void show();
     void hide();

commit aba13e37bc5e7ac93f69a56c0d7a099a50c210be

Fix compilation of QML Designer.
Breakage introduced by bb13b8d65e8a0748750ba03a04e411c1474cfe91 .

Change-Id: Ib3b13167f5ada0e406275cb4ff4dddfedc222345
Reviewed-by: Thomas Hartmann <Thomas.Hartmann@digia.com>

src/plugins/qmldesigner/components/formeditor/resizecontroller.cpp
@@ -164,7 +164,9 @@ ResizeController::~ResizeController()
 
 ResizeController &ResizeController::operator =(const ResizeController &other)
 {
-    m_data = other.m_data;
+    if (this != &other)
+        m_data = other.m_data;
+    return *this;
 }
 
 