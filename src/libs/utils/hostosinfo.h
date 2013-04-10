qt-creator/qt-creator

commit 3c6f76d16c142c2b98564fc9167b67564ead7fe5

HostOsInfo: Add some more useful abstractions.
Namely:
- path list separator
- executable suffix
- file name case sensitivity

All of these are duplicated in various places in the current Creator
code.

Change-Id: I86eb4662fa3c2071759bd728cae1aaf7111ae686
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@nokia.com>

src/libs/utils/hostosinfo.h
@@ -32,6 +32,14 @@
 
 #include "utils_global.h"
 
+#include <QString>
+
+#ifdef Q_OS_WIN
+#define QTC_HOST_EXE_SUFFIX ".exe"
+#else
+#define QTC_HOST_EXE_SUFFIX ""
+#endif // Q_OS_WIN
+
 namespace Utils {
 
 class QTCREATOR_UTILS_EXPORT HostOsInfo
@@ -46,6 +54,24 @@ public:
     static bool isLinuxHost() { return hostOs() == HostOsLinux; }
     static bool isMacHost() { return hostOs() == HostOsMac; }
     static inline bool isAnyUnixHost();
+
+    static QString appendExecutableSuffix(const QString &executable)
+    {
+        QString finalName = executable;
+        if (isWindowsHost())
+            finalName += QLatin1String(QTC_HOST_EXE_SUFFIX);
+        return finalName;
+    }
+
+    static Qt::CaseSensitivity fileNameCaseSensitivity()
+    {
+        return isWindowsHost() ? Qt::CaseInsensitive: Qt::CaseSensitive;
+    }
+
+    static QChar pathListSeparator()
+    {
+        return isWindowsHost() ? QLatin1Char(';') : QLatin1Char(':');
+    }
 };
 
 HostOsInfo::HostOs HostOsInfo::hostOs()

src/plugins/qt4projectmanager/qt4nodes.cpp
@@ -2270,8 +2270,7 @@ TargetInformation Qt4ProFileNode::targetInformation(QtSupport::ProFileReader *re
     result.executable = QDir::cleanPath(wd + QLatin1Char('/') + result.target);
     //qDebug() << "##### updateTarget sets:" << result.workingDir << result.executable;
 
-    if (Utils::HostOsInfo::isWindowsHost())
-        result.executable += QLatin1String(".exe");
+    Utils::HostOsInfo::appendExecutableSuffix(result.executable);
     result.valid = true;
     return result;
 }

src/libs/utils/environment.cpp
@@ -62,11 +62,6 @@ Q_GLOBAL_STATIC(SystemEnvironment, staticSystemEnvironment)
 
 namespace Utils {
 
-static QChar varSeparator()
-{
-    return HostOsInfo::isWindowsHost() ? QLatin1Char(';') : QLatin1Char(':');
-}
-
 static bool sortEnvironmentItem(const EnvironmentItem &a, const EnvironmentItem &b)
 {
     return a.name < b.name;
@@ -172,12 +167,14 @@ void Environment::prependOrSet(const QString&key, const QString &value, const QS
 
 void Environment::appendOrSetPath(const QString &value)
 {
-    appendOrSet(QLatin1String("PATH"), QDir::toNativeSeparators(value), QString(varSeparator()));
+    appendOrSet(QLatin1String("PATH"), QDir::toNativeSeparators(value),
+            QString(HostOsInfo::pathListSeparator()));
 }
 
 void Environment::prependOrSetPath(const QString &value)
 {
-    prependOrSet(QLatin1String("PATH"), QDir::toNativeSeparators(value), QString(varSeparator()));
+    prependOrSet(QLatin1String("PATH"), QDir::toNativeSeparators(value),
+            QString(HostOsInfo::pathListSeparator()));
 }
 
 void Environment::prependOrSetLibrarySearchPath(const QString &value)
@@ -271,7 +268,8 @@ QString Environment::searchInPath(const QStringList &executables,
 
 QStringList Environment::path() const
 {
-    return m_values.value(QLatin1String("PATH")).split(varSeparator(), QString::SkipEmptyParts);
+    return m_values.value(QLatin1String("PATH")).split(HostOsInfo::pathListSeparator(),
+                                                       QString::SkipEmptyParts);
 }
 
 QString Environment::value(const QString &key) const

src/libs/utils/fileutils.cpp
@@ -413,9 +413,6 @@ TempFileSaver::~TempFileSaver()
     On windows filenames are compared case insensitively.
 */
 
-const Qt::CaseSensitivity FileName::cs
-    = HostOsInfo::isWindowsHost() ? Qt::CaseInsensitive : Qt::CaseSensitive;
-
 FileName::FileName()
     : QString()
 {
@@ -492,7 +489,7 @@ FileName::FileName(const QString &string)
 
 bool FileName::operator==(const FileName &other) const
 {
-    return QString::compare(*this, other, cs) == 0;
+    return QString::compare(*this, other, HostOsInfo::fileNameCaseSensitivity()) == 0;
 }
 
 bool FileName::operator!=(const FileName &other) const
@@ -502,12 +499,12 @@ bool FileName::operator!=(const FileName &other) const
 
 bool FileName::operator<(const FileName &other) const
 {
-    return QString::compare(*this, other, cs) < 0;
+    return QString::compare(*this, other, HostOsInfo::fileNameCaseSensitivity()) < 0;
 }
 
 bool FileName::operator<=(const FileName &other) const
 {
-    return QString::compare(*this, other, cs) <= 0;
+    return QString::compare(*this, other, HostOsInfo::fileNameCaseSensitivity()) <= 0;
 }
 
 bool FileName::operator>(const FileName &other) const
@@ -523,7 +520,7 @@ bool FileName::operator>=(const FileName &other) const
 /// \returns whether FileName is a child of \a s
 bool FileName::isChildOf(const FileName &s) const
 {
-    if (!QString::startsWith(s, cs))
+    if (!QString::startsWith(s, HostOsInfo::fileNameCaseSensitivity()))
         return false;
     if (size() <= s.size())
         return false;
@@ -539,7 +536,7 @@ bool FileName::isChildOf(const QDir &dir) const
 /// \returns whether FileName endsWith \a s
 bool FileName::endsWith(const QString &s) const
 {
-    return QString::endsWith(s, cs);
+    return QString::endsWith(s, HostOsInfo::fileNameCaseSensitivity());
 }
 
 /// \returns the relativeChildPath of FileName to parent if FileName is a child of parent

src/libs/utils/fileutils.h
@@ -175,7 +175,6 @@ public:
     using QString::isNull;
     using QString::clear;
 private:
-    static const Qt::CaseSensitivity cs;
     FileName(const QString &string);
 };
 

src/libs/utils/pathlisteditor.cpp
@@ -94,7 +94,7 @@ void PathListPlainTextEdit::insertFromMimeData(const QMimeData *source)
     if (source->hasText()) {
         // replace separator
         QString text = source->text().trimmed();
-        text.replace(PathListEditor::separator(), QLatin1Char('\n'));
+        text.replace(HostOsInfo::pathListSeparator(), QLatin1Char('\n'));
         QSharedPointer<QMimeData> fixed(new QMimeData);
         fixed->setText(text);
         QPlainTextEdit::insertFromMimeData(fixed.data());
@@ -190,7 +190,7 @@ int PathListEditor::lastAddActionIndex()
 
 QString PathListEditor::pathListString() const
 {
-    return pathList().join(separator());
+    return pathList().join(HostOsInfo::pathListSeparator());
 }
 
 QStringList PathListEditor::pathList() const
@@ -216,7 +216,8 @@ void PathListEditor::setPathList(const QString &pathString)
     if (pathString.isEmpty()) {
         clear();
     } else {
-        setPathList(pathString.split(separator(), QString::SkipEmptyParts));
+        setPathList(pathString.split(HostOsInfo::pathListSeparator(),
+                QString::SkipEmptyParts));
     }
 }
 
@@ -252,11 +253,6 @@ void PathListEditor::slotInsert()
     const QString dir = QFileDialog::getExistingDirectory(this, d->fileDialogTitle);
     if (!dir.isEmpty())
         insertPathAtCursor(QDir::toNativeSeparators(dir));
-}
-
-QChar PathListEditor::separator()
-{
-    return HostOsInfo::isWindowsHost() ? QLatin1Char(';') : QLatin1Char(':');
 }
 
 // Add a button "Import from 'Path'"

src/libs/utils/pathlisteditor.h
@@ -54,8 +54,6 @@ public:
     QStringList pathList() const;
     QString fileDialogTitle() const;
 
-    static QChar separator();
-
     // Add a convenience action "Import from 'Path'" (environment variable)
     void addEnvVariableImportAction(const QString &var);
 

src/libs/utils/synchronousprocess.cpp
@@ -665,7 +665,7 @@ QString SynchronousProcess::locateBinary(const QString &path, const QString &bin
             return currentDirBinary;
     }
 
-    const QStringList paths = path.split(pathSeparator());
+    const QStringList paths = path.split(HostOsInfo::pathListSeparator());
     if (paths.empty())
         return QString();
     const QStringList::const_iterator cend = paths.constEnd();
@@ -682,11 +682,6 @@ QString SynchronousProcess::locateBinary(const QString &binary)
 {
     const QByteArray path = qgetenv("PATH");
     return locateBinary(QString::fromLocal8Bit(path), binary);
-}
-
-QChar SynchronousProcess::pathSeparator()
-{
-    return HostOsInfo::isWindowsHost() ? QLatin1Char(';') : QLatin1Char(':');
 }
 
 } // namespace Utils

commit 66f9337fdf9a7f41b901f6871c275ded8e6ed1e7

Qt4RunConfiguration: Add missing .exe
Broke in 3c6f76d16c142c2b98564fc9167b67564ead7fe5

Task-number: QTCREATORBUG-9046
Change-Id: I9bace8f8cefaefe9adf73aa3c5ebd93dae66e258
Reviewed-by: Eike Ziller <eike.ziller@digia.com>
Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>

src/libs/utils/hostosinfo.h
@@ -58,7 +58,7 @@ public:
     static bool isMacHost() { return hostOs() == HostOsMac; }
     static inline bool isAnyUnixHost();
 
-    static QString appendExecutableSuffix(const QString &executable)
+    static QString withExecutableSuffix(const QString &executable)
     {
         QString finalName = executable;
         if (isWindowsHost())

src/plugins/qt4projectmanager/qt4nodes.cpp
@@ -2180,7 +2180,7 @@ TargetInformation Qt4ProFileNode::targetInformation(QtSupport::ProFileReader *re
     result.executable = QDir::cleanPath(destDir + QLatin1Char('/') + result.target);
     //qDebug() << "##### updateTarget sets:" << result.workingDir << result.executable;
 
-    Utils::HostOsInfo::appendExecutableSuffix(result.executable);
+    result.executable = Utils::HostOsInfo::withExecutableSuffix(result.executable);
     result.valid = true;
 
     if (readerBP)