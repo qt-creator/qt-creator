53fb29bddab0e57f933a5438dddacbf740e4e636
Tobias Nätterlund <tobias.naetterlund.qnx@kdab.com>
2013-04-19 13:50:17 +0200

    QtQuick2ApplicationViewer: properly resolve install paths
    This is a port of commit 2982545d to also apply it to
    the qtquick2applicationviewer.cpp template.
    
    Change-Id: Ia9d964d9fc98ea957c52aea783c6148449552755
    Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>
    Reviewed-by: Eike Ziller <eike.ziller@digia.com>

--- share/qtcreator/templates/qtquick2app/qtquick2applicationviewer/qtquick2applicationviewer.cpp
+++ share/qtcreator/templates/qtquick2app/qtquick2applicationviewer/qtquick2applicationviewer.cpp
@@ -29,9 +29,13 @@ QString QtQuick2ApplicationViewerPrivate::adjustPath(const QString &path)
 #elif defined(Q_OS_QNX)
     if (!QDir::isAbsolutePath(path))
         return QString::fromLatin1("app/native/%1").arg(path);
-#elif defined(Q_OS_UNIX) && !defined(Q_OS_ANDROID)
-    const QString pathInInstallDir =
+#elif !defined(Q_OS_ANDROID)
+    QString pathInInstallDir =
             QString::fromLatin1("%1/../%2").arg(QCoreApplication::applicationDirPath(), path);
+    if (QFileInfo(pathInInstallDir).exists())
+        return pathInInstallDir;
+    pathInInstallDir =
+            QString::fromLatin1("%1/%2").arg(QCoreApplication::applicationDirPath(), path);
     if (QFileInfo(pathInInstallDir).exists())
         return pathInInstallDir;
 #endif
