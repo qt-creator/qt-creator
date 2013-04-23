qt-creator/qt-creator

commit 6a4310a44afc0396b462f9ac9f3524161155645a

EditorManager: Remove code duplication.
Change-Id: I366852f788c1d28351e9f853b16d180886892c55
Reviewed-by: David Schulz <david.schulz@digia.com>

src/plugins/coreplugin/editormanager/editormanager.cpp
@@ -2176,11 +2176,7 @@ Core::IEditor *EditorManager::duplicateEditor(Core::IEditor *editor)
 
 void EditorManager::split(Qt::Orientation orientation)
 {
-    SplitterOrView *view = d->m_currentView;
-
-    if (!view)
-        view = d->m_currentEditor ? d->m_splitter->findView(d->m_currentEditor)
-                       : d->m_splitter->findFirstView();
+    SplitterOrView *view = currentSplitterOrView();
 
     if (view && !view->splitter())
         view->split(orientation);
@@ -2200,9 +2196,7 @@ void EditorManager::splitSideBySide()
 
 void EditorManager::removeCurrentSplit()
 {
-    SplitterOrView *viewToClose = d->m_currentView;
-    if (!viewToClose && d->m_currentEditor)
-        viewToClose = d->m_splitter->findView(d->m_currentEditor);
+    SplitterOrView *viewToClose = currentSplitterOrView();
 
     if (!viewToClose || viewToClose->isSplitter() || viewToClose == d->m_splitter)
         return;
@@ -2231,14 +2225,7 @@ void EditorManager::gotoOtherSplit()
     if (!d->m_splitter->isSplitter())
         splitSideBySide();
 
-    SplitterOrView *currentView = d->m_currentView;
-    if (!currentView && d->m_currentEditor)
-        currentView = d->m_splitter->findView(d->m_currentEditor);
-    if (!currentView)
-        currentView = d->m_splitter->findFirstView();
-    SplitterOrView *view = d->m_splitter->findNextView(currentView);
-    if (!view)
-        view = d->m_splitter->findFirstView();
+    SplitterOrView *view = currentSplitterOrView();
     if (view) {
         if (IEditor *editor = view->editor()) {
             setCurrentEditor(editor, true);

commit dbef8d21af281c827ff49391e15129a496c66ff0

Fix "open link in other split"
I.e. ctrl+alt+click in the code editor.

Broke with 6a4310a44afc0396b462f9ac9f3524161155645a

Change-Id: I63e8f61f8d6f082773eaf70e427d706000b4d01d
Reviewed-by: Petar Perisin <petar.perisin@gmail.com>
Reviewed-by: David Schulz <david.schulz@digia.com>

src/plugins/coreplugin/editormanager/editormanager.cpp
@@ -2226,6 +2226,9 @@ void EditorManager::gotoOtherSplit()
         splitSideBySide();
 
     SplitterOrView *view = currentSplitterOrView();
+    view = d->m_splitter->findNextView(view);
+    if (!view)
+        view = d->m_splitter->findFirstView();
     if (view) {
         if (IEditor *editor = view->editor()) {
             setCurrentEditor(editor, true);