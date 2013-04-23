849534ec6fd20ef8f2c5821e96a5e0e3cfbd32b5
Nikolai Kosjar <nikolai.kosjar@digia.com>
2013-04-23 11:46:55 +0200

    Revert "C++: fix support for typedef of templated typedefs"
    Still crashes when opening the Qt Creator project,
    g++ (Ubuntu/Linaro 4.7.2-2ubuntu1) 4.7.2 (Ubuntu 12.10).
    
    This reverts commit 564c9b2842663062658a0febdcc5787098d871b2.
    
    Change-Id: Ief5c0aad463d245f68805f747d277ac298796c3d
    Reviewed-by: Nikolai Kosjar <nikolai.kosjar@digia.com>

--- src/libs/cplusplus/ResolveExpression.cpp
+++ src/libs/cplusplus/ResolveExpression.cpp
@@ -690,15 +690,8 @@ bool ResolveExpression::visit(CallAST *ast)
             }
 
         } else if (Function *funTy = ty->asFunctionType()) {
-            if (maybeValidPrototype(funTy, actualArgumentCount)) {
-                LookupItem item;
-                item.setType(funTy->returnType().simplified());
-                item.setScope(scope);
-                // we have to remember a binding because it can be a template instantiation
-                item.setBinding(result.binding());
-
-                _results.append(item);
-            }
+            if (maybeValidPrototype(funTy, actualArgumentCount))
+                addResult(funTy->returnType().simplified(), scope);
 
         } else if (Class *classTy = ty->asClassType()) {
             // Constructor call
