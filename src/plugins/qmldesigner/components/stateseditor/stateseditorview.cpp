1c6a3b9dbfab31eb36838da9bba609fe469d27e6
Thomas Hartmann <Thomas.Hartmann@digia.com>
2013-05-21 11:22:32 +0200

    QmlDesigner: Check for QtQuick 2.1 in states editor
    Task-number: QTCREATORBUG-9298
    Change-Id: I8660e7d9614e77d23e1ef6515904941505f410dd
    Reviewed-by: Thomas Hartmann <Thomas.Hartmann@digia.com>
    Reviewed-by: Marco Bubke <marco.bubke@digia.com>

--- src/plugins/qmldesigner/components/stateseditor/stateseditorview.cpp
+++ src/plugins/qmldesigner/components/stateseditor/stateseditorview.cpp
@@ -135,7 +135,8 @@ void StatesEditorView::addState()
         if ((rootStateGroup().allStates().count() < 1) && //QtQuick import might be missing
             (!model()->hasImport(Import::createLibraryImport("QtQuick", "1.0"), true)
              && !model()->hasImport(Import::createLibraryImport("QtQuick", "1.1"), true)
-             && !model()->hasImport(Import::createLibraryImport("QtQuick", "2.0"), true)))
+             && !model()->hasImport(Import::createLibraryImport("QtQuick", "2.0"), true)
+             && !model()->hasImport(Import::createLibraryImport("QtQuick", "2.1"), true)))
             model()->changeImports(QList<Import>() << Import::createLibraryImport("QtQuick", "1.0"), QList<Import>());
         ModelNode newState = rootStateGroup().addState(newStateName);
         setCurrentState(newState);
