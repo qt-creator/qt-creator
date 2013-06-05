qt-creator/qt-creator

commit 92392454b74b5f79af5095e4f5725f0d4f57e6f1

Git: Do not pop stash if a command is in progress
Running interactive rebase and choosing edit for a commit causes
stash pop when the commit is expected to be edited

Change-Id: I37249e8486b609dedba143bd130059444f882303
Reviewed-by: Petar Perisin <petar.perisin@gmail.com>
Reviewed-by: Tobias Hunger <tobias.hunger@digia.com>

src/plugins/git/gitclient.cpp
@@ -648,12 +648,13 @@ public:
 
     ~ConflictHandler()
     {
+        GitClient *client = GitPlugin::instance()->gitClient();
         if (m_commit.isEmpty()) {
             GitPlugin::instance()->gitVersionControl()->emitRepositoryChanged(m_workingDirectory);
-            GitPlugin::instance()->gitClient()->endStashScope(m_workingDirectory);
+            if (client->checkCommandInProgress(m_workingDirectory) != GitClient::NoCommand)
+                client->endStashScope(m_workingDirectory);
         } else {
-            GitPlugin::instance()->gitClient()->handleMergeConflicts(
-                        m_workingDirectory, m_commit, m_command);
+            client->handleMergeConflicts(m_workingDirectory, m_commit, m_command);
         }
     }
 

commit 2ee83d4c9e1166a81d1d7025674f9e201e3c8460

Git: Fix stash pop on end of a continuous command
Broken in 92392454b74b5f79af5095e4f5725f0d4f57e6f1

Change-Id: I4e3d3b1c2fbec66c5471baedf1996529b852b868
Reviewed-by: Petar Perisin <petar.perisin@gmail.com>
Reviewed-by: Tobias Hunger <tobias.hunger@digia.com>

src/plugins/git/gitclient.cpp
@@ -651,7 +651,7 @@ public:
         GitClient *client = GitPlugin::instance()->gitClient();
         if (m_commit.isEmpty()) {
             GitPlugin::instance()->gitVersionControl()->emitRepositoryChanged(m_workingDirectory);
-            if (client->checkCommandInProgress(m_workingDirectory) != GitClient::NoCommand)
+            if (client->checkCommandInProgress(m_workingDirectory) == GitClient::NoCommand)
                 client->endStashScope(m_workingDirectory);
         } else {
             client->handleMergeConflicts(m_workingDirectory, m_commit, m_command);