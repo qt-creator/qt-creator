24b8ef317db2e4116b8549a1047f7e07b2e0cc5d
Tobias Hunger <tobias.hunger@digia.com>
2013-06-12 14:53:10 +0200

    Disable publishing action if we can not publish the project
    Make sure the publishing action is disabled when we can not publish
    a project. Currently the action is enabled and gets the user to a
    selection dialog with misleading "I'm afraid I can not do that"
    information.
    
    Change-Id: Id3d749e4ada466a2af505586a7d248f2cdc814fe
    Reviewed-by: Christian Kandeler <christian.kandeler@digia.com>
    Reviewed-by: Eike Ziller <eike.ziller@digia.com>

--- src/plugins/projectexplorer/projectexplorer.cpp
+++ src/plugins/projectexplorer/projectexplorer.cpp
@@ -1929,7 +1929,18 @@ void ProjectExplorerPlugin::updateActions()
 
     d->m_cancelBuildAction->setEnabled(d->m_buildManager->isBuilding());
 
-    d->m_publishAction->setEnabled(!d->m_session->projects().isEmpty());
+    bool canPublish = false;
+    if (project) {
+        const QList<IPublishingWizardFactory *> &factories
+                = ExtensionSystem::PluginManager::getObjects<IPublishingWizardFactory>();
+        foreach (const IPublishingWizardFactory *const factory, factories) {
+            if (factory->canCreateWizard(project)) {
+                canPublish = true;
+                break;
+            }
+        }
+    }
+    d->m_publishAction->setEnabled(canPublish);
 
     d->m_projectSelectorAction->setEnabled(!session()->projects().isEmpty());
     d->m_projectSelectorActionMenu->setEnabled(!session()->projects().isEmpty());
