76bf4a8a633063cb125e643960f6456bd87e2d82
Kai Koehne <kai.koehne@digia.com>
2013-06-20 12:34:02 +0200

    Fix copyright headers for commercial plugin
    Change-Id: Ic3f7926e1e13144961e5fb068c618c8e14c0f926
    Reviewed-by: Christiaan Janssen <christiaan.janssen@digia.com>

--- plugins/qmlprofilerextended/qmlprofilerextendedconstants.h
+++ plugins/qmlprofilerextended/qmlprofilerextendedconstants.h
@@ -1,17 +1,18 @@
 /****************************************************************************
 **
-** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
-** Contact: http://www.qt-project.org/legal
+** Copyright (C) 2013 Digia Plc
+** All rights reserved.
+** For any questions to Digia, please use contact form at http://qt.digia.com <http://qt.digia.com/>
 **
-** This file is part of Qt Creator.
+** This file is part of the Qt Enterprise Qt Quick Profiler Add-on.
 **
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
+** Licensees holding valid Qt Enterprise licenses may use this file in
+** accordance with the Qt Enterprise License Agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and Digia.  For licensing terms and
-** conditions see http://qt.digia.com/licensing.  For further information
-** use the contact form at http://qt.digia.com/contact-us.
+** a written agreement between you and Digia.
+**
+** If you have questions regarding the use of this file, please use
+** contact form at http://qt.digia.com <http://qt.digia.com/>
 **
 ****************************************************************************/
 
