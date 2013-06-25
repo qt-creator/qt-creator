a88599aee24e25316ce8d6cbd5724c628948daee
Eike Ziller <eike.ziller@digia.com>
2013-06-25 12:10:04 +0200

    Fix escape key for "About Qt Creator"
    Change-Id: I15c49541a6914c8db58784935de43f0b046895b0
    Reviewed-by: Robert Loehning <robert.loehning@digia.com>

--- src/plugins/coreplugin/versiondialog.h
+++ src/plugins/coreplugin/versiondialog.h
@@ -32,6 +32,10 @@
 
 #include <QDialog>
 
+QT_BEGIN_NAMESPACE
+class QEvent;
+QT_END_NAMESPACE
+
 namespace Core {
 namespace Internal {
 
@@ -40,6 +44,9 @@ class VersionDialog : public QDialog
     Q_OBJECT
 public:
     explicit VersionDialog(QWidget *parent);
+
+    bool event(QEvent *event);
+
 };
 
 } // namespace Internal
