qt-creator/qt-creator

commit a0d6df7bd85bf88e4ac3643d97d35e04389f6309

C++: also refresh files not in the project when project changes.
System headers and other file which are not explicitly mentioned in the
project must be reparsed when the project changes.

Task-number: QTCREATORBUG-9056
Change-Id: I32f1206d241a078a4d9b15fac5813f365a1ba303
Reviewed-by: Nikolai Kosjar <nikolai.kosjar@digia.com>

src/plugins/cpptools/cppmodelmanager.cpp
@@ -970,10 +970,16 @@ void CppModelManager::updateProjectInfo(const ProjectInfo &pinfo)
 
         foreach (const ProjectInfo &projectInfo, m_projects) {
             foreach (const ProjectPart::Ptr &projectPart, projectInfo.projectParts()) {
-                foreach (const ProjectFile &cxxFile, projectPart->files)
+                foreach (const ProjectFile &cxxFile, projectPart->files) {
                     m_srcToProjectPart[cxxFile.path].append(projectPart);
+                    foreach (const QString &fileName, m_snapshot.allIncludesForDocument(cxxFile.path))
+                        m_snapshot.remove(fileName);
+                    m_snapshot.remove(cxxFile.path);
+                }
             }
         }
+
+        m_snapshot.remove(configurationFileName());
     }
 
     if (!qgetenv("QTCREATOR_DUMP_PROJECT_INFO").isEmpty())

src/plugins/cpptools/cpptoolsplugin.h
@@ -137,6 +137,7 @@ private slots:
 
     void test_modelmanager_paths();
     void test_modelmanager_framework_headers();
+    void test_modelmanager_refresh();
 
 private:
     void test_completion();

src/plugins/cpptools/cppmodelmanager_test.cpp
@@ -138,3 +138,51 @@ void CppToolsPlugin::test_modelmanager_framework_headers()
         QVERIFY(chars.startsWith("success"));
     }
 }
+
+/// QTCREATORBUG-9056
+void CppToolsPlugin::test_modelmanager_refresh()
+{
+    ModelManagerTestHelper helper;
+    CppModelManager *mm = CppModelManager::instance();
+
+    const QString testCpp(testSource(QLatin1String("test_modelmanager_refresh.cpp")));
+    const QString testHeader(testSource(QLatin1String("test_modelmanager_refresh.h")));
+
+    Project *project = helper.createProject(QLatin1String("test_modelmanager_refresh"));
+    ProjectInfo pi = mm->projectInfo(project);
+    QCOMPARE(pi.project().data(), project);
+
+    ProjectPart::Ptr part(new ProjectPart);
+    pi.appendProjectPart(part);
+    part->cxxVersion = ProjectPart::CXX98;
+    part->qtVersion = ProjectPart::Qt5;
+    part->defines = QByteArray("#define OH_BEHAVE -1\n");
+    part->includePaths = QStringList() << testIncludeDir(false);
+    part->files.append(ProjectFile(testCpp, ProjectFile::CXXSource));
+
+    mm->updateProjectInfo(pi);
+    mm->updateSourceFiles(QStringList() << testCpp);
+
+    QStringList refreshedFiles = helper.waitForRefreshedSourceFiles();
+
+    QCOMPARE(refreshedFiles.size(), 1);
+    QVERIFY(refreshedFiles.contains(testCpp));
+    CPlusPlus::Snapshot snapshot = mm->snapshot();
+    QVERIFY(snapshot.contains(testHeader));
+    QVERIFY(snapshot.contains(testCpp));
+
+    part->defines = QByteArray();
+    mm->updateProjectInfo(pi);
+    snapshot = mm->snapshot();
+    QVERIFY(!snapshot.contains(testHeader));
+    QVERIFY(!snapshot.contains(testCpp));
+
+    mm->updateSourceFiles(QStringList() << testCpp);
+    refreshedFiles = helper.waitForRefreshedSourceFiles();
+
+    QCOMPARE(refreshedFiles.size(), 1);
+    QVERIFY(refreshedFiles.contains(testCpp));
+    snapshot = mm->snapshot();
+    QVERIFY(snapshot.contains(testHeader));
+    QVERIFY(snapshot.contains(testCpp));
+}

src/libs/cplusplus/CppDocument.cpp
@@ -705,6 +705,25 @@ Document::Ptr Snapshot::documentFromSource(const QByteArray &preprocessedCode,
     return newDoc;
 }
 
+QSet<QString> Snapshot::allIncludesForDocument(const QString &fileName) const
+{
+    QSet<QString> result;
+    allIncludesForDocument_helper(fileName, result);
+    return result;
+}
+
+void Snapshot::allIncludesForDocument_helper(const QString &fileName, QSet<QString> &result) const
+{
+    if (Document::Ptr doc = document(fileName)) {
+        foreach (const QString &inc, doc->includedFiles()) {
+            if (!result.contains(inc)) {
+                result.insert(inc);
+                allIncludesForDocument_helper(inc, result);
+            }
+        }
+    }
+}
+
 Document::Ptr Snapshot::document(const QString &fileName) const
 {
     return _documents.value(fileName);
@@ -713,22 +732,13 @@ Document::Ptr Snapshot::document(const QString &fileName) const
 Snapshot Snapshot::simplified(Document::Ptr doc) const
 {
     Snapshot snapshot;
-    simplified_helper(doc, &snapshot);
+
+    if (doc) {
+        snapshot.insert(doc);
+        foreach (const QString &fileName, allIncludesForDocument(doc->fileName()))
+            if (Document::Ptr inc = document(fileName))
+                snapshot.insert(inc);
+    }
+
     return snapshot;
 }
-
-void Snapshot::simplified_helper(Document::Ptr doc, Snapshot *snapshot) const
-{
-    if (! doc)
-        return;
-
-    if (! snapshot->contains(doc->fileName())) {
-        snapshot->insert(doc);
-
-        foreach (const Document::Include &incl, doc->includes()) {
-            Document::Ptr includedDoc = document(incl.fileName());
-            simplified_helper(includedDoc, snapshot);
-        }
-    }
-}
-

src/libs/cplusplus/CppDocument.h
@@ -389,8 +389,10 @@ public:
     Document::Ptr documentFromSource(const QByteArray &preprocessedDocument,
                                      const QString &fileName) const;
 
+    QSet<QString> allIncludesForDocument(const QString &fileName) const;
+
 private:
-    void simplified_helper(Document::Ptr doc, Snapshot *snapshot) const;
+    void allIncludesForDocument_helper(const QString &fileName, QSet<QString> &result) const;
 
 private:
     _Base _documents;

src/plugins/cpptools/modelmanagertesthelper.cpp
@@ -54,6 +54,7 @@ ModelManagerTestHelper::ModelManagerTestHelper(QObject *parent) :
 
     connect(this, SIGNAL(aboutToRemoveProject(ProjectExplorer::Project*)), mm, SLOT(onAboutToRemoveProject(ProjectExplorer::Project*)));
     connect(this, SIGNAL(projectAdded(ProjectExplorer::Project*)), mm, SLOT(onProjectAdded(ProjectExplorer::Project*)));
+    connect(mm, SIGNAL(sourceFilesRefreshed(QStringList)), this, SLOT(sourceFilesRefreshed(QStringList)));
 
     cleanup();
     verifyClean();
@@ -97,3 +98,19 @@ ModelManagerTestHelper::Project *ModelManagerTestHelper::createProject(const QSt
     return tp;
 }
 
+QStringList ModelManagerTestHelper::waitForRefreshedSourceFiles()
+{
+    m_refreshHappened = false;
+
+    while (!m_refreshHappened)
+        QCoreApplication::processEvents();
+
+    return m_lastRefreshedSourceFiles;
+}
+
+
+void ModelManagerTestHelper::sourceFilesRefreshed(const QStringList &files)
+{
+    m_lastRefreshedSourceFiles = files;
+    m_refreshHappened = true;
+}

src/plugins/cpptools/modelmanagertesthelper.h
@@ -83,9 +83,18 @@ public:
 
     Project *createProject(const QString &name);
 
+    QStringList waitForRefreshedSourceFiles();
+
 signals:
     void aboutToRemoveProject(ProjectExplorer::Project *project);
     void projectAdded(ProjectExplorer::Project*);
+
+public slots:
+    void sourceFilesRefreshed(const QStringList &files);
+
+private:
+    bool m_refreshHappened;
+    QStringList m_lastRefreshedSourceFiles;
 };
 
 } // namespace Internal

tests/cppmodelmanager/testdata/sources/test_modelmanager_refresh.cpp
@@ -1,0 +1,1 @@
+#include "test_modelmanager_refresh.h"

commit 1fad00f9991b86c3a329410c95f148b6ce3ef0d3

CppTools: Fix snapshot in case another project is opened
When a second project was opened the snapshot was reset. That resulted
in all kinds of problems since from then on the code model effectively
was not aware of the files of the first project.

The regression was introduced by commit a0d6df7b.

Change-Id: I1ccc9de68177205b49a4ba8ead2bc8abe4592b32
Reviewed-by: Fawzi Mohamed <fawzi.mohamed@digia.com>

src/plugins/cpptools/cppmodelmanager.cpp
@@ -556,23 +556,31 @@ void CppModelManager::updateProjectInfo(const ProjectInfo &pinfo)
             return;
 
         ProjectExplorer::Project *project = pinfo.project().data();
-        m_projects.insert(project, pinfo);
-        m_dirty = true;
-
-        m_srcToProjectPart.clear();
-
-        foreach (const ProjectInfo &projectInfo, m_projects) {
-            foreach (const ProjectPart::Ptr &projectPart, projectInfo.projectParts()) {
+        ProjectInfo oldProjectInfo = m_projects.value(project);
+        if (oldProjectInfo.isValid()) {
+            foreach (const ProjectPart::Ptr &projectPart, oldProjectInfo.projectParts()) {
                 foreach (const ProjectFile &cxxFile, projectPart->files) {
-                    m_srcToProjectPart[cxxFile.path].append(projectPart);
-                    foreach (const QString &fileName, m_snapshot.allIncludesForDocument(cxxFile.path))
+                    foreach (const QString &fileName,
+                             m_snapshot.allIncludesForDocument(cxxFile.path)) {
                         m_snapshot.remove(fileName);
+                    }
                     m_snapshot.remove(cxxFile.path);
                 }
             }
         }
-
         m_snapshot.remove(configurationFileName());
+
+        m_projects.insert(project, pinfo);
+        m_dirty = true;
+
+        m_srcToProjectPart.clear();
+        foreach (const ProjectInfo &projectInfo, m_projects) {
+            foreach (const ProjectPart::Ptr &projectPart, projectInfo.projectParts()) {
+                foreach (const ProjectFile &cxxFile, projectPart->files) {
+                    m_srcToProjectPart[cxxFile.path].append(projectPart);
+                }
+            }
+        }
     }
 
     if (!qgetenv("QTCREATOR_DUMP_PROJECT_INFO").isEmpty())

src/plugins/cpptools/cpptoolsplugin.h
@@ -163,6 +163,7 @@ private slots:
     void test_modelmanager_framework_headers();
     void test_modelmanager_refresh_1();
     void test_modelmanager_refresh_2();
+    void test_modelmanager_snapshot_after_two_projects();
 
 private:
     void test_completion();

src/plugins/cpptools/cppmodelmanager_test.cpp
@@ -96,6 +96,42 @@ private:
     const QString m_testDataDirectory;
 };
 
+
+// TODO: When possible, use this helper class in all tests
+class ProjectCreator
+{
+public:
+    ProjectCreator(ModelManagerTestHelper *modelManagerTestHelper)
+        : modelManagerTestHelper(modelManagerTestHelper)
+    {}
+
+    /// 'files' is expected to be a list of file names that reside in 'dir'.
+    void create(const QString &name, const QString &dir, const QStringList files)
+    {
+        const TestDataDirectory projectDir(dir);
+        foreach (const QString &file, files)
+            projectFiles << projectDir.file(file);
+
+        Project *project = modelManagerTestHelper->createProject(name);
+        projectInfo = CppModelManager::instance()->projectInfo(project);
+        QCOMPARE(projectInfo.project().data(), project);
+
+        ProjectPart::Ptr part(new ProjectPart);
+        projectInfo.appendProjectPart(part);
+        part->cxxVersion = ProjectPart::CXX98;
+        part->qtVersion = ProjectPart::Qt5;
+        foreach (const QString &file, projectFiles) {
+            ProjectFile projectFile(file, ProjectFile::classify(file));
+            part->files.append(projectFile);
+        }
+    }
+
+    ModelManagerTestHelper *modelManagerTestHelper;
+    ProjectInfo projectInfo;
+    QStringList projectFiles;
+};
+
+
 } // anonymous namespace
 
 void CppToolsPlugin::test_modelmanager_paths()
@@ -277,4 +313,50 @@ void CppToolsPlugin::test_modelmanager_refresh_2()
         document = snapshot.document(testCpp);
         QVERIFY(document->diagnosticMessages().isEmpty());
     }
+}
+
+void CppToolsPlugin::test_modelmanager_snapshot_after_two_projects()
+{
+    QStringList refreshedFiles;
+    ModelManagerTestHelper helper;
+    ProjectCreator project1(&helper);
+    ProjectCreator project2(&helper);
+    CppModelManager *mm = CppModelManager::instance();
+
+    // Project 1
+    project1.create(QLatin1String("snapshot_after_two_projects.1"),
+                    QLatin1String("testdata_project1"),
+                    QStringList() << QLatin1String("foo.h")
+                                  << QLatin1String("foo.cpp")
+                                  << QLatin1String("main.cpp"));
+
+    mm->updateProjectInfo(project1.projectInfo);
+    mm->updateSourceFiles(project1.projectFiles);
+    refreshedFiles = helper.waitForRefreshedSourceFiles();
+    QCOMPARE(refreshedFiles.toSet(), project1.projectFiles.toSet());
+    const int snapshotSizeAfterProject1 = mm->snapshot().size();
+
+    foreach (const QString &file, project1.projectFiles)
+        QVERIFY(mm->snapshot().contains(file));
+
+    // Project 2
+    project2.create(QLatin1String("snapshot_after_two_projects.2"),
+                    QLatin1String("testdata_project2"),
+                    QStringList() << QLatin1String("bar.h")
+                                  << QLatin1String("bar.cpp")
+                                  << QLatin1String("main.cpp"));
+
+    mm->updateProjectInfo(project2.projectInfo);
+    mm->updateSourceFiles(project2.projectFiles);
+    refreshedFiles = helper.waitForRefreshedSourceFiles();
+    QCOMPARE(refreshedFiles.toSet(), project2.projectFiles.toSet());
+
+    const int snapshotSizeAfterProject2 = mm->snapshot().size();
+    QVERIFY(snapshotSizeAfterProject2 > snapshotSizeAfterProject1);
+    QVERIFY(snapshotSizeAfterProject2 >= snapshotSizeAfterProject1 + project2.projectFiles.size());
+
+    foreach (const QString &file, project1.projectFiles)
+        QVERIFY(mm->snapshot().contains(file));
+    foreach (const QString &file, project2.projectFiles)
+        QVERIFY(mm->snapshot().contains(file));
 }

tests/cppmodelmanager/testdata_project1/foo.cpp
@@ -1,0 +1,7 @@
+// Copyright header
+
+#include "foo.h"
+
+Foo::Foo()
+{
+}

tests/cppmodelmanager/testdata_project1/foo.h
@@ -1,0 +1,12 @@
+// Copyright header
+
+#ifndef FOO_H
+#define FOO_H
+
+class Foo
+{
+public:
+    Foo();
+};
+
+#endif // FOO_H

tests/cppmodelmanager/testdata_project1/main.cpp
@@ -1,0 +1,9 @@
+// Copyright header
+
+#include "foo.h"
+
+int main()
+{
+    Foo foo;
+    return 1;
+}

tests/cppmodelmanager/testdata_project2/bar.cpp
@@ -1,0 +1,7 @@
+// Copyright header
+
+#include "bar.h"
+
+Bar::Bar()
+{
+}

tests/cppmodelmanager/testdata_project2/bar.h
@@ -1,0 +1,12 @@
+// Copyright header
+
+#ifndef BAR_H
+#define BAR_H
+
+class Bar
+{
+public:
+    Bar();
+};
+
+#endif // BAR_H

tests/cppmodelmanager/testdata_project2/main.cpp
@@ -1,0 +1,9 @@
+// Copyright header
+
+#include "bar.h"
+
+int main()
+{
+    Bar bar;
+    return 1;
+}