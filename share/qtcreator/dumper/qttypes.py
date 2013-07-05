26e97398ab643f69482286af15f8a7d81a30ffa8
hjk <hjk121@nokiamail.com>
2013-07-05 18:44:53 +0200

    Debugger: Fix LLDB QMap dumper
    It needs spaces _exactly_ as found in the debug info.
    
    Change-Id: I3ad0052b364c5c3cfea17d8169de8a235d37b891
    Reviewed-by: hjk <hjk121@nokiamail.com>

--- share/qtcreator/dumper/qttypes.py
+++ share/qtcreator/dumper/qttypes.py
@@ -976,7 +976,9 @@ def qdumpHelper__Qt5_QMap(d, value, forceLong):
         keyType = d.templateArgument(value.type, 0)
         valueType = d.templateArgument(value.type, 1)
         isCompact = d.isMapCompact(keyType, valueType)
-        nodeType = d.lookupType(d.ns + "QMapNode<%s,%s>" % (keyType, valueType))
+        # Note: The space in the QMapNode lookup below is
+        # important for LLDB.
+        nodeType = d.lookupType(d.ns + "QMapNode<%s, %s>" % (keyType, valueType))
         if isCompact:
             innerType = valueType
         else:
