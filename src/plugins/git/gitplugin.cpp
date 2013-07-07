45ccd7d919c23ce816ed180f5bd4ed2f2a186d06
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2013-07-07 12:57:56 +0300

    Git: Fix crash on checkout branch
    Task-number: QTCREATORBUG-9756
    Change-Id: I3bdba870737bbffde6c0e6a9046a58ba78f097be
    Reviewed-by: Eike Ziller <eike.ziller@digia.com>

--- src/plugins/git/gitplugin.cpp
+++ src/plugins/git/gitplugin.cpp
@@ -690,7 +690,7 @@ bool GitPlugin::initialize(const QStringList &arguments, QString *errorMessage)
     connect(Core::ICore::vcsManager(), SIGNAL(repositoryChanged(QString)),
             this, SLOT(updateContinueAndAbortCommands()));
     connect(Core::ICore::vcsManager(), SIGNAL(repositoryChanged(QString)),
-            this, SLOT(updateBranches(QString)));
+            this, SLOT(updateBranches(QString)), Qt::QueuedConnection);
 
     if (!Core::ICore::mimeDatabase()->addMimeTypes(QLatin1String(RC_GIT_MIME_XML), errorMessage))
         return false;
