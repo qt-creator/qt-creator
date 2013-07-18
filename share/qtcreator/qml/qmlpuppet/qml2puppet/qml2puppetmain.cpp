44bbfa3bc420140c395ee5240154a8200f3472ec
Marco Bubke <marco.bubke@digia.com>
2013-07-18 16:08:37 +0200

    QmlDesigner: Better argument checking for the qml2puppet
    Change-Id: Ib4c83d9bb30705c23b9966696ebc7015501da154
    Reviewed-by: Tim Jenssen <tim.jenssen@digia.com>

--- share/qtcreator/qml/qmlpuppet/qml2puppet/qml2puppetmain.cpp
+++ share/qtcreator/qml/qmlpuppet/qml2puppet/qml2puppetmain.cpp
@@ -31,6 +31,7 @@
 
 #include <QApplication>
 #include <QStringList>
+#include <QFileInfo>
 
 #include <qt5nodeinstanceclientproxy.h>
 
@@ -61,11 +62,32 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationName("Qml2Puppet");
     QCoreApplication::setApplicationVersion("1.0.0");
 
-    if (application.arguments().count() < 2) {
+    if (application.arguments().count() < 2
+            || (application.arguments().at(1) == "--readcapturedstream" && application.arguments().count() < 3)) {
         qDebug() << "Usage:\n";
         qDebug() << "--test";
         qDebug() << "--version";
+        qDebug() << "--readcapturedstream <stream file> [control stream file]";
+
         return -1;
+    }
+
+    if (application.arguments().at(1) == "--readcapturedstream" && application.arguments().count() > 2) {
+        QFileInfo inputStreamFileInfo(application.arguments().at(2));
+        if (!inputStreamFileInfo.exists()) {
+            qDebug() << "Input stream does not exist:" << inputStreamFileInfo.absoluteFilePath();
+
+            return -1;
+        }
+
+        if (application.arguments().count() > 3) {
+            QFileInfo controlStreamFileInfo(application.arguments().at(3));
+            if (!controlStreamFileInfo.exists()) {
+                qDebug() << "Output stream does not exist:" << controlStreamFileInfo.absoluteFilePath();
+
+                return -1;
+            }
+        }
     }
 
     if (application.arguments().count() == 2 && application.arguments().at(1) == "--test") {
