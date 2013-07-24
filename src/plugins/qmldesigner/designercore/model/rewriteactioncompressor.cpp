9a8f5383d5fe5570331711966b8a7c5f2b83fa65
Marco Bubke <marco.bubke@digia.com>
2013-07-24 14:47:38 +0200

    QmlDesigner: Improve short name
    Change-Id: I773964fac0b4981a9d33bc047b8e5e9599c2a3b0
    Reviewed-by: Tim Jenssen <tim.jenssen@digia.com>

--- src/plugins/qmldesigner/designercore/model/rewriteactioncompressor.cpp
+++ src/plugins/qmldesigner/designercore/model/rewriteactioncompressor.cpp
@@ -38,17 +38,17 @@
 using namespace QmlDesigner;
 using namespace QmlDesigner::Internal;
 
-static bool nodeOrParentInSet(const ModelNode &node, const QSet<ModelNode> &nodeSet)
+static bool nodeOrParentInSet(const ModelNode &modelNode, const QSet<ModelNode> &nodeSet)
 {
-    ModelNode n = node;
-    while (n.isValid()) {
-        if (nodeSet.contains(n))
+    ModelNode currentModelnode = modelNode;
+    while (currentModelnode.isValid()) {
+        if (nodeSet.contains(currentModelnode))
             return true;
 
-        if (!n.hasParentProperty())
+        if (!currentModelnode.hasParentProperty())
             return false;
 
-        n = n.parentProperty().parentModelNode();
+        currentModelnode = currentModelnode.parentProperty().parentModelNode();
     }
 
     return false;
