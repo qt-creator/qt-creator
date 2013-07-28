qt-creator/qt-creator

commit 771fe03cfeda0b8514556ca29889fb35116a41c5

Git - couple fixes in branch based auto stash
- do not start stash scope when creating auto stash
- return if reset fails

Change-Id: I471abc1d430351192c46ac78041949ba81f11b09
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/git/branchdialog.cpp
@@ -192,15 +192,17 @@ void BranchDialog::checkout()
         m_model->checkoutBranch(idx);
     } else if (branchCheckoutDialog.exec() == QDialog::Accepted && m_model) {
 
-        QString stashMessage;
-        if (branchCheckoutDialog.makeStashOfCurrentBranch()
-                || branchCheckoutDialog.moveLocalChangesToNextBranch()) {
-
-            if (!gitClient->beginStashScope(m_repository, currentBranch + QLatin1String("-AutoStash"), NoPrompt))
+        if (branchCheckoutDialog.makeStashOfCurrentBranch()) {
+            if (!gitClient->executeSynchronousStash(m_repository,
+                            currentBranch + QLatin1String("-AutoStash"))) {
                 return;
-            stashMessage = gitClient->stashInfo(m_repository).stashMessage();
+            }
+        } else if (branchCheckoutDialog.moveLocalChangesToNextBranch()) {
+            if (!gitClient->beginStashScope(m_repository, QLatin1String("Checkout"), NoPrompt))
+                return;
         } else if (branchCheckoutDialog.discardLocalChanges()) {
-            gitClient->synchronousReset(m_repository);
+            if (!gitClient->synchronousReset(m_repository))
+                return;
         }
 
         m_model->checkoutBranch(idx);
@@ -214,7 +216,7 @@ void BranchDialog::checkout()
             }
         }
 
-        if (!stashMessage.isEmpty() && branchCheckoutDialog.moveLocalChangesToNextBranch())
+        if (branchCheckoutDialog.moveLocalChangesToNextBranch())
             gitClient->endStashScope(m_repository);
         else if (branchCheckoutDialog.popStashOfNextBranch())
             gitClient->synchronousStashRestore(m_repository, stashName, true);

commit 5d1d2d2b626089bf8c2710352ebddb9f3b8d8c8e

Git: Fix autostash for branches
Broken by 771fe03cfeda0b8514556ca29889fb35116a41c5

Change-Id: Ifb629535d2197bbee771050c94952068834614f3
Reviewed-by: Petar Perisin <petar.perisin@gmail.com>

src/plugins/git/branchdialog.cpp
@@ -193,8 +193,8 @@ void BranchDialog::checkout()
     } else if (branchCheckoutDialog.exec() == QDialog::Accepted && m_model) {
 
         if (branchCheckoutDialog.makeStashOfCurrentBranch()) {
-            if (!gitClient->executeSynchronousStash(m_repository,
-                            currentBranch + QLatin1String("-AutoStash"))) {
+            if (gitClient->synchronousStash(m_repository,
+                           currentBranch + QLatin1String("-AutoStash")).isEmpty()) {
                 return;
             }
         } else if (branchCheckoutDialog.moveLocalChangesToNextBranch()) {