05c6a5724f9479341a0ffc0839ae9ab597ac39d3
Aurindam Jana <aurindam.jana@digia.com>
2013-07-29 17:03:32 +0200

    Revert back copyright headers
    The copyright was changed in
    Ic8279755f0188ab53253a62322fcccf1c17b6aaf
    
    Change-Id: If4251d13b6e653d49913d1c50044177491326edc
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@digia.com>
    Reviewed-by: hjk <hjk121@nokiamail.com>

--- src/plugins/qmlprofiler/qmlprofilerruncontrolfactory.h
+++ src/plugins/qmlprofiler/qmlprofilerruncontrolfactory.h
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
+** Copyright (C) 2013 Kläralvdalens Datakonsult AB, a KDAB Group company, info@kdab.com
 ** Contact: http://www.qt-project.org/legal
 **
 ** This file is part of Qt Creator.
