15a9019191f8c84eb07edadd6cfb62eb5181cec1
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2013-08-04 20:57:50 +0300

    CustomToolChain: emit dirty() on changes
    Change-Id: I0c9a199562ff6b5953d22d42b28f814e53ffb3d9
    Reviewed-by: André Hartmann <aha_1980@gmx.de>
    Reviewed-by: Tobias Hunger <tobias.hunger@digia.com>

--- src/plugins/projectexplorer/customtoolchain.cpp
+++ src/plugins/projectexplorer/customtoolchain.cpp
@@ -444,9 +444,13 @@ CustomToolChainConfigWidget::CustomToolChainConfigWidget(CustomToolChain *tc) :
     m_predefinedDetails->updateSummaryText();
     m_headerDetails->updateSummaryText();
 
+    connect(m_compilerCommand, SIGNAL(changed(QString)), this, SIGNAL(dirty()));
+    connect(m_makeCommand, SIGNAL(changed(QString)), this, SIGNAL(dirty()));
     connect(m_abiWidget, SIGNAL(abiChanged()), this, SIGNAL(dirty()));
     connect(m_predefinedMacros, SIGNAL(textChanged()), this, SLOT(updateSummaries()));
     connect(m_headerPaths, SIGNAL(textChanged()), this, SLOT(updateSummaries()));
+    connect(m_cxx11Flags, SIGNAL(textChanged(QString)), this, SIGNAL(dirty()));
+    connect(m_mkspecs, SIGNAL(textChanged(QString)), this, SIGNAL(dirty()));
 }
 
 void CustomToolChainConfigWidget::updateSummaries()
@@ -455,6 +459,7 @@ void CustomToolChainConfigWidget::updateSummaries()
         m_predefinedDetails->updateSummaryText();
     else
         m_headerDetails->updateSummaryText();
+    emit dirty();
 }
 
 void CustomToolChainConfigWidget::applyImpl()
