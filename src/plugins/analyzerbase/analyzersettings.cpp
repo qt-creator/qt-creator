qt-creator/qt-creator

commit 15caea1d260273070aaa399c4643da5e68be886f

Analyzer: Handle createProjectSettings outside IAnalyzerTool
Less indirection.

Change-Id: If702a0a44c1fc96510fd1c5411fda51660dde10b
Reviewed-by: Aurindam Jana <aurindam.jana@digia.com>

src/plugins/analyzerbase/analyzersettings.cpp
@@ -151,16 +151,18 @@ void AnalyzerGlobalSettings::registerConfig(AbstractAnalyzerSubConfig *config)
 }
 
 
+static QList<AnalyzerSubConfigFactory *> theAnalyzerSubConfigFactories()
+{
+    static QList<AnalyzerSubConfigFactory *> theFactories;
+    return theFactories;
+}
+
 AnalyzerRunConfigurationAspect::AnalyzerRunConfigurationAspect()
     : AnalyzerSettings((QObject *)0), m_useGlobalSettings(true)
 {
-    QList<IAnalyzerTool*> tools = AnalyzerManager::tools();
     // add sub configs
-    foreach (IAnalyzerTool *tool, tools) {
-        AbstractAnalyzerSubConfig *config = tool->createProjectSettings();
-        if (config)
-            m_customConfigurations.append(config);
-    }
+    foreach (AnalyzerSubConfigFactory *factory, theAnalyzerSubConfigFactories())
+        m_customConfigurations.append(factory->createProjectSettings());
 
     m_subConfigs = AnalyzerGlobalSettings::instance()->subConfigs();
     resetCustomToGlobalSettings();
@@ -182,6 +184,11 @@ AnalyzerRunConfigurationAspect::AnalyzerRunConfigurationAspect(const AnalyzerRun
 AnalyzerRunConfigurationAspect::~AnalyzerRunConfigurationAspect()
 {
     qDeleteAll(m_customConfigurations);
+}
+
+void AnalyzerRunConfigurationAspect::registerConfigFactory(AnalyzerSubConfigFactory *factory)
+{
+    theAnalyzerSubConfigFactories().append(factory);
 }
 
 QString AnalyzerRunConfigurationAspect::displayName() const

src/plugins/analyzerbase/analyzermanager.cpp
@@ -684,11 +684,6 @@ QDockWidget *AnalyzerManager::createDockWidget(IAnalyzerTool *tool, const QStrin
     return dockWidget;
 }
 
-QList<IAnalyzerTool *> AnalyzerManager::tools()
-{
-    return d->m_tools;
-}
-
 void AnalyzerManager::selectTool(IAnalyzerTool *tool, StartMode mode)
 {
     d->selectTool(d->actionFromToolAndMode(tool, mode));

src/plugins/analyzerbase/analyzermanager.h
@@ -75,7 +75,6 @@ public:
     static Utils::FancyMainWindow *mainWindow();
 
     static void showMode();
-    static QList<IAnalyzerTool *> tools();
     static void selectTool(IAnalyzerTool *tool, StartMode mode);
     static void startTool();
     static void stopTool();

src/plugins/analyzerbase/analyzerrunconfigwidget.h
@@ -46,7 +46,6 @@ class DetailsWidget;
 
 namespace Analyzer {
 
-class AnalyzerSettings;
 class AbstractAnalyzerSubConfig;
 
 namespace Internal {

src/plugins/analyzerbase/analyzersettings.h
@@ -81,6 +81,17 @@ public:
     virtual AbstractAnalyzerSubConfig *clone() = 0;
 };
 
+class ANALYZER_EXPORT AnalyzerSubConfigFactory : public QObject
+{
+    Q_OBJECT
+
+public:
+    AnalyzerSubConfigFactory() {}
+
+    /// Factory method to create the project tool setting
+    virtual AbstractAnalyzerSubConfig *createProjectSettings() = 0;
+};
+
 /**
  * Shared interface for the global and per-project settings.
  *
@@ -167,6 +178,8 @@ public:
     AnalyzerRunConfigurationAspect();
     AnalyzerRunConfigurationAspect(const AnalyzerRunConfigurationAspect *other);
     ~AnalyzerRunConfigurationAspect();
+
+    static void registerConfigFactory(AnalyzerSubConfigFactory *factory);
 
     QString displayName() const;
     virtual QVariantMap toMap() const;

src/plugins/analyzerbase/ianalyzertool.cpp
@@ -60,11 +60,6 @@ IAnalyzerTool::IAnalyzerTool(QObject *parent)
     : QObject(parent)
 {}
 
-AbstractAnalyzerSubConfig *IAnalyzerTool::createProjectSettings()
-{
-    return 0;
-}
-
 static bool buildTypeAccepted(IAnalyzerTool::ToolMode toolMode,
                        BuildConfiguration::BuildType buildType)
 {

src/plugins/analyzerbase/ianalyzertool.h
@@ -113,9 +113,6 @@ public:
 
     /// Called when tools gets deselected.
     virtual void toolDeselected() const {}
-
-    /// Factory method to create the project tool setting
-    virtual AbstractAnalyzerSubConfig *createProjectSettings();
 };
 
 } // namespace Analyzer

src/plugins/valgrind/memchecktool.cpp
@@ -297,11 +297,6 @@ QString MemcheckTool::description() const
               "memory leaks");
 }
 
-AbstractAnalyzerSubConfig *MemcheckTool::createProjectSettings()
-{
-    return new ValgrindProjectSettings();
-}
-
 IAnalyzerTool::ToolMode MemcheckTool::toolMode() const
 {
     return DebugMode;

commit 84a28d1ec0b342b579abfd152afda7622c793fd9

Analyzer: Fix recent regression in aspect creation
Introduced in 15caea1d260273070aaa399c4643da5e68be886f.

Change-Id: I04da456928aa2bbf30467770d5a9da35c0e87d9f
Reviewed-by: hjk <hjk121@nokiamail.com>

src/plugins/analyzerbase/analyzersettings.cpp
@@ -151,7 +151,7 @@ void AnalyzerGlobalSettings::registerConfig(AbstractAnalyzerSubConfig *config)
 }
 
 
-static QList<AnalyzerSubConfigFactory *> theAnalyzerSubConfigFactories()
+static QList<AnalyzerSubConfigFactory *> &theAnalyzerSubConfigFactories()
 {
     static QList<AnalyzerSubConfigFactory *> theFactories;
     return theFactories;