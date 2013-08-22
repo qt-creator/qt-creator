54a12ebb797fa6c6805dc9d7a102b8f0339243a6
Nikolai Kosjar <nikolai.kosjar@digia.com>
2013-08-22 12:13:07 +0200

    C++: Fix crash in LookupContext::lookupType()
    Task-number: QTCREATORBUG-10019
    Change-Id: I7970c2f1ea3044e11250b8dbaf9b29ba3e7f34ef
    Reviewed-by: hjk <hjk121@nokiamail.com>
    Reviewed-by: Erik Verbruggen <erik.verbruggen@digia.com>

--- src/libs/cplusplus/LookupContext.cpp
+++ src/libs/cplusplus/LookupContext.cpp
@@ -308,7 +308,7 @@ ClassOrNamespace *LookupContext::lookupType(const Name *name, Scope *scope,
                                             ClassOrNamespace* enclosingTemplateInstantiation,
                                             QSet<const Declaration *> typedefsBeingResolved) const
 {
-    if (! scope) {
+    if (! scope || ! name) {
         return 0;
     } else if (Block *block = scope->asBlock()) {
         for (unsigned i = 0; i < block->memberCount(); ++i) {
