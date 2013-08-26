2665eced9b6ae9c6e8d8cc698c9a13017657945f
Christian Stenger <christian.stenger@digia.com>
2013-08-26 11:19:31 +0200

    Squish: Move common function to shared script
    Change-Id: I59d341f9f7a5c86bc3b723dbdc1e1596d464c92c
    Reviewed-by: Robert Loehning <robert.loehning@digia.com>

--- tests/system/suite_WELP/tst_WELP03/test.py
+++ tests/system/suite_WELP/tst_WELP03/test.py
@@ -59,16 +59,6 @@ def handlePackagingMessageBoxes():
         except:
             break
 
-def qt5SDKPath():
-    if platform.system() in ('Microsoft', 'Windows'):
-        return os.path.abspath("C:/Qt/Qt5.0.1/5.0.1/msvc2010")
-    elif platform.system() == 'Linux':
-        if __is64BitOS__():
-            return os.path.expanduser("~/Qt5.0.1/5.0.1/gcc_64")
-        return os.path.expanduser("~/Qt5.0.1/5.0.1/gcc")
-    else:
-        return os.path.expanduser("~/Qt5.0.1/5.0.1/clang_64")
-
 def main():
     global sdkPath, webPageContentLoadedValue
     # open Qt Creator
