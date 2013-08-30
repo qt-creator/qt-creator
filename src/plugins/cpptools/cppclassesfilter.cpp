qt-creator/qt-creator

commit 760aa0f8bce34e094abecdd99c77c359fb96bb67

CppTools: Get rid of SearchSymbols::setSeparateScope()
This will make it easier to use a single SearchSymbols instance and a
single run serving all locator filters.

Change-Id: Idb6a3693ad356227d46d0b28fb4c3a5db62b4ac4
Reviewed-by: Erik Verbruggen <erik.verbruggen@digia.com>

src/plugins/cpptools/cppclassesfilter.cpp
@@ -41,9 +41,24 @@ CppClassesFilter::CppClassesFilter(CppModelManager *manager)
     setDisplayName(tr("C++ Classes"));
 
     search.setSymbolsToSearchFor(SymbolSearcher::Classes);
-    search.setSeparateScope(true);
 }
 
 CppClassesFilter::~CppClassesFilter()
 {
+}
+
+QString CppClassesFilter::stringToMatchUserInputAgainst(const ModelItemInfo &info)
+{
+    return info.symbolName;
+}
+
+Locator::FilterEntry CppClassesFilter::filterEntryFromModelItemInfo(const ModelItemInfo &info)
+{
+    const QVariant id = qVariantFromValue(info);
+    Locator::FilterEntry filterEntry(this, info.symbolName, id, info.icon);
+    filterEntry.extraInfo = info.symbolScope.isEmpty()
+        ? info.shortNativeFilePath()
+        : info.symbolScope;
+
+    return filterEntry;
 }

src/plugins/cpptools/cppclassesfilter.h
@@ -42,6 +42,10 @@ class CPPTOOLS_EXPORT CppClassesFilter : public Internal::CppLocatorFilter
 public:
     CppClassesFilter(Internal::CppModelManager *manager);
     ~CppClassesFilter();
+
+private:
+    QString stringToMatchUserInputAgainst(const ModelItemInfo &info);
+    Locator::FilterEntry filterEntryFromModelItemInfo(const ModelItemInfo &info);
 };
 
 } // namespace CppTools

src/plugins/cpptools/cppfunctionsfilter.cpp
@@ -40,9 +40,25 @@ CppFunctionsFilter::CppFunctionsFilter(CppModelManager *manager)
     setIncludedByDefault(false);
 
     search.setSymbolsToSearchFor(SymbolSearcher::Functions);
-    search.setSeparateScope(true);
 }
 
 CppFunctionsFilter::~CppFunctionsFilter()
 {
 }
+
+QString CppFunctionsFilter::stringToMatchUserInputAgainst(const CppTools::ModelItemInfo &info)
+{
+    return info.symbolName;
+}
+
+Locator::FilterEntry CppFunctionsFilter::filterEntryFromModelItemInfo(const CppTools::ModelItemInfo &info)
+{
+    const QVariant id = qVariantFromValue(info);
+    Locator::FilterEntry filterEntry(this, info.symbolName + info.symbolType, id, info.icon);
+    filterEntry.extraInfo = info.symbolScope.isEmpty()
+        ? info.shortNativeFilePath()
+        : info.symbolScope;
+
+    return filterEntry;
+}
+

src/plugins/cpptools/cppfunctionsfilter.h
@@ -42,6 +42,10 @@ class CppFunctionsFilter : public CppLocatorFilter
 public:
     CppFunctionsFilter(CppModelManager *manager);
     ~CppFunctionsFilter();
+
+private:
+    QString stringToMatchUserInputAgainst(const ModelItemInfo &info);
+    Locator::FilterEntry filterEntryFromModelItemInfo(const ModelItemInfo &info);
 };
 
 } // namespace Internal

src/plugins/cpptools/cpplocatorfilter.cpp
@@ -30,12 +30,9 @@
 #include "cpplocatorfilter.h"
 #include "cppmodelmanager.h"
 
-#include <utils/fileutils.h>
-
 #include <QStringMatcher>
 
 using namespace CppTools::Internal;
-using namespace Utils;
 
 static const int MaxPendingDocuments = 10;
 
@@ -111,6 +108,23 @@ void CppLocatorFilter::onAboutToRemoveFiles(const QStringList &files)
         m_searchList.remove(file);
 }
 
+QString CppLocatorFilter::stringToMatchUserInputAgainst(const CppTools::ModelItemInfo &info)
+{
+    return info.scopedSymbolName();
+}
+
+Locator::FilterEntry CppLocatorFilter::filterEntryFromModelItemInfo(const CppTools::ModelItemInfo &info)
+{
+    const QVariant id = qVariantFromValue(info);
+    const QString name = info.symbolScope.isEmpty() ? info.symbolName : info.scopedSymbolName();
+    Locator::FilterEntry filterEntry(this, name, id, info.icon);
+    filterEntry.extraInfo = info.type == ModelItemInfo::Class || info.type == ModelItemInfo::Enum
+        ? info.shortNativeFilePath()
+        : info.symbolType;
+
+    return filterEntry;
+}
+
 void CppLocatorFilter::refresh(QFutureInterface<void> &future)
 {
     Q_UNUSED(future)
@@ -146,19 +160,11 @@ QList<Locator::FilterEntry> CppLocatorFilter::matchesFor(QFutureInterface<Locato
 
         const QList<ModelItemInfo> items = it.value();
         foreach (const ModelItemInfo &info, items) {
-            if ((hasWildcard && regexp.exactMatch(info.symbolName))
-                    || (!hasWildcard && matcher.indexIn(info.symbolName) != -1)) {
-
-                QVariant id = qVariantFromValue(info);
-                Locator::FilterEntry filterEntry(this, info.symbolName, id, info.icon);
-                if (!info.symbolType.isEmpty()) {
-                    filterEntry.extraInfo = info.symbolType;
-                } else {
-                    filterEntry.extraInfo = FileUtils::shortNativePath(
-                        FileName::fromString(info.fileName));
-                }
-
-                if (info.symbolName.startsWith(entry, caseSensitivityForPrefix))
+            const QString matchString = stringToMatchUserInputAgainst(info);
+            if ((hasWildcard && regexp.exactMatch(matchString))
+                    || (!hasWildcard && matcher.indexIn(matchString) != -1)) {
+                const Locator::FilterEntry filterEntry = filterEntryFromModelItemInfo(info);
+                if (matchString.startsWith(entry, caseSensitivityForPrefix))
                     betterEntries.append(filterEntry);
                 else
                     goodEntries.append(filterEntry);

src/plugins/cpptools/cpplocatorfilter.h
@@ -63,6 +63,10 @@ private slots:
     void onAboutToRemoveFiles(const QStringList &files);
 
 private:
+    virtual QString stringToMatchUserInputAgainst(const ModelItemInfo &info);
+    virtual Locator::FilterEntry filterEntryFromModelItemInfo(const ModelItemInfo &info);
+
+private:
     CppModelManager *m_manager;
 
     QHash<QString, QList<ModelItemInfo> > m_searchList;

src/plugins/cpptools/cpplocatorfilter_test.cpp
@@ -152,7 +152,7 @@ void CppToolsPlugin::test_cpplocatorfilters_CppLocatorFilter()
     ResultDataList results = ResultData::fromFilterEntryList(test.matchesFor(searchText));
 //    ResultData::printFilterEntries(results);
     QVERIFY(!results.isEmpty());
-    QCOMPARE(expectedResults, results);
+    QCOMPARE(results, expectedResults);
 }
 
 void CppToolsPlugin::test_cpplocatorfilters_CppLocatorFilter_data()

src/plugins/cpptools/builtinindexingsupport.cpp
@@ -100,7 +100,6 @@ public:
 
         SearchSymbols search;
         search.setSymbolsToSearchFor(m_parameters.types);
-        search.setSeparateScope(true);
         CPlusPlus::Snapshot::const_iterator it = m_snapshot.begin();
 
         QString findString = (m_parameters.flags & Find::FindRegularExpression
@@ -120,11 +119,14 @@ public:
                 foreach (const ModelItemInfo &info, modelInfos) {
                     int index = matcher.indexIn(info.symbolName);
                     if (index != -1) {
-                        QStringList path = info.fullyQualifiedName.mid(0,
-                            info.fullyQualifiedName.size() - 1);
+                        QString text = info.typeNameRepresentation();
+                        if (text.isEmpty())
+                            text = info.symbolName;
+
                         Find::SearchResultItem item;
-                        item.path = path;
-                        item.text = info.symbolName;
+                        item.path = info.symbolScope.split(QLatin1String("::"),
+                                                           QString::SkipEmptyParts);
+                        item.text = text;
                         item.textMarkPos = -1;
                         item.textMarkLength = 0;
                         item.icon = info.icon;

commit f0c9ef58ecae21d83ffbcd9ed24c1aaa2646d370

CppTools: Fix displaying symbols of CppLocatorFilter
The left column of the locator filter displayed the fully qualified
name, which is a bit too long...

Regression introduced by commit 760aa0f.

Change-Id: I57fb44cd205c6e54158de22b6638e4a28014749c
Reviewed-by: Erik Verbruggen <erik.verbruggen@digia.com>

src/plugins/cpptools/cppclassesfilter.cpp
@@ -47,14 +47,6 @@ CppClassesFilter::~CppClassesFilter()
 {
 }
 
-QString CppClassesFilter::stringToMatchUserInputAgainst(const ModelItemInfo &info,
-                                                        bool userInputContainsColonColon)
-{
-    if (userInputContainsColonColon)
-        return info.scopedSymbolName();
-    return info.symbolName;
-}
-
 Locator::FilterEntry CppClassesFilter::filterEntryFromModelItemInfo(const ModelItemInfo &info)
 {
     const QVariant id = qVariantFromValue(info);

src/plugins/cpptools/cppclassesfilter.h
@@ -44,8 +44,6 @@ public:
     ~CppClassesFilter();
 
 private:
-    QString stringToMatchUserInputAgainst(const ModelItemInfo &info,
-                                          bool userInputContainsColonColon);
     Locator::FilterEntry filterEntryFromModelItemInfo(const ModelItemInfo &info);
 };
 

src/plugins/cpptools/cppfunctionsfilter.cpp
@@ -46,14 +46,6 @@ CppFunctionsFilter::~CppFunctionsFilter()
 {
 }
 
-QString CppFunctionsFilter::stringToMatchUserInputAgainst(const CppTools::ModelItemInfo &info,
-                                                          bool userInputContainsColonColon)
-{
-    if (userInputContainsColonColon)
-        return info.scopedSymbolName();
-    return info.symbolName;
-}
-
 Locator::FilterEntry CppFunctionsFilter::filterEntryFromModelItemInfo(const CppTools::ModelItemInfo &info)
 {
     const QVariant id = qVariantFromValue(info);

src/plugins/cpptools/cppfunctionsfilter.h
@@ -44,8 +44,6 @@ public:
     ~CppFunctionsFilter();
 
 private:
-    QString stringToMatchUserInputAgainst(const ModelItemInfo &info,
-                                          bool userInputContainsColonColon);
     Locator::FilterEntry filterEntryFromModelItemInfo(const ModelItemInfo &info);
 };
 

src/plugins/cpptools/cpplocatorfilter.cpp
@@ -108,18 +108,10 @@ void CppLocatorFilter::onAboutToRemoveFiles(const QStringList &files)
         m_searchList.remove(file);
 }
 
-QString CppLocatorFilter::stringToMatchUserInputAgainst(const CppTools::ModelItemInfo &info,
-                                                        bool userInputContainsColonColon)
-{
-    Q_UNUSED(userInputContainsColonColon)
-    return info.scopedSymbolName();
-}
-
 Locator::FilterEntry CppLocatorFilter::filterEntryFromModelItemInfo(const CppTools::ModelItemInfo &info)
 {
     const QVariant id = qVariantFromValue(info);
-    const QString name = info.symbolScope.isEmpty() ? info.symbolName : info.scopedSymbolName();
-    Locator::FilterEntry filterEntry(this, name, id, info.icon);
+    Locator::FilterEntry filterEntry(this, info.symbolName, id, info.icon);
     filterEntry.extraInfo = info.type == ModelItemInfo::Class || info.type == ModelItemInfo::Enum
         ? info.shortNativeFilePath()
         : info.symbolType;
@@ -163,7 +155,7 @@ QList<Locator::FilterEntry> CppLocatorFilter::matchesFor(QFutureInterface<Locato
 
         const QList<ModelItemInfo> items = it.value();
         foreach (const ModelItemInfo &info, items) {
-            const QString matchString = stringToMatchUserInputAgainst(info, hasColonColon);
+            const QString matchString = hasColonColon ? info.scopedSymbolName() : info.symbolName;
             if ((hasWildcard && regexp.exactMatch(matchString))
                     || (!hasWildcard && matcher.indexIn(matchString) != -1)) {
                 const Locator::FilterEntry filterEntry = filterEntryFromModelItemInfo(info);

src/plugins/cpptools/cpplocatorfilter.h
@@ -63,8 +63,6 @@ private slots:
     void onAboutToRemoveFiles(const QStringList &files);
 
 private:
-    virtual QString stringToMatchUserInputAgainst(const ModelItemInfo &info,
-                                                  bool userInputContainsColonColon);
     virtual Locator::FilterEntry filterEntryFromModelItemInfo(const ModelItemInfo &info);
 
 private:

src/plugins/cpptools/cpplocatorfilter_test.cpp
@@ -214,20 +214,17 @@ void CppToolsPlugin::test_cpplocatorfilters_CppLocatorFilter_data()
         << _("my")
         << (QList<ResultData>()
             << ResultData(_("MyClass"), testFileShort)
-            << ResultData(_("MyClass::MyClass"), _("()"))
-            << ResultData(_("MyClass::function2"), _("(bool, int)"))
+            << ResultData(_("MyClass"), _("()"))
+            << ResultData(_("MyClass"), _("()"))
+            << ResultData(_("MyClass"), testFileShort)
+            << ResultData(_("MyClass"), _("()"))
+            << ResultData(_("MyClass"), testFileShort)
             << ResultData(_("MyEnum"), testFileShort)
-            << ResultData(_("MyNamespace::MyClass"), testFileShort)
-            << ResultData(_("MyNamespace::MyClass::MyClass"), _("()"))
-            << ResultData(_("MyNamespace::MyClass::function2"), _("(bool, int)"))
-            << ResultData(_("MyNamespace::MyEnum"), testFileShort)
-            << ResultData(_("MyNamespace::myFunction"), _("(bool, int)"))
+            << ResultData(_("MyEnum"), testFileShort)
+            << ResultData(_("MyEnum"), testFileShort)
             << ResultData(_("myFunction"), _("(bool, int)"))
-            << ResultData(_("<anonymous namespace>::MyClass"), testFileShort)
-            << ResultData(_("<anonymous namespace>::MyClass::MyClass"), _("()"))
-            << ResultData(_("<anonymous namespace>::MyClass::function2"), _("(bool, int)"))
-            << ResultData(_("<anonymous namespace>::MyEnum"), testFileShort)
-            << ResultData(_("<anonymous namespace>::myFunction"), _("(bool, int)"))
+            << ResultData(_("myFunction"), _("(bool, int)"))
+            << ResultData(_("myFunction"), _("(bool, int)"))
             );
 }
 