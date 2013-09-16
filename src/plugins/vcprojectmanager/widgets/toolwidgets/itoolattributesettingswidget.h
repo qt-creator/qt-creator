5c3f3c865b1ee9d8ad3720f871eceab3ea4f9ff1
Radovan Zivkovic <pivonroll@gmail.com>
2013-09-16 23:12:38 +0200

    Fixed typo in Radovan's last name in license header.
    Change-Id: I254995a2e65bf5771357ce07b25f93b7c41214c4
    Reviewed-by: hjk <hjk121@nokiamail.com>

--- src/plugins/vcprojectmanager/widgets/toolwidgets/itoolattributesettingswidget.h
+++ src/plugins/vcprojectmanager/widgets/toolwidgets/itoolattributesettingswidget.h
@@ -1,7 +1,7 @@
 /**************************************************************************
 **
 ** Copyright (c) 2013 Bojan Petrovic
-** Copyright (c) 2013 Radovan Zivkvoic
+** Copyright (c) 2013 Radovan Zivkovic
 ** Contact: http://www.qt-project.org/legal
 **
 ** This file is part of Qt Creator.
