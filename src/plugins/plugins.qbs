qt-creator/qt-creator

commit cf56178df17db9a4840e52627ec89532c76c41e5

Port welcome screen to Qt Quick 2
This ports the welcome screen to use QtQuick 2.0.
I have disabled the plugin on Qt4 builds.

Change-Id: Ia921d0747c8f7d4441c88fc9fb77b822496091f4
Reviewed-by: Thomas Hartmann <Thomas.Hartmann@digia.com>

src/plugins/plugins.qbs
@@ -2,6 +2,7 @@ import qbs
 
 Project {
     name: "Plugins"
+
     references: [
         "analyzerbase/analyzerbase.qbs",
         "android/android.qbs",
@@ -56,6 +57,11 @@ Project {
         "updateinfo/updateinfo.qbs",
         "valgrind/valgrind.qbs",
         "vcsbase/vcsbase.qbs",
-        "welcome/welcome.qbs"
     ]
+
+    Group {
+        condition: qtcore.versionMajor >= 5
+        references: [ "welcome/welcome.qbs" ]
+    }
+
 }

src/plugins/projectexplorer/projectexplorer.qbs
@@ -5,7 +5,7 @@ import "../QtcPlugin.qbs" as QtcPlugin
 QtcPlugin {
     name: "ProjectExplorer"
 
-    Depends { name: "Qt"; submodules: ["widgets", "xml", "network", "script", "declarative"] }
+    Depends { name: "Qt"; submodules: ["widgets", "xml", "network", "script", "quick"] }
     Depends { name: "Core" }
     Depends { name: "Locator" }
     Depends { name: "Find" }
@@ -109,7 +109,6 @@ QtcPlugin {
             "projectmodels.cpp", "projectmodels.h",
             "projectnodes.cpp", "projectnodes.h",
             "projecttreewidget.cpp", "projecttreewidget.h",
-            "projectwelcomepage.cpp", "projectwelcomepage.h",
             "projectwindow.cpp", "projectwindow.h",
             "projectwizardpage.cpp", "projectwizardpage.h", "projectwizardpage.ui",
             "removetaskhandler.cpp", "removetaskhandler.h",
@@ -136,6 +135,15 @@ QtcPlugin {
             "toolchainoptionspage.cpp", "toolchainoptionspage.h",
             "vcsannotatetaskhandler.cpp", "vcsannotatetaskhandler.h",
         ]
+
+        Group {
+            condition: qtcore.versionMajor >= 5
+            references: [
+                "projectwelcomepage.cpp",
+                "projectwelcomepage.h"
+            ]
+        }
+
     }
 
     Group {

src/plugins/qtsupport/qtsupport.qbs
@@ -5,7 +5,7 @@ import "../QtcPlugin.qbs" as QtcPlugin
 QtcPlugin {
     name: "QtSupport"
 
-    Depends { name: "Qt"; submodules: ["widgets", "declarative"] }
+    Depends { name: "Qt"; submodules: ["widgets", "quick"] }
     Depends { name: "Core" }
     Depends { name: "ProjectExplorer" }
     Depends { name: "TextEditor" }
@@ -70,8 +70,6 @@ QtcPlugin {
         "debugginghelperbuildtask.h",
         "exampleslistmodel.cpp",
         "exampleslistmodel.h",
-        "gettingstartedwelcomepage.cpp",
-        "gettingstartedwelcomepage.h",
         "profilereader.cpp",
         "profilereader.h",
         "qmldebugginglibrary.cpp",
@@ -111,6 +109,15 @@ QtcPlugin {
         "images/qt_project.png",
         "images/qt_qrc.png",
     ]
+
+    Group {
+        condition: qtcore.versionMajor >= 5
+        files: [
+            "gettingstartedwelcomepage.cpp",
+            "gettingstartedwelcomepage.h"
+        ]
+    }
+
 
     Export {
         Depends { name: "cpp" }

src/plugins/welcome/welcome.qbs
@@ -5,7 +5,7 @@ import "../QtcPlugin.qbs" as QtcPlugin
 QtcPlugin {
     name: "Welcome"
 
-    Depends { name: "Qt"; submodules: ["widgets", "network", "declarative"] }
+    Depends { name: "Qt"; submodules: ["widgets", "network", "quick"] }
     Depends { name: "Core" }
     Depends { name: "ProjectExplorer" }
 

share/qtcreator/qml/qmlobserver/startup/Logo.qml
@@ -27,7 +27,7 @@
 **
 ****************************************************************************/
 
-import QtQuick 1.0
+import QtQuick 2.1
 
 Rectangle {
     id: myApp

share/qtcreator/welcomescreen/develop.qml
@@ -27,7 +27,7 @@
 **
 ****************************************************************************/
 
-import QtQuick 1.1
+import QtQuick 2.1
 import widgets 1.0
 
 Rectangle {

share/qtcreator/welcomescreen/dummydata/examplesModel.qml
@@ -27,7 +27,7 @@
 **
 ****************************************************************************/
 
-import QtQuick 1.0
+import QtQuick 2.1
 
 ListModel {
     ListElement {

share/qtcreator/welcomescreen/dummydata/pagesModel.qml
@@ -27,7 +27,7 @@
 **
 ****************************************************************************/
 
-import QtQuick 1.0
+import QtQuick 2.1
 
 ListModel {
     ListElement {

commit 3ae8d6b36a6091817c3033ac75647aa2badedf41

Fix qbs build.
Commit cf56178d introduced an impressive amount of syntax errors.

Change-Id: Ibaf8b8384b06ee92b658b71f08ac8d14e92c19b0
Reviewed-by: Thomas Hartmann <Thomas.Hartmann@digia.com>
Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>

src/plugins/plugins.qbs
@@ -2,6 +2,7 @@ import qbs
 
 Project {
     name: "Plugins"
+    property bool buildWelcomePlugin: Qt.core.versionMajor >= 5
 
     references: [
         "analyzerbase/analyzerbase.qbs",
@@ -57,11 +58,6 @@ Project {
         "updateinfo/updateinfo.qbs",
         "valgrind/valgrind.qbs",
         "vcsbase/vcsbase.qbs",
+        "welcome/welcome.qbs"
     ]
-
-    Group {
-        condition: qtcore.versionMajor >= 5
-        references: [ "welcome/welcome.qbs" ]
-    }
-
 }

src/plugins/projectexplorer/projectexplorer.qbs
@@ -135,15 +135,15 @@ QtcPlugin {
             "toolchainoptionspage.cpp", "toolchainoptionspage.h",
             "vcsannotatetaskhandler.cpp", "vcsannotatetaskhandler.h",
         ]
+    }
 
-        Group {
-            condition: qtcore.versionMajor >= 5
-            references: [
-                "projectwelcomepage.cpp",
-                "projectwelcomepage.h"
-            ]
-        }
-
+    Group {
+        name: "Project Welcome Page"
+        condition: project.buildWelcomePlugin
+        files: [
+            "projectwelcomepage.cpp",
+            "projectwelcomepage.h"
+        ]
     }
 
     Group {

src/plugins/qtsupport/qtsupport.qbs
@@ -111,7 +111,8 @@ QtcPlugin {
     ]
 
     Group {
-        condition: qtcore.versionMajor >= 5
+        name: "Getting Started Welcome Page"
+        condition: project.buildWelcomePlugin
         files: [
             "gettingstartedwelcomepage.cpp",
             "gettingstartedwelcomepage.h"

src/plugins/welcome/welcome.qbs
@@ -4,6 +4,7 @@ import "../QtcPlugin.qbs" as QtcPlugin
 
 QtcPlugin {
     name: "Welcome"
+    condition: project.buildWelcomePlugin
 
     Depends { name: "Qt"; submodules: ["widgets", "network", "quick"] }
     Depends { name: "Core" }