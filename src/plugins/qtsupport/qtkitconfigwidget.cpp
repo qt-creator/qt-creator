67bb37a204fffc3b05a5cf36694ad659f398d5af
Tobias Hunger <tobias.hunger@digia.com>
2013-09-25 11:44:16 +0200

    QtKitInformation: Also show invalid Qt versions in the config widget
    They are used, so show them.
    
    Change-Id: I28f7dca3d2597c5d9bd43dbc07ba59ebc6309606
    Reviewed-by: Daniel Teske <daniel.teske@digia.com>

--- src/plugins/qtsupport/qtkitconfigwidget.cpp
+++ src/plugins/qtsupport/qtkitconfigwidget.cpp
@@ -106,28 +106,34 @@ QWidget *QtKitConfigWidget::buttonWidget() const
     return m_manageButton;
 }
 
+static QString itemNameFor(const BaseQtVersion *v)
+{
+    QTC_CHECK(v);
+    QString name = v->displayName();
+    if (!v->isValid())
+        name = QCoreApplication::translate("QtSupport::Internal::QtKitConfigWidget", "%1 (invalid)").arg(v->displayName());
+    return name;
+}
+
 void QtKitConfigWidget::versionsChanged(const QList<int> &added, const QList<int> &removed,
                                         const QList<int> &changed)
 {
     foreach (const int id, added) {
         BaseQtVersion *v = QtVersionManager::version(id);
         QTC_CHECK(v);
-        if (!v->isValid())
-            continue;
         QTC_CHECK(findQtVersion(id) < 0);
-        m_combo->addItem(v->displayName(), id);
+        m_combo->addItem(itemNameFor(v), id);
     }
     foreach (const int id, removed) {
         int pos = findQtVersion(id);
         if (pos >= 0) // We do not include invalid Qt versions, so do not try to remove those.
             m_combo->removeItem(pos);
-
     }
     foreach (const int id, changed) {
         BaseQtVersion *v = QtVersionManager::version(id);
         int pos = findQtVersion(id);
-        if (pos >= 0) // We do not include invalid Qt versions, so do not try to remove those.
-            m_combo->setItemText(pos, v->displayName());
+        QTC_CHECK(pos >= 0);
+        m_combo->setItemText(pos, itemNameFor(v));
     }
 }
 
