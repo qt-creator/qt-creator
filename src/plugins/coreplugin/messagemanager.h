qt-creator/qt-creator

commit 527046acc212691b20be60d450c249d8d7dcbe12

MessageManager: General cleanup.
Apply 'static' pattern, rename 'printToOutputPane' to 'write'

Change-Id: I6f31f1a8c914661e2a2182c0b6256b41ead28de7
Reviewed-by: David Schulz <david.schulz@digia.com>

src/plugins/coreplugin/messagemanager.h
@@ -38,39 +38,37 @@
 
 namespace Core {
 
-namespace Internal {
-class MessageOutputWindow;
-class MainWindow;
-}
+namespace Internal { class MainWindow; }
 
 class CORE_EXPORT MessageManager : public QObject
 {
     Q_OBJECT
 
 public:
-    static MessageManager *instance();
+    static QObject *instance();
 
-    void showOutputPane();
+    static void showOutputPane();
+
     enum PrintToOutputPaneFlag {
-        NoModeSwitch = Core::IOutputPane::NoModeSwitch,
-        ModeSwitch = Core::IOutputPane::ModeSwitch,
-        WithFocus = Core::IOutputPane::WithFocus,
-        EnsureSizeHint = Core::IOutputPane::EnsureSizeHint,
-        Silent = 256,
-        Flash = 512 };
+        NoModeSwitch   = IOutputPane::NoModeSwitch,
+        ModeSwitch     = IOutputPane::ModeSwitch,
+        WithFocus      = IOutputPane::WithFocus,
+        EnsureSizeHint = IOutputPane::EnsureSizeHint,
+        Silent         = 256,
+        Flash          = 51
+    };
 
     Q_DECLARE_FLAGS(PrintToOutputPaneFlags, PrintToOutputPaneFlag)
 
+    static void write(const QString &text); // imply NoModeSwitch
+
 public slots:
-    void printToOutputPane(const QString &text, Core::MessageManager::PrintToOutputPaneFlags flags);
+    static void write(const QString &text, Core::MessageManager::PrintToOutputPaneFlags flags);
 
 private:
     MessageManager();
     ~MessageManager();
-    void init();
-
-    Internal::MessageOutputWindow *m_messageOutputWindow;
-
+    static void init();
     friend class Core::Internal::MainWindow;
 };
 

src/plugins/android/androiddeploystep.cpp
@@ -53,6 +53,7 @@
 
 #define ASSERT_STATE(state) ASSERT_STATE_GENERIC(State, state, m_state)
 
+using namespace Core;
 using namespace ProjectExplorer;
 using namespace Qt4ProjectManager;
 
@@ -62,7 +63,7 @@ namespace Internal {
 static const char USE_LOCAL_QT_KEY[] = "Qt4ProjectManager.AndroidDeployStep.UseLocalQtLibs";
 static const char DEPLOY_ACTION_KEY[] = "Qt4ProjectManager.AndroidDeployStep.DeployAction";
 
-const Core::Id AndroidDeployStep::Id("Qt4ProjectManager.AndroidDeployStep");
+const Id AndroidDeployStep::Id("Qt4ProjectManager.AndroidDeployStep");
 
 AndroidDeployStep::AndroidDeployStep(ProjectExplorer::BuildStepList *parent)
     : BuildStep(parent, Id)
@@ -216,13 +217,13 @@ void AndroidDeployStep::cleanLibsOnDevice()
         QString avdName = AndroidConfigurations::instance().findAvd(&deviceAPILevel, targetArch);
         if (avdName.isEmpty()) {
             // No avd found, don't create one just error out
-            Core::MessageManager::instance()->printToOutputPane(tr("Could not find a device."), Core::MessageManager::NoModeSwitch);
+            MessageManager::write(tr("Could not find a device."));
             return;
         }
         deviceSerialNumber = AndroidConfigurations::instance().startAVD(avdName, deviceAPILevel, targetArch);
     }
     if (!deviceSerialNumber.length()) {
-        Core::MessageManager::instance()->printToOutputPane(tr("Could not run adb. No device found."), Core::MessageManager::NoModeSwitch);
+        MessageManager::write(tr("Could not run adb. No device found."));
         return;
     }
     QProcess *process = new QProcess(this);
@@ -230,9 +231,7 @@ void AndroidDeployStep::cleanLibsOnDevice()
     arguments << QLatin1String("shell") << QLatin1String("rm") << QLatin1String("-r") << QLatin1String("/data/local/tmp/qt");
     connect(process, SIGNAL(finished(int)), this, SLOT(processFinished()));
     const QString adb = AndroidConfigurations::instance().adbToolPath().toString();
-    Core::MessageManager::instance()->printToOutputPane(adb + QLatin1String(" ")
-                                                        + arguments.join(QLatin1String(" ")),
-                                                        Core::MessageManager::NoModeSwitch);
+    MessageManager::write(adb + QLatin1Char(' ') + arguments.join(QLatin1Char(' ')));
     process->start(adb, arguments);
     if (!process->waitForStarted(500))
         delete process;
@@ -242,9 +241,8 @@ void AndroidDeployStep::processFinished()
 {
     QProcess *process = qobject_cast<QProcess *>(sender());
     QTC_ASSERT(process, return);
-    Core::MessageManager::instance()->printToOutputPane(QString::fromLocal8Bit(process->readAll()), Core::MessageManager::NoModeSwitch);
-    Core::MessageManager::instance()->printToOutputPane(tr("adb finished with exit code %1.").arg(process->exitCode()),
-                                                        Core::MessageManager::NoModeSwitch);
+    MessageManager::write(QString::fromLocal8Bit(process->readAll()));
+    MessageManager::write(tr("adb finished with exit code %1.").arg(process->exitCode()));
     process->deleteLater();
 }
 
@@ -274,14 +272,13 @@ void AndroidDeployStep::installQASIPackage(const QString &packagePath)
     if (deviceSerialNumber.isEmpty()) {
         QString avdName = AndroidConfigurations::instance().findAvd(&deviceAPILevel, targetArch);
         if (avdName.isEmpty()) {
-            Core::MessageManager::instance()->printToOutputPane(tr("No device found."),
-                                                                Core::MessageManager::NoModeSwitch);
+            MessageManager::write(tr("No device found."));
             return;
         }
         deviceSerialNumber = AndroidConfigurations::instance().startAVD(avdName, deviceAPILevel, targetArch);
     }
     if (!deviceSerialNumber.length()) {
-        Core::MessageManager::instance()->printToOutputPane(tr("Could not run adb. No device found."), Core::MessageManager::NoModeSwitch);
+        MessageManager::write(tr("Could not run adb. No device found."));
         return;
     }
 
@@ -291,9 +288,7 @@ void AndroidDeployStep::installQASIPackage(const QString &packagePath)
 
     connect(process, SIGNAL(finished(int)), this, SLOT(processFinished()));
     const QString adb = AndroidConfigurations::instance().adbToolPath().toString();
-    Core::MessageManager::instance()->printToOutputPane(adb + QLatin1String(" ")
-                                                        + arguments.join(QLatin1String(" ")),
-                                                        Core::MessageManager::NoModeSwitch);
+    MessageManager::write(adb + QLatin1Char(' ') + arguments.join(QLatin1Char(' ')));
     process->start(adb, arguments);
     if (!process->waitForFinished(500))
         delete process;

src/plugins/coreplugin/externaltool.cpp
@@ -610,7 +610,7 @@ void ExternalToolRunner::run()
     if (!m_resolvedWorkingDirectory.isEmpty())
         m_process->setWorkingDirectory(m_resolvedWorkingDirectory);
     m_process->setCommand(m_resolvedExecutable, m_resolvedArguments);
-    MessageManager::instance()->printToOutputPane(
+    MessageManager::write(
                 tr("Starting external tool '%1' %2").arg(m_resolvedExecutable, m_resolvedArguments), MessageManager::Silent);
     m_process->start();
 }
@@ -632,7 +632,7 @@ void ExternalToolRunner::finished(int exitCode, QProcess::ExitStatus status)
         if (m_tool->modifiesCurrentDocument())
             DocumentManager::unexpectFileChange(m_expectedFileName);
     }
-    MessageManager::instance()->printToOutputPane(
+    MessageManager::write(
                 tr("'%1' finished").arg(m_resolvedExecutable), MessageManager::Silent);
     deleteLater();
 }
@@ -653,7 +653,7 @@ void ExternalToolRunner::readStandardOutput()
     QByteArray data = m_process->readAllStandardOutput();
     QString output = m_outputCodec->toUnicode(data.constData(), data.length(), &m_outputCodecState);
     if (m_tool->outputHandling() == ExternalTool::ShowInPane)
-        MessageManager::instance()->printToOutputPane(output, MessageManager::NoModeSwitch);
+        MessageManager::write(output);
     else if (m_tool->outputHandling() == ExternalTool::ReplaceSelection)
         m_processOutput.append(output);
 }
@@ -665,7 +665,7 @@ void ExternalToolRunner::readStandardError()
     QByteArray data = m_process->readAllStandardError();
     QString output = m_outputCodec->toUnicode(data.constData(), data.length(), &m_errorCodecState);
     if (m_tool->errorHandling() == ExternalTool::ShowInPane)
-        MessageManager::instance()->printToOutputPane(output, MessageManager::NoModeSwitch);
+        MessageManager::write(output);
     else if (m_tool->errorHandling() == ExternalTool::ReplaceSelection)
         m_processOutput.append(output);
 }
@@ -767,7 +767,7 @@ void ExternalToolManager::menuActivated()
     QTC_ASSERT(tool, return);
     ExternalToolRunner *runner = new ExternalToolRunner(tool);
     if (runner->hasError())
-        MessageManager::instance()->printToOutputPane(runner->errorString(), MessageManager::NoModeSwitch);
+        MessageManager::write(runner->errorString());
 }
 
 QMap<QString, QList<Internal::ExternalTool *> > ExternalToolManager::toolsByCategory() const

src/plugins/coreplugin/mainwindow.cpp
@@ -332,7 +332,7 @@ bool MainWindow::init(QString *errorMessage)
     m_outputView->setWidget(OutputPaneManager::instance()->buttonsWidget());
     m_outputView->setPosition(Core::StatusBarWidget::Second);
     ExtensionSystem::PluginManager::addObject(m_outputView);
-    m_messageManager->init();
+    MessageManager::init();
     return true;
 }
 

src/plugins/coreplugin/messagemanager.cpp
@@ -35,12 +35,17 @@
 using namespace Core;
 
 static MessageManager *m_instance = 0;
-MessageManager *MessageManager::instance() { return m_instance; }
+Internal::MessageOutputWindow *m_messageOutputWindow = 0;
+
+QObject *MessageManager::instance()
+{
+    return m_instance;
+}
 
 MessageManager::MessageManager()
-    : m_messageOutputWindow(0)
 {
     m_instance = this;
+    m_messageOutputWindow = 0;
     qRegisterMetaType<Core::MessageManager::PrintToOutputPaneFlags>();
 }
 
@@ -50,7 +55,6 @@ MessageManager::~MessageManager()
         ExtensionSystem::PluginManager::removeObject(m_messageOutputWindow);
         delete m_messageOutputWindow;
     }
-
     m_instance = 0;
 }
 
@@ -66,7 +70,12 @@ void MessageManager::showOutputPane()
         m_messageOutputWindow->popup(IOutputPane::ModeSwitch);
 }
 
-void MessageManager::printToOutputPane(const QString &text, PrintToOutputPaneFlags flags)
+void MessageManager::write(const QString &text)
+{
+    write(text, NoModeSwitch);
+}
+
+void MessageManager::write(const QString &text, PrintToOutputPaneFlags flags)
 {
     if (!m_messageOutputWindow)
         return;

src/plugins/cpaster/cpasterplugin.cpp
@@ -306,7 +306,7 @@ void CodepasterPlugin::finishPost(const QString &link)
 {
     if (m_settings->copyToClipboard)
         QApplication::clipboard()->setText(link);
-    MessageManager::instance()->printToOutputPane(link, m_settings->displayOutput ? Core::MessageManager::ModeSwitch : Core::MessageManager::Silent);
+    MessageManager::write(link, m_settings->displayOutput ? Core::MessageManager::ModeSwitch : Core::MessageManager::Silent);
 }
 
 // Extract the characters that can be used for a file name from a title
@@ -346,14 +346,13 @@ void CodepasterPlugin::finishFetch(const QString &titleDescription,
                                    const QString &content,
                                    bool error)
 {
-    Core::MessageManager *messageManager = MessageManager::instance();
     // Failure?
     if (error) {
-        messageManager->printToOutputPane(content, Core::MessageManager::NoModeSwitch);
+        MessageManager::write(content);
         return;
     }
     if (content.isEmpty()) {
-        messageManager->printToOutputPane(tr("Empty snippet received for \"%1\".").arg(titleDescription), Core::MessageManager::NoModeSwitch);
+        MessageManager::write(tr("Empty snippet received for \"%1\".").arg(titleDescription));
         return;
     }
     // If the mime type has a preferred suffix (cpp/h/patch...), use that for
@@ -363,7 +362,7 @@ void CodepasterPlugin::finishFetch(const QString &titleDescription,
     // Default to "txt".
     QByteArray byteContent = content.toUtf8();
     QString suffix;
-    if (const Core::MimeType mimeType = Core::MimeDatabase::findByData(byteContent))
+    if (const MimeType mimeType = MimeDatabase::findByData(byteContent))
         suffix = mimeType.preferredSuffix();
     if (suffix.isEmpty())
          suffix = QLatin1String("txt");
@@ -372,7 +371,7 @@ void CodepasterPlugin::finishFetch(const QString &titleDescription,
     saver.setAutoRemove(false);
     saver.write(byteContent);
     if (!saver.finalize()) {
-        messageManager->printToOutputPane(saver.errorString(), Core::MessageManager::NoModeSwitch);
+        MessageManager::write(saver.errorString());
         return;
     }
     const QString fileName = saver.fileName();

src/plugins/cpaster/fileshareprotocol.cpp
@@ -205,11 +205,10 @@ void FileShareProtocol::paste(const QString &text,
         saver.setResult(&writer);
     }
     if (!saver.finalize()) {
-        Core::MessageManager::instance()->printToOutputPane(saver.errorString(), Core::MessageManager::NoModeSwitch);
+        Core::MessageManager::write(saver.errorString());
         return;
     }
 
-    const QString msg = tr("Pasted: %1").arg(saver.fileName());
-    Core::MessageManager::instance()->printToOutputPane(msg, Core::MessageManager::NoModeSwitch);
+    Core::MessageManager::write(tr("Pasted: %1").arg(saver.fileName()));
 }
 } // namespace CodePaster

src/plugins/debugger/debuggerplugin.cpp
@@ -1479,7 +1479,7 @@ void DebuggerPluginPrivate::parseCommandLineArguments()
         errorMessage = tr("Error evaluating command line arguments: %1")
             .arg(errorMessage);
         qWarning("%s\n", qPrintable(errorMessage));
-        Core::MessageManager::instance()->printToOutputPane(errorMessage, Core::MessageManager::NoModeSwitch);
+        MessageManager::write(errorMessage);
     }
     if (!m_scheduledStarts.isEmpty())
         QTimer::singleShot(0, this, SLOT(runScheduled()));

commit ef298df4b2a24905607e0cc2dff692fd527f649b

MessageManager: Fix constant broken by 527046acc212691b20be60d450c249d8d7dcbe12
Task-number: QTCREATORBUG-10255
Change-Id: I3d8ffd8441c5bb5f3dc39b1cdbb4e82740e5d4ea
Reviewed-by: David Schulz <david.schulz@digia.com>

src/plugins/coreplugin/messagemanager.h
@@ -55,7 +55,7 @@ public:
         WithFocus      = IOutputPane::WithFocus,
         EnsureSizeHint = IOutputPane::EnsureSizeHint,
         Silent         = 256,
-        Flash          = 51
+        Flash          = 512
     };
 
     Q_DECLARE_FLAGS(PrintToOutputPaneFlags, PrintToOutputPaneFlag)