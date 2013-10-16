qt-creator/qt-creator

commit 113c5600c909791ddfea3d424a41d982dcdda6b7

Move displayName from IEditor to IDocument
The display name is not editor instance specific, but belongs to the
document.

Change-Id: I3c936f04a86e10e6ca30063d85036d85b4b5880e
Reviewed-by: Daniel Teske <daniel.teske@digia.com>

src/plugins/texteditor/plaintexteditor.cpp
@@ -69,7 +69,6 @@ PlainTextEditorWidget::PlainTextEditorWidget(QWidget *parent)
     setIndenter(new NormalIndenter); // Currently only "normal" indentation is supported.
 
     setMimeType(QLatin1String(TextEditor::Constants::C_TEXTEDITOR_MIMETYPE_TEXT));
-    setDisplayName(tr(Core::Constants::K_DEFAULT_TEXT_EDITOR_DISPLAY_NAME));
 
     m_commentDefinition.clearCommentStyles();
 

src/plugins/bazaar/bazaarplugin.cpp
@@ -561,7 +561,7 @@ void BazaarPlugin::showCommitWidget(const QList<VcsBase::VcsBaseClient::StatusIt
 
     const QString msg = tr("Commit changes for \"%1\".").
             arg(QDir::toNativeSeparators(m_submitRepository));
-    commitEditor->setDisplayName(msg);
+    commitEditor->document()->setDisplayName(msg);
 
     const BranchInfo branch = m_client->synchronousBranchQuery(m_submitRepository);
     commitEditor->setFields(m_submitRepository, branch,

src/plugins/bazaar/commiteditor.cpp
@@ -30,6 +30,7 @@
 #include "branchinfo.h"
 #include "bazaarcommitwidget.h"
 
+#include <coreplugin/idocument.h>
 #include <vcsbase/submitfilemodel.h>
 
 #include <QDebug>
@@ -40,7 +41,7 @@ CommitEditor::CommitEditor(const VcsBase::VcsBaseSubmitEditorParameters *paramet
     : VcsBase::VcsBaseSubmitEditor(parameters, new BazaarCommitWidget(parent)),
       m_fileModel(0)
 {
-    setDisplayName(tr("Commit Editor"));
+    document()->setDisplayName(tr("Commit Editor"));
 }
 
 BazaarCommitWidget *CommitEditor::commitWidget()

src/plugins/bineditor/bineditorplugin.cpp
@@ -208,11 +208,6 @@ public:
         }
     }
 
-    void setFilePath(const QString &newName) {
-        m_widget->editor()->setDisplayName(QFileInfo(newName).fileName());
-        IDocument::setFilePath(newName);
-    }
-
     bool open(QString *errorString, const QString &fileName, quint64 offset = 0) {
         QFile file(fileName);
         quint64 size = static_cast<quint64>(file.size());
@@ -337,9 +332,8 @@ public:
         widget->setEditor(this);
 
         connect(m_widget, SIGNAL(cursorPositionChanged(int)), SLOT(updateCursorPosition(int)));
-        connect(m_file, SIGNAL(changed()), SIGNAL(changed()));
         connect(m_addressEdit, SIGNAL(editingFinished()), SLOT(jumpToAddress()));
-        connect(m_widget, SIGNAL(modificationChanged(bool)), SIGNAL(changed()));
+        connect(m_widget, SIGNAL(modificationChanged(bool)), m_file, SIGNAL(changed()));
         updateCursorPosition(m_widget->cursorPosition());
     }
 
@@ -358,8 +352,6 @@ public:
     }
     Core::IDocument *document() { return m_file; }
     Core::Id id() const { return Core::Id(Core::Constants::K_DEFAULT_BINARY_EDITOR_ID); }
-    QString displayName() const { return m_displayName; }
-    void setDisplayName(const QString &title) { m_displayName = title; emit changed(); }
 
     QWidget *toolBar() { return m_toolBar; }
 
@@ -377,7 +369,6 @@ private slots:
 
 private:
     BinEditorWidget *m_widget;
-    QString m_displayName;
     BinEditorDocument *m_file;
     QToolBar *m_toolBar;
     QLineEdit *m_addressEdit;

src/plugins/clearcase/clearcasesubmiteditor.cpp
@@ -31,6 +31,7 @@
 #include "clearcasesubmiteditor.h"
 #include "clearcasesubmiteditorwidget.h"
 
+#include <coreplugin/idocument.h>
 #include <vcsbase/submiteditorwidget.h>
 #include <vcsbase/submitfilemodel.h>
 
@@ -40,7 +41,7 @@ ClearCaseSubmitEditor::ClearCaseSubmitEditor(const VcsBase::VcsBaseSubmitEditorP
                                              QWidget *parentWidget) :
     VcsBase::VcsBaseSubmitEditor(parameters, new ClearCaseSubmitEditorWidget(parentWidget))
 {
-    setDisplayName(tr("ClearCase Check In"));
+    document()->setDisplayName(tr("ClearCase Check In"));
 }
 
 ClearCaseSubmitEditorWidget *ClearCaseSubmitEditor::submitEditorWidget()

src/plugins/cmakeprojectmanager/cmakeeditor.cpp
@@ -60,7 +60,7 @@ CMakeEditor::CMakeEditor(CMakeEditorWidget *editor)
 {
     setContext(Core::Context(CMakeProjectManager::Constants::C_CMAKEEDITOR,
               TextEditor::Constants::C_TEXTEDITOR));
-    connect(this, SIGNAL(changed()), this, SLOT(markAsChanged()));
+    connect(document(), SIGNAL(changed()), this, SLOT(markAsChanged()));
 }
 
 Core::IEditor *CMakeEditor::duplicate(QWidget *parent)

src/plugins/cmakeprojectmanager/cmakeproject.cpp
@@ -85,9 +85,9 @@ using namespace ProjectExplorer;
 // Who sets up the environment for cl.exe ? INCLUDEPATH and so on
 
 // Test for form editor (loosely coupled)
-static inline bool isFormWindowEditor(const QObject *o)
+static inline bool isFormWindowDocument(const QObject *o)
 {
-    return o && !qstrcmp(o->metaObject()->className(), "Designer::FormWindowEditor");
+    return o && !qstrcmp(o->metaObject()->className(), "Designer::Internal::FormWindowFile");
 }
 
 // Return contents of form editor (loosely coupled)
@@ -822,8 +822,8 @@ void CMakeProject::updateCodeModelSupportFromEditor(const QString &uiFileName,
 void CMakeProject::editorChanged(Core::IEditor *editor)
 {
     // Handle old editor
-    if (isFormWindowEditor(m_lastEditor)) {
-        disconnect(m_lastEditor, SIGNAL(changed()), this, SLOT(uiEditorContentsChanged()));
+    if (m_lastEditor && isFormWindowDocument(m_lastEditor->document())) {
+        disconnect(m_lastEditor->document(), SIGNAL(changed()), this, SLOT(uiDocumentContentsChanged()));
         if (m_dirtyUic) {
             const QString contents =  formWindowEditorContents(m_lastEditor);
             updateCodeModelSupportFromEditor(m_lastEditor->document()->filePath(), contents);
@@ -834,8 +834,8 @@ void CMakeProject::editorChanged(Core::IEditor *editor)
     m_lastEditor = editor;
 
     // Handle new editor
-    if (isFormWindowEditor(editor))
-        connect(editor, SIGNAL(changed()), this, SLOT(uiEditorContentsChanged()));
+    if (editor && isFormWindowDocument(editor->document()))
+        connect(editor->document(), SIGNAL(changed()), this, SLOT(uiDocumentContentsChanged()));
 }
 
 void CMakeProject::editorAboutToClose(Core::IEditor *editor)
@@ -843,8 +843,8 @@ void CMakeProject::editorAboutToClose(Core::IEditor *editor)
     if (m_lastEditor == editor) {
         // Oh no our editor is going to be closed
         // get the content first
-        if (isFormWindowEditor(m_lastEditor)) {
-            disconnect(m_lastEditor, SIGNAL(changed()), this, SLOT(uiEditorContentsChanged()));
+        if (isFormWindowDocument(m_lastEditor->document())) {
+            disconnect(m_lastEditor->document(), SIGNAL(changed()), this, SLOT(uiDocumentContentsChanged()));
             if (m_dirtyUic) {
                 const QString contents = formWindowEditorContents(m_lastEditor);
                 updateCodeModelSupportFromEditor(m_lastEditor->document()->filePath(), contents);
@@ -855,10 +855,10 @@ void CMakeProject::editorAboutToClose(Core::IEditor *editor)
     }
 }
 
-void CMakeProject::uiEditorContentsChanged()
+void CMakeProject::uiDocumentContentsChanged()
 {
     // cast sender, get filename
-    if (!m_dirtyUic && isFormWindowEditor(sender()))
+    if (!m_dirtyUic && isFormWindowDocument(sender()))
         m_dirtyUic = true;
 }
 

src/plugins/cmakeprojectmanager/cmakeproject.h
@@ -119,7 +119,7 @@ private slots:
 
     void editorChanged(Core::IEditor *editor);
     void editorAboutToClose(Core::IEditor *editor);
-    void uiEditorContentsChanged();
+    void uiDocumentContentsChanged();
     void buildStateChanged(ProjectExplorer::Project *project);
     void updateRunConfigurations();
 

commit d6f21bf549ad3fcb458a01fe1a7322b562cfb1fb

Fix document appearing as it is all changed on open
Broken by 113c5600c909791ddfea3d424a41d982dcdda6b7

Apparently SyntaxHighlighter::rehighlight is called too early

Task-number: QTCREATORBUG-10026
Change-Id: Ifa76c156b5b84b79c869d2de1a0ad69c13ac6b48
Reviewed-by: David Schulz <david.schulz@digia.com>

src/plugins/texteditor/plaintexteditor.cpp
@@ -70,7 +70,8 @@ PlainTextEditorWidget::PlainTextEditorWidget(QWidget *parent)
 
     m_commentDefinition.clearCommentStyles();
 
-    connect(editorDocument(), SIGNAL(changed()), this, SLOT(configure()));
+    // If configure() is called immediately the whole document is considered modified
+    connect(editorDocument(), SIGNAL(changed()), this, SLOT(configure()), Qt::QueuedConnection);
     connect(Manager::instance(), SIGNAL(mimeTypesRegistered()), this, SLOT(configure()));
 }
 