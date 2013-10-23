bc81930f17f5fe897fd0ede57169a52751b72f73
Eike Ziller <eike.ziller@digia.com>
2013-10-23 14:13:06 +0200

    Give preprocessor dialog button a tool tip.
    Change-Id: Iad756a8f18736da79cca63232eb00fcee8a2f38c
    Reviewed-by: David Schulz <david.schulz@digia.com>

--- src/plugins/cppeditor/cppeditor.h
+++ src/plugins/cppeditor/cppeditor.h
@@ -50,6 +50,7 @@
 QT_BEGIN_NAMESPACE
 class QComboBox;
 class QSortFilterProxyModel;
+class QToolButton;
 QT_END_NAMESPACE
 
 namespace CPlusPlus {
@@ -175,6 +176,7 @@ private Q_SLOTS:
     void onFunctionDeclDefLinkFound(QSharedPointer<FunctionDeclDefLink> link);
     void onDocumentUpdated();
     void onContentsChanged(int position, int charsRemoved, int charsAdded);
+    void updatePreprocessorButtonTooltip();
 
     void updateSemanticInfo(const CppTools::SemanticInfo &semanticInfo);
     void highlightSymbolUsages(int from, int to);
@@ -259,6 +261,7 @@ private:
 
     QScopedPointer<FollowSymbolUnderCursor> m_followSymbolUnderCursor;
     QString m_preProcessorAdditions;
+    QToolButton *m_preprocessorButton;
 };
 
 } // namespace Internal
