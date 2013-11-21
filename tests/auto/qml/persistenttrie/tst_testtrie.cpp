dc04d92af6a5b7b2fbb7385f437b34b27a964a8a
Fawzi Mohamed <fawzi.mohamed@digia.com>
2013-11-22 00:19:46 +0100

    qmljs: fix persistent trie tests
    Change-Id: I77baa46fd6f01d252fdd7a035d477433659c56dc
    Reviewed-by: Thomas Hartmann <Thomas.Hartmann@digia.com>

--- tests/auto/qml/persistenttrie/tst_testtrie.cpp
+++ tests/auto/qml/persistenttrie/tst_testtrie.cpp
@@ -350,7 +350,7 @@ void interactiveCompletionTester(){
         res = matchStrengthSort(line,res);
         qDebug() << "possible completions:[";
         foreach (const QString &s, res) {
-            qDebug() << s;
+            qDebug() << matchStrength(line,s) << " " << s;
         }
         qDebug() << "]";
     }
