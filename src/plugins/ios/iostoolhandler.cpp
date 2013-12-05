qt-creator/qt-creator

commit fccffba04bbac0439583272b1c73151e9a00b0c6

ios: fixing DYLD_FALLBACK_FRAMEWORK_PATH
adding /System/Library/Frameworks for completeness

Change-Id: If2fbe015af591eb3ff820b2ea2f732b2d4c08e01
Reviewed-by: Fawzi Mohamed <fawzi.mohamed@digia.com>

src/plugins/ios/iostoolhandler.cpp
@@ -204,12 +204,15 @@ IosToolHandlerPrivate::IosToolHandlerPrivate(IosToolHandler::DeviceType devType,
     foreach (const QString &k, env.keys())
         if (k.startsWith(QLatin1String("DYLD_")))
             env.remove(k);
+    QStringList frameworkPaths;
     QString xcPath = IosConfigurations::developerPath().appendPath(QLatin1String("../OtherFrameworks")).toFileInfo().canonicalFilePath();
-    env.insert(QLatin1String("DYLD_FALLBACK_FRAMEWORK_PATH"),
-               xcPath.isEmpty() ?
-                   QString::fromLatin1("/System/Library/PrivateFrameworks")
-                 : (xcPath + QLatin1String(":/System/Library/PrivateFrameworks")));
-
+    if (!xcPath.isEmpty())
+        frameworkPaths << xcPath;
+    frameworkPaths << QLatin1String("/System/Library/Frameworks")
+                   << QLatin1String("/System/Library/PrivateFrameworks");
+    env.insert(QLatin1String("DYLD_FALLBACK_FRAMEWORK_PATH"), frameworkPaths.join(QLatin1Char(':')));
+    if (debugToolHandler)
+        qDebug() << "IosToolHandler runEnv:" << env.toStringList();
     process.setProcessEnvironment(env);
     QObject::connect(&process, SIGNAL(readyReadStandardOutput()), q, SLOT(subprocessHasData()));
     QObject::connect(&process, SIGNAL(finished(int,QProcess::ExitStatus)),

commit 9206fefb6ba0952cdc4927ae8b7962e870e1fb5b

Fix build failure
QStringList::join doesn't accept a QLatin1Char on my system. This fixes a build
regression introduced in commit fccffba04bbac0439583272b1c73151e9a00b0c6.

Change-Id: I98d8339032cb5ea315e09860aac3db91ab21d4c5
Reviewed-by: André Hartmann <aha_1980@gmx.de>
Reviewed-by: Orgad Shaneh <orgads@gmail.com>
Reviewed-by: Eike Ziller <eike.ziller@digia.com>

src/plugins/ios/iostoolhandler.cpp
@@ -214,7 +214,7 @@ IosToolHandlerPrivate::IosToolHandlerPrivate(IosToolHandler::DeviceType devType,
         frameworkPaths << xcPath;
     frameworkPaths << QLatin1String("/System/Library/Frameworks")
                    << QLatin1String("/System/Library/PrivateFrameworks");
-    env.insert(QLatin1String("DYLD_FALLBACK_FRAMEWORK_PATH"), frameworkPaths.join(QLatin1Char(':')));
+    env.insert(QLatin1String("DYLD_FALLBACK_FRAMEWORK_PATH"), frameworkPaths.join(QLatin1String(":")));
     if (debugToolHandler)
         qDebug() << "IosToolHandler runEnv:" << env.toStringList();
     process.setProcessEnvironment(env);