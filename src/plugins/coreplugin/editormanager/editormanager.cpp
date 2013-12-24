ab10ac13cb54ce7385d5a1c6f9b99deb9b3064a9
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2013-12-24 23:28:45 +0200

    EditorManager: Add a soft assertion for editor existence
    Change-Id: Ieb1e13a25dd2ac4fbe2c1dba0adbbe93dcc6b4e7
    Reviewed-by: hjk <hjk121@nokiamail.com>
    Reviewed-by: Eike Ziller <eike.ziller@digia.com>

--- src/plugins/coreplugin/editormanager/editormanager.cpp
+++ src/plugins/coreplugin/editormanager/editormanager.cpp
@@ -1261,6 +1261,7 @@ Core::IEditor *EditorManager::placeEditor(Core::Internal::EditorView *view, Core
 
 void EditorManager::activateEditor(Core::IEditor *editor, OpenEditorFlags flags)
 {
+    QTC_ASSERT(editor, return);
     EditorView *view = viewForEditor(editor);
     // an IEditor doesn't have to belong to a view, it might be kept in storage by the editor model
     if (!view)
