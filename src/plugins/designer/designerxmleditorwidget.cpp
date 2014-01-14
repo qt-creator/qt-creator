qt-creator/qt-creator

commit a3608f595ad13f8925ad797da8ca7ddec6e36051

Editors: Remove BaseTextEditorWidget::setBaseTextDocument
First step in never changing the text document after construction,
because that is actually not really supported.
Second step will be to fix that for the editor duplicate() methods too.

Change-Id: I3d112ba0895e7ee7ac6dd8ae2318f0de7d2cebc9
Reviewed-by: Nikolai Kosjar <nikolai.kosjar@digia.com>

src/plugins/designer/designerxmleditorwidget.cpp
@@ -39,13 +39,11 @@ namespace Internal {
 
 DesignerXmlEditorWidget::DesignerXmlEditorWidget(QDesignerFormWindowInterface *form,
                                                  QWidget *parent) :
-    TextEditor::PlainTextEditorWidget(parent),
-    m_file(new FormWindowFile(form, this)),
+    TextEditor::PlainTextEditorWidget(new FormWindowFile(form, this), parent),
     m_designerEditor(new FormWindowEditor(this))
 {
-    setBaseTextDocument(m_file);
     setReadOnly(true);
-    configure(m_file->mimeType());
+    configure(baseTextDocument()->mimeType());
 }
 
 TextEditor::BaseTextEditor *DesignerXmlEditorWidget::createEditor()
@@ -62,7 +60,7 @@ FormWindowEditor *DesignerXmlEditorWidget::designerEditor() const
 
 Internal::FormWindowFile *DesignerXmlEditorWidget::formWindowFile() const
 {
-    return m_file.data();
+    return qobject_cast<FormWindowFile *>(baseTextDocument());
 }
 
 } // namespace Internal

src/plugins/android/androidmanifesteditorwidget.cpp
@@ -96,14 +96,12 @@ Project *androidProject(const QString &file)
 } // anonymous namespace
 
 AndroidManifestEditorWidget::AndroidManifestEditorWidget(QWidget *parent)
-    : TextEditor::PlainTextEditorWidget(parent),
+    : TextEditor::PlainTextEditorWidget(new AndroidManifestDocument(this), parent),
       m_dirty(false),
       m_stayClean(false),
       m_setAppName(false),
       m_appNameInStringsXml(false)
 {
-    setBaseTextDocument(QSharedPointer<AndroidManifestDocument>(new AndroidManifestDocument(this)));
-
     configure(QLatin1String(Constants::ANDROID_MANIFEST_MIME_TYPE));
 
     initializePage();
@@ -125,7 +123,6 @@ TextEditor::BaseTextEditor *AndroidManifestEditorWidget::createEditor()
 {
     return new AndroidManifestEditor(this);
 }
-
 
 void AndroidManifestEditorWidget::initializePage()
 {

src/plugins/cmakeprojectmanager/cmakeeditor.cpp
@@ -116,10 +116,8 @@ void CMakeEditor::build()
 //
 
 CMakeEditorWidget::CMakeEditorWidget(QWidget *parent)
-    : BaseTextEditorWidget(parent)
+    : BaseTextEditorWidget(new CMakeDocument(), parent)
 {
-    setBaseTextDocument(QSharedPointer<CMakeDocument>(new CMakeDocument));
-
     baseTextDocument()->setSyntaxHighlighter(new CMakeHighlighter);
 
     m_commentDefinition.clearCommentStyles();

src/plugins/cpptools/cpppointerdeclarationformatter_test.cpp
@@ -104,7 +104,7 @@ public:
 
         // Open file
         QScopedPointer<TextEditor::BaseTextEditorWidget> editorWidget(
-            new TextEditor::PlainTextEditorWidget(0));
+            new TextEditor::PlainTextEditorWidget);
         QString error;
         editorWidget->open(&error, document->fileName(), document->fileName());
         QVERIFY(error.isEmpty());

src/plugins/designer/designerxmleditorwidget.h
@@ -67,7 +67,6 @@ protected:
     virtual TextEditor::BaseTextEditor *createEditor();
 
 private:
-    QSharedPointer<Internal::FormWindowFile> m_file;
     FormWindowEditor *m_designerEditor;
 };
 

src/plugins/genericprojectmanager/genericprojectfileseditor.cpp
@@ -109,8 +109,6 @@ Core::IEditor *ProjectFilesEditor::duplicate()
 ProjectFilesEditorWidget::ProjectFilesEditorWidget(QWidget *parent)
     : BaseTextEditorWidget(parent)
 {
-    QSharedPointer<BaseTextDocument> doc(new BaseTextDocument());
-    setBaseTextDocument(doc);
 }
 
 BaseTextEditor *ProjectFilesEditorWidget::createEditor()

src/plugins/qmakeprojectmanager/profileeditor.cpp
@@ -83,11 +83,8 @@ TextEditor::CompletionAssistProvider *ProFileEditor::completionAssistProvider()
 //
 
 ProFileEditorWidget::ProFileEditorWidget(ProFileEditorFactory *factory, QWidget *parent)
-    : BaseTextEditorWidget(parent), m_factory(factory)
+    : BaseTextEditorWidget(new ProFileDocument(), parent), m_factory(factory)
 {
-    QSharedPointer<ProFileDocument> doc(new ProFileDocument());
-    setBaseTextDocument(doc);
-
     baseTextDocument()->setSyntaxHighlighter(new ProFileHighlighter);
     m_commentDefinition.clearCommentStyles();
     m_commentDefinition.singleLine = QLatin1Char('#');

src/plugins/texteditor/basetexteditor.cpp
@@ -188,9 +188,19 @@ QString BaseTextEditorWidget::convertToPlainText(const QString &txt)
 static const char kTextBlockMimeType[] = "application/vnd.qtcreator.blocktext";
 static const char kVerticalTextBlockMimeType[] = "application/vnd.qtcreator.vblocktext";
 
-
 BaseTextEditorWidget::BaseTextEditorWidget(QWidget *parent)
     : QPlainTextEdit(parent)
+{
+    ctor(QSharedPointer<BaseTextDocument>(new BaseTextDocument));
+}
+
+BaseTextEditorWidget::BaseTextEditorWidget(BaseTextDocument *doc, QWidget *parent)
+    : QPlainTextEdit(parent)
+{
+    ctor(QSharedPointer<BaseTextDocument>(doc));
+}
+
+void BaseTextEditorWidget::ctor(const QSharedPointer<BaseTextDocument> &doc)
 {
     d = new BaseTextEditorWidgetPrivate;
     d->q = this;
@@ -203,6 +213,7 @@ BaseTextEditorWidget::BaseTextEditorWidget(QWidget *parent)
     d->m_searchResultOverlay = new TextEditorOverlay(this);
     d->m_refactorOverlay = new RefactorOverlay(this);
 
+    d->m_document = doc;
     d->setupDocumentSignals(d->m_document);
 
     d->m_lastScrollPos = -1;
@@ -2079,14 +2090,6 @@ void BaseTextEditorWidget::duplicateFrom(BaseTextEditorWidget *widget)
     d->m_document = widget->d->m_document;
 }
 
-void BaseTextEditorWidget::setBaseTextDocument(const QSharedPointer<BaseTextDocument> &doc)
-{
-    if (!doc.isNull()) {
-        d->setupDocumentSignals(doc);
-        d->m_document = doc;
-    }
-}
-
 void BaseTextEditorWidget::documentAboutToBeReloaded()
 {
     //memorize cursor position
@@ -2405,7 +2408,6 @@ BaseTextEditorWidgetPrivate::BaseTextEditorWidgetPrivate()
     q(0),
     m_contentsChanged(false),
     m_lastCursorChangeWasInteresting(false),
-    m_document(new BaseTextDocument),
     m_parenthesesMatchingEnabled(false),
     m_updateTimer(0),
     m_formatRange(false),

commit 28385f84eb1e3493d9b7433dcd7d4bde5858db87

Designer: Fix crash
Introduced by a3608f595ad13f8925ad797da8ca7ddec6e36051

Task-number: QTCREATORBUG-11267
Change-Id: I1cb09dd18ed49fcbbfe0d9890d971e7ca4e93ba5
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>

src/plugins/designer/designerxmleditorwidget.cpp
@@ -39,7 +39,7 @@ namespace Internal {
 
 DesignerXmlEditorWidget::DesignerXmlEditorWidget(QDesignerFormWindowInterface *form,
                                                  QWidget *parent) :
-    TextEditor::PlainTextEditorWidget(new FormWindowFile(form, this), parent),
+    TextEditor::PlainTextEditorWidget(new FormWindowFile(form), parent),
     m_designerEditor(new FormWindowEditor(this))
 {
     setReadOnly(true);