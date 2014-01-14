90081329da753de534b4f05d00534738b42a46a1
Eike Ziller <eike.ziller@digia.com>
2014-01-14 09:49:30 +0100

    TextEditors: remove unnecessary cast
    Change-Id: I7c2dc83143aab2c2c0e16736dd26c53c75de024a
    Reviewed-by: Eike Ziller <eike.ziller@digia.com>

--- src/plugins/texteditor/codeassist/basicproposalitem.cpp
+++ src/plugins/texteditor/codeassist/basicproposalitem.cpp
@@ -123,7 +123,7 @@ void BasicProposalItem::applyContextualContent(BaseTextEditor *editor, int baseP
 
 void BasicProposalItem::applySnippet(BaseTextEditor *editor, int basePosition) const
 {
-    BaseTextEditorWidget *editorWidget = static_cast<BaseTextEditorWidget *>(editor->widget());
+    BaseTextEditorWidget *editorWidget = editor->editorWidget();
     QTextCursor tc = editorWidget->textCursor();
     tc.setPosition(basePosition, QTextCursor::KeepAnchor);
     editorWidget->insertCodeSnippet(tc, data().toString());
