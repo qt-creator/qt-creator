qt-creator/qt-creator

commit 3ad38766112a6e0af67ad18c7244e251dc911f19

TextEditors: Move indenter to document.
Change-Id: I8bdf7134562d007aa16cc63a34f5a15a708577ae
Reviewed-by: Eike Ziller <eike.ziller@digia.com>

src/plugins/qmljseditor/qmljseditor.cpp
@@ -476,7 +476,6 @@ void QmlJSTextEditorWidget::ctor()
     setParenthesesMatchingEnabled(true);
     setMarksVisible(true);
     setCodeFoldingSupported(true);
-    setIndenter(new Indenter);
     setAutoCompleter(new AutoCompleter);
     setLanguageSettingsId(QmlJSTools::Constants::QML_JS_SETTINGS_ID);
 

src/plugins/cppeditor/cppeditor.cpp
@@ -514,6 +514,7 @@ Q_GLOBAL_STATIC(CppTools::SymbolFinder, symbolFinder)
 CPPEditorWidget::CPPEditorWidget(QWidget *parent)
     : TextEditor::BaseTextEditorWidget(parent)
 {
+    baseTextDocument()->setIndenter(new CppTools::CppQtStyleIndenter);
     ctor();
 }
 
@@ -539,7 +540,6 @@ void CPPEditorWidget::ctor()
     setParenthesesMatchingEnabled(true);
     setMarksVisible(true);
     setCodeFoldingSupported(true);
-    setIndenter(new CppTools::CppQtStyleIndenter);
     setAutoCompleter(new CppAutoCompleter);
 
     baseTextDocument()->setSyntaxHighlighter(new CppHighlighter);

src/plugins/cppeditor/cppsnippetprovider.cpp
@@ -64,6 +64,6 @@ QString CppSnippetProvider::displayName() const
 void CppSnippetProvider::decorateEditor(TextEditor::SnippetEditorWidget *editor) const
 {
     editor->setSyntaxHighlighter(new CppHighlighter);
-    editor->setIndenter(new CppTools::CppQtStyleIndenter);
+    editor->baseTextDocument()->setIndenter(new CppTools::CppQtStyleIndenter);
     editor->setAutoCompleter(new CppAutoCompleter);
 }

src/plugins/cpptools/cppcodestylesettingspage.cpp
@@ -475,7 +475,7 @@ void CppCodeStylePreferencesWidget::updatePreview()
         QTextCursor tc = preview->textCursor();
         tc.beginEditBlock();
         while (block.isValid()) {
-            preview->indenter()->indentBlock(doc, block, QChar::Null, ts);
+            preview->baseTextDocument()->indenter()->indentBlock(doc, block, QChar::Null, ts);
 
             block = block.next();
         }

src/plugins/fakevim/fakevimplugin.cpp
@@ -1752,7 +1752,7 @@ void FakeVimPluginPrivate::checkForElectricCharacter(bool *result, QChar c)
     if (!handler)
         return;
     if (BaseTextEditorWidget *bt = qobject_cast<BaseTextEditorWidget *>(handler->widget()))
-        *result = bt->indenter()->isElectricCharacter(c);
+        *result = bt->baseTextDocument()->indenter()->isElectricCharacter(c);
 }
 
 void FakeVimPluginPrivate::handleExCommand(bool *handled, const ExCommand &cmd)
@@ -1947,7 +1947,7 @@ void FakeVimPluginPrivate::indentRegion(int beginBlock, int endBlock,
             while (!cursor.atBlockEnd())
                 cursor.deleteChar();
         } else {
-            bt->indenter()->indentBlock(doc, block, typedChar, tabSettings);
+            bt->baseTextDocument()->indenter()->indentBlock(doc, block, typedChar, tabSettings);
         }
         block = block.next();
     }

src/plugins/glsleditor/glsleditor.cpp
@@ -143,6 +143,7 @@ void Document::addRange(const QTextCursor &cursor, GLSL::Scope *scope)
 GLSLTextEditorWidget::GLSLTextEditorWidget(QWidget *parent)
     : TextEditor::BaseTextEditorWidget(parent)
 {
+    baseTextDocument()->setIndenter(new GLSLIndenter());
     ctor();
 }
 
@@ -158,7 +159,6 @@ void GLSLTextEditorWidget::ctor()
     setParenthesesMatchingEnabled(true);
     setMarksVisible(true);
     setCodeFoldingSupported(true);
-    setIndenter(new GLSLIndenter());
     setAutoCompleter(new GLSLCompleter());
 
     m_updateDocumentTimer = new QTimer(this);

src/plugins/pythoneditor/pythoneditorwidget.cpp
@@ -50,6 +50,7 @@ namespace Internal {
 EditorWidget::EditorWidget(QWidget *parent)
     : TextEditor::BaseTextEditorWidget(parent)
 {
+    baseTextDocument()->setIndenter(new PythonIndenter());
     ctor();
 }
 
@@ -69,7 +70,6 @@ void EditorWidget::ctor()
     setMarksVisible(true);
     setCodeFoldingSupported(true);
 
-    setIndenter(new PythonIndenter());
     new PythonHighlighter(baseTextDocument());
 }
 

src/plugins/qmljseditor/qmljssnippetprovider.cpp
@@ -65,6 +65,6 @@ QString QmlJSSnippetProvider::displayName() const
 void QmlJSSnippetProvider::decorateEditor(TextEditor::SnippetEditorWidget *editor) const
 {
     editor->setSyntaxHighlighter(new Highlighter);
-    editor->setIndenter(new Indenter);
+    editor->baseTextDocument()->setIndenter(new Indenter);
     editor->setAutoCompleter(new AutoCompleter);
 }

commit ae5ef38e3c8bb76bbdfac727953d584e1f52303d

QmlJSEditor: Fix auto-indentation
Broke with 3ad38766112a6e0af67ad18c7244e251dc911f19

Change-Id: Id67e574b11e92d83124081f05262110033c8504e
Reviewed-by: Fawzi Mohamed <fawzi.mohamed@digia.com>

src/plugins/qmljseditor/qmljseditor.cpp
@@ -48,7 +48,6 @@
 #include <qmljs/qmljsutils.h>
 
 #include <qmljstools/qmljstoolsconstants.h>
-#include <qmljstools/qmljsindenter.h>
 
 #include <coreplugin/actionmanager/actionmanager.h>
 #include <coreplugin/actionmanager/actioncontainer.h>

src/plugins/qmljseditor/qmljseditordocument.cpp
@@ -32,8 +32,9 @@
 #include "qmljseditordocument_p.h"
 #include "qmljshighlighter.h"
 
-#include <qmljstools/qmljsqtstylecodeformatter.h>
+#include <qmljstools/qmljsindenter.h>
 #include <qmljstools/qmljsmodelmanager.h>
+#include <qmljstools/qmljsqtstylecodeformatter.h>
 
 using namespace QmlJSEditor;
 using namespace QmlJSEditor::Internal;
@@ -74,6 +75,7 @@ QmlJSEditorDocument::QmlJSEditorDocument()
     connect(this, SIGNAL(tabSettingsChanged()),
             m_d, SLOT(invalidateFormatterCache()));
     setSyntaxHighlighter(new Highlighter(document()));
+    setIndenter(new Indenter);
 }
 
 QmlJSEditorDocument::~QmlJSEditorDocument()