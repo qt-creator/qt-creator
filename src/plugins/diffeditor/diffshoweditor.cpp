qt-creator/qt-creator

commit 3fa6b6737c48049cb3d6e06ef0c1cfcb30dfd595

TextEditors: Move font settings to document
Change-Id: I325764cbaa9684fa2758b0ea6c6ea58de142cb73
Reviewed-by: Eike Ziller <eike.ziller@digia.com>

src/plugins/diffeditor/diffshoweditor.cpp
@@ -125,7 +125,7 @@ DiffShowEditor::DiffShowEditor(DiffEditorWidget *editorWidget)
             m_diffShowWidget, SLOT(setFontSettings(TextEditor::FontSettings)));
     m_diffShowWidget->setDisplaySettings(TextEditorSettings::displaySettings());
     m_diffShowWidget->setCodeStyle(TextEditorSettings::codeStyle());
-    m_diffShowWidget->setFontSettings(TextEditorSettings::fontSettings());
+    m_diffShowWidget->baseTextDocument()->setFontSettings(TextEditorSettings::fontSettings());
 }
 
 DiffShowEditor::~DiffShowEditor()

src/plugins/cppeditor/cppeditor.cpp
@@ -1529,13 +1529,14 @@ TextEditor::CompletionAssistProvider *CPPEditor::completionAssistProvider()
     return CppModelManagerInterface::instance()->cppEditorSupport(this)->completionAssistProvider();
 }
 
-void CPPEditorWidget::setFontSettings(const TextEditor::FontSettings &fs)
+void CPPEditorWidget::applyFontSettings()
 {
-    TextEditor::BaseTextEditorWidget::setFontSettings(fs);
+    TextEditor::BaseTextEditorWidget::applyFontSettings();
     TextEditor::SyntaxHighlighter *highlighter = baseTextDocument()->syntaxHighlighter();
     if (!highlighter)
         return;
 
+    const TextEditor::FontSettings &fs = baseTextDocument()->fontSettings();
     m_occurrencesFormat = fs.toTextCharFormat(TextEditor::C_OCCURRENCES);
     m_occurrencesUnusedFormat = fs.toTextCharFormat(TextEditor::C_OCCURRENCES_UNUSED);
     m_occurrencesUnusedFormat.setUnderlineStyle(QTextCharFormat::WaveUnderline);

src/plugins/cppeditor/cppeditor.h
@@ -145,7 +145,6 @@ Q_SIGNALS:
     void outlineModelIndexChanged(const QModelIndex &index);
 
 public Q_SLOTS:
-    virtual void setFontSettings(const TextEditor::FontSettings &);
     void setSortedOutline(bool sort);
     void switchDeclarationDefinition(bool inNextSplit);
     void renameSymbolUnderCursor();
@@ -162,6 +161,7 @@ protected:
     void contextMenuEvent(QContextMenuEvent *);
     void keyPressEvent(QKeyEvent *e);
 
+    void applyFontSettings();
     TextEditor::BaseTextEditor *createEditor();
 
     const CPlusPlus::Macro *findCanonicalMacro(const QTextCursor &cursor,

src/plugins/cpptools/cppcodestylesettingspage.cpp
@@ -497,7 +497,7 @@ void CppCodeStylePreferencesWidget::decorateEditors(const TextEditor::FontSettin
     }
 
     foreach (TextEditor::SnippetEditorWidget *editor, m_previews) {
-        editor->setFontSettings(fontSettings);
+        editor->baseTextDocument()->setFontSettings(fontSettings);
         if (provider)
             provider->decorateEditor(editor);
     }

src/plugins/diffeditor/diffeditorwidget.cpp
@@ -185,7 +185,6 @@ public:
 
 public slots:
     void setDisplaySettings(const DisplaySettings &ds);
-    void setFontSettings(const TextEditor::FontSettings &fs);
 
 signals:
     void jumpToOriginalFileRequested(int diffFileIndex,
@@ -194,6 +193,7 @@ signals:
 
 protected:
     virtual int extraAreaWidth(int *markWidthPtr = 0) const { return BaseTextEditorWidget::extraAreaWidth(markWidthPtr); }
+    void applyFontSettings();
     BaseTextEditor *createEditor() { return new DiffViewEditorEditable(this); }
     virtual QString lineNumber(int blockNumber) const;
     virtual int lineNumberDigits() const;
@@ -356,9 +356,10 @@ void DiffViewEditorWidget::setDisplaySettings(const DisplaySettings &ds)
     BaseTextEditorWidget::setDisplaySettings(settings);
 }
 
-void DiffViewEditorWidget::setFontSettings(const TextEditor::FontSettings &fs)
+void DiffViewEditorWidget::applyFontSettings()
 {
-    BaseTextEditorWidget::setFontSettings(fs);
+    BaseTextEditorWidget::applyFontSettings();
+    const TextEditor::FontSettings &fs = baseTextDocument()->fontSettings();
     m_fileLineForeground = fs.formatFor(C_DIFF_FILE_LINE).foreground();
     m_chunkLineForeground = fs.formatFor(C_DIFF_CONTEXT_LINE).foreground();
     m_textForeground = fs.toTextCharFormat(C_TEXT).foreground().color();
@@ -1578,8 +1579,8 @@ static QTextCharFormat fullWidthFormatForTextStyle(const TextEditor::FontSetting
 
 void DiffEditorWidget::setFontSettings(const TextEditor::FontSettings &fontSettings)
 {
-    m_leftEditor->setFontSettings(fontSettings);
-    m_rightEditor->setFontSettings(fontSettings);
+    m_leftEditor->baseTextDocument()->setFontSettings(fontSettings);
+    m_rightEditor->baseTextDocument()->setFontSettings(fontSettings);
 
     m_fileLineFormat = fullWidthFormatForTextStyle(fontSettings, C_DIFF_FILE_LINE);
     m_chunkLineFormat = fullWidthFormatForTextStyle(fontSettings, C_DIFF_CONTEXT_LINE);

src/plugins/projectexplorer/environmentitemswidget.cpp
@@ -71,7 +71,7 @@ EnvironmentItemsWidget::EnvironmentItemsWidget(QWidget *parent) :
     QWidget(parent), d(new EnvironmentItemsWidgetPrivate)
 {
     d->m_editor = new TextEditor::SnippetEditorWidget(this);
-    d->m_editor->setFontSettings(TextEditor::TextEditorSettings::fontSettings());
+    d->m_editor->baseTextDocument()->setFontSettings(TextEditor::TextEditorSettings::fontSettings());
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addWidget(d->m_editor);
 }

src/plugins/pythoneditor/pythoneditorwidget.cpp
@@ -89,14 +89,14 @@ void EditorWidget::unCommentSelection()
   Handles common IDE fonts&colors settings
   (Tools -> Options -> Text editor -> Fonts and colors)
   */
-void EditorWidget::setFontSettings(const TextEditor::FontSettings &fs)
+void EditorWidget::applyFontSettings()
 {
-    TextEditor::BaseTextEditorWidget::setFontSettings(fs);
+    TextEditor::BaseTextEditorWidget::applyFontSettings();
 
     PythonHighlighter *highlighter =
             qobject_cast<PythonHighlighter *>(baseTextDocument()->syntaxHighlighter());
     if (highlighter)
-        highlighter->setFontSettings(fs);
+        highlighter->setFontSettings(baseTextDocument()->fontSettings());
 }
 
 TextEditor::BaseTextEditor *EditorWidget::createEditor()

src/plugins/pythoneditor/pythoneditorwidget.h
@@ -47,10 +47,8 @@ public:
 
     virtual void unCommentSelection();
 
-public slots:
-    virtual void setFontSettings(const TextEditor::FontSettings &);
-
 protected:
+    void applyFontSettings();
     TextEditor::BaseTextEditor *createEditor();
 
 private:

commit 90e9799f141ebbf504d66a627be1a3c6cc90b713

Fix a warning about no setFontSettings() slot
Caused by change 3fa6b6737c48049cb3d6e06ef0c1cfcb30dfd595

Change-Id: I4b1244423dfea83b1a23f6ecb03026956f14f35d
Reviewed-by: Eike Ziller <eike.ziller@digia.com>

src/plugins/diffeditor/diffshoweditor.cpp
@@ -122,7 +122,7 @@ DiffShowEditor::DiffShowEditor(SideBySideDiffEditorWidget *editorWidget)
     connect(TextEditorSettings::instance(), SIGNAL(displaySettingsChanged(TextEditor::DisplaySettings)),
             m_diffShowWidget, SLOT(setDisplaySettings(TextEditor::DisplaySettings)));
     connect(TextEditorSettings::instance(), SIGNAL(fontSettingsChanged(TextEditor::FontSettings)),
-            m_diffShowWidget, SLOT(setFontSettings(TextEditor::FontSettings)));
+            m_diffShowWidget->baseTextDocument(), SLOT(setFontSettings(TextEditor::FontSettings)));
     m_diffShowWidget->setDisplaySettings(TextEditorSettings::displaySettings());
     m_diffShowWidget->setCodeStyle(TextEditorSettings::codeStyle());
     m_diffShowWidget->baseTextDocument()->setFontSettings(TextEditorSettings::fontSettings());