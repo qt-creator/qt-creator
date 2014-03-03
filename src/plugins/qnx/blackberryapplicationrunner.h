83ad1724f4ee57465b4ac007901d9b3b9fcdd974
Mehdi Fekari <mfekari@blackberry.com>
2014-03-03 09:58:13 +0100

    Qnx: Check runtime version when debugging
    This cannot be done in the "check device" deploy step
    since the deploy configuration does not know which
    run mode will be used, which is mandatory since this check
    is only required with debug mode.
    
    Task-number: QTCREATORBUG-11513
    
    Change-Id: I25121cf61afcf947c3873a6d2767877892ab4afc
    Reviewed-by: Nicolas Arnaud-Cormos <nicolas@kdab.com>

--- src/plugins/qnx/blackberryapplicationrunner.h
+++ src/plugins/qnx/blackberryapplicationrunner.h
@@ -45,12 +45,14 @@
 #include <QDateTime>
 
 namespace QSsh { class SshRemoteProcessRunner; }
+namespace ProjectExplorer { class Target; }
 
 namespace Qnx {
 namespace Internal {
 
 class BlackBerryRunConfiguration;
 class BlackBerryLogProcessRunner;
+class BlackBerryDeviceInformation;
 
 class BlackBerryApplicationRunner : public QObject
 {
@@ -89,12 +91,15 @@ private slots:
     void setApplicationId(const QString &applicationId);
 
     void launchApplication();
+    void checkDeployMode();
     void startLogProcessRunner();
 
     void displayConnectionOutput(Core::Id deviceId, const QString &output);
+    void checkDeviceRuntimeVersion(int status);
 
 private:
     void reset();
+    void queryDeviceInformation();
 
     bool m_cppDebugMode;
 
@@ -113,11 +118,13 @@ private:
     QProcess *m_launchProcess;
     QProcess *m_stopProcess;
     BlackBerryProcessParser m_launchStopProcessParser;
+    BlackBerryDeviceInformation *m_deviceInfo;
 
     BlackBerryLogProcessRunner *m_logProcessRunner;
 
     QTimer *m_runningStateTimer;
     QProcess *m_runningStateProcess;
+    ProjectExplorer::Target *m_target;
 };
 
 } // namespace Internal
