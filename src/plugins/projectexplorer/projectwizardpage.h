qt-creator/qt-creator

commit 5969c01fa625fa41f0b45907c31c411792c4ffee

ProjectExtensionsPage: Rework project combo box
Show a actual tree in the combobox.

Task-number: QTCREATORBUG-12002
Change-Id: I22b62f444923193972109a096bc6eef26a31bf9f
Reviewed-by: Nikolai Kosjar <nikolai.kosjar@digia.com>
Reviewed-by: Daniel Teske <daniel.teske@digia.com>

src/plugins/projectexplorer/projectwizardpage.h
@@ -32,8 +32,15 @@
 
 #include <QWizardPage>
 
+QT_BEGIN_NAMESPACE
+class QTreeView;
+QT_END_NAMESPACE
+
 namespace ProjectExplorer {
+class FolderNode;
 namespace Internal {
+class AddNewModel;
+class AddNewTree;
 
 namespace Ui { class WizardPage; }
 
@@ -46,11 +53,9 @@ public:
     explicit ProjectWizardPage(QWidget *parent = 0);
     virtual ~ProjectWizardPage();
 
-    void setProjects(const QStringList &);
-    void setProjectToolTips(const QStringList &);
-
-    int currentProjectIndex() const;
-    void setCurrentProjectIndex(int);
+    void setModel(AddNewModel *model);
+    void setBestNode(ProjectExplorer::Internal::AddNewTree *tree);
+    FolderNode *currentNode() const;
 
     void setNoneLabel(const QString &label);
     void setAdditionalInfo(const QString &text);
@@ -71,9 +76,11 @@ private slots:
 
 private:
     void setProjectToolTip(const QString &);
+    bool expandTree(const QModelIndex &root);
 
     Ui::WizardPage *m_ui;
     QStringList m_projectToolTips;
+    AddNewModel *m_model;
 };
 
 } // namespace Internal

src/libs/utils/treeviewcombobox.cpp
@@ -57,18 +57,41 @@ TreeViewComboBox::TreeViewComboBox(QWidget *parent)
     m_view->viewport()->installEventFilter(this);
 }
 
+QModelIndex TreeViewComboBox::indexAbove(QModelIndex index)
+{
+    do
+        index = m_view->indexAbove(index);
+    while (index.isValid() && !(model()->flags(index) & Qt::ItemIsSelectable));
+    return index;
+}
+
+QModelIndex TreeViewComboBox::indexBelow(QModelIndex index)
+{
+    do
+        index = m_view->indexBelow(index);
+    while (index.isValid() && !(model()->flags(index) & Qt::ItemIsSelectable));
+    return index;
+}
+
+QModelIndex TreeViewComboBox::lastIndex(const QModelIndex index)
+{
+    if (index.isValid() && !m_view->isExpanded(index))
+        return index;
+
+    int rows = m_view->model()->rowCount(index);
+    if (rows == 0)
+        return index;
+    return lastIndex(m_view->model()->index(rows - 1, 0, index));
+}
+
 void TreeViewComboBox::wheelEvent(QWheelEvent *e)
 {
     QModelIndex index = m_view->currentIndex();
-    if (e->delta() > 0) {
-        do
-            index = m_view->indexAbove(index);
-        while (index.isValid() && !(model()->flags(index) & Qt::ItemIsSelectable));
-    } else if (e->delta() < 0) {
-        do
-            index = m_view->indexBelow(index);
-        while (index.isValid() && !(model()->flags(index) & Qt::ItemIsSelectable));
-    }
+    if (e->delta() > 0)
+        index = indexAbove(index);
+    else if (e->delta() < 0)
+        index = indexBelow(index);
+
     e->accept();
     if (!index.isValid())
         return;
@@ -79,8 +102,34 @@ void TreeViewComboBox::wheelEvent(QWheelEvent *e)
     emit activated(index.row());
 }
 
+void TreeViewComboBox::keyPressEvent(QKeyEvent *e)
+{
+    if (e->key() == Qt::Key_Up || e->key() == Qt::Key_PageUp) {
+        setCurrentIndex(indexAbove(m_view->currentIndex()));
+    } else if (e->key() == Qt::Key_Down || e->key() == Qt::Key_PageDown) {
+        setCurrentIndex(indexBelow(m_view->currentIndex()));
+    } else if (e->key() == Qt::Key_Home) {
+        QModelIndex index = m_view->model()->index(0, 0);
+        if (index.isValid() && !model()->flags(index) & Qt::ItemIsSelectable)
+            index = indexBelow(index);
+        setCurrentIndex(index);
+    } else if (e->key() == Qt::Key_End) {
+        QModelIndex index = lastIndex(m_view->rootIndex());
+        if (index.isValid() && !model()->flags(index) & Qt::ItemIsSelectable)
+            index = indexAbove(index);
+        setCurrentIndex(index);
+    } else {
+        QComboBox::keyPressEvent(e);
+        return;
+    }
+
+    e->accept();
+}
+
 void TreeViewComboBox::setCurrentIndex(const QModelIndex &index)
 {
+    if (!index.isValid())
+        return;
     setRootModelIndex(model()->parent(index));
     QComboBox::setCurrentIndex(index.row());
     setRootModelIndex(QModelIndex());

src/plugins/projectexplorer/addnewmodel.cpp
@@ -1,0 +1,213 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of Qt Creator.
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+****************************************************************************/
+
+#include "addnewmodel.h"
+
+#include "projectexplorer.h"
+
+using namespace ProjectExplorer;
+using namespace ProjectExplorer::Internal;
+
+AddNewTree::AddNewTree(const QString &displayName)
+    : m_parent(0),
+      m_children(QList<AddNewTree *>()),
+      m_displayName(displayName),
+      m_node(0),
+      m_canAdd(true),
+      m_priority(-1)
+{
+
+}
+
+AddNewTree::AddNewTree(FolderNode *node, QList<AddNewTree *> children, const QString &displayName)
+    : m_parent(0),
+      m_children(children),
+      m_displayName(displayName),
+      m_node(0),
+      m_canAdd(false),
+      m_priority(-1)
+{
+    if (node)
+        m_toolTip = ProjectExplorerPlugin::directoryFor(node);
+    foreach (AddNewTree *child, m_children)
+        child->m_parent = this;
+}
+
+AddNewTree::AddNewTree(FolderNode *node, QList<AddNewTree *> children, const FolderNode::AddNewInformation &info)
+    : m_parent(0),
+      m_children(children),
+      m_displayName(info.displayName),
+      m_node(node),
+      m_canAdd(true),
+      m_priority(info.priority)
+{
+    if (node)
+        m_toolTip = ProjectExplorerPlugin::directoryFor(node);
+    foreach (AddNewTree *child, m_children)
+        child->m_parent = this;
+}
+
+AddNewTree::~AddNewTree()
+{
+    qDeleteAll(m_children);
+}
+
+AddNewTree *AddNewTree::parent() const
+{
+    return m_parent;
+}
+
+QList<AddNewTree *> AddNewTree::children() const
+{
+    return m_children;
+}
+
+bool AddNewTree::canAdd() const
+{
+    return m_canAdd;
+}
+
+QString AddNewTree::displayName() const
+{
+    return m_displayName;
+}
+
+QString AddNewTree::toolTip() const
+{
+    return m_toolTip;
+}
+
+FolderNode *AddNewTree::node() const
+{
+    return m_node;
+}
+
+int AddNewTree::priority() const
+{
+    return m_priority;
+}
+
+AddNewModel::AddNewModel(AddNewTree *root)
+    : m_root(root)
+{
+
+}
+
+AddNewModel::~AddNewModel()
+{
+    delete m_root;
+}
+
+int AddNewModel::rowCount(const QModelIndex &parent) const
+{
+    if (!parent.isValid())
+        return m_root->children().size();
+    AddNewTree *tree = static_cast<AddNewTree *>(parent.internalPointer());
+    return tree->children().size();
+}
+
+int AddNewModel::columnCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+    return 1;
+}
+
+QModelIndex AddNewModel::index(int row, int column, const QModelIndex &parent) const
+{
+    if (column != 0)
+        return QModelIndex();
+    if (!parent.isValid()) {
+        if (row >= 0 && row < m_root->children().size())
+            return createIndex(row, column, m_root->children().at(row));
+        return QModelIndex();
+    }
+    AddNewTree *tree = static_cast<AddNewTree *>(parent.internalPointer());
+    if (row >= 0 && row < tree->children().size())
+        return createIndex(row, column, tree->children().at(row));
+    return QModelIndex();
+}
+
+QModelIndex AddNewModel::parent(const QModelIndex &child) const
+{
+    if (!child.isValid())
+        return QModelIndex();
+    AddNewTree *childTree = static_cast<AddNewTree *>(child.internalPointer());
+    if (childTree == m_root)
+        return QModelIndex();
+    AddNewTree *parent = childTree->parent();
+    if (parent == m_root)
+        return QModelIndex();
+    AddNewTree *grandparent = parent->parent();
+    for (int i = 0; i < grandparent->children().size(); ++i) {
+        if (grandparent->children().at(i) == parent)
+            return createIndex(i, 0, parent);
+    }
+    return QModelIndex();
+}
+
+QVariant AddNewModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid())
+        return QVariant();
+    AddNewTree *tree = static_cast<AddNewTree *>(index.internalPointer());
+    if (role == Qt::DisplayRole)
+        return tree->displayName();
+    else if (role == Qt::ToolTipRole)
+        return tree->toolTip();
+    return QVariant();
+}
+
+Qt::ItemFlags AddNewModel::flags(const QModelIndex &index) const
+{
+    AddNewTree *tree = static_cast<AddNewTree *>(index.internalPointer());
+    if (tree && tree->canAdd())
+        return Qt::ItemIsSelectable | Qt::ItemIsEnabled;
+    return Qt::NoItemFlags;
+}
+
+FolderNode *AddNewModel::nodeForIndex(const QModelIndex &index) const
+{
+    if (!index.isValid())
+        return m_root->node();
+    AddNewTree *tree = static_cast<AddNewTree *>(index.internalPointer());
+    return tree->node();
+}
+
+QModelIndex AddNewModel::indexForTree(AddNewTree *tree) const
+{
+    if (!tree)
+        return index(0, 0, QModelIndex());
+    AddNewTree *parent = tree->parent();
+    if (!parent)
+        return QModelIndex();
+    for (int i = 0; i < parent->children().size(); ++i)
+        if (parent->children().at(i) == tree)
+            return createIndex(i, 0, tree);
+    return QModelIndex();
+}

src/plugins/projectexplorer/addnewmodel.h
@@ -27,47 +27,62 @@
 **
 ****************************************************************************/
 
-#ifndef TREEVIEWCOMBOBOX_H
-#define TREEVIEWCOMBOBOX_H
+#ifndef ADDNEWMODEL_H
+#define ADDNEWMODEL_H
 
-#include "utils_global.h"
+#include "projectnodes.h"
 
-#include <QComboBox>
-#include <QTreeView>
+#include <QAbstractItemModel>
 
-namespace Utils {
+namespace ProjectExplorer {
+class FolderNode;
 
-class QTCREATOR_UTILS_EXPORT TreeViewComboBoxView : public QTreeView
+namespace Internal {
+
+class AddNewTree
 {
-    Q_OBJECT
 public:
-    TreeViewComboBoxView(QWidget *parent = 0);
-    void adjustWidth(int width);
+    AddNewTree(const QString &displayName);
+    AddNewTree(FolderNode *node, QList<AddNewTree *> children, const QString &displayName);
+    AddNewTree(FolderNode *node, QList<AddNewTree *> children, const FolderNode::AddNewInformation &info);
+    ~AddNewTree();
+
+    AddNewTree *parent() const;
+    QList<AddNewTree *> children() const;
+
+    bool canAdd() const;
+    QString displayName() const;
+    QString toolTip() const;
+    FolderNode *node() const;
+    int priority() const;
+private:
+    AddNewTree *m_parent;
+    QList<AddNewTree *> m_children;
+    QString m_displayName;
+    QString m_toolTip;
+    FolderNode *m_node;
+    bool m_canAdd;
+    int m_priority;
 };
 
-class QTCREATOR_UTILS_EXPORT TreeViewComboBox : public QComboBox
+class AddNewModel : public QAbstractItemModel
 {
-    Q_OBJECT
 public:
-    TreeViewComboBox(QWidget *parent = 0);
+    AddNewModel(AddNewTree *root);
+    ~AddNewModel();
+    int rowCount(const QModelIndex &parent) const;
+    int columnCount(const QModelIndex &parent) const;
+    QModelIndex index(int row, int column, const QModelIndex &parent) const;
+    QModelIndex parent(const QModelIndex &child) const;
+    QVariant data(const QModelIndex &index, int role) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
 
-    void wheelEvent(QWheelEvent *e);
-    void keyPressEvent(QKeyEvent *e);
-    void setCurrentIndex(const QModelIndex &index);
-    bool eventFilter(QObject* object, QEvent* event);
-    void showPopup();
-    void hidePopup();
-
-    TreeViewComboBoxView *view() const;
-
+    FolderNode *nodeForIndex(const QModelIndex &index) const;
+    QModelIndex indexForTree(AddNewTree *tree) const;
 private:
-    QModelIndex indexBelow(QModelIndex index);
-    QModelIndex indexAbove(QModelIndex index);
-    QModelIndex lastIndex(const QModelIndex index);
-
-    TreeViewComboBoxView *m_view;
-    bool m_skipNextHide;
+    AddNewTree *m_root;
 };
 }
+}
 
-#endif // TREEVIEWCOMBOBOX_H
+#endif // ADDNEWMODEL_H

src/plugins/projectexplorer/projectexplorer.pro
@@ -145,7 +145,8 @@ HEADERS += projectexplorer.h \
     customparser.h \
     customparserconfigdialog.h \
     ipotentialkit.h \
-    selectablefilesmodel.h
+    selectablefilesmodel.h \
+    addnewmodel.h
 
 SOURCES += projectexplorer.cpp \
     abi.cpp \
@@ -277,7 +278,8 @@ SOURCES += projectexplorer.cpp \
     customparser.cpp \
     customparserconfigdialog.cpp \
     ipotentialkit.cpp \
-    selectablefilesmodel.cpp
+    selectablefilesmodel.cpp \
+    addnewmodel.cpp
 
 FORMS += processstep.ui \
     editorsettingspropertiespage.ui \

src/plugins/projectexplorer/projectfilewizardextension.h
@@ -35,6 +35,7 @@
 #include <coreplugin/ifilewizardextension.h>
 
 namespace ProjectExplorer {
+class FolderNode;
 
 namespace Internal {
 
@@ -52,14 +53,11 @@ public:
                  bool *removeOpenProjectAttribute, QString *errorMessage);
     void applyCodeStyle(Core::GeneratedFile *file) const;
 
-    void setProjectIndex(int i);
-
 public slots:
     void firstExtensionPageShown(const QList<Core::GeneratedFile> &files, const QVariantMap &extraValues);
     void initializeVersionControlChoices();
 
 private:
-    void initProjectChoices(const QList<Core::GeneratedFile> &generatedFiles, const QVariantMap &extraValues);
     bool processProject(const QList<Core::GeneratedFile> &files,
                         bool *removeOpenProjectAttribute, QString *errorMessage);
     bool processVersionControl(const QList<Core::GeneratedFile> &files, QString *errorMessage);

src/plugins/projectexplorer/projectwizardpage.cpp
@@ -27,6 +27,7 @@
 **
 ****************************************************************************/
 
+#include "addnewmodel.h"
 #include "projectwizardpage.h"
 #include "ui_projectwizardpage.h"
 
@@ -36,6 +37,7 @@
 
 #include <QDir>
 #include <QTextStream>
+#include <QTreeView>
 
 /*!
     \class ProjectExplorer::Internal::ProjectWizardPage
@@ -51,7 +53,8 @@ using namespace Internal;
 
 ProjectWizardPage::ProjectWizardPage(QWidget *parent) :
     QWizardPage(parent),
-    m_ui(new Ui::WizardPage)
+    m_ui(new Ui::WizardPage),
+    m_model(0)
 {
     m_ui->setupUi(this);
     m_ui->vcsManageButton->setText(Core::ICore::msgShowOptionsDialog());
@@ -64,19 +67,64 @@ ProjectWizardPage::ProjectWizardPage(QWidget *parent) :
 ProjectWizardPage::~ProjectWizardPage()
 {
     delete m_ui;
+    delete m_model;
 }
 
-void ProjectWizardPage::setProjects(const QStringList &p)
+void ProjectWizardPage::setModel(AddNewModel *model)
 {
-    m_ui->projectComboBox->clear();
-    m_ui->projectComboBox->addItems(p);
-    m_ui->projectComboBox->setEnabled(p.size() > 1);
-    m_ui->projectLabel->setEnabled(p.size() > 1);
+    delete m_model;
+    m_model = model;
+
+    // TODO see OverViewCombo and OverView for click event filter
+    m_ui->projectComboBox->setModel(model);
+    bool enabled = m_model->rowCount(QModelIndex()) > 1;
+    m_ui->projectComboBox->setEnabled(enabled);
+
+    expandTree(QModelIndex());
 }
 
-void ProjectWizardPage::setProjectToolTips(const QStringList &t)
+bool ProjectWizardPage::expandTree(const QModelIndex &root)
 {
-    m_projectToolTips = t;
+    bool expand = false;
+    if (!root.isValid()) // always expand root
+        expand = true;
+
+    // Check children
+    int count = m_model->rowCount(root);
+    for (int i = 0; i < count; ++i) {
+        if (expandTree(m_model->index(i, 0, root)))
+            expand = true;
+    }
+
+    // Apply to self
+    if (expand)
+        m_ui->projectComboBox->view()->expand(root);
+    else
+        m_ui->projectComboBox->view()->collapse(root);
+
+    // if we are a high priority node, our *parent* needs to be expanded
+    AddNewTree *tree = static_cast<AddNewTree *>(root.internalPointer());
+    if (tree && tree->priority() >= 100)
+        expand = true;
+
+    return expand;
+}
+
+void ProjectWizardPage::setBestNode(AddNewTree *tree)
+{
+    QModelIndex index = m_model->indexForTree(tree);
+    m_ui->projectComboBox->setCurrentIndex(index);
+
+    while (index.isValid()) {
+        m_ui->projectComboBox->view()->expand(index);
+        index = index.parent();
+    }
+}
+
+FolderNode *ProjectWizardPage::currentNode() const
+{
+    QModelIndex index = m_ui->projectComboBox->view()->currentIndex();
+    return m_model->nodeForIndex(index);
 }
 
 void ProjectWizardPage::setAddingSubProject(bool addingSubProject)
@@ -84,16 +132,6 @@ void ProjectWizardPage::setAddingSubProject(bool addingSubProject)
     m_ui->projectLabel->setText(addingSubProject ?
                                     tr("Add as a subproject to project:")
                                   : tr("Add to &project:"));
-}
-
-int ProjectWizardPage::currentProjectIndex() const
-{
-    return m_ui->projectComboBox->currentIndex();
-}
-
-void ProjectWizardPage::setCurrentProjectIndex(int idx)
-{
-    m_ui->projectComboBox->setCurrentIndex(idx);
 }
 
 void ProjectWizardPage::setNoneLabel(const QString &label)

commit 7c76854b756dc61ffb67ad1ec3c776367dc5fd8b

ProjectExplorer: Fix Qt4 compile
Error introduced with 5969c01fa625fa41f0b45907c31c411792c4ffee.

Change-Id: If7ef7959f9d55ffdab9b5f60fbc1fb1ddf75a119
Reviewed-by: Friedemann Kleint <Friedemann.Kleint@digia.com>

src/plugins/projectexplorer/projectwizardpage.h
@@ -34,6 +34,7 @@
 
 QT_BEGIN_NAMESPACE
 class QTreeView;
+class QModelIndex;
 QT_END_NAMESPACE
 
 namespace ProjectExplorer {