qt-creator/qt-creator

commit 40052046fdb2d5b5db4a6f2cd1c8ee36f2703f14

Debugger: Rework display length limitation systems
There are two values now, one to limit an entry in the L&E view
(default 100) and a hard upper limit (at 1 mio).

If displayed values are elided, the true length is shown in addition.

Change-Id: I180b70446c18e258c164e5af75b88d4c8b6c53f2
Reviewed-by: hjk <hjk121@nokiamail.com>

share/qtcreator/debugger/dumper.py
@@ -71,6 +71,19 @@ except:
         return "Normal"
 
 
+class ReportItem:
+    """
+    Helper structure to keep temporary "best" information about a value
+    or a type scheduled to be reported. This might get overridden be
+    subsequent better guesses during a putItem() run.
+    """
+    def __init__(self):
+        self.value = None
+        self.priority = -100
+        self.encoding = None
+        self.elided = 0
+
+
 class Blob(object):
     """
     Helper structure to keep a blob of bytes, possibly
@@ -310,6 +323,7 @@ class DumperBase:
         # Later set, or not set:
         # cachedQtVersion
         self.stringCutOff = 10000
+        self.displayStringLimit = 100
 
         # This is a cache mapping from 'type name' to 'display alternatives'.
         self.qqFormats = {}
@@ -375,12 +389,11 @@ class DumperBase:
         # assume no Qt 3 support by default
         return False
 
+    # Clamps size to limit.
     def computeLimit(self, size, limit):
-        if limit is None:
-            return size
-        if limit == 0:
-            return min(size, self.stringCutOff)
-        return min(size, limit)
+        if limit is None or size <= limit:
+            return 0, size
+        return size, limit
 
     def vectorDataHelper(self, addr):
         if self.qtVersion() >= 0x050000:
@@ -419,53 +432,50 @@ class DumperBase:
         return data, size, alloc
 
     # addr is the begin of a QByteArrayData structure
-    def encodeStringHelper(self, addr, limit = 0):
+    def encodeStringHelper(self, addr, limit):
         # Should not happen, but we get it with LLDB as result
         # of inferior calls
         if addr == 0:
-            return ""
+            return 0, ""
         data, size, alloc = self.byteArrayDataHelper(addr)
         if alloc != 0:
             self.check(0 <= size and size <= alloc and alloc <= 100*1000*1000)
-        limit = self.computeLimit(size, limit)
-        s = self.readMemory(data, 2 * limit)
-        if limit < size:
-            s += "2e002e002e00"
-        return s
+        elided, shown = self.computeLimit(size, limit)
+        return elided, self.readMemory(data, 2 * shown)
 
-    def encodeByteArrayHelper(self, addr, limit = None):
+    def encodeByteArrayHelper(self, addr, limit):
         data, size, alloc = self.byteArrayDataHelper(addr)
         if alloc != 0:
             self.check(0 <= size and size <= alloc and alloc <= 100*1000*1000)
-        limit = self.computeLimit(size, limit)
-        s = self.readMemory(data, limit)
-        if limit < size:
-            s += "2e2e2e"
-        return s
+        elided, shown = self.computeLimit(size, limit)
+        return elided, self.readMemory(data, shown)
 
     def readMemory(self, addr, size):
         data = self.extractBlob(addr, size).toBytes()
         return self.hexencode(data)
 
     def encodeByteArray(self, value, limit = 0):
-        return self.encodeByteArrayHelper(self.extractPointer(value), limit)
+        elided, data = self.encodeByteArrayHelper(self.extractPointer(value), limit)
+        return data
 
     def byteArrayData(self, value):
         return self.byteArrayDataHelper(self.extractPointer(value))
 
     def putByteArrayValue(self, value):
-        return self.putValue(self.encodeByteArray(value, self.stringCutOff), Hex2EncodedLatin1)
+        elided, data = self.encodeByteArrayHelper(self.extractPointer(value), self.displayStringLimit)
+        self.putValue(data, Hex2EncodedLatin1, elided=elided)
 
     def putByteArrayValueByAddress(self, addr):
-        self.putValue(self.encodeByteArrayHelper(self.extractPointer(addr)),
-            Hex2EncodedLatin1)
+        elided, data = self.encodeByteArrayHelper(addr, self.displayStringLimit)
+        self.putValue(data, Hex2EncodedLatin1, elided=elided)
 
     def putStringValueByAddress(self, addr):
-        self.putValue(self.encodeStringHelper(self.extractPointer(addr)),
-            Hex4EncodedLittleEndian)
+        elided, data = self.encodeStringHelper(self.extractPointer(addr), self.displayStringLimit)
+        self.putValue(data, Hex4EncodedLittleEndian, elided=elided)
 
     def encodeString(self, value, limit = 0):
-        return self.encodeStringHelper(self.extractPointer(value), limit)
+        elided, data = self.encodeStringHelper(self.extractPointer(value), limit)
+        return data
 
     def stringData(self, value):
         return self.byteArrayDataHelper(self.extractPointer(value))
@@ -499,7 +509,8 @@ class DumperBase:
         return inner.strip()
 
     def putStringValue(self, value):
-        return self.putValue(self.encodeString(value, self.stringCutOff), Hex4EncodedLittleEndian)
+        elided, data = self.encodeStringHelper(self.extractPointer(value), self.displayStringLimit)
+        self.putValue(data, Hex4EncodedLittleEndian, elided=elided)
 
     def putAddressItem(self, name, value, type = ""):
         with SubItem(self, name):
@@ -619,27 +630,16 @@ class DumperBase:
     def findFirstZero(self, p, maximum):
         for i in xrange(maximum):
             if int(p.dereference()) == 0:
-                return i
+                return 0, i
             p = p + 1
-        return maximum + 1
+        # Real end is unknown.
+        return -1, maximum
 
-    def encodeCArray(self, p, innerType, suffix):
+    def encodeCArray(self, p, innerType, limit):
         t = self.lookupType(innerType)
         p = p.cast(t.pointer())
-        limit = self.findFirstZero(p, self.stringCutOff)
-        s = self.readMemory(p, limit * t.sizeof)
-        if limit > self.stringCutOff:
-            s += suffix
-        return s
-
-    def encodeCharArray(self, p):
-        return self.encodeCArray(p, "unsigned char", "2e2e2e")
-
-    def encodeChar2Array(self, p):
-        return self.encodeCArray(p, "unsigned short", "2e002e002e00")
-
-    def encodeChar4Array(self, p):
-        return self.encodeCArray(p, "unsigned int", "2e0000002e0000002e000000")
+        elided, shown = self.findFirstZero(p, limit)
+        return elided, self.readMemory(p, shown * t.sizeof)
 
     def putItemCount(self, count, maximum = 1000000000):
         # This needs to override the default value, so don't use 'put' directly.
@@ -654,25 +654,33 @@ class DumperBase:
 
     def putType(self, type, priority = 0):
         # Higher priority values override lower ones.
-        if priority >= self.currentTypePriority:
-            self.currentType = str(type)
-            self.currentTypePriority = priority
+        if priority >= self.currentType.priority:
+            self.currentType.value = str(type)
+            self.currentType.priority = priority
 
-    def putValue(self, value, encoding = None, priority = 0):
+    def putValue(self, value, encoding = None, priority = 0, elided = None):
         # Higher priority values override lower ones.
-        if priority >= self.currentValuePriority:
-            self.currentValue = value
-            self.currentValuePriority = priority
-            self.currentValueEncoding = encoding
+        # elided = 0 indicates all data is available in value,
+        # otherwise it's the true length.
+        if priority >= self.currentValue.priority:
+            self.currentValue.value = value
+            self.currentValue.priority = priority
+            self.currentValue.encoding = encoding
+            self.currentValue.elided = elided
 
     def putEmptyValue(self, priority = -10):
-        if priority >= self.currentValuePriority:
-            self.currentValue = ""
-            self.currentValuePriority = priority
-            self.currentValueEncoding = None
+        if priority >= self.currentValue.priority:
+            self.currentValue.value = ""
+            self.currentValue.priority = priority
+            self.currentValue.encoding = None
+            self.currentValue.elided = None
 
     def putName(self, name):
         self.put('name="%s",' % name)
+
+    def putBetterType(self, type):
+        self.currentType.value = str(type)
+        self.currentType.priority += 1
 
     def putNoType(self):
         # FIXME: replace with something that does not need special handling
@@ -682,7 +690,7 @@ class DumperBase:
     def putInaccessible(self):
         #self.putBetterType(" ")
         self.putNumChild(0)
-        self.currentValue = None
+        self.currentValue.value = None
 
     def putNamedSubItem(self, component, value, name):
         with SubItem(self, component):
@@ -805,7 +813,8 @@ class DumperBase:
         if format == None and innerTypeName == "char":
             # Use Latin1 as default for char *.
             self.putType(typeName)
-            self.putValue(self.encodeCharArray(value), Hex2EncodedLatin1)
+            (elided, data) = self.encodeCArray(value, "unsigned char", self.displayStringLimit)
+            self.putValue(data, Hex2EncodedLatin1, elided=elided)
             self.putNumChild(0)
             return
 
@@ -823,35 +832,40 @@ class DumperBase:
         if format == Latin1StringFormat:
             # Explicitly requested Latin1 formatting.
             self.putType(typeName)
-            self.putValue(self.encodeCharArray(value), Hex2EncodedLatin1)
+            (elided, data) = self.encodeCArray(value, "unsigned char", self.displayStringLimit)
+            self.putValue(data, Hex2EncodedLatin1, elided=elided)
             self.putNumChild(0)
             return
 
         if format == Utf8StringFormat:
             # Explicitly requested UTF-8 formatting.
             self.putType(typeName)
-            self.putValue(self.encodeCharArray(value), Hex2EncodedUtf8)
+            (elided, data) = self.encodeCArray(value, "unsigned char", self.displayStringLimit)
+            self.putValue(data, Hex2EncodedUtf8, elided=elided)
             self.putNumChild(0)
             return
 
         if format == Local8BitStringFormat:
             # Explicitly requested local 8 bit formatting.
             self.putType(typeName)
-            self.putValue(self.encodeCharArray(value), Hex2EncodedLocal8Bit)
+            (elided, data) = self.encodeCArray(value, "unsigned char", self.displayStringLimit)
+            self.putValue(data, Hex2EncodedLocal8Bit, elided=elided)
             self.putNumChild(0)
             return
 
         if format == Utf16StringFormat:
             # Explicitly requested UTF-16 formatting.
             self.putType(typeName)
-            self.putValue(self.encodeChar2Array(value), Hex4EncodedLittleEndian)
+            (elided, data) = self.encodeCArray(value, "unsigned short", self.displayStringLimit)
+            self.putValue(data, Hex4EncodedLittleEndian, elided=elided)
             self.putNumChild(0)
             return
 
         if format == Ucs4StringFormat:
             # Explicitly requested UCS-4 formatting.
             self.putType(typeName)
-            self.putValue(self.encodeChar4Array(value), Hex8EncodedLittleEndian)
+            (elided, data) = self.encodeCArray(value, "unsigned int", self.displayStringLimit)
+            self.putValue(data, Hex8EncodedLittleEndian, elided=elided)
             self.putNumChild(0)
             return
 
@@ -1226,7 +1240,7 @@ class DumperBase:
         format = self.formats.get(self.currentIName)
         if format is None:
             if type is None:
-                type = self.currentType
+                type = self.currentType.value
             needle = self.stripForFormat(str(type))
             format = self.typeformats.get(needle)
         return format
@@ -1256,8 +1270,8 @@ class DumperBase:
         if not hasPlot():
             return
         if not self.isSimpleType(typeobj):
-            #self.putValue(self.currentValue + " (not plottable)")
-            self.putValue(self.currentValue)
+            #self.putValue(self.currentValue.value + " (not plottable)")
+            self.putValue(self.currentValue.value)
             self.putField("plottable", "0")
             return
         global gnuplotPipe
@@ -1435,8 +1449,8 @@ class DumperBase:
                     value = self.parseAndEvaluate(exp)
                     self.putItem(value)
                 except RuntimeError:
-                    self.currentType = " "
-                    self.currentValue = "<no such value>"
+                    self.currentType.value = " "
+                    self.currentValue.value = "<no such value>"
                     self.currentChildNumChild = -1
                     self.currentNumChild = 0
                     self.putNumChild(0)

share/qtcreator/debugger/gdbbridge.py
@@ -367,11 +367,8 @@ class Dumper(DumperBase):
         self.currentChildNumChild = -1
         self.currentMaxNumChild = -1
         self.currentNumChild = -1
-        self.currentValue = None
-        self.currentValuePriority = -100
-        self.currentValueEncoding = None
-        self.currentType = None
-        self.currentTypePriority = -100
+        self.currentValue = ReportItem()
+        self.currentType = ReportItem()
         self.currentAddress = None
         self.typeformats = {}
         self.formats = {}
@@ -402,6 +399,8 @@ class Dumper(DumperBase):
                 self.expandedINames = set(arg[pos:].split(","))
             elif arg.startswith("stringcutoff:"):
                 self.stringCutOff = int(arg[pos:])
+            elif arg.startswith("displaystringlimit:"):
+                self.displayStringLimit = int(arg[pos:])
             elif arg.startswith("typeformats:"):
                 for f in arg[pos:].split(","):
                     pos = f.find("=")
@@ -525,39 +524,39 @@ class Dumper(DumperBase):
             self.put('name="%s",' % item.name)
         item.savedIName = self.currentIName
         item.savedValue = self.currentValue
-        item.savedValuePriority = self.currentValuePriority
-        item.savedValueEncoding = self.currentValueEncoding
         item.savedType = self.currentType
-        item.savedTypePriority = self.currentTypePriority
         item.savedCurrentAddress = self.currentAddress
         self.currentIName = item.iname
-        self.currentValuePriority = -100
-        self.currentValueEncoding = None
-        self.currentType = ""
-        self.currentTypePriority = -100
+        self.currentValue = ReportItem();
+        self.currentType = ReportItem();
         self.currentAddress = None
 
     def exitSubItem(self, item, exType, exValue, exTraceBack):
-        #warn(" CURRENT VALUE: %s %s %s" % (self.currentValue,
-        #    self.currentValueEncoding, self.currentValuePriority))
+        #warn("CURRENT VALUE: %s: %s %s %s %s" % (
+        #    self.currentIName,
+        #    self.currentValue.value,
+        #    self.currentValue.elided,
+        #    self.currentValue.encoding,
+        #    self.currentValue.priority))
         if not exType is None:
             if self.passExceptions:
                 showException("SUBITEM", exType, exValue, exTraceBack)
             self.putNumChild(0)
             self.putValue("<not accessible>")
         try:
-            #warn("TYPE VALUE: %s" % self.currentValue)
-            typeName = stripClassTag(self.currentType)
-            #warn("TYPE: '%s'  DEFAULT: '%s' % (typeName, self.currentChildType))
+            #warn("CURRENT TYPE: %s" % self.currentType.value)
+            typeName = stripClassTag(self.currentType.value)
 
             if len(typeName) > 0 and typeName != self.currentChildType:
                 self.put('type="%s",' % typeName) # str(type.unqualified()) ?
-            if  self.currentValue is None:
+            if  self.currentValue.value is None:
                 self.put('value="<not accessible>",numchild="0",')
             else:
-                if not self.currentValueEncoding is None:
-                    self.put('valueencoded="%d",' % self.currentValueEncoding)
-                self.put('value="%s",' % self.currentValue)
+                if not self.currentValue.encoding is None:
+                    self.put('valueencoded="%d",' % self.currentValue.encoding)
+                if self.currentValue.elided:
+                    self.put('valueelided="%d",' % self.currentValue.elided)
+                self.put('value="%s",' % self.currentValue.value)
         except:
             pass
         if not self.currentAddress is None:
@@ -565,10 +564,7 @@ class Dumper(DumperBase):
         self.put('},')
         self.currentIName = item.savedIName
         self.currentValue = item.savedValue
-        self.currentValuePriority = item.savedValuePriority
-        self.currentValueEncoding = item.savedValueEncoding
         self.currentType = item.savedType
-        self.currentTypePriority = item.savedTypePriority
         self.currentAddress = item.savedCurrentAddress
         return True
 
@@ -887,10 +883,6 @@ class Dumper(DumperBase):
         # Memoize good results.
         self.isQt3Support = lambda: self.cachedIsQt3Suport
         return self.cachedIsQt3Suport
-
-    def putBetterType(self, type):
-        self.currentType = str(type)
-        self.currentTypePriority = self.currentTypePriority + 1
 
     def putAddress(self, addr):
         if self.currentPrintsAddress:

commit 3ae6c09714e502eb9100d8bbc810fa35309d61f8

Debugger: Regression cleanup
Sneaked in with 40052046fdb2d5.

Change-Id: I711c5b8e6869647a447a5f17f693de571b5f0337
Reviewed-by: Christian Stenger <christian.stenger@digia.com>

share/qtcreator/debugger/dumper.py
@@ -77,11 +77,15 @@ class ReportItem:
     or a type scheduled to be reported. This might get overridden be
     subsequent better guesses during a putItem() run.
     """
-    def __init__(self):
-        self.value = None
-        self.priority = -100
-        self.encoding = None
-        self.elided = 0
+    def __init__(self, value = None, encoding = None, priority = -100, elided = None):
+        self.value = value
+        self.priority = priority
+        self.encoding = encoding
+        self.elided = elided
+
+    def __str__(self):
+        return "Item(value: %s, encoding: %s, priority: %s, elided: %s)" \
+            % (self.value, self.encoding, self.priority, self.elided)
 
 
 class Blob(object):
@@ -391,6 +395,8 @@ class DumperBase:
 
     # Clamps size to limit.
     def computeLimit(self, size, limit):
+        if limit == 0:
+            limit = self.displayStringLimit
         if limit is None or size <= limit:
             return 0, size
         return size, limit
@@ -461,17 +467,13 @@ class DumperBase:
     def byteArrayData(self, value):
         return self.byteArrayDataHelper(self.extractPointer(value))
 
-    def putByteArrayValue(self, value):
-        elided, data = self.encodeByteArrayHelper(self.extractPointer(value), self.displayStringLimit)
-        self.putValue(data, Hex2EncodedLatin1, elided=elided)
-
     def putByteArrayValueByAddress(self, addr):
         elided, data = self.encodeByteArrayHelper(addr, self.displayStringLimit)
         self.putValue(data, Hex2EncodedLatin1, elided=elided)
 
-    def putStringValueByAddress(self, addr):
-        elided, data = self.encodeStringHelper(self.extractPointer(addr), self.displayStringLimit)
-        self.putValue(data, Hex4EncodedLittleEndian, elided=elided)
+    def putByteArrayValue(self, value):
+        elided, data = self.encodeByteArrayHelper(self.extractPointer(value), self.displayStringLimit)
+        self.putValue(data, Hex2EncodedLatin1, elided=elided)
 
     def encodeString(self, value, limit = 0):
         elided, data = self.encodeStringHelper(self.extractPointer(value), limit)
@@ -507,6 +509,10 @@ class DumperBase:
                     inner += c
                     skipSpace = False
         return inner.strip()
+
+    def putStringValueByAddress(self, addr):
+        elided, data = self.encodeStringHelper(addr, self.displayStringLimit)
+        self.putValue(data, Hex4EncodedLittleEndian, elided=elided)
 
     def putStringValue(self, value):
         elided, data = self.encodeStringHelper(self.extractPointer(value), self.displayStringLimit)
@@ -663,23 +669,20 @@ class DumperBase:
         # elided = 0 indicates all data is available in value,
         # otherwise it's the true length.
         if priority >= self.currentValue.priority:
-            self.currentValue.value = value
-            self.currentValue.priority = priority
-            self.currentValue.encoding = encoding
-            self.currentValue.elided = elided
+            self.currentValue = ReportItem(value, encoding, priority, elided)
 
     def putEmptyValue(self, priority = -10):
         if priority >= self.currentValue.priority:
-            self.currentValue.value = ""
-            self.currentValue.priority = priority
-            self.currentValue.encoding = None
-            self.currentValue.elided = None
+            self.currentValue = ReportItem("", None, priority, None)
 
     def putName(self, name):
         self.put('name="%s",' % name)
 
     def putBetterType(self, type):
-        self.currentType.value = str(type)
+        if isinstance(type, ReportItem):
+            self.currentType.value = str(type.value)
+        else:
+            self.currentType.value = str(type)
         self.currentType.priority += 1
 
     def putNoType(self):
@@ -1158,7 +1161,7 @@ class DumperBase:
                                 self.putItem(v)
                                 propertyCount += 1
 
-            self.putValue('<%s items>' % propertyCount if propertyCount else '<>0 items>')
+            self.putValue(str('<%s items>' % propertyCount if propertyCount else '<>0 items>'))
             self.putNumChild(1)
 
         with SubItem(self, "[methods]"):

share/qtcreator/debugger/gdbbridge.py
@@ -532,23 +532,18 @@ class Dumper(DumperBase):
         self.currentAddress = None
 
     def exitSubItem(self, item, exType, exValue, exTraceBack):
-        #warn("CURRENT VALUE: %s: %s %s %s %s" % (
-        #    self.currentIName,
-        #    self.currentValue.value,
-        #    self.currentValue.elided,
-        #    self.currentValue.encoding,
-        #    self.currentValue.priority))
+        #warn("CURRENT VALUE: %s: %s %s" % (self.currentIName, self.currentValue, self.currentType))
         if not exType is None:
             if self.passExceptions:
                 showException("SUBITEM", exType, exValue, exTraceBack)
             self.putNumChild(0)
             self.putValue("<not accessible>")
         try:
-            #warn("CURRENT TYPE: %s" % self.currentType.value)
-            typeName = stripClassTag(self.currentType.value)
+            if self.currentType.value:
+                typeName = stripClassTag(self.currentType.value)
+                if len(typeName) > 0 and typeName != self.currentChildType:
+                    self.put('type="%s",' % typeName) # str(type.unqualified()) ?
 
-            if len(typeName) > 0 and typeName != self.currentChildType:
-                self.put('type="%s",' % typeName) # str(type.unqualified()) ?
             if  self.currentValue.value is None:
                 self.put('value="<not accessible>",numchild="0",')
             else:
@@ -1045,7 +1040,7 @@ class Dumper(DumperBase):
                 # generic pointer." with MinGW's gcc 4.5 when it "identifies"
                 # a "QWidget &" as "void &" and with optimized out code.
                 self.putItem(value.cast(type.target().unqualified()))
-                self.putBetterType("%s &" % self.currentType)
+                self.putBetterType("%s &" % self.currentType.value)
                 return
             except RuntimeError:
                 self.putValue("<optimized out reference>")

share/qtcreator/debugger/lldbbridge.py
@@ -319,9 +319,10 @@ class Dumper(DumperBase):
             self.putNumChild(0)
             self.putValue("<not accessible>")
         try:
-            typeName = self.currentType.value
-            if len(typeName) > 0 and typeName != self.currentChildType:
-                self.put('type="%s",' % typeName) # str(type.unqualified()) ?
+            if self.currentType.value:
+                typeName = self.currentType.value
+                if len(typeName) > 0 and typeName != self.currentChildType:
+                    self.put('type="%s",' % typeName) # str(type.unqualified()) ?
             if  self.currentValue.value is None:
                 self.put('value="<not accessible>",numchild="0",')
             else:

share/qtcreator/debugger/qttypes.py
@@ -239,7 +239,7 @@ def qdump__QTimeZone(d, value):
         d.putNumChild(0)
         return
     idAddr = base + 2 * d.ptrSize() # [QSharedData] + [vptr]
-    d.putByteArrayValueByAddress(idAddr)
+    d.putByteArrayValue(idAddr)
     d.putPlainChildren(value["d"])
 
 
@@ -392,7 +392,7 @@ def qdump__QDir(d, value):
         dirEntryOffset = fileInfosOffset + d.ptrSize()
         absoluteDirEntryOffset = dirEntryOffset + fileSystemEntrySize
 
-    d.putStringValueByAddress(privAddress + dirEntryOffset)
+    d.putStringValue(privAddress + dirEntryOffset)
     if d.isExpanded():
         with Children(d):
             ns = d.qtNamespace()
@@ -429,7 +429,7 @@ def qdump__QFile(d, value):
             offset = 140 if is32bit else 232
     privAddress = d.extractPointer(d.addressOf(value) + d.ptrSize())
     fileNameAddress = privAddress + offset
-    d.putStringValueByAddress(fileNameAddress)
+    d.putStringValue(fileNameAddress)
     d.putNumChild(1)
     if d.isExpanded():
         with Children(d):
@@ -447,7 +447,7 @@ def qdump__QFileInfo(d, value):
     #    d.putPlainChildren(value)
     #    return
     filePathAddress = privAddress + d.ptrSize()
-    d.putStringValueByAddress(filePathAddress)
+    d.putStringValue(filePathAddress)
     d.putNumChild(1)
     if d.isExpanded():
         ns = d.qtNamespace()
@@ -705,7 +705,7 @@ def qdump__QHostAddress(d, value):
     ipStringAddress = privAddress + (0 if isQt5 else 24)
     isParsedAddress = privAddress + 24 + 2 * sizeofQString
     # value.d.d->ipString
-    ipString = d.encodeStringHelper(d.extractPointer(ipStringAddress), limit=100)
+    ipString = d.encodeString(ipStringAddress, limit=100)
     if d.extractByte(isParsedAddress) and len(ipString) > 0:
         d.putValue(ipString, Hex4EncodedLittleEndian)
     else:
@@ -720,7 +720,7 @@ def qdump__QHostAddress(d, value):
             data = d.readMemory(privAddress + a6Offset, 16)
             address = ':'.join("%x" % int(data[i:i+4], 16) for i in xrange(0, 32, 4))
             scopeId = privAddress + sizeofQString + (0 if isQt5 else 24)
-            scopeId = d.encodeStringHelper(d.extractPointer(scopeId), limit=100)
+            scopeId = d.encodeString(scopeId, limit=100)
             d.putValue("%s%%%s" % (address, scopeId), IPv6AddressAndHexScopeId)
         elif proto == 0:
             # value.d.d->a
@@ -1531,7 +1531,7 @@ def qdump__QRegExp(d, value):
     privAddress = d.extractPointer(value)
     engineKeyAddress = privAddress + d.ptrSize()
     patternAddress = engineKeyAddress
-    d.putStringValueByAddress(patternAddress)
+    d.putStringValue(patternAddress)
     d.putNumChild(1)
     if d.isExpanded():
         with Children(d):
@@ -1843,13 +1843,13 @@ def qdump__QUrl(d, value):
             d.putValue("<invalid>")
             return
         schemeAddr = privAddress + 2 * d.intSize()
-        scheme = d.encodeStringHelper(d.extractPointer(schemeAddr), limit=1000)
-        userName = d.encodeStringHelper(d.extractPointer(schemeAddr + 1 * d.ptrSize()), limit=100)
-        password = d.encodeStringHelper(d.extractPointer(schemeAddr + 2 * d.ptrSize()), limit=100)
-        host = d.encodeStringHelper(d.extractPointer(schemeAddr + 3 * d.ptrSize()), limit=100)
-        path = d.encodeStringHelper(d.extractPointer(schemeAddr + 4 * d.ptrSize()), limit=1000)
-        query = d.encodeStringHelper(d.extractPointer(schemeAddr + 5 * d.ptrSize()), limit=10000)
-        fragment = d.encodeStringHelper(d.extractPointer(schemeAddr + 6 * d.ptrSize()), limit=10000)
+        scheme = d.encodeString(schemeAddr, limit=1000)
+        userName = d.encodeString(schemeAddr + 1 * d.ptrSize(), limit=100)
+        password = d.encodeString(schemeAddr + 2 * d.ptrSize(), limit=100)
+        host = d.encodeString(schemeAddr + 3 * d.ptrSize(), limit=100)
+        path = d.encodeString(schemeAddr + 4 * d.ptrSize(), limit=1000)
+        query = d.encodeString(schemeAddr + 5 * d.ptrSize(), limit=10000)
+        fragment = d.encodeString(schemeAddr + 6 * d.ptrSize(), limit=10000)
         port = d.extractInt(d.extractPointer(value) + d.intSize())
 
         url = scheme

tests/auto/debugger/tst_dumpers.cpp
@@ -1062,7 +1062,7 @@ void tst_Dumpers::dumper()
     //qDebug() << "Starting qmake: " << cmd;
     QStringList options;
 #ifdef Q_OS_MAC
-    if (m_qtVersion < 0x050000)
+    if (m_qtVersion && m_qtVersion < 0x050000)
         options << QLatin1String("-spec") << QLatin1String("unsupported/macx-clang");
 #endif
     qmake.start(cmd, options);
@@ -1424,8 +1424,8 @@ void tst_Dumpers::dumper_data()
                + Check("ba1.13", "[13]", "2", "char")
 
                + CheckType("ba2", "@QByteArray")
-               + Check("s", '"' + QByteArray(10000, 'x') + '"', "@QString")
-               + Check("ss", '"' + QByteArray(10000, 'c') + '"', "std::string")
+               + Check("s", '"' + QByteArray(100, 'x') + '"', "@QString")
+               + Check("ss", '"' + QByteArray(100, 'c') + '"', "std::string")
 
                + Check("buf1", "\"" + QByteArray(1, (char)0xee) + "\"", "@QByteArray")
                + Check("buf2", "\"" + QByteArray(1, (char)0xee) + "\"", "@QByteArray")