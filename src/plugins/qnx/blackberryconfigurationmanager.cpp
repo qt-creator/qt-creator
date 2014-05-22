qt-creator/qt-creator

commit 0e67b293b7eaf0bff055d1587577c453397ba153

Qnx: Fix lifecycle of the BlackBerryConfigurationManager
This fixes a crash when this class tries to show a UI while it is
destructed.

Task-number: QTCREATORBUG-11712
Change-Id: If4aca14a3ca4792bec1a45903a1f38287d16dbc4
Reviewed-by: Tobias Hunger <tobias.hunger@digia.com>

src/plugins/qnx/blackberryconfigurationmanager.cpp
@@ -62,6 +62,8 @@ using namespace ProjectExplorer;
 namespace Qnx {
 namespace Internal {
 
+BlackBerryConfigurationManager *BlackBerryConfigurationManager::m_instance = 0;
+
 namespace {
 const QLatin1String SettingsGroup("BlackBerryConfiguration");
 const QLatin1String NDKLocationKey("NDKLocation"); // For 10.1 NDK support (< QTC 3.0)
@@ -93,6 +95,7 @@ BlackBerryConfigurationManager::BlackBerryConfigurationManager(QObject *parent)
     m_writer = new Utils::PersistentSettingsWriter(bbConfigSettingsFileName(),
                                                    QLatin1String("BlackBerryConfigurations"));
     connect(Core::ICore::instance(), SIGNAL(saveSettingsRequested()), this, SLOT(saveSettings()));
+    m_instance = this;
 }
 
 void BlackBerryConfigurationManager::saveConfigurations()
@@ -466,15 +469,14 @@ void BlackBerryConfigurationManager::saveSettings()
     saveConfigurations();
 }
 
-BlackBerryConfigurationManager &BlackBerryConfigurationManager::instance()
+BlackBerryConfigurationManager *BlackBerryConfigurationManager::instance()
 {
-    static BlackBerryConfigurationManager instance;
-
-    return instance;
+    return m_instance;
 }
 
 BlackBerryConfigurationManager::~BlackBerryConfigurationManager()
 {
+    m_instance = 0;
     qDeleteAll(m_apiLevels);
     qDeleteAll(m_runtimes);
     delete m_writer;

src/plugins/qnx/blackberryconfigurationmanager.h
@@ -45,12 +45,13 @@ namespace Internal {
 
 class BlackBerryApiLevelConfiguration;
 class BlackBerryRuntimeConfiguration;
+class QNXPlugin;
 
 class BlackBerryConfigurationManager : public QObject
 {
     Q_OBJECT
 public:
-    static BlackBerryConfigurationManager &instance();
+    static BlackBerryConfigurationManager *instance();
     ~BlackBerryConfigurationManager();
     bool addApiLevel(BlackBerryApiLevelConfiguration *config);
     void removeApiLevel(BlackBerryApiLevelConfiguration *config);
@@ -92,6 +93,9 @@ signals:
 
 private:
     BlackBerryConfigurationManager(QObject *parent = 0);
+
+    static BlackBerryConfigurationManager *m_instance;
+
     QList<BlackBerryApiLevelConfiguration*> m_apiLevels;
     QList<BlackBerryRuntimeConfiguration*> m_runtimes;
 
@@ -107,6 +111,8 @@ private:
 
     void insertApiLevelByVersion(BlackBerryApiLevelConfiguration* apiLevel);
     void insertRuntimeByVersion(BlackBerryRuntimeConfiguration* runtime);
+
+    friend class QNXPlugin;
 };
 
 } // namespace Internal

src/plugins/qnx/qnxplugin.cpp
@@ -93,6 +93,7 @@ bool QNXPlugin::initialize(const QStringList &arguments, QString *errorString)
     Q_UNUSED(errorString)
 
     // Handles BlackBerry
+    addAutoReleasedObject(new BlackBerryConfigurationManager);
     addAutoReleasedObject(new BlackBerryQtVersionFactory);
     addAutoReleasedObject(new BlackBerryDeployConfigurationFactory);
     addAutoReleasedObject(new BlackBerryDeviceConfigurationFactory);
@@ -134,7 +135,8 @@ bool QNXPlugin::initialize(const QStringList &arguments, QString *errorString)
     }
     addAutoReleasedObject(new BarDescriptorEditorFactory);
 
-    connect(ProjectExplorer::KitManager::instance(), SIGNAL(kitsLoaded()), &BlackBerryConfigurationManager::instance(), SLOT(loadSettings()));
+    connect(ProjectExplorer::KitManager::instance(), SIGNAL(kitsLoaded()),
+            BlackBerryConfigurationManager::instance(), SLOT(loadSettings()));
 
     return true;
 }
@@ -479,12 +481,12 @@ void QNXPlugin::testConfigurationManager_data()
 
 void QNXPlugin::testConfigurationManager()
 {
-    BlackBerryConfigurationManager &manager = BlackBerryConfigurationManager::instance();
+    BlackBerryConfigurationManager *manager = BlackBerryConfigurationManager::instance();
 
-    QCOMPARE(manager.apiLevels().count(), 0);
-    QCOMPARE(manager.activeApiLevels().count(), 0);
-    QCOMPARE(manager.defaultApiLevel(), static_cast<BlackBerryApiLevelConfiguration*>(0));
-    QVERIFY(manager.newestApiLevelEnabled());
+    QCOMPARE(manager->apiLevels().count(), 0);
+    QCOMPARE(manager->activeApiLevels().count(), 0);
+    QCOMPARE(manager->defaultApiLevel(), static_cast<BlackBerryApiLevelConfiguration*>(0));
+    QVERIFY(manager->newestApiLevelEnabled());
 
     QFETCH(QVariantMap, newerConfiguration);
     QFETCH(QVariantMap, olderConfiguration);
@@ -495,32 +497,32 @@ void QNXPlugin::testConfigurationManager()
     BlackBerryApiLevelConfiguration *oldConfig =
             new BlackBerryApiLevelConfiguration(olderConfiguration);
 
-    QVERIFY(manager.addApiLevel(oldConfig));
-    QVERIFY(manager.newestApiLevelEnabled());
-    QCOMPARE(manager.defaultApiLevel(), oldConfig);
+    QVERIFY(manager->addApiLevel(oldConfig));
+    QVERIFY(manager->newestApiLevelEnabled());
+    QCOMPARE(manager->defaultApiLevel(), oldConfig);
 
-    manager.setDefaultConfiguration(oldConfig);
+    manager->setDefaultConfiguration(oldConfig);
 
-    QCOMPARE(manager.defaultApiLevel(), oldConfig);
-    QCOMPARE(manager.apiLevels().first(), oldConfig);
-    QVERIFY(!manager.newestApiLevelEnabled());
+    QCOMPARE(manager->defaultApiLevel(), oldConfig);
+    QCOMPARE(manager->apiLevels().first(), oldConfig);
+    QVERIFY(!manager->newestApiLevelEnabled());
 
-    QVERIFY(manager.addApiLevel(newerConfig));
-    QCOMPARE(manager.apiLevels().first(), newerConfig);
-    QCOMPARE(manager.defaultApiLevel(), oldConfig);
+    QVERIFY(manager->addApiLevel(newerConfig));
+    QCOMPARE(manager->apiLevels().first(), newerConfig);
+    QCOMPARE(manager->defaultApiLevel(), oldConfig);
 
-    manager.setDefaultConfiguration(0);
-    QVERIFY(manager.newestApiLevelEnabled());
-    QCOMPARE(manager.defaultApiLevel(), newerConfig);
+    manager->setDefaultConfiguration(0);
+    QVERIFY(manager->newestApiLevelEnabled());
+    QCOMPARE(manager->defaultApiLevel(), newerConfig);
 
-    manager.setDefaultConfiguration(oldConfig);
-    manager.removeApiLevel(oldConfig);
-    QCOMPARE(manager.defaultApiLevel(), newerConfig);
-    QVERIFY(manager.newestApiLevelEnabled());
+    manager->setDefaultConfiguration(oldConfig);
+    manager->removeApiLevel(oldConfig);
+    QCOMPARE(manager->defaultApiLevel(), newerConfig);
+    QVERIFY(manager->newestApiLevelEnabled());
 
-    manager.removeApiLevel(newerConfig);
-    QCOMPARE(manager.defaultApiLevel(), static_cast<BlackBerryApiLevelConfiguration*>(0));
-    QVERIFY(manager.newestApiLevelEnabled());
+    manager->removeApiLevel(newerConfig);
+    QCOMPARE(manager->defaultApiLevel(), static_cast<BlackBerryApiLevelConfiguration*>(0));
+    QVERIFY(manager->newestApiLevelEnabled());
 }
 
 #endif

src/plugins/qnx/blackberryapilevelconfiguration.cpp
@@ -445,7 +445,7 @@ bool BlackBerryApiLevelConfiguration::activate()
     if (qt5X86Version)
         createKit(qt5X86Version, x86ToolChain, x86DebuggerItemId);
 
-    BlackBerryConfigurationManager::instance().emitSettingsChanged();
+    BlackBerryConfigurationManager::instance()->emitSettingsChanged();
 
     return true;
 }
@@ -469,7 +469,7 @@ void BlackBerryApiLevelConfiguration::deactivate()
         }
     }
 
-    BlackBerryConfigurationManager::instance().emitSettingsChanged();
+    BlackBerryConfigurationManager::instance()->emitSettingsChanged();
 }
 
 #ifdef WITH_TESTS

src/plugins/qnx/blackberrycertificate.cpp
@@ -54,7 +54,7 @@ BlackBerryCertificate::BlackBerryCertificate(const QString &fileName,
 {
     m_process->setProcessChannelMode(QProcess::MergedChannels);
     m_process->setEnvironment(Utils::EnvironmentItem::toStringList(
-             BlackBerryConfigurationManager::instance().defaultConfigurationEnv()));
+             BlackBerryConfigurationManager::instance()->defaultConfigurationEnv()));
 }
 
 void BlackBerryCertificate::load()

src/plugins/qnx/blackberrycreatecertificatedialog.cpp
@@ -79,7 +79,7 @@ QString BlackBerryCreateCertificateDialog::author() const
 
 QString BlackBerryCreateCertificateDialog::certPath() const
 {
-    return BlackBerryConfigurationManager::instance().defaultKeystorePath();
+    return BlackBerryConfigurationManager::instance()->defaultKeystorePath();
 }
 
 QString BlackBerryCreateCertificateDialog::keystorePassword() const

src/plugins/qnx/blackberrydebugtokenrequestdialog.cpp
@@ -142,8 +142,6 @@ void BlackBerryDebugTokenRequestDialog::requestDebugToken()
         }
     }
 
-    BlackBerryConfigurationManager &configuration = BlackBerryConfigurationManager::instance();
-
     bool ok;
     const QString cskPassword = m_utils.cskPassword(this, &ok);
 
@@ -160,7 +158,7 @@ void BlackBerryDebugTokenRequestDialog::requestDebugToken()
     }
 
     m_requester->requestDebugToken(m_ui->debugTokenPath->path(),
-            cskPassword, configuration.defaultKeystorePath(),
+            cskPassword, BlackBerryConfigurationManager::instance()->defaultKeystorePath(),
             certificatePassword, m_ui->devicePin->text());
 }
 

src/plugins/qnx/blackberrydeviceconnection.cpp
@@ -61,7 +61,7 @@ BlackBerryDeviceConnection::BlackBerryDeviceConnection() :
 void BlackBerryDeviceConnection::connectDevice(const ProjectExplorer::IDevice::ConstPtr &device)
 {
     Utils::Environment env = Utils::Environment::systemEnvironment();
-    env.modify(BlackBerryConfigurationManager::instance().defaultConfigurationEnv());
+    env.modify(BlackBerryConfigurationManager::instance()->defaultConfigurationEnv());
 
     m_process->setEnvironment(env.toStringList());
 

commit 5962f2b5b1b17e7a091ed4641a21119d85bb388d

Qnx: Fix broken configuration manager test
The change 0e67b293b7eaf0bff055d1587577c453397ba153
broke the configuration manager test since
the manager gets now instantiated when loading the plugin
whereas the test assumes that it would be instantiated
when ::instance() is called the first time.

Change-Id: I8f873fa56305da37590e401e8cfb98b8c92fe89d
Reviewed-by: Christian Stenger <christian.stenger@digia.com>
Reviewed-by: Sergio Ahumada <sahumada@blackberry.com>

src/plugins/qnx/blackberryconfigurationmanager.cpp
@@ -258,6 +258,19 @@ void BlackBerryConfigurationManager::emitSettingsChanged()
     emit settingsChanged();
 }
 
+#ifdef WITH_TESTS
+void BlackBerryConfigurationManager::initUnitTest()
+{
+    foreach (BlackBerryApiLevelConfiguration *apiLevel, m_apiLevels)
+        removeApiLevel(apiLevel);
+
+    foreach (BlackBerryRuntimeConfiguration *runtime, m_runtimes)
+        removeRuntime(runtime);
+
+    m_defaultConfiguration = 0;
+}
+#endif
+
 void BlackBerryConfigurationManager::setKitsAutoDetectionSource()
 {
     foreach (Kit *kit, KitManager::kits()) {

src/plugins/qnx/blackberryconfigurationmanager.h
@@ -82,6 +82,10 @@ public:
 
     void emitSettingsChanged();
 
+#ifdef WITH_TESTS
+    void initUnitTest();
+#endif
+
 public slots:
     void loadSettings();
     void saveSettings();

src/plugins/qnx/qnxplugin.cpp
@@ -482,6 +482,7 @@ void QnxPlugin::testConfigurationManager_data()
 void QnxPlugin::testConfigurationManager()
 {
     BlackBerryConfigurationManager *manager = BlackBerryConfigurationManager::instance();
+    manager->initUnitTest();
 
     QCOMPARE(manager->apiLevels().count(), 0);
     QCOMPARE(manager->activeApiLevels().count(), 0);