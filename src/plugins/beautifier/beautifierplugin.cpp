qt-creator/qt-creator

commit ca433d9b2f20ad565ceecbae5357b4c81e580d9e

Beautifier: Add new class Command and option to format text via piping
The tool's formatting command is now enclosed in Command. For the
formatting itself it can be defined whether to use a temporary file or
the pipe.
Additionally, settings can return the current tool's version - if
needed.

Change-Id: I0e242c3e8016ed77cad92cc97a19fe3384dda858
Reviewed-by: David Schulz <david.schulz@digia.com>

src/plugins/beautifier/beautifierplugin.cpp
@@ -30,6 +30,8 @@
 #include "beautifierplugin.h"
 
 #include "beautifierconstants.h"
+#include "command.h"
+
 #include "artisticstyle/artisticstyle.h"
 #include "clangformat/clangformat.h"
 #include "uncrustify/uncrustify.h"
@@ -116,47 +118,83 @@ void BeautifierPlugin::updateActions(Core::IEditor *editor)
         m_tools.at(i)->updateActions(editor);
 }
 
-QString BeautifierPlugin::format(const QString &text, QStringList command, const QString &fileName)
+QString BeautifierPlugin::format(const QString &text, const Command &command, const QString &fileName)
 {
-    if (command.isEmpty())
+    const QString executable = command.executable();
+    if (executable.isEmpty())
         return QString();
 
-    // Save text to temporary file
-    QFileInfo fi(fileName);
-    Utils::TempFileSaver sourceFile(fi.absolutePath() + QLatin1String("/qtc_beautifier_XXXXXXXX.")
-                                    + fi.suffix());
-    sourceFile.setAutoRemove(true);
-    sourceFile.write(text.toUtf8());
-    if (!sourceFile.finalize()) {
-        showError(tr("Cannot create temporary file \"%1\": %2.")
-                  .arg(sourceFile.fileName()).arg(sourceFile.errorString()));
-        return QString();
+    switch (command.processing()) {
+    case Command::FileProcessing: {
+        // Save text to temporary file
+        QFileInfo fi(fileName);
+        Utils::TempFileSaver sourceFile(QDir::tempPath() + QLatin1String("/qtc_beautifier_XXXXXXXX.")
+                                        + fi.suffix());
+        sourceFile.setAutoRemove(true);
+        sourceFile.write(text.toUtf8());
+        if (!sourceFile.finalize()) {
+            showError(tr("Cannot create temporary file \"%1\": %2.")
+                      .arg(sourceFile.fileName()).arg(sourceFile.errorString()));
+            return QString();
+        }
+
+        // Format temporary file
+        QProcess process;
+        QStringList options = command.options();
+        options.replaceInStrings(QLatin1String("%file"), sourceFile.fileName());
+        process.start(executable, options);
+        if (!process.waitForFinished()) {
+            showError(tr("Cannot call %1 or some other error occurred.").arg(executable));
+            return QString();
+        }
+        const QByteArray output = process.readAllStandardError();
+        if (!output.isEmpty())
+            showError(executable + QLatin1String(": ") + QString::fromUtf8(output));
+
+        // Read text back
+        Utils::FileReader reader;
+        if (!reader.fetch(sourceFile.fileName(), QIODevice::Text)) {
+            showError(tr("Cannot read file \"%1\": %2.")
+                      .arg(sourceFile.fileName()).arg(reader.errorString()));
+            return QString();
+        }
+        return QString::fromUtf8(reader.data());
+    } break;
+
+    case Command::PipeProcessing: {
+        QProcess process;
+        QStringList options = command.options();
+        options.replaceInStrings(QLatin1String("%file"), fileName);
+        process.start(executable, options);
+        if (!process.waitForStarted()) {
+            showError(tr("Cannot call %1 or some other error occurred.").arg(executable));
+            return QString();
+        }
+        process.write(text.toUtf8());
+        process.closeWriteChannel();
+        if (!process.waitForFinished()) {
+            showError(tr("Cannot call %1 or some other error occurred.").arg(executable));
+            return QString();
+        }
+        const QByteArray error = process.readAllStandardError();
+        if (!error.isEmpty()) {
+            showError(executable + QLatin1String(": ") + QString::fromUtf8(error));
+            return QString();
+        }
+
+        if (command.pipeAddsNewline()) {
+            QString formatted = QString::fromUtf8(process.readAllStandardOutput());
+            formatted.remove(QRegExp(QLatin1String("(\\r\\n|\\n)$")));
+            return formatted;
+        }
+        return QString::fromUtf8(process.readAllStandardOutput());
+    }
     }
 
-    // Format temporary file
-    QProcess process;
-    command.replaceInStrings(QLatin1String("%file"), sourceFile.fileName());
-    const QString processProgram = command.takeFirst();
-    process.start(processProgram, command);
-    if (!process.waitForFinished()) {
-        showError(tr("Cannot call %1 or some other error occurred.").arg(processProgram));
-        return QString();
-    }
-    const QByteArray output = process.readAllStandardError();
-    if (!output.isEmpty())
-        showError(processProgram + QLatin1String(": ") + QString::fromLocal8Bit(output));
-
-    // Read text back
-    Utils::FileReader reader;
-    if (!reader.fetch(sourceFile.fileName(), QIODevice::Text)) {
-        showError(tr("Cannot read file \"%1\": %2.")
-                  .arg(sourceFile.fileName()).arg(reader.errorString()));
-        return QString();
-    }
-    return QString::fromUtf8(reader.data());
+    return QString();
 }
 
-void BeautifierPlugin::formatCurrentFile(QStringList command)
+void BeautifierPlugin::formatCurrentFile(const Command &command)
 {
     QPlainTextEdit *textEditor = 0;
     if (TextEditor::BaseTextEditor *editor

src/plugins/beautifier/abstractsettings.cpp
@@ -44,7 +44,8 @@ namespace Beautifier {
 namespace Internal {
 
 AbstractSettings::AbstractSettings(const QString &name, const QString &ending)
-    : m_name(name)
+    : m_version(0)
+    , m_name(name)
     , m_ending(ending)
     , m_styleDir(Core::ICore::userResourcePath() + QLatin1Char('/')
                  + QLatin1String(Beautifier::Constants::SETTINGS_DIRNAME) + QLatin1Char('/')
@@ -135,6 +136,18 @@ void AbstractSettings::setCommand(const QString &command)
         return;
 
     m_command = command;
+    updateVersion();
+}
+
+int AbstractSettings::version() const
+{
+    return m_version;
+}
+
+void AbstractSettings::updateVersion()
+{
+    // If a beautifier needs to know the current tool's version, reimplement and store the version
+    // in m_version.
 }
 
 QStringList AbstractSettings::options()

src/plugins/beautifier/abstractsettings.h
@@ -67,6 +67,8 @@ public:
 
     QString command() const;
     void setCommand(const QString &command);
+    int version() const;
+    virtual void updateVersion();
 
     QStringList options();
     QString documentation(const QString &option) const;
@@ -74,6 +76,7 @@ public:
 protected:
     QMap<QString, QString> m_styles;
     QMap<QString, QVariant> m_settings;
+    int m_version;
 
     void readDocumentation();
 

src/plugins/beautifier/artisticstyle/artisticstyle.cpp
@@ -27,6 +27,8 @@
 **
 ****************************************************************************/
 
+// Tested with version 2.01, 2.02, 2.02.1, 2.03 and 2.04
+
 #include "artisticstyle.h"
 
 #include "artisticstyleconstants.h"
@@ -35,6 +37,7 @@
 
 #include "../beautifierconstants.h"
 #include "../beautifierplugin.h"
+#include "../command.h"
 
 #include <coreplugin/actionmanager/actioncontainer.h>
 #include <coreplugin/actionmanager/actionmanager.h>
@@ -136,11 +139,19 @@ void ArtisticStyle::formatFile()
         BeautifierPlugin::showError(BeautifierPlugin::msgCannotGetConfigurationFile(
                                         QLatin1String(Constants::ArtisticStyle::DISPLAY_NAME)));
     } else {
-        BeautifierPlugin::formatCurrentFile(QStringList()
-                                            << m_settings->command()
-                                            << QLatin1String("-q")
-                                            << QLatin1String("--options=") + cfgFileName
-                                            << QLatin1String("%file"));
+        Command command;
+        command.setExecutable(m_settings->command());
+        command.addOption(QLatin1String("-q"));
+        command.addOption(QLatin1String("--options=") + cfgFileName);
+
+        if (m_settings->version() > ArtisticStyleSettings::Version_2_03) {
+            command.setProcessing(Command::PipeProcessing);
+            command.setPipeAddsNewline(true);
+        } else {
+            command.addOption(QLatin1String("%file"));
+        }
+
+        BeautifierPlugin::formatCurrentFile(command);
     }
 }
 

src/plugins/beautifier/artisticstyle/artisticstylesettings.cpp
@@ -39,6 +39,7 @@
 #include <QFile>
 #include <QFileInfo>
 #include <QProcess>
+#include <QtConcurrent>
 #include <QTextDocument> // Qt::escape() in Qt 4
 #include <QXmlStreamWriter>
 
@@ -57,12 +58,48 @@ ArtisticStyleSettings::ArtisticStyleSettings() :
     AbstractSettings(QLatin1String(Constants::ArtisticStyle::SETTINGS_NAME),
                      QLatin1String(".astyle"))
 {
+    connect(&m_versionWatcher, SIGNAL(finished()), this, SLOT(helperSetVersion()));
+
     setCommand(QLatin1String("astyle"));
     m_settings.insert(QLatin1String(kUseOtherFiles), QVariant(true));
     m_settings.insert(QLatin1String(kUseHomeFile), QVariant(false));
     m_settings.insert(QLatin1String(kUseCustomStyle), QVariant(false));
     m_settings.insert(QLatin1String(kCustomStyle), QVariant());
     read();
+}
+
+void ArtisticStyleSettings::updateVersion()
+{
+    if (m_versionFuture.isRunning())
+        m_versionFuture.cancel();
+
+    m_versionFuture = QtConcurrent::run(this, &ArtisticStyleSettings::helperUpdateVersion);
+    m_versionWatcher.setFuture(m_versionFuture);
+}
+
+int ArtisticStyleSettings::helperUpdateVersion() const
+{
+    QProcess process;
+    process.start(command(), QStringList() << QLatin1String("--version"));
+    if (!process.waitForFinished()) {
+        process.kill();
+        return 0;
+    }
+
+    // The version in Artistic Style is printed like "Artistic Style Version 2.04"
+    const QString version = QString::fromUtf8(process.readAllStandardError()).trimmed();
+    const QRegExp rx(QLatin1String("([2-9]{1})\\.([0-9]{2})(\\.[1-9]{1})?$"));
+    if (rx.indexIn(version) != -1) {
+        const int major = rx.cap(1).toInt() * 100;
+        const int minor = rx.cap(2).toInt();
+        return major + minor;
+    }
+    return 0;
+}
+
+void ArtisticStyleSettings::helperSetVersion()
+{
+    m_version = m_versionWatcher.result();
 }
 
 bool ArtisticStyleSettings::useOtherFiles() const

src/plugins/beautifier/artisticstyle/artisticstylesettings.h
@@ -34,14 +34,25 @@
 
 #include <utils/qtcoverride.h>
 
+#include <QFuture>
+#include <QFutureWatcher>
+
 namespace Beautifier {
 namespace Internal {
 namespace ArtisticStyle {
 
-class ArtisticStyleSettings : public AbstractSettings
+class ArtisticStyleSettings : public QObject, public AbstractSettings
 {
+    Q_OBJECT
+
 public:
+    enum ArtisticStyleVersion {
+        Version_2_03 = 203
+    };
+
     ArtisticStyleSettings();
+
+    void updateVersion() QTC_OVERRIDE;
 
     bool useOtherFiles() const;
     void setUseOtherFiles(bool useOtherFiles);
@@ -57,6 +68,14 @@ public:
 
     QString documentationFilePath() const QTC_OVERRIDE;
     void createDocumentationFile() const QTC_OVERRIDE;
+
+private slots:
+    void helperSetVersion();
+
+private:
+    QFuture<int> m_versionFuture;
+    QFutureWatcher<int> m_versionWatcher;
+    int helperUpdateVersion() const;
 };
 
 } // namespace ArtisticStyle

src/plugins/beautifier/beautifier.pro
@@ -7,6 +7,7 @@ HEADERS += \
     beautifierabstracttool.h \
     beautifierconstants.h \
     beautifierplugin.h \
+    command.h \
     configurationdialog.h \
     configurationeditor.h \
     configurationpanel.h \
@@ -26,6 +27,7 @@ HEADERS += \
 SOURCES += \
     abstractsettings.cpp \
     beautifierplugin.cpp \
+    command.cpp \
     configurationdialog.cpp \
     configurationeditor.cpp \
     configurationpanel.cpp \

src/plugins/beautifier/beautifier.qbs
@@ -22,6 +22,8 @@ QtcPlugin {
         "beautifierconstants.h",
         "beautifierplugin.cpp",
         "beautifierplugin.h",
+        "command.cpp",
+        "command.h",
         "configurationdialog.cpp",
         "configurationdialog.h",
         "configurationdialog.ui",

commit 22d4cdabae5fa7e1603e270bd16044eaaaed6c66

Beautifier: Fix compilation
Error introduced with ca433d9b2f20ad565ceecbae5357b4c81e580d9e.

Change-Id: I4b9604c20dd28b5b8843c0e476cd480d6191f904
Reviewed-by: David Schulz <david.schulz@digia.com>

src/plugins/beautifier/beautifierplugin.cpp
@@ -55,6 +55,7 @@
 #include <QAction>
 #include <QDir>
 #include <QFileInfo>
+#include <QDir>
 #include <QPlainTextEdit>
 #include <QProcess>
 #include <QScrollBar>