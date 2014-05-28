e3a416bf4bcfc37db57afae5ccc5d26a47018874
Erik Verbruggen <erik.verbruggen@digia.com>
2014-05-28 11:40:10 +0200

    Make QTC_ASSERT fatal depending on env. var.
    When the environment variable QTC_FATAL_ASSERTS is not empty, make
    QTC_ASSERT abort (or equivalent) when the condition is not met. The
    purpose is to make certain debugging cases easier without having users
    apply a change and recompile.
    
    Change-Id: I8350ed27f78dca94db0c9848bd8af4b91cec2222
    Reviewed-by: hjk <hjk121@nokiamail.com>

--- src/libs/utils/qtcassert.cpp
+++ src/libs/utils/qtcassert.cpp
@@ -29,11 +29,17 @@
 
 #include "qtcassert.h"
 
+#include <QByteArray>
+
 namespace Utils {
 
 void writeAssertLocation(const char *msg)
 {
-    qDebug("SOFT ASSERT: %s", msg);
+    static bool goBoom = !qgetenv("QTC_FATAL_ASSERTS").isEmpty();
+    if (goBoom)
+        qFatal("SOFT ASSERT made fatal: %s", msg);
+    else
+        qDebug("SOFT ASSERT: %s", msg);
 }
 
 } // namespace Utils
