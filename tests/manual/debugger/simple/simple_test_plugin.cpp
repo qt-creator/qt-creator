76f829164a5e2a2eb8371dc5dbdd8909b57b21ea
Robert Loehning <robert.loehning@digia.com>
2014-06-06 16:11:53 +0200

    debugger: Add some code for testing ctor in plugin
    Change-Id: I0c35549804befc8aed034e35c9cce24f57e41337
    Reviewed-by: hjk <hjk121@nokiamail.com>

--- tests/manual/debugger/simple/simple_test_plugin.cpp
+++ tests/manual/debugger/simple/simple_test_plugin.cpp
@@ -30,9 +30,19 @@
 #include <stdio.h>
 #include <qglobal.h>
 
+class Something {
+public:
+    Something() {
+        int i = 0;
+        ++i;
+        return;
+    }
+};
 
 extern "C" Q_DECL_EXPORT int pluginTest()
 {
+    static Something sst;
+    Something st;
     int s = 0;
     for (int i = 1; i != 2000; ++i)
         s += i;
