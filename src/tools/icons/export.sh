e54764fb9eb4e32b9abdc9d3446eee36d3092fa8
Alessandro Portale <alessandro.portale@digia.com>
2014-06-09 22:11:48 +0200

    Icons for Qt Creator.
    In various eras, we collected images/icons in .png format
    from different sources. In some cases from hired designers,
    some contributors, and even some Qt Creator developers.
    That causes more or less subtle in consistencies ranging
    from color schemes to shadow directions.
    
    Now - in the era of High-Dpi - we suddenly need high
    resolution "@2x" versions of our graphics, but do not have
    the original graphics files to easily generate them.
    
    This approach here supplies many of the graphics in SVG,
    editable with Inkscape. Existing pngs are re-created as
    vector graphic. That way, the original graphics
    sources are finally public and version controlled. Also,
    we potentially get more consistent graphics and
    High-resolution versions.
    
    The shell script executes Inkscape to export the images and
    icons as pngs.
    
    Change-Id: Id426ee94b151833cd52e29a8234966aba9cd38bc
    Reviewed-by: Orgad Shaneh <orgads@gmail.com>
    Reviewed-by: Adam Strzelecki <ono@java.pl>
    Reviewed-by: Alessandro Portale <alessandro.portale@digia.com>

---
+++ src/tools/icons/export.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+#############################################################################
+##
+## Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
+## Contact: http://www.qt-project.org/legal
+##
+## This file is part of Qt Creator.
+##
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
+## Software or, alternatively, in accordance with the terms contained in
+## a written agreement between you and Digia.  For licensing terms and
+## conditions see http://qt.digia.com/licensing.  For further information
+## use the contact form at http://qt.digia.com/contact-us.
+##
+## GNU Lesser General Public License Usage
+## Alternatively, this file may be used under the terms of the GNU Lesser
+## General Public License version 2.1 as published by the Free Software
+## Foundation and appearing in the file LICENSE.LGPL included in the
+## packaging of this file.  Please review the following information to
+## ensure the GNU Lesser General Public License version 2.1 requirements
+## will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+##
+## In addition, as a special exception, Digia gives you certain additional
+## rights.  These rights are described in the Digia Qt LGPL Exception
+## version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+##
+#############################################################################
+
+# This script calls Inkscape to rasterize several images into png files.
+# The images end up in the final position of the source tree.
+# Each images is generated as normal and high resolution variant.
+# Each png file is afterwards optimized with optipng.
+
+cd `dirname $0`
+
+# QTC_SRC is expected to point to the source root of Qt Creator
+if [ -z "$QTC_SRC" ]; then
+    QTC_SRC=$(cd ../../..; pwd)
+fi
+
+# Inkscape is required by this script
+if ! hash inkscape 2>/dev/null; then
+    echo "Inkscape was not found in PATH" >&2
+    exit 1
+fi
+
+# The svg element IDs of images to export. They correspond to the
+# path and base name of each image in the Qt Creator sources.
+ids=$(sed -n 's,^[[:space:]]*id="\(src/[^"]*\)".*$,\1,p' qtcreatoricons.svg)
+
+# The shell mode of Inkscape is used to execute several export commands
+# with one launch of Inkscape. The commands are first all written into a
+# file "inkscape_commands" and then piped into Inkscape's shell.
+for i in $ids; do
+    # The normal resolution (default = 90 dpi)
+    echo "qtcreatoricons.svg --export-id=$i --export-id-only --export-png=$QTC_SRC/$i.png"
+    # The "@2x" resolution
+    echo "qtcreatoricons.svg --export-id=$i --export-id-only --export-png=$QTC_SRC/$i@2x.png --export-dpi=180"
+done | inkscape --shell
+
+if hash optipng 2>/dev/null; then
+    # Optimizing pngs via optipng
+    for i in $ids; do
+        optipng -o7 -strip "all" $QTC_SRC/$i.png
+        optipng -o7 -strip "all" $QTC_SRC/$i@2x.png
+    done
+else
+    echo "optipng was not found in PATH. Please do not push the unoptimized .pngs to the main repository." >&2
+fi
