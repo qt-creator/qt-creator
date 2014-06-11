df47e5f689a6c0509b383381d9e3d7ad3481e557
Eike Ziller <eike.ziller@digia.com>
2014-06-11 17:52:51 +0200

    Add search to class view
    Change-Id: I7796a5856b4d299c6593df8a8af762dec4da60f2
    Reviewed-by: Nikolai Kosjar <nikolai.kosjar@digia.com>

--- src/plugins/classview/classviewnavigationwidget.cpp
+++ src/plugins/classview/classviewnavigationwidget.cpp
@@ -34,6 +34,7 @@
 #include "classviewutils.h"
 #include "classviewconstants.h"
 
+#include <coreplugin/find/treeviewfind.h>
 #include <utils/navigationtreeview.h>
 #include <utils/qtcassert.h>
 
@@ -97,7 +98,8 @@ NavigationWidget::NavigationWidget(QWidget *parent) :
     verticalLayout->setContentsMargins(0, 0, 0, 0);
     treeView = new ::Utils::NavigationTreeView(this);
     treeView->setEditTriggers(QAbstractItemView::NoEditTriggers);
-    verticalLayout->addWidget(treeView);
+    verticalLayout->addWidget(Core::TreeViewFind::createSearchableWrapper(
+                              treeView, Core::TreeViewFind::FetchMoreWhileSearching));
 
     // tree model
     treeModel = new TreeItemModel(this);
