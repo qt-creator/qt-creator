qt-creator/qt-creator

commit 2ad3147542a45436733ddd7014993bcc3022a654

QmakeNodes::addNewInformation: Be responsible subfolder nodes
And increase the priority for those too. Fixes the case:

a.pro
 b.pri
   test
 c.pri

Right click on test and add new file. In that case b.pri should be the
node to which the file is added.

Task-number: QTCREATORBUG-12272
Change-Id: I54d144861bc06545db8e71ed33513a0df971dae2
Reviewed-by: Eike Ziller <eike.ziller@digia.com>

src/plugins/qmakeprojectmanager/qmakenodes.cpp
@@ -1069,7 +1069,7 @@ bool QmakePriFileNode::renameFile(const QString &filePath, const QString &newFil
 ProjectExplorer::FolderNode::AddNewInformation QmakePriFileNode::addNewInformation(const QStringList &files, Node *context) const
 {
     Q_UNUSED(files)
-    return ProjectExplorer::FolderNode::AddNewInformation(QFileInfo(path()).fileName(), context == this ? 120 : 90);
+    return ProjectExplorer::FolderNode::AddNewInformation(QFileInfo(path()).fileName(), context->projectNode() == this ? 120 : 90);
 }
 
 bool QmakePriFileNode::priFileWritable(const QString &path)
@@ -1576,7 +1576,7 @@ bool QmakeProFileNode::showInSimpleTree() const
 ProjectExplorer::FolderNode::AddNewInformation QmakeProFileNode::addNewInformation(const QStringList &files, Node *context) const
 {
     Q_UNUSED(files)
-    return AddNewInformation(QFileInfo(path()).fileName(), context == this ? 120 : 100);
+    return AddNewInformation(QFileInfo(path()).fileName(), context->projectNode() == this ? 120 : 100);
 }
 
 bool QmakeProFileNode::showInSimpleTree(QmakeProjectType projectType) const

commit 5933136b199d14530d20feea7182ecb83503f5cf

Fix crash when adding files from window menu
Introduced by 2ad3147542a45436733ddd7014993bcc3022a654

Task-number: QTCREATORBUG-12409
Change-Id: I422d315c5b9d22052251a0869d314adff7741af0
Sanity-Review: Qt Sanity Bot <qt_sanitybot@qt-project.org>
Reviewed-by: Christian Stenger <christian.stenger@digia.com>

src/plugins/qmakeprojectmanager/qmakenodes.cpp
@@ -1069,7 +1069,7 @@ bool QmakePriFileNode::renameFile(const QString &filePath, const QString &newFil
 ProjectExplorer::FolderNode::AddNewInformation QmakePriFileNode::addNewInformation(const QStringList &files, Node *context) const
 {
     Q_UNUSED(files)
-    return ProjectExplorer::FolderNode::AddNewInformation(QFileInfo(path()).fileName(), context->projectNode() == this ? 120 : 90);
+    return ProjectExplorer::FolderNode::AddNewInformation(QFileInfo(path()).fileName(), context && context->projectNode() == this ? 120 : 90);
 }
 
 bool QmakePriFileNode::priFileWritable(const QString &path)
@@ -1576,7 +1576,7 @@ bool QmakeProFileNode::showInSimpleTree() const
 ProjectExplorer::FolderNode::AddNewInformation QmakeProFileNode::addNewInformation(const QStringList &files, Node *context) const
 {
     Q_UNUSED(files)
-    return AddNewInformation(QFileInfo(path()).fileName(), context->projectNode() == this ? 120 : 100);
+    return AddNewInformation(QFileInfo(path()).fileName(), context && context->projectNode() == this ? 120 : 100);
 }
 
 bool QmakeProFileNode::showInSimpleTree(QmakeProjectType projectType) const