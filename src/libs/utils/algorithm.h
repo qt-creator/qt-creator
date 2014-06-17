3247028deec3ad4beb6d90e19f53f1b1e0e3b13e
Christian Stenger <christian.stenger@digia.com>
2014-06-17 07:43:35 +0200

    Core: Utils: Fix compile with Qt4
    Change-Id: Ibe4b504b84e467b1450b63bbbc7b569558fd8733
    Reviewed-by: hjk <hjk121@nokiamail.com>

--- src/libs/utils/algorithm.h
+++ src/libs/utils/algorithm.h
@@ -32,7 +32,17 @@
 
 #include <algorithm>
 
-#include <qcompilerdetection.h> // for Q_REQUIRE_RESULT
+#if QT_VERSION < 0x050000
+    #ifndef Q_REQUIRED_RESULT
+    # if defined(Q_CC_GNU)
+    #   define Q_REQUIRED_RESULT __attribute__ ((warn_unused_result))
+    # else
+    #   define Q_REQUIRED_RESULT
+    # endif
+    #endif
+#else
+    #include <qcompilerdetection.h> // for Q_REQUIRED_RESULT
+#endif
 
 namespace Utils
 {
