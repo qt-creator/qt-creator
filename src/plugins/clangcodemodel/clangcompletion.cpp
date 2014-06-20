qt-creator/qt-creator

commit 12103e0f676f762658310b885f8d427cdd755a18

Reinvent deprecated qSort as Utils::sort
Change-Id: I4f6011cc2b6127037249aabc2426a88ad7108ebf
Reviewed-by: Orgad Shaneh <orgads@gmail.com>
Reviewed-by: Daniel Teske <daniel.teske@digia.com>
Reviewed-by: hjk <hjk121@nokiamail.com>

src/plugins/clangcodemodel/clangcompletion.cpp
@@ -173,7 +173,7 @@ static QList<CodeCompletionResult> unfilteredCompletion(const ClangCompletionAss
     }
 
     QList<CodeCompletionResult> result = wrapper->codeCompleteAt(line, column + 1, unsavedFiles);
-    qSort(result);
+    std::sort(result);
 
     if (DebugTiming)
         qDebug() << "... Completion done in" << t.elapsed() << "ms, with" << result.count() << "items.";

commit 5ee81929acf77428d817c6b808034660389c9273

Fix clang compilation
Was broken in 12103e0f676f762658310b885f8d427cdd755a18

Change-Id: I28ce266f609a232bf1ed99bb044ffc6a3b561d57
Reviewed-by: hjk <hjk121@nokiamail.com>

src/plugins/clangcodemodel/clangcompletion.cpp
@@ -55,6 +55,8 @@
 #include <texteditor/texteditorsettings.h>
 #include <texteditor/completionsettings.h>
 
+#include <utils/algorithm.h>
+
 #include <QCoreApplication>
 #include <QDirIterator>
 #include <QTextCursor>
@@ -173,7 +175,7 @@ static QList<CodeCompletionResult> unfilteredCompletion(const ClangCompletionAss
     }
 
     QList<CodeCompletionResult> result = wrapper->codeCompleteAt(line, column + 1, unsavedFiles);
-    std::sort(result);
+    Utils::sort(result);
 
     if (DebugTiming)
         qDebug() << "... Completion done in" << t.elapsed() << "ms, with" << result.count() << "items.";