qt-creator/qt-creator

commit 204de740ec09333b2eafd3fa195d9daa3f226ed9

QmakeProject: Replace applicationProFilePaths by idsForNodes
Change-Id: Ie10e8cb93f26eb2087272646668fafd480b0208d
Reviewed-by: Eike Ziller <eike.ziller@digia.com>

src/plugins/qnx/blackberryrunconfigurationfactory.cpp
@@ -61,11 +61,9 @@ QList<Core::Id> BlackBerryRunConfigurationFactory::availableCreationIds(ProjectE
     if (!qt4Project)
         return ids;
 
-    QStringList proFiles = qt4Project->applicationProFilePathes(QLatin1String(Constants::QNX_BB_RUNCONFIGURATION_PREFIX));
-    foreach (const QString &pf, proFiles)
-        ids << Core::Id::fromString(pf);
-
-    return ids;
+    QList<QmakeProjectManager::QmakeProFileNode *> nodes = qt4Project->applicationProFiles();
+    return QmakeProjectManager::QmakeProject::idsForNodes(Core::Id(Constants::QNX_BB_RUNCONFIGURATION_PREFIX),
+                                                          nodes);
 }
 
 QString BlackBerryRunConfigurationFactory::displayNameForId(const Core::Id id) const

src/libs/utils/algorithm.h
@@ -1,0 +1,55 @@
+/****************************************************************************
+**
+** Copyright (C) 2014 Digia Plc and/or its subsidiary(-ies).
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of Qt Creator.
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and Digia.  For licensing terms and
+** conditions see http://qt.digia.com/licensing.  For further information
+** use the contact form at http://qt.digia.com/contact-us.
+**
+** GNU Lesser General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Digia gives you certain additional
+** rights.  These rights are described in the Digia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+****************************************************************************/
+
+#ifndef ALGORITHM_H
+#define ALGORITHM_H
+
+#include <algorithm>
+
+#include <qcompilerdetection.h> // for Q_REQUIRE_RESULT
+
+namespace Utils
+{
+
+// Note: add overloads for other container types as needed
+template<typename T, typename F>
+Q_REQUIRED_RESULT
+auto transform(const QList<T> &container, F function) -> QList<decltype(function(T()))>
+{
+    QList<decltype(function(T()))> result;
+    result.reserve(container.size());
+    std::transform(container.begin(), container.end(),
+                   std::back_inserter(result),
+                   function);
+    return result;
+}
+
+}
+
+#endif // ALGORITHM_H

src/libs/utils/utils-lib.pri
@@ -181,7 +181,8 @@ HEADERS += \
     $$PWD/winutils.h \
     $$PWD/itemviews.h \
     $$PWD/treeviewcombobox.h \
-    $$PWD/scopedswap.h
+    $$PWD/scopedswap.h \
+    $$PWD/algorithm.h
 
 FORMS += $$PWD/filewizardpage.ui \
     $$PWD/projectintropage.ui \

src/plugins/android/androidrunfactories.cpp
@@ -97,9 +97,7 @@ QList<Core::Id> AndroidRunConfigurationFactory::availableCreationIds(Target *par
                                                            << ApplicationTemplate
                                                            << LibraryTemplate);
     const Core::Id base = Core::Id(ANDROID_RC_ID_PREFIX);
-    foreach (QmakeProFileNode *node, nodes)
-        ids << base.withSuffix(node->path());
-    return ids;
+    return QmakeProject::idsForNodes(base, nodes);
 }
 
 QString AndroidRunConfigurationFactory::displayNameForId(const Core::Id id) const

src/plugins/ios/iosrunfactories.cpp
@@ -105,9 +105,7 @@ QList<Core::Id> IosRunConfigurationFactory::availableCreationIds(Target *parent)
                                                            << LibraryTemplate
                                                            << AuxTemplate);
 
-    foreach (QmakeProFileNode *node, nodes)
-        ids << baseId.withSuffix(node->path());
-    return ids;
+    return QmakeProject::idsForNodes(baseId, nodes);
 }
 
 QString IosRunConfigurationFactory::displayNameForId(const Core::Id id) const

src/plugins/qmakeprojectmanager/desktopqmakerunconfiguration.cpp
@@ -674,10 +674,8 @@ QList<Core::Id> DesktopQmakeRunConfigurationFactory::availableCreationIds(Target
         return result;
 
     QmakeProject *project = static_cast<QmakeProject *>(parent->project());
-    QStringList proFiles = project->applicationProFilePathes(QLatin1String(QMAKE_RC_PREFIX));
-    foreach (const QString &pf, proFiles)
-        result << Core::Id::fromString(pf);
-    return result;
+    QList<QmakeProFileNode *> nodes = project->applicationProFiles();
+    return QmakeProject::idsForNodes(Core::Id(QMAKE_RC_PREFIX), nodes);
 }
 
 QString DesktopQmakeRunConfigurationFactory::displayNameForId(const Core::Id id) const

src/plugins/qmakeprojectmanager/qmakeproject.cpp
@@ -40,6 +40,7 @@
 #include "wizards/abstractmobileapp.h"
 #include "wizards/qtquickapp.h"
 
+#include <utils/algorithm.h>
 #include <coreplugin/icontext.h>
 #include <coreplugin/icore.h>
 #include <coreplugin/progressmanager/progressmanager.h>
@@ -1102,12 +1103,11 @@ bool QmakeProject::hasApplicationProFile(const QString &path) const
     return false;
 }
 
-QStringList QmakeProject::applicationProFilePathes(const QString &prepend, Parsing parse) const
+QList<Core::Id> QmakeProject::idsForNodes(const Core::Id base, const QList<QmakeProFileNode *> &nodes)
 {
-    QStringList proFiles;
-    foreach (QmakeProFileNode *node, applicationProFiles(parse))
-        proFiles.append(prepend + node->path());
-    return proFiles;
+    return Utils::transform(nodes, [&base](QmakeProFileNode *node) {
+        return base.withSuffix(node->path());
+    });
 }
 
 void QmakeProject::activeTargetWasChanged()

src/plugins/qmakeprojectmanager/qmakeproject.h
@@ -91,7 +91,8 @@ public:
                                           Parsing parse = ExactParse) const;
     QList<QmakeProFileNode *> applicationProFiles(Parsing parse = ExactParse) const;
     bool hasApplicationProFile(const QString &path) const;
-    QStringList applicationProFilePathes(const QString &prepend = QString(), Parsing parse = ExactParse) const;
+
+    static QList<Core::Id> idsForNodes(const Core::Id base, const QList<QmakeProFileNode *> &nodes);
 
     void notifyChanged(const QString &name);
 

commit 3043dc73beca574b83f2bce506a659d2dc1a437a

Qnx: Fix the Id for the BlackBerry run configuration
The id was broken in commit 204de740ec09333b2eafd3fa195d9daa3f226ed9

Change-Id: Id5243632102f7d4ef5ac6532a036002361f26ccc
Reviewed-by: Daniel Teske <daniel.teske@digia.com>

src/plugins/qnx/blackberryrunconfigurationfactory.cpp
@@ -65,7 +65,7 @@ QList<Core::Id> BlackBerryRunConfigurationFactory::availableCreationIds(ProjectE
     QList<QmakeProjectManager::QmakeProFileNode *>  nodes = qt4Project->applicationProFiles();
     if (mode == AutoCreate)
         nodes = QmakeProject::nodesWithQtcRunnable(nodes);
-    return QmakeProject::idsForNodes(Core::Id(Constants::QNX_QNX_RUNCONFIGURATION_PREFIX),
+    return QmakeProject::idsForNodes(Core::Id(Constants::QNX_BB_RUNCONFIGURATION_PREFIX),
                                      nodes);
 }
 