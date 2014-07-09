qt-creator/qt-creator

commit 12103e0f676f762658310b885f8d427cdd755a18

Reinvent deprecated qSort as Utils::sort
Change-Id: I4f6011cc2b6127037249aabc2426a88ad7108ebf
Reviewed-by: Orgad Shaneh <orgads@gmail.com>
Reviewed-by: Daniel Teske <daniel.teske@digia.com>
Reviewed-by: hjk <hjk121@nokiamail.com>

src/plugins/projectexplorer/miniprojecttargetselector.cpp
@@ -33,6 +33,7 @@
 #include "kitmanager.h"
 #include "target.h"
 
+#include <utils/algorithm.h>
 #include <utils/styledbar.h>
 #include <utils/stylehelper.h>
 
@@ -731,23 +732,6 @@ bool MiniProjectTargetSelector::event(QEvent *event)
 
 }
 
-class IndexSorter
-{
-public:
-    enum SortOrder { Less = 0, Greater = 1};
-
-    IndexSorter(QVector<int> result, SortOrder order)
-        : m_result(result), m_order(order)
-    { }
-
-    bool operator()(int i, int j)
-    { return (m_result[i] < m_result[j]) ^ bool(m_order); }
-
-private:
-    QVector<int> m_result;
-    SortOrder m_order;
-};
-
 // does some fancy calculations to ensure proper widths for the list widgets
 QVector<int> MiniProjectTargetSelector::listWidgetWidths(int minSize, int maxSize)
 {
@@ -796,8 +780,9 @@ QVector<int> MiniProjectTargetSelector::listWidgetWidths(int minSize, int maxSiz
         if (result[i] != -1)
             indexes.append(i);
 
-    IndexSorter indexSorter(result, tooSmall ? IndexSorter::Less : IndexSorter::Greater);
-    qSort(indexes.begin(), indexes.end(), indexSorter);
+    Utils::sort(indexes, [&](int i, int j) -> bool {
+        return (result[i] > result[j]) ^ tooSmall;
+    });
 
     int i = 0;
     int first = result[indexes.first()]; // biggest or smallest

commit ece0c7c93747587131d6252b1a705b92470c09e5

MiniProjectTargetSelector: Fix wrong sorting
We need to sort indexes based on results. Also fix that with
too tooSmall being true, <= was used instead of <.

Broken in 12103e0f676f762658310b885f8d427cdd755a18

Change-Id: Ia8b7484fdea41776050b1a7ef85789cc49deab78
Reviewed-by: Nikita Baryshnikov <nib952051@gmail.com>
Reviewed-by: Daniel Teske <daniel.teske@digia.com>

src/plugins/projectexplorer/miniprojecttargetselector.cpp
@@ -780,9 +780,15 @@ QVector<int> MiniProjectTargetSelector::listWidgetWidths(int minSize, int maxSiz
         if (result[i] != -1)
             indexes.append(i);
 
-    Utils::sort(indexes, [&](int i, int j) -> bool {
-        return (result[i] > result[j]) ^ tooSmall;
-    });
+    if (tooSmall) {
+        Utils::sort(indexes, [&result](int i, int j) {
+            return result[i] < result[j];
+        });
+    } else {
+        Utils::sort(indexes, [&result](int i, int j) {
+            return result[i] > result[j];
+        });
+    }
 
     int i = 0;
     int first = result[indexes.first()]; // biggest or smallest