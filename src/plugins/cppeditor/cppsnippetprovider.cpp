qt-creator/qt-creator

commit d85a4f61550af6d42abbca61e2d2ad1102dc8d27

TextEditor: Move SnippetsEditor to new editor construction scheme
Change-Id: I10612d86b6a634e7370c1a29507f709132f4e54b
Reviewed-by: Christian Stenger <christian.stenger@digia.com>

src/plugins/texteditor/snippets/snippeteditor.cpp
@@ -31,10 +31,11 @@
 
 #include <texteditor/basetextdocument.h>
 #include <texteditor/texteditorconstants.h>
+#include <utils/qtcassert.h>
 
 #include <QFocusEvent>
 
-using namespace TextEditor;
+namespace TextEditor {
 
 /*!
     \class TextEditor::SnippetEditorWidget
@@ -45,15 +46,15 @@ using namespace TextEditor;
 
 SnippetEditor::SnippetEditor()
 {
-    setContext(Core::Context(Constants::SNIPPET_EDITOR_ID, Constants::C_TEXTEDITOR));
+    addContext(Constants::SNIPPET_EDITOR_ID);
+    setEditorCreator([]() { return new SnippetEditor; });
+    setWidgetCreator([]() { return new SnippetEditorWidget; });
+    setDocumentCreator([]() { return new BaseTextDocument(Constants::SNIPPET_EDITOR_ID); });
 }
 
 SnippetEditorWidget::SnippetEditorWidget(QWidget *parent)
     : BaseTextEditorWidget(parent)
 {
-    BaseTextDocumentPtr doc(new BaseTextDocument);
-    doc->setId(Constants::SNIPPET_EDITOR_ID);
-    setTextDocument(doc);
     setFrameStyle(QFrame::StyledPanel | QFrame::Sunken);
     setHighlightCurrentLine(false);
     setLineNumbersVisible(false);
@@ -76,5 +77,7 @@ void SnippetEditorWidget::focusOutEvent(QFocusEvent *event)
 
 BaseTextEditor *SnippetEditorWidget::createEditor()
 {
-    return new SnippetEditor;
+    QTC_ASSERT("should not happen anymore" && false, return 0);
 }
+
+} // namespace

src/plugins/texteditor/snippets/snippeteditor.h
@@ -47,8 +47,6 @@ class TEXTEDITOR_EXPORT SnippetEditor : public BaseTextEditor
 
 public:
     SnippetEditor();
-
-    Core::IEditor *duplicate() { return 0; }
 };
 
 class TEXTEDITOR_EXPORT SnippetEditorWidget : public BaseTextEditorWidget
@@ -56,7 +54,7 @@ class TEXTEDITOR_EXPORT SnippetEditorWidget : public BaseTextEditorWidget
     Q_OBJECT
 
 public:
-    SnippetEditorWidget(QWidget *parent);
+    SnippetEditorWidget(QWidget *parent = 0);
 
     void setSyntaxHighlighter(SyntaxHighlighter *highlighter);
 

commit ba5124052456601e0942a3c4ac48bfe1623c58f0

TextEditor: Fix snippet editor crash introduced in d85a4f615
The SnippetEditorWidget setup is unusual as it doesn't have
an associated SnippetEditorFactory/SnippetEditor creation chain
and requires manual intervention. This was lost in the
conversion to the new construction scheme.

Change-Id: I0919295603432e525c2abd0da762acc8207f996c
Reviewed-by: Christian Stenger <christian.stenger@digia.com>

src/plugins/cppeditor/cppsnippetprovider.cpp
@@ -62,7 +62,7 @@ QString CppSnippetProvider::displayName() const
 
 void CppSnippetProvider::decorateEditor(TextEditor::SnippetEditorWidget *editor) const
 {
-    editor->setSyntaxHighlighter(new CppHighlighter);
+    editor->textDocument()->setSyntaxHighlighter(new CppHighlighter);
     editor->textDocument()->setIndenter(new CppTools::CppQtStyleIndenter);
     editor->setAutoCompleter(new CppAutoCompleter);
 }

src/plugins/projectexplorer/environmentitemswidget.cpp
@@ -71,7 +71,6 @@ EnvironmentItemsWidget::EnvironmentItemsWidget(QWidget *parent) :
     QWidget(parent), d(new EnvironmentItemsWidgetPrivate)
 {
     d->m_editor = new TextEditor::SnippetEditorWidget(this);
-    d->m_editor->textDocument()->setFontSettings(TextEditor::TextEditorSettings::fontSettings());
     QVBoxLayout *layout = new QVBoxLayout(this);
     layout->addWidget(d->m_editor);
 }

src/plugins/qmljseditor/qmljssnippetprovider.cpp
@@ -64,7 +64,7 @@ QString QmlJSSnippetProvider::displayName() const
 
 void QmlJSSnippetProvider::decorateEditor(TextEditor::SnippetEditorWidget *editor) const
 {
-    editor->setSyntaxHighlighter(new Highlighter);
+    editor->textDocument()->setSyntaxHighlighter(new Highlighter);
     editor->textDocument()->setIndenter(new Indenter);
     editor->setAutoCompleter(new AutoCompleter);
 }

src/plugins/texteditor/basetexteditor.cpp
@@ -185,6 +185,47 @@ private:
     QSizeF m_size;
 };
 
+class TextEditExtraArea : public QWidget
+{
+public:
+    TextEditExtraArea(BaseTextEditorWidget *edit)
+        : QWidget(edit)
+    {
+        textEdit = edit;
+        setAutoFillBackground(true);
+    }
+
+protected:
+    QSize sizeHint() const {
+        return QSize(textEdit->extraAreaWidth(), 0);
+    }
+    void paintEvent(QPaintEvent *event) {
+        textEdit->extraAreaPaintEvent(event);
+    }
+    void mousePressEvent(QMouseEvent *event) {
+        textEdit->extraAreaMouseEvent(event);
+    }
+    void mouseMoveEvent(QMouseEvent *event) {
+        textEdit->extraAreaMouseEvent(event);
+    }
+    void mouseReleaseEvent(QMouseEvent *event) {
+        textEdit->extraAreaMouseEvent(event);
+    }
+    void leaveEvent(QEvent *event) {
+        textEdit->extraAreaLeaveEvent(event);
+    }
+    void contextMenuEvent(QContextMenuEvent *event) {
+        textEdit->extraAreaContextMenuEvent(event);
+    }
+
+    void wheelEvent(QWheelEvent *event) {
+        QCoreApplication::sendEvent(textEdit->viewport(), event);
+    }
+
+private:
+    BaseTextEditorWidget *textEdit;
+};
+
 class BaseTextEditorPrivate
 {
 public:
@@ -460,6 +501,9 @@ BaseTextEditorWidgetPrivate::BaseTextEditorWidgetPrivate(BaseTextEditorWidget *p
     aggregate->add(baseTextFind);
     aggregate->add(q);
 
+    m_extraArea = new TextEditExtraArea(q);
+    m_extraArea->setMouseTracking(true);
+
     m_stretchWidget = new QWidget;
     m_stretchWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
     m_toolBar = new QToolBar;
@@ -484,47 +528,6 @@ BaseTextEditorWidgetPrivate::BaseTextEditorWidgetPrivate(BaseTextEditorWidget *p
         }
     });
 }
-
-class TextEditExtraArea : public QWidget
-{
-public:
-    TextEditExtraArea(BaseTextEditorWidget *edit)
-        : QWidget(edit)
-    {
-        textEdit = edit;
-        setAutoFillBackground(true);
-    }
-
-protected:
-    QSize sizeHint() const {
-        return QSize(textEdit->extraAreaWidth(), 0);
-    }
-    void paintEvent(QPaintEvent *event) {
-        textEdit->extraAreaPaintEvent(event);
-    }
-    void mousePressEvent(QMouseEvent *event) {
-        textEdit->extraAreaMouseEvent(event);
-    }
-    void mouseMoveEvent(QMouseEvent *event) {
-        textEdit->extraAreaMouseEvent(event);
-    }
-    void mouseReleaseEvent(QMouseEvent *event) {
-        textEdit->extraAreaMouseEvent(event);
-    }
-    void leaveEvent(QEvent *event) {
-        textEdit->extraAreaLeaveEvent(event);
-    }
-    void contextMenuEvent(QContextMenuEvent *event) {
-        textEdit->extraAreaContextMenuEvent(event);
-    }
-
-    void wheelEvent(QWheelEvent *event) {
-        QCoreApplication::sendEvent(textEdit->viewport(), event);
-    }
-
-private:
-    BaseTextEditorWidget *textEdit;
-};
 
 } // namespace Internal
 
@@ -566,6 +569,9 @@ static const char kTextBlockMimeType[] = "application/vnd.qtcreator.blocktext";
 BaseTextEditorWidget::BaseTextEditorWidget(QWidget *parent)
     : QPlainTextEdit(parent)
 {
+    // "Needed", as the creation below triggers ChildEvents that are
+    // passed to this object's event() which uses 'd'.
+    d = 0;
     d = new BaseTextEditorWidgetPrivate(this);
 }
 
@@ -576,8 +582,6 @@ void BaseTextEditorWidget::setTextDocument(const QSharedPointer<BaseTextDocument
 
 void BaseTextEditorWidgetPrivate::ctor(const QSharedPointer<BaseTextDocument> &doc)
 {
-    m_extraArea = new TextEditExtraArea(q);
-    m_extraArea->setMouseTracking(true);
     q->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
 
     m_overlay = new TextEditorOverlay(q);
@@ -2482,12 +2486,9 @@ void BaseTextEditorWidget::convertPosition(int pos, int *line, int *column) cons
 
 bool BaseTextEditorWidget::event(QEvent *e)
 {
-#if QT_VERSION >= 0x050000
-    if (e->type() != QEvent::InputMethodQuery)
+    // FIXME: That's far too heavy, and triggers e.g for ChildEvent
+    if (d && e->type() != QEvent::InputMethodQuery)
         d->m_contentsChanged = false;
-#else
-    d->m_contentsChanged = false;
-#endif
     switch (e->type()) {
     case QEvent::ShortcutOverride:
         if (static_cast<QKeyEvent*>(e)->key() == Qt::Key_Escape && d->m_snippetOverlay->isVisible()) {

src/plugins/texteditor/codestyleeditor.cpp
@@ -54,7 +54,6 @@ CodeStyleEditor::CodeStyleEditor(ICodeStylePreferencesFactory *factory,
     CodeStyleSelectorWidget *selector = new CodeStyleSelectorWidget(factory, this);
     selector->setCodeStyle(codeStyle);
     m_preview = new SnippetEditorWidget(this);
-    m_preview->textDocument()->setFontSettings(TextEditorSettings::fontSettings());
     DisplaySettings displaySettings = m_preview->displaySettings();
     displaySettings.m_visualizeWhitespace = true;
     m_preview->setDisplaySettings(displaySettings);

src/plugins/texteditor/snippets/snippeteditor.cpp
@@ -31,6 +31,7 @@
 
 #include <texteditor/basetextdocument.h>
 #include <texteditor/texteditorconstants.h>
+#include <texteditor/texteditorsettings.h>
 #include <utils/qtcassert.h>
 
 #include <QFocusEvent>
@@ -44,26 +45,23 @@ namespace TextEditor {
     \ingroup Snippets
 */
 
-SnippetEditor::SnippetEditor()
-{
-    addContext(Constants::SNIPPET_EDITOR_ID);
-    setEditorCreator([]() { return new SnippetEditor; });
-    setWidgetCreator([]() { return new SnippetEditorWidget; });
-    setDocumentCreator([]() { return new BaseTextDocument(Constants::SNIPPET_EDITOR_ID); });
-}
 
 SnippetEditorWidget::SnippetEditorWidget(QWidget *parent)
     : BaseTextEditorWidget(parent)
 {
+    setSimpleTextDocument(TextEditor::Constants::SNIPPET_EDITOR_ID);
+    textDocument()->setFontSettings(TextEditorSettings::fontSettings());
+
+    // Should not be necessary in this case, but the base text editor
+    // implementation assumes a valid associated editor.
+    auto dummy = new BaseTextEditor;
+    dummy->addContext(Constants::SNIPPET_EDITOR_ID);
+    dummy->setEditorWidget(this);
+
     setFrameStyle(QFrame::StyledPanel | QFrame::Sunken);
     setHighlightCurrentLine(false);
     setLineNumbersVisible(false);
     setParenthesesMatchingEnabled(true);
-}
-
-void SnippetEditorWidget::setSyntaxHighlighter(TextEditor::SyntaxHighlighter *highlighter)
-{
-    textDocument()->setSyntaxHighlighter(highlighter);
 }
 
 void SnippetEditorWidget::focusOutEvent(QFocusEvent *event)

src/plugins/texteditor/snippets/snippeteditor.h
@@ -35,28 +35,12 @@
 
 namespace TextEditor {
 
-class SnippetEditorWidget;
-class SyntaxHighlighter;
-class Indenter;
-
-// Should not be necessary in this case, but the base text editor assumes a
-// valid editable interface.
-class TEXTEDITOR_EXPORT SnippetEditor : public BaseTextEditor
-{
-    Q_OBJECT
-
-public:
-    SnippetEditor();
-};
-
 class TEXTEDITOR_EXPORT SnippetEditorWidget : public BaseTextEditorWidget
 {
     Q_OBJECT
 
 public:
     SnippetEditorWidget(QWidget *parent = 0);
-
-    void setSyntaxHighlighter(SyntaxHighlighter *highlighter);
 
 signals:
     void snippetContentChanged();
@@ -68,6 +52,6 @@ protected:
     virtual BaseTextEditor *createEditor();
 };
 
-} // TextEditor
+} // namespace TextEditor
 
 #endif // SNIPPETEDITOR_H

src/plugins/texteditor/snippets/snippetssettingspage.cpp
@@ -36,6 +36,7 @@
 #include "ui_snippetssettingspage.h"
 
 #include <coreplugin/icore.h>
+#include <texteditor/texteditorconstants.h>
 #include <texteditor/texteditorsettings.h>
 #include <extensionsystem/pluginmanager.h>
 #include <utils/headerviewstretcher.h>
@@ -336,7 +337,6 @@ void SnippetsSettingsPagePrivate::configureUi(QWidget *w)
     foreach (ISnippetProvider *provider, providers) {
         m_ui.groupCombo->addItem(provider->displayName(), provider->groupId());
         SnippetEditorWidget *snippetEditor = new SnippetEditorWidget(w);
-        snippetEditor->textDocument()->setFontSettings(TextEditorSettings::fontSettings());
         provider->decorateEditor(snippetEditor);
         m_ui.snippetsEditorStack->insertWidget(m_ui.groupCombo->count() - 1, snippetEditor);
         connect(snippetEditor, SIGNAL(snippetContentChanged()), this, SLOT(setSnippetContent()));