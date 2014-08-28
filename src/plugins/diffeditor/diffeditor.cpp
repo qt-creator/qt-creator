qt-creator/qt-creator

commit c094592f8a70c8c6999d976343f304607f6b2dde

BaseTextEditor: Un-slot most widget actions
Change-Id: Id642fc518d03dad5202e0d7bbb6b8b6325abbbab
Reviewed-by: Christian Stenger <christian.stenger@digia.com>

src/plugins/texteditor/basetexteditor.h
@@ -392,7 +392,6 @@ public:
     enum Side { Left, Right };
     void insertExtraToolBarWidget(Side side, QWidget *widget);
 
-public slots:
     virtual void copy();
     virtual void paste();
     virtual void cut();
@@ -495,7 +494,8 @@ public slots:
 
     void configureMimeType(const QString &mimeType);
     void configureMimeType(const Core::MimeType &mimeType);
-    void inSnippetMode(bool *active);
+
+    Q_INVOKABLE void inSnippetMode(bool *active); // Used by FakeVim.
 
 signals:
     void assistFinished();

src/plugins/texteditor/basetexteditor.cpp
@@ -626,7 +626,7 @@ void BaseTextEditorWidgetPrivate::ctor(const QSharedPointer<BaseTextDocument> &d
     QObject::connect(q, &QPlainTextEdit::blockCountChanged,
                      this, &BaseTextEditorWidgetPrivate::slotUpdateExtraAreaWidth);
 
-    QObject::connect(q, &BaseTextEditorWidget::modificationChanged, m_extraArea,
+    QObject::connect(q, &QPlainTextEdit::modificationChanged, m_extraArea,
                      static_cast<void (QWidget::*)()>(&QWidget::update));
 
     QObject::connect(q, &QPlainTextEdit::cursorPositionChanged,
@@ -981,10 +981,10 @@ bool BaseTextEditorWidget::open(QString *errorString, const QString &fileName, c
         moveCursor(QTextCursor::Start);
         d->updateCannotDecodeInfo();
         if (d->m_fileEncodingLabel) {
-            connect(d->m_fileEncodingLabel, SIGNAL(clicked()), this,
-                    SLOT(selectEncoding()), Qt::UniqueConnection);
-            connect(d->m_document->document(), SIGNAL(modificationChanged(bool)), this,
-                    SLOT(updateTextCodecLabel()), Qt::UniqueConnection);
+            connect(d->m_fileEncodingLabel, &LineColumnLabel::clicked,
+                    this, &BaseTextEditorWidget::selectEncoding, Qt::UniqueConnection);
+            connect(d->m_document->document(), &QTextDocument::modificationChanged,
+                    this, &BaseTextEditorWidget::updateTextCodecLabel, Qt::UniqueConnection);
             updateTextCodecLabel();
         }
         return true;

commit 761f8de751e0d52105dba3df7473f10c0a884e45

TextEditor: Fix regression introduced by recent c094592f8a
Change-Id: I9fe2bc726454c8c44f59bf4c720a11765d1bb744
Reviewed-by: Nikita Baryshnikov <nib952051@gmail.com>
Reviewed-by: Christian Stenger <christian.stenger@digia.com>

src/plugins/diffeditor/diffeditor.cpp
@@ -229,14 +229,10 @@ DiffEditor::DiffEditor(const QSharedPointer<DiffEditorDocument> &doc)
 
     connect(m_descriptionWidget, SIGNAL(expandBranchesRequested()),
             m_document->controller(), SLOT(expandBranchesRequested()));
-    connect(TextEditorSettings::instance(),
-            SIGNAL(displaySettingsChanged(TextEditor::DisplaySettings)),
-            m_descriptionWidget,
-            SLOT(setDisplaySettings(TextEditor::DisplaySettings)));
-    connect(TextEditorSettings::instance(),
-            SIGNAL(fontSettingsChanged(TextEditor::FontSettings)),
-            m_descriptionWidget->textDocument(),
-            SLOT(setFontSettings(TextEditor::FontSettings)));
+    connect(TextEditorSettings::instance(), &TextEditorSettings::displaySettingsChanged,
+            m_descriptionWidget, &BaseTextEditorWidget::setDisplaySettings);
+    connect(TextEditorSettings::instance(), &TextEditorSettings::fontSettingsChanged,
+            m_descriptionWidget->textDocument(), &BaseTextDocument::setFontSettings);
 
     m_descriptionWidget->setDisplaySettings(
                 TextEditorSettings::displaySettings());

src/plugins/diffeditor/sidebysidediffeditorwidget.cpp
@@ -790,9 +790,8 @@ SideBySideDiffEditorWidget::SideBySideDiffEditorWidget(QWidget *parent)
     m_leftEditor = new SideDiffEditorWidget(this);
     m_leftEditor->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     m_leftEditor->setReadOnly(true);
-    connect(TextEditorSettings::instance(),
-            SIGNAL(displaySettingsChanged(TextEditor::DisplaySettings)),
-            m_leftEditor, SLOT(setDisplaySettings(TextEditor::DisplaySettings)));
+    connect(TextEditorSettings::instance(), &TextEditorSettings::displaySettingsChanged,
+            m_leftEditor, &SideDiffEditorWidget::setDisplaySettings);
     m_leftEditor->setDisplaySettings(TextEditorSettings::displaySettings());
     m_leftEditor->setCodeStyle(TextEditorSettings::codeStyle());
     connect(m_leftEditor, SIGNAL(jumpToOriginalFileRequested(int,int,int)),
@@ -803,9 +802,8 @@ SideBySideDiffEditorWidget::SideBySideDiffEditorWidget(QWidget *parent)
 
     m_rightEditor = new SideDiffEditorWidget(this);
     m_rightEditor->setReadOnly(true);
-    connect(TextEditorSettings::instance(),
-            SIGNAL(displaySettingsChanged(TextEditor::DisplaySettings)),
-            m_rightEditor, SLOT(setDisplaySettings(TextEditor::DisplaySettings)));
+    connect(TextEditorSettings::instance(), &TextEditorSettings::displaySettingsChanged,
+            m_rightEditor, &SideDiffEditorWidget::setDisplaySettings);
     m_rightEditor->setDisplaySettings(TextEditorSettings::displaySettings());
     m_rightEditor->setCodeStyle(TextEditorSettings::codeStyle());
     connect(m_rightEditor, SIGNAL(jumpToOriginalFileRequested(int,int,int)),

src/plugins/diffeditor/unifieddiffeditorwidget.cpp
@@ -91,9 +91,8 @@ UnifiedDiffEditorWidget::UnifiedDiffEditorWidget(QWidget *parent)
     SelectableTextEditorWidget::setDisplaySettings(settings);
 
     setReadOnly(true);
-    connect(TextEditorSettings::instance(),
-            SIGNAL(displaySettingsChanged(TextEditor::DisplaySettings)),
-            this, SLOT(setDisplaySettings(TextEditor::DisplaySettings)));
+    connect(TextEditorSettings::instance(), &TextEditorSettings::displaySettingsChanged,
+            this, &UnifiedDiffEditorWidget::setDisplaySettings);
     setDisplaySettings(TextEditorSettings::displaySettings());
     setCodeStyle(TextEditorSettings::codeStyle());
 

src/plugins/texteditor/basetexteditor.h
@@ -400,13 +400,14 @@ public:
     virtual void format();
     virtual void rewrapParagraph();
     virtual void unCommentSelection();
+
     virtual void setDisplaySettings(const TextEditor::DisplaySettings &);
-    virtual void setMarginSettings(const TextEditor::MarginSettings &);
-    virtual void setBehaviorSettings(const TextEditor::BehaviorSettings &);
-    virtual void setTypingSettings(const TextEditor::TypingSettings &);
-    virtual void setStorageSettings(const TextEditor::StorageSettings &);
-    virtual void setCompletionSettings(const TextEditor::CompletionSettings &);
-    virtual void setExtraEncodingSettings(const TextEditor::ExtraEncodingSettings &);
+    void setMarginSettings(const TextEditor::MarginSettings &);
+    void setBehaviorSettings(const TextEditor::BehaviorSettings &);
+    void setTypingSettings(const TextEditor::TypingSettings &);
+    void setStorageSettings(const TextEditor::StorageSettings &);
+    void setCompletionSettings(const TextEditor::CompletionSettings &);
+    void setExtraEncodingSettings(const TextEditor::ExtraEncodingSettings &);
 
     void circularPaste();
     void switchUtf8bom();