qt-creator/qt-creator

commit c655d7e598931f78b105cef700b77cbed124a289

CppEditor: Convert to new construction scheme
Change-Id: I947f76b4dbc3a197cd49993855c3b0f5e3dc7b49
Reviewed-by: Christian Stenger <christian.stenger@digia.com>

src/plugins/cppeditor/cppeditor.cpp
@@ -148,20 +148,25 @@ CppEditorWidgetPrivate::CppEditorWidgetPrivate(CppEditorWidget *q)
 {
 }
 
-CppEditorWidget::CppEditorWidget(TextEditor::BaseTextDocumentPtr doc, CppEditor *editor)
+CppEditorWidget::CppEditorWidget()
+   : d(new CppEditorWidgetPrivate(this))
 {
     qRegisterMetaType<SemanticInfo>("CppTools::SemanticInfo");
+}
 
-    editor->setEditorWidget(this);
+void CppEditorWidget::finalizeInitialization()
+{
+    d->m_cppEditorDocument = qobject_cast<CppEditorDocument *>(textDocument());
 
-    setTextDocument(doc);
-    d.reset(new CppEditorWidgetPrivate(this));
-    setAutoCompleter(new CppAutoCompleter);
     setLanguageSettingsId(CppTools::Constants::CPP_SETTINGS_ID);
     setCodeFoldingSupported(true);
     setMarksVisible(true);
     setParenthesesMatchingEnabled(true);
     setRevisionsVisible(true);
+
+    // function combo box sorting
+    connect(CppEditorPlugin::instance(), &CppEditorPlugin::outlineSortingChanged,
+            outline(), &CppEditorOutline::setSorted);
 
     connect(d->m_cppEditorDocument, &CppEditorDocument::codeWarningsUpdated,
             this, &CppEditorWidget::onCodeWarningsUpdated);
@@ -222,25 +227,17 @@ CppEditorWidget::CppEditorWidget(TextEditor::BaseTextDocumentPtr doc, CppEditor 
     connect(d->m_preprocessorButton, SIGNAL(clicked()), this, SLOT(showPreProcessorWidget()));
     insertExtraToolBarWidget(TextEditor::BaseTextEditorWidget::Left, d->m_preprocessorButton);
     insertExtraToolBarWidget(TextEditor::BaseTextEditorWidget::Left, d->m_cppEditorOutline->widget());
+
+//    updateSemanticInfo(semanticInfo());
+//    updateFunctionDeclDefLink();
+//    d->m_cppEditorOutline->update();
+//    const ExtraSelectionKind selectionKind = CodeWarningsSelection;
+//    setExtraSelections(selectionKind, extraSelections(selectionKind));
 }
 
 CppEditorWidget::~CppEditorWidget()
 {
     // non-inline destructor, see section "Forward Declared Pointers" of QScopedPointer.
-}
-
-CppEditorWidget *CppEditorWidget::duplicate(CppEditor *editor) const
-{
-    QTC_ASSERT(editor, return 0);
-
-    CppEditorWidget *widget = new CppEditorWidget(textDocumentPtr(), editor);
-    widget->updateSemanticInfo(semanticInfo());
-    widget->updateFunctionDeclDefLink();
-    widget->d->m_cppEditorOutline->update();
-    const ExtraSelectionKind selectionKind = CodeWarningsSelection;
-    widget->setExtraSelections(selectionKind, extraSelections(selectionKind));
-
-    return widget;
 }
 
 CppEditorDocument *CppEditorWidget::cppEditorDocument() const
@@ -562,15 +559,6 @@ void CppEditorWidget::keyPressEvent(QKeyEvent *e)
         return;
 
     TextEditor::BaseTextEditorWidget::keyPressEvent(e);
-}
-
-Core::IEditor *CppEditor::duplicate()
-{
-    CppEditor *editor = new CppEditor;
-    CppEditorWidget *widget = qobject_cast<CppEditorWidget *>(editorWidget())->duplicate(editor);
-    CppEditorPlugin::instance()->initializeEditor(widget);
-    editor->configureCodeAssistant();
-    return editor;
 }
 
 bool CppEditor::open(QString *errorString, const QString &fileName, const QString &realFileName)

src/plugins/cppeditor/cppeditor.h
@@ -57,8 +57,6 @@ class CppEditor : public TextEditor::BaseTextEditor
 public:
     CppEditor();
 
-    Core::IEditor *duplicate() QTC_OVERRIDE;
-
     bool open(QString *errorString,
               const QString &fileName,
               const QString &realFileName) QTC_OVERRIDE;
@@ -72,10 +70,10 @@ public:
     static Link linkToSymbol(CPlusPlus::Symbol *symbol);
 
 public:
-    CppEditorWidget(TextEditor::BaseTextDocumentPtr doc, CppEditor *editor);
+    CppEditorWidget();
     ~CppEditorWidget();
 
-    CppEditorWidget *duplicate(CppEditor *editor) const;
+    void finalizeInitialization();
 
     CppEditorDocument *cppEditorDocument() const;
     CppEditorOutline *outline() const;

src/plugins/cppeditor/cppeditorplugin.cpp
@@ -29,11 +29,12 @@
 
 #include "cppeditorplugin.h"
 
+#include "cppautocompleter.h"
 #include "cppclasswizard.h"
 #include "cppcodemodelinspectordialog.h"
 #include "cppeditorconstants.h"
-#include "cppeditordocument.h"
 #include "cppeditor.h"
+#include "cppeditordocument.h"
 #include "cppeditoroutline.h"
 #include "cppfilewizard.h"
 #include "cpphighlighter.h"
@@ -63,6 +64,7 @@
 #include <QStringList>
 
 using namespace Core;
+using namespace TextEditor;
 
 namespace CppEditor {
 namespace Internal {
@@ -73,37 +75,36 @@ enum { QUICKFIX_INTERVAL = 20 };
 
 //////////////////////////// CppEditorFactory /////////////////////////////
 
-CppEditorFactory::CppEditorFactory(CppEditorPlugin *owner) :
-    m_owner(owner)
+class CppEditorFactory : public BaseTextEditorFactory
 {
-    setId(Constants::CPPEDITOR_ID);
-    setDisplayName(qApp->translate("OpenWith::Editors", Constants::CPPEDITOR_DISPLAY_NAME));
-    addMimeType(Constants::C_SOURCE_MIMETYPE);
-    addMimeType(Constants::C_HEADER_MIMETYPE);
-    addMimeType(Constants::CPP_SOURCE_MIMETYPE);
-    addMimeType(Constants::CPP_HEADER_MIMETYPE);
+public:
+    CppEditorFactory()
+    {
+        setId(Constants::CPPEDITOR_ID);
+        setDisplayName(qApp->translate("OpenWith::Editors", Constants::CPPEDITOR_DISPLAY_NAME));
+        addMimeType(Constants::C_SOURCE_MIMETYPE);
+        addMimeType(Constants::C_HEADER_MIMETYPE);
+        addMimeType(Constants::CPP_SOURCE_MIMETYPE);
+        addMimeType(Constants::CPP_HEADER_MIMETYPE);
 
-    new TextEditor::TextEditorActionHandler(this, Constants::C_CPPEDITOR,
-        TextEditor::TextEditorActionHandler::Format
-        | TextEditor::TextEditorActionHandler::UnCommentSelection
-        | TextEditor::TextEditorActionHandler::UnCollapseAll
-        | TextEditor::TextEditorActionHandler::FollowSymbolUnderCursor);
+        setDocumentCreator([]() { return new CppEditorDocument; });
+        setEditorWidgetCreator([]() { return new CppEditorWidget; });
+        setEditorCreator([]() { return new CppEditor; });
+        setAutoCompleterCreator([]() { return new CppAutoCompleter; });
 
-    if (!Utils::HostOsInfo::isMacHost() && !Utils::HostOsInfo::isWindowsHost()) {
-        FileIconProvider::registerIconOverlayForMimeType(":/cppeditor/images/qt_cpp.png", Constants::CPP_SOURCE_MIMETYPE);
-        FileIconProvider::registerIconOverlayForMimeType(":/cppeditor/images/qt_c.png", Constants::C_SOURCE_MIMETYPE);
-        FileIconProvider::registerIconOverlayForMimeType(":/cppeditor/images/qt_h.png", Constants::CPP_HEADER_MIMETYPE);
+        setEditorActionHandlers(Constants::C_CPPEDITOR,
+                                TextEditorActionHandler::Format
+                              | TextEditorActionHandler::UnCommentSelection
+                              | TextEditorActionHandler::UnCollapseAll
+                              | TextEditorActionHandler::FollowSymbolUnderCursor);
+
+        if (!Utils::HostOsInfo::isMacHost() && !Utils::HostOsInfo::isWindowsHost()) {
+            FileIconProvider::registerIconOverlayForMimeType(":/cppeditor/images/qt_cpp.png", Constants::CPP_SOURCE_MIMETYPE);
+            FileIconProvider::registerIconOverlayForMimeType(":/cppeditor/images/qt_c.png", Constants::C_SOURCE_MIMETYPE);
+            FileIconProvider::registerIconOverlayForMimeType(":/cppeditor/images/qt_h.png", Constants::CPP_HEADER_MIMETYPE);
+        }
     }
-}
-
-IEditor *CppEditorFactory::createEditor()
-{
-    CppEditor *editor = new CppEditor;
-    CppEditorWidget *widget = new CppEditorWidget(BaseTextDocumentPtr(new CppEditorDocument), editor);
-    m_owner->initializeEditor(widget);
-    editor->configureCodeAssistant();
-    return editor;
-}
+};
 
 ///////////////////////////////// CppEditorPlugin //////////////////////////////////
 
@@ -131,13 +132,6 @@ CppEditorPlugin *CppEditorPlugin::instance()
     return m_instance;
 }
 
-void CppEditorPlugin::initializeEditor(CppEditorWidget *editor)
-{
-    // function combo box sorting
-    connect(this, SIGNAL(outlineSortingChanged(bool)),
-            editor->outline(), SLOT(setSorted(bool)));
-}
-
 void CppEditorPlugin::setSortedOutline(bool sorted)
 {
     m_sortedOutline = sorted;
@@ -159,7 +153,7 @@ bool CppEditorPlugin::initialize(const QStringList & /*arguments*/, QString *err
     if (!Core::MimeDatabase::addMimeTypes(QLatin1String(":/cppeditor/CppEditor.mimetypes.xml"), errorMessage))
         return false;
 
-    addAutoReleasedObject(new CppEditorFactory(this));
+    addAutoReleasedObject(new CppEditorFactory);
     addAutoReleasedObject(new CppHoverHandler);
     addAutoReleasedObject(new CppOutlineWidgetFactory);
     addAutoReleasedObject(new CppTypeHierarchyFactory);

src/plugins/cppeditor/cppeditorplugin.h
@@ -62,9 +62,6 @@ public:
     void extensionsInitialized();
     ShutdownFlag aboutToShutdown();
 
-    // Connect editor to settings changed signals.
-    void initializeEditor(CppEditorWidget *editor);
-
     bool sortedOutline() const;
 
     CppQuickFixAssistProvider *quickFixProvider() const;
@@ -248,19 +245,6 @@ private:
     QPointer<CppCodeModelInspectorDialog> m_cppCodeModelInspectorDialog;
 
     QPointer<TextEditor::BaseTextEditor> m_currentEditor;
-};
-
-class CppEditorFactory : public Core::IEditorFactory
-{
-    Q_OBJECT
-
-public:
-    CppEditorFactory(CppEditorPlugin *owner);
-
-    Core::IEditor *createEditor();
-
-private:
-    CppEditorPlugin *m_owner;
 };
 
 } // namespace Internal

commit e9e111ffa851cc8ff6580c89000ddbc8de02d7e4

CppEditor: Fix local renaming
CppLocalRenaming was connected to the initial QPlainTextEdit::document()
and not the "updated" QTextDocument set by
BaseTextEditorWidgetPrivate::setupDocumentSignals().

Broken by

    commit c655d7e598931f78b105cef700b77cbed124a289
    CppEditor: Convert to new construction scheme

Change-Id: I2e785a9d62a3bd39d9e613776cfc8e35dd9ff5d4
Reviewed-by: Christian Stenger <christian.stenger@digia.com>

src/plugins/cppeditor/cppeditor.cpp
@@ -187,6 +187,8 @@ void CppEditorWidget::finalizeInitialization()
                 d->m_lastSemanticInfo.localUses = localUses;
     });
 
+    connect(document(), SIGNAL(contentsChange(int,int,int)),
+            &d->m_localRenaming, SLOT(onContentsChangeOfEditorWidgetDocument(int,int,int)));
     connect(&d->m_localRenaming, &CppLocalRenaming::finished, [this] {
         cppEditorDocument()->semanticRehighlight();
     });

src/plugins/cppeditor/cpplocalrenaming.cpp
@@ -68,8 +68,6 @@ CppLocalRenaming::CppLocalRenaming(TextEditor::BaseTextEditorWidget *editorWidge
     , m_firstRenameChangeExpected(false)
 {
     forgetRenamingSelection();
-    connect(m_editorWidget->document(), SIGNAL(contentsChange(int,int,int)),
-            this, SLOT(onContentsChangeOfEditorWidgetDocument(int,int,int)));
 }
 
 void CppLocalRenaming::updateSelectionsForVariableUnderCursor(