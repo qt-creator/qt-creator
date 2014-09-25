qt-creator/qt-creator

commit ba42ceb0cb7827a9bbad29a1e08d4a25339ea57d

C++: nested class with enclosing template class
The parent of instantiation of nested class of template class
should be the instantiation of enclosing template class.

To prevent the infinite loop for case with local typedef of enclosing
template we should not change a parent of typedefed instatiation of
enclosing template. Example:
template <typename T>
struct Enclosing
{
  typedef Enclosing<T> EnclosingT;// first case
  struct Nested
  {
    typedef Enclosing<T> EnclosingT;// second case
  };
};

Task-number: QTCREATORBUG-11752
Task-number: QTCREATORBUG-11999
Change-Id: Iadd7b5ef73ee0c4881f59c9dabfe03339f55827b
Reviewed-by: Nikolai Kosjar <nikolai.kosjar@digia.com>

src/libs/cplusplus/LookupContext.cpp
@@ -88,6 +88,19 @@ static void path_helper(Symbol *symbol, QList<const Name *> *names)
     }
 }
 
+static bool isNestedInstantiationEnclosingTemplate(
+        ClassOrNamespace *nestedClassOrNamespaceInstantiation,
+        ClassOrNamespace *enclosingTemplateClassInstantiation)
+{
+    while (enclosingTemplateClassInstantiation) {
+        if (enclosingTemplateClassInstantiation == nestedClassOrNamespaceInstantiation)
+            return false;
+        enclosingTemplateClassInstantiation = enclosingTemplateClassInstantiation->parent();
+    }
+
+    return true;
+}
+
 namespace CPlusPlus {
 
 static inline bool compareName(const Name *name, const Name *other)
@@ -1282,6 +1295,10 @@ void ClassOrNamespace::NestedClassInstantiator::instantiate(ClassOrNamespace *en
             }
         }
 
+        if (isNestedInstantiationEnclosingTemplate(nestedClassOrNamespaceInstantiation,
+                                                   enclosingTemplateClass)) {
+            nestedClassOrNamespaceInstantiation->_parent = enclosingTemplateClassInstantiation;
+        }
         instantiate(nestedClassOrNamespace, nestedClassOrNamespaceInstantiation);
 
         enclosingTemplateClassInstantiation->_classOrNamespaces[nestedName] =

src/plugins/cpptools/cppcompletion_test.cpp
@@ -306,8 +306,6 @@ void CppToolsPlugin::test_completion()
     expectedCompletions.sort();
 
     QEXPECT_FAIL("enum_in_function_in_struct_in_function", "doesn't work", Abort);
-    QEXPECT_FAIL("nested_class_in_template_class_QTCREATORBUG-11752",
-                 "related to infiniteLoopLocalTypedef_QTCREATORBUG-11999", Abort);
     QCOMPARE(actualCompletions, expectedCompletions);
 }
 
@@ -2261,6 +2259,26 @@ void CppToolsPlugin::test_completion_data()
             "   @\n"
             "}\n"
         ) << _("n2.n1.t.") << (QStringList()
+            << QLatin1String("foo")
+            << QLatin1String("Foo"));
+
+    QTest::newRow("infiniteLoopLocalTypedef_QTCREATORBUG-11999") << _(
+            "template <typename T>\n"
+            "struct Temp\n"
+            "{\n"
+            "   struct Nested\n"
+            "   {\n"
+            "       typedef Temp<T> TempT;\n"
+            "       T t;\n"
+            "   };\n"
+            "   Nested nested;\n"
+            "};\n"
+            "struct Foo { int foo; };\n"
+            "void fun() {\n"
+            "   Temp<Foo> tempFoo;\n"
+            "   @\n"
+            "}\n"
+        ) << _("tempFoo.nested.t.") << (QStringList()
             << QLatin1String("foo")
             << QLatin1String("Foo"));
 

commit 4da91bef8dc3898c155b0b2047a7254a369654d0

C++: Fix endless loop in lookup on Windows
...for templates with typedefs referencing the respectively other
template (basic_ostream <-> ostreambuf_iterator).

Tested with MSVC 2013.

Regression introduced by ba42ceb0cb7827a9bbad29a1e08d4a25339ea57d.

Task-number: QTCREATORBUG-13064
Task-number: QTCREATORBUG-13065
Change-Id: I71a45c720663a73c3302eb7da731e6ad2d8f0fbd
Reviewed-by: Przemyslaw Gorszkowski <pgorszkowski@gmail.com>
Reviewed-by: Fawzi Mohamed <fawzi.mohamed@digia.com>

src/libs/cplusplus/LookupContext.cpp
@@ -92,7 +92,10 @@ static bool isNestedInstantiationEnclosingTemplate(
         ClassOrNamespace *nestedClassOrNamespaceInstantiation,
         ClassOrNamespace *enclosingTemplateClassInstantiation)
 {
-    while (enclosingTemplateClassInstantiation) {
+    QList<ClassOrNamespace *> processed;
+    while (enclosingTemplateClassInstantiation
+           && !processed.contains(enclosingTemplateClassInstantiation)) {
+        processed.append(enclosingTemplateClassInstantiation);
         if (enclosingTemplateClassInstantiation == nestedClassOrNamespaceInstantiation)
             return false;
         enclosingTemplateClassInstantiation = enclosingTemplateClassInstantiation->parent();

tests/auto/cplusplus/checksymbols/tst_checksymbols.cpp
@@ -191,6 +191,8 @@ private slots:
 
     void test_checksymbols_macroUses();
     void test_checksymbols_macroUses_data();
+
+    void test_checksymbols_infiniteLoop();
 };
 
 void tst_CheckSymbols::test_checksymbols()
@@ -1749,6 +1751,50 @@ void tst_CheckSymbols::test_checksymbols_macroUses_data()
         << (UseList()
             << Use(1, 9, 3, Highlighting::MacroUse)
             << Use(2, 11, 3, Highlighting::MacroUse));
+}
+
+void tst_CheckSymbols::test_checksymbols_infiniteLoop()
+{
+    const QByteArray source1 =
+        "#include \"file2.h\"\n"
+        "\n"
+        "template<class _Elem, class _Traits>\n"
+        "class basic_ios {\n"
+        "        typedef basic_ostream<_Elem, _Traits> _Myos;\n"
+        "};\n"
+        "\n"
+        "template<class _Elem, class _Traits>\n"
+        "class basic_ostream {\n"
+        "        typedef basic_ostream<_Elem, _Traits> _Myt;\n"
+        "        typedef ostreambuf_iterator<_Elem, _Traits> _Iter;\n"
+        "};\n"
+        ;
+    const QString filePath1 = QDir::tempPath() + QLatin1String("/file1.h");
+    CppTools::Tests::TestCase::writeFile(filePath1, source1);
+
+    const QByteArray source2 =
+        "template<class _Elem, class _Traits>\n"
+        "class basic_streambuf {\n"
+        "        typedef basic_streambuf<_Elem, _Traits> _Myt;\n"
+        "};\n"
+        "\n"
+        "template<class _Elem, class _Traits>\n"
+        "class ostreambuf_iterator {\n"
+        "    typedef _Traits traits_type;\n"
+        "        typedef basic_streambuf<_Elem, _Traits> streambuf_type;\n"
+        "        typedef basic_ostream<_Elem, _Traits> ostream_type;\n"
+        "};\n"
+        ;
+    const QString filePath2 = QDir::tempPath() + QLatin1String("/file2.h");
+    CppTools::Tests::TestCase::writeFile(filePath2, source2);
+
+    const Document::Ptr document1 = TestCase::createDocument(filePath1, source1);
+    document1->addIncludeFile(Document::Include("file2.h", filePath2, 1, Client::IncludeLocal));
+    Snapshot snapshot;
+    snapshot.insert(document1);
+    snapshot.insert(TestCase::createDocument(filePath2, source2));
+
+    TestCase::runCheckSymbols(document1, snapshot);
 }
 
 QTEST_APPLESS_MAIN(tst_CheckSymbols)