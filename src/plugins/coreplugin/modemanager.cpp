qt-creator/qt-creator

commit d52ee686d1ae63090e066faf4e2b66b90620b774

Save settings less aggressively.
Remove saving settings on auto-save, and only save settings when
switching away from projects mode, instead of any mode switch.

Change-Id: I3369b7f5ee785467d57c47e1a194a8ef989b98fb
Reviewed-by: hjk <hjk121@nokiamail.com>
Reviewed-by: Eike Ziller <eike.ziller@digia.com>

src/plugins/coreplugin/modemanager.cpp
@@ -72,7 +72,6 @@ struct ModeManagerPrivate
     QSignalMapper *m_signalMapper;
     Context m_addedContexts;
     int m_oldCurrent;
-    bool m_saveSettingsOnModeChange;
     bool m_modeSelectorVisible;
 };
 
@@ -100,15 +99,12 @@ ModeManager::ModeManager(Internal::MainWindow *mainWindow,
     d->m_oldCurrent = -1;
     d->m_actionBar = new Internal::FancyActionBar(modeStack);
     d->m_modeStack->addCornerWidget(d->m_actionBar);
-    d->m_saveSettingsOnModeChange = false;
     d->m_modeSelectorVisible = true;
     d->m_modeStack->setSelectionWidgetVisible(d->m_modeSelectorVisible);
 
     connect(d->m_modeStack, SIGNAL(currentAboutToShow(int)), SLOT(currentTabAboutToChange(int)));
     connect(d->m_modeStack, SIGNAL(currentChanged(int)), SLOT(currentTabChanged(int)));
     connect(d->m_signalMapper, SIGNAL(mapped(int)), this, SLOT(slotActivateMode(int)));
-    connect(ExtensionSystem::PluginManager::instance(), SIGNAL(initializationDone()), this, SLOT(handleStartup()));
-    connect(ICore::instance(), SIGNAL(coreAboutToClose()), this, SLOT(handleShutdown()));
 }
 
 void ModeManager::init()
@@ -239,12 +235,6 @@ void ModeManager::enabledStateChanged()
     }
 }
 
-void ModeManager::handleStartup()
-{ d->m_saveSettingsOnModeChange = true; }
-
-void ModeManager::handleShutdown()
-{ d->m_saveSettingsOnModeChange = false; }
-
 void ModeManager::aboutToRemoveObject(QObject *obj)
 {
     IMode *mode = Aggregation::query<IMode>(obj);
@@ -283,11 +273,8 @@ void ModeManager::currentTabAboutToChange(int index)
 {
     if (index >= 0) {
         IMode *mode = d->m_modes.at(index);
-        if (mode) {
-            if (d->m_saveSettingsOnModeChange)
-                ICore::saveSettings();
+        if (mode)
             emit currentModeAboutToChange(mode);
-        }
     }
 }
 

src/plugins/coreplugin/modemanager.h
@@ -82,8 +82,6 @@ private slots:
     void currentTabChanged(int index);
     void updateModeToolTip();
     void enabledStateChanged();
-    void handleStartup();
-    void handleShutdown();
 
 private:
     explicit ModeManager(Internal::MainWindow *mainWindow, Internal::FancyTabWidget *modeStack);

src/plugins/projectexplorer/projectexplorer.cpp
@@ -1497,7 +1497,8 @@ void ProjectExplorerPlugin::updateWelcomePage()
 
 void ProjectExplorerPlugin::currentModeChanged(IMode *mode, IMode *oldMode)
 {
-    Q_UNUSED(oldMode);
+    if (oldMode && oldMode->id() == ProjectExplorer::Constants::MODE_SESSION)
+        ICore::saveSettings();
     if (mode && mode->id() == Core::Constants::MODE_WELCOME)
         updateWelcomePage();
 }

src/plugins/coreplugin/editormanager/editormanager.cpp
@@ -1783,9 +1783,6 @@ void EditorManager::autoSave()
     if (!errors.isEmpty())
         QMessageBox::critical(ICore::mainWindow(), tr("File Error"),
                               errors.join(QLatin1String("\n")));
-
-    // Also save settings while accessing the disk anyway:
-    ICore::saveSettings();
 }
 
 MakeWritableResult EditorManager::makeFileWritable(IDocument *document)

src/plugins/coreplugin/icore.cpp
@@ -317,7 +317,7 @@ ICore::ICore(MainWindow *mainwindow)
     m_mainwindow = mainwindow;
     // Save settings once after all plugins are initialized:
     connect(ExtensionSystem::PluginManager::instance(), SIGNAL(initializationDone()),
-            this, SIGNAL(saveSettingsRequested()));
+            this, SLOT(saveSettings()));
 }
 
 ICore::~ICore()

src/plugins/coreplugin/icore.h
@@ -118,6 +118,7 @@ public:
 
     static void emitNewItemsDialogRequested();
 
+public slots:
     static void saveSettings();
 
 signals:

commit fe8ed7f5a666629ae43f8fc0888484a8bfa3b210

Session: Fix saving session data on shutdown from project mode
This was broken in d52ee686d1ae63090e066faf4e2b66b90620b774.

Task-number: QTCREATORBUG-13098
Change-Id: Ia075e13fdd00e561b78058a3cab0cb02d2eb5fe6
Reviewed-by: Eike Ziller <eike.ziller@digia.com>

src/plugins/coreplugin/modemanager.cpp
@@ -320,7 +320,7 @@ bool ModeManager::isModeSelectorVisible()
     return d->m_modeSelectorVisible;
 }
 
-QObject *ModeManager::instance()
+ModeManager *ModeManager::instance()
 {
     return m_instance;
 }

src/plugins/coreplugin/modemanager.h
@@ -52,7 +52,7 @@ class CORE_EXPORT ModeManager : public QObject
     Q_OBJECT
 
 public:
-    static QObject *instance();
+    static ModeManager *instance();
 
     static IMode *currentMode();
     static IMode *mode(Id id);

src/plugins/projectexplorer/projectexplorer.cpp
@@ -1277,6 +1277,8 @@ void ProjectExplorerPlugin::updateRunWithoutDeployMenu()
 
 ExtensionSystem::IPlugin::ShutdownFlag ProjectExplorerPlugin::aboutToShutdown()
 {
+    disconnect(ModeManager::instance(), &ModeManager::currentModeChanged,
+               this, &ProjectExplorerPlugin::currentModeChanged);
     d->m_proWindow->aboutToShutdown(); // disconnect from session
     SessionManager::closeAllProjects();
     d->m_projectsMode = 0;
@@ -1673,9 +1675,8 @@ void ProjectExplorerPlugin::restoreSession()
         SessionManager::loadSession(d->m_sessionToRestoreAtStartup);
 
     // update welcome page
-    connect(ModeManager::instance(),
-            SIGNAL(currentModeChanged(Core::IMode*,Core::IMode*)),
-            SLOT(currentModeChanged(Core::IMode*,Core::IMode*)));
+    connect(ModeManager::instance(), &ModeManager::currentModeChanged,
+               this, &ProjectExplorerPlugin::currentModeChanged);
 #if HAS_WELCOME_PAGE
     connect(d->m_welcomePage, SIGNAL(requestSession(QString)), this, SLOT(loadSession(QString)));
     connect(d->m_welcomePage, SIGNAL(requestProject(QString)), this, SLOT(openProjectWelcomePage(QString)));