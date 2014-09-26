qt-creator/qt-creator

commit fe8ed7f5a666629ae43f8fc0888484a8bfa3b210

Session: Fix saving session data on shutdown from project mode
This was broken in d52ee686d1ae63090e066faf4e2b66b90620b774.

Task-number: QTCREATORBUG-13098
Change-Id: Ia075e13fdd00e561b78058a3cab0cb02d2eb5fe6
Reviewed-by: Eike Ziller <eike.ziller@digia.com>

src/plugins/projectexplorer/projectexplorer.cpp
@@ -1277,6 +1277,8 @@ void ProjectExplorerPlugin::updateRunWithoutDeployMenu()
 
 ExtensionSystem::IPlugin::ShutdownFlag ProjectExplorerPlugin::aboutToShutdown()
 {
+    disconnect(ModeManager::instance(), &ModeManager::currentModeChanged,
+               this, &ProjectExplorerPlugin::currentModeChanged);
     d->m_proWindow->aboutToShutdown(); // disconnect from session
     SessionManager::closeAllProjects();
     d->m_projectsMode = 0;
@@ -1673,9 +1675,8 @@ void ProjectExplorerPlugin::restoreSession()
         SessionManager::loadSession(d->m_sessionToRestoreAtStartup);
 
     // update welcome page
-    connect(ModeManager::instance(),
-            SIGNAL(currentModeChanged(Core::IMode*,Core::IMode*)),
-            SLOT(currentModeChanged(Core::IMode*,Core::IMode*)));
+    connect(ModeManager::instance(), &ModeManager::currentModeChanged,
+               this, &ProjectExplorerPlugin::currentModeChanged);
 #if HAS_WELCOME_PAGE
     connect(d->m_welcomePage, SIGNAL(requestSession(QString)), this, SLOT(loadSession(QString)));
     connect(d->m_welcomePage, SIGNAL(requestProject(QString)), this, SLOT(openProjectWelcomePage(QString)));

src/plugins/coreplugin/modemanager.cpp
@@ -320,7 +320,7 @@ bool ModeManager::isModeSelectorVisible()
     return d->m_modeSelectorVisible;
 }
 
-QObject *ModeManager::instance()
+ModeManager *ModeManager::instance()
 {
     return m_instance;
 }

src/plugins/coreplugin/modemanager.h
@@ -52,7 +52,7 @@ class CORE_EXPORT ModeManager : public QObject
     Q_OBJECT
 
 public:
-    static QObject *instance();
+    static ModeManager *instance();
 
     static IMode *currentMode();
     static IMode *mode(Id id);

commit 9d3ae49e3014ffff59bf174412afaf9c7b800bd5

Session: Fix compilation
This was broken in fe8ed7f5a666629ae43f8fc0888484a8bfa3b210

Change-Id: Iff4dffa6ba6cfdeeb3664c026ce642b80e4deeeb
Reviewed-by: Daniel Teske <daniel.teske@digia.com>

src/plugins/projectexplorer/projectexplorer.cpp
@@ -1277,8 +1277,8 @@ void ProjectExplorerPlugin::updateRunWithoutDeployMenu()
 
 ExtensionSystem::IPlugin::ShutdownFlag ProjectExplorerPlugin::aboutToShutdown()
 {
-    disconnect(ModeManager::instance(), &ModeManager::currentModeChanged,
-               this, &ProjectExplorerPlugin::currentModeChanged);
+    disconnect(ModeManager::instance(), SIGNAL(currentModeChanged(Core::IMode*,Core::IMode*)),
+               this, SLOT(currentModeChanged(Core::IMode*,Core::IMode*)));
     d->m_proWindow->aboutToShutdown(); // disconnect from session
     SessionManager::closeAllProjects();
     d->m_projectsMode = 0;
@@ -1675,8 +1675,8 @@ void ProjectExplorerPlugin::restoreSession()
         SessionManager::loadSession(d->m_sessionToRestoreAtStartup);
 
     // update welcome page
-    connect(ModeManager::instance(), &ModeManager::currentModeChanged,
-               this, &ProjectExplorerPlugin::currentModeChanged);
+    connect(ModeManager::instance(), SIGNAL(currentModeChanged(Core::IMode*,Core::IMode*)),
+            this, SLOT(currentModeChanged(Core::IMode*,Core::IMode*)));
 #if HAS_WELCOME_PAGE
     connect(d->m_welcomePage, SIGNAL(requestSession(QString)), this, SLOT(loadSession(QString)));
     connect(d->m_welcomePage, SIGNAL(requestProject(QString)), this, SLOT(openProjectWelcomePage(QString)));