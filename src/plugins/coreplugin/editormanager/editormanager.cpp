qt-creator/qt-creator

commit fd9b2af9217ea295614b3061863c6b29080c42fe

Editors: Support drag & drop from bookmarks pane
Change-Id: I9b68d35b04968779f111885ba7a24f18fecfada5
Reviewed-by: Daniel Teske <daniel.teske@digia.com>

src/plugins/coreplugin/editormanager/editormanager.cpp
@@ -577,6 +577,19 @@ IEditor *EditorManagerPrivate::openEditor(EditorView *view, const QString &fileN
     return result;
 }
 
+IEditor *EditorManagerPrivate::openEditorAt(EditorView *view, const QString &fileName, int line,
+                                            int column, Id editorId,
+                                            EditorManager::OpenEditorFlags flags, bool *newEditor)
+{
+    EditorManager::cutForwardNavigationHistory();
+    EditorManager::addCurrentPositionToNavigationHistory();
+    EditorManager::OpenEditorFlags tempFlags = flags | EditorManager::IgnoreNavigationHistory;
+    Core::IEditor *editor = openEditor(view, fileName, editorId, tempFlags, newEditor);
+    if (editor && line != -1)
+        editor->gotoLine(line, column);
+    return editor;
+}
+
 IEditor *EditorManagerPrivate::activateEditorForDocument(EditorView *view, IDocument *document,
                                                          EditorManager::OpenEditorFlags flags)
 {
@@ -2222,14 +2235,8 @@ IEditor *EditorManager::openEditor(const QString &fileName, Id editorId,
 IEditor *EditorManager::openEditorAt(const QString &fileName, int line, int column,
                                      Id editorId, OpenEditorFlags flags, bool *newEditor)
 {
-    m_instance->cutForwardNavigationHistory();
-    m_instance->addCurrentPositionToNavigationHistory();
-    OpenEditorFlags tempFlags = flags | IgnoreNavigationHistory;
-    Core::IEditor *editor = Core::EditorManager::openEditor(fileName, editorId,
-            tempFlags, newEditor);
-    if (editor && line != -1)
-        editor->gotoLine(line, column);
-    return editor;
+    return EditorManagerPrivate::openEditorAt(EditorManagerPrivate::currentEditorView(),
+                                              fileName, line, column, editorId, flags, newEditor);
 }
 
 // Extract line number suffix. Return the suffix (e.g. ":132") and truncates the filename accordingly.

src/libs/utils/fileutils.cpp
@@ -40,7 +40,6 @@
 #include <QDragEnterEvent>
 #include <QDropEvent>
 #include <QMessageBox>
-#include <QMimeData>
 #include <QTimer>
 #include <QUrl>
 
@@ -705,8 +704,16 @@ FileName &FileName::appendString(QChar str)
     return *this;
 }
 
-static bool isDesktopFileManagerDrop(const QMimeData *d, QStringList *files = 0)
+static bool isFileDrop(const QMimeData *d, QList<FileDropSupport::FileSpec> *files = 0)
 {
+    // internal drop
+    if (const FileDropMimeData *internalData = qobject_cast<const FileDropMimeData *>(d)) {
+        if (files)
+            *files = internalData->files();
+        return true;
+    }
+
+    // external drop
     if (files)
         files->clear();
     // Extract dropped files from Mime data.
@@ -723,7 +730,7 @@ static bool isDesktopFileManagerDrop(const QMimeData *d, QStringList *files = 0)
         if (!fileName.isEmpty()) {
             hasFiles = true;
             if (files)
-                files->push_back(fileName);
+                files->append(FileDropSupport::FileSpec(fileName));
             else
                 break; // No result list, sufficient for checking
         }
@@ -745,27 +752,12 @@ QStringList FileDropSupport::mimeTypesForFilePaths()
     return QStringList() << QStringLiteral("text/uri-list");
 }
 
-QMimeData *FileDropSupport::mimeDataForFilePaths(const QStringList &filePaths)
-{
-    QList<QUrl> localUrls = Utils::transform(filePaths, [filePaths](const QString &path) {
-        return QUrl::fromLocalFile(path);
-    });
-    auto data = new QMimeData;
-    data->setUrls(localUrls);
-    return data;
-}
-
-QMimeData *FileDropSupport::mimeDataForFilePath(const QString &filePath)
-{
-    return mimeDataForFilePaths(QStringList() << filePath);
-}
-
 bool FileDropSupport::eventFilter(QObject *obj, QEvent *event)
 {
     Q_UNUSED(obj)
     if (event->type() == QEvent::DragEnter) {
         auto dee = static_cast<QDragEnterEvent *>(event);
-        if (isDesktopFileManagerDrop(dee->mimeData())
+        if (isFileDrop(dee->mimeData())
                 && (!m_filterFunction || m_filterFunction(dee)))
             event->accept();
         else
@@ -776,8 +768,8 @@ bool FileDropSupport::eventFilter(QObject *obj, QEvent *event)
         return true;
     } else if (event->type() == QEvent::Drop) {
         auto de = static_cast<QDropEvent *>(event);
-        QStringList tempFiles;
-        if (isDesktopFileManagerDrop(de->mimeData(), &tempFiles)
+        QList<FileSpec> tempFiles;
+        if (isFileDrop(de->mimeData(), &tempFiles)
                 && (!m_filterFunction || m_filterFunction(de))) {
             event->accept();
             de->acceptProposedAction();
@@ -798,6 +790,21 @@ void FileDropSupport::emitFilesDropped()
     QTC_ASSERT(!m_files.isEmpty(), return);
     emit filesDropped(m_files);
     m_files.clear();
+}
+
+void FileDropMimeData::addFile(const QString &filePath, int line, int column)
+{
+    // standard mime data
+    QList<QUrl> currentUrls = urls();
+    currentUrls.append(QUrl::fromLocalFile(filePath));
+    setUrls(currentUrls);
+    // special mime data
+    m_files.append(FileDropSupport::FileSpec(filePath, line, column));
+}
+
+QList<FileDropSupport::FileSpec> FileDropMimeData::files() const
+{
+    return m_files;
 }
 
 } // namespace Utils

src/libs/utils/fileutils.h
@@ -35,6 +35,7 @@
 #include <QCoreApplication>
 #include <QXmlStreamWriter> // Mac.
 #include <QMetaType>
+#include <QMimeData>
 #include <QStringList>
 
 #include <functional>
@@ -48,7 +49,6 @@ class QDir;
 class QDropEvent;
 class QFile;
 class QFileInfo;
-class QMimeData;
 class QTemporaryFile;
 class QTextStream;
 class QWidget;
@@ -202,18 +202,22 @@ class QTCREATOR_UTILS_EXPORT FileDropSupport : public QObject
 {
     Q_OBJECT
 public:
-     // returns true if the event should be accepted
+    struct FileSpec {
+        FileSpec(const QString &path, int r = -1, int c = -1) : filePath(path), line(r), column(c) {}
+        QString filePath;
+        int line;
+        int column;
+    };
+    // returns true if the event should be accepted
     typedef std::function<bool(QDropEvent*)> DropFilterFunction;
 
     FileDropSupport(QWidget *parentWidget, const DropFilterFunction &filterFunction
                     = DropFilterFunction());
 
     static QStringList mimeTypesForFilePaths();
-    static QMimeData *mimeDataForFilePaths(const QStringList &filePaths);
-    static QMimeData *mimeDataForFilePath(const QString &filePath);
 
 signals:
-    void filesDropped(const QStringList &files);
+    void filesDropped(const QList<Utils::FileDropSupport::FileSpec> &files);
 
 protected:
     bool eventFilter(QObject *obj, QEvent *event);
@@ -223,8 +227,19 @@ private slots:
 
 private:
     DropFilterFunction m_filterFunction;
-    QStringList m_files;
+    QList<FileSpec> m_files;
 
+};
+
+class QTCREATOR_UTILS_EXPORT FileDropMimeData : public QMimeData
+{
+    Q_OBJECT
+public:
+    void addFile(const QString &filePath, int line = -1, int column = -1);
+    QList<FileDropSupport::FileSpec> files() const;
+
+private:
+    QList<FileDropSupport::FileSpec> m_files;
 };
 
 } // namespace Utils

src/plugins/bookmarks/bookmarkmanager.cpp
@@ -229,6 +229,8 @@ BookmarkView::BookmarkView(BookmarkManager *manager)  :
     setSelectionModel(manager->selectionModel());
     setSelectionMode(QAbstractItemView::SingleSelection);
     setSelectionBehavior(QAbstractItemView::SelectRows);
+    setDragEnabled(true);
+    setDragDropMode(QAbstractItemView::DragOnly);
 
     connect(this, SIGNAL(clicked(QModelIndex)),
             this, SLOT(gotoBookmark(QModelIndex)));
@@ -406,6 +408,35 @@ QVariant BookmarkManager::data(const QModelIndex &index, int role) const
     if (role == Qt::ToolTipRole)
         return QDir::toNativeSeparators(bookMark->fileName());
     return QVariant();
+}
+
+Qt::ItemFlags BookmarkManager::flags(const QModelIndex &index) const
+{
+    if (!index.isValid() || index.column() !=0 || index.row() < 0 || index.row() >= m_bookmarksList.count())
+        return Qt::NoItemFlags;
+    return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsDragEnabled;
+}
+
+Qt::DropActions BookmarkManager::supportedDragActions() const
+{
+    return Qt::MoveAction;
+}
+
+QStringList BookmarkManager::mimeTypes() const
+{
+    return FileDropSupport::mimeTypesForFilePaths();
+}
+
+QMimeData *BookmarkManager::mimeData(const QModelIndexList &indexes) const
+{
+    auto data = new Utils::FileDropMimeData;
+    foreach (const QModelIndex &index, indexes) {
+        if (!index.isValid() || index.column() != 0 || index.row() < 0 || index.row() >= m_bookmarksList.count())
+            continue;
+        Bookmark *bookMark = m_bookmarksList.at(index.row());
+        data->addFile(bookMark->fileName(), bookMark->lineNumber());
+    }
+    return data;
 }
 
 void BookmarkManager::toggleBookmark()

src/plugins/bookmarks/bookmarkmanager.h
@@ -74,6 +74,11 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const;
     int columnCount(const QModelIndex &parent = QModelIndex()) const;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+    Qt::ItemFlags flags(const QModelIndex &index) const;
+
+    Qt::DropActions supportedDragActions() const;
+    QStringList mimeTypes() const;
+    QMimeData *mimeData(const QModelIndexList &indexes) const;
 
     // this QItemSelectionModel is shared by all views
     QItemSelectionModel *selectionModel() const;

src/plugins/coreplugin/editormanager/documentmodel.cpp
@@ -426,14 +426,14 @@ Qt::ItemFlags DocumentModelPrivate::flags(const QModelIndex &index) const
 
 QMimeData *DocumentModelPrivate::mimeData(const QModelIndexList &indexes) const
 {
-    QStringList filePaths;
+    auto data = new Utils::FileDropMimeData;
     foreach (const QModelIndex &index, indexes) {
         const DocumentModel::Entry *e = DocumentModel::entryAtRow(index.row());
         if (!e || e->fileName().isEmpty())
             continue;
-        filePaths.append(e->fileName());
+        data->addFile(e->fileName());
     }
-    return Utils::FileDropSupport::mimeDataForFilePaths(filePaths);
+    return data;
 }
 
 int DocumentModel::rowOfDocument(IDocument *document)

src/plugins/coreplugin/editormanager/editormanager_p.h
@@ -75,6 +75,13 @@ public:
                                Id editorId = Id(),
                                EditorManager::OpenEditorFlags flags = EditorManager::NoFlags,
                                bool *newEditor = 0);
+    static IEditor *openEditorAt(EditorView *view,
+                                 const QString &fileName,
+                                 int line,
+                                 int column = 0,
+                                 Id editorId = Id(),
+                                 EditorManager::OpenEditorFlags flags = EditorManager::NoFlags,
+                                 bool *newEditor = 0);
     static IEditor *duplicateEditor(IEditor *editor);
     static IEditor *activateEditor(EditorView *view, IEditor *editor,
                                    EditorManager::OpenEditorFlags flags = EditorManager::NoFlags);

src/plugins/coreplugin/editormanager/editorview.cpp
@@ -40,7 +40,6 @@
 #include <coreplugin/minisplitter.h>
 #include <coreplugin/editormanager/ieditor.h>
 #include <coreplugin/findplaceholder.h>
-#include <utils/fileutils.h>
 #include <utils/qtcassert.h>
 
 #include <QDebug>
@@ -125,8 +124,8 @@ EditorView::EditorView(SplitterOrView *parentSplitterOrView, QWidget *parent) :
     auto dropSupport = new Utils::FileDropSupport(this, [this](QDropEvent *event) {
         return event->source() != m_toolBar; // do not accept drops on ourselves
     });
-    connect(dropSupport, SIGNAL(filesDropped(QStringList)),
-            this, SLOT(openDroppedFiles(QStringList)));
+    connect(dropSupport, &Utils::FileDropSupport::filesDropped,
+            this, &EditorView::openDroppedFiles);
 
     updateNavigatorActions();
 }
@@ -350,11 +349,12 @@ void EditorView::closeSplit()
     EditorManagerPrivate::updateActions();
 }
 
-void EditorView::openDroppedFiles(const QStringList &files)
+void EditorView::openDroppedFiles(const QList<Utils::FileDropSupport::FileSpec> &files)
 {
     const int count = files.size();
     for (int i = 0; i < count; ++i) {
-        EditorManagerPrivate::openEditor(this, files.at(i), Id(),
+        const Utils::FileDropSupport::FileSpec spec = files.at(i);
+        EditorManagerPrivate::openEditorAt(this, spec.filePath, spec.line, spec.column, Id(),
                                   i < count - 1 ? EditorManager::DoNotChangeCurrentEditor
                                                   | EditorManager::DoNotMakeVisible
                                                 : EditorManager::NoFlags);

commit 6daeaf3bd62abe0ccce1a7ed5c5c48b96d09a249

Fix follow symbol in next split, and simplify code
Follow symbol in next split broke with fd9b2af9217ea295614b3061863c6b29080c42fe
because the flag was no longer handled through EditorManager::openEditorAt
Also remove the flag NoNewSplits which is no longer used, and simplify
how links are opened from the C++ editor.

Change-Id: I845cc3a0a43d82b79d5c46a273232b69fd6e3ea9
Reviewed-by: Eike Ziller <eike.ziller@digia.com>

src/plugins/coreplugin/editormanager/editormanager.cpp
@@ -2221,12 +2221,8 @@ EditorManager::ExternalEditorList
 IEditor *EditorManager::openEditor(const QString &fileName, Id editorId,
                                    OpenEditorFlags flags, bool *newEditor)
 {
-    if (flags & EditorManager::OpenInOtherSplit) {
-        if (flags & EditorManager::NoNewSplits)
-            EditorManagerPrivate::gotoNextSplit();
-        else
-            EditorManager::gotoOtherSplit();
-    }
+    if (flags & EditorManager::OpenInOtherSplit)
+        EditorManager::gotoOtherSplit();
 
     return EditorManagerPrivate::openEditor(EditorManagerPrivate::currentEditorView(),
                                             fileName, editorId, flags, newEditor);
@@ -2235,6 +2231,9 @@ IEditor *EditorManager::openEditor(const QString &fileName, Id editorId,
 IEditor *EditorManager::openEditorAt(const QString &fileName, int line, int column,
                                      Id editorId, OpenEditorFlags flags, bool *newEditor)
 {
+    if (flags & EditorManager::OpenInOtherSplit)
+        EditorManager::gotoOtherSplit();
+
     return EditorManagerPrivate::openEditorAt(EditorManagerPrivate::currentEditorView(),
                                               fileName, line, column, editorId, flags, newEditor);
 }
@@ -2297,12 +2296,8 @@ IEditor *EditorManager::openEditorWithContents(Id editorId,
     if (debugEditorManager)
         qDebug() << Q_FUNC_INFO << editorId.name() << titlePattern << contents;
 
-    if (flags & EditorManager::OpenInOtherSplit) {
-        if (flags & EditorManager::NoNewSplits)
-            EditorManagerPrivate::gotoNextSplit();
-        else
+    if (flags & EditorManager::OpenInOtherSplit)
             EditorManager::gotoOtherSplit();
-    }
 
     QApplication::setOverrideCursor(QCursor(Qt::WaitCursor));
 

src/plugins/coreplugin/editormanager/editormanager.h
@@ -105,8 +105,7 @@ public:
         IgnoreNavigationHistory = 2,
         DoNotMakeVisible = 4,
         CanContainLineNumber = 8,
-        OpenInOtherSplit = 16,
-        NoNewSplits = 32
+        OpenInOtherSplit = 16
     };
     Q_DECLARE_FLAGS(OpenEditorFlags, OpenEditorFlag)
 

src/plugins/cppeditor/cppeditor.cpp
@@ -417,7 +417,7 @@ void CppEditorWidget::switchDeclarationDefinition(bool inNextSplit)
 
     // Open Editor at link position
     if (symbolLink.hasValidTarget())
-        openCppEditorAt(symbolLink, inNextSplit != alwaysOpenLinksInNextSplit());
+        openLink(symbolLink, inNextSplit != alwaysOpenLinksInNextSplit());
 }
 
 CppEditorWidget::Link CppEditorWidget::findLinkAt(const QTextCursor &cursor, bool resolveTarget,
@@ -574,21 +574,6 @@ CppEditorWidget::Link CppEditorWidget::linkToSymbol(CPlusPlus::Symbol *symbol)
         column = 0;
 
     return Link(filename, line, column);
-}
-
-bool CppEditorWidget::openCppEditorAt(const Link &link, bool inNextSplit)
-{
-    if (!link.hasValidTarget())
-        return false;
-
-    EditorManager::OpenEditorFlags flags;
-    if (inNextSplit)
-        flags |= EditorManager::OpenInOtherSplit;
-    return EditorManager::openEditorAt(link.targetFileName,
-                                       link.targetLine,
-                                       link.targetColumn,
-                                       Constants::CPPEDITOR_ID,
-                                       flags);
 }
 
 void CppEditorWidget::updateSemanticInfo(const SemanticInfo &semanticInfo,

src/plugins/cppeditor/cppeditor.h
@@ -104,9 +104,6 @@ protected:
 
     void applyFontSettings() Q_DECL_OVERRIDE;
 
-    bool openLink(const Link &link, bool inNextSplit) Q_DECL_OVERRIDE
-    { return openCppEditorAt(link, inNextSplit); }
-
     Link findLinkAt(const QTextCursor &, bool resolveTarget = true,
                     bool inNextSplit = false) Q_DECL_OVERRIDE;
 
@@ -139,8 +136,6 @@ private slots:
 private:
     void finalizeInitialization() Q_DECL_OVERRIDE;
     void finalizeInitializationAfterDuplication(BaseTextEditorWidget *other) Q_DECL_OVERRIDE;
-
-    static bool openCppEditorAt(const Link &, bool inNextSplit = false);
 
     unsigned documentRevision() const;
 

src/plugins/texteditor/texteditor.cpp
@@ -5334,16 +5334,18 @@ bool BaseTextEditorWidget::openLink(const Link &link, bool inNextSplit)
     if (!link.hasValidTarget())
         return false;
 
-    if (inNextSplit) {
-        EditorManager::gotoOtherSplit();
-    } else if (textDocument()->filePath() == link.targetFileName) {
+    if (!inNextSplit && textDocument()->filePath() == link.targetFileName) {
         EditorManager::addCurrentPositionToNavigationHistory();
         gotoLine(link.targetLine, link.targetColumn);
         setFocus();
         return true;
     }
+    EditorManager::OpenEditorFlags flags;
+    if (inNextSplit)
+        flags |= EditorManager::OpenInOtherSplit;
 
-    return EditorManager::openEditorAt(link.targetFileName, link.targetLine, link.targetColumn);
+    return EditorManager::openEditorAt(link.targetFileName, link.targetLine, link.targetColumn,
+                                       Id(), flags);
 }
 
 void BaseTextEditorWidgetPrivate::updateLink(QMouseEvent *e)

src/plugins/texteditor/texteditor.h
@@ -580,10 +580,9 @@ protected:
                             bool inNextSplit = false);
 
     /*!
-       Reimplement this function if you want to customize the way a link is
-       opened. Returns whether the link was opened successfully.
+       Returns whether the link was opened successfully.
      */
-    virtual bool openLink(const Link &link, bool inNextSplit = false);
+    bool openLink(const Link &link, bool inNextSplit = false);
 
     /*!
       Reimplement this function to change the default replacement text.