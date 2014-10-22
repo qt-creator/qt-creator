qt-creator/qt-creator

commit 5003bc60d0ec8188e0f4b9458b821fb7ffb55de9

MacroExpander: Set the "legacy" global project values directly
This currently duplicates a few lines, but lets us address the
"better" context-specific case properly later.

Change-Id: I0e8249823d4f6d6e8f3a5f4c86b2cb0410ae3195
Reviewed-by: Tobias Hunger <tobias.hunger@theqtcompany.com>

src/plugins/projectexplorer/projectexplorer.cpp
@@ -120,6 +120,7 @@
 #include <coreplugin/fileutils.h>
 #include <coreplugin/removefiledialog.h>
 #include <texteditor/findinfiles.h>
+#include <ssh/sshconnection.h>
 
 #include <utils/algorithm.h>
 #include <utils/fileutils.h>
@@ -179,6 +180,12 @@ static BuildConfiguration *activeBuildConfiguration()
 {
     Target *target = activeTarget();
     return target ? target->activeBuildConfiguration() : 0;
+}
+
+static Kit *currentKit()
+{
+    Target *target = activeTarget();
+    return target ? target->kit() : 0;
 }
 
 class ProjectExplorerPluginPrivate : public QObject
@@ -344,27 +351,6 @@ ProjectExplorerPlugin::~ProjectExplorerPlugin()
 ProjectExplorerPlugin *ProjectExplorerPlugin::instance()
 {
     return m_instance;
-}
-
-static QString variableValue(const char *variable)
-{
-    QString projectName;
-    QString projectFilePath;
-    Kit *kit = 0;
-    QString buildConfigurationName;
-    if (Project *project = ProjectExplorerPlugin::currentProject()) {
-        projectName = project->displayName();
-        if (IDocument *doc = project->document())
-            projectFilePath = doc->filePath();
-        if (BuildConfiguration *buildConfiguration = activeBuildConfiguration())
-            buildConfigurationName = buildConfiguration->displayName();
-        if (project->activeTarget())
-            kit = project->activeTarget()->kit();
-    }
-    ProjectMacroExpander expander(projectFilePath, projectName, kit, buildConfigurationName);
-    QString result;
-    expander.resolveMacro(QString::fromUtf8(variable), &result);
-    return result;
 }
 
 bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *error)
@@ -1136,6 +1122,9 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
 
     updateWelcomePage();
 
+    // FIXME: These are mostly "legacy"/"convenience" entries, relying on
+    // the global entry point ProjectExplorer::currentProject(). They should
+    // not be used in the Run/Build configuration pages.
     Utils::MacroExpander *expander = Utils::globalMacroExpander();
     expander->registerFileVariables(Constants::VAR_CURRENTPROJECT_PREFIX,
         tr("Current project's main file"),
@@ -1156,40 +1145,73 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
 
     expander->registerVariable(Constants::VAR_CURRENTPROJECT_NAME,
         tr("The current project's name."),
-        []() { return variableValue(Constants::VAR_CURRENTPROJECT_NAME); });
+        []() -> QString {
+            Project *project = ProjectExplorerPlugin::currentProject();
+            return project ? project->displayName() : QString();
+        });
 
     expander->registerVariable(Constants::VAR_CURRENTKIT_NAME,
         tr("The currently active kit's name."),
-        []() { return variableValue(Constants::VAR_CURRENTKIT_NAME); });
+        []() -> QString {
+            Kit *kit = currentKit();
+            return kit ? kit->displayName() : QString();
+        });
 
     expander->registerVariable(Constants::VAR_CURRENTKIT_FILESYSTEMNAME,
         tr("The currently active kit's name in a filesystem friendly version."),
-        []() { return variableValue(Constants::VAR_CURRENTKIT_FILESYSTEMNAME); });
+        []() -> QString {
+            Kit *kit = currentKit();
+            return kit ? kit->fileSystemFriendlyName() : QString();
+        });
 
     expander->registerVariable(Constants::VAR_CURRENTKIT_ID,
         tr("The currently active kit's id."),
-        []() { return variableValue(Constants::VAR_CURRENTKIT_ID); });
+        []() -> QString {
+            Kit *kit = currentKit();
+            return kit ? kit->id().toString() : QString();
+        });
 
     expander->registerVariable(Constants::VAR_CURRENTDEVICE_HOSTADDRESS,
         tr("The host address of the device in the currently active kit."),
-        []() { return variableValue(Constants::VAR_CURRENTDEVICE_HOSTADDRESS); });
+        []() -> QString {
+            Kit *kit = currentKit();
+            const IDevice::ConstPtr device = DeviceKitInformation::device(kit);
+            return device ? device->sshParameters().host : QString();
+        });
 
     expander->registerVariable(Constants::VAR_CURRENTDEVICE_SSHPORT,
         tr("The SSH port of the device in the currently active kit."),
-        []() { return variableValue(Constants::VAR_CURRENTDEVICE_SSHPORT); });
+        []() -> QString {
+            Kit *kit = currentKit();
+            const IDevice::ConstPtr device = DeviceKitInformation::device(kit);
+            return device ? QString::number(device->sshParameters().port) : QString();
+        });
 
     expander->registerVariable(Constants::VAR_CURRENTDEVICE_USERNAME,
         tr("The username with which to log into the device in the currently active kit."),
-        []() { return variableValue(Constants::VAR_CURRENTDEVICE_USERNAME); });
+        []() -> QString {
+            Kit *kit = currentKit();
+            const IDevice::ConstPtr device = DeviceKitInformation::device(kit);
+            return device ? device->sshParameters().userName : QString();
+        });
+
 
     expander->registerVariable(Constants::VAR_CURRENTDEVICE_PRIVATEKEYFILE,
         tr("The private key file with which to authenticate when logging into the device "
            "in the currently active kit."),
-        []() { return variableValue(Constants::VAR_CURRENTDEVICE_PRIVATEKEYFILE); });
+        []() -> QString {
+            Kit *kit = currentKit();
+            const IDevice::ConstPtr device = DeviceKitInformation::device(kit);
+            return device ? device->sshParameters().privateKeyFile : QString();
+        });
 
     expander->registerVariable(Constants::VAR_CURRENTBUILD_NAME,
         tr("The currently active build configuration's name."),
-        []() { return variableValue(Constants::VAR_CURRENTBUILD_NAME); });
+        [&]() -> QString {
+            BuildConfiguration *bc = activeBuildConfiguration();
+            return bc ? bc->displayName() : QString();
+        });
+
 
     expander->registerVariable(Constants::VAR_CURRENTBUILD_TYPE,
         tr("The currently active build configuration's type."),

commit 63e6612f17c1e09be4e3f182bfe08ac50394ae08

ProjectExplorer: Fix compile for older gcc
Issue was introduced in 5003bc60d0ec8188e0f4b9458b821fb7ffb55de9;

Change-Id: Ibe016f884e4eea8093173ced497ee4c62f1265c3
Reviewed-by: hjk <hjk121@nokiamail.com>

src/plugins/projectexplorer/projectexplorer.cpp
@@ -1144,7 +1144,7 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
 
     expander->registerVariable(Constants::VAR_CURRENTPROJECT_NAME,
         tr("The current project's name."),
-        []() -> QString {
+        [this]() -> QString {
             Project *project = ProjectExplorerPlugin::currentProject();
             return project ? project->displayName() : QString();
         });