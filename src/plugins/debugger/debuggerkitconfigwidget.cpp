qt-creator/qt-creator

commit cabf14ba3003e57cce2917f8fad50a09d3967f43

Debugger: Use Qt5-style connects in DebuggerKitConfigWidget
Change-Id: I1cac24387cc74ade9113b4ecbe503871e9c6150b
Reviewed-by: hjk <hjk121@nokiamail.com>

src/plugins/debugger/debuggerkitconfigwidget.cpp
@@ -81,19 +81,21 @@ DebuggerKitConfigWidget::DebuggerKitConfigWidget(Kit *workingCopy, const KitInfo
         m_comboBox->addItem(item.displayName(), item.id());
 
     refresh();
-    connect(m_comboBox, SIGNAL(currentIndexChanged(int)), this, SLOT(currentDebuggerChanged(int)));
+    connect(m_comboBox, &QComboBox::currentIndexChanged,
+            this, &DebuggerKitConfigWidget::currentDebuggerChanged);
 
     m_manageButton = new QPushButton(KitConfigWidget::msgManage());
     m_manageButton->setContentsMargins(0, 0, 0, 0);
-    connect(m_manageButton, SIGNAL(clicked()), this, SLOT(manageDebuggers()));
+    connect(m_manageButton, &QAbstractButton::clicked,
+            this, &DebuggerKitConfigWidget::manageDebuggers);
 
-    QObject *manager = DebuggerItemManager::instance();
-    connect(manager, SIGNAL(debuggerAdded(QVariant)),
-            this, SLOT(onDebuggerAdded(QVariant)));
-    connect(manager, SIGNAL(debuggerUpdated(QVariant)),
-            this, SLOT(onDebuggerUpdated(QVariant)));
-    connect(manager, SIGNAL(debuggerRemoved(QVariant)),
-            this, SLOT(onDebuggerRemoved(QVariant)));
+    DebuggerItemManager *manager = DebuggerItemManager::instance();
+    connect(manager, &DebuggerItemManager::debuggerAdded,
+            this, &DebuggerKitConfigWidget::onDebuggerAdded);
+    connect(manager, &DebuggerItemManager::debuggerUpdated,
+            this, &DebuggerKitConfigWidget::onDebuggerUpdated);
+    connect(manager, &DebuggerItemManager::debuggerRemoved,
+            this, &DebuggerKitConfigWidget::onDebuggerRemoved);
 }
 
 DebuggerKitConfigWidget::~DebuggerKitConfigWidget()

src/plugins/debugger/debuggeritemmanager.cpp
@@ -123,7 +123,7 @@ DebuggerItemManager::DebuggerItemManager(QObject *parent)
             this, SLOT(saveDebuggers()));
 }
 
-QObject *DebuggerItemManager::instance()
+DebuggerItemManager *DebuggerItemManager::instance()
 {
     return m_instance;
 }

src/plugins/debugger/debuggeritemmanager.h
@@ -54,7 +54,7 @@ class DEBUGGER_EXPORT DebuggerItemManager : public QObject
     Q_OBJECT
 
 public:
-    static QObject *instance();
+    static DebuggerItemManager *instance();
     ~DebuggerItemManager();
 
     static QList<DebuggerItem> debuggers();

commit 4b44d2c6c52d2ad669a53470f06b104a29e72c24

Fix MSVC2010 compilation
Broken in cabf14ba3003e57cce2917f8fad50a09d3967f43

Change-Id: I83da1254ceb4636f2b75f5ef75c9f790fd13f140
Reviewed-by: Nikolai Kosjar <nikolai.kosjar@theqtcompany.com>

src/plugins/debugger/debuggerkitconfigwidget.cpp
@@ -81,7 +81,7 @@ DebuggerKitConfigWidget::DebuggerKitConfigWidget(Kit *workingCopy, const KitInfo
         m_comboBox->addItem(item.displayName(), item.id());
 
     refresh();
-    connect(m_comboBox, &QComboBox::currentIndexChanged,
+    connect(m_comboBox, static_cast<void (QComboBox::*)(int)>(&QComboBox::currentIndexChanged),
             this, &DebuggerKitConfigWidget::currentDebuggerChanged);
 
     m_manageButton = new QPushButton(KitConfigWidget::msgManage());