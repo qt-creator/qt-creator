qt-creator/qt-creator

commit 3bafbe710f1e1745c0a70031cd1a6a2350addc5f

Debugger: Fix Android startup
Change-Id: I495412434ccffdb33528bbbe030c7a4bfa7e387c
Reviewed-by: hjk <hjk121@nokiamail.com>

src/plugins/debugger/debuggerengine.cpp
@@ -846,6 +846,21 @@ void DebuggerEngine::notifyEngineRemoteSetupFinished(const RemoteSetupResult &re
 
         if (d->remoteSetupState() != RemoteSetupCancelled)
             d->setRemoteSetupState(RemoteSetupSucceeded);
+
+        if (result.gdbServerPort != InvalidPid) {
+            QString &rc = d->m_startParameters.remoteChannel;
+            const int sepIndex = rc.lastIndexOf(QLatin1Char(':'));
+            if (sepIndex != -1) {
+                rc.replace(sepIndex + 1, rc.count() - sepIndex - 1,
+                           QString::number(result.gdbServerPort));
+            }
+        }
+
+        if (result.qmlServerPort != InvalidPort) {
+            d->m_startParameters.qmlServerPort = result.qmlServerPort;
+            d->m_startParameters.processArgs.replace(_("%qml_port%"), QString::number(result.qmlServerPort));
+        }
+
     } else {
         showMessage(_("NOTE: REMOTE SETUP FAILED: ") + result.reason);
     }

src/plugins/debugger/gdb/remotegdbserveradapter.cpp
@@ -476,6 +476,7 @@ void GdbRemoteServerEngine::notifyEngineRemoteServerRunning
     m_isMulti = true;
     m_targetPid = inferiorPid;
     m_serverChannel = serverChannel;
+    m_startAttempted = true;
     startGdb();
 }
 
@@ -489,27 +490,13 @@ void GdbRemoteServerEngine::notifyEngineRemoteSetupFinished(const RemoteSetupRes
         return;
     }
 
-    DebuggerStartParameters &params = isMasterEngine()
-            ? startParameters()  : masterEngine()->startParameters();
-    if (result.gdbServerPort != -1) {
-        QString &rc = params.remoteChannel;
-        const int sepIndex = rc.lastIndexOf(QLatin1Char(':'));
-        if (sepIndex != -1) {
-            rc.replace(sepIndex + 1, rc.count() - sepIndex - 1,
-                       QString::number(result.gdbServerPort));
-        }
-    }
-    if (result.qmlServerPort != -1) {
-        params.qmlServerPort = result.qmlServerPort;
-        params.processArgs.replace(_("%qml_port%"), QString::number(result.qmlServerPort));
-    }
-
     // TODO: Aren't these redundant?
-    m_isMulti = params.multiProcess;
+    m_isMulti = startParameters().multiProcess;
     m_targetPid = -1;
-    m_serverChannel = params.remoteChannel.toLatin1();
+    m_serverChannel = startParameters().remoteChannel.toLatin1();
 
-    startGdb();
+    if (!m_startAttempted)
+        startGdb();
 }
 
 } // namespace Internal

src/plugins/debugger/gdb/remotegdbserveradapter.h
@@ -91,6 +91,7 @@ private:
 
     QProcess m_uploadProc;
     bool m_isMulti;
+    bool m_startAttempted;
     int m_targetPid;
     QByteArray m_serverChannel;
 };

src/plugins/debugger/lldb/lldbengine.cpp
@@ -1342,7 +1342,9 @@ void LldbEngine::notifyEngineRemoteSetupFinished(const RemoteSetupResult &result
     QTC_ASSERT(state() == EngineSetupRequested, qDebug() << state());
     DebuggerEngine::notifyEngineRemoteSetupFinished(result);
 
-    if (!result.success) {
+    if (result.success) {
+        startLldb();
+    } else {
         showMessage(_("ADAPTER START FAILED"));
         if (!result.reason.isEmpty()) {
             const QString title = tr("Adapter start failed");
@@ -1351,21 +1353,6 @@ void LldbEngine::notifyEngineRemoteSetupFinished(const RemoteSetupResult &result
         notifyEngineSetupFailed();
         return;
     }
-
-    if (result.qmlServerPort != InvalidPort)
-        startParameters().qmlServerPort = result.qmlServerPort;
-    if (result.inferiorPid != InvalidPid) {
-        if (startParameters().startMode == AttachExternal) {
-            startParameters().attachPID = result.inferiorPid;
-        } else {
-            QString &rc = startParameters().remoteChannel;
-            const int sepIndex = rc.lastIndexOf(QLatin1Char(':'));
-            if (sepIndex != -1)
-                rc.replace(sepIndex + 1, rc.count() - sepIndex - 1,
-                           QString::number(result.inferiorPid));
-        }
-    }
-    startLldb();
 }
 
 ///////////////////////////////////////////////////////////////////////

commit 44ba5ce3fd8c76ba7c6ede5828fde430a843dcb7

Fix debugging in iOS Simulator
Broke with 3bafbe710f1e1745c0a70031cd1a6a2350addc5f
We need to have the attachPid set with the inferiorPid after we got it
from the iossim tool.

Task-number: QTCREATORBUG-13261
Change-Id: I4f876817bed5566b5025d23416dd85676bc2e68b
Reviewed-by: Fawzi Mohamed <fawzi.mohamed@digia.com>

src/plugins/debugger/debuggerengine.cpp
@@ -866,6 +866,9 @@ void DebuggerEngine::notifyEngineRemoteSetupFinished(const RemoteSetupResult &re
                 rc.replace(sepIndex + 1, rc.count() - sepIndex - 1,
                            QString::number(result.gdbServerPort));
             }
+        } else if (result.inferiorPid != InvalidPid && startParameters().startMode == AttachExternal) {
+            // e.g. iOS Simulator
+            startParameters().attachPID = result.inferiorPid;
         }
 
         if (result.qmlServerPort != InvalidPort) {