86b5b5c2f39f58953f312658534fa918cc775299
Tobias Hunger <tobias.hunger@theqtcompany.com>
2015-02-24 12:51:17 +0100

    VcsBaseSubmitEditor: Remove queued connection
    Those should no longer be necessary and some people report warnings
    about queueing Qt::ApplicationState when doing commits in creator.
    
    Change-Id: I7bf1be57b8a92212ef677d1a608c0f22d20cab8b
    Reviewed-by: Orgad Shaneh <orgads@gmail.com>

--- src/plugins/vcsbase/vcsbasesubmiteditor.cpp
+++ src/plugins/vcsbase/vcsbasesubmiteditor.cpp
@@ -236,17 +236,16 @@ VcsBaseSubmitEditor::VcsBaseSubmitEditor(const VcsBaseSubmitEditorParameters *pa
     slotUpdateEditorSettings(settings);
     connect(VcsPlugin::instance(), &VcsPlugin::settingsChanged,
             this, &VcsBaseSubmitEditor::slotUpdateEditorSettings);
-    // Commit data refresh might lead to closing the editor, so use a queued connection
     connect(Core::EditorManager::instance(), &Core::EditorManager::currentEditorChanged,
             this, [this]() {
                 if (Core::EditorManager::currentEditor() == this)
                     updateFileModel();
-            }, Qt::QueuedConnection);
+            });
     connect(qApp, &QApplication::applicationStateChanged,
             this, [this](Qt::ApplicationState state) {
                 if (state == Qt::ApplicationActive)
                     updateFileModel();
-            }, Qt::QueuedConnection);
+            });
 
     auto aggregate = new Aggregation::Aggregate;
     aggregate->add(new Core::BaseTextFind(descriptionEdit));
