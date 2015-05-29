qt-creator/qt-creator

commit fa28f1cb303d79ff3feb1416ef0637785dade8fc

Disable run buttons until tests are found
Change-Id: I418b63dbdd4a641fa02a61b792ec6dbfafe39749
Reviewed-by: Christian Stenger <christian.stenger@theqtcompany.com>

plugins/autotest/testresultspane.cpp
@@ -100,11 +100,13 @@ void TestResultsPane::createToolButtons()
     m_runAll = new QToolButton(m_treeView);
     m_runAll->setIcon(QIcon(QLatin1String(":/images/run.png")));
     m_runAll->setToolTip(tr("Run All Tests"));
+    m_runAll->setEnabled(false);
     connect(m_runAll, &QToolButton::clicked, this, &TestResultsPane::onRunAllTriggered);
 
     m_runSelected = new QToolButton(m_treeView);
     m_runSelected->setIcon(QIcon(QLatin1String(":/images/runselected.png")));
     m_runSelected->setToolTip(tr("Run Selected Tests"));
+    m_runSelected->setEnabled(false);
     connect(m_runSelected, &QToolButton::clicked, this, &TestResultsPane::onRunSelectedTriggered);
 
     m_stopTestRun = new QToolButton(m_treeView);

commit 21afdf227aeec4a5d4cfe8d32588cce25d0e4cf5

Fix wrong state of Run/Run All buttons
Introduced with fa28f1cb303d79ff3feb1416ef0637785dade8fc

Change-Id: I80f5e08bcf2b630fcf7a4a77d47db6e7b7d26e50
Reviewed-by: Robert Loehning <robert.loehning@theqtcompany.com>

plugins/autotest/testresultspane.cpp
@@ -190,6 +190,8 @@ void TestResultsPane::visibilityChanged(bool visible)
             return;
         connect(TestTreeModel::instance(), &TestTreeModel::testTreeModelChanged,
                 this, &TestResultsPane::onTestTreeModelChanged);
+        // make sure run/run all are in correct state
+        onTestTreeModelChanged();
         m_wasVisibleBefore = true;
         TestTreeModel::instance()->enableParsing();
     } else {