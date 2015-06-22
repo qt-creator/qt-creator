e228c11a21f2ad3213cf367966f6c47a06ce7b5e
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2015-06-22 12:29:40 +0300

    Debugger: Remove false soft assertion on Windows
    Some libraries do not provide full names.
    
    For example:
    frame={level=\"1\",addr=\"0x2b669230\",func=\"*__GI_abort\",file=\"abort.c
    \",fullname=\"\",line=\"117\",from=\"D:/sysroot/octeon17/lib32/libc.so.
    6\",language=\"c\"}
    
    Change-Id: Ibd273d451844d467395d8d7708b87f2cfc7a139e
    Reviewed-by: Christian Stenger <christian.stenger@theqtcompany.com>
    Reviewed-by: hjk <hjk@theqtcompany.com>

--- src/plugins/debugger/gdb/gdbengine.cpp
+++ src/plugins/debugger/gdb/gdbengine.cpp
@@ -1811,7 +1811,8 @@ QString GdbEngine::cleanupFullName(const QString &fileName)
     // Gdb running on windows often delivers "fullnames" which
     // (a) have no drive letter and (b) are not normalized.
     if (Abi::hostAbi().os() == Abi::WindowsOS) {
-        QTC_ASSERT(!fileName.isEmpty(), return QString());
+        if (fileName.isEmpty())
+            return QString();
         QFileInfo fi(fileName);
         if (fi.isReadable())
             cleanFilePath = QDir::cleanPath(fi.absoluteFilePath());
