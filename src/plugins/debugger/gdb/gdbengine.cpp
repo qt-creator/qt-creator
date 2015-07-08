qt-creator/qt-creator

commit a28ac6307fc98f3c46328ff94ea92f3eff028a85

Debugger: Fix "Restart debugging session" regression
Introduced with the support for fork-follows-child.
With 'Restart', we temporarily don't have a living
thread, so that can't be taken as indication for
ending the debugging session.

Task-number: QTCREATORBUG-14393
Change-Id: I0070b9ca7e6897dab48bdbcf493d03df4709be98
Reviewed-by: Christian Stenger <christian.stenger@theqtcompany.com>

src/plugins/debugger/gdb/gdbengine.cpp
@@ -240,6 +240,7 @@ GdbEngine::GdbEngine(const DebuggerRunParameters &startParameters)
     m_terminalTrap = startParameters.useTerminal;
     m_fullStartDone = false;
     m_systemDumpersLoaded = false;
+    m_rerunPending = true;
 
     m_debugInfoTaskHandler = new DebugInfoTaskHandler(this);
     //ExtensionSystem::PluginManager::addObject(m_debugInfoTaskHandler);
@@ -1972,8 +1973,12 @@ void GdbEngine::handleThreadGroupCreated(const GdbMi &result)
 void GdbEngine::handleThreadGroupExited(const GdbMi &result)
 {
     QByteArray groupId = result["id"].data();
-    if (threadsHandler()->notifyGroupExited(groupId))
-        notifyInferiorExited();
+    if (threadsHandler()->notifyGroupExited(groupId)) {
+        if (m_rerunPending)
+            m_rerunPending = false;
+        else
+            notifyInferiorExited();
+    }
 }
 
 int GdbEngine::currentFrame() const
@@ -4322,6 +4327,7 @@ void GdbEngine::resetInferior()
             }
         }
     }
+    m_rerunPending = true;
     requestInterruptInferior();
     runEngine();
 }

src/plugins/debugger/gdb/gdbengine.h
@@ -211,6 +211,7 @@ private:
     CommandsDoneCallback m_commandsDoneCallback;
 
     QList<DebuggerCommand> m_commandsToRunOnTemporaryBreak;
+    bool m_rerunPending;
 
 private: ////////// Gdb Output, State & Capability Handling //////////
 protected:

commit 11d79e4f23305718a521f359e416297f007884ef

Debugger: Make Shift-F5 end a debugger session again
This is a regression introduced with a28ac630.

Change-Id: If870c44592de77bec10cd48b4a84fff06e788388
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>
Reviewed-by: Robert Loehning <robert.loehning@theqtcompany.com>
Reviewed-by: Christian Stenger <christian.stenger@theqtcompany.com>

src/plugins/debugger/gdb/gdbengine.cpp
@@ -240,7 +240,7 @@ GdbEngine::GdbEngine(const DebuggerRunParameters &startParameters)
     m_terminalTrap = startParameters.useTerminal;
     m_fullStartDone = false;
     m_systemDumpersLoaded = false;
-    m_rerunPending = true;
+    m_rerunPending = false;
 
     m_debugInfoTaskHandler = new DebugInfoTaskHandler(this);
     //ExtensionSystem::PluginManager::addObject(m_debugInfoTaskHandler);