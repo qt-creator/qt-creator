83d1aa0d7220720c28b83c2d6932959cb33ca3a8
Nikolai Kosjar <nikolai.kosjar@theqtcompany.com>
2015-07-14 10:15:52 +0200

    Clang: Avoid heap-use-after-free in IpcCommunicator
    ...on shutdown.
    
    Now the slot will be disconnected on destruction.
    
    Change-Id: Ie511bf5400c41eee3245879ea355293e7bb5c7cd
    Reviewed-by: Marco Bubke <marco.bubke@theqtcompany.com>

--- src/plugins/clangcodemodel/clangbackendipcintegration.cpp
+++ src/plugins/clangcodemodel/clangbackendipcintegration.cpp
@@ -223,10 +223,8 @@ IpcCommunicator::IpcCommunicator()
 
     connect(Core::EditorManager::instance(), &Core::EditorManager::editorAboutToClose,
             this, &IpcCommunicator::onEditorAboutToClose);
-
-    connect(Core::ICore::instance(), &Core::ICore::coreAboutToClose, [this]() {
-        m_sendMode = IgnoreSendRequests;
-    });
+    connect(Core::ICore::instance(), &Core::ICore::coreAboutToClose,
+            this, &IpcCommunicator::onCoreAboutToClose);
 
     initializeBackend();
 }
@@ -374,6 +372,11 @@ void IpcCommunicator::onEditorAboutToClose(Core::IEditor *editor)
 {
     if (auto *textEditor = qobject_cast<TextEditor::BaseTextEditor *>(editor))
         m_ipcReceiver.deleteProcessorsOfEditorWidget(textEditor->editorWidget());
+}
+
+void IpcCommunicator::onCoreAboutToClose()
+{
+    m_sendMode = IgnoreSendRequests;
 }
 
 void IpcCommunicator::initializeBackendWithCurrentData()
--- src/plugins/clangcodemodel/clangbackendipcintegration.h
+++ src/plugins/clangcodemodel/clangbackendipcintegration.h
@@ -143,6 +143,7 @@ private:
 
     void onBackendRestarted();
     void onEditorAboutToClose(Core::IEditor *editor);
+    void onCoreAboutToClose();
 
     IpcReceiver m_ipcReceiver;
     ClangBackEnd::ConnectionClient m_connection;
