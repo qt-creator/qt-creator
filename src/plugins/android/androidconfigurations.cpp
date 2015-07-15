qt-creator/qt-creator

commit e6d1141e1e524cce1bb43a1a859bfbd5441c02d8

ToolChain: Get rid of type() in favor of typeId()
Change-Id: I8fc68c266acb55a7c3e014697a7b526784914f7a
Reviewed-by: Daniel Teske <daniel.teske@theqtcompany.com>

src/plugins/android/androidconfigurations.cpp
@@ -1218,9 +1218,9 @@ static bool equalKits(Kit *a, Kit *b)
     ToolChain *btc = ToolChainKitInformation::toolChain(b);
     if (atc == btc)
         return true;
-    if (!atc || atc->type() != QLatin1String(Constants::ANDROID_TOOLCHAIN_TYPE))
+    if (!atc || atc->typeId() != Constants::ANDROID_TOOLCHAIN_TYPE)
         return false;
-    if (!btc || btc->type() != QLatin1String(Constants::ANDROID_TOOLCHAIN_TYPE))
+    if (!btc || btc->typeId() != Constants::ANDROID_TOOLCHAIN_TYPE)
         return false;
     AndroidToolChain *aatc = static_cast<AndroidToolChain *>(atc);
     AndroidToolChain *bbtc = static_cast<AndroidToolChain *>(btc);
@@ -1252,7 +1252,7 @@ void AndroidConfigurations::registerNewToolChains()
 void AndroidConfigurations::removeOldToolChains()
 {
     foreach (ToolChain *tc, ToolChainManager::toolChains()) {
-        if (tc->type() == QLatin1String(Constants::ANDROID_TOOLCHAIN_TYPE)) {
+        if (tc->typeId() == Constants::ANDROID_TOOLCHAIN_TYPE) {
             if (!tc->isValid())
                 ToolChainManager::deregisterToolChain(tc);
         }
@@ -1268,7 +1268,7 @@ void AndroidConfigurations::updateAutomaticKitList()
         foreach (ToolChain *tc, ToolChainManager::toolChains()) {
             if (!tc->isAutoDetected())
                 continue;
-            if (tc->type() != QLatin1String(Constants::ANDROID_TOOLCHAIN_TYPE))
+            if (tc->typeId() != Constants::ANDROID_TOOLCHAIN_TYPE)
                 continue;
             if (!tc->isValid()) // going to be deleted
                 continue;
@@ -1369,7 +1369,7 @@ void AndroidConfigurations::updateAutomaticKitList()
     foreach (Kit *k, existingKits) {
         ToolChain *tc = ToolChainKitInformation::toolChain(k);
         QtSupport::BaseQtVersion *qtVersion = QtSupport::QtKitInformation::qtVersion(k);
-        if (tc && tc->type() == QLatin1String(Constants::ANDROID_TOOLCHAIN_TYPE)
+        if (tc && tc->typeId() == Constants::ANDROID_TOOLCHAIN_TYPE
                 && tc->isValid()
                 && qtVersion && qtVersion->type() == QLatin1String(Constants::ANDROIDQT)) {
             k->makeUnSticky();

src/plugins/android/androidgdbserverkitinformation.cpp
@@ -90,7 +90,7 @@ bool AndroidGdbServerKitInformation::isAndroidKit(const Kit *kit)
     ToolChain *tc = ToolChainKitInformation::toolChain(kit);
     if (qt && tc)
         return qt->type() == QLatin1String(Constants::ANDROIDQT)
-                && tc->type() == QLatin1String(Constants::ANDROID_TOOLCHAIN_TYPE);
+                && tc->typeId() == Constants::ANDROID_TOOLCHAIN_TYPE;
     return false;
 
 }
@@ -108,7 +108,7 @@ void AndroidGdbServerKitInformation::setGdbSever(Kit *kit, const FileName &gdbSe
 FileName AndroidGdbServerKitInformation::autoDetect(Kit *kit)
 {
     ToolChain *tc = ToolChainKitInformation::toolChain(kit);
-    if (!tc || tc->type() != QLatin1String(Constants::ANDROID_TOOLCHAIN_TYPE))
+    if (!tc || tc->typeId() != Constants::ANDROID_TOOLCHAIN_TYPE)
         return FileName();
     AndroidToolChain *atc = static_cast<AndroidToolChain *>(tc);
     return atc->suggestedGdbServer();

src/plugins/android/androidtoolchain.cpp
@@ -92,11 +92,6 @@ AndroidToolChain::AndroidToolChain(const AndroidToolChain &tc) :
 AndroidToolChain::~AndroidToolChain()
 { }
 
-QString AndroidToolChain::type() const
-{
-    return QLatin1String(Constants::ANDROID_TOOLCHAIN_TYPE);
-}
-
 QString AndroidToolChain::typeDisplayName() const
 {
     return AndroidToolChainFactory::tr("Android GCC");

src/plugins/android/androidtoolchain.h
@@ -42,7 +42,6 @@ class AndroidToolChain : public ProjectExplorer::GccToolChain
 public:
     ~AndroidToolChain() override;
 
-    QString type() const override;
     QString typeDisplayName() const override;
 
     bool isValid() const override;

src/plugins/cpptools/cppprojects.cpp
@@ -113,7 +113,7 @@ void ProjectPart::evaluateToolchain(const ToolChain *tc,
     }
 
     toolchainDefines = tc->predefinedMacros(commandLineFlags);
-    toolchainType = tc->type();
+    toolchainType = tc->typeId();
     updateLanguageFeatures();
 }
 
@@ -711,7 +711,7 @@ bool CompilerOptionsBuilder::excludeDefineLine(const QByteArray &defineLine) con
     // The right-hand sides are gcc built-ins that clang does not understand, and they'd
     // override clang's own (non-macro, it seems) definitions of the symbols on the left-hand
     // side.
-    const bool isGccToolchain = m_projectPart->toolchainType == QLatin1String("gcc");
+    const bool isGccToolchain = m_projectPart->toolchainType == ProjectExplorer::Constants::GCC_TOOLCHAIN_ID;
     if (isGccToolchain && defineLine.contains("has_include"))
         return true;
 

src/plugins/cpptools/cppprojects.h
@@ -130,7 +130,7 @@ public: // fields
     QString projectConfigFile; // currently only used by the Generic Project Manager
     QByteArray projectDefines;
     QByteArray toolchainDefines;
-    QString toolchainType;
+    Core::Id toolchainType;
     QList<HeaderPath> headerPaths;
     QStringList precompiledHeaders;
     LanguageVersion languageVersion;

src/plugins/ios/iosbuildstep.cpp
@@ -201,7 +201,8 @@ QStringList IosBuildStep::defaultArguments() const
         qCWarning(iosLog) << "IosBuildStep had an unknown buildType "
                           << target()->activeBuildConfiguration()->buildType();
     }
-    if (tc->type() == QLatin1String("gcc") || tc->type() == QLatin1String("clang")) {
+    if (tc->typeId() == ProjectExplorer::Constants::GCC_TOOLCHAIN_ID
+            || tc->typeId() == ProjectExplorer::Constants::CLANG_TOOLCHAIN_ID) {
         GccToolChain *gtc = static_cast<GccToolChain *>(tc);
         res << gtc->platformCodeGenFlags();
     }

src/plugins/ios/iosconfigurations.cpp
@@ -100,7 +100,8 @@ void IosConfigurations::updateAutomaticKitList()
     foreach (ToolChain *tc, ToolChainManager::toolChains()) {
         if (!tc->isAutoDetected()) // use also user toolchains?
             continue;
-        if (tc->type() != QLatin1String("clang") && tc->type() != QLatin1String("gcc"))
+        if (tc->typeId() != ProjectExplorer::Constants::CLANG_TOOLCHAIN_ID
+                && tc->typeId() != ProjectExplorer::Constants::GCC_TOOLCHAIN_ID)
             continue;
         GccToolChain *toolchain = static_cast<GccToolChain *>(tc);
         QMapIterator<QString, Platform> iter(platforms);

commit 17c04dccc0cce33cdc78d6885430229c2c2b1abc

Android: Compare typeId() to the correct constant
Broken in e6d1141e1e524cce1bb43a1a859bfbd5441c02d8,
which changed the code to use typeId() instead of type() but
didn't change to what it was compared.

Change-Id: I5b4f76395bd193ee30ee606023483d3bd0b062e8
Reviewed-by: Tobias Hunger <tobias.hunger@theqtcompany.com>

src/plugins/android/androidconfigurations.cpp
@@ -1218,9 +1218,9 @@ static bool equalKits(Kit *a, Kit *b)
     ToolChain *btc = ToolChainKitInformation::toolChain(b);
     if (atc == btc)
         return true;
-    if (!atc || atc->typeId() != Constants::ANDROID_TOOLCHAIN_TYPE)
+    if (!atc || atc->typeId() != Constants::ANDROID_TOOLCHAIN_ID)
         return false;
-    if (!btc || btc->typeId() != Constants::ANDROID_TOOLCHAIN_TYPE)
+    if (!btc || btc->typeId() != Constants::ANDROID_TOOLCHAIN_ID)
         return false;
     AndroidToolChain *aatc = static_cast<AndroidToolChain *>(atc);
     AndroidToolChain *bbtc = static_cast<AndroidToolChain *>(btc);
@@ -1252,7 +1252,7 @@ void AndroidConfigurations::registerNewToolChains()
 void AndroidConfigurations::removeOldToolChains()
 {
     foreach (ToolChain *tc, ToolChainManager::toolChains()) {
-        if (tc->typeId() == Constants::ANDROID_TOOLCHAIN_TYPE) {
+        if (tc->typeId() == Constants::ANDROID_TOOLCHAIN_ID) {
             if (!tc->isValid())
                 ToolChainManager::deregisterToolChain(tc);
         }
@@ -1268,7 +1268,7 @@ void AndroidConfigurations::updateAutomaticKitList()
         foreach (ToolChain *tc, ToolChainManager::toolChains()) {
             if (!tc->isAutoDetected())
                 continue;
-            if (tc->typeId() != Constants::ANDROID_TOOLCHAIN_TYPE)
+            if (tc->typeId() != Constants::ANDROID_TOOLCHAIN_ID)
                 continue;
             if (!tc->isValid()) // going to be deleted
                 continue;
@@ -1369,7 +1369,7 @@ void AndroidConfigurations::updateAutomaticKitList()
     foreach (Kit *k, existingKits) {
         ToolChain *tc = ToolChainKitInformation::toolChain(k);
         QtSupport::BaseQtVersion *qtVersion = QtSupport::QtKitInformation::qtVersion(k);
-        if (tc && tc->typeId() == Constants::ANDROID_TOOLCHAIN_TYPE
+        if (tc && tc->typeId() == Constants::ANDROID_TOOLCHAIN_ID
                 && tc->isValid()
                 && qtVersion && qtVersion->type() == QLatin1String(Constants::ANDROIDQT)) {
             k->makeUnSticky();

src/plugins/android/androidconstants.h
@@ -58,7 +58,6 @@ const char ANDROID_SETTINGS_CATEGORY[] = "XA.Android";
 const char ANDROID_SETTINGS_TR_CATEGORY[] = QT_TRANSLATE_NOOP("Android", "Android");
 const char ANDROID_SETTINGS_CATEGORY_ICON[] = ":/android/images/QtAndroid.png";
 const char ANDROID_TOOLCHAIN_ID[] = "Qt4ProjectManager.ToolChain.Android";
-const char ANDROID_TOOLCHAIN_TYPE[] = "androidgcc";
 const char ANDROIDQT[] = "Qt4ProjectManager.QtVersion.Android";
 
 const char ANDROID_DEVICE_TYPE[] = "Android.Device.Type";

src/plugins/android/androidgdbserverkitinformation.cpp
@@ -90,7 +90,7 @@ bool AndroidGdbServerKitInformation::isAndroidKit(const Kit *kit)
     ToolChain *tc = ToolChainKitInformation::toolChain(kit);
     if (qt && tc)
         return qt->type() == QLatin1String(Constants::ANDROIDQT)
-                && tc->typeId() == Constants::ANDROID_TOOLCHAIN_TYPE;
+                && tc->typeId() == Constants::ANDROID_TOOLCHAIN_ID;
     return false;
 
 }
@@ -108,7 +108,7 @@ void AndroidGdbServerKitInformation::setGdbSever(Kit *kit, const FileName &gdbSe
 FileName AndroidGdbServerKitInformation::autoDetect(Kit *kit)
 {
     ToolChain *tc = ToolChainKitInformation::toolChain(kit);
-    if (!tc || tc->typeId() != Constants::ANDROID_TOOLCHAIN_TYPE)
+    if (!tc || tc->typeId() != Constants::ANDROID_TOOLCHAIN_ID)
         return FileName();
     AndroidToolChain *atc = static_cast<AndroidToolChain *>(tc);
     return atc->suggestedGdbServer();