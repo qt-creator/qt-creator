225e2ae02eaf3327e8923cdc6d6719a689cfa1b3
Thomas Hartmann <Thomas.Hartmann@theqtcompany.com>
2015-07-16 13:49:26 +0200

    QmlDesigner: Cleanup DesignDocumentView
    Change-Id: I64ebe10542957f812b86a998e922a24111ad4f99
    Reviewed-by: Tim Jenssen <tim.jenssen@theqtcompany.com>

--- src/plugins/qmldesigner/components/integration/designdocumentview.h
+++ src/plugins/qmldesigner/components/integration/designdocumentview.h
@@ -43,47 +43,6 @@ public:
     DesignDocumentView(QObject *parent = 0);
     ~DesignDocumentView();
 
-    virtual void nodeCreated(const ModelNode &createdNode) override;
-    virtual void nodeAboutToBeRemoved(const ModelNode &removedNode) override;
-    virtual void nodeRemoved(const ModelNode &removedNode, const NodeAbstractProperty &parentProperty, PropertyChangeFlags propertyChange) override;
-    virtual void nodeAboutToBeReparented(const ModelNode &node,
-                                         const NodeAbstractProperty &newPropertyParent,
-                                         const NodeAbstractProperty &oldPropertyParent,
-                                         AbstractView::PropertyChangeFlags propertyChange)  override;
-    virtual void nodeReparented(const ModelNode &node,
-                                const NodeAbstractProperty &newPropertyParent,
-                                const NodeAbstractProperty &oldPropertyParent,
-                                AbstractView::PropertyChangeFlags propertyChange)  override;
-    virtual void nodeIdChanged(const ModelNode& node, const QString& newId, const QString& oldId) override;
-    virtual void propertiesAboutToBeRemoved(const QList<AbstractProperty>& propertyList) override;
-    virtual void propertiesRemoved(const QList<AbstractProperty>& propertyList) override;
-    virtual void variantPropertiesChanged(const QList<VariantProperty>& propertyList, PropertyChangeFlags propertyChange) override;
-    virtual void bindingPropertiesChanged(const QList<BindingProperty>& propertyList, PropertyChangeFlags propertyChange) override;
-    virtual void signalHandlerPropertiesChanged(const QVector<SignalHandlerProperty>& propertyList, PropertyChangeFlags propertyChange) override;
-    virtual void rootNodeTypeChanged(const QString &type, int majorVersion, int minorVersion) override;
-
-    virtual void selectedNodesChanged(const QList<ModelNode> &selectedNodeList,
-                                      const QList<ModelNode> &lastSelectedNodeList) override;
-
-    virtual void nodeOrderChanged(const NodeListProperty &listProperty, const ModelNode &movedNode, int oldIndex) override;
-    virtual void scriptFunctionsChanged(const ModelNode &node, const QStringList &scriptFunctionList) override;
-    void instancePropertyChange(const QList<QPair<ModelNode, PropertyName> > &propertyList) override;
-    void instancesCompleted(const QVector<ModelNode> &completedNodeList) override;
-    void instanceInformationsChange(const QMultiHash<ModelNode, InformationName> &informationChangeHash) override;
-    void instancesRenderImageChanged(const QVector<ModelNode> &nodeList) override;
-    void instancesPreviewImageChanged(const QVector<ModelNode> &nodeList) override;
-    void instancesChildrenChanged(const QVector<ModelNode> &nodeList) override;
-    void instancesToken(const QString &tokenName, int tokenNumber, const QVector<ModelNode> &nodeVector) override;
-
-    void nodeSourceChanged(const ModelNode &modelNode, const QString &newNodeSource) override;
-
-    void rewriterBeginTransaction();
-    void rewriterEndTransaction();
-
-    void currentStateChanged(const ModelNode &node) override;
-
-    void importsChanged(const QList<Import> &addedImports, const QList<Import> &removedImports) override;
-
     ModelNode insertModel(const ModelNode &modelNode)
     { return m_modelMerger.insertModel(modelNode); }
     void replaceModel(const ModelNode &modelNode)
