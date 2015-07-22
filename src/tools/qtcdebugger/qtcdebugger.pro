qt-creator/qt-creator

commit 7309e8bb643642b37d414be802b0530b70168d0d

Introduce and make use of qtcreatortool.pri.
To factor out things common to (most) tools.

Change-Id: I5a4605f230d35280eda71e5f195d49d7bb929ae0
Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

src/tools/qtcdebugger/qtcdebugger.pro
@@ -1,15 +1,9 @@
 include(../../shared/registryaccess/registryaccess.pri)
+include(../../qtcreatortool.pri)
 QT += widgets
 TARGET = qtcdebugger
-TEMPLATE = app
 SOURCES += main.cpp
 
-DESTDIR=../../../bin
-
-target.path=$$QTC_PREFIX/bin
-INSTALLS+=target
-
-include(../../../qtcreator.pri)
 app_info.input = $$PWD/../../app/app_version.h.in
 app_info.output = $$OUT_PWD/app_version.h
 QMAKE_SUBSTITUTES += app_info

src/qtcreatortool.pri
@@ -1,0 +1,11 @@
+include(../qtcreator.pri)
+include(rpath.pri)
+
+TEMPLATE = app
+
+CONFIG += console
+CONFIG -= app_bundle
+
+DESTDIR = $${IDE_LIBEXEC_PATH}
+target.path  = $${QTC_PREFIX}/bin # FIXME: libexec, more or less
+INSTALLS += target

src/tools/buildoutputparser/buildoutputparser.pro
@@ -1,20 +1,12 @@
-TEMPLATE = app
-TARGET = buildoutputparser
 QTC_LIB_DEPENDS = utils
 QTC_PLUGIN_DEPENDS = projectexplorer qtsupport qmakeprojectmanager
-
 QT = core gui
-CONFIG += console
-CONFIG -= app_bundle
 
-include(../../../qtcreator.pri)
-include(../../rpath.pri)
+include(../../qtcreatortool.pri)
+
+TARGET = buildoutputparser
 
 win32|equals(TEST, 1):DEFINES += HAS_MSVC_PARSER
-
-DESTDIR = $$IDE_BIN_PATH
-target.path = $$QTC_PREFIX/bin
-INSTALLS += target
 
 SOURCES = \
     main.cpp \

src/tools/qtcreatorcrashhandler/qtcreatorcrashhandler.pro
@@ -1,10 +1,6 @@
-include(../../../qtcreator.pri)
+include(../../qtcreatortool.pri)
 
 TARGET = qtcreator_crash_handler
-DESTDIR = $$IDE_LIBEXEC_PATH
-
-CONFIG -= app_bundle
-TEMPLATE = app
 
 SOURCES += \
     main.cpp \
@@ -32,6 +28,3 @@ HEADERS += \
 
 FORMS += \
     crashhandlerdialog.ui
-
-target.path = $$QTC_PREFIX/bin
-INSTALLS += target

src/tools/qtpromaker/qtpromaker.pro
@@ -1,15 +1,9 @@
 QT        -= gui
 
-include(../../../qtcreator.pri)
+include(../../qtcreatortool.pri)
 
-TEMPLATE  = app
 TARGET    = qtpromaker
-DESTDIR   = $$IDE_LIBEXEC_PATH
 
-CONFIG    += console warn_on
-CONFIG    -= app_bundle
+CONFIG    += warn_on
 
 SOURCES   += main.cpp
-
-target.path  = $$QTC_PREFIX/bin # FIXME: libexec, more or less
-INSTALLS    += target

src/tools/sdktool/sdktool.pro
@@ -1,11 +1,7 @@
 QTC_LIB_DEPENDS += \
     utils
 
-include(../../../qtcreator.pri)
-include(../../rpath.pri)
-
-CONFIG += console
-CONFIG -= app_bundle
+include(../../qtcreatortool.pri)
 
 QT -= gui test
 
@@ -50,9 +46,5 @@ HEADERS += \
     rmtoolchainoperation.h \
     settings.h \
 
-DESTDIR=$$IDE_LIBEXEC_PATH
 macx:DEFINES += "DATA_PATH=\"\\\".\\\"\""
 else:DEFINES += "DATA_PATH=\"\\\"../share/qtcreator\\\"\""
-
-target.path  = $$QTC_PREFIX/bin # FIXME: libexec, more or less
-INSTALLS += target

src/tools/wininterrupt/wininterrupt.pro
@@ -1,7 +1,7 @@
-CONFIG          += warn_on console use_c_linker static
-CONFIG          -= qt app_bundle
+CONFIG          += warn_on use_c_linker static
+CONFIG          -= qt
 
-include(../../../qtcreator.pri)
+include(../../qtcreatortool.pri)
 
 # Switch to statically linked CRT. Note: There will be only one
 # global state of the CRT, reconsider if other DLLs are required!
@@ -18,9 +18,6 @@ win32-msvc* {
 
 SOURCES = wininterrupt.c
 
-TEMPLATE        = app
-DESTDIR         = $$IDE_LIBEXEC_PATH
-
 build_all:!build_pass {
     CONFIG -= build_all
     CONFIG += release
@@ -35,6 +32,3 @@ contains(ENV_CPU, ^AMD64$) {
 } else {
     TARGET = win32interrupt
 }
-
-target.path  = $$QTC_PREFIX/bin # FIXME: libexec, more or less
-INSTALLS    += target

src/tools/winrtdebughelper/winrtdebughelper.pro
@@ -1,10 +1,8 @@
-CONFIG          += warn_on console
-CONFIG          -= qt app_bundle
+CONFIG          += warn_on
+CONFIG          -= qt
 
-include(../../../qtcreator.pri)
+include(../../qtcreatortool.pri)
 
-TEMPLATE = app
-DESTDIR = $$IDE_LIBEXEC_PATH
 SOURCES = winrtdebughelper.cpp
 
 build_all:!build_pass {
@@ -13,5 +11,3 @@ build_all:!build_pass {
 }
 
 TARGET = winrtdebughelper
-target.path = $$QTC_PREFIX/bin # FIXME: libexec, more or less
-INSTALLS += target

commit 631d33de39fa24b489f41281b43269190a366f5c

Remove "console" from CONFIG in qtcdebugger.pro
Change 7309e8bb643642b37d414be802b0530b70168d0d
caused "console" to be added to the CONFIG variable,
which resulted in qtcdebugger launching an additional
command prompt window.

Change-Id: I05c74c4ba7a4441ba2a7d3a1ad96fe3b11c74663
Task-number: QTCREATORBUG-14759
Reviewed-by: hjk <hjk@theqtcompany.com>
Reviewed-by: Christian Kandeler <christian.kandeler@theqtcompany.com>

src/tools/qtcdebugger/qtcdebugger.pro
@@ -1,5 +1,6 @@
 include(../../shared/registryaccess/registryaccess.pri)
 include(../../qtcreatortool.pri)
+CONFIG -= console
 QT += widgets
 TARGET = qtcdebugger
 SOURCES += main.cpp