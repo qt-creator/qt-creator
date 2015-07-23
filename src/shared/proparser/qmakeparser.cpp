qt-creator/qt-creator

commit dedcd25924743e494055c1e0195aeef0f3983a1d

QmakeProject: Fix crash on updating sources
ProFileCacheManager::discardFile(s) may remove an entry from
the ProfileCache only when it is unused (which is the case iff
ent->locker is zero).

Change-Id: I9df2079087af6bd0d35dd121db6222e8a6ec9389
Task-number: QTCREATORBUG-14730
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>

src/shared/proparser/qmakeparser.cpp
@@ -61,6 +61,15 @@ void ProFileCache::discardFile(const QString &fileName)
 #endif
     QHash<QString, Entry>::Iterator it = parsed_files.find(fileName);
     if (it != parsed_files.end()) {
+        if (it->locker) {
+            if (!it->locker->done)
+                it->locker->cond.wait(&mutex);
+            do {
+                lck.unlock();
+                QThread::sleep(100);
+                lck.relock();
+            } while (it->locker);
+        }
         if (it->pro)
             it->pro->deref();
         parsed_files.erase(it);
@@ -77,6 +86,15 @@ void ProFileCache::discardFiles(const QString &prefix)
             end = parsed_files.end();
     while (it != end)
         if (it.key().startsWith(prefix)) {
+            if (it->locker) {
+                if (!it->locker->done)
+                    it->locker->cond.wait(&mutex);
+                do {
+                    lck.unlock();
+                    QThread::sleep(100);
+                    lck.relock();
+                } while (it->locker);
+            }
             if (it->pro)
                 it->pro->deref();
             it = parsed_files.erase(it);

commit b335b2a083e456e2b44b1e9454a0f4cd41e2a397

QmakeParser: Fix tests compilation
locker only exists with PROPARSER_THREAD_SAFE defined.

Broken by dedcd25924743e494055c1e0195aeef0f3983a1d

Change-Id: Ieee60dc6f567ab1c3a59697a1760afb2990496f6
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>

src/shared/proparser/qmakeparser.cpp
@@ -61,6 +61,7 @@ void ProFileCache::discardFile(const QString &fileName)
 #endif
     QHash<QString, Entry>::Iterator it = parsed_files.find(fileName);
     if (it != parsed_files.end()) {
+#ifdef PROPARSER_THREAD_SAFE
         if (it->locker) {
             if (!it->locker->done)
                 it->locker->cond.wait(&mutex);
@@ -70,6 +71,7 @@ void ProFileCache::discardFile(const QString &fileName)
                 lck.relock();
             } while (it->locker);
         }
+#endif
         if (it->pro)
             it->pro->deref();
         parsed_files.erase(it);
@@ -86,6 +88,7 @@ void ProFileCache::discardFiles(const QString &prefix)
             end = parsed_files.end();
     while (it != end)
         if (it.key().startsWith(prefix)) {
+#ifdef PROPARSER_THREAD_SAFE
             if (it->locker) {
                 if (!it->locker->done)
                     it->locker->cond.wait(&mutex);
@@ -95,6 +98,7 @@ void ProFileCache::discardFiles(const QString &prefix)
                     lck.relock();
                 } while (it->locker);
             }
+#endif
             if (it->pro)
                 it->pro->deref();
             it = parsed_files.erase(it);