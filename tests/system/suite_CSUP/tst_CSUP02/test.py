fb6baf9188e90a4c03d5d00e4eaf021a996333ef
Robert Loehning <robert.loehning@theqtcompany.com>
2015-07-30 13:12:16 +0200

    Squish: Run tst_CSUP02 on Clang code model, too
    Task-number: QTCREATORBUG-11591
    Change-Id: I8b4a77d6a1184e225bcf3a9a496e1bdb7cb0d81c
    Reviewed-by: Christian Stenger <christian.stenger@theqtcompany.com>

--- tests/system/suite_CSUP/tst_CSUP02/test.py
+++ tests/system/suite_CSUP/tst_CSUP02/test.py
@@ -32,52 +32,64 @@ source("../../shared/qtcreator.py")
 
 # entry of test
 def main():
-    startApplication("qtcreator" + SettingsPath)
+    clangLoaded = startCreatorTryingClang()
     if not startedWithoutPluginError():
         return
     # create qt quick application
 # Step 1: Open test .pro project.
     createNewQtQuickApplication(tempDir(), "SampleApp")
+    models = iterateAvailableCodeModels()
+    for current in models:
+        if current != models[0]:
+            selectCodeModel(current)
+        test.log("Testing code model: %s" % current)
 # Step 2: Open .cpp file in Edit mode.
-    if not openDocument("SampleApp.Sources.main\\.cpp"):
-        test.fatal("Could not open main.cpp")
-        invokeMenuItem("File", "Exit")
-        return
-    test.verify(checkIfObjectExists(":Qt Creator_CppEditor::Internal::CPPEditorWidget"),
-                "Step 2: Verifying if: .cpp file is opened in Edit mode.")
+        if not openDocument("SampleApp.Sources.main\\.cpp"):
+            test.fatal("Could not open main.cpp")
+            invokeMenuItem("File", "Exit")
+            return
+        test.verify(checkIfObjectExists(":Qt Creator_CppEditor::Internal::CPPEditorWidget"),
+                    "Step 2: Verifying if: .cpp file is opened in Edit mode.")
 # Steps 3&4: Insert text "class" to new line in Editor mode and press Ctrl+Space.
 # Focus "class derived from QObject" in the list and press Tab or Enter to complete the code.
-    editorWidget = findObject(":Qt Creator_CppEditor::Internal::CPPEditorWidget")
-    mouseClick(editorWidget, 5, 5, 0, Qt.LeftButton)
-    type(editorWidget, "<Return>")
-    type(editorWidget, "<Up>")
-    type(editorWidget, "class")
-    if platform.system() == "Darwin":
-        type(editorWidget, "<Meta+Space>")
-    else:
-        type(editorWidget, "<Ctrl+Space>")
-    type(waitForObject(":popupFrame_Proposal_QListView"), "<Down>")
-    listView = waitForObject(":popupFrame_Proposal_QListView")
-    test.compare("class derived from QObject", str(listView.model().data(listView.currentIndex())),
-                 "Verifying selecting the correct entry.")
-    type(waitForObject(":popupFrame_Proposal_QListView"), "<Return>")
-    test.verify(str(editorWidget.plainText).startswith("class name : public QObject"),
-                "Steps 3&4: Verifying if: The list of suggestions is opened. It is "
-                "possible to select one of the suggestions. Code with several "
-                "variables is inserted.")
+        editorWidget = findObject(":Qt Creator_CppEditor::Internal::CPPEditorWidget")
+        mouseClick(editorWidget, 5, 5, 0, Qt.LeftButton)
+        type(editorWidget, "<Return>")
+        type(editorWidget, "<Up>")
+        type(editorWidget, "class")
+        if platform.system() == "Darwin":
+            type(editorWidget, "<Meta+Space>")
+        else:
+            type(editorWidget, "<Ctrl+Space>")
+        type(waitForObject(":popupFrame_Proposal_QListView"), "<Down>")
+        if current == "Clang":
+            # different order with Clang code model
+            type(waitForObject(":popupFrame_Proposal_QListView"), "<Down>")
+        listView = waitForObject(":popupFrame_Proposal_QListView")
+        test.compare("class derived from QObject", str(listView.model().data(listView.currentIndex())),
+                     "Verifying selecting the correct entry.")
+        type(waitForObject(":popupFrame_Proposal_QListView"), "<Return>")
+        test.verify(str(editorWidget.plainText).startswith("class name : public QObject"),
+                    "Steps 3&4: Verifying if: The list of suggestions is opened. It is "
+                    "possible to select one of the suggestions. Code with several "
+                    "variables is inserted.")
 # Step 5: Press Tab to move between the variables and specify values for them. For example write "Myname" for variable "name".
-    type(editorWidget, "<Tab>")
-    type(editorWidget, "<Tab>")
-    type(editorWidget, "<Tab>")
-    type(editorWidget, "Myname")
-    pattern = "(?<=class)\s+Myname\s*:\s*public\s+QObject\s*\{\s*Q_OBJECT\s+public:\s+Myname\(\)\s*\{\}\s+virtual\s+~Myname\(\)\s*\{\}\s+\};"
-    result = re.search(pattern, str(editorWidget.plainText))
-    if result:
-        test.passes("Step 5: Verifying if: A value for a variable is inserted and all "
-                    "instances of the variable within the snippet are renamed.")
-    else:
-        test.fail("Step 5: Seems that not all instances of variable had been renamed "
-                  "- Content of editor:\n%s" % editorWidget.plainText)
+        type(editorWidget, "<Tab>")
+        type(editorWidget, "<Tab>")
+        type(editorWidget, "<Tab>")
+        type(editorWidget, "Myname")
+        pattern = "(?<=class)\s+Myname\s*:\s*public\s+QObject\s*\{\s*Q_OBJECT\s+public:\s+Myname\(\)\s*\{\}\s+virtual\s+~Myname\(\)\s*\{\}\s+\};"
+        result = re.search(pattern, str(editorWidget.plainText))
+        if result:
+            test.passes("Step 5: Verifying if: A value for a variable is inserted and all "
+                        "instances of the variable within the snippet are renamed.")
+        else:
+            test.fail("Step 5: Seems that not all instances of variable had been renamed "
+                      "- Content of editor:\n%s" % editorWidget.plainText)
+        invokeMenuItem('File', 'Revert "main.cpp" to Saved')
+        clickButton(waitForObject(":Revert to Saved.Proceed_QPushButton"))
+        snooze(1)   # 'Close "main.cpp"' might still be disabled
+        # editor must be closed to get the second code model applied on re-opening the file
+        invokeMenuItem('File', 'Close "main.cpp"')
     # exit qt creator
-    invokeMenuItem("File", "Save All")
     invokeMenuItem("File", "Exit")
