qt-creator/qt-creator

commit 1d5091e48f6df341dbd4436843c25afe25cbf4ce

Do not use deprecated Qt functionality.
Replace all* remaining deprecated Qt 4 functions with
their Qt 5 counterparts. This means we no longer need to
define the QT_DISABLE_DEPRECATED_BEFORE macro.
This patch is relatively small because most source-compatible
changes of this kind have been done before.

* The one exception is the QmlDesigner, which uses QWeakPointer
in a deprecated way all over the place.

Change-Id: Id4b839c6685f3b5bdf2b89137f95231758ec53c7
Reviewed-by: Eike Ziller <eike.ziller@digia.com>
Reviewed-by: hjk <hjk121@nokiamail.com>

src/app/main.cpp
@@ -53,8 +53,8 @@
 #include <QNetworkProxyFactory>
 
 #include <QApplication>
-#include <QDesktopServices>
 #include <QMessageBox>
+#include <QStandardPaths>
 
 #ifdef ENABLE_QT_BREAKPAD
 #include <qtsystemexceptionhandler.h>
@@ -214,7 +214,8 @@ static inline QStringList getPluginPaths()
     //    "%LOCALAPPDATA%\QtProject\qtcreator" on Windows Vista and later
     //    "$XDG_DATA_HOME/data/QtProject/qtcreator" or "~/.local/share/data/QtProject/qtcreator" on Linux
     //    "~/Library/Application Support/QtProject/Qt Creator" on Mac
-    pluginPath = QDesktopServices::storageLocation(QDesktopServices::DataLocation);
+    pluginPath = QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation)
+            + QLatin1String("/data");
     pluginPath += QLatin1Char('/')
             + QLatin1String(Core::Constants::IDE_SETTINGSVARIANT_STR)
             + QLatin1Char('/');
@@ -295,11 +296,6 @@ int main(int argc, char **argv)
 
     rl.rlim_cur = qMin((rlim_t)OPEN_MAX, rl.rlim_max);
     setrlimit(RLIMIT_NOFILE, &rl);
-#endif
-
-#if defined(Q_OS_UNIX) && !defined(Q_OS_MAC)
-    // QML is unusable with the xlib backend
-    QApplication::setGraphicsSystem(QLatin1String("raster"));
 #endif
 
     SharedTools::QtSingleApplication app((QLatin1String(appNameC)), argc, argv);

commit 7ea6b21b6d9480f6ed5975d75b2d83273856944a

Unbreak user-installed plugin loading on non-Linux
There is no "data" subdirectory on Windows and OS X.

Broke in 1d5091e48f6df341dbd4436843c25afe25cbf4ce

Change-Id: I1c5e5b60847028c9f3d734eeb6995e99013162b1
Reviewed-by: Christian Kandeler <christian.kandeler@theqtcompany.com>

src/app/main.cpp
@@ -218,8 +218,10 @@ static inline QStringList getPluginPaths()
     //    "%LOCALAPPDATA%\QtProject\qtcreator" on Windows Vista and later
     //    "$XDG_DATA_HOME/data/QtProject/qtcreator" or "~/.local/share/data/QtProject/qtcreator" on Linux
     //    "~/Library/Application Support/QtProject/Qt Creator" on Mac
-    pluginPath = QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation)
-            + QLatin1String("/data");
+    pluginPath = QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation);
+#if defined(Q_OS_UNIX) && !defined(Q_OS_MAC)
+    pluginPath += QLatin1String("/data");
+#endif
     pluginPath += QLatin1Char('/')
             + QLatin1String(Core::Constants::IDE_SETTINGSVARIANT_STR)
             + QLatin1Char('/');