6bb78124abaf6493c3f76444ef3179bbdea01cbc
Tobias Hunger <tobias.hunger@theqtcompany.com>
2015-09-09 10:41:14 +0200

    Plain C/CPP Wizards: Use proper main signature
    Change-Id: I1d6e83cda2685201aa4126e993dac85cda8315b3
    Reviewed-by: Orgad Shaneh <orgads@gmail.com>

--- share/qtcreator/templates/wizards/projects/plainc/main.c
+++ share/qtcreator/templates/wizards/projects/plainc/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
     printf("Hello World!\\n");
     return 0;
--- share/qtcreator/templates/wizards/projects/plaincpp/main.cpp
+++ share/qtcreator/templates/wizards/projects/plaincpp/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
     cout << "Hello World!" << endl;
     return 0;
