qt-creator/qt-creator

commit 283b2f1c12140eb6432ee84f3bf45e4015524227

WinDebugInterface: Flush messages on being stopped
I found this while investigating a bug report.

Change-Id: Ic756dfc4b1dfe1b1a647d5b10441391c97046176
Reviewed-by: Kai Koehne <kai.koehne@theqtcompany.com>

src/plugins/projectexplorer/windebuginterface.cpp
@@ -144,21 +144,27 @@ bool WinDebugInterface::runLoop()
 
     QMap<qint64, QString> delayedMessages;
 
+    auto flushMessages = [&delayedMessages](){
+        auto it = delayedMessages.constBegin();
+        auto end = delayedMessages.constEnd();
+        for (; it != end; ++it)
+            emit debugOutput(it.key(), it.value());
+        delayedMessages.clear();
+        timer.start();
+    };
+
     while (true) {
         DWORD timeout = INFINITE;
         if (!delayedMessages.isEmpty()) // if we have delayed message, don't wait forever
             timeout = qMax(60 - timer.elapsed(), 1);
         const DWORD ret = WaitForMultipleObjects(HandleCount, m_waitHandles, FALSE, timeout);
 
-        if (ret == WAIT_FAILED || ret - WAIT_OBJECT_0 == TerminateEventHandle)
+        if (ret == WAIT_FAILED || ret - WAIT_OBJECT_0 == TerminateEventHandle) {
+            flushMessages();
             break;
+        }
         if (ret == WAIT_TIMEOUT) {
-            auto it = delayedMessages.constBegin();
-            auto end = delayedMessages.constEnd();
-            for (; it != end; ++it)
-                emit debugOutput(it.key(), it.value());
-            delayedMessages.clear();
-            timer.start();
+            flushMessages();
             SetEvent(m_bufferReadyEvent);
         } else if (ret - WAIT_OBJECT_0 == DataReadyEventHandle) {
             if (*processId != m_creatorPid) {
@@ -166,12 +172,7 @@ bool WinDebugInterface::runLoop()
                     delayedMessages[*processId].append(QString::fromLocal8Bit(message));
                 } else {
                     delayedMessages[*processId] += QString::fromLocal8Bit(message);
-                    auto it = delayedMessages.constBegin();
-                    auto end = delayedMessages.constEnd();
-                    for (; it != end; ++it)
-                        emit debugOutput(it.key(), it.value());
-                    delayedMessages.clear();
-                    timer.start();
+                    flushMessages();
                 }
             }
             SetEvent(m_bufferReadyEvent);

commit e4fd6c0bbf6bcf7aa7e82ffd0e5ebbcde3816714

ProjectExplorer: Fix compiling windebuginterface.
Broken by 283b2f1c1.

Change-Id: I114de32b1f9fd6487201e01b769d653d5d8ab551
Reviewed-by: Christian Stenger <christian.stenger@theqtcompany.com>
Reviewed-by: Kai Koehne <kai.koehne@theqtcompany.com>

src/plugins/projectexplorer/windebuginterface.cpp
@@ -144,7 +144,7 @@ bool WinDebugInterface::runLoop()
 
     QMap<qint64, QString> delayedMessages;
 
-    auto flushMessages = [&delayedMessages](){
+    auto flushMessages = [this, &delayedMessages, &timer](){
         auto it = delayedMessages.constBegin();
         auto end = delayedMessages.constEnd();
         for (; it != end; ++it)