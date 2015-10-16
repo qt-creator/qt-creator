qt-creator/qt-creator

commit bdb0f264d2df0f59b3a9f0bc4732cd019ecfc1ff

Allow to clear search string
Change-Id: If88d74755e9f95d2aabd2aa770c9bdbb5bd98309
Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

src/plugins/coreplugin/find/searchresultwidget.cpp
@@ -42,6 +42,7 @@
 
 #include <utils/qtcassert.h>
 #include <utils/theme/theme.h>
+#include <utils/fancylineedit.h>
 
 #include <QDir>
 #include <QFrame>
@@ -61,13 +62,17 @@ namespace Core {
 namespace Internal {
 
 class WideEnoughLineEdit : public QLineEdit
+class WideEnoughLineEdit : public Utils::FancyLineEdit
 {
     Q_OBJECT
 
 public:
-    WideEnoughLineEdit(QWidget *parent) : QLineEdit(parent)
+    WideEnoughLineEdit(QWidget *parent) : Utils::FancyLineEdit(parent)
     {
+        setFiltering(true);
+        setPlaceholderText(QString());
         connect(this, &QLineEdit::textChanged, this, &QLineEdit::updateGeometry);
+
     }
 
     QSize sizeHint() const

src/plugins/coreplugin/find/findtoolwindow.cpp
@@ -56,6 +56,7 @@ FindToolWindow::FindToolWindow(FindPlugin *plugin, QWidget *parent)
 {
     m_instance = this;
     m_ui.setupUi(this);
+    m_ui.searchTerm->setFiltering(true);
     m_ui.searchTerm->setPlaceholderText(QString());
     setFocusProxy(m_ui.searchTerm);
 

commit 52e35e7e9daf78e4e5774f1b94cb7a57495d9acf

Fix compilation
Was broken after bdb0f264d2df0f59b3a9f0bc4732cd019ecfc1ff

Change-Id: I3581da9d27f9609c2b3e4f74443ee59feef739dc
Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

src/plugins/coreplugin/find/searchresultwidget.cpp
@@ -61,7 +61,6 @@ using namespace Utils;
 namespace Core {
 namespace Internal {
 
-class WideEnoughLineEdit : public QLineEdit
 class WideEnoughLineEdit : public Utils::FancyLineEdit
 {
     Q_OBJECT