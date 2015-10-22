1a9e097c8095c9b814f3509e828e23d45c744f10
Robert Loehning <robert.loehning@theqtcompany.com>
2015-10-22 18:26:09 +0200

    Squish: Update tab's name in tst_installed_languages
    Change-Id: I22f69a589d8bb32c3fa7d984ada67043340e5e27
    Reviewed-by: Christian Stenger <christian.stenger@theqtcompany.com>

--- tests/system/suite_general/tst_installed_languages/test.py
+++ tests/system/suite_general/tst_installed_languages/test.py
@@ -39,7 +39,7 @@ def main():
         invokeMenuItem("Tools", "Options...")
         waitForObjectItem(":Options_QListView", "Environment")
         clickItem(":Options_QListView", "Environment", 14, 15, 0, Qt.LeftButton)
-        clickOnTab(":Options.qt_tabwidget_tabbar_QTabBar", "General")
+        clickOnTab(":Options.qt_tabwidget_tabbar_QTabBar", "Interface")
         languageName = testData.field(lang, "language")
         if "%1" in languageName:
             country = str(QLocale.countryToString(QLocale(testData.field(lang, "ISO")).country()))
