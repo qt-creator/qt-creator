qt-creator/qt-creator

commit b26920c3a32727e66453ba40455bd00742aff365

Fixed bug in reformatting QML code (duplicated property name).
Change-Id: I1af68aa979fbbc38edd0255c346b9df371c8fd26
Reviewed-by: Erik Verbruggen <erik.verbruggen@theqtcompany.com>

src/libs/qmljs/qmljsreformatter.cpp
@@ -548,12 +548,11 @@ protected:
                 out(ast->typeToken);
             }
             out(" ");
-            out(ast->identifierToken);
             if (ast->statement) {
+                out(ast->identifierToken);
                 out(": ", ast->colonToken);
                 accept(ast->statement);
             } else if (ast->binding) {
-                out(": ", ast->colonToken);
                 accept(ast->binding);
             }
         } else { // signal

commit a8d1df2c0c513dab46805d4c151c7fafa93d2484

QmlJs: fixed a regression in reformatter.
The commit b26920c3a32727e66453ba40455bd00742aff365 introduced a bug
in reformatting properties lacking an object initializer.

Change-Id: I30f9c9c8a005660e7af4a6c97d0b1ff1be461548
Reviewed-by: Christian Stenger <christian.stenger@theqtcompany.com>

src/libs/qmljs/qmljsreformatter.cpp
@@ -554,6 +554,8 @@ protected:
                 accept(ast->statement);
             } else if (ast->binding) {
                 accept(ast->binding);
+            } else {
+                out(ast->identifierToken);
             }
         } else { // signal
             out("signal ");