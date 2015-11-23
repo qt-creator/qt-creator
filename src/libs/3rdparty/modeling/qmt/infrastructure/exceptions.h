864a6be91a7e03cd051f87a916644d96980c59d3
Jochen Becher <jochen_becher@gmx.de>
2015-11-23 22:05:06 +0100

    ModelEditor: Add missing virtual destructor for base class
    Change-Id: Ie3fce0a78bd83046e3a92a9463557317ba001153
    Reviewed-by: Tobias Hunger <tobias.hunger@theqtcompany.com>

--- src/libs/3rdparty/modeling/qmt/infrastructure/exceptions.h
+++ src/libs/3rdparty/modeling/qmt/infrastructure/exceptions.h
@@ -39,6 +39,7 @@ class Exception
 {
 public:
     Exception(const QString &errorMessage);
+    virtual ~Exception() = default;
 
     QString errorMessage() const { return m_errorMessage; }
     void setErrorMessage(const QString &errorMessage) { m_errorMessage = errorMessage; }
