46b5eefcbfca8a82cab1353002b915ff6bce89bc
Jake Petroules <jake.petroules@theqtcompany.com>
2015-11-30 01:54:33 -0800

    Explicitly document usage of Qt bug workaround.
    Task-number: QTBUG-49694
    Change-Id: I4516949af78320a1cdead9bce8ac90fe2ddc7fa9
    Reviewed-by: Tobias Hunger <tobias.hunger@theqtcompany.com>
    Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@theqtcompany.com>

--- src/libs/utils/qtcprocess.cpp
+++ src/libs/utils/qtcprocess.cpp
@@ -693,6 +693,8 @@ void QtcProcess::start()
             qWarning("QtcProcess::start: Empty environment set when running '%s'.", qPrintable(m_command));
         env = m_environment;
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 5, 0)
+        // QTBUG-49694
         // If the process environment has no libraryPath,
         // Qt will copy creator's libraryPath into the process environment.
         // That's brain dead, and we work around it
@@ -702,6 +704,7 @@ void QtcProcess::start()
             if (env.constFind(libraryPath) == env.constEnd())
                 env.set(libraryPath, QString());
         }
+#endif
         QProcess::setEnvironment(env.toStringList());
     } else {
         env = Environment::systemEnvironment();
