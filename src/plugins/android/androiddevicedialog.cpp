qt-creator/qt-creator

commit 4bdba9c1317b9432de060b54e5d527709809e586

AndroidDeviceDialog: show progress indicator on manual refresh too
Change-Id: I089c2f64e06a3a421177c206faff6a6fe02d01ca
Reviewed-by: Tobias Hunger <tobias.hunger@theqtcompany.com>

src/plugins/android/androiddevicedialog.cpp
@@ -512,6 +512,7 @@ bool AndroidDeviceDialog::saveDeviceSelection()
 void AndroidDeviceDialog::refreshDeviceList()
 {
     m_ui->refreshDevicesButton->setEnabled(false);
+    m_progressIndicator->show();
     m_futureWatcherRefreshDevices.setFuture(QtConcurrent::run(&AndroidDeviceDialog::refreshDevices,
                                                               AndroidConfigurations::currentConfig().adbToolPath().toString(),
                                                               AndroidConfigurations::currentConfig().androidToolPath().toString(),

commit c5c8618aa154b60ba1c9e0167dde2d6d305898a0

Fixup "AndroidDeviceDialog: show progress indicator on manual refresh too"
This fixes segfault introduced with 4bdba9c (AndroidDeviceDialog: show
progress indicator on manual refresh too).

Change-Id: I72c7ead9de4abd46098ebf0ee0f8ce260669f088
Reviewed-by: BogDan Vatra <bogdan@kdab.com>

src/plugins/android/androiddevicedialog.cpp
@@ -461,8 +461,6 @@ AndroidDeviceDialog::AndroidDeviceDialog(int apiLevel, const QString &abi, Andro
     connect(&m_futureWatcherRefreshDevices, &QFutureWatcherBase::finished,
             this, &AndroidDeviceDialog::devicesRefreshed);
 
-    refreshDeviceList();
-
     connect(m_ui->deviceView->selectionModel(), &QItemSelectionModel::currentChanged,
             this, &AndroidDeviceDialog::enableOkayButton);
 
@@ -483,6 +481,8 @@ AndroidDeviceDialog::AndroidDeviceDialog(int apiLevel, const QString &abi, Andro
     connect(m_ui->lookingForDeviceCancel, &QPushButton::clicked,
             this, &AndroidDeviceDialog::defaultDeviceClear);
     m_defaultDeviceTimer.start();
+
+    refreshDeviceList();
 }
 
 AndroidDeviceDialog::~AndroidDeviceDialog()