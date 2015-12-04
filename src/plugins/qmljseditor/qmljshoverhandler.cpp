qt-creator/qt-creator

commit 6ed775f56a17a6fc3bb48323bd43373d8589b7b0

Fix wrong context help if tool tip was not shown for focus widget
The tool tip never has focus, so it cannot become the IContext that is
checked for context help. So, integrate the help id into Utils::ToolTip
and check the tool tip first when checking for context help.
As a side effect the [F1] button and help id for the tool tip is now also
available for use outside of the text editors.

Task-number: QTCREATORBUG-5345
Change-Id: Id975703caf161d1183c247e8ad8bb693b90fd306
Reviewed-by: Nikolai Kosjar <nikolai.kosjar@theqtcompany.com>
Reviewed-by: David Schulz <david.schulz@theqtcompany.com>

src/plugins/texteditor/basehoverhandler.h
@@ -59,8 +59,6 @@ protected:
     void appendToolTip(const QString &extension);
     const QString &toolTip() const;
 
-    void addF1ToToolTip();
-
     void setIsDiagnosticTooltip(bool isDiagnosticTooltip);
     bool isDiagnosticTooltip() const;
 

src/libs/utils/tooltip/tips.cpp
@@ -55,6 +55,17 @@ QTipLabel::QTipLabel(QWidget *parent) :
     QLabel(parent, Qt::ToolTip | Qt::BypassGraphicsProxyWidget)
 {}
 
+void QTipLabel::setHelpId(const QString &id)
+{
+    m_helpId = id;
+    update();
+}
+
+QString QTipLabel::helpId() const
+{
+    return m_helpId;
+}
+
 
 ColorTip::ColorTip(QWidget *parent)
     : QTipLabel(parent)
@@ -88,9 +99,9 @@ bool ColorTip::canHandleContentReplacement(int typeId) const
     return typeId == ToolTip::ColorContent;
 }
 
-bool ColorTip::equals(int typeId, const QVariant &other) const
+bool ColorTip::equals(int typeId, const QVariant &other, const QString &otherHelpId) const
 {
-    return typeId == ToolTip::ColorContent && other == m_color;
+    return typeId == ToolTip::ColorContent && otherHelpId == helpId() && other == m_color;
 }
 
 void ColorTip::paintEvent(QPaintEvent *event)
@@ -131,7 +142,12 @@ void TextTip::setContent(const QVariant &content)
 
 void TextTip::configure(const QPoint &pos, QWidget *w)
 {
-    setText(m_text);
+    if (helpId().isEmpty())
+        setText(m_text);
+    else
+        setText(QString::fromLatin1("<table><tr><td valign=middle>%1</td><td>&nbsp;&nbsp;"
+                                    "<img src=\":/utils/tooltip/images/f1.png\"></td>"
+                                    "</tr></table>").arg(m_text));
 
     // Make it look good with the default ToolTip font on Mac, which has a small descent.
     QFontMetrics fm(font());
@@ -166,9 +182,9 @@ int TextTip::showTime() const
     return 10000 + 40 * qMax(0, m_text.size() - 100);
 }
 
-bool TextTip::equals(int typeId, const QVariant &other) const
+bool TextTip::equals(int typeId, const QVariant &other, const QString &otherHelpId) const
 {
-    return typeId == ToolTip::TextContent && other.toString() == m_text;
+    return typeId == ToolTip::TextContent && otherHelpId == helpId() && other.toString() == m_text;
 }
 
 void TextTip::paintEvent(QPaintEvent *event)
@@ -245,9 +261,10 @@ bool WidgetTip::canHandleContentReplacement(int typeId) const
     return false;
 }
 
-bool WidgetTip::equals(int typeId, const QVariant &other) const
+bool WidgetTip::equals(int typeId, const QVariant &other, const QString &otherHelpId) const
 {
-    return typeId == ToolTip::WidgetContent && other.value<QWidget *>() == m_widget;
+    return typeId == ToolTip::WidgetContent && otherHelpId == helpId()
+            && other.value<QWidget *>() == m_widget;
 }
 
 // need to include it here to force it to be inside the namespaces

src/libs/utils/tooltip/tips.h
@@ -56,7 +56,12 @@ public:
     virtual int showTime() const = 0;
     virtual void configure(const QPoint &pos, QWidget *w) = 0;
     virtual bool canHandleContentReplacement(int typeId) const = 0;
-    virtual bool equals(int typeId, const QVariant &other) const = 0;
+    virtual bool equals(int typeId, const QVariant &other, const QString &helpId) const = 0;
+    virtual void setHelpId(const QString &id);
+    virtual QString helpId() const;
+
+private:
+    QString m_helpId;
 };
 
 class TextTip : public QTipLabel
@@ -68,7 +73,7 @@ public:
     virtual void configure(const QPoint &pos, QWidget *w);
     virtual bool canHandleContentReplacement(int typeId) const;
     virtual int showTime() const;
-    virtual bool equals(int typeId, const QVariant &other) const;
+    virtual bool equals(int typeId, const QVariant &other, const QString &otherHelpId) const;
     virtual void paintEvent(QPaintEvent *event);
     virtual void resizeEvent(QResizeEvent *event);
 
@@ -85,7 +90,7 @@ public:
     virtual void configure(const QPoint &pos, QWidget *w);
     virtual bool canHandleContentReplacement(int typeId) const;
     virtual int showTime() const { return 4000; }
-    virtual bool equals(int typeId, const QVariant &other) const;
+    virtual bool equals(int typeId, const QVariant &other, const QString &otherHelpId) const;
     virtual void paintEvent(QPaintEvent *event);
 
 private:
@@ -105,7 +110,7 @@ public:
     virtual void configure(const QPoint &pos, QWidget *w);
     virtual bool canHandleContentReplacement(int typeId) const;
     virtual int showTime() const { return 30000; }
-    virtual bool equals(int typeId, const QVariant &other) const;
+    virtual bool equals(int typeId, const QVariant &other, const QString &otherHelpId) const;
     virtual bool isInteractive() const { return true; }
 
 private:

src/libs/utils/tooltip/tooltip.cpp
@@ -66,28 +66,28 @@ ToolTip *ToolTip::instance()
     return &tooltip;
 }
 
-void ToolTip::show(const QPoint &pos, const QString &content, QWidget *w, const QRect &rect)
+void ToolTip::show(const QPoint &pos, const QString &content, QWidget *w, const QString &helpId, const QRect &rect)
 {
     if (content.isEmpty())
         instance()->hideTipWithDelay();
     else
-        instance()->showInternal(pos, QVariant(content), TextContent, w, rect);
+        instance()->showInternal(pos, QVariant(content), TextContent, w, helpId, rect);
 }
 
-void ToolTip::show(const QPoint &pos, const QColor &color, QWidget *w, const QRect &rect)
+void ToolTip::show(const QPoint &pos, const QColor &color, QWidget *w, const QString &helpId, const QRect &rect)
 {
     if (!color.isValid())
         instance()->hideTipWithDelay();
     else
-        instance()->showInternal(pos, QVariant(color), ColorContent, w, rect);
+        instance()->showInternal(pos, QVariant(color), ColorContent, w, helpId, rect);
 }
 
-void ToolTip::show(const QPoint &pos, QWidget *content, QWidget *w, const QRect &rect)
+void ToolTip::show(const QPoint &pos, QWidget *content, QWidget *w, const QString &helpId, const QRect &rect)
 {
     if (!content)
         instance()->hideTipWithDelay();
     else
-        instance()->showInternal(pos, QVariant::fromValue(content), WidgetContent, w, rect);
+        instance()->showInternal(pos, QVariant::fromValue(content), WidgetContent, w, helpId, rect);
 }
 
 void ToolTip::move(const QPoint &pos, QWidget *w)
@@ -111,10 +111,15 @@ bool ToolTip::pinToolTip(QWidget *w, QWidget *parent)
     return false;
 }
 
+QString ToolTip::contextHelpId()
+{
+    return instance()->m_tip ? instance()->m_tip->helpId() : QString();
+}
+
 bool ToolTip::acceptShow(const QVariant &content,
                          int typeId,
                          const QPoint &pos,
-                         QWidget *w,
+                         QWidget *w, const QString &helpId,
                          const QRect &rect)
 {
     if (isVisible()) {
@@ -123,8 +128,9 @@ bool ToolTip::acceptShow(const QVariant &content,
             QPoint localPos = pos;
             if (w)
                 localPos = w->mapFromGlobal(pos);
-            if (tipChanged(localPos, content, typeId, w)) {
+            if (tipChanged(localPos, content, typeId, w, helpId)) {
                 m_tip->setContent(content);
+                m_tip->setHelpId(helpId);
                 setUp(pos, w, rect);
             }
             return false;
@@ -155,9 +161,10 @@ void ToolTip::setUp(const QPoint &pos, QWidget *w, const QRect &rect)
     m_showTimer.start(m_tip->showTime());
 }
 
-bool ToolTip::tipChanged(const QPoint &pos, const QVariant &content, int typeId, QWidget *w) const
+bool ToolTip::tipChanged(const QPoint &pos, const QVariant &content, int typeId, QWidget *w,
+                         const QString &helpId) const
 {
-    if (!m_tip->equals(typeId, content) || m_widget != w)
+    if (!m_tip->equals(typeId, content, helpId) || m_widget != w)
         return true;
     if (!m_rect.isNull())
         return !m_rect.contains(pos);
@@ -220,12 +227,13 @@ void ToolTip::hideTipImmediately()
     m_showTimer.stop();
     m_hideDelayTimer.stop();
     qApp->removeEventFilter(this);
+    emit hidden();
 }
 
 void ToolTip::showInternal(const QPoint &pos, const QVariant &content,
-                           int typeId, QWidget *w, const QRect &rect)
+                           int typeId, QWidget *w, const QString &helpId, const QRect &rect)
 {
-    if (acceptShow(content, typeId, pos, w, rect)) {
+    if (acceptShow(content, typeId, pos, w, helpId, rect)) {
         QWidget *target = 0;
         if (HostOsInfo::isWindowsHost())
             target = QApplication::desktop()->screen(Internal::screenNumber(pos, w));
@@ -244,10 +252,12 @@ void ToolTip::showInternal(const QPoint &pos, const QVariant &content,
                 break;
         }
         m_tip->setContent(content);
+        m_tip->setHelpId(helpId);
         setUp(pos, w, rect);
         qApp->installEventFilter(this);
         showTip();
     }
+    emit shown();
 }
 
 void ToolTip::placeTip(const QPoint &pos, QWidget *w)

src/libs/utils/tooltip/tooltip.h
@@ -75,9 +75,12 @@ public:
 
     static ToolTip *instance();
 
-    static void show(const QPoint &pos, const QString &content, QWidget *w = 0, const QRect &rect = QRect());
-    static void show(const QPoint &pos, const QColor &color, QWidget *w = 0, const QRect &rect = QRect());
-    static void show(const QPoint &pos, QWidget *content, QWidget *w = 0, const QRect &rect = QRect());
+    static void show(const QPoint &pos, const QString &content, QWidget *w = 0,
+                     const QString &helpId = QString(), const QRect &rect = QRect());
+    static void show(const QPoint &pos, const QColor &color, QWidget *w = 0,
+                     const QString &helpId = QString(), const QRect &rect = QRect());
+    static void show(const QPoint &pos, QWidget *content, QWidget *w = 0,
+                     const QString &helpId = QString(), const QRect &rect = QRect());
     static void move(const QPoint &pos, QWidget *w);
     static void hide();
     static bool isVisible();
@@ -88,12 +91,21 @@ public:
     // using WidgetContent
     static bool pinToolTip(QWidget *w, QWidget *parent);
 
+    static QString contextHelpId();
+
+signals:
+    void shown();
+    void hidden();
+
 private:
-    void showInternal(const QPoint &pos, const QVariant &content, int typeId, QWidget *w, const QRect &rect);
+    void showInternal(const QPoint &pos, const QVariant &content, int typeId, QWidget *w,
+                      const QString &helpId, const QRect &rect);
     void hideTipImmediately();
-    bool acceptShow(const QVariant &content, int typeId, const QPoint &pos, QWidget *w, const QRect &rect);
+    bool acceptShow(const QVariant &content, int typeId, const QPoint &pos, QWidget *w,
+                    const QString &helpId, const QRect &rect);
     void setUp(const QPoint &pos, QWidget *w, const QRect &rect);
-    bool tipChanged(const QPoint &pos, const QVariant &content, int typeId, QWidget *w) const;
+    bool tipChanged(const QPoint &pos, const QVariant &content, int typeId, QWidget *w,
+                    const QString &helpId) const;
     void setTipRect(QWidget *w, const QRect &rect);
     void placeTip(const QPoint &pos, QWidget *w);
     void showTip();
@@ -104,6 +116,7 @@ private:
     QRect m_rect;
     QTimer m_showTimer;
     QTimer m_hideDelayTimer;
+    QString m_helpId;
 };
 
 } // namespace Utils

src/libs/utils/utils.qrc
@@ -14,5 +14,6 @@
         <file>images/progressindicator_small.png</file>
         <file>images/progressindicator_small@2x.png</file>
         <file>images/triangle_vert.png</file>
+        <file>tooltip/images/f1.png</file>
     </qresource>
 </RCC>

commit ddc43b7329045cc9f12be75821ef140689c6f3b0

Fix that html tags instead of F1 button were for QML tool tips
Regression introduced by 6ed775f56a17a6fc3bb48323bd43373d8589b7b0

Change-Id: Ibef3453339d431dcd7b890d18c27f42f198070e8
Task-number: QTCREATORBUG-15225
Reviewed-by: Marco Benelli <marco.benelli@theqtcompany.com>
Reviewed-by: Thomas Hartmann <Thomas.Hartmann@theqtcompany.com>
Reviewed-by: Robert Loehning <robert.loehning@theqtcompany.com>
Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

src/plugins/qmljseditor/qmljshoverhandler.cpp
@@ -375,7 +375,7 @@ void QmlJSHoverHandler::operateTooltip(TextEditorWidget *editorWidget, const QPo
     else if (m_colorTip.isValid())
         Utils::ToolTip::show(point, m_colorTip, editorWidget);
     else
-        Utils::ToolTip::show(point, toolTip(), editorWidget);
+        BaseHoverHandler::operateTooltip(editorWidget, point);
 }
 
 void QmlJSHoverHandler::prettyPrintTooltip(const Value *value,

src/plugins/texteditor/basehoverhandler.h
@@ -65,13 +65,13 @@ protected:
     void setLastHelpItemIdentified(const HelpItem &help);
     const HelpItem &lastHelpItemIdentified() const;
 
+    virtual void decorateToolTip();
+    virtual void operateTooltip(TextEditorWidget *editorWidget, const QPoint &point);
 private:
     void clear();
     void process(TextEditorWidget *widget, int pos);
 
     virtual void identifyMatch(TextEditorWidget *editorWidget, int pos) = 0;
-    virtual void decorateToolTip();
-    virtual void operateTooltip(TextEditorWidget *editorWidget, const QPoint &point);
 
     bool m_diagnosticTooltip;
     QString m_toolTip;