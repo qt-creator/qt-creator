qt-creator/qt-creator

commit 59ac67c0bb6b2fa999bce830e0ba590ef58fa0ed

Qbs: properly install Info.plist, PkgInfo, and app icon.
The icns files have also been moved into an asset catalog because
"source code" as a rule should be stored in the preferred form of
modification.

Change-Id: I883db5704a46d8d7cf601f7fb4471619ebbc34bf
Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

qbs/imports/QtcProduct.qbs
@@ -3,6 +3,7 @@ import QtcFunctions
 
 Product {
     property string installDir
+    property string installSourceBase
 
     Depends { name: "cpp" }
     cpp.defines: project.generalDefines
@@ -23,5 +24,6 @@ Product {
         fileTagsFilter: product.type
         qbs.install: true
         qbs.installDir: installDir
+        qbs.installSourceBase: installSourceBase
     }
 }

src/app/app.qbs
@@ -1,9 +1,20 @@
 import qbs
 
 QtcProduct {
+    Depends { name: "bundle" }
+    Depends { name: "ib"; condition: qbs.targetOS.contains("osx") }
+
+    bundle.isBundle: true
+    bundle.infoPlistFile: "Info.plist"
+
+    ib.appIconName: "qtcreator"
+
     type: ["application"]
     name: project.ide_app_target
     consoleApplication: qbs.debugInformation
+    version: project.qtcreator_version
+
+    installSourceBase: buildDirectory
 
     cpp.rpaths: qbs.targetOS.contains("osx") ? ["@executable_path/../Frameworks"]
                                              : ["$ORIGIN/../" + project.libDirName + "/qtcreator"]
@@ -18,7 +29,9 @@ QtcProduct {
     Depends { name: "ExtensionSystem" }
 
     files: [
+        "Info.plist",
         "main.cpp",
+        "qtcreator.xcassets",
         "qtcreator.rc",
         "../shared/qtsingleapplication/qtsingleapplication.h",
         "../shared/qtsingleapplication/qtsingleapplication.cpp",
@@ -54,8 +67,8 @@ QtcProduct {
     }
 
     Group {
-        fileTagsFilter: product.type
+        fileTagsFilter: ["infoplist", "pkginfo", "compiled_assetcatalog"]
         qbs.install: true
-        qbs.installDir: project.ide_bin_path
+        qbs.installSourceBase: installSourceBase
     }
 }

qtcreator.qbs
@@ -2,7 +2,7 @@ import qbs 1.0
 
 Project {
     name: "Qt Creator"
-    minimumQbsVersion: "1.4"
+    minimumQbsVersion: "1.4.3"
     property bool withAutotests: qbs.buildVariant === "debug"
     property string ide_version_major: '3'
     property string ide_version_minor: '5'

src/app/Info.plist
@@ -235,7 +235,7 @@
         <key>NSHumanReadableCopyright</key>
         <string>(C) 2015 The Qt Company Ltd</string>
 	<key>CFBundleIconFile</key>
-	<string>$$ICON</string>
+	<string>qtcreator</string>
 	<key>CFBundlePackageType</key>
 	<string>APPL</string>
 	<key>CFBundleSignature</key>
@@ -245,9 +245,9 @@
 	<key>CFBundleIdentifier</key>
 	<string>org.qt-project.qtcreator</string>
 	<key>CFBundleVersion</key>
-	<string>$$QTCREATOR_VERSION</string>
+	<string>@FULL_VERSION@</string>
 	<key>CFBundleShortVersionString</key>
-	<string>$$QTCREATOR_VERSION</string>
+	<string>@SHORT_VERSION@</string>
 	<key>LSMinimumSystemVersion</key>
 	<string>10.7.0</string>
 </dict>

src/app/app.pro
@@ -5,6 +5,7 @@ TEMPLATE = app
 CONFIG += qtc_runnable
 TARGET = $$IDE_APP_TARGET
 DESTDIR = $$IDE_APP_PATH
+VERSION = $$QTCREATOR_VERSION
 QT -= testlib
 
 HEADERS += ../tools/qtcreatorcrashhandler/crashhandlersetup.h
@@ -24,20 +25,30 @@ win32 {
     INSTALLS += target
 } else:macx {
     LIBS += -framework CoreFoundation
-    ICON = qtcreator.icns
-    FILETYPES.files = profile.icns prifile.icns
-    FILETYPES.path = Contents/Resources
-    QMAKE_BUNDLE_DATA += FILETYPES
-    info.input = Info.plist.in
-    info.output = $$IDE_BIN_PATH/../Info.plist
-    QMAKE_SUBSTITUTES = info
+    ASSETCATALOG.files = $$PWD/qtcreator.xcassets
+    macx-xcode {
+        QMAKE_BUNDLE_DATA += ASSETCATALOG
+    } else {
+        ASSETCATALOG.output = $$IDE_BIN_PATH/../Resources/qtcreator.icns
+        ASSETCATALOG.commands = xcrun actool \
+            --app-icon qtcreator \
+            --output-partial-info-plist /dev/null \
+            --platform macosx \
+            --minimum-deployment-target 10.7 \
+            --compile $$shell_quote($$IDE_BIN_PATH/../Resources) \
+            $$shell_quote($$PWD/qtcreator.xcassets)
+        ASSETCATALOG.input = ASSETCATALOG.files
+        ASSETCATALOG.CONFIG += no_link
+        QMAKE_EXTRA_COMPILERS += ASSETCATALOG
+    }
+    QMAKE_INFO_PLIST = Info.plist
 } else {
     target.path  = $$INSTALL_BIN_PATH
     INSTALLS    += target
 }
 
 DISTFILES += qtcreator.rc \
-    Info.plist.in \
+    Info.plist \
     $$PWD/app_version.h.in
 
 QMAKE_SUBSTITUTES += $$PWD/app_version.h.in

commit f97e5f15455205f5e161f549e87ba83f5a6dd05e

Fix Qt Creator executable installation for qbs.
Commit 59ac67c0bb broke this for non-OS X.

Change-Id: Iac23a3e37bd9964af4ffb79be256f59c0b0fedb9
Reviewed-by: Jake Petroules <jake.petroules@theqtcompany.com>

qbs/imports/QtcProduct.qbs
@@ -3,7 +3,6 @@ import QtcFunctions
 
 Product {
     property string installDir
-    property string installSourceBase
 
     Depends { name: "cpp" }
     cpp.defines: project.generalDefines
@@ -24,6 +23,5 @@ Product {
         fileTagsFilter: product.type
         qbs.install: true
         qbs.installDir: installDir
-        qbs.installSourceBase: installSourceBase
     }
 }

src/app/app.qbs
@@ -4,7 +4,6 @@ QtcProduct {
     Depends { name: "bundle" }
     Depends { name: "ib"; condition: qbs.targetOS.contains("osx") }
 
-    bundle.isBundle: true
     bundle.infoPlistFile: "Info.plist" // TODO: Remove for qbs 1.6
 
     Properties {
@@ -12,12 +11,17 @@ QtcProduct {
         ib.appIconName: "qtcreator"
     }
 
+    Properties {
+        condition: qbs.targetOS.contains("windows")
+        consoleApplication: qbs.debugInformation
+    }
+    consoleApplication: false
+
     type: ["application"]
     name: project.ide_app_target
-    consoleApplication: qbs.debugInformation
     version: project.qtcreator_version
 
-    installSourceBase: buildDirectory
+    installDir: project.ide_bin_path
 
     cpp.rpaths: qbs.targetOS.contains("osx") ? ["@executable_path/../Frameworks"]
                                              : ["$ORIGIN/../" + project.libDirName + "/qtcreator"]
@@ -73,6 +77,6 @@ QtcProduct {
         condition: qbs.targetOS.contains("osx")
         fileTagsFilter: ["infoplist", "pkginfo", "compiled_assetcatalog"]
         qbs.install: true
-        qbs.installSourceBase: installSourceBase
+        qbs.installSourceBase: product.buildDirectory
     }
 }