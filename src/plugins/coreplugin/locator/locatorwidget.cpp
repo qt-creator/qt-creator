qt-creator/qt-creator

commit b6e4e966f4352797bee50a807058f755c96c2f3f

Locator: Remove whitespace from start and end
It is not very intuitive, and can easily happen when copy&pasting into
locator.

Change-Id: I51d226b8e834c43eedc042035a9b90aa7ee4844f
Task-number: QTCREATORBUG-15399
Reviewed-by: David Schulz <david.schulz@theqtcompany.com>

src/plugins/coreplugin/locator/locatorwidget.cpp
@@ -464,24 +464,25 @@ void LocatorWidget::showPopupNow()
 
 QList<ILocatorFilter *> LocatorWidget::filtersFor(const QString &text, QString &searchText)
 {
+    const QString trimmedText = text.trimmed();
     QList<ILocatorFilter *> filters = m_locatorPlugin->filters();
-    const int whiteSpace = text.indexOf(QLatin1Char(' '));
+    const int whiteSpace = trimmedText.indexOf(QLatin1Char(' '));
     QString prefix;
     if (whiteSpace >= 0)
-        prefix = text.left(whiteSpace);
+        prefix = trimmedText.left(whiteSpace);
     if (!prefix.isEmpty()) {
         prefix = prefix.toLower();
         QList<ILocatorFilter *> prefixFilters;
         foreach (ILocatorFilter *filter, filters) {
             if (prefix == filter->shortcutString()) {
-                searchText = text.mid(whiteSpace+1);
+                searchText = trimmedText.mid(whiteSpace).trimmed();
                 prefixFilters << filter;
             }
         }
         if (!prefixFilters.isEmpty())
             return prefixFilters;
     }
-    searchText = text;
+    searchText = trimmedText;
     QList<ILocatorFilter *> activeFilters;
     foreach (ILocatorFilter *filter, filters)
         if (filter->isIncludedByDefault())

commit 16b1430d2a7f421491966402fdf8d7a52a5b66e2

Locator: Fix handling of prefix with empty input
For example: "f ", "m ".

Broken by b6e4e966f4352797bee50a807058f755c96c2f3f.

Change-Id: Ic1e389c52579c86479e2c066eadfb4aaf4b66700
Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

src/plugins/coreplugin/locator/locatorwidget.cpp
@@ -464,25 +464,27 @@ void LocatorWidget::showPopupNow()
 
 QList<ILocatorFilter *> LocatorWidget::filtersFor(const QString &text, QString &searchText)
 {
-    const QString trimmedText = text.trimmed();
-    QList<ILocatorFilter *> filters = m_locatorPlugin->filters();
-    const int whiteSpace = trimmedText.indexOf(QLatin1Char(' '));
-    QString prefix;
-    if (whiteSpace >= 0)
-        prefix = trimmedText.left(whiteSpace);
-    if (!prefix.isEmpty()) {
-        prefix = prefix.toLower();
+    const int length = text.size();
+    int firstNonSpace;
+    for (firstNonSpace = 0; firstNonSpace < length; ++firstNonSpace) {
+        if (!text.at(firstNonSpace).isSpace())
+            break;
+    }
+    const int whiteSpace = text.indexOf(QChar::Space, firstNonSpace);
+    const QList<ILocatorFilter *> filters = m_locatorPlugin->filters();
+    if (whiteSpace >= 0) {
+        const QString prefix = text.mid(firstNonSpace, whiteSpace - firstNonSpace).toLower();
         QList<ILocatorFilter *> prefixFilters;
         foreach (ILocatorFilter *filter, filters) {
             if (prefix == filter->shortcutString()) {
-                searchText = trimmedText.mid(whiteSpace).trimmed();
+                searchText = text.mid(whiteSpace).trimmed();
                 prefixFilters << filter;
             }
         }
         if (!prefixFilters.isEmpty())
             return prefixFilters;
     }
-    searchText = trimmedText;
+    searchText = text.trimmed();
     QList<ILocatorFilter *> activeFilters;
     foreach (ILocatorFilter *filter, filters)
         if (filter->isIncludedByDefault())