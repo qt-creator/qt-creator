4dba05a94cc39d67995bef775e3122ee336cc827
Nikolai Kosjar <nikolai.kosjar@theqtcompany.com>
2016-01-14 10:59:10 +0100

    Update license: Add "GPLv3 with exceptions"
    Change-Id: I2567bec6384497994f4c1a996e98a8d4ed6d3452
    Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

--- plugins/clangstaticanalyzer/clangstaticanalyzerrunner.cpp
+++ plugins/clangstaticanalyzer/clangstaticanalyzerrunner.cpp
@@ -1,18 +1,25 @@
 /****************************************************************************
 **
-** Copyright (C) 2015 The Qt Company Ltd
-** All rights reserved.
-** For any questions to The Qt Company, please use contact form at http://www.qt.io/contact-us
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
 **
-** This file is part of the Qt Enterprise ClangStaticAnalyzer Add-on.
+** This file is part of Qt Creator.
 **
-** Licensees holding valid Qt Enterprise licenses may use this file in
-** accordance with the Qt Enterprise License Agreement provided with the
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
 ** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company.
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
 **
-** If you have questions regarding the use of this file, please use
-** contact form at http://www.qt.io/contact-us
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
 **
 ****************************************************************************/
 
