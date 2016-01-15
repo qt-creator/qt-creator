b1f757af236dc20bb279c767feec00d59fe2a0c2
Tobias Hunger <tobias.hunger@theqtcompany.com>
2016-01-15 14:52:30 +0100

    Update License according to aggrement with Free Qt Foundation
    * Make sure all scripts come with a license
    * Update the license in the scripts
    * Add scripts to help with license changes
    
    Change-Id: I38a25aa1287ee8a75e0f0307d1b30320d781ad1f
    Reviewed-by: Tobias Hunger <tobias.hunger@theqtcompany.com>

--- scripts/deployqtHelper_mac.sh
+++ scripts/deployqtHelper_mac.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+
+############################################################################
+#
+# Copyright (C) 2016 The Qt Company Ltd.
+# Contact: https://www.qt.io/licensing/
+#
+# This file is part of Qt Creator.
+#
+# Commercial License Usage
+# Licensees holding valid commercial Qt licenses may use this file in
+# accordance with the commercial license agreement provided with the
+# Software or, alternatively, in accordance with the terms contained in
+# a written agreement between you and The Qt Company. For licensing terms
+# and conditions see https://www.qt.io/terms-conditions. For further
+# information use the contact form at https://www.qt.io/contact-us.
+#
+# GNU General Public License Usage
+# Alternatively, this file may be used under the terms of the GNU
+# General Public License version 3 as published by the Free Software
+# Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+# included in the packaging of this file. Please review the following
+# information to ensure the GNU General Public License requirements will
+# be met: https://www.gnu.org/licenses/gpl-3.0.html.
+#
+############################################################################
+
 [ $# -lt 5 ] && echo "Usage: $(basename $0) <app folder> <qt translations folder> <qt plugin folder> <qt quick imports folder> <qt quick 2 imports folder>" && exit 2
 [ $(uname -s) != "Darwin" ] && echo "Run this script on Mac OS X" && exit 2;
 
