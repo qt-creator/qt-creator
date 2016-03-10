qt-creator/qt-creator

commit f62663e8e43c2d7b65446976a1293903bb3dcaee

Polish: Remove some more useless "slots" from headers
Change-Id: I9ba3b93e53e63153c1573d51c1aac53b7e088362
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/clearcase/clearcaseplugin.h
@@ -164,19 +164,19 @@ public:
     inline bool isFakeCleartool() const { return m_fakeClearTool; }
 #endif
 
-public slots:
     void vcsAnnotate(const QString &workingDir, const QString &file,
                      const QString &revision = QString(), int lineNumber = -1) const;
     bool newActivity();
     void updateStreamAndView();
 
-private slots:
-    void annotateVersion(const QString &workingDirectory, const QString &file, const QString &revision, int lineNumber);
-    void describe(const QString &source, const QString &changeNr);
-    void syncSlot();
-    void updateStatusActions();
+protected:
+    void updateActions(VcsBase::VcsBasePlugin::ActionState) override;
+    bool submitEditorAboutToClose() override;
+    QString ccGet(const QString &workingDir, const QString &file, const QString &prefix = QString());
+    QList<QStringPair> ccGetActivities() const;
 
 #ifdef WITH_TESTS
+private slots:
     void initTestCase();
     void cleanupTestCase();
     void testDiffFileResolving_data();
@@ -194,13 +194,12 @@ private slots:
     void testVcsStatusDynamicNotManaged();
 #endif
 
-protected:
-    void updateActions(VcsBase::VcsBasePlugin::ActionState) override;
-    bool submitEditorAboutToClose() override;
-    QString ccGet(const QString &workingDir, const QString &file, const QString &prefix = QString());
-    QList<QStringPair> ccGetActivities() const;
-
 private:
+    void annotateVersion(const QString &workingDirectory, const QString &file, const QString &revision, int lineNumber);
+    void describe(const QString &source, const QString &changeNr);
+    void syncSlot();
+    void updateStatusActions();
+
     void checkOutCurrentFile();
     void addCurrentFile();
     void undoCheckOutCurrent();

src/plugins/bazaar/bazaarcontrol.h
@@ -64,7 +64,6 @@ public:
                                                      const QString &localName,
                                                      const QStringList &extraArgs) override;
 
-public slots:
     // To be connected to the VCSTask's success signal to emit the repository/
     // files changed signals according to the variant's type:
     // String -> repository, StringList -> files

src/plugins/bazaar/uncommitdialog.h
@@ -43,10 +43,9 @@ public:
     QStringList extraOptions() const;
     QString revision() const;
 
-private slots:
+private:
     void dryRun();
 
-private:
     Ui::UnCommitDialog *m_ui;
 };
 

src/plugins/clearcase/activityselector.h
@@ -49,13 +49,11 @@ public:
     bool refresh();
     bool changed() { return m_changed; }
 
-public slots:
     void newActivity();
 
-private slots:
+private:
     void userChanged();
 
-private:
     ClearCasePlugin *m_plugin;
     bool m_changed = false;
     QComboBox *m_cmbActivity;

src/plugins/clearcase/checkoutdialog.h
@@ -40,8 +40,10 @@ class CheckOutDialog : public QDialog
     Q_OBJECT
 
 public:
-    explicit CheckOutDialog(const QString &fileName, bool isUcm, bool showComment, QWidget *parent = 0);
-    ~CheckOutDialog();
+    explicit CheckOutDialog(const QString &fileName, bool isUcm, bool showComment,
+                            QWidget *parent = nullptr);
+    ~CheckOutDialog() override;
+
     QString activity() const;
     QString comment() const;
     bool isReserved() const;
@@ -51,10 +53,10 @@ public:
     void hideHijack();
 
     void hideComment();
-private slots:
-    void toggleUnreserved(bool checked);
 
 private:
+    void toggleUnreserved(bool checked);
+
     Ui::CheckOutDialog *ui;
     ActivitySelector *m_actSelector = nullptr;
 };

src/plugins/clearcase/clearcasesync.h
@@ -57,8 +57,8 @@ private:
     ClearCasePlugin *const m_plugin;
     QSharedPointer<StatusMap> m_statusMap;
 
-public slots:
 #ifdef WITH_TESTS
+public slots:
     void verifyParseStatus(const QString &fileName, const QString &cleartoolLsLine,
                            const FileStatus::Status);
     void verifyFileNotManaged();
@@ -66,7 +66,6 @@ public slots:
     void verifyFileCheckedOutDynamicView();
     void verifyFileCheckedInDynamicView();
     void verifyFileNotManagedDynamicView();
-
 #endif
 };
 

src/plugins/cmakeprojectmanager/cmakeprojectplugin.h
@@ -45,8 +45,8 @@ public:
 
     void extensionsInitialized() override;
 
-private slots:
 #ifdef WITH_TESTS
+private slots:
     void testCMakeParser_data();
     void testCMakeParser();
 #endif

src/plugins/cpaster/cpasterplugin.h
@@ -49,7 +49,6 @@ class CodePasterServiceImpl : public QObject, public CodePaster::Service
 public:
     explicit CodePasterServiceImpl(QObject *parent = 0);
 
-public slots:
     void postText(const QString &text, const QString &mimeType) override;
     void postCurrentEditor() override;
     void postClipboard() override;

commit 361ea2462953e6bc1bd8a6c90b208cb9004dd8e6

ClearCase: Fix plugin unit tests
This function is called by QMetaObject::invokeMethod.

Broke with f62663e8e43c2d7b65446976a1293903bb3dcaee.

Change-Id: I0efc2f24043e0313d500840e6022f3d73ca8a127
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/clearcase/clearcaseplugin.h
@@ -200,7 +200,7 @@ public slots:
 private:
     void annotateVersion(const QString &workingDirectory, const QString &file, const QString &revision, int lineNumber);
     void syncSlot();
-    void updateStatusActions();
+    Q_INVOKABLE void updateStatusActions();
 
     void checkOutCurrentFile();
     void addCurrentFile();