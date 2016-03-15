750aeb919c87bdbdef8a340639ef96a14bd84616
Tobias Hunger <tobias.hunger@theqtcompany.com>
2016-03-15 13:35:43 +0100

    Remove duplicate mime type constants from CppEditor
    Use those in its dependency CppTools instead.
    
    Change-Id: I376782863784a878e84ce2cd5d0c6da68b05ab61
    Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

--- src/plugins/cppeditor/cppeditorplugin.cpp
+++ src/plugins/cppeditor/cppeditorplugin.cpp
@@ -83,10 +83,10 @@ public:
     {
         setId(Constants::CPPEDITOR_ID);
         setDisplayName(qApp->translate("OpenWith::Editors", Constants::CPPEDITOR_DISPLAY_NAME));
-        addMimeType(Constants::C_SOURCE_MIMETYPE);
-        addMimeType(Constants::C_HEADER_MIMETYPE);
-        addMimeType(Constants::CPP_SOURCE_MIMETYPE);
-        addMimeType(Constants::CPP_HEADER_MIMETYPE);
+        addMimeType(CppTools::Constants::C_SOURCE_MIMETYPE);
+        addMimeType(CppTools::Constants::C_HEADER_MIMETYPE);
+        addMimeType(CppTools::Constants::CPP_SOURCE_MIMETYPE);
+        addMimeType(CppTools::Constants::CPP_HEADER_MIMETYPE);
 
         setDocumentCreator([]() { return new CppEditorDocument; });
         setEditorWidgetCreator([]() { return new CppEditorWidget; });
@@ -268,13 +268,13 @@ void CppEditorPlugin::extensionsInitialized()
     if (!HostOsInfo::isMacHost() && !HostOsInfo::isWindowsHost()) {
         FileIconProvider::registerIconOverlayForMimeType(
                     QIcon(creatorTheme()->imageFile(Theme::IconOverlayCppSource, QLatin1String(":/cppeditor/images/qt_cpp.png"))),
-                    Constants::CPP_SOURCE_MIMETYPE);
+                    CppTools::Constants::CPP_SOURCE_MIMETYPE);
         FileIconProvider::registerIconOverlayForMimeType(
                     QIcon(creatorTheme()->imageFile(Theme::IconOverlayCSource, QLatin1String(":/cppeditor/images/qt_c.png"))),
-                    Constants::C_SOURCE_MIMETYPE);
+                    CppTools::Constants::C_SOURCE_MIMETYPE);
         FileIconProvider::registerIconOverlayForMimeType(
                     QIcon(creatorTheme()->imageFile(Theme::IconOverlayCppHeader, QLatin1String(":/cppeditor/images/qt_h.png"))),
-                    Constants::CPP_HEADER_MIMETYPE);
+                    CppTools::Constants::CPP_HEADER_MIMETYPE);
     }
 }
 
