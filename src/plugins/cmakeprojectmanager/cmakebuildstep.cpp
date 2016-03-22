qt-creator/qt-creator

commit 578c107a16f91f5cbf520a8cc3f2ece8b0f9660a

CMake: Do not try to parse progress information lines
The lines with "[x%] Building something" do not contain errors,
so there is no need to parse them (causing false positives to show
up).

Change-Id: Iee87deb73db9e82040cab5401b74c70be91e6191
Reviewed-by: Alexander Drozdov <adrozdoff@gmail.com>
Reviewed-by: Tim Jenssen <tim.jenssen@theqtcompany.com>

src/plugins/cmakeprojectmanager/cmakebuildstep.cpp
@@ -270,6 +270,7 @@ void CMakeBuildStep::stdOutput(const QString &line)
         int percent = m_percentProgress.cap(1).toInt(&ok);
         if (ok)
             futureInterface()->setProgressValue(percent);
+        return;
     } else if (m_ninjaProgress.indexIn(line) != -1) {
         m_useNinja = true;
         bool ok = false;
@@ -281,6 +282,7 @@ void CMakeBuildStep::stdOutput(const QString &line)
                 futureInterface()->setProgressValue(percent);
             }
         }
+        return;
     }
     if (m_useNinja)
         AbstractProcessStep::stdError(line);

commit 13dea114e2bca76115c8e0957cb91812a4539046

CMake: Show build progress in compile output
Broken by 578c107a16f91f5cbf520a8cc3f2ece8b0f9660a.

Change-Id: I2427dbc36f70a20a453fc6fd13f29589f07a7fcf
Reviewed-by: Tim Jenssen <tim.jenssen@theqtcompany.com>

src/plugins/cmakeprojectmanager/cmakebuildstep.cpp
@@ -266,12 +266,14 @@ bool CMakeBuildStep::immutable() const
 void CMakeBuildStep::stdOutput(const QString &line)
 {
     if (m_percentProgress.indexIn(line) != -1) {
+        AbstractProcessStep::stdOutput(line);
         bool ok = false;
         int percent = m_percentProgress.cap(1).toInt(&ok);
         if (ok)
             futureInterface()->setProgressValue(percent);
         return;
     } else if (m_ninjaProgress.indexIn(line) != -1) {
+        AbstractProcessStep::stdOutput(line);
         m_useNinja = true;
         bool ok = false;
         int done = m_ninjaProgress.cap(1).toInt(&ok);