qt-creator/qt-creator

commit 58be2708a353128fd83555d8e20b5a32d6efd6d7

ProjectExplorer: Use Runnable in DeviceProcess::start
Change-Id: I8ce5b536745db11980f43449a055b7ebf9da83d2
Reviewed-by: Christian Stenger <christian.stenger@theqtcompany.com>
Reviewed-by: Christian Kandeler <christian.kandeler@theqtcompany.com>

src/plugins/projectexplorer/devicesupport/sshdeviceprocess.cpp
@@ -26,6 +26,7 @@
 #include "sshdeviceprocess.h"
 
 #include "idevice.h"
+#include "../runnables.h"
 
 #include <ssh/sshconnection.h>
 #include <ssh/sshconnectionmanager.h>
@@ -47,8 +48,7 @@ public:
     bool serverSupportsSignals;
     QSsh::SshConnection *connection;
     QSsh::SshRemoteProcess::Ptr process;
-    QString executable;
-    QStringList arguments;
+    StandardRunnable runnable;
     QString errorMessage;
     QSsh::SshRemoteProcess::ExitStatus exitStatus;
     DeviceProcessSignalOperation::Ptr killOperation;
@@ -57,7 +57,6 @@ public:
     QByteArray stdErr;
     int exitCode;
     enum State { Inactive, Connecting, Connected, ProcessRunning } state;
-    Utils::Environment environment;
 
     void setState(State newState);
     void doSignal(QSsh::SshRemoteProcess::Signal signal);
@@ -78,15 +77,15 @@ SshDeviceProcess::~SshDeviceProcess()
     delete d;
 }
 
-void SshDeviceProcess::start(const QString &executable, const QStringList &arguments)
+void SshDeviceProcess::start(const Runnable &runnable)
 {
     QTC_ASSERT(d->state == SshDeviceProcessPrivate::Inactive, return);
+    QTC_ASSERT(runnable.is<StandardRunnable>(), return);
     d->setState(SshDeviceProcessPrivate::Connecting);
 
     d->errorMessage.clear();
     d->exitCode = -1;
-    d->executable = executable;
-    d->arguments = arguments;
+    d->runnable = runnable.as<StandardRunnable>();
     d->connection = QSsh::acquireConnection(device()->sshParameters());
     connect(d->connection, SIGNAL(error(QSsh::SshError)), SLOT(handleConnectionError()));
     connect(d->connection, SIGNAL(disconnected()), SLOT(handleDisconnected()));
@@ -113,16 +112,6 @@ void SshDeviceProcess::terminate()
 void SshDeviceProcess::kill()
 {
     d->doSignal(QSsh::SshRemoteProcess::KillSignal);
-}
-
-QString SshDeviceProcess::executable() const
-{
-    return d->executable;
-}
-
-QStringList SshDeviceProcess::arguments() const
-{
-    return d->arguments;
 }
 
 QProcess::ProcessState SshDeviceProcess::state() const
@@ -157,16 +146,6 @@ QString SshDeviceProcess::errorString() const
     return d->errorMessage;
 }
 
-Utils::Environment SshDeviceProcess::environment() const
-{
-    return d->environment;
-}
-
-void SshDeviceProcess::setEnvironment(const Utils::Environment &env)
-{
-    d->environment = env;
-}
-
 QByteArray SshDeviceProcess::readAllStandardOutput()
 {
     const QByteArray data = d->stdOut;
@@ -191,14 +170,14 @@ void SshDeviceProcess::handleConnected()
     QTC_ASSERT(d->state == SshDeviceProcessPrivate::Connecting, return);
     d->setState(SshDeviceProcessPrivate::Connected);
 
-    d->process = d->connection->createRemoteProcess(fullCommandLine().toUtf8());
+    d->process = d->connection->createRemoteProcess(fullCommandLine(d->runnable).toUtf8());
     connect(d->process.data(), SIGNAL(started()), SLOT(handleProcessStarted()));
     connect(d->process.data(), SIGNAL(closed(int)), SLOT(handleProcessFinished(int)));
     connect(d->process.data(), SIGNAL(readyReadStandardOutput()), SLOT(handleStdout()));
     connect(d->process.data(), SIGNAL(readyReadStandardError()), SLOT(handleStderr()));
 
     d->process->clearEnvironment();
-    const Utils::Environment env = environment();
+    const Utils::Environment env = d->runnable.environment;
     for (Utils::Environment::const_iterator it = env.constBegin(); it != env.constEnd(); ++it)
         d->process->addToEnvironment(env.key(it).toUtf8(), env.value(it).toUtf8());
     d->process->start();
@@ -292,11 +271,11 @@ void SshDeviceProcess::handleKillOperationTimeout()
     emit finished();
 }
 
-QString SshDeviceProcess::fullCommandLine() const
+QString SshDeviceProcess::fullCommandLine(const StandardRunnable &runnable) const
 {
-    QString cmdLine = executable();
-    if (!arguments().isEmpty())
-        cmdLine.append(QLatin1Char(' ')).append(arguments().join(QLatin1Char(' ')));
+    QString cmdLine = runnable.executable;
+    if (!runnable.commandLineArguments.isEmpty())
+        cmdLine.append(QLatin1Char(' ')).append(runnable.commandLineArguments);
     return cmdLine;
 }
 
@@ -318,7 +297,7 @@ void SshDeviceProcess::SshDeviceProcessPrivate::doSignal(QSsh::SshRemoteProcess:
         } else {
             DeviceProcessSignalOperation::Ptr signalOperation = q->device()->signalOperation();
             if (signal == QSsh::SshRemoteProcess::IntSignal) {
-                signalOperation->interruptProcess(executable);
+                signalOperation->interruptProcess(runnable.executable);
             } else {
                 if (killOperation) // We are already in the process of killing the app.
                     return;
@@ -326,7 +305,7 @@ void SshDeviceProcess::SshDeviceProcessPrivate::doSignal(QSsh::SshRemoteProcess:
                 connect(signalOperation.data(), SIGNAL(finished(QString)), q,
                         SLOT(handleKillOperationFinished(QString)));
                 killTimer.start(5000);
-                signalOperation->killProcess(executable);
+                signalOperation->killProcess(runnable.executable);
             }
         }
         break;

src/plugins/baremetal/baremetaldebugsupport.cpp
@@ -34,6 +34,11 @@
 #include <debugger/debuggerstartparameters.h>
 
 #include <projectexplorer/devicesupport/deviceapplicationrunner.h>
+#include <projectexplorer/runnables.h>
+
+#include <utils/qtcprocess.h>
+
+using namespace ProjectExplorer;
 
 namespace BareMetal {
 namespace Internal {
@@ -167,9 +172,10 @@ void BareMetalDebugSupport::startExecution()
     connect(m_appRunner, &ProjectExplorer::DeviceApplicationRunner::reportError,
             this, &BareMetalDebugSupport::appRunnerError);
 
-    const QString cmd = p->executable();
-    const QStringList args = p->arguments();
-    m_appRunner->start(m_device, cmd, args);
+    StandardRunnable r;
+    r.executable = p->executable();
+    r.commandLineArguments = Utils::QtcProcess::joinArgs(p->arguments(), Utils::OsTypeLinux);
+    m_appRunner->start(m_device, r);
 }
 
 void BareMetalDebugSupport::setFinished()

src/plugins/baremetal/gdbserverproviderprocess.cpp
@@ -26,12 +26,15 @@
 #include "gdbserverproviderprocess.h"
 
 #include <projectexplorer/devicesupport/idevice.h>
+#include <projectexplorer/runnables.h>
 
 #include <utils/environment.h>
 #include <utils/qtcprocess.h>
 #include <utils/qtcassert.h>
 
 #include <QStringList>
+
+using namespace ProjectExplorer;
 
 namespace BareMetal {
 namespace Internal {
@@ -60,12 +63,12 @@ GdbServerProviderProcess::GdbServerProviderProcess(
             this, &ProjectExplorer::DeviceProcess::started);
 }
 
-void GdbServerProviderProcess::start(const QString &executable, const QStringList &arguments)
+void GdbServerProviderProcess::start(const ProjectExplorer::Runnable &runnable)
 {
+    QTC_ASSERT(runnable.is<StandardRunnable>(), return);
     QTC_ASSERT(m_process->state() == QProcess::NotRunning, return);
-    QString args;
-    Utils::QtcProcess::addArgs(&args, arguments);
-    m_process->setCommand(executable, args);
+    auto r = runnable.as<StandardRunnable>();
+    m_process->setCommand(r.executable, r.commandLineArguments);
     m_process->start();
 }
 
@@ -102,21 +105,6 @@ int GdbServerProviderProcess::exitCode() const
 QString GdbServerProviderProcess::errorString() const
 {
     return m_process->errorString();
-}
-
-Utils::Environment GdbServerProviderProcess::environment() const
-{
-    return Utils::Environment(m_process->processEnvironment().toStringList());
-}
-
-void GdbServerProviderProcess::setEnvironment(const Utils::Environment &env)
-{
-    m_process->setProcessEnvironment(env.toProcessEnvironment());
-}
-
-void GdbServerProviderProcess::setWorkingDirectory(const QString &dir)
-{
-    m_process->setWorkingDirectory(dir);
 }
 
 QByteArray GdbServerProviderProcess::readAllStandardOutput()

src/plugins/baremetal/gdbserverproviderprocess.h
@@ -28,6 +28,7 @@
 
 #include <projectexplorer/devicesupport/deviceprocess.h>
 
+namespace ProjectExplorer { class Runnable; }
 namespace Utils { class QtcProcess; }
 
 namespace BareMetal {
@@ -41,7 +42,7 @@ public:
             const QSharedPointer<const ProjectExplorer::IDevice> &device,
             QObject *parent = 0);
 
-    void start(const QString &executable, const QStringList &arguments);
+    void start(const ProjectExplorer::Runnable &runnable);
     void interrupt();
     void terminate();
     void kill();
@@ -50,10 +51,6 @@ public:
     QProcess::ExitStatus exitStatus() const;
     int exitCode() const;
     QString errorString() const;
-
-    Utils::Environment environment() const;
-    void setEnvironment(const Utils::Environment &);
-    void setWorkingDirectory(const QString &);
 
     QByteArray readAllStandardOutput();
     QByteArray readAllStandardError();

src/plugins/projectexplorer/devicesupport/desktopdeviceprocess.cpp
@@ -24,7 +24,9 @@
 ****************************************************************************/
 
 #include "desktopdeviceprocess.h"
+
 #include "idevice.h"
+#include "../runnables.h"
 
 #include <utils/environment.h>
 #include <utils/qtcassert.h>
@@ -48,10 +50,14 @@ DesktopDeviceProcess::DesktopDeviceProcess(const QSharedPointer<const IDevice> &
     connect(&m_process, &QProcess::started, this, &DeviceProcess::started);
 }
 
-void DesktopDeviceProcess::start(const QString &executable, const QStringList &arguments)
+void DesktopDeviceProcess::start(const Runnable &runnable)
 {
+    QTC_ASSERT(runnable.is<StandardRunnable>(), return);
     QTC_ASSERT(m_process.state() == QProcess::NotRunning, return);
-    m_process.start(executable, arguments);
+    auto r = runnable.as<StandardRunnable>();
+    m_process.setProcessEnvironment(r.environment.toProcessEnvironment());
+    m_process.setWorkingDirectory(r.workingDirectory);
+    m_process.start(r.executable, Utils::QtcProcess::splitArgs(r.commandLineArguments));
 }
 
 void DesktopDeviceProcess::interrupt()
@@ -87,21 +93,6 @@ int DesktopDeviceProcess::exitCode() const
 QString DesktopDeviceProcess::errorString() const
 {
     return m_process.errorString();
-}
-
-Utils::Environment DesktopDeviceProcess::environment() const
-{
-    return Utils::Environment(m_process.processEnvironment().toStringList());
-}
-
-void DesktopDeviceProcess::setEnvironment(const Utils::Environment &env)
-{
-    m_process.setProcessEnvironment(env.toProcessEnvironment());
-}
-
-void DesktopDeviceProcess::setWorkingDirectory(const QString &directory)
-{
-    m_process.setWorkingDirectory(directory);
 }
 
 QByteArray DesktopDeviceProcess::readAllStandardOutput()

src/plugins/projectexplorer/devicesupport/desktopdeviceprocess.h
@@ -38,7 +38,7 @@ class DesktopDeviceProcess : public DeviceProcess
 public:
     DesktopDeviceProcess(const QSharedPointer<const IDevice> &device, QObject *parent = 0);
 
-    void start(const QString &executable, const QStringList &arguments) override;
+    void start(const Runnable &runnable) override;
     void interrupt() override;
     void terminate() override;
     void kill() override;
@@ -47,11 +47,6 @@ public:
     QProcess::ExitStatus exitStatus() const override;
     int exitCode() const override;
     QString errorString() const override;
-
-    Utils::Environment environment() const override;
-    void setEnvironment(const Utils::Environment &env) override;
-
-    void setWorkingDirectory(const QString &directory) override;
 
     QByteArray readAllStandardOutput() override;
     QByteArray readAllStandardError() override;

src/plugins/projectexplorer/devicesupport/deviceapplicationrunner.cpp
@@ -26,6 +26,7 @@
 #include "deviceapplicationrunner.h"
 
 #include "sshdeviceprocess.h"
+#include "../runnables.h"
 
 #include <ssh/sshconnection.h>
 #include <ssh/sshconnectionmanager.h>
@@ -46,8 +47,6 @@ class DeviceApplicationRunner::DeviceApplicationRunnerPrivate
 {
 public:
     DeviceProcess *deviceProcess;
-    Utils::Environment environment;
-    QString workingDir;
     State state;
     bool stopRequested;
     bool success;
@@ -67,18 +66,7 @@ DeviceApplicationRunner::~DeviceApplicationRunner()
     delete d;
 }
 
-void DeviceApplicationRunner::setEnvironment(const Utils::Environment &env)
-{
-    d->environment = env;
-}
-
-void DeviceApplicationRunner::setWorkingDirectory(const QString &workingDirectory)
-{
-    d->workingDir = workingDirectory;
-}
-
-void DeviceApplicationRunner::start(const IDevice::ConstPtr &device,
-        const QString &command, const QStringList &arguments)
+void DeviceApplicationRunner::start(const IDevice::ConstPtr &device, const Runnable &runnable)
 {
     QTC_ASSERT(d->state == Inactive, return);
 
@@ -95,7 +83,7 @@ void DeviceApplicationRunner::start(const IDevice::ConstPtr &device,
         return;
     }
 
-    if (command.isEmpty()) {
+    if (runnable.is<StandardRunnable>() && runnable.as<StandardRunnable>().executable.isEmpty()) {
         doReportError(tr("Cannot run: No command given."));
         setFinished();
         return;
@@ -115,9 +103,7 @@ void DeviceApplicationRunner::start(const IDevice::ConstPtr &device,
             this, &DeviceApplicationRunner::handleApplicationError);
     connect(d->deviceProcess, &DeviceProcess::finished,
             this, &DeviceApplicationRunner::handleApplicationFinished);
-    d->deviceProcess->setEnvironment(d->environment);
-    d->deviceProcess->setWorkingDirectory(d->workingDir);
-    d->deviceProcess->start(command, arguments);
+    d->deviceProcess->start(runnable);
 }
 
 void DeviceApplicationRunner::stop()

src/plugins/projectexplorer/devicesupport/deviceapplicationrunner.h
@@ -33,13 +33,9 @@
 #include <QObject>
 #include <QProcess>
 
-QT_BEGIN_NAMESPACE
-class QStringList;
-QT_END_NAMESPACE
-
-namespace Utils { class Environment; }
-
 namespace ProjectExplorer {
+
+class Runnable;
 
 class PROJECTEXPLORER_EXPORT DeviceApplicationRunner : public QObject
 {
@@ -49,11 +45,7 @@ public:
     explicit DeviceApplicationRunner(QObject *parent = 0);
     ~DeviceApplicationRunner();
 
-    void setEnvironment(const Utils::Environment &env);
-    void setWorkingDirectory(const QString &workingDirectory);
-
-    void start(const IDevice::ConstPtr &device, const QString &command,
-               const QStringList &arguments);
+    void start(const IDevice::ConstPtr &device, const Runnable &runnable);
     void stop();
 
 signals:

commit 24660c23e3b71e5e68639cd691b50cce75e62219

RemoteLinux: Fix fetching of remote environment
A QLatin1String isn't a StandardRunnable.
This fixes a regression introduced in 58be2708a.

Change-Id: I6b3bb337227631011a6bc26c938945bb0e912e62
Reviewed-by: Tobias Hunger <tobias.hunger@theqtcompany.com>

src/plugins/projectexplorer/devicesupport/sshdeviceprocess.cpp
@@ -80,7 +80,11 @@ SshDeviceProcess::~SshDeviceProcess()
 void SshDeviceProcess::start(const Runnable &runnable)
 {
     QTC_ASSERT(d->state == SshDeviceProcessPrivate::Inactive, return);
-    QTC_ASSERT(runnable.is<StandardRunnable>(), return);
+    if (!runnable.is<StandardRunnable>()) {
+        d->errorMessage = tr("Internal error");
+        error(QProcess::FailedToStart);
+        return;
+    }
     d->setState(SshDeviceProcessPrivate::Connecting);
 
     d->errorMessage.clear();

src/plugins/remotelinux/remotelinuxenvironmentreader.cpp
@@ -30,6 +30,7 @@
 #include <projectexplorer/kitinformation.h>
 #include <projectexplorer/runconfiguration.h>
 #include <projectexplorer/target.h>
+#include <projectexplorer/runnables.h>
 
 using namespace ProjectExplorer;
 
@@ -58,7 +59,9 @@ void RemoteLinuxEnvironmentReader::start()
             this, &RemoteLinuxEnvironmentReader::handleError);
     connect(m_deviceProcess, &DeviceProcess::finished,
             this, &RemoteLinuxEnvironmentReader::remoteProcessFinished);
-    m_deviceProcess->start(QLatin1String("env"));
+    StandardRunnable runnable;
+    runnable.executable = QLatin1String("env");
+    m_deviceProcess->start(runnable);
 }
 
 void RemoteLinuxEnvironmentReader::stop()