qt-creator/qt-creator

commit 5e47d35fcd8c750afc6d0453165066f969e282c2

Use Core::Id in ModeManager interface
Makes it more uniform to use and allows placeholder widget
creation to be independent of mode creations.

Change-Id: I4021bc9db7f8c78f0374c0cc3b3331506959afe4
Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

src/plugins/coreplugin/modemanager.cpp
@@ -115,15 +115,15 @@ ModeManager::~ModeManager()
     m_instance = 0;
 }
 
-IMode *ModeManager::currentMode()
+Id ModeManager::currentMode()
 {
     int currentIndex = d->m_modeStack->currentIndex();
     if (currentIndex < 0)
         return 0;
-    return d->m_modes.at(currentIndex);
+    return d->m_modes.at(currentIndex)->id();
 }
 
-IMode *ModeManager::mode(Id id)
+static IMode *findMode(Id id)
 {
     const int index = indexOf(id);
     if (index >= 0)
@@ -205,7 +205,7 @@ void ModeManager::enabledStateChanged()
     d->m_modeStack->setTabEnabled(index, mode->isEnabled());
 
     // Make sure we leave any disabled mode to prevent possible crashes:
-    if (mode == currentMode() && !mode->isEnabled()) {
+    if (mode->id() == currentMode() && !mode->isEnabled()) {
         // This assumes that there is always at least one enabled mode.
         for (int i = 0; i < d->m_modes.count(); ++i) {
             if (d->m_modes.at(i) != mode &&
@@ -256,7 +256,7 @@ void ModeManager::currentTabAboutToChange(int index)
     if (index >= 0) {
         IMode *mode = d->m_modes.at(index);
         if (mode)
-            emit currentModeAboutToChange(mode);
+            emit currentModeAboutToChange(mode->id());
     }
 }
 
@@ -276,13 +276,13 @@ void ModeManager::currentTabChanged(int index)
         if (d->m_oldCurrent >= 0)
             oldMode = d->m_modes.at(d->m_oldCurrent);
         d->m_oldCurrent = index;
-        emit currentModeChanged(mode, oldMode);
+        emit currentModeChanged(mode ? mode->id() : Id(), oldMode ? oldMode->id() : Id());
     }
 }
 
 void ModeManager::setFocusToCurrentMode()
 {
-    IMode *mode = currentMode();
+    IMode *mode = findMode(currentMode());
     QTC_ASSERT(mode, return);
     QWidget *widget = mode->widget();
     if (widget) {

src/plugins/coreplugin/designmode.cpp
@@ -197,7 +197,7 @@ void DesignMode::currentEditorChanged(IEditor *editor)
 
     if (!mimeEditorAvailable) {
         setActiveContext(Context());
-        if (ModeManager::currentMode() == this)
+        if (ModeManager::currentMode() == id())
             ModeManager::activateMode(Constants::MODE_EDIT);
         setEnabled(false);
         d->m_currentEditor = 0;
@@ -217,11 +217,11 @@ void DesignMode::updateActions()
     emit actionsUpdated(d->m_currentEditor.data());
 }
 
-void DesignMode::updateContext(IMode *newMode, IMode *oldMode)
+void DesignMode::updateContext(Id newMode, Id oldMode)
 {
-    if (newMode == this)
+    if (newMode == id())
         ICore::addAdditionalContext(d->m_activeContext);
-    else if (oldMode == this)
+    else if (oldMode == id())
         ICore::removeAdditionalContext(d->m_activeContext);
 }
 
@@ -230,7 +230,7 @@ void DesignMode::setActiveContext(const Context &context)
     if (d->m_activeContext == context)
         return;
 
-    if (ModeManager::currentMode() == this)
+    if (ModeManager::currentMode() == id())
         ICore::updateAdditionalContexts(d->m_activeContext, context);
 
     d->m_activeContext = context;

src/plugins/coreplugin/designmode.h
@@ -68,7 +68,7 @@ private:
     void updateActions();
 
     void currentEditorChanged(IEditor *editor);
-    void updateContext(IMode *newMode, IMode *oldMode);
+    void updateContext(Id newMode, Id oldMode);
     void setActiveContext(const Context &context);
 
     DesignModePrivate *d;

src/plugins/coreplugin/editmode.cpp
@@ -58,25 +58,25 @@ EditMode::EditMode() :
     m_rightSplitWidgetLayout->setMargin(0);
     QWidget *rightSplitWidget = new QWidget;
     rightSplitWidget->setLayout(m_rightSplitWidgetLayout);
-    auto editorPlaceHolder = new EditorManagerPlaceHolder(this);
+    auto editorPlaceHolder = new EditorManagerPlaceHolder(Constants::MODE_EDIT);
     m_rightSplitWidgetLayout->insertWidget(0, editorPlaceHolder);
 
     MiniSplitter *rightPaneSplitter = new MiniSplitter;
     rightPaneSplitter->insertWidget(0, rightSplitWidget);
-    rightPaneSplitter->insertWidget(1, new RightPanePlaceHolder(this));
+    rightPaneSplitter->insertWidget(1, new RightPanePlaceHolder(Constants::MODE_EDIT));
     rightPaneSplitter->setStretchFactor(0, 1);
     rightPaneSplitter->setStretchFactor(1, 0);
 
     MiniSplitter *splitter = new MiniSplitter;
     splitter->setOrientation(Qt::Vertical);
     splitter->insertWidget(0, rightPaneSplitter);
-    QWidget *outputPane = new OutputPanePlaceHolder(this, splitter);
+    QWidget *outputPane = new OutputPanePlaceHolder(Constants::MODE_EDIT, splitter);
     outputPane->setObjectName(QLatin1String("EditModeOutputPanePlaceHolder"));
     splitter->insertWidget(1, outputPane);
     splitter->setStretchFactor(0, 3);
     splitter->setStretchFactor(1, 0);
 
-    m_splitter->insertWidget(0, new NavigationWidgetPlaceHolder(this));
+    m_splitter->insertWidget(0, new NavigationWidgetPlaceHolder(Constants::MODE_EDIT));
     m_splitter->insertWidget(1, splitter);
     m_splitter->setStretchFactor(0, 0);
     m_splitter->setStretchFactor(1, 1);
@@ -100,9 +100,9 @@ EditMode::~EditMode()
     delete m_splitter;
 }
 
-void EditMode::grabEditorManager(IMode *mode)
+void EditMode::grabEditorManager(Id mode)
 {
-    if (mode != this)
+    if (mode != id())
         return;
 
     if (EditorManager::currentEditor())

src/plugins/coreplugin/editmode.h
@@ -47,7 +47,7 @@ public:
     ~EditMode();
 
 private:
-    void grabEditorManager(IMode *mode);
+    void grabEditorManager(Id mode);
 
     QSplitter *m_splitter;
     QVBoxLayout *m_rightSplitWidgetLayout;

src/plugins/coreplugin/editormanager/editormanager.cpp
@@ -121,7 +121,7 @@ using namespace Utils;
 
 //===================EditorManager=====================
 
-EditorManagerPlaceHolder::EditorManagerPlaceHolder(IMode *mode, QWidget *parent)
+EditorManagerPlaceHolder::EditorManagerPlaceHolder(Id mode, QWidget *parent)
     : QWidget(parent), m_mode(mode)
 {
     setLayout(new QVBoxLayout);
@@ -143,7 +143,7 @@ EditorManagerPlaceHolder::~EditorManagerPlaceHolder()
     }
 }
 
-void EditorManagerPlaceHolder::currentModeChanged(IMode *mode)
+void EditorManagerPlaceHolder::currentModeChanged(Id mode)
 {
     if (m_mode == mode) {
         QWidget *previousFocus = 0;

src/plugins/coreplugin/editormanager/editormanager.h
@@ -72,13 +72,13 @@ class CORE_EXPORT EditorManagerPlaceHolder : public QWidget
 {
     Q_OBJECT
 public:
-    explicit EditorManagerPlaceHolder(IMode *mode, QWidget *parent = 0);
+    explicit EditorManagerPlaceHolder(Id mode, QWidget *parent = 0);
     ~EditorManagerPlaceHolder();
 
 private:
-    void currentModeChanged(IMode *);
+    void currentModeChanged(Id mode);
 
-    IMode *m_mode;
+    Id m_mode;
 };
 
 class CORE_EXPORT EditorManager : public QObject

src/plugins/coreplugin/modemanager.h
@@ -25,7 +25,7 @@
 
 #pragma once
 
-#include <coreplugin/core_global.h>
+#include <coreplugin/id.h>
 #include <QObject>
 
 QT_BEGIN_NAMESPACE
@@ -33,9 +33,6 @@ class QAction;
 QT_END_NAMESPACE
 
 namespace Core {
-
-class Id;
-class IMode;
 
 namespace Internal {
     class MainWindow;
@@ -49,8 +46,7 @@ class CORE_EXPORT ModeManager : public QObject
 public:
     static ModeManager *instance();
 
-    static IMode *currentMode();
-    static IMode *mode(Id id);
+    static Id currentMode();
 
     static void addAction(QAction *action, int priority);
     static void addProjectSelector(QAction *action);
@@ -63,10 +59,10 @@ public slots:
     static void setModeSelectorVisible(bool visible);
 
 signals:
-    void currentModeAboutToChange(Core::IMode *mode);
+    void currentModeAboutToChange(Core::Id mode);
 
     // the default argument '=0' is important for connects without the oldMode argument.
-    void currentModeChanged(Core::IMode *mode, Core::IMode *oldMode = 0);
+    void currentModeChanged(Core::Id mode, Core::Id oldMode = Core::Id());
 
 private:
     explicit ModeManager(Internal::MainWindow *mainWindow, Internal::FancyTabWidget *modeStack);

commit 6fc7fb5f578f449302de5b6c60b6a7a2ffc17618

Core: Use Id() instead of 0 for invalid modes.
Fixes a regression introduced in 5e47d35.

Change-Id: I73dbf34008b616ee70b3ac8024af0197b8273a83
Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

src/plugins/coreplugin/modemanager.cpp
@@ -119,7 +119,7 @@ Id ModeManager::currentMode()
 {
     int currentIndex = d->m_modeStack->currentIndex();
     if (currentIndex < 0)
-        return 0;
+        return Id();
     return d->m_modes.at(currentIndex)->id();
 }
 