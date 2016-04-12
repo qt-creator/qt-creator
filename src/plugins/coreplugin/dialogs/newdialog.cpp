qt-creator/qt-creator

commit 2cd5b448e13c733046e6904df78ff97ac1b28104

NewDialog: Simplify code a bit
Convert some connects to Qt5-style while at it.

Change-Id: Ib5d8af8ccded1005302ad2da3c185c73a8992bd6
Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

src/plugins/coreplugin/dialogs/newdialog.cpp
@@ -224,20 +224,15 @@ NewDialog::NewDialog(QWidget *parent) :
     m_ui->templatesView->setModel(m_filterProxyModel);
     m_ui->templatesView->setIconSize(QSize(ICON_SIZE, ICON_SIZE));
 
-    connect(m_ui->templateCategoryView->selectionModel(),
-            SIGNAL(currentChanged(QModelIndex,QModelIndex)),
-            this, SLOT(currentCategoryChanged(QModelIndex)));
+    connect(m_ui->templateCategoryView->selectionModel(), &QItemSelectionModel::currentChanged,
+            this, &NewDialog::currentCategoryChanged);
 
-    connect(m_ui->templatesView->selectionModel(),
-            SIGNAL(currentChanged(QModelIndex,QModelIndex)),
-            this, SLOT(currentItemChanged(QModelIndex)));
+    connect(m_ui->templatesView->selectionModel(), &QItemSelectionModel::currentChanged,
+            this, &NewDialog::currentItemChanged);
 
-    connect(m_ui->templatesView,
-            SIGNAL(doubleClicked(QModelIndex)),
-            this, SLOT(okButtonClicked()));
-
-    connect(m_okButton, SIGNAL(clicked()), this, SLOT(okButtonClicked()));
-    connect(m_ui->buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+    connect(m_ui->templatesView, &QListView::doubleClicked, this, &NewDialog::accept);
+    connect(m_ui->buttonBox, &QDialogButtonBox::accepted, this, &NewDialog::accept);
+    connect(m_ui->buttonBox, &QDialogButtonBox::rejected, this, &NewDialog::reject);
 
     connect(m_ui->comboBox, SIGNAL(currentIndexChanged(QString)), this, SLOT(setSelectedPlatform(QString)));
 }
@@ -453,18 +448,16 @@ void NewDialog::saveState()
         m_lastCategory = currentItem->data(Qt::UserRole).toString();
 }
 
-void NewDialog::okButtonClicked()
+void NewDialog::accept()
 {
-    if (m_ui->templatesView->currentIndex().isValid()) {
-        hide();
-        saveState();
+    saveState();
+    QDialog::accept();
 
+    if (m_ui->templatesView->currentIndex().isValid()) {
         IWizardFactory *wizard = currentWizardFactory();
         QTC_ASSERT(wizard, accept(); return);
         QString path = wizard->runPath(m_defaultLocation);
         wizard->runWizard(path, ICore::dialogParent(), selectedPlatform(), m_extraVariables);
-
-        close();
     }
 }
 

src/plugins/coreplugin/dialogs/newdialog.h
@@ -75,7 +75,7 @@ protected:
 private slots:
     void currentCategoryChanged(const QModelIndex &);
     void currentItemChanged(const QModelIndex &);
-    void okButtonClicked();
+    void accept();
     void reject();
     void updateOkButton();
     void setSelectedPlatform(const QString &platform);

commit 916a78f05014f1f1d1d9d6b5f4480b860cb1701b

New Dialog: Don't not stay in NewDialog after calling QDialog::accept
Accept closes the dialog, and the dialog is flagged as DeleteOnClose. So
we may not run the wizard from it directly.

Regression introduced by 2cd5b448e13c733046e6904df78ff97ac1b28104

Change-Id: Ic2e4ed436cb8f9e02cc987ff5156c2c4e832b4fb
Task-number: QTCREATORBUG-15968
Reviewed-by: Tobias Hunger <tobias.hunger@theqtcompany.com>

src/plugins/coreplugin/dialogs/newdialog.cpp
@@ -41,6 +41,7 @@
 #include <QPushButton>
 #include <QSortFilterProxyModel>
 #include <QStandardItem>
+#include <QTimer>
 
 Q_DECLARE_METATYPE(Core::IWizardFactory*)
 
@@ -458,17 +459,25 @@ void NewDialog::saveState()
     ICore::settings()->setValue(QLatin1String(LAST_PLATFORM_KEY), m_ui->comboBox->currentData());
 }
 
+static void runWizard(IWizardFactory *wizard, const QString &defaultLocation, Id platform,
+                      const QVariantMap &variables)
+{
+    QString path = wizard->runPath(defaultLocation);
+    wizard->runWizard(path, ICore::dialogParent(), platform, variables);
+}
+
 void NewDialog::accept()
 {
     saveState();
-    QDialog::accept();
-
     if (m_ui->templatesView->currentIndex().isValid()) {
         IWizardFactory *wizard = currentWizardFactory();
-        QTC_ASSERT(wizard, accept(); return);
-        QString path = wizard->runPath(m_defaultLocation);
-        wizard->runWizard(path, ICore::dialogParent(), selectedPlatform(), m_extraVariables);
+        QTC_CHECK(wizard);
+        if (wizard) {
+            QTimer::singleShot(0, std::bind(&runWizard, wizard, m_defaultLocation,
+                                            selectedPlatform(), m_extraVariables));
+        }
     }
+    QDialog::accept();
 }
 
 void NewDialog::reject()