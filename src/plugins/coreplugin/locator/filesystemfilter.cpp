qt-creator/qt-creator

commit 08f807a13465b8664cafaa4206076d9d083a6e9a

Core: Use a consistent initial directory in open/save/locator
Use fileDialogInitialDirectory() for all of them.

Conditionally initialize lastVisitedDirectory to projects directory to
preserve existing behavior on Open, when no project is open yet.

Change-Id: I318a7c7741e70a68758f0cc068ce956135103f3f
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/coreplugin/locator/filesystemfilter.cpp
@@ -26,6 +26,7 @@
 #include "filesystemfilter.h"
 #include "locatorwidget.h"
 #include <coreplugin/coreconstants.h>
+#include <coreplugin/documentmanager.h>
 #include <coreplugin/editormanager/ieditor.h>
 #include <coreplugin/editormanager/editormanager.h>
 #include <coreplugin/idocument.h>
@@ -63,13 +64,7 @@ FileSystemFilter::FileSystemFilter(LocatorWidget *locatorWidget)
 void FileSystemFilter::prepareSearch(const QString &entry)
 {
     Q_UNUSED(entry)
-    IDocument *document= EditorManager::currentDocument();
-    if (document && !document->filePath().isEmpty()) {
-        const QFileInfo info = document->filePath().toFileInfo();
-        m_currentDocumentDirectory = info.absolutePath() + QLatin1Char('/');
-    } else {
-        m_currentDocumentDirectory.clear();
-    }
+    m_currentDocumentDirectory = DocumentManager::fileDialogInitialDirectory();
 }
 
 QList<LocatorFilterEntry> FileSystemFilter::matchesFor(QFutureInterface<LocatorFilterEntry> &future,

src/plugins/coreplugin/documentmanager.cpp
@@ -229,6 +229,9 @@ DocumentManager::DocumentManager(QObject *parent)
     qApp->installEventFilter(this);
 
     readSettings();
+
+    if (d->m_useProjectsDirectory)
+        setFileDialogLastVisitedDirectory(d->m_projectsDirectory);
 }
 
 DocumentManager::~DocumentManager()
@@ -886,13 +889,7 @@ QStringList DocumentManager::getOpenFileNames(const QString &filters,
                                               const QString &pathIn,
                                               QString *selectedFilter)
 {
-    QString path = pathIn;
-    if (path.isEmpty()) {
-        if (EditorManager::currentDocument() && !EditorManager::currentDocument()->isTemporary())
-            path = EditorManager::currentDocument()->filePath().toString();
-        if (path.isEmpty() && useProjectsDirectory())
-            path = projectsDirectory();
-    }
+    const QString &path = pathIn.isEmpty() ? fileDialogInitialDirectory() : pathIn;
     const QStringList files = QFileDialog::getOpenFileNames(ICore::dialogParent(),
                                                       tr("Open File"),
                                                       path, filters,

commit aa2e1353795940fdd423c1d756bbd3243244ed70

Fix initial directory of file system locator filter
Broke with 08f807a13465b8664cafaa4206076d9d083a6e9a

Task-number: QTCREATORBUG-16242
Change-Id: If630e37d546815cb1347d51d3c2af5f4033e47bd
Reviewed-by: David Schulz <david.schulz@theqtcompany.com>

src/plugins/coreplugin/locator/filesystemfilter.cpp
@@ -80,7 +80,7 @@ QList<LocatorFilterEntry> FileSystemFilter::matchesFor(QFutureInterface<LocatorF
         if (filePath.startsWith(QLatin1String("~/")))
             directory.replace(0, 1, QDir::homePath());
         else if (!m_currentDocumentDirectory.isEmpty())
-            directory.prepend(m_currentDocumentDirectory);
+            directory.prepend(m_currentDocumentDirectory + "/");
     }
     QDir dirInfo(directory);
     QDir::Filters dirFilter = QDir::Dirs|QDir::Drives|QDir::NoDot;