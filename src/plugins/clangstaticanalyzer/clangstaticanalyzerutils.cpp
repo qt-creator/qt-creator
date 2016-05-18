qt-creator/qt-creator

commit 506fc40a319bc785cb6fadfb0598aa9a3d0213ae

ClangStaticAnalyzer: Fix accepting "clang" executable
The user can set "clang.exe" in the options, which is fine. However, "clang"
was problematic on Windows since we failed to replace it with "clang-cl" for
the actual run for MSVC toolchains.

Change-Id: I2f96428d15c7406e90a38c797817aef34e7a9541
Reviewed-by: Christian Kandeler <christian.kandeler@theqtcompany.com>
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/clangstaticanalyzer/clangstaticanalyzerutils.cpp
@@ -30,6 +30,7 @@
 
 #include <projectexplorer/projectexplorerconstants.h>
 
+#include <utils/hostosinfo.h>
 #include <utils/environment.h>
 
 #include <QCoreApplication>
@@ -50,8 +51,12 @@ namespace Internal {
 QString clangExecutableFromSettings(Core::Id toolchainType, bool *isValid)
 {
     QString exeFromSettings = ClangStaticAnalyzerSettings::instance()->clangExecutable();
-    if (toolchainType == ProjectExplorer::Constants::MSVC_TOOLCHAIN_TYPEID)
-        exeFromSettings.replace(QLatin1String("clang.exe"), QLatin1String("clang-cl.exe"));
+    if (toolchainType == ProjectExplorer::Constants::MSVC_TOOLCHAIN_TYPEID) {
+        if (exeFromSettings.endsWith(QLatin1String(QTC_HOST_EXE_SUFFIX)))
+            exeFromSettings.chop(int(qstrlen(QTC_HOST_EXE_SUFFIX)));
+        if (exeFromSettings.endsWith(QLatin1String("clang")))
+            exeFromSettings.append(QLatin1String("-cl"));
+    }
     return clangExecutable(exeFromSettings, isValid);
 }
 

commit a592f9b37a586b02e28abf3749b5218fe684aadf

Clang: Fix accepting absolute path clang executable
Commit 506fc40a319bc785cb6fadfb0598aa9a3d0213ae broke running the analyzer with
absolute paths.

If the clang executable name is provided, the ".exe" suffix is not needed since
we get the absolute path with Utils::Environment::searchInPath(). However, for
absolute path we need to ensure the presence of the suffix in order to point to
an actually existing file.

Also, compare case insensitive.

Task-number: QTCREATORBUG-16234
Change-Id: I9f39386c099cc544297239eca56a95c2e7430555
Reviewed-by: Eike Ziller <eike.ziller@qt.io>
Reviewed-by: Alessandro Portale <alessandro.portale@theqtcompany.com>
Reviewed-by: Tim Jenssen <tim.jenssen@theqtcompany.com>

src/plugins/clangstaticanalyzer/clangstaticanalyzerutils.cpp
@@ -50,25 +50,29 @@ namespace Internal {
 
 QString clangExecutableFromSettings(Core::Id toolchainType, bool *isValid)
 {
-    QString exeFromSettings = ClangStaticAnalyzerSettings::instance()->clangExecutable();
-    if (toolchainType == ProjectExplorer::Constants::MSVC_TOOLCHAIN_TYPEID) {
-        if (exeFromSettings.endsWith(QLatin1String(QTC_HOST_EXE_SUFFIX)))
-            exeFromSettings.chop(int(qstrlen(QTC_HOST_EXE_SUFFIX)));
-        if (exeFromSettings.endsWith(QLatin1String("clang")))
-            exeFromSettings.append(QLatin1String("-cl"));
-    }
-    return clangExecutable(exeFromSettings, isValid);
-}
-
-QString clangExecutable(const QString &fileNameOrPath, bool *isValid)
-{
-    QString executable = fileNameOrPath;
+    QString executable = ClangStaticAnalyzerSettings::instance()->clangExecutable();
     if (executable.isEmpty()) {
         *isValid = false;
         return executable;
     }
 
-    if (!QFileInfo(executable).isAbsolute()) {
+    const QString hostExeSuffix = QLatin1String(QTC_HOST_EXE_SUFFIX);
+    const Qt::CaseSensitivity caseSensitivity = Utils::HostOsInfo::fileNameCaseSensitivity();
+    const bool hasSuffix = executable.endsWith(hostExeSuffix, caseSensitivity);
+
+    if (toolchainType == ProjectExplorer::Constants::MSVC_TOOLCHAIN_TYPEID) {
+        if (hasSuffix)
+            executable.chop(hostExeSuffix.length());
+        executable.append(QLatin1String("-cl"));
+        if (hasSuffix)
+            executable.append(hostExeSuffix);
+    }
+
+    const QFileInfo fileInfo = QFileInfo(executable);
+    if (fileInfo.isAbsolute()) {
+        if (!hasSuffix)
+            executable.append(hostExeSuffix);
+    } else {
         const Utils::Environment &environment = Utils::Environment::systemEnvironment();
         const QString executableFromPath = environment.searchInPath(executable).toString();
         if (executableFromPath.isEmpty()) {

src/plugins/clangstaticanalyzer/clangstaticanalyzerutils.h
@@ -41,7 +41,6 @@ namespace Internal {
 
 bool isClangExecutableUsable(const QString &filePath, QString *errorMessage = 0);
 
-QString clangExecutable(const QString &fileNameOrPath, bool *isValid);
 QString clangExecutableFromSettings(Core::Id toolchainType, bool *isValid);
 
 QString createFullLocationString(const Debugger::DiagnosticLocation &location);