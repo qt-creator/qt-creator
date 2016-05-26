179153829a7bf2d72ffa290b2e1da4bdb58eea90
David Schulz <david.schulz@theqtcompany.com>
2016-05-26 10:43:25 +0200

    CPP: Remove unused function.
    Change-Id: Ib55fd0c059b2e5e117250eb4671b3352ab41b310
    Reviewed-by: Nikita Baryshnikov <nib952051@gmail.com>
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/libs/cplusplus/MatchingText.cpp
+++ src/libs/cplusplus/MatchingText.cpp
@@ -62,19 +62,6 @@ static bool isCompleteStringLiteral(const BackwardsScanner &tk, int index)
     return false;
 }
 
-static bool isCompleteCharLiteral(const BackwardsScanner &tk, int index)
-{
-    const QStringRef text = tk.textRef(index);
-
-    if (text.length() < 2)
-        return false;
-
-    else if (text.at(text.length() - 1) == QLatin1Char('\''))
-        return text.at(text.length() - 2) != QLatin1Char('\\'); // ### not exactly.
-
-    return false;
-}
-
 static bool isEscaped(const QTextCursor &tc)
 {
     const QTextDocument *doc = tc.document();
