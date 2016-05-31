qt-creator/qt-creator

commit 0c441563288ccbbffb8fcffa04f676398a6f2abb

OpenEditorsWindow: Add all history items with a model entry
Also add an history item for which the document is closed, if there is a
DocumentModel::Entry for it. Makes it easier to share some code between
code paths, and is a preparation for making it possible to suspend
editors also later on (not only on session load).

Change-Id: I9d1e50da989eb7e3fc33cbd27394d68750a9d1e9
Reviewed-by: David Schulz <david.schulz@theqtcompany.com>

src/plugins/coreplugin/editormanager/documentmodel.cpp
@@ -381,14 +381,6 @@ void DocumentModel::removeEditor(IEditor *editor, bool *lastOneForDocument)
     }
 }
 
-void DocumentModel::removeDocument(const QString &fileName)
-{
-    int index = d->indexOfFilePath(Utils::FileName::fromString(fileName));
-    // For non suspended entries, we wouldn't know what to do with the associated editors
-    QTC_ASSERT(d->m_entries.at(index)->isSuspended, return);
-    d->removeDocument(index);
-}
-
 void DocumentModelPrivate::removeDocument(int idx)
 {
     if (idx < 0)
@@ -464,6 +456,14 @@ DocumentModel::Entry *DocumentModel::entryForDocument(IDocument *document)
 {
     return Utils::findOrDefault(d->m_entries,
                                 [&document](Entry *entry) { return entry->document == document; });
+}
+
+DocumentModel::Entry *DocumentModel::entryForFilePath(const Utils::FileName &filePath)
+{
+    const int index = d->indexOfFilePath(filePath);
+    if (index < 0)
+        return nullptr;
+    return d->m_entries.at(index);
 }
 
 QList<IDocument *> DocumentModel::openedDocuments()

src/plugins/coreplugin/editormanager/documentmodel.h
@@ -72,6 +72,7 @@ public:
     static QList<Entry *> entries();
     static int indexOfDocument(IDocument *document);
     static Entry *entryForDocument(IDocument *document);
+    static Entry *entryForFilePath(const Utils::FileName &filePath);
     static QList<IDocument *> openedDocuments();
 
     static IDocument *documentForFilePath(const QString &filePath);
@@ -85,7 +86,6 @@ public:
     static void addSuspendedDocument(const QString &fileName, const QString &displayName, Id id);
     static Entry *firstSuspendedEntry();
     static void removeEditor(IEditor *editor, bool *lastOneForDocument);
-    static void removeDocument(const QString &fileName);
     static void removeEntry(Entry *entry);
     static void removeAllSuspendedEntries();
 
@@ -94,5 +94,7 @@ private:
 };
 
 } // namespace Core
+
+Q_DECLARE_METATYPE(Core::DocumentModel::Entry *)
 
 #endif // DOCUMENTMODEL_H

src/plugins/coreplugin/editormanager/editormanager.cpp
@@ -1245,23 +1245,27 @@ IEditor *EditorManagerPrivate::activateEditor(EditorView *view, IEditor *editor,
     return editor;
 }
 
-void EditorManagerPrivate::activateEditorForEntry(EditorView *view, DocumentModel::Entry *entry, EditorManager::OpenEditorFlags flags)
+bool EditorManagerPrivate::activateEditorForEntry(EditorView *view, DocumentModel::Entry *entry,
+                                                  EditorManager::OpenEditorFlags flags)
 {
-    QTC_ASSERT(view, return);
+    QTC_ASSERT(view, return false);
     if (!entry) { // no document
         view->setCurrentEditor(0);
         setCurrentView(view);
         setCurrentEditor(0);
-        return;
+        return false;
     }
     IDocument *document = entry->document;
     if (!entry->isSuspended)  {
-        activateEditorForDocument(view, document, flags);
-        return;
+        IEditor *editor = activateEditorForDocument(view, document, flags);
+        return editor != nullptr;
     }
 
-    if (!openEditor(view, entry->fileName().toString(), entry->id(), flags))
+    if (!openEditor(view, entry->fileName().toString(), entry->id(), flags)) {
         DocumentModel::removeEntry(entry);
+        return false;
+    }
+    return true;
 }
 
 void EditorManagerPrivate::closeEditorOrDocument(IEditor *editor)

src/plugins/coreplugin/editormanager/editormanager_p.h
@@ -87,7 +87,7 @@ public:
                                    EditorManager::OpenEditorFlags flags = EditorManager::NoFlags);
     static IEditor *activateEditorForDocument(EditorView *view, IDocument *document,
                                               EditorManager::OpenEditorFlags flags = 0);
-    static void activateEditorForEntry(EditorView *view, DocumentModel::Entry *entry,
+    static bool activateEditorForEntry(EditorView *view, DocumentModel::Entry *entry,
                                        EditorManager::OpenEditorFlags flags = EditorManager::NoFlags);
     /* closes the document if there is no other editor on the document visible */
     static void closeEditorOrDocument(IEditor *editor);

src/plugins/coreplugin/editormanager/openeditorswindow.cpp
@@ -25,7 +25,6 @@
 
 #include "openeditorswindow.h"
 
-#include "documentmodel.h"
 #include "editormanager.h"
 #include "editormanager_p.h"
 #include "editorview.h"
@@ -44,6 +43,12 @@ Q_DECLARE_METATYPE(Core::IDocument*)
 
 using namespace Core;
 using namespace Core::Internal;
+
+enum class Role
+{
+    Entry = Qt::UserRole,
+    View = Qt::UserRole + 1
+};
 
 OpenEditorsWindow::OpenEditorsWindow(QWidget *parent) :
     QFrame(parent, Qt::Popup),
@@ -173,14 +178,14 @@ void OpenEditorsWindow::setEditors(const QList<EditLocation> &globalHistory, Edi
 {
     m_editorList->clear();
 
-    QSet<IDocument*> documentsDone;
-    addHistoryItems(view->editorHistory(), view, documentsDone);
+    QSet<const DocumentModel::Entry *> entriesDone;
+    addHistoryItems(view->editorHistory(), view, entriesDone);
 
     // add missing editors from the global history
-    addHistoryItems(globalHistory, view, documentsDone);
+    addHistoryItems(globalHistory, view, entriesDone);
 
     // add purely suspended editors which are not initialised yet
-    addSuspendedItems();
+    addRemainingItems(view, entriesDone);
 }
 
 
@@ -188,16 +193,11 @@ void OpenEditorsWindow::selectEditor(QTreeWidgetItem *item)
 {
     if (!item)
         return;
-    if (IDocument *document = item->data(0, Qt::UserRole).value<IDocument*>()) {
-        EditorView *view = item->data(0, Qt::UserRole+1).value<EditorView*>();
-        EditorManagerPrivate::activateEditorForDocument(view, document);
-    } else {
-        if (!EditorManager::openEditor(
-                    item->toolTip(0), item->data(0, Qt::UserRole+2).value<Id>())) {
-            DocumentModel::removeDocument(item->toolTip(0));
-            delete item;
-        }
-    }
+    auto entry = item->data(0, int(Role::Entry)).value<DocumentModel::Entry *>();
+    QTC_ASSERT(entry, return);
+    auto view = item->data(0, int(Role::View)).value<EditorView *>();
+    if (!EditorManagerPrivate::activateEditorForEntry(view, entry))
+        delete item;
 }
 
 void OpenEditorsWindow::editorClicked(QTreeWidgetItem *item)
@@ -212,48 +212,51 @@ void OpenEditorsWindow::ensureCurrentVisible()
     m_editorList->scrollTo(m_editorList->currentIndex(), QAbstractItemView::PositionAtCenter);
 }
 
+static DocumentModel::Entry *entryForEditLocation(const EditLocation &item)
+{
+    if (!item.document.isNull())
+        return DocumentModel::entryForDocument(item.document);
+    return DocumentModel::entryForFilePath(Utils::FileName::fromString(item.fileName));
+}
 
 void OpenEditorsWindow::addHistoryItems(const QList<EditLocation> &history, EditorView *view,
-                                        QSet<IDocument *> &documentsDone)
+                                        QSet<const DocumentModel::Entry *> &entriesDone)
 {
     foreach (const EditLocation &hi, history) {
-        if (hi.document.isNull() || documentsDone.contains(hi.document))
-            continue;
-        documentsDone.insert(hi.document.data());
-        DocumentModel::Entry *entry = DocumentModel::entryForDocument(hi.document);
-        QString title = entry ? entry->displayName() : hi.document->displayName();
-        QTC_ASSERT(!title.isEmpty(), continue);
-        QTreeWidgetItem *item = new QTreeWidgetItem();
-        if (hi.document->isModified())
-            title += tr("*");
-        item->setIcon(0, !hi.document->filePath().isEmpty() && hi.document->isFileReadOnly()
-                      ? DocumentModel::lockedIcon() : m_emptyIcon);
-        item->setText(0, title);
-        item->setToolTip(0, hi.document->filePath().toString());
-        item->setData(0, Qt::UserRole, QVariant::fromValue(hi.document.data()));
-        item->setData(0, Qt::UserRole+1, QVariant::fromValue(view));
-        item->setTextAlignment(0, Qt::AlignLeft);
-
-        m_editorList->addTopLevelItem(item);
-
-        if (m_editorList->topLevelItemCount() == 1)
-            m_editorList->setCurrentItem(item);
+        if (DocumentModel::Entry *entry = entryForEditLocation(hi))
+            addItem(entry, entriesDone, view);
     }
 }
 
-void OpenEditorsWindow::addSuspendedItems()
+void OpenEditorsWindow::addRemainingItems(EditorView *view,
+                                          QSet<const DocumentModel::Entry *> &entriesDone)
 {
-    foreach (DocumentModel::Entry *entry, DocumentModel::entries()) {
-        if (!entry->isSuspended)
-            continue;
-        QTreeWidgetItem *item = new QTreeWidgetItem();
-        QString title = entry->displayName();
-        item->setIcon(0, m_emptyIcon);
-        item->setText(0, title);
-        item->setToolTip(0, entry->fileName().toString());
-        item->setData(0, Qt::UserRole+2, QVariant::fromValue(entry->id()));
-        item->setTextAlignment(0, Qt::AlignLeft);
+    foreach (DocumentModel::Entry *entry, DocumentModel::entries())
+        addItem(entry, entriesDone, view);
+}
 
-        m_editorList->addTopLevelItem(item);
-    }
+void OpenEditorsWindow::addItem(DocumentModel::Entry *entry,
+                                QSet<const DocumentModel::Entry *> &entriesDone,
+                                EditorView *view)
+{
+    if (entriesDone.contains(entry))
+        return;
+    entriesDone.insert(entry);
+    QString title = entry->displayName();
+    QTC_ASSERT(!title.isEmpty(), return);
+    QTreeWidgetItem *item = new QTreeWidgetItem();
+    if (entry->document->isModified())
+        title += tr("*");
+    item->setIcon(0, !entry->fileName().isEmpty() && entry->document->isFileReadOnly()
+                  ? DocumentModel::lockedIcon() : m_emptyIcon);
+    item->setText(0, title);
+    item->setToolTip(0, entry->fileName().toString());
+    item->setData(0, int(Role::Entry), QVariant::fromValue(entry));
+    item->setData(0, int(Role::View), QVariant::fromValue(view));
+    item->setTextAlignment(0, Qt::AlignLeft);
+
+    m_editorList->addTopLevelItem(item);
+
+    if (m_editorList->topLevelItemCount() == 1)
+        m_editorList->setCurrentItem(item);
 }

src/plugins/coreplugin/editormanager/openeditorswindow.h
@@ -26,6 +26,7 @@
 #ifndef OPENEDITORSWINDOW_H
 #define OPENEDITORSWINDOW_H
 
+#include "documentmodel.h"
 #include "editorview.h"
 
 #include <QFrame>
@@ -78,8 +79,12 @@ private:
     void editorClicked(QTreeWidgetItem *item);
     void selectEditor(QTreeWidgetItem *item);
 
-    void addHistoryItems(const QList<EditLocation> &history, EditorView *view, QSet<IDocument*> &documentsDone);
-    void addSuspendedItems();
+    void addHistoryItems(const QList<EditLocation> &history, EditorView *view,
+                         QSet<const DocumentModel::Entry *> &entriesDone);
+    void addRemainingItems(EditorView *view,
+                           QSet<const DocumentModel::Entry *> &entriesDone);
+    void addItem(DocumentModel::Entry *entry, QSet<const DocumentModel::Entry *> &entriesDone,
+                 EditorView *view);
     void ensureCurrentVisible();
     void selectUpDown(bool up);
 

commit 876099d865e2de987b3a120ae640a781e7c04287

DocumentModel: Avoid deletion of entries for suspended documents
When opening a previously suspended document, reuse the existing entry
instead of replacing it. That way existing pointers to the entry are not
invalidated.

Fixes a regression introduced by 0c441563288ccbbffb8fcffa04f676398a6f2abb
that lead to crashes when selecting suspended documents from the
history popup (ctrl+tab / opt+tab) with the mouse.

Change-Id: Id41cae1132e4435c8e8bbd8624eaf840b02dc31f
Reviewed-by: Alexandru Croitor <alexandru.croitor@theqtcompany.com>
Reviewed-by: David Schulz <david.schulz@theqtcompany.com>

src/plugins/coreplugin/editormanager/documentmodel.cpp
@@ -230,18 +230,14 @@ void DocumentModelPrivate::addEntry(DocumentModel::Entry *entry)
         DocumentModel::Entry *previousEntry = m_entries.at(previousIndex);
         const bool replace = !entry->isSuspended && previousEntry->isSuspended;
         if (replace) {
-            delete previousEntry;
-            m_entries[previousIndex] = entry;
-            if (!fixedPath.isEmpty())
-                m_entryByFixedPath[fixedPath] = entry;
-        } else {
-            delete entry;
-            entry = previousEntry;
+            previousEntry->isSuspended = false;
+            delete previousEntry->document;
+            previousEntry->document = entry->document;
+            connect(previousEntry->document, &IDocument::changed, this, &DocumentModelPrivate::itemChanged);
         }
-        previousEntry = 0;
-        disambiguateDisplayNames(entry);
-        if (replace)
-            connect(entry->document, &IDocument::changed, this, &DocumentModelPrivate::itemChanged);
+        delete entry;
+        entry = nullptr;
+        disambiguateDisplayNames(previousEntry);
         return;
     }
 