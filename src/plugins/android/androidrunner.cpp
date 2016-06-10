qt-creator/qt-creator

commit ddefe062c73e35def585f8fc6c90a4f18e47c0f4

Fix up QProcess::waitForFinished()
waitForFinish returns false if the process is no longer running at
the time of the call. Handle that throughout the codebase.

Change-Id: Ia7194095454e82efbd4eb88f2d55926bdd09e094
Reviewed-by: hjk <hjk@theqtcompany.com>

src/plugins/android/androidrunner.cpp
@@ -37,6 +37,7 @@
 #include <qtsupport/qtkitinformation.h>
 #include <utils/qtcassert.h>
 #include <utils/runextensions.h>
+#include <utils/synchronousprocess.h>
 
 #include <QApplication>
 #include <QDir>
@@ -169,10 +170,11 @@ AndroidRunner::AndroidRunner(QObject *parent,
 
 
     // Detect busybox, as we need to pass -w to ps to get wide output.
-    QProcess psProc;
-    psProc.start(m_adb, selector() << _("shell") << _("readlink") << _("$(which ps)"));
-    psProc.waitForFinished();
-    QByteArray which = psProc.readAll();
+    Utils::SynchronousProcess psProc;
+    psProc.setTimeoutS(5);
+    Utils::SynchronousProcessResponse response
+            = psProc.run(m_adb, selector() << _("shell") << _("readlink") << _("$(which ps)"));
+    const QString which = response.allOutput();
     m_isBusyBox = which.startsWith("busybox");
 
     m_checkPIDTimer.setInterval(1000);
@@ -302,10 +304,8 @@ void AndroidRunner::checkPID()
 
 void AndroidRunner::forceStop()
 {
-    QProcess proc;
-    proc.start(m_adb, selector() << _("shell") << _("am") << _("force-stop")
-               << m_androidRunnable.packageName);
-    proc.waitForFinished();
+    runAdb(selector() << _("shell") << _("am") << _("force-stop") << m_androidRunnable.packageName,
+           nullptr, 30);
 
     // try killing it via kill -9
     const QByteArray out = runPs();
@@ -334,34 +334,22 @@ void AndroidRunner::asyncStart()
 {
     QMutexLocker locker(&m_mutex);
     forceStop();
+    QString errorMessage;
 
-    if (m_useCppDebugger) {
-        // Remove pong file.
-        QProcess adb;
-        adb.start(m_adb, selector() << _("shell") << _("rm") << m_pongFile);
-        adb.waitForFinished();
-    }
+    if (m_useCppDebugger)
+        runAdb(selector() << _("shell") << _("rm") << m_pongFile); // Remove pong file.
 
-    foreach (const QStringList &entry, m_androidRunnable.beforeStartADBCommands) {
-        QProcess adb;
-        adb.start(m_adb, selector() << entry);
-        adb.waitForFinished();
-    }
+    foreach (const QStringList &entry, m_androidRunnable.beforeStartADBCommands)
+        runAdb(selector() << entry);
 
     QStringList args = selector();
     args << _("shell") << _("am") << _("start") << _("-n") << m_androidRunnable.intentName;
 
     if (m_useCppDebugger) {
-        QProcess adb;
-        adb.start(m_adb, selector() << _("forward")
-                  << QString::fromLatin1("tcp:%1").arg(m_localGdbServerPort.number())
-                  << _("localfilesystem:") + m_gdbserverSocket);
-        if (!adb.waitForStarted()) {
-            emit remoteProcessFinished(tr("Failed to forward C++ debugging ports. Reason: %1.").arg(adb.errorString()));
-            return;
-        }
-        if (!adb.waitForFinished(10000)) {
-            emit remoteProcessFinished(tr("Failed to forward C++ debugging ports."));
+        if (!runAdb(selector() << _("forward")
+                    << QString::fromLatin1("tcp:%1").arg(m_localGdbServerPort.number())
+                    << _("localfilesystem:") + m_gdbserverSocket, &errorMessage)) {
+            emit remoteProcessFinished(tr("Failed to forward C++ debugging ports. Reason: %1.").arg(errorMessage));
             return;
         }
 
@@ -380,15 +368,9 @@ void AndroidRunner::asyncStart()
         args << _("-e") << _("gdbserver_socket") << m_gdbserverSocket;
 
         if (m_handShakeMethod == SocketHandShake) {
-            QProcess adb;
             const QString port = QString::fromLatin1("tcp:%1").arg(socketHandShakePort);
-            adb.start(m_adb, selector() << _("forward") << port << _("localabstract:") + pingPongSocket);
-            if (!adb.waitForStarted()) {
-                emit remoteProcessFinished(tr("Failed to forward ping pong ports. Reason: %1.").arg(adb.errorString()));
-                return;
-            }
-            if (!adb.waitForFinished()) {
-                emit remoteProcessFinished(tr("Failed to forward ping pong ports."));
+            if (!runAdb(selector() << _("forward") << port << _("localabstract:") + pingPongSocket, &errorMessage)) {
+                emit remoteProcessFinished(tr("Failed to forward ping pong ports. Reason: %1.").arg(errorMessage));
                 return;
             }
         }
@@ -397,14 +379,8 @@ void AndroidRunner::asyncStart()
     if (m_qmlDebugServices != QmlDebug::NoQmlDebugServices) {
         // currently forward to same port on device and host
         const QString port = QString::fromLatin1("tcp:%1").arg(m_qmlPort.number());
-        QProcess adb;
-        adb.start(m_adb, selector() << _("forward") << port << port);
-        if (!adb.waitForStarted()) {
-            emit remoteProcessFinished(tr("Failed to forward QML debugging ports. Reason: %1.").arg(adb.errorString()));
-            return;
-        }
-        if (!adb.waitForFinished()) {
-            emit remoteProcessFinished(tr("Failed to forward QML debugging ports."));
+        if (!runAdb(selector() << _("forward") << port << port, &errorMessage)) {
+            emit remoteProcessFinished(tr("Failed to forward QML debugging ports. Reason: %1.").arg(errorMessage));
             return;
         }
 
@@ -414,15 +390,8 @@ void AndroidRunner::asyncStart()
                 .arg(m_qmlPort.number()).arg(QmlDebug::qmlDebugServices(m_qmlDebugServices));
     }
 
-    QProcess adb;
-    adb.start(m_adb, args);
-    if (!adb.waitForStarted()) {
-        emit remoteProcessFinished(tr("Failed to start the activity. Reason: %1.").arg(adb.errorString()));
-        return;
-    }
-    if (!adb.waitForFinished(10000)) {
-        adb.terminate();
-        emit remoteProcessFinished(tr("Unable to start \"%1\".").arg(m_androidRunnable.packageName));
+    if (!runAdb(args, &errorMessage)) {
+        emit remoteProcessFinished(tr("Failed to start the activity. Reason: %1.").arg(errorMessage));
         return;
     }
 
@@ -476,9 +445,7 @@ void AndroidRunner::asyncStart()
                 tmp.open();
                 tmp.close();
 
-                QProcess process;
-                process.start(m_adb, selector() << _("pull") << m_pingFile << tmp.fileName());
-                process.waitForFinished();
+                runAdb(selector() << _("pull") << m_pingFile << tmp.fileName());
 
                 QFile res(tmp.fileName());
                 const bool doBreak = res.size();
@@ -510,18 +477,28 @@ bool AndroidRunner::adbShellAmNeedsQuotes()
     // The command will fail with a complaint about the "--dummy"
     // option on newer SDKs, and with "No intent supplied" on older ones.
     // In case the test itself fails assume a new SDK.
-    QProcess adb;
-    adb.start(m_adb, selector() << _("shell") << _("am") << _("start")
-                                << _("-e") << _("dummy") <<_("dummy --dummy"));
-    if (!adb.waitForStarted())
+    Utils::SynchronousProcess adb;
+    adb.setTimeoutS(10);
+    Utils::SynchronousProcessResponse response
+            = adb.run(m_adb, selector() << _("shell") << _("am") << _("start")
+                      << _("-e") << _("dummy") <<_("dummy --dummy"));
+    if (response.result == Utils::SynchronousProcessResponse::StartFailed
+            || response.result != Utils::SynchronousProcessResponse::Finished)
         return true;
 
-    if (!adb.waitForFinished(10000))
-        return true;
+    const QString output = response.allOutput();
+    return output.contains(QLatin1String("Error: No intent supplied"));
+}
 
-    QByteArray output = adb.readAllStandardError() + adb.readAllStandardOutput();
-    bool oldSdk = output.contains("Error: No intent supplied");
-    return !oldSdk;
+bool AndroidRunner::runAdb(const QStringList &args, QString *errorMessage, int timeoutS)
+{
+    Utils::SynchronousProcess adb;
+    adb.setTimeoutS(timeoutS);
+    Utils::SynchronousProcessResponse response
+            = adb.run(m_adb, args);
+    if (errorMessage)
+        *errorMessage = response.exitMessage(m_adb, timeoutS);
+    return response.result == Utils::SynchronousProcessResponse::Finished;
 }
 
 void AndroidRunner::handleRemoteDebuggerRunning()
@@ -535,9 +512,7 @@ void AndroidRunner::handleRemoteDebuggerRunning()
             QTemporaryFile tmp(QDir::tempPath() + _("/pingpong"));
             tmp.open();
 
-            QProcess process;
-            process.start(m_adb, selector() << _("push") << tmp.fileName() << m_pongFile);
-            process.waitForFinished();
+            runAdb(selector() << _("push") << tmp.fileName() << m_pongFile);
         }
         QTC_CHECK(m_processPID != -1);
     }
@@ -558,11 +533,8 @@ void AndroidRunner::stop()
     m_adbLogcatProcess.waitForFinished();
     m_psProc.kill();
     m_psProc.waitForFinished();
-    foreach (const QStringList &entry, m_androidRunnable.afterFinishADBCommands) {
-        QProcess adb;
-        adb.start(m_adb, selector() << entry);
-        adb.waitForFinished();
-    }
+    foreach (const QStringList &entry, m_androidRunnable.afterFinishADBCommands)
+        runAdb(selector() << entry);
 }
 
 void AndroidRunner::logcatProcess(const QByteArray &text, QByteArray &buffer, bool onlyError)
@@ -622,19 +594,9 @@ void AndroidRunner::logcatReadStandardOutput()
 
 void AndroidRunner::adbKill(qint64 pid)
 {
-    {
-        QProcess process;
-        process.start(m_adb, selector() << _("shell")
-            << _("kill") << QLatin1String("-9") << QString::number(pid));
-        process.waitForFinished();
-    }
-    {
-        QProcess process;
-        process.start(m_adb, selector() << _("shell")
-            << _("run-as") << m_androidRunnable.packageName
-            << _("kill") << QLatin1String("-9") << QString::number(pid));
-        process.waitForFinished();
-    }
+    runAdb(selector() << _("shell") << _("kill") << QLatin1String("-9") << QString::number(pid));
+    runAdb(selector() << _("shell") << _("run-as") << m_androidRunnable.packageName
+           << _("kill") << QLatin1String("-9") << QString::number(pid));
 }
 
 QString AndroidRunner::displayName() const

src/libs/utils/buildablehelperlibrary.cpp
@@ -40,24 +40,22 @@ bool BuildableHelperLibrary::isQtChooser(const QFileInfo &info)
 
 QString BuildableHelperLibrary::qtChooserToQmakePath(const QString &path)
 {
-    const char toolDir[] = "QTTOOLDIR=\"";
-    QProcess proc;
-    proc.start(path, QStringList(QLatin1String("-print-env")));
-    if (!proc.waitForStarted(1000))
+    const QString toolDir = QLatin1String("QTTOOLDIR=\"");
+    SynchronousProcess proc;
+    proc.setTimeoutS(1);
+    SynchronousProcessResponse response = proc.run(path, QStringList(QLatin1String("-print-env")));
+    if (response.result != SynchronousProcessResponse::Finished)
         return QString();
-    if (!proc.waitForFinished(1000))
-        return QString();
-    QByteArray output = proc.readAllStandardOutput();
+    const QString output = response.stdOut;
     int pos = output.indexOf(toolDir);
     if (pos == -1)
         return QString();
-    pos += int(sizeof(toolDir)) - 1;
+    pos += toolDir.count() - 1;
     int end = output.indexOf('\"', pos);
     if (end == -1)
         return QString();
 
-    QString result = QString::fromLocal8Bit(output.mid(pos, end - pos)) + QLatin1String("/qmake");
-    return result;
+    return output.mid(pos, end - pos) + QLatin1String("/qmake");
 }
 
 static bool isQmake(const QString &path)
@@ -104,23 +102,15 @@ QString BuildableHelperLibrary::qtVersionForQMake(const QString &qmakePath)
     if (qmakePath.isEmpty())
         return QString();
 
-    QProcess qmake;
-    qmake.start(qmakePath, QStringList(QLatin1String("--version")));
-    if (!qmake.waitForStarted()) {
-        qWarning("Cannot start '%s': %s", qPrintable(qmakePath), qPrintable(qmake.errorString()));
-        return QString();
-    }
-    if (!qmake.waitForFinished())      {
-        SynchronousProcess::stopProcess(qmake);
-        qWarning("Timeout running '%s'.", qPrintable(qmakePath));
-        return QString();
-    }
-    if (qmake.exitStatus() != QProcess::NormalExit) {
-        qWarning("'%s' crashed.", qPrintable(qmakePath));
+    SynchronousProcess qmake;
+    qmake.setTimeoutS(5);
+    SynchronousProcessResponse response = qmake.run(qmakePath, QStringList(QLatin1String("--version")));
+    if (response.result != SynchronousProcessResponse::Finished) {
+        qWarning() << response.exitMessage(qmakePath, 5);
         return QString();
     }
 
-    const QString output = QString::fromLocal8Bit(qmake.readAllStandardOutput());
+    const QString output = response.allOutput();
     static QRegExp regexp(QLatin1String("(QMake version|QMake version:)[\\s]*([\\d.]*)"),
                           Qt::CaseInsensitive);
     regexp.indexIn(output);

src/libs/utils/consoleprocess_unix.cpp
@@ -211,7 +211,7 @@ void ConsoleProcess::stop()
     killStub();
     if (isRunning()) {
         d->m_process.terminate();
-        if (!d->m_process.waitForFinished(1000)) {
+        if (!d->m_process.waitForFinished(1000) && d->m_process.state() == QProcess::Running) {
             d->m_process.kill();
             d->m_process.waitForFinished();
         }

src/libs/utils/pathchooser.cpp
@@ -129,16 +129,12 @@ QString BinaryVersionToolTipEventFilter::toolVersion(const QString &binary, cons
 {
     if (binary.isEmpty())
         return QString();
-    QProcess proc;
-    proc.start(binary, arguments);
-    if (!proc.waitForStarted())
+    SynchronousProcess proc;
+    proc.setTimeoutS(1);
+    SynchronousProcessResponse response = proc.run(binary, arguments);
+    if (response.result != SynchronousProcessResponse::Finished)
         return QString();
-    if (!proc.waitForFinished()) {
-        SynchronousProcess::stopProcess(proc);
-        return QString();
-    }
-    return QString::fromLocal8Bit(QByteArray(proc.readAllStandardOutput()
-        + proc.readAllStandardError()));
+    return response.allOutput();
 }
 
 // Extends BinaryVersionToolTipEventFilter to prepend the existing pathchooser

src/libs/utils/synchronousprocess.cpp
@@ -137,6 +137,17 @@ QString SynchronousProcessResponse::exitMessage(const QString &binary, int timeo
     return QString();
 }
 
+QString SynchronousProcessResponse::allOutput() const
+{
+    if (!stdOut.isEmpty() && !stdErr.isEmpty()) {
+        if (stdOut.endsWith(QLatin1Char('\n')))
+            return stdOut + stdErr;
+        else
+            return stdOut + QLatin1Char('\n') + stdErr;
+    }
+    return !stdOut.isEmpty() ? stdOut : stdErr;
+}
+
 QTCREATOR_UTILS_EXPORT QDebug operator<<(QDebug str, const SynchronousProcessResponse& r)
 {
     QDebug nsp = str.nospace();
@@ -579,9 +590,8 @@ bool SynchronousProcess::readDataFromProcess(QProcess &p, int timeoutS,
     bool finished = false;
     bool hasData = false;
     do {
-        finished = p.state() == QProcess::NotRunning
-                || p.waitForFinished(timeoutS > 0 ? timeoutS * 1000 : -1);
-        hasData = false;
+        finished = p.waitForFinished(timeoutS > 0 ? timeoutS * 1000 : -1)
+                || p.state() == QProcess::NotRunning;
         // First check 'stdout'
         if (p.bytesAvailable()) { // applies to readChannel() only
             hasData = true;
@@ -608,13 +618,13 @@ bool SynchronousProcess::readDataFromProcess(QProcess &p, int timeoutS,
 
 bool SynchronousProcess::stopProcess(QProcess &p)
 {
-    if (p.state() != QProcess::Running)
+    if (p.state() == QProcess::NotRunning)
         return true;
     p.terminate();
-    if (p.waitForFinished(300))
+    if (p.waitForFinished(300) && p.state() == QProcess::Running)
         return true;
     p.kill();
-    return p.waitForFinished(300);
+    return p.waitForFinished(300) || p.state() == QProcess::NotRunning;
 }
 
 // Path utilities

src/libs/utils/synchronousprocess.h
@@ -59,6 +59,7 @@ struct QTCREATOR_UTILS_EXPORT SynchronousProcessResponse
 
     // Helper to format an exit message.
     QString exitMessage(const QString &binary, int timeoutS) const;
+    QString allOutput() const;
 
     Result result;
     int exitCode;

src/plugins/android/androidbuildapkstep.cpp
@@ -43,10 +43,11 @@
 
 #include <qtsupport/qtkitinformation.h>
 
-#include <utils/qtcprocess.h>
+#include <utils/synchronousprocess.h>
 
 #include <QInputDialog>
 #include <QMessageBox>
+#include <QProcess>
 
 namespace Android {
 using namespace Internal;
@@ -279,29 +280,32 @@ bool AndroidBuildApkStep::verboseOutput() const
 QAbstractItemModel *AndroidBuildApkStep::keystoreCertificates()
 {
     QString rawCerts;
-    QProcess keytoolProc;
     while (!rawCerts.length() || !m_keystorePasswd.length()) {
-        QStringList params;
-        params << QLatin1String("-list") << QLatin1String("-v") << QLatin1String("-keystore") << m_keystorePath.toUserOutput() << QLatin1String("-storepass");
+        QStringList params
+                = { QLatin1String("-list"), QLatin1String("-v"), QLatin1String("-keystore"),
+                    m_keystorePath.toUserOutput(), QLatin1String("-storepass") };
         if (!m_keystorePasswd.length())
             keystorePassword();
         if (!m_keystorePasswd.length())
-            return 0;
+            return nullptr;
         params << m_keystorePasswd;
         params << QLatin1String("-J-Duser.language=en");
-        keytoolProc.start(AndroidConfigurations::currentConfig().keytoolPath().toString(), params);
-        if (!keytoolProc.waitForStarted() || !keytoolProc.waitForFinished()) {
+
+        Utils::SynchronousProcess keytoolProc;
+        keytoolProc.setTimeoutS(30);
+        const Utils::SynchronousProcessResponse response
+                = keytoolProc.run(AndroidConfigurations::currentConfig().keytoolPath().toString(), params);
+        if (response.result != Utils::SynchronousProcessResponse::Finished) {
             QMessageBox::critical(0, tr("Error"),
                                   tr("Failed to run keytool."));
-            return 0;
+            return nullptr;
         }
 
-        if (keytoolProc.exitCode()) {
-            QMessageBox::critical(0, tr("Error"),
-                                  tr("Invalid password."));
+        if (response.exitCode != 0) {
+            QMessageBox::critical(0, tr("Error"), tr("Invalid password."));
             m_keystorePasswd.clear();
         }
-        rawCerts = QString::fromLatin1(keytoolProc.readAllStandardOutput());
+        rawCerts = response.stdOut;
     }
     return new CertificatesModel(rawCerts, this);
 }

commit 4ce0494284dfc3bf832e54f9315b9bf6d02a4d28

Android: Fix adbShellAmNeedsQuotes
Regressions was introduced by ddefe062c73e35def585f8fc6c90a4f18e47c0f4

Change-Id: I0bc3f75b788472db78998c32d0007d4250a62f5a
Reviewed-by: hjk <hjk@theqtcompany.com>

src/plugins/android/androidrunner.cpp
@@ -487,7 +487,8 @@ bool AndroidRunner::adbShellAmNeedsQuotes()
         return true;
 
     const QString output = response.allOutput();
-    return output.contains(QLatin1String("Error: No intent supplied"));
+    const bool oldSdk = output.contains("Error: No intent supplied");
+    return !oldSdk;
 }
 
 bool AndroidRunner::runAdb(const QStringList &args, QString *errorMessage, int timeoutS)