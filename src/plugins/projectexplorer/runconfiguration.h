qt-creator/qt-creator

commit 3f11ef92167ba4622a5326eb7ef1fc67b61866eb

Avoid using dynamic_cast<>
dynamic_cast<> breaks in cross-library situations. In the past
the issue was witnessed on Mac, this time on FreeBSD in
various configurations.

The workaround deployed here is to manually create unique
type ids in form of (addresses of) global variables.

Task-id: QTCREATORBUG-16462
Change-Id: Ie28fbb3d31d06c1a722a3d9ea808831191298e71
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>
Reviewed-by: Ralf Nolden <nolden@kde.org>
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/projectexplorer/runconfiguration.h
@@ -159,6 +159,7 @@ public:
     virtual ~ClonableConcept() = default;
     virtual ClonableConcept *clone() const = 0;
     virtual bool equals(const std::unique_ptr<ClonableConcept> &other) const = 0;
+    virtual void *typeId() const = 0;
 };
 
 template <class T>
@@ -168,11 +169,14 @@ public:
     ClonableModel(const T &data) : m_data(data) { }
     ~ClonableModel() Q_DECL_NOEXCEPT { } // gcc 4.7.3
     ClonableConcept *clone() const override { return new ClonableModel(*this); }
+    void *typeId() const { return T::staticTypeId; }
 
     bool equals(const std::unique_ptr<ClonableConcept> &other) const override
     {
-        auto that = dynamic_cast<const ClonableModel<T> *>(other.get());
-        return that && m_data == that->m_data;
+        if (other->typeId() != typeId())
+            return false;
+        auto that = static_cast<const ClonableModel<T> *>(other.get());
+        return m_data == that->m_data;
     }
 
     T m_data;
@@ -189,7 +193,7 @@ public:
     void operator=(Runnable other) { d = std::move(other.d); }
 
     template <class T> bool is() const {
-        return dynamic_cast<ClonableModel<T> *>(d.get()) != 0;
+        return d.get()->typeId() == T::staticTypeId;
     }
 
     template <class T> const T &as() const {
@@ -213,7 +217,7 @@ public:
     void operator=(Connection other) { d = std::move(other.d); }
 
     template <class T> bool is() const {
-        return dynamic_cast<ClonableModel<T> *>(d.get()) != 0;
+        return d.get()->typeId() == T::staticTypeId;
     }
 
     template <class T> const T &as() const {

src/plugins/android/android.pro
@@ -89,7 +89,8 @@ SOURCES += \
     avddialog.cpp \
     androidbuildapkstep.cpp \
     androidbuildapkwidget.cpp \
-    androidqtsupport.cpp
+    androidqtsupport.cpp \
+    androidrunnable.cpp
 
 FORMS += \
     androidsettingswidget.ui \

src/plugins/android/android.qbs
@@ -76,6 +76,7 @@ QtcPlugin {
         "androidruncontrol.h",
         "androidrunfactories.cpp",
         "androidrunfactories.h",
+        "androidrunnable.cpp",
         "androidrunnable.h",
         "androidrunner.cpp",
         "androidrunner.h",

src/plugins/android/androiddevice.cpp
@@ -27,6 +27,8 @@
 #include "androidconstants.h"
 #include "androidsignaloperation.h"
 
+#include <projectexplorer/runnables.h>
+
 #include <QCoreApplication>
 
 using namespace ProjectExplorer;

src/plugins/android/androidrunnable.cpp
@@ -23,42 +23,10 @@
 **
 ****************************************************************************/
 
-#pragma once
+#include "androidrunnable.h"
 
-#include "runconfiguration.h"
+namespace Android {
 
-#include "applicationlauncher.h"
-#include "devicesupport/idevice.h"
+void *AndroidRunnable::staticTypeId = &AndroidRunnable::staticTypeId;
 
-#include <utils/environment.h>
-
-namespace ProjectExplorer {
-
-class PROJECTEXPLORER_EXPORT StandardRunnable
-{
-public:
-    QString executable;
-    QString commandLineArguments;
-    QString workingDirectory;
-    Utils::Environment environment;
-    ApplicationLauncher::Mode runMode = ApplicationLauncher::Gui;
-    IDevice::ConstPtr device; // Override the kit's device. Keep unset by default.
-
-    static void *staticTypeId;
-};
-
-PROJECTEXPLORER_EXPORT bool operator==(const StandardRunnable &r1, const StandardRunnable &r2);
-
-class PROJECTEXPLORER_EXPORT HostName
-{
-public:
-    explicit HostName(const QString &host) : m_host(host) {}
-    QString host() const { return m_host; }
-
-    static void *staticTypeId;
-
-private:
-    QString m_host;
-};
-
-} // namespace ProjectExplorer
+} // namespace Android

src/plugins/android/androidrunnable.h
@@ -40,6 +40,8 @@ struct ANDROID_EXPORT AndroidRunnable
     QVector<QStringList> beforeStartADBCommands;
     QVector<QStringList> afterFinishADBCommands;
     QString deviceSerialNumber;
+
+    static void *staticTypeId;
 };
 
 inline bool operator==(const AndroidRunnable &r1, const AndroidRunnable &r2)

src/plugins/debugger/analyzer/analyzerstartparameters.h
@@ -42,6 +42,8 @@ public:
     QString analyzerHost;
     QString analyzerSocket;
     quint16 analyzerPort = 0;
+
+    static void *staticTypeId;
 };
 
 DEBUGGER_EXPORT bool operator==(const AnalyzerConnection &c1, const AnalyzerConnection &c2);

src/plugins/debugger/debuggerplugin.cpp
@@ -3834,6 +3834,9 @@ QList<QObject *> DebuggerPlugin::createTestObjects() const
 #endif // if  WITH_TESTS
 
 } // namespace Internal
+
+void *AnalyzerConnection::staticTypeId = &AnalyzerConnection::staticTypeId;
+
 } // namespace Debugger
 
 #include "debuggerplugin.moc"

commit 438697219ff8ae4ad78ed5cd176abcce10bc7348

ProjectExplorer: Fix comparison with default-constructed Runnables
Regression was introduced with 3f11ef92167.

The original check asserting 'that != 0' is still needed without
dynamic_cast, as d-pointer of 'other' can be statically 0 for
default-constructed Runnable objects.

Change-Id: Ibee898ffc61fb2f4a6a6276390fdf6d517fde546
Reviewed-by: Eike Ziller <eike.ziller@qt.io>
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/plugins/projectexplorer/runconfiguration.h
@@ -173,6 +173,8 @@ public:
 
     bool equals(const std::unique_ptr<ClonableConcept> &other) const override
     {
+        if (!other.get())
+            return false;
         if (other->typeId() != typeId())
             return false;
         auto that = static_cast<const ClonableModel<T> *>(other.get());