0db6f2a372f153f3f5403dd96f310b68d7b55de1
Thomas Hartmann <Thomas.Hartmann@theqtcompany.com>
2016-07-01 14:36:34 +0200

    QmlDesigner: A StateChangeScript can be the child of a State
    I found this while testing our demos.
    
    Change-Id: I575e1d2f487a2ba137ac8181f4980c30b171f726
    Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

--- src/libs/qmljs/qmljscheck.cpp
+++ src/libs/qmljs/qmljscheck.cpp
@@ -821,7 +821,8 @@ void Check::visitQmlObject(Node *ast, UiQualifiedId *typeId,
     const QString typeName = getRightMostIdentifier(typeId)->name.toString();
 
     if (!m_typeStack.isEmpty() && m_typeStack.last() == QLatin1String("State")
-            && typeId->name.toString() != "PropertyChanges")
+            && typeId->name.toString() != "PropertyChanges"
+            && typeId->name.toString() != "StateChangeScript")
         addMessage(StateCannotHaveChildItem, typeErrorLocation, typeName);
 
     if (checkTypeForDesignerSupport(typeId))
