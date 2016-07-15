7c48f9c5675e42bc4b5c9eeb382f6f869c25190a
hjk <hjk@theqtcompany.com>
2016-07-15 13:48:24 +0200

    QNX: Prevent crash on failing attach to running process
    Change-Id: Ibe8c8e83f81e7111bdd782881eb9f44df71521fe
    Reviewed-by: Andy Shaw <andy.shaw@qt.io>

--- src/plugins/qnx/qnxplugin.cpp
+++ src/plugins/qnx/qnxplugin.cpp
@@ -115,6 +115,6 @@ void QnxPlugin::updateDebuggerActions()
         }
     }
 
-    m_attachToQnxApplication->setVisible(hasValidQnxKit);
-    m_debugSeparator->setVisible(hasValidQnxKit);
+    m_attachToQnxApplication->setVisible(false && hasValidQnxKit); // FIXME
+    m_debugSeparator->setVisible(false && hasValidQnxKit); // FIXME QTCREATORBUG-16608
 }
