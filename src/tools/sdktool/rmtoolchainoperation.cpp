7ed7ecd4a583c8a59fc444cd840993ba285dfce1
Tobias Hunger <tobias.hunger@qt.io>
2016-07-15 12:07:32 +0200

    SDKtool: De-noise rmtoolchainoperation
    Change-Id: I65f670a3590ad8502195d431bbcdb68c0b77273b
    Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

--- src/tools/sdktool/rmtoolchainoperation.cpp
+++ src/tools/sdktool/rmtoolchainoperation.cpp
@@ -43,17 +43,17 @@ const char ID[] = "ProjectExplorer.ToolChain.Id";
 
 QString RmToolChainOperation::name() const
 {
-    return QLatin1String("rmTC");
+    return "rmTC";
 }
 
 QString RmToolChainOperation::helpText() const
 {
-    return QLatin1String("remove a tool chain from Qt Creator");
+    return "remove a tool chain from Qt Creator";
 }
 
 QString RmToolChainOperation::argumentsHelpText() const
 {
-    return QLatin1String("    --id <ID>  The id of the tool chain to remove.\n");
+    return "    --id <ID>  The id of the tool chain to remove.\n";
 }
 
 bool RmToolChainOperation::setArguments(const QStringList &args)
@@ -62,7 +62,7 @@ bool RmToolChainOperation::setArguments(const QStringList &args)
         const QString current = args.at(i);
         const QString next = ((i + 1) < args.count()) ? args.at(i + 1) : QString();
 
-        if (current == QLatin1String("--id")) {
+        if (current == "--id") {
             if (next.isNull()) {
                 std::cerr << "No parameter for --id given." << std::endl << std::endl;
                 return false;
@@ -81,7 +81,7 @@ bool RmToolChainOperation::setArguments(const QStringList &args)
 
 int RmToolChainOperation::execute() const
 {
-    QVariantMap map = load(QLatin1String("ToolChains"));
+    QVariantMap map = load("ToolChains");
     if (map.isEmpty())
         return 0;
 
@@ -89,7 +89,7 @@ int RmToolChainOperation::execute() const
     if (result == map)
         return 2;
 
-    return save(result, QLatin1String("ToolChains")) ? 0 : 3;
+    return save(result, "ToolChains") ? 0 : 3;
 }
 
 #ifdef WITH_TESTS
@@ -97,38 +97,35 @@ bool RmToolChainOperation::test() const
 {
     // Add toolchain:
     QVariantMap map = AddToolChainOperation::initializeToolChains();
-    map = AddToolChainOperation::addToolChain(map, QLatin1String("testId"), QLatin1String("name"), QLatin1String("/tmp/test"),
-                                              QLatin1String("test-abi"), QLatin1String("test-abi,test-abi2"),
-                                              KeyValuePairList() << KeyValuePair(QLatin1String("ExtraKey"), QVariant(QLatin1String("ExtraValue"))));
-    map = AddToolChainOperation::addToolChain(map, QLatin1String("testId2"), QLatin1String("other name"), QLatin1String("/tmp/test2"),
-                                              QLatin1String("test-abi"), QLatin1String("test-abi,test-abi2"),
-                                              KeyValuePairList());
+    map = AddToolChainOperation::addToolChain(map, "testId", "name", "/tmp/test", "test-abi",
+                                              "test-abi,test-abi2",
+                                              KeyValuePairList({ KeyValuePair("ExtraKey", QVariant("ExtraValue")) }));
+    map = AddToolChainOperation::addToolChain(map, "testId2", "other name", "/tmp/test2", "test-abi",
+                                              "test-abi,test-abi2", KeyValuePairList());
 
-    QVariantMap result = rmToolChain(QVariantMap(), QLatin1String("nonexistant"));
+    QVariantMap result = rmToolChain(QVariantMap(), "nonexistent");
     if (!result.isEmpty())
         return false;
 
-    result = rmToolChain(map, QLatin1String("nonexistant"));
+    result = rmToolChain(map, "nonexistent");
     if (result != map)
         return false;
 
-    result = rmToolChain(map, QLatin1String("testId2"));
+    result = rmToolChain(map, "testId2");
     if (result == map
-            || result.value(QLatin1String(COUNT), 0).toInt() != 1
-            || !result.contains(QLatin1String("ToolChain.0"))
-            || result.value(QLatin1String("ToolChain.0")) != map.value(QLatin1String("ToolChain.0")))
+            || result.value(COUNT, 0).toInt() != 1
+            || !result.contains("ToolChain.0") || result.value("ToolChain.0") != map.value("ToolChain.0"))
         return false;
 
-    result = rmToolChain(map, QLatin1String("testId"));
+    result = rmToolChain(map, "testId");
     if (result == map
-            || result.value(QLatin1String(COUNT), 0).toInt() != 1
-            || !result.contains(QLatin1String("ToolChain.0"))
-            || result.value(QLatin1String("ToolChain.0")) != map.value(QLatin1String("ToolChain.1")))
+            || result.value(COUNT, 0).toInt() != 1
+            || !result.contains("ToolChain.0") || result.value("ToolChain.0") != map.value("ToolChain.1"))
         return false;
 
-    result = rmToolChain(result, QLatin1String("testId2"));
+    result = rmToolChain(result, "testId2");
     if (result == map
-            || result.value(QLatin1String(COUNT), 0).toInt() != 0)
+            || result.value(COUNT, 0).toInt() != 0)
         return false;
 
     return true;
@@ -139,7 +136,7 @@ QVariantMap RmToolChainOperation::rmToolChain(const QVariantMap &map, const QStr
 {
     // Find count of tool chains:
     bool ok;
-    int count = GetOperation::get(map, QLatin1String(COUNT)).toInt(&ok);
+    int count = GetOperation::get(map, COUNT).toInt(&ok);
     if (!ok || count < 0) {
         std::cerr << "Error: Count found in toolchains file seems wrong." << std::endl;
         return map;
@@ -148,14 +145,14 @@ QVariantMap RmToolChainOperation::rmToolChain(const QVariantMap &map, const QStr
     QVariantList tcList;
     for (int i = 0; i < count; ++i) {
         QVariantMap tcData = GetOperation::get(map, QString::fromLatin1(PREFIX) + QString::number(i)).toMap();
-        if (tcData.value(QLatin1String(ID)).toString() != id)
+        if (tcData.value(ID).toString() != id)
             tcList.append(tcData);
     }
 
     QVariantMap newMap = AddToolChainOperation::initializeToolChains();
     for (int i = 0; i < tcList.count(); ++i)
         newMap.insert(QString::fromLatin1(PREFIX) + QString::number(i), tcList.at(i));
-    newMap.insert(QLatin1String(COUNT), tcList.count());
+    newMap.insert(COUNT, tcList.count());
 
     return newMap;
 }
