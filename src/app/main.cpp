qt-creator/qt-creator

commit 05b5b6167324a034047fe5ccfc4741a30f3d1d44

Loading .ttf fonts from share/qtcreator/fonts
We add all .ttf fonts found in share/qtcreator/fonts to
QFontDatabase.

Change-Id: I133a5298bd4696b99fa2350758baf8abd8b16fd8
Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

src/app/main.cpp
@@ -36,6 +36,7 @@
 
 #include <QDebug>
 #include <QDir>
+#include <QFontDatabase>
 #include <QFileInfo>
 #include <QLibraryInfo>
 #include <QLoggingCategory>
@@ -295,6 +296,14 @@ static inline QSettings *userSettings()
 static const char *SHARE_PATH =
         Utils::HostOsInfo::isMacHost() ? "/../Resources" : "/../share/qtcreator";
 
+void loadFonts()
+{
+    const QDir dir(QCoreApplication::applicationDirPath() + SHARE_PATH + "/fonts/");
+
+    foreach (const QFileInfo &fileInfo, dir.entryList(QStringList("*.ttf"), QDir::Files))
+        QFontDatabase::addApplicationFont(fileInfo.absoluteFilePath());
+}
+
 int main(int argc, char **argv)
 {
     const char *highDpiEnvironmentVariable = setHighDpiEnvironmentVariable();
@@ -310,6 +319,8 @@ int main(int argc, char **argv)
 #endif
 
     SharedTools::QtSingleApplication app((QLatin1String(appNameC)), argc, argv);
+
+    loadFonts();
 
     if (highDpiEnvironmentVariable)
         qunsetenv(highDpiEnvironmentVariable);

commit b7021f83f322f851c9c6b57e28e2a892fbc42c89

Fix compile with Qt5.5 on OSX
Broke with 05b5b6167324a034047fe5ccfc4741a30f3d1d44

Change-Id: I60c9c243ad858563e793663bb0ce2ffc960c7868
Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

src/app/main.cpp
@@ -298,7 +298,8 @@ static const char *SHARE_PATH =
 
 void loadFonts()
 {
-    const QDir dir(QCoreApplication::applicationDirPath() + SHARE_PATH + "/fonts/");
+    const QDir dir(QCoreApplication::applicationDirPath() + QLatin1String(SHARE_PATH)
+                   + QLatin1String("/fonts/"));
 
     foreach (const QFileInfo &fileInfo, dir.entryList(QStringList("*.ttf"), QDir::Files))
         QFontDatabase::addApplicationFont(fileInfo.absoluteFilePath());