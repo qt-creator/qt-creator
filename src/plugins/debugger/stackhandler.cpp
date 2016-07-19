qt-creator/qt-creator

commit 2d79bdc29c242a04aad3519858ad64712be4de8e

Debugger: Remove some uses of semi-global currentEngine()
Make use of recent TreeModel improvements in various
tool views, push more operations into the engine-
owned data models, specifically context menu creation.

Change-Id: I479c97102b9fb81611c6461c6df1cec59295179a
Reviewed-by: Christian Stenger <christian.stenger@qt.io>
Reviewed-by: hjk <hjk@qt.io>

src/plugins/debugger/stackhandler.cpp
@@ -27,25 +27,36 @@
 
 #include "debuggeractions.h"
 #include "debuggercore.h"
-#include "debuggericons.h"
+#include "debuggerdialogs.h"
 #include "debuggerengine.h"
+#include "debuggericons.h"
 #include "debuggerprotocol.h"
+#include "memoryagent.h"
 #include "simplifytype.h"
 
+#include <coreplugin/icore.h>
+#include <coreplugin/messagebox.h>
+
+#include <utils/basetreeview.h>
 #include <utils/fileutils.h>
 #include <utils/qtcassert.h>
 #include <utils/savedaction.h>
 
+#include <QApplication>
+#include <QClipboard>
+#include <QContextMenuEvent>
 #include <QDebug>
+#include <QDir>
+#include <QFile>
+#include <QFileDialog>
+#include <QInputDialog>
+#include <QMenu>
+#include <QTextStream>
+
+using namespace Utils;
 
 namespace Debugger {
 namespace Internal {
-
-////////////////////////////////////////////////////////////////////////
-//
-// StackHandler
-//
-////////////////////////////////////////////////////////////////////////
 
 /*!
     \class Debugger::Internal::StackHandler
@@ -56,13 +67,14 @@ namespace Internal {
 StackHandler::StackHandler(DebuggerEngine *engine)
   : m_engine(engine)
 {
-    setObjectName(QLatin1String("StackModel"));
-    m_resetLocationScheduled = false;
-    m_contentsValid = false;
-    m_currentIndex = -1;
-    m_canExpand = false;
+    setObjectName("StackModel");
+
     connect(action(OperateByInstruction), &QAction::triggered,
         this, &StackHandler::resetModel);
+    connect(action(ExpandStack), &QAction::triggered,
+        this, &StackHandler::reloadFullStack);
+    connect(action(MaximalStackDepth), &QAction::triggered,
+        this, &StackHandler::reloadFullStack);
 }
 
 StackHandler::~StackHandler()
@@ -104,12 +116,12 @@ QVariant StackHandler::data(const QModelIndex &index, int role) const
         case StackFunctionNameColumn:
             return simplifyType(frame.function);
         case StackFileNameColumn:
-            return frame.file.isEmpty() ? frame.module : Utils::FileName::fromString(frame.file).fileName();
+            return frame.file.isEmpty() ? frame.module : FileName::fromString(frame.file).fileName();
         case StackLineNumberColumn:
             return frame.line > 0 ? QVariant(frame.line) : QVariant();
         case StackAddressColumn:
             if (frame.address)
-                return QString::fromLatin1("0x%1").arg(frame.address, 0, 16);
+                return QString("0x%1").arg(frame.address, 0, 16);
             return QString();
         }
         return QVariant();
@@ -151,7 +163,24 @@ Qt::ItemFlags StackHandler::flags(const QModelIndex &index) const
     const StackFrame &frame = m_stackFrames.at(index.row());
     const bool isValid = frame.isUsable() || boolSetting(OperateByInstruction);
     return isValid && m_contentsValid
-        ? QAbstractTableModel::flags(index) : Qt::ItemFlags();
+            ? QAbstractTableModel::flags(index) : Qt::ItemFlags();
+}
+
+bool StackHandler::setData(const QModelIndex &idx, const QVariant &data, int role)
+{
+    Q_UNUSED(idx)
+
+    if (role == BaseTreeView::ItemActivatedRole) {
+        return true;
+    }
+
+    if (role == BaseTreeView::ItemViewEventRole) {
+        ItemViewEvent ev = data.value<ItemViewEvent>();
+        if (ev.type() == QEvent::ContextMenu)
+            return contextMenuEvent(ev);
+    }
+
+    return false;
 }
 
 StackFrame StackHandler::currentFrame() const
@@ -284,6 +313,172 @@ void StackHandler::resetLocation()
         m_resetLocationScheduled = false;
         endResetModel();
     }
+}
+
+// Input a function to be disassembled. Accept CDB syntax
+// 'Module!function' for module specification
+
+static StackFrame inputFunctionForDisassembly()
+{
+    StackFrame frame;
+    QInputDialog dialog;
+    dialog.setInputMode(QInputDialog::TextInput);
+    dialog.setLabelText(StackHandler::tr("Function:"));
+    dialog.setWindowTitle(StackHandler::tr("Disassemble Function"));
+    dialog.setWindowFlags(dialog.windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    if (dialog.exec() != QDialog::Accepted)
+        return frame;
+    const QString function = dialog.textValue();
+    if (function.isEmpty())
+        return frame;
+    const int bangPos = function.indexOf('!');
+    if (bangPos != -1) {
+        frame.module = function.left(bangPos);
+        frame.function = function.mid(bangPos + 1);
+    } else {
+        frame.function = function;
+    }
+    frame.line = 42; // trick gdb into mixed mode.
+    return frame;
+}
+
+// Write stack frames as task file for displaying it in the build issues pane.
+void StackHandler::saveTaskFile()
+{
+    QFile file;
+    QFileDialog fileDialog(Core::ICore::dialogParent());
+    fileDialog.setAcceptMode(QFileDialog::AcceptSave);
+    fileDialog.selectFile(QDir::currentPath() + "/stack.tasks");
+    while (!file.isOpen()) {
+        if (fileDialog.exec() != QDialog::Accepted)
+            return;
+        const QString fileName = fileDialog.selectedFiles().front();
+        file.setFileName(fileName);
+        if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+            QString msg = tr("Cannot open \"%1\": %2")
+                    .arg(QDir::toNativeSeparators(fileName), file.errorString());
+            Core::AsynchronousMessageBox::warning(tr("Cannot Open Task File"), msg);
+        }
+    }
+
+    QTextStream str(&file);
+    foreach (const StackFrame &frame, frames()) {
+        if (frame.isUsable())
+            str << frame.file << '\t' << frame.line << "\tstack\tFrame #" << frame.level << '\n';
+    }
+}
+
+bool StackHandler::contextMenuEvent(const ItemViewEvent &ev)
+{
+    auto menu = new QMenu;
+
+    const int row = ev.index().row();
+    StackFrame frame;
+    if (row >= 0 && row < stackSize())
+        frame = frameAt(row);
+    const quint64 address = frame.address;
+
+    menu->addAction(action(ExpandStack));
+
+    addAction(menu, tr("Copy Contents to Clipboard"), true, [this] { copyContentsToClipboard(); });
+    addAction(menu, tr("Save as Task File..."), true, [this] { saveTaskFile(); });
+
+    if (m_engine->hasCapability(CreateFullBacktraceCapability))
+        menu->addAction(action(CreateFullBacktrace));
+
+    if (m_engine->hasCapability(AdditionalQmlStackCapability))
+        addAction(menu, tr("Load QML Stack"), true, [this] { m_engine->loadAdditionalQmlStack(); });
+
+    if (m_engine->hasCapability(ShowMemoryCapability))
+        addAction(menu, tr("Open Memory Editor at 0x%1").arg(address, 0, 16),
+                  tr("Open Memory Editor"),
+                  address,
+                  [this, row, frame, address] {
+                        MemoryViewSetupData data;
+                        data.startAddress = address;
+                        data.title = tr("Memory at Frame #%1 (%2) 0x%3").
+                            arg(row).arg(frame.function).arg(address, 0, 16);
+                        data.markup.push_back(MemoryMarkup(address, 1, QColor(Qt::blue).lighter(),
+                                                  tr("Frame #%1 (%2)").arg(row).arg(frame.function)));
+                        m_engine->openMemoryView(data);
+                   });
+
+    if (m_engine->hasCapability(DisassemblerCapability)) {
+        addAction(menu, tr("Open Disassembler at 0x%1").arg(address, 0, 16),
+                  tr("Open Disassembler"),
+                  address,
+                  [this, frame] { m_engine->openDisassemblerView(frame); });
+
+        addAction(menu, tr("Open Disassembler at Address..."), true,
+                  [this, address] {
+                        AddressDialog dialog;
+                        if (address)
+                            dialog.setAddress(address);
+                        if (dialog.exec() == QDialog::Accepted)
+                            m_engine->openDisassemblerView(Location(dialog.address()));
+                   });
+
+        addAction(menu, tr("Disassemble Function..."), true,
+                  [this, address] {
+                        const StackFrame frame = inputFunctionForDisassembly();
+                        if (!frame.function.isEmpty())
+                            m_engine->openDisassemblerView(Location(frame));
+                  });
+    }
+
+    if (m_engine->hasCapability(ShowModuleSymbolsCapability)) {
+        addAction(menu, tr("Try to Load Unknown Symbols"), true,
+                  [this] { m_engine->loadSymbolsForStack(); });
+    }
+
+    if (m_engine->hasCapability(MemoryAddressCapability))
+        menu->addAction(action(UseAddressInStackView));
+
+    menu->addSeparator();
+    menu->addAction(action(UseToolTipsInStackView));
+    menu->addSeparator();
+    menu->addAction(action(SettingsDialog));
+    menu->popup(ev.globalPos());
+    return true;
+}
+
+void StackHandler::copyContentsToClipboard()
+{
+    QString str;
+    int n = rowCount();
+    int m = columnCount();
+    QVector<int> largestColumnWidths(m, 0);
+
+    // First, find the widths of the largest columns,
+    // so that we can print them out nicely aligned.
+    for (int i = 0; i != n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            const QModelIndex idx = index(i, j);
+            const int columnWidth = data(idx, Qt::DisplayRole).toString().size();
+            if (columnWidth > largestColumnWidths.at(j))
+                largestColumnWidths[j] = columnWidth;
+        }
+    }
+
+    for (int i = 0; i != n; ++i) {
+        for (int j = 0; j != m; ++j) {
+            QModelIndex idx = index(i, j);
+            const QString columnEntry = data(idx, Qt::DisplayRole).toString();
+            str += columnEntry;
+            const int difference = largestColumnWidths.at(j) - columnEntry.size();
+            // Add one extra space between columns.
+            str += QString().fill(' ', difference > 0 ? difference + 1 : 1);
+        }
+        str += '\n';
+    }
+    QClipboard *clipboard = QApplication::clipboard();
+    clipboard->setText(str, QClipboard::Selection);
+    clipboard->setText(str, QClipboard::Clipboard);
+}
+
+void StackHandler::reloadFullStack()
+{
+    m_engine->reloadFullStack();
 }
 
 } // namespace Internal

src/libs/utils/basetreeview.cpp
@@ -248,6 +248,7 @@ BaseTreeView::BaseTreeView(QWidget *parent)
     setSelectionMode(QAbstractItemView::ExtendedSelection);
     setUniformRowHeights(true);
     setItemDelegate(new BaseTreeViewDelegate(this));
+    setAlternatingRowColors(false);
 
     QHeaderView *h = header();
     h->setDefaultAlignment(Qt::AlignLeft);
@@ -303,6 +304,13 @@ void BaseTreeView::setModel(QAbstractItemModel *m)
                 connect(model(), c[i].qsignal, c[i].receiver, c[i].qslot);
         }
         d->restoreState();
+
+        QVariant delegateBlob = m->data(QModelIndex(), ItemDelegateRole);
+        if (delegateBlob.isValid()) {
+            auto delegate = delegateBlob.value<QAbstractItemDelegate *>();
+            delegate->setParent(this);
+            setItemDelegate(delegate);
+        }
     }
 }
 
@@ -312,6 +320,44 @@ void BaseTreeView::mousePressEvent(QMouseEvent *ev)
     const QModelIndex mi = indexAt(ev->pos());
     if (!mi.isValid())
         d->toggleColumnWidth(columnAt(ev->x()));
+}
+
+void BaseTreeView::contextMenuEvent(QContextMenuEvent *ev)
+{
+    QPoint pos = ev->pos();
+    QModelIndex index = indexAt(pos);
+    if (!model()->setData(index, describeEvent(ev, pos, index), ItemViewEventRole))
+        TreeView::contextMenuEvent(ev);
+}
+
+void BaseTreeView::keyPressEvent(QKeyEvent *ev)
+{
+    if (!model()->setData(QModelIndex(), describeEvent(ev), ItemViewEventRole))
+        TreeView::keyPressEvent(ev);
+}
+
+void BaseTreeView::dragEnterEvent(QDragEnterEvent *ev)
+{
+    if (!model()->setData(QModelIndex(), describeEvent(ev), ItemViewEventRole))
+        TreeView::dragEnterEvent(ev);
+}
+
+void BaseTreeView::dropEvent(QDropEvent *ev)
+{
+    if (!model()->setData(QModelIndex(), describeEvent(ev), ItemViewEventRole))
+        TreeView::dropEvent(ev);
+}
+
+void BaseTreeView::dragMoveEvent(QDragMoveEvent *ev)
+{
+    if (!model()->setData(QModelIndex(), describeEvent(ev), ItemViewEventRole))
+        TreeView::dragMoveEvent(ev);
+}
+
+void BaseTreeView::mouseDoubleClickEvent(QMouseEvent *ev)
+{
+    if (!model()->setData(QModelIndex(), describeEvent(ev), ItemViewEventRole))
+        TreeView::mouseDoubleClickEvent(ev);
 }
 
 void BaseTreeView::showEvent(QShowEvent *ev)
@@ -346,6 +392,35 @@ void BaseTreeView::hideProgressIndicator()
         d->m_progressIndicator->hide();
 }
 
+QVariant BaseTreeView::describeEvent(QEvent *ev, const QPoint &pos, const QModelIndex &idx)
+{
+    ItemViewEvent event;
+    event.m_view = this;
+    event.m_event = ev;
+    event.m_pos = pos;
+    event.m_index = idx;
+
+    QItemSelectionModel *selection = selectionModel();
+    event.m_selectedRows = selection->selectedRows();
+    if (event.m_selectedRows.isEmpty()) {
+        QModelIndex current = selection->currentIndex();
+        if (current.isValid())
+            event.m_selectedRows.append(current);
+    }
+
+    return QVariant::fromValue(event);
+}
+
+void BaseTreeView::rowActivated(const QModelIndex &index)
+{
+    model()->setData(index, QVariant(), ItemActivatedRole);
+}
+
+void BaseTreeView::rowClicked(const QModelIndex &index)
+{
+    model()->setData(index, QVariant(), ItemClickedRole);
+}
+
 void BaseTreeView::setSettings(QSettings *settings, const QByteArray &key)
 {
     QTC_ASSERT(!d->m_settings, qDebug() << "DUPLICATED setSettings" << key);
@@ -354,16 +429,9 @@ void BaseTreeView::setSettings(QSettings *settings, const QByteArray &key)
     d->readSettings();
 }
 
-QModelIndexList BaseTreeView::activeRows() const
+QModelIndexList ItemViewEvent::currentOrSelectedRows() const
 {
-    QItemSelectionModel *selection = selectionModel();
-    QModelIndexList indices = selection->selectedRows();
-    if (indices.isEmpty()) {
-        QModelIndex current = selection->currentIndex();
-        if (current.isValid())
-            indices.append(current);
-    }
-    return indices;
+    return m_selectedRows.isEmpty() ? QModelIndexList() << m_index : m_selectedRows;
 }
 
 } // namespace Utils

src/libs/utils/basetreeview.h
@@ -42,20 +42,31 @@ class QTCREATOR_UTILS_EXPORT BaseTreeView : public TreeView
     Q_OBJECT
 
 public:
-    enum { ExtraIndicesForColumnWidth = 12734 };
+    enum {
+        ExtraIndicesForColumnWidth = 12734,
+
+        ItemViewEventRole = Qt::UserRole + 12735,
+
+        ItemActivatedRole,
+        ItemClickedRole,
+
+        ItemDelegateRole,
+    };
 
     BaseTreeView(QWidget *parent = 0);
     ~BaseTreeView();
 
     void setSettings(QSettings *settings, const QByteArray &key);
-    QModelIndexList activeRows() const;
-
-    virtual void rowActivated(const QModelIndex &) {}
-    virtual void rowClicked(const QModelIndex &) {}
 
     void setModel(QAbstractItemModel *model) override;
     void mousePressEvent(QMouseEvent *ev) override;
+    void contextMenuEvent(QContextMenuEvent *ev) override;
     void showEvent(QShowEvent *ev) override;
+    void keyPressEvent(QKeyEvent *ev) override;
+    void dragEnterEvent(QDragEnterEvent *ev) override;
+    void dropEvent(QDropEvent *ev) override;
+    void dragMoveEvent(QDragMoveEvent *ev) override;
+    void mouseDoubleClickEvent(QMouseEvent *ev) override;
 
     void showProgressIndicator();
     void hideProgressIndicator();
@@ -63,11 +74,70 @@ public:
 signals:
     void aboutToShow();
 
-public slots:
-    void setAlternatingRowColorsHelper(bool on) { setAlternatingRowColors(on); }
-
 private:
+    QVariant describeEvent(QEvent *ev,
+                           const QPoint &pos = QPoint(),
+                           const QModelIndex &idx = QModelIndex());
+    void rowActivated(const QModelIndex &index);
+    void rowClicked(const QModelIndex &index);
+
     Internal::BaseTreeViewPrivate *d;
 };
 
+template <typename Event> struct EventCode;
+template <> struct EventCode<QDragEnterEvent> { enum { code = QEvent::DragEnter }; };
+template <> struct EventCode<QDragLeaveEvent> { enum { code = QEvent::DragLeave }; };
+template <> struct EventCode<QDragMoveEvent> { enum { code = QEvent::DragMove }; };
+template <> struct EventCode<QDropEvent> { enum { code = QEvent::Drop }; };
+template <> struct EventCode<QContextMenuEvent> { enum { code = QEvent::ContextMenu }; };
+template <> struct EventCode<QMouseEvent> { enum { code = QEvent::MouseButtonPress }; };
+template <> struct EventCode<QKeyEvent> { enum { code = QEvent::KeyPress }; };
+
+template <class T> T *checkEventType(QEvent *ev)
+{
+    const int cc = EventCode<T>::code;
+    const int tt = ev->type();
+    if (cc == tt)
+        return static_cast<T *>(ev);
+    if (cc == QEvent::MouseButtonPress) {
+        if (tt == QEvent::MouseButtonDblClick || tt == QEvent::MouseButtonRelease || tt == QEvent::MouseMove)
+            return static_cast<T *>(ev);
+    }
+    if (cc == QEvent::KeyPress && tt == QEvent::KeyRelease)
+        return static_cast<T *>(ev);
+    return nullptr;
+}
+
+class QTCREATOR_UTILS_EXPORT ItemViewEvent
+{
+public:
+    ItemViewEvent() {}
+
+    template <class T> T *as() const {
+        return checkEventType<T>(m_event);
+    }
+
+    template <class T> T *as(QEvent::Type t) const {
+        return m_event->type() == t ? as<T>() : nullptr;
+    }
+
+    QEvent::Type type() const { return m_event->type(); }
+    BaseTreeView *view() const { return m_view; }
+    QPoint pos() const { return m_pos; }
+    QPoint globalPos() const { return m_view->mapToGlobal(m_pos); }
+    QModelIndex index() const { return m_index; }
+    QModelIndexList selectedRows() const { return m_selectedRows; }
+    QModelIndexList currentOrSelectedRows() const;
+
+private:
+    friend class BaseTreeView;
+    QEvent *m_event = nullptr;
+    BaseTreeView *m_view = nullptr;
+    QPoint m_pos;
+    QModelIndex m_index;
+    QModelIndexList m_selectedRows;
+};
+
 } // namespace Utils
+
+Q_DECLARE_METATYPE(Utils::ItemViewEvent);

src/plugins/debugger/breakhandler.h
@@ -33,6 +33,8 @@
 #include <QCoreApplication>
 #include <QPointer>
 
+namespace Utils { class ItemViewEvent; }
+
 namespace Debugger {
 namespace Internal {
 
@@ -157,7 +159,9 @@ inline uint qHash(const Debugger::Internal::Breakpoint &b) { return b.hash(); }
 
 typedef QList<Breakpoint> Breakpoints;
 
-class BreakHandler : public Utils::LeveledTreeModel<Utils::TreeItem, BreakpointItem, LocationItem>
+using BreakModel = Utils::LeveledTreeModel<Utils::TypedTreeItem<BreakpointItem>, BreakpointItem, LocationItem>;
+
+class BreakHandler : public BreakModel
 {
     Q_OBJECT
 
@@ -200,8 +204,15 @@ public:
     void setWatchpointAtExpression(const QString &exp);
 
     Breakpoint breakpointById(BreakpointModelId id) const;
+    void editBreakpoint(Breakpoint bp, QWidget *parent);
 
 private:
+    QVariant data(const QModelIndex &idx, int role) const override;
+    bool setData(const QModelIndex &idx, const QVariant &value, int role) override;
+    void timerEvent(QTimerEvent *event) override;
+
+    bool contextMenuEvent(const Utils::ItemViewEvent &ev);
+
     friend class BreakpointItem;
     friend class Breakpoint;
 
@@ -209,12 +220,17 @@ private:
     void saveBreakpoints();
 
     void appendBreakpointInternal(const BreakpointParameters &data);
+    void deleteBreakpoints(const Breakpoints &bps);
+    void deleteAllBreakpoints();
+    void setBreakpointsEnabled(const Breakpoints &bps, bool enabled);
+    void addBreakpoint();
+    void editBreakpoints(const Breakpoints &bps, QWidget *parent);
 
     Q_SLOT void changeLineNumberFromMarkerHelper(Debugger::Internal::BreakpointModelId id);
     Q_SLOT void deletionHelper(Debugger::Internal::BreakpointModelId id);
 
     void scheduleSynchronization();
-    void timerEvent(QTimerEvent *event);
+
     int m_syncTimerId;
 };
 

src/plugins/debugger/breakwindow.h
@@ -1,59 +1,0 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of Qt Creator.
-**
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 as published by the Free Software
-** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-****************************************************************************/
-
-#pragma once
-
-#include "breakhandler.h"
-#include <utils/basetreeview.h>
-
-namespace Debugger {
-namespace Internal {
-
-class BreakTreeView : public Utils::BaseTreeView
-{
-    Q_OBJECT
-
-public:
-    BreakTreeView();
-
-    static void editBreakpoint(Breakpoint bp, QWidget *parent);
-
-private:
-    void rowActivated(const QModelIndex &index);
-    void contextMenuEvent(QContextMenuEvent *ev);
-    void keyPressEvent(QKeyEvent *ev);
-    void mouseDoubleClickEvent(QMouseEvent *ev);
-
-    void showAddressColumn(bool on);
-    void deleteBreakpoints(const Breakpoints &bps);
-    void deleteAllBreakpoints();
-    void addBreakpoint();
-    void editBreakpoints(const Breakpoints &bps);
-    void associateBreakpoint(const Breakpoints &bps, int thread);
-    void setBreakpointsEnabled(const Breakpoints &bps, bool enabled);
-};
-
-} // namespace Internal
-} // namespace Debugger

src/plugins/debugger/debugger.pro
@@ -14,7 +14,6 @@ CONFIG += exceptions
 HEADERS += \
     breakhandler.h \
     breakpoint.h \
-    breakwindow.h \
     commonoptionspage.h \
     debugger_global.h \
     debuggeractions.h \
@@ -40,22 +39,18 @@ HEADERS += \
     logwindow.h \
     memoryagent.h \
     moduleshandler.h \
-    moduleswindow.h \
     outputcollector.h \
     procinterrupt.h \
     registerhandler.h \
-    registerwindow.h \
     snapshothandler.h \
     snapshotwindow.h \
     sourceagent.h \
     sourcefileshandler.h \
-    sourcefileswindow.h \
     sourceutils.h \
     stackframe.h \
     stackhandler.h \
     stackwindow.h \
     terminal.h \
-    threadswindow.h \
     watchhandler.h \
     watchutils.h \
     watchwindow.h \
@@ -74,7 +69,6 @@ HEADERS += \
 SOURCES += \
     breakhandler.cpp \
     breakpoint.cpp \
-    breakwindow.cpp \
     commonoptionspage.cpp \
     debuggeractions.cpp \
     debuggerdialogs.cpp \
@@ -95,21 +89,17 @@ SOURCES += \
     logwindow.cpp \
     memoryagent.cpp \
     moduleshandler.cpp \
-    moduleswindow.cpp \
     outputcollector.cpp \
     procinterrupt.cpp \
     registerhandler.cpp \
-    registerwindow.cpp \
     snapshothandler.cpp \
     snapshotwindow.cpp \
     sourceagent.cpp \
     sourcefileshandler.cpp \
-    sourcefileswindow.cpp \
     sourceutils.cpp \
     stackhandler.cpp \
     stackwindow.cpp \
     threadshandler.cpp \
-    threadswindow.cpp \
     terminal.cpp \
     watchdata.cpp \
     watchhandler.cpp \

commit 901ad4693d3cb44bc467f00fdc8167f03ebd86cc

Debugger: Fix stack frame switching by mouse
Regression introduced by 2d79bdc2.

Change-Id: Ib5befb9c0730c9c2b46891139a9b15154300eced
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/debugger/stackhandler.cpp
@@ -168,9 +168,8 @@ Qt::ItemFlags StackHandler::flags(const QModelIndex &index) const
 
 bool StackHandler::setData(const QModelIndex &idx, const QVariant &data, int role)
 {
-    Q_UNUSED(idx)
-
-    if (role == BaseTreeView::ItemActivatedRole) {
+    if (role == BaseTreeView::ItemActivatedRole || role == BaseTreeView::ItemClickedRole) {
+        m_engine->activateFrame(idx.row());
         return true;
     }
 