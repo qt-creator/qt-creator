98f7a00e7070c856c4a50a89eab6cb63b299db21
Alessandro Portale <alessandro.portale@qt.io>
2016-07-25 11:58:28 +0200

    Move "Ok", "Error" and "Notloaded" icons from ExtentionSystem to Utils
    These icons are useful in other situations (e.g. QmlDesigner)
    
    Change-Id: I31751b8ca8fea34c532d54d423e5fef07b370a4a
    Reviewed-by: Thomas Hartmann <Thomas.Hartmann@theqtcompany.com>

--- src/libs/utils/utilsicons.h
+++ src/libs/utils/utilsicons.h
@@ -50,6 +50,12 @@ const Utils::Icon ZOOM({
         {QLatin1String(":/utils/images/zoom.png"), Utils::Theme::PanelTextColorMid}}, Utils::Icon::Tint);
 const Utils::Icon ZOOM_TOOLBAR({
         {QLatin1String(":/utils/images/zoom.png"), Utils::Theme::IconsBaseColor}});
+const Utils::Icon OK({
+        {QLatin1String(":/utils/images/ok.png"), Utils::Theme::IconsRunToolBarColor}}, Icon::Tint);
+const Utils::Icon NOTLOADED({
+        {QLatin1String(":/utils/images/notloaded.png"), Utils::Theme::IconsErrorColor}}, Icon::Tint);
+const Utils::Icon ERROR({
+        {QLatin1String(":/utils/images/error.png"), Utils::Theme::IconsErrorColor}}, Icon::Tint);
 
 } // namespace Icons
 } // namespace Utils
