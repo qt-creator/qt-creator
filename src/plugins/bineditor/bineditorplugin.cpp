07706a9a8e375432c650ba04c3ad64b052470cfb
David Schulz <david.schulz@qt.io>
2016-08-19 08:28:24 +0200

    BinEditor: Do not call openImpl without a file name
    Task-number: QTCREATORBUG-16751
    Change-Id: I64133932b405707f4d7e1e2213e1ddf73cbcc387
    Reviewed-by: hjk <hjk@qt.io>

--- src/plugins/bineditor/bineditorplugin.cpp
+++ src/plugins/bineditor/bineditorplugin.cpp
@@ -340,7 +340,8 @@ public:
 
     void provideNewRange(quint64 offset)
     {
-        openImpl(0, filePath().toString(), offset);
+        if (filePath().exists())
+            openImpl(0, filePath().toString(), offset);
     }
 
 public:
