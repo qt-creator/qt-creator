qt-creator/qt-creator

commit 15fbfaf2e94b72cfd54bb0bd6f2b9ab87b870795

Move icons to Utils
This way we can use them from libraries, not only from plugins.

Change-Id: Ic35cfd5f04d638d87606bf272b2c00ded1267c1b
Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

src/libs/utils/utilsicons.h
@@ -30,32 +30,158 @@
 namespace Utils {
 namespace Icons {
 
-const Utils::Icon EDIT_CLEAR({
-        {QLatin1String(":/core/images/editclear.png"), Utils::Theme::PanelTextColorMid}}, Utils::Icon::Tint);
-const Utils::Icon LOCKED_TOOLBAR({
+const Icon EDIT_CLEAR({
+        {QLatin1String(":/utils/images/editclear.png"), Utils::Theme::PanelTextColorMid}}, Icon::Tint);
+const Icon LOCKED_TOOLBAR({
         {QLatin1String(":/utils/images/locked.png"), Utils::Theme::IconsBaseColor}});
-const Utils::Icon LOCKED({
+const Icon LOCKED({
         {QLatin1String(":/utils/images/locked.png"), Utils::Theme::PanelTextColorDark}}, Icon::Tint);
-const Utils::Icon UNLOCKED_TOOLBAR({
+const Icon UNLOCKED_TOOLBAR({
         {QLatin1String(":/utils/images/unlocked.png"), Utils::Theme::IconsBaseColor}});
-const Utils::Icon NEXT({
-        {QLatin1String(":/utils/images/next.png"), Utils::Theme::IconsWarningColor}}, Utils::Icon::MenuTintedStyle);
-const Utils::Icon NEXT_TOOLBAR({
+const Icon NEXT({
+        {QLatin1String(":/utils/images/next.png"), Utils::Theme::IconsWarningColor}}, Icon::MenuTintedStyle);
+const Icon NEXT_TOOLBAR({
         {QLatin1String(":/utils/images/next.png"), Utils::Theme::IconsNavigationArrowsColor}});
-const Utils::Icon PREV({
-        {QLatin1String(":/utils/images/prev.png"), Utils::Theme::IconsWarningColor}}, Utils::Icon::MenuTintedStyle);
-const Utils::Icon PREV_TOOLBAR({
+const Icon PREV({
+        {QLatin1String(":/utils/images/prev.png"), Utils::Theme::IconsWarningColor}}, Icon::MenuTintedStyle);
+const Icon PREV_TOOLBAR({
         {QLatin1String(":/utils/images/prev.png"), Utils::Theme::IconsNavigationArrowsColor}});
-const Utils::Icon ZOOM({
-        {QLatin1String(":/utils/images/zoom.png"), Utils::Theme::PanelTextColorMid}}, Utils::Icon::Tint);
-const Utils::Icon ZOOM_TOOLBAR({
+const Icon ZOOM({
+        {QLatin1String(":/utils/images/zoom.png"), Utils::Theme::PanelTextColorMid}}, Icon::Tint);
+const Icon ZOOM_TOOLBAR({
         {QLatin1String(":/utils/images/zoom.png"), Utils::Theme::IconsBaseColor}});
-const Utils::Icon OK({
+const Icon OK({
         {QLatin1String(":/utils/images/ok.png"), Utils::Theme::IconsRunToolBarColor}}, Icon::Tint);
-const Utils::Icon NOTLOADED({
+const Icon NOTLOADED({
         {QLatin1String(":/utils/images/notloaded.png"), Utils::Theme::IconsErrorColor}}, Icon::Tint);
 const Utils::Icon BROKEN({
         {QLatin1String(":/utils/images/broken.png"), Utils::Theme::IconsErrorColor}}, Icon::Tint);
+const Icon ERROR({
+        {QLatin1String(":/utils/images/warningfill.png"), Theme::BackgroundColorNormal},
+        {QLatin1String(":/utils/images/error.png"), Theme::IconsErrorColor}}, Icon::Tint);
+
+const Icon NEWFILE(
+        QLatin1String(":/utils/images/filenew.png"));
+const Icon OPENFILE(
+        QLatin1String(":/utils/images/fileopen.png"));
+const Icon SAVEFILE({
+        {QLatin1String(":/utils/images/filesave.png"), Theme::PanelTextColorMid}}, Icon::Tint);
+const Icon SAVEFILE_TOOLBAR({
+        {QLatin1String(":/utils/images/filesave.png"), Theme::IconsBaseColor}});
+const Icon UNDO(
+        QLatin1String(":/utils/images/undo.png"));
+const Icon REDO(
+        QLatin1String(":/utils/images/redo.png"));
+const Icon COPY(
+        QLatin1String(":/utils/images/editcopy.png"));
+const Icon PASTE(
+        QLatin1String(":/utils/images/editpaste.png"));
+const Icon CUT(
+        QLatin1String(":/utils/images/editcut.png"));
+const Icon DIR(
+        QLatin1String(":/utils/images/dir.png"));
+const Icon RESET({
+        {QLatin1String(":/utils/images/reset.png"), Theme::PanelTextColorMid}}, Icon::Tint);
+const Icon RESET_TOOLBAR({
+        {QLatin1String(":/utils/images/reset.png"), Theme::IconsBaseColor}});
+const Icon DARK_CLOSE(
+        QLatin1String(":/utils/images/darkclose.png"));
+const Icon QTCREATORLOGO_BIG(
+        QLatin1String(":/utils/images/qtcreatorlogo-big.png"));
+
+const Icon ARROW_UP({
+        {QLatin1String(":/utils/images/arrowup.png"), Theme::IconsBaseColor}});
+const Icon ARROW_DOWN({
+        {QLatin1String(":/utils/images/arrowdown.png"), Theme::IconsBaseColor}});
+const Icon MINUS({
+        {QLatin1String(":/utils/images/minus.png"), Theme::IconsBaseColor}});
+const Icon PLUS({
+        {QLatin1String(":/utils/images/plus.png"), Theme::IconsBaseColor}});
+const Icon MAGNIFIER({
+        {QLatin1String(":/utils/images/magnifier.png"), Theme::PanelTextColorMid}}, Icon::Tint);
+const Icon CLEAN({
+        {QLatin1String(":/utils/images/clean_pane_small.png"), Theme::PanelTextColorMid}}, Icon::Tint);
+const Icon CLEAN_TOOLBAR({
+        {QLatin1String(":/utils/images/clean_pane_small.png"), Theme::IconsBaseColor}});
+const Icon RELOAD({
+        {QLatin1String(":/utils/images/reload_gray.png"), Theme::IconsBaseColor}});
+const Icon TOGGLE_SIDEBAR({
+        {QLatin1String(":/utils/images/sidebaricon.png"), Theme::PanelTextColorMid}}, Icon::MenuTintedStyle);
+const Icon TOGGLE_SIDEBAR_TOOLBAR({
+        {QLatin1String(":/utils/images/sidebaricon.png"), Theme::IconsBaseColor}});
+const Icon CLOSE_TOOLBAR({
+        {QLatin1String(":/utils/images/close.png"), Theme::IconsBaseColor}});
+const Icon CLOSE_FOREGROUND({
+        {QLatin1String(":/utils/images/close.png"), Theme::PanelTextColorDark}}, Icon::Tint);
+const Icon CLOSE_BACKGROUND({
+        {QLatin1String(":/utils/images/close.png"), Theme::PanelTextColorLight}}, Icon::Tint);
+const Icon SPLIT_HORIZONTAL({
+        {QLatin1String(":/utils/images/splitbutton_horizontal.png"), Theme::PanelTextColorMid}}, Icon::MenuTintedStyle);
+const Icon SPLIT_HORIZONTAL_TOOLBAR({
+        {QLatin1String(":/utils/images/splitbutton_horizontal.png"), Theme::IconsBaseColor}});
+const Icon SPLIT_VERTICAL({
+        {QLatin1String(":/utils/images/splitbutton_vertical.png"), Theme::PanelTextColorMid}}, Icon::MenuTintedStyle);
+const Icon SPLIT_VERTICAL_TOOLBAR({
+        {QLatin1String(":/utils/images/splitbutton_vertical.png"), Theme::IconsBaseColor}});
+const Icon CLOSE_SPLIT_TOP({
+        {QLatin1String(":/utils/images/splitbutton_closetop.png"), Theme::IconsBaseColor}});
+const Icon CLOSE_SPLIT_BOTTOM({
+        {QLatin1String(":/utils/images/splitbutton_closebottom.png"), Theme::IconsBaseColor}});
+const Icon CLOSE_SPLIT_LEFT({
+        {QLatin1String(":/utils/images/splitbutton_closeleft.png"), Theme::IconsBaseColor}});
+const Icon CLOSE_SPLIT_RIGHT({
+        {QLatin1String(":/utils/images/splitbutton_closeright.png"), Theme::IconsBaseColor}});
+const Icon FILTER({
+        {QLatin1String(":/utils/images/filtericon.png"), Theme::IconsBaseColor}});
+const Icon LINK({
+        {QLatin1String(":/utils/images/linkicon.png"), Theme::IconsBaseColor}});
+const Icon WARNING({
+        {QLatin1String(":/utils/images/warningfill.png"), Theme::BackgroundColorNormal},
+        {QLatin1String(":/utils/images/warning.png"), Theme::IconsWarningColor}}, Icon::Tint);
+const Icon WARNING_TOOLBAR({
+        {QLatin1String(":/utils/images/warning.png"), Theme::IconsWarningToolBarColor}});
+const Icon ERROR_TOOLBAR({
+        {QLatin1String(":/utils/images/error.png"), Theme::IconsErrorToolBarColor}});
+const Icon ERROR_TASKBAR({
+        {QLatin1String(":/utils/images/compile_error_taskbar.png"), Theme::IconsErrorColor}}, Icon::Tint);
+const Icon INFO({
+        {QLatin1String(":/utils/images/warningfill.png"), Theme::BackgroundColorNormal},
+        {QLatin1String(":/utils/images/info.png"), Theme::IconsInfoColor}}, Icon::Tint);
+const Icon INFO_TOOLBAR({
+        {QLatin1String(":/utils/images/info.png"), Theme::IconsInfoToolBarColor}});
+const Icon EXPAND_ALL_TOOLBAR({
+        {QLatin1String(":/find/images/expand.png"), Theme::IconsBaseColor}});
+const Icon TOOLBAR_EXTENSION({
+        {QLatin1String(":/utils/images/extension.png"), Theme::IconsBaseColor}});
+const Icon RUN_SMALL({
+        {QLatin1String(":/utils/images/run_small.png"), Utils::Theme::IconsRunColor}}, Icon::MenuTintedStyle);
+const Icon RUN_SMALL_TOOLBAR({
+        {QLatin1String(":/utils/images/run_small.png"), Utils::Theme::IconsRunToolBarColor}});
+const Icon STOP_SMALL({
+        {QLatin1String(":/utils/images/stop_small.png"), Utils::Theme::IconsStopColor}}, Icon::MenuTintedStyle);
+const Icon STOP_SMALL_TOOLBAR({
+        {QLatin1String(":/utils/images/stop_small.png"), Utils::Theme::IconsStopToolBarColor}});
+const Icon INTERRUPT_SMALL({
+        {QLatin1String(":/utils/images/interrupt_small.png"), Utils::Theme::IconsInterruptColor}}, Icon::MenuTintedStyle);
+const Icon INTERRUPT_SMALL_TOOLBAR({
+        {QLatin1String(":/utils/images/interrupt_small.png"), Utils::Theme::IconsInterruptToolBarColor}});
+const Icon BOUNDING_RECT({
+        {QLatin1String(":/utils/images/boundingrect.png"), Theme::IconsBaseColor}});
+const Icon EYE_OPEN_TOOLBAR({
+        {QLatin1String(":/utils/images/eye_open.png"), Theme::IconsBaseColor}});
+const Icon EYE_CLOSED_TOOLBAR({
+        {QLatin1String(":/utils/images/eye_closed.png"), Theme::IconsBaseColor}});
+const Icon REPLACE({
+        {QLatin1String(":/utils/images/replace_a.png"), Theme::PanelTextColorMid},
+        {QLatin1String(":/utils/images/replace_b.png"), Theme::IconsInfoColor}}, Icon::Tint);
+const Icon EXPAND({
+        {QLatin1String(":/utils/images/expand.png"), Theme::PanelTextColorMid}}, Icon::Tint);
+const Icon EXPAND_TOOLBAR({
+        {QLatin1String(":/utils/images/expand.png"), Theme::IconsBaseColor}});
+const Icon COLLAPSE({
+        {QLatin1String(":/utils/images/collapse.png"), Theme::PanelTextColorMid}}, Icon::Tint);
+const Icon COLLAPSE_TOOLBAR({
+        {QLatin1String(":/utils/images/collapse.png"), Theme::IconsBaseColor}});
 
 } // namespace Icons
 } // namespace Utils

commit 285c8a7543aae0d71949451f04c99a4924900104

Fix wrong icon path
Regression introduced by 15fbfaf2e94b72cfd54bb0bd6f2b9ab87b870795

Change-Id: I795519fbf8f0370399583af8268bb4cc6bd3530d
Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

src/libs/utils/utilsicons.cpp
@@ -178,6 +178,7 @@ const Icon COLLAPSE({
         {QLatin1String(":/utils/images/collapse.png"), Theme::PanelTextColorMid}}, Icon::Tint);
 const Icon COLLAPSE_TOOLBAR({
         {QLatin1String(":/utils/images/collapse.png"), Theme::IconsBaseColor}});
+const Icon EMPTY14(":/utils/images/empty14.png");
 
 } // namespace Icons
 } // namespace Utils

src/libs/utils/utilsicons.h
@@ -105,6 +105,7 @@ QTCREATOR_UTILS_EXPORT extern const Icon EXPAND;
 QTCREATOR_UTILS_EXPORT extern const Icon EXPAND_TOOLBAR;
 QTCREATOR_UTILS_EXPORT extern const Icon COLLAPSE;
 QTCREATOR_UTILS_EXPORT extern const Icon COLLAPSE_TOOLBAR;
+QTCREATOR_UTILS_EXPORT extern const Icon EMPTY14;
 
 } // namespace Icons
 } // namespace Utils

src/plugins/coreplugin/editormanager/openeditorswindow.cpp
@@ -32,6 +32,7 @@
 
 #include <utils/hostosinfo.h>
 #include <utils/qtcassert.h>
+#include <utils/utilsicons.h>
 
 #include <QFocusEvent>
 #include <QHeaderView>
@@ -52,7 +53,7 @@ enum class Role
 
 OpenEditorsWindow::OpenEditorsWindow(QWidget *parent) :
     QFrame(parent, Qt::Popup),
-    m_emptyIcon(QLatin1String(":/core/images/empty14.png")),
+    m_emptyIcon(Utils::Icons::EMPTY14.icon()),
     m_editorList(new OpenEditorsTreeWidget(this))
 {
     setMinimumSize(300, 200);