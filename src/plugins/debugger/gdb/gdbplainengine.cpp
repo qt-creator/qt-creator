qt-creator/qt-creator

commit 588443b259d366dfc0e98db133914cf969c11c55

Debugger: Use a StandardRunnable for the debugger process
One step further to separate the debugger environment from the
inferior environment and to make it possible to configure a
working directory. Guessing one from the inferior's working
directory is not always a good idea.

Change-Id: I33d139c0f228ec0870556b82bc6aecca0a8e62d6
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/debugger/gdb/gdbplainengine.cpp
@@ -121,10 +121,6 @@ void GdbPlainEngine::setupEngine()
     }
     gdbArgs.append("--tty=" + m_outputCollector.serverName());
 
-    QString workingDirectory = runParameters().inferior.workingDirectory;
-    if (!workingDirectory.isEmpty() && QFileInfo::exists(workingDirectory))
-        m_gdbProc.setWorkingDirectory(workingDirectory);
-
     startGdb(gdbArgs);
 }
 

src/plugins/android/androidconfigurations.cpp
@@ -1268,7 +1268,7 @@ void AndroidConfigurations::updateAutomaticKitList()
 
         // Update code for 3.0 beta, which shipped with a bug for the debugger settings
         ToolChain *tc = ToolChainKitInformation::toolChain(k, ToolChain::Language::Cxx);
-        if (tc && Debugger::DebuggerKitInformation::debuggerCommand(k) != tc->suggestedDebugger()) {
+        if (tc && Debugger::DebuggerKitInformation::runnable(k).executable != tc->suggestedDebugger().toString()) {
             Debugger::DebuggerItem debugger;
             debugger.setCommand(tc->suggestedDebugger());
             debugger.setEngineType(Debugger::GdbEngineType);

src/plugins/debugger/cdb/cdbengine.cpp
@@ -506,7 +506,7 @@ bool CdbEngine::launchCDB(const DebuggerRunParameters &sp, QString *errorMessage
     // Determine binary (force MSVC), extension lib name and path to use
     // The extension is passed as relative name with the path variable set
     //(does not work with absolute path names)
-    const QString executable = sp.debuggerCommand;
+    const QString executable = sp.debugger.executable;
     if (executable.isEmpty()) {
         *errorMessage = tr("There is no CDB executable specified.");
         return false;
@@ -975,7 +975,7 @@ void CdbEngine::doInterruptInferior(SpecialStopMode sm)
     connect(m_signalOperation.data(), &DeviceProcessSignalOperation::finished,
             this, &CdbEngine::handleDoInterruptInferior);
 
-    m_signalOperation->setDebuggerCommand(runParameters().debuggerCommand);
+    m_signalOperation->setDebuggerCommand(runParameters().debugger.executable);
     m_signalOperation->interruptProcess(inferiorPid());
 }
 

src/plugins/debugger/debuggerengine.cpp
@@ -104,7 +104,7 @@ QDebug operator<<(QDebug str, const DebuggerRunParameters &sp)
             << " coreFile=" << sp.coreFile
             << " processArgs=" << sp.inferior.commandLineArguments
             << " inferior environment=<" << sp.inferior.environment.size() << " variables>"
-            << " debugger environment=<" << sp.debuggerEnvironment.size() << " variables>"
+            << " debugger environment=<" << sp.debugger.environment.size() << " variables>"
             << " workingDir=" << sp.inferior.workingDirectory
             << " attachPID=" << sp.attachPID
             << " useTerminal=" << sp.useTerminal

src/plugins/debugger/debuggerengine.h
@@ -27,10 +27,12 @@
 
 #include "debugger_global.h"
 #include "debuggerconstants.h"
+#include "debuggeritem.h"
 #include "debuggerprotocol.h"
 #include "debuggerstartparameters.h"
 
 #include <projectexplorer/devicesupport/idevice.h>
+#include <projectexplorer/runnables.h>
 #include <texteditor/textmark.h>
 
 #include <QObject>
@@ -89,7 +91,7 @@ public:
     bool breakOnMain = false;
     bool multiProcess = false; // Whether to set detach-on-fork off.
 
-    QString debuggerCommand;
+    ProjectExplorer::StandardRunnable debugger;
     QString coreFile;
     QString overrideStartScript; // Used in attach to core and remote debugging
     QString startMessage; // First status message shown.

src/plugins/debugger/debuggeritem.cpp
@@ -29,6 +29,7 @@
 #include "debuggeroptionspage.h"
 #include "debuggerprotocol.h"
 
+#include <coreplugin/coreicons.h>
 #include <projectexplorer/abi.h>
 
 #include <utils/fileutils.h>
@@ -59,6 +60,7 @@ const char DEBUGGER_INFORMATION_AUTODETECTION_SOURCE[] = "AutoDetectionSource";
 const char DEBUGGER_INFORMATION_VERSION[] = "Version";
 const char DEBUGGER_INFORMATION_ABIS[] = "Abis";
 const char DEBUGGER_INFORMATION_LASTMODIFIED[] = "LastModified";
+const char DEBUGGER_INFORMATION_WORKINGDIRECTORY[] = "WorkingDirectory";
 
 namespace Debugger {
 
@@ -81,8 +83,9 @@ DebuggerItem::DebuggerItem(const QVariant &id)
 
 DebuggerItem::DebuggerItem(const QVariantMap &data)
 {
-    m_command = FileName::fromUserInput(data.value(QLatin1String(DEBUGGER_INFORMATION_COMMAND)).toString());
     m_id = data.value(QLatin1String(DEBUGGER_INFORMATION_ID)).toString();
+    m_command = FileName::fromUserInput(data.value(QLatin1String(DEBUGGER_INFORMATION_COMMAND)).toString());
+    m_workingDirectory = FileName::fromUserInput(data.value(DEBUGGER_INFORMATION_WORKINGDIRECTORY).toString());
     m_unexpandedDisplayName = data.value(QLatin1String(DEBUGGER_INFORMATION_DISPLAYNAME)).toString();
     m_isAutoDetected = data.value(QLatin1String(DEBUGGER_INFORMATION_AUTODETECTED), false).toBool();
     m_autoDetectionSource = data.value(QLatin1String(DEBUGGER_INFORMATION_AUTODETECTION_SOURCE)).toString();
@@ -213,9 +216,15 @@ QDateTime DebuggerItem::lastModified() const
     return m_lastModified;
 }
 
-bool DebuggerItem::isGood() const
+QIcon DebuggerItem::decoration() const
 {
-    return m_engineType != NoEngineType;
+    if (m_engineType == NoEngineType)
+        return Core::Icons::ERROR.icon();
+    if (!m_command.toFileInfo().isExecutable())
+        return Core::Icons::WARNING.icon();
+    if (!m_workingDirectory.isEmpty() && !m_workingDirectory.toFileInfo().isDir())
+        return Core::Icons::WARNING.icon();
+    return QIcon();
 }
 
 QString DebuggerItem::validityMessage() const
@@ -230,7 +239,8 @@ bool DebuggerItem::operator==(const DebuggerItem &other) const
     return m_id == other.m_id
             && m_unexpandedDisplayName == other.m_unexpandedDisplayName
             && m_isAutoDetected == other.m_isAutoDetected
-            && m_command == other.m_command;
+            && m_command == other.m_command
+            && m_workingDirectory == other.m_workingDirectory;
 }
 
 QVariantMap DebuggerItem::toMap() const
@@ -239,6 +249,7 @@ QVariantMap DebuggerItem::toMap() const
     data.insert(QLatin1String(DEBUGGER_INFORMATION_DISPLAYNAME), m_unexpandedDisplayName);
     data.insert(QLatin1String(DEBUGGER_INFORMATION_ID), m_id);
     data.insert(QLatin1String(DEBUGGER_INFORMATION_COMMAND), m_command.toString());
+    data.insert(QLatin1String(DEBUGGER_INFORMATION_WORKINGDIRECTORY), m_workingDirectory.toString());
     data.insert(QLatin1String(DEBUGGER_INFORMATION_ENGINETYPE), int(m_engineType));
     data.insert(QLatin1String(DEBUGGER_INFORMATION_AUTODETECTED), m_isAutoDetected);
     data.insert(QLatin1String(DEBUGGER_INFORMATION_AUTODETECTION_SOURCE), m_autoDetectionSource);

src/plugins/debugger/debuggeritem.h
@@ -94,13 +94,16 @@ public:
     QStringList abiNames() const;
     QDateTime lastModified() const;
 
-    bool isGood() const;
+    QIcon decoration() const;
     QString validityMessage() const;
 
     bool operator==(const DebuggerItem &other) const;
     bool operator!=(const DebuggerItem &other) const { return !operator==(other); }
 
     void reinitializeFromFile();
+
+    Utils::FileName workingDirectory() const { return m_workingDirectory; }
+    void setWorkingDirectory(const Utils::FileName &workingPath) { m_workingDirectory = workingPath; }
 
 private:
     DebuggerItem(const QVariant &id);
@@ -110,6 +113,7 @@ private:
     QString m_unexpandedDisplayName;
     DebuggerEngineType m_engineType;
     Utils::FileName m_command;
+    Utils::FileName m_workingDirectory;
     bool m_isAutoDetected;
     QString m_autoDetectionSource;
     QString m_version;

src/plugins/debugger/debuggerkitinformation.cpp
@@ -32,6 +32,7 @@
 #include <projectexplorer/toolchain.h>
 #include <projectexplorer/projectexplorerconstants.h>
 
+#include <utils/environment.h>
 #include <utils/fileutils.h>
 #include <utils/macroexpander.h>
 #include <utils/qtcassert.h>
@@ -244,6 +245,18 @@ const DebuggerItem *DebuggerKitInformation::debugger(const Kit *kit)
     return DebuggerItemManager::findById(id);
 }
 
+StandardRunnable DebuggerKitInformation::runnable(const Kit *kit)
+{
+    StandardRunnable runnable;
+    if (const DebuggerItem *item = debugger(kit)) {
+        runnable.executable = item->command().toString();
+        runnable.workingDirectory = item->workingDirectory().toString();
+        runnable.environment = Utils::Environment::systemEnvironment();
+        runnable.environment.set("LC_NUMERIC", "C");
+    }
+    return runnable;
+}
+
 bool DebuggerKitInformation::isValidDebugger(const Kit *k)
 {
     return debuggerConfigurationErrors(k) == 0;
@@ -324,14 +337,6 @@ void DebuggerKitInformation::addToMacroExpander(Kit *kit, MacroExpander *expande
 KitInformation::ItemList DebuggerKitInformation::toUserOutput(const Kit *k) const
 {
     return ItemList() << qMakePair(tr("Debugger"), displayString(k));
-}
-
-FileName DebuggerKitInformation::debuggerCommand(const Kit *k)
-{
-    const DebuggerItem *item = debugger(k);
-    if (item)
-        return item->command();
-    return FileName();
 }
 
 DebuggerEngineType DebuggerKitInformation::engineType(const Kit *k)

commit f9a44fc486382c35775464ed0ca3b5c8af142539

Debugger: Fix working directory when debugging
The debugger is not executed in the working directory configured for the
run configuration, but in the directory configured in the kit. We have to
cd to the inferior's working directory before execution.

This was broken by 588443b2.

Change-Id: Ib25a82c0cb6e4d1269a2e3fd4bf1f71893005cfa
Reviewed-by: hjk <hjk@qt.io>

src/plugins/debugger/gdb/gdbplainengine.cpp
@@ -52,8 +52,11 @@ void GdbPlainEngine::setupInferior()
 {
     QTC_ASSERT(state() == InferiorSetupRequested, qDebug() << state());
     setEnvironmentVariables();
-    if (!runParameters().inferior.commandLineArguments.isEmpty()) {
-        QString args = runParameters().inferior.commandLineArguments;
+    const DebuggerRunParameters &rp = runParameters();
+    if (!rp.inferior.workingDirectory.isEmpty())
+        runCommand({"cd " + rp.inferior.workingDirectory, NoFlags});
+    if (!rp.inferior.commandLineArguments.isEmpty()) {
+        QString args = rp.inferior.commandLineArguments;
         runCommand({"-exec-arguments " + args, NoFlags});
     }
 