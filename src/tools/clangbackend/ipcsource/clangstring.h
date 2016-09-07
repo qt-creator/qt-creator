43d6726c7058c9a874a0a2bee25c42277b474572
Marco Bubke <marco.bubke@qt.io>
2016-09-07 11:58:34 +0200

    Clang: Add equal operators for ClangString
    Converting them in a Utf8String in an inner loop can be quite heavy.
    
    Change-Id: I4f2ccde47a6e3ca08ed054462eb7772ded9e55c9
    Reviewed-by: Nikolai Kosjar <nikolai.kosjar@qt.io>

--- src/tools/clangbackend/ipcsource/clangstring.h
+++ src/tools/clangbackend/ipcsource/clangstring.h
@@ -85,7 +85,38 @@ public:
         return cxString.data == nullptr;
     }
 
+    friend bool operator==(const ClangString &first, const ClangString &second)
+    {
+        return std::strcmp(first.cString(), second.cString()) == 0;
+    }
 
+    template<std::size_t Size>
+    friend bool operator==(const ClangString &first, const char(&second)[Size])
+    {
+        return std::strncmp(first.cString(), second, Size) == 0; // Size includes \0
+    }
+
+    template<std::size_t Size>
+    friend bool operator==(const char(&first)[Size], const ClangString &second)
+    {
+        return second == first;
+    }
+
+    template<typename Type,
+             typename = typename std::enable_if<std::is_pointer<Type>::value>::type
+             >
+    friend bool operator==(const ClangString &first, Type second)
+    {
+        return std::strcmp(first.cString(), second) == 0;
+    }
+
+    template<typename Type,
+             typename = typename std::enable_if<std::is_pointer<Type>::value>::type
+             >
+    friend bool operator==(Type first, const ClangString &second)
+    {
+        return second == first;
+    }
 
 private:
     CXString cxString;
--- tests/unit/unittest/clangstring-test.cpp
+++ tests/unit/unittest/clangstring-test.cpp
@@ -35,6 +35,7 @@
 namespace {
 
 using ::testing::StrEq;
+using ::testing::Eq;
 
 using ClangBackEnd::ClangString;
 
@@ -88,4 +89,82 @@ TEST(ClangString, SpellingAsCString)
 
     ASSERT_THAT(text.cString(), StrEq("text"));
 }
+
+TEST(ClangString, EqualBetweenClangStrings)
+{
+    ClangString text(CXString{"text", 0});
+    ClangString text2(CXString{"text", 0});
+
+    bool textIsEqual = text == text2;
+
+    ASSERT_TRUE(textIsEqual);
+}
+
+TEST(ClangString, NotEqualBetweenClangStrings)
+{
+    ClangString text(CXString{"text", 0});
+    ClangString text2(CXString{"text ", 0});
+
+    bool textIsEqual = text == text2;
+
+    ASSERT_FALSE(textIsEqual);
+}
+
+TEST(ClangString, EqualClangStringAndCString)
+{
+    ClangString text(CXString{"text", 0});
+
+    bool textIsEqual = text == "text";
+
+    ASSERT_TRUE(textIsEqual);
+}
+
+TEST(ClangString, NotEqualClangStringAndCString)
+{
+    ClangString text(CXString{"text", 0});
+
+    bool textIsEqual = text == "text ";
+
+    ASSERT_FALSE(textIsEqual);
+}
+
+TEST(ClangString, EqualCStringAndClangString)
+{
+    ClangString text(CXString{"text", 0});
+
+    bool textIsEqual = "text" == text;
+
+    ASSERT_TRUE(textIsEqual);
+}
+
+TEST(ClangString, EqualClangStringPointerAndCString)
+{
+    ClangString text(CXString{"text", 0});
+    const char *cString = "text";
+
+    bool textIsEqual = cString == text;
+
+    ASSERT_TRUE(textIsEqual);
+}
+
+TEST(ClangString, NotEqualClangStringPointerAndCString)
+{
+    ClangString text(CXString{"text", 0});
+    const char *cString = "text ";
+
+    bool textIsEqual = cString == text;
+
+    ASSERT_FALSE(textIsEqual);
+}
+
+TEST(ClangString, EqualCStringAndClangStringPointer)
+{
+    ClangString text(CXString{"text", 0});
+    const char *cString = "text";
+
+    bool textIsEqual = text == cString;
+
+    ASSERT_TRUE(textIsEqual);
+}
+
 }
