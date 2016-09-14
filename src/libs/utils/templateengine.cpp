qt-creator/qt-creator

commit 8641277121632de574e1bc76a76d8727e9b7cd03

TemplateEngine: Do not append trailing newline
Change-Id: I99731186ae1e14c79728ebc6c74a5b842cf25069
Reviewed-by: David Schulz <david.schulz@theqtcompany.com>

src/libs/utils/templateengine.cpp
@@ -211,8 +211,9 @@ bool PreprocessContext::process(const QString &in, QString *out, QString *errorM
             break;
         case OtherSection: // Rest: Append according to current condition.
             if (top.condition) {
+                if (l != 0)
+                    out->append(newLine);
                 out->append(lines.at(l));
-                out->append(newLine);
             }
             break;
         } // switch section

commit 6f963fd56ef44694d42f5b9f5ea08909fa3b6a8c

Utils: Fix logic in TemplateEngine and adjust auto test
Broke with 8641277121632de574e1bc76a76d8727e9b7cd03.

Change-Id: I49174428bf940ef4c3fa7f6ebd3124a907a555b8
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/libs/utils/templateengine.cpp
@@ -139,6 +139,7 @@ bool PreprocessContext::process(const QString &in, QString *out, QString *errorM
     const QChar newLine = QLatin1Char('\n');
     const QStringList lines = in.split(newLine, QString::KeepEmptyParts);
     const int lineCount = lines.size();
+    bool first = true;
     for (int l = 0; l < lineCount; l++) {
         // Check for element of the stack (be it dummy, else something is wrong).
         if (m_sectionStack.isEmpty()) {
@@ -206,7 +207,9 @@ bool PreprocessContext::process(const QString &in, QString *out, QString *errorM
             break;
         case OtherSection: // Rest: Append according to current condition.
             if (top.condition) {
-                if (l != 0)
+                if (first)
+                    first = false;
+                else
                     out->append(newLine);
                 out->append(lines.at(l));
             }

tests/auto/utils/templateengine/tst_templateengine.cpp
@@ -45,15 +45,15 @@ void tst_TemplateEngine::testTemplateEngine_data()
     QTest::addColumn<QString>("expectedErrorMessage");
     QTest::newRow("if")
         << QString::fromLatin1("@if 1\nline 1\n@elsif 0\nline 2\n@else\nline 3\n@endif\n")
-        << QString::fromLatin1("line 1\n\n")
+        << QString::fromLatin1("line 1\n")
         << QString();
     QTest::newRow("elsif")
         << QString::fromLatin1("@if 0\nline 1\n@elsif 1\nline 2\n@else\nline 3\n@endif\n")
-        << QString::fromLatin1("line 2\n\n")
+        << QString::fromLatin1("line 2\n")
         << QString();
     QTest::newRow("else")
         << QString::fromLatin1("@if 0\nline 1\n@elsif 0\nline 2\n@else\nline 3\n@endif\n")
-        << QString::fromLatin1("line 3\n\n")
+        << QString::fromLatin1("line 3\n")
         << QString();
     QTest::newRow("nested-if")
         << QString::fromLatin1("@if 1\n"
@@ -61,7 +61,7 @@ void tst_TemplateEngine::testTemplateEngine_data()
                                "@else\n"
                                "  @if 1\nline 4\n@elsif 0\nline 5\n@else\nline 6\n@endif\n"
                                "@endif\n")
-        << QString::fromLatin1("line 1\n\n")
+        << QString::fromLatin1("line 1\n")
         << QString();
     QTest::newRow("nested-else")
         << QString::fromLatin1("@if 0\n"
@@ -69,7 +69,7 @@ void tst_TemplateEngine::testTemplateEngine_data()
                                "@else\n"
                                "  @if 1\nline 4\n@elsif 0\nline 5\n@else\nline 6\n@endif\n"
                                "@endif\n")
-        << QString::fromLatin1("line 4\n\n")
+        << QString::fromLatin1("line 4\n")
         << QString();
     QTest::newRow("twice-nested-if")
         << QString::fromLatin1("@if 0\n"
@@ -81,7 +81,7 @@ void tst_TemplateEngine::testTemplateEngine_data()
                                "    @if 1\nline 3\n@else\nline 4\n@endif\n"
                                "  @endif\n"
                                "@endif\n")
-        << QString::fromLatin1("line 3\n\n")
+        << QString::fromLatin1("line 3\n")
         << QString();
 }
 