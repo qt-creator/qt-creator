33d67828f6d4ff96031fab692df7d9f189b3023f
Takumi ASAKI <asaki@sra.co.jp>
2016-09-15 11:00:46 +0900

    EditorManager: Use "URL" in message
    Change-Id: Icfd31b7cd89a727a3fbfc53ad53f036f7f7c3120
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>

--- src/plugins/coreplugin/editormanager/systemeditor.cpp
+++ src/plugins/coreplugin/editormanager/systemeditor.cpp
@@ -61,7 +61,7 @@ bool SystemEditor::startEditor(const QString &fileName, QString *errorMessage)
     url.setScheme(QLatin1String("file"));
     if (!QDesktopServices::openUrl(url)) {
         if (errorMessage)
-            *errorMessage = tr("Could not open url %1.").arg(url.toString());
+            *errorMessage = tr("Could not open URL %1.").arg(url.toString());
         return false;
     }
     return true;
