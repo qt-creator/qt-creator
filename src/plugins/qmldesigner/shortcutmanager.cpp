qt-creator/qt-creator

commit 5da074f95a0a8fcc548a5b0bad4fe50a5fa11e5a

QmlDesigner: Fix ESC issues
We do not want to switch to the editor whenever pressing ESCAPE.
Using shortcut override does not work for QQuickWidget.

Therefore we register our own action and disable it.

Change-Id: I399265a5254767d2825edf3fe823dd35177a6ce5
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/plugins/qmldesigner/shortcutmanager.cpp
@@ -218,6 +218,11 @@ void ShortCutManager::registerActions(const Core::Context &qmlDesignerMainContex
     command->setAttribute(Core::Command::CA_Hide);
     viewsMenu->addAction(command);
 
+    /* Registering disabled action for Escape, because Qt Quick does not support shortcut overrides. */
+    command = Core::ActionManager::registerAction(&m_escapeAction, Core::Constants::S_RETURNTOEDITOR, qmlDesignerMainContext);
+    command->setDefaultKeySequence(QKeySequence(Qt::Key_Escape));
+    m_escapeAction.setEnabled(false);
+
     Core::ActionManager::registerAction(&m_hideSidebarsAction, Core::Constants::TOGGLE_SIDEBAR, qmlDesignerMainContext);
 }
 

share/qtcreator/qmldesigner/propertyEditorQmlSources/HelperWidgets/ExpressionTextField.qml
@@ -133,6 +133,8 @@ Controls.TextField {
         event.accepted = true
         if (textField.completionActive) {
             listView.model = null
+        } else {
+            textField.rejected()
         }
     }
 

src/plugins/qmldesigner/shortcutmanager.h
@@ -91,6 +91,7 @@ private:
     QAction m_toggleLeftSidebarAction;
     QAction m_toggleRightSidebarAction;
     QAction m_goIntoComponentAction;
+    QAction m_escapeAction;
 };
 
 } // namespace QmlDesigner

commit fc641923124c111b8966d6d19b9d92a85207586f

QmlDesigner: Fix compilation with Qt 5.6
Broke by 5da074f95a0a8fcc548a5b0bad4fe50a5fa11e5a.

Change-Id: Icd0400e7b19a564ba2777e1cceadd4994eb867f8
Reviewed-by: Christian Stenger <christian.stenger@qt.io>
Reviewed-by: Thomas Hartmann <Thomas.Hartmann@theqtcompany.com>

src/plugins/qmldesigner/shortcutmanager.cpp
@@ -69,7 +69,8 @@ ShortCutManager::ShortCutManager()
     m_restoreDefaultViewAction(tr("&Restore Default View"), 0),
     m_toggleLeftSidebarAction(tr("Toggle &Left Sidebar"), 0),
     m_toggleRightSidebarAction(tr("Toggle &Right Sidebar"), 0),
-    m_goIntoComponentAction (tr("&Go into Component"), 0)
+    m_goIntoComponentAction(tr("&Go into Component"), 0),
+    m_escapeAction(this)
 {
 
 }