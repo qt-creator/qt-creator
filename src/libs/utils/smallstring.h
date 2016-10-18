c56f3f235bbc77695563c152fbe9304e8fb8e164
Marco Bubke <marco.bubke@qt.io>
2016-10-18 18:15:49 +0200

    Utils: Remove noexcept from SmallString constructor
    Change-Id: I561b523bfac91098f721a5d53806670a3b682514
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- src/libs/utils/smallstring.h
+++ src/libs/utils/smallstring.h
@@ -118,7 +118,7 @@ public:
     template<typename Type,
              typename = typename std::enable_if<std::is_pointer<Type>::value>::type
              >
-    SmallString(Type characterPointer) noexcept
+    SmallString(Type characterPointer)
         : SmallString(characterPointer, std::strlen(characterPointer))
     {
         static_assert(!std::is_array<Type>::value, "Input type is array and not char pointer!");
