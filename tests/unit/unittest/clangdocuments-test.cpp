qt-creator/qt-creator

commit 380d756a03346e5bbb83101f49ed6f6696c42c6a

Clang: Hook up supportive translation unit on first edit
Parsing happens rotationally on the translation units.

The recently parsed translation unit is used for completion jobs while
the older version is used for parse jobs.

Advantages:
  A1. A completion job cannot be blocked anymore by currently running
      parse job.
  A2. Faster triggering of parse jobs. A reparse was triggered about
      1650ms after the last keystroke. This is down to 500ms now since we
      do not have a blocking translation unit for the completion anymore.

Disadvantages:
  D1. Memory consumption is doubled for an edited document.
      This could be addressed by suspending the second translation unit
      after some time of inactivity.
  D2. Setup of the supportive translation unit takes some time.

Change-Id: I958c883c01f274530f5482c788c15cd38d6f4c3e
Reviewed-by: David Schulz <david.schulz@qt.io>

src/tools/clangbackend/ipcsource/clangcodemodelserver.cpp
@@ -27,6 +27,7 @@
 
 #include "clangdocuments.h"
 #include "clangfilesystemwatcher.h"
+#include "clangtranslationunits.h"
 #include "codecompleter.h"
 #include "diagnosticset.h"
 #include "highlightingmarks.h"
@@ -126,10 +127,16 @@ void ClangCodeModelServer::updateTranslationUnitsForEditor(const UpdateTranslati
     try {
         const auto newerFileContainers = documents.newerFileContainers(message.fileContainers());
         if (newerFileContainers.size() > 0) {
-            documents.update(newerFileContainers);
+            const std::vector<Document> updateDocuments = documents.update(newerFileContainers);
             unsavedFiles.createOrUpdate(newerFileContainers);
 
-            updateDocumentAnnotationsTimer.start(updateDocumentAnnotationsTimeOutInMs);
+            // Start the jobs on the next event loop iteration since otherwise
+            // we might block the translation unit for a completion request
+            // that comes right after this message.
+            updateDocumentAnnotationsTimer.start(0);
+            QTimer::singleShot(0, [this, updateDocuments](){
+                startInitializingSupportiveTranslationUnits(updateDocuments);
+            });
         }
     } catch (const std::exception &exception) {
         qWarning() << "Error in ClangCodeModelServer::updateTranslationUnitsForEditor:" << exception.what();
@@ -286,7 +293,9 @@ void ClangCodeModelServer::processJobsForDirtyAndVisibleDocuments()
     for (const auto &document : documents.documents()) {
         if (document.isNeedingReparse() && document.isVisibleInEditor()) {
             DocumentProcessor processor = documentProcessors().processor(document);
-            processor.addJob(createJobRequest(document, JobRequest::Type::UpdateDocumentAnnotations));
+            processor.addJob(createJobRequest(document,
+                                              JobRequest::Type::UpdateDocumentAnnotations,
+                                              PreferredTranslationUnit::PreviouslyParsed));
         }
     }
 
@@ -297,14 +306,37 @@ void ClangCodeModelServer::processInitialJobsForDocuments(const std::vector<Docu
 {
     for (const auto &document : documents) {
         DocumentProcessor processor = documentProcessors().create(document);
+        const auto jobRequestCreator = [this](const Document &document,
+                JobRequest::Type jobRequestType,
+                PreferredTranslationUnit preferredTranslationUnit) {
+            return createJobRequest(document, jobRequestType, preferredTranslationUnit);
+        };
+        processor.setJobRequestCreator(jobRequestCreator);
+
         processor.addJob(createJobRequest(document, JobRequest::Type::UpdateDocumentAnnotations));
         processor.addJob(createJobRequest(document, JobRequest::Type::CreateInitialDocumentPreamble));
         processor.process();
     }
 }
 
-JobRequest ClangCodeModelServer::createJobRequest(const Document &document,
-                                                  JobRequest::Type type) const
+void ClangCodeModelServer::startInitializingSupportiveTranslationUnits(
+        const std::vector<Document> &documents)
+{
+    for (const Document &document : documents) {
+        try {
+            DocumentProcessor processor = documentProcessors().processor(document);
+            if (!processor.hasSupportiveTranslationUnit())
+                processor.startInitializingSupportiveTranslationUnit();
+        } catch (const DocumentProcessorDoesNotExist &) {
+            // OK, document was already closed.
+        }
+    }
+}
+
+JobRequest ClangCodeModelServer::createJobRequest(
+        const Document &document,
+        JobRequest::Type type,
+        PreferredTranslationUnit preferredTranslationUnit) const
 {
     JobRequest jobRequest;
     jobRequest.type = type;
@@ -313,6 +345,7 @@ JobRequest ClangCodeModelServer::createJobRequest(const Document &document,
     jobRequest.projectPartId = document.projectPartId();
     jobRequest.unsavedFilesChangeTimePoint = unsavedFiles.lastChangeTimePoint();
     jobRequest.documentRevision = document.documentRevision();
+    jobRequest.preferredTranslationUnit = preferredTranslationUnit;
     const ProjectPart &projectPart = projects.project(document.projectPartId());
     jobRequest.projectChangeTimePoint = projectPart.lastChangeTimePoint();
 

src/tools/clangbackend/ipcsource/clangcodemodelserver.h
@@ -65,16 +65,20 @@ public: // for tests
     int queueSizeForTestsOnly();
     bool isTimerRunningForTestOnly() const;
     void setUpdateDocumentAnnotationsTimeOutInMsForTestsOnly(int value);
+    DocumentProcessors &documentProcessors();
 
 private:
-    DocumentProcessors &documentProcessors();
 
     void startDocumentAnnotationsTimerIfFileIsNotOpenAsDocument(const Utf8String &filePath);
     void addJobRequestsForDirtyAndVisibleDocuments();
     void processJobsForDirtyAndVisibleDocuments();
     void processInitialJobsForDocuments(const std::vector<Document> &documents);
+    void startInitializingSupportiveTranslationUnits(const std::vector<Document> &documents);
 
-    JobRequest createJobRequest(const Document &document, JobRequest::Type type) const;
+    JobRequest createJobRequest(const Document &document,
+                                JobRequest::Type type,
+                                PreferredTranslationUnit preferredTranslationUnit
+                                    = PreferredTranslationUnit::RecentlyParsed) const;
 
 private:
     ProjectParts projects;

tests/unit/unittest/clangipcserver-test.cpp
@@ -32,6 +32,7 @@
 #include <highlightingmarkcontainer.h>
 #include <clangcodemodelclientproxy.h>
 #include <clangcodemodelserverproxy.h>
+#include <clangtranslationunits.h>
 #include <requestdocumentannotations.h>
 #include <clangexceptions.h>
 
@@ -134,6 +135,8 @@ protected:
                       const Utf8String &projectPartId = Utf8String());
     void completeCodeInFileA();
     void completeCodeInFileB();
+
+    bool isSupportiveTranslationUnitInitialized(const Utf8String &filePath);
 
     void expectDocumentAnnotationsChanged(int count);
     void expectCompletion(const CodeCompletion &completion);
@@ -286,6 +289,50 @@ TEST_F(ClangClangCodeModelServer, SetCurrentAndVisibleEditor)
     ASSERT_TRUE(functionDocument.isVisibleInEditor());
 }
 
+TEST_F(ClangClangCodeModelServer, StartCompletionJobFirstOnEditThatTriggersCompletion)
+{
+    registerProjectAndFile(filePathA, 2);
+    ASSERT_TRUE(waitUntilAllJobsFinished());
+    expectCompletionFromFileA();
+
+    updateUnsavedContent(filePathA, unsavedContent(filePathAUnsavedVersion2), 1);
+    completeCodeInFileA();
+
+    const QList<Jobs::RunningJob> jobs = clangServer.runningJobsForTestsOnly();
+    ASSERT_THAT(jobs.size(), Eq(1));
+    ASSERT_THAT(jobs.first().jobRequest.type, Eq(JobRequest::Type::CompleteCode));
+}
+
+TEST_F(ClangClangCodeModelServer, SupportiveTranslationUnitNotInitializedAfterRegister)
+{
+    registerProjectAndFile(filePathA, 1);
+
+    ASSERT_TRUE(waitUntilAllJobsFinished());
+    ASSERT_FALSE(isSupportiveTranslationUnitInitialized(filePathA));
+}
+
+TEST_F(ClangClangCodeModelServer, SupportiveTranslationUnitIsSetupAfterFirstEdit)
+{
+    registerProjectAndFile(filePathA, 2);
+    ASSERT_TRUE(waitUntilAllJobsFinished());
+
+    updateUnsavedContent(filePathA, unsavedContent(filePathAUnsavedVersion2), 1);
+
+    ASSERT_TRUE(waitUntilAllJobsFinished());
+    ASSERT_TRUE(isSupportiveTranslationUnitInitialized(filePathA));
+}
+
+TEST_F(ClangClangCodeModelServer, OpenDocumentAndEdit)
+{
+    registerProjectAndFile(filePathA, 4);
+    ASSERT_TRUE(waitUntilAllJobsFinished());
+
+    for (unsigned revision = 1; revision <= 3; ++revision) {
+        updateUnsavedContent(filePathA, unsavedContent(filePathAUnsavedVersion2), revision);
+        ASSERT_TRUE(waitUntilAllJobsFinished());
+    }
+}
+
 TEST_F(ClangClangCodeModelServer, IsNotCurrentCurrentAndVisibleEditorAnymore)
 {
     registerProjectAndFilesAndWaitForFinished();
@@ -409,6 +456,16 @@ void ClangClangCodeModelServer::completeCodeInFileA()
 void ClangClangCodeModelServer::completeCodeInFileB()
 {
     completeCode(filePathB, 35, 1);
+}
+
+bool ClangClangCodeModelServer::isSupportiveTranslationUnitInitialized(const Utf8String &filePath)
+{
+    Document document = clangServer.documentsForTestOnly().document(filePath, projectPartId);
+    DocumentProcessor documentProcessor = clangServer.documentProcessors().processor(document);
+
+    return document.translationUnits().size() == 2
+        && documentProcessor.hasSupportiveTranslationUnit()
+        && documentProcessor.isSupportiveTranslationUnitInitialized();
 }
 
 void ClangClangCodeModelServer::expectCompletion(const CodeCompletion &completion)

src/plugins/clangcodemodel/clangeditordocumentprocessor.cpp
@@ -72,6 +72,11 @@ ClangEditorDocumentProcessor::ClangEditorDocumentProcessor(
     , m_semanticHighlighter(document)
     , m_builtinProcessor(document, /*enableSemanticHighlighter=*/ false)
 {
+    m_updateTranslationUnitTimer.setSingleShot(true);
+    m_updateTranslationUnitTimer.setInterval(350);
+    connect(&m_updateTranslationUnitTimer, &QTimer::timeout,
+            this, &ClangEditorDocumentProcessor::updateTranslationUnitIfProjectPartExists);
+
     // Forwarding the semantic info from the builtin processor enables us to provide all
     // editor (widget) related features that are not yet implemented by the clang plugin.
     connect(&m_builtinProcessor, &CppTools::BuiltinEditorDocumentProcessor::cppDocumentUpdated,
@@ -82,6 +87,8 @@ ClangEditorDocumentProcessor::ClangEditorDocumentProcessor(
 
 ClangEditorDocumentProcessor::~ClangEditorDocumentProcessor()
 {
+    m_updateTranslationUnitTimer.stop();
+
     m_parserWatcher.cancel();
     m_parserWatcher.waitForFinished();
 
@@ -93,7 +100,7 @@ ClangEditorDocumentProcessor::~ClangEditorDocumentProcessor()
 
 void ClangEditorDocumentProcessor::run()
 {
-    updateTranslationUnitIfProjectPartExists();
+    m_updateTranslationUnitTimer.start();
 
     // Run clang parser
     disconnect(&m_parserWatcher, &QFutureWatcher<void>::finished,
@@ -249,6 +256,11 @@ void ClangEditorDocumentProcessor::addDiagnosticToolTipToLayout(uint line,
 {
     foreach (const auto &diagnostic, m_diagnosticManager.diagnosticsAt(line, column))
         addToolTipToLayout(diagnostic, target);
+}
+
+void ClangEditorDocumentProcessor::editorDocumentTimerRestarted()
+{
+    m_updateTranslationUnitTimer.stop(); // Wait for the next call to run().
 }
 
 ClangBackEnd::FileContainer ClangEditorDocumentProcessor::fileContainerWithArguments() const

src/plugins/clangcodemodel/clangeditordocumentprocessor.h
@@ -32,6 +32,7 @@
 #include <cpptools/semantichighlighter.h>
 
 #include <QFutureWatcher>
+#include <QTimer>
 
 namespace ClangBackEnd {
 class DiagnosticContainer;
@@ -78,6 +79,8 @@ public:
     bool hasDiagnosticsAt(uint line, uint column) const override;
     void addDiagnosticToolTipToLayout(uint line, uint column, QLayout *target) const override;
 
+    void editorDocumentTimerRestarted() override;
+
     ClangBackEnd::FileContainer fileContainerWithArguments() const;
 
     void clearDiagnosticsWithFixIts();
@@ -102,6 +105,7 @@ private:
     QSharedPointer<ClangEditorDocumentParser> m_parser;
     CppTools::ProjectPart::Ptr m_projectPart;
     QFutureWatcher<void> m_parserWatcher;
+    QTimer m_updateTranslationUnitTimer;
     unsigned m_parserRevision;
 
     CppTools::SemanticHighlighter m_semanticHighlighter;

src/plugins/cppeditor/cppeditordocument.cpp
@@ -230,12 +230,14 @@ void CppEditorDocument::scheduleProcessDocument()
 {
     m_processorRevision = document()->revision();
     m_processorTimer.start();
+    processor()->editorDocumentTimerRestarted();
 }
 
 void CppEditorDocument::processDocument()
 {
     if (processor()->isParserRunning() || m_processorRevision != contentsRevision()) {
         m_processorTimer.start();
+        processor()->editorDocumentTimerRestarted();
         return;
     }
 

src/plugins/cpptools/baseeditordocumentprocessor.cpp
@@ -67,6 +67,10 @@ void BaseEditorDocumentProcessor::addDiagnosticToolTipToLayout(uint, uint, QLayo
 {
 }
 
+void BaseEditorDocumentProcessor::editorDocumentTimerRestarted()
+{
+}
+
 void BaseEditorDocumentProcessor::runParser(QFutureInterface<void> &future,
                                             BaseEditorDocumentParser::Ptr parser,
                                             const WorkingCopy workingCopy)

src/plugins/cpptools/baseeditordocumentprocessor.h
@@ -67,6 +67,8 @@ public:
     virtual bool hasDiagnosticsAt(uint line, uint column) const;
     virtual void addDiagnosticToolTipToLayout(uint line, uint column, QLayout *layout) const;
 
+    virtual void editorDocumentTimerRestarted();
+
 signals:
     // Signal interface to implement
     void codeWarningsUpdated(unsigned revision,

commit 4fbdbdb1ee382a582035a4e9256ca2b7d4f3b943

Clang: Fix delayed reparse of dirty and visible but not current documents
1. Open document foo.h
2. Create a split and open foo.cpp (#including "foo.h")
3. Edit foo.h (e.g. by introducing a syntax error, so that foo.cpp will
   indicate header errors in the toolbar or as info bar)
=> Actual: foo.cpp will be reparsed immediately.
 Expected: foo.cpp should be reparsed after a delay.

This saves resources (cpu time) and minimizes poping up of the header
info bar while editing header files in splits.

Regression introduced by

    commit 380d756a03346e5bbb83101f49ed6f6696c42c6a
    Clang: Hook up supportive translation unit on first edit

Change-Id: Ib5fd90e49415dfc3aefacab7cd627b0e1937f5fc
Reviewed-by: David Schulz <david.schulz@qt.io>

src/tools/clangbackend/ipcsource/clangcodemodelserver.cpp
@@ -50,6 +50,8 @@
 #include <updatetranslationunitsforeditormessage.h>
 #include <updatevisibletranslationunitsmessage.h>
 
+#include <utils/qtcassert.h>
+
 #include <QCoreApplication>
 #include <QDebug>
 
@@ -57,14 +59,19 @@ namespace ClangBackEnd {
 
 ClangCodeModelServer::ClangCodeModelServer()
     : documents(projects, unsavedFiles)
-    , updateDocumentAnnotationsTimeOutInMs(1500)
 {
     updateDocumentAnnotationsTimer.setSingleShot(true);
-
     QObject::connect(&updateDocumentAnnotationsTimer,
                      &QTimer::timeout,
                      [this]() {
         processJobsForDirtyAndVisibleDocuments();
+    });
+
+    updateVisibleButNotCurrentDocumentsTimer.setSingleShot(true);
+    QObject::connect(&updateVisibleButNotCurrentDocumentsTimer,
+                     &QTimer::timeout,
+                     [this]() {
+        processJobsForDirtyAndVisibleButNotCurrentDocuments();
     });
 
     QObject::connect(documents.clangFileSystemWatcher(),
@@ -265,16 +272,44 @@ bool ClangCodeModelServer::isTimerRunningForTestOnly() const
 
 void ClangCodeModelServer::processJobsForDirtyAndVisibleDocuments()
 {
-    for (const auto &document : documents.documents()) {
-        if (document.isNeedingReparse() && document.isVisibleInEditor()) {
-            DocumentProcessor processor = documentProcessors().processor(document);
-            processor.addJob(createJobRequest(document,
-                                              JobRequest::Type::UpdateDocumentAnnotations,
-                                              PreferredTranslationUnit::PreviouslyParsed));
-        }
-    }
+    processJobsForDirtyCurrentDocument();
+    processTimerForVisibleButNotCurrentDocuments();
+}
 
-    documentProcessors().process();
+void ClangCodeModelServer::processJobsForDirtyCurrentDocument()
+{
+    const auto currentDirtyDocuments = documents.filtered([](const Document &document) {
+        return document.isNeedingReparse() && document.isUsedByCurrentEditor();
+    });
+    QTC_CHECK(currentDirtyDocuments.size() <= 1);
+
+    addAndRunUpdateJobs(currentDirtyDocuments);
+}
+
+void ClangCodeModelServer::addAndRunUpdateJobs(const std::vector<Document> &documents)
+{
+    for (const auto &document : documents) {
+        DocumentProcessor processor = documentProcessors().processor(document);
+        processor.addJob(createJobRequest(document,
+                                          JobRequest::Type::UpdateDocumentAnnotations,
+                                          PreferredTranslationUnit::PreviouslyParsed));
+        processor.process();
+    }
+}
+
+void ClangCodeModelServer::processTimerForVisibleButNotCurrentDocuments()
+{
+    if (documents.dirtyAndVisibleButNotCurrentDocuments().empty()) {
+        updateVisibleButNotCurrentDocumentsTimer.stop();
+    } else {
+        updateVisibleButNotCurrentDocumentsTimer.start(
+                    updateVisibleButNotCurrentDocumentsTimeOutInMs);
+    }
+}
+
+void ClangCodeModelServer::processJobsForDirtyAndVisibleButNotCurrentDocuments()
+{
+    addAndRunUpdateJobs(documents.dirtyAndVisibleButNotCurrentDocuments());
 }
 
 void ClangCodeModelServer::processInitialJobsForDocuments(const std::vector<Document> &documents)
@@ -330,6 +365,11 @@ JobRequest ClangCodeModelServer::createJobRequest(
 void ClangCodeModelServer::setUpdateDocumentAnnotationsTimeOutInMsForTestsOnly(int value)
 {
     updateDocumentAnnotationsTimeOutInMs = value;
+}
+
+void ClangCodeModelServer::setUpdateVisibleButNotCurrentDocumentsTimeOutInMsForTestsOnly(int value)
+{
+    updateVisibleButNotCurrentDocumentsTimeOutInMs = value;
 }
 
 DocumentProcessors &ClangCodeModelServer::documentProcessors()

src/tools/clangbackend/ipcsource/clangcodemodelserver.h
@@ -65,15 +65,21 @@ public: // for tests
     int queueSizeForTestsOnly();
     bool isTimerRunningForTestOnly() const;
     void setUpdateDocumentAnnotationsTimeOutInMsForTestsOnly(int value);
+    void setUpdateVisibleButNotCurrentDocumentsTimeOutInMsForTestsOnly(int value);
     DocumentProcessors &documentProcessors();
 
 private:
-
     void startDocumentAnnotationsTimerIfFileIsNotOpenAsDocument(const Utf8String &filePath);
-    void addJobRequestsForDirtyAndVisibleDocuments();
-    void processJobsForDirtyAndVisibleDocuments();
+
     void processInitialJobsForDocuments(const std::vector<Document> &documents);
     void startInitializingSupportiveTranslationUnits(const std::vector<Document> &documents);
+
+    void processJobsForDirtyAndVisibleDocuments();
+    void processJobsForDirtyCurrentDocument();
+    void processTimerForVisibleButNotCurrentDocuments();
+    void processJobsForDirtyAndVisibleButNotCurrentDocuments();
+
+    void addAndRunUpdateJobs(const std::vector<Document> &documents);
 
     JobRequest createJobRequest(const Document &document,
                                 JobRequest::Type type,
@@ -88,7 +94,10 @@ private:
     QScopedPointer<DocumentProcessors> documentProcessors_; // Delayed initialization
 
     QTimer updateDocumentAnnotationsTimer;
-    int updateDocumentAnnotationsTimeOutInMs;
+    int updateDocumentAnnotationsTimeOutInMs = 1500;
+
+    QTimer updateVisibleButNotCurrentDocumentsTimer;
+    int updateVisibleButNotCurrentDocumentsTimeOutInMs = 2000;
 };
 
 } // namespace ClangBackEnd

src/tools/clangbackend/ipcsource/clangdocuments.cpp
@@ -33,6 +33,8 @@
 #include <skippedsourceranges.h>
 #include <unsavedfiles.h>
 
+#include <utils/algorithm.h>
+
 #include <QDebug>
 
 #include <algorithm>
@@ -141,6 +143,20 @@ bool Documents::hasDocument(const Utf8String &filePath,
 const std::vector<Document> &Documents::documents() const
 {
     return documents_;
+}
+
+const std::vector<Document> Documents::filtered(const IsMatchingDocument &isMatchingDocument) const
+{
+    return Utils::filtered(documents_, isMatchingDocument);
+}
+
+std::vector<Document> Documents::dirtyAndVisibleButNotCurrentDocuments() const
+{
+    return filtered([](const Document &document) {
+        return document.isNeedingReparse()
+            && document.isVisibleInEditor()
+            && !document.isUsedByCurrentEditor();
+    });
 }
 
 UnsavedFiles Documents::unsavedFiles() const

src/tools/clangbackend/ipcsource/clangdocuments.h
@@ -32,6 +32,7 @@
 
 #include <QVector>
 
+#include <functional>
 #include <vector>
 
 namespace ClangBackEnd {
@@ -57,6 +58,9 @@ public:
     bool hasDocumentWithFilePath(const Utf8String &filePath) const;
 
     const std::vector<Document> &documents() const;
+    using IsMatchingDocument = std::function<bool(const Document &document)>;
+    const std::vector<Document> filtered(const IsMatchingDocument &isMatchingDocument) const;
+    std::vector<Document> dirtyAndVisibleButNotCurrentDocuments() const;
 
     UnsavedFiles unsavedFiles() const;
 

tests/unit/unittest/clangdocuments-test.cpp
@@ -289,6 +289,42 @@ TEST_F(Documents, HasNotDocument)
     ASSERT_FALSE(documents.hasDocument(filePath, projectPartId));
 }
 
+TEST_F(Documents, FilteredPositive)
+{
+    documents.create({{filePath, projectPartId}});
+    const auto isMatchingFilePath = [this](const Document &document) {
+        return document.filePath() == filePath;
+    };
+
+    const bool hasMatches = !documents.filtered(isMatchingFilePath).empty();
+
+    ASSERT_TRUE(hasMatches);
+}
+
+TEST_F(Documents, FilteredNegative)
+{
+    documents.create({{filePath, projectPartId}});
+    const auto isMatchingNothing = [](const Document &) {
+        return false;
+    };
+
+    const bool hasMatches = !documents.filtered(isMatchingNothing).empty();
+
+    ASSERT_FALSE(hasMatches);
+}
+
+TEST_F(Documents, DirtyAndVisibleButNotCurrentDocuments)
+{
+    documents.create({{filePath, projectPartId}});
+    documents.updateDocumentsWithChangedDependency(filePath);
+    documents.setVisibleInEditors({filePath});
+    documents.setUsedByCurrentEditor(Utf8String());
+
+    const bool hasMatches = !documents.dirtyAndVisibleButNotCurrentDocuments().empty();
+
+    ASSERT_TRUE(hasMatches);
+}
+
 TEST_F(Documents, isUsedByCurrentEditor)
 {
     documents.create({fileContainer});

tests/unit/unittest/clangipcserver-test.cpp
@@ -375,6 +375,7 @@ void ClangClangCodeModelServer::SetUp()
 {
     clangServer.setClient(&mockClangCodeModelClient);
     clangServer.setUpdateDocumentAnnotationsTimeOutInMsForTestsOnly(0);
+    clangServer.setUpdateVisibleButNotCurrentDocumentsTimeOutInMsForTestsOnly(0);
 }
 
 void ClangClangCodeModelServer::TearDown()