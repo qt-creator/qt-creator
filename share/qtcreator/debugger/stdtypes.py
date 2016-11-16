0334438b400d81e28bd5bc8730b06cbaf97003b3
hjk <hjk@qt.io>
2016-11-16 17:37:42 +0100

    Debugger: Use more direct access in std::deque dumper
    This effectively fixes the LLDB dumper for std::deque and std::stack by
    avoiding a code path in which LLDB returns wrong data (the type and
    offset for the _Deque_iterator::_M_first member - surprisingly the
    other three members there get reported correctly).
    
    Change-Id: Iae1ba8b81987442ed2d486b383a145a77ce49cd7
    Reviewed-by: David Schulz <david.schulz@qt.io>
    Reviewed-by: hjk <hjk@qt.io>

--- share/qtcreator/debugger/stdtypes.py
+++ share/qtcreator/debugger/stdtypes.py
@@ -64,44 +64,31 @@ def qdump__std__deque(d, value):
     innerSize = innerType.size()
     bufsize = 1
     if innerSize < 512:
-        bufsize = int(512 / innerSize)
+        bufsize = 512 // innerSize
 
-    #(mapptr, mapsize, startCur, startFirst, startLast, startNode,
-    #          finishCur, finishFirst, finishLast, finishNode) = d.split("pppppppppp", value)
-#
-#    numInBuf = bufsize * (int((finishNode - startNode) / innerSize) - 1)
-#    numFirstItems  = int((startLast - startCur) / innerSize)
-#    numLastItems = int((finishCur - finishFirst) / innerSize)
+    (mapptr, mapsize, startCur, startFirst, startLast, startNode,
+              finishCur, finishFirst, finishLast, finishNode) = value.split("pppppppppp")
 
-    impl = value["_M_impl"]
-    start = impl["_M_start"]
-    finish = impl["_M_finish"]
-    size = bufsize * ((finish["_M_node"].pointer() - start["_M_node"].pointer()) // d.ptrSize() - 1)
-    size += ((finish["_M_cur"].pointer() - finish["_M_first"].pointer()) // innerSize)
-    size += ((start["_M_last"].pointer() - start["_M_cur"].pointer()) // innerSize)
+    size = bufsize * ((finishNode - startNode) // d.ptrSize() - 1)
+    size += (finishCur - finishFirst) // innerSize
+    size += (startLast - startCur) // innerSize
 
     d.check(0 <= size and size <= 1000 * 1000 * 1000)
     d.putItemCount(size)
     if d.isExpanded():
         with Children(d, size, maxNumChild=2000, childType=innerType):
-            pcur = start["_M_cur"].pointer()
-            pfirst = start["_M_first"]
-            plast = start["_M_last"].pointer()
-            pnode = start["_M_node"]
+            pcur = startCur
+            plast = startLast
+            pnode = startNode
             for i in d.childRange():
                 d.putSubItem(i, d.createValue(pcur, innerType))
                 pcur += innerSize
                 if pcur == plast:
-                    # FIXME: Remove pointer operation.
-                    newnode = pnode + 1 # Type is std::_Deque_iterator<Foo, Foo&, Foo*>::_Map_pointer\"} a.k.a 'Foo **'
-                    #warn("TYPE: %s" % pnode.type)
-                    #warn("PNODE: 0x%x %s" % (pnode.pointer(), pnode))
-                    #warn("NEWNODE: 0x%x %s" % (newnode.pointer(), newnode))
-                    pnode = newnode
-                    #warn("PNODE 2: 0x%x %s" % (pnode.pointer(), pnode))
-                    pfirst = newnode.dereference().pointer()
+                    newnode = pnode + d.ptrSize()
+                    pfirst = d.extractPointer(newnode)
                     plast = pfirst + bufsize * d.ptrSize()
                     pcur = pfirst
+                    pnode = newnode
 
 def qdump__std__deque__QNX(d, value):
     innerType = value.type[0]
