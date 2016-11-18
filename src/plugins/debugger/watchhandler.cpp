qt-creator/qt-creator

commit d771ba89211c57abc9f6b94a4047edd43271dd1d

Debugger: Don't clear unrelated views when updating locals
If the list of partial expressions is empty we need to tell the watch
handler that we are only updating the locals view, not e.g. the
inspector view.

Change-Id: Iedc74ffc66a8435faa272d053849b831f6b8cbbe
Task-number: QTCREATORBUG-16692
Reviewed-by: David Schulz <david.schulz@qt.io>
Reviewed-by: hjk <hjk@qt.io>

src/plugins/debugger/watchhandler.cpp
@@ -1999,8 +1999,12 @@ void WatchHandler::resetWatchers()
     loadSessionData();
 }
 
-void WatchHandler::notifyUpdateStarted(const QStringList &inames)
+void WatchHandler::notifyUpdateStarted(const UpdateParameters &updateParameters)
 {
+    QStringList inames = updateParameters.partialVariables();
+    if (inames.isEmpty())
+        inames.append("local");
+
     auto marker = [](WatchItem *item) { item->outdated = true; };
 
     if (inames.isEmpty()) {

src/plugins/debugger/cdb/cdbengine.cpp
@@ -1217,7 +1217,7 @@ void CdbEngine::activateFrame(int index)
 void CdbEngine::doUpdateLocals(const UpdateParameters &updateParameters)
 {
     if (m_pythonVersion > 0x030000) {
-        watchHandler()->notifyUpdateStarted(updateParameters.partialVariables());
+        watchHandler()->notifyUpdateStarted(updateParameters);
 
         DebuggerCommand cmd("theDumper.fetchVariables", ScriptCommand);
         watchHandler()->appendFormatRequests(&cmd);
@@ -1270,7 +1270,7 @@ void CdbEngine::doUpdateLocals(const UpdateParameters &updateParameters)
             return;
         }
 
-        watchHandler()->notifyUpdateStarted(updateParameters.partialVariables());
+        watchHandler()->notifyUpdateStarted(updateParameters);
 
         /* Watchers: Forcibly discard old symbol group as switching from
      * thread 0/frame 0 -> thread 1/assembly -> thread 0/frame 0 will otherwise re-use it

src/plugins/debugger/debuggerengine.cpp
@@ -2061,7 +2061,7 @@ void DebuggerEngine::updateItem(const QString &iname)
         WatchModelBase *model = handler->model();
         QTC_CHECK(model);
         if (item && !model->hasChildren(model->indexForItem(item))) {
-            handler->notifyUpdateStarted({iname});
+            handler->notifyUpdateStarted(UpdateParameters(iname));
             item->setValue(decodeData({}, "notaccessible"));
             item->setHasChildren(false);
             item->outdated = false;

src/plugins/debugger/debuggerengine.h
@@ -122,7 +122,8 @@ public:
 class UpdateParameters
 {
 public:
-    UpdateParameters() {}
+    UpdateParameters(const QString &partialVariable = QString()) :
+        partialVariable(partialVariable) {}
 
     QStringList partialVariables() const
     {

src/plugins/debugger/gdb/gdbengine.cpp
@@ -4493,7 +4493,7 @@ void GdbEngine::doUpdateLocals(const UpdateParameters &params)
 {
     m_pendingBreakpointRequests = 0;
 
-    watchHandler()->notifyUpdateStarted(params.partialVariables());
+    watchHandler()->notifyUpdateStarted(params);
 
     DebuggerCommand cmd("fetchVariables", Discardable|InUpdateLocals|PythonCommand);
     watchHandler()->appendFormatRequests(&cmd);

src/plugins/debugger/lldb/lldbengine.cpp
@@ -796,7 +796,7 @@ void LldbEngine::assignValueInDebugger(WatchItem *,
 
 void LldbEngine::doUpdateLocals(const UpdateParameters &params)
 {
-    watchHandler()->notifyUpdateStarted(params.partialVariables());
+    watchHandler()->notifyUpdateStarted(params);
 
     DebuggerCommand cmd("fetchVariables");
     watchHandler()->appendFormatRequests(&cmd);

src/plugins/debugger/qml/qmlengine.cpp
@@ -2156,7 +2156,7 @@ void QmlEnginePrivate::handleFrame(const QVariantMap &response)
 
     StackHandler *stackHandler = engine->stackHandler();
     WatchHandler * watchHandler = engine->watchHandler();
-    watchHandler->notifyUpdateStarted({"local"});
+    watchHandler->notifyUpdateStarted();
 
     const int frameIndex = stackHandler->currentIndex();
     if (frameIndex < 0)

src/plugins/debugger/watchhandler.h
@@ -26,6 +26,7 @@
 #pragma once
 
 #include "watchdata.h"
+#include "debuggerengine.h"
 
 #include <QVector>
 
@@ -111,7 +112,7 @@ public:
     void resetValueCache();
     void resetWatchers();
 
-    void notifyUpdateStarted(const QStringList &inames = {});
+    void notifyUpdateStarted(const UpdateParameters &updateParameters = UpdateParameters());
     void notifyUpdateFinished();
 
     void reexpandItems();

commit a8697e4716ba6d51b6a842d2ce192101a150c8ec

Debugger: Don't accumulate return value displays
This fixes a regression introduced with d771ba89.

Change-Id: Icc44ef9ad41f9e5cdb3848fae7a1ba8af2626344
Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

src/plugins/debugger/watchhandler.cpp
@@ -2004,7 +2004,7 @@ void WatchHandler::notifyUpdateStarted(const UpdateParameters &updateParameters)
 {
     QStringList inames = updateParameters.partialVariables();
     if (inames.isEmpty())
-        inames.append("local");
+        inames = QStringList({ "local", "return" });
 
     auto marker = [](WatchItem *item) { item->outdated = true; };
 