qt-creator/qt-creator

commit 033862f3057818df3123d07fc6cb34e193250c53

Themed Icons: Introduce Utils::Icon
Instead of describing icons via file name or in the themed icons case
via
a string that is a list of mask/color pairs, we have now a class for it.

Icons are now listed in per-plugin *icons.h headers.

RunControl::m_icon was The only place left where an icon property was in
fact a string. This patch changes that member to be a Utils::Icon.

Change-Id: Ibcfa8bb25e6d2e330c567ee7ccc0b97ead603177
Reviewed-by: Eike Ziller <eike.ziller@theqtcompany.com>

src/plugins/diffeditor/diffview.cpp
@@ -30,11 +30,11 @@
 
 #include "diffview.h"
 
+#include "diffeditoricons.h"
 #include "unifieddiffeditorwidget.h"
 #include "sidebysidediffeditorwidget.h"
 
 #include <utils/qtcassert.h>
-#include <utils/themehelper.h>
 
 #include <QCoreApplication>
 
@@ -97,8 +97,7 @@ void IDiffView::setSyncToolTip(const QString &text)
 UnifiedView::UnifiedView() : m_widget(0)
 {
     setId(UNIFIED_VIEW_ID);
-    setIcon(Utils::ThemeHelper::themedIcon(
-                QLatin1String(":/diffeditor/images/unifieddiff.png")));
+    setIcon(Icons::UNIFIED_DIFF.icon());
     setToolTip(QCoreApplication::translate("DiffEditor::UnifiedView", "Switch to Unified Diff Editor"));
 }
 
@@ -154,8 +153,7 @@ void UnifiedView::setSync(bool sync)
 SideBySideView::SideBySideView() : m_widget(0)
 {
     setId(SIDE_BY_SIDE_VIEW_ID);
-    setIcon(Utils::ThemeHelper::themedIcon(
-                QLatin1String(":/diffeditor/images/sidebysidediff.png")));
+    setIcon(Icons::UNIFIED_DIFF.icon());
     setToolTip(QCoreApplication::translate("DiffEditor::SideBySideView",
                                            "Switch to Side By Side Diff Editor"));
     setSupportsSync(true);

commit f778880a3b5ac5aaae40e77fb2543fb64afea149

Fix SideBySide diff editor icon
Broken in 033862f3057818df3123d07fc6cb34e193250c53.

Change-Id: I8930de03b02dcf2859b11218b25fa0099e957791
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/diffeditor/diffview.cpp
@@ -148,7 +148,7 @@ void UnifiedView::setSync(bool sync)
 SideBySideView::SideBySideView() : m_widget(0)
 {
     setId(SIDE_BY_SIDE_VIEW_ID);
-    setIcon(Icons::UNIFIED_DIFF.icon());
+    setIcon(Icons::SIDEBYSIDE_DIFF.icon());
     setToolTip(QCoreApplication::translate("DiffEditor::SideBySideView",
                                            "Switch to Side By Side Diff Editor"));
     setSupportsSync(true);