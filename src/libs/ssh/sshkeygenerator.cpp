qt-creator/qt-creator

commit cc04b84917ef0f90e2017bf652b7327ce5edda9c

Fix compiler warnings on windows.
Ignore some warnings inside 3rd party code and fix a lot of conversion
warnings.

Change-Id: I909f2f31a4639015bf7dd028d2d435ff1d1167bc
Reviewed-by: Christian Kandeler <christian.kandeler@theqtcompany.com>
Reviewed-by: Eike Ziller <eike.ziller@qt.io>
Reviewed-by: Christian Stenger <christian.stenger@theqtcompany.com>
Reviewed-by: David Schulz <david.schulz@theqtcompany.com>

src/libs/ssh/sshkeygenerator.cpp
@@ -115,7 +115,7 @@ void SshKeyGenerator::generatePkcs8KeyString(const KeyPtr &key, bool privateKey,
         keyData = &m_publicKey;
     }
     pipe.end_msg();
-    keyData->resize(pipe.remaining(pipe.message_count() - 1));
+    keyData->resize(static_cast<int>(pipe.remaining(pipe.message_count()) - 1));
     pipe.read(convertByteArray(*keyData), keyData->size(),
         pipe.message_count() - 1);
 }
@@ -147,7 +147,8 @@ void SshKeyGenerator::generateOpenSslPublicKeyString(const KeyPtr &key)
     case Ecdsa: {
         const auto ecdsaKey = key.dynamicCast<ECDSA_PrivateKey>();
         q = convertByteArray(EC2OSP(ecdsaKey->public_point(), PointGFp::UNCOMPRESSED));
-        keyId = SshCapabilities::ecdsaPubKeyAlgoForKeyWidth(ecdsaKey->private_value().bytes());
+        keyId = SshCapabilities::ecdsaPubKeyAlgoForKeyWidth(
+                    static_cast<int>(ecdsaKey->private_value().bytes()));
         break;
     }
     }

src/libs/3rdparty/botan/botan.pri
@@ -36,7 +36,7 @@ win32 {
 
     win32-msvc* {
         QMAKE_CXXFLAGS_EXCEPTIONS_ON = -EHs
-        QMAKE_CXXFLAGS += -wd4251 -wd4290 -wd4250
+        QMAKE_CXXFLAGS += -wd4251 -wd4290 -wd4250 -wd4297 -wd4267 -wd4334
         DEFINES += BOTAN_BUILD_COMPILER_IS_MSVC BOTAN_TARGET_OS_HAS_GMTIME_S _SCL_SECURE_NO_WARNINGS
     } else {
         QMAKE_CFLAGS += -fpermissive -finline-functions -Wno-long-long

src/libs/clangbackendipc/connectionserver.cpp
@@ -70,7 +70,7 @@ void ConnectionServer::setIpcServer(IpcServerInterface *ipcServer)
 
 int ConnectionServer::clientProxyCount() const
 {
-    return ipcClientProxies.size();
+    return static_cast<int>(ipcClientProxies.size());
 }
 
 void ConnectionServer::timerEvent(QTimerEvent *timerEvent)

src/libs/glsl/glslparser.cpp
@@ -58,30 +58,30 @@ Parser::Parser(Engine *engine, const char *source, unsigned size, int variant)
 
         switch (tk.kind) {
         case T_LEFT_PAREN:
-            parenStack.push(_tokens.size());
+            parenStack.push(static_cast<int>(_tokens.size()));
             break;
         case T_LEFT_BRACKET:
-            bracketStack.push(_tokens.size());
+            bracketStack.push(static_cast<int>(_tokens.size()));
             break;
         case T_LEFT_BRACE:
-            braceStack.push(_tokens.size());
+            braceStack.push(static_cast<int>(_tokens.size()));
             break;
 
         case T_RIGHT_PAREN:
             if (! parenStack.empty()) {
-                _tokens[parenStack.top()].matchingBrace = _tokens.size();
+                _tokens[parenStack.top()].matchingBrace = static_cast<int>(_tokens.size());
                 parenStack.pop();
             }
             break;
         case T_RIGHT_BRACKET:
             if (! bracketStack.empty()) {
-                _tokens[bracketStack.top()].matchingBrace = _tokens.size();
+                _tokens[bracketStack.top()].matchingBrace = static_cast<int>(_tokens.size());
                 bracketStack.pop();
             }
             break;
         case T_RIGHT_BRACE:
             if (! braceStack.empty()) {
-                _tokens[braceStack.top()].matchingBrace = _tokens.size();
+                _tokens[braceStack.top()].matchingBrace = static_cast<int>(_tokens.size());
                 braceStack.pop();
             }
             break;

src/libs/glsl/glslparser.h
@@ -114,7 +114,7 @@ private:
     inline int consumeToken() {
         if (_index < int(_tokens.size()))
             return _index++;
-        return _tokens.size() - 1;
+        return static_cast<int>(_tokens.size()) - 1;
     }
     inline const Token &tokenAt(int index) const {
         if (index == 0)

src/libs/qtcreatorcdbext/common.h
@@ -34,7 +34,9 @@
 
 #include <windows.h>
 #define KDEXT_64BIT
+#pragma warning( disable : 4838  )
 #include <wdbgexts.h>
+#pragma warning( default : 4838  )
 #include <dbgeng.h>
 
 typedef IDebugControl3 CIDebugControl;

src/libs/ssh/sshbotanconversions_p.h
@@ -45,7 +45,7 @@ inline Botan::byte *convertByteArray(QByteArray &a)
 
 inline QByteArray convertByteArray(const Botan::SecureVector<Botan::byte> &v)
 {
-    return QByteArray(reinterpret_cast<const char *>(v.begin()), v.size());
+    return QByteArray(reinterpret_cast<const char *>(v.begin()), static_cast<int>(v.size()));
 }
 
 inline const char *botanKeyExchangeAlgoName(const QByteArray &rfcAlgoName)

src/libs/ssh/sshcryptofacility.cpp
@@ -82,11 +82,11 @@ void SshAbstractCryptoFacility::recreateKeys(const SshKeyExchange &kex)
     BlockCipher * const cipher
             = af.prototype_block_cipher(botanCryptAlgoName(rfcCryptAlgoName))->clone();
 
-    m_cipherBlockSize = cipher->block_size();
+    m_cipherBlockSize = static_cast<quint32>(cipher->block_size());
     const QByteArray ivData = generateHash(kex, ivChar(), m_cipherBlockSize);
     const InitializationVector iv(convertByteArray(ivData), m_cipherBlockSize);
 
-    const quint32 keySize = cipher->key_spec().maximum_keylength();
+    const quint32 keySize = static_cast<quint32>(cipher->key_spec().maximum_keylength());
     const QByteArray cryptKeyData = generateHash(kex, keyChar(), keySize);
     SymmetricKey cryptKey(convertByteArray(cryptKeyData), keySize);
     Keyed_Filter * const cipherMode
@@ -118,8 +118,9 @@ void SshAbstractCryptoFacility::convert(QByteArray &data, quint32 offset,
     }
     m_pipe->process_msg(reinterpret_cast<const byte *>(data.constData()) + offset,
         dataSize);
-    quint32 bytesRead = m_pipe->read(reinterpret_cast<byte *>(data.data()) + offset,
-        dataSize, m_pipe->message_count() - 1); // Can't use Pipe::LAST_MESSAGE because of a VC bug.
+     // Can't use Pipe::LAST_MESSAGE because of a VC bug.
+    quint32 bytesRead = static_cast<quint32>(m_pipe->read(
+          reinterpret_cast<byte *>(data.data()) + offset, dataSize, m_pipe->message_count() - 1));
     if (bytesRead != dataSize) {
         throw SshClientException(SshInternalError,
                 QLatin1String("Internal error: Botan::Pipe::read() returned unexpected value"));
@@ -261,7 +262,8 @@ bool SshEncryptionFacility::createAuthenticationKeyFromPKCS8(const QByteArray &p
                          << rsaKey->get_d();
         } else if (auto * const ecdsaKey = dynamic_cast<ECDSA_PrivateKey *>(m_authKey.data())) {
             const BigInt value = ecdsaKey->private_value();
-            m_authKeyAlgoName = SshCapabilities::ecdsaPubKeyAlgoForKeyWidth(value.bytes());
+            m_authKeyAlgoName = SshCapabilities::ecdsaPubKeyAlgoForKeyWidth(
+                        static_cast<int>(value.bytes()));
             pubKeyParams << ecdsaKey->public_point().get_affine_x()
                          << ecdsaKey->public_point().get_affine_y();
             allKeyParams << pubKeyParams << value;
@@ -346,7 +348,8 @@ bool SshEncryptionFacility::createAuthenticationKeyFromOpenSSL(const QByteArray 
         } else {
             BigInt privKey;
             sequence.decode_octet_string_bigint(privKey);
-            m_authKeyAlgoName = SshCapabilities::ecdsaPubKeyAlgoForKeyWidth(privKey.bytes());
+            m_authKeyAlgoName = SshCapabilities::ecdsaPubKeyAlgoForKeyWidth(
+                        static_cast<int>(privKey.bytes()));
             const EC_Group group(SshCapabilities::oid(m_authKeyAlgoName));
             auto * const key = new ECDSA_PrivateKey(m_rng, group, privKey);
             m_authKey.reset(key);

commit d5dd9c080867bcc45c6efd2a8aebf7ab5147e59f

SSH: Fix typo in key generator that leads to a crash
Introduced in cc04b849.

Task-number: QTCREATORBUG-17349
Change-Id: Ib392ebbffa3d3bb94890f1a55e7288859709b046
Reviewed-by: David Schulz <david.schulz@qt.io>

src/libs/ssh/sshkeygenerator.cpp
@@ -115,7 +115,7 @@ void SshKeyGenerator::generatePkcs8KeyString(const KeyPtr &key, bool privateKey,
         keyData = &m_publicKey;
     }
     pipe.end_msg();
-    keyData->resize(static_cast<int>(pipe.remaining(pipe.message_count()) - 1));
+    keyData->resize(static_cast<int>(pipe.remaining(pipe.message_count() - 1)));
     pipe.read(convertByteArray(*keyData), keyData->size(),
         pipe.message_count() - 1);
 }