41ceac474b7089bfd4257bf4d12a7d68a6566d0d
Christian Stenger <christian.stenger@qt.io>
2016-12-19 13:54:53 +0100

    AutoTest: Avoid building tree of global items
    Global items normally have no test name set. This patch avoids
    adding global items below other global items.
    
    Change-Id: Iabada78144ec48fc6687b360f79f1ef017c772a1
    Reviewed-by: David Schulz <david.schulz@qt.io>

--- src/plugins/autotest/testresultmodel.cpp
+++ src/plugins/autotest/testresultmodel.cpp
@@ -292,7 +292,7 @@ TestResultItem *TestResultModel::findParentItemFor(const TestResultItem *item,
     const TestResult *result = item->testResult();
     const QString &name = result->name();
 
-    if (root == nullptr) {
+    if (root == nullptr && !name.isEmpty()) {
         for (int row = rootItem()->childCount() - 1; row >= 0; --row) {
             TestResultItem *tmp = static_cast<TestResultItem *>(rootItem()->childAt(row));
             if (tmp->testResult()->name() == name) {
