qt-creator/qt-creator

commit 696b2d426163450a2a554624b2121e07281a76d7

Debugger: Fix extremely slow enum name resolving
Task-number: QTCREATORBUG-17598
Change-Id: Ib32586c0513ce058301c1187e263c17b0a716f94
Reviewed-by: hjk <hjk@qt.io>

share/qtcreator/debugger/gdbbridge.py
@@ -379,10 +379,12 @@ class Dumper(DumperBase):
 
     def nativeTypeEnumDisplay(self, nativeType, intval):
         try:
-            val = gdb.parse_and_eval('(%s)%d' % (nativeType, intval))
-            return  '%s (%d)' % (val, intval)
+            for field in nativeType.fields():
+                if field.enumval == intval:
+                    return '%s (%d)' % (field.name, intval)
         except:
-            return '%d' % intval
+            pass
+        return '%d' % intval
 
     def nativeTypeId(self, nativeType):
         name = str(nativeType)

commit 0c9f23db96f53420d42d01ae8f8d6b549dce7587

GDB: Fix printing of flag enums
It worked with plain GDB (parse_and_eval), and was broken by
696b2d426163450a2a554624b2121e07281a76d7.

Change-Id: Ibc143efdbd90681a2f9f4d84b75063eb98078f2b
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

share/qtcreator/debugger/gdbbridge.py
@@ -379,9 +379,27 @@ class Dumper(DumperBase):
 
     def nativeTypeEnumDisplay(self, nativeType, intval):
         try:
+            enumerators = []
             for field in nativeType.fields():
+                # If we found an exact match, return it immediately
                 if field.enumval == intval:
                     return '%s (%d)' % (field.name, intval)
+                enumerators.append((field.name, field.enumval))
+
+            # No match was found, try to return as flags
+            enumerators.sort(key = lambda x: x[1])
+            flags = []
+            v = intval
+            found = False
+            for (name, value) in enumerators:
+                if v & value != 0:
+                    flags.append(name)
+                    v = v & ~value
+                    found = True
+            if not found or v != 0:
+                # Leftover value
+                flags.append('unknown:%d' % v)
+            return "(%s) (%d)" % (" | ".join(flags), intval)
         except:
             pass
         return '%d' % intval

tests/auto/debugger/tst_dumpers.cpp
@@ -5094,6 +5094,20 @@ void tst_Dumpers::dumper_data()
                 + Check("fd", "d (2)", "Foo");
 
 
+    QTest::newRow("EnumFlags")
+            << Data("\n"
+                    "enum Flags { one = 1, two = 2, four = 4 };\n",
+                    "Flags fone = one; unused(&fone);\n"
+                    "Flags fthree = (Flags)(one|two); unused(&fthree);\n"
+                    "Flags fmixed = (Flags)(two|8); unused(&fmixed);\n"
+                    "Flags fbad = (Flags)(24); unused(&fbad);\n")
+               + GdbEngine
+               + Check("fone", "one (1)", "Flags")
+               + Check("fthree", "(one | two) (3)", "Flags")
+               + Check("fmixed", "(two | unknown:8) (10)", "Flags")
+               + Check("fbad", "(unknown:24) (24)", "Flags");
+
+
     QTest::newRow("Array")
             << Data("double a1[3][3];\n"
                     "for (int i = 0; i != 3; ++i)\n"