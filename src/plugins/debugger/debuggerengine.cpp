qt-creator/qt-creator

commit f8ad72deb318f36e87d6862800d13dbfc4d1f78b

Debugger: Report PID back to RunControl
Change-Id: I4830d2703de86c3fce8356271a2262c14c8f1307
Reviewed-by: hjk <hjk@qt.io>

src/plugins/debugger/debuggerengine.cpp
@@ -1434,6 +1434,7 @@ void DebuggerEngine::notifyInferiorPid(qint64 pid)
         return;
     d->m_inferiorPid = pid;
     if (pid) {
+        d->m_runControl->setApplicationProcessHandle(ProcessHandle(pid));
         showMessage(tr("Taking notice of pid %1").arg(pid));
         if (d->m_runParameters.startMode == StartInternal
             || d->m_runParameters.startMode == StartExternal

commit 7c29ae462be583fa4a1ce31183ab728372b8f6ef

Debugger: Fix crash in combined debugging
Fixes regression introduced in f8ad72deb3: Slave engines don't
have a RunControl pointer of their own, use the master pointer.

Change-Id: I1c695a1454cafc3532fb1b79374fac4f3ead607e
Reviewed-by: hjk <hjk@qt.io>

src/plugins/debugger/debuggerengine.cpp
@@ -1434,7 +1434,7 @@ void DebuggerEngine::notifyInferiorPid(qint64 pid)
         return;
     d->m_inferiorPid = pid;
     if (pid) {
-        d->m_runControl->setApplicationProcessHandle(ProcessHandle(pid));
+        runControl()->setApplicationProcessHandle(ProcessHandle(pid));
         showMessage(tr("Taking notice of pid %1").arg(pid));
         if (d->m_runParameters.startMode == StartInternal
             || d->m_runParameters.startMode == StartExternal