a7a29fa6d6341ac6afd8e8b49f7c24fa06477fd1
Thomas Hartmann <thomas.hartmann@qt.io>
2017-03-10 15:16:35 +0100

    QmlDesigner: Fix translation functions in QmlObjectNode
    Change-Id: Icea185a508319f7f2f7d7d11e41c73043d5b56d0
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- src/plugins/qmldesigner/designercore/model/qmlobjectnode.cpp
+++ src/plugins/qmldesigner/designercore/model/qmlobjectnode.cpp
@@ -189,7 +189,7 @@ bool QmlObjectNode::isTranslatableText(const PropertyName &name) const
     if (modelNode().metaInfo().isValid() && modelNode().metaInfo().hasProperty(name))
         if (modelNode().metaInfo().propertyTypeName(name) == "QString" || modelNode().metaInfo().propertyTypeName(name) == "string") {
             if (modelNode().hasBindingProperty(name)) {
-                static QRegExp regularExpressionPatter(QLatin1String("qsTr(?:|Id)\\((\".*\")\\)"));
+                static QRegExp regularExpressionPatter(QLatin1String("qsTr(|Id|anslate)\\(\".*\"\\)"));
                 return regularExpressionPatter.exactMatch(modelNode().bindingProperty(name).expression());
             }
 
@@ -202,9 +202,10 @@ bool QmlObjectNode::isTranslatableText(const PropertyName &name) const
 QString QmlObjectNode::stripedTranslatableText(const PropertyName &name) const
 {
     if (modelNode().hasBindingProperty(name)) {
-        static QRegExp regularExpressionPatter(QLatin1String("qsTr(?:|Id)\\(\"(.*)\"\\)"));
+        static QRegExp regularExpressionPatter(QLatin1String("qsTr(|Id|anslate)\\(\"(.*)\"\\)"));
         if (regularExpressionPatter.exactMatch(modelNode().bindingProperty(name).expression()))
-            return regularExpressionPatter.cap(1);
+            return regularExpressionPatter.cap(2);
+        return instanceValue(name).toString();
     } else {
         return modelNode().variantProperty(name).value().toString();
     }
