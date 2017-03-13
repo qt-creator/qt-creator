qt-creator/qt-creator

commit b0ac6435b3786854e7a8d10cf4d8df49ca74618a

Git: Add new class GitRemote
Allows to split a remote URL and performs
some validation checks on the elements.

Change-Id: I048373076b1a1553fdd7bed2986a41cc087138b0
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/git/gerrit/gerritserver.cpp
@@ -113,39 +113,27 @@ QString GerritServer::url(UrlType urlType) const
 
 bool GerritServer::fillFromRemote(const QString &remote, const GerritParameters &parameters)
 {
-    static const QRegularExpression remotePattern(
-                "^(?:(?<protocol>[^:]+)://)?(?:(?<user>[^@]+)@)?(?<host>[^:/]+)"
-                "(?::(?<port>\\d+))?:?(?<path>/.*)$");
+    const GitRemote r(remote);
+    if (!r.isValid)
+        return false;
 
-    // Skip local remotes (refer to the root or relative path)
-    if (remote.isEmpty() || remote.startsWith('/') || remote.startsWith('.'))
-        return false;
-    // On Windows, local paths typically starts with <drive>:
-    if (HostOsInfo::isWindowsHost() && remote[1] == ':')
-        return false;
-    QRegularExpressionMatch match = remotePattern.match(remote);
-    if (!match.hasMatch())
-        return false;
-    const QString protocol = match.captured("protocol");
-    if (protocol == "https")
+    if (r.protocol == "https")
         type = GerritServer::Https;
-    else if (protocol == "http")
+    else if (r.protocol == "http")
         type = GerritServer::Http;
-    else if (protocol.isEmpty() || protocol == "ssh")
+    else if (r.protocol.isEmpty() || r.protocol == "ssh")
         type = GerritServer::Ssh;
     else
         return false;
-    const QString userName = match.captured("user");
-    user.userName = userName.isEmpty() ? parameters.server.user.userName : userName;
-    host = match.captured("host");
-    port = match.captured("port").toUShort();
-    if (host.contains("github.com")) // Clearly not gerrit
+
+    user.userName = r.userName.isEmpty() ? parameters.server.user.userName : r.userName;
+    if (r.host.contains("github.com")) // Clearly not gerrit
         return false;
     if (type != GerritServer::Ssh) {
         curlBinary = parameters.curl;
         if (curlBinary.isEmpty() || !QFile::exists(curlBinary))
             return false;
-        rootPath = match.captured("path");
+        rootPath = r.path;
         // Strip the last part of the path, which is always the repo name
         // The rest of the path needs to be inspected to find the root path
         // (can be http://example.net/review)

src/plugins/git/gitclient.cpp
@@ -3191,6 +3191,39 @@ void GitClient::StashInfo::end()
     m_stashResult = NotStashed;
 }
 
+// GitRemote
+
+GitRemote::GitRemote(const QString &url)
+{
+    static const QRegularExpression remotePattern(
+                "^(?:(?<protocol>[^:]+)://)?(?:(?<user>[^@]+)@)?(?<host>[^:/]+)"
+                "(?::(?<port>\\d+))?:?(?<path>/.*)$");
+
+    if (url.isEmpty())
+        return;
+
+    // Check for local remotes (refer to the root or relative path)
+    // On Windows, local paths typically starts with <drive>:
+    if (url.startsWith('/') || url.startsWith('.')
+            || (HostOsInfo::isWindowsHost() && url[1] == ':')) {
+        protocol = "file";
+        path = QDir::fromNativeSeparators(url);
+        isValid = QDir(path).exists() || QDir(path + ".git").exists();
+        return;
+    }
+
+    const QRegularExpressionMatch match = remotePattern.match(url);
+    if (!match.hasMatch())
+        return;
+
+    protocol = match.captured("protocol");
+    userName = match.captured("user");
+    host     = match.captured("host");
+    port     = match.captured("port").toUShort();
+    path     = match.captured("path");
+    isValid  = true;
+}
+
 } // namespace Internal
 } // namespace Git
 

src/plugins/git/gitclient.h
@@ -380,5 +380,17 @@ private:
     QFutureSynchronizer<void> m_synchronizer; // for commit updates
 };
 
+class GitRemote {
+public:
+    GitRemote(const QString &url);
+
+    QString protocol;
+    QString userName;
+    QString host;
+    QString path;
+    quint16 port = 0;
+    bool    isValid = false;
+};
+
 } // namespace Internal
 } // namespace Git

commit f71971323e17e6aba686ef039e81e026b836ae78

Gerrit: Fix host/port resolving
Broke by b0ac6435b3786854e7a8d10cf4d8df49ca74618a.

Change-Id: I66f9c598b5e7466b93857cb19c40d1d6c796793a
Reviewed-by: André Hartmann <aha_1980@gmx.de>

src/plugins/git/gerrit/gerritserver.cpp
@@ -126,9 +126,11 @@ bool GerritServer::fillFromRemote(const QString &remote, const GerritParameters 
     else
         return false;
 
-    user.userName = r.userName.isEmpty() ? parameters.server.user.userName : r.userName;
     if (r.host.contains("github.com")) // Clearly not gerrit
         return false;
+    host = r.host;
+    port = r.port;
+    user.userName = r.userName.isEmpty() ? parameters.server.user.userName : r.userName;
     if (type != GerritServer::Ssh) {
         curlBinary = parameters.curl;
         if (curlBinary.isEmpty() || !QFile::exists(curlBinary))