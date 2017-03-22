9c12e0b7dc3d1ddc5dbf9569922e746d5ed79937
Thomas Hartmann <thomas.hartmann@qt.io>
2017-03-22 14:19:54 +0100

    QmlDesigner: Fix warning
    Change-Id: Idc3c8340b7eabef74d70bc0158307e464d5413ec
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- src/plugins/qmldesigner/designercore/model/modeltotextmerger.cpp
+++ src/plugins/qmldesigner/designercore/model/modeltotextmerger.cpp
@@ -113,7 +113,7 @@ void ModelToTextMerger::propertiesChanged(const QList<AbstractProperty>& propert
             break;
 
         default:
-            Q_ASSERT(!"Unknown PropertyChangeFlags");
+            Q_ASSERT(false); //Unknown PropertyChange flag
         }
     }
 }
@@ -178,7 +178,7 @@ void ModelToTextMerger::nodeReparented(const ModelNode &node, const NodeAbstract
             break;
 
         default:
-            Q_ASSERT(!"Unknown PropertyChange value");
+            Q_ASSERT(false); //Unknown PropertyChange value
         }
     } else {
         // old is outside of hierarchy, new is outside of hierarchy, so who cares?
@@ -198,7 +198,7 @@ void ModelToTextMerger::nodeSlidAround(const ModelNode &movingNode, const ModelN
     if (!inFrontOfNode.isValid() || movingNode.parentProperty() == inFrontOfNode.parentProperty())
         schedule(new MoveNodeRewriteAction(movingNode, inFrontOfNode));
     else
-        Q_ASSERT(!"Nodes do not belong to the same containing property");
+        Q_ASSERT(false); //Nodes do not belong to the same containing property
 }
 
 RewriterView *ModelToTextMerger::view()
@@ -348,7 +348,7 @@ QmlRefactoring::PropertyType ModelToTextMerger::propertyType(const AbstractPrope
     else if (property.isVariantProperty())
         return QmlRefactoring::ScriptBinding;
 
-    Q_ASSERT(!"cannot convert property type");
+    Q_ASSERT(false); //Cannot convert property type
     return (QmlRefactoring::PropertyType) -1;
 }
 
