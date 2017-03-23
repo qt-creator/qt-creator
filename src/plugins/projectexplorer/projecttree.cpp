qt-creator/qt-creator

commit ebae6426f8246f21ac6354147d67efcde782c624

ProjectManager: Remove SessionNode
Instead, get root project nodes directly from the project.

Change-Id: I5cf95a7dce1fa29c1adc26013ad03cc017f38a6d
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/plugins/projectexplorer/projecttree.cpp
@@ -261,9 +261,6 @@ void ProjectTree::updateContext()
 
 void ProjectTree::emitSubtreeChanged(FolderNode *node)
 {
-    if (!SessionManager::sessionNode()->isAncesterOf(node))
-        return;
-
     emit s_instance->subtreeChanged(node);
 }
 
@@ -318,16 +315,15 @@ bool ProjectTree::hasFocus(ProjectTreeWidget *widget)
 void ProjectTree::showContextMenu(ProjectTreeWidget *focus, const QPoint &globalPos, Node *node)
 {
     QMenu *contextMenu = nullptr;
+    Project *project = SessionManager::projectForNode(node);
+    emit s_instance->aboutToShowContextMenu(project, node);
 
-    if (!node)
-        node = SessionManager::sessionNode();
-    if (node->nodeType() != NodeType::Session) {
-        Project *project = SessionManager::projectForNode(node);
-
-        emit s_instance->aboutToShowContextMenu(project, node);
+    if (!node) {
+        contextMenu = Core::ActionManager::actionContainer(Constants::M_SESSIONCONTEXT)->menu();
+    } else {
         switch (node->nodeType()) {
         case NodeType::Project:
-            if (node->parentFolderNode() == SessionManager::sessionNode())
+            if (node->parentFolderNode())
                 contextMenu = Core::ActionManager::actionContainer(Constants::M_PROJECTCONTEXT)->menu();
             else
                 contextMenu = Core::ActionManager::actionContainer(Constants::M_SUBPROJECTCONTEXT)->menu();
@@ -342,10 +338,6 @@ void ProjectTree::showContextMenu(ProjectTreeWidget *focus, const QPoint &global
         default:
             qWarning("ProjectExplorerPlugin::showContextMenu - Missing handler for node type");
         }
-    } else { // session item
-        emit s_instance->aboutToShowContextMenu(nullptr, node);
-
-        contextMenu = Core::ActionManager::actionContainer(Constants::M_SESSIONCONTEXT)->menu();
     }
 
     if (contextMenu && contextMenu->actions().count() > 0) {

src/plugins/modeleditor/pxnodecontroller.cpp
@@ -198,8 +198,6 @@ void PxNodeController::addExplorerNode(const ProjectExplorer::Node *node,
         menu->popup(QCursor::pos());
         break;
     }
-    case ProjectExplorer::NodeType::Session:
-        break;
     }
 }
 

src/plugins/modeleditor/pxnodeutilities.cpp
@@ -80,9 +80,6 @@ QString PxNodeUtilities::calcRelativePath(const ProjectExplorer::Node *node,
     case ProjectExplorer::NodeType::Project:
         nodePath = node->filePath().toString();
         break;
-    case ProjectExplorer::NodeType::Session:
-        QTC_ASSERT(false, return QString());
-        break;
     }
 
     return qmt::NameController::calcRelativePath(nodePath, anchorFolder);

src/plugins/projectexplorer/project.cpp
@@ -39,6 +39,9 @@
 #include <coreplugin/idocument.h>
 #include <coreplugin/icontext.h>
 #include <coreplugin/icore.h>
+#include <coreplugin/iversioncontrol.h>
+#include <coreplugin/vcsmanager.h>
+
 #include <projectexplorer/buildmanager.h>
 #include <projectexplorer/kitmanager.h>
 #include <projectexplorer/projecttree.h>
@@ -80,18 +83,52 @@ const char PLUGIN_SETTINGS_KEY[] = "ProjectExplorer.Project.PluginSettings";
 } // namespace
 
 namespace ProjectExplorer {
+
+class ContainerNode : public ProjectNode
+{
+public:
+    ContainerNode(Project *project)
+        : ProjectNode(Utils::FileName()),
+          m_project(project)
+    {}
+
+    QString displayName() const final
+    {
+        QString name = m_project->displayName();
+
+        const QFileInfo fi = m_project->projectFilePath().toFileInfo();
+        const QString dir = fi.isDir() ? fi.absoluteFilePath() : fi.absolutePath();
+        if (Core::IVersionControl *vc = Core::VcsManager::findVersionControlForDirectory(dir)) {
+            QString vcsTopic = vc->vcsTopic(dir);
+            if (!vcsTopic.isEmpty())
+                name += " [" + vcsTopic + ']';
+        }
+
+        return name;
+    }
+
+    QList<ProjectAction> supportedActions(Node *) const final
+    {
+        return {};
+    }
+
+private:
+    Project *m_project;
+};
+
 // -------------------------------------------------------------------------
 // Project
 // -------------------------------------------------------------------------
-
 class ProjectPrivate
 {
 public:
+    ProjectPrivate(Project *owner) : m_containerNode(owner) {}
     ~ProjectPrivate();
 
     Core::Id m_id;
     Core::IDocument *m_document = nullptr;
     ProjectNode *m_rootProjectNode = nullptr;
+    ContainerNode m_containerNode;
     QList<Target *> m_targets;
     Target *m_activeTarget = nullptr;
     EditorConfiguration m_editorConfiguration;
@@ -117,7 +154,7 @@ ProjectPrivate::~ProjectPrivate()
     delete m_accessor;
 }
 
-Project::Project() : d(new ProjectPrivate)
+Project::Project() : d(new ProjectPrivate(this))
 {
     d->m_macroExpander.setDisplayName(tr("Project"));
     d->m_macroExpander.registerVariable("Project:Name", tr("Project Name"),
@@ -427,8 +464,11 @@ void Project::setRootProjectNode(ProjectNode *root)
     ProjectTree::applyTreeManager(root);
 
     d->m_rootProjectNode = root;
-    emit projectTreeChanged(this, QPrivateSignal());
-    // Do not delete oldNode! The ProjectTree owns that!
+    if (root)
+        d->m_rootProjectNode->setParentFolderNode(&d->m_containerNode);
+    ProjectTree::emitSubtreeChanged(d->m_rootProjectNode);
+
+    delete d->m_rootProjectNode;
 }
 
 Target *Project::restoreTarget(const QVariantMap &data)
@@ -530,6 +570,11 @@ Utils::FileName Project::projectDirectory(const Utils::FileName &top)
 ProjectNode *Project::rootProjectNode() const
 {
     return d->m_rootProjectNode;
+}
+
+ProjectNode *Project::containerNode() const
+{
+    return &d->m_containerNode;
 }
 
 Project::RestoreResult Project::fromMap(const QVariantMap &map, QString *errorMessage)

src/plugins/projectexplorer/project.h
@@ -81,6 +81,7 @@ public:
     static Utils::FileName projectDirectory(const Utils::FileName &top);
 
     virtual ProjectNode *rootProjectNode() const;
+    ProjectNode *containerNode() const;
 
     bool hasActiveBuildSettings() const;
 
@@ -142,7 +143,6 @@ public:
     Utils::MacroExpander *macroExpander() const;
 
 signals:
-    void projectTreeChanged(Project *project, QPrivateSignal);
     void displayNameChanged();
     void fileListChanged();
 

src/plugins/projectexplorer/projectmodels.cpp
@@ -71,7 +71,7 @@ static bool sortWrapperNodes(const WrapperNode *w1, const WrapperNode *w2)
 }
 
 FlatModel::FlatModel(QObject *parent)
-    : TreeModel<WrapperNode, WrapperNode>(new WrapperNode(SessionManager::sessionNode()), parent)
+    : TreeModel<WrapperNode, WrapperNode>(new WrapperNode(nullptr), parent)
 {
     ProjectTree *tree = ProjectTree::instance();
     connect(tree, &ProjectTree::subtreeChanged, this, &FlatModel::update);
@@ -93,17 +93,7 @@ QVariant FlatModel::data(const QModelIndex &index, int role) const
         FolderNode *folderNode = node->asFolderNode();
         switch (role) {
         case Qt::DisplayRole: {
-            QString name = node->displayName();
-            if (node->nodeType() == NodeType::Project
-                    && node->parentFolderNode()
-                    && node->parentFolderNode()->nodeType() == NodeType::Session) {
-                const QString vcsTopic = static_cast<ProjectNode *>(node)->vcsTopic();
-
-                if (!vcsTopic.isEmpty())
-                    name += QLatin1String(" [") + vcsTopic + QLatin1Char(']');
-            }
-
-            result = name;
+            result = node->displayName();
             break;
         }
         case Qt::EditRole: {
@@ -124,7 +114,7 @@ QVariant FlatModel::data(const QModelIndex &index, int role) const
         case Qt::FontRole: {
             QFont font;
             if (Project *project = SessionManager::startupProject()) {
-                if (node == SessionManager::nodeForProject(project))
+                if (node == project->containerNode())
                     font.setBold(true);
             }
             result = font;
@@ -187,23 +177,44 @@ void FlatModel::update()
 
 void FlatModel::rebuildModel()
 {
+    QList<Project *> projects = SessionManager::projects();
+
+    Utils::sort(projects, [](Project *p1, Project *p2) {
+        const int displayNameResult = caseFriendlyCompare(p1->displayName(), p2->displayName());
+        if (displayNameResult != 0)
+            return displayNameResult < 0;
+        return p1 < p2; // sort by pointer value
+    });
+
     QSet<Node *> seen;
 
     rootItem()->removeChildren();
-    for (Node *node : SessionManager::sessionNode()->nodes()) {
-        if (ProjectNode *projectNode = node->asProjectNode()) {
-            if (!seen.contains(projectNode))
-                addProjectNode(rootItem(), projectNode, &seen);
+    for (Project *project : projects) {
+        WrapperNode *container = new WrapperNode(project->containerNode());
+
+        ProjectNode *projectNode = project->rootProjectNode();
+        if (projectNode) {
+            addFolderNode(container, projectNode, &seen);
+        } else {
+            FileNode *projectFileNode = new FileNode(project->projectFilePath(), FileType::Project, false);
+            seen.insert(projectFileNode);
+            container->appendChild(new WrapperNode(projectFileNode));
         }
+
+        container->sortChildren(&sortWrapperNodes);
+        rootItem()->appendChild(container);
     }
-    rootItem()->sortChildren(&sortWrapperNodes);
 
     forAllItems([this](WrapperNode *node) {
-        const QString path = node->m_node->filePath().toString();
-        const QString displayName = node->m_node->displayName();
-        ExpandData ed(path, displayName);
-        if (m_toExpand.contains(ed))
+        if (node->m_node) {
+            const QString path = node->m_node->filePath().toString();
+            const QString displayName = node->m_node->displayName();
+            ExpandData ed(path, displayName);
+            if (m_toExpand.contains(ed))
+                emit requestExpansion(node->index());
+        } else {
             emit requestExpansion(node->index());
+        }
     });
 }
 
@@ -227,7 +238,7 @@ ExpandData FlatModel::expandDataForNode(const Node *node) const
 
 void FlatModel::handleProjectAdded(Project *project)
 {
-    Node *node = SessionManager::nodeForProject(project);
+    Node *node = project->rootProjectNode();
     m_toExpand.insert(expandDataForNode(node));
     if (WrapperNode *wrapper = wrapperForNode(node)) {
         wrapper->forFirstLevelChildren([this](WrapperNode *child) {
@@ -249,21 +260,6 @@ void FlatModel::saveExpandData()
     // TODO if there are multiple ProjectTreeWidgets, the last one saves the data
     QList<QVariant> data = Utils::transform<QList>(m_toExpand, &ExpandData::toSettings);
     SessionManager::setValue(QLatin1String("ProjectTree.ExpandData"), data);
-}
-
-void FlatModel::addProjectNode(WrapperNode *parent, ProjectNode *projectNode, QSet<Node *> *seen)
-{
-    seen->insert(projectNode);
-    auto node = new WrapperNode(projectNode);
-    parent->appendChild(node);
-    addFolderNode(node, projectNode, seen);
-    for (Node *subNode : projectNode->nodes()) {
-        if (ProjectNode *subProjectNode = subNode->asProjectNode()) {
-            if (!seen->contains(subProjectNode))
-                addProjectNode(node, subProjectNode, seen);
-        }
-    }
-    node->sortChildren(&sortWrapperNodes);
 }
 
 void FlatModel::addFolderNode(WrapperNode *parent, FolderNode *folderNode, QSet<Node *> *seen)
@@ -372,16 +368,6 @@ const QLoggingCategory &FlatModel::logger()
 {
     static QLoggingCategory logger("qtc.projectexplorer.flatmodel");
     return logger;
-}
-
-bool isSorted(const QList<Node *> &nodes)
-{
-    int size = nodes.size();
-    for (int i = 0; i < size -1; ++i) {
-        if (!sortNodes(nodes.at(i), nodes.at(i+1)))
-            return false;
-    }
-    return true;
 }
 
 namespace Internal {

src/plugins/projectexplorer/projectmodels.h
@@ -94,7 +94,6 @@ private:
 
     void update();
     void rebuildModel();
-    void addProjectNode(WrapperNode *parent, ProjectNode *projectNode, QSet<Node *> *seen);
     void addFolderNode(WrapperNode *parent, FolderNode *folderNode, QSet<Node *> *seen);
 
     ExpandData expandDataForNode(const Node *node) const;

src/plugins/projectexplorer/projectnodes.cpp
@@ -214,9 +214,12 @@ bool Node::isEnabled() const
 
 QList<ProjectAction> Node::supportedActions(Node *node) const
 {
-    QList<ProjectAction> list = parentFolderNode()->supportedActions(node);
-    list.append(InheritedFromParent);
-    return list;
+    if (FolderNode *folder = parentFolderNode()) {
+        QList<ProjectAction> list = folder->supportedActions(node);
+        list.append(InheritedFromParent);
+        return list;
+    }
+    return {};
 }
 
 void Node::setEnabled(bool enabled)
@@ -702,18 +705,6 @@ ProjectNode::ProjectNode(const Utils::FileName &projectFilePath) :
     setDisplayName(projectFilePath.fileName());
 }
 
-QString ProjectNode::vcsTopic() const
-{
-    const QFileInfo fi = filePath().toFileInfo();
-    const QString dir = fi.isDir() ? fi.absoluteFilePath() : fi.absolutePath();
-
-    if (Core::IVersionControl *const vc =
-            Core::VcsManager::findVersionControlForDirectory(dir))
-        return vc->vcsTopic(dir);
-
-    return QString();
-}
-
 bool ProjectNode::canAddSubProject(const QString &proFilePath) const
 {
     Q_UNUSED(proFilePath)
@@ -795,30 +786,6 @@ ProjectNode *ProjectNode::projectNode(const Utils::FileName &file) const
 bool FolderNode::isEmpty() const
 {
     return m_nodes.isEmpty();
-}
-
-/*!
-  \class ProjectExplorer::SessionNode
-*/
-
-SessionNode::SessionNode() :
-    FolderNode(Utils::FileName::fromString("session"), NodeType::Session)
-{ }
-
-QList<ProjectAction> SessionNode::supportedActions(Node *node) const
-{
-    Q_UNUSED(node)
-    return QList<ProjectAction>();
-}
-
-bool SessionNode::showInSimpleTree() const
-{
-    return true;
-}
-
-QString SessionNode::addFileFilter() const
-{
-    return QString::fromLatin1("*.c; *.cc; *.cpp; *.cp; *.cxx; *.c++; *.h; *.hh; *.hpp; *.hxx;");
 }
 
 } // namespace ProjectExplorer

commit 7022b784c6c17fc5ead54f702c5801e392c78ff9

ProjectExplorer: Fix project/subproject item context menu mixup
Off-by one negation introduced in ebae642.

Change-Id: I26701e170ea4e0dc18f114c6c032b8fa85fb4a98
Reviewed-by: Florian Apolloner <florian@apolloner.eu>
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/plugins/projectexplorer/projecttree.cpp
@@ -323,7 +323,7 @@ void ProjectTree::showContextMenu(ProjectTreeWidget *focus, const QPoint &global
     } else {
         switch (node->nodeType()) {
         case NodeType::Project:
-            if (node->parentFolderNode())
+            if (!node->parentFolderNode())
                 contextMenu = Core::ActionManager::actionContainer(Constants::M_PROJECTCONTEXT)->menu();
             else
                 contextMenu = Core::ActionManager::actionContainer(Constants::M_SUBPROJECTCONTEXT)->menu();