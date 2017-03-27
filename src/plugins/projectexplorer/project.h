3aa13958fde29b6f4fb0cad407a3cc9cf1b273c9
hjk <hjk@qt.io>
2017-03-27 18:43:42 +0200

    ProjectExplorer: De-virtualize Project::files()
    It's never overloaded.
    
    Change-Id: Ia733e7525a98c19060abbbfc2ae49c06be8b8d5d
    Reviewed-by: Orgad Shaneh <orgads@gmail.com>

--- src/plugins/projectexplorer/project.h
+++ src/plugins/projectexplorer/project.h
@@ -116,8 +116,8 @@ public:
         GeneratedFiles = 0x2,
         AllFiles       = SourceFiles | GeneratedFiles
     };
-    virtual QStringList files(FilesMode fileMode,
-                              const std::function<bool(const FileNode *)> &filter = {}) const;
+    QStringList files(FilesMode fileMode,
+                      const std::function<bool(const FileNode *)> &filter = {}) const;
     virtual QStringList filesGeneratedFrom(const QString &sourceFile) const;
 
     static QString makeUnique(const QString &preferredName, const QStringList &usedNames);
