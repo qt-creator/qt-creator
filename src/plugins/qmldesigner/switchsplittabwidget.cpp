qt-creator/qt-creator

commit 6f1d23711dfe57d20623e755a077c38f354f6143

QmlDesigner: Keep background color in integrated text editor
Applying a .css style overrides the text editor background color
from the theme.
To avoid this we cannot apply any .css on the integrated text editor or
its parents:

 * Only apply the center widget .css to the tab bar.
 * Apply the scrollbar.css only to the scrollbars.

Change-Id: Id9b009a208ea2830c64be8174dbb548ada692560
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/plugins/qmldesigner/switchsplittabwidget.cpp
@@ -24,8 +24,10 @@
 ****************************************************************************/
 
 #include "switchsplittabwidget.h"
+#include <theme.h>
 
 #include <utils/utilsicons.h>
+#include <utils/fileutils.h>
 
 #include <QVector>
 #include <QBoxLayout>
@@ -47,6 +49,9 @@ SwitchSplitTabWidget::SwitchSplitTabWidget(QWidget *parent)
     setObjectName("backgroundWidget");
     m_splitter->setObjectName("centralTabWidget");
     m_splitter->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+
+    QString sheet = QString::fromUtf8(Utils::FileReader::fetchQrc(":/qmldesigner/centerwidget.css"));
+    m_tabBarBackground->setStyleSheet(Theme::replaceCssColors(sheet));
 
     m_tabBar->setObjectName("centralTabBar");
     m_tabBar->setShape(QTabBar::RoundedEast);

src/plugins/qmldesigner/components/texteditor/texteditorwidget.cpp
@@ -40,6 +40,7 @@
 #include <coreplugin/editormanager/editormanager.h>
 
 #include <QEvent>
+#include <QScrollBar>
 #include <QVBoxLayout>
 
 #include <vector>
@@ -61,7 +62,6 @@ TextEditorWidget::TextEditorWidget(TextEditorView *textEditorView)
     m_updateSelectionTimer.setInterval(200);
 
     connect(&m_updateSelectionTimer, &QTimer::timeout, this, &TextEditorWidget::updateSelectionByCursorPosition);
-    setStyleSheet(Theme::replaceCssColors(QString::fromUtf8(Utils::FileReader::fetchQrc(QLatin1String(":/qmldesigner/scrollbar.css")))));
 }
 
 void TextEditorWidget::setTextEditor(TextEditor::BaseTextEditor *textEditor)
@@ -85,6 +85,9 @@ void TextEditorWidget::setTextEditor(TextEditor::BaseTextEditor *textEditor)
         });
 
         textEditor->editorWidget()->installEventFilter(this);
+        static QString styleSheet = Theme::replaceCssColors(QString::fromUtf8(Utils::FileReader::fetchQrc(QLatin1String(":/qmldesigner/scrollbar.css"))));
+        textEditor->editorWidget()->verticalScrollBar()->setStyleSheet(styleSheet);
+        textEditor->editorWidget()->horizontalScrollBar()->setStyleSheet(styleSheet);
     }
 
     if (oldEditor)

src/plugins/qmldesigner/designmodewidget.cpp
@@ -454,10 +454,6 @@ static Core::MiniSplitter *createCentralSplitter(const QList<WidgetInfo> &widget
 
     SwitchSplitTabWidget *switchSplitTabWidget = new SwitchSplitTabWidget();
 
-    QString sheet = QString::fromUtf8(Utils::FileReader::fetchQrc(":/qmldesigner/centerwidget.css"));
-    switchSplitTabWidget->setStyleSheet(Theme::replaceCssColors(sheet));
-
-
     foreach (const WidgetInfo &widgetInfo, widgetInfos) {
         if (widgetInfo.placementHint == widgetInfo.CentralPane)
             switchSplitTabWidget->addTab(widgetInfo.widget, widgetInfo.tabName);

commit e21843e30937b381d6f118c2a948070225e1f0cb

QmlDesigner: set splitter handle zero again
Was broken since 6f1d23711dfe57d20623e755a077c38f354f6143 which sets
css only at widgets where it is necessary.

Change-Id: Ia60672136ad8c73791806c920051918dbb23748c
Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

src/plugins/qmldesigner/components/resources/centerwidget.css
@@ -34,11 +34,3 @@ QToolButton#centralTabBar {
     height: 16px;
     border: none;
 }
-
-QSplitter::handle:horizontal {
-    width: 0px;
-}
-
-QSplitter::handle:vertical {
-    height: 0px;
-}

src/plugins/qmldesigner/switchsplittabwidget.cpp
@@ -49,6 +49,7 @@ SwitchSplitTabWidget::SwitchSplitTabWidget(QWidget *parent)
     setObjectName("backgroundWidget");
     m_splitter->setObjectName("centralTabWidget");
     m_splitter->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    m_splitter->setHandleWidth(0);
 
     QString sheet = QString::fromUtf8(Utils::FileReader::fetchQrc(":/qmldesigner/centerwidget.css"));
     m_tabBarBackground->setStyleSheet(Theme::replaceCssColors(sheet));