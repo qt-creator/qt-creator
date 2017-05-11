967f4d427141f566c5f211dc1c676cfea3afbd01
Thomas Hartmann <thomas.hartmann@qt.io>
2017-05-11 17:15:10 +0200

    QmlDesigner: Add missing override
    Change-Id: I949a5e96c0a0ac0e2b88860c191095543de417f5
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- src/plugins/qmldesigner/components/navigator/navigatorview.h
+++ src/plugins/qmldesigner/components/navigator/navigatorview.h
@@ -64,7 +64,7 @@ public:
     void importsChanged(const QList<Import> &addedImports, const QList<Import> &removedImports) override;
 
     void nodeAboutToBeRemoved(const ModelNode &removedNode) override;
-    void nodeRemoved(const ModelNode &removedNode, const NodeAbstractProperty &parentProperty, PropertyChangeFlags propertyChange);
+    void nodeRemoved(const ModelNode &removedNode, const NodeAbstractProperty &parentProperty, PropertyChangeFlags propertyChange) override;
     void nodeOrderChanged(const NodeListProperty &listProperty, const ModelNode &movedNode, int oldIndex) override;
 
     void nodeReparented(const ModelNode &node, const NodeAbstractProperty &newPropertyParent, const NodeAbstractProperty &oldPropertyParent, AbstractView::PropertyChangeFlags propertyChange) override;
