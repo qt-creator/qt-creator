cf807253e4a6dbb90bddc8db7426599220d92d4e
Friedemann Kleint <Friedemann.Kleint@qt.io>
2017-05-12 08:55:01 +0200

    QML application wizards: Add code to bail out on load errors
    When something goes wrong with deployment or there is a syntax
    error in the QML files, the old code would enter the main
    loop not showing any windows. Worse, the binary is then locked
    on Windows and cannot be overwritten. Add a clause checking
    whether there are any root objects and bail out on failure.
    
    Task-number: QTBUG-60764
    Change-Id: I3620a09ec4331dc04a5194dfd2ece2ff44bdc429
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>
    Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

--- share/qtcreator/templates/wizards/projects/qtquickapplication/main.cpp
+++ share/qtcreator/templates/wizards/projects/qtquickapplication/main.cpp
@@ -10,6 +10,8 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
     return app.exec();
 }
--- share/qtcreator/templates/wizards/projects/qtquickcontrols2application/main.cpp
+++ share/qtcreator/templates/wizards/projects/qtquickcontrols2application/main.cpp
@@ -11,6 +11,8 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
     return app.exec();
 }
