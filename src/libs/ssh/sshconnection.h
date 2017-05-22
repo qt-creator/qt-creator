34702f8441cf890e08cbea91760615c188eac34d
Christian Kandeler <christian.kandeler@qt.io>
2017-05-22 12:03:41 +0200

    SSH: Fix enum values
    Newly introduced enum values need to get a higher value than the
    existing ones of the same type. Otherwise such values will be
    interpreted wrongly when loaded from settings created by earlier
    versions of Qt Creator.
    
    Task-number: QTCREATORBUG-18235
    Change-Id: I0f8497352d1ef382b5c8a65db4e1dea20b6296da
    Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

--- src/libs/ssh/sshconnection.h
+++ src/libs/ssh/sshconnection.h
@@ -65,11 +65,12 @@ public:
     enum AuthenticationType {
         AuthenticationTypePassword,
         AuthenticationTypePublicKey,
-        AuthenticationTypeAgent,
         AuthenticationTypeKeyboardInteractive,
 
         // Some servers disable "password", others disable "keyboard-interactive".
-        AuthenticationTypeTryAllPasswordBasedMethods
+        AuthenticationTypeTryAllPasswordBasedMethods,
+
+        AuthenticationTypeAgent,
     };
 
     SshConnectionParameters();
