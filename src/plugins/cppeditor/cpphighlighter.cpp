qt-creator/qt-creator

commit a92694596e804b2fc551d82f62d4ab5ab797130b

Editor: Fix visual whitespace highlighting
in comments and strings

Change-Id: I94cb478419afde77299b3be1ec252a21c17a1658
Reviewed-by: Leandro Melo <leandro.melo@nokia.com>

src/plugins/cppeditor/cpphighlighter.cpp
@@ -29,10 +29,11 @@
 **************************************************************************/
 
 #include "cpphighlighter.h"
-#include <cpptools/cppdoxygen.h>
 
 #include <Token.h>
 #include <cplusplus/SimpleLexer.h>
+#include <cplusplus/Lexer.h>
+#include <cpptools/cppdoxygen.h>
 #include <cpptools/cpptoolsreuse.h>
 #include <texteditor/basetextdocumentlayout.h>
 
@@ -79,10 +80,12 @@ void CppHighlighter::highlightBlock(const QString &text)
         setCurrentBlockState(previousState);
         BaseTextDocumentLayout::clearParentheses(currentBlock());
         if (text.length())  {// the empty line can still contain whitespace
-            if (!initialState)
-                setFormat(0, text.length(), m_formats[CppVisualWhitespace]);
+            if (initialState == Lexer::State_MultiLineComment)
+                highlightLine(text, 0, text.length(), m_formats[CppCommentFormat]);
+            else if (initialState == Lexer::State_MultiLineDoxyComment)
+                highlightLine(text, 0, text.length(), m_formats[CppDoxygenCommentFormat]);
             else
-                setFormat(0, text.length(), m_formats[CppCommentFormat]);
+                setFormat(0, text.length(), m_formats[CppVisualWhitespace]);
         }
         BaseTextDocumentLayout::setFoldingIndent(currentBlock(), foldingIndent);
         return;
@@ -106,12 +109,8 @@ void CppHighlighter::highlightBlock(const QString &text)
                                tokens.at(i - 1).length();
         }
 
-        if (previousTokenEnd != tk.begin()) {
-            if (initialState && tk.isComment())
-                setFormat(previousTokenEnd, tk.begin() - previousTokenEnd, m_formats[CppCommentFormat]);
-            else
-                setFormat(previousTokenEnd, tk.begin() - previousTokenEnd, m_formats[CppVisualWhitespace]);
-        }
+        if (previousTokenEnd != tk.begin())
+            setFormat(previousTokenEnd, tk.begin() - previousTokenEnd, m_formats[CppVisualWhitespace]);
 
         if (tk.is(T_LPAREN) || tk.is(T_LBRACE) || tk.is(T_LBRACKET)) {
             const QChar c = text.at(tk.begin());
@@ -166,14 +165,15 @@ void CppHighlighter::highlightBlock(const QString &text)
             setFormat(tk.begin(), tk.length(), m_formats[CppNumberFormat]);
 
         else if (tk.isStringLiteral() || tk.isCharLiteral())
-            setFormat(tk.begin(), tk.length(), m_formats[CppStringFormat]);
+            highlightLine(text, tk.begin(), tk.length(), m_formats[CppStringFormat]);
 
         else if (tk.isComment()) {
+            const int startPosition = initialState ? previousTokenEnd : tk.begin();
             if (tk.is(T_COMMENT) || tk.is(T_CPP_COMMENT))
-                setFormat(tk.begin(), tk.length(), m_formats[CppCommentFormat]);
+                highlightLine(text, startPosition, tk.end() - startPosition, m_formats[CppCommentFormat]);
 
             else // a doxygen comment
-                highlightDoxygenComment(text, tk.begin(), tk.length());
+                highlightDoxygenComment(text, startPosition, tk.end() - startPosition);
 
             // we need to insert a close comment parenthesis, if
             //  - the line starts in a C Comment (initalState != 0)
@@ -208,12 +208,9 @@ void CppHighlighter::highlightBlock(const QString &text)
     }
 
     // mark the trailing white spaces
-    {
-        const Token tk = tokens.last();
-        const int lastTokenEnd = tk.begin() + tk.length();
-        if (text.length() > lastTokenEnd)
-            highlightLine(text, lastTokenEnd, text.length() - lastTokenEnd, QTextCharFormat());
-    }
+    const int lastTokenEnd = tokens.last().end();
+    if (text.length() > lastTokenEnd)
+        highlightLine(text, lastTokenEnd, text.length() - lastTokenEnd, m_formats[CppVisualWhitespace]);
 
     if (! initialState && state && ! tokens.isEmpty()) {
         parentheses.append(Parenthesis(Parenthesis::Opened, QLatin1Char('+'),
@@ -335,7 +332,8 @@ bool CppHighlighter::isPPKeyword(const QStringRef &text) const
 void CppHighlighter::highlightLine(const QString &text, int position, int length,
                                    const QTextCharFormat &format)
 {
-    const QTextCharFormat visualSpaceFormat = m_formats[CppVisualWhitespace];
+    QTextCharFormat visualSpaceFormat = m_formats[CppVisualWhitespace];
+    visualSpaceFormat.setBackground(format.background());
 
     const int end = position + length;
     int index = position;

commit 21a246ac385b8d29659e6e3d73c1e44bceb6bd8a

Fix highlighting of C/C++ line continuation backslashes
The backslash itself is not a token, so we must make sure to only format
the trailing whitespace characters as visual whitespace, instead of
everything trailing.

Broke in a92694596e804b2fc551d82f62d4ab5ab797130b

Task-number: QTCREATORBUG-987
Change-Id: I4e9e84d29513ea317d7e3bde6c6b4c43749cb649
Reviewed-by: David Schulz <david.schulz@qt.io>

src/plugins/cppeditor/cpphighlighter.cpp
@@ -214,7 +214,7 @@ void CppHighlighter::highlightBlock(const QString &text)
     // mark the trailing white spaces
     const int lastTokenEnd = tokens.last().utf16charsEnd();
     if (text.length() > lastTokenEnd)
-        setFormatWithSpaces(text, lastTokenEnd, text.length() - lastTokenEnd, formatForCategory(C_VISUAL_WHITESPACE));
+        formatSpaces(text, lastTokenEnd, text.length() - lastTokenEnd);
 
     if (!initialLexerState && lexerState && !tokens.isEmpty()) {
         const Token &lastToken = tokens.last();

src/plugins/texteditor/syntaxhighlighter.cpp
@@ -477,15 +477,15 @@ void SyntaxHighlighter::setFormat(int start, int count, const QFont &font)
     setFormat(start, count, format);
 }
 
-void SyntaxHighlighter::formatSpaces(const QString &text)
+void SyntaxHighlighter::formatSpaces(const QString &text, int start, int count)
 {
     Q_D(const SyntaxHighlighter);
-    int offset = 0;
-    const int length = text.length();
-    while (offset < length) {
+    int offset = start;
+    const int end = std::min(start + count, text.length());
+    while (offset < end) {
         if (text.at(offset).isSpace()) {
             int start = offset++;
-            while (offset < length && text.at(offset).isSpace())
+            while (offset < end && text.at(offset).isSpace())
                 ++offset;
             setFormat(start, offset - start, d->whitespaceFormat);
         } else {

src/plugins/texteditor/syntaxhighlighter.h
@@ -33,6 +33,7 @@
 #include <QTextLayout>
 
 #include <functional>
+#include <limits.h>
 
 QT_BEGIN_NAMESPACE
 class QTextDocument;
@@ -84,7 +85,7 @@ protected:
     void setFormat(int start, int count, const QFont &font);
     QTextCharFormat format(int pos) const;
 
-    void formatSpaces(const QString &text);
+    void formatSpaces(const QString &text, int start = 0, int count = INT_MAX);
     void setFormatWithSpaces(const QString &text, int start, int count,
                              const QTextCharFormat &format);
 