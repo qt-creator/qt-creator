qt-creator/qt-creator

commit 2c6adc1e747575af5198cc7ea2277b9a70366d3c

VCS: Replace QToolButtons in VCS toolbar with QActions
Change VcsBaseEditorParameterWidget to add actions/widgets to an
existing toolbar, instead of being a widget itself.

The class is renamed in a follow-up commit.

Task-number: QTCREATORBUG-14934
Change-Id: I473a439d12a096f4cbb64f06faa0598ee72000de
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>
Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

src/plugins/git/gitclient.cpp
@@ -394,8 +394,8 @@ class BaseGitDiffArgumentsWidget : public VcsBaseEditorParameterWidget
     Q_OBJECT
 
 public:
-    BaseGitDiffArgumentsWidget(VcsBaseClientSettings &settings, QWidget *parent = nullptr) :
-        VcsBaseEditorParameterWidget(parent)
+    BaseGitDiffArgumentsWidget(VcsBaseClientSettings &settings, QToolBar *toolBar) :
+        VcsBaseEditorParameterWidget(toolBar)
     {
         m_patienceButton
                 = addToggleButton("--patience", tr("Patience"),
@@ -408,8 +408,8 @@ public:
     }
 
 protected:
-    QToolButton *m_patienceButton;
-    QToolButton *m_ignoreWSButton;
+    QAction *m_patienceButton;
+    QAction *m_ignoreWSButton;
 };
 
 class GitBlameArgumentsWidget : public VcsBaseEditorParameterWidget
@@ -417,8 +417,8 @@ class GitBlameArgumentsWidget : public VcsBaseEditorParameterWidget
     Q_OBJECT
 
 public:
-    GitBlameArgumentsWidget(VcsBaseClientSettings &settings, QWidget *parent = nullptr) :
-        VcsBaseEditorParameterWidget(parent)
+    GitBlameArgumentsWidget(VcsBaseClientSettings &settings, QToolBar *toolBar) :
+        VcsBaseEditorParameterWidget(toolBar)
     {
         mapSetting(addToggleButton(QString(), tr("Omit Date"),
                                    tr("Hide the date of a change from the output.")),
@@ -434,22 +434,22 @@ class GitLogArgumentsWidget : public BaseGitDiffArgumentsWidget
     Q_OBJECT
 
 public:
-    GitLogArgumentsWidget(VcsBaseClientSettings &settings, QWidget *parent = nullptr) :
-        BaseGitDiffArgumentsWidget(settings, parent)
+    GitLogArgumentsWidget(VcsBaseClientSettings &settings, QToolBar *toolBar = nullptr) :
+        BaseGitDiffArgumentsWidget(settings, toolBar)
     {
-        QToolButton *diffButton = addToggleButton("--patch", tr("Show Diff"),
+        QAction *diffButton = addToggleButton("--patch", tr("Show Diff"),
                                               tr("Show difference."));
         mapSetting(diffButton, settings.boolPointer(GitSettings::logDiffKey));
-        connect(diffButton, &QToolButton::toggled, m_patienceButton, &QToolButton::setVisible);
-        connect(diffButton, &QToolButton::toggled, m_ignoreWSButton, &QToolButton::setVisible);
+        connect(diffButton, &QAction::toggled, m_patienceButton, &QAction::setVisible);
+        connect(diffButton, &QAction::toggled, m_ignoreWSButton, &QAction::setVisible);
         m_patienceButton->setVisible(diffButton->isChecked());
         m_ignoreWSButton->setVisible(diffButton->isChecked());
         const QStringList graphArguments = {
             "--graph", "--oneline", "--topo-order",
             QLatin1String("--pretty=format:") + graphLogFormatC
         };
-        QToolButton *graphButton = addToggleButton(graphArguments, tr("Graph"),
-                                                   tr("Show textual graph log."));
+        QAction *graphButton = addToggleButton(graphArguments, tr("Graph"),
+                                               tr("Show textual graph log."));
         mapSetting(graphButton, settings.boolPointer(GitSettings::graphLogKey));
     }
 };
@@ -813,7 +813,7 @@ void GitClient::log(const QString &workingDirectory, const QString &fileName,
     QString msgArg;
     if (!fileName.isEmpty())
         msgArg = fileName;
-    else if (!args.isEmpty())
+    else if (!args.isEmpty() && !args.first().startsWith('-'))
         msgArg = args.first();
     else
         msgArg = workingDirectory;
@@ -824,11 +824,14 @@ void GitClient::log(const QString &workingDirectory, const QString &fileName,
     const QString sourceFile = VcsBaseEditor::getSource(workingDir, fileName);
     VcsBaseEditorWidget *editor = createVcsEditor(editorId, title, sourceFile,
                                                   codecFor(CodecLogOutput), "logTitle", msgArg);
-    if (!editor->configurationWidget()) {
-        auto *argWidget = new GitLogArgumentsWidget(settings());
+    QStringList effectiveArgs = args;
+    if (!editor->configurationAdded()) {
+        auto *argWidget = new GitLogArgumentsWidget(settings(), editor->toolBar());
+        argWidget->setBaseArguments(args);
         connect(argWidget, &VcsBaseEditorParameterWidget::commandExecutionRequested,
-                [=]() { this->log(workingDir, fileName, enableAnnotationContextMenu, args); });
-        editor->setConfigurationWidget(argWidget);
+                [=]() { this->log(workingDir, fileName, enableAnnotationContextMenu, argWidget->arguments()); });
+        effectiveArgs = argWidget->arguments();
+        editor->setConfigurationAdded();
     }
     editor->setFileLogAnnotateEnabled(enableAnnotationContextMenu);
     editor->setWorkingDirectory(workingDir);
@@ -838,11 +841,7 @@ void GitClient::log(const QString &workingDirectory, const QString &fileName,
     if (logCount > 0)
         arguments << "-n" << QString::number(logCount);
 
-    auto *argWidget = editor->configurationWidget();
-    argWidget->setBaseArguments(args);
-    QStringList userArgs = argWidget->arguments();
-
-    arguments.append(userArgs);
+    arguments.append(effectiveArgs);
 
     if (!fileName.isEmpty())
         arguments << "--follow" << "--" << fileName;
@@ -911,21 +910,22 @@ VcsBaseEditorWidget *GitClient::annotate(
     VcsBaseEditorWidget *editor
             = createVcsEditor(editorId, title, sourceFile, codecFor(CodecSource, sourceFile),
                               "blameFileName", id);
-    if (!editor->configurationWidget()) {
-        auto *argWidget = new GitBlameArgumentsWidget(settings());
+    QStringList effectiveArgs = extraOptions;
+    if (!editor->configurationAdded()) {
+        auto *argWidget = new GitBlameArgumentsWidget(settings(), editor->toolBar());
         argWidget->setBaseArguments(extraOptions);
         connect(argWidget, &VcsBaseEditorParameterWidget::commandExecutionRequested,
                 [=] {
                     const int line = VcsBaseEditor::lineNumberOfCurrentEditor();
-                    annotate(workingDir, file, revision, line, extraOptions);
+                    annotate(workingDir, file, revision, line, argWidget->arguments());
                 } );
-        editor->setConfigurationWidget(argWidget);
+        effectiveArgs = argWidget->arguments();
+        editor->setConfigurationAdded();
     }
 
     editor->setWorkingDirectory(workingDir);
     QStringList arguments = { "blame", "--root" };
-    arguments << editor->configurationWidget()->arguments();
-    arguments << "--" << file;
+    arguments << effectiveArgs << "--" << file;
     if (!revision.isEmpty())
         arguments << revision;
     vcsExec(workingDir, arguments, editor, false, 0, lineNumber);

src/plugins/perforce/perforceplugin.cpp
@@ -1199,7 +1199,7 @@ class PerforceDiffParameterWidget : public VcsBaseEditorParameterWidget
 {
     Q_OBJECT
 public:
-    explicit PerforceDiffParameterWidget(const PerforceDiffParameters &p, QWidget *parent = 0);
+    explicit PerforceDiffParameterWidget(const PerforceDiffParameters &p, QToolBar *toolBar);
     void triggerReRun();
 
 signals:
@@ -1209,8 +1209,9 @@ private:
     const PerforceDiffParameters m_parameters;
 };
 
-PerforceDiffParameterWidget::PerforceDiffParameterWidget(const PerforceDiffParameters &p, QWidget *parent) :
-    VcsBaseEditorParameterWidget(parent), m_parameters(p)
+PerforceDiffParameterWidget::PerforceDiffParameterWidget(const PerforceDiffParameters &p,
+                                                         QToolBar *toolBar) :
+    VcsBaseEditorParameterWidget(toolBar), m_parameters(p)
 {
     setBaseArguments(p.diffArguments);
     addToggleButton(QLatin1String("w"), tr("Ignore Whitespace"));
@@ -1269,12 +1270,12 @@ void PerforcePlugin::p4Diff(const PerforceDiffParameters &p)
     auto diffEditorWidget = qobject_cast<VcsBaseEditorWidget *>(editor->widget());
     // Wire up the parameter widget to trigger a re-run on
     // parameter change and 'revert' from inside the diff editor.
-    auto pw = new PerforceDiffParameterWidget(p);
+    auto pw = new PerforceDiffParameterWidget(p, diffEditorWidget->toolBar());
     connect(pw, &PerforceDiffParameterWidget::reRunDiff,
             this, [this](const PerforceDiffParameters &p) { p4Diff(p); });
     connect(diffEditorWidget, &VcsBaseEditorWidget::diffChunkReverted,
             pw, &PerforceDiffParameterWidget::triggerReRun);
-    diffEditorWidget->setConfigurationWidget(pw);
+    diffEditorWidget->setConfigurationAdded();
 }
 
 void PerforcePlugin::describe(const QString & source, const QString &n)

src/plugins/vcsbase/vcsbaseclient.cpp
@@ -270,21 +270,21 @@ void VcsBaseClientImpl::saveSettings()
 class VcsBaseClientPrivate
 {
 public:
-    VcsBaseEditorParameterWidget *createDiffEditor();
-    VcsBaseEditorParameterWidget *createLogEditor();
+    VcsBaseEditorParameterWidget *createDiffEditor(VcsBaseEditorWidget *editor);
+    VcsBaseEditorParameterWidget *createLogEditor(VcsBaseEditorWidget *editor);
 
     VcsBaseClient::ParameterWidgetCreator m_diffParamWidgetCreator;
     VcsBaseClient::ParameterWidgetCreator m_logParamWidgetCreator;
 };
 
-VcsBaseEditorParameterWidget *VcsBaseClientPrivate::createDiffEditor()
+VcsBaseEditorParameterWidget *VcsBaseClientPrivate::createDiffEditor(VcsBaseEditorWidget *editor)
 {
-    return m_diffParamWidgetCreator ? m_diffParamWidgetCreator() : 0;
+    return m_diffParamWidgetCreator ? m_diffParamWidgetCreator(editor->toolBar()) : 0;
 }
 
-VcsBaseEditorParameterWidget *VcsBaseClientPrivate::createLogEditor()
+VcsBaseEditorParameterWidget *VcsBaseClientPrivate::createLogEditor(VcsBaseEditorWidget *editor)
 {
-    return m_logParamWidgetCreator ? m_logParamWidgetCreator() : 0;
+    return m_logParamWidgetCreator ? m_logParamWidgetCreator(editor->toolBar()) : 0;
 }
 
 VcsBaseClient::StatusItem::StatusItem(const QString &s, const QString &f) :
@@ -425,19 +425,21 @@ void VcsBaseClient::diff(const QString &workingDir, const QStringList &files,
                                                   vcsCmdString.toLatin1().constData(), id);
     editor->setWorkingDirectory(workingDir);
 
-    VcsBaseEditorParameterWidget *paramWidget = editor->configurationWidget();
-    if (!paramWidget && (paramWidget = d->createDiffEditor())) {
-        // editor has been just created, createVcsEditor() didn't set a configuration widget yet
-        connect(editor, &VcsBaseEditorWidget::diffChunkReverted,
-                paramWidget, &VcsBaseEditorParameterWidget::executeCommand);
-        connect(paramWidget, &VcsBaseEditorParameterWidget::commandExecutionRequested,
-                [=] { diff(workingDir, files, extraOptions); } );
-        editor->setConfigurationWidget(paramWidget);
+    QStringList effectiveArgs = extraOptions;
+    if (!editor->configurationAdded()) {
+        if (VcsBaseEditorParameterWidget *paramWidget = d->createDiffEditor(editor)) {
+            // editor has been just created, createVcsEditor() didn't set a configuration widget yet
+            connect(editor, &VcsBaseEditorWidget::diffChunkReverted,
+                    paramWidget, &VcsBaseEditorParameterWidget::executeCommand);
+            connect(paramWidget, &VcsBaseEditorParameterWidget::commandExecutionRequested,
+                [=] { diff(workingDir, files, extraOptions + paramWidget->arguments()); } );
+            effectiveArgs = paramWidget->arguments();
+            editor->setConfigurationAdded();
+        }
     }
 
     QStringList args;
-    const QStringList paramArgs = paramWidget != 0 ? paramWidget->arguments() : QStringList();
-    args << vcsCmdString << extraOptions << paramArgs << files;
+    args << vcsCmdString << effectiveArgs << files;
     QTextCodec *codec = source.isEmpty() ? static_cast<QTextCodec *>(0) : VcsBaseEditor::getCodec(source);
     VcsCommand *command = createCommand(workingDir, editor);
     command->setCodec(codec);
@@ -458,17 +460,20 @@ void VcsBaseClient::log(const QString &workingDir, const QStringList &files,
                                                   vcsCmdString.toLatin1().constData(), id);
     editor->setFileLogAnnotateEnabled(enableAnnotationContextMenu);
 
-    VcsBaseEditorParameterWidget *paramWidget = editor->configurationWidget();
-    if (!paramWidget && (paramWidget = d->createLogEditor())) {
-        // editor has been just created, createVcsEditor() didn't set a configuration widget yet
-        connect(paramWidget, &VcsBaseEditorParameterWidget::commandExecutionRequested,
-                [=]() { this->log(workingDir, files, extraOptions, enableAnnotationContextMenu); } );
-        editor->setConfigurationWidget(paramWidget);
+    QStringList effectiveArgs = extraOptions;
+    if (!editor->configurationAdded()) {
+        if (VcsBaseEditorParameterWidget *paramWidget = d->createLogEditor(editor)) {
+            // editor has been just created, createVcsEditor() didn't set a configuration widget yet
+            connect(paramWidget, &VcsBaseEditorParameterWidget::commandExecutionRequested,
+                [=]() { this->log(workingDir, files, extraOptions + paramWidget->arguments(),
+                                  enableAnnotationContextMenu); } );
+            effectiveArgs = paramWidget->arguments();
+            editor->setConfigurationAdded();
+        }
     }
 
     QStringList args;
-    const QStringList paramArgs = paramWidget != 0 ? paramWidget->arguments() : QStringList();
-    args << vcsCmdString << extraOptions << paramArgs << files;
+    args << vcsCmdString << effectiveArgs << files;
     enqueueJob(createCommand(workingDir, editor), args);
 }
 

src/plugins/vcsbase/vcsbaseeditor.cpp
@@ -565,7 +565,7 @@ public:
     QString m_annotateRevisionTextFormat;
     QString m_annotatePreviousRevisionTextFormat;
     QString m_copyRevisionTextFormat;
-    VcsBaseEditorParameterWidget *m_configurationWidget = nullptr;
+    bool m_configurationAdded = false;
     QList<AbstractTextCursorHandler *> m_textCursorHandlers;
     QPointer<VcsCommand> m_command;
     QObject *m_describeReceiver = nullptr;
@@ -1390,20 +1390,14 @@ QString VcsBaseEditor::getTitleId(const QString &workingDirectory,
     return rc;
 }
 
-bool VcsBaseEditorWidget::setConfigurationWidget(VcsBaseEditorParameterWidget *w)
+void VcsBaseEditorWidget::setConfigurationAdded()
 {
-    if (d->m_configurationWidget)
-        return false;
-
-    d->m_configurationWidget = w;
-    insertExtraToolBarWidget(TextEditorWidget::Right, w);
-
-    return true;
+    d->m_configurationAdded = true;
 }
 
-VcsBaseEditorParameterWidget *VcsBaseEditorWidget::configurationWidget() const
+bool VcsBaseEditorWidget::configurationAdded() const
 {
-    return d->m_configurationWidget;
+    return d->m_configurationAdded;
 }
 
 void VcsBaseEditorWidget::setCommand(VcsCommand *command)

src/plugins/vcsbase/vcsbaseeditor.h
@@ -209,8 +209,8 @@ public:
 
     EditorContentType contentType() const;
 
-    bool setConfigurationWidget(VcsBaseEditorParameterWidget *w);
-    VcsBaseEditorParameterWidget *configurationWidget() const;
+    void setConfigurationAdded();
+    bool configurationAdded() const;
 
     void setCommand(VcsCommand *command);
 

src/plugins/bazaar/bazaarclient.cpp
@@ -50,8 +50,8 @@ class BazaarDiffParameterWidget : public VcsBaseEditorParameterWidget
 {
     Q_OBJECT
 public:
-    BazaarDiffParameterWidget(VcsBaseClientSettings &settings, QWidget *parent = 0) :
-        VcsBaseEditorParameterWidget(parent)
+    BazaarDiffParameterWidget(VcsBaseClientSettings &settings, QToolBar *toolBar) :
+        VcsBaseEditorParameterWidget(toolBar)
     {
         mapSetting(addToggleButton(QLatin1String("-w"), tr("Ignore Whitespace")),
                    settings.boolPointer(BazaarSettings::diffIgnoreWhiteSpaceKey));
@@ -77,8 +77,8 @@ class BazaarLogParameterWidget : public VcsBaseEditorParameterWidget
 {
     Q_OBJECT
 public:
-    BazaarLogParameterWidget(VcsBaseClientSettings &settings, QWidget *parent = 0) :
-        VcsBaseEditorParameterWidget(parent)
+    BazaarLogParameterWidget(VcsBaseClientSettings &settings, QToolBar *toolBar) :
+        VcsBaseEditorParameterWidget(toolBar)
     {
         mapSetting(addToggleButton(QLatin1String("--verbose"), tr("Verbose"),
                                    tr("Show files changed in each revision.")),
@@ -102,8 +102,12 @@ public:
 
 BazaarClient::BazaarClient() : VcsBaseClient(new BazaarSettings)
 {
-    setDiffParameterWidgetCreator([this] { return new BazaarDiffParameterWidget(settings()); });
-    setLogParameterWidgetCreator([this] { return new BazaarLogParameterWidget(settings()); });
+    setDiffParameterWidgetCreator([this](QToolBar *toolBar) {
+        return new BazaarDiffParameterWidget(settings(), toolBar);
+    });
+    setLogParameterWidgetCreator([this](QToolBar *toolBar) {
+        return new BazaarLogParameterWidget(settings(), toolBar);
+    });
 }
 
 bool BazaarClient::synchronousSetUserId()

src/plugins/cvs/cvsclient.cpp
@@ -47,16 +47,15 @@ class CvsDiffParameterWidget : public VcsBaseEditorParameterWidget
 {
     Q_OBJECT
 public:
-    explicit CvsDiffParameterWidget(VcsBaseClientSettings &settings, QWidget *parent = 0);
+    CvsDiffParameterWidget(VcsBaseClientSettings &settings, QToolBar *toolBar);
     QStringList arguments() const;
 
 private:
     VcsBaseClientSettings &m_settings;
 };
 
-CvsDiffParameterWidget::CvsDiffParameterWidget(VcsBaseClientSettings &settings,
-                                               QWidget *parent) :
-    VcsBaseEditorParameterWidget(parent),
+CvsDiffParameterWidget::CvsDiffParameterWidget(VcsBaseClientSettings &settings, QToolBar *toolBar) :
+    VcsBaseEditorParameterWidget(toolBar),
     m_settings(settings)
 {
     mapSetting(addToggleButton(QLatin1String("-w"), tr("Ignore Whitespace")),
@@ -76,7 +75,9 @@ QStringList CvsDiffParameterWidget::arguments() const
 
 CvsClient::CvsClient() : VcsBaseClient(new CvsSettings)
 {
-    setDiffParameterWidgetCreator([this] { return new CvsDiffParameterWidget(settings()); });
+    setDiffParameterWidgetCreator([this](QToolBar *toolBar) {
+        return new CvsDiffParameterWidget(settings(), toolBar);
+    });
 }
 
 CvsSettings &CvsClient::settings() const

src/plugins/mercurial/mercurialclient.cpp
@@ -54,8 +54,8 @@ class MercurialDiffParameterWidget : public VcsBaseEditorParameterWidget
 {
     Q_OBJECT
 public:
-    MercurialDiffParameterWidget(VcsBaseClientSettings &settings, QWidget *parent = 0) :
-        VcsBaseEditorParameterWidget(parent)
+    MercurialDiffParameterWidget(VcsBaseClientSettings &settings, QToolBar *toolBar) :
+        VcsBaseEditorParameterWidget(toolBar)
     {
         mapSetting(addToggleButton(QLatin1String("-w"), tr("Ignore Whitespace")),
                    settings.boolPointer(MercurialSettings::diffIgnoreWhiteSpaceKey));
@@ -66,7 +66,9 @@ public:
 
 MercurialClient::MercurialClient() : VcsBaseClient(new MercurialSettings)
 {
-    setDiffParameterWidgetCreator([this] { return new MercurialDiffParameterWidget(settings()); });
+    setDiffParameterWidgetCreator([this](QToolBar *toolBar) {
+        return new MercurialDiffParameterWidget(settings(), toolBar);
+    });
 }
 
 bool MercurialClient::manifestSync(const QString &repository, const QString &relativeFilename)

commit a36536813f9aa00944fd8fcab2e2689a385d5387

VCS: Set user choices on editor reload
Broke by 2c6adc1e74.

Change-Id: Iba435899c7b0da4b270f4ef8ece84c1839bf6b72
Reviewed-by: André Hartmann <aha_1980@gmx.de>
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/plugins/git/gitclient.cpp
@@ -839,14 +839,13 @@ void GitClient::log(const QString &workingDirectory, const QString &fileName,
     const QString sourceFile = VcsBaseEditor::getSource(workingDir, fileName);
     VcsBaseEditorWidget *editor = createVcsEditor(editorId, title, sourceFile,
                                                   codecFor(CodecLogOutput), "logTitle", msgArg);
-    QStringList effectiveArgs = args;
-    if (!editor->configurationAdded()) {
-        auto *argWidget = new GitLogArgumentsWidget(settings(), editor->toolBar());
+    VcsBaseEditorConfig *argWidget = editor->editorConfig();
+    if (!argWidget) {
+        argWidget = new GitLogArgumentsWidget(settings(), editor->toolBar());
         argWidget->setBaseArguments(args);
         connect(argWidget, &VcsBaseEditorConfig::commandExecutionRequested,
-                [=]() { this->log(workingDir, fileName, enableAnnotationContextMenu, argWidget->arguments()); });
-        effectiveArgs = argWidget->arguments();
-        editor->setConfigurationAdded();
+                [=]() { this->log(workingDir, fileName, enableAnnotationContextMenu, args); });
+        editor->setEditorConfig(argWidget);
     }
     editor->setFileLogAnnotateEnabled(enableAnnotationContextMenu);
     editor->setWorkingDirectory(workingDir);
@@ -856,7 +855,7 @@ void GitClient::log(const QString &workingDirectory, const QString &fileName,
     if (logCount > 0)
         arguments << "-n" << QString::number(logCount);
 
-    arguments.append(effectiveArgs);
+    arguments << argWidget->arguments();
 
     if (!fileName.isEmpty())
         arguments << "--follow" << "--" << fileName;
@@ -926,22 +925,21 @@ VcsBaseEditorWidget *GitClient::annotate(
     VcsBaseEditorWidget *editor
             = createVcsEditor(editorId, title, sourceFile, codecFor(CodecSource, sourceFile),
                               "blameFileName", id);
-    QStringList effectiveArgs = extraOptions;
-    if (!editor->configurationAdded()) {
-        auto *argWidget = new GitBlameArgumentsWidget(settings(), editor->toolBar());
+    VcsBaseEditorConfig *argWidget = editor->editorConfig();
+    if (!argWidget) {
+        argWidget = new GitBlameArgumentsWidget(settings(), editor->toolBar());
         argWidget->setBaseArguments(extraOptions);
         connect(argWidget, &VcsBaseEditorConfig::commandExecutionRequested,
                 [=] {
                     const int line = VcsBaseEditor::lineNumberOfCurrentEditor();
-                    annotate(workingDir, file, revision, line, argWidget->arguments());
+                    annotate(workingDir, file, revision, line, extraOptions);
                 } );
-        effectiveArgs = argWidget->arguments();
-        editor->setConfigurationAdded();
+        editor->setEditorConfig(argWidget);
     }
 
     editor->setWorkingDirectory(workingDir);
     QStringList arguments = {"blame", "--root"};
-    arguments << effectiveArgs << "--" << file;
+    arguments << argWidget->arguments() << "--" << file;
     if (!revision.isEmpty())
         arguments << revision;
     vcsExec(workingDir, arguments, editor, false, 0, lineNumber);

src/plugins/perforce/perforceplugin.cpp
@@ -1275,7 +1275,7 @@ void PerforcePlugin::p4Diff(const PerforceDiffParameters &p)
             this, [this](const PerforceDiffParameters &p) { p4Diff(p); });
     connect(diffEditorWidget, &VcsBaseEditorWidget::diffChunkReverted,
             pw, &PerforceDiffConfig::triggerReRun);
-    diffEditorWidget->setConfigurationAdded();
+    diffEditorWidget->setEditorConfig(pw);
 }
 
 void PerforcePlugin::describe(const QString & source, const QString &n)

src/plugins/vcsbase/vcsbaseclient.cpp
@@ -433,21 +433,26 @@ void VcsBaseClient::diff(const QString &workingDir, const QStringList &files,
                                                   vcsCmdString.toLatin1().constData(), id);
     editor->setWorkingDirectory(workingDir);
 
-    QStringList effectiveArgs = extraOptions;
-    if (!editor->configurationAdded()) {
-        if (VcsBaseEditorConfig *paramWidget = d->createDiffEditor(editor)) {
+    VcsBaseEditorConfig *paramWidget = editor->editorConfig();
+    if (!paramWidget) {
+        paramWidget = d->createDiffEditor(editor);
+        if (paramWidget) {
+            paramWidget->setBaseArguments(extraOptions);
             // editor has been just created, createVcsEditor() didn't set a configuration widget yet
             connect(editor, &VcsBaseEditorWidget::diffChunkReverted,
                     paramWidget, &VcsBaseEditorConfig::executeCommand);
             connect(paramWidget, &VcsBaseEditorConfig::commandExecutionRequested,
-                [=] { diff(workingDir, files, extraOptions + paramWidget->arguments()); } );
-            effectiveArgs = paramWidget->arguments();
-            editor->setConfigurationAdded();
+                [=] { diff(workingDir, files, extraOptions); } );
+            editor->setEditorConfig(paramWidget);
         }
     }
 
-    QStringList args;
-    args << vcsCmdString << effectiveArgs << files;
+    QStringList args = {vcsCmdString};
+    if (paramWidget)
+        args << paramWidget->arguments();
+    else
+        args << extraOptions;
+    args << files;
     QTextCodec *codec = source.isEmpty() ? static_cast<QTextCodec *>(0) : VcsBaseEditor::getCodec(source);
     VcsCommand *command = createCommand(workingDir, editor);
     command->setCodec(codec);
@@ -468,20 +473,24 @@ void VcsBaseClient::log(const QString &workingDir, const QStringList &files,
                                                   vcsCmdString.toLatin1().constData(), id);
     editor->setFileLogAnnotateEnabled(enableAnnotationContextMenu);
 
-    QStringList effectiveArgs = extraOptions;
-    if (!editor->configurationAdded()) {
-        if (VcsBaseEditorConfig *paramWidget = d->createLogEditor(editor)) {
+    VcsBaseEditorConfig *paramWidget = editor->editorConfig();
+    if (!paramWidget) {
+        paramWidget = d->createLogEditor(editor);
+        if (paramWidget) {
+            paramWidget->setBaseArguments(extraOptions);
             // editor has been just created, createVcsEditor() didn't set a configuration widget yet
             connect(paramWidget, &VcsBaseEditorConfig::commandExecutionRequested,
-                [=]() { this->log(workingDir, files, extraOptions + paramWidget->arguments(),
-                                  enableAnnotationContextMenu); } );
-            effectiveArgs = paramWidget->arguments();
-            editor->setConfigurationAdded();
+                [=] { this->log(workingDir, files, extraOptions, enableAnnotationContextMenu); } );
+            editor->setEditorConfig(paramWidget);
         }
     }
 
-    QStringList args;
-    args << vcsCmdString << effectiveArgs << files;
+    QStringList args = {vcsCmdString};
+    if (paramWidget)
+        args << paramWidget->arguments();
+    else
+        args << extraOptions;
+    args << files;
     enqueueJob(createCommand(workingDir, editor), args);
 }
 

src/plugins/vcsbase/vcsbaseeditor.cpp
@@ -559,7 +559,7 @@ public:
     QString m_annotateRevisionTextFormat;
     QString m_annotatePreviousRevisionTextFormat;
     QString m_copyRevisionTextFormat;
-    bool m_configurationAdded = false;
+    VcsBaseEditorConfig *m_config = nullptr;
     QList<AbstractTextCursorHandler *> m_textCursorHandlers;
     QPointer<VcsCommand> m_command;
     VcsBaseEditorWidget::DescribeFunc m_describeFunc = nullptr;
@@ -1380,14 +1380,14 @@ QString VcsBaseEditor::getTitleId(const QString &workingDirectory,
     return rc;
 }
 
-void VcsBaseEditorWidget::setConfigurationAdded()
+void VcsBaseEditorWidget::setEditorConfig(VcsBaseEditorConfig *config)
 {
-    d->m_configurationAdded = true;
+    d->m_config = config;
 }
 
-bool VcsBaseEditorWidget::configurationAdded() const
+VcsBaseEditorConfig *VcsBaseEditorWidget::editorConfig() const
 {
-    return d->m_configurationAdded;
+    return d->m_config;
 }
 
 void VcsBaseEditorWidget::setCommand(VcsCommand *command)

src/plugins/vcsbase/vcsbaseeditor.h
@@ -207,8 +207,8 @@ public:
 
     EditorContentType contentType() const;
 
-    void setConfigurationAdded();
-    bool configurationAdded() const;
+    void setEditorConfig(VcsBaseEditorConfig *config);
+    VcsBaseEditorConfig *editorConfig() const;
 
     void setCommand(VcsCommand *command);
 