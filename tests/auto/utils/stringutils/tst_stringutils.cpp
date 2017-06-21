qt-creator/qt-creator

commit 4e96f2ce22e1a01b08b196e9b940034d50b87460

MacroExpander: Prevent loop with subproviders
Prevent a loop where a subprovider of a macroexpander creates
the macroexpander itself.

Change-Id: Id2f3e29651aeb22c818091d8c785a6ea01545463
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/libs/utils/macroexpander.cpp
@@ -53,16 +53,22 @@ public:
         : m_accumulating(false), m_aborted(false), m_lockDepth(0)
     {}
 
-    bool resolveMacro(const QString &name, QString *ret)
+    bool resolveMacro(const QString &name, QString *ret, QSet<AbstractMacroExpander *> &seen)
     {
+        // Prevent loops:
+        const int count = seen.count();
+        seen.insert(this);
+        if (seen.count() == count)
+            return false;
+
         bool found;
         *ret = value(name.toUtf8(), &found);
         if (found)
             return true;
 
-        found = Utils::anyOf(m_subProviders, [name, ret] (const MacroExpanderProvider &p) -> bool {
+        found = Utils::anyOf(m_subProviders, [name, ret, &seen] (const MacroExpanderProvider &p) -> bool {
             MacroExpander *expander = p ? p() : 0;
-            return expander && expander->resolveMacro(name, ret);
+            return expander && expander->d->resolveMacro(name, ret, seen);
         });
 
         if (found)
@@ -75,7 +81,7 @@ public:
         if (found)
             return true;
 
-        return this == globalMacroExpander()->d ? false : globalMacroExpander()->d->resolveMacro(name, ret);
+        return this == globalMacroExpander()->d ? false : globalMacroExpander()->d->resolveMacro(name, ret, seen);
     }
 
     QString value(const QByteArray &variable, bool *found)
@@ -243,7 +249,8 @@ MacroExpander::~MacroExpander()
  */
 bool MacroExpander::resolveMacro(const QString &name, QString *ret) const
 {
-    return d->resolveMacro(name, ret);
+    QSet<AbstractMacroExpander*> seen;
+    return d->resolveMacro(name, ret, seen);
 }
 
 /*!

src/libs/utils/stringutils.cpp
@@ -31,6 +31,7 @@
 
 #include <QDir>
 #include <QRegularExpression>
+#include <QSet>
 
 #include <limits.h>
 
@@ -153,7 +154,8 @@ bool AbstractMacroExpander::expandNestedMacros(const QString &str, int *pos, QSt
                 *pos = i;
                 return true;
             }
-            if (resolveMacro(varName, ret)) {
+            QSet<AbstractMacroExpander*> seen;
+            if (resolveMacro(varName, ret, seen)) {
                 *pos = i;
                 if (!pattern.isEmpty() && currArg == &replace) {
                     const QRegularExpression regexp(pattern);

src/libs/utils/stringutils.h
@@ -65,7 +65,7 @@ public:
     //! \param name The name of the expando
     //! \param ret Replacement string on output
     //! \return True if the expando was found
-    virtual bool resolveMacro(const QString &name, QString *ret) = 0;
+    virtual bool resolveMacro(const QString &name, QString *ret, QSet<AbstractMacroExpander *> &seen) = 0;
 private:
     bool expandNestedMacros(const QString &str, int *pos, QString *ret);
 };

commit bd259129b5b50d2a816d2d99a1b45bedb57e7563

Tests: Fix compile
Broke with 4e96f2ce.

Change-Id: I7c467cc92cc8444bd3b1ef41eebff089668a0f31
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

tests/auto/qtcprocess/tst_qtcprocess.cpp
@@ -34,8 +34,14 @@ using namespace Utils;
 
 class MacroMapExpander : public AbstractMacroExpander {
 public:
-    virtual bool resolveMacro(const QString &name, QString *ret)
+    virtual bool resolveMacro(const QString &name, QString *ret, QSet<AbstractMacroExpander*> &seen)
     {
+        // loop prevention
+        const int count = seen.count();
+        seen.insert(this);
+        if (seen.count() == count)
+            return false;
+
         QHash<QString, QString>::const_iterator it = m_map.constFind(name);
         if (it != m_map.constEnd()) {
             *ret = it.value();

tests/auto/utils/stringutils/tst_stringutils.cpp
@@ -32,8 +32,14 @@
 class TestMacroExpander : public Utils::AbstractMacroExpander
 {
 public:
-    virtual bool resolveMacro(const QString &name, QString *ret)
+    virtual bool resolveMacro(const QString &name, QString *ret, QSet<AbstractMacroExpander*> &seen)
     {
+        // loop prevention
+        const int count = seen.count();
+        seen.insert(this);
+        if (seen.count() == count)
+            return false;
+
         if (name == QLatin1String("foo")) {
             *ret = QLatin1String("a");
             return true;