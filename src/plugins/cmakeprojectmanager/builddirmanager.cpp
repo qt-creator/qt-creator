qt-creator/qt-creator

commit e42f3db9f0ab1ec28bf9ba414add84d3b552118f

CMake: Avoid duplicate build targets
Make sure that build targets that get added explicitly are never reported
by any of the readers, too. This makes sure entries will not show up twice
in the UI.

Task-number: QTCREATORBUG-18409
Change-Id: Id5039add262211fd87cd5c2884df2af0fcf24577
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/plugins/cmakeprojectmanager/builddirmanager.cpp
@@ -336,7 +336,12 @@ QList<CMakeBuildTarget> BuildDirManager::buildTargets() const
         m_buildTargets.append(utilityTarget(CMakeBuildStep::installTarget(), this));
         m_buildTargets.append(utilityTarget(CMakeBuildStep::testTarget(), this));
 
-        m_buildTargets.append(m_reader->buildTargets());
+        m_buildTargets.append(Utils::filtered(m_reader->buildTargets(), [](const CMakeBuildTarget &bt) {
+            return bt.title == CMakeBuildStep::allTarget()
+                    || bt.title == CMakeBuildStep::cleanTarget()
+                    || bt.title == CMakeBuildStep::installTarget()
+                    || bt.title == CMakeBuildStep::testTarget();
+        }));
     }
     return m_buildTargets;
 }

commit 8ede32677e2dd86e3a7f474d33c1656fe3ea0f9b

CMake: Fix logic for target filtering
Broken by e42f3db9f0ab1ec28bf9ba414add84d3b552118f from today:-/ Sorry.

Change-Id: I4803039939de5aae893a701e2bd6399d3fbfae1f
Reviewed-by: Florian Apolloner <florian@apolloner.eu>
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/plugins/cmakeprojectmanager/builddirmanager.cpp
@@ -337,10 +337,10 @@ QList<CMakeBuildTarget> BuildDirManager::buildTargets() const
         m_buildTargets.append(utilityTarget(CMakeBuildStep::testTarget(), this));
 
         m_buildTargets.append(Utils::filtered(m_reader->buildTargets(), [](const CMakeBuildTarget &bt) {
-            return bt.title == CMakeBuildStep::allTarget()
-                    || bt.title == CMakeBuildStep::cleanTarget()
-                    || bt.title == CMakeBuildStep::installTarget()
-                    || bt.title == CMakeBuildStep::testTarget();
+            return bt.title != CMakeBuildStep::allTarget()
+                    && bt.title != CMakeBuildStep::cleanTarget()
+                    && bt.title != CMakeBuildStep::installTarget()
+                    && bt.title != CMakeBuildStep::testTarget();
         }));
     }
     return m_buildTargets;