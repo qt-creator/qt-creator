1607a34a9aee965f65b8e57817c52c6c10c9ef41
Ivan Donchevskii <ivan.donchevskii@qt.io>
2017-06-29 15:04:01 +0200

    Utils: use unique_ptr for QFile object
    Do not use raw pointer.
    
    Change-Id: I6810c3ec3a60a43829130cf5e5a00e52db95b965
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/libs/utils/fileutils.cpp
+++ src/libs/utils/fileutils.cpp
@@ -395,18 +395,11 @@ FileSaverBase::FileSaverBase()
 {
 }
 
-FileSaverBase::~FileSaverBase()
-{
-    delete m_file;
-}
-
 bool FileSaverBase::finalize()
 {
     m_file->close();
     setResult(m_file->error() == QFile::NoError);
-    // We delete the object, so it is really closed even if it is a QTemporaryFile.
-    delete m_file;
-    m_file = 0;
+    m_file.reset();
     return !m_hasError;
 }
 
@@ -488,10 +481,10 @@ FileSaver::FileSaver(const QString &filename, QIODevice::OpenMode mode)
         }
     }
     if (mode & (QIODevice::ReadOnly | QIODevice::Append)) {
-        m_file = new QFile(filename);
+        m_file.reset(new QFile{filename});
         m_isSafe = false;
     } else {
-        m_file = new SaveFile(filename);
+        m_file.reset(new SaveFile{filename});
         m_isSafe = true;
     }
     if (!m_file->open(QIODevice::WriteOnly | mode)) {
@@ -507,22 +500,22 @@ bool FileSaver::finalize()
     if (!m_isSafe)
         return FileSaverBase::finalize();
 
-    SaveFile *sf = static_cast<SaveFile *>(m_file);
+    SaveFile *sf = static_cast<SaveFile *>(m_file.get());
     if (m_hasError) {
         if (sf->isOpen())
             sf->rollback();
     } else {
         setResult(sf->commit());
     }
-    delete sf;
-    m_file = 0;
+    m_file.reset();
     return !m_hasError;
 }
 
 TempFileSaver::TempFileSaver(const QString &templ)
     : m_autoRemove(true)
 {
-    QTemporaryFile *tempFile = new QTemporaryFile();
+    m_file.reset(new QTemporaryFile{});
+    QTemporaryFile *tempFile = static_cast<QTemporaryFile *>(m_file.get());
     if (!templ.isEmpty())
         tempFile->setFileTemplate(templ);
     tempFile->setAutoRemove(false);
@@ -532,14 +525,12 @@ TempFileSaver::TempFileSaver(const QString &templ)
                 tempFile->errorString());
         m_hasError = true;
     }
-    m_file = tempFile;
     m_fileName = tempFile->fileName();
 }
 
 TempFileSaver::~TempFileSaver()
 {
-    delete m_file;
-    m_file = 0;
+    m_file.reset();
     if (m_autoRemove)
         QFile::remove(m_fileName);
 }
