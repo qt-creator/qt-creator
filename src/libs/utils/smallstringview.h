qt-creator/qt-creator

commit dae4477cd367d115e2011cfedae6b1d5dbc22cb7

Clang: Make file ids unique
Clang file ids are only unique for one query. Because we query in parallel
we have to manage our own unique ids.

Change-Id: I67d57d8b1766cab75ad252a14e57bbf9dc5fdb79
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/libs/utils/smallstringview.h
@@ -104,6 +104,11 @@ public:
         return const_reverse_iterator(begin() - static_cast<std::size_t>(1));
     }
 
+    operator std::string() const
+    {
+        return std::string(data(), size());
+    }
+
 private:
     const char *m_pointer;
     size_type m_size;

src/libs/clangbackendipc/clangbackendipc-lib.pri
@@ -155,6 +155,7 @@ HEADERS += \
     $$PWD/pchmanagerclientproxy.h \
     $$PWD/projectpartpch.h \
     $$PWD/precompiledheadersupdatedmessage.h \
+    $$PWD/stringcache.h \
     $$PWD/removepchprojectpartsmessage.h \
     $$PWD/clangcodemodelclientmessages.h \
     $$PWD/clangcodemodelservermessages.h \

src/libs/clangbackendipc/filepath.h
@@ -37,66 +37,62 @@ class FilePath
 {
 public:
     FilePath() = default;
-    explicit FilePath(Utils::SmallString &&filePath)
+    explicit FilePath(Utils::PathString &&filePath)
+        : m_path(std::move(filePath))
     {
-        auto foundReverse = std::find(filePath.rbegin(), filePath.rend(), '/');
+        auto foundReverse = std::find(m_path.rbegin(), m_path.rend(), '/');
         auto found = foundReverse.base();
+        --found;
 
-        Utils::SmallString fileName(found, filePath.end());
-        if (foundReverse != filePath.rend())
-            filePath.resize(std::size_t(std::distance(filePath.begin(), --found)));
+        m_slashIndex = std::size_t(std::distance(m_path.begin(), found));
+    }
 
-        directory_ = std::move(filePath);
-        name_ = std::move(fileName);
+    explicit FilePath(const Utils::PathString &filePath)
+        : FilePath(filePath.clone())
+    {
+    }
+
+    explicit FilePath(Utils::PathString &&filePath, std::size_t slashIndex)
+        : m_path(std::move(filePath)),
+          m_slashIndex(slashIndex)
+    {
     }
 
     explicit FilePath(const QString &filePath)
-        : FilePath(Utils::SmallString(filePath))
+        : FilePath(Utils::PathString(filePath))
     {
     }
 
-    FilePath(Utils::SmallString &&directory, Utils::SmallString &&name)
-        : directory_(std::move(directory)),
-          name_(std::move(name))
+    FilePath(const Utils::PathString &directory, const Utils::PathString &name)
+        : m_path({std::move(directory), "/", std::move(name)}),
+          m_slashIndex(directory.size())
     {}
 
-    const Utils::SmallString &directory() const
+    Utils::SmallStringView directory() const
     {
-        return directory_;
+        return m_path.mid(0, m_slashIndex);
     }
 
-    Utils::SmallString takeDirectory()
+    Utils::SmallStringView name() const
     {
-        return std::move(directory_);
+        return m_path.mid(m_slashIndex + 1, m_path.size() - m_slashIndex - 1);
     }
 
-    const Utils::SmallString &name() const
+    const Utils::PathString &path()  const
     {
-        return name_;
-    }
-
-    Utils::SmallString takeName()
-    {
-        return std::move(name_);
-    }
-
-    Utils::PathString path()  const
-    {
-        return {directory_, "/", name_};
+        return m_path;
     }
 
     friend QDataStream &operator<<(QDataStream &out, const FilePath &filePath)
     {
-        out << filePath.directory_;
-        out << filePath.name_;
+        out << filePath.m_path;
 
         return out;
     }
 
     friend QDataStream &operator>>(QDataStream &in, FilePath &filePath)
     {
-        in >> filePath.directory_;
-        in >> filePath.name_;
+        in >> filePath.m_path;
 
         return in;
     }
@@ -110,24 +106,22 @@ public:
 
     friend bool operator==(const FilePath &first, const FilePath &second)
     {
-        return first.name_ == second.name_
-            && first.directory_ == second.directory_;
+        return first.m_path == second.m_path;
     }
 
     friend bool operator<(const FilePath &first, const FilePath &second)
     {
-        return std::tie(first.name_, first.directory_)
-             < std::tie(second.name_, second.directory_);
+        return first.m_path < second.m_path;
     }
 
     FilePath clone() const
     {
-        return FilePath(directory_.clone(), name_.clone());
+        return FilePath(m_path.clone(), m_slashIndex);
     }
 
 private:
-    Utils::SmallString directory_;
-    Utils::SmallString name_;
+    Utils::PathString m_path = "/";
+    std::size_t m_slashIndex = 0;
 };
 
 CMBIPC_EXPORT QDebug operator<<(QDebug debug, const FilePath &filePath);

src/libs/clangbackendipc/sourcefilepathcontainerbase.h
@@ -42,13 +42,10 @@ public:
     {
     }
 
-    void insertFilePath(uint fileId, Utils::SmallString &&fileDirectory, Utils::SmallString &&fileName)
+    void insertFilePath(uint fileId, Utils::PathString &&filePath)
     {
-        if (m_filePathHash.find(fileId) == m_filePathHash.end()) {
-            m_filePathHash.emplace(std::piecewise_construct,
-                                 std::forward_as_tuple(fileId),
-                                 std::forward_as_tuple(std::move(fileDirectory), std::move(fileName)));
-        }
+        if (m_filePathHash.find(fileId) == m_filePathHash.end())
+            m_filePathHash.emplace(fileId, std::move(filePath));
     }
 
     void reserve(std::size_t size)

src/libs/clangbackendipc/sourcerangewithtextcontainer.h
@@ -110,6 +110,8 @@ private:
     Utils::SmallString m_text;
 };
 
+using SourceRangeWithTextContainers = std::vector<SourceRangeWithTextContainer>;
+
 CMBIPC_EXPORT QDebug operator<<(QDebug debug, const SourceRangeWithTextContainer &container);
 std::ostream &operator<<(std::ostream &os, const SourceRangeWithTextContainer &container);
 } // namespace ClangBackEnd

src/libs/clangbackendipc/stringcache.h
@@ -25,16 +25,26 @@
 
 #pragma once
 
-#include "clangpchmanagerbackend_global.h"
-
 #include <utils/smallstringview.h>
 
 #include <algorithm>
+#include <mutex>
 #include <vector>
 
 namespace ClangBackEnd {
 
-template <typename StringType>
+class NonLockingMutex
+{
+public:
+    constexpr NonLockingMutex() noexcept {}
+    NonLockingMutex(const NonLockingMutex&) = delete;
+    NonLockingMutex& operator=(const NonLockingMutex&) = delete;
+    void lock() {}
+    void unlock() {}
+};
+
+template <typename StringType,
+          typename Mutex = NonLockingMutex>
 class StringCache
 {
     class StringCacheEntry
@@ -79,6 +89,8 @@ public:
 
     uint stringId(Utils::SmallStringView stringView)
     {
+        std::lock_guard<Mutex> lock(m_mutex);
+
         Found found = find(stringView);
 
         if (!found.wasFound)
@@ -89,6 +101,8 @@ public:
 
     std::vector<uint> stringIds(const std::vector<StringType> &strings)
     {
+        std::lock_guard<Mutex> lock(m_mutex);
+
         std::vector<uint> ids;
         ids.reserve(strings.size());
 
@@ -102,11 +116,15 @@ public:
 
     const StringType &string(uint id) const
     {
+        std::lock_guard<Mutex> lock(m_mutex);
+
         return m_strings.at(m_indices.at(id)).string;
     }
 
     std::vector<StringType> strings(const std::vector<uint> &ids) const
     {
+        std::lock_guard<Mutex> lock(m_mutex);
+
         std::vector<StringType> strings;
         strings.reserve(ids.size());
 
@@ -155,6 +173,7 @@ private:
 private:
     StringCacheEntries m_strings;
     std::vector<uint> m_indices;
+    mutable Mutex m_mutex;
 };
 
 } // namespace ClangBackEnd

src/libs/utils/smallstringio.h
@@ -68,8 +68,8 @@ QDataStream &operator>>(QDataStream &in, BasicSmallString<Size> &string)
     return in;
 }
 
-inline
-QDebug &operator<<(QDebug &debug, const SmallString &string)
+template <typename String>
+QDebug &operator<<(QDebug &debug, const String &string)
 {
     using QT_PREPEND_NAMESPACE(operator<<);
 

src/plugins/clangrefactoring/refactoringclient.cpp
@@ -104,19 +104,6 @@ void RefactoringClient::setRefactoringConnectionClient(
     this->connectionClient = connectionClient;
 }
 
-namespace {
-
-Utils::SmallString concatenateFilePath(const ClangBackEnd::FilePath &filePath)
-{
-    Utils::SmallString concatenatedFilePath = filePath.directory().clone();
-    concatenatedFilePath.append("/");
-    concatenatedFilePath.append(filePath.name().clone());
-
-    return concatenatedFilePath;
-}
-
-}
-
 std::unordered_map<uint, QString> RefactoringClient::convertFilePaths(
         const ClangBackEnd::FilePathDict &filePaths)
 {
@@ -126,7 +113,7 @@ std::unordered_map<uint, QString> RefactoringClient::convertFilePaths(
 
     auto convertFilePath = [] (const ClangBackEnd::FilePathDict::value_type &dictonaryEntry) {
         return std::make_pair(dictonaryEntry.first,
-                              concatenateFilePath(dictonaryEntry.second).toQString());
+                              dictonaryEntry.second.path().toQString());
     };
 
     std::transform(filePaths.begin(),

commit 4ab18cde0e881eb1949ffd36ec2e52a951cec628

Fix build on Clang/macOS
Broke with dae4477cd367d115e2011cfedae6b1d5dbc22cb7

Change-Id: Ia948d7d70f9e30f2f59b38da231831c0e78c79e1
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/libs/utils/smallstringview.h
@@ -30,6 +30,7 @@
 #include <QtGlobal>
 
 #include <cstring>
+#include <string>
 
 namespace Utils {
 