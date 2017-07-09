e49ccd0747b9fa9699ac44da9b26e0a8f349b5f5
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2017-07-09 10:29:15 +0300

    Beautifier: Minor cleanup
    * Use initializer list and Utils::transform
    * Remove unneeded override
    
    Change-Id: I36ff9b9900fa6169ed1529bbbbdba13075353332
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/plugins/beautifier/generalsettings.cpp
+++ src/plugins/beautifier/generalsettings.cpp
@@ -28,6 +28,7 @@
 #include "beautifierconstants.h"
 
 #include <coreplugin/icore.h>
+#include <utils/algorithm.h>
 #include <utils/mimetypes/mimedatabase.h>
 
 namespace Beautifier {
@@ -99,11 +100,7 @@ QList<Utils::MimeType> GeneralSettings::autoFormatMime() const
 
 QString GeneralSettings::autoFormatMimeAsString() const
 {
-    QStringList types;
-    types.reserve(m_autoFormatMime.count());
-    for (auto t : m_autoFormatMime)
-        types << t.name();
-    return types.join("; ");
+    return Utils::transform(m_autoFormatMime, &Utils::MimeType::name).join("; ");
 }
 
 void GeneralSettings::setAutoFormatMime(const QList<Utils::MimeType> &autoFormatMime)
