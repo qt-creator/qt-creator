qt-creator/qt-creator

commit 89f02cba2ca65f84d99b0b35f555da024f3e2234

ProjectExplorer: Split Target and ToolRunners into smaller tasks
This increases re-usability of activities like 'port gathering',
and makes their use less dependent on actual device implementations.

Change-Id: I017cb74874f2b38c487ba2d03906a675d5618647
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/qnx/slog2inforunner.cpp
@@ -25,7 +25,9 @@
 
 #include "slog2inforunner.h"
 
+#include "qnxdevice.h"
 #include "qnxdeviceprocess.h"
+#include "qnxrunconfiguration.h"
 
 #include <projectexplorer/runnables.h>
 #include <utils/qtcassert.h>
@@ -33,28 +35,33 @@
 #include <QRegExp>
 
 using namespace ProjectExplorer;
+using namespace Utils;
 
 namespace Qnx {
 namespace Internal {
 
-Slog2InfoRunner::Slog2InfoRunner(const QString &applicationId,
-                                 const RemoteLinux::LinuxDevice::ConstPtr &device, QObject *parent)
-    : QObject(parent)
-    , m_applicationId(applicationId)
-    , m_found(false)
-    , m_currentLogs(false)
+Slog2InfoRunner::Slog2InfoRunner(RunControl *runControl)
+    : RunWorker(runControl)
 {
+    auto qnxRunConfig = qobject_cast<QnxRunConfiguration *>(runControl->runConfiguration());
+    QTC_ASSERT(qnxRunConfig, return);
+    m_applicationId = FileName::fromString(qnxRunConfig->remoteExecutableFilePath()).fileName();
+}
+
+void Slog2InfoRunner::printMissingWarning()
+{
+    appendMessage(tr("Warning: \"slog2info\" is not found on the device, debug output not available."), ErrorMessageFormat);
     // See QTCREATORBUG-10712 for details.
     // We need to limit length of ApplicationId to 63 otherwise it would not match one in slog2info.
     m_applicationId.truncate(63);
 
-    m_testProcess = new QnxDeviceProcess(device, this);
+    m_testProcess = new QnxDeviceProcess(device(), this);
     connect(m_testProcess, &DeviceProcess::finished, this, &Slog2InfoRunner::handleTestProcessCompleted);
 
-    m_launchDateTimeProcess = new SshDeviceProcess(device, this);
+    m_launchDateTimeProcess = new SshDeviceProcess(device(), this);
     connect(m_launchDateTimeProcess, &DeviceProcess::finished, this, &Slog2InfoRunner::launchSlog2Info);
 
-    m_logProcess = new QnxDeviceProcess(device, this);
+    m_logProcess = new QnxDeviceProcess(device(), this);
     connect(m_logProcess, &DeviceProcess::readyReadStandardOutput, this, &Slog2InfoRunner::readLogStandardOutput);
     connect(m_logProcess, &DeviceProcess::readyReadStandardError, this, &Slog2InfoRunner::readLogStandardError);
     connect(m_logProcess, &DeviceProcess::error, this, &Slog2InfoRunner::handleLogError);
@@ -88,10 +95,14 @@ bool Slog2InfoRunner::commandFound() const
 void Slog2InfoRunner::handleTestProcessCompleted()
 {
     m_found = (m_testProcess->exitCode() == 0);
-    if (m_found)
+    if (m_found) {
         readLaunchTime();
-    else
-        emit commandMissing();
+    } else {
+        QnxDevice::ConstPtr qnxDevice = device().dynamicCast<const QnxDevice>();
+        if (qnxDevice->qnxVersion() > 0x060500) {
+            printMissingWarning();
+        }
+    }
 }
 
 void Slog2InfoRunner::readLaunchTime()
@@ -174,18 +185,18 @@ void Slog2InfoRunner::processLogLine(const QString &line)
     if (bufferName == QLatin1String("default") && bufferId == 8900)
         return;
 
-    emit output(regexp.cap(6).trimmed() + QLatin1Char('\n'), Utils::StdOutFormat);
+    appendMessage(regexp.cap(6).trimmed() + '\n', Utils::StdOutFormat);
 }
 
 void Slog2InfoRunner::readLogStandardError()
 {
-    const QString message = QString::fromLatin1(m_logProcess->readAllStandardError());
-    emit output(message, Utils::StdErrFormat);
+    appendMessage(QString::fromLatin1(m_logProcess->readAllStandardError()), Utils::StdErrFormat);
 }
 
 void Slog2InfoRunner::handleLogError()
 {
-    emit output(tr("Cannot show slog2info output. Error: %1").arg(m_logProcess->errorString()), Utils::StdErrFormat);
+    appendMessage(tr("Cannot show slog2info output. Error: %1")
+                  .arg(m_logProcess->errorString()), Utils::StdErrFormat);
 }
 
 } // namespace Internal

src/libs/utils/port.h
@@ -27,8 +27,11 @@
 
 #include "utils_global.h"
 #include "qtcassert.h"
-#include <limits>
+
 #include <QMetaType>
+#include <QString>
+
+#include <limits>
 
 namespace Utils {
 
@@ -49,6 +52,8 @@ public:
 
     quint16 number() const { QTC_ASSERT(isValid(), return 0); return quint16(m_port); }
     bool isValid() const { return m_port != -1; }
+
+    QString toString() const { return QString::number(m_port); }
 
 private:
     int m_port;

src/plugins/android/androidanalyzesupport.cpp
@@ -48,7 +48,8 @@ namespace Internal {
 
 RunControl *AndroidAnalyzeSupport::createAnalyzeRunControl(RunConfiguration *runConfig, Core::Id runMode)
 {
-    RunControl *runControl = Debugger::createAnalyzerRunControl(runConfig, runMode);
+    auto runControl = new RunControl(runConfig, runMode);
+    runControl->createWorker(runMode);
     QTC_ASSERT(runControl, return 0);
     AnalyzerConnection connection;
     if (runMode == ProjectExplorer::Constants::QML_PROFILER_RUN_MODE) {
@@ -64,7 +65,7 @@ RunControl *AndroidAnalyzeSupport::createAnalyzeRunControl(RunConfiguration *run
 }
 
 AndroidAnalyzeSupport::AndroidAnalyzeSupport(RunControl *runControl)
-    : ToolRunner(runControl)
+    : RunWorker(runControl)
 {
     auto runner = new AndroidRunner(this, runControl->runConfiguration(), runControl->runMode());
 

src/plugins/android/androidanalyzesupport.h
@@ -34,7 +34,7 @@
 namespace Android {
 namespace Internal {
 
-class AndroidAnalyzeSupport : public ProjectExplorer::ToolRunner
+class AndroidAnalyzeSupport : public ProjectExplorer::RunWorker
 {
     Q_OBJECT
 

src/plugins/android/androidrunnable.h
@@ -40,6 +40,7 @@ struct ANDROID_EXPORT AndroidRunnable
     QVector<QStringList> afterFinishADBCommands;
     QString deviceSerialNumber;
 
+    QString displayName() const { return packageName; }
     static void *staticTypeId;
 };
 

src/plugins/clangstaticanalyzer/clangstaticanalyzerruncontrol.cpp
@@ -71,8 +71,9 @@ namespace Internal {
 
 ClangStaticAnalyzerToolRunner::ClangStaticAnalyzerToolRunner(RunControl *runControl,
                                                              QString *errorMessage)
-    : ToolRunner(runControl)
+    : RunWorker(runControl)
 {
+    setDisplayName("ClangStaticAnalyzerRunner");
     runControl->setDisplayName(tr("Clang Static Analyzer"));
     runControl->setIcon(ProjectExplorer::Icons::ANALYZER_START_SMALL_TOOLBAR);
     runControl->setSupportsReRunning(false);
@@ -585,7 +586,7 @@ void ClangStaticAnalyzerToolRunner::start()
         return;
     }
 
-    reportSuccess();
+    reportStarted();
 
     while (m_runners.size() < parallelRuns && !m_unitsToProcess.isEmpty())
         analyzeNextFile();

src/plugins/clangstaticanalyzer/clangstaticanalyzerruncontrol.h
@@ -47,7 +47,7 @@ struct AnalyzeUnit {
 };
 typedef QList<AnalyzeUnit> AnalyzeUnits;
 
-class ClangStaticAnalyzerToolRunner : public ProjectExplorer::ToolRunner
+class ClangStaticAnalyzerToolRunner : public ProjectExplorer::RunWorker
 {
     Q_OBJECT
 

src/plugins/clangstaticanalyzer/clangstaticanalyzertool.cpp
@@ -145,7 +145,7 @@ ClangStaticAnalyzerTool::ClangStaticAnalyzerTool()
         {{ClangStaticAnalyzerDockId, m_diagnosticView, {}, Perspective::SplitVertical}}
     ));
 
-    Debugger::registerAction(Constants::CLANGSTATICANALYZER_RUN_MODE, {});
+    //Debugger::registerAction(Constants::CLANGSTATICANALYZER_RUN_MODE, {});
     action = new QAction(tr("Clang Static Analyzer"), this);
     action->setToolTip(toolTip);
     menu->addAction(ActionManager::registerAction(action, "ClangStaticAnalyzer.Action"),

commit 4048629d1a5940032267f80e44c6032e881abd4e

Qnx: Create slog2 runner processes at more reasonable times
89f02cba2ca65f introduced a regression.

Change-Id: I3ae4ecc7f2dfcd477ffe7a277a87acd90aad5339
Reviewed-by: Dan Cape <dcape@qnx.com>
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

src/plugins/qnx/slog2inforunner.cpp
@@ -46,11 +46,7 @@ Slog2InfoRunner::Slog2InfoRunner(RunControl *runControl)
     auto qnxRunConfig = qobject_cast<QnxRunConfiguration *>(runControl->runConfiguration());
     QTC_ASSERT(qnxRunConfig, return);
     m_applicationId = FileName::fromString(qnxRunConfig->remoteExecutableFilePath()).fileName();
-}
 
-void Slog2InfoRunner::printMissingWarning()
-{
-    appendMessage(tr("Warning: \"slog2info\" is not found on the device, debug output not available."), ErrorMessageFormat);
     // See QTCREATORBUG-10712 for details.
     // We need to limit length of ApplicationId to 63 otherwise it would not match one in slog2info.
     m_applicationId.truncate(63);
@@ -67,6 +63,11 @@ void Slog2InfoRunner::printMissingWarning()
     connect(m_logProcess, &DeviceProcess::error, this, &Slog2InfoRunner::handleLogError);
     connect(m_logProcess, &DeviceProcess::started, this, &Slog2InfoRunner::started);
     connect(m_logProcess, &DeviceProcess::finished, this, &Slog2InfoRunner::finished);
+}
+
+void Slog2InfoRunner::printMissingWarning()
+{
+    appendMessage(tr("Warning: \"slog2info\" is not found on the device, debug output not available."), ErrorMessageFormat);
 }
 
 void Slog2InfoRunner::start()