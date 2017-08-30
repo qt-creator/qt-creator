1c7d1672fa0fe58d878f42ae135acb68ada553e5
Tobias Hunger <tobias.hunger@qt.io>
2017-08-30 10:03:33 +0200

    CppEditor: Fix typo in comment
    Change-Id: Ia6fca1df88ec5d2ed79de07b3633894a255c88d8
    Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>
    Reviewed-by: Nikolai Kosjar <nikolai.kosjar@qt.io>

--- src/plugins/cppeditor/cppincludehierarchy.cpp
+++ src/plugins/cppeditor/cppincludehierarchy.cpp
@@ -407,7 +407,7 @@ void CppIncludeHierarchyWidget::perform()
     m_inspectedFile->setText(m_editor->textDocument()->displayName());
     m_inspectedFile->setLink(TextEditorWidget::Link(document));
 
-    // expand "Includes" adn "Included by"
+    // expand "Includes" and "Included by"
     m_treeView->expand(m_model.index(0, 0));
     m_treeView->expand(m_model.index(1, 0));
 
