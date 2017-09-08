50e3085f225eae5bea9b778d84fa40d3bed17621
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2017-09-08 16:15:02 +0300

    Tooltip: Hide when Escape is pressed
    Change-Id: Ib516cbbd9b51ddac3dac48668e3341830233af4b
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/libs/utils/tooltip/tooltip.cpp
+++ src/libs/utils/tooltip/tooltip.cpp
@@ -305,9 +305,11 @@ bool ToolTip::eventFilter(QObject *o, QEvent *event)
 
     switch (event->type()) {
     case QEvent::KeyPress:
-    case QEvent::KeyRelease:
+    case QEvent::KeyRelease: {
+        int key = static_cast<QKeyEvent *>(event)->key();
+        if (key == Qt::Key_Escape)
+            hideTipImmediately();
         if (HostOsInfo::isMacHost()) {
-            int key = static_cast<QKeyEvent *>(event)->key();
             Qt::KeyboardModifiers mody = static_cast<QKeyEvent *>(event)->modifiers();
             if (!(mody & Qt::KeyboardModifierMask)
                 && key != Qt::Key_Shift && key != Qt::Key_Control
@@ -315,6 +317,7 @@ bool ToolTip::eventFilter(QObject *o, QEvent *event)
                 hideTipWithDelay();
         }
         break;
+    }
     case QEvent::Leave:
         if (o == m_tip && !m_tip->isAncestorOf(QApplication::focusWidget()))
             hideTipWithDelay();
