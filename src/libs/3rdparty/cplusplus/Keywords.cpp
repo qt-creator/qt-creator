qt-creator/qt-creator

commit 0d1fef41f76c04ede629406b6d15847c453f6d96

C++ Disable bool keywords in C files
The keywords bool, true, and false don't exist in C90.

When using C99 and stdbool.h, they are defined as macros.

Amends commit 7b2c09a1186f2cf69f276017ba08da89560f55be

Change-Id: I2f2391dd11be841d7d7b249db0b4add8523e53a5
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/libs/3rdparty/cplusplus/Keywords.cpp
@@ -99,7 +99,7 @@ static inline int classify4(const char *s, LanguageFeatures features)
       }
     }
   }
-  else if (s[0] == 'b') {
+  else if (features.cxxEnabled && s[0] == 'b') {
     if (s[1] == 'o') {
       if (s[2] == 'o') {
         if (s[3] == 'l') {
@@ -173,7 +173,7 @@ static inline int classify4(const char *s, LanguageFeatures features)
         }
       }
     }
-    else if (s[1] == 'r') {
+    else if (features.cxxEnabled && s[1] == 'r') {
       if (s[2] == 'u') {
         if (s[3] == 'e') {
           return T_TRUE;
@@ -255,7 +255,7 @@ static inline int classify5(const char *s, LanguageFeatures features)
       }
     }
   }
-  else if (s[0] == 'f') {
+  else if (features.cxxEnabled && s[0] == 'f') {
     if (s[1] == 'a') {
       if (s[2] == 'l') {
         if (s[3] == 's') {

commit 201a09b5c5b71fb6cdd242801c0af3c54061c960

C++: Fix highlighting of "float" keyword for C files
... in the Classic Code Model.

Broken by 0d1fef41f76c04ede629406b6d15847c453f6d96

Task-number: QTCREATORBUG-18879
Change-Id: I7d0222cc81e8847827af042e89d3b75c57f5d454
Reviewed-by: Orgad Shaneh <orgads@gmail.com>
Reviewed-by: Eike Ziller <eike.ziller@qt.io>
Reviewed-by: Nikolai Kosjar <nikolai.kosjar@qt.io>

src/libs/3rdparty/cplusplus/Keywords.cpp
@@ -255,8 +255,8 @@ static inline int classify5(const char *s, LanguageFeatures features)
       }
     }
   }
-  else if (features.cxxEnabled && s[0] == 'f') {
-    if (s[1] == 'a') {
+  else if (s[0] == 'f') {
+    if (features.cxxEnabled && s[1] == 'a') {
       if (s[2] == 'l') {
         if (s[3] == 's') {
           if (s[4] == 'e') {