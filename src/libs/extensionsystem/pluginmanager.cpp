816fdb579b9486f9f9acdc87965a5aae418c14bf
Robert Loehning <robert.loehning@qt.io>
2017-09-13 17:32:46 +0200

    ExtensionSystem: Add missing spaces
    Change-Id: I9fc5df6c33bf802e1e4f85f394db40e64f3e1b55
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/libs/extensionsystem/pluginmanager.cpp
+++ src/libs/extensionsystem/pluginmanager.cpp
@@ -1322,11 +1322,11 @@ bool PluginManagerPrivate::loadQueue(PluginSpec *spec, QList<PluginSpec *> &queu
         spec->d->errorString += QLatin1Char('\n');
         int index = circularityCheckQueue.indexOf(spec);
         for (int i = index; i < circularityCheckQueue.size(); ++i) {
-            spec->d->errorString.append(PluginManager::tr("%1(%2) depends on")
+            spec->d->errorString.append(PluginManager::tr("%1 (%2) depends on")
                 .arg(circularityCheckQueue.at(i)->name()).arg(circularityCheckQueue.at(i)->version()));
             spec->d->errorString += QLatin1Char('\n');
         }
-        spec->d->errorString.append(PluginManager::tr("%1(%2)").arg(spec->name()).arg(spec->version()));
+        spec->d->errorString.append(PluginManager::tr("%1 (%2)").arg(spec->name()).arg(spec->version()));
         return false;
     }
     circularityCheckQueue.append(spec);
@@ -1348,7 +1348,7 @@ bool PluginManagerPrivate::loadQueue(PluginSpec *spec, QList<PluginSpec *> &queu
         if (!loadQueue(depSpec, queue, circularityCheckQueue)) {
             spec->d->hasError = true;
             spec->d->errorString =
-                PluginManager::tr("Cannot load plugin because dependency failed to load: %1(%2)\nReason: %3")
+                PluginManager::tr("Cannot load plugin because dependency failed to load: %1 (%2)\nReason: %3")
                     .arg(depSpec->name()).arg(depSpec->version()).arg(depSpec->errorString());
             return false;
         }
