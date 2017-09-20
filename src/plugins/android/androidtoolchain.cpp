qt-creator/qt-creator

commit 71b56d2b9c3264bd481915c763aac685c1ad24d0

Some QString -> FileName transformation
Change-Id: I4a8a8f68bb1e52750380218793ec3029b488c01f
Reviewed-by: Daniel Teske <daniel.teske@digia.com>

src/plugins/android/androidtoolchain.cpp
@@ -217,16 +217,16 @@ QString AndroidToolChain::makeCommand(const Utils::Environment &env) const
 {
     QStringList extraDirectories = AndroidConfigurations::currentConfig().makeExtraSearchDirectories();
     if (HostOsInfo::isWindowsHost()) {
-        QString tmp = env.searchInPath(QLatin1String("ma-make.exe"), extraDirectories);
+        FileName tmp = env.searchInPath(QLatin1String("ma-make.exe"), extraDirectories);
         if (!tmp.isEmpty())
-            return tmp;
+            return QString();
         tmp = env.searchInPath(QLatin1String("mingw32-make"), extraDirectories);
-        return tmp.isEmpty() ? QLatin1String("mingw32-make") : tmp;
+        return tmp.isEmpty() ? QLatin1String("mingw32-make") : tmp.toString();
     }
 
     QString make = QLatin1String("make");
-    QString tmp = env.searchInPath(make, extraDirectories);
-    return tmp.isEmpty() ? make : tmp;
+    FileName tmp = env.searchInPath(make, extraDirectories);
+    return tmp.isEmpty() ? make : tmp.toString();
 }
 
 QString AndroidToolChain::ndkToolChainVersion() const

commit 899c518c6daffb3c6440642550593e2cc76ab6c4

Android: Fix ma-make detection
Broken by 71b56d2b9c3264bd481915c763aac685c1ad24d0

Change-Id: I8d92cb1f0ecfcb401f063ba49c357acac8cc832c
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/android/androidtoolchain.cpp
@@ -213,7 +213,7 @@ QString AndroidToolChain::makeCommand(const Environment &env) const
     if (HostOsInfo::isWindowsHost()) {
         FileName tmp = env.searchInPath(QLatin1String("ma-make.exe"), extraDirectories);
         if (!tmp.isEmpty())
-            return QString();
+            return tmp.toString();
         tmp = env.searchInPath(QLatin1String("mingw32-make"), extraDirectories);
         return tmp.isEmpty() ? QLatin1String("mingw32-make") : tmp.toString();
     }