qt-creator/qt-creator

commit 1acd9d30dc68fd2b1690a54e2bdc278dfd46cceb

BareMetal: fix Run configuration
Run configuration is broken for remote GDB and hardware debuggers:
just first target can be handled for project.

Task-number: QTCREATORBUG-18434
Change-Id: I3fcff2d906f377dd5a161f74d2c6b21bc5ab943f
Reviewed-by: Alexander Drozdov <adrozdoff@gmail.com>
Reviewed-by: hjk <hjk@qt.io>

src/plugins/baremetal/baremetalrunconfiguration.cpp
@@ -131,8 +131,10 @@ QString BareMetalRunConfiguration::defaultDisplayName()
 
 QString BareMetalRunConfiguration::localExecutableFilePath() const
 {
+    const QString targetName = QFileInfo(m_projectFilePath).completeBaseName();
+
     return target()->applicationTargets()
-            .targetForProject(FileName::fromString(m_projectFilePath)).toString();
+              .targetFilePath(FileName::fromString(targetName).toString()).toString();
 }
 
 QString BareMetalRunConfiguration::arguments() const
@@ -158,9 +160,10 @@ QString BareMetalRunConfiguration::projectFilePath() const
 QString BareMetalRunConfiguration::buildSystemTarget() const
 {
     const BuildTargetInfoList targets = target()->applicationTargets();
-    const Utils::FileName projectFilePath = Utils::FileName::fromString(m_projectFilePath);
+    const Utils::FileName projectFilePath = Utils::FileName::fromString(QFileInfo(m_projectFilePath).path());
+    const QString targetName = QFileInfo(m_projectFilePath).completeBaseName();
     auto bst = std::find_if(targets.list.constBegin(), targets.list.constEnd(),
-                            [&projectFilePath](const BuildTargetInfo &bti) { return bti.projectFilePath == projectFilePath; });
+                            [&projectFilePath,&targetName](const BuildTargetInfo &bti) { return bti.projectFilePath == projectFilePath && bti.targetName == targetName; });
     return (bst == targets.list.constEnd()) ? QString() : bst->targetName;
 }
 

src/plugins/baremetal/baremetalrunconfigurationfactory.cpp
@@ -60,8 +60,9 @@ bool BareMetalRunConfigurationFactory::canCreate(Target *parent, Core::Id id) co
 {
     if (!canHandle(parent))
         return false;
+    const QString targetName = QFileInfo(pathFromId(id)).completeBaseName();
     return id == BareMetalCustomRunConfiguration::runConfigId()
-            || !parent->applicationTargets().targetForProject(pathFromId(id)).isEmpty();
+            || !parent->applicationTargets().targetFilePath(targetName).isEmpty();
 }
 
 bool BareMetalRunConfigurationFactory::canRestore(Target *parent, const QVariantMap &map) const
@@ -89,7 +90,7 @@ QList<Core::Id> BareMetalRunConfigurationFactory::availableCreationIds(Target *p
 
     const Core::Id base = Core::Id(BareMetalRunConfiguration::IdPrefix);
     foreach (const BuildTargetInfo &bti, parent->applicationTargets().list)
-        result << base.withSuffix(bti.projectFilePath.toString());
+        result << base.withSuffix(bti.projectFilePath.toString() + QLatin1Char('/') + bti.targetName);
     result << BareMetalCustomRunConfiguration::runConfigId();
     return result;
 }

commit ec4269f6a14b2591802ac9317f4fdc8c9418a651

BareMetal: Fix target file name resolving
Broke by 1acd9d30dc68fd2b1690a54e2bdc278dfd46cceb.

Change-Id: I0dbf14aeb345743eb22a033ac3570c6af4eb0bb2
Reviewed-by: hjk <hjk@qt.io>
Reviewed-by: Denis Shienkov <denis.shienkov@gmail.com>
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>
Reviewed-by: Alexander Drozdov <adrozdoff@gmail.com>
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/baremetal/baremetalrunconfiguration.cpp
@@ -124,17 +124,15 @@ QString BareMetalRunConfiguration::defaultDisplayName()
 {
     if (!m_projectFilePath.isEmpty())
         //: %1 is the name of the project run via hardware debugger
-        return tr("%1 (via GDB server or hardware debugger)").arg(QFileInfo(m_projectFilePath).completeBaseName());
+        return tr("%1 (via GDB server or hardware debugger)").arg(QFileInfo(m_projectFilePath).fileName());
     //: Bare Metal run configuration default run name
     return tr("Run on GDB server or hardware debugger");
 }
 
 QString BareMetalRunConfiguration::localExecutableFilePath() const
 {
-    const QString targetName = QFileInfo(m_projectFilePath).completeBaseName();
-
-    return target()->applicationTargets()
-              .targetFilePath(FileName::fromString(targetName).toString()).toString();
+    const QString targetName = QFileInfo(m_projectFilePath).fileName();
+    return target()->applicationTargets().targetFilePath(targetName).toString();
 }
 
 QString BareMetalRunConfiguration::arguments() const
@@ -161,7 +159,7 @@ QString BareMetalRunConfiguration::buildSystemTarget() const
 {
     const BuildTargetInfoList targets = target()->applicationTargets();
     const Utils::FileName projectFilePath = Utils::FileName::fromString(QFileInfo(m_projectFilePath).path());
-    const QString targetName = QFileInfo(m_projectFilePath).completeBaseName();
+    const QString targetName = QFileInfo(m_projectFilePath).fileName();
     auto bst = std::find_if(targets.list.constBegin(), targets.list.constEnd(),
                             [&projectFilePath,&targetName](const BuildTargetInfo &bti) { return bti.projectFilePath == projectFilePath && bti.targetName == targetName; });
     return (bst == targets.list.constEnd()) ? QString() : bst->targetName;

src/plugins/baremetal/baremetalrunconfigurationfactory.cpp
@@ -60,7 +60,7 @@ bool BareMetalRunConfigurationFactory::canCreate(Target *parent, Core::Id id) co
 {
     if (!canHandle(parent))
         return false;
-    const QString targetName = QFileInfo(pathFromId(id)).completeBaseName();
+    const QString targetName = QFileInfo(pathFromId(id)).fileName();
     return id == BareMetalCustomRunConfiguration::runConfigId()
             || !parent->applicationTargets().targetFilePath(targetName).isEmpty();
 }
@@ -100,7 +100,7 @@ QString BareMetalRunConfigurationFactory::displayNameForId(Core::Id id) const
     if (id == BareMetalCustomRunConfiguration::runConfigId())
         return BareMetalCustomRunConfiguration::runConfigDefaultDisplayName();
     return tr("%1 (on GDB server or hardware debugger)")
-        .arg(QFileInfo(pathFromId(id)).completeBaseName());
+        .arg(QFileInfo(pathFromId(id)).fileName());
 }
 
 RunConfiguration *BareMetalRunConfigurationFactory::doCreate(Target *parent, Core::Id id)