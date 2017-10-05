qt-creator/qt-creator

commit 58a69c461aac0227957ea5db9a9bc9828fa74eee

Debugger: Remove DebuggerRunParameter::masterEngineType
It is a fixed value in the respective contexts, no need to track
this 'state' separately.

Change-Id: Id442202ff7a72baf5a2b19ba4bb9dfec5f47fb70
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/debugger/debuggerruncontrol.cpp
@@ -195,7 +195,6 @@ void DebuggerRunTool::setStartMode(DebuggerStartMode startMode)
         m_runParameters.startMode = AttachToRemoteProcess;
         m_runParameters.isCppDebugging = false;
         m_runParameters.isQmlDebugging = true;
-        m_runParameters.masterEngineType = QmlEngineType;
         m_runParameters.closeMode = KillAtClose;
 
         // FIXME: This is horribly wrong.
@@ -420,29 +419,6 @@ void DebuggerRunTool::addSearchDirectory(const QString &dir)
     m_runParameters.additionalSearchDirectories.append(dir);
 }
 
-static QLatin1String engineTypeName(DebuggerEngineType et)
-{
-    switch (et) {
-    case Debugger::NoEngineType:
-        break;
-    case Debugger::GdbEngineType:
-        return QLatin1String("Gdb engine");
-    case Debugger::CdbEngineType:
-        return QLatin1String("Cdb engine");
-    case Debugger::PdbEngineType:
-        return QLatin1String("Pdb engine");
-    case Debugger::QmlEngineType:
-        return QLatin1String("QML engine");
-    case Debugger::QmlCppEngineType:
-        return QLatin1String("QML C++ engine");
-    case Debugger::LldbEngineType:
-        return QLatin1String("LLDB command line engine");
-    case Debugger::AllEngineTypes:
-        break;
-    }
-    return QLatin1String("No engine");
-}
-
 void DebuggerRunTool::start()
 {
     Debugger::Internal::saveModeToRestore();
@@ -501,24 +477,19 @@ void DebuggerRunTool::start()
                 QTC_CHECK(false);
                 break;
         }
-    }
 
-    switch (m_runParameters.masterEngineType) {
-        case QmlEngineType:
-            m_engine = createQmlEngine();
-            break;
-        case QmlCppEngineType:
+        if (m_runParameters.isQmlDebugging) {
             if (cppEngine)
                 m_engine = createQmlCppEngine(cppEngine);
-            break;
-        default:
+            else
+                m_engine = createQmlEngine();
+        } else {
             m_engine = cppEngine;
-            break;
+        }
     }
 
     if (!m_engine) {
-        reportFailure(DebuggerPlugin::tr("Unable to create a debugging engine of the type \"%1\"").
-                       arg(engineTypeName(m_runParameters.masterEngineType)));
+        reportFailure(DebuggerPlugin::tr("Unable to create a debugging engine"));
         return;
     }
 
@@ -668,31 +639,24 @@ bool DebuggerRunTool::fixupParameters()
         }
     }
 
-    if (rp.masterEngineType == NoEngineType) {
-        if (rp.isQmlDebugging) {
-            QmlDebug::QmlDebugServicesPreset service;
-            if (rp.isCppDebugging) {
-                if (rp.nativeMixedEnabled) {
-                    service = QmlDebug::QmlNativeDebuggerServices;
-                } else {
-                    rp.masterEngineType = QmlCppEngineType;
-                    service = QmlDebug::QmlDebuggerServices;
-                }
+    if (rp.isQmlDebugging) {
+        QmlDebug::QmlDebugServicesPreset service;
+        if (rp.isCppDebugging) {
+            if (rp.nativeMixedEnabled) {
+                service = QmlDebug::QmlNativeDebuggerServices;
             } else {
-                rp.masterEngineType = QmlEngineType;
                 service = QmlDebug::QmlDebuggerServices;
             }
-            if (rp.startMode != AttachExternal && rp.startMode != AttachCrashedExternal) {
-                QString qmlarg = rp.isCppDebugging && rp.nativeMixedEnabled
-                        ? QmlDebug::qmlDebugNativeArguments(service, false)
-                        : QmlDebug::qmlDebugTcpArguments(service, Port(rp.qmlServer.port()));
-                QtcProcess::addArg(&rp.inferior.commandLineArguments, qmlarg);
-            }
+        } else {
+            service = QmlDebug::QmlDebuggerServices;
+        }
+        if (rp.startMode != AttachExternal && rp.startMode != AttachCrashedExternal) {
+            QString qmlarg = rp.isCppDebugging && rp.nativeMixedEnabled
+                    ? QmlDebug::qmlDebugNativeArguments(service, false)
+                    : QmlDebug::qmlDebugTcpArguments(service, Port(rp.qmlServer.port()));
+            QtcProcess::addArg(&rp.inferior.commandLineArguments, qmlarg);
         }
     }
-
-    if (rp.masterEngineType == NoEngineType)
-        rp.masterEngineType = rp.cppEngineType;
 
     if (rp.startMode == NoStartMode)
         rp.startMode = StartInternal;
@@ -813,8 +777,7 @@ DebuggerRunTool::DebuggerRunTool(RunControl *runControl, Kit *kit)
                     m_runParameters.inferior.commandLineArguments.append(' ');
                 m_runParameters.inferior.commandLineArguments.append(args);
             }
-            m_runParameters.cppEngineType = PdbEngineType;
-            m_runParameters.masterEngineType = PdbEngineType;
+            m_engine = createPdbEngine();
         }
     }
 }

src/plugins/debugger/debuggerconstants.h
@@ -191,15 +191,7 @@ enum DebuggerEngineType
     GdbEngineType     = 0x001,
     CdbEngineType     = 0x004,
     PdbEngineType     = 0x008,
-    QmlEngineType     = 0x020,
-    QmlCppEngineType  = 0x040,
-    LldbEngineType    = 0x100,
-    AllEngineTypes = GdbEngineType
-        | CdbEngineType
-        | PdbEngineType
-        | QmlEngineType
-        | QmlCppEngineType
-        | LldbEngineType
+    LldbEngineType    = 0x100
 };
 
 enum DebuggerLanguage

src/plugins/debugger/debuggerengine.h
@@ -128,7 +128,6 @@ public:
     // Macro-expanded and passed to debugger startup.
     QString additionalStartupCommands;
 
-    DebuggerEngineType masterEngineType = NoEngineType;
     DebuggerEngineType cppEngineType = NoEngineType;
 
     bool isCppDebugging = true;

commit 9089b3acbd17f2d011f9a4045b12116e4a469067

Debugger: Only set cppEngine if there's any C++ debugging
Fixes a regression introduced yesterday with 58a69c461aac0.

Change-Id: I9eea3d0adf45e2b65b7d395280ae0e0fbbe01127
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/debugger/debuggerruncontrol.cpp
@@ -474,7 +474,7 @@ void DebuggerRunTool::start()
                 cppEngine = createPdbEngine();
                 break;
             default:
-                QTC_CHECK(false);
+                // Can happen for pure Qml.
                 break;
         }
 
@@ -720,7 +720,6 @@ DebuggerRunTool::DebuggerRunTool(RunControl *runControl, Kit *kit)
         kit = runConfig->target()->kit();
     QTC_ASSERT(kit, return);
 
-    m_runParameters.cppEngineType = DebuggerKitInformation::engineType(kit);
     m_runParameters.sysRoot = SysRootKitInformation::sysRoot(kit).toString();
     m_runParameters.macroExpander = kit->macroExpander();
     m_runParameters.debugger = DebuggerKitInformation::runnable(kit);
@@ -739,6 +738,9 @@ DebuggerRunTool::DebuggerRunTool(RunControl *runControl, Kit *kit)
         m_runParameters.isQmlDebugging = aspect->useQmlDebugger();
         m_runParameters.multiProcess = aspect->useMultiProcess();
     }
+
+    if (m_runParameters.isCppDebugging)
+        m_runParameters.cppEngineType = DebuggerKitInformation::engineType(kit);
 
     const QByteArray envBinary = qgetenv("QTC_DEBUGGER_PATH");
     if (!envBinary.isEmpty())