qt-creator/qt-creator

commit 597488bef426f13e895b433b71923fbb704729bb

Debugger: Move some code around
Parts of the Debugger engine rampdown mechanisms are
essentially one code path now, don't distribute that
over four classes.

Change-Id: I4c6edbc0db94d1f658b0c1d9c154552f286db876
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/debugger/debuggerengine.cpp
@@ -246,10 +246,18 @@ public:
         m_engine->setState(DebuggerFinished);
         resetLocation();
         if (isMasterEngine()) {
-            m_engine->showMessage("NOTE: FINISH DEBUGGER");
-            QTC_ASSERT(state() == DebuggerFinished, qDebug() << m_engine << state());
-            if (isMasterEngine() && m_runTool)
-                m_runTool->debuggingFinished();
+            showMessage(tr("Debugging has finished"), NormalMessageFormat);
+            if (m_runTool) {
+                m_progress.setProgressValue(1000);
+                m_progress.reportFinished();
+                m_modulesHandler.removeAll();
+                m_stackHandler.removeAll();
+                m_threadsHandler.removeAll();
+                m_watchHandler.cleanup();
+                Internal::runControlFinished(m_runTool);
+                m_runTool->reportStopped();
+                m_runTool.clear();
+            }
         }
     }
 
@@ -556,29 +564,6 @@ void DebuggerEngine::gotoLocation(const Location &loc)
         d->m_locationMark.reset(new LocationMark(this, file, line));
 }
 
-// Called from RunControl.
-void DebuggerEngine::handleStartFailed()
-{
-    showMessage("HANDLE RUNCONTROL START FAILED");
-    d->m_runTool.clear();
-    d->m_progress.setProgressValue(900);
-    d->m_progress.reportCanceled();
-    d->m_progress.reportFinished();
-}
-
-// Called from RunControl.
-void DebuggerEngine::handleFinished()
-{
-    showMessage("HANDLE RUNCONTROL FINISHED");
-    d->m_runTool.clear();
-    d->m_progress.setProgressValue(1000);
-    d->m_progress.reportFinished();
-    modulesHandler()->removeAll();
-    stackHandler()->removeAll();
-    threadsHandler()->removeAll();
-    watchHandler()->cleanup();
-}
-
 const DebuggerRunParameters &DebuggerEngine::runParameters() const
 {
     return runTool()->runParameters();
@@ -679,8 +664,14 @@ void DebuggerEngine::notifyEngineSetupFailed()
     showMessage("NOTE: ENGINE SETUP FAILED");
     QTC_ASSERT(state() == EngineSetupRequested, qDebug() << this << state());
     setState(EngineSetupFailed);
-    if (isMasterEngine() && runTool())
-        runTool()->startFailed();
+    if (isMasterEngine() && runTool()) {
+        showMessage(tr("Debugging has failed"), NormalMessageFormat);
+        d->m_runTool.clear();
+        d->m_progress.setProgressValue(900);
+        d->m_progress.reportCanceled();
+        d->m_progress.reportFinished();
+    }
+
     setState(DebuggerFinished);
 }
 

src/plugins/debugger/debuggerengine.h
@@ -337,8 +337,6 @@ public:
     virtual QAbstractItemModel *sourceFilesModel() const;
 
     void progressPing();
-    void handleFinished();
-    void handleStartFailed();
     bool debuggerActionsEnabled() const;
     static bool debuggerActionsEnabled(DebuggerState state);
 

src/plugins/debugger/debuggerplugin.cpp
@@ -2946,8 +2946,6 @@ void DebuggerPluginPrivate::runControlStarted(DebuggerRunTool *runTool)
 
 void DebuggerPluginPrivate::runControlFinished(DebuggerRunTool *runTool)
 {
-    if (runTool && runTool->engine())
-        runTool->engine()->handleFinished();
     showStatusMessage(tr("Debugger finished."));
     m_snapshotHandler->removeSnapshot(runTool);
     if (m_snapshotHandler->size() == 0) {

src/plugins/debugger/debuggerruncontrol.cpp
@@ -603,24 +603,11 @@ void DebuggerRunTool::start()
     m_engine->start();
 }
 
-void DebuggerRunTool::startFailed()
-{
-    appendMessage(tr("Debugging has failed"), NormalMessageFormat);
-    m_engine->handleStartFailed();
-}
-
 void DebuggerRunTool::stop()
 {
     m_isDying = true;
     QTC_ASSERT(m_engine, reportStopped(); return);
     m_engine->quitDebugger();
-}
-
-void DebuggerRunTool::debuggingFinished()
-{
-    appendMessage(tr("Debugging has finished"), NormalMessageFormat);
-    Internal::runControlFinished(this);
-    reportStopped();
 }
 
 const DebuggerRunParameters &DebuggerRunTool::runParameters() const

src/plugins/debugger/debuggerruncontrol.h
@@ -58,13 +58,10 @@ public:
     void start() override;
     void stop() override;
 
-    void startFailed();
-
     void notifyInferiorIll();
     Q_SLOT void notifyInferiorExited(); // Called from Android.
     void quitDebugger();
     void abortDebugger();
-    void debuggingFinished();
 
     const Internal::DebuggerRunParameters &runParameters() const;
 

commit a1b486011aa3833b77325554ea4d249e49145f1f

Debugger: Fix placing of finished message
This message had been displayed on the Application Output pane before.
Restore this behavior which broke with 597488be.

Change-Id: I29dcae877f2f19bce05e0ec1f32274b1e7cca25f
Reviewed-by: hjk <hjk@qt.io>

src/plugins/debugger/debuggerengine.cpp
@@ -238,7 +238,6 @@ public:
         m_engine->setState(DebuggerFinished);
         resetLocation();
         if (isMasterEngine()) {
-            showMessage(tr("Debugging has finished"), NormalMessageFormat);
             if (m_runTool) {
                 m_progress.setProgressValue(1000);
                 m_progress.reportFinished();
@@ -248,6 +247,7 @@ public:
                 m_watchHandler.cleanup();
                 Internal::runControlFinished(m_runTool);
                 m_runTool->reportStopped();
+                m_runTool->appendMessage(tr("Debugging has finished"), NormalMessageFormat);
                 m_runTool.clear();
             }
         }