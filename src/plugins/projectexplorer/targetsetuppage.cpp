b39687645df5088a8aa72dab91a7072b19f1fa0c
Tobias Hunger <tobias.hunger@qt.io>
2017-11-06 17:15:06 +0100

    TargetSetupPage: Ignore changes while ProjectImporter is updating
    This causes the same kit to be processed repeatedly, which should not
    cause harm, but waste CPU cycles.
    
    Change-Id: Ie42272aacec6d8350398b65973570282c0c8d287
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- src/plugins/projectexplorer/targetsetuppage.cpp
+++ src/plugins/projectexplorer/targetsetuppage.cpp
@@ -344,11 +344,11 @@ void TargetSetupPage::handleKitAddition(Kit *k)
 
 void TargetSetupPage::handleKitRemoval(Kit *k)
 {
-    if (m_importer)
-        m_importer->cleanupKit(k);
-
     if (isUpdating())
         return;
+
+    if (m_importer)
+        m_importer->cleanupKit(k);
 
     removeWidget(k);
     updateVisibility();
