qt-creator/qt-creator

commit 4a66cae8545cd58cf9d3006a2bad91845cffa14b

ProjectExplorer: Use ids in RunConfigFactory:: setSupportedProjectType
More similar to what build/deploy uses.

Change-Id: Icf8bd7031d00a6e2831f8c1f3b1bdcaa8bf259b4
Reviewed-by: hjk <hjk@qt.io>

src/plugins/qmakeprojectmanager/desktopqmakerunconfiguration.cpp
@@ -25,9 +25,10 @@
 
 #include "desktopqmakerunconfiguration.h"
 
+#include "qmakebuildconfiguration.h"
 #include "qmakenodes.h"
 #include "qmakeproject.h"
-#include "qmakebuildconfiguration.h"
+#include "qmakeprojectmanagerconstants.h"
 
 #include <coreplugin/variablechooser.h>
 #include <projectexplorer/localenvironmentaspect.h>
@@ -441,8 +442,8 @@ DesktopQmakeRunConfigurationFactory::DesktopQmakeRunConfigurationFactory(QObject
 {
     setObjectName("DesktopQmakeRunConfigurationFactory");
     registerRunConfiguration<DesktopQmakeRunConfiguration>(QMAKE_RC_PREFIX);
-    setSupportedProjectType<QmakeProject>();
-    setSupportedTargetDeviceTypes({Constants::DESKTOP_DEVICE_TYPE});
+    addSupportedProjectType(QmakeProjectManager::Constants::PROJECT_ID);
+    setSupportedTargetDeviceTypes({ProjectExplorer::Constants::DESKTOP_DEVICE_TYPE});
 }
 
 bool DesktopQmakeRunConfigurationFactory::canCreateHelper(Target *parent, const QString &buildTarget) const

src/plugins/cmakeprojectmanager/cmakerunconfiguration.cpp
@@ -238,7 +238,7 @@ CMakeRunConfigurationFactory::CMakeRunConfigurationFactory(QObject *parent) :
 {
     setObjectName("CMakeRunConfigurationFactory");
     registerRunConfiguration<CMakeRunConfiguration>(CMAKE_RC_PREFIX);
-    setSupportedProjectType<CMakeProject>();
+    addSupportedProjectType(CMakeProjectManager::Constants::CMAKEPROJECT_ID);
 }
 
 QList<QString> CMakeRunConfigurationFactory::availableBuildTargets(Target *parent, CreationMode) const

src/plugins/ios/iosrunfactories.cpp
@@ -30,11 +30,11 @@
 
 #include <projectexplorer/project.h>
 #include <projectexplorer/projectexplorerconstants.h>
-#include <projectexplorer/projectexplorer.h>
 #include <projectexplorer/target.h>
 
 #include <qmakeprojectmanager/qmakenodes.h>
 #include <qmakeprojectmanager/qmakeproject.h>
+#include <qmakeprojectmanager/qmakeprojectmanagerconstants.h>
 
 using namespace ProjectExplorer;
 using namespace QmakeProjectManager;
@@ -48,7 +48,7 @@ IosRunConfigurationFactory::IosRunConfigurationFactory(QObject *parent)
     setObjectName("IosRunConfigurationFactory");
     registerRunConfiguration<IosRunConfiguration>(Constants::IOS_RC_ID_PREFIX);
     setSupportedTargetDeviceTypes({Constants::IOS_DEVICE_TYPE, Constants::IOS_SIMULATOR_TYPE});
-    setSupportedProjectType<QmakeProject>();
+    addSupportedProjectType(QmakeProjectManager::Constants::QMAKEPROJECT_ID);
 }
 
 bool IosRunConfigurationFactory::canCreateHelper(Target *parent, const QString &buildTarget) const

src/plugins/nim/project/nimrunconfigurationfactory.cpp
@@ -39,7 +39,7 @@ namespace Nim {
 NimRunConfigurationFactory::NimRunConfigurationFactory()
 {
     registerRunConfiguration<NimRunConfiguration>(Constants::C_NIMRUNCONFIGURATION_ID);
-    setSupportedProjectType<NimProject>();
+    addSupportedProjectType(Constants::C_NIMPROJECT_ID);
 }
 
 QList<QString> NimRunConfigurationFactory::availableBuildTargets(Target *, CreationMode) const

src/plugins/projectexplorer/runconfiguration.cpp
@@ -475,17 +475,26 @@ void IRunConfigurationFactory::setSupportedTargetDeviceTypes(const QList<Core::I
     m_supportedTargetDeviceTypes = ids;
 }
 
+void IRunConfigurationFactory::addSupportedProjectType(Core::Id id)
+{
+    m_supportedProjectTypes.append(id);
+}
+
 bool IRunConfigurationFactory::canHandle(Target *target) const
 {
-    if (m_projectTypeChecker && !m_projectTypeChecker(target->project()))
+    const Project *project = target->project();
+    Kit *kit = target->kit();
+
+    if (!project->supportsKit(kit))
         return false;
 
-    if (!target->project()->supportsKit(target->kit()))
-        return false;
+    if (!m_supportedProjectTypes.isEmpty())
+        if (!m_supportedProjectTypes.contains(project->id()))
+            return false;
 
     if (!m_supportedTargetDeviceTypes.isEmpty())
         if (!m_supportedTargetDeviceTypes.contains(
-                    DeviceTypeKitInformation::deviceTypeId(target->kit())))
+                    DeviceTypeKitInformation::deviceTypeId(kit)))
             return false;
 
     return true;

src/plugins/projectexplorer/runconfiguration.h
@@ -334,20 +334,13 @@ protected:
         m_runConfigBaseId = runConfigBaseId;
     }
 
-    using ProjectTypeChecker = std::function<bool(Project *)>;
-
-    template <class ProjectType>
-    void setSupportedProjectType()
-    {
-        m_projectTypeChecker = [](Project *p) { return qobject_cast<ProjectType *>(p) != nullptr; };
-    }
-
+    void addSupportedProjectType(Core::Id id);
     void setSupportedTargetDeviceTypes(const QList<Core::Id> &ids);
 
 private:
     RunConfigurationCreator m_creator;
     Core::Id m_runConfigBaseId;
-    ProjectTypeChecker m_projectTypeChecker;
+    QList<Core::Id> m_supportedProjectTypes;
     QList<Core::Id> m_supportedTargetDeviceTypes;
 };
 

src/plugins/pythoneditor/pythoneditorplugin.cpp
@@ -262,7 +262,7 @@ public:
     {
         setObjectName("PythonRunConfigurationFactory");
         registerRunConfiguration<PythonRunConfiguration>(PythonRunConfigurationPrefix);
-        setSupportedProjectType<PythonProject>();
+        addSupportedProjectType(PythonProjectId);
     }
 
     QList<QString> availableBuildTargets(Target *parent, CreationMode mode) const override

src/plugins/qbsprojectmanager/qbsrunconfiguration.cpp
@@ -25,6 +25,7 @@
 
 #include "qbsrunconfiguration.h"
 
+#include "qbsprojectmanagerconstants.h"
 #include "qbsdeployconfigurationfactory.h"
 #include "qbsinstallstep.h"
 #include "qbsproject.h"
@@ -352,8 +353,8 @@ QbsRunConfigurationFactory::QbsRunConfigurationFactory(QObject *parent) :
 {
     setObjectName("QbsRunConfigurationFactory");
     registerRunConfiguration<QbsRunConfiguration>(QBS_RC_PREFIX);
-    setSupportedProjectType<QbsProject>();
-    setSupportedTargetDeviceTypes({Constants::DESKTOP_DEVICE_TYPE});
+    addSupportedProjectType(Constants::PROJECT_ID);
+    setSupportedTargetDeviceTypes({ProjectExplorer::Constants::DESKTOP_DEVICE_TYPE});
 }
 
 bool QbsRunConfigurationFactory::canCreateHelper(Target *parent, const QString &buildTarget) const

commit 60fb35a23c7827ab9448edbb62a3dd4dabd04550

QMakePM: Fix typo in supported project type
Introduced with 4a66cae8545cd58cf9d3006a2bad91845cffa14b.

Change-Id: I7b10391160132024cac5c3bab8e44693ff4a89b8
Reviewed-by: hjk <hjk@qt.io>

src/plugins/qmakeprojectmanager/desktopqmakerunconfiguration.cpp
@@ -442,7 +442,7 @@ DesktopQmakeRunConfigurationFactory::DesktopQmakeRunConfigurationFactory(QObject
 {
     setObjectName("DesktopQmakeRunConfigurationFactory");
     registerRunConfiguration<DesktopQmakeRunConfiguration>(QMAKE_RC_PREFIX);
-    addSupportedProjectType(QmakeProjectManager::Constants::PROJECT_ID);
+    addSupportedProjectType(QmakeProjectManager::Constants::QMAKEPROJECT_ID);
     setSupportedTargetDeviceTypes({ProjectExplorer::Constants::DESKTOP_DEVICE_TYPE});
 }
 