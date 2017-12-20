qt-creator/qt-creator

commit 21c66ce5fdaf9fcf91b4ba72adba691fc64be4a3

Combine some SshConnectionParameter members
Combine host, port, username and password into a 'url' member and
add some convenience accessors.

Change-Id: Iddc26ff00dad1285c96aa56f196dbc4febe8e974
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

src/libs/ssh/sshconnection.cpp
@@ -63,21 +63,21 @@ namespace QSsh {
 const QByteArray ClientId("SSH-2.0-QtCreator\r\n");
 
 SshConnectionParameters::SshConnectionParameters() :
-    timeout(0),  authenticationType(AuthenticationTypePublicKey), port(0),
+    timeout(0), authenticationType(AuthenticationTypePublicKey),
     hostKeyCheckingMode(SshHostKeyCheckingNone)
 {
+    url.setPort(0);
     options |= SshIgnoreDefaultProxy;
     options |= SshEnableStrictConformanceChecks;
 }
 
 static inline bool equals(const SshConnectionParameters &p1, const SshConnectionParameters &p2)
 {
-    return p1.host == p2.host && p1.userName == p2.userName
+    return p1.url == p2.url
             && p1.authenticationType == p2.authenticationType
-            && (p1.authenticationType == SshConnectionParameters::AuthenticationTypePassword ?
-                    p1.password == p2.password : p1.privateKeyFile == p2.privateKeyFile)
+            && p1.privateKeyFile == p2.privateKeyFile
             && p1.hostKeyCheckingMode == p2.hostKeyCheckingMode
-            && p1.timeout == p2.timeout && p1.port == p2.port;
+            && p1.timeout == p2.timeout;
 }
 
 bool operator==(const SshConnectionParameters &p1, const SshConnectionParameters &p2)
@@ -521,11 +521,11 @@ void SshConnectionPrivate::handleServiceAcceptPacket()
         m_triedAllPasswordBasedMethods = false;
         // Fall-through.
     case SshConnectionParameters::AuthenticationTypePassword:
-        m_sendFacility.sendUserAuthByPasswordRequestPacket(m_connParams.userName.toUtf8(),
-                SshCapabilities::SshConnectionService, m_connParams.password.toUtf8());
+        m_sendFacility.sendUserAuthByPasswordRequestPacket(m_connParams.userName().toUtf8(),
+                SshCapabilities::SshConnectionService, m_connParams.password().toUtf8());
         break;
     case SshConnectionParameters::AuthenticationTypeKeyboardInteractive:
-        m_sendFacility.sendUserAuthByKeyboardInteractiveRequestPacket(m_connParams.userName.toUtf8(),
+        m_sendFacility.sendUserAuthByKeyboardInteractiveRequestPacket(m_connParams.userName().toUtf8(),
                 SshCapabilities::SshConnectionService);
         break;
     case SshConnectionParameters::AuthenticationTypePublicKey:
@@ -575,7 +575,7 @@ void SshConnectionPrivate::handleUserAuthInfoRequestPacket()
 
     // Not very interactive, admittedly, but we don't want to be for now.
     for (int i = 0;  i < requestPacket.prompts.count(); ++i)
-        responses << m_connParams.password;
+        responses << m_connParams.password();
     m_sendFacility.sendUserAuthInfoResponsePacket(responses);
 }
 
@@ -626,7 +626,7 @@ void SshConnectionPrivate::handleUserAuthFailurePacket()
         && !m_triedAllPasswordBasedMethods) {
         m_triedAllPasswordBasedMethods = true;
         m_sendFacility.sendUserAuthByKeyboardInteractiveRequestPacket(
-                    m_connParams.userName.toUtf8(),
+                    m_connParams.userName().toUtf8(),
                     SshCapabilities::SshConnectionService);
         return;
     }
@@ -840,7 +840,7 @@ void SshConnectionPrivate::tryAllAgentKeys()
     qCDebug(sshLog) << "trying authentication with" << keys.count()
                     << "public keys received from agent";
     foreach (const QByteArray &key, keys) {
-        m_sendFacility.sendQueryPublicKeyPacket(m_connParams.userName.toUtf8(),
+        m_sendFacility.sendQueryPublicKeyPacket(m_connParams.userName().toUtf8(),
                                                 SshCapabilities::SshConnectionService, key);
         m_pendingKeyChecks.enqueue(key);
     }
@@ -860,7 +860,7 @@ void SshConnectionPrivate::authenticateWithPublicKey()
         signature = m_agentSignature;
     }
 
-    m_sendFacility.sendUserAuthByPublicKeyRequestPacket(m_connParams.userName.toUtf8(),
+    m_sendFacility.sendUserAuthByPublicKeyRequestPacket(m_connParams.userName().toUtf8(),
             SshCapabilities::SshConnectionService, key, signature);
 }
 
@@ -929,7 +929,7 @@ void SshConnectionPrivate::connectToHost()
     m_state = SocketConnecting;
     m_keyExchangeState = NoKeyExchange;
     m_timeoutTimer.start();
-    m_socket->connectToHost(m_connParams.host, m_connParams.port);
+    m_socket->connectToHost(m_connParams.host(), m_connParams.port());
 }
 
 void SshConnectionPrivate::closeConnection(SshErrorCode sshError,

src/libs/ssh/sshconnection.h
@@ -36,6 +36,7 @@
 #include <QSharedPointer>
 #include <QString>
 #include <QHostAddress>
+#include <QUrl>
 
 namespace QSsh {
 class SftpChannel;
@@ -75,13 +76,19 @@ public:
 
     SshConnectionParameters();
 
-    QString host;
-    QString userName;
-    QString password;
+    QString host() const { return url.host(); }
+    quint16 port() const { return url.port(); }
+    QString userName() const { return url.userName(); }
+    QString password() const { return url.password(); }
+    void setHost(const QString &host) { url.setHost(host); }
+    void setPort(int port) { url.setPort(port); }
+    void setUserName(const QString &name) { url.setUserName(name); }
+    void setPassword(const QString &password) { url.setPassword(password); }
+
+    QUrl url;
     QString privateKeyFile;
     int timeout; // In seconds.
     AuthenticationType authenticationType;
-    quint16 port;
     SshConnectionOptions options;
     SshHostKeyCheckingMode hostKeyCheckingMode;
     SshHostKeyDatabasePtr hostKeyDatabase;

src/libs/ssh/sshkeyexchange.cpp
@@ -237,7 +237,7 @@ void SshKeyExchange::checkHostKey(const QByteArray &hostKey)
 {
     if (m_connParams.hostKeyCheckingMode == SshHostKeyCheckingNone) {
         if (m_connParams.hostKeyDatabase)
-            m_connParams.hostKeyDatabase->insertHostKey(m_connParams.host, hostKey);
+            m_connParams.hostKeyDatabase->insertHostKey(m_connParams.host(), hostKey);
         return;
     }
 
@@ -247,7 +247,7 @@ void SshKeyExchange::checkHostKey(const QByteArray &hostKey)
                                         "if host key checking is enabled."));
     }
 
-    switch (m_connParams.hostKeyDatabase->matchHostKey(m_connParams.host, hostKey)) {
+    switch (m_connParams.hostKeyDatabase->matchHostKey(m_connParams.host(), hostKey)) {
     case SshHostKeyDatabase::KeyLookupMatch:
         return; // Nothing to do.
     case SshHostKeyDatabase::KeyLookupMismatch:
@@ -259,14 +259,14 @@ void SshKeyExchange::checkHostKey(const QByteArray &hostKey)
             throwHostKeyException();
         break;
     }
-    m_connParams.hostKeyDatabase->insertHostKey(m_connParams.host, hostKey);
+    m_connParams.hostKeyDatabase->insertHostKey(m_connParams.host(), hostKey);
 }
 
 void SshKeyExchange::throwHostKeyException()
 {
     throw SshServerException(SSH_DISCONNECT_HOST_KEY_NOT_VERIFIABLE, "Host key changed",
                              SSH_TR("Host key of machine \"%1\" has changed.")
-                             .arg(m_connParams.host));
+                             .arg(m_connParams.host()));
 }
 
 } // namespace Internal

src/plugins/baremetal/baremetaldevice.cpp
@@ -104,8 +104,8 @@ void BareMetalDevice::setChannelByServerProvider(GdbServerProvider *provider)
     if (colon < 0)
         return;
     QSsh::SshConnectionParameters sshParams = sshParameters();
-    sshParams.host = channel.left(colon);
-    sshParams.port = channel.mid(colon + 1).toUShort();
+    sshParams.setHost(channel.left(colon));
+    sshParams.setPort(channel.mid(colon + 1).toUShort());
     setSshParameters(sshParams);
 }
 
@@ -121,8 +121,8 @@ void BareMetalDevice::fromMap(const QVariantMap &map)
             const QSsh::SshConnectionParameters sshParams = sshParameters();
             DefaultGdbServerProvider *newProvider = new DefaultGdbServerProvider;
             newProvider->setDisplayName(name);
-            newProvider->m_host = sshParams.host;
-            newProvider->m_port = sshParams.port;
+            newProvider->m_host = sshParams.host();
+            newProvider->m_port = sshParams.port();
             if (GdbServerProviderManager::registerProvider(newProvider))
                 gdbServerProvider = newProvider->id();
             else

src/plugins/debugger/analyzer/startremotedialog.cpp
@@ -64,7 +64,7 @@ StartRemoteDialog::StartRemoteDialog(QWidget *parent)
     d->kitChooser = new KitChooser(this);
     d->kitChooser->setKitPredicate([](const Kit *kit) {
         const IDevice::ConstPtr device = DeviceKitInformation::device(kit);
-        return kit->isValid() && device && !device->sshParameters().host.isEmpty();
+        return kit->isValid() && device && !device->sshParameters().host().isEmpty();
     });
     d->executable = new QLineEdit(this);
     d->arguments = new QLineEdit(this);

src/plugins/debugger/debuggerdialogs.cpp
@@ -429,7 +429,7 @@ void StartApplicationDialog::run(bool attachRemote)
     if (!inputAddress.isEmpty())
         debugger->setRemoteChannel(inputAddress);
     else
-        debugger->setRemoteChannel(dev->sshParameters().host, newParameters.serverPort);
+        debugger->setRemoteChannel(dev->sshParameters().host(), newParameters.serverPort);
     debugger->setRunControlName(newParameters.displayName());
     debugger->setBreakOnMain(newParameters.breakAtMain);
     debugger->setDebugInfoLocation(newParameters.debugInfoLocation);

src/plugins/debugger/debuggerplugin.cpp
@@ -2141,7 +2141,7 @@ void DebuggerPluginPrivate::attachToQmlPort()
     debugger->setQmlServer(qmlServer);
 
     QSsh::SshConnectionParameters sshParameters = device->sshParameters();
-    debugger->setRemoteChannel(sshParameters.host, sshParameters.port);
+    debugger->setRemoteChannel(sshParameters.host(), sshParameters.port());
     debugger->setStartMode(AttachToQmlServer);
 
     debugger->startRunControl();

src/plugins/debugger/debuggerruncontrol.cpp
@@ -972,7 +972,7 @@ Port GdbServerPortsGatherer::gdbServerPort() const
 QString GdbServerPortsGatherer::gdbServerChannel() const
 {
     QUrl url = channel(0);
-    const QString host = m_device->sshParameters().host;
+    const QString host = m_device->sshParameters().host();
     return QString("%1:%2").arg(host).arg(url.port());
 }
 

commit 3006a13869d3610586ce867f1cf92c5f94d9d1ac

Tests: Fix compile
Broke with 21c66ce5.

Change-Id: Iea3447a8226300333eacf8fa0df587d8ef197e8f
Reviewed-by: Nikita Baryshnikov <nib952051@gmail.com>
Reviewed-by: hjk <hjk@qt.io>

tests/manual/ssh/errorhandling/main.cpp
@@ -55,36 +55,36 @@ public:
             qDebug("Error: Unconnected SSH connection creates SFTP channel.");
 
         SshConnectionParameters noHost;
-        noHost.host = QLatin1String("hgdfxgfhgxfhxgfchxgcf");
-        noHost.port = 12345;
+        noHost.setHost("hgdfxgfhgxfhxgfchxgcf");
+        noHost.setPort(12345);
         noHost.timeout = 10;
         noHost.authenticationType
                 = SshConnectionParameters::AuthenticationTypeTryAllPasswordBasedMethods;
 
         SshConnectionParameters noUser;
-        noUser.host = QLatin1String("localhost");
-        noUser.port = 22;
+        noUser.setHost("localhost");
+        noUser.setPort(22);
         noUser.timeout = 30;
         noUser.authenticationType
                 = SshConnectionParameters::AuthenticationTypeTryAllPasswordBasedMethods;
-        noUser.userName = QLatin1String("dumdidumpuffpuff");
-        noUser.password = QLatin1String("whatever");
+        noUser.setUserName("dumdidumpuffpuff");
+        noUser.setPassword("whatever");
 
         SshConnectionParameters wrongPwd;
-        wrongPwd.host = QLatin1String("localhost");
-        wrongPwd.port = 22;
+        wrongPwd.setHost("localhost");
+        wrongPwd.setPort(22);
         wrongPwd.timeout = 30;
         wrongPwd.authenticationType
                 = SshConnectionParameters::AuthenticationTypeTryAllPasswordBasedMethods;
-        wrongPwd.userName = QLatin1String("root");
-        noUser.password = QLatin1String("thiscantpossiblybeapasswordcanit");
+        wrongPwd.setUserName("root");
+        noUser.setPassword("thiscantpossiblybeapasswordcanit");
 
         SshConnectionParameters invalidKeyFile;
-        invalidKeyFile.host = QLatin1String("localhost");
-        invalidKeyFile.port = 22;
+        invalidKeyFile.setHost("localhost");
+        invalidKeyFile.setPort(22);
         invalidKeyFile.timeout = 30;
         invalidKeyFile.authenticationType = SshConnectionParameters::AuthenticationTypePublicKey;
-        invalidKeyFile.userName = QLatin1String("root");
+        invalidKeyFile.setUserName("root");
         invalidKeyFile.privateKeyFile
             = QLatin1String("somefilenamethatwedontexpecttocontainavalidkey");
 

tests/manual/ssh/remoteprocess/argumentscollector.cpp
@@ -52,13 +52,19 @@ SshConnectionParameters ArgumentsCollector::collect(bool &success) const
         int port;
 
         for (pos = 1; pos < m_arguments.count() - 1; ++pos) {
-            if (checkAndSetStringArg(pos, parameters.host, "-h")
-                || checkAndSetStringArg(pos, parameters.userName, "-u"))
+            QString host;
+            QString user;
+            if (checkAndSetStringArg(pos, host, "-h") || checkAndSetStringArg(pos, user, "-u")) {
+                parameters.setHost(host);
+                parameters.setUserName(user);
                 continue;
+            }
             if (checkAndSetIntArg(pos, port, portGiven, "-p")
                 || checkAndSetIntArg(pos, parameters.timeout, timeoutGiven, "-t"))
                 continue;
-            if (checkAndSetStringArg(pos, parameters.password, "-pwd")) {
+            QString pass;
+            if (checkAndSetStringArg(pos, pass, "-pwd")) {
+                parameters.setPassword(pass);
                 if (!parameters.privateKeyFile.isEmpty())
                     throw ArgumentErrorException(QLatin1String("-pwd and -k are mutually exclusive."));
                 parameters.authenticationType
@@ -67,7 +73,7 @@ SshConnectionParameters ArgumentsCollector::collect(bool &success) const
                 continue;
             }
             if (checkAndSetStringArg(pos, parameters.privateKeyFile, "-k")) {
-                if (!parameters.password.isEmpty())
+                if (!parameters.password().isEmpty())
                     throw ArgumentErrorException(QLatin1String("-pwd and -k are mutually exclusive."));
                 parameters.authenticationType
                     = SshConnectionParameters::AuthenticationTypePublicKey;
@@ -89,17 +95,15 @@ SshConnectionParameters ArgumentsCollector::collect(bool &success) const
             parameters.privateKeyFile = QDir::homePath() + QLatin1String("/.ssh/id_rsa");
         }
 
-        if (parameters.userName.isEmpty()) {
-            parameters.userName
-                = QProcessEnvironment::systemEnvironment().value(QLatin1String("USER"));
-        }
-        if (parameters.userName.isEmpty())
+        if (parameters.userName().isEmpty())
+            parameters.setUserName(QProcessEnvironment::systemEnvironment().value("USER"));
+        if (parameters.userName().isEmpty())
             throw ArgumentErrorException(QLatin1String("No user name given."));
 
-        if (parameters.host.isEmpty())
+        if (parameters.host().isEmpty())
             throw ArgumentErrorException(QLatin1String("No host given."));
 
-        parameters.port = portGiven ? port : 22;
+        parameters.setPort(portGiven ? port : 22);
         if (!timeoutGiven)
             parameters.timeout = 30;
         success = true;

tests/manual/ssh/sftp/argumentscollector.cpp
@@ -50,15 +50,21 @@ Parameters ArgumentsCollector::collect(bool &success) const
         int pos;
         int port;
         for (pos = 1; pos < m_arguments.count() - 1; ++pos) {
-            if (checkAndSetStringArg(pos, parameters.sshParams.host, "-h")
-                || checkAndSetStringArg(pos, parameters.sshParams.userName, "-u"))
+            QString host;
+            QString user;
+            if (checkAndSetStringArg(pos, host, "-h") || checkAndSetStringArg(pos, user, "-u")) {
+                parameters.sshParams.setHost(host);
+                parameters.sshParams.setUserName(user);
                 continue;
+            }
             if (checkAndSetIntArg(pos, port, portGiven, "-p")
                 || checkAndSetIntArg(pos, parameters.sshParams.timeout, timeoutGiven, "-t")
                 || checkAndSetIntArg(pos, parameters.smallFileCount, smallFileCountGiven, "-c")
                 || checkAndSetIntArg(pos, parameters.bigFileSize, bigFileSizeGiven, "-s"))
                 continue;
-            if (checkAndSetStringArg(pos, parameters.sshParams.password, "-pwd")) {
+            QString pass;
+            if (checkAndSetStringArg(pos, pass, "-pwd")) {
+                parameters.sshParams.setPassword(pass);
                 if (!parameters.sshParams.privateKeyFile.isEmpty())
                     throw ArgumentErrorException(QLatin1String("-pwd and -k are mutually exclusive."));
                 parameters.sshParams.authenticationType
@@ -67,7 +73,7 @@ Parameters ArgumentsCollector::collect(bool &success) const
                 continue;
             }
             if (checkAndSetStringArg(pos, parameters.sshParams.privateKeyFile, "-k")) {
-                if (!parameters.sshParams.password.isEmpty())
+                if (!parameters.sshParams.password().isEmpty())
                     throw ArgumentErrorException(QLatin1String("-pwd and -k are mutually exclusive."));
                 parameters.sshParams.authenticationType
                     = SshConnectionParameters::AuthenticationTypePublicKey;
@@ -86,12 +92,12 @@ Parameters ArgumentsCollector::collect(bool &success) const
 
         if (!authTypeGiven)
             throw ArgumentErrorException(QLatin1String("No authentication argument given."));
-        if (parameters.sshParams.host.isEmpty())
+        if (parameters.sshParams.host().isEmpty())
             throw ArgumentErrorException(QLatin1String("No host given."));
-        if (parameters.sshParams.userName.isEmpty())
+        if (parameters.sshParams.userName().isEmpty())
             throw ArgumentErrorException(QLatin1String("No user name given."));
 
-        parameters.sshParams.port = portGiven ? port : 22;
+        parameters.sshParams.setPort(portGiven ? port : 22);
         if (!timeoutGiven)
             parameters.sshParams.timeout = 30;
         if (!smallFileCountGiven)

tests/manual/ssh/sftp/sftptest.cpp
@@ -60,7 +60,7 @@ void SftpTest::run()
     connect(m_connection, &SshConnection::error, this, &SftpTest::handleError);
     connect(m_connection, &SshConnection::disconnected, this, &SftpTest::handleDisconnected);
     std::cout << "Connecting to host '"
-        << qPrintable(m_parameters.sshParams.host) << "'..." << std::endl;
+        << qPrintable(m_parameters.sshParams.host()) << "'..." << std::endl;
     m_state = Connecting;
     m_connection->connectToHost();
 }

tests/manual/ssh/sftpfsmodel/window.cpp
@@ -57,12 +57,12 @@ void SftpFsWindow::connectToHost()
 {
     m_ui->connectButton->setEnabled(false);
     SshConnectionParameters sshParams;
-    sshParams.host = m_ui->hostLineEdit->text();
-    sshParams.userName = m_ui->userLineEdit->text();
+    sshParams.setHost(m_ui->hostLineEdit->text());
+    sshParams.setUserName(m_ui->userLineEdit->text());
     sshParams.authenticationType
             = SshConnectionParameters::AuthenticationTypeTryAllPasswordBasedMethods;
-    sshParams.password = m_ui->passwordLineEdit->text();
-    sshParams.port = m_ui->portSpinBox->value();
+    sshParams.setPassword(m_ui->passwordLineEdit->text());
+    sshParams.setPort(m_ui->portSpinBox->value());
     sshParams.timeout = 10;
     m_fsModel = new SftpFileSystemModel(this);
     if (m_ui->useModelTesterCheckBox->isChecked())

tests/manual/ssh/tunnel/argumentscollector.cpp
@@ -43,7 +43,7 @@ SshConnectionParameters ArgumentsCollector::collect(bool &success) const
 {
     SshConnectionParameters parameters;
     parameters.options &= ~SshIgnoreDefaultProxy;
-    parameters.host = QLatin1String("localhost");
+    parameters.setHost("localhost");
 
     try {
         bool authTypeGiven = false;
@@ -54,12 +54,17 @@ SshConnectionParameters ArgumentsCollector::collect(bool &success) const
         int port;
 
         for (pos = 1; pos < m_arguments.count() - 1; ++pos) {
-            if (checkAndSetStringArg(pos, parameters.userName, "-u"))
+            QString user;
+            if (checkAndSetStringArg(pos, user, "-u")) {
+                parameters.setUserName(user);
                 continue;
+            }
             if (checkAndSetIntArg(pos, port, portGiven, "-p")
                 || checkAndSetIntArg(pos, parameters.timeout, timeoutGiven, "-t"))
                 continue;
-            if (checkAndSetStringArg(pos, parameters.password, "-pwd")) {
+            QString pass;
+            if (checkAndSetStringArg(pos, pass, "-pwd")) {
+                parameters.setPassword(pass);
                 if (!parameters.privateKeyFile.isEmpty())
                     throw ArgumentErrorException(QLatin1String("-pwd and -k are mutually exclusive."));
                 parameters.authenticationType
@@ -68,7 +73,7 @@ SshConnectionParameters ArgumentsCollector::collect(bool &success) const
                 continue;
             }
             if (checkAndSetStringArg(pos, parameters.privateKeyFile, "-k")) {
-                if (!parameters.password.isEmpty())
+                if (!parameters.password().isEmpty())
                     throw ArgumentErrorException(QLatin1String("-pwd and -k are mutually exclusive."));
                 parameters.authenticationType
                     = SshConnectionParameters::AuthenticationTypePublicKey;
@@ -90,17 +95,15 @@ SshConnectionParameters ArgumentsCollector::collect(bool &success) const
             parameters.privateKeyFile = QDir::homePath() + QLatin1String("/.ssh/id_rsa");
         }
 
-        if (parameters.userName.isEmpty()) {
-            parameters.userName
-                = QProcessEnvironment::systemEnvironment().value(QLatin1String("USER"));
-        }
-        if (parameters.userName.isEmpty())
+        if (parameters.userName().isEmpty())
+            parameters.setUserName(QProcessEnvironment::systemEnvironment().value("USER"));
+        if (parameters.userName().isEmpty())
             throw ArgumentErrorException(QLatin1String("No user name given."));
 
-        if (parameters.host.isEmpty())
+        if (parameters.host().isEmpty())
             throw ArgumentErrorException(QLatin1String("No host given."));
 
-        parameters.port = portGiven ? port : 22;
+        parameters.setPort(portGiven ? port : 22);
         if (!timeoutGiven)
             parameters.timeout = 30;
         success = true;

tests/manual/ssh/tunnel/main.cpp
@@ -42,13 +42,13 @@ int main(int argc, char *argv[])
     bool parseSuccess;
     QSsh::SshConnectionParameters parameters
         = ArgumentsCollector(app.arguments()).collect(parseSuccess);
-    parameters.host = QLatin1String("127.0.0.1");
+    parameters.setHost("127.0.0.1");
     if (!parseSuccess)
         return EXIT_FAILURE;
 
     DirectTunnel direct(parameters);
 
-    parameters.host = QLatin1String("127.0.0.2");
+    parameters.setHost("127.0.0.2");
     ForwardTunnel forward(parameters);
     forward.run();
 