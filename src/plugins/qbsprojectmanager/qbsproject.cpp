qt-creator/qt-creator

commit e52ebbf217499e50a00ec35095aaf0f606416491

ProjectExplorer: Add subscribeSignal method to targets and projects
Add a subscribeSignal method to targets and projects that will make
sure all signals of all project configurations added during the lifetime
of the project/target will get connected (if the type matches).

Use this to connect to some signal in all BuildConfigurations of
a project and get rid of code that keeps connecting to the current
build configuration.

Use Project::buildEnvironmentChanged as an example and convert its
usages.

Change-Id: I689bcebac4b191bf3f8a18765bf18eaac371c5fe
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/plugins/qbsprojectmanager/qbsproject.cpp
@@ -140,7 +140,15 @@ QbsProject::QbsProject(const FileName &fileName) :
     connect(this, &Project::activeTargetChanged, this, &QbsProject::changeActiveTarget);
     connect(this, &Project::addedTarget, this, &QbsProject::targetWasAdded);
     connect(this, &Project::removedTarget, this, &QbsProject::targetWasRemoved);
-    connect(this, &Project::environmentChanged, this, &QbsProject::delayParsing);
+    subscribeSignal(&BuildConfiguration::environmentChanged, this, [this]() {
+        if (static_cast<BuildConfiguration *>(sender())->isActive())
+            startParsing();
+    });
+    connect(this, &Project::activeProjectConfigurationChanged,
+            this, [this](ProjectConfiguration *pc) {
+        if (pc->isActive())
+            startParsing();
+    });
 
     connect(&m_parsingDelay, &QTimer::timeout, this, &QbsProject::startParsing);
 

src/plugins/autotoolsprojectmanager/makestep.cpp
@@ -236,8 +236,15 @@ MakeStepConfigWidget::MakeStepConfigWidget(MakeStep *makeStep) :
             makeStep, &MakeStep::setAdditionalArguments);
     connect(makeStep, &MakeStep::additionalArgumentsChanged,
             this, &MakeStepConfigWidget::updateDetails);
-    connect(m_makeStep->project(), &Project::environmentChanged,
-            this, &MakeStepConfigWidget::updateDetails);
+    m_makeStep->project()->subscribeSignal(&BuildConfiguration::environmentChanged, this, [this]() {
+        if (static_cast<BuildConfiguration *>(sender())->isActive())
+            updateDetails();
+    });
+    connect(makeStep->project(), &Project::activeProjectConfigurationChanged,
+            this, [this](ProjectConfiguration *pc) {
+        if (pc->isActive())
+            updateDetails();
+    });
 }
 
 QString MakeStepConfigWidget::displayName() const

src/plugins/cmakeprojectmanager/cmakebuildstep.cpp
@@ -475,8 +475,16 @@ CMakeBuildStepConfigWidget::CMakeBuildStepConfigWidget(CMakeBuildStep *buildStep
 
     connect(m_buildStep, &CMakeBuildStep::buildTargetsChanged, this, &CMakeBuildStepConfigWidget::buildTargetsChanged);
     connect(m_buildStep, &CMakeBuildStep::targetToBuildChanged, this, &CMakeBuildStepConfigWidget::selectedBuildTargetsChanged);
-    connect(static_cast<CMakeProject *>(m_buildStep->project()), &CMakeProject::environmentChanged,
-            this, &CMakeBuildStepConfigWidget::updateDetails);
+    m_buildStep->project()->subscribeSignal(&BuildConfiguration::environmentChanged, this, [this]() {
+        if (static_cast<BuildConfiguration *>(sender())->isActive())
+            updateDetails();
+    });
+    connect(m_buildStep->project(), &Project::activeProjectConfigurationChanged,
+            this, [this](ProjectConfiguration *pc) {
+        if (pc->isActive())
+            updateDetails();
+    });
+
 }
 
 void CMakeBuildStepConfigWidget::toolArgumentsEdited()

src/plugins/genericprojectmanager/genericmakestep.cpp
@@ -232,7 +232,7 @@ GenericMakeStepConfigWidget::GenericMakeStepConfigWidget(GenericMakeStep *makeSt
 
     m_ui->makeLineEdit->setText(m_makeStep->m_makeCommand);
     m_ui->makeArgumentsLineEdit->setText(m_makeStep->m_makeArguments);
-    updateMakeOverrrideLabel();
+    updateMakeOverrideLabel();
     updateDetails();
 
     connect(m_ui->targetsList, &QListWidget::itemChanged,
@@ -243,17 +243,26 @@ GenericMakeStepConfigWidget::GenericMakeStepConfigWidget(GenericMakeStep *makeSt
             this, &GenericMakeStepConfigWidget::makeArgumentsLineEditTextEdited);
 
     connect(ProjectExplorerPlugin::instance(), &ProjectExplorerPlugin::settingsChanged,
-            this, &GenericMakeStepConfigWidget::updateMakeOverrrideLabel);
+            this, &GenericMakeStepConfigWidget::updateMakeOverrideLabel);
     connect(ProjectExplorerPlugin::instance(), &ProjectExplorerPlugin::settingsChanged,
             this, &GenericMakeStepConfigWidget::updateDetails);
 
     connect(m_makeStep->target(), &Target::kitChanged,
-            this, &GenericMakeStepConfigWidget::updateMakeOverrrideLabel);
+            this, &GenericMakeStepConfigWidget::updateMakeOverrideLabel);
 
-    connect(pro, &GenericProject::environmentChanged,
-            this, &GenericMakeStepConfigWidget::updateMakeOverrrideLabel);
-    connect(pro, &GenericProject::environmentChanged,
-            this, &GenericMakeStepConfigWidget::updateDetails);
+    pro->subscribeSignal(&BuildConfiguration::environmentChanged, this, [this]() {
+        if (static_cast<BuildConfiguration *>(sender())->isActive()) {
+            updateMakeOverrideLabel();
+            updateDetails();
+        }
+    });
+    connect(pro, &Project::activeProjectConfigurationChanged,
+            this, [this](ProjectConfiguration *pc) {
+        if (pc->isActive()) {
+            updateMakeOverrideLabel();
+            updateDetails();
+        }
+    });
 }
 
 GenericMakeStepConfigWidget::~GenericMakeStepConfigWidget()
@@ -266,7 +275,7 @@ QString GenericMakeStepConfigWidget::displayName() const
     return tr("Make", "GenericMakestep display name.");
 }
 
-void GenericMakeStepConfigWidget::updateMakeOverrrideLabel()
+void GenericMakeStepConfigWidget::updateMakeOverrideLabel()
 {
     BuildConfiguration *bc = m_makeStep->buildConfiguration();
     if (!bc)

src/plugins/genericprojectmanager/genericmakestep.h
@@ -91,7 +91,7 @@ private:
     void itemChanged(QListWidgetItem *item);
     void makeLineEditTextEdited();
     void makeArgumentsLineEditTextEdited();
-    void updateMakeOverrrideLabel();
+    void updateMakeOverrideLabel();
     void updateDetails();
 
     Ui::GenericMakeStep *m_ui;

src/plugins/ios/iosbuildstep.cpp
@@ -260,7 +260,15 @@ IosBuildStepConfigWidget::IosBuildStepConfigWidget(IosBuildStep *buildStep)
             this, &IosBuildStepConfigWidget::updateDetails);
     connect(m_buildStep->target(), &Target::kitChanged,
             this, &IosBuildStepConfigWidget::updateDetails);
-    connect(pro, &Project::environmentChanged, this, &IosBuildStepConfigWidget::updateDetails);
+    pro->subscribeSignal(&BuildConfiguration::environmentChanged, this, [this]() {
+        if (static_cast<BuildConfiguration *>(sender())->isActive())
+            updateDetails();
+    });
+    connect(pro, &Project::activeProjectConfigurationChanged,
+            this, [this](ProjectConfiguration *pc) {
+        if (pc->isActive())
+            updateDetails();
+    });
 }
 
 IosBuildStepConfigWidget::~IosBuildStepConfigWidget()

src/plugins/ios/iosdsymbuildstep.cpp
@@ -260,7 +260,15 @@ IosPresetBuildStepConfigWidget::IosPresetBuildStepConfigWidget(IosPresetBuildSte
             this, &IosPresetBuildStepConfigWidget::updateDetails);
     connect(m_buildStep->target(), &Target::kitChanged,
             this, &IosPresetBuildStepConfigWidget::updateDetails);
-    connect(pro, &Project::environmentChanged, this, &IosPresetBuildStepConfigWidget::updateDetails);
+    pro->subscribeSignal(&BuildConfiguration::environmentChanged, this, [this]() {
+        if (static_cast<BuildConfiguration *>(sender())->isActive())
+            updateDetails();
+    });
+    connect(pro, &Project::activeProjectConfigurationChanged,
+            this, [this](ProjectConfiguration *pc) {
+        if (pc->isActive())
+            updateDetails();
+    });
 }
 
 IosPresetBuildStepConfigWidget::~IosPresetBuildStepConfigWidget()

src/plugins/projectexplorer/buildenvironmentwidget.cpp
@@ -53,7 +53,7 @@ BuildEnvironmentWidget::BuildEnvironmentWidget(BuildConfiguration *bc) :
 
     m_buildConfiguration = bc;
 
-    connect(m_buildConfiguration->target(), &Target::environmentChanged,
+    connect(m_buildConfiguration, &BuildConfiguration::environmentChanged,
             this, &BuildEnvironmentWidget::environmentChanged);
 
     m_clearSystemEnvironmentCheckBox->setChecked(!m_buildConfiguration->useSystemEnvironment());

commit af62a9590c33dd9daec1bab49cc4a9494410aa62

QbsProjectManager: Fix reparsing after switching build configurations
After switching to a different build configuration, property changes in
the build config widget would no longer cause project reparsing. Likely
caused by e52ebbf217.

Change-Id: Ifebec74475def49885232ad71d4de01f51568fcb
Reviewed-by: hjk <hjk@qt.io>

src/plugins/qbsprojectmanager/qbsproject.cpp
@@ -125,7 +125,6 @@ QbsProject::QbsProject(const FileName &fileName) :
     m_parsingScheduled(false),
     m_cancelStatus(CancelStatusNone),
     m_cppCodeModelUpdater(new CppTools::CppProjectUpdater(this)),
-    m_currentBc(0),
     m_extraCompilersPending(false)
 {
     m_parsingDelay.setInterval(1000); // delay parsing by 1s.
@@ -148,6 +147,7 @@ QbsProject::QbsProject(const FileName &fileName) :
     };
     subscribeSignal(&BuildConfiguration::environmentChanged, this, delayedParsing);
     subscribeSignal(&BuildConfiguration::buildDirectoryChanged, this, delayedParsing);
+    subscribeSignal(&QbsBuildConfiguration::qbsConfigurationChanged, this, delayedParsing);
     subscribeSignal(&Target::activeBuildConfigurationChanged, this, delayedParsing);
 
     connect(&m_parsingDelay, &QTimer::timeout, this, &QbsProject::startParsing);
@@ -336,11 +336,6 @@ bool QbsProject::renameFileInProduct(const QString &oldPath, const QString &newP
         return false;
 
     return addFilesToProduct(QStringList() << newPath, newProductData, newGroupData, &dummy);
-}
-
-void QbsProject::invalidate()
-{
-    prepareForParsing();
 }
 
 static qbs::AbstractJob *doBuildOrClean(const qbs::Project &project,
@@ -546,29 +541,8 @@ void QbsProject::handleRuleExecutionDone()
 
 void QbsProject::changeActiveTarget(Target *t)
 {
-    BuildConfiguration *bc = 0;
-    if (t) {
+    if (t)
         m_qbsProject = m_qbsProjects.value(t);
-        if (t->kit())
-            bc = t->activeBuildConfiguration();
-    }
-    buildConfigurationChanged(bc);
-}
-
-void QbsProject::buildConfigurationChanged(BuildConfiguration *bc)
-{
-    if (m_currentBc)
-        disconnect(m_currentBc, &QbsBuildConfiguration::qbsConfigurationChanged,
-                   this, &QbsProject::delayParsing);
-
-    m_currentBc = qobject_cast<QbsBuildConfiguration *>(bc);
-    if (m_currentBc) {
-        connect(m_currentBc, &QbsBuildConfiguration::qbsConfigurationChanged,
-                this, &QbsProject::delayParsing);
-        delayParsing();
-    } else {
-        invalidate();
-    }
 }
 
 void QbsProject::startParsing()

src/plugins/qbsprojectmanager/qbsproject.h
@@ -110,7 +110,6 @@ public:
 
     void configureAsExampleProject(const QSet<Core::Id> &platforms) final;
 
-    void invalidate();
     void delayParsing();
 
 private:
@@ -119,7 +118,6 @@ private:
     void rebuildProjectTree();
 
     void changeActiveTarget(ProjectExplorer::Target *t);
-    void buildConfigurationChanged(ProjectExplorer::BuildConfiguration *bc);
     void startParsing();
 
     void parse(const QVariantMap &config, const Utils::Environment &env, const QString &dir,
@@ -168,7 +166,6 @@ private:
     CppTools::CppProjectUpdater *m_cppCodeModelUpdater = nullptr;
     CppTools::ProjectInfo m_cppCodeModelProjectInfo;
 
-    QbsBuildConfiguration *m_currentBc;
     mutable ProjectExplorer::ProjectImporter *m_importer = nullptr;
 
     QTimer m_parsingDelay;