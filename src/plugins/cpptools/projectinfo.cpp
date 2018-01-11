qt-creator/qt-creator

commit e51683412b311aaafbf4c649a514695c4e7a156c

ProjectPart: save codegen flags in project part
And add them to other clang code model arguments.
These flags provide architecture for cross-compilation
when ios kit is selected.

Task-number: QTCREATORBUG-19437
Task-number: QTCREATORBUG-19430
Change-Id: I7a485f49d637371bb28b2096086d7d8a4b0c404a
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/plugins/cpptools/projectinfo.cpp
@@ -26,6 +26,7 @@
 #include "projectinfo.h"
 
 #include <projectexplorer/abi.h>
+#include <projectexplorer/gcctoolchain.h>
 #include <projectexplorer/kitinformation.h>
 #include <projectexplorer/projectexplorerconstants.h>
 
@@ -41,10 +42,11 @@ ToolChainInfo::ToolChainInfo(const ProjectExplorer::ToolChain *toolChain,
                 = toolChain->targetAbi().osFlavor() == ProjectExplorer::Abi::WindowsMsvc2015Flavor;
         wordWidth = toolChain->targetAbi().wordWidth();
         targetTriple = toolChain->originalTargetTriple();
+        extraCodeModelFlags = toolChain->extraCodeModelFlags();
 
         // ...and save the potentially expensive operations for later so that
         // they can be run from a worker thread.
-        sysRoothPath = ProjectExplorer::SysRootKitInformation::sysRoot(kit).toString();
+        sysRootPath = ProjectExplorer::SysRootKitInformation::sysRoot(kit).toString();
         headerPathsRunner = toolChain->createSystemHeaderPathsRunner();
         predefinedMacrosRunner = toolChain->createPredefinedMacrosRunner();
     }

src/plugins/projectexplorer/toolchain.h
@@ -100,6 +100,7 @@ public:
     virtual Abi targetAbi() const = 0;
     virtual QList<Abi> supportedAbis() const;
     virtual QString originalTargetTriple() const { return QString(); }
+    virtual QStringList extraCodeModelFlags() const { return QStringList(); }
 
     virtual bool isValid() const = 0;
 

src/plugins/cpptools/compileroptionsbuilder.cpp
@@ -52,6 +52,7 @@ QStringList CompilerOptionsBuilder::build(CppTools::ProjectFile::Kind fileKind, 
 
     addWordWidth();
     addTargetTriple();
+    addExtraCodeModelFlags();
     addLanguageOption(fileKind);
     addOptionsForLanguage(/*checkForBorlandExtensions*/ true);
     enableExceptions();
@@ -102,6 +103,14 @@ void CompilerOptionsBuilder::addTargetTriple()
         m_options.append(QLatin1String("-target"));
         m_options.append(m_projectPart.toolChainTargetTriple);
     }
+}
+
+void CompilerOptionsBuilder::addExtraCodeModelFlags()
+{
+    // extraCodeModelFlags keep build architecture for cross-compilation.
+    // In case of iOS build target triple has aarch64 archtecture set which makes
+    // code model fail with CXError_Failure. To fix that we explicitly provide architecture.
+    m_options.append(m_projectPart.extraCodeModelFlags);
 }
 
 void CompilerOptionsBuilder::enableExceptions()

src/plugins/cpptools/compileroptionsbuilder.h
@@ -45,6 +45,7 @@ public:
     virtual ~CompilerOptionsBuilder() {}
 
     virtual void addTargetTriple();
+    virtual void addExtraCodeModelFlags();
     virtual void enableExceptions();
     virtual void addPredefinedHeaderPathsOptions();
     virtual void addLanguageOption(ProjectFile::Kind fileKind);

src/plugins/cpptools/cppprojectinfogenerator.cpp
@@ -52,6 +52,7 @@ public:
         m_projectPart.isMsvc2015Toolchain = m_tcInfo.isMsvc2015ToolChain;
         m_projectPart.toolChainWordWidth = mapWordWith(m_tcInfo.wordWidth);
         m_projectPart.toolChainTargetTriple = m_tcInfo.targetTriple;
+        m_projectPart.extraCodeModelFlags = m_tcInfo.extraCodeModelFlags;
 
         m_projectPart.warningFlags = m_flags.warningFlags;
 
@@ -128,7 +129,7 @@ private:
 
         const QList<ProjectExplorer::HeaderPath> systemHeaderPaths
                 = m_tcInfo.headerPathsRunner(m_flags.commandLineFlags,
-                                             m_tcInfo.sysRoothPath);
+                                             m_tcInfo.sysRootPath);
 
         ProjectPartHeaderPaths &headerPaths = m_projectPart.headerPaths;
         for (const ProjectExplorer::HeaderPath &header : systemHeaderPaths) {

src/plugins/cpptools/projectinfo.h
@@ -54,8 +54,9 @@ public:
     bool isMsvc2015ToolChain = false;
     unsigned wordWidth = 0;
     QString targetTriple;
+    QStringList extraCodeModelFlags;
 
-    QString sysRoothPath; // For headerPathsRunner.
+    QString sysRootPath; // For headerPathsRunner.
     ProjectExplorer::ToolChain::SystemHeaderPathsRunner headerPathsRunner;
     ProjectExplorer::ToolChain::PredefinedMacrosRunner predefinedMacrosRunner;
 };

src/plugins/cpptools/projectpart.h
@@ -140,6 +140,7 @@ public:
     ProjectExplorer::Macros toolChainMacros;
     ToolChainWordWidth toolChainWordWidth = WordWidth32Bit;
     QString toolChainTargetTriple;
+    QStringList extraCodeModelFlags;
     BuildTargetType buildTargetType = Unknown;
 };
 

src/plugins/projectexplorer/gcctoolchain.cpp
@@ -774,6 +774,11 @@ void GccToolChain::setPlatformCodeGenFlags(const QStringList &flags)
     }
 }
 
+QStringList GccToolChain::extraCodeModelFlags() const
+{
+    return platformCodeGenFlags();
+}
+
 /*!
     Code gen flags that have to be passed to the compiler.
  */

commit 6285a6e50f2e844cbd2f2d11ffccc99aa7291552

CppTools: Fix build
Broke by e51683412b311aaafbf4c649a514695c4e7a156c.

Change-Id: I68d1457931f2237a63da4ac2e34f1650b9ff6ea6
Reviewed-by: Ivan Donchevskii <ivan.donchevskii@qt.io>

src/plugins/cpptools/projectinfo.cpp
@@ -26,7 +26,7 @@
 #include "projectinfo.h"
 
 #include <projectexplorer/abi.h>
-#include <projectexplorer/gcctoolchain.h>
+#include <projectexplorer/toolchain.h>
 #include <projectexplorer/kitinformation.h>
 #include <projectexplorer/projectexplorerconstants.h>
 

tests/unit/mockup/projectexplorer/toolchain.h
@@ -64,6 +64,7 @@ public:
     virtual PredefinedMacrosRunner createPredefinedMacrosRunner() const { return PredefinedMacrosRunner(); }
 
     virtual QString originalTargetTriple() const { return QString(); }
+    virtual QStringList extraCodeModelFlags() const { return QStringList(); }
 };
 
 } // namespace ProjectExplorer