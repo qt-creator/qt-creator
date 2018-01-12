qt-creator/qt-creator

commit f42ea09b74878edc215b317cba08c1f7c7b48a47

Do not construct settings/resource paths from QSettings object
These paths are controlled by ICore.

Add ICore::installerResourcePath() to replace constructions with
settings(SystemScope)
Replace constructions with settings()->fileName() by usage of
ICore::userResourcePath().

Change-Id: I5a9fa9d09f2563c39dc5d11a4586da825c62f9ac
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/plugins/projectexplorer/kitmanager.cpp
@@ -53,12 +53,11 @@ const char KIT_DATA_KEY[] = "Profile.";
 const char KIT_COUNT_KEY[] = "Profile.Count";
 const char KIT_FILE_VERSION_KEY[] = "Version";
 const char KIT_DEFAULT_KEY[] = "Profile.Default";
-const char KIT_FILENAME[] = "/qtcreator/profiles.xml";
+const char KIT_FILENAME[] = "/profiles.xml";
 
 static FileName settingsFileName()
 {
-    QFileInfo settingsLocation(ICore::settings()->fileName());
-    return FileName::fromString(settingsLocation.absolutePath() + QLatin1String(KIT_FILENAME));
+    return FileName::fromString(ICore::resourcePath() + KIT_FILENAME);
 }
 
 // --------------------------------------------------------------------------
@@ -131,8 +130,7 @@ void KitManager::restoreKits()
     QList<Kit *> sdkKits;
 
     // read all kits from SDK
-    QFileInfo systemSettingsFile(ICore::settings(QSettings::SystemScope)->fileName());
-    QFileInfo kitFile(systemSettingsFile.absolutePath() + QLatin1String(KIT_FILENAME));
+    QFileInfo kitFile(ICore::installerResourcePath() + KIT_FILENAME);
     if (kitFile.exists()) {
         KitList system = restoreKits(FileName(kitFile));
         // make sure we mark these as autodetected and run additional setup logic

src/plugins/projectexplorer/toolchainmanager.cpp
@@ -44,14 +44,13 @@
 static const char TOOLCHAIN_DATA_KEY[] = "ToolChain.";
 static const char TOOLCHAIN_COUNT_KEY[] = "ToolChain.Count";
 static const char TOOLCHAIN_FILE_VERSION_KEY[] = "Version";
-static const char TOOLCHAIN_FILENAME[] = "/qtcreator/toolchains.xml";
+static const char TOOLCHAIN_FILENAME[] = "/toolchains.xml";
 
 using namespace Utils;
 
 static FileName settingsFileName(const QString &path)
 {
-    QFileInfo settingsLocation(Core::ICore::settings()->fileName());
-    return FileName::fromString(settingsLocation.absolutePath() + path);
+    return FileName::fromString(Core::ICore::resourcePath() + path);
 }
 
 namespace ProjectExplorer {
@@ -304,9 +303,8 @@ void ToolChainManager::restoreToolChains()
 
 QList<ToolChain *> ToolChainManager::readSystemFileToolChains()
 {
-    QFileInfo systemSettingsFile(Core::ICore::settings(QSettings::SystemScope)->fileName());
-    QList<ToolChain *> systemTcs
-            = restoreFromFile(FileName::fromString(systemSettingsFile.absolutePath() + QLatin1String(TOOLCHAIN_FILENAME)));
+    QList<ToolChain *> systemTcs = restoreFromFile(
+        FileName::fromString(Core::ICore::installerResourcePath() + TOOLCHAIN_FILENAME));
 
     foreach (ToolChain *tc, systemTcs)
         tc->setDetection(ToolChain::AutoDetection);

src/plugins/qtsupport/qtversionmanager.cpp
@@ -63,7 +63,7 @@ using namespace Internal;
 const char QTVERSION_DATA_KEY[] = "QtVersion.";
 const char QTVERSION_TYPE_KEY[] = "QtVersion.Type";
 const char QTVERSION_FILE_VERSION_KEY[] = "Version";
-const char QTVERSION_FILENAME[] = "/qtcreator/qtversion.xml";
+const char QTVERSION_FILENAME[] = "/qtversion.xml";
 
 static QMap<int, BaseQtVersion *> m_versions;
 static int m_idcount = 0;
@@ -77,16 +77,12 @@ enum { debug = 0 };
 
 static FileName globalSettingsFileName()
 {
-    QSettings *globalSettings = ExtensionSystem::PluginManager::globalSettings();
-    return FileName::fromString(QFileInfo(globalSettings->fileName()).absolutePath()
-                                       + QLatin1String(QTVERSION_FILENAME));
+    return FileName::fromString(Core::ICore::installerResourcePath() + QTVERSION_FILENAME);
 }
 
 static FileName settingsFileName(const QString &path)
 {
-    QSettings *settings = ExtensionSystem::PluginManager::settings();
-    QFileInfo settingsLocation(settings->fileName());
-    return FileName::fromString(settingsLocation.absolutePath() + path);
+    return FileName::fromString(Core::ICore::resourcePath() + path);
 }
 
 

src/plugins/android/androidconfigurations.cpp
@@ -127,8 +127,7 @@ namespace {
 
     static QString sdkSettingsFileName()
     {
-        return QFileInfo(Core::ICore::settings(QSettings::SystemScope)->fileName()).absolutePath()
-                + QLatin1String("/qtcreator/android.xml");
+        return Core::ICore::installerResourcePath() + "/android.xml";
     }
 
     static bool is32BitUserSpace()

src/plugins/baremetal/gdbserverprovidermanager.cpp
@@ -47,19 +47,13 @@ namespace Internal {
 const char dataKeyC[] = "GdbServerProvider.";
 const char countKeyC[] = "GdbServerProvider.Count";
 const char fileVersionKeyC[] = "Version";
-const char fileNameKeyC[] = "/qtcreator/gdbserverproviders.xml";
-
-static Utils::FileName settingsFileName(const QString &path)
-{
-    const QFileInfo settingsLocation(Core::ICore::settings()->fileName());
-    return Utils::FileName::fromString(settingsLocation.absolutePath() + path);
-}
+const char fileNameKeyC[] = "/gdbserverproviders.xml";
 
 static GdbServerProviderManager *m_instance = 0;
 
 GdbServerProviderManager::GdbServerProviderManager(QObject *parent)
     : QObject(parent)
-    , m_configFile(settingsFileName(QLatin1String(fileNameKeyC)))
+    , m_configFile(Utils::FileName::fromString(Core::ICore::userResourcePath() + fileNameKeyC))
     , m_factories({new DefaultGdbServerProviderFactory,
                    new OpenOcdGdbServerProviderFactory,
                    new StLinkUtilGdbServerProviderFactory})

src/plugins/cmakeprojectmanager/cmaketoolmanager.cpp
@@ -46,7 +46,7 @@ const char CMAKETOOL_COUNT_KEY[] = "CMakeTools.Count";
 const char CMAKETOOL_DEFAULT_KEY[] = "CMakeTools.Default";
 const char CMAKETOOL_DATA_KEY[] = "CMakeTools.";
 const char CMAKETOOL_FILE_VERSION_KEY[] = "Version";
-const char CMAKETOOL_FILENAME[] = "/qtcreator/cmaketools.xml";
+const char CMAKETOOL_FILENAME[] = "/cmaketools.xml";
 
 class CMakeToolManagerPrivate
 {
@@ -71,8 +71,7 @@ static void addCMakeTool(CMakeTool *item)
 
 static FileName userSettingsFileName()
 {
-    QFileInfo settingsLocation(ICore::settings()->fileName());
-    return FileName::fromString(settingsLocation.absolutePath() + QLatin1String(CMAKETOOL_FILENAME));
+    return FileName::fromString(ICore::userResourcePath() + CMAKETOOL_FILENAME);
 }
 
 static QList<CMakeTool *> readCMakeTools(const FileName &fileName, Core::Id *defaultId, bool fromSDK)
@@ -325,9 +324,8 @@ void CMakeToolManager::restoreCMakeTools()
 {
     Core::Id defaultId;
 
-    QFileInfo systemSettingsFile(ICore::settings(QSettings::SystemScope)->fileName());
-    FileName sdkSettingsFile = FileName::fromString(systemSettingsFile.absolutePath()
-                                                    + QLatin1String(CMAKETOOL_FILENAME));
+    FileName sdkSettingsFile = FileName::fromString(ICore::installerResourcePath()
+                                                    + CMAKETOOL_FILENAME);
 
     QList<CMakeTool *> toolsToRegister = readCMakeTools(sdkSettingsFile, &defaultId, true);
 

src/plugins/coreplugin/icore.cpp
@@ -421,6 +421,12 @@ QString ICore::userResourcePath()
     return urp;
 }
 
+QString ICore::installerResourcePath()
+{
+    return QFileInfo(settings(QSettings::SystemScope)->fileName()).path() + '/'
+           + Constants::IDE_ID;
+}
+
 QString ICore::documentationPath()
 {
     return QDir::cleanPath(QCoreApplication::applicationDirPath() + '/' + RELATIVE_DOC_PATH);

src/plugins/coreplugin/icore.h
@@ -94,6 +94,7 @@ public:
 
     static QString resourcePath();
     static QString userResourcePath();
+    static QString installerResourcePath();
     static QString documentationPath();
     static QString libexecPath();
 

commit 01de1e1fbe56d7676158828070570ae4fcec1604

Fix reading of user Qt versions, toolchains and kits
Broke by f42ea09b74878edc215b317cba08c1f7c7b48a47.

Change-Id: I90be029caf80fc6efe89c3329a183b396e0dc564
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/projectexplorer/kitmanager.cpp
@@ -57,7 +57,7 @@ const char KIT_FILENAME[] = "/profiles.xml";
 
 static FileName settingsFileName()
 {
-    return FileName::fromString(ICore::resourcePath() + KIT_FILENAME);
+    return FileName::fromString(ICore::userResourcePath() + KIT_FILENAME);
 }
 
 // --------------------------------------------------------------------------

src/plugins/projectexplorer/toolchainmanager.cpp
@@ -50,7 +50,7 @@ using namespace Utils;
 
 static FileName settingsFileName(const QString &path)
 {
-    return FileName::fromString(Core::ICore::resourcePath() + path);
+    return FileName::fromString(Core::ICore::userResourcePath() + path);
 }
 
 namespace ProjectExplorer {

src/plugins/qtsupport/qtversionmanager.cpp
@@ -82,7 +82,7 @@ static FileName globalSettingsFileName()
 
 static FileName settingsFileName(const QString &path)
 {
-    return FileName::fromString(Core::ICore::resourcePath() + path);
+    return FileName::fromString(Core::ICore::userResourcePath() + path);
 }
 
 