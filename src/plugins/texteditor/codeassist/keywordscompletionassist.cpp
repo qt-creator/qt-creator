qt-creator/qt-creator

commit ed5adb286a80de3e7d70332aa581b8880a72b4ab

KeywordCompletion: Do not forget half the completions
Broken with my recent commit to this file. Sorry.

Change-Id: I2fef1c01e33c9ea5ddf3fb3bd92401e3dc360c83
Reviewed-by: David Schulz <david.schulz@qt.io>

src/plugins/texteditor/codeassist/keywordscompletionassist.cpp
@@ -186,8 +186,8 @@ IAssistProposal *KeywordsCompletionAssistProcessor::perform(const AssistInterfac
         return new FunctionHintProposal(m_startPosition, model);
     } else {
         QList<AssistProposalItemInterface *> items = m_snippetCollector.collect();
-        addWordsToProposalList(&items, m_keywords.variables(), m_variableIcon);
-        addWordsToProposalList(&items, m_keywords.functions(), m_functionIcon);
+        items.append(generateProposalList(m_keywords.variables(), m_variableIcon));
+        items.append(generateProposalList(m_keywords.variables(), m_variableIcon));
         return new GenericProposal(m_startPosition, items);
     }
 }
@@ -252,13 +252,10 @@ bool KeywordsCompletionAssistProcessor::isInComment() const
     return lineBeginning.contains(startOfCommentChar());
 }
 
-void KeywordsCompletionAssistProcessor::addWordsToProposalList(QList<AssistProposalItemInterface *> *items,
-                                                               const QStringList &words, const QIcon &icon)
+QList<AssistProposalItemInterface *>
+KeywordsCompletionAssistProcessor::generateProposalList(const QStringList &words, const QIcon &icon)
 {
-    if (!items)
-        return;
-
-    *items = Utils::transform(words, [this, &icon](const QString &word) -> AssistProposalItemInterface * {
+    return Utils::transform(words, [this, &icon](const QString &word) -> AssistProposalItemInterface * {
         AssistProposalItem *item = new KeywordsAssistProposalItem(m_keywords.isFunction(word));
         item->setText(word);
         item->setIcon(icon);

src/plugins/texteditor/codeassist/keywordscompletionassist.h
@@ -97,8 +97,7 @@ private:
     bool acceptsIdleEditor();
     int findStartOfName(int pos = -1);
     bool isInComment() const;
-    void addWordsToProposalList(QList<AssistProposalItemInterface *> *items,
-                                const QStringList &words, const QIcon &icon);
+    QList<AssistProposalItemInterface *> generateProposalList(const QStringList &words, const QIcon &icon);
 
     int m_startPosition = -1;
     TextEditor::SnippetAssistCollector m_snippetCollector;

commit 52b717d3d10af7ad4770f9cb96c3943d438c627d

Editor: Fix completing functions with keywords assist
broken since ed5adb286a80de3e7d70332aa581b8880a72b4ab

Change-Id: Ic0fbdd2bcf544862d843abc83ecde8a917909c19
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/texteditor/codeassist/keywordscompletionassist.cpp
@@ -212,7 +212,7 @@ IAssistProposal *KeywordsCompletionAssistProcessor::perform(const AssistInterfac
     } else {
         QList<AssistProposalItemInterface *> items = m_snippetCollector.collect();
         items.append(generateProposalList(m_keywords.variables(), m_variableIcon));
-        items.append(generateProposalList(m_keywords.variables(), m_variableIcon));
+        items.append(generateProposalList(m_keywords.functions(), m_functionIcon));
         return new GenericProposal(startPosition, items);
     }
 }