7282e055546c93390881627f9cbb149559df735f
Robert Loehning <robert.loehning@qt.io>
2018-02-08 20:51:43 +0100

    Improve translated strings
    Change-Id: I7a45c6f8dcbb0e1b3c17a60823664fe1943c5654
    Reviewed-by: Orgad Shaneh <orgads@gmail.com>
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>

--- src/plugins/clangcodemodel/clanghoverhandler.cpp
+++ src/plugins/clangcodemodel/clanghoverhandler.cpp
@@ -196,7 +196,7 @@ void ClangHoverHandler::processToolTipInfo(const CppTools::ToolTipInfo &info)
     }
 
     if (!info.sizeInBytes.isEmpty())
-        text.append(tr("\n\n%1 bytes").arg(info.sizeInBytes));
+        text.append("\n\n" + tr("%n bytes", 0, info.sizeInBytes));
 
     setToolTip(text);
     m_reportPriority(priority());
