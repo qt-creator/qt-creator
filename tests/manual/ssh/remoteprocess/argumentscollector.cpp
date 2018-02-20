f9deab1140f788d39b4283ee1b32c722774d1b0c
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2018-02-20 23:28:24 +0200

    Tests: Initialize some local variables
    Clang warns about those (-Wconditional-uninitialized).
    
    Change-Id: I993b6267208a5ec0a443ddcb1ebac52aea2c92f0
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- tests/manual/ssh/remoteprocess/argumentscollector.cpp
+++ tests/manual/ssh/remoteprocess/argumentscollector.cpp
@@ -49,7 +49,7 @@ SshConnectionParameters ArgumentsCollector::collect(bool &success) const
         bool timeoutGiven = false;
         bool proxySettingGiven = false;
         int pos;
-        int port;
+        int port = 22;
 
         for (pos = 1; pos < m_arguments.count() - 1; ++pos) {
             QString host;
