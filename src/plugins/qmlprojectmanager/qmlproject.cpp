qt-creator/qt-creator

commit 30638df441515901c6d43b2c67a5ca4a53eaf814

QmlProject: Resolve import paths relative to target directory
On desktop the target directory is the same as the source directory. On
remote devices, it can be different. We need to resolve when we know
where to deploy.

Change-Id: I3f2bc088476ae73dac5231cb24f277c055f7d044
Task-number: QTCREATORBUG-19888
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/plugins/qmlprojectmanager/qmlprojectrunconfiguration.cpp
@@ -121,22 +121,35 @@ QString QmlProjectRunConfiguration::executable() const
     return qmlscene.isEmpty() ? QString("qmlscene") : qmlscene;
 }
 
+static QStringList makeAbsolute(const Utils::FileName &path, const QStringList &relativePaths)
+{
+    if (path.isEmpty())
+        return relativePaths;
+
+    const QDir baseDir(path.toString());
+    return Utils::transform(relativePaths, [&baseDir](const QString &path) {
+        return QDir::cleanPath(baseDir.absoluteFilePath(path));
+    });
+}
+
 QString QmlProjectRunConfiguration::commandLineArguments() const
 {
     // arguments in .user file
     QString args = m_qmlViewerArgs;
-    const IDevice::ConstPtr device = DeviceKitInformation::device(target()->kit());
+    const Target *currentTarget = target();
+    const IDevice::ConstPtr device = DeviceKitInformation::device(currentTarget->kit());
     const Utils::OsType osType = device ? device->osType() : Utils::HostOsInfo::hostOs();
 
     // arguments from .qmlproject file
-    QmlProject *project = static_cast<QmlProject *>(target()->project());
-    foreach (const QString &importPath, project->customImportPaths()) {
+    const QmlProject *project = static_cast<QmlProject *>(currentTarget->project());
+    foreach (const QString &importPath,
+             makeAbsolute(project->targetDirectory(currentTarget), project->customImportPaths())) {
         Utils::QtcProcess::addArg(&args, QLatin1String("-I"), osType);
         Utils::QtcProcess::addArg(&args, importPath, osType);
     }
 
-    const QString main
-            = project->targetFile(Utils::FileName::fromString(mainScript()), target()).toString();                                             ;
+    const QString main = project->targetFile(Utils::FileName::fromString(mainScript()),
+                                             currentTarget).toString();
     if (!main.isEmpty())
         Utils::QtcProcess::addArg(&args, main, osType);
     return args;

src/plugins/qmlprojectmanager/fileformat/qmlprojectitem.cpp
@@ -47,8 +47,6 @@ void QmlProjectItem::setSourceDirectory(const QString &directoryPath)
                     this, &QmlProjectItem::qmlFilesChanged);
         }
     }
-
-    setImportPaths(m_importPaths);
 }
 
 void QmlProjectItem::setTargetDirectory(const QString &directoryPath)
@@ -60,17 +58,6 @@ void QmlProjectItem::setImportPaths(const QStringList &importPaths)
 {
     if (m_importPaths != importPaths)
         m_importPaths = importPaths;
-
-    // convert to absolute paths
-    QStringList absoluteImportPaths;
-    const QDir sourceDir(sourceDirectory());
-    foreach (const QString &importPath, importPaths)
-        absoluteImportPaths += QDir::cleanPath(sourceDir.absoluteFilePath(importPath));
-
-    if (m_absoluteImportPaths == absoluteImportPaths)
-        return;
-
-    m_absoluteImportPaths = absoluteImportPaths;
 }
 
 /* Returns list of absolute paths */

src/plugins/qmlprojectmanager/fileformat/qmlprojectitem.h
@@ -51,7 +51,7 @@ public:
     QString targetDirectory() const { return m_targetDirectory; }
     void setTargetDirectory(const QString &directoryPath);
 
-    QStringList importPaths() const { return m_absoluteImportPaths; }
+    QStringList importPaths() const { return m_importPaths; }
     void setImportPaths(const QStringList &paths);
 
     QStringList files() const;
@@ -72,7 +72,6 @@ protected:
     QString m_sourceDirectory;
     QString m_targetDirectory;
     QStringList m_importPaths;
-    QStringList m_absoluteImportPaths;
     QString m_mainFile;
     QList<Utils::EnvironmentItem> m_environment;
     QList<QmlProjectContentItem *> m_content; // content property

commit 0474216e2ffa72cc67fe17964466b967de1c5be9

QmlProject: Fix import paths for code model
This fixes a regression introduced by
30638df441515901c6d43b2c67a5ca4a53eaf814.

Custom import paths have to be absolute for the code model.

Change-Id: I232229fa58c3900fe684ca4529b2ed08bf93e8be
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>
Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

src/plugins/qmlprojectmanager/qmlproject.cpp
@@ -177,7 +177,7 @@ void QmlProject::refresh(RefreshOptions options)
 
     QmlJS::ModelManagerInterface::ProjectInfo projectInfo =
             modelManager->defaultProjectInfoForProject(this);
-    foreach (const QString &searchPath, customImportPaths())
+    foreach (const QString &searchPath, makeAbsolute(canonicalProjectDir(), customImportPaths()))
         projectInfo.importPaths.maybeInsert(Utils::FileName::fromString(searchPath),
                                             QmlJS::Dialect::Qml);
 
@@ -247,6 +247,17 @@ bool QmlProject::addFiles(const QStringList &filePaths)
 void QmlProject::refreshProjectFile()
 {
     refresh(QmlProject::ProjectFile | Files);
+}
+
+QStringList QmlProject::makeAbsolute(const Utils::FileName &path, const QStringList &relativePaths)
+{
+    if (path.isEmpty())
+        return relativePaths;
+
+    const QDir baseDir(path.toString());
+    return Utils::transform(relativePaths, [&baseDir](const QString &path) {
+        return QDir::cleanPath(baseDir.absoluteFilePath(path));
+    });
 }
 
 void QmlProject::refreshFiles(const QSet<QString> &/*added*/, const QSet<QString> &removed)

src/plugins/qmlprojectmanager/qmlproject.h
@@ -75,6 +75,7 @@ public:
 
     void refreshProjectFile();
 
+    static QStringList makeAbsolute(const Utils::FileName &path, const QStringList &relativePaths);
 protected:
     RestoreResult fromMap(const QVariantMap &map, QString *errorMessage) override;
     bool setupTarget(ProjectExplorer::Target *t) override;

src/plugins/qmlprojectmanager/qmlprojectrunconfiguration.cpp
@@ -121,17 +121,6 @@ QString QmlProjectRunConfiguration::executable() const
     return qmlscene.isEmpty() ? QString("qmlscene") : qmlscene;
 }
 
-static QStringList makeAbsolute(const Utils::FileName &path, const QStringList &relativePaths)
-{
-    if (path.isEmpty())
-        return relativePaths;
-
-    const QDir baseDir(path.toString());
-    return Utils::transform(relativePaths, [&baseDir](const QString &path) {
-        return QDir::cleanPath(baseDir.absoluteFilePath(path));
-    });
-}
-
 QString QmlProjectRunConfiguration::commandLineArguments() const
 {
     // arguments in .user file
@@ -143,7 +132,7 @@ QString QmlProjectRunConfiguration::commandLineArguments() const
     // arguments from .qmlproject file
     const QmlProject *project = static_cast<QmlProject *>(currentTarget->project());
     foreach (const QString &importPath,
-             makeAbsolute(project->targetDirectory(currentTarget), project->customImportPaths())) {
+             QmlProject::makeAbsolute(project->targetDirectory(currentTarget), project->customImportPaths())) {
         Utils::QtcProcess::addArg(&args, QLatin1String("-I"), osType);
         Utils::QtcProcess::addArg(&args, importPath, osType);
     }