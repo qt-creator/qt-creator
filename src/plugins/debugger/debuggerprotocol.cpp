qt-creator/qt-creator

commit e8a8003b16d1b53d2d005bfd32bae53a93d23e5a

Debugger: Fix wrong GDB version detection for SLES
GDB version on SLES is reported as

GNU gdb (GDB; SUSE Linux Enterprise XX) Y.Z.W

So, the content between parentheses must be skipped, otherwise
the wrong digits are parsed.

Change-Id: I05eb1373bc6558dfe088c1328142ff21d48f5591
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/debugger/debuggerprotocol.cpp
@@ -438,7 +438,7 @@ void extractGdbVersion(const QString &msg,
     const QChar dot(QLatin1Char('.'));
 
     const bool ignoreParenthesisContent = msg.contains(QLatin1String("rubenvb"))
-                                       || msg.contains(QLatin1String("openSUSE"));
+                                       || msg.contains(QLatin1String("SUSE"));
 
     const QChar parOpen(QLatin1Char('('));
     const QChar parClose(QLatin1Char(')'));

commit 23781b3abc0862900e88445cf2fe0b523a6a82a6

Debugger: Fix detection of gdb on older SUSE
Broke with e8a8003b16.

Change-Id: I0e9662bc8fb4115de0c63d07c82de78ef73004a5
Reviewed-by: hjk <hjk@qt.io>

src/plugins/debugger/debuggerprotocol.cpp
@@ -438,7 +438,8 @@ void extractGdbVersion(const QString &msg,
     const QChar dot(QLatin1Char('.'));
 
     const bool ignoreParenthesisContent = msg.contains(QLatin1String("rubenvb"))
-                                       || msg.contains(QLatin1String("SUSE"));
+                                       || msg.contains(QLatin1String("openSUSE"))
+                                       || msg.contains(QLatin1String("SUSE Linux Enterprise"));
 
     const QChar parOpen(QLatin1Char('('));
     const QChar parClose(QLatin1Char(')'));

tests/auto/debugger/tst_gdb.cpp
@@ -98,6 +98,10 @@ void tst_gdb::version_data()
         << "GNU gdb (GDB) SUSE (6.8.91.20090930-2.4)"
         << 60891 << 20090930 << false << false;
 
+    QTest::newRow("SLES")
+        << "GNU gdb (GDB; SUSE Linux Enterprise 10) 7.9.1"
+        << 70901 << 0 << false << false;
+
     QTest::newRow("Apple")
         << "GNU gdb 6.3.50-20050815 (Apple version gdb-1461.2)"
         << 60350 << 1461 << true << false;