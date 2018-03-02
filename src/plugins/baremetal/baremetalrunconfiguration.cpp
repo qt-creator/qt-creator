qt-creator/qt-creator

commit 53a151074ad37d12e730fbd85ec0e0675d00f6d0

ProjectExplorer/all: Re-organize BuildSteps/{Deploy,Build}Config setup
This follow the rough pattern of recent *RunConfigurationFactory changes
for build and deploy configurations.

- Collapse the two lines of constructors similar to what
  890c1906e6fb2ec did for RunConfigurations
  * Deploy* was purely mechanical
  * Build* ctors are split in connects() in the ctor body
    to create "empty shell for clone" etc
    and build step additions in initialize() functions which
    are only used in the create() case.
  -- Allows to collapse the shared 'ctor()' functions, too.

- Move FooBuildConfigurationFactory::create() implementations
  to FooBuildConfiguration() constructor. That was a strange
  and unneeded ping-pong between factories and objects, and
  furthermore allows one level less of indirection (and for a
  later, left out here, some reduction of the
  FooBuildConfiguration interfaces that were only used to
  accommodate the *Factory::create() functions.

- Most {Build,Deploy}Configuration{,Factory} classes had a canHandle(),
  but there wasn't one in the base classses. Have one there.

- Most canHandle() functions were checking simple restrictions on
  e.g. project or target types, specify those by setters in the
  constructors instead and check them in the base canHandle()

- clone() is generally replaced by a creation of a "shell object"
  and a fromMap(source->toMap()), implemented in the base, there
  are two cases left for Android and Qbs that needed(?) some extra
  polish

- generally use canHandle() in base implementation, instead
  of doing that in all Derived::canFoo()

- as a result, canCreate/create/canClone/clone reimplementations
  are not needed anymore, keep the base implementation for
  now (could be inlined into their only users later), but
  de-virtualize them.

- Combine Ios{Preset,DSym}BuildStepFactory. There was only one
  'dsym' build step they could create.

- Split the 'mangled' id into the ProjectConfiguration subtype

src/plugins/baremetal/baremetalrunconfiguration.cpp
@@ -45,19 +45,8 @@ namespace Internal {
 const char ProFileKey[] = "Qt4ProjectManager.MaemoRunConfiguration.ProFile";
 const char WorkingDirectoryKey[] = "BareMetal.RunConfig.WorkingDirectory";
 
-static QString pathFromId(Core::Id id)
-{
-    if (id == BareMetalCustomRunConfiguration::runConfigId())
-        return QString();
-
-    QByteArray idStr = id.name();
-    if (!idStr.startsWith(BareMetalRunConfiguration::IdPrefix))
-        return QString();
-    return QString::fromUtf8(idStr.mid(int(strlen(BareMetalRunConfiguration::IdPrefix))));
-}
-
 BareMetalRunConfiguration::BareMetalRunConfiguration(Target *target)
-    : RunConfiguration(target)
+    : RunConfiguration(target, IdPrefix)
 {
     addExtraAspect(new ArgumentsAspect(this, "Qt4ProjectManager.MaemoRunConfiguration.Arguments"));
     connect(target, &Target::deploymentDataChanged,
@@ -68,17 +57,9 @@ BareMetalRunConfiguration::BareMetalRunConfiguration(Target *target)
             this, &BareMetalRunConfiguration::handleBuildSystemDataUpdated); // Handles device changes, etc.
 }
 
-void BareMetalRunConfiguration::initialize(const Core::Id id)
+QString BareMetalRunConfiguration::extraId() const
 {
-    RunConfiguration::initialize(id);
-    m_projectFilePath = pathFromId(id);
-
-    setDefaultDisplayName(defaultDisplayName());
-}
-
-QString BareMetalRunConfiguration::targetNameFromId(Core::Id id)
-{
-    return QFileInfo(pathFromId(id)).fileName();
+    return m_projectFilePath;
 }
 
 QWidget *BareMetalRunConfiguration::createConfigurationWidget()
@@ -110,8 +91,11 @@ bool BareMetalRunConfiguration::fromMap(const QVariantMap &map)
             = QDir::cleanPath(dir.filePath(map.value(QLatin1String(ProFileKey)).toString()));
     m_workingDirectory = map.value(QLatin1String(WorkingDirectoryKey)).toString();
 
-    setDefaultDisplayName(defaultDisplayName());
+    // Hack for old-style mangled ids. FIXME: Remove.
+    if (m_projectFilePath.isEmpty())
+        m_projectFilePath = ProjectExplorer::idFromMap(map).suffixAfter(id());
 
+    setDefaultDisplayName(defaultDisplayName());
     return true;
 }
 

commit 64df51bae489b7a5b352bdb196fad1f3620fdcdd

BareMetal: fix run configuration creation
Run configuration was broken by
53a151074ad37d12e730fbd85ec0e0675d00f6d0. Add configuratio menu shows
valid variants for targets, but after adding, target information lost
from the extraId info. As a result, impossible to run BareMetal targets.

Change-Id: I82991d62e2d308fdf333f20fe0338a121845b20e
Reviewed-by: hjk <hjk@qt.io>

src/plugins/baremetal/baremetalrunconfiguration.cpp
@@ -42,7 +42,6 @@ using namespace Utils;
 namespace BareMetal {
 namespace Internal {
 
-const char ProFileKey[] = "Qt4ProjectManager.MaemoRunConfiguration.ProFile";
 const char WorkingDirectoryKey[] = "BareMetal.RunConfig.WorkingDirectory";
 
 BareMetalRunConfiguration::BareMetalRunConfiguration(Target *target)
@@ -76,7 +75,6 @@ QVariantMap BareMetalRunConfiguration::toMap() const
 {
     QVariantMap map(RunConfiguration::toMap());
     const QDir dir = QDir(target()->project()->projectDirectory().toString());
-    map.insert(QLatin1String(ProFileKey), dir.relativeFilePath(m_projectFilePath));
     map.insert(QLatin1String(WorkingDirectoryKey), m_workingDirectory);
     return map;
 }
@@ -86,14 +84,8 @@ bool BareMetalRunConfiguration::fromMap(const QVariantMap &map)
     if (!RunConfiguration::fromMap(map))
         return false;
 
-    const QDir dir = QDir(target()->project()->projectDirectory().toString());
-    m_projectFilePath
-            = QDir::cleanPath(dir.filePath(map.value(QLatin1String(ProFileKey)).toString()));
     m_workingDirectory = map.value(QLatin1String(WorkingDirectoryKey)).toString();
-
-    // Hack for old-style mangled ids. FIXME: Remove.
-    if (m_projectFilePath.isEmpty())
-        m_projectFilePath = ProjectExplorer::idFromMap(map).suffixAfter(id());
+    m_projectFilePath = ProjectExplorer::idFromMap(map).suffixAfter(id());
 
     setDefaultDisplayName(defaultDisplayName());
     return true;