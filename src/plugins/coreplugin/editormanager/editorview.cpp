qt-creator/qt-creator

commit 324de13b4e6703db778010d0682ac26cff359516

Fix ordering of items in open documents popup
When the document history was updated, it was cleaned of any items which
no longer had a document open for them. This worked fine when that only
happened when user closed documents. Nowadays this also happens when
suspending documents without user interaction.

Only remove items from the document history if they are no longer
available even as a suspended document.

Task-number: QTCREATORBUG-19758
Change-Id: I131a24823b5c456879b67a35b039768a15bd7716
Reviewed-by: Konstantin Tokarev <annulen@yandex.ru>
Reviewed-by: David Schulz <david.schulz@qt.io>

src/plugins/coreplugin/editormanager/editorview.cpp
@@ -268,11 +268,10 @@ void EditorView::updateEditorHistory(IEditor *editor, QList<EditLocation> &histo
     location.state = QVariant(state);
 
     for (int i = 0; i < history.size(); ++i) {
-        if (history.at(i).document == 0
-            || history.at(i).document == document
-            ){
+        const EditLocation &item = history.at(i);
+        if (item.document == document
+                || !DocumentModel::indexOfFilePath(FileName::fromString(item.fileName))) {
             history.removeAt(i--);
-            continue;
         }
     }
     history.prepend(location);

src/plugins/coreplugin/editormanager/documentmodel.cpp
@@ -517,6 +517,11 @@ Utils::optional<int> DocumentModel::indexOfDocument(IDocument *document)
     return d->indexOfDocument(document);
 }
 
+Utils::optional<int> DocumentModel::indexOfFilePath(const Utils::FileName &filePath)
+{
+    return d->indexOfFilePath(filePath);
+}
+
 DocumentModel::Entry *DocumentModel::entryForDocument(IDocument *document)
 {
     return Utils::findOrDefault(d->m_entries,

src/plugins/coreplugin/editormanager/documentmodel.h
@@ -69,6 +69,7 @@ public:
     static int entryCount();
     static QList<Entry *> entries();
     static Utils::optional<int> indexOfDocument(IDocument *document);
+    static Utils::optional<int> indexOfFilePath(const Utils::FileName &filePath);
     static Entry *entryForDocument(IDocument *document);
     static Entry *entryForFilePath(const Utils::FileName &filePath);
     static QList<IDocument *> openedDocuments();

commit 64fe131c142377821a1e38671e5684946616a33f

Fix ordering of items without file name in open documents popup
An item that has no file name (like Git Commit, Diff etc.) was always
last in the list.

Broke by 324de13b4e6703db778010d0682ac26cff359516.

Change-Id: Ied3add2a372b4b176ee800e8c023c3cef5495488
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/coreplugin/editormanager/editorview.cpp
@@ -270,7 +270,8 @@ void EditorView::updateEditorHistory(IEditor *editor, QList<EditLocation> &histo
     for (int i = 0; i < history.size(); ++i) {
         const EditLocation &item = history.at(i);
         if (item.document == document
-                || !DocumentModel::indexOfFilePath(FileName::fromString(item.fileName))) {
+                || (!item.document
+                    && !DocumentModel::indexOfFilePath(FileName::fromString(item.fileName)))) {
             history.removeAt(i--);
         }
     }