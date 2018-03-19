qt-creator/qt-creator

commit e20d6200436da24bfed43f0738c94e4bc3ce751b

ProjectExplorer: Let RunConfiguration declare what nodes it belongs to
This feature in question is the availability of the "Run" button in the
context menu of certain project nodes in the project tree to run
something presumably related to/build from that (sub)project.

Previously, the decision was made for certain qmake based projects
(those targeting Desktop, iOS and VxWorks) by some indirection
through the corresponding RunConfigurationFactories.

The patch lets the RunConfigurations decide themselves directly
and removes the indirection, potentially opening the feature for
other qmake based RCs, as well as other combinations (e.g.
PythonRunConfiguration could be associated with its .py file,
without the need to have a dummy project)

Change-Id: Ic489bd1dfa25fcd9102ffa4fa30125565dd2e40e
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/plugins/projectexplorer/projectexplorer.cpp
@@ -2969,10 +2969,17 @@ void ProjectExplorerPluginPrivate::updateContextMenuActions()
             pn = const_cast<ProjectNode*>(currentNode->asProjectNode());
 
         if (pn) {
-            if (ProjectTree::currentProject() && pn == ProjectTree::currentProject()->rootProjectNode()) {
+            Project *project = ProjectTree::currentProject();
+            if (project && pn == project->rootProjectNode()) {
                 m_runActionContextMenu->setVisible(true);
             } else {
-                QList<RunConfiguration *> runConfigs = pn->runConfigurations();
+                QList<RunConfiguration *> runConfigs;
+                if (Target *t = project->activeTarget()) {
+                    for (RunConfiguration *rc : t->runConfigurations()) {
+                        if (rc->canRunForNode(pn))
+                            runConfigs.append(rc);
+                    }
+                }
                 if (runConfigs.count() == 1) {
                     m_runActionContextMenu->setVisible(true);
                     m_runActionContextMenu->setData(QVariant::fromValue(runConfigs.first()));

src/plugins/ios/iosrunconfiguration.cpp
@@ -157,6 +157,11 @@ void IosRunConfiguration::updateEnabledState()
     return RunConfiguration::updateEnabledState();
 }
 
+bool IosRunConfiguration::canRunForNode(const Node *node) const
+{
+    return node->filePath() == m_profilePath;
+}
+
 IosDeployStep *IosRunConfiguration::deployStep() const
 {
     DeployConfiguration *config = target()->activeDeployConfiguration();

src/plugins/ios/iosrunconfiguration.h
@@ -74,6 +74,7 @@ private:
     friend class IosRunConfigurationWidget;
     void updateDisplayNames();
     void updateEnabledState() final;
+    bool canRunForNode(const ProjectExplorer::Node *node) const final;
 
     Utils::FileName m_profilePath;
     IosDeviceType m_deviceType;

src/plugins/ios/iosrunfactories.cpp
@@ -59,11 +59,5 @@ IosRunConfigurationFactory::availableCreators(Target *parent) const
                                                     ProjectType::SharedLibraryTemplate});
 }
 
-bool IosRunConfigurationFactory::hasRunConfigForProFile(RunConfiguration *rc, const Utils::FileName &n) const
-{
-    auto iosRc = qobject_cast<IosRunConfiguration *>(rc);
-    return iosRc && iosRc->profilePath() == n;
-}
-
 } // namespace Internal
 } // namespace Ios

src/plugins/ios/iosrunfactories.h
@@ -26,17 +26,11 @@
 #pragma once
 
 #include <projectexplorer/runconfiguration.h>
-#include <qmakeprojectmanager/qmakerunconfigurationfactory.h>
-
-namespace ProjectExplorer {
-class Target;
-class Node;
-} // namespace ProjectExplorer
 
 namespace Ios {
 namespace Internal {
 
-class IosRunConfigurationFactory : public QmakeProjectManager::QmakeRunConfigurationFactory
+class IosRunConfigurationFactory : public ProjectExplorer::RunConfigurationFactory
 {
     Q_OBJECT
 
@@ -45,8 +39,6 @@ public:
 
     QList<ProjectExplorer::RunConfigurationCreationInfo>
     availableCreators(ProjectExplorer::Target *parent) const override;
-
-    bool hasRunConfigForProFile(ProjectExplorer::RunConfiguration *rc, const Utils::FileName &n) const override;
 };
 
 } // namespace Internal

src/plugins/projectexplorer/projectnodes.cpp
@@ -846,16 +846,6 @@ bool ProjectNode::deploysFolder(const QString &folder) const
     return false;
 }
 
-/*!
-  \function bool ProjectNode::runConfigurations() const
-
-  Returns a list of \c RunConfiguration suitable for this node.
-  */
-QList<RunConfiguration *> ProjectNode::runConfigurations() const
-{
-    return QList<RunConfiguration *>();
-}
-
 ProjectNode *ProjectNode::projectNode(const Utils::FileName &file) const
 {
     for (Node *node : m_nodes) {

src/plugins/projectexplorer/projectnodes.h
@@ -41,7 +41,6 @@ namespace Core { class IVersionControl; }
 namespace ProjectExplorer {
 
 class Project;
-class RunConfiguration;
 
 enum class NodeType : quint16 {
     File = 1,
@@ -332,8 +331,6 @@ public:
 
     // by default returns false
     virtual bool deploysFolder(const QString &folder) const;
-
-    virtual QList<RunConfiguration *> runConfigurations() const;
 
     ProjectNode *projectNode(const Utils::FileName &file) const;
 

src/plugins/projectexplorer/runconfiguration.h
@@ -48,6 +48,7 @@ namespace ProjectExplorer {
 class Abi;
 class BuildConfiguration;
 class IRunConfigurationAspect;
+class Node;
 class RunConfigurationFactory;
 class RunConfiguration;
 class RunConfigurationCreationInfo;
@@ -251,6 +252,7 @@ public:
     void addExtraAspect(IRunConfigurationAspect *aspect);
 
     static RunConfiguration *startupRunConfiguration();
+    virtual bool canRunForNode(const ProjectExplorer::Node *) const { return false; }
 
     using AspectFactory = std::function<IRunConfigurationAspect *(RunConfiguration *)>;
     template <class T> static void registerAspect()

commit 1e033d8a085ba7373359c84f4afed37c52c5b27e

ProjectExplorer: Prevent crash on project closing
Introduced with e20d6200436d, currentProject will be null when
a project gets closed. The previous code path had access to a
locally cached and still valid pointer to the project that's
about to be closed.

Change-Id: I3260b4ed1ac325fc2be0e8bba934d7888b1d698c
Reviewed-by: Ivan Donchevskii <ivan.donchevskii@qt.io>

src/plugins/projectexplorer/projectexplorer.cpp
@@ -2968,9 +2968,9 @@ void ProjectExplorerPluginPrivate::updateContextMenuActions()
         else
             pn = const_cast<ProjectNode*>(currentNode->asProjectNode());
 
-        if (pn) {
-            Project *project = ProjectTree::currentProject();
-            if (project && pn == project->rootProjectNode()) {
+        Project *project = ProjectTree::currentProject();
+        if (pn && project) {
+            if (pn == project->rootProjectNode()) {
                 m_runActionContextMenu->setVisible(true);
             } else {
                 QList<RunConfiguration *> runConfigs;