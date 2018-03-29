qt-creator/qt-creator

commit 8d1a592d1637b52e539695afdef7f5564f6d723b

FancyLineEdit: Remove property initialText
The same can be easily achieved by placeholderText nowadays.

Change-Id: Icfe0652ae8536c8077cf45a03844275aa1eddad6
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/libs/utils/fancylineedit.cpp
@@ -105,7 +105,6 @@ public:
     QColor m_okTextColor;
     QColor m_errorTextColor = Qt::red;
     QString m_errorMessage;
-    QString m_initialText;
 };
 
 FancyLineEditPrivate::FancyLineEditPrivate(FancyLineEdit *parent) :
@@ -373,20 +372,6 @@ void FancyLineEdit::setFiltering(bool on)
     }
 }
 
-QString FancyLineEdit::initialText() const
-{
-    return d->m_initialText;
-}
-
-void FancyLineEdit::setInitialText(const QString &t)
-{
-    if (d->m_initialText != t) {
-        d->m_initialText = t;
-        d->m_firstChange = true;
-        setText(t);
-    }
-}
-
 QColor FancyLineEdit::errorColor() const
 {
     return d->m_errorTextColor;
@@ -458,13 +443,13 @@ void FancyLineEdit::validate()
     }
 
     d->m_errorMessage.clear();
-    // Are we displaying the initial text?
-    const bool isDisplayingInitialText = !d->m_initialText.isEmpty() && t == d->m_initialText;
-    const State newState = isDisplayingInitialText ?
-                               DisplayingInitialText :
+    // Are we displaying the placeholder text?
+    const bool isDisplayingPlaceholderText = !placeholderText().isEmpty() && t.isEmpty();
+    const State newState = isDisplayingPlaceholderText ?
+                               DisplayingPlaceholderText :
                                (d->m_validationFunction(this, &d->m_errorMessage) ? Valid : Invalid);
     setToolTip(d->m_errorMessage);
-    // Changed..figure out if valid changed. DisplayingInitialText is not valid,
+    // Changed..figure out if valid changed. DisplayingPlaceholderText is not valid,
     // but should not show error color. Also trigger on the first change.
     if (newState != d->m_state || d->m_firstChange) {
         const bool validHasChanged = (d->m_state == Valid) != (newState == Valid);

src/libs/utils/fancylineedit.h
@@ -73,7 +73,6 @@ class QTCREATOR_UTILS_EXPORT FancyLineEdit : public CompletingLineEdit
     Q_ENUMS(Side)
 
     // Validation.
-    Q_PROPERTY(QString initialText READ initialText WRITE setInitialText DESIGNABLE true)
     Q_PROPERTY(QColor errorColor READ errorColor WRITE setErrorColor DESIGNABLE true)
     Q_PROPERTY(QColor okColor READ okColor WRITE setOkColor DESIGNABLE true)
 
@@ -123,14 +122,11 @@ public:
 
     // line edit, (out)errorMessage -> valid?
     typedef std::function<bool(FancyLineEdit *, QString *)> ValidationFunction;
-    enum State { Invalid, DisplayingInitialText, Valid };
+    enum State { Invalid, DisplayingPlaceholderText, Valid };
 
     State state() const;
     bool isValid() const;
     QString errorMessage() const;
-
-    QString initialText() const;
-    void setInitialText(const QString &);
 
     QColor errorColor() const;
     void setErrorColor(const  QColor &c);

src/libs/utils/projectintropage.cpp
@@ -85,7 +85,7 @@ ProjectIntroPage::ProjectIntroPage(QWidget *parent) :
 {
     d->m_ui.setupUi(this);
     hideStatusLabel();
-    d->m_ui.nameLineEdit->setInitialText(tr("<Enter_Name>"));
+    d->m_ui.nameLineEdit->setPlaceholderText(tr("Enter project name"));
     d->m_ui.nameLineEdit->setFocus();
     d->m_ui.nameLineEdit->setValidationFunction([this](FancyLineEdit *edit, QString *errorString) {
         return validateProjectName(edit->text(), errorString);
@@ -178,13 +178,13 @@ bool ProjectIntroPage::validate()
         return false;
     }
 
-    // Name valid? Ignore 'DisplayingInitialText' state.
+    // Name valid? Ignore 'DisplayingPlaceholderText' state.
     bool nameValid = false;
     switch (d->m_ui.nameLineEdit->state()) {
     case FancyLineEdit::Invalid:
         displayStatusMessage(Error, d->m_ui.nameLineEdit->errorMessage());
         return false;
-    case FancyLineEdit::DisplayingInitialText:
+    case FancyLineEdit::DisplayingPlaceholderText:
         break;
     case FancyLineEdit::Valid:
         nameValid = true;

commit 26bfa469db1fbfb0f5f231c0ac9c780926e5d522

Fix that clearing shortcut in settings did no longer work
This broke in 8d1a592d1637b52e539695afdef7f5564f6d723b

We should ask the validator of a fancy line edit if an entry is valid,
also if the line edit is empty and shows a placeholder text, even if we
then do not mark the place holder with the error color, etc.

In case of the shortcut settings, the validator has side effects that
are important.

Change-Id: I41c0d98ed3c6e9e68df4484a47c108b00ddbbfe7
Reviewed-by: David Schulz <david.schulz@qt.io>

src/libs/utils/fancylineedit.cpp
@@ -445,9 +445,9 @@ void FancyLineEdit::validate()
     d->m_errorMessage.clear();
     // Are we displaying the placeholder text?
     const bool isDisplayingPlaceholderText = !placeholderText().isEmpty() && t.isEmpty();
-    const State newState = isDisplayingPlaceholderText ?
-                               DisplayingPlaceholderText :
-                               (d->m_validationFunction(this, &d->m_errorMessage) ? Valid : Invalid);
+    const bool validates = d->m_validationFunction(this, &d->m_errorMessage);
+    const State newState = isDisplayingPlaceholderText ? DisplayingPlaceholderText
+                                                       : (validates ? Valid : Invalid);
     setToolTip(d->m_errorMessage);
     // Changed..figure out if valid changed. DisplayingPlaceholderText is not valid,
     // but should not show error color. Also trigger on the first change.