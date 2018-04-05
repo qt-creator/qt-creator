qt-creator/qt-creator

commit 2f8c075a0111f496269fdb6d67c9d40fcb461c6a

Utils: Make default constructor of SmallString constexpr
Change-Id: Ibf381c9f18de878b1f67e28498cd28270e9220c7
Reviewed-by: Ivan Donchevskii <ivan.donchevskii@qt.io>

src/libs/utils/smallstringlayout.h
@@ -98,7 +98,7 @@ struct ALIGNAS_16 StringDataLayout {
                 : ((MaximumShortStringDataAreaSize + 2) % 16) == 0,
                   "Size + 1 must be dividable by 16 if under 64 and Size + 2 must be dividable by 16 if over 64!");
 
-    StringDataLayout() noexcept = default;
+    constexpr StringDataLayout() noexcept = default;
 
     constexpr StringDataLayout(const char *string,
                                size_type size) noexcept

src/libs/utils/smallstring.h
@@ -69,7 +69,7 @@ public:
                 ? sizeof(Internal::StringDataLayout<Size>) == Size + 1
                 : sizeof(Internal::StringDataLayout<Size>) == Size + 2,
                   "Size is wrong");
-
+    constexpr
     BasicSmallString() noexcept
         : m_data(Internal::StringDataLayout<Size>())
     {

commit bf285af0071bf30730a5d61fa80b7bc890465177

Utils: Fix compile with gcc 4.9
Otherwise compile fails with
 constexpr constructor does not have empty body.
Broke with 2f8c075a0111f496269fdb6d67c9d40fcb461c6a.

Change-Id: I04ce03eaf51ddb3b4233abc4b51a553355e28e04
Reviewed-by: Ivan Donchevskii <ivan.donchevskii@qt.io>

src/libs/utils/smallstringlayout.h
@@ -155,7 +155,6 @@ struct StringDataLayout {
                 : ((MaximumShortStringDataAreaSize + 2) % 16) == 0,
                   "Size + 1 must be dividable by 16 if under 64 and Size + 2 must be dividable by 16 if over 64!");
 
-    constexpr
     StringDataLayout() noexcept
     {
         reset();