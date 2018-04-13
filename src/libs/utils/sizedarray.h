qt-creator/qt-creator

commit 4b0bcbdcb6b28e806de4ba31fa5fecd06fd9188e

Clang: Locator filter for the symbol database
There are no symbol queries for the locator filters. The signature
generation is still not implemented but for simple cases it should work.

Change-Id: Ic6b04fbe1e7e057892f194ac139615c47d6ec33f
Reviewed-by: Ivan Donchevskii <ivan.donchevskii@qt.io>

src/libs/utils/sizedarray.h
@@ -54,6 +54,13 @@ public:
     using std::array<T, MaxSize>::rend;
     using std::array<T, MaxSize>::crend;
 
+    constexpr SizedArray() = default;
+    constexpr SizedArray(std::initializer_list<T> list)
+        : m_size(list.size())
+    {
+        std::copy(list.begin(), list.end(), begin());
+    }
+
     constexpr size_type size() const
     {
         return m_size;

src/libs/clangsupport/clangsupport_global.h
@@ -198,6 +198,17 @@ struct HighlightingTypes {
     MixinHighlightingTypes mixinHighlightingTypes;
 };
 
+enum class SourceLocationKind : uchar
+{
+    None = 0,
+    Declaration,
+    DeclarationReference,
+    Definition,
+    MacroDefinition = 128,
+    MacroUsage,
+    MacroUndefinition
+};
+
 enum class SymbolKind : uchar
 {
     None = 0,
@@ -207,6 +218,8 @@ enum class SymbolKind : uchar
     Variable,
     Macro
 };
+
+using SymbolKinds = Utils::SizedArray<SymbolKind, 8>;
 
 enum class SymbolTag : uchar
 {

src/libs/clangsupport/refactoringdatabaseinitializer.h
@@ -62,10 +62,11 @@ public:
         table.setName("symbols");
         table.addColumn("symbolId", Sqlite::ColumnType::Integer, Sqlite::Contraint::PrimaryKey);
         const Sqlite::Column &usrColumn = table.addColumn("usr", Sqlite::ColumnType::Text);
-        table.addColumn("symbolName", Sqlite::ColumnType::Text);
+        const Sqlite::Column &symbolNameColumn = table.addColumn("symbolName", Sqlite::ColumnType::Text);
         const Sqlite::Column &symbolKindColumn = table.addColumn("symbolKind", Sqlite::ColumnType::Integer);
+        table.addColumn("signature", Sqlite::ColumnType::Text);
         table.addIndex({usrColumn});
-        table.addIndex({symbolKindColumn});
+        table.addIndex({symbolKindColumn, symbolNameColumn});
 
         table.initialize(database);
     }
@@ -79,7 +80,9 @@ public:
         const Sqlite::Column &lineColumn = table.addColumn("line", Sqlite::ColumnType::Integer);
         const Sqlite::Column &columnColumn = table.addColumn("column", Sqlite::ColumnType::Integer);
         const Sqlite::Column &sourceIdColumn = table.addColumn("sourceId", Sqlite::ColumnType::Integer);
+        const Sqlite::Column &locationKindColumn = table.addColumn("locationKind", Sqlite::ColumnType::Integer);
         table.addUniqueIndex({sourceIdColumn, lineColumn, columnColumn});
+        table.addIndex({sourceIdColumn, locationKindColumn});
 
         table.initialize(database);
     }

src/plugins/clangrefactoring/clangrefactoring-source.pri
@@ -6,10 +6,6 @@ HEADERS += \
     $$PWD/clangqueryhighlighter.h \
     $$PWD/clangqueryhighlightmarker.h \
     $$PWD/clangqueryprojectsfindfilter.h \
-    $$PWD/classesfilter.h \
-    $$PWD/functionsfilter.h \
-    $$PWD/includesfilter.h \
-    $$PWD/locatorfilter.h \
     $$PWD/projectpartutilities.h \
     $$PWD/refactoringclient.h \
     $$PWD/refactoringconnectionclient.h \
@@ -20,24 +16,19 @@ HEADERS += \
     $$PWD/symbolsfindfilter.h \
     $$PWD/symbolqueryinterface.h \
     $$PWD/symbol.h \
-    $$PWD/class.h \
-    $$PWD/enum.h \
-    $$PWD/function.h \
-    $$PWD/include.h \
-    $$PWD/projectpartproviderinterface.h
+    $$PWD/projectpartproviderinterface.h \
+    $$PWD/editormanagerinterface.h \
+    $$PWD/locatorfilter.h
 
 SOURCES += \
     $$PWD/clangqueryexamplehighlighter.cpp \
     $$PWD/clangqueryhighlighter.cpp \
     $$PWD/clangqueryprojectsfindfilter.cpp \
-    $$PWD/classesfilter.cpp \
-    $$PWD/functionsfilter.cpp \
-    $$PWD/includesfilter.cpp \
-    $$PWD/locatorfilter.cpp \
     $$PWD/projectpartutilities.cpp \
     $$PWD/refactoringclient.cpp \
     $$PWD/refactoringconnectionclient.cpp \
     $$PWD/refactoringengine.cpp \
     $$PWD/refactoringprojectupdater.cpp \
     $$PWD/searchhandle.cpp \
-    $$PWD/symbolsfindfilter.cpp
+    $$PWD/symbolsfindfilter.cpp \
+    $$PWD/locatorfilter.cpp

src/plugins/clangrefactoring/clangrefactoring.pro
@@ -14,17 +14,14 @@ HEADERS += \
     clangqueryprojectsfindfilterwidget.h \
     clangquerytexteditorwidget.h \
     qtcreatorclangqueryfindfilter.h \
-    qtcreatorclassesfilter.h \
-    qtcreatorfunctionsfilter.h \
-    qtcreatorincludesfilter.h \
-    qtcreatorlocatorfilter.h \
     qtcreatorsearch.h \
     qtcreatorsearchhandle.h \
     qtcreatorsymbolsfindfilter.h \
     querysqlitestatementfactory.h \
     sourcelocations.h \
     symbolsfindfilterconfigwidget.h \
-    symbolquery.h
+    symbolquery.h \
+    qtcreatoreditormanager.h
 
 SOURCES += \
     clangrefactoringplugin.cpp \
@@ -34,14 +31,11 @@ SOURCES += \
     clangqueryprojectsfindfilterwidget.cpp \
     clangquerytexteditorwidget.cpp \
     qtcreatorclangqueryfindfilter.cpp \
-    qtcreatorclassesfilter.cpp \
-    qtcreatorincludesfilter.cpp \
-    qtcreatorfunctionsfilter.cpp \
-    qtcreatorlocatorfilter.cpp \
     qtcreatorsearch.cpp \
     qtcreatorsearchhandle.cpp \
     qtcreatorsymbolsfindfilter.cpp \
-    symbolsfindfilterconfigwidget.cpp
+    symbolsfindfilterconfigwidget.cpp \
+    qtcreatoreditormanager.cpp
 
 FORMS += \
     clangqueryprojectsfindfilter.ui

src/plugins/clangrefactoring/clangrefactoringplugin.cpp
@@ -24,11 +24,9 @@
 ****************************************************************************/
 
 #include "clangrefactoringplugin.h"
-#include "qtcreatorclassesfilter.h"
-#include "qtcreatorfunctionsfilter.h"
-#include "qtcreatorincludesfilter.h"
-#include "qtcreatorlocatorfilter.h"
+#include "locatorfilter.h"
 #include "qtcreatorsymbolsfindfilter.h"
+#include "qtcreatoreditormanager.h"
 #include "querysqlitestatementfactory.h"
 #include "sqlitedatabase.h"
 #include "sqlitereadstatement.h"
@@ -41,6 +39,7 @@
 
 #include <coreplugin/icore.h>
 #include <extensionsystem/pluginmanager.h>
+#include <cpptools/cpptoolsconstants.h>
 
 #include <refactoringdatabaseinitializer.h>
 #include <filepathcaching.h>
@@ -77,11 +76,11 @@ class ClangRefactoringPluginData
 public:
     using QuerySqliteReadStatementFactory = QuerySqliteStatementFactory<Sqlite::Database,
                                                                         Sqlite::ReadStatement>;
-
     Sqlite::Database database{Utils::PathString{Core::ICore::userResourcePath() + "/symbol-experimental-v1.db"}, 1000ms};
     ClangBackEnd::RefactoringDatabaseInitializer<Sqlite::Database> databaseInitializer{database};
     ClangBackEnd::FilePathCaching filePathCache{database};
     RefactoringClient refactoringClient;
+    QtCreatorEditorManager editorManager{filePathCache};
     ClangBackEnd::RefactoringConnectionClient connectionClient{&refactoringClient};
     QuerySqliteReadStatementFactory statementFactory{database};
     SymbolQuery<QuerySqliteReadStatementFactory> symbolQuery{statementFactory};
@@ -110,7 +109,7 @@ static bool useClangFilters()
 
 bool ClangRefactoringPlugin::initialize(const QStringList & /*arguments*/, QString * /*errorMessage*/)
 {
-    d.reset(new ClangRefactoringPluginData);
+    d = std::make_unique<ClangRefactoringPluginData>();
 
     d->refactoringClient.setRefactoringEngine(&d->engine);
     d->refactoringClient.setRefactoringConnectionClient(&d->connectionClient);
@@ -175,11 +174,29 @@ void ClangRefactoringPlugin::initializeFilters()
         return;
 
     CppTools::CppModelManager *modelManager = CppTools::CppModelManager::instance();
-    modelManager->setLocatorFilter(std::make_unique<QtcreatorLocatorFilter>(d->symbolQuery));
-    modelManager->setClassesFilter(std::make_unique<QtcreatorClassesFilter>(d->symbolQuery));
-    modelManager->setIncludesFilter(std::make_unique<QtcreatorIncludesFilter>(d->symbolQuery));
-    modelManager->setFunctionsFilter(std::make_unique<QtcreatorFunctionsFilter>(d->symbolQuery));
-    modelManager->setSymbolsFindFilter(std::make_unique<QtcreatorSymbolsFindFilter>());
+    modelManager->setClassesFilter(std::make_unique<LocatorFilter>(
+                                       d->symbolQuery,
+                                       d->editorManager,
+                                       ClangBackEnd::SymbolKinds{ClangBackEnd::SymbolKind::Record},
+                                       CppTools::Constants::CLASSES_FILTER_ID,
+                                       CppTools::Constants::CLASSES_FILTER_DISPLAY_NAME,
+                                       "c"));
+    modelManager->setFunctionsFilter(std::make_unique<LocatorFilter>(
+                                       d->symbolQuery,
+                                       d->editorManager,
+                                       ClangBackEnd::SymbolKinds{ClangBackEnd::SymbolKind::Function},
+                                       CppTools::Constants::FUNCTIONS_FILTER_ID,
+                                       CppTools::Constants::FUNCTIONS_FILTER_DISPLAY_NAME,
+                                       "m"));
+    modelManager->setLocatorFilter(std::make_unique<LocatorFilter>(
+                                         d->symbolQuery,
+                                         d->editorManager,
+                                         ClangBackEnd::SymbolKinds{ClangBackEnd::SymbolKind::Record,
+                                                     ClangBackEnd::SymbolKind::Enumeration,
+                                                     ClangBackEnd::SymbolKind::Function},
+                                         CppTools::Constants::LOCATOR_FILTER_ID,
+                                         CppTools::Constants::LOCATOR_FILTER_DISPLAY_NAME,
+                                         ":"));
 }
 
 } // namespace ClangRefactoring

src/plugins/clangrefactoring/class.h
@@ -1,33 +1,0 @@
-/****************************************************************************
-**
-** Copyright (C) 2018 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of Qt Creator.
-**
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 as published by the Free Software
-** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-****************************************************************************/
-
-#pragma once
-
-#include "symbol.h"
-
-namespace ClangRefactoring {
-using Class = Symbol;
-using Classes = std::vector<Class>;
-} // namespace ClangRefactoring

src/plugins/clangrefactoring/classesfilter.cpp
@@ -1,66 +1,0 @@
-/****************************************************************************
-**
-** Copyright (C) 2017 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of Qt Creator.
-**
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 as published by the Free Software
-** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-****************************************************************************/
-
-#include "classesfilter.h"
-
-#include <cpptools/cpptoolsconstants.h>
-
-#include <utils/algorithm.h>
-
-namespace ClangRefactoring {
-
-ClassesFilter::ClassesFilter(SymbolQueryInterface &symbolQuery)
-    : m_symbolQuery(symbolQuery)
-{
-    setId(CppTools::Constants::CLASSES_FILTER_ID);
-    setDisplayName(CppTools::Constants::CLASSES_FILTER_DISPLAY_NAME);
-    setShortcutString(QLatin1String("c"));
-    setIncludedByDefault(false);
-}
-
-QList<Core::LocatorFilterEntry> ClassesFilter::matchesFor(
-        QFutureInterface<Core::LocatorFilterEntry> &, const QString &entry)
-{
-    using EntryList = QList<Core::LocatorFilterEntry>;
-    const SymbolString entryString(entry);
-    const Classes classes = m_symbolQuery.symbolsContaining(SymbolType::Class, entryString);
-    return Utils::transform<EntryList>(classes, [this](const Class &classInfo) {
-        Core::LocatorFilterEntry entry{this,
-                                       classInfo.name.toQString(),
-                                       qVariantFromValue(classInfo)};
-        entry.extraInfo = classInfo.path.path().toQString();
-        return entry;
-    });
-}
-
-void ClassesFilter::accept(Core::LocatorFilterEntry, QString *, int *, int *) const
-{
-}
-
-void ClassesFilter::refresh(QFutureInterface<void> &)
-{
-}
-
-} // namespace ClangRefactoring

commit 9e29a32e1ab3af42a4d70984449ba33b3092a50b

Utils: Fix compile with gcc4.9
Otherwise compile fails with
 constexpr constructor does not have empty body.
Broke with 4b0bcbdcb6b28e806de4ba31fa5fecd06fd9188e.

Change-Id: I5f058cd4ef11dddb72c8fc88b7184d41f4ffc15a
Reviewed-by: Ivan Donchevskii <ivan.donchevskii@qt.io>

src/libs/utils/sizedarray.h
@@ -55,7 +55,7 @@ public:
     using std::array<T, MaxSize>::crend;
 
     constexpr SizedArray() = default;
-    constexpr SizedArray(std::initializer_list<T> list)
+    SizedArray(std::initializer_list<T> list)
         : m_size(list.size())
     {
         std::copy(list.begin(), list.end(), begin());