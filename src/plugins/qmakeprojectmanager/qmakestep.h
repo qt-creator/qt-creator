c0b8ac7966065afbab9511474b4fa38748d0ca84
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2018-05-14 21:59:18 +0300

    QmakePM: Expand qmake arguments for summary, effective call and parser
    For example, if you set MAKEFILE=Makefile.%{CurrentBuild:Name}, the
    effective qmake call should show Makefile.foo and so should the command-
    line summary (with details collapsed).
    
    Change-Id: Ide0c0b0758b92f77f7cc97dd538db818575c91dd
    Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

--- src/plugins/qmakeprojectmanager/qmakestep.h
+++ src/plugins/qmakeprojectmanager/qmakestep.h
@@ -120,8 +120,15 @@ public:
     bool immutable() const override;
     void setForced(bool b);
 
+    enum class ArgumentFlag {
+        OmitProjectPath = 0x01,
+        Expand = 0x02
+    };
+    Q_DECLARE_FLAGS(ArgumentFlags, ArgumentFlag);
+
     // the complete argument line
-    QString allArguments(const QtSupport::BaseQtVersion *v, bool shorted = false) const;
+    QString allArguments(const QtSupport::BaseQtVersion *v,
+                         ArgumentFlags flags = ArgumentFlags()) const;
     QMakeStepConfig deducedArguments() const;
     // arguments passed to the pro file parser
     QStringList parserArguments();
@@ -231,3 +238,5 @@ private:
 };
 
 } // namespace QmakeProjectManager
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(QmakeProjectManager::QMakeStep::ArgumentFlags);
