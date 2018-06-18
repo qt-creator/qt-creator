d98fd607b89e9c8b9d185913d76120dc9910b7f3
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2018-06-18 18:12:11 +0300

    Core: Fix indentation
    Amends commit b16ba2ba59b2877d6b6827f2d3717fd40c6a6627.
    
    Change-Id: I6f6e25884eb06ed723eacd4152941c58f6924f3e
    Reviewed-by: Robert Loehning <robert.loehning@qt.io>

--- src/plugins/coreplugin/mimetypesettings.cpp
+++ src/plugins/coreplugin/mimetypesettings.cpp
@@ -88,7 +88,7 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant headerData(int section, Qt::Orientation orientation,
-                                int role = Qt::DisplayRole) const override;
+                        int role = Qt::DisplayRole) const override;
     QVariant data(const QModelIndex &modelIndex, int role = Qt::DisplayRole) const override;
 
     void load();
