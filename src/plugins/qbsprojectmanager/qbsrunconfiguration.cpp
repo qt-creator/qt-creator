11ffd3d702e2223f95fb8c5f69edbaa3fa7fe4f3
Christian Kandeler <christian.kandeler@qt.io>
2018-07-27 14:05:23 +0200

    QbsProjectManager: Add run config checkbox for using debug frameworks
    As it already exists in the qmake project manager.
    
    Task-number: QTCREATORBUG-20411
    Change-Id: I8673e091b3f5772fdc1ddcac0e567f690960f1dd
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- src/plugins/qbsprojectmanager/qbsrunconfiguration.cpp
+++ src/plugins/qbsprojectmanager/qbsrunconfiguration.cpp
@@ -69,6 +69,12 @@ QbsRunConfiguration::QbsRunConfiguration(Target *target, Core::Id id)
     addExtraAspect(libAspect);
     connect(libAspect, &UseLibraryPathsAspect::changed,
             envAspect, &EnvironmentAspect::environmentChanged);
+    if (HostOsInfo::isMacHost()) {
+        auto dyldAspect = new UseDyldSuffixAspect(this, "Qbs.RunConfiguration.UseDyldImageSuffix");
+        addExtraAspect(dyldAspect);
+        connect(dyldAspect, &UseDyldSuffixAspect::changed,
+                envAspect, &EnvironmentAspect::environmentChanged);
+    }
 
     connect(project(), &Project::parsingFinished, this,
             [envAspect]() { envAspect->buildEnvironmentHasChanged(); });
@@ -109,6 +115,10 @@ void QbsRunConfiguration::doAdditionalSetup(const RunConfigurationCreationInfo &
 
 void QbsRunConfiguration::addToBaseEnvironment(Utils::Environment &env) const
 {
+    if (auto dyldAspect = extraAspect<UseDyldSuffixAspect>()) {
+        if (dyldAspect->value())
+            env.set("DYLD_IMAGE_SUFFIX", "_debug");
+    }
     bool usingLibraryPaths = extraAspect<UseLibraryPathsAspect>()->value();
 
     const auto key = qMakePair(env.toStringList(), usingLibraryPaths);
