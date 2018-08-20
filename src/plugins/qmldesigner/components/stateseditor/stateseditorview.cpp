028d4b54aa1de8e3e3aab0b37014b259030481a4
Thomas Hartmann <thomas.hartmann@qt.io>
2018-08-20 13:40:11 +0200

    QmlDesigner: Always give root item id if state added
    This is required to access the state.
    
    Change-Id: I647edfd824760164b5e0a621c23ac6931c0ceec0
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- src/plugins/qmldesigner/components/stateseditor/stateseditorview.cpp
+++ src/plugins/qmldesigner/components/stateseditor/stateseditorview.cpp
@@ -152,6 +152,7 @@ void StatesEditorView::addState()
     }
 
     try {
+        rootModelNode().validId();
         if ((rootStateGroup().allStates().count() < 1) && //QtQuick import might be missing
                 (!model()->hasImport(Import::createLibraryImport("QtQuick", "1.0"), true, true))) {
             model()->changeImports({Import::createLibraryImport("QtQuick", "1.0")}, {});
