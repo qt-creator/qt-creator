qt-creator/qt-creator

commit 4e475fb5e6dd48b9b23357846f9e25c9bef50690

BinEditor: Close removed files silently.
The file cannot be saved as creator just
holds a chunk of the file.

Task-number: QTCREATORBUG-13014
Change-Id: I53c79f4ca5102673e66c5bd3f901c373ec8e0d90
Reviewed-by: Christian Stenger <christian.stenger@digia.com>

src/plugins/bineditor/bineditorplugin.cpp
@@ -247,6 +247,12 @@ public:
         return true;
     }
 
+    ReloadBehavior reloadBehavior(ChangeTrigger state, ChangeType type) const
+    {
+        Q_UNUSED(state)
+        return type == TypeRemoved ? BehaviorSilent : BehaviorAsk;
+    }
+
     bool save(QString *errorString, const QString &fn, bool autoSave)
     {
         QTC_ASSERT(!autoSave, return true); // bineditor does not support autosave - it would be a bit expensive

commit 18833bed2403e80d5b97bacd6ffc3cb0f4adb852

Fix reload behavior of binary editor
It should silenty reload internally triggered changes and permission
changes.

Broke in 4e475fb5e6dd48b9b23357846f9e25c9bef50690

Change-Id: I900adac72f51ea5e070c9c4efb59c09296526c42
Reviewed-by: David Schulz <david.schulz@qt.io>

src/plugins/bineditor/bineditorplugin.cpp
@@ -228,8 +228,7 @@ public:
 
     ReloadBehavior reloadBehavior(ChangeTrigger state, ChangeType type) const override
     {
-        Q_UNUSED(state)
-        return type == TypeRemoved ? BehaviorSilent : BehaviorAsk;
+        return type == TypeRemoved ? BehaviorSilent : IDocument::reloadBehavior(state, type);
     }
 
     bool save(QString *errorString, const QString &fn, bool autoSave) override