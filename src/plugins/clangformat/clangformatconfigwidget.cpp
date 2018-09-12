f73e53b169e7d3da1572ea8357d1d4949fe21b3f
Tobias Hunger <tobias.hunger@qt.io>
2018-09-12 12:57:59 +0200

    ClangFormat: Fix warning about initialization order
    Change-Id: I83858454b5654924be502b249d3b355af7aab1b8
    Reviewed-by: Ivan Donchevskii <ivan.donchevskii@qt.io>

--- src/plugins/clangformat/clangformatconfigwidget.cpp
+++ src/plugins/clangformat/clangformatconfigwidget.cpp
@@ -132,8 +132,8 @@ static QByteArray tableToYAML(QTableWidget *table)
 ClangFormatConfigWidget::ClangFormatConfigWidget(ProjectExplorer::Project *project,
                                                  QWidget *parent)
     : QWidget(parent)
-    , m_ui(new Ui::ClangFormatConfigWidget)
     , m_project(project)
+    , m_ui(std::make_unique<Ui::ClangFormatConfigWidget>())
 {
     m_ui->setupUi(this);
 
