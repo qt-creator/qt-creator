qt-creator/qt-creator

commit 4b9945c32981105bfae3ba59f9565757f0dc7417

ProjectExplorer: Rework executable display handling in runconfigs
The basic idea is to provide an easy-to-configure reusable
building block to provide an "Executable" in a run configuration.

This patch here extracts some generic from the previous
ExecutableAspect into a BaseStringAspect and bases the new
ExecutableAspect implementation on it. It also adds the
the ability to make it editable or to manually override an
executable, and adds a SymbolFile aspect for the accompanying
sources of debug information.

Use in RemoteLinux and Qnx for starters and demonstration purposes.
In the end the Qnx runconfiguration widget does not depend on the
RemoteLinux implementation anymore by replacing the previous
code-sharing-by-inheritance by direct (and less) code.

Change-Id: I91b55820455256a8526e39458364b6cb74e63cd7
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/plugins/qnx/qnxrunconfiguration.cpp
@@ -26,17 +26,12 @@
 #include "qnxrunconfiguration.h"
 
 #include "qnxconstants.h"
-#include "qnxdevicefactory.h"
 
 #include <projectexplorer/runnables.h>
-#include <projectexplorer/target.h>
-
-#include <remotelinux/remotelinuxrunconfigurationwidget.h>
 
 #include <utils/environment.h>
 
-#include <QLabel>
-#include <QLineEdit>
+#include <QFormLayout>
 
 using namespace ProjectExplorer;
 using namespace RemoteLinux;
@@ -44,60 +39,41 @@ using namespace RemoteLinux;
 namespace Qnx {
 namespace Internal {
 
-const char QtLibPathKey[] = "Qt4ProjectManager.QnxRunConfiguration.QtLibPath";
-
 QnxRunConfiguration::QnxRunConfiguration(Target *target)
     : RemoteLinuxRunConfiguration(target, Constants::QNX_QNX_RUNCONFIGURATION_PREFIX)
-{}
+{
+    auto libAspect = new QtLibPathAspect(this);
+    libAspect->setSettingsKey("Qt4ProjectManager.QnxRunConfiguration.QtLibPath");
+    libAspect->setLabelText(tr("Path to Qt libraries on device"));
+    addExtraAspect(libAspect);
+}
 
 Runnable QnxRunConfiguration::runnable() const
 {
     auto r = RemoteLinuxRunConfiguration::runnable().as<StandardRunnable>();
-    if (!m_qtLibPath.isEmpty()) {
-        r.environment.appendOrSet(QLatin1String("LD_LIBRARY_PATH"),
-                        m_qtLibPath + QLatin1String("/lib:$LD_LIBRARY_PATH"));
-        r.environment.appendOrSet(QLatin1String("QML_IMPORT_PATH"),
-                        m_qtLibPath + QLatin1String("/imports:$QML_IMPORT_PATH"));
-        r.environment.appendOrSet(QLatin1String("QML2_IMPORT_PATH"),
-                        m_qtLibPath + QLatin1String("/qml:$QML2_IMPORT_PATH"));
-        r.environment.appendOrSet(QLatin1String("QT_PLUGIN_PATH"),
-                        m_qtLibPath + QLatin1String("/plugins:$QT_PLUGIN_PATH"));
-        r.environment.set(QLatin1String("QT_QPA_FONTDIR"),
-                        m_qtLibPath + QLatin1String("/lib/fonts"));
+    QString libPath = extraAspect<QtLibPathAspect>()->value();
+    if (!libPath.isEmpty()) {
+        r.environment.appendOrSet("LD_LIBRARY_PATH", libPath + "/lib:$LD_LIBRARY_PATH");
+        r.environment.appendOrSet("QML_IMPORT_PATH", libPath + "/imports:$QML_IMPORT_PATH");
+        r.environment.appendOrSet("QML2_IMPORT_PATH", libPath + "/qml:$QML2_IMPORT_PATH");
+        r.environment.appendOrSet("QT_PLUGIN_PATH", libPath + "/plugins:$QT_PLUGIN_PATH");
+        r.environment.set("QT_QPA_FONTDIR", libPath + "/lib/fonts");
     }
     return r;
 }
 
 QWidget *QnxRunConfiguration::createConfigurationWidget()
 {
-    auto rcWidget = qobject_cast<RemoteLinuxRunConfigurationWidget *>
-        (RemoteLinuxRunConfiguration::createConfigurationWidget());
+    auto widget = new QWidget;
+    auto formLayout = new QFormLayout(widget);
 
-    auto label = new QLabel(tr("Path to Qt libraries on device:"));
-    auto lineEdit = new QLineEdit(m_qtLibPath);
+    extraAspect<ExecutableAspect>()->addToMainConfigurationWidget(widget, formLayout);
+    extraAspect<SymbolFileAspect>()->addToMainConfigurationWidget(widget, formLayout);
+    extraAspect<ArgumentsAspect>()->addToMainConfigurationWidget(widget, formLayout);
+    extraAspect<WorkingDirectoryAspect>()->addToMainConfigurationWidget(widget, formLayout);
+    extraAspect<QtLibPathAspect>()->addToMainConfigurationWidget(widget, formLayout);
 
-    connect(lineEdit, &QLineEdit::textChanged,
-            this, [this](const QString &path) { m_qtLibPath = path; });
-
-    rcWidget->addFormLayoutRow(label, lineEdit);
-
-    return rcWidget;
-}
-
-QVariantMap QnxRunConfiguration::toMap() const
-{
-    QVariantMap map(RemoteLinuxRunConfiguration::toMap());
-    map.insert(QLatin1String(QtLibPathKey), m_qtLibPath);
-    return map;
-}
-
-bool QnxRunConfiguration::fromMap(const QVariantMap &map)
-{
-    if (!RemoteLinuxRunConfiguration::fromMap(map))
-        return false;
-
-    m_qtLibPath = map.value(QLatin1String(QtLibPathKey)).toString();
-    return true;
+    return wrapWidget(widget);
 }
 
 // QnxRunConfigurationFactory

src/plugins/projectexplorer/runconfigurationaspects.cpp
@@ -30,6 +30,7 @@
 #include "projectexplorer.h"
 #include "projectexplorersettings.h"
 #include "runconfiguration.h"
+#include "target.h"
 
 #include <utils/utilsicons.h>
 #include <utils/fancylineedit.h>
@@ -294,58 +295,259 @@ void ArgumentsAspect::addToMainConfigurationWidget(QWidget *parent, QFormLayout 
 }
 
 /*!
+    \class ProjectExplorer::BaseStringAspect
+*/
+
+BaseStringAspect::BaseStringAspect(RunConfiguration *rc)
+    : IRunConfigurationAspect(rc)
+{
+}
+
+QString BaseStringAspect::value() const
+{
+    return m_value;
+}
+
+void BaseStringAspect::setValue(const QString &value)
+{
+    m_value = value;
+    update();
+}
+
+void BaseStringAspect::fromMap(const QVariantMap &map)
+{
+    if (!settingsKey().isEmpty())
+        m_value = map.value(settingsKey()).toString();
+    if (m_checker)
+        m_checker->fromMap(map);
+}
+
+void BaseStringAspect::toMap(QVariantMap &map) const
+{
+    if (!settingsKey().isEmpty())
+        map.insert(settingsKey(), m_value);
+    if (m_checker)
+        m_checker->toMap(map);
+}
+
+FileName BaseStringAspect::fileName() const
+{
+    return FileName::fromString(m_value);
+}
+
+void BaseStringAspect::setLabelText(const QString &labelText)
+{
+    m_labelText = labelText;
+    if (m_label)
+        m_label->setText(labelText);
+}
+
+QString BaseStringAspect::labelText() const
+{
+    return m_labelText;
+}
+
+void BaseStringAspect::setDisplayFilter(const std::function<QString(const QString &)> &displayFilter)
+{
+    m_displayFilter = displayFilter;
+}
+
+bool BaseStringAspect::isChecked() const
+{
+    return !m_checker || m_checker->value();
+}
+
+void BaseStringAspect::setDisplayStyle(DisplayStyle displayStyle)
+{
+    m_displayStyle = displayStyle;
+}
+
+void BaseStringAspect::setPlaceHolderText(const QString &placeHolderText)
+{
+    m_placeHolderText = placeHolderText;
+    if (m_lineEditDisplay)
+        m_lineEditDisplay->setPlaceholderText(placeHolderText);
+}
+
+void BaseStringAspect::addToMainConfigurationWidget(QWidget *parent, QFormLayout *layout)
+{
+    QTC_CHECK(!m_label);
+    m_label = new QLabel(parent);
+    m_label->setTextInteractionFlags(Qt::TextSelectableByMouse);
+
+    auto hbox = new QHBoxLayout;
+    switch (m_displayStyle) {
+    case PathChooserDisplay:
+        m_pathChooserDisplay = new PathChooser(parent);
+        m_pathChooserDisplay->setExpectedKind(PathChooser::File);
+        connect(m_pathChooserDisplay, &PathChooser::pathChanged,
+                this, &BaseStringAspect::setValue);
+        hbox->addWidget(m_pathChooserDisplay);
+        break;
+    case LineEditDisplay:
+        m_lineEditDisplay = new FancyLineEdit(parent);
+        m_lineEditDisplay->setPlaceholderText(m_placeHolderText);
+        connect(m_lineEditDisplay, &FancyLineEdit::textEdited,
+                this, &BaseStringAspect::setValue);
+        hbox->addWidget(m_lineEditDisplay);
+        break;
+    case LabelDisplay:
+        m_labelDisplay = new QLabel(parent);
+        m_labelDisplay->setTextInteractionFlags(Qt::TextSelectableByMouse);
+        hbox->addWidget(m_labelDisplay);
+        break;
+    }
+
+    if (m_checker) {
+        auto form = new QFormLayout;
+        form->setContentsMargins(0, 0, 0, 0);
+        form->setFormAlignment(Qt::AlignLeft | Qt::AlignVCenter);
+        m_checker->addToMainConfigurationWidget(parent, form);
+        hbox->addLayout(form);
+    }
+    layout->addRow(m_label, hbox);
+
+    update();
+}
+
+void BaseStringAspect::update()
+{
+    const QString displayedString = m_displayFilter ? m_displayFilter(m_value) : m_value;
+    const bool enabled = !m_checker || m_checker->value();
+
+    if (m_pathChooserDisplay) {
+        m_pathChooserDisplay->setFileName(FileName::fromString(displayedString));
+        m_pathChooserDisplay->setEnabled(enabled);
+    }
+
+    if (m_lineEditDisplay) {
+        m_lineEditDisplay->setText(displayedString);
+        m_lineEditDisplay->setEnabled(enabled);
+    }
+
+    if (m_labelDisplay)
+        m_labelDisplay->setText(displayedString);
+
+    if (m_label)
+        m_label->setText(m_labelText);
+}
+
+void BaseStringAspect::makeCheckable(const QString &checkerLabel, const QString &checkerKey)
+{
+    QTC_ASSERT(!m_checker, return);
+    m_checker = new BaseBoolAspect(runConfiguration());
+    m_checker->setLabel(checkerLabel);
+    m_checker->setSettingsKey(checkerKey);
+
+    connect(m_checker, &BaseBoolAspect::changed, this, &BaseStringAspect::update);
+
+    update();
+}
+
+/*!
     \class ProjectExplorer::ExecutableAspect
 */
 
-ExecutableAspect::ExecutableAspect(RunConfiguration *runConfig, bool isRemote, const QString &label)
-    : IRunConfigurationAspect(runConfig), m_isRemote(isRemote), m_labelString(label)
+ExecutableAspect::ExecutableAspect(RunConfiguration *rc)
+    : IRunConfigurationAspect(rc), m_executable(rc)
 {
     setDisplayName(tr("Executable"));
     setId("ExecutableAspect");
+    setExecutablePathStyle(HostOsInfo::hostOs());
+    m_executable.setPlaceHolderText(tr("<unknown>"));
+    m_executable.setLabelText(tr("Executable:"));
+    m_executable.setDisplayStyle(BaseStringAspect::LabelDisplay);
 }
 
-Utils::FileName ExecutableAspect::executable() const
+void ExecutableAspect::setExecutablePathStyle(OsType osType)
 {
-    return m_executable;
+    if (osType == OsTypeWindows) {
+        // Stolen from QDir::toNativeSeparators which cannot be used directly as it
+        // depends on host type, whereas we need a configurable value here.
+        m_executable.setDisplayFilter([&](const QString &pathName) {
+            int i = pathName.indexOf(QLatin1Char('/'));
+            if (i != -1) {
+                QString n(pathName);
+
+                QChar * const data = n.data();
+                data[i++] = QLatin1Char('\\');
+
+                for (; i < n.length(); ++i) {
+                    if (data[i] == QLatin1Char('/'))
+                        data[i] = QLatin1Char('\\');
+                }
+
+                return n;
+            }
+            return pathName;
+        });
+    } else {
+        m_executable.setDisplayFilter({});
+    }
 }
 
-void ExecutableAspect::setExecutable(const FileName &executable)
+void ExecutableAspect::makeOverridable(const QString &overridingKey, const QString &useOverridableKey)
 {
-    m_executable = executable;
-    if (m_executableDisplay)
-        m_executableDisplay->setText(executableText());
+    QTC_ASSERT(!m_alternativeExecutable, return);
+    m_alternativeExecutable = new BaseStringAspect(runConfiguration());
+    m_alternativeExecutable->setDisplayStyle(BaseStringAspect::LineEditDisplay);
+    m_alternativeExecutable->setLabelText(tr("Alternate executable on device:"));
+    m_alternativeExecutable->setSettingsKey(overridingKey);
+    m_alternativeExecutable->makeCheckable(tr("Use this command instead"), useOverridableKey);
 }
 
-QString ExecutableAspect::executableText() const
+FileName ExecutableAspect::executable() const
 {
-    if (m_executable.isEmpty())
-        return tr("<unknown>");
-    if (m_isRemote)
-        return m_executable.toString();
-    return m_executable.toUserOutput();
+    if (m_alternativeExecutable && m_alternativeExecutable->isChecked())
+        return m_alternativeExecutable->fileName();
+
+    return m_executable.fileName();
 }
 
 void ExecutableAspect::addToMainConfigurationWidget(QWidget *parent, QFormLayout *layout)
 {
-    QTC_CHECK(!m_executableDisplay);
-    m_executableDisplay = new QLabel(parent);
-    m_executableDisplay->setTextInteractionFlags(Qt::TextSelectableByMouse);
-    m_executableDisplay->setText(executableText());
+    m_executable.addToMainConfigurationWidget(parent, layout);
+    if (m_alternativeExecutable)
+        m_alternativeExecutable->addToMainConfigurationWidget(parent, layout);
+}
 
-    QString labelText = m_labelString;
-    if (labelText.isEmpty())
-        labelText = m_isRemote ? tr("Remote Executable") : tr("Executable");
-    layout->addRow(labelText + ':', m_executableDisplay);
+void ExecutableAspect::setLabelText(const QString &labelText)
+{
+    m_executable.setLabelText(labelText);
+}
+
+void ExecutableAspect::setPlaceHolderText(const QString &placeHolderText)
+{
+    m_executable.setPlaceHolderText(placeHolderText);
+}
+
+void ExecutableAspect::setExecutable(const FileName &executable)
+{
+   m_executable.setValue(executable.toString());
+}
+
+void ExecutableAspect::fromMap(const QVariantMap &map)
+{
+    m_executable.fromMap(map);
+    if (m_alternativeExecutable)
+        m_alternativeExecutable->fromMap(map);
+}
+
+void ExecutableAspect::toMap(QVariantMap &map) const
+{
+    m_executable.toMap(map);
+    if (m_alternativeExecutable)
+        m_alternativeExecutable->toMap(map);
 }
 
 /*!
     \class ProjectExplorer::BaseBoolAspect
 */
 
-BaseBoolAspect::BaseBoolAspect(RunConfiguration *runConfig, const QString &key) :
-    IRunConfigurationAspect(runConfig)
+BaseBoolAspect::BaseBoolAspect(RunConfiguration *runConfig, const QString &settingsKey)
+    : IRunConfigurationAspect(runConfig)
 {
-    setSettingsKey(key);
+    setSettingsKey(settingsKey);
 }
 
 void BaseBoolAspect::addToMainConfigurationWidget(QWidget *parent, QFormLayout *layout)

src/plugins/projectexplorer/runconfigurationaspects.h
@@ -29,10 +29,12 @@
 #include "applicationlauncher.h"
 
 #include <utils/fileutils.h>
+#include <utils/osspecificaspects.h>
 
 QT_BEGIN_NAMESPACE
 class QCheckBox;
 class QLabel;
+class QLineEdit;
 class QFormLayout;
 class QToolButton;
 QT_END_NAMESPACE
@@ -79,7 +81,8 @@ class PROJECTEXPLORER_EXPORT WorkingDirectoryAspect : public IRunConfigurationAs
     Q_OBJECT
 
 public:
-    explicit WorkingDirectoryAspect(RunConfiguration *runConfig, const QString &settingsKey);
+    explicit WorkingDirectoryAspect(RunConfiguration *runConfig,
+                                    const QString &settingsKey = QString());
 
     void addToMainConfigurationWidget(QWidget *parent, QFormLayout *layout);
 
@@ -107,7 +110,7 @@ class PROJECTEXPLORER_EXPORT ArgumentsAspect : public IRunConfigurationAspect
     Q_OBJECT
 
 public:
-    explicit ArgumentsAspect(RunConfiguration *runConfig, const QString &settingsKey);
+    explicit ArgumentsAspect(RunConfiguration *runConfig, const QString &settingsKey = QString());
 
     void addToMainConfigurationWidget(QWidget *parent, QFormLayout *layout);
 
@@ -127,49 +130,27 @@ private:
     QPointer<Utils::FancyLineEdit> m_chooser;
 };
 
-class PROJECTEXPLORER_EXPORT ExecutableAspect : public IRunConfigurationAspect
-{
-    Q_OBJECT
-
-public:
-    ExecutableAspect(RunConfiguration *runConfig,
-                     bool isRemote = false,
-                     const QString &label = QString());
-
-    void addToMainConfigurationWidget(QWidget *parent, QFormLayout *layout);
-
-    Utils::FileName executable() const;
-    void setExecutable(const Utils::FileName &executable);
-
-private:
-    QString executableText() const;
-
-    Utils::FileName m_executable;
-    bool m_isRemote = false;
-    QString m_labelString;
-    QPointer<QLabel> m_executableDisplay;
-};
-
 class PROJECTEXPLORER_EXPORT BaseBoolAspect : public IRunConfigurationAspect
 {
     Q_OBJECT
 
 public:
-    BaseBoolAspect(RunConfiguration *rc, const QString &settingsKey);
+    explicit BaseBoolAspect(RunConfiguration *rc, const QString &settingsKey = QString());
 
     void addToMainConfigurationWidget(QWidget *parent, QFormLayout *layout);
+
     bool value() const;
     void setValue(bool val);
 
     void setLabel(const QString &label);
 
+    void fromMap(const QVariantMap &map) override;
+    void toMap(QVariantMap &map) const override;
+
 signals:
     void changed();
 
 private:
-    void fromMap(const QVariantMap &map) override;
-    void toMap(QVariantMap &map) const override;
-
     bool m_value = false;
     QString m_label;
     QPointer<QCheckBox> m_checkBox; // Owned by RunConfigWidget
@@ -189,6 +170,87 @@ class PROJECTEXPLORER_EXPORT UseDyldSuffixAspect : public BaseBoolAspect
 
 public:
     UseDyldSuffixAspect(RunConfiguration *rc, const QString &settingsKey);
+};
+
+class PROJECTEXPLORER_EXPORT BaseStringAspect : public IRunConfigurationAspect
+{
+    Q_OBJECT
+
+public:
+    explicit BaseStringAspect(RunConfiguration *rc);
+
+    void addToMainConfigurationWidget(QWidget *parent, QFormLayout *layout);
+
+    QString value() const;
+    void setValue(const QString &val);
+
+    QString labelText() const;
+    void setLabelText(const QString &labelText);
+
+    void setDisplayFilter(const std::function<QString (const QString &)> &displayFilter);
+    void setPlaceHolderText(const QString &placeHolderText);
+
+    bool isChecked() const;
+    void makeCheckable(const QString &optionalLabel, const QString &optionalBaseKey);
+
+    enum DisplayStyle { LabelDisplay, LineEditDisplay, PathChooserDisplay };
+    void setDisplayStyle(DisplayStyle style);
+
+    void fromMap(const QVariantMap &map) override;
+    void toMap(QVariantMap &map) const override;
+
+    Utils::FileName fileName() const;
+    void setFileName(const Utils::FileName &val);
+
+private:
+    void update();
+
+    DisplayStyle m_displayStyle = LabelDisplay;
+    QString m_labelText;
+    std::function<QString(const QString &)> m_displayFilter;
+    BaseBoolAspect *m_checker = nullptr;
+
+    QString m_value;
+    QString m_placeHolderText;
+    QPointer<QLabel> m_label;
+    QPointer<QLabel> m_labelDisplay;
+    QPointer<Utils::FancyLineEdit> m_lineEditDisplay;
+    QPointer<Utils::PathChooser> m_pathChooserDisplay;
+};
+
+class PROJECTEXPLORER_EXPORT ExecutableAspect : public IRunConfigurationAspect
+{
+    Q_OBJECT
+
+public:
+    explicit ExecutableAspect(RunConfiguration *rc);
+
+    Utils::FileName executable() const;
+    void setExecutable(const Utils::FileName &executable);
+
+    void makeOverridable(const QString &overridingKey, const QString &useOverridableKey);
+    void addToMainConfigurationWidget(QWidget *parent, QFormLayout *layout);
+    void setLabelText(const QString &labelText);
+    void setPlaceHolderText(const QString &placeHolderText);
+    void setExecutablePathStyle(Utils::OsType osType);
+
+protected:
+    void fromMap(const QVariantMap &map) override;
+    void toMap(QVariantMap &map) const override;
+
+private:
+    QString executableText() const;
+
+    BaseStringAspect m_executable;
+    BaseStringAspect *m_alternativeExecutable = nullptr;
+};
+
+class PROJECTEXPLORER_EXPORT SymbolFileAspect : public BaseStringAspect
+{
+    Q_OBJECT
+
+public:
+     SymbolFileAspect(RunConfiguration *rc) : BaseStringAspect(rc) {}
 };
 
 } // namespace ProjectExplorer

src/plugins/qnx/qnxdebugsupport.cpp
@@ -45,6 +45,7 @@
 #include <projectexplorer/kitchooser.h>
 #include <projectexplorer/kitinformation.h>
 #include <projectexplorer/projectexplorer.h>
+#include <projectexplorer/runconfigurationaspects.h>
 #include <projectexplorer/runnables.h>
 #include <projectexplorer/target.h>
 #include <projectexplorer/toolchain.h>
@@ -157,7 +158,6 @@ QnxDebugSupport::QnxDebugSupport(RunControl *runControl)
     setSolibSearchPath(searchPaths(k));
     if (auto qtVersion = dynamic_cast<QnxQtVersion *>(QtSupport::QtKitInformation::qtVersion(k)))
         setSysRoot(qtVersion->qnxTarget());
-    setSymbolFile(runConfig->localExecutableFilePath());
 }
 
 
@@ -263,8 +263,10 @@ void QnxAttachDebugSupport::showProcessesDialog()
     const int pid = process.pid;
 //    QString projectSourceDirectory = dlg.projectSource();
     QString localExecutable = dlg.localExecutable();
-    if (localExecutable.isEmpty())
-        localExecutable = runConfig->localExecutableFilePath();
+    if (localExecutable.isEmpty()) {
+        if (auto aspect = runConfig->extraAspect<SymbolFileAspect>())
+            localExecutable = aspect->fileName().toString();
+    }
 
     auto runControl = new RunControl(runConfig, ProjectExplorer::Constants::DEBUG_RUN_MODE);
     auto debugger = new QnxAttachDebugSupport(runControl);

src/plugins/qnx/qnxrunconfiguration.h
@@ -25,10 +25,19 @@
 
 #pragma once
 
+#include <projectexplorer/runconfigurationaspects.h>
 #include <remotelinux/remotelinuxrunconfiguration.h>
 
 namespace Qnx {
 namespace Internal {
+
+class QtLibPathAspect : public ProjectExplorer::BaseStringAspect
+{
+    Q_OBJECT
+
+public:
+    QtLibPathAspect(ProjectExplorer::RunConfiguration *rc) : BaseStringAspect(rc) {}
+};
 
 class QnxRunConfiguration : public RemoteLinux::RemoteLinuxRunConfiguration
 {
@@ -40,11 +49,6 @@ public:
 private:
     ProjectExplorer::Runnable runnable() const override;
     QWidget *createConfigurationWidget() override;
-
-    QVariantMap toMap() const override;
-    bool fromMap(const QVariantMap &map) override;
-
-    QString m_qtLibPath;
 };
 
 class QnxRunConfigurationFactory : public ProjectExplorer::RunConfigurationFactory

src/plugins/qnx/slog2inforunner.cpp
@@ -29,7 +29,9 @@
 #include "qnxdeviceprocess.h"
 #include "qnxrunconfiguration.h"
 
+#include <projectexplorer/runconfigurationaspects.h>
 #include <projectexplorer/runnables.h>
+
 #include <utils/qtcassert.h>
 
 #include <QRegExp>
@@ -44,9 +46,7 @@ Slog2InfoRunner::Slog2InfoRunner(RunControl *runControl)
     : RunWorker(runControl)
 {
     setDisplayName("Slog2InfoRunner");
-    auto qnxRunConfig = qobject_cast<QnxRunConfiguration *>(runControl->runConfiguration());
-    QTC_ASSERT(qnxRunConfig, return);
-    m_applicationId = FileName::fromString(qnxRunConfig->remoteExecutableFilePath()).fileName();
+    m_applicationId = runControl->runConfiguration()->extraAspect<ExecutableAspect>()->executable().fileName();
 
     // See QTCREATORBUG-10712 for details.
     // We need to limit length of ApplicationId to 63 otherwise it would not match one in slog2info.

src/plugins/remotelinux/remotelinux.pro
@@ -14,7 +14,6 @@ HEADERS += \
     publickeydeploymentdialog.h \
     genericlinuxdeviceconfigurationwizard.h \
     genericlinuxdeviceconfigurationfactory.h \
-    remotelinuxrunconfigurationwidget.h \
     remotelinuxdebugsupport.h \
     genericlinuxdeviceconfigurationwizardpages.h \
     abstractremotelinuxdeploystep.h \
@@ -58,7 +57,6 @@ SOURCES += \
     publickeydeploymentdialog.cpp \
     genericlinuxdeviceconfigurationwizard.cpp \
     genericlinuxdeviceconfigurationfactory.cpp \
-    remotelinuxrunconfigurationwidget.cpp \
     remotelinuxdebugsupport.cpp \
     genericlinuxdeviceconfigurationwizardpages.cpp \
     abstractremotelinuxdeploystep.cpp \

src/plugins/remotelinux/remotelinux.qbs
@@ -91,8 +91,6 @@ Project {
             "remotelinuxqmltoolingsupport.h",
             "remotelinuxrunconfiguration.cpp",
             "remotelinuxrunconfiguration.h",
-            "remotelinuxrunconfigurationwidget.cpp",
-            "remotelinuxrunconfigurationwidget.h",
             "remotelinuxsignaloperation.cpp",
             "remotelinuxsignaloperation.h",
             "remotelinuxutils.cpp",

commit fa50153fc8f0ec992224007fa4101253d7554fcd

Qnx: Fix passing path to Qt libraries on device to run configuration
The line edit was gone.
Broke in 4b9945c32981105bfae3ba59f9565757f0dc7417

Task-number: QTCREATORBUG-21100
Change-Id: Ib341df783d6b8060226863637c65f081635deaf0
Reviewed-by: hjk <hjk@qt.io>

src/plugins/qnx/qnxrunconfiguration.cpp
@@ -39,6 +39,7 @@ QnxRunConfiguration::QnxRunConfiguration(Target *target, Core::Id id)
     auto libAspect = new QtLibPathAspect(this);
     libAspect->setSettingsKey("Qt4ProjectManager.QnxRunConfiguration.QtLibPath");
     libAspect->setLabelText(tr("Path to Qt libraries on device"));
+    libAspect->setDisplayStyle(BaseStringAspect::LineEditDisplay);
     addExtraAspect(libAspect);
 }
 