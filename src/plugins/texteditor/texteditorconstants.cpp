6059d560bcce7b23e60dc194de6a2eaa76962ed9
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2018-09-21 01:28:48 +0300

    TextEditor: Add punctuation to nameForStyle
    This amends commit 50e5aacb026e79995c9967b12eabb9d37336a841.
    
    Change-Id: I9ff8dc5757f527f499f6268b7c92b938af4cd0d3
    Reviewed-by: Ivan Donchevskii <ivan.donchevskii@qt.io>

--- src/plugins/texteditor/texteditorconstants.cpp
+++ src/plugins/texteditor/texteditorconstants.cpp
@@ -64,6 +64,7 @@ const char *nameForStyle(TextStyle style)
     case C_PRIMITIVE_TYPE:      return "PrimitiveType";
     case C_OPERATOR:            return "Operator";
     case C_OVERLOADED_OPERATOR: return "Overloaded Operator";
+    case C_PUNCTUATION:         return "Punctuation";
     case C_PREPROCESSOR:        return "Preprocessor";
     case C_LABEL:               return "Label";
     case C_COMMENT:             return "Comment";
