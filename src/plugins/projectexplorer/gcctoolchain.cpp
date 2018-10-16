qt-creator/qt-creator

commit 6aa11eb75e2374bc1c0c643588b3d06d797759be

CppTools/ProjectExplorer: Remove enum duplication
... between CppTools::ProjectPart and ProjectExplorer::ToolChain.

Change-Id: I8b448747e454adbed77547460383b8515462cc81
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>
Reviewed-by: Ivan Donchevskii <ivan.donchevskii@qt.io>

src/plugins/projectexplorer/gcctoolchain.cpp
@@ -302,9 +302,9 @@ QString GccToolChain::defaultDisplayName() const
                                                                   compilerCommand().parentDir().toUserOutput());
 }
 
-ToolChain::CompilerFlags GccToolChain::defaultCompilerFlags() const
+LanguageExtensions GccToolChain::defaultLanguageExtensions() const
 {
-    return CompilerFlags(GnuExtensions);
+    return LanguageExtension::Gnu;
 }
 
 QString GccToolChain::typeDisplayName() const
@@ -449,7 +449,7 @@ ToolChain::MacroInspectionRunner GccToolChain::createMacroInspectionRunner() con
         macroCache->insert(arguments, report);
 
         qCDebug(gccLog) << "MacroInspectionReport for code model:";
-        qCDebug(gccLog) << "Language version:" << report.languageVersion;
+        qCDebug(gccLog) << "Language version:" << static_cast<int>(report.languageVersion);
         for (const Macro &m : filteredMacros) {
             qCDebug(gccLog) << compilerCommand.toUserOutput()
                             << (lang == Constants::CXX_LANGUAGE_ID ? ": C++ [" : ": C [")
@@ -476,29 +476,29 @@ ProjectExplorer::Macros GccToolChain::predefinedMacros(const QStringList &cxxfla
 }
 
 /**
- * @brief Parses gcc flags -std=*, -fopenmp, -fms-extensions, -ansi.
+ * @brief Parses gcc flags -std=*, -fopenmp, -fms-extensions.
  * @see http://gcc.gnu.org/onlinedocs/gcc/C-Dialect-Options.html
  */
-ToolChain::CompilerFlags GccToolChain::compilerFlags(const QStringList &cxxflags) const
+LanguageExtensions GccToolChain::languageExtensions(const QStringList &cxxflags) const
 {
-    CompilerFlags flags = defaultCompilerFlags();
+    LanguageExtensions extensions = defaultLanguageExtensions();
 
     const QStringList allCxxflags = m_platformCodeGenFlags + cxxflags; // add only cxxflags is empty?
     foreach (const QString &flag, allCxxflags) {
         if (flag.startsWith("-std=")) {
             const QByteArray std = flag.mid(5).toLatin1();
             if (std.startsWith("gnu"))
-                flags |= CompilerFlags(GnuExtensions);
+                extensions |= LanguageExtension::Gnu;
             else
-                flags &= ~CompilerFlags(GnuExtensions);
+                extensions &= ~LanguageExtensions(LanguageExtension::Gnu);
         } else if (flag == "-fopenmp") {
-            flags |= OpenMP;
+            extensions |= LanguageExtension::Gnu;
         } else if (flag == "-fms-extensions") {
-            flags |= MicrosoftExtensions;
+            extensions |= LanguageExtension::Microsoft;
         }
     }
 
-    return flags;
+    return extensions;
 }
 
 WarningFlags GccToolChain::warningFlags(const QStringList &cflags) const
@@ -1234,15 +1234,15 @@ QString ClangToolChain::makeCommand(const Environment &environment) const
 }
 
 /**
- * @brief Similar to \a GccToolchain::compilerFlags, but recognizes
+ * @brief Similar to \a GccToolchain::languageExtensions, but recognizes
  * "-fborland-extensions".
  */
-ToolChain::CompilerFlags ClangToolChain::compilerFlags(const QStringList &cxxflags) const
+LanguageExtensions ClangToolChain::languageExtensions(const QStringList &cxxflags) const
 {
-    CompilerFlags flags = GccToolChain::compilerFlags(cxxflags);
+    LanguageExtensions extensions = GccToolChain::languageExtensions(cxxflags);
     if (cxxflags.contains("-fborland-extensions"))
-        flags |= BorlandExtensions;
-    return flags;
+        extensions |= LanguageExtension::Borland;
+    return extensions;
 }
 
 WarningFlags ClangToolChain::warningFlags(const QStringList &cflags) const
@@ -1283,9 +1283,9 @@ void ClangToolChain::addToEnvironment(Environment &env) const
     env.unset("PWD");
 }
 
-ToolChain::CompilerFlags ClangToolChain::defaultCompilerFlags() const
+LanguageExtensions ClangToolChain::defaultLanguageExtensions() const
 {
-    return CompilerFlags(GnuExtensions);
+    return LanguageExtension::Gnu;
 }
 
 IOutputParser *ClangToolChain::outputParser() const
@@ -1476,25 +1476,25 @@ QString LinuxIccToolChain::typeDisplayName() const
 }
 
 /**
- * Similar to \a GccToolchain::compilerFlags, but uses "-openmp" instead of
+ * Similar to \a GccToolchain::languageExtensions, but uses "-openmp" instead of
  * "-fopenmp" and "-fms-dialect[=ver]" instead of "-fms-extensions".
  * @see UNIX manual for "icc"
  */
-ToolChain::CompilerFlags LinuxIccToolChain::compilerFlags(const QStringList &cxxflags) const
+LanguageExtensions LinuxIccToolChain::languageExtensions(const QStringList &cxxflags) const
 {
     QStringList copy = cxxflags;
     copy.removeAll("-fopenmp");
     copy.removeAll("-fms-extensions");
 
-    CompilerFlags flags = GccToolChain::compilerFlags(cxxflags);
+    LanguageExtensions extensions = GccToolChain::languageExtensions(cxxflags);
     if (cxxflags.contains("-openmp"))
-        flags |= OpenMP;
+        extensions |= LanguageExtension::OpenMP;
     if (cxxflags.contains("-fms-dialect")
             || cxxflags.contains("-fms-dialect=8")
             || cxxflags.contains("-fms-dialect=9")
             || cxxflags.contains("-fms-dialect=10"))
-        flags |= MicrosoftExtensions;
-    return flags;
+        extensions |= LanguageExtension::Microsoft;
+    return extensions;
 }
 
 IOutputParser *LinuxIccToolChain::outputParser() const

src/plugins/clangcodemodel/clangeditordocumentprocessor.cpp
@@ -469,7 +469,7 @@ private:
         // Determine file kind with respect to ambiguous headers.
         CppTools::ProjectFile::Kind fileKind = CppTools::ProjectFile::classify(m_filePath);
         if (fileKind == CppTools::ProjectFile::AmbiguousHeader) {
-            fileKind = m_projectPart.languageVersion <= CppTools::ProjectPart::LatestCVersion
+            fileKind = m_projectPart.languageVersion <= ProjectExplorer::LanguageVersion::LatestC
                  ? CppTools::ProjectFile::CHeader
                  : CppTools::ProjectFile::CXXHeader;
         }

src/plugins/cppcheck/cppchecktool.cpp
@@ -137,7 +137,7 @@ QStringList CppcheckTool::additionalArguments(const CppTools::ProjectPart &part)
     if (!m_options.guessArguments)
         return result;
 
-    using Version = CppTools::ProjectPart::LanguageVersion;
+    using Version = ProjectExplorer::LanguageVersion;
     switch (part.languageVersion) {
     case Version::C89:
         result.push_back("--std=c89 --language=c");

src/plugins/cpptools/compileroptionsbuilder.cpp
@@ -59,7 +59,7 @@ QStringList CompilerOptionsBuilder::build(CppTools::ProjectFile::Kind fileKind, 
     m_options.clear();
 
     if (fileKind == ProjectFile::CHeader || fileKind == ProjectFile::CSource) {
-        QTC_ASSERT(m_projectPart.languageVersion <= ProjectPart::LatestCVersion,
+        QTC_ASSERT(m_projectPart.languageVersion <= ProjectExplorer::LanguageVersion::LatestC,
                    return QStringList(););
     }
 
@@ -196,7 +196,7 @@ void CompilerOptionsBuilder::addExtraCodeModelFlags()
 
 void CompilerOptionsBuilder::enableExceptions()
 {
-    if (m_projectPart.languageVersion > ProjectPart::LatestCVersion)
+    if (m_projectPart.languageVersion > ProjectExplorer::LanguageVersion::LatestC)
         add(QLatin1String("-fcxx-exceptions"));
     add(QLatin1String("-fexceptions"));
 }
@@ -378,7 +378,8 @@ void CompilerOptionsBuilder::addMacros(const ProjectExplorer::Macros &macros)
 
 void CompilerOptionsBuilder::updateLanguageOption(ProjectFile::Kind fileKind)
 {
-    const bool objcExt = m_projectPart.languageExtensions & ProjectPart::ObjectiveCExtensions;
+    const bool objcExt = m_projectPart.languageExtensions
+                         & ProjectExplorer::LanguageExtension::ObjectiveC;
     const QStringList options = createLanguageOptionGcc(fileKind, objcExt);
     if (options.isEmpty())
         return;
@@ -394,48 +395,51 @@ void CompilerOptionsBuilder::updateLanguageOption(ProjectFile::Kind fileKind)
 
 void CompilerOptionsBuilder::addOptionsForLanguage(bool checkForBorlandExtensions)
 {
+    using ProjectExplorer::LanguageExtension;
+    using ProjectExplorer::LanguageVersion;
+
     QStringList opts;
-    const ProjectPart::LanguageExtensions languageExtensions = m_projectPart.languageExtensions;
-    const bool gnuExtensions = languageExtensions & ProjectPart::GnuExtensions;
+    const ProjectExplorer::LanguageExtensions languageExtensions = m_projectPart.languageExtensions;
+    const bool gnuExtensions = languageExtensions & LanguageExtension::Gnu;
 
     switch (m_projectPart.languageVersion) {
-    case ProjectPart::C89:
+    case LanguageVersion::C89:
         opts << (gnuExtensions ? QLatin1String("-std=gnu89") : QLatin1String("-std=c89"));
         break;
-    case ProjectPart::C99:
+    case LanguageVersion::C99:
         opts << (gnuExtensions ? QLatin1String("-std=gnu99") : QLatin1String("-std=c99"));
         break;
-    case ProjectPart::C11:
+    case LanguageVersion::C11:
         opts << (gnuExtensions ? QLatin1String("-std=gnu11") : QLatin1String("-std=c11"));
         break;
-    case ProjectPart::C18:
+    case LanguageVersion::C18:
         // Clang 6, 7 and current trunk do not accept "gnu18"/"c18", so use the "*17" variants.
         opts << (gnuExtensions ? QLatin1String("-std=gnu17") : QLatin1String("-std=c17"));
         break;
-    case ProjectPart::CXX11:
+    case LanguageVersion::CXX11:
         opts << (gnuExtensions ? QLatin1String("-std=gnu++11") : QLatin1String("-std=c++11"));
         break;
-    case ProjectPart::CXX98:
+    case LanguageVersion::CXX98:
         opts << (gnuExtensions ? QLatin1String("-std=gnu++98") : QLatin1String("-std=c++98"));
         break;
-    case ProjectPart::CXX03:
+    case LanguageVersion::CXX03:
         opts << (gnuExtensions ? QLatin1String("-std=gnu++03") : QLatin1String("-std=c++03"));
         break;
-    case ProjectPart::CXX14:
+    case LanguageVersion::CXX14:
         opts << (gnuExtensions ? QLatin1String("-std=gnu++14") : QLatin1String("-std=c++14"));
         break;
-    case ProjectPart::CXX17:
+    case LanguageVersion::CXX17:
         opts << (gnuExtensions ? QLatin1String("-std=gnu++17") : QLatin1String("-std=c++17"));
         break;
-    case ProjectPart::CXX2a:
+    case LanguageVersion::CXX2a:
         opts << (gnuExtensions ? QLatin1String("-std=gnu++2a") : QLatin1String("-std=c++2a"));
         break;
     }
 
-    if (languageExtensions & ProjectPart::MicrosoftExtensions)
+    if (languageExtensions & LanguageExtension::Microsoft)
         opts << QLatin1String("-fms-extensions");
 
-    if (checkForBorlandExtensions && (languageExtensions & ProjectPart::BorlandExtensions))
+    if (checkForBorlandExtensions && (languageExtensions & LanguageExtension::Borland))
         opts << QLatin1String("-fborland-extensions");
 
     m_options.append(opts);

src/plugins/cpptools/cppcodemodelinspectordumper.cpp
@@ -103,9 +103,9 @@ QString Utils::toString(ProjectExplorer::HeaderPathType type)
     return QString();
 }
 
-QString Utils::toString(ProjectPart::LanguageVersion languageVersion)
+QString Utils::toString(ProjectExplorer::LanguageVersion languageVersion)
 {
-#define CASE_LANGUAGEVERSION(x) case ProjectPart::x: return QLatin1String(#x)
+#define CASE_LANGUAGEVERSION(x) case ProjectExplorer::LanguageVersion::x: return QLatin1String(#x)
     switch (languageVersion) {
     CASE_LANGUAGEVERSION(C89);
     CASE_LANGUAGEVERSION(C99);
@@ -123,19 +123,19 @@ QString Utils::toString(ProjectPart::LanguageVersion languageVersion)
     return QString();
 }
 
-QString Utils::toString(ProjectPart::LanguageExtensions languageExtension)
+QString Utils::toString(ProjectExplorer::LanguageExtensions languageExtension)
 {
     QString result;
 
-#define CASE_LANGUAGE_EXTENSION(ext) if (languageExtension & ProjectPart::ext) \
+#define CASE_LANGUAGE_EXTENSION(ext) if (languageExtension & ProjectExplorer::LanguageExtension::ext) \
     result += QLatin1String(#ext ", ");
 
-    CASE_LANGUAGE_EXTENSION(NoExtensions);
-    CASE_LANGUAGE_EXTENSION(GnuExtensions);
-    CASE_LANGUAGE_EXTENSION(MicrosoftExtensions);
-    CASE_LANGUAGE_EXTENSION(BorlandExtensions);
-    CASE_LANGUAGE_EXTENSION(OpenMPExtensions);
-    CASE_LANGUAGE_EXTENSION(ObjectiveCExtensions);
+    CASE_LANGUAGE_EXTENSION(None);
+    CASE_LANGUAGE_EXTENSION(Gnu);
+    CASE_LANGUAGE_EXTENSION(Microsoft);
+    CASE_LANGUAGE_EXTENSION(Borland);
+    CASE_LANGUAGE_EXTENSION(OpenMP);
+    CASE_LANGUAGE_EXTENSION(ObjectiveC);
 #undef CASE_LANGUAGE_EXTENSION
     if (result.endsWith(QLatin1String(", ")))
         result.chop(2);

src/plugins/cpptools/cppcodemodelinspectordumper.h
@@ -47,8 +47,8 @@ struct CPPTOOLS_EXPORT Utils
     static QString toString(CPlusPlus::Document::CheckMode checkMode);
     static QString toString(CPlusPlus::Document::DiagnosticMessage::Level level);
     static QString toString(ProjectExplorer::HeaderPathType type);
-    static QString toString(CppTools::ProjectPart::LanguageVersion languageVersion);
-    static QString toString(CppTools::ProjectPart::LanguageExtensions languageExtension);
+    static QString toString(ProjectExplorer::LanguageVersion languageVersion);
+    static QString toString(ProjectExplorer::LanguageExtensions languageExtension);
     static QString toString(CppTools::ProjectPart::QtVersion qtVersion);
     static QString toString(CppTools::ProjectPart::BuildTargetType buildTargetType);
     static QString toString(const QVector<CppTools::ProjectFile> &projectFiles);

src/plugins/cpptools/cppmodelmanager.cpp
@@ -1170,8 +1170,9 @@ ProjectPart::Ptr CppModelManager::fallbackProjectPart()
 
     // Do not activate ObjectiveCExtensions since this will lead to the
     // "objective-c++" language option for a project-less *.cpp file.
-    part->languageExtensions = ProjectPart::AllExtensions;
-    part->languageExtensions &= ~ProjectPart::ObjectiveCExtensions;
+    part->languageExtensions = ProjectExplorer::LanguageExtension::All;
+    part->languageExtensions &= ~ProjectExplorer::LanguageExtensions(
+        ProjectExplorer::LanguageExtension::ObjectiveC);
 
     part->qtVersion = ProjectPart::Qt5;
     part->updateLanguageFeatures();

src/plugins/cpptools/cppprojectinfogenerator.cpp
@@ -68,16 +68,16 @@ public:
         if (m_tcInfo.macroInspectionRunner) {
             auto macroInspectionReport = m_tcInfo.macroInspectionRunner(m_flags.commandLineFlags);
             m_projectPart.toolChainMacros = macroInspectionReport.macros;
-            m_projectPart.languageVersion = static_cast<ProjectPart::LanguageVersion>(
-                macroInspectionReport.languageVersion);
+            m_projectPart.languageVersion = macroInspectionReport.languageVersion;
         } else { // No compiler set in kit.
             if (m_language == Language::C)
-                m_projectPart.languageVersion = ProjectPart::LanguageVersion::LatestCVersion;
+                m_projectPart.languageVersion = ProjectExplorer::LanguageVersion::LatestC;
             if (m_language == Language::CXX)
-                m_projectPart.languageVersion = ProjectPart::LanguageVersion::LatestCxxVersion;
+                m_projectPart.languageVersion = ProjectExplorer::LanguageVersion::LatestCxx;
         }
 
-        mapLanguageExtensions();
+        m_projectPart.languageExtensions = m_flags.languageExtensions;
+
         addHeaderPaths();
     }
 
@@ -87,25 +87,6 @@ private:
         return wordWidth == 64
                 ? ProjectPart::WordWidth64Bit
                 : ProjectPart::WordWidth32Bit;
-    }
-
-    void mapLanguageExtensions()
-    {
-        using namespace ProjectExplorer;
-
-        const ToolChain::CompilerFlags &compilerFlags = m_flags.compilerFlags;
-        ProjectPart::LanguageExtensions &languageExtensions = m_projectPart.languageExtensions;
-
-        if (compilerFlags & ToolChain::BorlandExtensions)
-            languageExtensions |= ProjectPart::BorlandExtensions;
-        if (compilerFlags & ToolChain::GnuExtensions)
-            languageExtensions |= ProjectPart::GnuExtensions;
-        if (compilerFlags & ToolChain::MicrosoftExtensions)
-            languageExtensions |= ProjectPart::MicrosoftExtensions;
-        if (compilerFlags & ToolChain::OpenMP)
-            languageExtensions |= ProjectPart::OpenMPExtensions;
-        if (compilerFlags & ToolChain::ObjectiveC)
-            languageExtensions |= ProjectPart::ObjectiveCExtensions;
     }
 
     void addHeaderPaths()
@@ -181,6 +162,8 @@ static ProjectPart::Ptr projectPartFromRawProjectPart(const RawProjectPart &rawP
 
 QVector<ProjectPart::Ptr> ProjectInfoGenerator::createProjectParts(const RawProjectPart &rawProjectPart)
 {
+    using ProjectExplorer::LanguageExtension;
+
     QVector<ProjectPart::Ptr> result;
     ProjectFileCategorizer cat(rawProjectPart.displayName,
                                rawProjectPart.files,
@@ -196,7 +179,7 @@ QVector<ProjectPart::Ptr> ProjectInfoGenerator::createProjectParts(const RawProj
                                         cat.cxxSources(),
                                         cat.partName("C++"),
                                         Language::CXX,
-                                        ProjectPart::NoExtensions);
+                                        LanguageExtension::None);
         }
 
         if (cat.hasObjcxxSources()) {
@@ -205,7 +188,7 @@ QVector<ProjectPart::Ptr> ProjectInfoGenerator::createProjectParts(const RawProj
                                         cat.objcxxSources(),
                                         cat.partName("Obj-C++"),
                                         Language::CXX,
-                                        ProjectPart::ObjectiveCExtensions);
+                                        LanguageExtension::ObjectiveC);
         }
 
         if (cat.hasCSources()) {
@@ -214,7 +197,7 @@ QVector<ProjectPart::Ptr> ProjectInfoGenerator::createProjectParts(const RawProj
                                         cat.cSources(),
                                         cat.partName("C"),
                                         Language::C,
-                                        ProjectPart::NoExtensions);
+                                        LanguageExtension::None);
         }
 
         if (cat.hasObjcSources()) {
@@ -223,18 +206,19 @@ QVector<ProjectPart::Ptr> ProjectInfoGenerator::createProjectParts(const RawProj
                                         cat.objcSources(),
                                         cat.partName("Obj-C"),
                                         Language::C,
-                                        ProjectPart::ObjectiveCExtensions);
+                                        LanguageExtension::ObjectiveC);
         }
     }
     return result;
 }
 
-ProjectPart::Ptr ProjectInfoGenerator::createProjectPart(const RawProjectPart &rawProjectPart,
-                                                         const ProjectPart::Ptr &templateProjectPart,
-                                                         const ProjectFiles &projectFiles,
-                                                         const QString &partName,
-                                                         Language language,
-                                                         ProjectPart::LanguageExtensions languageExtensions)
+ProjectPart::Ptr ProjectInfoGenerator::createProjectPart(
+    const RawProjectPart &rawProjectPart,
+    const ProjectPart::Ptr &templateProjectPart,
+    const ProjectFiles &projectFiles,
+    const QString &partName,
+    Language language,
+    ProjectExplorer::LanguageExtensions languageExtensions)
 {
     ProjectPart::Ptr part(templateProjectPart->copy());
     part->displayName = partName;

commit db4e7b2224914ca9b4d67f57c404659087779824

ProjectExplorer: Fix detection of OpemMP language extension
Regression introduced by

    commit 6aa11eb75e2374bc1c0c643588b3d06d797759be
    CppTools/ProjectExplorer: Remove enum duplication

Task-number: QTCREATORBUG-21213
Change-Id: I76ab42cf0433f437ca08b4582b91afc43b2f85fd
Reviewed-by: Ivan Donchevskii <ivan.donchevskii@qt.io>

src/plugins/projectexplorer/gcctoolchain.cpp
@@ -489,7 +489,7 @@ LanguageExtensions GccToolChain::languageExtensions(const QStringList &cxxflags)
             else
                 extensions &= ~LanguageExtensions(LanguageExtension::Gnu);
         } else if (flag == "-fopenmp") {
-            extensions |= LanguageExtension::Gnu;
+            extensions |= LanguageExtension::OpenMP;
         } else if (flag == "-fms-extensions") {
             extensions |= LanguageExtension::Microsoft;
         }