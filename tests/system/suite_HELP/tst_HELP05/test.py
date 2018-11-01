0bc120b87c393299940682b8b53bb0bf2575fbeb
Robert Loehning <robert.loehning@qt.io>
2018-11-01 11:54:21 +0100

    Squish: Remove redundant line
    Change-Id: I819ace3fc7c3223c5bcd52377db3e7833371c7ec
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- tests/system/suite_HELP/tst_HELP05/test.py
+++ tests/system/suite_HELP/tst_HELP05/test.py
@@ -34,9 +34,7 @@ def verifyInteractiveQMLHelp(lineText, helpText):
     homeKey = "<Home>"
     if platform.system() == "Darwin":
         homeKey = "<Ctrl+Left>"
-        type(editorArea, homeKey)
-    else:
-        type(editorArea, homeKey)
+    type(editorArea, homeKey)
     snooze(1)
     # call help
     type(editorArea, "<F1>")
