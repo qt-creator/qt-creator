qt-creator/qt-creator

commit b2052561cca185ca18b114110ae401ad9cb43aec

Debugger: improve evaluate expression with cdb
Replace Evaluate with AddSymbol and create a PyValue representing
that expression

Change-Id: Ibb8ab70c41df81e9d7d683b633a070caa03036d3
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

share/qtcreator/debugger/cdbbridge.py
@@ -225,13 +225,7 @@ class Dumper(DumperBase):
         return None
 
     def parseAndEvaluate(self, exp):
-        val = cdbext.parseAndEvaluate(exp)
-        if val is None:
-            return None
-        value = self.Value(self)
-        value.type = self.lookupType('void *')
-        value.ldata = val.to_bytes(8, sys.byteorder)
-        return value
+        return self.fromNativeValue(cdbext.parseAndEvaluate(exp))
 
     def isWindowsTarget(self):
         return True
@@ -316,12 +310,12 @@ class Dumper(DumperBase):
             if namespaceIndex > 0:
                 namespace = name[:namespaceIndex + 2]
         self.qtNamespace = lambda: namespace
-        self.qtCustomEventFunc = cdbext.parseAndEvaluate('%s!%sQObject::customEvent'
-                                                         % (self.qtCoreModuleName(), namespace))
+        self.qtCustomEventFunc = self.parseAndEvaluate('%s!%sQObject::customEvent'
+                                        % (self.qtCoreModuleName(), namespace)).address()
         return namespace
 
     def qtVersion(self):
-        qtVersion = self.findValueByExpression('((void**)&%s)[2]' % self.qtHookDataSymbolName())
+        qtVersion = self.parseAndEvaluate('((void**)&%s)[2]' % self.qtHookDataSymbolName()).integer()
         if qtVersion is None and self.qtCoreModuleName() is not None:
             try:
                 versionValue = cdbext.call(self.qtCoreModuleName() + '!qVersion()')

src/libs/qtcreatorcdbext/pycdbextmodule.cpp
@@ -106,12 +106,13 @@ static PyObject *cdbext_parseAndEvaluate(PyObject *, PyObject *args) // -> Value
     ULONG oldExpressionSyntax;
     control->GetExpressionSyntax(&oldExpressionSyntax);
     control->SetExpressionSyntax(DEBUG_EXPR_CPLUSPLUS);
-    DEBUG_VALUE value;
-    HRESULT hr = control->Evaluate(expr, DEBUG_VALUE_INT64, &value, NULL);
+    IDebugSymbolGroup2 *symbolGroup = CurrentSymbolGroup::get();
+    ULONG index = DEBUG_ANY_ID;
+    HRESULT hr = symbolGroup->AddSymbol(expr, &index);
     control->SetExpressionSyntax(oldExpressionSyntax);
     if (FAILED(hr))
         Py_RETURN_NONE;
-    return Py_BuildValue("K", value.I64);
+    return createPythonObject(PyValue(index, symbolGroup));
 }
 
 static PyObject *cdbext_resolveSymbol(PyObject *, PyObject *args) // -> Value

commit 12f4ff5e5b6fe4a7ca9c5ec2ab4c210d106c2bc1

Debugger: Fix Qt version detection in the cdbbridge
broken by b2052561cca185ca18b114110ae401ad9cb43aec

Fixes: QTCREATORBUG-21446
Change-Id: I303bb943a3b39703a6c5898aab99b89d35d1d2d7
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

share/qtcreator/debugger/cdbbridge.py
@@ -323,15 +323,18 @@ class Dumper(DumperBase):
         return namespace
 
     def qtVersion(self):
-        qtVersion = self.parseAndEvaluate('((void**)&%s)[2]' % self.qtHookDataSymbolName()).integer()
-        if qtVersion is None and self.qtCoreModuleName() is not None:
-            try:
-                versionValue = cdbext.call(self.qtCoreModuleName() + '!qVersion()')
-                version = self.extractCString(self.fromNativeValue(versionValue).address())
-                (major, minor, patch) = version.decode('latin1').split('.')
-                qtVersion = 0x10000 * int(major) + 0x100 * int(minor) + int(patch)
-            except:
-                pass
+        qtVersion = None
+        try:
+            qtVersion = self.parseAndEvaluate('((void**)&%s)[2]' % self.qtHookDataSymbolName()).integer()
+        except:
+            if self.qtCoreModuleName() is not None:
+                try:
+                    versionValue = cdbext.call(self.qtCoreModuleName() + '!qVersion()')
+                    version = self.extractCString(self.fromNativeValue(versionValue).address())
+                    (major, minor, patch) = version.decode('latin1').split('.')
+                    qtVersion = 0x10000 * int(major) + 0x100 * int(minor) + int(patch)
+                except:
+                    pass
         if qtVersion is None:
             qtVersion = self.fallbackQtVersion
         self.qtVersion = lambda: qtVersion