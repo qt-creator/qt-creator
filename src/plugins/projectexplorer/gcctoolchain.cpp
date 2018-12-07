qt-creator/qt-creator

commit 4a02611cd5af785ecdbba4013a51c2788894b58f

Accept --gcctoolchain= argument to GCC-based compilers
Change-Id: I12c9d2e489b210433e9270de22783bbdc5c9544e
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/plugins/projectexplorer/gcctoolchain.cpp
@@ -365,7 +365,9 @@ QByteArray GccToolChain::predefinedMacros(const QStringList &cxxflags) const
     QStringList arguments = gccPredefinedMacrosOptions();
     for (int iArg = 0; iArg < allCxxflags.length(); ++iArg) {
         const QString &a = allCxxflags.at(iArg);
-        if (a == QLatin1String("-arch")) {
+        if (a.startsWith("--gcc-toolchain=")) {
+            arguments << a;
+        } else if (a == QLatin1String("-arch")) {
             if (++iArg < allCxxflags.length() && !arguments.contains(a))
                 arguments << a << allCxxflags.at(iArg);
         } else if (a == QLatin1String("--sysroot") || a == QLatin1String("-isysroot")
@@ -539,7 +541,7 @@ QList<HeaderPath> GccToolChain::systemHeaderPaths(const QStringList &cxxflags, c
         QStringList flags;
         flags << m_platformCodeGenFlags << cxxflags;
         foreach (const QString &a, flags) {
-            if (a.startsWith(QLatin1String("-stdlib=")))
+            if (a.startsWith(QLatin1String("-stdlib=")) || a.startsWith("--gcctoolchain="))
                 arguments << a;
         }
 

commit a87af9412566a9eb2d1ac5263843dca7df693647

ProjectExplorer: Fix typo of --gcc-toolchain
...introduced in

  commit 4a02611cd5af785ecdbba4013a51c2788894b58f
  Accept --gcctoolchain= argument to GCC-based compilers

Change-Id: Ief85a5e29286e9113e6adb8053bd77eeac22ae1f
Reviewed-by: Ivan Donchevskii <ivan.donchevskii@qt.io>

src/plugins/projectexplorer/gcctoolchain.cpp
@@ -545,7 +545,7 @@ QStringList GccToolChain::gccPrepareArguments(const QStringList &flags,
     allFlags << platformCodeGenFlags << flags;
     for (int i = 0; i < allFlags.size(); ++i) {
         const QString &flag = allFlags.at(i);
-        if (flag.startsWith("-stdlib=") || flag.startsWith("--gcctoolchain=")
+        if (flag.startsWith("-stdlib=") || flag.startsWith("--gcc-toolchain=")
             || flag.startsWith("-B") || (flag.startsWith("-isystem") && flag.length() > 8)) {
             arguments << flag;
         } else if (!hasKitSysroot) {