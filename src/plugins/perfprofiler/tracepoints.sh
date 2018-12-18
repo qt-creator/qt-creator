f8cd78482634dd0f173f0d5dab3c95be371fb9ae
Ulf Hermann <ulf.hermann@qt.io>
2018-12-18 18:50:27 +0100

    Weed out inlined functions when defining trace points
    For the allocation functions we only want matched pairs of entry and
    return. Otherwise the memory tracking will get confused. Inlined
    instances of functions don't have return points, and in general we are
    not interested in things that are inlined in libc. We are tracking all
    externally visible functions that return allocated memory after all.
    
    (cherry-picked from tqtc-perfprofiler, because of mismatch between
    imported and original version)
    
    Change-Id: Ibadb07f8c2e6cabeb473cded7fcc30da8ac2d82d
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- src/plugins/perfprofiler/tracepoints.sh
+++ src/plugins/perfprofiler/tracepoints.sh
@@ -38,6 +38,25 @@ register_arguments() {
     return ARGUMENTS
 }
 
+remove_tracepoints() {
+    perf probe -d "perfprofiler_$1_$2*"
+    return 0
+}
+
+match_tracepoints() {
+    MACHINE=$1
+    NAME=$2
+
+    BASE="perfprofiler_${MACHINE}_${NAME}"
+    RETURN=`perf probe -l "${BASE}_ret" | awk '{print $3}'`
+    ENTRY=`echo ${RETURN} | awk '{sub(/%return/, ""); print $1}'`
+    BAD=`perf probe -l "${BASE}*" | awk '{ if ($3 != "'$RETURN'" && $3 != "'$ENTRY'") { print $1 } }'`
+    for PROBE in $BAD; do
+        perf probe -d $PROBE
+    done
+    return 0
+}
+
 set_simple_tracepoint() {
     MACHINE="$1"
     shift
@@ -58,7 +77,6 @@ set_simple_tracepoint() {
     done
 
     FULL_NAME="perfprofiler_${MACHINE}_${NAME}"
-    perf probe -d "${FULL_NAME}"
     perf probe -x "${BINARY}" -a "${FULL_NAME}=${FUNCTION} ${ARGUMENTS}" || return 1
 
     return 0
@@ -68,6 +86,7 @@ set_symbolic_tracepoint() {
     >&2 echo "</pre><h4>Trying symbolic trace point for $4 on $1 ...</h4><pre>"
     set_simple_tracepoint "$1" "$2" "$3" "$4" "$5" "\$params" || return 1
     PARAM=`perf probe -l | awk "/perfprofiler_$1_$3"'/{sub(/\)$/, ""); print $(NF)}'`
+    perf probe -d "perfprofiler_$1_$3"
     set_simple_tracepoint "$1" "$2" "$3" "$4" "$5" "$PARAM" || return 1
     return 0
 }
@@ -105,6 +124,17 @@ HOST_MACHINE=`uname -m`
 find /lib -name libc.so.6 | while read LIBC; do
     echo $LIBC | awk -F '/' '{print $(NF-1)}' | while IFS='-' read MACHINE KERNEL SYSTEM; do
         if [ "$MACHINE" = "lib" ]; then MACHINE=$HOST_MACHINE; fi
+        >&2 echo "</pre><h3>Removing old trace points for $MACHINE</h3><pre>"
+        remove_tracepoints "$MACHINE" "malloc"
+        remove_tracepoints "$MACHINE" "free"
+        remove_tracepoints "$MACHINE" "cfree"
+        remove_tracepoints "$MACHINE" "realloc"
+        remove_tracepoints "$MACHINE" "calloc"
+        remove_tracepoints "$MACHINE" "valloc"
+        remove_tracepoints "$MACHINE" "pvalloc"
+        remove_tracepoints "$MACHINE" "memalign"
+        remove_tracepoints "$MACHINE" "aligned_alloc"
+
         >&2 echo "</pre><h3>Adding trace points for $MACHINE</h3><pre>"
         set_tracepoint "$MACHINE" "$LIBC" "malloc" "malloc" "requested_amount" && \
             set_tracepoint "$MACHINE" "$LIBC" "malloc_ret" "malloc%return" "\$retval" "obtained_id" && \
@@ -121,11 +151,20 @@ find /lib -name libc.so.6 | while read LIBC; do
             set_tracepoint "$MACHINE" "$LIBC" "memalign" "memalign" "align requested_amount" && \
             set_tracepoint "$MACHINE" "$LIBC" "memalign_ret" "memalign%return" "\$retval" "obtained_id" && \
             echo $MACHINE || \
-            >&2 echo "</pre>!!! Failed to create trace points for $MACHINE !!!<pre>"
+            >&2 echo "</pre>!!! Failed to create some trace points for $MACHINE !!!<pre>"
 
         # aligned_alloc is conditional on C11 and may not be available
         set_tracepoint "$MACHINE" "$LIBC" "aligned_alloc" "aligned_alloc" "align requested_amount" && \
             set_tracepoint "$MACHINE" "$LIBC" "aligned_alloc_ret" "aligned_alloc%return" "\$retval" "obtained_id"
+
+        >&2 echo "</pre><h3>Removing inlined mallocs for $MACHINE</h3><pre>"
+        match_tracepoints "$MACHINE" "malloc"
+        match_tracepoints "$MACHINE" "realloc"
+        match_tracepoints "$MACHINE" "calloc"
+        match_tracepoints "$MACHINE" "valloc"
+        match_tracepoints "$MACHINE" "pvalloc"
+        match_tracepoints "$MACHINE" "memalign"
+        match_tracepoints "$MACHINE" "aligned_alloc"
     done
 done
 
