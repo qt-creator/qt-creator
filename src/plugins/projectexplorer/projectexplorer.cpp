b1ced4a87d80b1c92ad1e9b05789ca38de0f9d25
hjk <hjk@qt.io>
2019-01-07 12:52:30 +0100

    ProjectExplorer: Persist "Automatically create runconfigs" setting
    Task-number: QTCREATORBUG-21796
    Change-Id: I2875bf96c6028409235c6236321d6e2884967cca
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- src/plugins/projectexplorer/projectexplorer.cpp
+++ src/plugins/projectexplorer/projectexplorer.cpp
@@ -1273,6 +1273,8 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
             s->value(QLatin1String("ProjectExplorer/Settings/AddLibraryPathsToRunEnv"), true).toBool();
     dd->m_projectExplorerSettings.prompToStopRunControl =
             s->value(QLatin1String("ProjectExplorer/Settings/PromptToStopRunControl"), false).toBool();
+    dd->m_projectExplorerSettings.automaticallyCreateRunConfigurations =
+            s->value(QLatin1String("ProjectExplorer/Settings/AutomaticallyCreateRunConfigurations"), true).toBool();
     dd->m_projectExplorerSettings.maxAppOutputChars =
             s->value(QLatin1String("ProjectExplorer/Settings/MaxAppOutputLines"),
                      Core::Constants::DEFAULT_MAX_CHAR_COUNT).toInt() * 100;
@@ -1801,6 +1803,8 @@ void ProjectExplorerPluginPrivate::savePersistentSettings()
     s->setValue(QLatin1String("ProjectExplorer/Settings/AutoRestoreLastSession"), dd->m_projectExplorerSettings.autorestoreLastSession);
     s->setValue(QLatin1String("ProjectExplorer/Settings/AddLibraryPathsToRunEnv"), dd->m_projectExplorerSettings.addLibraryPathsToRunEnv);
     s->setValue(QLatin1String("ProjectExplorer/Settings/PromptToStopRunControl"), dd->m_projectExplorerSettings.prompToStopRunControl);
+    s->setValue(QLatin1String("ProjectExplorer/Settings/AutomaticallyCreateRunConfigurations"),
+                dd->m_projectExplorerSettings.automaticallyCreateRunConfigurations);
     s->setValue(QLatin1String("ProjectExplorer/Settings/MaxAppOutputLines"), dd->m_projectExplorerSettings.maxAppOutputChars / 100);
     s->setValue(QLatin1String("ProjectExplorer/Settings/MaxBuildOutputLines"), dd->m_projectExplorerSettings.maxBuildOutputChars / 100);
     s->setValue(QLatin1String("ProjectExplorer/Settings/EnvironmentId"), dd->m_projectExplorerSettings.environmentId.toByteArray());
