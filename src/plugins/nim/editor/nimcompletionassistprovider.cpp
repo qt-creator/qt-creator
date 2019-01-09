qt-creator/qt-creator

commit 4f6ce9d61494593a8ab90093d832dcaa8a1c76cf

Nim: Add code completion with NimSuggest
This patch add basic support for completion by using
the NimSuggest executable.
The user is expected to configure the path to NimSuggest
by using the new tools configuration page in the Nim
settings.
For now we don't support advanced completion of function
parameters. This will be added in further patches.

Change-Id: I5097ee2b3992486071591109c400d9c84da5fc00
Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>
Reviewed-by: hjk <hjk@qt.io>
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/nim/editor/nimcompletionassistprovider.cpp
@@ -1,0 +1,287 @@
+/****************************************************************************
+**
+** Copyright (C) Filippo Cucchetto <filippocucchetto@gmail.com>
+** Contact: http://www.qt.io/licensing
+**
+** This file is part of Qt Creator.
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+****************************************************************************/
+
+#include "nimcompletionassistprovider.h"
+#include "suggest/nimsuggestcache.h"
+#include "suggest/nimsuggest.h"
+
+#include <projectexplorer/session.h>
+#include <texteditor/codeassist/assistinterface.h>
+#include <texteditor/codeassist/assistproposalitem.h>
+#include <texteditor/codeassist/genericproposal.h>
+#include <texteditor/codeassist/iassistprocessor.h>
+#include <utils/qtcassert.h>
+#include <utils/utilsicons.h>
+#include <utils/textutils.h>
+#include <utils/algorithm.h>
+
+#include <QApplication>
+#include <QDebug>
+#include <QSet>
+#include <QTemporaryFile>
+#include <QTextStream>
+#include <QTextDocument>
+#include <QThread>
+
+using namespace ProjectExplorer;
+using namespace TextEditor;
+
+namespace Nim {
+
+bool isIdentifierChar(QChar c)
+{
+    return c.isLetterOrNumber() || c == QLatin1Char('_');
+}
+
+bool isActivationChar(QChar c)
+{
+    static const QSet<QChar> chars {QLatin1Char('.'), QLatin1Char('(')};
+    return chars.contains(c);
+}
+
+class NimCompletionAssistProcessor : public QObject, public TextEditor::IAssistProcessor
+{
+    Q_OBJECT
+
+public:
+    TextEditor::IAssistProposal *perform(const TextEditor::AssistInterface *interface) final
+    {
+        QTC_ASSERT(this->thread() == qApp->thread(), return nullptr);
+
+        if (interface->reason() == IdleEditor && !acceptsIdleEditor(interface))
+            return nullptr;
+
+        Suggest::NimSuggest *suggest = nimSuggestInstance(interface);
+        QTC_ASSERT(suggest, return nullptr);
+
+        if (suggest->executablePath().isEmpty() || suggest->projectFile().isEmpty())
+            return nullptr;
+
+        if (!suggest->isReady()) {
+            m_interface = interface;
+            QObject::connect(suggest, &Suggest::NimSuggest::readyChanged,
+                             this, &NimCompletionAssistProcessor::onNimSuggestReady);
+        } else {
+            doPerform(interface, suggest);
+        }
+
+        m_running = true;
+        return nullptr;
+    }
+
+    bool running()
+    {
+        return m_running;
+    }
+
+private:
+    void onNimSuggestReady(bool ready)
+    {
+        auto suggest = dynamic_cast<Suggest::NimSuggest *>(sender());
+        QTC_ASSERT(suggest, return);
+        QTC_ASSERT(m_interface, return);
+
+        if (!ready || !suggest) {
+            m_running = false;
+            setAsyncProposalAvailable(nullptr);
+        } else {
+            doPerform(m_interface, suggest);
+        }
+    }
+
+    void doPerform(const TextEditor::AssistInterface *interface,
+                   Suggest::NimSuggest *suggest)
+    {
+        int pos = findCompletionPos(interface);
+
+        std::unique_ptr<QTemporaryFile> dirtyFile = writeDirtyFile(interface);
+        QTC_ASSERT(dirtyFile, return);
+
+        std::shared_ptr<Suggest::SugRequest> request = sendRequest(interface,
+                                                                   suggest,
+                                                                   dirtyFile->fileName(),
+                                                                   pos);
+        QTC_ASSERT(request, return);
+        connect(request.get(), &Suggest::SugRequest::finished, this,
+                &NimCompletionAssistProcessor::onRequestFinished);
+
+        m_pos = pos;
+        m_dirtyFile = std::move(dirtyFile);
+        m_request = std::move(request);
+    }
+
+    void onRequestFinished()
+    {
+        auto items = Utils::transform<QList>(m_request->lines(), &createProposal);
+        setAsyncProposalAvailable(new GenericProposal(m_pos, items));
+        m_running = false;
+        m_dirtyFile.reset();
+        m_request.reset();
+    }
+
+    static bool acceptsIdleEditor(const AssistInterface *interface)
+    {
+        int pos = interface->position();
+        QChar c = interface->textDocument()->characterAt(pos - 1);
+        return isIdentifierChar(c) || isActivationChar(c);
+    }
+
+    static int findCompletionPos(const AssistInterface *interface)
+    {
+        int pos = interface->position();
+        while (isIdentifierChar(interface->textDocument()->characterAt(pos - 1)))
+            pos--;
+        return pos;
+    }
+
+    static Suggest::NimSuggest *nimSuggestInstance(const AssistInterface *interface)
+    {
+        auto filename = Utils::FileName::fromString(interface->fileName());
+        return Nim::Suggest::NimSuggestCache::instance().get(filename);
+    }
+
+    static std::shared_ptr<Suggest::SugRequest> sendRequest(const AssistInterface *interface,
+                                                            Suggest::NimSuggest *suggest,
+                                                            QString dirtyFile,
+                                                            int pos)
+    {
+        int line = 0, column = 0;
+        Utils::Text::convertPosition(interface->textDocument(), pos, &line, &column);
+        QTC_ASSERT(column >= 1, return nullptr);
+        auto filename = Utils::FileName::fromString(interface->fileName());
+        return suggest->sug(filename.toString(), line, column - 1, dirtyFile);
+    }
+
+    static std::unique_ptr<QTemporaryFile> writeDirtyFile(const TextEditor::AssistInterface *interface)
+    {
+        auto result = std::make_unique<QTemporaryFile>("qtcnim.XXXXXX.nim");
+        QTC_ASSERT(result->open(), return nullptr);
+        QTextStream stream(result.get());
+        stream << interface->textDocument()->toPlainText();
+        result->close();
+        return result;
+    }
+
+    static AssistProposalItemInterface *createProposal(const Nim::Suggest::Line &line)
+    {
+        auto item = new AssistProposalItem();
+        item->setIcon(Utils::CodeModelIcon::iconForType(symbolIcon(line.symbol_kind)));
+        item->setText(line.data.back());
+        item->setDetail(line.symbol_type);
+        item->setOrder(symbolOrder(line.symbol_kind));
+        return item;
+    }
+
+    static int symbolOrder(Suggest::Line::SymbolKind kind)
+    {
+        switch (kind) {
+        case Suggest::Line::SymbolKind::skField:
+            return 2;
+        case Suggest::Line::SymbolKind::skVar:
+        case Suggest::Line::SymbolKind::skLet:
+        case Suggest::Line::SymbolKind::skEnumField:
+        case Suggest::Line::SymbolKind::skResult:
+        case Suggest::Line::SymbolKind::skForVar:
+        case Suggest::Line::SymbolKind::skParam:
+        case Suggest::Line::SymbolKind::skLabel:
+        case Suggest::Line::SymbolKind::skGenericParam:
+            return 1;
+        default:
+            return 0;
+        }
+    }
+
+    static Utils::CodeModelIcon::Type symbolIcon(Suggest::Line::SymbolKind kind)
+    {
+        switch (kind) {
+        case Suggest::Line::SymbolKind::skField:
+            return Utils::CodeModelIcon::Property;
+        case Suggest::Line::SymbolKind::skVar:
+        case Suggest::Line::SymbolKind::skLet:
+        case Suggest::Line::SymbolKind::skEnumField:
+        case Suggest::Line::SymbolKind::skResult:
+        case Suggest::Line::SymbolKind::skForVar:
+        case Suggest::Line::SymbolKind::skParam:
+        case Suggest::Line::SymbolKind::skLabel:
+        case Suggest::Line::SymbolKind::skGenericParam:
+            return Utils::CodeModelIcon::VarPublic;
+        case Suggest::Line::SymbolKind::skProc:
+        case Suggest::Line::SymbolKind::skFunc:
+        case Suggest::Line::SymbolKind::skMethod:
+        case Suggest::Line::SymbolKind::skIterator:
+        case Suggest::Line::SymbolKind::skConverter:
+            return Utils::CodeModelIcon::FuncPublic;
+        case Suggest::Line::SymbolKind::skType:
+            return Utils::CodeModelIcon::Struct;
+        case Suggest::Line::SymbolKind::skMacro:
+        case Suggest::Line::SymbolKind::skTemplate:
+            return Utils::CodeModelIcon::Macro;
+        case Suggest::Line::SymbolKind::skModule:
+        case Suggest::Line::SymbolKind::skPackage:
+            return Utils::CodeModelIcon::Namespace;
+        case Suggest::Line::SymbolKind::skUnknown:
+        case Suggest::Line::SymbolKind::skConditional:
+        case Suggest::Line::SymbolKind::skDynLib:
+        case Suggest::Line::SymbolKind::skTemp:
+        case Suggest::Line::SymbolKind::skConst:
+        case Suggest::Line::SymbolKind::skStub:
+        case Suggest::Line::SymbolKind::skAlias:
+        default:
+            return Utils::CodeModelIcon::Type::Unknown;
+        }
+    }
+
+    bool m_running = false;
+    int m_pos = -1;
+    std::weak_ptr<Suggest::NimSuggest> m_suggest;
+    std::shared_ptr<Suggest::SugRequest> m_request;
+    std::unique_ptr<QTemporaryFile> m_dirtyFile;
+    const TextEditor::AssistInterface *m_interface = nullptr;
+};
+
+
+TextEditor::IAssistProcessor *NimCompletionAssistProvider::createProcessor() const
+{
+    return new NimCompletionAssistProcessor();
+}
+
+int NimCompletionAssistProvider::activationCharSequenceLength() const
+{
+    return 1;
+}
+
+bool NimCompletionAssistProvider::isActivationCharSequence(const QString &sequence) const
+{
+    return sequence.size() && isActivationChar(sequence.front());
+}
+
+IAssistProvider::RunType NimCompletionAssistProvider::runType() const
+{
+    return RunType::Asynchronous;
+}
+
+}
+
+#include "nimcompletionassistprovider.moc"

src/plugins/nim/editor/nimcompletionassistprovider.h
@@ -25,39 +25,19 @@
 
 #pragma once
 
-#include <QObject>
-
-namespace TextEditor { class SimpleCodeStylePreferences; }
+#include <texteditor/codeassist/completionassistprovider.h>
 
 namespace Nim {
 
-class NimSettings : public QObject
+class NimCompletionAssistProvider : public TextEditor::CompletionAssistProvider
 {
     Q_OBJECT
 
 public:
-    NimSettings(QObject *parent = nullptr);
-    ~NimSettings() override;
-
-    QString nimSuggestPath() const;
-    void setNimSuggestPath(const QString &path);
-
-    void save();
-
-    static TextEditor::SimpleCodeStylePreferences *globalCodeStyle();
-
-signals:
-    void nimSuggestPathChanged(QString path);
-
-private:
-    void InitializeCodeStyleSettings();
-    void TerminateCodeStyleSettings();
-
-    void InitializeNimSuggestSettings();
-    void TerminateNimSuggestSettings();
-
-    QString m_nimSuggestPath;
+    TextEditor::IAssistProcessor *createProcessor() const final;
+    int activationCharSequenceLength() const final;
+    bool isActivationCharSequence(const QString &sequence) const final;
+    RunType runType() const final;
 };
 
-} // namespace Nim
-
+}

src/plugins/nim/editor/nimeditorfactory.cpp
@@ -26,6 +26,7 @@
 #include "nimeditorfactory.h"
 #include "nimindenter.h"
 #include "nimhighlighter.h"
+#include "nimcompletionassistprovider.h"
 
 #include "../nimconstants.h"
 #include "../nimplugin.h"
@@ -65,6 +66,7 @@ NimEditorFactory::NimEditorFactory()
     setSyntaxHighlighterCreator([]() {
         return new NimHighlighter;
     });
+    setCompletionAssistProvider(new NimCompletionAssistProvider());
     setCommentDefinition(CommentDefinition::HashStyle);
     setParenthesesMatchingEnabled(true);
     setCodeFoldingSupported(true);

src/plugins/nim/nim.pro
@@ -11,6 +11,7 @@ INCLUDEPATH += $$PWD
 HEADERS += \
     nimplugin.h \
     nimconstants.h \
+    editor/nimcompletionassistprovider.h \
     editor/nimhighlighter.h \
     editor/nimindenter.h \
     tools/nimlexer.h \
@@ -29,11 +30,20 @@ HEADERS += \
     settings/nimcodestylepreferencesfactory.h \
     settings/nimsettings.h \
     settings/nimcodestylepreferenceswidget.h \
+    settings/nimtoolssettingspage.h \
     project/nimtoolchain.h \
-    project/nimtoolchainfactory.h
+    project/nimtoolchainfactory.h \
+    suggest/client.h \
+    suggest/clientrequests.h \
+    suggest/nimsuggest.h \
+    suggest/nimsuggestcache.h \
+    suggest/server.h \
+    suggest/sexprlexer.h \
+    suggest/sexprparser.h
 
 SOURCES += \
     nimplugin.cpp \
+    editor/nimcompletionassistprovider.cpp \
     editor/nimhighlighter.cpp \
     editor/nimindenter.cpp \
     tools/nimlexer.cpp \
@@ -51,10 +61,17 @@ SOURCES += \
     settings/nimcodestylepreferencesfactory.cpp \
     settings/nimsettings.cpp \
     settings/nimcodestylepreferenceswidget.cpp \
+    settings/nimtoolssettingspage.cpp \
     project/nimtoolchain.cpp \
-    project/nimtoolchainfactory.cpp
+    project/nimtoolchainfactory.cpp \
+    suggest/client.cpp \
+    suggest/clientrequests.cpp \
+    suggest/nimsuggest.cpp \
+    suggest/nimsuggestcache.cpp \
+    suggest/server.cpp
 
 FORMS += \
     project/nimcompilerbuildstepconfigwidget.ui \
     project/nimcompilercleanstepconfigwidget.ui \
-    settings/nimcodestylepreferenceswidget.ui
+    settings/nimcodestylepreferenceswidget.ui \
+    settings/nimtoolssettingswidget.ui

src/plugins/nim/nim.qbs
@@ -27,7 +27,8 @@ QtcPlugin {
         files: [
             "nimeditorfactory.h", "nimeditorfactory.cpp",
             "nimhighlighter.h", "nimhighlighter.cpp",
-            "nimindenter.h", "nimindenter.cpp"
+            "nimindenter.h", "nimindenter.cpp",
+            "nimcompletionassistprovider.h", "nimcompletionassistprovider.cpp"
         ]
     }
 
@@ -57,6 +58,7 @@ QtcPlugin {
             "nimcodestylepreferenceswidget.h", "nimcodestylepreferenceswidget.cpp", "nimcodestylepreferenceswidget.ui",
             "nimcodestylesettingspage.h", "nimcodestylesettingspage.cpp",
             "nimsettings.h", "nimsettings.cpp",
+            "nimtoolssettingspage.h", "nimtoolssettingspage.cpp", "nimtoolssettingswidget.ui"
         ]
     }
 
@@ -66,6 +68,20 @@ QtcPlugin {
         files: [
             "nimlexer.h", "nimlexer.cpp",
             "sourcecodestream.h"
+        ]
+    }
+
+    Group {
+        name: "Suggest"
+        prefix: "suggest/"
+        files: [
+            "client.h", "client.cpp",
+            "clientrequests.h", "clientrequests.cpp",
+            "nimsuggest.h", "nimsuggest.cpp",
+            "nimsuggestcache.h", "nimsuggestcache.cpp",
+            "server.h", "server.cpp",
+            "sexprlexer.h",
+            "sexprparser.h",
         ]
     }
 }

src/plugins/nim/nimconstants.h
@@ -77,6 +77,12 @@ const char C_NIMCODESTYLESETTINGSPAGE_ID[] = "Nim.NimCodeStyleSettings";
 const char C_NIMCODESTYLESETTINGSPAGE_DISPLAY[] = QT_TRANSLATE_NOOP("NimCodeStyleSettingsPage", "Code Style");
 const char C_NIMCODESTYLESETTINGSPAGE_CATEGORY[] = "Z.Nim";
 const char C_NIMCODESTYLESETTINGSPAGE_CATEGORY_DISPLAY[] = QT_TRANSLATE_NOOP("NimCodeStyleSettingsPage", "Nim");
+
+const char C_NIMTOOLSSETTINGSPAGE_ID[] = "Nim.NimToolsSettings";
+const char C_NIMTOOLSSETTINGSPAGE_DISPLAY[] = QT_TRANSLATE_NOOP("NimToolsSettingsPage", "Tools");
+const char C_NIMTOOLSSETTINGSPAGE_CATEGORY[] = "Z.Nim";
+const char C_NIMTOOLSSETTINGSPAGE_CATEGORY_DISPLAY[] = QT_TRANSLATE_NOOP("NimToolsSettingsPage", "Nim");
+
 const char C_NIMLANGUAGE_NAME[] = QT_TRANSLATE_NOOP("NimCodeStylePreferencesFactory", "Nim");
 const char C_NIMGLOBALCODESTYLE_ID[] = "NimGlobal";
 const QString C_NIMSNIPPETSGROUP_ID = QStringLiteral("Nim.NimSnippetsGroup");
@@ -102,6 +108,10 @@ const char C_NIM_MIMETYPE[] = "text/x-nim";
 const char C_NIM_SCRIPT_MIMETYPE[] = "text/x-nim-script";
 const char C_NIM_MIME_ICON[] = "text-x-nim";
 const char C_NIM_PROJECT_MIMETYPE[] = "text/x-nim-project";
+
+const char C_NIM_SETTINGS_GROUP[] = "Nim";
+const char C_NIM_SETTINGS_NIMSUGGEST_GROUP[] = "NimSuggest";
+const char C_NIM_SETTINGS_COMMAND[] = "Command";
 
 }
 }

src/plugins/nim/nimplugin.cpp
@@ -36,7 +36,9 @@
 #include "project/nimtoolchainfactory.h"
 #include "settings/nimcodestylepreferencesfactory.h"
 #include "settings/nimcodestylesettingspage.h"
+#include "settings/nimtoolssettingspage.h"
 #include "settings/nimsettings.h"
+#include "suggest/nimsuggestcache.h"
 
 #include <coreplugin/fileiconprovider.h>
 #include <projectexplorer/projectmanager.h>
@@ -51,6 +53,15 @@ namespace Nim {
 class NimPluginPrivate
 {
 public:
+    NimPluginPrivate()
+        : toolsSettingsPage(&settings)
+    {
+        Suggest::NimSuggestCache::instance().setExecutablePath(settings.nimSuggestPath());
+        QObject::connect(&settings, &NimSettings::nimSuggestPathChanged,
+                         &Suggest::NimSuggestCache::instance(),
+                         &Suggest::NimSuggestCache::setExecutablePath);
+    }
+
     NimSettings settings;
     NimEditorFactory editorFactory;
     NimBuildConfigurationFactory buildConfigFactory;
@@ -58,6 +69,7 @@ public:
     NimCompilerBuildStepFactory buildStepFactory;
     NimCompilerCleanStepFactory cleanStepFactory;
     NimCodeStyleSettingsPage codeStyleSettingsPage;
+    NimToolsSettingsPage toolsSettingsPage;
     NimCodeStylePreferencesFactory codeStylePreferencesPage;
     NimToolChainFactory toolChainFactory;
 };
@@ -89,7 +101,8 @@ void NimPlugin::extensionsInitialized()
 {
     // Add MIME overlay icons (these icons displayed at Project dock panel)
     const QIcon icon = Utils::Icon({{":/nim/images/settingscategory_nim.png",
-                                     Utils::Theme::PanelTextColorDark}}, Utils::Icon::Tint).icon();
+            Utils::Theme::PanelTextColorDark
+        }}, Utils::Icon::Tint).icon();
     if (!icon.isNull()) {
         Core::FileIconProvider::registerIconOverlayForMimeType(icon, Constants::C_NIM_MIMETYPE);
         Core::FileIconProvider::registerIconOverlayForMimeType(icon, Constants::C_NIM_SCRIPT_MIMETYPE);

src/plugins/nim/project/nimproject.cpp
@@ -47,6 +47,9 @@
 #include <utils/qtcassert.h>
 #include <utils/runextensions.h>
 
+#include <coreplugin/editormanager/editormanager.h>
+#include <coreplugin/editormanager/ieditor.h>
+
 #include <QFileInfo>
 #include <QQueue>
 
@@ -68,7 +71,8 @@ NimProject::NimProject(const FileName &fileName) : Project(Constants::C_NIM_MIME
     connect(&m_projectScanTimer, &QTimer::timeout, this, &NimProject::collectProjectFiles);
     connect(this, &Project::settingsLoaded, this, &NimProject::collectProjectFiles);
 
-    connect(&m_futureWatcher, &QFutureWatcher<QList<FileNode *>>::finished, this, &NimProject::updateProject);
+    connect(&m_futureWatcher, &QFutureWatcher<QList<FileNode *>>::finished, this,
+            &NimProject::updateProject);
 }
 
 void NimProject::scheduleProjectScan()
@@ -86,7 +90,9 @@ void NimProject::scheduleProjectScan()
 
 bool NimProject::addFiles(const QStringList &filePaths)
 {
-    m_excludedFiles = Utils::filtered(m_excludedFiles, [&](const QString &f) { return !filePaths.contains(f); });
+    m_excludedFiles = Utils::filtered(m_excludedFiles, [&](const QString & f) {
+        return !filePaths.contains(f);
+    });
     scheduleProjectScan();
     return true;
 }
@@ -112,8 +118,9 @@ void NimProject::collectProjectFiles()
     m_lastProjectScan.start();
     QTC_ASSERT(!m_futureWatcher.future().isRunning(), return);
     FileName prjDir = projectDirectory();
-    QFuture<QList<ProjectExplorer::FileNode *>> future = Utils::runAsync([prjDir, excluded = m_excludedFiles] {
-        return FileNode::scanForFiles(prjDir, [excluded](const FileName &fn) -> FileNode * {
+    QFuture<QList<ProjectExplorer::FileNode *>> future = Utils::runAsync([prjDir,
+    excluded = m_excludedFiles] {
+        return FileNode::scanForFiles(prjDir, [excluded](const FileName & fn) -> FileNode * {
             const QString fileName = fn.fileName();
             if (excluded.contains(fn.toString())
                     || fileName.endsWith(".nimproject", HostOsInfo::fileNameCaseSensitivity())
@@ -132,7 +139,9 @@ void NimProject::updateProject()
 
     auto newRoot = std::make_unique<NimProjectNode>(*this, projectDirectory());
 
-    for (FileNode *node : m_futureWatcher.future().result())
+    QList<FileNode *> files = m_futureWatcher.future().result();
+
+    for (FileNode *node : files)
         newRoot->addNestedNode(std::unique_ptr<FileNode>(node));
 
     newRoot->setDisplayName(displayName());
@@ -144,7 +153,8 @@ void NimProject::updateProject()
 QList<Task> NimProject::projectIssues(const Kit *k) const
 {
     QList<Task> result = Project::projectIssues(k);
-    auto tc = dynamic_cast<NimToolChain*>(ToolChainKitInformation::toolChain(k, Constants::C_NIMLANGUAGE_ID));
+    auto tc = dynamic_cast<NimToolChain *>(ToolChainKitInformation::toolChain(k,
+                                                                              Constants::C_NIMLANGUAGE_ID));
     if (!tc) {
         result.append(createProjectTask(Task::TaskType::Error, tr("No Nim compiler set.")));
         return result;
@@ -157,7 +167,9 @@ QList<Task> NimProject::projectIssues(const Kit *k) const
 
 FileNameList NimProject::nimFiles() const
 {
-    return files([](const ProjectExplorer::Node *n) { return AllFiles(n) && n->filePath().endsWith(".nim"); });
+    return files([](const ProjectExplorer::Node *n) {
+        return AllFiles(n) && n->filePath().endsWith(".nim");
+    });
 }
 
 QVariantMap NimProject::toMap() const

commit 327fed3d194d4a43b77a8bda301ce47c1fe48f70

Nim: Fix compile for Qt 5.9
Broke with 4f6ce9d6149.

Change-Id: I25f65ab3e1fc3e6a85a8fe589c7711bae4428347
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/nim/editor/nimcompletionassistprovider.cpp
@@ -274,7 +274,7 @@ int NimCompletionAssistProvider::activationCharSequenceLength() const
 
 bool NimCompletionAssistProvider::isActivationCharSequence(const QString &sequence) const
 {
-    return sequence.size() && isActivationChar(sequence.front());
+    return !sequence.isEmpty() && isActivationChar(sequence.at(0));
 }
 
 IAssistProvider::RunType NimCompletionAssistProvider::runType() const