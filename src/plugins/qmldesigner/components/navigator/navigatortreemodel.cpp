qt-creator/qt-creator

commit 1e711d50dc4c3f648ec2cd042dd718edb5c55f2d

QmlDesigner: Fix notifier for navigator
We have to update the complete row, since column 1 contains
the alias indicator and column 2 contains the visibilty indicator.

Task-number: QDS-274
Change-Id: Ib1688b5bceab144f9cd8ff3a784dc1efa6d4ea54
Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

src/plugins/qmldesigner/components/navigator/navigatortreemodel.cpp
@@ -600,7 +600,7 @@ bool NavigatorTreeModel::setData(const QModelIndex &index, const QVariant &value
 void NavigatorTreeModel::notifyDataChanged(const ModelNode &modelNode)
 {
     const QModelIndex index = indexForModelNode(modelNode);
-    dataChanged(index, index);
+    dataChanged(index, index.siblingAtColumn(2));
 }
 
 static QList<ModelNode> collectParents(const QList<ModelNode> &modelNodes)

commit 6439b5f52e5b89e6c99b18cadd308e3d627b0eba

QmlDesigner: Fix compile Qt 5.9 and 5.10
Broke with 1e711d50dc.

Change-Id: I6a4c192d4c924b9a63801b5802aeb17681b2608e
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/plugins/qmldesigner/components/navigator/navigatortreemodel.cpp
@@ -600,7 +600,9 @@ bool NavigatorTreeModel::setData(const QModelIndex &index, const QVariant &value
 void NavigatorTreeModel::notifyDataChanged(const ModelNode &modelNode)
 {
     const QModelIndex index = indexForModelNode(modelNode);
-    dataChanged(index, index.siblingAtColumn(2));
+    const QAbstractItemModel *model = index.model();
+    const QModelIndex sibling = model ? model->sibling(index.row(), 2, index) : QModelIndex();
+    dataChanged(index, sibling);
 }
 
 static QList<ModelNode> collectParents(const QList<ModelNode> &modelNodes)