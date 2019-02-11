6ae786bcc8e317c9a8faa2354b2e9a1fe1cc4db6
Christian Stenger <christian.stenger@qt.io>
2019-02-11 13:46:20 +0100

    TextEditor: Fix build for Qt5.9
    Do not use functions that are not supported with
    the minimum supported Qt for building QC.
    Partially reverts 963dc84cc5d1.
    
    Change-Id: Ife03143a7cf5a8f428754040e7004efe42d70a8a
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/plugins/texteditor/texteditor.cpp
+++ src/plugins/texteditor/texteditor.cpp
@@ -8252,7 +8252,11 @@ void TextEditorWidgetPrivate::updateTabStops()
     // to be set as an int. A work around is to access directly the QTextOption.
     qreal charWidth = QFontMetricsF(q->font()).width(QLatin1Char(' '));
     QTextOption option = q->document()->defaultTextOption();
+#if QT_VERSION >= QT_VERSION_CHECK(5, 10, 0)
     option.setTabStopDistance(charWidth * m_document->tabSettings().m_tabSize);
+#else
+    option.setTabStop(charWidth * m_document->tabSettings().m_tabSize);
+#endif
     q->document()->setDefaultTextOption(option);
 }
 
