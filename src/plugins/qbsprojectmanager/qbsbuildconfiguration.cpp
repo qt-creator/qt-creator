qt-creator/qt-creator

commit a43331ba67f4fa33b60338ac04a2dc4a455bb253

Move "builddirectory" template from Core to ProjectExplorer
Move Core::DocumentManager::buildDirectory to
ProjectExplorerPlugin::buildDirectoryTemplate.

Move the setter along.

Change-Id: I3f1739723e800d04d2934149369b8881208305b4
Reviewed-by: hjk <hjk@qt.io>

src/plugins/qbsprojectmanager/qbsbuildconfiguration.cpp
@@ -66,7 +66,7 @@ static FileName defaultBuildDirectory(const QString &projectFilePath, const Kit 
     const QString projectName = QFileInfo(projectFilePath).completeBaseName();
     ProjectMacroExpander expander(projectFilePath, projectName, k, bcName, buildType);
     QString projectDir = Project::projectDirectory(FileName::fromString(projectFilePath)).toString();
-    QString buildPath = expander.expand(Core::DocumentManager::buildDirectory());
+    QString buildPath = expander.expand(ProjectExplorerPlugin::defaultBuildDirectory());
     return FileName::fromString(FileUtils::resolvePath(projectDir, buildPath));
 }
 

src/plugins/qbsprojectmanager/qbsprojectimporter.cpp
@@ -93,7 +93,7 @@ static QString buildDir(const QString &projectFilePath, const Kit *k)
                                   BuildConfiguration::Unknown);
     const QString projectDir
             = Project::projectDirectory(FileName::fromString(projectFilePath)).toString();
-    const QString buildPath = expander.expand(Core::DocumentManager::buildDirectory());
+    const QString buildPath = expander.expand(ProjectExplorerPlugin::defaultBuildDirectory());
     return FileUtils::resolvePath(projectDir, buildPath);
 }
 

src/plugins/cmakeprojectmanager/cmakebuildconfiguration.cpp
@@ -35,7 +35,6 @@
 #include "cmakeprojectmanager.h"
 #include "cmakeprojectnodes.h"
 
-#include <coreplugin/documentmanager.h>
 #include <coreplugin/icore.h>
 
 #include <projectexplorer/buildmanager.h>
@@ -160,7 +159,7 @@ FileName CMakeBuildConfiguration::shadowBuildDirectory(const FileName &projectFi
     const QString projectName = projectFilePath.parentDir().fileName();
     ProjectMacroExpander expander(projectFilePath.toString(), projectName, k, bcName, buildType);
     QDir projectDir = QDir(Project::projectDirectory(projectFilePath).toString());
-    QString buildPath = expander.expand(Core::DocumentManager::buildDirectory());
+    QString buildPath = expander.expand(ProjectExplorerPlugin::buildDirectoryTemplate());
     return FileName::fromUserInput(projectDir.absoluteFilePath(buildPath));
 }
 

src/plugins/coreplugin/coreconstants.h
@@ -209,8 +209,6 @@ const char DEFAULT_THEME[] = "flat";
 
 const char TR_CLEAR_MENU[]         = QT_TRANSLATE_NOOP("Core", "Clear Menu");
 
-const char DEFAULT_BUILD_DIRECTORY[] = "../%{JS: Util.asciify(\"build-%{CurrentProject:Name}-%{CurrentKit:FileSystemName}-%{CurrentBuild:Name}\")}";
-
 const int MODEBAR_ICON_SIZE = 34;
 const int MODEBAR_ICONSONLY_BUTTON_SIZE = MODEBAR_ICON_SIZE + 4;
 const int DEFAULT_MAX_CHAR_COUNT = 10000000;

src/plugins/coreplugin/documentmanager.cpp
@@ -105,7 +105,6 @@ static const char editorsKeyC[] = "EditorIds";
 static const char directoryGroupC[] = "Directories";
 static const char projectDirectoryKeyC[] = "Projects";
 static const char useProjectDirectoryKeyC[] = "UseProjectsDirectory";
-static const char buildDirectoryKeyC[] = "BuildDirectory.Template";
 
 using namespace Utils;
 
@@ -155,16 +154,16 @@ public:
     QList<DocumentManager::RecentFile> m_recentFiles;
     static const int m_maxRecentFiles = 7;
 
-    QFileSystemWatcher *m_fileWatcher = nullptr; // Delayed creation.
-    QFileSystemWatcher *m_linkWatcher = nullptr; // Delayed creation (only UNIX/if a link is seen).
     bool m_postponeAutoReload = false;
     bool m_blockActivated = false;
     bool m_checkOnFocusChange = false;
+    bool m_useProjectsDirectory = true;
+
+    QFileSystemWatcher *m_fileWatcher = nullptr; // Delayed creation.
+    QFileSystemWatcher *m_linkWatcher = nullptr; // Delayed creation (only UNIX/if a link is seen).
     QString m_lastVisitedDirectory = QDir::currentPath();
     QString m_defaultLocationForNewFiles;
     FileName m_projectsDirectory;
-    bool m_useProjectsDirectory = true;
-    QString m_buildDirectory;
     // When we are calling into an IDocument
     // we don't want to receive a changed()
     // signal
@@ -1313,7 +1312,6 @@ void DocumentManager::saveSettings()
     s->beginGroup(QLatin1String(directoryGroupC));
     s->setValue(QLatin1String(projectDirectoryKeyC), d->m_projectsDirectory.toString());
     s->setValue(QLatin1String(useProjectDirectoryKeyC), d->m_useProjectsDirectory);
-    s->setValue(QLatin1String(buildDirectoryKeyC), d->m_buildDirectory);
     s->endGroup();
 }
 
@@ -1345,13 +1343,6 @@ void readSettings()
         d->m_projectsDirectory = FileName::fromString(PathChooser::homePath());
     d->m_useProjectsDirectory = s->value(QLatin1String(useProjectDirectoryKeyC),
                                          d->m_useProjectsDirectory).toBool();
-
-    const QString settingsShadowDir = s->value(QLatin1String(buildDirectoryKeyC),
-                                               QString()).toString();
-    if (!settingsShadowDir.isEmpty())
-        d->m_buildDirectory = settingsShadowDir;
-    else
-        d->m_buildDirectory = QLatin1String(Constants::DEFAULT_BUILD_DIRECTORY);
 
     s->endGroup();
 }
@@ -1420,26 +1411,6 @@ void DocumentManager::setProjectsDirectory(const FileName &directory)
         d->m_projectsDirectory = directory;
         emit m_instance->projectsDirectoryChanged(d->m_projectsDirectory);
     }
-}
-
-/*!
-    Returns the default build directory.
-
-    \sa setBuildDirectory
-*/
-QString DocumentManager::buildDirectory()
-{
-    return d->m_buildDirectory;
-}
-
-/*!
-    Sets the shadow build directory to \a directory.
-
-    \sa buildDirectory
-*/
-void DocumentManager::setBuildDirectory(const QString &directory)
-{
-    d->m_buildDirectory = directory;
 }
 
 /*!

src/plugins/coreplugin/documentmanager.h
@@ -141,9 +141,6 @@ public:
     static Utils::FileName projectsDirectory();
     static void setProjectsDirectory(const Utils::FileName &directory);
 
-    static QString buildDirectory();
-    static void setBuildDirectory(const QString &directory);
-
     /* Used to notify e.g. the code model to update the given files. Does *not*
        lead to any editors to reload or any other editor manager actions. */
     static void notifyFilesChangedInternally(const QStringList &files);

src/plugins/nim/project/nimbuildconfiguration.cpp
@@ -34,12 +34,12 @@
 
 #include "../nimconstants.h"
 
-#include <coreplugin/documentmanager.h>
 #include <projectexplorer/buildconfiguration.h>
 #include <projectexplorer/buildinfo.h>
 #include <projectexplorer/buildsteplist.h>
 #include <projectexplorer/buildstep.h>
 #include <projectexplorer/kit.h>
+#include <projectexplorer/projectexplorer.h>
 #include <projectexplorer/projectexplorerconstants.h>
 #include <projectexplorer/projectmacroexpander.h>
 #include <projectexplorer/target.h>
@@ -59,7 +59,7 @@ static FileName defaultBuildDirectory(const Kit *k,
     QFileInfo projectFileInfo(projectFilePath);
 
     ProjectMacroExpander expander(projectFilePath, projectFileInfo.baseName(), k, bc, buildType);
-    QString buildDirectory = expander.expand(Core::DocumentManager::buildDirectory());
+    QString buildDirectory = expander.expand(ProjectExplorerPlugin::buildDirectoryTemplate());
 
     if (FileUtils::isAbsolutePath(buildDirectory))
         return FileName::fromString(buildDirectory);

src/plugins/projectexplorer/projectexplorer.cpp
@@ -237,6 +237,10 @@ const char RUNMENUCONTEXTMENU[]   = "Project.RunMenu";
 const char FOLDER_OPEN_LOCATIONS_CONTEXT_MENU[]  = "Project.F.OpenLocation.CtxMenu";
 const char PROJECT_OPEN_LOCATIONS_CONTEXT_MENU[]  = "Project.P.OpenLocation.CtxMenu";
 
+// Default directories:
+const char DEFAULT_BUILD_DIRECTORY_TEMPLATE[] = "../%{JS: Util.asciify(\"build-%{CurrentProject:Name}-%{CurrentKit:FileSystemName}-%{CurrentBuild:Name}\")}";
+const char DEFAULT_BUILD_DIRECTORY_TEMPLATE_KEY[] = "Directories/BuildDirectory.Template";
+
 } // namespace Constants
 
 
@@ -1294,6 +1298,10 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
     if (tmp < 0 || tmp > ProjectExplorerSettings::StopSameBuildDir)
         tmp = Utils::HostOsInfo::isWindowsHost() ? 1 : 0;
     dd->m_projectExplorerSettings.stopBeforeBuild = ProjectExplorerSettings::StopBeforeBuild(tmp);
+    dd->m_projectExplorerSettings.buildDirectoryTemplate
+            = s->value(Constants::DEFAULT_BUILD_DIRECTORY_TEMPLATE_KEY).toString();
+    if (dd->m_projectExplorerSettings.buildDirectoryTemplate.isEmpty())
+        dd->m_projectExplorerSettings.buildDirectoryTemplate = Constants::DEFAULT_BUILD_DIRECTORY_TEMPLATE;
 
     auto buildManager = new BuildManager(this, dd->m_cancelBuildAction);
     connect(buildManager, &BuildManager::buildStateChanged,
@@ -1825,6 +1833,9 @@ void ProjectExplorerPluginPrivate::savePersistentSettings()
     s->setValue(QLatin1String("ProjectExplorer/Settings/MaxBuildOutputLines"), dd->m_projectExplorerSettings.maxBuildOutputChars / 100);
     s->setValue(QLatin1String("ProjectExplorer/Settings/EnvironmentId"), dd->m_projectExplorerSettings.environmentId.toByteArray());
     s->setValue(QLatin1String("ProjectExplorer/Settings/StopBeforeBuild"), dd->m_projectExplorerSettings.stopBeforeBuild);
+
+    // Store this in the Core directory scope for backward compatibility!
+    s->setValue(Constants::DEFAULT_BUILD_DIRECTORY_TEMPLATE_KEY, dd->m_projectExplorerSettings.buildDirectoryTemplate);
 }
 
 void ProjectExplorerPlugin::openProjectWelcomePage(const QString &fileName)
@@ -3598,6 +3609,31 @@ void ProjectExplorerPlugin::openOpenProjectDialog()
     const QStringList files = DocumentManager::getOpenFileNames(dd->m_projectFilterString, path);
     if (!files.isEmpty())
         ICore::openFiles(files, ICore::SwitchMode);
+}
+
+/*!
+    Returns the current build directory template.
+
+    \sa setBuildDirectoryTemplate
+*/
+QString ProjectExplorerPlugin::buildDirectoryTemplate()
+{
+    return dd->m_projectExplorerSettings.buildDirectoryTemplate;
+}
+
+/*!
+    Sets the current build directory template to \a directory.
+
+    \sa defaultbuildDirectory
+*/
+void ProjectExplorerPlugin::setBuildDirectoryTemplate(const QString &dir)
+{
+    dd->m_projectExplorerSettings.buildDirectoryTemplate = dir;
+}
+
+QString ProjectExplorerPlugin::defaultBuildDirectoryTemplate()
+{
+    return Constants::DEFAULT_BUILD_DIRECTORY_TEMPLATE;
 }
 
 QList<QPair<QString, QString> > ProjectExplorerPlugin::recentProjects()

commit 312c4a14339655ca6c80de0f8444ccf54931f68c

QbsProjectManager: Fix default build directory
Commit a43331ba67 broke the build, and commit 4a3546f58a fixed it, but
used the wrong function.

Fixes: QTCREATORBUG-22142
Change-Id: I8a66bc8a4501f017726149544d73a728ab753e32
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/qbsprojectmanager/qbsbuildconfiguration.cpp
@@ -71,7 +71,7 @@ static FileName defaultBuildDirectory(const QString &projectFilePath, const Kit 
     const QString projectName = QFileInfo(projectFilePath).completeBaseName();
     ProjectMacroExpander expander(projectFilePath, projectName, k, bcName, buildType);
     QString projectDir = Project::projectDirectory(FileName::fromString(projectFilePath)).toString();
-    QString buildPath = expander.expand(ProjectExplorerPlugin::defaultBuildDirectoryTemplate());
+    QString buildPath = expander.expand(ProjectExplorerPlugin::buildDirectoryTemplate());
     return FileName::fromString(FileUtils::resolvePath(projectDir, buildPath));
 }
 

src/plugins/qbsprojectmanager/qbsprojectimporter.cpp
@@ -94,7 +94,7 @@ static QString buildDir(const QString &projectFilePath, const Kit *k)
                                   BuildConfiguration::Unknown);
     const QString projectDir
             = Project::projectDirectory(FileName::fromString(projectFilePath)).toString();
-    const QString buildPath = expander.expand(ProjectExplorerPlugin::defaultBuildDirectoryTemplate());
+    const QString buildPath = expander.expand(ProjectExplorerPlugin::buildDirectoryTemplate());
     return FileUtils::resolvePath(projectDir, buildPath);
 }
 