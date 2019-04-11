57cac11e2082c68b6e8b821e4bf6388a784cc2eb
Eike Ziller <eike.ziller@qt.io>
2019-04-11 11:57:53 +0200

    Make task details visible by default
    It used to hide to bottom part of the vertical scrollbar of editors, but
    it moved a bit to the left. It will still hide some UI, but the issue is
    probably less ugly now, and can still be turned off by users.
    
    Change-Id: I9522658f658300212cd205f9e07b165b2dd393f8
    Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

--- src/plugins/coreplugin/progressmanager/progressmanager.cpp
+++ src/plugins/coreplugin/progressmanager/progressmanager.cpp
@@ -292,7 +292,7 @@ void ProgressManagerPrivate::readSettings()
 {
     QSettings *settings = ICore::settings();
     settings->beginGroup(QLatin1String(kSettingsGroup));
-    m_progressViewPinned = settings->value(QLatin1String(kDetailsPinned), false).toBool();
+    m_progressViewPinned = settings->value(QLatin1String(kDetailsPinned), true).toBool();
     settings->endGroup();
 }
 
