qt-creator/qt-creator

commit b503ea7f06147f9e64cd03d9454aafb40997c3f7

Add more Python features
On the wizard related to Main Windows, not there are more
options to specify the name of the files and classes.

It's possible to Add and Remove files on Python projects
that use the new .pyproject file.

Improved the empty application,
to template the .pyproject file and use the proper name
for the file.

Added the icons for file overlay.

Change-Id: Iaba7feda69e0f608260b5fb1d1b04b2a42b08c2d
Reviewed-by: hjk <hjk@qt.io>

src/plugins/qmakeprojectmanager/qmakenodetreebuilder.cpp
@@ -69,6 +69,8 @@ const FileTypeDataStorage fileTypeDataStorage[] = {
       ProjectExplorer::Constants::FILEOVERLAY_QRC, "*.qrc;" },
     { FileType::QML, QT_TRANSLATE_NOOP("QmakeProjectManager::QmakePriFile", "QML"),
       ProjectExplorer::Constants::FILEOVERLAY_QML, "*.qml;" },
+    { FileType::Source, QT_TRANSLATE_NOOP("QmakeProjectManager::QmakePriFile", "Python sources"),
+      ProjectExplorer::Constants::FILEOVERLAY_PY, "*.py;" },
     { FileType::Unknown, QT_TRANSLATE_NOOP("QmakeProjectManager::QmakePriFile", "Other files"),
       ProjectExplorer::Constants::FILEOVERLAY_UNKNOWN, "*;" }
 };

share/qtcreator/templates/wizards/projects/qtforpythonapplication/main.pyproject
@@ -1,3 +1,3 @@
 {
-    "files": ["main.py"]
+    "files": ["%{SrcFileName}"]
 }

share/qtcreator/templates/wizards/projects/qtforpythonapplication/main_mainwindow.py
@@ -1,15 +1,29 @@
 # This Python file uses the following encoding: utf-8
 import sys
+@if '%{BaseCB}' === 'QWidget'
+from PySide2.QtWidgets import QApplication, QWidget
+@endif
+@if '%{BaseCB}' === 'QMainWindow'
 from PySide2.QtWidgets import QApplication, QMainWindow
+@endif
 
 
-class MainWindow(QMainWindow):
+@if '%{BaseCB}'
+class %{Class}(%{BaseCB}):
+@else
+class %{Class}:
+@endif
     def __init__(self):
+@if '%{BaseCB}' === 'QWidget'
+        QWidget.__init__(self)
+@endif
+@if '%{BaseCB}' === 'QMainWindow'
         QMainWindow.__init__(self)
+@endif
 
 
 if __name__ == "__main__":
     app = QApplication([])
-    window = MainWindow()
+    window = %{Class}()
     window.show()
     sys.exit(app.exec_())

src/plugins/projectexplorer/projectexplorer.qrc
@@ -73,6 +73,8 @@
         <file>images/fileoverlay_cpp@2x.png</file>
         <file>images/fileoverlay_h.png</file>
         <file>images/fileoverlay_h@2x.png</file>
+        <file>images/fileoverlay_py.png</file>
+        <file>images/fileoverlay_py@2x.png</file>
         <file>images/fileoverlay_unknown.png</file>
         <file>images/fileoverlay_unknown@2x.png</file>
         <file>images/cancelbuild_overlay.png</file>

src/plugins/projectexplorer/projectexplorerconstants.h
@@ -210,6 +210,7 @@ const char FILEOVERLAY_QRC[]=":/projectexplorer/images/fileoverlay_qrc.png";
 const char FILEOVERLAY_CPP[]=":/projectexplorer/images/fileoverlay_cpp.png";
 const char FILEOVERLAY_H[]=":/projectexplorer/images/fileoverlay_h.png";
 const char FILEOVERLAY_SCXML[]=":/projectexplorer/images/fileoverlay_scxml.png";
+const char FILEOVERLAY_PY[]=":/projectexplorer/images/fileoverlay_py.png";
 const char FILEOVERLAY_UNKNOWN[]=":/projectexplorer/images/fileoverlay_unknown.png";
 
 const char ADD_FILES_DIALOG_FILTER_HISTORY_KEY[] = "ProjectExplorer.AddFilesFilterKey";

src/plugins/pythoneditor/pythoneditorplugin.cpp
@@ -94,6 +94,9 @@ public:
     bool needsConfiguration() const final { return false; }
     bool needsBuildConfigurations() const final { return false; }
 
+    bool writePyProjectFile(const QString &fileName, QString &content,
+                            const QStringList &rawList, QString *errorMessage);
+
 private:
     RestoreResult fromMap(const QVariantMap &map, QString *errorMessage) override;
     bool setupTarget(Target *t) override
@@ -402,16 +405,59 @@ bool PythonProject::saveRawFileList(const QStringList &rawFileList)
 bool PythonProject::saveRawList(const QStringList &rawList, const QString &fileName)
 {
     FileChangeBlocker changeGuarg(fileName);
-    // Make sure we can open the file for writing
-    FileSaver saver(fileName, QIODevice::Text);
-    if (!saver.hasError()) {
-        QTextStream stream(saver.file());
-        foreach (const QString &filePath, rawList)
-            stream << filePath << '\n';
-        saver.setResult(&stream);
+    bool result = false;
+
+    // New project file
+    if (fileName.endsWith(".pyproject")) {
+        FileSaver saver(fileName, QIODevice::ReadOnly | QIODevice::Text);
+        if (!saver.hasError()) {
+            QString content = QTextStream(saver.file()).readAll();
+            if (saver.finalize(ICore::mainWindow())) {
+                QString errorMessage;
+                result = writePyProjectFile(fileName, content, rawList, &errorMessage);
+                if (!errorMessage.isEmpty())
+                    Core::MessageManager::write(errorMessage);
+                }
+        }
+    } else { // Old project file
+        FileSaver saver(fileName, QIODevice::WriteOnly | QIODevice::Text);
+        if (!saver.hasError()) {
+            QTextStream stream(saver.file());
+            for (const QString &filePath : rawList)
+                stream << filePath << '\n';
+            saver.setResult(&stream);
+            result = saver.finalize(ICore::mainWindow());
+        }
     }
-    bool result = saver.finalize(ICore::mainWindow());
+
     return result;
+}
+
+bool PythonProject::writePyProjectFile(const QString &fileName, QString &content,
+                                       const QStringList &rawList, QString *errorMessage)
+{
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        *errorMessage = PythonProject::tr("Unable to open \"%1\" for reading: %2")
+                        .arg(fileName, file.errorString());
+        return false;
+    }
+
+    // Build list of files with the current rawList for the JSON file
+    QString files("[");
+    for (const QString &f : rawList)
+        if (!f.endsWith(".pyproject"))
+            files += QString("\"%1\",").arg(f);
+    files = files.left(files.lastIndexOf(',')); // Removing leading comma
+    files += ']';
+
+    // Removing everything inside square parenthesis
+    // to replace it with the new list of files for the JSON file.
+    QRegularExpression pattern(R"(\[.*\])");
+    content.replace(pattern, files);
+    file.write(content.toUtf8());
+
+    return true;
 }
 
 bool PythonProject::addFiles(const QStringList &filePaths)
@@ -422,10 +468,7 @@ bool PythonProject::addFiles(const QStringList &filePaths)
     foreach (const QString &filePath, filePaths)
         newList.append(baseDir.relativeFilePath(filePath));
 
-    bool result = saveRawList(newList, projectFilePath().toString());
-    refresh();
-
-    return result;
+    return saveRawFileList(newList);
 }
 
 bool PythonProject::removeFiles(const QStringList &filePaths)
@@ -701,11 +744,10 @@ bool PythonEditorPlugin::initialize(const QStringList &arguments, QString *error
 
 void PythonEditorPlugin::extensionsInitialized()
 {
-    // Initialize editor actions handler
     // Add MIME overlay icons (these icons displayed at Project dock panel)
-    const QIcon icon = QIcon::fromTheme(C_PY_MIME_ICON);
-    if (!icon.isNull())
-        Core::FileIconProvider::registerIconOverlayForMimeType(icon, C_PY_MIMETYPE);
+    QString imageFile = creatorTheme()->imageFile(Theme::IconOverlayPro,
+                                                  ProjectExplorer::Constants::FILEOVERLAY_PY);
+    FileIconProvider::registerIconOverlayForSuffix(imageFile, "py");
 
     TaskHub::addCategory(PythonErrorTaskCategory, "Python", true);
 }

commit e76efa69d53daa3725483835739c569bf97c928f

QmakeProjectManager: Remove python file type
This is a partial regression from b503ea7f06.

The main goal was just to have an overlay on the Python files,
but since qmake and python don't really interact, adding a new
FileType triggered all the contents from "Other sources" to
be duplicated in the "Python sources" node.

The Python sources node is not necessary at the moment,
so I remove it to avoid duplicated tree elements.

Change-Id: I61e9e51327689636328a746059159c962270e805
Fixes: QTCREATORBUG-22292
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

src/plugins/projectexplorer/projectnodes.h
@@ -49,7 +49,6 @@ enum class FileType : quint16 {
     Unknown = 0,
     Header,
     Source,
-    PythonSource,
     Form,
     StateChart,
     Resource,

src/plugins/qmakeprojectmanager/qmakenodetreebuilder.cpp
@@ -61,8 +61,6 @@ const FileTypeDataStorage fileTypeDataStorage[] = {
       ProjectExplorer::Constants::FILEOVERLAY_H, "*.h; *.hh; *.hpp; *.hxx;"},
     { FileType::Source, QT_TRANSLATE_NOOP("QmakeProjectManager::QmakePriFile", "Sources"),
       ProjectExplorer::Constants::FILEOVERLAY_CPP, "*.c; *.cc; *.cpp; *.cp; *.cxx; *.c++;" },
-    { FileType::PythonSource, QT_TRANSLATE_NOOP("QmakeProjectManager::QmakePriFile", "Python sources"),
-      ProjectExplorer::Constants::FILEOVERLAY_PY, "*.py;" },
     { FileType::Form, QT_TRANSLATE_NOOP("QmakeProjectManager::QmakePriFile", "Forms"),
       ProjectExplorer::Constants::FILEOVERLAY_UI, "*.ui;" },
     { FileType::StateChart, QT_TRANSLATE_NOOP("QmakeProjectManager::QmakePriFile", "State charts"),