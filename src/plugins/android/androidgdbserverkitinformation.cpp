qt-creator/qt-creator

commit d4565be6557fca844dcf4bf427e0ff9c0838df50

ProjectExplorer: Drop KitAspect::defaultValue()
The functionality of this function overlapped with KitAspect::setup(),
leading to unclear responsibilities and resulting in bugs such as the
one fixed by 776d54e435.
Therefore, we drop the defaultValue() function, merging its
implementation with setup() where applicable.

Change-Id: Iefa9c3df8b76e97ddf9ad388516621f7ea6558d4
Reviewed-by: hjk <hjk@qt.io>

src/plugins/android/androidgdbserverkitinformation.cpp
@@ -80,9 +80,9 @@ AndroidGdbServerKitAspect::AndroidGdbServerKitAspect()
     setPriority(27999); // Just one less than Debugger!
 }
 
-QVariant AndroidGdbServerKitAspect::defaultValue(const Kit *kit) const
+void AndroidGdbServerKitAspect::setup(Kit *kit)
 {
-    return autoDetect(kit).toString();
+    kit->setValue(id(), autoDetect(kit).toString());
 }
 
 QList<Task> AndroidGdbServerKitAspect::validate(const Kit *) const

src/plugins/cmakeprojectmanager/cmakekitinformation.cpp
@@ -245,12 +245,6 @@ void CMakeKitAspect::setCMakeTool(Kit *k, const Core::Id id)
         k->setValue(TOOL_ID, toSet.toSetting());
 }
 
-QVariant CMakeKitAspect::defaultValue(const Kit *k) const
-{
-    const Core::Id id = k ? defaultCMakeToolId() : Core::Id();
-    return id.toSetting();
-}
-
 QList<Task> CMakeKitAspect::validate(const Kit *k) const
 {
     QList<Task> result;
@@ -275,8 +269,7 @@ void CMakeKitAspect::setup(Kit *k)
 
 void CMakeKitAspect::fix(Kit *k)
 {
-    if (!CMakeKitAspect::cmakeTool(k))
-        setup(k);
+    setup(k);
 }
 
 KitAspect::ItemList CMakeKitAspect::toUserOutput(const Kit *k) const

src/plugins/projectexplorer/kitinformation.cpp
@@ -122,12 +122,6 @@ SysRootKitAspect::SysRootKitAspect()
     setPriority(31000);
 }
 
-QVariant SysRootKitAspect::defaultValue(const Kit *k) const
-{
-    Q_UNUSED(k)
-    return QString();
-}
-
 QList<Task> SysRootKitAspect::validate(const Kit *k) const
 {
     QList<Task> result;
@@ -375,12 +369,6 @@ static QVariant defaultToolChainValue()
     return result;
 }
 
-QVariant ToolChainKitAspect::defaultValue(const Kit *k) const
-{
-    Q_UNUSED(k);
-    return defaultToolChainValue();
-}
-
 QList<Task> ToolChainKitAspect::validate(const Kit *k) const
 {
     QList<Task> result;
@@ -494,7 +482,9 @@ void ToolChainKitAspect::setup(Kit *k)
     QTC_ASSERT(ToolChainManager::isLoaded(), return);
     QTC_ASSERT(k, return);
 
-    const QVariantMap value = k->value(ToolChainKitAspect::id()).toMap();
+    QVariantMap value = k->value(id()).toMap();
+    if (value.empty())
+        value = defaultToolChainValue().toMap();
 
     for (auto i = value.constBegin(); i != value.constEnd(); ++i) {
         Core::Id l = findLanguage(i.key());
@@ -813,10 +803,10 @@ DeviceTypeKitAspect::DeviceTypeKitAspect()
     makeEssential();
 }
 
-QVariant DeviceTypeKitAspect::defaultValue(const Kit *k) const
+void DeviceTypeKitAspect::setup(Kit *k)
 {
-    Q_UNUSED(k);
-    return QByteArray(Constants::DESKTOP_DEVICE_TYPE);
+    if (k)
+        k->setValue(id(), QByteArray(Constants::DESKTOP_DEVICE_TYPE));
 }
 
 QList<Task> DeviceTypeKitAspect::validate(const Kit *k) const
@@ -1242,12 +1232,6 @@ EnvironmentKitAspect::EnvironmentKitAspect()
     setDisplayName(tr("Environment"));
     setDescription(tr("Additional build environment settings when using this kit."));
     setPriority(29000);
-}
-
-QVariant EnvironmentKitAspect::defaultValue(const Kit *k) const
-{
-    Q_UNUSED(k)
-    return QStringList();
 }
 
 QList<Task> EnvironmentKitAspect::validate(const Kit *k) const

src/plugins/qtsupport/qtkitinformation.cpp
@@ -161,20 +161,19 @@ QtKitAspect::QtKitAspect()
             this, &QtKitAspect::kitsWereLoaded);
 }
 
-QVariant QtKitAspect::defaultValue(const Kit *k) const
+void QtKitAspect::setup(ProjectExplorer::Kit *k)
 {
     Q_UNUSED(k);
 
     // find "Qt in PATH":
     BaseQtVersion *result = QtVersionManager::version(equal(&BaseQtVersion::autodetectionSource,
                                                             QString::fromLatin1("PATH")));
-    if (result)
-        return result->uniqueId();
-
-    // Use *any* desktop Qt:
-    result = QtVersionManager::version(equal(&BaseQtVersion::type,
-                                             QString::fromLatin1(QtSupport::Constants::DESKTOPQT)));
-    return result ? result->uniqueId() : -1;
+    if (!result) {
+        // Use *any* desktop Qt:
+        result = QtVersionManager::version(equal(&BaseQtVersion::type,
+            QString::fromLatin1(QtSupport::Constants::DESKTOPQT)));
+    }
+    k->setValue(id(), result ? result->uniqueId() : -1);
 }
 
 QList<ProjectExplorer::Task> QtKitAspect::validate(const ProjectExplorer::Kit *k) const

src/plugins/android/androidgdbserverkitinformation.h
@@ -36,7 +36,7 @@ class AndroidGdbServerKitAspect : public ProjectExplorer::KitAspect
 public:
     AndroidGdbServerKitAspect();
 
-    QVariant defaultValue(const ProjectExplorer::Kit *) const override;
+    void setup(ProjectExplorer::Kit *) override;
     QList<ProjectExplorer::Task> validate(const ProjectExplorer::Kit *) const override;
     bool isApplicableToKit(const ProjectExplorer::Kit *k) const override;
     ItemList toUserOutput(const ProjectExplorer::Kit *) const override;

src/plugins/cmakeprojectmanager/cmakekitinformation.h
@@ -47,7 +47,6 @@ public:
     static void setCMakeTool(ProjectExplorer::Kit *k, const Core::Id id);
 
     // KitAspect interface
-    QVariant defaultValue(const ProjectExplorer::Kit *k) const final;
     QList<ProjectExplorer::Task> validate(const ProjectExplorer::Kit *k) const final;
     void setup(ProjectExplorer::Kit *k) final;
     void fix(ProjectExplorer::Kit *k) final;
@@ -78,13 +77,15 @@ public:
     static QStringList generatorArguments(const ProjectExplorer::Kit *k);
 
     // KitAspect interface
-    QVariant defaultValue(const ProjectExplorer::Kit *k) const final;
     QList<ProjectExplorer::Task> validate(const ProjectExplorer::Kit *k) const final;
     void setup(ProjectExplorer::Kit *k) final;
     void fix(ProjectExplorer::Kit *k) final;
     void upgrade(ProjectExplorer::Kit *k) final;
     ItemList toUserOutput(const ProjectExplorer::Kit *k) const final;
     ProjectExplorer::KitAspectWidget *createConfigWidget(ProjectExplorer::Kit *k) const final;
+
+private:
+    QVariant defaultValue(const ProjectExplorer::Kit *k) const;
 };
 
 class CMakeConfigurationKitAspect : public ProjectExplorer::KitAspect
@@ -102,12 +103,14 @@ public:
     static CMakeConfig defaultConfiguration(const ProjectExplorer::Kit *k);
 
     // KitAspect interface
-    QVariant defaultValue(const ProjectExplorer::Kit *k) const final;
     QList<ProjectExplorer::Task> validate(const ProjectExplorer::Kit *k) const final;
     void setup(ProjectExplorer::Kit *k) final;
     void fix(ProjectExplorer::Kit *k) final;
     ItemList toUserOutput(const ProjectExplorer::Kit *k) const final;
     ProjectExplorer::KitAspectWidget *createConfigWidget(ProjectExplorer::Kit *k) const final;
+
+private:
+    QVariant defaultValue(const ProjectExplorer::Kit *k) const;
 };
 
 } // namespace Internal

src/plugins/debugger/debuggerkitinformation.cpp
@@ -152,8 +152,6 @@ DebuggerKitAspect::DebuggerKitAspect()
     setPriority(28000);
 }
 
-QVariant DebuggerKitAspect::defaultValue(const Kit *) const { return QVariant(); }
-
 void DebuggerKitAspect::setup(Kit *k)
 {
     QTC_ASSERT(k, return);

src/plugins/debugger/debuggerkitinformation.h
@@ -41,7 +41,6 @@ class DEBUGGER_EXPORT DebuggerKitAspect : public ProjectExplorer::KitAspect
 public:
     DebuggerKitAspect();
 
-    QVariant defaultValue(const ProjectExplorer::Kit *k) const override;
     QList<ProjectExplorer::Task> validate(const ProjectExplorer::Kit *k) const override
         { return DebuggerKitAspect::validateDebugger(k); }
 

commit 534fcfcd455731ca17c773ab943636d25bf1fb0d

Fix Qt version for auto-detected kits
Fixes a regression in d4565be6557fca844dcf4bf427e0ff9c0838df50 that
leads to all desktop Kits being assigned a default desktop Qt version,
even though the installer registered these kits with a specific Qt.

Implementations of KitAspect::setup may not unconditionally override an
existing value, since it is called on the kits restored from the install
settings. This is needed because the installer registers toolchains and
debuggers by referring to them via an ABI ("this kit has a debugger that
can handle this specific ABI"), since the installer doesn't know e.g.
about installed MSVC toolchains itself.
That's why ToolChainKitAspect and DebuggerKitAspect may modify an
existing value in their "setup" method. If this should be moved
somewhere else, e.g. "fix", should be investigated, but in a separate
refactoring.

Change-Id: Ibd99e4da03cd7130c49294f4ac79cd8e346fb1b7
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

src/plugins/android/androidgdbserverkitinformation.cpp
@@ -82,7 +82,8 @@ AndroidGdbServerKitAspect::AndroidGdbServerKitAspect()
 
 void AndroidGdbServerKitAspect::setup(Kit *kit)
 {
-    kit->setValue(id(), autoDetect(kit).toString());
+    if (kit && !kit->hasValue(id()))
+        kit->setValue(id(), autoDetect(kit).toString());
 }
 
 QList<Task> AndroidGdbServerKitAspect::validate(const Kit *) const

src/plugins/cmakeprojectmanager/cmakekitinformation.cpp
@@ -692,6 +692,8 @@ QList<Task> CMakeGeneratorKitAspect::validate(const Kit *k) const
 
 void CMakeGeneratorKitAspect::setup(Kit *k)
 {
+    if (!k || k->hasValue(id()))
+        return;
     GeneratorInfo info;
     info.fromVariant(defaultValue(k));
     setGeneratorInfo(k, info);

src/plugins/projectexplorer/kitinformation.cpp
@@ -805,7 +805,7 @@ DeviceTypeKitAspect::DeviceTypeKitAspect()
 
 void DeviceTypeKitAspect::setup(Kit *k)
 {
-    if (k)
+    if (k && !k->hasValue(id()))
         k->setValue(id(), QByteArray(Constants::DESKTOP_DEVICE_TYPE));
 }
 

src/plugins/qtsupport/qtkitinformation.cpp
@@ -163,6 +163,8 @@ QtKitAspect::QtKitAspect()
 
 void QtKitAspect::setup(ProjectExplorer::Kit *k)
 {
+    if (!k || k->hasValue(id()))
+        return;
     const Abi tcAbi = ToolChainKitAspect::targetAbi(k);
     const Core::Id deviceType = DeviceTypeKitAspect::deviceTypeId(k);
 