qt-creator/qt-creator

commit 376c6b9d59cef608448acc6cec30c32e3836c4dd

Add curve editor
Moved qmldesignerextension into component

Updated build systems

Change-Id: I8d2d0757a1639a472d426b66c0c8ae6fb84cc3d2
Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

src/plugins/qmldesigner/components/curveeditor/curveeditormodel.h
@@ -1,0 +1,70 @@
+/****************************************************************************
+**
+** Copyright (C) 2019 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt Design Tooling
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+****************************************************************************/
+
+#pragma once
+
+#include "detail/treemodel.h"
+
+#include <vector>
+
+class QPointF;
+
+namespace DesignTools {
+
+struct CurveEditorStyle;
+
+class AnimationCurve;
+class PropertyTreeItem;
+class TreeItem;
+
+class CurveEditorModel : public TreeModel
+{
+    Q_OBJECT
+
+signals:
+    void currentFrameChanged(int frame);
+
+    void curveChanged(PropertyTreeItem *item);
+
+public:
+    virtual double minimumTime() const = 0;
+
+    virtual double maximumTime() const = 0;
+
+    virtual CurveEditorStyle style() const = 0;
+
+public:
+    CurveEditorModel(QObject *parent = nullptr);
+
+    ~CurveEditorModel() override;
+
+    void setCurrentFrame(int frame);
+
+    void setCurve(unsigned int id, const AnimationCurve &curve);
+
+    void reset(const std::vector<TreeItem *> &items);
+};
+
+} // End namespace DesignTools.

src/plugins/qmldesigner/components/curveeditor/detail/curveeditorstyledialog.h
@@ -1,0 +1,125 @@
+
+/****************************************************************************
+**
+** Copyright (C) 2019 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt Design Tooling
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+****************************************************************************/
+
+#pragma once
+
+#include <QDialog>
+
+class QPushButton;
+class QSpinBox;
+class QDoubleSpinBox;
+
+namespace DesignTools {
+
+class ColorControl;
+
+struct CurveEditorStyle;
+
+class CurveEditorStyleDialog : public QDialog
+{
+    Q_OBJECT
+
+signals:
+    void styleChanged(const CurveEditorStyle &style);
+
+public:
+    CurveEditorStyleDialog(CurveEditorStyle &style, QWidget *parent = nullptr);
+
+    CurveEditorStyle style() const;
+
+private:
+    void emitStyleChanged();
+
+    void printStyle();
+
+private:
+    QPushButton *m_printButton;
+
+    ColorControl *m_background;
+
+    ColorControl *m_backgroundAlternate;
+
+    ColorControl *m_fontColor;
+
+    ColorControl *m_gridColor;
+
+    QDoubleSpinBox *m_canvasMargin;
+
+    QSpinBox *m_zoomInWidth;
+
+    QSpinBox *m_zoomInHeight;
+
+    QDoubleSpinBox *m_timeAxisHeight;
+
+    QDoubleSpinBox *m_timeOffsetLeft;
+
+    QDoubleSpinBox *m_timeOffsetRight;
+
+    ColorControl *m_rangeBarColor;
+
+    ColorControl *m_rangeBarCapsColor;
+
+    QDoubleSpinBox *m_valueAxisWidth;
+
+    QDoubleSpinBox *m_valueOffsetTop;
+
+    QDoubleSpinBox *m_valueOffsetBottom;
+
+    // HandleItem
+    QDoubleSpinBox *m_handleSize;
+
+    QDoubleSpinBox *m_handleLineWidth;
+
+    ColorControl *m_handleColor;
+
+    ColorControl *m_handleSelectionColor;
+
+    // KeyframeItem
+    QDoubleSpinBox *m_keyframeSize;
+
+    ColorControl *m_keyframeColor;
+
+    ColorControl *m_keyframeSelectionColor;
+
+    // CurveItem
+    QDoubleSpinBox *m_curveWidth;
+
+    ColorControl *m_curveColor;
+
+    ColorControl *m_curveSelectionColor;
+
+    // TreeItem
+    QDoubleSpinBox *m_treeMargins;
+
+    // Playhead
+    QDoubleSpinBox *m_playheadWidth;
+
+    QDoubleSpinBox *m_playheadRadius;
+
+    ColorControl *m_playheadColor;
+};
+
+} // End namespace DesignTools.

src/plugins/qmldesigner/components/curveeditor/detail/curvesegment.h
@@ -1,0 +1,63 @@
+/****************************************************************************
+**
+** Copyright (C) 2019 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt Design Tooling
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+****************************************************************************/
+
+#pragma once
+
+#include "keyframe.h"
+
+#include <vector>
+
+class QPointF;
+
+namespace DesignTools {
+
+class CurveSegment
+{
+public:
+    CurveSegment();
+
+    CurveSegment(const Keyframe &first, const Keyframe &last);
+
+    bool containsX(double x) const;
+
+    QPointF evaluate(double t) const;
+
+    std::vector<QPointF> extrema() const;
+
+    std::vector<double> yForX(double x) const;
+
+    std::vector<double> xForY(double y) const;
+
+    void setLeft(const Keyframe &frame);
+
+    void setRight(const Keyframe &frame);
+
+private:
+    Keyframe m_left;
+
+    Keyframe m_right;
+};
+
+} // End namespace DesignTools.

src/plugins/qmldesigner/components/curveeditor/detail/playhead.h
@@ -1,0 +1,70 @@
+/****************************************************************************
+**
+** Copyright (C) 2019 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt Design Tooling
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+****************************************************************************/
+
+#pragma once
+
+#include <QRectF>
+#include <QTimer>
+
+class QPainter;
+
+namespace DesignTools {
+
+class GraphicsView;
+
+class Playhead
+{
+public:
+    Playhead(GraphicsView *view);
+
+    int currentFrame() const;
+
+    void paint(QPainter *painter, GraphicsView *view) const;
+
+    void moveToFrame(int frame, GraphicsView *view);
+
+    void resize(GraphicsView *view);
+
+    bool mousePress(const QPointF &pos);
+
+    bool mouseMove(const QPointF &pos, GraphicsView *view);
+
+    void mouseRelease(GraphicsView *view);
+
+private:
+    void mouseMoveOutOfBounds(GraphicsView *view);
+
+    int m_frame;
+
+    bool m_moving;
+
+    QRectF m_rect;
+
+    QTimer m_timer;
+
+    GraphicsView *m_view;
+};
+
+} // End namespace DesignTools.

src/plugins/qmldesigner/components/curveeditor/detail/utils.h
@@ -1,0 +1,50 @@
+/****************************************************************************
+**
+** Copyright (C) 2019 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt Design Tooling
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+****************************************************************************/
+
+#pragma once
+
+class QColor;
+class QPalette;
+class QPointF;
+class QRectF;
+class QTransform;
+
+namespace DesignTools {
+
+double clamp(double val, double lo, double hi);
+
+double lerp(double blend, double a, double b);
+
+double scaleX(const QTransform &transform);
+
+double scaleY(const QTransform &transform);
+
+void grow(QRectF &rect, const QPointF &point);
+
+QRectF bbox(const QRectF &rect, const QTransform &transform);
+
+QPalette singleColorPalette(const QColor &color);
+
+} // End namespace DesignTools.

src/plugins/qmldesigner/components/curveeditor/treeitem.h
@@ -1,0 +1,135 @@
+
+/****************************************************************************
+**
+** Copyright (C) 2019 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the Qt Design Tooling
+**
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+****************************************************************************/
+
+#pragma once
+
+#include "animationcurve.h"
+
+#include <QIcon>
+#include <QString>
+
+#include <vector>
+
+class QIcon;
+class QVariant;
+
+namespace DesignTools {
+
+class NodeTreeItem;
+class PropertyTreeItem;
+
+class TreeItem
+{
+public:
+    TreeItem(const QString &name);
+
+    virtual ~TreeItem();
+
+    virtual QIcon icon() const;
+
+    virtual NodeTreeItem *asNodeItem();
+
+    virtual PropertyTreeItem *asPropertyItem();
+
+    unsigned int id() const;
+
+    bool locked() const;
+
+    bool pinned() const;
+
+    int row() const;
+
+    int column() const;
+
+    int rowCount() const;
+
+    int columnCount() const;
+
+    TreeItem *parent() const;
+
+    TreeItem *child(int row) const;
+
+    TreeItem *find(unsigned int row) const;
+
+    QVariant data(int column) const;
+
+    QVariant headerData(int column) const;
+
+    void setId(unsigned int &id);
+
+    void addChild(TreeItem *child);
+
+    void setLocked(bool locked);
+
+    void setPinned(bool pinned);
+
+protected:
+    QString m_name;
+
+    unsigned int m_id;
+
+    bool m_locked;
+
+    bool m_pinned;
+
+    TreeItem *m_parent;
+
+    std::vector<TreeItem *> m_children;
+};
+
+
+class NodeTreeItem : public TreeItem
+{
+public:
+    NodeTreeItem(const QString &name, const QIcon &icon);
+
+    NodeTreeItem *asNodeItem() override;
+
+    QIcon icon() const override;
+
+private:
+    QIcon m_icon;
+};
+
+
+class PropertyTreeItem : public TreeItem
+{
+public:
+    PropertyTreeItem(const QString &name, const AnimationCurve &curve);
+
+    PropertyTreeItem *asPropertyItem() override;
+
+    AnimationCurve curve() const;
+
+    void setCurve(const AnimationCurve &curve);
+
+private:
+    using TreeItem::addChild;
+
+    AnimationCurve m_curve;
+};
+
+} // End namespace DesignTools.

commit 3e37a93c2e632d3a13b49339650303d8c25fa378

QmlDesigner: Fix compile with namespaced Qt
Broke with 376c6b9d59c.

Change-Id: If98cb76aa5c68c7c2ada5578ab58c37956617c19
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/plugins/qmldesigner/components/curveeditor/curveeditormodel.h
@@ -29,7 +29,9 @@
 
 #include <vector>
 
+QT_BEGIN_NAMESPACE
 class QPointF;
+QT_END_NAMESPACE
 
 namespace DesignTools {
 

src/plugins/qmldesigner/components/curveeditor/detail/curveeditorstyledialog.h
@@ -28,9 +28,11 @@
 
 #include <QDialog>
 
+QT_BEGIN_NAMESPACE
 class QPushButton;
 class QSpinBox;
 class QDoubleSpinBox;
+QT_END_NAMESPACE
 
 namespace DesignTools {
 

src/plugins/qmldesigner/components/curveeditor/detail/curvesegment.h
@@ -29,7 +29,9 @@
 
 #include <vector>
 
+QT_BEGIN_NAMESPACE
 class QPointF;
+QT_END_NAMESPACE
 
 namespace DesignTools {
 

src/plugins/qmldesigner/components/curveeditor/detail/playhead.h
@@ -28,7 +28,9 @@
 #include <QRectF>
 #include <QTimer>
 
+QT_BEGIN_NAMESPACE
 class QPainter;
+QT_END_NAMESPACE
 
 namespace DesignTools {
 

src/plugins/qmldesigner/components/curveeditor/detail/utils.h
@@ -25,11 +25,13 @@
 
 #pragma once
 
+QT_BEGIN_NAMESPACE
 class QColor;
 class QPalette;
 class QPointF;
 class QRectF;
 class QTransform;
+QT_END_NAMESPACE
 
 namespace DesignTools {
 

src/plugins/qmldesigner/components/curveeditor/treeitem.h
@@ -33,8 +33,10 @@
 
 #include <vector>
 
+QT_BEGIN_NAMESPACE
 class QIcon;
 class QVariant;
+QT_END_NAMESPACE
 
 namespace DesignTools {
 