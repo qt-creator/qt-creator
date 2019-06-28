1571384e4998b9e20ad54be8479c60def8bfaba4
David Schulz <david.schulz@qt.io>
2019-06-28 09:22:40 +0200

    Debugger: add dptr child to QDir for msvc targets
    Makes it possible to access the entry lists if these were populated by
    the code.
    
    Change-Id: I066824d2ce7f75a38d208155156c93d06458f8b3
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- share/qtcreator/debugger/qttypes.py
+++ share/qtcreator/debugger/qttypes.py
@@ -396,8 +396,7 @@ def qdump__QDateTime(d, value):
 
 
 def qdump__QDir(d, value):
-    if not d.isMsvcTarget():
-        d.putNumChild(1)
+    d.putNumChild(1)
     privAddress = d.extractPointer(value)
     bit32 = d.ptrSize() == 4
     qt5 = d.qtVersion() >= 0x050000
@@ -468,21 +467,22 @@ def qdump__QDir(d, value):
         absoluteDirEntryOffset = dirEntryOffset + fileSystemEntrySize
 
     d.putStringValue(privAddress + dirEntryOffset)
-    if d.isExpanded() and not d.isMsvcTarget():
+    if d.isExpanded():
         with Children(d):
-            ns = d.qtNamespace()
-            d.call('int', value, 'count')  # Fill cache.
-            #d.putCallItem('absolutePath', '@QString', value, 'absolutePath')
-            #d.putCallItem('canonicalPath', '@QString', value, 'canonicalPath')
-            with SubItem(d, 'absolutePath'):
-                typ = d.lookupType(ns + 'QString')
-                d.putItem(d.createValue(privAddress + absoluteDirEntryOffset, typ))
-            with SubItem(d, 'entryInfoList'):
-                typ = d.lookupType(ns + 'QFileInfo')
-                qdumpHelper_QList(d, privAddress + fileInfosOffset, typ)
-            with SubItem(d, 'entryList'):
-                typ = d.lookupType(ns + 'QStringList')
-                d.putItem(d.createValue(privAddress + filesOffset, typ))
+            if not d.isMsvcTarget():
+                ns = d.qtNamespace()
+                d.call('int', value, 'count')  # Fill cache.
+                #d.putCallItem('absolutePath', '@QString', value, 'absolutePath')
+                #d.putCallItem('canonicalPath', '@QString', value, 'canonicalPath')
+                with SubItem(d, 'absolutePath'):
+                    typ = d.lookupType(ns + 'QString')
+                    d.putItem(d.createValue(privAddress + absoluteDirEntryOffset, typ))
+                with SubItem(d, 'entryInfoList'):
+                    typ = d.lookupType(ns + 'QFileInfo')
+                    qdumpHelper_QList(d, privAddress + fileInfosOffset, typ)
+                with SubItem(d, 'entryList'):
+                    typ = d.lookupType(ns + 'QStringList')
+                    d.putItem(d.createValue(privAddress + filesOffset, typ))
             d.putFields(value)
 
 
