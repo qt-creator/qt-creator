a27b50dcaf6231e8dadd6d26d03d204648f398c0
Friedemann Kleint <Friedemann.Kleint@qt.io>
2019-07-03 15:40:45 +0200

    algorithm.h: Fix compilation with Qt 5.13.1
    Fix the version check for the developer branch.
    
    Amends c2fd7743e07ce98c5852f1c2998ff63b54cbc596.
    
    Change-Id: I552330bcbbd00367d8b7044d3bf5dc6952d1a469
    Reviewed-by: hjk <hjk@qt.io>

--- src/libs/utils/algorithm.h
+++ src/libs/utils/algorithm.h
@@ -1249,7 +1249,7 @@ OutputIterator set_union(InputIterator1 first1,
 template <class T>
 QSet<T> toSet(const QList<T> &list)
 {
-#if (QT_VERSION <= QT_VERSION_CHECK(5, 13, 0))
+#if (QT_VERSION < QT_VERSION_CHECK(5, 14, 0))
     return list.toSet();
 #else
     return QSet<T>(list.begin(), list.end());
@@ -1259,7 +1259,7 @@ QSet<T> toSet(const QList<T> &list)
 template <class T>
 QList<T> toList(const QSet<T> &set)
 {
-#if (QT_VERSION <= QT_VERSION_CHECK(5, 13, 0))
+#if (QT_VERSION < QT_VERSION_CHECK(5, 14, 0))
     return set.toList();
 #else
     return QList<T>(set.begin(), set.end());
