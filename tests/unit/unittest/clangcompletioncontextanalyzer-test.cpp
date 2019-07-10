qt-creator/qt-creator

commit 82d6d20acb04ec970ee609e46e4f7d9543b416f4

Clang: Do not show completions after comma in initializer list
Appends 0852f889d1.

Do not automatically show global completion when initializer list
does not have a type, e.g. auto foo = {{},<cursor>};

Change-Id: I233fef71c60bb79211000df70bf5b04fa2d9df37
Reviewed-by: Marco Bubke <marco.bubke@qt.io>

src/plugins/clangcodemodel/clangcompletioncontextanalyzer.cpp
@@ -110,6 +110,8 @@ int ClangCompletionContextAnalyzer::startOfFunctionCall(int endOfOperator) const
     functionNameSelector.setPosition(functionNameStart);
     functionNameSelector.setPosition(index, QTextCursor::KeepAnchor);
     const QString functionName = functionNameSelector.selectedText().trimmed();
+    if (functionName.isEmpty() && m_completionOperator == T_LBRACE)
+        return endOfOperator;
 
     return functionName.isEmpty() ? -1 : functionNameStart;
 }
@@ -139,7 +141,10 @@ void ClangCompletionContextAnalyzer::handleCommaInFunctionCall()
         const int start = expressionUnderCursor.startOfFunctionCall(textCursor);
         m_positionEndOfExpression = start;
         m_positionForProposal = start + 1; // After '(' of function call
-        m_completionOperator = T_LPAREN;
+        if (m_interface->characterAt(start) == '(')
+            m_completionOperator = T_LPAREN;
+        else
+            m_completionOperator = T_LBRACE;
     }
 }
 

src/libs/cplusplus/ExpressionUnderCursor.cpp
@@ -264,7 +264,7 @@ int ExpressionUnderCursor::startOfFunctionCall(const QTextCursor &cursor) const
             break;
         } else if (tk.is(T_LPAREN) || tk.is(T_LBRACE)) {
             return scanner.startPosition() + tk.utf16charsBegin();
-        } else if (tk.is(T_RPAREN)) {
+        } else if (tk.is(T_RPAREN) || tk.is(T_RBRACE)) {
             int matchingBrace = scanner.startOfMatchingBrace(index);
 
             if (matchingBrace == index) // If no matching brace found

commit 2aa067f28e973df757b65830ba6c278ebc03e6e6

Clang: Fix adding snippets after opening brace
The regression was introduced by

    commit 82d6d20acb04ec970ee609e46e4f7d9543b416f4
    Clang: Do not show completions after comma in initializer list

This fixes ClangCodeCompletionTest::testCompleteGlobals().

Change-Id: Ic126f1e3eaed158196268e3682ff32bc02deb7b9
Reviewed-by: Cristian Adam <cristian.adam@qt.io>
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/clangcodemodel/clangcompletioncontextanalyzer.cpp
@@ -168,6 +168,7 @@ void ClangCompletionContextAnalyzer::handleFunctionCall(int afterOperatorPositio
         } else {
             const int functionNameStart = startOfFunctionCall(afterOperatorPosition);
             if (functionNameStart >= 0) {
+                m_addSnippets = functionNameStart == afterOperatorPosition;
                 // Always pass the position right after '(' to libclang because
                 // positions after the comma might be problematic if a preceding
                 // argument is invalid code.

tests/unit/unittest/clangcompletioncontextanalyzer-test.cpp
@@ -564,4 +564,25 @@ TEST_F(ClangCompletionContextAnalyzer, QualifiedFunctionNameStartPosition)
     ASSERT_THAT(functionNameStartPosition, 1);
 }
 
+TEST_F(ClangCompletionContextAnalyzer, SnippetsAfterOpeningBrace)
+{
+    auto analyzer = runAnalyzer("{@");
+
+    ASSERT_TRUE(analyzer.addSnippets());
 }
+
+TEST_F(ClangCompletionContextAnalyzer, NoSnippetsAfterFunctionCallLike_OpeningBrace)
+{
+    auto analyzer = runAnalyzer("foo{@");
+
+    ASSERT_FALSE(analyzer.addSnippets());
+}
+
+TEST_F(ClangCompletionContextAnalyzer, NoSnippetsAfterFunctionCallLike_OpeningParen)
+{
+    auto analyzer = runAnalyzer("foo(@");
+
+    ASSERT_FALSE(analyzer.addSnippets());
+}
+
+} // namespace