cf641fc04f6a3ba1f467bcb36d8ab0fb90bdf8a9
Christian Kandeler <christian.kandeler@qt.io>
2019-07-15 17:01:41 +0200

    RemoteLinux: Suppress "-j" option in MakeInstallStep
    Depending on the build system, the job count option as used in the
    MakeStep class might not be available.
    
    Task-number: QTCREATORBUG-22689
    Change-Id: I8c6cf31bbd270816a527ab465a2125d7dfc1f7de
    Reviewed-by: André Hartmann <aha_1980@gmx.de>

--- src/plugins/projectexplorer/makestep.h
+++ src/plugins/projectexplorer/makestep.h
@@ -71,7 +71,7 @@ public:
     static QString msgNoMakeCommand();
     static Task makeCommandMissingTask();
 
-    bool isJobCountSupported() const;
+    virtual bool isJobCountSupported() const;
     int jobCount() const;
     void setJobCount(int count);
     bool jobCountOverridesMakeflags() const;
