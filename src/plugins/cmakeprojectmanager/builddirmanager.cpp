qt-creator/qt-creator

commit d5383c92a8392b6a89584f6e823bdc1378d00264

CMake: Run cmake with all arguments when no CMakeCache.txt file is found
Always run cmake with all arguments when no CMakeCache.txt file is
found. This allows for the builddirreaders to become a bit simpler.

Change-Id: I67839fd560fb77f6832b1cefc3ff016baa6bae89
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/cmakeprojectmanager/builddirmanager.cpp
@@ -285,7 +285,9 @@ void BuildDirManager::parse(int reparseParameters)
 
     TaskHub::clearTasks(ProjectExplorer::Constants::TASK_CATEGORY_BUILDSYSTEM);
 
-    if (reparseParameters & REPARSE_CHECK_CONFIGURATION) {
+    if (m_parameters.workDirectory.toFileInfo().exists("CMakeCache.txt")) {
+        reparseParameters |= REPARSE_FORCE_CONFIGURATION | REPARSE_FORCE_CMAKE_RUN;
+    } else if (reparseParameters & REPARSE_CHECK_CONFIGURATION) {
         if (checkConfiguration())
             reparseParameters |= REPARSE_FORCE_CONFIGURATION;
     }

commit 700c88418cea9484c60e25448723a63b63a329cd

CMake: Fix inverted logic bug
This was introduced by d5383c92a8392b6a89584f6e823bdc1378d00264

Change-Id: I88736c14d4dc811b2b375ee2a9b97d3114524d01
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>
Reviewed-by: Cristian Adam <cristian.adam@qt.io>

src/plugins/cmakeprojectmanager/builddirmanager.cpp
@@ -291,7 +291,7 @@ void BuildDirManager::parse(int reparseParameters)
 
     TaskHub::clearTasks(ProjectExplorer::Constants::TASK_CATEGORY_BUILDSYSTEM);
 
-    if (m_parameters.workDirectory.toFileInfo().exists("CMakeCache.txt")) {
+    if (!m_parameters.workDirectory.toFileInfo().exists("CMakeCache.txt")) {
         reparseParameters |= REPARSE_FORCE_CONFIGURATION | REPARSE_FORCE_CMAKE_RUN;
     } else if (reparseParameters & REPARSE_CHECK_CONFIGURATION) {
         if (checkConfiguration())