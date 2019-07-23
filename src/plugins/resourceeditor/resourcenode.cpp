qt-creator/qt-creator

commit 92da9d1e6764c1f3f7e08bf69c35f02b9bb10406

ProjectExplorer: De-virtualize FolderNode::showWhenEmpty
Change-Id: Id3d019f449afcfe72b92d505c7822c3b10e2e8e7
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

src/plugins/resourceeditor/resourcenode.cpp
@@ -489,11 +489,6 @@ bool ResourceTopLevelNode::showInSimpleTree() const
     return true;
 }
 
-bool ResourceTopLevelNode::showWhenEmpty() const
-{
-    return true;
-}
-
 ResourceFolderNode::ResourceFolderNode(const QString &prefix, const QString &lang, ResourceTopLevelNode *parent)
     : FolderNode(FileName(parent->filePath()).appendPath(prefix)),
       // TOOD Why add existing directory doesn't work
@@ -501,7 +496,7 @@ ResourceFolderNode::ResourceFolderNode(const QString &prefix, const QString &lan
       m_prefix(prefix),
       m_lang(lang)
 {
-
+    setShowWhenEmpty(true);
 }
 
 ResourceFolderNode::~ResourceFolderNode() = default;

src/plugins/projectexplorer/projectnodes.cpp
@@ -762,7 +762,7 @@ bool FolderNode::showInSimpleTree() const
 
 bool FolderNode::showWhenEmpty() const
 {
-    return false;
+    return m_showWhenEmpty;
 }
 
 /*!
@@ -902,6 +902,11 @@ void FolderNode::handleSubTreeChanged(FolderNode *node)
 {
     if (FolderNode *parent = parentFolderNode())
         parent->handleSubTreeChanged(node);
+}
+
+void FolderNode::setShowWhenEmpty(bool showWhenEmpty)
+{
+    m_showWhenEmpty = showWhenEmpty;
 }
 
 ContainerNode::ContainerNode(Project *project)

src/plugins/projectexplorer/projectnodes.h
@@ -281,8 +281,10 @@ public:
 
     // determines if node will be shown in the flat view, by default folder and projects aren't shown
     virtual bool showInSimpleTree() const;
+
     // determines if node will always be shown when hiding empty directories
-    virtual bool showWhenEmpty() const;
+    bool showWhenEmpty() const;
+    void setShowWhenEmpty(bool showWhenEmpty);
 
     void addNode(std::unique_ptr<Node> &&node);
     std::unique_ptr<Node> takeNode(Node *node);
@@ -302,6 +304,7 @@ private:
     QString m_displayName;
     QString m_addFileFilter;
     mutable QIcon m_icon;
+    bool m_showWhenEmpty = false;
 };
 
 class PROJECTEXPLORER_EXPORT VirtualFolderNode : public FolderNode

src/plugins/resourceeditor/resourcenode.h
@@ -50,7 +50,6 @@ public:
 
     AddNewInformation addNewInformation(const QStringList &files, Node *context) const override;
     bool showInSimpleTree() const override;
-    bool showWhenEmpty() const override;
     bool removeNonExistingFiles();
 
     QString contents() const { return m_contents; }

commit 9b1edfb37115250e1faef315816673770d89deab

ProjectExplorer: Do not hide empty qrc files
This broke with 92da9d1e67.

Fixes: QTCREATORBUG-22737
Change-Id: Ic4ec9f298e3e7c1b35b1c94096e5684555f48b2b
Reviewed-by: hjk <hjk@qt.io>

src/plugins/resourceeditor/resourcenode.cpp
@@ -233,6 +233,7 @@ ResourceTopLevelNode::ResourceTopLevelNode(const FilePath &filePath,
     setPriority(Node::DefaultFilePriority);
     setListInProject(true);
     setAddFileFilter("*.png; *.jpg; *.gif; *.svg; *.ico; *.qml; *.qml.ui");
+    setShowWhenEmpty(true);
 
     if (!filePath.isEmpty()) {
         QFileInfo fi = filePath.toFileInfo();
@@ -468,7 +469,6 @@ ResourceFolderNode::ResourceFolderNode(const QString &prefix, const QString &lan
       m_prefix(prefix),
       m_lang(lang)
 {
-    setShowWhenEmpty(true);
 }
 
 ResourceFolderNode::~ResourceFolderNode() = default;