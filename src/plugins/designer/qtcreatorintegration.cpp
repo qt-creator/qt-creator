qt-creator/qt-creator

commit e3b1106afae5de6cd54ce627a0b11be041624591

Compile fix with recent Qt dev
The reasoning in 1b4766e26c6b did not take into account that the scope
of QT_NO_JAVA_STYLE_ITERATORS may change over time, as done with
f70905448f6 in Qt base.

Change-Id: Ib1966ff26c4d36d5f62e149d6b45baa4aecf825d
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/designer/qtcreatorintegration.cpp
@@ -516,13 +516,11 @@ bool QtCreatorIntegration::navigateToSlot(const QString &objectName,
                 newDocTable.insert(i.value());
         }
     } else {
-        const CppTools::WorkingCopy workingCopy =
-                CppTools::CppModelManager::instance()->workingCopy();
         const Utils::FilePath configFileName =
                 Utils::FilePath::fromString(CppTools::CppModelManager::configurationFileName());
-        QHashIterator<Utils::FilePath, QPair<QByteArray, unsigned> > it = workingCopy.iterator();
-        while (it.hasNext()) {
-            it.next();
+        const CppTools::WorkingCopy::Table &elements =
+                CppTools::CppModelManager::instance()->workingCopy().elements();
+        for (auto it = elements.cbegin(), end = elements.cend(); it != end; ++it) {
             const Utils::FilePath &fileName = it.key();
             if (fileName != configFileName)
                 newDocTable.insert(docTable.document(fileName));

commit 0bd07243ed162383d4eb5c0d50f2cb6d493b1b5f

Designer: Fix crash when executing plugin unit tests
Broke with e3b1106afae5.

Change-Id: Ifc35bfa1a4e0985427ea60f49609ff0fc3528a4d
Reviewed-by: Nikolai Kosjar <nikolai.kosjar@qt.io>

src/plugins/designer/qtcreatorintegration.cpp
@@ -518,7 +518,7 @@ bool QtCreatorIntegration::navigateToSlot(const QString &objectName,
     } else {
         const Utils::FilePath configFileName =
                 Utils::FilePath::fromString(CppTools::CppModelManager::configurationFileName());
-        const CppTools::WorkingCopy::Table &elements =
+        const CppTools::WorkingCopy::Table elements =
                 CppTools::CppModelManager::instance()->workingCopy().elements();
         for (auto it = elements.cbegin(), end = elements.cend(); it != end; ++it) {
             const Utils::FilePath &fileName = it.key();