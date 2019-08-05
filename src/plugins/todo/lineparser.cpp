qt-creator/qt-creator

commit 94041d7b449983d18af29f0c9c81d602f5635715

Todo: Replace last use of Java-style iterators
Change-Id: I4c6b4910522f2ec9eff6a7f615ea91bfaf99077d
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/todo/lineparser.cpp
@@ -104,17 +104,13 @@ QList<LineParser::KeywordEntry> LineParser::keywordEntriesFromCandidates(
         return QList<KeywordEntry>();
 
     // Convert candidates to entries
+    std::vector<KeywordEntry> tmp;
+    for (auto it = candidates.cbegin(), end = candidates.cend(); it != end; ++it)
+        tmp.emplace_back(it.key(), it.value(), QString());
+
     QList<KeywordEntry> entries;
-    QMapIterator<int, int> i(candidates);
-    i.toBack();
-
-    while (i.hasPrevious()) {
-        i.previous();
-
-        KeywordEntry entry;
-
-        entry.keywordStart = i.key();
-        entry.keywordIndex = i.value();
+    for (auto it = tmp.crbegin(), end = tmp.crend(); it != end; ++it) {
+        KeywordEntry entry = *it;
 
         int keywordLength = m_keywords.at(entry.keywordIndex).name.length();
 

commit 9d02477ae6244c33a158e31d890a468a86d1e466

Fix crash in TODO plugin
There was a mixup of keyword index and start introduced in
94041d7b449983d18af29f0c9c81d602f5635715

Change-Id: I1be979762d6ccab8a60382f919288278ddf136c6
Reviewed-by: hjk <hjk@qt.io>

src/plugins/todo/lineparser.cpp
@@ -106,7 +106,7 @@ QList<LineParser::KeywordEntry> LineParser::keywordEntriesFromCandidates(
     // Convert candidates to entries
     std::vector<KeywordEntry> tmp;
     for (auto it = candidates.cbegin(), end = candidates.cend(); it != end; ++it)
-        tmp.emplace_back(KeywordEntry{it.key(), it.value(), QString()});
+        tmp.emplace_back(KeywordEntry{it.value(), it.key(), QString()});
 
     QList<KeywordEntry> entries;
     for (auto it = tmp.crbegin(), end = tmp.crend(); it != end; ++it) {