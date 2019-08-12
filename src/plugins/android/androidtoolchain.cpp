qt-creator/qt-creator

commit 33a9a03121c1ee4b2c56f8024a457b27b202d622

Android: Avoid a temporary QFileInfo
Change-Id: I58f31177506c1e25f07dc51d5b24ac368ebf9024
Reviewed-by: Orgad Shaneh <orgads@gmail.com>
Reviewed-by: Vikas Pachdha <vikas.pachdha@qt.io>

src/plugins/android/androidtoolchain.cpp
@@ -93,7 +93,7 @@ void AndroidToolChain::addToEnvironment(Environment &env) const
     env.set(QLatin1String("ANDROID_NDK_HOST"),
             AndroidConfigurations::currentConfig().toolchainHost());
     const Utils::FileName javaHome = AndroidConfigurations::currentConfig().openJDKLocation();
-    if (!javaHome.isEmpty() && javaHome.toFileInfo().exists()) {
+    if (!javaHome.exists()) {
         env.set(QLatin1String("JAVA_HOME"), javaHome.toString());
         const FileName javaBin = javaHome.pathAppended("bin");
         if (!Utils::contains(env.path(), [&javaBin](const Utils::FileName &p) { return p == javaBin; }))

commit 821f799ff3d017e9cdc8bfe3c96fed1982de8753

Android: Fix adding Java SDK to environment
33a9a03121c1ee4b2c56f8024a457b27b202d622 broke it by inverting the
condition.

Task-number: QTCREATORBUG-22504
Change-Id: Id0b66ec025281e6d6c52b7309f35a3bdfb8e7170
Reviewed-by: hjk <hjk@qt.io>

src/plugins/android/androidtoolchain.cpp
@@ -93,7 +93,7 @@ void AndroidToolChain::addToEnvironment(Environment &env) const
     env.set(QLatin1String("ANDROID_NDK_HOST"),
             AndroidConfigurations::currentConfig().toolchainHost());
     const Utils::FilePath javaHome = AndroidConfigurations::currentConfig().openJDKLocation();
-    if (!javaHome.exists()) {
+    if (javaHome.exists()) {
         env.set(QLatin1String("JAVA_HOME"), javaHome.toString());
         const FilePath javaBin = javaHome.pathAppended("bin");
         if (!Utils::contains(env.path(), [&javaBin](const Utils::FilePath &p) { return p == javaBin; }))