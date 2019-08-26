qt-creator/qt-creator

commit 5dbfd46bcfe177b8308c70a66f7d404429249f6b

ProjectExplorer: Merge BuildConfigurationFactory::availableBuilds
... and availableSetups as far as mechanically possible.

Change-Id: Ia1d7babe943eea25da97cef7838187c234378673
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

src/plugins/cmakeprojectmanager/cmakebuildconfiguration.cpp
@@ -481,31 +481,21 @@ BuildConfiguration::BuildType CMakeBuildConfigurationFactory::cmakeBuildTypeToBu
         return BuildConfiguration::Unknown;
 }
 
-QList<BuildInfo>
-    CMakeBuildConfigurationFactory::availableBuilds(const Kit *k, const FilePath &projectPath) const
+QList<BuildInfo> CMakeBuildConfigurationFactory::availableBuilds
+    (const Kit *k, const FilePath &projectPath, bool forSetup) const
 {
     QList<BuildInfo> result;
+
+    FilePath path = forSetup ? Project::projectDirectory(projectPath) : projectPath;
 
     for (int type = BuildTypeNone; type != BuildTypeLast; ++type) {
-        result << createBuildInfo(k,
-                                  projectPath.toString(),
-                                  BuildType(type));
-    }
-    return result;
-}
-
-QList<BuildInfo>
-    CMakeBuildConfigurationFactory::availableSetups(const Kit *k, const FilePath &projectPath) const
-{
-    QList<BuildInfo> result;
-    for (int type = BuildTypeDebug; type != BuildTypeLast; ++type) {
-        BuildInfo info = createBuildInfo(k,
-                                         ProjectExplorer::Project::projectDirectory(projectPath).toString(),
-                                         BuildType(type));
-        info.displayName = info.typeName;
-        info.buildDirectory
-                = CMakeBuildConfiguration::shadowBuildDirectory(projectPath, k,
-                                                                info.displayName, info.buildType);
+        BuildInfo info = createBuildInfo(k, path.toString(), BuildType(type));
+        if (forSetup) {
+            info.displayName = info.typeName;
+            info.buildDirectory
+                    = CMakeBuildConfiguration::shadowBuildDirectory(projectPath, k,
+                                                                    info.displayName, info.buildType);
+        }
         result << info;
     }
     return result;

src/plugins/autotoolsprojectmanager/autotoolsbuildconfiguration.cpp
@@ -97,28 +97,18 @@ AutotoolsBuildConfigurationFactory::AutotoolsBuildConfigurationFactory()
     setSupportedProjectMimeTypeName(Constants::MAKEFILE_MIMETYPE);
 }
 
-QList<BuildInfo> AutotoolsBuildConfigurationFactory::availableBuilds(const Kit *k, const FilePath &projectPath) const
-{
-    return {createBuildInfo(k, projectPath)};
-}
-
-QList<BuildInfo> AutotoolsBuildConfigurationFactory::availableSetups(const Kit *k, const FilePath &projectPath) const
-{
-    const QString path = projectPath.toFileInfo().absolutePath();
-    BuildInfo info = createBuildInfo(k, Utils::FilePath::fromString(path));
-    //: The name of the build configuration created by default for a autotools project.
-    info.displayName = tr("Default");
-    return {info};
-}
-
-BuildInfo AutotoolsBuildConfigurationFactory::createBuildInfo(const Kit *k,
-                                                              const Utils::FilePath &buildDir) const
+QList<BuildInfo> AutotoolsBuildConfigurationFactory::availableBuilds
+    (const Kit *k, const FilePath &projectPath, bool forSetup) const
 {
     BuildInfo info(this);
     info.typeName = tr("Build");
-    info.buildDirectory = buildDir;
+    info.buildDirectory = forSetup ? FilePath::fromString(projectPath.toFileInfo().absolutePath()) : projectPath;
     info.kitId = k->id();
-    return info;
+    if (forSetup) {
+        //: The name of the build configuration created by default for a autotools project.
+        info.displayName = tr("Default");
+    }
+    return {info};
 }
 
 BuildConfiguration::BuildType AutotoolsBuildConfiguration::buildType() const

src/plugins/autotoolsprojectmanager/autotoolsbuildconfiguration.h
@@ -29,8 +29,6 @@
 
 #include <projectexplorer/buildconfiguration.h>
 
-namespace Utils { class FilePath; }
-
 namespace AutotoolsProjectManager {
 namespace Internal {
 
@@ -54,11 +52,8 @@ public:
 
 private:
     QList<ProjectExplorer::BuildInfo> availableBuilds(const ProjectExplorer::Kit *k,
-                                                      const Utils::FilePath &projectPath) const override;
-    QList<ProjectExplorer::BuildInfo> availableSetups(const ProjectExplorer::Kit *k,
-                                                      const Utils::FilePath &projectPath) const override;
-
-    ProjectExplorer::BuildInfo createBuildInfo(const ProjectExplorer::Kit *k, const Utils::FilePath &buildDir) const;
+                                                      const Utils::FilePath &projectPath,
+                                                      bool forSetup) const override;
 };
 
 } // namespace Internal

src/plugins/cmakeprojectmanager/cmakebuildconfiguration.h
@@ -135,9 +135,8 @@ public:
     static ProjectExplorer::BuildConfiguration::BuildType cmakeBuildTypeToBuildType(const BuildType &in);
 
     QList<ProjectExplorer::BuildInfo> availableBuilds(const ProjectExplorer::Kit *k,
-                                                      const Utils::FilePath &projectPath) const override;
-    QList<ProjectExplorer::BuildInfo> availableSetups(const ProjectExplorer::Kit *k,
-                                                      const Utils::FilePath &projectPath) const override;
+                                                      const Utils::FilePath &projectPath,
+                                                      bool forSetup) const override;
 
 private:
     ProjectExplorer::BuildInfo createBuildInfo(const ProjectExplorer::Kit *k,

src/plugins/compilationdatabaseprojectmanager/compilationdatabaseproject.cpp
@@ -59,6 +59,7 @@
 #endif
 
 using namespace ProjectExplorer;
+using namespace Utils;
 
 namespace CompilationDatabaseProjectManager {
 namespace Internal {
@@ -529,26 +530,15 @@ CompilationDatabaseBuildConfigurationFactory::CompilationDatabaseBuildConfigurat
     setSupportedProjectMimeTypeName(Constants::COMPILATIONDATABASEMIMETYPE);
 }
 
-static QList<ProjectExplorer::BuildInfo> defaultBuildInfos(
-    const ProjectExplorer::BuildConfigurationFactory *factory, const QString &name)
+QList<BuildInfo> CompilationDatabaseBuildConfigurationFactory::availableBuilds
+    (const Kit *, const FilePath &, bool) const
 {
-    ProjectExplorer::BuildInfo info(factory);
+    const QString name = tr("Release");
+    ProjectExplorer::BuildInfo info(this);
     info.typeName = name;
     info.displayName = name;
     info.buildType = BuildConfiguration::Release;
-    QList<ProjectExplorer::BuildInfo> buildInfos;
-    buildInfos << info;
-    return buildInfos;
-}
-
-QList<BuildInfo> CompilationDatabaseBuildConfigurationFactory::availableBuilds(const Kit *, const Utils::FilePath &) const
-{
-    return defaultBuildInfos(this, tr("Release"));
-}
-
-QList<BuildInfo> CompilationDatabaseBuildConfigurationFactory::availableSetups(const Kit *, const Utils::FilePath &) const
-{
-    return defaultBuildInfos(this, tr("Release"));
+    return {info};
 }
 
 } // namespace Internal

src/plugins/compilationdatabaseprojectmanager/compilationdatabaseproject.h
@@ -104,10 +104,9 @@ class CompilationDatabaseBuildConfigurationFactory
 public:
     CompilationDatabaseBuildConfigurationFactory();
 
-    QList<ProjectExplorer::BuildInfo> availableBuilds(
-        const ProjectExplorer::Kit *k, const Utils::FilePath &projectPath) const override;
-    QList<ProjectExplorer::BuildInfo> availableSetups(
-        const ProjectExplorer::Kit *k, const Utils::FilePath &projectPath) const override;
+    QList<ProjectExplorer::BuildInfo> availableBuilds(const ProjectExplorer::Kit *k,
+                                                      const Utils::FilePath &projectPath,
+                                                      bool forSetup) const override;
 };
 
 } // namespace Internal

src/plugins/genericprojectmanager/genericbuildconfiguration.cpp
@@ -85,31 +85,20 @@ GenericBuildConfigurationFactory::GenericBuildConfigurationFactory()
     setSupportedProjectMimeTypeName(Constants::GENERICMIMETYPE);
 }
 
-GenericBuildConfigurationFactory::~GenericBuildConfigurationFactory() = default;
-
-QList<BuildInfo>
-    GenericBuildConfigurationFactory::availableBuilds(const Kit *k, const FilePath &projectPath) const
-{
-    return {createBuildInfo(k, projectPath)};
-}
-
-QList<BuildInfo>
-    GenericBuildConfigurationFactory::availableSetups(const Kit *k, const FilePath &projectPath) const
-{
-    BuildInfo info = createBuildInfo(k, Project::projectDirectory(projectPath));
-    //: The name of the build configuration created by default for a generic project.
-    info.displayName = tr("Default");
-    return {info};
-}
-
-BuildInfo
-    GenericBuildConfigurationFactory::createBuildInfo(const Kit *k, const FilePath &buildDir) const
+QList<BuildInfo> GenericBuildConfigurationFactory::availableBuilds
+    (const Kit *k, const FilePath &projectPath, bool forSetup) const
 {
     BuildInfo info(this);
     info.typeName = tr("Build");
-    info.buildDirectory = buildDir;
+    info.buildDirectory = forSetup ? Project::projectDirectory(projectPath) : projectPath;
     info.kitId = k->id();
-    return info;
+
+    if (forSetup)  {
+        //: The name of the build configuration created by default for a generic project.
+        info.displayName = tr("Default");
+    }
+
+    return {info};
 }
 
 BuildConfiguration::BuildType GenericBuildConfiguration::buildType() const

src/plugins/genericprojectmanager/genericbuildconfiguration.h
@@ -50,15 +50,11 @@ class GenericBuildConfigurationFactory : public ProjectExplorer::BuildConfigurat
 
 public:
     GenericBuildConfigurationFactory();
-    ~GenericBuildConfigurationFactory() override;
 
 private:
     QList<ProjectExplorer::BuildInfo> availableBuilds(const ProjectExplorer::Kit *k,
-                                                      const Utils::FilePath &projectPath) const override;
-    QList<ProjectExplorer::BuildInfo> availableSetups(const ProjectExplorer::Kit *k,
-                                                      const Utils::FilePath &projectPath) const override;
-
-    ProjectExplorer::BuildInfo createBuildInfo(const ProjectExplorer::Kit *k, const Utils::FilePath &buildDir) const;
+                                                      const Utils::FilePath &projectPath,
+                                                      bool forSetup) const override;
 };
 
 } // namespace Internal

commit 10d3772b7acfeda1b9ff49a343498e04670a80fc

CMake: Fix regression that a "Build" build configuration was available
And the default selected when configuring a project, since it was the
first.

Broke with 5dbfd46bcfe177b8308c70a66f7d404429249f6b which merged
"availableBuilds" and "availableSetups".

Change-Id: Ibc8c241595f3fee6f35e08fa83e499550eff869b
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/plugins/cmakeprojectmanager/cmakebuildconfiguration.cpp
@@ -623,7 +623,7 @@ QList<BuildInfo> CMakeBuildConfigurationFactory::availableBuilds(const Kit *k,
 
     FilePath path = forSetup ? Project::projectDirectory(projectPath) : projectPath;
 
-    for (int type = BuildTypeNone; type != BuildTypeLast; ++type) {
+    for (int type = BuildTypeDebug; type != BuildTypeLast; ++type) {
         BuildInfo info = createBuildInfo(k, path.toString(), BuildType(type));
         if (forSetup) {
             info.displayName = info.typeName;