qt-creator/qt-creator

commit d1b997474d6107b90d09451a45288e7d564b533f

CppLocatorFilter: Add highlighting auto-tests
Change-Id: Ibd5b8318040876b46bfb14561c69019abd8dd8b7
Reviewed-by: Nikolai Kosjar <nikolai.kosjar@qt.io>

src/plugins/coreplugin/locator/locatorfiltertest.cpp
@@ -56,21 +56,36 @@ QList<LocatorFilterEntry> BasicLocatorFilterTest::matchesFor(const QString &sear
 
 ResultData::ResultData() = default;
 
-ResultData::ResultData(const QString &textColumn1, const QString &textColumn2)
-    : textColumn1(textColumn1), textColumn2(textColumn2)
+ResultData::ResultData(const QString &textColumn1, const QString &textColumn2,
+                       const QString &highlightPositions)
+    : textColumn1(textColumn1), textColumn2(textColumn2), highlight(highlightPositions)
 {
 }
 
 bool ResultData::operator==(const ResultData &other) const
 {
-    return textColumn1 == other.textColumn1 && textColumn2 == other.textColumn2;
+    const bool highlightEqual = highlight.isEmpty()
+            || other.highlight.isEmpty()
+            || highlight == other.highlight;
+
+    return textColumn1 == other.textColumn1 && textColumn2 == other.textColumn2 && highlightEqual;
 }
 
 ResultData::ResultDataList ResultData::fromFilterEntryList(const QList<LocatorFilterEntry> &entries)
 {
     ResultDataList result;
-    for (const LocatorFilterEntry &entry : entries)
-        result << ResultData(entry.displayName, entry.extraInfo);
+    for (const LocatorFilterEntry &entry : entries) {
+        ResultData data(entry.displayName, entry.extraInfo);
+        data.highlight = QString(entry.displayName.size(), QChar(' '));
+        for (int i = 0; i < entry.highlightInfo.starts.size(); ++i) {
+            const int start = entry.highlightInfo.starts.at(i);
+            const int length = entry.highlightInfo.lengths.at(i);
+            for (int j = 0; j < length; ++j)
+                data.highlight[start + j] = '~';
+        }
+        result << data;
+    }
+
     return result;
 }
 

src/plugins/coreplugin/locator/locatorfiltertest.h
@@ -54,7 +54,8 @@ public:
     using ResultDataList = QList<ResultData>;
 
     ResultData();
-    ResultData(const QString &textColumn1, const QString &textColumn2);
+    ResultData(const QString &textColumn1, const QString &textColumn2,
+               const QString &highlightPositions = QString());
 
     bool operator==(const ResultData &other) const;
 
@@ -65,6 +66,8 @@ public:
 
     QString textColumn1;
     QString textColumn2;
+    QString highlight;
+    LocatorFilterEntry::HighlightInfo::DataType dataType;
 };
 
 using ResultDataList = ResultData::ResultDataList;
@@ -80,7 +83,8 @@ namespace QTest {
 
 template<> inline char *toString(const Core::Tests::ResultData &data)
 {
-    QByteArray ba = "\"" + data.textColumn1.toUtf8() + "\", \"" + data.textColumn2.toUtf8() + "\"";
+    const QByteArray ba = "\n\"" + data.textColumn1.toUtf8() + "\", \"" + data.textColumn2.toUtf8()
+            + "\"\n\"" + data.highlight.toUtf8() + "\"";
     return qstrdup(ba.data());
 }
 

src/plugins/cpptools/cpplocatorfilter_test.cpp
@@ -93,14 +93,15 @@ class CppCurrentDocumentFilterTestCase
 {
 public:
     CppCurrentDocumentFilterTestCase(const QString &fileName,
-                                     const ResultDataList &expectedResults)
+                                     const ResultDataList &expectedResults,
+                                     const QString &searchText = QString())
         : BasicLocatorFilterTest(CppTools::CppModelManager::instance()->currentDocumentFilter())
         , m_fileName(fileName)
     {
         QVERIFY(succeededSoFar());
         QVERIFY(!m_fileName.isEmpty());
 
-        ResultDataList results = ResultData::fromFilterEntryList(matchesFor());
+        ResultDataList results = ResultData::fromFilterEntryList(matchesFor(searchText));
         if (debug) {
             ResultData::printFilterEntries(expectedResults, "Expected:");
             ResultData::printFilterEntries(results, "Results:");
@@ -371,4 +372,54 @@ void CppToolsPlugin::test_cpplocatorfilters_CppCurrentDocumentFilter()
     };
 
     CppCurrentDocumentFilterTestCase(testFile, expectedResults);
+}
+
+void CppToolsPlugin::test_cpplocatorfilters_CppCurrentDocumentHighlighting()
+{
+    MyTestDataDir testDirectory("testdata_basic");
+    const QString testFile = testDirectory.file("file1.cpp");
+
+    const QString searchText = "pos";
+    const ResultDataList expectedResults{
+        ResultData("Pos", "",
+                   "~~~"),
+        ResultData("pointOfService()", "",
+                   "~    ~ ~        "),
+        ResultData("positiveNumber()", "",
+                   "~~~             "),
+        ResultData("somePositionWithin()", "",
+                   "    ~~~             "),
+        ResultData("matchArgument(Pos)", "",
+                   "              ~~~ ")
+       };
+
+    Tests::VerifyCleanCppModelManager verify;
+
+    CppCurrentDocumentFilterTestCase(testFile, expectedResults, searchText);
+}
+
+void CppToolsPlugin::test_cpplocatorfilters_CppFunctionsFilterHighlighting()
+{
+    MyTestDataDir testDirectory("testdata_basic");
+    const QString testFile = testDirectory.file("file1.cpp");
+    const QString testFileShort = FilePath::fromString(testFile).shortNativePath();
+
+    const QString searchText = "pos";
+    const ResultDataList expectedResults{
+        ResultData("positiveNumber()", testFileShort,
+                   "~~~             "),
+        ResultData("somePositionWithin()", testFileShort,
+                   "    ~~~             "),
+        ResultData("pointOfService()", testFileShort,
+                   "~    ~ ~        "),
+        ResultData("matchArgument(Pos)", testFileShort,
+                   "              ~~~ ")
+       };
+
+    CppModelManager *cppModelManager = CppModelManager::instance();
+    ILocatorFilter *filter = cppModelManager->functionsFilter();
+
+    Tests::VerifyCleanCppModelManager verify;
+
+    CppLocatorFilterTestCase(filter, testFile, searchText, expectedResults);
 }

src/plugins/cpptools/cpptoolsplugin.h
@@ -159,6 +159,8 @@ private slots:
     void test_cpplocatorfilters_CppLocatorFilter();
     void test_cpplocatorfilters_CppLocatorFilter_data();
     void test_cpplocatorfilters_CppCurrentDocumentFilter();
+    void test_cpplocatorfilters_CppCurrentDocumentHighlighting();
+    void test_cpplocatorfilters_CppFunctionsFilterHighlighting();
 
     void test_builtinsymbolsearcher();
     void test_builtinsymbolsearcher_data();

commit c3b4dbe4e2cfa0a168f51e81f6b7ae43bf61774c

CppLocatorFilter: Fix crash in tests
Introduced by d1b997474d6107b90d09451a45288e7d564b533f

Change-Id: I3d5a7cff36be362ae048ce0eb1b09bc325ec8246
Reviewed-by: Nikolai Kosjar <nikolai.kosjar@qt.io>

src/plugins/coreplugin/locator/locatorfiltertest.cpp
@@ -80,6 +80,8 @@ ResultData::ResultDataList ResultData::fromFilterEntryList(const QList<LocatorFi
         for (int i = 0; i < entry.highlightInfo.starts.size(); ++i) {
             const int start = entry.highlightInfo.starts.at(i);
             const int length = entry.highlightInfo.lengths.at(i);
+            if (start < 0 || start + length > data.highlight.size())
+                break;
             for (int j = 0; j < length; ++j)
                 data.highlight[start + j] = '~';
         }