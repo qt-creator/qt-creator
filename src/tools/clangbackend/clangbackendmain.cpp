qt-creator/qt-creator

commit ccab8354551ab1e0b4c79bba66469e32945a750b

Debugging: Enable Windows debugging for clang tools
On Windows the debugging of clang tools is not working due to the
nature of how processes are started.

This makes it possible to use DebugViewer or DebugViewPP

Change-Id: I2c1e4bb7a8fafd6f07570e5d2ba4df5b44e9d44d
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/tools/clangbackend/clangbackendmain.cpp
@@ -57,25 +57,11 @@ QString processArguments(QCoreApplication &application)
 }
 
 #ifdef Q_OS_WIN
-struct MessageHandler {
-    MessageHandler(QtMessageHandler handler)
-    {
-        defaultHandler = qInstallMessageHandler(handler);
-    }
-
-    ~MessageHandler()
-    {
-        qInstallMessageHandler(defaultHandler);
-    }
-
-    static QtMessageHandler defaultHandler;
-};
-
-QtMessageHandler MessageHandler::defaultHandler = nullptr;
-
-static void messageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+extern "C" void OutputDebugStringW(const wchar_t* msg);
+static void messageOutput(QtMsgType type, const QMessageLogContext &/*context*/,
+                          const QString &msg)
 {
-    MessageHandler::defaultHandler(type, context, msg);
+    OutputDebugStringW(msg.toStdWString().c_str());
     std::wcout << msg.toStdWString() << std::endl;
     if (type == QtFatalMsg)
         abort();
@@ -85,7 +71,7 @@ static void messageOutput(QtMsgType type, const QMessageLogContext &context, con
 int main(int argc, char *argv[])
 {
 #ifdef Q_OS_WIN
-    MessageHandler messageHandler(&messageOutput);
+     qInstallMessageHandler(&messageOutput);
 #endif
     QCoreApplication::setOrganizationName(QStringLiteral("QtProject"));
     QCoreApplication::setOrganizationDomain(QStringLiteral("qt-project.org"));

src/tools/clangpchmanagerbackend/clangpchmanagerbackendmain.cpp
@@ -247,8 +247,10 @@ struct Data // because we have a cycle dependency
 };
 
 #ifdef Q_OS_WIN
+extern "C" void OutputDebugStringW(const wchar_t* msg);
 static void messageOutput(QtMsgType type, const QMessageLogContext &, const QString &msg)
 {
+    OutputDebugStringW(msg.toStdWString().c_str());
     std::wcout << msg.toStdWString() << std::endl;
     if (type == QtFatalMsg)
         abort();

src/tools/clangrefactoringbackend/clangrefactoringbackendmain.cpp
@@ -131,8 +131,10 @@ struct Data // because we have a cycle dependency
 };
 
 #ifdef Q_OS_WIN
+extern "C" void OutputDebugStringW(const wchar_t* msg);
 static void messageOutput(QtMsgType type, const QMessageLogContext &, const QString &msg)
 {
+    OutputDebugStringW(msg.toStdWString().c_str());
     std::wcout << msg.toStdWString() << std::endl;
     if (type == QtFatalMsg)
         abort();

commit 2e63d2c7d37d76fd6d2b87e37aa52a570e236adf

Fix build of clangrefactoring backend on Windows
Broke in ccab8354551ab1e0b4c79bba66469e32945a750b

Change-Id: Ic5db7597cb6cc3c798987997d8fe50c4d801ec8e
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/tools/clangbackend/clangbackendmain.cpp
@@ -57,7 +57,7 @@ QString processArguments(QCoreApplication &application)
 }
 
 #ifdef Q_OS_WIN
-extern "C" void OutputDebugStringW(const wchar_t* msg);
+extern "C" void __stdcall OutputDebugStringW(const wchar_t* msg);
 static void messageOutput(QtMsgType type, const QMessageLogContext &/*context*/,
                           const QString &msg)
 {

src/tools/clangpchmanagerbackend/clangpchmanagerbackendmain.cpp
@@ -247,7 +247,7 @@ struct Data // because we have a cycle dependency
 };
 
 #ifdef Q_OS_WIN
-extern "C" void OutputDebugStringW(const wchar_t* msg);
+extern "C" void __stdcall OutputDebugStringW(const wchar_t* msg);
 static void messageOutput(QtMsgType type, const QMessageLogContext &, const QString &msg)
 {
     OutputDebugStringW(msg.toStdWString().c_str());

src/tools/clangrefactoringbackend/clangrefactoringbackendmain.cpp
@@ -131,7 +131,7 @@ struct Data // because we have a cycle dependency
 };
 
 #ifdef Q_OS_WIN
-extern "C" void OutputDebugStringW(const wchar_t* msg);
+extern "C" void __stdcall OutputDebugStringW(const wchar_t* msg);
 static void messageOutput(QtMsgType type, const QMessageLogContext &, const QString &msg)
 {
     OutputDebugStringW(msg.toStdWString().c_str());