qt-creator/qt-creator

commit ad6968f45f5c04d0f0cb3b7bae74f985b55bd353

Git: Add a toggle button for Follow Renames on Log
Fixes: QTCREATORBUG-22826
Change-Id: I6d5075dfe16bc3c5d0914dc182523f0295379396
Reviewed-by: André Hartmann <aha_1980@gmx.de>

src/plugins/git/gitclient.cpp
@@ -635,6 +635,10 @@ public:
                                                tr("Show textual graph log."));
         mapSetting(graphButton, settings.boolPointer(GitSettings::graphLogKey));
 
+        QAction *followButton = addToggleButton("--follow", tr("Follow"),
+                                               tr("Show log also for previous names of the file."));
+        mapSetting(followButton, settings.boolPointer(GitSettings::followRenamesKey));
+
         addButton(tr("Reload"), Utils::Icons::RELOAD.icon());
     }
 };
@@ -1032,7 +1036,7 @@ void GitClient::log(const QString &workingDirectory, const QString &fileName,
     arguments << argWidget->arguments();
 
     if (!fileName.isEmpty())
-        arguments << "--follow" << "--" << fileName;
+        arguments << "--" << fileName;
 
     vcsExec(workingDir, arguments, editor);
 }

src/plugins/git/gitsettings.cpp
@@ -45,6 +45,7 @@ const QLatin1String GitSettings::logDiffKey("LogDiff");
 const QLatin1String GitSettings::repositoryBrowserCmd("RepositoryBrowserCmd");
 const QLatin1String GitSettings::graphLogKey("GraphLog");
 const QLatin1String GitSettings::firstParentKey("FirstParent");
+const QLatin1String GitSettings::followRenamesKey("FollowRenames");
 const QLatin1String GitSettings::lastResetIndexKey("LastResetIndex");
 
 GitSettings::GitSettings()
@@ -66,6 +67,7 @@ GitSettings::GitSettings()
     declareKey(repositoryBrowserCmd, QString());
     declareKey(graphLogKey, false);
     declareKey(firstParentKey, false);
+    declareKey(followRenamesKey, true);
     declareKey(lastResetIndexKey, 0);
 }
 

src/plugins/git/gitsettings.h
@@ -56,6 +56,7 @@ public:
     static const QLatin1String repositoryBrowserCmd;
     static const QLatin1String graphLogKey;
     static const QLatin1String firstParentKey;
+    static const QLatin1String followRenamesKey;
     static const QLatin1String lastResetIndexKey;
 
     Utils::FilePath gitExecutable(bool *ok = nullptr, QString *errorMessage = nullptr) const;

commit 4ae6c2a89bf9ba5c589a3e1b4d61e99a8acba4fc

Git: Fix log for repo/project
Broke by ad6968f45f5c04d0f0cb3b7bae74f985b55bd353.

Change-Id: I06ff8384a088f6d9137b9711f855efa8d6bf4a74
Reviewed-by: André Hartmann <aha_1980@gmx.de>
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/git/gitclient.cpp
@@ -612,7 +612,7 @@ class GitLogArgumentsWidget : public BaseGitDiffArgumentsWidget
     Q_OBJECT
 
 public:
-    GitLogArgumentsWidget(VcsBaseClientSettings &settings, QToolBar *toolBar = nullptr) :
+    GitLogArgumentsWidget(VcsBaseClientSettings &settings, bool fileRelated, QToolBar *toolBar) :
         BaseGitDiffArgumentsWidget(settings, toolBar)
     {
         QAction *diffButton = addToggleButton("--patch", tr("Show Diff"),
@@ -635,9 +635,12 @@ public:
                                                tr("Show textual graph log."));
         mapSetting(graphButton, settings.boolPointer(GitSettings::graphLogKey));
 
-        QAction *followButton = addToggleButton("--follow", tr("Follow"),
-                                               tr("Show log also for previous names of the file."));
-        mapSetting(followButton, settings.boolPointer(GitSettings::followRenamesKey));
+        if (fileRelated) {
+            QAction *followButton = addToggleButton(
+                        "--follow", tr("Follow"),
+                        tr("Show log also for previous names of the file."));
+            mapSetting(followButton, settings.boolPointer(GitSettings::followRenamesKey));
+        }
 
         addButton(tr("Reload"), Utils::Icons::RELOAD.icon());
     }
@@ -1019,7 +1022,7 @@ void GitClient::log(const QString &workingDirectory, const QString &fileName,
                                                   codecFor(CodecLogOutput), "logTitle", msgArg);
     VcsBaseEditorConfig *argWidget = editor->editorConfig();
     if (!argWidget) {
-        argWidget = new GitLogArgumentsWidget(settings(), editor->toolBar());
+        argWidget = new GitLogArgumentsWidget(settings(), !fileName.isEmpty(), editor->toolBar());
         argWidget->setBaseArguments(args);
         connect(argWidget, &VcsBaseEditorConfig::commandExecutionRequested, this,
                 [=]() { this->log(workingDir, fileName, enableAnnotationContextMenu, args); });