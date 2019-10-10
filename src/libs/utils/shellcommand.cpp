qt-creator/qt-creator

commit 00b692e67e5fcfb4946d1da8094b05c9bf5f35ad

Utils: Use CommandLine in ShellCommand
... and adapt users.

Change-Id: I218523ffe34720d5fe199aa0ca6892a8dc2985fc
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/libs/utils/shellcommand.cpp
@@ -68,12 +68,11 @@ class ShellCommandPrivate
 {
 public:
     struct Job {
-        explicit Job(const QString &wd, const FilePath &b, const QStringList &a, int t,
+        explicit Job(const QString &wd, const CommandLine &command, int t,
                      const ExitCodeInterpreter &interpreter);
 
         QString workingDirectory;
-        FilePath binary;
-        QStringList arguments;
+        CommandLine command;
         ExitCodeInterpreter exitCodeInterpreter;
         int timeoutS;
     };
@@ -113,11 +112,10 @@ ShellCommandPrivate::~ShellCommandPrivate()
     delete m_progressParser;
 }
 
-ShellCommandPrivate::Job::Job(const QString &wd, const FilePath &b, const QStringList &a,
+ShellCommandPrivate::Job::Job(const QString &wd, const CommandLine &command,
                               int t, const ExitCodeInterpreter &interpreter) :
     workingDirectory(wd),
-    binary(b),
-    arguments(a),
+    command(command),
     exitCodeInterpreter(interpreter),
     timeoutS(t)
 {
@@ -146,14 +144,14 @@ QString ShellCommand::displayName() const
         return d->m_displayName;
     if (!d->m_jobs.isEmpty()) {
         const Internal::ShellCommandPrivate::Job &job = d->m_jobs.at(0);
-        QString result = job.binary.toFileInfo().baseName();
+        QString result = job.command.executable().toFileInfo().baseName();
         if (!result.isEmpty())
             result[0] = result.at(0).toTitleCase();
         else
             result = tr("UNKNOWN");
 
-        if (!job.arguments.isEmpty())
-            result += QLatin1Char(' ') + job.arguments.at(0);
+        if (!job.command.arguments().isEmpty())
+            result += ' ' + job.command.arguments().at(0);
 
         return result;
     }
@@ -195,17 +193,17 @@ void ShellCommand::addFlags(unsigned f)
     d->m_flags |= f;
 }
 
-void ShellCommand::addJob(const FilePath &binary, const QStringList &arguments,
+void ShellCommand::addJob(const CommandLine &command,
                           const QString &workingDirectory, const ExitCodeInterpreter &interpreter)
 {
-    addJob(binary, arguments, defaultTimeoutS(), workingDirectory, interpreter);
+    addJob(command, defaultTimeoutS(), workingDirectory, interpreter);
 }
 
-void ShellCommand::addJob(const FilePath &binary, const QStringList &arguments, int timeoutS,
+void ShellCommand::addJob(const CommandLine &command, int timeoutS,
                           const QString &workingDirectory, const ExitCodeInterpreter &interpreter)
 {
-    d->m_jobs.push_back(Internal::ShellCommandPrivate::Job(workDirectory(workingDirectory), binary,
-                                                           arguments, timeoutS, interpreter));
+    d->m_jobs.push_back(Internal::ShellCommandPrivate::Job(workDirectory(workingDirectory), command,
+                                                           timeoutS, interpreter));
 }
 
 void ShellCommand::execute()
@@ -287,7 +285,7 @@ void ShellCommand::run(QFutureInterface<void> &future)
     for (int j = 0; j < count; j++) {
         const Internal::ShellCommandPrivate::Job &job = d->m_jobs.at(j);
         SynchronousProcessResponse resp
-                = runCommand(job.binary, job.arguments, job.timeoutS, job.workingDirectory,
+                = runCommand(job.command, job.timeoutS, job.workingDirectory,
                              job.exitCodeInterpreter);
         stdOut += resp.stdOut();
         stdErr += resp.stdErr();
@@ -319,8 +317,7 @@ void ShellCommand::run(QFutureInterface<void> &future)
     this->deleteLater();
 }
 
-SynchronousProcessResponse ShellCommand::runCommand(const FilePath &binary,
-                                                    const QStringList &arguments, int timeoutS,
+SynchronousProcessResponse ShellCommand::runCommand(const CommandLine &command, int timeoutS,
                                                     const QString &workingDirectory,
                                                     const ExitCodeInterpreter &interpreter)
 {
@@ -328,7 +325,7 @@ SynchronousProcessResponse ShellCommand::runCommand(const FilePath &binary,
 
     const QString dir = workDirectory(workingDirectory);
 
-    if (binary.isEmpty()) {
+    if (command.executable().isEmpty()) {
         response.result = SynchronousProcessResponse::StartFailed;
         return response;
     }
@@ -336,23 +333,23 @@ SynchronousProcessResponse ShellCommand::runCommand(const FilePath &binary,
     QSharedPointer<OutputProxy> proxy(d->m_proxyFactory());
 
     if (!(d->m_flags & SuppressCommandLogging))
-        emit proxy->appendCommand(dir, binary, arguments);
+        emit proxy->appendCommand(dir, command);
 
     if ((d->m_flags & FullySynchronously)
             || (!(d->m_flags & NoFullySync)
                 && QThread::currentThread() == QCoreApplication::instance()->thread())) {
-        response = runFullySynchronous({binary, arguments}, proxy, timeoutS, dir, interpreter);
+        response = runFullySynchronous(command, proxy, timeoutS, dir, interpreter);
     } else {
-        response = runSynchronous({binary, arguments}, proxy, timeoutS, dir, interpreter);
+        response = runSynchronous(command, proxy, timeoutS, dir, interpreter);
     }
 
     if (!d->m_aborted) {
         // Success/Fail message in appropriate window?
         if (response.result == SynchronousProcessResponse::Finished) {
             if (d->m_flags & ShowSuccessMessage)
-                emit proxy->appendMessage(response.exitMessage(binary.toUserOutput(), timeoutS));
+                emit proxy->appendMessage(response.exitMessage(command.toUserOutput(), timeoutS));
         } else if (!(d->m_flags & SuppressFailMessage)) {
-            emit proxy->appendError(response.exitMessage(binary.toUserOutput(), timeoutS));
+            emit proxy->appendError(response.exitMessage(command.toUserOutput(), timeoutS));
         }
     }
 

src/libs/utils/shellcommand.h
@@ -46,7 +46,7 @@ QT_END_NAMESPACE
 
 namespace Utils {
 
-class FilePath;
+class CommandLine;
 namespace Internal { class ShellCommandPrivate; }
 
 class QTCREATOR_UTILS_EXPORT ProgressParser
@@ -79,8 +79,7 @@ signals:
     void append(const QString &text);
     void appendSilently(const QString &text);
     void appendError(const QString &text);
-    void appendCommand(const QString &workingDirectory, const Utils::FilePath &binary,
-                       const QStringList &args);
+    void appendCommand(const QString &workingDirectory, const Utils::CommandLine &command);
     void appendMessage(const QString &text);
 };
 
@@ -112,10 +111,12 @@ public:
     QString displayName() const;
     void setDisplayName(const QString &name);
 
-    void addJob(const FilePath &binary, const QStringList &arguments,
-                const QString &workingDirectory = QString(), const ExitCodeInterpreter &interpreter = defaultExitCodeInterpreter);
-    void addJob(const FilePath &binary, const QStringList &arguments, int timeoutS,
-                const QString &workingDirectory = QString(), const ExitCodeInterpreter &interpreter = defaultExitCodeInterpreter);
+    void addJob(const CommandLine &command,
+                const QString &workingDirectory = QString(),
+                const ExitCodeInterpreter &interpreter = defaultExitCodeInterpreter);
+    void addJob(const CommandLine &command, int timeoutS,
+                const QString &workingDirectory = QString(),
+                const ExitCodeInterpreter &interpreter = defaultExitCodeInterpreter);
     void execute(); // Execute tasks asynchronously!
     void abort();
     bool lastExecutionSuccess() const;
@@ -145,7 +146,7 @@ public:
     // This is called once per job in a thread.
     // When called from the UI thread it will execute fully synchronously, so no signals will
     // be triggered!
-    virtual SynchronousProcessResponse runCommand(const FilePath &binary, const QStringList &arguments,
+    virtual SynchronousProcessResponse runCommand(const CommandLine &command,
                                                   int timeoutS,
                                                   const QString &workingDirectory = QString(),
                                                   const ExitCodeInterpreter &interpreter = defaultExitCodeInterpreter);

src/plugins/bazaar/bazaarcontrol.cpp
@@ -150,7 +150,7 @@ Core::ShellCommand *BazaarControl::createInitialCheckoutCommand(const QString &u
     QProcessEnvironment env = m_bazaarClient->processEnvironment();
     env.insert(QLatin1String("BZR_PROGRESS_BAR"), QLatin1String("text"));
     auto command = new VcsBase::VcsCommand(baseDirectory.toString(), env);
-    command->addJob(m_bazaarClient->vcsBinary(), args, -1);
+    command->addJob({m_bazaarClient->vcsBinary(), args}, -1);
     return command;
 }
 

src/plugins/clearcase/clearcaseplugin.cpp
@@ -1452,8 +1452,9 @@ ClearCasePlugin::runCleartool(const QString &workingDir,
     }
 
     const SynchronousProcessResponse sp_resp =
-            VcsBasePlugin::runVcs(workingDir, FilePath::fromUserInput(executable),
-                                  arguments, timeOutS,
+            VcsBasePlugin::runVcs(workingDir,
+                                  {FilePath::fromUserInput(executable), arguments},
+                                  timeOutS,
                                   flags, outputCodec);
 
     response.error = sp_resp.result != SynchronousProcessResponse::Finished;

src/plugins/cvs/cvscontrol.cpp
@@ -151,7 +151,7 @@ Core::ShellCommand *CvsControl::createInitialCheckoutCommand(const QString &url,
     auto command = new VcsBase::VcsCommand(baseDirectory.toString(),
                                            QProcessEnvironment::systemEnvironment());
     command->setDisplayName(tr("CVS Checkout"));
-    command->addJob(m_plugin->client()->vcsBinary(), settings.addOptions(args), -1);
+    command->addJob({m_plugin->client()->vcsBinary(), settings.addOptions(args)}, -1);
     return command;
 }
 

src/plugins/cvs/cvsplugin.cpp
@@ -1109,7 +1109,7 @@ CvsResponse CvsPlugin::runCvs(const QString &workingDirectory,
     }
     // Run, connect stderr to the output window
     const SynchronousProcessResponse sp_resp =
-            runVcs(workingDirectory, executable, client()->settings().addOptions(arguments),
+            runVcs(workingDirectory, {executable, client()->settings().addOptions(arguments)},
                    timeOutS, flags, outputCodec);
 
     response.result = CvsResponse::OtherError;

src/plugins/git/gerrit/gerritmodel.cpp
@@ -315,7 +315,7 @@ void QueryContext::start()
     m_progress.reportStarted();
     // Order: synchronous call to error handling if something goes wrong.
     VcsOutputWindow::appendCommand(
-                m_process.workingDirectory(), Utils::FilePath::fromString(m_binary), m_arguments);
+                m_process.workingDirectory(), {Utils::FilePath::fromString(m_binary), m_arguments});
     m_timer.start();
     m_process.start(m_binary, m_arguments);
     m_process.closeWriteChannel();

src/plugins/git/gerrit/gerritplugin.cpp
@@ -170,7 +170,7 @@ void FetchContext::start()
     m_progress.reportStarted();
     // Order: initialize future before starting the process in case error handling is invoked.
     const QStringList args = m_change->gitFetchArguments(m_server);
-    VcsBase::VcsOutputWindow::appendCommand(m_repository, m_git, args);
+    VcsBase::VcsOutputWindow::appendCommand(m_repository, {m_git, args});
     m_process.start(m_git.toString(), args);
     m_process.closeWriteChannel();
 }

commit b519c8bc92bc80c44f7c7285c9e3921320e3f67b

Utils: Fix display of first argument
Broke in 00b692e67e5fcfb4946d1da8094b05c9bf5f35ad.

Change-Id: I65fae0f439a49ccce136626c7c88acd9c9c1693b
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/libs/utils/shellcommand.cpp
@@ -151,7 +151,7 @@ QString ShellCommand::displayName() const
             result = tr("UNKNOWN");
 
         if (!job.command.arguments().isEmpty())
-            result += ' ' + job.command.arguments().at(0);
+            result += ' ' + job.command.splitArguments().at(0);
 
         return result;
     }