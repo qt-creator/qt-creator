qt-creator/qt-creator

commit b128d498b2da55a9f87cd2eabb802acc0c48e7d4

CMake Buildsystem: Enable visibility settings for binaries
This should not matter, but makes the add_qtc_executable targets
match up with the PCH, so that they should build a bit faster.

Change-Id: Ibecaaf7fd98d5b927d52e4956b1bea4a5775afba
Reviewed-by: Cristian Adam <cristian.adam@qt.io>
Reviewed-by: Tobias Hunger <tobias.hunger@qt.io>

src/shared/qtcreator_pch.h
@@ -37,18 +37,7 @@
 # undef _POSIX_
 #endif
 
-#include <QCoreApplication>
-#include <QList>
-#include <QVariant>
-#include <QObject>
-#include <QRegExp>
-#include <QString>
-#include <QStringList>
-#include <QTextCodec>
-#include <QPointer>
-#include <QScopedPointer>
-#include <QSharedPointer>
-#include <QDebug>
+#include <QtCore>
 
 #if QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
 #include <QTextStream>

cmake/QtCreatorAPI.cmake
@@ -838,6 +838,8 @@ function(add_qtc_executable name)
     INSTALL_RPATH "${_RPATH_BASE}/${_RELATIVE_LIB_PATH}"
     RUNTIME_OUTPUT_DIRECTORY "${_output_binary_dir}/${_DESTINATION}"
     QT_SKIP_TRANSLATION "${skip_translation}"
+    CXX_VISIBILITY_PRESET hidden
+    VISIBILITY_INLINES_HIDDEN ON
     ${_arg_PROPERTIES}
   )
   append_extra_translations("${name}" "${_arg_EXTRA_TRANSLATIONS}")

src/shared/qtcreator_gui_pch.h
@@ -32,16 +32,7 @@
 
 #if defined __cplusplus
 
-#include <QEvent>
-#include <QTimer>
-#include <QApplication>
-#include <QBitmap>
-#include <QCursor>
-#include <QImage>
-#include <QLayout>
-#include <QPainter>
-#include <QPixmap>
-#include <QStyle>
-#include <QWidget>
+#include <QtGui>
+#include <QtWidgets>
 
 #endif

commit 6463a686f6b6530c005fe6b340152b331bca9600

Build Fix: Workaround symbols inclusion from PCH headers
Broken in b128d498b2da55a9f87cd2eabb802acc0c48e7d4

Tested with MinGW 8.1
Tested with VisualStudio 2017

Change-Id: I8f43efe43016ea95866ee018ac9a47373a37373b
Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

src/shared/qtcreator_pch.h
@@ -29,13 +29,36 @@
  */
 
 #if defined __cplusplus
-#include <QtGlobal>
+#include <QtCore/qsystemdetection.h>
 
-#ifdef Q_WS_WIN
-# define _POSIX_
-# include <limits.h>
-# undef _POSIX_
+#ifdef Q_OS_WIN
+#define WIN32_LEAN_AND_MEAN
+
+// lib/Utils needs defines for Windows 8
+#ifdef Q_CC_MINGW
+#define WINVER _WIN32_WINNT_WIN8
+#define _WIN32_WINNT _WIN32_WINNT_WIN8
+#endif // Q_CC_MINGW
+#define NOHELP
+#include <qt_windows.h>
+
+#undef DELETE
+#undef IN
+#undef OUT
+#undef ERROR
+#undef ABSOLUTE
+
+//QT_NO_FLOAT16_OPERATORS is used on Visual Studio 2017 (and earlier):
+//when including <QFloat16> and <bitset> in the same translation unit,
+//it would cause a compilation error due to a toolchain bug (see [QTBUG-72073])
+#if _MSC_VER <= 1920
+#define QT_NO_FLOAT16_OPERATORS
 #endif
+
+#define _POSIX_
+#include <limits.h>
+#undef _POSIX_
+#endif // Q_OS_WIN
 
 #include <QtCore>
 
@@ -48,7 +71,7 @@ using Qt::dec;
 using Qt::showbase;
 using Qt::hex;
 using Qt::noforcesign;
-#endif
+#endif //QT_VERSION >= QT_VERSION_CHECK(6, 0, 0)
 
 #include <stdlib.h>
-#endif
+#endif //defined __cplusplus