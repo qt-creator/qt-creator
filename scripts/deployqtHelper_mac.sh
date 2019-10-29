qt-creator/qt-creator

commit ca15e1ed0c04034373b56937de2e18bcfa2cfc57

QmlDesigner: add 3d assetimporter plugins
macdeployqt was not enough because this is a runtime dependency

Change-Id: Idde6db46a4761288aec364cc83969a533e69816b
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

scripts/deployqtHelper_mac.sh
@@ -56,6 +56,19 @@ if [ ! -d "$designerDestDir" ]; then
     done
 fi
 
+# collect 3d assetimporter plugins
+assetimporterDestDir="$app_path/Contents/PlugIns/assetimporters"
+assetimporterSrcDir="$plugin_src/assetimporters"
+if [ -d "$assetimporterSrcDir" ]; then
+    if [ ! -d "$assetimporterDestDir" ]; then
+        echo "- Copying 3d assetimporter plugins"
+        mkdir -p "$assetimporterDestDir"
+        for plugin in "$assetimporterSrcDir"/*.dylib; do
+            cp "$plugin" "$assetimporterDestDir"/ || exit 1
+        done
+    fi
+if
+
 # copy Qt Quick 1 imports
 importsDir="$app_path/Contents/Imports/qtquick1"
 if [ -d "$quick1_src" ]; then

commit 7cec3cfc94920295a01bce084c1e5d550a0bf353

Scripts: Fix helper script on macOS
Broke with ca15e1ed0c04034373b56937de2e18bcfa2cfc57.

Change-Id: Ife884234ff8716b3019f2d302598c887d1498a92
Reviewed-by: David Schulz <david.schulz@qt.io>

scripts/deployqtHelper_mac.sh
@@ -67,7 +67,7 @@ if [ -d "$assetimporterSrcDir" ]; then
             cp "$plugin" "$assetimporterDestDir"/ || exit 1
         done
     fi
-if
+fi
 
 # copy Qt Quick 1 imports
 importsDir="$app_path/Contents/Imports/qtquick1"