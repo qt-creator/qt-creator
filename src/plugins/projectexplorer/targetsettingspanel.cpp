qt-creator/qt-creator

commit 6fd29da2cb6b7325631163bad1fdc79f0f71de77

Target setup page: Un-queue connection to kit manager
If we have a project with two enabled targets and we remove both of the
associated kits, one of the targets temporarily becomes the active one.
If the activeTargetChanged() signal gets queued, then the receiver will
be called with an invalid target pointer, as the target has been removed
in the meantime.
It is unclear why the signal was queued in the first place. Presumably
the original reasoning is no longer applicable after various
refactorings.
This amends (and effectively reverts) commit c13b20f160.

Task-number: QTCREATORBUG-23075
Change-Id: I3c63c763319734a4d52b3bb221919b20fd123dbe
Reviewed-by: hjk <hjk@qt.io>
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/projectexplorer/targetsettingspanel.cpp
@@ -674,7 +674,7 @@ TargetGroupItem::TargetGroupItem(const QString &displayName, Project *project)
     QObject::connect(project, &Project::removedTarget,
             d.get(), &TargetGroupItemPrivate::handleTargetRemoved);
     QObject::connect(project, &Project::activeTargetChanged,
-            d.get(), &TargetGroupItemPrivate::handleTargetChanged, Qt::QueuedConnection);
+            d.get(), &TargetGroupItemPrivate::handleTargetChanged);
 }
 
 TargetGroupItem::~TargetGroupItem() = default;
@@ -748,8 +748,10 @@ TargetItem *TargetGroupItem::currentTargetItem() const
 
 TargetItem *TargetGroupItem::targetItem(Target *target) const
 {
-    if (target)
-        return findFirstLevelChild([target](TargetItem *item) { return item->target() == target; });
+    if (target) {
+        Id needle = target->id(); // Unconfigured project have no active target.
+        return findFirstLevelChild([needle](TargetItem *item) { return item->m_kitId == needle; });
+    }
     return nullptr;
 }
 

commit 3c3f7afcbcadeb7e42a8a43e74cc14d921140efe

Target setup page: Un-queue another connection to kit manager
Amends 6fd29da2cb, which caused a regression in that a newly activated
target was not selected anymore, so when switching to project mode, the
build configuration was not shown without user intervention.

Change-Id: If723fe886f3e478cbdbcd7a77cd7fb71eca69813
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/projectexplorer/targetsettingspanel.cpp
@@ -669,8 +669,7 @@ TargetGroupItem::TargetGroupItem(const QString &displayName, Project *project)
 {
     d->m_displayName = displayName;
     QObject::connect(project, &Project::addedTarget,
-            d.get(), &TargetGroupItemPrivate::handleTargetAdded,
-            Qt::QueuedConnection);
+            d.get(), &TargetGroupItemPrivate::handleTargetAdded);
     QObject::connect(project, &Project::removedTarget,
             d.get(), &TargetGroupItemPrivate::handleTargetRemoved);
     QObject::connect(project, &Project::activeTargetChanged,