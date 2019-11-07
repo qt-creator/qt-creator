qt-creator/qt-creator

commit 8b4344f2a295db6ba0aad42148e67c62009bab79

ProjectExplorer: When running in the terminal on macOS, make it close
... after the application has finished and the user pressed the return
key as requested.

Fixes: QTCREATORBUG-15138
Change-Id: Id5b9fce553eb96b10aa76ccbd87fb66f02218110
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

share/qtcreator/scripts/openTerminal.py
@@ -105,7 +105,8 @@ def main():
                     login_script() +
                     'cd ' + quote_shell(os.getcwd()) + '\n' +
                     ' '.join([quote_shell(arg) for arg in sys.argv[1:]]) + '\n' +
-                    'rm ' + quoted_shell_script + '\n'
+                    'rm ' + quoted_shell_script + '\n' +
+                    'exit\n' if len(sys.argv) > 1 else ''
                     )
         shell_script.write(commands)
         shell_script.flush()

commit e6060d19d7918ec1cddbe7f3a6f49ea1a40edd5c

macOS: Environment with 'Open Terminal Here'
Regression introduced with 8b4344f2a295db6ba0aad42148e67c62009bab79

Change-Id: Ic839a6151e2b5fb06c8a685f5b5097d5d6894bbe
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

share/qtcreator/scripts/openTerminal.py
@@ -106,7 +106,7 @@ def main():
                     'cd ' + quote_shell(os.getcwd()) + '\n' +
                     ' '.join([quote_shell(arg) for arg in sys.argv[1:]]) + '\n' +
                     'rm ' + quoted_shell_script + '\n' +
-                    'exit\n' if len(sys.argv) > 1 else ''
+                    ('exit\n' if len(sys.argv) > 1 else '')
                     )
         shell_script.write(commands)
         shell_script.flush()