qt-creator/qt-creator

commit 6b31f9cf23cac79c10a9871961e3de79be657b38

Android multi arch support
[ChangeLog][Android] Android multi arch support for qmake

Change-Id: Ib8b1874604a3392130c96fbc00b26713b3d788ae
Reviewed-by: hjk <hjk@qt.io>

src/plugins/qbsprojectmanager/defaultpropertyprovider.cpp
@@ -45,6 +45,8 @@
 
 #include <android/androidconstants.h>
 #include <ios/iosconstants.h>
+#include <qtsupport/baseqtversion.h>
+#include <qtsupport/qtkitinformation.h>
 #include <winrt/winrtconstants.h>
 
 #include <QDir>
@@ -292,6 +294,18 @@ QVariantMap DefaultPropertyProvider::autoGeneratedProperties(const ProjectExplor
                 data.insert("Android.sdk.ndkDir", ndkDir);
                 data.insert("Android.ndk.ndkDir", ndkDir);
             }
+        }
+        data.remove(QBS_ARCHITECTURES);
+        data.remove(QBS_ARCHITECTURE);
+        QtSupport::BaseQtVersion *qtVersion = QtSupport::QtKitAspect::qtVersion(k);
+        if (qtVersion) {
+            QStringList abis;
+            for (const auto &abi : qtVersion->qtAbis())
+                abis << abi.param();
+            if (abis.size() == 1)
+                data.insert(QLatin1String(QBS_ARCHITECTURE), abis.first());
+            else
+                data.insert(QLatin1String(QBS_ARCHITECTURES), abis);
         }
     } else {
         Utils::FilePath cCompilerPath;

share/qtcreator/templates/wizards/projects/qtquickapplication/CMakeLists.txt
@@ -20,7 +20,8 @@ find_package(Qt5 COMPONENTS Core Quick REQUIRED)
 @endif
 
 if(ANDROID)
-    add_library(%{ProjectName} SHARED
+    set(TARGET %{ProjectName}_${ANDROID_ABI})
+    add_library(${TARGET} SHARED
       %{MainCppFileName}
       qml.qrc
 @if %{HasTranslation}
@@ -28,7 +29,8 @@ if(ANDROID)
 @endif
     )
 else()
-    add_executable(%{ProjectName}
+    set(TARGET %{ProjectName})
+    add_executable(${TARGET}
       %{MainCppFileName}
       qml.qrc
 @if %{HasTranslation}
@@ -37,9 +39,9 @@ else()
     )
 endif()
 
-target_compile_definitions(%{ProjectName}
+target_compile_definitions(${TARGET}
   PRIVATE $<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>:QT_QML_DEBUG>)
-target_link_libraries(%{ProjectName}
+target_link_libraries(${TARGET}
   PRIVATE Qt5::Core Qt5::Quick)
 @if %{HasTranslation}
 

src/plugins/android/CMakeLists.txt
@@ -18,7 +18,6 @@ add_qtc_plugin(Android
     androiddevicedialog.cpp androiddevicedialog.h androiddevicedialog.ui
     androiderrormessage.cpp androiderrormessage.h
     androidextralibrarylistmodel.cpp androidextralibrarylistmodel.h
-    androidgdbserverkitinformation.cpp androidgdbserverkitinformation.h
     androidglobal.h
     androidmanager.cpp androidmanager.h
     androidmanifestdocument.cpp androidmanifestdocument.h

src/plugins/android/android.pro
@@ -24,7 +24,6 @@ HEADERS += \
     javaparser.h \
     androidplugin.h \
     androiddevice.h \
-    androidgdbserverkitinformation.h \
     androidqmltoolingsupport.h \
     androidmanifesteditorfactory.h \
     androidmanifesteditor.h \
@@ -69,7 +68,6 @@ SOURCES += \
     javaparser.cpp \
     androidplugin.cpp \
     androiddevice.cpp \
-    androidgdbserverkitinformation.cpp \
     androidqmltoolingsupport.cpp \
     androidmanifesteditorfactory.cpp \
     androidmanifesteditor.cpp \

src/plugins/android/android.qbs
@@ -49,8 +49,6 @@ Project {
             "androiderrormessage.cpp",
             "androidextralibrarylistmodel.cpp",
             "androidextralibrarylistmodel.h",
-            "androidgdbserverkitinformation.cpp",
-            "androidgdbserverkitinformation.h",
             "androidglobal.h",
             "androidmanager.cpp",
             "androidmanager.h",

src/plugins/android/androidbuildapkstep.cpp
@@ -205,8 +205,9 @@ bool AndroidBuildApkStep::init()
     setOutputParser(parser);
 
     m_openPackageLocationForRun = m_openPackageLocation;
-    m_apkPath = AndroidManager::apkPath(target()).toString();
-    qCDebug(buildapkstepLog) << "APK path:" << m_apkPath;
+    m_packagePath = m_buildAAB ? AndroidManager::aabPath(target()).toString()
+                           : AndroidManager::apkPath(target()).toString();
+    qCDebug(buildapkstepLog) << "Package path:" << m_packagePath;
 
     if (!AbstractProcessStep::init())
         return false;
@@ -245,6 +246,9 @@ bool AndroidBuildApkStep::init()
         arguments << "--verbose";
 
     arguments << "--gradle";
+
+    if (m_buildAAB)
+        arguments << "--aab" <<  "--jarsigner";
 
     if (m_useMinistro)
         arguments << "--deployment" << "ministro";
@@ -286,7 +290,7 @@ bool AndroidBuildApkStep::init()
 
 void AndroidBuildApkStep::showInGraphicalShell()
 {
-    Core::FileUtils::showInGraphicalShell(Core::ICore::mainWindow(), m_apkPath);
+    Core::FileUtils::showInGraphicalShell(Core::ICore::mainWindow(), m_packagePath);
 }
 
 ProjectExplorer::BuildStepConfigWidget *AndroidBuildApkStep::createConfigWidget()
@@ -373,14 +377,15 @@ void AndroidBuildApkStep::doRun()
 
     auto setup = [this] {
         auto bc = target()->activeBuildConfiguration();
-        Utils::FilePath androidLibsDir = bc->buildDirectory()
-                .pathAppended("android-build/libs")
-                .pathAppended(AndroidManager::targetArch(target()));
-        if (!androidLibsDir.exists() && !QDir{bc->buildDirectory().toString()}.mkpath(androidLibsDir.toString()))
-            return false;
+        const auto androidAbis = AndroidManager::applicationAbis(target());
+        for (const auto &abi : androidAbis) {
+            Utils::FilePath androidLibsDir = bc->buildDirectory()
+                    .pathAppended("android-build/libs")
+                    .pathAppended(abi);
+            if (!androidLibsDir.exists() && !QDir{bc->buildDirectory().toString()}.mkpath(androidLibsDir.toString()))
+                return false;
+        }
 
-
-        QJsonObject deploySettings = Android::AndroidManager::deploymentSettings(target());
         RunConfiguration *rc = target()->activeRunConfiguration();
         const QString buildKey = rc ? rc->buildKey() : QString();
         const ProjectNode *node = rc ? target()->project()->findNodeForBuildKey(buildKey) : nullptr;
@@ -392,11 +397,40 @@ void AndroidBuildApkStep::doRun()
         if (targets.isEmpty())
             return true; // qmake does this job for us
 
+        QJsonObject deploySettings = Android::AndroidManager::deploymentSettings(target());
+        QJsonObject architectures;
+
         // Copy targets to android build folder
-        for (const auto &target : targets) {
-            if (!copyFileIfNewer(target, androidLibsDir.pathAppended(QFileInfo{target}.fileName()).toString()))
-                return false;
+        QString applicationBinary = target()->activeRunConfiguration()->buildTargetInfo().targetFilePath.toFileInfo().fileName();
+        for (const auto &abi : androidAbis) {
+            QString targetSuffix = QString{"_%1.so"}.arg(abi);
+            if (applicationBinary.endsWith(targetSuffix)) {
+                // Keep only TargetName from "lib[TargetName]_abi.so"
+                applicationBinary.remove(0, 3).chop(targetSuffix.size());
+            }
+
+            Utils::FilePath androidLibsDir = bc->buildDirectory()
+                                                 .pathAppended("android-build/libs")
+                                                 .pathAppended(abi);
+            for (const auto &target : targets) {
+                if (target.endsWith(targetSuffix)) {
+                    if (!copyFileIfNewer(target, androidLibsDir.pathAppended(QFileInfo{target}.fileName()).toString()))
+                        return false;
+                    if (abi == "x86") {
+                        architectures[abi] = "i686-linux-android";
+                    } else if (abi == "x86_64") {
+                        architectures[abi] = "x86_64-linux-android";
+                    } else if (abi == "arm64-v8a") {
+                        architectures[abi] = "aarch64-linux-android";
+                    } else {
+                        architectures[abi] = "arm-linux-androideabi";
+                    }
+                }
+            }
         }
+
+        deploySettings["application-binary"] = applicationBinary;
+        deploySettings["architectures"] = architectures;
 
         QString extraLibs = node->data(Android::Constants::AndroidExtraLibs).toString();
         if (!extraLibs.isEmpty())
@@ -485,8 +519,8 @@ QVariant AndroidBuildApkStep::data(Core::Id id) const
         return AndroidConfigurations::currentConfig().bestNdkPlatformMatch(AndroidManager::minimumSDK(target())).mid(8);
     if (id == Constants::NdkLocation)
         return QVariant::fromValue(AndroidConfigurations::currentConfig().ndkLocation());
-    if (id == Constants::AndroidABI)
-        return AndroidManager::targetArch(target());
+    if (id == Constants::AndroidABIs)
+        return AndroidManager::applicationAbis(target());
 
     return AbstractProcessStep::data(id);
 }
@@ -521,6 +555,16 @@ bool AndroidBuildApkStep::signPackage() const
 void AndroidBuildApkStep::setSignPackage(bool b)
 {
     m_signPackage = b;
+}
+
+bool AndroidBuildApkStep::buildAAB() const
+{
+    return m_buildAAB;
+}
+
+void AndroidBuildApkStep::setBuildAAB(bool aab)
+{
+    m_buildAAB = aab;
 }
 
 bool AndroidBuildApkStep::openPackageLocation() const

src/plugins/android/androidbuildapkstep.h
@@ -61,6 +61,9 @@ public:
     bool signPackage() const;
     void setSignPackage(bool b);
 
+    bool buildAAB() const;
+    void setBuildAAB(bool aab);
+
     bool openPackageLocation() const;
     void setOpenPackageLocation(bool open);
 
@@ -89,6 +92,7 @@ private:
 
     void doRun() override;
 
+    bool m_buildAAB = false;
     bool m_signPackage = false;
     bool m_verbose = false;
     bool m_useMinistro = false;
@@ -101,7 +105,7 @@ private:
     QString m_keystorePasswd;
     QString m_certificateAlias;
     QString m_certificatePasswd;
-    QString m_apkPath;
+    QString m_packagePath;
 
     QString m_command;
     QString m_argumentsPasswordConcealed;

src/plugins/android/androidbuildapkwidget.cpp
@@ -236,6 +236,13 @@ QWidget *AndroidBuildApkWidget::createAdvancedGroup()
             m_step, &AndroidBuildApkStep::setUseMinistro);
 
     auto vbox = new QVBoxLayout(group);
+    QtSupport::BaseQtVersion *version = QtSupport::QtKitAspect::qtVersion(step()->target()->kit());
+    if (version && version->qtVersion() >= QtSupport::QtVersionNumber{5,14}) {
+        auto buildAAB = new QCheckBox(tr("Build .aab (Android App Bundle)"), group);
+        buildAAB->setChecked(m_step->buildAAB());
+        connect(buildAAB, &QAbstractButton::toggled, m_step, &AndroidBuildApkStep::setBuildAAB);
+        vbox->addWidget(buildAAB);
+    }
     vbox->addWidget(openPackageLocationCheckBox);
     vbox->addWidget(verboseOutputCheckBox);
     vbox->addWidget(m_addDebuggerCheckBox);

commit f1b2120ccb43e22586072b257c0fa9b2c247f4d4

QbsProjectManager: Fix setting qbs.architecture for Android
- Use values that qbs understands.
- Make sure qbs.architecture(s) is set also with no Qt present.

This was broken in 6b31f9cf23.

Fixes: QTCREATORBUG-23489
Change-Id: If7bf8862b850defa04e8f45eace99e08388d780f
Reviewed-by: BogDan Vatra <bogdan@kdab.com>

src/plugins/qbsprojectmanager/defaultpropertyprovider.cpp
@@ -295,13 +295,13 @@ QVariantMap DefaultPropertyProvider::autoGeneratedProperties(const ProjectExplor
                 data.insert("Android.ndk.ndkDir", ndkDir);
             }
         }
-        data.remove(QBS_ARCHITECTURES);
-        data.remove(QBS_ARCHITECTURE);
         QtSupport::BaseQtVersion *qtVersion = QtSupport::QtKitAspect::qtVersion(k);
         if (qtVersion) {
+            data.remove(QBS_ARCHITECTURES);
+            data.remove(QBS_ARCHITECTURE);
             QStringList abis;
             for (const auto &abi : qtVersion->qtAbis())
-                abis << abi.param();
+                abis << architecture(abi);
             if (abis.size() == 1)
                 data.insert(QLatin1String(QBS_ARCHITECTURE), abis.first());
             else