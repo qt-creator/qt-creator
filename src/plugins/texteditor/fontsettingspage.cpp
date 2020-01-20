qt-creator/qt-creator

commit 07b04c20560abed63240095094a2b95114ec61ad

TextEditor: Dissolve TextEditorOptionsPage intermediate hierarchy level
It's effectively setting only three properties nowadays, not enough
to justify a case of reuse-by-inheritance anymore.

Change-Id: I947fbc51e76c8d908da662dd58dce91614c28e08
Reviewed-by: David Schulz <david.schulz@qt.io>

src/plugins/texteditor/fontsettingspage.cpp
@@ -330,11 +330,14 @@ bool FormatDescription::showControl(FormatDescription::ShowControls showControl)
 FontSettingsPage::FontSettingsPage(const FormatDescriptions &fd,
                                    Core::Id id,
                                    QObject *parent) :
-    TextEditorOptionsPage(parent),
+    Core::IOptionsPage(parent),
     d_ptr(new FontSettingsPagePrivate(fd, id, tr("Font && Colors"), category().toString()))
 {
     setId(d_ptr->m_id);
     setDisplayName(d_ptr->m_displayName);
+    setCategory(TextEditor::Constants::TEXT_EDITOR_SETTINGS_CATEGORY);
+    setDisplayCategory(QCoreApplication::translate("TextEditor", "Text Editor"));
+    setCategoryIconPath(TextEditor::Constants::TEXT_EDITOR_SETTINGS_CATEGORY_ICON_PATH);
 }
 
 FontSettingsPage::~FontSettingsPage()

src/plugins/texteditor/CMakeLists.txt
@@ -92,7 +92,6 @@ add_qtc_plugin(TextEditor
     texteditor_p.h
     texteditoractionhandler.cpp texteditoractionhandler.h
     texteditorconstants.cpp texteditorconstants.h
-    texteditoroptionspage.cpp texteditoroptionspage.h
     texteditoroverlay.cpp texteditoroverlay.h
     texteditorplugin.cpp texteditorplugin.h
     texteditorsettings.cpp texteditorsettings.h

src/plugins/texteditor/behaviorsettingspage.cpp
@@ -88,9 +88,13 @@ void BehaviorSettingsPage::BehaviorSettingsPagePrivate::init()
 
 BehaviorSettingsPage::BehaviorSettingsPage(const BehaviorSettingsPageParameters &p,
                                            QObject *parent)
-  : TextEditorOptionsPage(parent),
+  : Core::IOptionsPage(parent),
     d(new BehaviorSettingsPagePrivate(p))
 {
+    setCategory(TextEditor::Constants::TEXT_EDITOR_SETTINGS_CATEGORY);
+    setDisplayCategory(QCoreApplication::translate("TextEditor", "Text Editor"));
+    setCategoryIconPath(TextEditor::Constants::TEXT_EDITOR_SETTINGS_CATEGORY_ICON_PATH);
+
     // global tab preferences for all other languages
     d->m_codeStyle = new SimpleCodeStylePreferences(this);
     d->m_codeStyle->setDisplayName(tr("Global", "Settings"));

src/plugins/texteditor/behaviorsettingspage.h
@@ -27,8 +27,9 @@
 
 #include "texteditor_global.h"
 
-#include "texteditoroptionspage.h"
 #include "tabsettingswidget.h"
+
+#include <coreplugin/dialogs/ioptionspage.h>
 
 namespace TextEditor {
 
@@ -48,7 +49,7 @@ public:
     QString settingsPrefix;
 };
 
-class BehaviorSettingsPage : public TextEditorOptionsPage
+class BehaviorSettingsPage : public Core::IOptionsPage
 {
     Q_OBJECT
 

src/plugins/texteditor/completionsettingspage.cpp
@@ -24,8 +24,10 @@
 ****************************************************************************/
 
 #include "completionsettingspage.h"
-#include "ui_completionsettingspage.h"
+
 #include "texteditorsettings.h"
+#include "texteditorconstants.h"
+#include "ui_completionsettingspage.h"
 
 #include <cpptools/cpptoolssettings.h>
 
@@ -38,10 +40,13 @@ using namespace TextEditor::Internal;
 using namespace CppTools;
 
 CompletionSettingsPage::CompletionSettingsPage(QObject *parent)
-    : TextEditor::TextEditorOptionsPage(parent)
+    : Core::IOptionsPage(parent)
 {
     setId("P.Completion");
     setDisplayName(tr("Completion"));
+    setCategory(TextEditor::Constants::TEXT_EDITOR_SETTINGS_CATEGORY);
+    setDisplayCategory(QCoreApplication::translate("TextEditor", "Text Editor"));
+    setCategoryIconPath(TextEditor::Constants::TEXT_EDITOR_SETTINGS_CATEGORY_ICON_PATH);
 
     QSettings *s = Core::ICore::settings();
     m_completionSettings.fromSettings(s);

src/plugins/texteditor/completionsettingspage.h
@@ -27,8 +27,8 @@
 
 #include "commentssettings.h"
 #include "completionsettings.h"
-#include "texteditoroptionspage.h"
 
+#include <coreplugin/dialogs/ioptionspage.h>
 
 #include <QPointer>
 
@@ -37,7 +37,7 @@ namespace Internal {
 
 namespace Ui { class CompletionSettingsPage; }
 
-class CompletionSettingsPage : public TextEditorOptionsPage
+class CompletionSettingsPage : public Core::IOptionsPage
 {
     Q_OBJECT
 

src/plugins/texteditor/displaysettingspage.cpp
@@ -24,8 +24,10 @@
 ****************************************************************************/
 
 #include "displaysettingspage.h"
+
 #include "displaysettings.h"
 #include "marginsettings.h"
+#include "texteditorconstants.h"
 #include "ui_displaysettingspage.h"
 
 #include <coreplugin/icore.h>
@@ -56,11 +58,14 @@ DisplaySettingsPage::DisplaySettingsPagePrivate::DisplaySettingsPagePrivate
 
 DisplaySettingsPage::DisplaySettingsPage(const DisplaySettingsPageParameters &p,
                                          QObject *parent)
-  : TextEditorOptionsPage(parent),
+  : Core::IOptionsPage(parent),
     d(new DisplaySettingsPagePrivate(p))
 {
     setId(p.id);
     setDisplayName(p.displayName);
+    setCategory(TextEditor::Constants::TEXT_EDITOR_SETTINGS_CATEGORY);
+    setDisplayCategory(QCoreApplication::translate("TextEditor", "Text Editor"));
+    setCategoryIconPath(TextEditor::Constants::TEXT_EDITOR_SETTINGS_CATEGORY_ICON_PATH);
 }
 
 DisplaySettingsPage::~DisplaySettingsPage()

src/plugins/texteditor/displaysettingspage.h
@@ -27,7 +27,7 @@
 
 #include "texteditor_global.h"
 
-#include "texteditoroptionspage.h"
+#include <coreplugin/dialogs/ioptionspage.h>
 
 namespace TextEditor {
 
@@ -42,7 +42,7 @@ public:
     QString settingsPrefix;
 };
 
-class DisplaySettingsPage : public TextEditorOptionsPage
+class DisplaySettingsPage : public Core::IOptionsPage
 {
     Q_OBJECT
 

commit 487df597f34b0e4ac3c847e18c57820adc20c7d3

Editor: Fix Font & Color settings loading
broken after 07b04c20560abed63240095094a2b95114ec61ad

Change-Id: I6ec9676aec6dbfee023a5e4710b69cdd709780ef
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/texteditor/fontsettingspage.cpp
@@ -331,7 +331,7 @@ FontSettingsPage::FontSettingsPage(const FormatDescriptions &fd,
                                    Core::Id id,
                                    QObject *parent) :
     Core::IOptionsPage(parent),
-    d_ptr(new FontSettingsPagePrivate(fd, id, tr("Font && Colors"), category().toString()))
+    d_ptr(new FontSettingsPagePrivate(fd, id, tr("Font && Colors"), TextEditor::Constants::TEXT_EDITOR_SETTINGS_CATEGORY))
 {
     setId(d_ptr->m_id);
     setDisplayName(d_ptr->m_displayName);