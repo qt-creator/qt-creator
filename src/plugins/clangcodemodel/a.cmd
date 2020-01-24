d2bdd5fc400b0cef3ac894bddbdfb7a4d545304a
Cristian Adam <cristian.adam@qt.io>
2020-01-24 11:01:13 +0100

    Remove stray debug cmd file
    Change-Id: I19a50e13951f876fcde26a34cd7db0a0e5359a55
    Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

--- src/plugins/clangcodemodel/a.cmd
+++
@@ -1,4 +0,0 @@
-git checkout -- clangcodemodelplugin.h
-git checkout -- clangcompletionassistinterface.cpp
-git checkout -- clangcompletionassistinterface.h
-git checkout -- clangcompletionassistprocessor.cpp
