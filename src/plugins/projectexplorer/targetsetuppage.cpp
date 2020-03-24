d3cf1549ee3050149427c8aff8d8e86190da4952
Robert Loehning <robert.loehning@qt.io>
2020-03-24 17:50:46 +0100

    TargetSetupPage: Translate less HTML
    Change-Id: I5dadcffcf6bb776d1c3e16f611783861428e61b9
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- src/plugins/projectexplorer/targetsetuppage.cpp
+++ src/plugins/projectexplorer/targetsetuppage.cpp
@@ -101,9 +101,11 @@ public:
 
         noValidKitLabel = new QLabel(setupTargetPage);
         noValidKitLabel->setWordWrap(true);
-        noValidKitLabel->setText(TargetSetupPage::tr("<span style=\" font-weight:600;\">No suitable kits found.</span><br/>"
-                                 "Add a kit in the <a href=\"buildandrun\">options</a> "
-                                 "or via the maintenance tool of the SDK."));
+        noValidKitLabel->setText("<span style=\" font-weight:600;\">"
+                                 + TargetSetupPage::tr("No suitable kits found.") + "</span><br/>"
+                                 + TargetSetupPage::tr("Add a kit in the <a href=\"buildandrun\">"
+                                                       "options</a> or via the maintenance tool of"
+                                                       " the SDK."));
         noValidKitLabel->setTextInteractionFlags(Qt::TextBrowserInteraction);
         noValidKitLabel->setVisible(false);
 
