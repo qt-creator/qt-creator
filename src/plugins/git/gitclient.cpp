qt-creator/qt-creator

commit cbb70513bfa935f668fad7b3aedf4e7492cffc02

Git: Allow colorful logs
Requires Git 2.3 or higher for RGB colors.

Can be enabled for the normal or graph log
with the "Color" tool button. For the diff
log, the old behavior is kept (highlighter
is used for the "normal" log, no colors at
all for the graph log)

Fixes: QTCREATORBUG-19624
Change-Id: I8a42a7ad0ebf99ceb459355310311d702dcf9977
Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/git/gitclient.cpp
@@ -84,12 +84,14 @@
 #include <QTextCodec>
 
 const char GIT_DIRECTORY[] = ".git";
-const char graphLogFormatC[] = "%h %d %an %s %ci";
 const char HEAD[] = "HEAD";
 const char CHERRY_PICK_HEAD[] = "CHERRY_PICK_HEAD";
 const char BRANCHES_PREFIX[] = "Branches: ";
 const char stashNamePrefix[] = "stash@{";
 const char noColorOption[] = "--no-color";
+const char colorOption[] = "--color=always";
+const char patchOption[] = "--patch";
+const char graphOption[] = "--graph";
 const char decorateOption[] = "--decorate";
 const char showFormatC[] =
         "--pretty=format:commit %H%d%n"
@@ -540,7 +542,7 @@ public:
         BaseGitDiffArgumentsWidget(settings, editor->toolBar())
     {
         QToolBar *toolBar = editor->toolBar();
-        QAction *diffButton = addToggleButton("--patch", tr("Diff"),
+        QAction *diffButton = addToggleButton(patchOption, tr("Diff"),
                                               tr("Show difference."));
         mapSetting(diffButton, settings.boolPointer(GitSettings::logDiffKey));
         connect(diffButton, &QAction::toggled, m_patienceButton, &QAction::setVisible);
@@ -568,13 +570,13 @@ public:
                                 tr("First Parent"),
                                 tr("Follow only the first parent on merge commits."));
         mapSetting(firstParentButton, settings.boolPointer(GitSettings::firstParentKey));
-        const QStringList graphArguments = {
-            "--graph", "--oneline", "--topo-order",
-            QLatin1String("--pretty=format:") + graphLogFormatC
-        };
-        QAction *graphButton = addToggleButton(graphArguments, tr("Graph"),
+        QAction *graphButton = addToggleButton(graphArguments(), tr("Graph"),
                                                tr("Show textual graph log."));
         mapSetting(graphButton, settings.boolPointer(GitSettings::graphLogKey));
+
+        QAction *colorButton = addToggleButton(QStringList{colorOption},
+                                        tr("Color"), tr("Use colors in log."));
+        mapSetting(colorButton, settings.boolPointer(GitSettings::colorLogKey));
 
         if (fileRelated) {
             QAction *followButton = addToggleButton(
@@ -584,6 +586,35 @@ public:
         }
 
         addReloadButton();
+    }
+
+    QStringList graphArguments() const
+    {
+        auto colorName = [](Theme::Color color) { return creatorTheme()->color(color).name(); };
+        const QString authorName = colorName(Theme::Git_AuthorName_TextColor);
+        const QString commitDate = colorName(Theme::Git_CommitDate_TextColor);
+        const QString commitHash = colorName(Theme::Git_CommitHash_TextColor);
+        const QString commitSubject = colorName(Theme::Git_CommitSubject_TextColor);
+        const QString decoration = colorName(Theme::Git_Decoration_TextColor);
+
+        const QString formatArg = QStringLiteral(
+                    "--pretty=format:"
+                    "%C(%1)%h%Creset "
+                    "%C(%2)%d%Creset "
+                    "%C(%3)%an%Creset "
+                    "%C(%4)%s%Creset "
+                    "%C(%5)%ci%Creset"
+                    ).arg(commitHash, decoration, authorName, commitSubject, commitDate);
+
+        QStringList graphArgs = {graphOption, "--oneline", "--topo-order"};
+
+        const unsigned gitVersion = GitClient::instance()->gitVersion();
+        if (gitVersion >= 0x020300U)
+            graphArgs << formatArg;
+        else
+            graphArgs << "--pretty=format:%h %d %an %s %ci";
+
+        return graphArgs;
     }
 };
 
@@ -1002,6 +1033,30 @@ void GitClient::status(const QString &workingDirectory) const
             Qt::QueuedConnection);
 }
 
+static QStringList normalLogArguments()
+{
+    const unsigned gitVersion = GitClient::instance()->gitVersion();
+    if (gitVersion < 0x020300U)
+        return {};
+
+    auto colorName = [](Theme::Color color) { return creatorTheme()->color(color).name(); };
+    const QString authorName = colorName(Theme::Git_AuthorName_TextColor);
+    const QString commitDate = colorName(Theme::Git_CommitDate_TextColor);
+    const QString commitHash = colorName(Theme::Git_CommitHash_TextColor);
+    const QString commitSubject = colorName(Theme::Git_CommitSubject_TextColor);
+    const QString decoration = colorName(Theme::Git_Decoration_TextColor);
+
+    const QString logArgs = QStringLiteral(
+                "--pretty=format:"
+                "Commit: %C(%1)%H%Creset %C(%2)%d%Creset%n"
+                "Author: %C(%3)%an <%ae>%Creset%n"
+                "Date:   %C(%4)%cD%Creset%n%n"
+                "%C(%5)%s%Creset%n%n%b%n"
+                ).arg(commitHash, decoration, authorName, commitDate, commitSubject);
+
+    return {logArgs};
+}
+
 void GitClient::log(const QString &workingDirectory, const QString &fileName,
                     bool enableAnnotationContextMenu, const QStringList &args)
 {
@@ -1031,12 +1086,17 @@ void GitClient::log(const QString &workingDirectory, const QString &fileName,
     editor->setFileLogAnnotateEnabled(enableAnnotationContextMenu);
     editor->setWorkingDirectory(workingDir);
 
-    QStringList arguments = {"log", noColorOption, decorateOption};
+    QStringList arguments = {"log", decorateOption};
     int logCount = settings().intValue(GitSettings::logCountKey);
     if (logCount > 0)
         arguments << "-n" << QString::number(logCount);
 
     arguments << argWidget->arguments();
+    if (arguments.contains(patchOption))
+        arguments.removeAll(colorOption);
+    if (!arguments.contains(graphOption) && !arguments.contains(patchOption))
+        arguments << normalLogArguments();
+
     const QString grepValue = editor->grepValue();
     if (!grepValue.isEmpty())
         arguments << "--grep=" + grepValue;

share/qtcreator/themes/dark.creatortheme
@@ -223,6 +223,12 @@ VcsBase_FileDeleted_TextColor=ffff6c6c
 VcsBase_FileRenamed_TextColor=ffffa500
 VcsBase_FileUnmerged_TextColor=ffff4040
 
+Git_AuthorName_TextColor=ceffe5
+Git_CommitDate_TextColor=00ff00
+Git_CommitHash_TextColor=ff0000
+Git_CommitSubject_TextColor=text
+Git_Decoration_TextColor=ff00ff
+
 Bookmarks_TextMarkColor=ff8080ff
 
 TextEditor_SearchResult_ScrollBarColor=ff00c000

share/qtcreator/themes/default.creatortheme
@@ -214,6 +214,12 @@ VcsBase_FileDeleted_TextColor=ff800000
 VcsBase_FileRenamed_TextColor=ffd77d00
 VcsBase_FileUnmerged_TextColor=ffee0000
 
+Git_AuthorName_TextColor=007af4
+Git_CommitDate_TextColor=006600
+Git_CommitHash_TextColor=ff0000
+Git_CommitSubject_TextColor=text
+Git_Decoration_TextColor=ff00ff
+
 Bookmarks_TextMarkColor=ffa0a0ff
 
 TextEditor_SearchResult_ScrollBarColor=ff00c000

share/qtcreator/themes/design.creatortheme
@@ -227,6 +227,12 @@ VcsBase_FileDeleted_TextColor=fffff6c6c
 VcsBase_FileRenamed_TextColor=ffffa500
 VcsBase_FileUnmerged_TextColor=ffff4040
 
+Git_AuthorName_TextColor=ceffe5
+Git_CommitDate_TextColor=00ff00
+Git_CommitHash_TextColor=ff4343
+Git_CommitSubject_TextColor=text
+Git_Decoration_TextColor=ff00ff
+
 Bookmarks_TextMarkColor=ff8080ff
 
 TextEditor_SearchResult_ScrollBarColor=ff00c000

share/qtcreator/themes/flat-dark.creatortheme
@@ -227,6 +227,12 @@ VcsBase_FileDeleted_TextColor=fffff6c6c
 VcsBase_FileRenamed_TextColor=ffffa500
 VcsBase_FileUnmerged_TextColor=ffff4040
 
+Git_AuthorName_TextColor=ceffe5
+Git_CommitDate_TextColor=00ff00
+Git_CommitHash_TextColor=ff4343
+Git_CommitSubject_TextColor=text
+Git_Decoration_TextColor=ff00ff
+
 Bookmarks_TextMarkColor=ff8080ff
 
 TextEditor_SearchResult_ScrollBarColor=ff00c000

share/qtcreator/themes/flat-light.creatortheme
@@ -223,6 +223,12 @@ VcsBase_FileDeleted_TextColor=ff800000
 VcsBase_FileRenamed_TextColor=ffd77d00
 VcsBase_FileUnmerged_TextColor=ffee0000
 
+Git_AuthorName_TextColor=007af4
+Git_CommitDate_TextColor=006600
+Git_CommitHash_TextColor=ff0000
+Git_CommitSubject_TextColor=text
+Git_Decoration_TextColor=ff00ff
+
 Bookmarks_TextMarkColor=ffa0a0ff
 
 TextEditor_SearchResult_ScrollBarColor=ff00c000

share/qtcreator/themes/flat.creatortheme
@@ -221,6 +221,12 @@ VcsBase_FileDeleted_TextColor=ff800000
 VcsBase_FileRenamed_TextColor=ffd77d00
 VcsBase_FileUnmerged_TextColor=ffee0000
 
+Git_AuthorName_TextColor=007af4
+Git_CommitDate_TextColor=006600
+Git_CommitHash_TextColor=ff0000
+Git_CommitSubject_TextColor=text
+Git_Decoration_TextColor=ff00ff
+
 Bookmarks_TextMarkColor=ffa0a0ff
 
 TextEditor_SearchResult_ScrollBarColor=ff00c000

src/libs/utils/theme/theme.h
@@ -279,6 +279,13 @@ public:
         VcsBase_FileRenamed_TextColor,
         VcsBase_FileUnmerged_TextColor,
 
+        /* Git Plugin */
+        Git_AuthorName_TextColor,
+        Git_CommitDate_TextColor,
+        Git_CommitHash_TextColor,
+        Git_CommitSubject_TextColor,
+        Git_Decoration_TextColor,
+
         /* Bookmarks Plugin */
         Bookmarks_TextMarkColor,
 

commit d1b0966996ffca71fadb4978c11a06ed3d6928f7

Git: Fix filling commit selection combobox for log
Broken by commit cbb70513bfa935, which changed the
format for the normal log to override the highlighter.

We still need the hightlighter for the log with diff.

Therefore, a separation between highlighting
and parsing the log is needed to populate the
combobox for commit selection again.

Change-Id: I902ce548fc25875f2cd67b165283ff1236329afa
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/git/gitclient.cpp
@@ -1048,7 +1048,7 @@ static QStringList normalLogArguments()
 
     const QString logArgs = QStringLiteral(
                 "--pretty=format:"
-                "Commit: %C(%1)%H%Creset %C(%2)%d%Creset%n"
+                "commit %C(%1)%H%Creset %C(%2)%d%Creset%n"
                 "Author: %C(%3)%an <%ae>%Creset%n"
                 "Date:   %C(%4)%cD%Creset%n%n"
                 "%C(%5)%s%Creset%n%n%b%n"
@@ -1092,8 +1092,12 @@ void GitClient::log(const QString &workingDirectory, const QString &fileName,
         arguments << "-n" << QString::number(logCount);
 
     arguments << argWidget->arguments();
-    if (arguments.contains(patchOption))
+    if (arguments.contains(patchOption)) {
         arguments.removeAll(colorOption);
+        editor->setHighlightingEnabled(true);
+    } else {
+        editor->setHighlightingEnabled(false);
+    }
     if (!arguments.contains(graphOption) && !arguments.contains(patchOption))
         arguments << normalLogArguments();
 

src/plugins/vcsbase/diffandloghighlighter.cpp
@@ -115,6 +115,7 @@ public:
     QTextCharFormat m_addedTrailingWhiteSpaceFormat;
 
     Internal::FoldingState m_foldingState;
+    bool m_enabled = true;
 };
 
 TextEditor::TextStyle DiffAndLogHighlighterPrivate::analyzeLine(const QString &text) const
@@ -179,16 +180,18 @@ void DiffAndLogHighlighter::highlightBlock(const QString &text)
     const int length = text.length();
     const TextEditor::TextStyle format = d->analyzeLine(text);
 
-    if (format == TextEditor::C_ADDED_LINE) {
+    if (d->m_enabled) {
+        if (format == TextEditor::C_ADDED_LINE) {
             // Mark trailing whitespace.
             const int trimmedLen = trimmedLength(text);
             setFormatWithSpaces(text, 0, trimmedLen, formatForCategory(format));
             if (trimmedLen != length)
                 setFormat(trimmedLen, length - trimmedLen, d->m_addedTrailingWhiteSpaceFormat);
-    } else if (format != TextEditor::C_TEXT) {
-        setFormatWithSpaces(text, 0, length, formatForCategory(format));
-    } else {
-        formatSpaces(text);
+        } else if (format != TextEditor::C_TEXT) {
+            setFormatWithSpaces(text, 0, length, formatForCategory(format));
+        } else {
+            formatSpaces(text);
+        }
     }
 
     // codefolding:
@@ -239,6 +242,11 @@ void DiffAndLogHighlighter::setFontSettings(const TextEditor::FontSettings &font
 {
     SyntaxHighlighter::setFontSettings(fontSettings);
     d->updateOtherFormats();
+}
+
+void DiffAndLogHighlighter::setEnabled(bool e)
+{
+    d->m_enabled = e;
 }
 
 } // namespace VcsBase

src/plugins/vcsbase/diffandloghighlighter.h
@@ -53,6 +53,8 @@ public:
 
     void setFontSettings(const TextEditor::FontSettings &fontSettings) override;
 
+    void setEnabled(bool e);
+
 private:
     friend class DiffAndLogHighlighterPrivate;
     DiffAndLogHighlighterPrivate *const d;

src/plugins/vcsbase/vcsbaseeditor.cpp
@@ -828,6 +828,12 @@ void VcsBaseEditorWidget::setFileLogAnnotateEnabled(bool e)
     d->m_fileLogAnnotateEnabled = e;
 }
 
+void VcsBaseEditorWidget::setHighlightingEnabled(bool e)
+{
+    auto dh = static_cast<DiffAndLogHighlighter *>(textDocument()->syntaxHighlighter());
+    dh->setEnabled(e);
+}
+
 QString VcsBaseEditorWidget::workingDirectory() const
 {
     return d->m_workingDirectory;

src/plugins/vcsbase/vcsbaseeditor.h
@@ -192,6 +192,8 @@ public:
     bool isFileLogAnnotateEnabled() const;
     void setFileLogAnnotateEnabled(bool e);
 
+    void setHighlightingEnabled(bool e);
+
     QTextCodec *codec() const;
     void setCodec(QTextCodec *);
 