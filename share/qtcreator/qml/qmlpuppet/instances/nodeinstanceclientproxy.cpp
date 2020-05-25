fd55b36b7c08f6e275f6a875382c94b4545474e2
Thomas Hartmann <thomas.hartmann@qt.io>
2020-05-25 14:43:05 +0200

    QmlDesigner: Increase alive timer
    Change-Id: I2f3c1c95f631925f66dad2ce1f7635756edb7ae5
    Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

--- share/qtcreator/qml/qmlpuppet/instances/nodeinstanceclientproxy.cpp
+++ share/qtcreator/qml/qmlpuppet/instances/nodeinstanceclientproxy.cpp
@@ -92,7 +92,7 @@ NodeInstanceClientProxy::NodeInstanceClientProxy(QObject *parent)
       m_synchronizeId(-1)
 {
     connect(&m_puppetAliveTimer, &QTimer::timeout, this, &NodeInstanceClientProxy::sendPuppetAliveCommand);
-    m_puppetAliveTimer.setInterval(1000);
+    m_puppetAliveTimer.setInterval(2000);
     m_puppetAliveTimer.start();
 }
 
