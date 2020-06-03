f392d134198bcce565ccb009d51cd29ac9b31639
Cristian Adam <cristian.adam@qt.io>
2020-06-03 18:45:12 +0200

    CMake: Fix for installation
    With CMake 3.17-dev the install step is failing because of the symlink.
    
    Change-Id: I012ce95f5cc61f4b5b9d6a4c48ded0f738b581f1
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- share/qtcreator/indexer_preincludes/QtCore/qglobal.h
+++ share/qtcreator/indexer_preincludes/QtCore/qglobal.h
@@ -1,1 +1,12 @@
-../qglobal.h
+#include_next <qglobal.h>
+
+#ifdef Q_DECL_IMPORT
+#undef Q_DECL_IMPORT
+#endif
+
+#ifdef Q_DECL_EXPORT
+#undef Q_DECL_EXPORT
+#endif
+
+#define Q_DECL_IMPORT
+#define Q_DECL_EXPORT
