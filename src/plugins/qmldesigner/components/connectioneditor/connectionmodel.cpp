3ad76aa57f75a41ea5dd1c35e4682c657ac32ce4
Thomas Hartmann <thomas.hartmann@qt.io>
2020-06-11 19:09:19 +0200

    QmlDesigner: Add trigger "action" for transition
    Change-Id: Ia498af6ad5014fbae61bd51ddb066ce61ba647b5
    Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

--- src/plugins/qmldesigner/components/connectioneditor/connectionmodel.cpp
+++ src/plugins/qmldesigner/components/connectioneditor/connectionmodel.cpp
@@ -271,6 +271,9 @@ void ConnectionModel::addConnection()
                     if (QmlItemNode(selectedNode).isFlowActionArea())
                         source = selectedNode.validId() + ".trigger()";
 
+                    if (QmlVisualNode(selectedNode).isFlowTransition())
+                        source = selectedNode.validId() + ".trigger()";
+
                     if (!connectionView()->selectedModelNodes().constFirst().id().isEmpty())
                     newNode.bindingProperty("target").setExpression(selectedNode.id());
                     else
