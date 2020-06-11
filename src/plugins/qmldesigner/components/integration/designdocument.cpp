2f66b1d49a71fbabd81de79c4d51e4804ea0fd8d
Thomas Hartmann <thomas.hartmann@qt.io>
2020-06-11 19:10:19 +0200

    QmlDesigner: Rename 3d-active-scene to active3dScene
    For annotations this has to be a valid QML property.
    
    Task-number: QDS-2269
    Change-Id: Ib6483a9dd673ddf24b8688909c599a518dfb361c
    Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

--- src/plugins/qmldesigner/components/integration/designdocument.cpp
+++ src/plugins/qmldesigner/components/integration/designdocument.cpp
@@ -469,7 +469,7 @@ void DesignDocument::paste()
                                            [](const ModelNode &node) { return !node.isSubclassOf("QtQuick3D.Node"); })
                               == selectedNodes.end();
             if (all3DNodes) {
-                int activeSceneId = rootModelNode().auxiliaryData("3d-active-scene").toInt();
+                int activeSceneId = rootModelNode().auxiliaryData("active3dScene").toInt();
                 if (activeSceneId != -1) {
                     NodeListProperty sceneNodeProperty
                             = QmlVisualNode::findSceneNodeProperty(rootModelNode().view(), activeSceneId);
@@ -515,7 +515,7 @@ void DesignDocument::paste()
             } else {
                 // if selection is empty and this is a 3D Node, paste it under the active scene
                 if (pastedNode.isSubclassOf("QtQuick3D.Node")) {
-                    int activeSceneId = rootModelNode().auxiliaryData("3d-active-scene").toInt();
+                    int activeSceneId = rootModelNode().auxiliaryData("active3dScene").toInt();
                     if (activeSceneId != -1) {
                         NodeListProperty sceneNodeProperty
                                 = QmlVisualNode::findSceneNodeProperty(rootModelNode().view(), activeSceneId);
