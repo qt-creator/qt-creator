9a0acda2c9aee63f265e8ddd5012263daa3b24c5
Marco Bubke <marco.bubke@qt.io>
2020-06-13 20:40:03 +0200

    ClangFormat: Fix warning
    Change-Id: Ifdf4bfb7662017b2ba2e28074808919f7a726a8a
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- src/plugins/clangformat/clangformatplugin.cpp
+++ src/plugins/clangformat/clangformatplugin.cpp
@@ -153,7 +153,12 @@ bool ClangFormatPlugin::initialize(const QStringList &arguments, QString *errorS
     }
     return true;
 #else
+#ifdef _MSC_VER
+#pragma message( \
+    "ClangFormat: building dummy plugin due to unmodified Clang, see README.md for more info")
+#else
 #warning ClangFormat: building dummy plugin due to unmodified Clang, see README.md for more info
+#endif
     *errorString = "Disabling ClangFormat plugin as it has not been built against a modified Clang's libFormat."
                    "For more information see the Qt Creator README at "
                    "https://code.qt.io/cgit/qt-creator/qt-creator.git/tree/README.md";
