d80a83c97c15f904866a0daaf4f66849f3c37473
hjk <hjk@qt.io>
2020-06-18 12:19:58 +0200

    Qt6: Update ToolTip hack
    Task-number: QTCREATORBUG-24098
    Change-Id: I4ddc43ac0135ff22fe78f6a0efc75d7be2a3c7f4
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/libs/utils/tooltip/tips.cpp
+++ src/libs/utils/tooltip/tips.cpp
@@ -74,15 +74,36 @@ const QMetaObject *TipLabel::metaObject() const
 {
     // CSS Tooltip styling depends on a the name of this class.
     // So set up a minimalist QMetaObject to fake a class name "QTipLabel":
+
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     static const uint tip_label_meta_data[15] = { 8 /* moc revision */ };
+
     static const QMetaObject tipMetaObject {
-         &QLabel::staticMetaObject,
-         QByteArrayLiteral("QTipLabel").data_ptr(),
-         tip_label_meta_data,
-         nullptr,
-         nullptr,
-         nullptr
+         &QLabel::staticMetaObject,                  // SuperData superdata;
+         QByteArrayLiteral("QTipLabel").data_ptr(),  // const QByteArrayData *stringdata;
+         tip_label_meta_data,                        // const uint *data;
+         nullptr,                                    // StaticMetacallFunction static_metacall;
+         nullptr,                                    // const SuperData *relatedMetaObjects;
+         nullptr                                     // void *extradata;
     };
+#else
+    static const uint tip_label_meta_data[15] = { 9 /* moc revision */ };
+
+    struct qt_meta_stringdata_Utils_t {
+        const uint offsetsAndSize[2];
+        char stringdata0[24];
+    } qt_meta_stringdata =  { 8, sizeof("QTipLabel"), "QTipLabel" };
+
+    static const QMetaObject tipMetaObject {
+        &QLabel::staticMetaObject,                    // SuperData superdata
+        qt_meta_stringdata.offsetsAndSize,            // const uint *stringdata;
+        tip_label_meta_data,                          // const uint *data;
+        nullptr,                                      // StaticMetacallFunction static_metacall;
+        nullptr,                                      // const SuperData *relatedMetaObjects;
+        nullptr,                                      // QtPrivate::QMetaTypeInterface *const *metaTypes;
+        nullptr,                                      // void *extradata;
+    };
+#endif
 
     return &tipMetaObject;
 }
