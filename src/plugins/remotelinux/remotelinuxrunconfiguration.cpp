qt-creator/qt-creator

commit c2127c9ec42a2a8ced7aff3547d844e6b421d718

ProjectExplorer: Introduce and use a Target::buildSystemUpdated signal
Change-Id: I497d46866146600a1ed9162bd720c574ee4d4769
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

src/plugins/remotelinux/remotelinuxrunconfiguration.cpp
@@ -29,6 +29,7 @@
 #include "remotelinuxx11forwardingaspect.h"
 #include "remotelinuxenvironmentaspect.h"
 
+#include <projectexplorer/buildsystem.h>
 #include <projectexplorer/buildtargetinfo.h>
 #include <projectexplorer/deploymentdata.h>
 #include <projectexplorer/kitinformation.h>
@@ -68,11 +69,7 @@ RemoteLinuxRunConfiguration::RemoteLinuxRunConfiguration(Target *target, Core::I
     if (HostOsInfo::isAnyUnixHost())
         addAspect<X11ForwardingAspect>();
 
-    connect(target, &Target::deploymentDataChanged,
-            this, &RemoteLinuxRunConfiguration::updateTargetInformation);
-    connect(target, &Target::applicationTargetsChanged,
-            this, &RemoteLinuxRunConfiguration::updateTargetInformation);
-    connect(target, &Target::parsingFinished,
+    connect(target, &Target::buildSystemUpdated,
             this, &RemoteLinuxRunConfiguration::updateTargetInformation);
     connect(target, &Target::kitChanged,
             this, &RemoteLinuxRunConfiguration::updateTargetInformation);

src/plugins/android/androidrunconfiguration.cpp
@@ -31,6 +31,7 @@
 #include "androidmanager.h"
 #include "adbcommandswidget.h"
 
+#include <projectexplorer/buildsystem.h>
 #include <projectexplorer/kitinformation.h>
 #include <projectexplorer/project.h>
 #include <projectexplorer/target.h>
@@ -132,7 +133,7 @@ AndroidRunConfiguration::AndroidRunConfiguration(Target *target, Core::Id id)
     postStartShellCmdAspect->setSettingsKey("Android.PostStartShellCmdListKey");
     postStartShellCmdAspect->setLabel(tr("Shell commands to run on Android device after application quits."));
 
-    connect(target, &Target::parsingFinished, this, [this] {
+    connect(target, &Target::buildSystemUpdated, this, [this] {
         updateTargetInformation();
         AndroidManager::updateGradleProperties(this->target(), buildKey());
     });

src/plugins/autotoolsprojectmanager/autotoolsbuildsystem.cpp
@@ -153,6 +153,8 @@ void AutotoolsBuildSystem::makefileParsingFinished()
 
     m_makefileParserThread->deleteLater();
     m_makefileParserThread = nullptr;
+
+    emitBuildSystemUpdated();
 }
 
 static QStringList filterIncludes(const QString &absSrc, const QString &absBuild,

src/plugins/baremetal/baremetalrunconfiguration.cpp
@@ -26,6 +26,7 @@
 #include "baremetalconstants.h"
 #include "baremetalrunconfiguration.h"
 
+#include <projectexplorer/buildsystem.h>
 #include <projectexplorer/buildtargetinfo.h>
 #include <projectexplorer/project.h>
 #include <projectexplorer/runconfigurationaspects.h>
@@ -49,13 +50,9 @@ BareMetalRunConfiguration::BareMetalRunConfiguration(Target *target, Core::Id id
     addAspect<ArgumentsAspect>();
     addAspect<WorkingDirectoryAspect>();
 
-    connect(target, &Target::deploymentDataChanged,
-            this, &BareMetalRunConfiguration::updateTargetInformation);
-    connect(target, &Target::applicationTargetsChanged,
-            this, &BareMetalRunConfiguration::updateTargetInformation);
     connect(target, &Target::kitChanged,
             this, &BareMetalRunConfiguration::updateTargetInformation); // Handles device changes, etc.
-    connect(target, &Target::parsingFinished,
+    connect(target, &Target::buildSystemUpdated,
             this, &BareMetalRunConfiguration::updateTargetInformation);
 }
 

src/plugins/boot2qt/qdbrunconfiguration.cpp
@@ -27,6 +27,7 @@
 
 #include "qdbconstants.h"
 
+#include <projectexplorer/buildsystem.h>
 #include <projectexplorer/buildtargetinfo.h>
 #include <projectexplorer/deploymentdata.h>
 #include <projectexplorer/project.h>
@@ -85,13 +86,9 @@ QdbRunConfiguration::QdbRunConfiguration(Target *target, Core::Id id)
     addAspect<WorkingDirectoryAspect>();
     addAspect<FullCommandLineAspect>(this);
 
-    connect(target, &Target::deploymentDataChanged,
-            this, &QdbRunConfiguration::updateTargetInformation);
-    connect(target, &Target::applicationTargetsChanged,
-            this, &QdbRunConfiguration::updateTargetInformation);
     connect(target, &Target::kitChanged,
             this, &QdbRunConfiguration::updateTargetInformation);
-    connect(target, &Target::parsingFinished,
+    connect(target, &Target::buildSystemUpdated,
             this, &QdbRunConfiguration::updateTargetInformation);
 
     setDefaultDisplayName(tr("Run on Boot2Qt Device"));

src/plugins/cmakeprojectmanager/cmakebuildsystem.cpp
@@ -362,6 +362,8 @@ void CMakeBuildSystem::combineScanAndParse()
     }
 
     m_currentGuard = {};
+
+    emitBuildSystemUpdated();
 }
 
 void CMakeBuildSystem::checkAndReportError(QString &errorMessage)

src/plugins/compilationdatabaseprojectmanager/compilationdatabaseproject.cpp
@@ -502,6 +502,8 @@ void CompilationDatabaseBuildSystem::updateDeploymentData()
         m_deployFileWatcher->addFile(deploymentFilePath.toString(),
                                      FileSystemWatcher::WatchModifiedDate);
     }
+
+    emitBuildSystemUpdated();
 }
 
 static TextEditor::TextDocument *createCompilationDatabaseDocument()

src/plugins/genericprojectmanager/genericproject.cpp
@@ -498,6 +498,8 @@ void GenericBuildSystem::refresh(RefreshOptions options)
     refreshCppCodeModel();
     updateDeploymentData();
     guard.markAsSuccess();
+
+    emitBuildSystemUpdated();
 }
 
 /**

commit fc53bae32895494dffcc4e2e80e5823148c37e2d

Fix the cmake/RemoteLinux combination
This broke in c2127c9ec4.

Task-number: QTCREATORBUG-21235
Change-Id: I732ce3ad46370141f62fc80792073f5a8ad2f234
Reviewed-by: hjk <hjk@qt.io>

src/plugins/remotelinux/remotelinuxrunconfiguration.cpp
@@ -79,6 +79,7 @@ RemoteLinuxRunConfiguration::RemoteLinuxRunConfiguration(Target *target, Core::I
     });
 
     connect(target, &Target::buildSystemUpdated, this, &RunConfiguration::update);
+    connect(target, &Target::deploymentDataChanged, this, &RunConfiguration::update);
     connect(target, &Target::kitChanged, this, &RunConfiguration::update);
 }
 