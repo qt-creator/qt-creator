59b84c8d19bdaac6cdafba2bd0173113a7a1bdc8
Christian Kandeler <christian.kandeler@qt.io>
2020-06-24 12:24:27 +0200

    Wizards: Update templates in regard to deprecation warnings
    The QT_DEPRECATED_WARNINGS macro has no effect anymore, so let's remove
    it along with the huge chunk of comments that took up an inordinate
    amount of space in the resulting project file. The concise comment for
    the QT_DISABLE_DEPRECATED_BEFORE macro is perfectly sufficient.
    
    Fixes: QTCREATORBUG-24244
    Change-Id: I549c16c2c037bb64c2833e4807047ab3a8b2103f
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/plugins/qmakeprojectmanager/wizards/qtprojectparameters.cpp
+++ src/plugins/qmakeprojectmanager/wizards/qtprojectparameters.cpp
@@ -112,14 +112,8 @@ void QtProjectParameters::writeProFile(QTextStream &str) const
 
     if (qtVersionSupport != SupportQt4Only) {
         str << "\n"
-               "# The following define makes your compiler emit warnings if you use\n"
-               "# any feature of Qt which has been marked as deprecated (the exact warnings\n"
-               "# depend on your compiler). Please consult the documentation of the\n"
-               "# deprecated API in order to know how to port your code away from it.\n"
-               "DEFINES += QT_DEPRECATED_WARNINGS\n\n"
-               "# You can also make your code fail to compile if you use deprecated APIs.\n"
+               "# You can make your code fail to compile if you use deprecated APIs.\n"
                "# In order to do so, uncomment the following line.\n"
-               "# You can also select to disable deprecated APIs only up to a certain version of Qt.\n"
                "#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0\n";
     }
 }
