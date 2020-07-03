qt-creator/qt-creator

commit 5efa84830bb41bfc11db9295df3ad34f5b9bd9f5

Debugger: Remove use of native values in dumper.py
Lowers the barrier for other debugger backends.

Change-Id: I09e0ad09548b6b4220175245cc0d845ac5aa29d0
Reviewed-by: Christian Stenger <christian.stenger@qt.io>
Reviewed-by: hjk <hjk@qt.io>

share/qtcreator/debugger/gdbbridge.py
@@ -248,13 +248,32 @@ class Dumper(DumperBase):
         return self.fromNativeValue(nativeValue)
 
     def fromNativeValue(self, nativeValue):
-        #self.check(isinstance(nativeType, gdb.Value))
+        #self.check(isinstance(nativeValue, gdb.Value))
+        nativeType = nativeValue.type
         val = self.Value(self)
-        val.nativeValue = nativeValue
         if not nativeValue.address is None:
             val.laddress = toInteger(nativeValue.address)
-        val.type = self.fromNativeType(nativeValue.type)
+        else:
+            size = nativeType.sizeof
+            chars = self.lookupNativeType("unsigned char")
+            y = nativeValue.cast(chars.array(0, int(nativeType.sizeof - 1)))
+            buf = bytearray(struct.pack('x' * size))
+            for i in range(size):
+                buf[i] = int(y[i])
+            val.ldata = bytes(buf)
+
+        val.type = self.fromNativeType(nativeType)
         val.lIsInScope = not nativeValue.is_optimized_out
+        code = nativeType.code
+        if code == gdb.TYPE_CODE_ENUM:
+            val.ldisplay = str(nativeValue)
+            intval = int(nativeValue)
+            if val.ldisplay != intval:
+                val.ldisplay += ' (%s)' % intval
+        elif code == gdb.TYPE_CODE_COMPLEX:
+            val.ldisplay = str(nativeValue)
+        elif code == gdb.TYPE_CODE_ARRAY:
+            val.type.ltarget = nativeValue[0].type.unqualified()
         return val
 
     def fromNativeType(self, nativeType):
@@ -284,18 +303,6 @@ class Dumper(DumperBase):
             gdb.TYPE_CODE_STRING : TypeCodeFortranString,
         }[nativeType.code]
         return typeobj
-
-    def nativeValueDereference(self, nativeValue):
-        return self.nativeValueDownCast(nativeValue.dereference())
-
-    def nativeValueCast(self, nativeValue, nativeType):
-        try:
-            return self.fromNativeValue(nativeValue.cast(nativeType))
-        except:
-            return None
-
-    def nativeValueAddressOf(self, nativeValue):
-        return toInteger(nativeValue.address)
 
     def nativeTypeDereference(self, nativeType):
         return self.fromNativeType(nativeType.strip_typedefs().target())
@@ -401,25 +408,6 @@ class Dumper(DumperBase):
         while typeobj.code == gdb.TYPE_CODE_TYPEDEF:
             typeobj = typeobj.strip_typedefs().unqualified()
         return self.fromNativeType(typeobj)
-
-    def nativeValueChildFromField(self, nativeValue, field):
-        #warn("EXTRACTING: %s FROM %s" % (field, nativeValue))
-        if field.nativeIndex is not None:
-            nativeField = nativeValue.type.fields()[field.nativeIndex]
-            #warn("NATIVE FIELD: %s" % nativeField)
-            if nativeField.is_base_class:
-                return self.fromNativeValue(nativeValue.cast(nativeField.type))
-            try:
-                # Fails with GDB 7.5.
-                return self.nativeValueDownCast(nativeValue[nativeField])
-            except:
-                # The generic handling is almost good enough, but does not
-                # downcast the produced values.
-                return None
-        if field.name is not None:
-            return self.nativeValueDownCast(nativeValue[field.name])
-        error("FIELD EXTARCTION FAILED: %s" % field)
-        return None
 
     def listOfLocals(self, partialVar):
         frame = gdb.selected_frame()
@@ -570,20 +558,6 @@ class Dumper(DumperBase):
             else:
                 return None
         return self.fromNativeValue(val)
-
-    def nativeValueAsBytes(self, nativeValue, size):
-        # Assume it's a (backend specific) Value.
-        if nativeValue.address:
-            return self.readRawMemory(nativeValue.address, size)
-        # No address. Possibly the result of an inferior call.
-        # Note: Only a cast to unsigned char[sizeof(origtype)] succeeds
-        # in this situation in gdb.
-        chars = self.lookupNativeType("unsigned char")
-        y = nativeValue.cast(chars.array(0, int(nativeValue.type.sizeof - 1)))
-        buf = bytearray(struct.pack('x' * size))
-        for i in range(size):
-            buf[i] = int(y[i])
-        return bytes(buf)
 
     def callHelper(self, rettype, value, function, args):
         # args is a tuple.
@@ -963,47 +937,16 @@ class Dumper(DumperBase):
             cmd = "set variable (%s)=%s" % (expr, value)
             gdb.execute(cmd)
 
-    def hasVTable(self, typeobj):
-        fields = typeobj.fields()
-        if len(fields) == 0:
-            return False
-        if fields[0].isBaseClass:
-            return hasVTable(fields[0].type)
-        return str(fields[0].type) ==  "int (**)(void)"
-
-    def dynamicTypeName(self, value):
-        if self.hasVTable(value.type):
-            #vtbl = str(gdb.parse_and_eval("{int(*)(int)}%s" % int(value.address)))
-            try:
-                # Fails on 7.1 due to the missing to_string.
-                vtbl = gdb.execute("info symbol {int*}%s" % int(value.address),
-                    to_string = True)
-                pos1 = vtbl.find("vtable ")
-                if pos1 != -1:
-                    pos1 += 11
-                    pos2 = vtbl.find(" +", pos1)
-                    if pos2 != -1:
-                        return vtbl[pos1 : pos2]
-            except:
-                pass
-        return str(value.type)
-
-    def nativeValueDownCast(self, nativeValue):
-        try:
-            return self.fromNativeValue(nativeValue.cast(nativeValue.dynamic_type))
-        except:
-            return self.fromNativeValue(nativeValue)
-
-    def expensiveDowncast(self, value):
-        try:
-            return value.cast(value.dynamic_type)
-        except:
-            pass
-        try:
-            return value.cast(self.lookupType(self.dynamicTypeName(value)))
-        except:
-            pass
-        return value
+    def nativeDynamicTypeName(self, address, baseType):
+        vtbl = gdb.execute("info symbol {%s*}0x%x" % (baseType.name, address), to_string = True)
+        pos1 = vtbl.find("vtable ")
+        if pos1 == -1:
+            return None
+        pos1 += 11
+        pos2 = vtbl.find(" +", pos1)
+        if pos2 == -1:
+            return None
+        return vtbl[pos1 : pos2]
 
     def enumExpression(self, enumType, enumValue):
         return self.qtNamespace() + "Qt::" + enumValue

share/qtcreator/debugger/lldbbridge.py
@@ -57,20 +57,6 @@ def check(exp):
     if not exp:
         raise RuntimeError("Check failed")
 
-def impl_SBValue__add__(self, offset):
-    if self.GetType().IsPointerType():
-        itemsize = self.GetType().GetPointeeType().GetByteSize()
-        address = self.GetValueAsUnsigned() + offset * itemsize
-        address = address & 0xFFFFFFFFFFFFFFFF  # Force unsigned
-        return self.CreateValueFromAddress(None, address,
-                self.GetType().GetPointeeType()).AddressOf()
-
-    raise RuntimeError("SBValue.__add__ not implemented: %s" % self.GetType())
-    return NotImplemented
-
-
-lldb.SBValue.__add__ = impl_SBValue__add__
-
 class Dumper(DumperBase):
     def __init__(self):
         DumperBase.__init__(self)
@@ -154,15 +140,42 @@ class Dumper(DumperBase):
 
     def fromNativeValue(self, nativeValue):
         nativeValue.SetPreferSyntheticValue(False)
+        nativeType = nativeValue.GetType()
         val = self.Value(self)
-        val.nativeValue = nativeValue
-        val.type = self.fromNativeType(nativeValue.type)
+        val.type = self.fromNativeType(nativeType)
         val.lIsInScope = nativeValue.IsInScope()
         #val.name = nativeValue.GetName()
-        try:
+        data = nativeValue.GetData()
+        error = lldb.SBError()
+        size = nativeValue.GetType().GetByteSize()
+        if size > 0: # Happens regularly e.g. for cross-shared-object types.
+            val.ldata = data.ReadRawData(error, 0, size)
+        code = nativeType.GetTypeClass()
+        if code not in (lldb.eTypeClassPointer, lldb.eTypeClassReference):
             val.laddress = int(nativeValue.GetLoadAddress())
-        except:
-            pass
+        if code == lldb.eTypeClassEnumeration:
+            intval = nativeValue.GetValueAsSigned()
+            if hasattr(nativeType, 'get_enum_members_array'):
+                for enumMember in nativeType.get_enum_members_array():
+                    # Even when asking for signed we get unsigned with LLDB 3.8.
+                    diff = enumMember.GetValueAsSigned() - intval
+                    mask = (1 << nativeType.GetByteSize() * 8) - 1
+                    if diff & mask == 0:
+                        path = nativeType.GetName().split('::')
+                        path[-1] = enumMember.GetName()
+                        val.ldisplay = "%s (%d)" % ('::'.join(path), intval)
+            val.ldisplay = "%d" % intval
+        elif code in (lldb.eTypeClassComplexInteger, lldb.eTypeClassComplexFloat):
+            val.ldisplay = str(nativeValue.GetValue())
+        elif code == lldb.eTypeClassArray:
+            if hasattr(nativeType, "GetArrayElementType"): # New in 3.8(?) / 350.x
+                val.type.ltarget = self.fromNativeType(nativeType.GetArrayElementType())
+            else:
+                fields = nativeType.get_fields_array()
+                if len(fields):
+                    val.type.ltarget = self.fromNativeType(fields[0])
+        elif code == lldb.eTypeClassVector:
+            val.type.ltarget = self.fromNativeType(nativeType.GetVectorElementType())
         return val
 
     def fromNativeType(self, nativeType):
@@ -217,6 +230,7 @@ class Dumper(DumperBase):
             addr = 0x7fffffffe0a0
         sbaddr = lldb.SBAddress(addr, self.target)
         dummyValue = self.target.CreateValueFromAddress('x', sbaddr, nativeType)
+        dummyValue.SetPreferSyntheticValue(False)
 
         anonNumber = 0
 
@@ -246,8 +260,8 @@ class Dumper(DumperBase):
                 anonNumber += 1
                 fieldName = "#%s" % anonNumber
             fieldType = dummyChild.GetType()
-            #warn("CHILD AT: %s: %s %s" % (i, fieldName, fieldType))
-            #warn(" AT: %s: %s %s" % (i, fieldName, fieldType))
+            #warn("CHILD AT: %s: %s %s" % (i, fieldName, fieldType.GetName()))
+            #warn(" AT: %s: %s %s" % (i, fieldName, fieldType.GetName()))
             caddr = dummyChild.AddressOf().GetValueAsUnsigned()
             child = self.Value(self)
             child.type = self.fromNativeType(fieldType)
@@ -298,15 +312,6 @@ class Dumper(DumperBase):
         #warn("FIELDS: %s" % fields)
         return fields
 
-    def nativeValueDereference(self, nativeValue):
-        result = nativeValue.Dereference()
-        if not result.IsValid():
-            return None
-        return self.fromNativeValue(result)
-
-    def nativeValueCast(self, nativeValue, nativeType):
-        return self.fromNativeValue(nativeValue.Cast(nativeType))
-
     def nativeTypeUnqualified(self, nativeType):
         return self.fromNativeType(nativeType.GetUnqualifiedType())
 
@@ -317,46 +322,6 @@ class Dumper(DumperBase):
         if hasattr(typeobj, 'GetCanonicalType'):
             return self.fromNativeType(typeobj.GetCanonicalType())
         return self.fromNativeType(typeobj)
-
-    def nativeValueChildFromNameHelper(self, nativeValue, fieldName):
-        val = nativeValue.GetChildMemberWithName(fieldName)
-        if val.IsValid():
-            return val
-        nativeType = nativeValue.GetType()
-        for i in xrange(nativeType.GetNumberOfDirectBaseClasses()):
-            baseField = nativeType.GetDirectBaseClassAtIndex(i)
-            baseType = baseField.GetType()
-            base = nativeValue.Cast(baseType)
-            val = self.nativeValueChildFromNameHelper(base, fieldName)
-            if val is not None:
-                return val
-        return None
-
-    def nativeValueChildFromField(self, nativeValue, field):
-        val = None
-        if field.isVirtualBase is True:
-            #warn("FETCHING VIRTUAL BASE: %s: %s" % (field.baseIndex, field.name))
-            pass
-        if field.nativeIndex is not None:
-            #warn("FETCHING BY NATIVE INDEX: %s: %s" % (field.nativeIndex, field.name))
-            val = nativeValue.GetChildAtIndex(field.nativeIndex)
-            if val.IsValid():
-                return self.fromNativeValue(val)
-        elif field.baseIndex is not None:
-            baseClass = nativeValue.GetType().GetDirectBaseClassAtIndex(field.baseIndex)
-            if baseClass.IsValid():
-                #warn("BASE IS VALID, TYPE: %s" % baseClass.GetType())
-                #warn("BASE BITPOS: %s" % field.lbitpos)
-                #warn("BASE BITSIZE: %s" % field.lbitsize)
-                # FIXME: This is wrong for virtual bases.
-                return None  # Let standard behavior kick in.
-        else:
-            #warn("FETCHING BY NAME: %s: %s" % (field, field.name))
-            val = self.nativeValueChildFromNameHelper(nativeValue, field.name)
-        if val is not None:
-            return self.fromNativeValue(val)
-        raise RuntimeError("No such member '%s' in %s" % (field.name, nativeValue.type))
-        return None
 
     def nativeTypeFirstBase(self, nativeType):
         #warn("FIRST BASE FROM: %s" % nativeType)
@@ -377,6 +342,13 @@ class Dumper(DumperBase):
                     path[-1] = enumMember.GetName()
                     return "%s (%d)" % ('::'.join(path), intval)
         return "%d" % intval
+
+    def nativeDynamicTypeName(self, address, baseType):
+        return None # FIXME: Seems sufficient, no idea why.
+        addr = self.target.ResolveLoadAddress(address)
+        ctx = self.target.ResolveSymbolContextForAddress(addr, 0)
+        sym = ctx.GetSymbol()
+        return sym.GetName()
 
     def stateName(self, s):
         try:
@@ -479,9 +451,6 @@ class Dumper(DumperBase):
             inner = self.extractTemplateArgument(nativeType.GetName(), position)
             #warn("INNER: %s" % inner)
             return self.lookupType(inner)
-
-    def nativeValueAddressOf(self, nativeValue):
-        return int(value.GetLoadAddress())
 
     def nativeTypeDereference(self, nativeType):
         return self.fromNativeType(nativeType.GetPointeeType())
@@ -917,15 +886,6 @@ class Dumper(DumperBase):
             return bytes()
         return res
 
-    def nativeValueAsBytes(self, nativeValue, size):
-        data = nativeValue.GetData()
-        buf = bytearray(struct.pack('x' * size))
-        error = lldb.SBError()
-        #data.ReadRawData(error, 0, buf)
-        for i in range(size):
-            buf[i] = data.GetUnsignedInt8(error, i)
-        return bytes(buf)
-
     def findStaticMetaObject(self, typeName):
         symbolName = self.mangleName(typeName + '::staticMetaObject')
         symbol = self.target.FindFirstGlobalVariable(symbolName)
@@ -983,6 +943,7 @@ class Dumper(DumperBase):
                     if len(statics):
                         for i in xrange(len(statics)):
                             staticVar = statics[i]
+                            staticVar.SetPreferSyntheticValue(False)
                             typename = staticVar.GetType().GetName()
                             name = staticVar.GetName()
                             with SubItem(self, i):
@@ -1004,6 +965,7 @@ class Dumper(DumperBase):
 
         variables = []
         for val in values:
+            val.SetPreferSyntheticValue(False)
             if not val.IsValid():
                 continue
             self.currentContextValue = val

share/qtcreator/debugger/qttypes.py
@@ -887,11 +887,11 @@ def qdump__QLocale(d, value):
     #d.check(index <= qqLocalesCount)
     dd = value.extractPointer()
     ns = d.qtNamespace()
-    (data, ref, numberOptions) = d.split("pi{int}", dd)
+    (data, ref, numberOptions) = d.split("pi4s", dd)
     (languageId, scriptId, countryId,
                   decimal, group, listt, percent, zero,
                   minus, plus, exponential) \
-        = d.split('{short}{short}{QChar}'
+        = d.split('2s{short}2s'
                 + '{QChar}{QChar}{short}{QChar}{QChar}'
                 + '{QChar}{QChar}{QChar}', data)
     d.putStringValue(d.call("const char *", value, "name"))
@@ -899,9 +899,9 @@ def qdump__QLocale(d, value):
     if d.isExpanded():
         with Children(d):
             prefix = ns + "QLocale::"
-            d.putSubItem("country", countryId.extend(4).cast(prefix + "Country"))
-            d.putSubItem("language", languageId.extend(4).cast(prefix + "Language"))
-            d.putSubItem("numberOptions", numberOptions.cast(prefix + "NumberOptions"))
+            d.putSubItem("country", d.createValue(countryId, prefix + "Country"))
+            d.putSubItem("language", d.createValue(languageId, prefix + "Language"))
+            d.putSubItem("numberOptions", d.createValue(numberOptions, prefix + "NumberOptions"))
             d.putSubItem("decimalPoint", decimal)
             d.putSubItem("exponential", exponential)
             d.putSubItem("percent", percent)
@@ -1189,7 +1189,7 @@ def qdump__QSet(d, value):
                     typeCode = 'Pi@{%s}' % keyType.name
                     (pnext, hashval, padding1, key) = d.split(typeCode, node)
                 with SubItem(d, i):
-                    d.putItem(key, i)
+                    d.putItem(key)
                 node = hashDataNextNode(node)
 
 

share/qtcreator/debugger/stdtypes.py
@@ -227,12 +227,11 @@ def qdump__std__map(d, value):
         pairPointer = pairType.pointer()
         with PairedChildren(d, size, pairType=pairType, maxNumChild=1000):
             node = impl["_M_header"]["_M_left"]
-            typeCode = "p@{%s}@{%s}" % (pairType[0].name, pairType[1].name)
+            nodeSize = node.dereference().type.size()
+            typeCode = "@{%s}@{%s}" % (pairType[0].name, pairType[1].name)
             for i in d.childRange():
-                pair = (node + 1).cast(pairPointer).dereference()
-                d.putPairItem(i, pair)
-                #(pp, pad1, key, pad2, value) = d.split(typeCode, node.integer())
-                #d.putPairItem(i, (key, value))
+                (pad1, key, pad2, value) = d.split(typeCode, node.pointer() + nodeSize)
+                d.putPairItem(i, (key, value))
                 if node["_M_right"].integer() == 0:
                     parent = node["_M_parent"]
                     while True:
@@ -384,9 +383,12 @@ def qdump__std__set(d, value):
     if d.isExpanded():
         valueType = value.type[0]
         node = impl["_M_header"]["_M_left"]
+        nodeSize = node.dereference().type.size()
+        typeCode = "@{%s}" % valueType.name
         with Children(d, size, maxNumChild=1000, childType=valueType):
             for i in d.childRange():
-                d.putSubItem(i, (node + 1).cast(valueType.pointer()).dereference())
+                (pad, val) = d.split(typeCode, node.pointer() + nodeSize)
+                d.putSubItem(i, val)
                 if node["_M_right"].integer() == 0:
                     parent = node["_M_parent"]
                     while node == parent["_M_right"]:

tests/auto/debugger/tst_dumpers.cpp
@@ -4106,34 +4106,34 @@ void tst_Dumpers::dumper_data()
                + CoreProfile()
 
                + Check("map1", "<3 items>", "std::map<@QString, Foo>")
-               + Check("map1.0", "[0]", "", "std::pair<@QString const, Foo>")
+               + Check("map1.0", "[0] \"22.0\"", "", "std::pair<@QString const, Foo>")
                + Check("map1.0.first", "\"22.0\"", "@QString")
                + Check("map1.0.second", "", "Foo")
                + Check("map1.0.second.a", "22", "int")
-               + Check("map1.1", "[1]", "", "std::pair<@QString const, Foo>")
+               + Check("map1.1", "[1] \"33.0\"", "", "std::pair<@QString const, Foo>")
                + Check("map1.2.first", "\"44.0\"", "@QString")
                + Check("map1.2.second", "", "Foo")
                + Check("map1.2.second.a", "44", "int")
 
                + Check("map2", "<2 items>", "std::map<char const*, Foo>")
-               + Check("map2.0", "[0]", "", "std::pair<char const* const, Foo>")
+               + Check("map2.0", "[0] \"22.0\"", "", "std::pair<char const* const, Foo>")
                + CheckType("map2.0.first", "char *")
 // FIXME
                //+ Check("map2.0.first.0", "50", "char")
                + Check("map2.0.second", "", "Foo")
                + Check("map2.0.second.a", "22", "int")
-               + Check("map2.1", "[1]", "", "std::pair<char const* const, Foo>")
+               + Check("map2.1", "[1] \"33.0\"", "", "std::pair<char const* const, Foo>")
                + CheckType("map2.1.first", "char *")
                //+ Check("map2.1.first.*first", "51 '3'", "char")
                + Check("map2.1.second", "", "Foo")
                + Check("map2.1.second.a", "33", "int")
 
                + Check("map3", "<2 items>", "std::map<unsigned int, @QStringList>")
-               + Check("map3.0", "[0]", "", "std::pair<unsigned int const, @QStringList>")
+               + Check("map3.0", "[0] 11", "<1 items>", "std::pair<unsigned int const, @QStringList>")
                + Check("map3.0.first", "11", "unsigned int")
                + Check("map3.0.second", "<1 items>", "@QStringList")
                + Check("map3.0.second.0", "[0]", "\"11\"", "@QString")
-               + Check("map3.1", "[1]", "", "std::pair<unsigned int const, @QStringList>")
+               + Check("map3.1", "[1] 22", "<1 items>", "std::pair<unsigned int const, @QStringList>")
                + Check("map3.1.first", "22", "unsigned int")
                + Check("map3.1.second", "<1 items>", "@QStringList")
                + Check("map3.1.second.0", "[0]", "\"22\"", "@QString")
@@ -4141,26 +4141,26 @@ void tst_Dumpers::dumper_data()
                + Check("map4.1.second.0", "[0]", "\"22\"", "@QString")
 
                + Check("map5", "<2 items>", "std::map<@QString, float>")
-               + Check("map5.0", "[0]", "", "std::pair<@QString const, float>")
+               + Check("map5.0", "[0] \"11.0\"", FloatValue("11"), "std::pair<@QString const, float>")
                + Check("map5.0.first", "\"11.0\"", "@QString")
                + Check("map5.0.second", FloatValue("11"), "float")
-               + Check("map5.1", "[1]", "", "std::pair<@QString const, float>")
+               + Check("map5.1", "[1] \"22.0\"", FloatValue("22"), "std::pair<@QString const, float>")
                + Check("map5.1.first", "\"22.0\"", "@QString")
                + Check("map5.1.second", FloatValue("22"), "float")
 
                + Check("map6", "<2 items>", "std::map<int, @QString>")
-               + Check("map6.0", "[0]", "", "std::pair<int const, @QString>")
+               + Check("map6.0", "[0] 11", "\"11.0\"", "std::pair<int const, @QString>")
                + Check("map6.0.first", "11", "int")
                + Check("map6.0.second", "\"11.0\"", "@QString")
-               + Check("map6.1", "[1]", "", "std::pair<int const, @QString>")
+               + Check("map6.1", "[1] 22", "\"22.0\"", "std::pair<int const, @QString>")
                + Check("map6.1.first", "22", "int")
                + Check("map6.1.second", "\"22.0\"", "@QString")
 
                + Check("map7", "<3 items>", "std::map<@QString, @QPointer<@QObject>>")
-               + Check("map7.0", "[0]", "", "std::pair<@QString const, @QPointer<@QObject>>")
+               + Check("map7.0", "[0] \".\"", "", "std::pair<@QString const, @QPointer<@QObject>>")
                + Check("map7.0.first", "\".\"", "@QString")
                + Check("map7.0.second", "", "@QPointer<@QObject>")
-               + Check("map7.2", "[2]", "", "std::pair<@QString const, @QPointer<@QObject>>")
+               + Check("map7.2", "[2] \"Welt\"", "", "std::pair<@QString const, @QPointer<@QObject>>")
                + Check("map7.2.first", "\"Welt\"", "@QString");
 
 
@@ -4948,9 +4948,10 @@ void tst_Dumpers::dumper_data()
     QTest::newRow("Bitfields")
             << Data("struct S\n"
                     "{\n"
-                    "    S() : x(2), y(3), c(1), b(0), f(5), d(6), i(7) {}\n"
+                    "    S() : x(2), y(3), z(39), c(1), b(0), f(5), d(6), i(7) {}\n"
                     "    unsigned int x : 3;\n"
                     "    unsigned int y : 4;\n"
+                    "    unsigned int z : 18;\n"
                     "    bool c : 1;\n"
                     "    bool b;\n"
                     "    float f;\n"
@@ -4966,7 +4967,8 @@ void tst_Dumpers::dumper_data()
                + Check("s.d", FloatValue("6"), "double")
                + Check("s.i", "7", "int")
                + Check("s.x", "2", "unsigned int")
-               + Check("s.y", "3", "unsigned int");
+               + Check("s.y", "3", "unsigned int")
+               + Check("s.z", "39", "unsigned int");
 
 
     QTest::newRow("Function")

commit 00e814fdd18dadbea5942d002914b99fb07c4048

Debugger: Respect "Use Dynamic Object Type for Display"
Broke in 5efa84830bb41bfc11db9295df3ad34f5b9bd9f5.

Task-number: QTCREATORBUG-24280
Change-Id: Ice4b9d826d2402efb354180886d35926a2513e5a
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

share/qtcreator/debugger/dumper.py
@@ -3201,13 +3201,17 @@ class DumperBase():
                     val.laddress = self.pointer()
                     if val.laddress is None and self.laddress is not None:
                         val.laddress = self.laddress
-                    val.type = self.dumper.nativeDynamicType(val.laddress, self.type.dereference())
+                    val.type = self.type.dereference()
+                    if self.dumper.useDynamicType:
+                        val.type = self.dumper.nativeDynamicType(val.laddress, val.type)
                 else:
                     val = self.dumper.nativeValueDereferenceReference(self)
             elif self.type.code == TypeCode.Pointer:
                 if self.nativeValue is None:
                     val.laddress = self.pointer()
-                    val.type = self.dumper.nativeDynamicType(val.laddress, self.type.dereference())
+                    val.type = self.type.dereference()
+                    if self.dumper.useDynamicType:
+                        val.type = self.dumper.nativeDynamicType(val.laddress, val.type)
                 else:
                     val = self.dumper.nativeValueDereferencePointer(self)
             else:
@@ -3662,7 +3666,9 @@ class DumperBase():
                                % type(targetTypish))
         val = self.Value(self)
         val.ldata = self.toPointerData(targetAddress)
-        targetType = self.createType(targetTypish).dynamicType(targetAddress)
+        targetType = self.createType(targetTypish)
+        if self.useDynamicType:
+            targetType = targetType.dynamicType(targetAddress)
         val.type = self.createPointerType(targetType)
         return val
 
@@ -3675,7 +3681,8 @@ class DumperBase():
                                % type(targetType))
         val = self.Value(self)
         val.ldata = self.toPointerData(targetAddress)
-        targetType = targetType.dynamicType(targetAddress)
+        if self.useDynamicType:
+            targetType = targetType.dynamicType(targetAddress)
         val.type = self.createReferenceType(targetType)
         return val
 
@@ -3848,7 +3855,8 @@ class DumperBase():
         if self.isInt(datish):  # Used as address.
             #DumperBase.warn('CREATING %s AT 0x%x' % (val.type.name, datish))
             val.laddress = datish
-            val.type = val.type.dynamicType(datish)
+            if self.useDynamicType:
+                val.type = val.type.dynamicType(datish)
             return val
         if isinstance(datish, bytes):
             #DumperBase.warn('CREATING %s WITH DATA %s' % (val.type.name, self.hexencode(datish)))

share/qtcreator/debugger/gdbbridge.py
@@ -229,10 +229,11 @@ class Dumper(DumperBase):
     def fromFrameValue(self, nativeValue):
         #DumperBase.warn('FROM FRAME VALUE: %s' % nativeValue.address)
         val = nativeValue
-        try:
-            val = nativeValue.cast(nativeValue.dynamic_type)
-        except:
-            pass
+        if self.useDynamicType:
+            try:
+                val = nativeValue.cast(nativeValue.dynamic_type)
+            except:
+                pass
         return self.fromNativeValue(val)
 
     def fromNativeValue(self, nativeValue):
@@ -1151,7 +1152,9 @@ class Dumper(DumperBase):
     def nativeValueDereferencePointer(self, value):
         # This is actually pretty expensive, up to 100ms.
         deref = value.nativeValue.dereference()
-        return self.fromNativeValue(deref.cast(deref.dynamic_type))
+        if self.useDynamicType:
+            deref = deref.cast(deref.dynamic_type)
+        return self.fromNativeValue(deref)
 
     def nativeValueDereferenceReference(self, value):
         nativeValue = value.nativeValue