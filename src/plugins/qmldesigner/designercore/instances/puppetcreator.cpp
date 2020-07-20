6e6b2aa855c30f264a38c9499e4c5183b5aedd0b
Tim Jenssen <tim.jenssen@qt.io>
2020-07-20 20:01:16 +0200

    Revert "qmlpuppet: do not set an empty QT_MULTILANGUAGE_DATABASE"
    This reverts commit 00b54f7faf33d679d62d309c9ccaf7ef60087483.
    
    Not sure why this was necessary - but it introduced a problem
    which needs a restart of the puppet - so it is easier to revert it.
    
    Change-Id: I4664b57eefc961ac814e4594b28bbd155be21042
    Reviewed-by: Marco Bubke <marco.bubke@qt.io>

--- src/plugins/qmldesigner/designercore/instances/puppetcreator.cpp
+++ src/plugins/qmldesigner/designercore/instances/puppetcreator.cpp
@@ -514,7 +514,7 @@ QProcessEnvironment PuppetCreator::processEnvironment() const
 
         if (auto *rc = m_target->activeRunConfiguration()) {
             if (auto multiLanguageAspect = rc->aspect<QmlProjectManager::QmlMultiLanguageAspect>()) {
-                if (multiLanguageAspect->value() && !multiLanguageAspect->databaseFilePath().isEmpty())
+                if (!multiLanguageAspect->databaseFilePath().isEmpty())
                     environment.set("QT_MULTILANGUAGE_DATABASE", multiLanguageAspect->databaseFilePath().toString());
             }
         }
