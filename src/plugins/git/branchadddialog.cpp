7919fd550a9528474b6a46d4b41ab4b229e40be2
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2020-08-04 23:32:35 +0300

    Git: Replace subsequent invalid characters with a single underscore
    ... in the Add Branch dialog.
    
    Change-Id: I7644cba6bf58316431e2daa9db5d2c26823c2711
    Reviewed-by: André Hartmann <aha_1980@gmx.de>

--- src/plugins/git/branchadddialog.cpp
+++ src/plugins/git/branchadddialog.cpp
@@ -50,7 +50,7 @@ class BranchNameValidator : public QValidator
 public:
     BranchNameValidator(const QStringList &localBranches, QObject *parent = nullptr) :
         QValidator(parent),
-        m_invalidChars(GitPlugin::invalidBranchAndRemoteNamePattern()),
+        m_invalidChars('(' + GitPlugin::invalidBranchAndRemoteNamePattern() + ")+"),
         m_localBranches(localBranches)
     {
     }
