058f1e69183508821204d25969ab5d63ba612c9c
Eike Ziller <eike.ziller@qt.io>
2020-08-19 07:25:43 +0200

    Fix some license headers
    Missing the GPL part.
    
    Fixes: QTCREATORBUG-24501
    Change-Id: I9e8e033399c050af29927d0ee735f9bcd05345bf
    Reviewed-by: hjk <hjk@qt.io>

--- tests/unit/unittest/gtest-std-printing.h
+++ tests/unit/unittest/gtest-std-printing.h
@@ -13,6 +13,14 @@
 ** and conditions see https://www.qt.io/terms-conditions. For further
 ** information use the contact form at https://www.qt.io/contact-us.
 **
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
 ****************************************************************************/
 
 #pragma once
