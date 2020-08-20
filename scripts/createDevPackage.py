d011e7df25cd69c0adf5c1d15b202f29cabf2783
Tim Jenssen <tim.jenssen@qt.io>
2020-08-20 18:51:44 +0200

    add some qmlpuppet cpp files to dev package
    Since external plugins are interacting with the puppet
    we need them in the dev package.
    
    Change-Id: I871629ad80583a09b842bb60cd5b6fab5b512fae
    Reviewed-by: Michael Winkelmann <michael.winkelmann@qt.io>
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- scripts/createDevPackage.py
+++ scripts/createDevPackage.py
@@ -59,6 +59,8 @@ source_include_patterns = [
     # directories
     r"^(?!(share|tests)/.*$)(.*/)?$",                     # look into all directories except under share/ and tests/
     r"^share/(qtcreator/(qml/(qmlpuppet/(.*/)?)?)?)?$", # for shared headers for qt quick designer plugins
+    r"^share/qtcreator/qml/qmlpuppet/commands/.*\.(h|pri|cpp|c|txt|md)$", #used by extra plugins
+    r"^share/qtcreator/qml/qmlpuppet/container/.*\.(h|pri|cpp|c|txt|md)$", #used by extra plugins
     r"^src/plugins/help/qlitehtml/.*\.(h|pri|cpp|c|txt|md)$", # litehtml is used by extra plugins
     # files
     r"^HACKING$",
