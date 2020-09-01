9c09d822514c6f1d7008c6c7457649d3532722f3
Morten Johan Sørvig <morten.sorvig@qt.io>
2020-09-01 12:55:01 +0200

    Limit workaround to 5.14.0
    QTBUG-80934 was fixed in Qt 5.14.1.
    
    Change-Id: I3f9f2a629bce40b13ad8836ae4b0f4a710241d49
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -342,7 +342,7 @@ static void setHighDpiEnvironmentVariable()
             && !qEnvironmentVariableIsSet("QT_SCALE_FACTOR")
             && !qEnvironmentVariableIsSet("QT_SCREEN_SCALE_FACTORS")) {
         QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
+#if QT_VERSION == QT_VERSION_CHECK(5, 14, 0)
         // work around QTBUG-80934
         QGuiApplication::setHighDpiScaleFactorRoundingPolicy(
             Qt::HighDpiScaleFactorRoundingPolicy::Round);
