e162775bdebfa240f1c683b120f537b7451efdc8
Christian Kandeler <christian.kandeler@qt.io>
2020-09-18 16:41:49 +0200

    C++ Unit test: Fix diagnostics export test on Windows
    The file offsets are different because of CRLF line endings.
    
    Change-Id: Ice859f3b279651b61d147bdd963f62897b090e47
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- tests/unit/unittest/readexporteddiagnostics-test.cpp
+++ tests/unit/unittest/readexporteddiagnostics-test.cpp
@@ -101,10 +101,18 @@ TEST_F(ReadExportedDiagnostics, UnexpectedFileContents)
     ASSERT_THAT(diags, IsEmpty());
 }
 
+static QString appendYamlSuffix(const char *filePathFragment)
+{
+    const QString yamlSuffix = QLatin1String(Utils::HostOsInfo::isWindowsHost()
+                                             ? "_win.yaml" : ".yaml");
+    return filePathFragment + yamlSuffix;
+}
+
 TEST_F(ReadExportedDiagnostics, Tidy)
 {
     const QString sourceFile = TESTDATA "tidy.modernize-use-nullptr.cpp";
-    const QString exportedFile = createFile(TESTDATA "tidy.modernize-use-nullptr.yaml", sourceFile);
+    const QString exportedFile = createFile(appendYamlSuffix(TESTDATA "tidy.modernize-use-nullptr"),
+                                            sourceFile);
     Diagnostic expectedDiag;
     expectedDiag.name = "modernize-use-nullptr";
     expectedDiag.location = {sourceFile, 2, 25};
@@ -142,7 +150,10 @@ TEST_F(ReadExportedDiagnostics, AcceptDiagsFromFilePaths_None)
 TEST_F(ReadExportedDiagnostics, Tidy_Clang)
 {
     const QString sourceFile = TESTDATA "clang.unused-parameter.cpp";
-    const QString exportedFile = createFile(TESTDATA "clang.unused-parameter.yaml", sourceFile);
+    const QString yamlSuffix
+            = QLatin1String(Utils::HostOsInfo::isWindowsHost() ? "_win.yaml" : ".yaml");
+    const QString exportedFile = createFile(appendYamlSuffix(TESTDATA "clang.unused-parameter"),
+                                            sourceFile);
     Diagnostic expectedDiag;
     expectedDiag.name = "clang-diagnostic-unused-parameter";
     expectedDiag.location = {sourceFile, 4, 12};
@@ -162,7 +173,8 @@ TEST_F(ReadExportedDiagnostics, Tidy_Clang)
 TEST_F(ReadExportedDiagnostics, Tidy_ClangAnalyzer)
 {
     const QString sourceFile = TESTDATA "clang-analyzer.dividezero.cpp";
-    const QString exportedFile = createFile(TESTDATA "clang-analyzer.dividezero.yaml", sourceFile);
+    const QString exportedFile = createFile(appendYamlSuffix(TESTDATA "clang-analyzer.dividezero"),
+                                            sourceFile);
     Diagnostic expectedDiag;
     expectedDiag.name = "clang-analyzer-core.DivideZero";
     expectedDiag.location = {sourceFile, 4, 15};
@@ -198,7 +210,7 @@ TEST_F(ReadExportedDiagnostics, Tidy_ClangAnalyzer)
 TEST_F(ReadExportedDiagnostics, Clazy)
 {
     const QString sourceFile = TESTDATA "clazy.qgetenv.cpp";
-    const QString exportedFile = createFile(TESTDATA "clazy.qgetenv.yaml", sourceFile);
+    const QString exportedFile = createFile(appendYamlSuffix(TESTDATA "clazy.qgetenv"), sourceFile);
     Diagnostic expectedDiag;
     expectedDiag.name = "clazy-qgetenv";
     expectedDiag.location = {sourceFile, 7, 5};
