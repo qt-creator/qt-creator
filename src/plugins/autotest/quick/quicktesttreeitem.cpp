qt-creator/qt-creator

commit 90fd6870c5a15ecc128116766bec30f673746750

AutoTest: Simplify  QuickTestTreeItem::markForRemovalRecursively()
Change-Id: I3ab1876aa2d4dc04b9b5d53f373751e0f5241e75
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/autotest/quick/quicktesttreeitem.cpp
@@ -430,15 +430,10 @@ QSet<QString> QuickTestTreeItem::internalTargets() const
 
 void QuickTestTreeItem::markForRemovalRecursively(const QString &filePath)
 {
-    static const Core::Id id = Core::Id(Constants::FRAMEWORK_PREFIX).withSuffix(
-                QuickTest::Constants::FRAMEWORK_NAME);
-    TestTreeItem::markForRemovalRecursively(filePath);
-    ITestFramework *framework = TestFrameworkManager::frameworkForId(id);
-    QTC_ASSERT(framework, return);
-    auto parser = dynamic_cast<QuickTestParser *>(framework->testParser());
+    auto parser = dynamic_cast<QuickTestParser *>(framework()->testParser());
     const QString proFile = parser->projectFileForMainCppFile(filePath);
     if (!proFile.isEmpty()) {
-        TestTreeItem *root = framework->rootNode();
+        TestTreeItem *root = framework()->rootNode();
         root->forAllChildren([proFile](TestTreeItem *it) {
             if (it->proFile() == proFile)
                 it->markForRemoval(true);

commit af9d8768dcc4d36fffdbe1342b6a1e978709af2b

AutoTest: Fix marking for removal and sweeping of Quick Tests
If opening and editing a QML file we did no more mark tests of this
file as to be removed. In turn we may have got "new" items when e.g.
just modifying the name of a function or test case and but did not lose
the old one.
Broke with 90fd6870c5a.

Change-Id: I86917089ffb9d4acb2f735f2f3e0f112d8cdc65a
Reviewed-by: David Schulz <david.schulz@qt.io>

src/plugins/autotest/quick/quicktesttreeitem.cpp
@@ -428,6 +428,7 @@ QSet<QString> QuickTestTreeItem::internalTargets() const
 
 void QuickTestTreeItem::markForRemovalRecursively(const QString &filePath)
 {
+    TestTreeItem::markForRemovalRecursively(filePath);
     auto parser = dynamic_cast<QuickTestParser *>(framework()->testParser());
     const QString proFile = parser->projectFileForMainCppFile(filePath);
     if (!proFile.isEmpty()) {