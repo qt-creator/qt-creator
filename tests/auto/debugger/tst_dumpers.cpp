73725f409a579b3574a81022b5ae3885144f7fb9
David Schulz <david.schulz@qt.io>
2020-10-06 15:01:11 +0200

    Debugger: set utf8_source qmake config value for cdb
    fixes cbor test and simplifies char array test
    
    Change-Id: I035e393e0e09dc8a16e27dd400a7557d3cf3a006
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- tests/auto/debugger/tst_dumpers.cpp
+++ tests/auto/debugger/tst_dumpers.cpp
@@ -1466,6 +1466,8 @@ void tst_Dumpers::dumper()
                 projectFile.write("DEFINES += _GLIBCXX_DEBUG\n");
             if (m_debuggerEngine == GdbEngine && m_debuggerVersion < 70500)
                 projectFile.write("QMAKE_CXXFLAGS += -gdwarf-3\n");
+            if (m_debuggerEngine == CdbEngine)
+                projectFile.write("CONFIG += utf8_source\n");
             projectFile.write(data.profileExtra.toUtf8());
         } else {
             projectFile.write(data.allProfile.toUtf8());
@@ -5668,16 +5670,13 @@ void tst_Dumpers::dumper_data()
 
                     "&s, &t, &w, &ch, &wch")
 
-               + CheckType("s", "char [5]") % NoCdbEngine
-               + CheckType("s", "char [4]") % CdbEngine
+               + CheckType("s", "char [5]")
                + Check("s.0", "[0]", "97", "char")
-               + CheckType("t", "char [6]") % NoCdbEngine
-               + CheckType("t", "char [5]") % CdbEngine
+               + CheckType("t", "char [6]")
                + Check("t.0", "[0]", "97", "char")
                + CheckType("w", "wchar_t [4]")
                + Check("ch.0", "[0]", "97", TypeDef("char", "CHAR"))
-               + CheckType("ch", "CHAR [5]") % NoCdbEngine
-               + CheckType("ch", "char [4]") % CdbEngine
+               + CheckType("ch", TypeDef("char [5]", "CHAR [5]"))
                + Check("wch.0", "[0]", "97", TypeDef("wchar_t", "WCHAR"))
                + CheckType("wch", TypeDef("wchar_t[4]", "WCHAR [4]"));
 
