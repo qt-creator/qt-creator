8ffd3d20a59a9b8d45963bb5fe2ed11063e0032e
Miikka Heikkinen <miikka.heikkinen@qt.io>
2020-11-02 16:14:32 +0200

    QmlDesigner: Add support for specifying extra files to library entry
    Extra files specified in the ItemLibraryEntry will be copied to the
    same directory as the qml doc.
    
    Change-Id: I6f10e7ba8114355cbf68bc4b306f0500a81dbad2
    Fixes: QDS-2635
    Reviewed-by: Mahmoud Badri <mahmoud.badri@qt.io>
    Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

--- src/plugins/qmldesigner/components/navigator/navigatortreemodel.cpp
+++ src/plugins/qmldesigner/components/navigator/navigatortreemodel.cpp
@@ -54,6 +54,7 @@
 #include <QPointF>
 #include <QDir>
 #include <QFileInfo>
+#include <QFile>
 #include <QPixmap>
 
 #include <coreplugin/messagebox.h>
@@ -680,6 +681,21 @@ void NavigatorTreeModel::handleItemLibraryItemDrop(const QMimeData *mimeData, in
 
             if (newQmlObjectNode.isValid())
                 m_view->setSelectedModelNode(newQmlObjectNode.modelNode());
+        }
+
+        const QStringList copyFiles = itemLibraryEntry.extraFilePaths();
+        if (!copyFiles.isEmpty()) {
+            // Files are copied into the same directory as the current qml document
+            for (const auto &copyFile : copyFiles) {
+                QFileInfo fi(copyFile);
+                const QString targetFile = QmlDesignerPlugin::instance()->documentManager()
+                        .currentFilePath().toFileInfo().dir().absoluteFilePath(fi.fileName());
+                // We don't want to overwrite existing default files
+                if (!QFileInfo::exists(targetFile)) {
+                    if (!QFile::copy(copyFile, targetFile))
+                        qWarning() << QStringLiteral("Copying extra file '%1' failed.").arg(copyFile);
+                }
+            }
         }
     }
 }
