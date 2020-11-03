cb34bfc4930d9f7f76bbacde27d094782cf21027
Assam Boudjelthia <assam.boudjelthia@qt.io>
2020-11-03 16:29:09 +0200

    Android: add line breaks to new AVD warnings
    Change-Id: Ie533c468717674959060c7732952892086f37640
    Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

--- src/plugins/android/avddialog.cpp
+++ src/plugins/android/avddialog.cpp
@@ -75,6 +75,7 @@ AvdDialog::AvdDialog(int minApiLevel, AndroidSdkManager *sdkManager, const QStri
     m_avdDialog.nameLineEdit->installEventFilter(this);
 
     m_avdDialog.warningText->setType(Utils::InfoLabel::Warning);
+    m_avdDialog.warningText->setElideMode(Qt::ElideNone);
 
     connect(&m_hideTipTimer, &QTimer::timeout, this, []() { Utils::ToolTip::hide(); });
 
@@ -246,14 +247,14 @@ void AvdDialog::updateApiLevelComboBox()
         m_avdDialog.targetApiComboBox->setEnabled(false);
         m_avdDialog.warningText->setVisible(true);
         m_avdDialog.warningText->setText(
-            tr("Cannot create a new AVD. No suitable Android system image is installed.\n"
+            tr("Cannot create a new AVD. No suitable Android system image is installed.<br/>"
                "Install a system image of at least API version %1 from the SDK Manager tab.")
                 .arg(m_minApiLevel));
         m_avdDialog.buttonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
     } else if (filteredList.isEmpty()) {
         m_avdDialog.targetApiComboBox->setEnabled(false);
         m_avdDialog.warningText->setVisible(true);
-        m_avdDialog.warningText->setText(tr("Cannot create an AVD for ABI %1. Install a system "
+        m_avdDialog.warningText->setText(tr("Cannot create an AVD for ABI %1.<br/>Install a system "
                                             "image for it from the SDK Manager tab first.")
                                              .arg(abi()));
         m_avdDialog.buttonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
