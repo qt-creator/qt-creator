da78629153e6ccf829a98c559777f9b451b16162
Eike Ziller <eike.ziller@qt.io>
2020-11-13 15:07:57 +0100

    cmake build: Use CMAKE_INSTALL_NAME_TOOL instead of hardcoding
    Amends 98db9774f2363314f51b1732646396112418320e
    
    Change-Id: I4aaaddcfc0e5cc31bb0646a3a3ccc18ee2fa5ff1
    Reviewed-by: Cristian Adam <cristian.adam@qt.io>

--- cmake/QtCreatorAPI.cmake
+++ cmake/QtCreatorAPI.cmake
@@ -693,14 +693,14 @@ function(add_qtc_executable name)
       set(_code)
       foreach(_rpath ${_rpaths_to_remove})
         set(_code "${_code}
-          execute_process(COMMAND /usr/bin/install_name_tool
+          execute_process(COMMAND \"${CMAKE_INSTALL_NAME_TOOL}\"
             -delete_rpath \"${_rpath}\"
             \"\$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/${_EXECUTABLE_FILE_PATH}\")"
         )
       endforeach()
       foreach(_rpath ${install_rpath})
         set(_code "${_code}
-          execute_process(COMMAND /usr/bin/install_name_tool
+          execute_process(COMMAND \"${CMAKE_INSTALL_NAME_TOOL}\"
             -add_rpath \"${_rpath}\"
             \"\$ENV{DESTDIR}\${CMAKE_INSTALL_PREFIX}/${_EXECUTABLE_FILE_PATH}\")"
         )
