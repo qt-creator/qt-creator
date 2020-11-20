06bc4203cfd1deb833d3b96357be60cb8d316fcb
David Schulz <david.schulz@qt.io>
2020-11-20 13:36:29 +0100

    CDB: fix signed char container dumper
    "signed char" might be reported as a template type but cannot be found
    when looked up, so just strip the "signed" from the typename and lookup
    char.
    
    Change-Id: I70e3b27d5c3bfd9e65da0a9f14b6a2df2c7e8dde
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/libs/qtcreatorcdbext/pytype.cpp
+++ src/libs/qtcreatorcdbext/pytype.cpp
@@ -426,6 +426,8 @@ PyType PyType::lookupType(const std::string &typeNameIn, ULONG64 module)
         typeName.erase(typeName.length() - 6);
     if (typeName == "__int64" || typeName == "unsigned __int64")
         typeName.erase(typeName.find("__"), 2);
+    if (typeName == "signed char")
+        typeName.erase(0, 7);
 
     const static std::regex typeNameRE("^[a-zA-Z_][a-zA-Z0-9_]*!?[a-zA-Z0-9_<>:, \\*\\&\\[\\]]*$");
     if (!std::regex_match(typeName, typeNameRE))
