qt-creator/qt-creator

commit 7b6ab79f3ecaea8e1c3796efe2cbfbf37e09a0a1

ClangTools: Do not include our wrapped Qt headers
These are only necessary for the code model and can prevent clazy from
finding certain issues.

Fixes: QTCREATORBUG-24845
Change-Id: I04ba6703812918c39ebbde1dbac5af85fe18622d
Reviewed-by: David Schulz <david.schulz@qt.io>

src/plugins/cpptools/compileroptionsbuilder.cpp
@@ -298,7 +298,8 @@ void CompilerOptionsBuilder::enableExceptions()
 
 void CompilerOptionsBuilder::insertWrappedQtHeaders()
 {
-    insertWrappedHeaders(wrappedQtHeadersIncludePath());
+    if (m_useTweakedHeaderPaths == UseTweakedHeaderPaths::Yes)
+        insertWrappedHeaders(wrappedQtHeadersIncludePath());
 }
 
 void CompilerOptionsBuilder::insertWrappedMingwHeaders()

src/plugins/clangtools/clangtoolruncontrol.cpp
@@ -121,7 +121,7 @@ AnalyzeUnit::AnalyzeUnit(const FileInfo &fileInfo,
 {
     CompilerOptionsBuilder optionsBuilder(*fileInfo.projectPart,
                                           UseSystemHeader::No,
-                                          UseTweakedHeaderPaths::Yes,
+                                          UseTweakedHeaderPaths::Tools,
                                           UseLanguageDefines::No,
                                           UseBuildSystemWarnings::No,
                                           clangVersion,

src/plugins/cpptools/compileroptionsbuilder.h
@@ -33,7 +33,7 @@ namespace CppTools {
 
 enum class UsePrecompiledHeaders : char { Yes, No };
 enum class UseSystemHeader : char { Yes, No };
-enum class UseTweakedHeaderPaths : char { Yes, No };
+enum class UseTweakedHeaderPaths : char { Yes, Tools, No };
 enum class UseToolchainMacros : char { Yes, No };
 enum class UseLanguageDefines : char { Yes, No };
 enum class UseBuildSystemWarnings : char { Yes, No };

commit ef05f29940ff52dd8f2b1b97e6dcb602eca98138

ClangTools: Use built-in header paths again
This was broken in 7b6ab79f3e.

Change-Id: I037474ecce62bfebfe167d8dce15a6e7dbf7a001
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/plugins/cpptools/compileroptionsbuilder.cpp
@@ -355,7 +355,7 @@ void CompilerOptionsBuilder::addHeaderPathOptions()
     for (const HeaderPath &headerPath : filter.systemHeaderPaths)
         addIncludeDirOptionForPath(headerPath);
 
-    if (m_useTweakedHeaderPaths == UseTweakedHeaderPaths::Yes) {
+    if (m_useTweakedHeaderPaths != UseTweakedHeaderPaths::No) {
         QTC_CHECK(!m_clangVersion.isEmpty()
                   && "Clang resource directory is required with UseTweakedHeaderPaths::Yes.");
 