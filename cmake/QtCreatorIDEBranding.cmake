c1a0e9d638db0342e541560e49500dad34a3df82
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2020-12-08 11:52:02 +0200

    Bump version to 4.15 beta
    Change-Id: I39f45b880649323f71ebf3bd3a6bb3677ca33fd2
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- cmake/QtCreatorIDEBranding.cmake
+++ cmake/QtCreatorIDEBranding.cmake
@@ -1,6 +1,6 @@
-set(IDE_VERSION "4.14.0")                             # The IDE version.
-set(IDE_VERSION_COMPAT "4.14.0")                      # The IDE Compatibility version.
-set(IDE_VERSION_DISPLAY "4.14.0")                     # The IDE display version.
+set(IDE_VERSION "4.14.82")                            # The IDE version.
+set(IDE_VERSION_COMPAT "4.14.82")                     # The IDE Compatibility version.
+set(IDE_VERSION_DISPLAY "4.15.0-beta1")                 # The IDE display version.
 set(IDE_COPYRIGHT_YEAR "2020")                        # The IDE current copyright year.
 
 set(IDE_SETTINGSVARIANT "QtProject")                  # The IDE settings variation.
