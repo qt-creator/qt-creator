qt-creator/qt-creator

commit 5daee38ee390b91c15d843992f68706026012914

QmlDesigner: Add compiler flag required to build Qt 6
Change-Id: I47defc9b8d4509f1a1aa367349c9b8268d876518
Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

share/qtcreator/qml/qmlpuppet/qml2puppet/qml2puppet.pri
@@ -2,6 +2,7 @@ QT += core gui widgets qml quick network
 QT += core-private qml-private quick-private gui-private
 
 CONFIG += c++17
+win32: QMAKE_CXXFLAGS += /std:c++17
 
 DEFINES -= QT_CREATOR
 

commit ec0135f517a05dfb67ea7b06ac6f2d8882230ca4

qml2puppet: Fix build with MinGW
Which was broken in 5daee38e.

Change-Id: Ic43ed4528cb5e2c831e07b17a2ee118583b54526
Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

share/qtcreator/qml/qmlpuppet/qml2puppet/qml2puppet.pri
@@ -2,7 +2,7 @@ QT += core gui widgets qml quick network
 QT += core-private qml-private quick-private gui-private
 
 CONFIG += c++17
-win32: QMAKE_CXXFLAGS += /std:c++17
+win32:!mingw: QMAKE_CXXFLAGS += /std:c++17
 
 DEFINES -= QT_CREATOR
 