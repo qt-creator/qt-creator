qt-creator/qt-creator

commit cf010911f7681b23aff6a150c50252374447addd

Don't allocate unneeded temporary containers
Fix clazy warnings: allocating an unneeded temporary container
[clazy-container-anti-pattern]

Change-Id: I4b4c2c634eea650bbdf3c12d982a17f899fc94ec
Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>
Reviewed-by: David Schulz <david.schulz@qt.io>
Reviewed-by: hjk <hjk@qt.io>

src/plugins/languageclient/client.cpp
@@ -111,22 +111,24 @@ Client::~Client()
     using namespace TextEditor;
     // FIXME: instead of replacing the completion provider in the text document store the
     // completion provider as a prioritised list in the text document
-    for (TextDocument *document : m_resetAssistProvider.keys())
-        resetAssistProviders(document);
-    for (Core::IEditor * editor : Core::DocumentModel::editorsForOpenedDocuments()) {
+    for (auto it = m_resetAssistProvider.cbegin(); it != m_resetAssistProvider.cend(); ++it)
+        resetAssistProviders(it.key());
+    const QList<Core::IEditor *> &editors = Core::DocumentModel::editorsForOpenedDocuments();
+    for (Core::IEditor *editor : editors) {
         if (auto textEditor = qobject_cast<BaseTextEditor *>(editor)) {
             TextEditorWidget *widget = textEditor->editorWidget();
             widget->setRefactorMarkers(RefactorMarker::filterOutType(widget->refactorMarkers(), id()));
             widget->removeHoverHandler(&m_hoverHandler);
         }
     }
-    for (const DocumentUri &uri : m_highlights.keys()) {
+    for (auto it = m_highlights.cbegin(); it != m_highlights.cend(); ++it) {
+        const DocumentUri &uri = it.key();
         if (TextDocument *doc = TextDocument::textDocumentForFilePath(uri.toFilePath())) {
             if (TextEditor::SyntaxHighlighter *highlighter = doc->syntaxHighlighter())
                 highlighter->clearAllExtraFormats();
         }
     }
-    for (IAssistProcessor *processor : m_runningAssistProcessors)
+    for (IAssistProcessor *processor : qAsConst(m_runningAssistProcessors))
         processor->setAsyncProposalAvailable(nullptr);
     updateEditorToolBar(m_openedDocument.keys());
 }
@@ -910,11 +912,11 @@ bool Client::reset()
     m_serverCapabilities = ServerCapabilities();
     m_dynamicCapabilities.reset();
     m_diagnosticManager.clearDiagnostics();
-    for (TextEditor::TextDocument *document : m_openedDocument.keys())
-        document->disconnect(this);
-    for (TextEditor::TextDocument *document : m_resetAssistProvider.keys())
-        resetAssistProviders(document);
-    for (TextEditor::IAssistProcessor *processor : m_runningAssistProcessors)
+    for (auto it = m_openedDocument.cbegin(); it != m_openedDocument.cend(); ++it)
+        it.key()->disconnect(this);
+    for (auto it = m_resetAssistProvider.cbegin(); it != m_resetAssistProvider.cend(); ++it)
+        resetAssistProviders(it.key());
+    for (TextEditor::IAssistProcessor *processor : qAsConst(m_runningAssistProcessors))
         processor->setAsyncProposalAvailable(nullptr);
     m_runningAssistProcessors.clear();
     return true;
@@ -1279,8 +1281,8 @@ void Client::initializeCallback(const InitializeRequest::Response &initResponse)
         TextEditor::IOutlineWidgetFactory::updateOutline();
     }
 
-    for (TextEditor::TextDocument *document : m_openedDocument.keys())
-        openDocument(document);
+    for (auto it = m_openedDocument.cbegin(); it != m_openedDocument.cend(); ++it)
+        openDocument(it.key());
 
     emit initialized(m_serverCapabilities);
 }

src/libs/utils/settingsaccessor.cpp
@@ -49,8 +49,8 @@ namespace Utils {
 QMessageBox::StandardButtons SettingsAccessor::Issue::allButtons() const
 {
     QMessageBox::StandardButtons result = QMessageBox::NoButton;
-    for (const QMessageBox::StandardButton &b : buttons.keys())
-        result |= b;
+    for (auto it = buttons.cbegin(); it != buttons.cend(); ++it)
+        result |= it.key();
     return result;
 }
 

src/plugins/android/avddialog.cpp
@@ -80,7 +80,7 @@ AvdDialog::AvdDialog(int minApiLevel, AndroidSdkManager *sdkManager, const QStri
     connect(&m_hideTipTimer, &QTimer::timeout, this, []() { Utils::ToolTip::hide(); });
 
     parseDeviceDefinitionsList();
-    for (const QString &type : DeviceTypeToStringMap.values())
+    for (const QString &type : DeviceTypeToStringMap)
         m_avdDialog.deviceDefinitionTypeComboBox->addItem(type);
 
     connect(m_avdDialog.deviceDefinitionTypeComboBox,

src/plugins/autotest/gtest/gtestparser.cpp
@@ -101,7 +101,7 @@ static bool handleGTest(QFutureInterface<TestParseResultPtr> futureInterface,
     GTestVisitor visitor(document);
     visitor.accept(ast);
 
-    QMap<GTestCaseSpec, GTestCodeLocationList> result = visitor.gtestFunctions();
+    const QMap<GTestCaseSpec, GTestCodeLocationList> result = visitor.gtestFunctions();
     QString proFile;
     const QList<CppTools::ProjectPart::Ptr> &ppList = modelManager->projectPart(filePath);
     if (!ppList.isEmpty())
@@ -109,7 +109,8 @@ static bool handleGTest(QFutureInterface<TestParseResultPtr> futureInterface,
     else
         return false; // happens if shutting down while parsing
 
-    for (const GTestCaseSpec &testSpec : result.keys()) {
+    for (auto it = result.cbegin(); it != result.cend(); ++it) {
+        const GTestCaseSpec &testSpec = it.key();
         GTestParseResult *parseResult = new GTestParseResult(base);
         parseResult->itemType = TestTreeItem::TestSuite;
         parseResult->fileName = filePath;
@@ -119,7 +120,7 @@ static bool handleGTest(QFutureInterface<TestParseResultPtr> futureInterface,
         parseResult->disabled = testSpec.disabled;
         parseResult->proFile = proFile;
 
-        for (const GTestCodeLocationAndType &location : result.value(testSpec)) {
+        for (const GTestCodeLocationAndType &location : it.value()) {
             GTestParseResult *testSet = new GTestParseResult(base);
             testSet->name = location.m_name;
             testSet->fileName = filePath;
@@ -134,7 +135,7 @@ static bool handleGTest(QFutureInterface<TestParseResultPtr> futureInterface,
 
         futureInterface.reportResult(TestParseResultPtr(parseResult));
     }
-    return !result.keys().isEmpty();
+    return !result.isEmpty();
 }
 
 bool GTestParser::processDocument(QFutureInterface<TestParseResultPtr> futureInterface,

src/plugins/autotest/testresultmodel.cpp
@@ -395,11 +395,10 @@ int TestResultModel::maxWidthOfLineNumber(const QFont &font)
 int TestResultModel::resultTypeCount(ResultType type) const
 {
     int result = 0;
-
-    for (const auto &id : m_testResultCount.keys()) {
+    for (auto it = m_testResultCount.cbegin(); it != m_testResultCount.cend(); ++it) {
         // if we got a result count from the framework prefer that over our counted results
-        int reported = m_reportedSummary[id].value(type);
-        result += reported != 0 ? reported : m_testResultCount.value(id).value(type);
+        int reported = m_reportedSummary[it.key()].value(type);
+        result += reported != 0 ? reported : it.value().value(type);
     }
     return result;
 }

src/plugins/autotest/testresultspane.cpp
@@ -507,9 +507,10 @@ void TestResultsPane::initializeFilterMenu()
     textAndType.insert(ResultType::MessageDebug, tr("Debug Messages"));
     textAndType.insert(ResultType::MessageWarn, tr("Warning Messages"));
     textAndType.insert(ResultType::MessageInternal, tr("Internal Messages"));
-    for (ResultType result : textAndType.keys()) {
+    for (auto it = textAndType.cbegin(); it != textAndType.cend(); ++it) {
+        const ResultType &result = it.key();
         QAction *action = new QAction(m_filterMenu);
-        action->setText(textAndType.value(result));
+        action->setText(it.value());
         action->setCheckable(true);
         action->setChecked(result != ResultType::MessageInternal || !omitIntern);
         action->setData(int(result));

src/plugins/autotest/testsettings.cpp
@@ -70,13 +70,14 @@ void TestSettings::toSettings(QSettings *s) const
     s->setValue(popupOnFailKey, popupOnFail);
     s->setValue(runAfterBuildKey, int(runAfterBuild));
     // store frameworks and their current active and grouping state
-    for (const Utils::Id &id : frameworks.keys()) {
-        s->setValue(id.toString(), frameworks.value(id));
+    for (auto it = frameworks.cbegin(); it != frameworks.cend(); ++it) {
+        const Utils::Id &id = it.key();
+        s->setValue(id.toString(), it.value());
         s->setValue(id.toString() + groupSuffix, frameworksGrouping.value(id));
     }
     // ..and the testtools as well
-    for (const Utils::Id &id : tools.keys())
-        s->setValue(id.toString(), tools.value(id));
+    for (auto it = tools.cbegin(); it != tools.cend(); ++it)
+        s->setValue(it.key().toString(), it.value());
     s->endGroup();
 }
 

src/plugins/clearcase/clearcaseplugin.cpp
@@ -1315,7 +1315,7 @@ void ClearCasePluginPrivate::diffActivity()
     }
 
     if ((m_settings.diffType == GraphicalDiff) && (filever.count() == 1)) {
-        QStringPair pair(filever.values().at(0));
+        QStringPair pair(filever.first());
         diffGraphical(pair.first, pair.second);
         return;
     }

commit 0cb6f07829af8f38159fa5e7a298418f1434e1e1

LanguageClient: Fix crash when language server dies
Do not remove items of a container while iterating over it
instead revert to old behavior of allocating a temporary.
Broke with cf010911f76.

Change-Id: Ie76974f1e74469f80ee28fb2011d66690dd9e917
Reviewed-by: David Schulz <david.schulz@qt.io>

src/plugins/languageclient/client.cpp
@@ -880,8 +880,9 @@ bool Client::reset()
     m_diagnosticManager.clearDiagnostics();
     for (auto it = m_openedDocument.cbegin(); it != m_openedDocument.cend(); ++it)
         it.key()->disconnect(this);
-    for (auto it = m_resetAssistProvider.cbegin(); it != m_resetAssistProvider.cend(); ++it)
-        resetAssistProviders(it.key());
+    // temporary container needed since m_resetAssistProvider is changed in resetAssistProviders
+    for (TextEditor::TextDocument *document : m_resetAssistProvider.keys())
+        resetAssistProviders(document);
     for (TextEditor::IAssistProcessor *processor : qAsConst(m_runningAssistProcessors))
         processor->setAsyncProposalAvailable(nullptr);
     m_runningAssistProcessors.clear();