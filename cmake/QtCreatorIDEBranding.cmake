fc5af03862d117b68a3fb99db083c47b9dd17ff6
Eike Ziller <eike.ziller@qt.io>
2021-02-24 19:50:39 +0100

    Bump version to 4.14.2
    Change-Id: I792587c71eadf0b2602d73e7fa4be324cc2766cf
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- cmake/QtCreatorIDEBranding.cmake
+++ cmake/QtCreatorIDEBranding.cmake
@@ -1,6 +1,6 @@
-set(IDE_VERSION "4.14.1")                             # The IDE version.
+set(IDE_VERSION "4.14.2")                             # The IDE version.
 set(IDE_VERSION_COMPAT "4.14.0")                      # The IDE Compatibility version.
-set(IDE_VERSION_DISPLAY "4.14.1")                     # The IDE display version.
+set(IDE_VERSION_DISPLAY "4.14.2")                     # The IDE display version.
 set(IDE_COPYRIGHT_YEAR "2020")                        # The IDE current copyright year.
 
 set(IDE_SETTINGSVARIANT "QtProject")                  # The IDE settings variation.
