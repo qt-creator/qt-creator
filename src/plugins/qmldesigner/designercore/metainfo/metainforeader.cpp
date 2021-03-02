7c112ed01d6745cf67ff39a601b0e0902fea328d
Eike Ziller <eike.ziller@qt.io>
2021-03-02 14:01:40 +0100

    QmlDesigner: Fix build with Qt 6
    No QList<T>::toSet anymore
    
    Change-Id: I6e42bd3a1e917c128142b4628277ed48844d1d4d
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>

--- src/plugins/qmldesigner/designercore/metainfo/metainforeader.cpp
+++ src/plugins/qmldesigner/designercore/metainfo/metainforeader.cpp
@@ -26,6 +26,8 @@
 #include "metainforeader.h"
 #include "metainfo.h"
 
+#include <utils/algorithm.h>
+
 #include <QString>
 #include <QFileInfo>
 #include <QDebug>
@@ -233,7 +235,7 @@ void MetaInfoReader::readImportsProperty(const QString &name, const QVariant &va
     } else if ((name == "priorityImports" || name == "showTagsForImports") && !values.isEmpty()) {
         // Flow tags are no longer shown, but the old property is still supported for prioritizing
         // imports to keep compatibility with old metainfo files.
-        m_metaInfo.itemLibraryInfo()->addPriorityImports(values.toSet());
+        m_metaInfo.itemLibraryInfo()->addPriorityImports(Utils::toSet(values));
     } else {
         addError(tr("Unknown property for Imports %1").arg(name), currentSourceLocation());
         setParserState(Error);
