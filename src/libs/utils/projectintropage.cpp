qt-creator/qt-creator

commit 8d1a592d1637b52e539695afdef7f5564f6d723b

FancyLineEdit: Remove property initialText
The same can be easily achieved by placeholderText nowadays.

Change-Id: Icfe0652ae8536c8077cf45a03844275aa1eddad6
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/libs/utils/projectintropage.cpp
@@ -85,7 +85,7 @@ ProjectIntroPage::ProjectIntroPage(QWidget *parent) :
 {
     d->m_ui.setupUi(this);
     hideStatusLabel();
-    d->m_ui.nameLineEdit->setInitialText(tr("<Enter_Name>"));
+    d->m_ui.nameLineEdit->setPlaceholderText(tr("Enter project name"));
     d->m_ui.nameLineEdit->setFocus();
     d->m_ui.nameLineEdit->setValidationFunction([this](FancyLineEdit *edit, QString *errorString) {
         return validateProjectName(edit->text(), errorString);
@@ -178,13 +178,13 @@ bool ProjectIntroPage::validate()
         return false;
     }
 
-    // Name valid? Ignore 'DisplayingInitialText' state.
+    // Name valid? Ignore 'DisplayingPlaceholderText' state.
     bool nameValid = false;
     switch (d->m_ui.nameLineEdit->state()) {
     case FancyLineEdit::Invalid:
         displayStatusMessage(Error, d->m_ui.nameLineEdit->errorMessage());
         return false;
-    case FancyLineEdit::DisplayingInitialText:
+    case FancyLineEdit::DisplayingPlaceholderText:
         break;
     case FancyLineEdit::Valid:
         nameValid = true;

src/libs/utils/fancylineedit.cpp
@@ -105,7 +105,6 @@ public:
     QColor m_okTextColor;
     QColor m_errorTextColor = Qt::red;
     QString m_errorMessage;
-    QString m_initialText;
 };
 
 FancyLineEditPrivate::FancyLineEditPrivate(FancyLineEdit *parent) :
@@ -373,20 +372,6 @@ void FancyLineEdit::setFiltering(bool on)
     }
 }
 
-QString FancyLineEdit::initialText() const
-{
-    return d->m_initialText;
-}
-
-void FancyLineEdit::setInitialText(const QString &t)
-{
-    if (d->m_initialText != t) {
-        d->m_initialText = t;
-        d->m_firstChange = true;
-        setText(t);
-    }
-}
-
 QColor FancyLineEdit::errorColor() const
 {
     return d->m_errorTextColor;
@@ -458,13 +443,13 @@ void FancyLineEdit::validate()
     }
 
     d->m_errorMessage.clear();
-    // Are we displaying the initial text?
-    const bool isDisplayingInitialText = !d->m_initialText.isEmpty() && t == d->m_initialText;
-    const State newState = isDisplayingInitialText ?
-                               DisplayingInitialText :
+    // Are we displaying the placeholder text?
+    const bool isDisplayingPlaceholderText = !placeholderText().isEmpty() && t.isEmpty();
+    const State newState = isDisplayingPlaceholderText ?
+                               DisplayingPlaceholderText :
                                (d->m_validationFunction(this, &d->m_errorMessage) ? Valid : Invalid);
     setToolTip(d->m_errorMessage);
-    // Changed..figure out if valid changed. DisplayingInitialText is not valid,
+    // Changed..figure out if valid changed. DisplayingPlaceholderText is not valid,
     // but should not show error color. Also trigger on the first change.
     if (newState != d->m_state || d->m_firstChange) {
         const bool validHasChanged = (d->m_state == Valid) != (newState == Valid);

src/libs/utils/fancylineedit.h
@@ -73,7 +73,6 @@ class QTCREATOR_UTILS_EXPORT FancyLineEdit : public CompletingLineEdit
     Q_ENUMS(Side)
 
     // Validation.
-    Q_PROPERTY(QString initialText READ initialText WRITE setInitialText DESIGNABLE true)
     Q_PROPERTY(QColor errorColor READ errorColor WRITE setErrorColor DESIGNABLE true)
     Q_PROPERTY(QColor okColor READ okColor WRITE setOkColor DESIGNABLE true)
 
@@ -123,14 +122,11 @@ public:
 
     // line edit, (out)errorMessage -> valid?
     typedef std::function<bool(FancyLineEdit *, QString *)> ValidationFunction;
-    enum State { Invalid, DisplayingInitialText, Valid };
+    enum State { Invalid, DisplayingPlaceholderText, Valid };
 
     State state() const;
     bool isValid() const;
     QString errorMessage() const;
-
-    QString initialText() const;
-    void setInitialText(const QString &);
 
     QColor errorColor() const;
     void setErrorColor(const  QColor &c);

commit f176a6f530264a71a73cf18b2f4f64c58995a3c7

Wizards: Fix error message if project name is empty
It showed "The project already exists" instead of "Name is empty".

Probably broke with 8d1a592d1637b52e539695afdef7f5564f6d723b

Fixes: QTCREATORBUG-25398
Change-Id: I0cbe5753d80bc30c93408886668e39278d99c97f
Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

src/libs/utils/projectintropage.cpp
@@ -165,16 +165,15 @@ bool ProjectIntroPage::validate()
         return false;
     }
 
-    // Name valid? Ignore 'DisplayingPlaceholderText' state.
-    bool nameValid = false;
+    // Name valid?
     switch (d->m_ui.nameLineEdit->state()) {
     case FancyLineEdit::Invalid:
         displayStatusMessage(InfoLabel::Error, d->m_ui.nameLineEdit->errorMessage());
         return false;
     case FancyLineEdit::DisplayingPlaceholderText:
-        break;
+        displayStatusMessage(InfoLabel::Error, tr("Name is empty."));
+        return false;
     case FancyLineEdit::Valid:
-        nameValid = true;
         break;
     }
 
@@ -183,12 +182,12 @@ bool ProjectIntroPage::validate()
                                    + QDir::fromNativeSeparators(d->m_ui.nameLineEdit->text()));
     if (!projectDirFile.exists()) { // All happy
         hideStatusLabel();
-        return nameValid;
+        return true;
     }
 
     if (projectDirFile.isDir()) {
         displayStatusMessage(InfoLabel::Warning, tr("The project already exists."));
-        return nameValid;
+        return true;
     }
     // Not a directory, but something else, likely causing directory creation to fail
     displayStatusMessage(InfoLabel::Error, tr("A file with that name already exists."));