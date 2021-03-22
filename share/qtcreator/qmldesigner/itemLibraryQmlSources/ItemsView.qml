qt-creator/qt-creator

commit db6ad528bb4a29c5c89e7e1a0d667ba82870cd9b

QmlDesigner: Implement new control design
* Update creator theme files and related enum
* Remove unused ActionIndicator functionality
* Update all controls (Colors, Positions, Sizes)
* Add global hover state and cleanup all other states
* Add global hover functionality to ButtonRow
* Add missing states
* Add new MenuItemWithIcon
* Add checkers.png for StateDelegate preview background
* Update copyright headers
* Replace AnchorButtons spacing
* Make ADS title bar buttons and tab bar buttons themeable
* Fix replace CSS colors to allow specifing alpha
* Fix replace CSS overwritting consecutive variables
* Make navigator icons and other elements themeable
* Make connection view themeable
* Make item library themeable
* Fix item library import path
* Update all HelperWidgets
* Fix GradientDialogPopup

Task-number: QDS-3768
Change-Id: I28a5fed178c95d098db052b8c2eabaa083d37b81
Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

share/qtcreator/qmldesigner/itemLibraryQmlSources/ItemsView.qml
@@ -193,8 +193,12 @@ ScrollView {
                             Grid {
                                 id: itemGrid
 
-                                columns: parent.width / styleConstants.cellWidth
-                                property int flexibleWidth: (parent.width - styleConstants.cellWidth * columns) / columns
+                                property real actualWidth: parent.width - itemGrid.leftPadding -itemGrid.rightPadding
+                                property int flexibleWidth: (itemGrid.actualWidth / columns) - styleConstants.cellWidth
+
+                                leftPadding: 6
+                                rightPadding: 6
+                                columns: itemGrid.actualWidth / styleConstants.cellWidth
 
                                 Repeater {
                                     model: itemModel

share/qtcreator/qmldesigner/propertyEditorQmlSources/imports/HelperWidgets/Section.qml
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2016 The Qt Company Ltd.
+** Copyright (C) 2021 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of Qt Creator.
@@ -62,7 +62,7 @@ Item {
 
     Rectangle {
         id: header
-        height: 20
+        height: StudioTheme.Values.sectionHeadHeight
         anchors.left: parent.left
         anchors.right: parent.right
         color: Qt.lighter(StudioTheme.Values.themeSectionHeadBackground, 1.0 + (0.2 * level))
@@ -82,8 +82,8 @@ Item {
             anchors.verticalCenter: parent.verticalCenter
             color: StudioTheme.Values.themeTextColor
             x: 22 + (level * levelShift)
-            font.bold: true
             font.pixelSize: StudioTheme.Values.myFontSize
+            font.capitalization: Font.AllUppercase
         }
 
         MouseArea {
@@ -112,15 +112,20 @@ Item {
         anchors.rightMargin: 5 + leftPadding
         anchors.leftMargin: 5 - leftPadding
         visible: false
-        color: "#666666"
+        color: StudioTheme.Values.themeControlOutline
     }
 
     default property alias __content: row.children
 
     readonly property alias contentItem: row
 
-    implicitHeight: Math.round(row.height + header.height
-                               + section.topPadding + section.bottomPadding)
+    implicitHeight: Math.round(row.height + header.height + topRow.height + bottomRow.height)
+
+    Row {
+        id: topRow
+        height: StudioTheme.Values.sectionHeadSpacerHeight
+        anchors.top: header.bottom
+    }
 
     Row {
         id: row
@@ -128,8 +133,13 @@ Item {
         anchors.leftMargin: section.leftPadding
         anchors.right: parent.right
         anchors.rightMargin: section.rightPadding
-        anchors.top: header.bottom
-        anchors.topMargin: section.topPadding
+        anchors.top: topRow.bottom
+    }
+
+    Row {
+        id: bottomRow
+        height: StudioTheme.Values.sectionHeadSpacerHeight
+        anchors.top: row.bottom
     }
 
     states: [

share/qtcreator/qmldesigner/propertyEditorQmlSources/imports/StudioControls/Section.qml
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2019 The Qt Company Ltd.
+** Copyright (C) 2021 The Qt Company Ltd.
 ** Contact: https://www.qt.io/licensing/
 **
 ** This file is part of Qt Creator.
@@ -34,7 +34,7 @@ Item {
     property int topPadding: 4
     property int rightPadding: 0
 
-    property int animationDuration: 0
+    property int animationDuration: 120
 
     property bool expanded: true
 
@@ -42,28 +42,27 @@ Item {
 
     Rectangle {
         id: header
-        height: StudioTheme.Values.height
+        height: StudioTheme.Values.sectionHeadHeight
 
         anchors.left: parent.left
         anchors.right: parent.right
 
-        color: StudioTheme.Values.themeControlBackground
+        color: StudioTheme.Values.themeSectionHeadBackground
 
         SectionLabel {
             id: label
             anchors.verticalCenter: parent.verticalCenter
             color: StudioTheme.Values.themeTextColor
             x: 22
-            //font.bold: true
             font.pixelSize: StudioTheme.Values.myFontSize
-            // TODO font size?
+            font.capitalization: Font.AllUppercase
         }
 
         SectionLabel {
             id: arrow
             width: StudioTheme.Values.spinControlIconSizeMulti
             height: StudioTheme.Values.spinControlIconSizeMulti
-            text: StudioTheme.Constants.upDownSquare2
+            text: StudioTheme.Constants.startNode
             color: StudioTheme.Values.themeTextColor
             renderType: Text.NativeRendering
             anchors.left: parent.left
@@ -74,7 +73,7 @@ Item {
             Behavior on rotation {
                 NumberAnimation {
                     easing.type: Easing.OutCubic
-                    duration: animationDuration
+                    duration: section.animationDuration
                 }
             }
         }
@@ -82,7 +81,6 @@ Item {
         MouseArea {
             anchors.fill: parent
             onClicked: {
-                section.animationDuration = 120
                 section.expanded = !section.expanded
                 if (!section.expanded) // TODO
                     section.forceActiveFocus()
@@ -94,7 +92,13 @@ Item {
 
     readonly property alias contentItem: row
 
-    implicitHeight: Math.round(row.height + header.height)
+    implicitHeight: Math.round(row.height + header.height + topRow.height + bottomRow.height)
+
+    Row {
+        id: topRow
+        height: StudioTheme.Values.sectionHeadSpacerHeight
+        anchors.top: header.bottom
+    }
 
     Row {
         id: row
@@ -102,18 +106,31 @@ Item {
         anchors.leftMargin: leftPadding
         anchors.right: parent.right
         anchors.rightMargin: rightPadding
-        anchors.top: header.bottom
-        anchors.topMargin: topPadding
+        anchors.top: topRow.bottom
+    }
+
+    Row {
+        id: bottomRow
+        height: StudioTheme.Values.sectionHeadSpacerHeight
+        anchors.top: row.bottom
     }
 
     Behavior on implicitHeight {
         NumberAnimation {
             easing.type: Easing.OutCubic
-            duration: animationDuration
+            duration: section.animationDuration
         }
     }
 
     states: [
+        State {
+            name: "Expanded"
+            when: section.expanded
+            PropertyChanges {
+                target: arrow
+                rotation: 90
+            }
+        },
         State {
             name: "Collapsed"
             when: !section.expanded
@@ -123,7 +140,7 @@ Item {
             }
             PropertyChanges {
                 target: arrow
-                rotation: -90
+                rotation: 0
             }
         }
     ]

commit 9e960d09a387eb795ecee1035fc6bde9b3237bb3

QmlDesigner: Correct item library spacings
Regression caused by db6ad528bb4a29c5c89e7e1a0d667ba82870cd9b

Fixes: QDS-3855
Change-Id: I4a91a66fcfaf6cbd013bc54a65344cb7f3171798
Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

share/qtcreator/qmldesigner/itemLibraryQmlSources/ItemsView.qml
@@ -159,8 +159,6 @@ ScrollView {
                                              : StudioTheme.Values.themeTextColor
                 leftPadding: 0
                 rightPadding: 0
-                topPadding: 0
-                bottomPadding: 0
                 expanded: importExpanded
                 expandOnClick: false
                 onToggleExpand: {
@@ -184,8 +182,8 @@ ScrollView {
                             hideHeader: categoryModel.rowCount() <= 1
                             leftPadding: 0
                             rightPadding: 0
-                            topPadding: 0
-                            bottomPadding: 0
+                            addTopPadding: categoryModel.rowCount() > 1
+                            addBottomPadding: index != categoryModel.rowCount() - 1
                             caption: categoryName + " (" + itemModel.rowCount() + ")"
                             visible: categoryVisible
                             expanded: categoryExpanded

share/qtcreator/qmldesigner/propertyEditorQmlSources/imports/HelperWidgets/Section.qml
@@ -40,15 +40,15 @@ Item {
     property alias showArrow: arrow.visible
 
     property int leftPadding: 8
-    property int topPadding: 4
     property int rightPadding: 0
-    property int bottomPadding: 4
 
     property bool expanded: true
     property int level: 0
     property int levelShift: 10
     property bool hideHeader: false
     property bool expandOnClick: true // if false, toggleExpand signal will be emitted instead
+    property bool addTopPadding: true
+    property bool addBottomPadding: true
 
     onHideHeaderChanged:
     {
@@ -120,11 +120,12 @@ Item {
 
     readonly property alias contentItem: row
 
-    implicitHeight: Math.round(row.height + header.height + topRow.height + bottomRow.height)
+    implicitHeight: Math.round(row.height + header.height + topSpacer.height + bottomSpacer.height)
 
-    Row {
-        id: topRow
-        height: StudioTheme.Values.sectionHeadSpacerHeight
+
+    Item {
+        id: topSpacer
+        height: addTopPadding && row.height > 0 ? StudioTheme.Values.sectionHeadSpacerHeight : 0
         anchors.top: header.bottom
     }
 
@@ -134,12 +135,12 @@ Item {
         anchors.leftMargin: section.leftPadding
         anchors.right: parent.right
         anchors.rightMargin: section.rightPadding
-        anchors.top: topRow.bottom
+        anchors.top: topSpacer.bottom
     }
 
-    Row {
-        id: bottomRow
-        height: StudioTheme.Values.sectionHeadSpacerHeight
+    Item {
+        id: bottomSpacer
+        height: addBottomPadding && row.height > 0 ? StudioTheme.Values.sectionHeadSpacerHeight : 0
         anchors.top: row.bottom
     }
 