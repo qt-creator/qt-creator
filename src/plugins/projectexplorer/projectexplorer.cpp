qt-creator/qt-creator

commit b55825a42000ee81e37c8e25bcd0784d26d6dc18

ProjectExplorer: Clean up variables
Global variables with names such as "CurrentProject*", "CurrentKit*" etc
are harmful, because the term "current project" as used in Qt Creator
does not refer to the "active project", but simply stands for the
project that contains the node that is currently selected in the project
tree, which in turn may or may not correspond to the current editor
document, depending on the "sync with editor" setting. In other words,
the "current project" is almost a random value with little meaning
outside the project tree itself.
Therefore, we remove "CurrentProject*" and friends, except the ones that
are currently intentionally in use. The latter get renamed to
"CurrentDocument:Project*", so their purpose becomes clear. Their old
names are kept around for backward compatibility, but are not suggested
by the variable chooser anymore, so new usages are unlikely and we can
remove them at some point.
We also add some ActiveProject* variants that have been requested in the
past.
Also remove the "CurrentSession" prefix that was deprecated six years
ago.

Fixes: QTCREATORBUG-12724
Fixes: QTCREATORBUG-24606
Change-Id: Ibba5d0e0ce3d2beb444a5eec01fbb9b745d90a1d
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/projectexplorer/projectexplorer.cpp
@@ -258,7 +258,7 @@ const char FOLDER_OPEN_LOCATIONS_CONTEXT_MENU[]  = "Project.F.OpenLocation.CtxMe
 const char PROJECT_OPEN_LOCATIONS_CONTEXT_MENU[]  = "Project.P.OpenLocation.CtxMenu";
 
 // Default directories:
-const char DEFAULT_BUILD_DIRECTORY_TEMPLATE[] = "../%{JS: Util.asciify(\"build-%{CurrentProject:Name}-%{CurrentKit:FileSystemName}-%{CurrentBuild:Name}\")}";
+const char DEFAULT_BUILD_DIRECTORY_TEMPLATE[] = "../%{JS: Util.asciify(\"build-%{Project:Name}-%{Kit:FileSystemName}-%{BuildConfig:Name}\")}";
 const char DEFAULT_BUILD_DIRECTORY_TEMPLATE_KEY[] = "Directories/BuildDirectory.Template";
 
 const char BUILD_BEFORE_DEPLOY_SETTINGS_KEY[] = "ProjectExplorer/Settings/BuildBeforeDeploy";
@@ -330,18 +330,6 @@ static BuildConfiguration *activeBuildConfiguration()
 {
     Target *target = activeTarget();
     return target ? target->activeBuildConfiguration() : nullptr;
-}
-
-static RunConfiguration *activeRunConfiguration()
-{
-    Target *target = activeTarget();
-    return target ? target->activeRunConfiguration() : nullptr;
-}
-
-static Kit *currentKit()
-{
-    Target *target = activeTarget();
-    return target ? target->kit() : nullptr;
 }
 
 static bool isTextFile(const QString &fileName)
@@ -1543,10 +1531,18 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
             = s->value(Constants::ABORT_BUILD_ALL_ON_ERROR_SETTINGS_KEY, true).toBool();
     dd->m_projectExplorerSettings.lowBuildPriority
             = s->value(Constants::LOW_BUILD_PRIORITY_SETTINGS_KEY, false).toBool();
+
     dd->m_buildPropertiesSettings.buildDirectoryTemplate
             = s->value(Constants::DEFAULT_BUILD_DIRECTORY_TEMPLATE_KEY).toString();
     if (dd->m_buildPropertiesSettings.buildDirectoryTemplate.isEmpty())
         dd->m_buildPropertiesSettings.buildDirectoryTemplate = Constants::DEFAULT_BUILD_DIRECTORY_TEMPLATE;
+    // TODO: Remove in ~4.16
+    dd->m_buildPropertiesSettings.buildDirectoryTemplate.replace("%{CurrentProject:Name}",
+                                                                 "%{Project:Name}");
+    dd->m_buildPropertiesSettings.buildDirectoryTemplate.replace("%{CurrentKit:FileSystemName}",
+                                                                 "%{Kit:FileSystemName}");
+    dd->m_buildPropertiesSettings.buildDirectoryTemplate.replace("%{CurrentBuild:Name}",
+                                                                 "%{BuildConfig:Name}");
 
     const auto loadTriStateValue = [&s](const QString &key) {
       return TriState::fromVariant(s->value(key, TriState::Default.toVariant()));
@@ -1745,6 +1741,7 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
     // FIXME: These are mostly "legacy"/"convenience" entries, relying on
     // the global entry point ProjectExplorer::currentProject(). They should
     // not be used in the Run/Build configuration pages.
+    // TODO: Remove the CurrentProject versions in ~4.16
     Utils::MacroExpander *expander = Utils::globalMacroExpander();
     expander->registerFileVariables(Constants::VAR_CURRENTPROJECT_PREFIX,
         tr("Current project's main file."),
@@ -1753,115 +1750,39 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
             if (Project *project = ProjectTree::currentProject())
                 projectFilePath = project->projectFilePath();
             return projectFilePath.toString();
-        });
-
-    expander->registerVariable("CurrentProject:BuildPath",
-        tr("Full build path of the current project's active build configuration."),
+        }, false);
+    expander->registerFileVariables("CurrentDocument:Project",
+        tr("Main file of the project the current document belongs to."),
         []() -> QString {
-            BuildConfiguration *bc = activeBuildConfiguration();
-            return bc ? bc->buildDirectory().toUserOutput() : QString();
-        });
+            Utils::FilePath projectFilePath;
+            if (Project *project = ProjectTree::currentProject())
+                projectFilePath = project->projectFilePath();
+            return projectFilePath.toString();
+        }, false);
 
     expander->registerVariable(Constants::VAR_CURRENTPROJECT_NAME,
         tr("The name of the current project."),
         []() -> QString {
             Project *project = ProjectTree::currentProject();
             return project ? project->displayName() : QString();
-        });
-
-    expander->registerVariable(Constants::VAR_CURRENTKIT_NAME,
-        tr("The name of the currently active kit."),
+        }, false);
+    expander->registerVariable("CurrentDocument:Project:Name",
+        tr("The name of the project the current document belongs to."),
         []() -> QString {
-            Kit *kit = currentKit();
-            return kit ? kit->displayName() : QString();
-        });
-
-    expander->registerVariable(Constants::VAR_CURRENTKIT_FILESYSTEMNAME,
-        tr("The name of the currently active kit as a filesystem-friendly version."),
-        []() -> QString {
-            Kit *kit = currentKit();
-            return kit ? kit->fileSystemFriendlyName() : QString();
-        });
-
-    expander->registerVariable(Constants::VAR_CURRENTKIT_ID,
-        tr("The ID of the currently active kit."),
-        []() -> QString {
-            Kit *kit = currentKit();
-            return kit ? kit->id().toString() : QString();
-        });
-
-    expander->registerVariable("CurrentDevice:HostAddress",
-        tr("The host address of the device in the currently active kit."),
-        []() -> QString {
-            Kit *kit = currentKit();
-            const IDevice::ConstPtr device = DeviceKitAspect::device(kit);
-            return device ? device->sshParameters().host() : QString();
-        });
-
-    expander->registerVariable("CurrentDevice:SshPort",
-        tr("The SSH port of the device in the currently active kit."),
-        []() -> QString {
-            Kit *kit = currentKit();
-            const IDevice::ConstPtr device = DeviceKitAspect::device(kit);
-            return device ? QString::number(device->sshParameters().port()) : QString();
-        });
-
-    expander->registerVariable("CurrentDevice:UserName",
-        tr("The username with which to log into the device in the currently active kit."),
-        []() -> QString {
-            Kit *kit = currentKit();
-            const IDevice::ConstPtr device = DeviceKitAspect::device(kit);
-            return device ? device->sshParameters().userName() : QString();
-        });
-
-
-    expander->registerVariable("CurrentDevice:PrivateKeyFile",
-        tr("The private key file with which to authenticate when logging into the device "
-           "in the currently active kit."),
-        []() -> QString {
-            Kit *kit = currentKit();
-            const IDevice::ConstPtr device = DeviceKitAspect::device(kit);
-            return device ? device->sshParameters().privateKeyFile : QString();
-        });
-
-    expander->registerVariable(Constants::VAR_CURRENTBUILD_NAME,
-        tr("The currently active build configuration's name."),
-        [&]() -> QString {
-            BuildConfiguration *bc = activeBuildConfiguration();
-            return bc ? bc->displayName() : QString();
-        });
-
-    expander->registerVariable(Constants::VAR_CURRENTRUN_NAME,
-        tr("The currently active run configuration's name."),
-        []() -> QString {
-            if (Target *target = activeTarget()) {
-                if (RunConfiguration *rc = target->activeRunConfiguration())
-                    return rc->displayName();
-            }
-            return QString();
-        });
-
-    expander->registerFileVariables("CurrentRun:Executable",
-        tr("The currently active run configuration's executable (if applicable)."),
-        []() -> QString {
-            if (Target *target = activeTarget()) {
-                if (RunConfiguration *rc = target->activeRunConfiguration())
-                    return rc->commandLine().executable().toString();
-            }
-            return QString();
-        });
-
-    expander->registerVariable(Constants::VAR_CURRENTBUILD_TYPE,
-        tr("The currently active build configuration's type."),
-        [&]() -> QString {
-            BuildConfiguration *bc = activeBuildConfiguration();
-            const BuildConfiguration::BuildType type
-                                   = bc ? bc->buildType() : BuildConfiguration::Unknown;
-            return BuildConfiguration::buildTypeName(type);
+            Project *project = ProjectTree::currentProject();
+            return project ? project->displayName() : QString();
         });
 
     expander->registerPrefix(Constants::VAR_CURRENTBUILD_ENV,
                              BuildConfiguration::tr("Variables in the current build environment"),
+                             [](const QString &var) {
+                                 if (BuildConfiguration *bc = activeBuildConfiguration())
+                                     return bc->environment().expandedValueForKey(var);
+                                 return QString();
+                             }, false);
+    expander->registerPrefix("CurrentDocument:Project:BuildConfig:Env",
+                             BuildConfiguration::tr("Variables in the active build environment "
+                                     "of the project containing the currently open document"),
                              [](const QString &var) {
                                  if (BuildConfiguration *bc = activeBuildConfiguration())
                                      return bc->environment().expandedValueForKey(var);
@@ -1874,24 +1795,21 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
                  return bc->environment();
              return Utils::Environment::systemEnvironment();
          }});
-
     Utils::EnvironmentProvider::addProvider(
-        {"CurrentRun:Env", tr("Current Run Environment"), []() {
-             if (RunConfiguration *rc = activeRunConfiguration())
-                if (auto envAspect = rc->aspect<EnvironmentAspect>())
-                    return envAspect->environment();
+        {"CurrentDocument:Project:BuildConfig:Env", tr("Current Build Environment"), []() {
+             if (BuildConfiguration *bc = activeBuildConfiguration())
+                 return bc->environment();
              return Utils::Environment::systemEnvironment();
          }});
 
-    QString fileDescription = tr("File where current session is saved.");
-    auto fileHandler = [] { return SessionManager::sessionNameToFileName(SessionManager::activeSession()).toString(); };
-    expander->registerFileVariables("Session", fileDescription, fileHandler);
-    expander->registerFileVariables("CurrentSession", fileDescription, fileHandler, false);
-
-    QString nameDescription = tr("Name of current session.");
-    auto nameHandler = [] { return SessionManager::activeSession(); };
-    expander->registerVariable("Session:Name", nameDescription, nameHandler);
-    expander->registerVariable("CurrentSession:Name", nameDescription, nameHandler, false);
+    const auto fileHandler = [] {
+        return SessionManager::sessionNameToFileName(SessionManager::activeSession()).toString();
+    };
+    expander->registerFileVariables("Session", tr("File where current session is saved."),
+                                    fileHandler);
+    expander->registerVariable("Session:Name", tr("Name of current session."), [] {
+        return SessionManager::activeSession();
+    });
 
     return true;
 }

doc/qtcreator/src/editors/creator-code-completion.qdoc
@@ -230,8 +230,9 @@
     %{variable}
     \endcode
 
-    For example, the following variable expands to the name of the current
-    project: \c {%{CurrentProject:Name}}.
+    For example, the following variable expands to the name of the
+    project containing the file that is currently open in the editor:
+    \c {%{CurrentDocument:Project:Name}}.
 
     Use unique variable names within a snippet, because all instances of a
     variable are renamed when you specify a value for it.

share/qtcreator/snippets/cpp.xml
@@ -59,8 +59,8 @@ public:
 <snippet group="C++" trigger="lic" id="license-configured" complement="" removed="false" modified="false">%{Cpp:LicenseTemplate}
 $$</snippet>
 <snippet group="C++" trigger="licbsd" id="license-bsd" complement="" removed="false" modified="false">/**
-@if ('%{CurrentProject:Name}' !== '')
- ** This file is part of the %{CurrentProject:Name} project.
+@if ('%{CurrentDocument:Project:Name}' !== '')
+ ** This file is part of the %{CurrentDocument:Project:Name} project.
 @endif
 @if ('%{Env:QTC_COPYRIGHT_USER}' === '' || '%{Env:QTC_COPYRIGHT_EMAIL}' === '')
  ** Copyright %{CurrentDate:yyyy} $copyright_user$ &lt;$copyright_email$&gt;.
@@ -95,8 +95,8 @@ $$</snippet>
 
 $$</snippet>
 <snippet group="C++" trigger="licgpl" id="license-gpl" complement="" removed="false" modified="false">/**
-@if ('%{CurrentProject:Name}' !== '')
- ** This file is part of the %{CurrentProject:Name} project.
+@if ('%{CurrentDocument:Project:Name}' !== '')
+ ** This file is part of the %{CurrentDocument:Project:Name} project.
 @endif
 @if ('%{Env:QTC_COPYRIGHT_USER}' === '' || '%{Env:QTC_COPYRIGHT_EMAIL}' === '')
  ** Copyright %{CurrentDate:yyyy} $copyright_user$ &lt;$copyright_email$&gt;.
@@ -120,8 +120,8 @@ $$</snippet>
 
 $$</snippet>
 <snippet group="C++" trigger="liclgpl" id="license-lgpl" complement="" removed="false" modified="false">/**
-@if ('%{CurrentProject:Name}' !== '')
- ** This file is part of the %{CurrentProject:Name} project.
+@if ('%{CurrentDocument:Project:Name}' !== '')
+ ** This file is part of the %{CurrentDocument:Project:Name} project.
 @endif
 @if ('%{Env:QTC_COPYRIGHT_USER}' === '' || '%{Env:QTC_COPYRIGHT_EMAIL}' === '')
  ** Copyright %{CurrentDate:yyyy} $copyright_user$ &lt;$copyright_email$&gt;.
@@ -153,7 +153,7 @@ $$</snippet>
 @endif
 ** Contact: https://www.qt.io/licensing/
 **
-** This file is part of %{CurrentProject:Name}
+** This file is part of %{CurrentDocument:Project:Name}
 **
 ** Commercial License Usage
 ** Licensees holding valid commercial Qt licenses may use this file in

src/libs/utils/macroexpander.cpp
@@ -343,10 +343,11 @@ static QByteArray fullPrefix(const QByteArray &prefix)
  * \sa registerVariables(), registerIntVariable(), registerFileVariables()
  */
 void MacroExpander::registerPrefix(const QByteArray &prefix, const QString &description,
-                                   const MacroExpander::PrefixFunction &value)
+                                   const MacroExpander::PrefixFunction &value, bool visible)
 {
     QByteArray tmp = fullPrefix(prefix);
-    d->m_descriptions.insert(tmp + "<value>", description);
+    if (visible)
+        d->m_descriptions.insert(tmp + "<value>", description);
     d->m_prefixMap.insert(tmp, value);
 }
 

src/libs/utils/macroexpander.h
@@ -68,7 +68,7 @@ public:
     using IntFunction = std::function<int()>;
 
     void registerPrefix(const QByteArray &prefix,
-        const QString &description, const PrefixFunction &value);
+        const QString &description, const PrefixFunction &value, bool visible = true);
 
     void registerVariable(const QByteArray &variable,
         const QString &description, const StringFunction &value,

src/plugins/coreplugin/locator/executefilter.cpp
@@ -97,7 +97,7 @@ void ExecuteFilter::accept(LocatorFilterEntry selection,
     bool found;
     QString workingDirectory = Utils::globalMacroExpander()->value("CurrentDocument:Path", &found);
     if (!found || workingDirectory.isEmpty())
-        workingDirectory = Utils::globalMacroExpander()->value("CurrentProject:Path", &found);
+        workingDirectory = Utils::globalMacroExpander()->value("CurrentDocument:Project:Path", &found);
 
     ExecuteData d;
     d.workingDirectory = workingDirectory;

src/plugins/projectexplorer/buildconfiguration.cpp
@@ -172,11 +172,18 @@ BuildConfiguration::BuildConfiguration(Target *target, Utils::Id id)
     expander->registerVariable("buildDir", tr("Build directory"),
             [this] { return buildDirectory().toUserOutput(); });
 
+    // TODO: Remove "Current" variants in ~4.16.
     expander->registerVariable(Constants::VAR_CURRENTBUILD_NAME, tr("Name of current build"),
             [this] { return displayName(); }, false);
 
+    expander->registerVariable("BuildConfig:Name", tr("Name of the build configuration"),
+            [this] { return displayName(); });
+
     expander->registerPrefix(Constants::VAR_CURRENTBUILD_ENV,
                              tr("Variables in the current build environment"),
+                             [this](const QString &var) { return environment().expandedValueForKey(var); }, false);
+    expander->registerPrefix("BuildConfig:Env",
+                             tr("Variables in the build configuration's environment"),
                              [this](const QString &var) { return environment().expandedValueForKey(var); });
 
     updateCacheAndEmitEnvironmentChanged();

src/plugins/projectexplorer/kit.cpp
@@ -98,20 +98,30 @@ public:
         for (KitAspect *aspect : KitManager::kitAspects())
             aspect->addToMacroExpander(kit, &m_macroExpander);
 
-        // This provides the same global fall back as the global expander
-        // without relying on the currentKit() discovery process there.
-        m_macroExpander.registerVariable(Constants::VAR_CURRENTKIT_NAME,
+        // TODO: Remove the "Current" variants in ~4.16
+        m_macroExpander.registerVariable("CurrentKit:Name",
             tr("The name of the currently active kit."),
             [kit] { return kit->displayName(); },
             false);
-        m_macroExpander.registerVariable(Constants::VAR_CURRENTKIT_FILESYSTEMNAME,
+        m_macroExpander.registerVariable("Kit:Name",
+            tr("The name of the kit."),
+            [kit] { return kit->displayName(); });
+
+        m_macroExpander.registerVariable("CurrentKit:FileSystemName",
             tr("The name of the currently active kit in a filesystem-friendly version."),
             [kit] { return kit->fileSystemFriendlyName(); },
             false);
-        m_macroExpander.registerVariable(Constants::VAR_CURRENTKIT_ID,
+        m_macroExpander.registerVariable("Kit:FileSystemName",
+            tr("The name of the kit in a filesystem-friendly version."),
+            [kit] { return kit->fileSystemFriendlyName(); });
+
+        m_macroExpander.registerVariable("CurrentKit:Id",
             tr("The id of the currently active kit."),
             [kit] { return kit->id().toString(); },
             false);
+        m_macroExpander.registerVariable("Kit:Id",
+            tr("The id of the kit."),
+            [kit] { return kit->id().toString(); });
     }
 
     DisplayName m_unexpandedDisplayName;

commit 9156c25c9b1ecf9b5c0202ec3c59158c879ba551

Fix environment selection of external tools
The run environment was missing, and instead build environment was
duplicated.

Amends b55825a42000ee81e37c8e25bcd0784d26d6dc18 which accidentally
changed this.

Fixes: QTCREATORBUG-25634
Change-Id: I9d01452f603e9592a8e1e46adfd861e260c5eabd
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

src/plugins/projectexplorer/projectexplorer.cpp
@@ -1839,25 +1839,32 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
                                      return bc->environment().expandedValueForKey(var);
                                  return QString();
                              }, false);
-    expander->registerPrefix("CurrentDocument:Project:BuildConfig:Env",
-                             BuildConfiguration::tr("Variables in the active build environment "
-                                     "of the project containing the currently open document."),
+    const char currentBuildEnvVar[] = "CurrentDocument:Project:BuildConfig:Env";
+    expander->registerPrefix(currentBuildEnvVar,
+                             BuildConfiguration::tr(
+                                 "Variables in the active build environment "
+                                 "of the project containing the currently open document."),
                              [](const QString &var) {
                                  if (BuildConfiguration *bc = currentBuildConfiguration())
                                      return bc->environment().expandedValueForKey(var);
                                  return QString();
                              });
     Utils::EnvironmentProvider::addProvider(
-        {Constants::VAR_CURRENTBUILD_ENV, tr("Current Build Environment"), []() {
+        {currentBuildEnvVar, tr("Current Build Environment"), []() {
              if (BuildConfiguration *bc = currentBuildConfiguration())
                  return bc->environment();
              return Utils::Environment::systemEnvironment();
          }});
     Utils::EnvironmentProvider::addProvider(
-        {"CurrentDocument:Project:BuildConfig:Env", tr("Current Build Environment"), []() {
-             if (BuildConfiguration *bc = currentBuildConfiguration())
-                  return bc->environment();
-              return Utils::Environment::systemEnvironment();
+        {"CurrentDocument:Project:RunConfig:Env", tr("Current Run Environment"), []() {
+             const Project *const project = ProjectTree::currentProject();
+             const Target *const target = project ? project->activeTarget() : nullptr;
+             const RunConfiguration *const rc = target ? target->activeRunConfiguration() : nullptr;
+             if (rc) {
+                 if (auto envAspect = rc->aspect<EnvironmentAspect>())
+                     return envAspect->environment();
+             }
+             return Utils::Environment::systemEnvironment();
          }});
 
     // Global variables for the active project.
@@ -1909,9 +1916,9 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
                  return bc->environment();
              return Utils::Environment::systemEnvironment();
          }});
-    expander->registerPrefix("ActiveProject:BuildConfig:Env",
+    expander->registerPrefix(activeBuildEnvVar,
                              BuildConfiguration::tr("Variables in the active build environment "
-                                     "of the active project."),
+                                                    "of the active project."),
                              [](const QString &var) {
                                  if (BuildConfiguration * const bc = activeBuildConfiguration())
                                      return bc->environment().expandedValueForKey(var);
@@ -1932,7 +1939,17 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
                 return rc->commandLine().executable().toString();
             return QString();
         });
-    expander->registerPrefix("ActiveProject:RunConfig:Env",
+    const char activeRunEnvVar[] = "ActiveProject:RunConfig:Env";
+    Utils::EnvironmentProvider::addProvider(
+        {activeRunEnvVar, tr("Active run environment of the active project."), [] {
+             if (const RunConfiguration *const rc = activeRunConfiguration()) {
+                 if (auto envAspect = rc->aspect<EnvironmentAspect>())
+                     return envAspect->environment();
+             }
+             return Utils::Environment::systemEnvironment();
+         }});
+    expander->registerPrefix(
+        activeRunEnvVar,
         tr("Variables in the environment of the active project's active run configuration."),
         [](const QString &var) {
             if (const RunConfiguration * const rc = activeRunConfiguration()) {
@@ -1940,7 +1957,7 @@ bool ProjectExplorerPlugin::initialize(const QStringList &arguments, QString *er
                     return envAspect->environment().expandedValueForKey(var);
             }
             return QString();
-    });
+        });
     expander->registerVariable("ActiveProject:RunConfig:WorkingDir",
         tr("The working directory of the active project's active run configuration."),
         [] {