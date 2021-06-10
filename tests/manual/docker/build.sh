a80546593d65779173d97360e98e474aa96caf19
hjk <hjk@qt.io>
2021-06-10 07:04:15 +0200

    Docker: Add another example Dockerfile for a run environment
    Without build tools, for demonstration purposes.
    
    Change-Id: I5940770aa0a5eb83d618ebdeb12436b38d3b21fa
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- tests/manual/docker/build.sh
+++ tests/manual/docker/build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker build -t qt-5-ubuntu-20.04 .
+
+docker build -t qt-5-ubuntu-20.04-build -f Dockerfile-qt-5-ubuntu-20.04-build .
+docker build -t qt-5-ubuntu-20.04-run -f Dockerfile-qt-5-ubuntu-20.04-run .
