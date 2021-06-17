5ae15f945f1150b545b656659d18417fa57d158e
David Schulz <david.schulz@qt.io>
2021-06-17 08:48:10 +0200

    LSP: prevent comment warning
    Change-Id: I269c1efe9e297daceb7bb030a3eec321616f7f66
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/libs/languageserverprotocol/lsptypes.h
+++ src/libs/languageserverprotocol/lsptypes.h
@@ -384,7 +384,7 @@ public:
      * - `*` to match one or more characters in a path segment
      * - `?` to match on one character in a path segment
      * - `**` to match any number of path segments, including none
-     * - `{}` to group sub patterns into an OR expression. (e.g. `**​/*.{ts,js}`
+     * - `{}` to group sub patterns into an OR expression. (e.g. `*.{ts,js}`
      *   matches all TypeScript and JavaScript files)
      * - `[]` to declare a range of characters to match in a path segment
      *   (e.g., `example.[0-9]` to match on `example.0`, `example.1`, …)
