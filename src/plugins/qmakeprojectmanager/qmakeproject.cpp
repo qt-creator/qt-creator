qt-creator/qt-creator

commit a30aa4421a0257b048197b51330e6bf5c2732af5

Qmake: Prevent unresponsive processes from hanging Qt Creator
That is, processes run via qmake's system() function.
Fun fact disovered while implementing this: "Canceling" the loading of a
qmake project did nothing at all except making the progress bar red at
the end of the parsing procedure. Now at least we terminate the
currently running processes invoked by system(), so the parsing threads
can continue and eventually finish.

Task-number: QTCREATORBUG-24825
Task-number: QTCREATORBUG-25000
Task-number: QTCREATORBUG-25194
Change-Id: Ic92ef200d3a49bbdeff0429ae7038fe3f1935b38
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/plugins/qmakeprojectmanager/qmakeproject.cpp
@@ -666,6 +666,18 @@ void QmakeBuildSystem::asyncUpdate()
                                    Constants::PROFILE_EVALUATE);
 
     m_asyncUpdateFutureInterface.reportStarted();
+    const auto watcher = new QFutureWatcher<void>(this);
+    connect(watcher, &QFutureWatcher<void>::canceled, this, [this, watcher] {
+        if (!m_qmakeGlobals)
+            return;
+        watcher->disconnect();
+        m_qmakeGlobals->killProcesses();
+    });
+    connect(watcher, &QFutureWatcher<void>::finished, this, [watcher] {
+        watcher->disconnect();
+        watcher->deleteLater();
+    });
+    watcher->setFuture(m_asyncUpdateFutureInterface.future());
 
     const Kit *const k = kit();
     QtSupport::BaseQtVersion *const qtVersion = QtSupport::QtKitAspect::qtVersion(k);

src/shared/proparser/qmakebuiltins.cpp
@@ -473,13 +473,28 @@ void QMakeEvaluator::runProcess(QProcess *proc, const QString &command) const
         proc->setProcessEnvironment(env);
     }
 # endif
+# ifdef PROEVALUATOR_THREAD_SAFE
+    m_option->mutex.lock();
+    if (m_option->canceled) {
+        m_option->mutex.unlock();
+        return;
+    }
+    m_option->runningProcs << proc;
+# endif
 # ifdef Q_OS_WIN
     proc->setNativeArguments(QLatin1String("/v:off /s /c \"") + command + QLatin1Char('"'));
     proc->start(m_option->getEnv(QLatin1String("COMSPEC")), QStringList());
 # else
     proc->start(QLatin1String("/bin/sh"), QStringList() << QLatin1String("-c") << command);
 # endif
+# ifdef PROEVALUATOR_THREAD_SAFE
+    m_option->mutex.unlock();
+# endif
     proc->waitForFinished(-1);
+# ifdef PROEVALUATOR_THREAD_SAFE
+    QMutexLocker(&m_option->mutex);
+    m_option->runningProcs.removeOne(proc);
+# endif
 }
 #endif
 
@@ -490,7 +505,7 @@ QByteArray QMakeEvaluator::getCommandOutput(const QString &args, int *exitCode) 
     QProcess proc;
     runProcess(&proc, args);
     *exitCode = (proc.exitStatus() == QProcess::NormalExit) ? proc.exitCode() : -1;
-    QByteArray errout = proc.readAllStandardError();
+    QByteArray errout = proc.isReadable() ? proc.readAllStandardError() : QByteArray();
 # ifdef PROEVALUATOR_FULL
     // FIXME: Qt really should have the option to set forwarding per channel
     fputs(errout.constData(), stderr);
@@ -503,7 +518,7 @@ QByteArray QMakeEvaluator::getCommandOutput(const QString &args, int *exitCode) 
             QString::fromLocal8Bit(errout));
     }
 # endif
-    out = proc.readAllStandardOutput();
+    out = proc.isReadable() ? proc.readAllStandardOutput() : QByteArray();
 # ifdef Q_OS_WIN
     // FIXME: Qt's line end conversion on sequential files should really be fixed
     out.replace("\r\n", "\n");

src/shared/proparser/qmakeglobals.cpp
@@ -89,6 +89,17 @@ QMakeGlobals::~QMakeGlobals()
     qDeleteAll(baseEnvs);
 }
 
+void QMakeGlobals::killProcesses()
+{
+#ifdef PROEVALUATOR_THREAD_SAFE
+    QMutexLocker lock(&mutex);
+    canceled = true;
+    for (QProcess * const proc : runningProcs)
+        proc->kill();
+    runningProcs.clear();
+#endif
+}
+
 QString QMakeGlobals::cleanSpec(QMakeCmdLineParserState &state, const QString &spec)
 {
     QString ret = QDir::cleanPath(spec);

src/shared/proparser/qmakeglobals.h
@@ -112,6 +112,8 @@ public:
     QString extra_cmds[4];
     bool runSystemFunction = false;
 
+    void killProcesses();
+
 #ifdef PROEVALUATOR_DEBUG
     int debugLevel;
 #endif
@@ -157,6 +159,8 @@ private:
 
 #ifdef PROEVALUATOR_THREAD_SAFE
     QMutex mutex;
+    bool canceled = false;
+    QList<QProcess *> runningProcs;
 #endif
     QHash<QMakeBaseKey, QMakeBaseEnv *> baseEnvs;
 

commit 4767dfcce32ca69d88861f5ab8a18b0a1110b0b4

qmake: Fix issues with executing system calls
Do not try to reuse the QFutureInterface that is used for the parsing
process. Reusing the QFutureInterface can lead to issues. So far no
problems were triggered, but a30aa4421a0257b048197b51330e6bf5c2732af5
introduced a watcher that tells the qmake parser to ignore all system
calls after the future was canceled. This was somehow, sometimes
triggered on the reused QFutureInterface even though the user didn't
cancel anyhing, leading to all system calls to bail out in the
subsequent run.

Using a new QFutureInterface instance for each parsing run solves the
issue.

Amends a30aa4421a0257b048197b51330e6bf5c2732af5

Fixes: QTCREATORBUG-25970
Change-Id: I6836c97038c36968e93815c6121bc284edbe19bb
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

src/plugins/qmakeprojectmanager/qmakeproject.cpp
@@ -284,8 +284,11 @@ QmakeBuildSystem::~QmakeBuildSystem()
     delete m_qmakeVfs;
     m_qmakeVfs = nullptr;
 
-    m_asyncUpdateFutureInterface.reportCanceled();
-    m_asyncUpdateFutureInterface.reportFinished();
+    if (m_asyncUpdateFutureInterface) {
+        m_asyncUpdateFutureInterface->reportCanceled();
+        m_asyncUpdateFutureInterface->reportFinished();
+        m_asyncUpdateFutureInterface.reset();
+    }
 }
 
 void QmakeBuildSystem::updateCodeModels()
@@ -592,8 +595,9 @@ void QmakeBuildSystem::incrementPendingEvaluateFutures()
     }
     ++m_pendingEvaluateFuturesCount;
     TRACE("pending inc to: " << m_pendingEvaluateFuturesCount);
-    m_asyncUpdateFutureInterface.setProgressRange(m_asyncUpdateFutureInterface.progressMinimum(),
-                                                  m_asyncUpdateFutureInterface.progressMaximum() + 1);
+    m_asyncUpdateFutureInterface->setProgressRange(m_asyncUpdateFutureInterface->progressMinimum(),
+                                                   m_asyncUpdateFutureInterface->progressMaximum()
+                                                       + 1);
 }
 
 void QmakeBuildSystem::decrementPendingEvaluateFutures()
@@ -606,15 +610,17 @@ void QmakeBuildSystem::decrementPendingEvaluateFutures()
         return; // We are closing the project!
     }
 
-    m_asyncUpdateFutureInterface.setProgressValue(m_asyncUpdateFutureInterface.progressValue() + 1);
+    m_asyncUpdateFutureInterface->setProgressValue(m_asyncUpdateFutureInterface->progressValue()
+                                                   + 1);
     if (m_pendingEvaluateFuturesCount == 0) {
         // We are done!
         setRootProjectNode(QmakeNodeTreeBuilder::buildTree(this));
 
         if (!m_rootProFile->validParse())
-            m_asyncUpdateFutureInterface.reportCanceled();
+            m_asyncUpdateFutureInterface->reportCanceled();
 
-        m_asyncUpdateFutureInterface.reportFinished();
+        m_asyncUpdateFutureInterface->reportFinished();
+        m_asyncUpdateFutureInterface.reset();
         m_cancelEvaluate = false;
 
         // TODO clear the profile cache ?
@@ -660,12 +666,13 @@ void QmakeBuildSystem::asyncUpdate()
         m_qmakeVfs->invalidateCache();
     }
 
-    m_asyncUpdateFutureInterface.setProgressRange(0, 0);
-    Core::ProgressManager::addTask(m_asyncUpdateFutureInterface.future(),
+    m_asyncUpdateFutureInterface.reset(new QFutureInterface<void>);
+    m_asyncUpdateFutureInterface->setProgressRange(0, 0);
+    Core::ProgressManager::addTask(m_asyncUpdateFutureInterface->future(),
                                    tr("Reading Project \"%1\"").arg(project()->displayName()),
                                    Constants::PROFILE_EVALUATE);
 
-    m_asyncUpdateFutureInterface.reportStarted();
+    m_asyncUpdateFutureInterface->reportStarted();
     const auto watcher = new QFutureWatcher<void>(this);
     connect(watcher, &QFutureWatcher<void>::canceled, this, [this, watcher] {
         if (!m_qmakeGlobals)
@@ -677,7 +684,7 @@ void QmakeBuildSystem::asyncUpdate()
         watcher->disconnect();
         watcher->deleteLater();
     });
-    watcher->setFuture(m_asyncUpdateFutureInterface.future());
+    watcher->setFuture(m_asyncUpdateFutureInterface->future());
 
     const Kit *const k = kit();
     QtSupport::BaseQtVersion *const qtVersion = QtSupport::QtKitAspect::qtVersion(k);
@@ -688,8 +695,9 @@ void QmakeBuildSystem::asyncUpdate()
                       .arg(project()->displayName(), k->displayName())
                 : tr("Cannot parse project \"%1\": No kit selected.").arg(project()->displayName());
         proFileParseError(errorMessage, project()->projectFilePath());
-        m_asyncUpdateFutureInterface.reportCanceled();
-        m_asyncUpdateFutureInterface.reportFinished();
+        m_asyncUpdateFutureInterface->reportCanceled();
+        m_asyncUpdateFutureInterface->reportFinished();
+        m_asyncUpdateFutureInterface.reset();
         return;
     }
 

src/plugins/qmakeprojectmanager/qmakeproject.h
@@ -198,7 +198,7 @@ private:
 
     QString m_qmakeSysroot;
 
-    QFutureInterface<void> m_asyncUpdateFutureInterface;
+    std::unique_ptr<QFutureInterface<void>> m_asyncUpdateFutureInterface;
     int m_pendingEvaluateFuturesCount = 0;
     AsyncUpdateState m_asyncUpdateState = Base;
     bool m_cancelEvaluate = false;