ee974ec491c09f67c4a47fe743680f49acb33a87
hjk <hjk@qt.io>
2021-07-12 11:50:48 +0200

    Utils: Mention environment dialog += and =+ to append and prepend
    Change-Id: I9aea9c7bbdfcbdc164c0233b0b12fd634c4b2a47
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/libs/utils/environmentdialog.cpp
+++ src/libs/utils/environmentdialog.cpp
@@ -46,6 +46,8 @@ Utils::optional<EnvironmentItems> EnvironmentDialog::getEnvironmentItems(
         tr("Edit Environment"),
         tr("Enter one environment variable per line.\n"
            "To set or change a variable, use VARIABLE=VALUE.\n"
+           "To append to a variable, use VARIABLE+=VALUE.\n"
+           "To prepend to a variable, use VARIABLE=+VALUE.\n"
            "Existing variables can be referenced in a VALUE with ${OTHER}.\n"
            "To clear a variable, put its name on a line with nothing else on it.\n"
            "To disable a variable, prefix the line with \"#\"."));
