qt-creator/qt-creator

commit c1f90aeca2a921397c36f53fd8d6a834b79e0b0d

ICore: Change some path API to use FilePath
Change-Id: Id841d6177206a021c9e606ce560b47d1ae6e52b9
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
Reviewed-by: hjk <hjk@qt.io>

src/plugins/texteditor/codestylepool.cpp
@@ -94,9 +94,7 @@ QByteArray CodeStylePoolPrivate::generateUniqueId(const QByteArray &id) const
 
 static QString customCodeStylesPath()
 {
-    QString path = Core::ICore::userResourcePath();
-    path.append(QLatin1String("/codestyles/"));
-    return path;
+    return Core::ICore::userResourcePath().pathAppended("codestyles").toString();
 }
 
 CodeStylePool::CodeStylePool(ICodeStylePreferencesFactory *factory, QObject *parent)

commit 842770b6b57d1326b23a83e99451ad4a3cde5f74

TextEditor: Fix path to code styles
Was broken by c1f90aeca2.

Fixes: QTCREATORBUG-25989
Change-Id: I91879e0adb3cd90be8abddd6a1ca02364b8b9fce
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/texteditor/codestylepool.cpp
@@ -92,9 +92,9 @@ QByteArray CodeStylePoolPrivate::generateUniqueId(const QByteArray &id) const
 }
 }
 
-static QString customCodeStylesPath()
+static Utils::FilePath customCodeStylesPath()
 {
-    return Core::ICore::userResourcePath("codestyles").toString();
+    return Core::ICore::userResourcePath("codestyles");
 }
 
 CodeStylePool::CodeStylePool(ICodeStylePreferencesFactory *factory, QObject *parent)
@@ -112,7 +112,7 @@ CodeStylePool::~CodeStylePool()
 QString CodeStylePool::settingsDir() const
 {
     const QString suffix = d->m_factory ? d->m_factory->languageId().toString() : QLatin1String("default");
-    return customCodeStylesPath().append(suffix);
+    return customCodeStylesPath().pathAppended(suffix).toString();
 }
 
 Utils::FilePath CodeStylePool::settingsPath(const QByteArray &id) const
@@ -261,7 +261,7 @@ void CodeStylePool::slotSaveCodeStyle()
 
 void CodeStylePool::saveCodeStyle(ICodeStylePreferences *codeStyle) const
 {
-    const QString codeStylesPath = customCodeStylesPath();
+    const QString codeStylesPath = customCodeStylesPath().toString();
 
     // Create the base directory when it doesn't exist
     if (!QFile::exists(codeStylesPath) && !QDir().mkpath(codeStylesPath)) {