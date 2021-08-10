27d414458e67e3c33bde131270f73233b737d8a5
Tim Jenssen <tim.jenssen@qt.io>
2021-08-10 09:44:49 +0200

    qmlpuppet: fix qt 5.15 builds
    Change-Id: I6d19624537a3980252d43624ecf24e110638225a
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

--- share/qtcreator/qml/qmlpuppet/qml2puppet/editor3d/generalhelper.cpp
+++ share/qtcreator/qml/qmlpuppet/qml2puppet/editor3d/generalhelper.cpp
@@ -328,7 +328,8 @@ void GeneralHelper::storeToolState(const QString &sceneId, const QString &tool, 
             handlePendingToolStateUpdate();
         QVariant theState;
         // Convert JS arrays to QVariantLists for easier handling down the line
-        if (state.metaType().id() != QMetaType::QString && state.canConvert(QMetaType::QVariantList))
+        // metaType().id() which only exist in Qt6 is the same as userType()
+        if (state.userType() != QMetaType::QString && state.canConvert(QMetaType::QVariantList))
             theState = state.value<QVariantList>();
         else
             theState = state;
