d7b42e5455ca9119be14b7317d27ba8270239ffe
Aleksei German <aleksei.german@qt.io>
2021-08-18 14:15:51 +0200

    Python: Fix for Wizard generated file names
     - Changed generated project filenames
     - Added default Class name
    
    Task-number: QDS-4897
    Change-Id: I16318cf7cbdcf078f4da01016ebeef8036a85eb0
    Reviewed-by: David Schulz <david.schulz@qt.io>
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- share/qtcreator/templates/wizards/projects/qtforpythonapplication/main_qtquick.py
+++ share/qtcreator/templates/wizards/projects/qtforpythonapplication/main_qtquick.py
@@ -10,7 +10,7 @@ from %{PySideVersion}.QtQml import QQmlApplicationEngine
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    engine.load(os.fspath(Path(__file__).resolve().parent / "main.qml"))
+    engine.load(os.fspath(Path(__file__).resolve().parent / "%{QmlFileName}"))
     if not engine.rootObjects():
         sys.exit(-1)
     sys.exit(app.exec_())
