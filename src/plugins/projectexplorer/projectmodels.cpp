070cb5e09d2c17051773cd2fa508c0d79c6b705d
Marco Bubke <marco.bubke@qt.io>
2021-08-23 09:31:01 +0200

    Projects: Fix deprecated iterator
    std::iterator is deprecated in C++ 17. The aliases should be set directly.
    The aliases are public too.
    
    Change-Id: Ib4dc259b03bd9386ba9d177340b89cc7208c8643
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>
    Reviewed-by: hjk <hjk@qt.io>

--- src/plugins/projectexplorer/projectmodels.cpp
+++ src/plugins/projectexplorer/projectmodels.cpp
@@ -73,9 +73,15 @@ namespace Internal {
 
 /// An output iterator whose assignment operator appends a clone of the operand to the list of
 /// children of the WrapperNode passed to the constructor.
-class Appender : public std::iterator<std::output_iterator_tag, void, void, void, void>
+class Appender
 {
 public:
+    using iterator_category = std::output_iterator_tag;
+    using value_type = void;
+    using difference_type = void;
+    using pointer = void;
+    using reference = void;
+
     explicit Appender(WrapperNode *parent) : m_parent(parent) {}
 
     Appender &operator=(const WrapperNode *node)
