595a4a621d5ba94ce6ff0ae3042b39baa795ef7d
Alessandro Portale <alessandro.portale@qt.io>
2021-08-26 13:00:36 +0200

    Tests: Avoid Qt6-related deprecation warnings
    Qt::AA_EnableHighDpiScaling and Qt::AA_UseHighDpiPixmaps are deprecated
    
    Change-Id: I36a289f00feeaa472ee911a567c2a6160e259317
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- tests/manual/widgets/crumblepath/tst_manual_widgets_crumblepath.cpp
+++ tests/manual/widgets/crumblepath/tst_manual_widgets_crumblepath.cpp
@@ -93,7 +93,9 @@ QWidget *shrinkingCrumblePath()
 
 int main(int argc, char *argv[])
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif // Qt < 6
 
     QApplication app(argc, argv);
 
--- tests/manual/widgets/manhattanstyle/tst_manual_widgets_manhattanstyle.cpp
+++ tests/manual/widgets/manhattanstyle/tst_manual_widgets_manhattanstyle.cpp
@@ -123,8 +123,10 @@ static QWidget *centralWidget()
 
 int main(int argc, char *argv[])
 {
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif // Qt < 6
 
     QApplication app(argc, argv);
 
