qt-creator/qt-creator

commit 8e352af0ecd3c330a96f7ca67067f0dfe200a0e4

Core: Add a default parameter to various ICore::*path functions
Saves some code on the user side.

Change-Id: I32cd220b6e533f5497a1865f9c34ab9db4cfda79
Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/coreplugin/mimetypesettings.cpp
@@ -53,7 +53,7 @@
 #include <QStyledItemDelegate>
 #include <QSortFilterProxyModel>
 
-static const char kModifiedMimeTypesFile[] = "/mimetypes/modifiedmimetypes.xml";
+static const char kModifiedMimeTypesFile[] = "mimetypes/modifiedmimetypes.xml";
 
 static const char mimeInfoTagC[] = "mime-info";
 static const char mimeTypeTagC[] = "mime-type";
@@ -564,8 +564,7 @@ static QPair<int, int> rangeFromString(const QString &offset)
 
 MimeTypeSettingsPrivate::UserMimeTypeHash MimeTypeSettingsPrivate::readUserModifiedMimeTypes()
 {
-    static Utils::FilePath modifiedMimeTypesPath = ICore::userResourcePath()
-                                                   + kModifiedMimeTypesFile;
+    static Utils::FilePath modifiedMimeTypesPath = ICore::userResourcePath(kModifiedMimeTypesFile);
     UserMimeTypeHash userMimeTypes;
     QFile file(modifiedMimeTypesPath.toString());
     if (file.open(QFile::ReadOnly)) {

commit 99399ab6ee84377ad0c1ec782120d66ea13287d7

Fix saving of MIME type settings
Was missing a slash after the userResourcePath.
Broke with 8e352af0ecd3c330a96f7ca67067f0dfe200a0e4

Change-Id: I2f60e3dcaa07b13cd2459ee6a3aa67b51075545e
Reviewed-by: hjk <hjk@qt.io>

src/plugins/coreplugin/mimetypesettings.cpp
@@ -502,8 +502,7 @@ void MimeTypeSettingsPrivate::ensurePendingMimeType(const Utils::MimeType &mimeT
 
 void MimeTypeSettingsPrivate::writeUserModifiedMimeTypes()
 {
-    static Utils::FilePath modifiedMimeTypesFile = ICore::userResourcePath()
-                                                   + kModifiedMimeTypesFile;
+    static Utils::FilePath modifiedMimeTypesFile = ICore::userResourcePath(kModifiedMimeTypesFile);
 
     if (QFile::exists(modifiedMimeTypesFile.toString())
             || QDir().mkpath(modifiedMimeTypesFile.parentDir().toString())) {