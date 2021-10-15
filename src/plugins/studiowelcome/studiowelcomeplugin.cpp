qt-creator/qt-creator

commit ea8400a9ffd365300fbbb25abec8f71e9c6a016a

StudioWelcome: Do not use TitilliumWeb as system font
Task-number: QDS-1081
Change-Id: I6c78fc0a95163f72168a29373a8e190d4bdf3167
Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

src/plugins/studiowelcome/studiowelcomeplugin.cpp
@@ -340,11 +340,6 @@ bool StudioWelcomePlugin::initialize(const QStringList &arguments, QString *erro
     qmlRegisterType<ProjectModel>("projectmodel", 1, 0, "ProjectModel");
     qmlRegisterType<UsageStatisticPluginModel>("usagestatistics", 1, 0, "UsageStatisticModel");
 
-    m_welcomeMode = new WelcomeMode;
-
-    QFontDatabase::addApplicationFont(":/studiofonts/TitilliumWeb-Regular.ttf");
-    QFont systemFont("Titillium Web", QApplication::font().pointSize());
-    QApplication::setFont(systemFont);
 
     m_removeSplashTimer.setSingleShot(true);
     m_removeSplashTimer.setInterval(15000);

commit a746510937d259a20a785a508741e1d659456dfe

StudioWelcome: Fix studio welcome plugin crash
crash introduced by ea8400a9ffd365300fbbb25abec8f71e9c6a016a

Change-Id: I7302e71076634caaf68dd52cf61451110629ce0d
Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>

src/plugins/studiowelcome/studiowelcomeplugin.cpp
@@ -340,6 +340,7 @@ bool StudioWelcomePlugin::initialize(const QStringList &arguments, QString *erro
     qmlRegisterType<ProjectModel>("projectmodel", 1, 0, "ProjectModel");
     qmlRegisterType<UsageStatisticPluginModel>("usagestatistics", 1, 0, "UsageStatisticModel");
 
+    m_welcomeMode = new WelcomeMode;
 
     m_removeSplashTimer.setSingleShot(true);
     m_removeSplashTimer.setInterval(15000);