ec07bf8f7c2b1899749b030a4daf2d08e70ce17e
Cristian Adam <cristian.adam@qt.io>
2022-01-04 18:31:39 +0000

    Revert "UnitTests: Disable some ClientServerOutsideProcess tests"
    This reverts commit c22c22c3d61376cca29e12824679fdcaf9309c1f.
    
    Reason for revert: With d65e47b5c6d21f58f40b5b8d468ff612fd2fe5b0 the tests are passing.
    
    Change-Id: Ide9c12e38c229315364489123f687b50632ffed8
    Reviewed-by: Marco Bubke <marco.bubke@qt.io>

--- tests/unit/unittest/clientserveroutsideprocess-test.cpp
+++ tests/unit/unittest/clientserveroutsideprocess-test.cpp
@@ -71,7 +71,7 @@ TEST_F(ClientServerOutsideProcessSlowTest, RestartProcessAsynchronously)
     ASSERT_TRUE(client.isConnected());
 }
 
-TEST_F(ClientServerOutsideProcessSlowTest, DISABLED_RestartProcessAfterAliveTimeout)
+TEST_F(ClientServerOutsideProcessSlowTest, RestartProcessAfterAliveTimeout)
 {
     QSignalSpy clientSpy(&client, &ConnectionClient::connectedToLocalSocket);
 
@@ -81,7 +81,7 @@ TEST_F(ClientServerOutsideProcessSlowTest, DISABLED_RestartProcessAfterAliveTime
     ASSERT_THAT(clientSpy, SizeIs(1));
 }
 
-TEST_F(ClientServerOutsideProcessSlowTest, DISABLED_RestartProcessAfterTermination)
+TEST_F(ClientServerOutsideProcessSlowTest, RestartProcessAfterTermination)
 {
     QSignalSpy clientSpy(&client, &ConnectionClient::connectedToLocalSocket);
 
