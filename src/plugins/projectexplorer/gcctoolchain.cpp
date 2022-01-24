35e908d835aa23cb9ba04e13298bb3316acee564
Christian Kandeler <christian.kandeler@qt.io>
2022-01-24 16:29:34 +0100

    Fix build
    Change-Id: Ie23c6c8b131507e9ae2579ef361002654f55e282
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/plugins/projectexplorer/gcctoolchain.cpp
+++ src/plugins/projectexplorer/gcctoolchain.cpp
@@ -1002,8 +1002,10 @@ GccToolChainFactory::GccToolChainFactory()
 Toolchains GccToolChainFactory::autoDetect(const ToolchainDetector &detector) const
 {
     // GCC is almost never what you want on macOS, but it is by default found in /usr/bin
-    if (HostOsInfo::isMacHost() && (!device || device->type() == Constants::DESKTOP_DEVICE_TYPE))
+    if (HostOsInfo::isMacHost()
+            && (!detector.device || detector.device->type() == Constants::DESKTOP_DEVICE_TYPE)) {
         return {};
+    }
     Toolchains tcs;
     static const auto tcChecker = [](const ToolChain *tc) {
         return tc->targetAbi().osFlavor() != Abi::WindowsMSysFlavor
