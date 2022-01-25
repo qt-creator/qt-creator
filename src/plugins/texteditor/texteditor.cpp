5a3648c8956c74cc39ed37b8ee2a7d324b0dfc35
Eike Ziller <eike.ziller@qt.io>
2022-01-25 11:29:34 +0100

    macOS: Fix Delete with Google Japanese IME
    Do not eat all ShortcutOverride events. Let the rest of the class &
    widget hierarchy know about them too.
    
    Fixes: QTCREATORBUG-26764
    Change-Id: I7e2f42183bc9cb3931aca3669a6703c83e39b4bc
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/plugins/texteditor/texteditor.cpp
+++ src/plugins/texteditor/texteditor.cpp
@@ -2914,7 +2914,7 @@ bool TextEditorWidget::event(QEvent *e)
                            && (ke->key() < Qt::Key_Escape));
             d->m_maybeFakeTooltipEvent = false;
         }
-        return true;
+        break;
     }
     case QEvent::ApplicationPaletteChange: {
         // slight hack: ignore palette changes
