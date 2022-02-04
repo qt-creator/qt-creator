8245ed30ae43ed6faf3d157d704ee03667b3ecb0
David Schulz <david.schulz@qt.io>
2022-02-04 11:27:35 +0100

    LanguageClient: support sorting outline widget items
    (cherry picked from commit 46a012bc11236c094c6881c94430804912c01aec)
    Change-Id: Iead37c07f09c2f961d506caef76562fb1b3dc308
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/plugins/languageclient/languageclientoutline.h
+++ src/plugins/languageclient/languageclientoutline.h
@@ -46,6 +46,7 @@ public:
 public:
     bool supportsEditor(Core::IEditor *editor) const override;
     TextEditor::IOutlineWidget *createWidget(Core::IEditor *editor) override;
+    bool supportsSorting() const override { return true; }
 };
 
 } // namespace LanguageClient
