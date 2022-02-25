49a7ada03fffd447b0d474a5dfb8f9a1657c06bb
Thomas Hartmann <thomas.hartmann@qt.io>
2022-02-25 18:00:10 +0100

    QmlDesigner: Remove superfluous code
    Change-Id: I0f2c746130b768844307798007fc1192e0dd2939
    Reviewed-by: Henning Gründl <henning.gruendl@qt.io>

--- src/plugins/qmldesigner/designercore/model/rewriterview.cpp
+++ src/plugins/qmldesigner/designercore/model/rewriterview.cpp
@@ -862,14 +862,8 @@ ModelNode RewriterView::nodeAtTextCursorPositionHelper(const ModelNode &root, in
 
     ModelNode lastNode = root;
 
-    int i = 0;
     for (const myPair &pair : data) {
         ModelNode node = pair.first;
-        i++;
-        if (i >= int(data.size())) {
-            lastNode = node;
-            break;
-        }
 
         const int nodeTextOffset = nodeOffset(node);
         const int nodeTextLength = m_textModifier->text().indexOf("}", nodeTextOffset) - nodeTextOffset - 1;
