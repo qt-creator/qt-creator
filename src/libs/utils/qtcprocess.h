6d853023c6b0c65ccd6693634d93cdf7119f5942
Jarek Kobus <jaroslaw.kobus@qt.io>
2022-03-23 16:39:47 +0100

    QtcProcess: Fix a comment
    Amends 8e0ae8ba96ec5ca4512e39a16135e2279e88a4fb
    
    Change-Id: Ie5b99525ec7de0565cd79ed52c7fc094d5509ddc
    Reviewed-by: hjk <hjk@qt.io>

--- src/libs/utils/qtcprocess.h
+++ src/libs/utils/qtcprocess.h
@@ -149,7 +149,7 @@ public:
                               qint64 *pid = nullptr);
 
     // Starts the command and waits for finish.
-    // User input processing is enabled when WithEventLoop was passed.
+    // User input processing is enabled when EventLoopMode::On was passed.
     void runBlocking(EventLoopMode eventLoopMode = EventLoopMode::Off);
 
     /* Timeout for hanging processes (triggers after no more output
