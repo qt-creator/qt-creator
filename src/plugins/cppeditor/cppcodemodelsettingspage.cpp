c97f0af531ec41fdc3619fd989bd681c409f029b
Christian Kandeler <christian.kandeler@qt.io>
2022-04-21 15:24:40 +0200

    ClangCodeModel: Don't ignore changes to clangd doc update threshold
    Changing the document update threshold value had no effect in the per-
    project settings.
    
    Change-Id: Ibd6e47bba3e0a77ed18b97486eda17718c46c59d
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: David Schulz <david.schulz@qt.io>

--- src/plugins/cppeditor/cppcodemodelsettingspage.cpp
+++ src/plugins/cppeditor/cppcodemodelsettingspage.cpp
@@ -381,6 +381,8 @@ ClangdSettingsWidget::ClangdSettingsWidget(const ClangdSettings::Data &settingsD
             this, &ClangdSettingsWidget::settingsDataChanged);
     connect(&d->threadLimitSpinBox, qOverload<int>(&QSpinBox::valueChanged),
             this, &ClangdSettingsWidget::settingsDataChanged);
+    connect(&d->documentUpdateThreshold, qOverload<int>(&QSpinBox::valueChanged),
+            this, &ClangdSettingsWidget::settingsDataChanged);
     connect(&d->clangdChooser, &Utils::PathChooser::pathChanged,
             this, &ClangdSettingsWidget::settingsDataChanged);
 }
