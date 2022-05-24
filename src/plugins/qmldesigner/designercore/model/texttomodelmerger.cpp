07d9075d478163fa15b24da424d9948d1d46ae05
Thomas Hartmann <thomas.hartmann@qt.io>
2022-05-24 16:54:52 +0200

    QmlDesigner: Add QtQuick 6.3 as supported version
    Change-Id: Icbb1d2a3c7584cc24f0d8dde467d07678925711c
    Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

--- src/plugins/qmldesigner/designercore/model/texttomodelmerger.cpp
+++ src/plugins/qmldesigner/designercore/model/texttomodelmerger.cpp
@@ -89,25 +89,9 @@ bool isSupportedAttachedProperties(const QString &propertyName)
 
 QStringList supportedVersionsList()
 {
-    static const QStringList list = {"2.0",
-                                     "2.1",
-                                     "2.2",
-                                     "2.3",
-                                     "2.4",
-                                     "2.5",
-                                     "2.6",
-                                     "2.7",
-                                     "2.8",
-                                     "2.9",
-                                     "2.10",
-                                     "2.11",
-                                     "2.12",
-                                     "2.13",
-                                     "2.14",
-                                     "2.15",
-                                     "6.0",
-                                     "6.1",
-                                     "6.2"};
+    static const QStringList list = {"2.0",  "2.1",  "2.2", "2.3",  "2.4",  "2.5",  "2.6",
+                                     "2.7",  "2.8",  "2.9", "2.10", "2.11", "2.12", "2.13",
+                                     "2.14", "2.15", "6.0", "6.1",  "6.2",  "6.3"};
     return list;
 }
 
