ee98c98676d96bbd633a92c976077adc129b82a1
Cristian Adam <cristian.adam@qt.io>
2022-05-24 12:43:57 +0200

    CMake: Remove libclang references
    Now that libclang is no longer used, make sure that all references
    to libclang are gone.
    
    Change-Id: I4b66eff992b22adb6a9c25d34053f2b36079583c
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- cmake/FindClang.cmake
+++ cmake/FindClang.cmake
@@ -1,20 +1,11 @@
 find_package(Clang CONFIG)
 
-# silence a lot of warnings from building against llvm
-if(MSVC AND TARGET libclang)
-    target_compile_options(libclang INTERFACE /wd4267)
-endif()
-
 option(CLANGTOOLING_LINK_CLANG_DYLIB "Force linking of Clang tooling against clang-cpp" NO)
 
-if (TARGET clangTooling AND NOT CLANGTOOLING_LINK_CLANG_DYLIB)
-  set(CLANG_TOOLING_LIBS libclang clangTooling clangQuery clangIndex)
-  set(CLANG_FORMAT_LIB clangFormat)
-elseif (TARGET clang-cpp)
-  set(CLANG_TOOLING_LIBS libclang clang-cpp)
+set(CLANG_FORMAT_LIB clangFormat)
+if (TARGET clang-cpp AND CLANGTOOLING_LINK_CLANG_DYLIB)
   set(CLANG_FORMAT_LIB clang-cpp)
 endif()
-
 
 if (WIN32 AND TARGET clangTooling)
   # check that we can compile against clangTooling
