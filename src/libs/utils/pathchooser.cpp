qt-creator/qt-creator

commit 8dacb123e0e7194b507e609c1016f8bcc3aaceaa

Utils: Pass MacroExpander to ArgumentsAspect constructor
In the past we wanted to keep the aspect constructor simple but
it turned out that exceptions were needed and accumulating, so those
are likely here to stay.

By passing also the MacroExpander to the ArgumentsAspect constructor
allows other single-purpose warts like the ProjectConfiguration::doPostInit()
machinery can be removed.

Change-Id: I148b0ca1ab0740270eecd0d3134620de65a86d4f
Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

src/plugins/baremetal/baremetalrunconfiguration.cpp
@@ -45,14 +45,14 @@ class BareMetalRunConfiguration final : public RunConfiguration
     Q_DECLARE_TR_FUNCTIONS(BareMetal::Internal::BareMetalRunConfiguration)
 
 public:
-    explicit BareMetalRunConfiguration(Target *target, Utils::Id id)
+    explicit BareMetalRunConfiguration(Target *target, Id id)
         : RunConfiguration(target, id)
     {
         const auto exeAspect = addAspect<ExecutableAspect>(target);
         exeAspect->setDisplayStyle(StringAspect::LabelDisplay);
         exeAspect->setPlaceHolderText(tr("Unknown"));
 
-        addAspect<ArgumentsAspect>();
+        addAspect<ArgumentsAspect>(macroExpander());
         addAspect<WorkingDirectoryAspect>(nullptr);
 
         setUpdater([this, exeAspect] {
@@ -79,7 +79,7 @@ public:
         exeAspect->setHistoryCompleter("BareMetal.CustomRunConfig.History");
         exeAspect->setExpectedKind(PathChooser::Any);
 
-        addAspect<ArgumentsAspect>();
+        addAspect<ArgumentsAspect>(macroExpander());
         addAspect<WorkingDirectoryAspect>(nullptr);
 
         setDefaultDisplayName(RunConfigurationFactory::decoratedTargetName(tr("Custom Executable"), target));

src/plugins/boot2qt/qdbrunconfiguration.cpp
@@ -56,9 +56,9 @@ public:
         auto exeAspect = rc->aspect<ExecutableAspect>();
         auto argumentsAspect = rc->aspect<ArgumentsAspect>();
 
-        auto updateCommandLine = [this, rc, exeAspect, argumentsAspect] {
+        auto updateCommandLine = [this, exeAspect, argumentsAspect] {
             const QString usedExecutable = exeAspect->executable().toString();
-            const QString args = argumentsAspect->arguments(rc->macroExpander());
+            const QString args = argumentsAspect->arguments();
             setValue(QString(Constants::AppcontrollerFilepath)
                      + ' ' + usedExecutable + ' ' + args);
         };
@@ -101,7 +101,7 @@ QdbRunConfiguration::QdbRunConfiguration(Target *target, Id id)
 
     auto envAspect = addAspect<RemoteLinux::RemoteLinuxEnvironmentAspect>(target);
 
-    addAspect<ArgumentsAspect>();
+    addAspect<ArgumentsAspect>(macroExpander());
     addAspect<WorkingDirectoryAspect>(envAspect);
     addAspect<FullCommandLineAspect>(this);
 

src/plugins/mesonprojectmanager/project/mesonrunconfiguration.cpp
@@ -45,7 +45,7 @@ MesonRunConfiguration::MesonRunConfiguration(ProjectExplorer::Target *target, Ut
     auto envAspect = addAspect<ProjectExplorer::LocalEnvironmentAspect>(target);
 
     addAspect<ProjectExplorer::ExecutableAspect>(target);
-    addAspect<ProjectExplorer::ArgumentsAspect>();
+    addAspect<ProjectExplorer::ArgumentsAspect>(macroExpander());
     addAspect<ProjectExplorer::WorkingDirectoryAspect>(envAspect);
     addAspect<ProjectExplorer::TerminalAspect>();
 

src/plugins/nim/project/nimblerunconfiguration.cpp
@@ -53,7 +53,7 @@ public:
     {
         auto envAspect = addAspect<LocalEnvironmentAspect>(target);
         addAspect<ExecutableAspect>(target);
-        addAspect<ArgumentsAspect>();
+        addAspect<ArgumentsAspect>(macroExpander());
         addAspect<WorkingDirectoryAspect>(envAspect);
         addAspect<TerminalAspect>();
 
@@ -90,7 +90,7 @@ public:
         : RunConfiguration(target, id)
     {
         addAspect<ExecutableAspect>(target)->setExecutable(Nim::nimblePathFromKit(target->kit()));
-        addAspect<ArgumentsAspect>()->setArguments("test");
+        addAspect<ArgumentsAspect>(macroExpander())->setArguments("test");
         addAspect<WorkingDirectoryAspect>(nullptr)->setDefaultWorkingDirectory(project()->projectDirectory());
         addAspect<TerminalAspect>();
 

src/plugins/nim/project/nimrunconfiguration.cpp
@@ -51,7 +51,7 @@ public:
     {
         auto envAspect = addAspect<LocalEnvironmentAspect>(target);
         addAspect<ExecutableAspect>(target);
-        addAspect<ArgumentsAspect>();
+        addAspect<ArgumentsAspect>(macroExpander());
         addAspect<WorkingDirectoryAspect>(envAspect);
         addAspect<TerminalAspect>();
 

src/plugins/projectexplorer/customexecutablerunconfiguration.cpp
@@ -53,7 +53,7 @@ CustomExecutableRunConfiguration::CustomExecutableRunConfiguration(Target *targe
     exeAspect->setExpectedKind(PathChooser::ExistingCommand);
     exeAspect->setEnvironmentChange(EnvironmentChange::fromFixedEnvironment(envAspect->environment()));
 
-    addAspect<ArgumentsAspect>();
+    addAspect<ArgumentsAspect>(macroExpander());
     addAspect<WorkingDirectoryAspect>(envAspect);
     addAspect<TerminalAspect>();
 

src/plugins/projectexplorer/desktoprunconfiguration.cpp
@@ -69,7 +69,7 @@ DesktopRunConfiguration::DesktopRunConfiguration(Target *target, Id id, Kind kin
     auto envAspect = addAspect<LocalEnvironmentAspect>(target);
 
     addAspect<ExecutableAspect>(target);
-    addAspect<ArgumentsAspect>();
+    addAspect<ArgumentsAspect>(macroExpander());
     addAspect<WorkingDirectoryAspect>(envAspect);
     addAspect<TerminalAspect>();
 

src/plugins/projectexplorer/runconfigurationaspects.cpp
@@ -298,11 +298,6 @@ void WorkingDirectoryAspect::setDefaultWorkingDirectory(const FilePath &defaultW
     }
 }
 
-void WorkingDirectoryAspect::setMacroExpander(MacroExpander *macroExpander)
-{
-    m_macroExpander = macroExpander;
-}
-
 /*!
     \internal
 */
@@ -320,7 +315,8 @@ PathChooser *WorkingDirectoryAspect::pathChooser() const
     arguments for an executable.
 */
 
-ArgumentsAspect::ArgumentsAspect()
+ArgumentsAspect::ArgumentsAspect(const MacroExpander *macroExpander)
+    : m_macroExpander(macroExpander)
 {
     setDisplayName(tr("Arguments"));
     setId("ArgumentsAspect");
@@ -336,14 +332,14 @@ ArgumentsAspect::ArgumentsAspect()
 
     Macros in the value are expanded using \a expander.
 */
-QString ArgumentsAspect::arguments(const MacroExpander *expander) const
+QString ArgumentsAspect::arguments() const
 {
-    QTC_ASSERT(expander, return m_arguments);
+    QTC_ASSERT(m_macroExpander, return m_arguments);
     if (m_currentlyExpanding)
         return m_arguments;
 
     m_currentlyExpanding = true;
-    const QString expanded = expander->expandProcessArgs(m_arguments);
+    const QString expanded = m_macroExpander->expandProcessArgs(m_arguments);
     m_currentlyExpanding = false;
     return expanded;
 }

commit 2837f4d7772cc3edc6dff2efee4bc06eb47b0bfb

ProjectExplorer: Pass macroexpander (again) to WorkingDirectoryAspect
Broke with 8dacb123e0e71.

Probably calls for a more centralized approach again. Later.

Change-Id: Ibf52487d68d7938d4b5dfb38f1f0a5b0e1a97bc3
Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
Reviewed-by: Christian Stenger <christian.stenger@qt.io>

src/libs/utils/pathchooser.cpp
@@ -192,7 +192,7 @@ public:
     EnvironmentChange m_environmentChange;
     BinaryVersionToolTipEventFilter *m_binaryVersionToolTipEventFilter = nullptr;
     QList<QAbstractButton *> m_buttons;
-    MacroExpander *m_macroExpander = globalMacroExpander();
+    const MacroExpander *m_macroExpander = globalMacroExpander();
     std::function<void()> m_openTerminal;
 };
 
@@ -732,7 +732,7 @@ void PathChooser::setHistoryCompleter(const QString &historyKey, bool restoreLas
     d->m_lineEdit->setHistoryCompleter(historyKey, restoreLastItemFromHistory);
 }
 
-void PathChooser::setMacroExpander(MacroExpander *macroExpander)
+void PathChooser::setMacroExpander(const MacroExpander *macroExpander)
 {
     d->m_macroExpander = macroExpander;
 }

src/libs/utils/pathchooser.h
@@ -136,7 +136,7 @@ public:
     // Sets a macro expander that is used when producing path and fileName.
     // By default, the global expander is used.
     // nullptr can be passed to disable macro expansion.
-    void setMacroExpander(MacroExpander *macroExpander);
+    void setMacroExpander(const MacroExpander *macroExpander);
 
     bool isReadOnly() const;
     void setReadOnly(bool b);

src/plugins/baremetal/baremetalrunconfiguration.cpp
@@ -53,7 +53,7 @@ public:
         exeAspect->setPlaceHolderText(tr("Unknown"));
 
         addAspect<ArgumentsAspect>(macroExpander());
-        addAspect<WorkingDirectoryAspect>(nullptr);
+        addAspect<WorkingDirectoryAspect>(macroExpander(), nullptr);
 
         setUpdater([this, exeAspect] {
             const BuildTargetInfo bti = buildTargetInfo();
@@ -80,7 +80,7 @@ public:
         exeAspect->setExpectedKind(PathChooser::Any);
 
         addAspect<ArgumentsAspect>(macroExpander());
-        addAspect<WorkingDirectoryAspect>(nullptr);
+        addAspect<WorkingDirectoryAspect>(macroExpander(), nullptr);
 
         setDefaultDisplayName(RunConfigurationFactory::decoratedTargetName(tr("Custom Executable"), target));
     }

src/plugins/boot2qt/qdbrunconfiguration.cpp
@@ -102,7 +102,7 @@ QdbRunConfiguration::QdbRunConfiguration(Target *target, Id id)
     auto envAspect = addAspect<RemoteLinux::RemoteLinuxEnvironmentAspect>(target);
 
     addAspect<ArgumentsAspect>(macroExpander());
-    addAspect<WorkingDirectoryAspect>(envAspect);
+    addAspect<WorkingDirectoryAspect>(macroExpander(), envAspect);
     addAspect<FullCommandLineAspect>(this);
 
     setUpdater([this, target, exeAspect, symbolsAspect] {

src/plugins/mesonprojectmanager/project/mesonrunconfiguration.cpp
@@ -46,7 +46,7 @@ MesonRunConfiguration::MesonRunConfiguration(ProjectExplorer::Target *target, Ut
 
     addAspect<ProjectExplorer::ExecutableAspect>(target);
     addAspect<ProjectExplorer::ArgumentsAspect>(macroExpander());
-    addAspect<ProjectExplorer::WorkingDirectoryAspect>(envAspect);
+    addAspect<ProjectExplorer::WorkingDirectoryAspect>(macroExpander(), envAspect);
     addAspect<ProjectExplorer::TerminalAspect>();
 
     auto libAspect = addAspect<ProjectExplorer::UseLibraryPathsAspect>();

src/plugins/nim/project/nimblerunconfiguration.cpp
@@ -54,7 +54,7 @@ public:
         auto envAspect = addAspect<LocalEnvironmentAspect>(target);
         addAspect<ExecutableAspect>(target);
         addAspect<ArgumentsAspect>(macroExpander());
-        addAspect<WorkingDirectoryAspect>(envAspect);
+        addAspect<WorkingDirectoryAspect>(macroExpander(), envAspect);
         addAspect<TerminalAspect>();
 
         setUpdater([this] {
@@ -91,7 +91,8 @@ public:
     {
         addAspect<ExecutableAspect>(target)->setExecutable(Nim::nimblePathFromKit(target->kit()));
         addAspect<ArgumentsAspect>(macroExpander())->setArguments("test");
-        addAspect<WorkingDirectoryAspect>(nullptr)->setDefaultWorkingDirectory(project()->projectDirectory());
+        addAspect<WorkingDirectoryAspect>(macroExpander(), nullptr)
+                ->setDefaultWorkingDirectory(project()->projectDirectory());
         addAspect<TerminalAspect>();
 
         setDisplayName(tr("Nimble Test"));

src/plugins/nim/project/nimrunconfiguration.cpp
@@ -52,7 +52,7 @@ public:
         auto envAspect = addAspect<LocalEnvironmentAspect>(target);
         addAspect<ExecutableAspect>(target);
         addAspect<ArgumentsAspect>(macroExpander());
-        addAspect<WorkingDirectoryAspect>(envAspect);
+        addAspect<WorkingDirectoryAspect>(macroExpander(), envAspect);
         addAspect<TerminalAspect>();
 
         setDisplayName(tr("Current Build Target"));

src/plugins/projectexplorer/customexecutablerunconfiguration.cpp
@@ -53,7 +53,7 @@ CustomExecutableRunConfiguration::CustomExecutableRunConfiguration(Target *targe
     exeAspect->setEnvironmentChange(EnvironmentChange::fromFixedEnvironment(envAspect->environment()));
 
     addAspect<ArgumentsAspect>(macroExpander());
-    addAspect<WorkingDirectoryAspect>(envAspect);
+    addAspect<WorkingDirectoryAspect>(macroExpander(), envAspect);
     addAspect<TerminalAspect>();
 
     connect(envAspect, &EnvironmentAspect::environmentChanged, this, [exeAspect, envAspect]  {

src/plugins/projectexplorer/desktoprunconfiguration.cpp
@@ -70,7 +70,7 @@ DesktopRunConfiguration::DesktopRunConfiguration(Target *target, Id id, Kind kin
 
     addAspect<ExecutableAspect>(target);
     addAspect<ArgumentsAspect>(macroExpander());
-    addAspect<WorkingDirectoryAspect>(envAspect);
+    addAspect<WorkingDirectoryAspect>(macroExpander(), envAspect);
     addAspect<TerminalAspect>();
 
     auto libAspect = addAspect<UseLibraryPathsAspect>();

src/plugins/projectexplorer/runconfigurationaspects.cpp
@@ -171,8 +171,9 @@ bool TerminalAspect::isUserSet() const
     working directory for running the executable.
 */
 
-WorkingDirectoryAspect::WorkingDirectoryAspect(EnvironmentAspect *envAspect)
-    : m_envAspect(envAspect)
+WorkingDirectoryAspect::WorkingDirectoryAspect(const MacroExpander *expander,
+                                               EnvironmentAspect *envAspect)
+    : m_envAspect(envAspect), m_macroExpander(expander)
 {
     setDisplayName(tr("Working Directory"));
     setId("WorkingDirectoryAspect");

src/plugins/projectexplorer/runconfigurationaspects.h
@@ -81,7 +81,8 @@ class PROJECTEXPLORER_EXPORT WorkingDirectoryAspect : public Utils::BaseAspect
     Q_OBJECT
 
 public:
-    explicit WorkingDirectoryAspect(EnvironmentAspect *envAspect);
+    explicit WorkingDirectoryAspect(const Utils::MacroExpander *expander,
+                                    EnvironmentAspect *envAspect);
 
     void addToLayout(Utils::LayoutBuilder &builder) override;
 
@@ -102,7 +103,7 @@ private:
     Utils::FilePath m_defaultWorkingDirectory;
     QPointer<Utils::PathChooser> m_chooser;
     QPointer<QToolButton> m_resetButton;
-    Utils::MacroExpander *m_macroExpander = nullptr;
+    const Utils::MacroExpander *m_macroExpander = nullptr;
 };
 
 class PROJECTEXPLORER_EXPORT ArgumentsAspect : public Utils::BaseAspect

src/plugins/python/pythonrunconfiguration.cpp
@@ -178,7 +178,7 @@ public:
 
         auto argumentsAspect = addAspect<ArgumentsAspect>(macroExpander());
 
-        addAspect<WorkingDirectoryAspect>(nullptr);
+        addAspect<WorkingDirectoryAspect>(macroExpander(), nullptr);
         addAspect<TerminalAspect>();
 
         setCommandLineGetter([bufferedAspect, interpreterAspect, argumentsAspect, scriptAspect] {

src/plugins/qnx/qnxrunconfiguration.cpp
@@ -61,7 +61,7 @@ QnxRunConfiguration::QnxRunConfiguration(Target *target, Id id)
     auto envAspect = addAspect<RemoteLinuxEnvironmentAspect>(target);
 
     addAspect<ArgumentsAspect>(macroExpander());
-    addAspect<WorkingDirectoryAspect>(envAspect);
+    addAspect<WorkingDirectoryAspect>(macroExpander(), envAspect);
     addAspect<TerminalAspect>();
 
     auto libAspect = addAspect<StringAspect>();

src/plugins/remotelinux/remotelinuxcustomrunconfiguration.cpp
@@ -72,7 +72,7 @@ RemoteLinuxCustomRunConfiguration::RemoteLinuxCustomRunConfiguration(Target *tar
     symbolsAspect->setDisplayStyle(SymbolFileAspect::PathChooserDisplay);
 
     addAspect<ArgumentsAspect>(macroExpander());
-    addAspect<WorkingDirectoryAspect>(envAspect);
+    addAspect<WorkingDirectoryAspect>(macroExpander(), envAspect);
     if (HostOsInfo::isAnyUnixHost())
         addAspect<TerminalAspect>();
     if (HostOsInfo::isAnyUnixHost())

src/plugins/remotelinux/remotelinuxrunconfiguration.cpp
@@ -71,7 +71,7 @@ RemoteLinuxRunConfiguration::RemoteLinuxRunConfiguration(Target *target, Id id)
     symbolsAspect->setDisplayStyle(SymbolFileAspect::LabelDisplay);
 
     addAspect<ArgumentsAspect>(macroExpander());
-    addAspect<WorkingDirectoryAspect>(envAspect);
+    addAspect<WorkingDirectoryAspect>(macroExpander(), envAspect);
     if (HostOsInfo::isAnyUnixHost())
         addAspect<TerminalAspect>();
     if (HostOsInfo::isAnyUnixHost())