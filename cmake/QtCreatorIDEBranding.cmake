5f4806632826e9e7c9c47efd8dbe937cd9d24462
Eike Ziller <eike.ziller@qt.io>
2022-06-17 13:03:16 +0200

    Bump version to 8.0.0-beta2
    Change-Id: I02a09e916d11c7ce28b2d58ba7517e582e14be06
    Reviewed-by: David Schulz <david.schulz@qt.io>

--- cmake/QtCreatorIDEBranding.cmake
+++ cmake/QtCreatorIDEBranding.cmake
@@ -1,6 +1,6 @@
-set(IDE_VERSION "7.82.0")                             # The IDE version.
-set(IDE_VERSION_COMPAT "7.82.0")                      # The IDE Compatibility version.
-set(IDE_VERSION_DISPLAY "8.0.0-beta1")                # The IDE display version.
+set(IDE_VERSION "7.83.0")                             # The IDE version.
+set(IDE_VERSION_COMPAT "7.83.0")                      # The IDE Compatibility version.
+set(IDE_VERSION_DISPLAY "8.0.0-beta2")                # The IDE display version.
 set(IDE_COPYRIGHT_YEAR "2022")                        # The IDE current copyright year.
 
 set(IDE_SETTINGSVARIANT "QtProject")                  # The IDE settings variation.
