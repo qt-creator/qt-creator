146f59e808bf0c60c0a373e6e569b08589d11d0f
BogDan Vatra <bogdan@kde.org>
2022-06-23 10:29:22 +0300

    Set solibSearchPath to LLDB
    Change-Id: Iedfc3850b568dc336eb3df309b2b36ab25d1aa20
    Reviewed-by: hjk <hjk@qt.io>

--- src/plugins/debugger/lldb/lldbengine.cpp
+++ src/plugins/debugger/lldb/lldbengine.cpp
@@ -278,6 +278,9 @@ void LldbEngine::handleLldbStarted()
                     "settings append target.source-map " + it.key() + ' ' + expand(it.value()));
     }
 
+    for (const QString &path : rp.solibSearchPath)
+        executeDebuggerCommand("settings append target.exec-search-paths " + path);
+
     DebuggerCommand cmd2("setupInferior");
     cmd2.arg("executable", rp.inferior.command.executable().toString());
     cmd2.arg("breakonmain", rp.breakOnMain);
