ae2e57b1678a061366a19d8a209b64f4ab8fa582
Eike Ziller <eike.ziller@qt.io>
2022-06-24 12:02:45 +0200

    Ignore .json.in and .svg files for translations
    Amends 30bb32d9aff0754f76cb994026f43271cbd1a2c6
    which added the .json.in files to the list of target sources.
    
    Change-Id: I7282cfcda50cbd156403a23cce06baed9698bb4d
    Reviewed-by: hjk <hjk@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

--- cmake/QtCreatorTranslations.cmake
+++ cmake/QtCreatorTranslations.cmake
@@ -27,6 +27,7 @@ function(_extract_ts_data_from_targets outprefix)
 
         set(_target_sources "")
         if(_source_files)
+          list(FILTER _source_files EXCLUDE REGEX ".*[.]json[.]in|.*[.]svg")
           list(APPEND _target_sources ${_source_files})
         endif()
         if(_extra_translations)
