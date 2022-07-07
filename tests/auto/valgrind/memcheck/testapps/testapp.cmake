c37e69ee2c754d386deb725c2134cae88114df81
Marco Bubke <marco.bubke@qt.io>
2022-07-07 18:47:33 +0200

    Valgrind: Disable warnings
    Change-Id: I76c2833828b52b24886e148ff37a07ef87a38336
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>

--- tests/auto/valgrind/memcheck/testapps/testapp.cmake
+++ tests/auto/valgrind/memcheck/testapps/testapp.cmake
@@ -1,4 +1,6 @@
 function(add_valgrind_testapp name)
   add_executable("${name}"
                  ${CMAKE_CURRENT_LIST_DIR}/main.cpp)
+  set_target_properties(${name} PROPERTIES QT_COMPILE_OPTIONS_DISABLE_WARNINGS ON)
+
 endfunction()
