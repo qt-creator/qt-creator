qt-creator/qt-creator

commit 1548eef10b8987142bf1cafa27278e5b816a11b7

Utils: Inline filewizardpage.ui
Change-Id: I23235349babfb5a6b97679fedfc6db279dc472a2
Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>
Reviewed-by: <github-actions-qt-creator@cristianadam.eu>

src/libs/utils/filewizardpage.cpp
@@ -24,7 +24,6 @@
 ****************************************************************************/
 
 #include "filewizardpage.h"
-#include "ui_filewizardpage.h"
 
 #include "wizard.h" // TODO: only because of SHORT_TITLE_PROPERTY
 
@@ -39,35 +38,63 @@
   and "Path:".
 */
 
+#include <utils/filenamevalidatinglineedit.h>
+#include <utils/layoutbuilder.h>
+#include <utils/pathchooser.h>
+
+#include <QApplication>
+#include <QFormLayout>
+#include <QLabel>
+
 namespace Utils {
 
 class FileWizardPagePrivate
 {
 public:
     FileWizardPagePrivate() = default;
-    Ui::WizardPage m_ui;
     bool m_complete = false;
+
+    QLabel *m_defaultSuffixLabel;
+    QLabel *m_nameLabel;
+    FileNameValidatingLineEdit *m_nameLineEdit;
+    QLabel *m_pathLabel;
+    PathChooser *m_pathChooser;
 };
 
 FileWizardPage::FileWizardPage(QWidget *parent) :
     WizardPage(parent),
     d(new FileWizardPagePrivate)
 {
-    d->m_ui.setupUi(this);
-    connect(d->m_ui.pathChooser, &PathChooser::validChanged,
+    setTitle(tr("Choose the Location"));
+    resize(368, 102);
+
+    d->m_defaultSuffixLabel = new QLabel;
+    d->m_nameLineEdit = new FileNameValidatingLineEdit;
+    d->m_pathChooser = new PathChooser;
+
+    using namespace Layouting;
+    const Break br;
+
+    Form {
+        Space(), d->m_defaultSuffixLabel, br,
+        tr("File name:"), d->m_nameLineEdit, br,
+        tr("Path:"), d->m_pathChooser
+    }.attachTo(this);
+
+    connect(d->m_pathChooser, &PathChooser::validChanged,
             this, &FileWizardPage::slotValidChanged);
-    connect(d->m_ui.nameLineEdit, &FancyLineEdit::validChanged,
+    connect(d->m_nameLineEdit, &FancyLineEdit::validChanged,
             this, &FileWizardPage::slotValidChanged);
 
-    connect(d->m_ui.pathChooser, &PathChooser::returnPressed,
+    connect(d->m_pathChooser, &PathChooser::returnPressed,
             this, &FileWizardPage::slotActivated);
-    connect(d->m_ui.nameLineEdit, &FancyLineEdit::validReturnPressed,
+    connect(d->m_nameLineEdit, &FancyLineEdit::validReturnPressed,
             this, &FileWizardPage::slotActivated);
 
     setProperty(SHORT_TITLE_PROPERTY, tr("Location"));
 
-    registerFieldWithName(QLatin1String("Path"), d->m_ui.pathChooser, "path", SIGNAL(pathChanged(QString)));
-    registerFieldWithName(QLatin1String("FileName"), d->m_ui.nameLineEdit);
+    registerFieldWithName(QLatin1String("Path"), d->m_pathChooser, "path", SIGNAL(pathChanged(QString)));
+    registerFieldWithName(QLatin1String("FileName"), d->m_nameLineEdit);
 }
 
 FileWizardPage::~FileWizardPage()
@@ -77,37 +104,37 @@ FileWizardPage::~FileWizardPage()
 
 QString FileWizardPage::fileName() const
 {
-    return d->m_ui.nameLineEdit->text();
+    return d->m_nameLineEdit->text();
 }
 
 FilePath FileWizardPage::filePath() const
 {
-    return d->m_ui.pathChooser->filePath();
+    return d->m_pathChooser->filePath();
 }
 
 void FileWizardPage::setFilePath(const FilePath &filePath)
 {
-    d->m_ui.pathChooser->setFilePath(filePath);
+    d->m_pathChooser->setFilePath(filePath);
 }
 
 QString FileWizardPage::path() const
 {
-    return d->m_ui.pathChooser->filePath().toString();
+    return d->m_pathChooser->filePath().toString();
 }
 
 void FileWizardPage::setPath(const QString &path)
 {
-    d->m_ui.pathChooser->setFilePath(FilePath::fromString(path));
+    d->m_pathChooser->setFilePath(FilePath::fromString(path));
 }
 
 void FileWizardPage::setFileName(const QString &name)
 {
-    d->m_ui.nameLineEdit->setText(name);
+    d->m_nameLineEdit->setText(name);
 }
 
 void FileWizardPage::setAllowDirectoriesInFileSelector(bool allow)
 {
-    d->m_ui.nameLineEdit->setAllowDirectories(allow);
+    d->m_nameLineEdit->setAllowDirectories(allow);
 }
 
 bool FileWizardPage::isComplete() const
@@ -117,12 +144,12 @@ bool FileWizardPage::isComplete() const
 
 void FileWizardPage::setFileNameLabel(const QString &label)
 {
-    d->m_ui.nameLabel->setText(label);
+    d->m_nameLabel->setText(label);
 }
 
 void FileWizardPage::setPathLabel(const QString &label)
 {
-    d->m_ui.pathLabel->setText(label);
+    d->m_pathLabel->setText(label);
 }
 
 void FileWizardPage::setDefaultSuffix(const QString &suffix)
@@ -132,7 +159,7 @@ void FileWizardPage::setDefaultSuffix(const QString &suffix)
         if (layout->rowCount() == 3)
             layout->removeRow(0);
     } else {
-        d->m_ui.defaultSuffixLabel->setText(
+        d->m_defaultSuffixLabel->setText(
             tr("The default suffix if you do not explicitly specify a file extension is \".%1\".")
                 .arg(suffix));
     }
@@ -140,17 +167,17 @@ void FileWizardPage::setDefaultSuffix(const QString &suffix)
 
 bool FileWizardPage::forceFirstCapitalLetterForFileName() const
 {
-    return d->m_ui.nameLineEdit->forceFirstCapitalLetter();
+    return d->m_nameLineEdit->forceFirstCapitalLetter();
 }
 
 void FileWizardPage::setForceFirstCapitalLetterForFileName(bool b)
 {
-    d->m_ui.nameLineEdit->setForceFirstCapitalLetter(b);
+    d->m_nameLineEdit->setForceFirstCapitalLetter(b);
 }
 
 void FileWizardPage::slotValidChanged()
 {
-    const bool newComplete = d->m_ui.pathChooser->isValid() && d->m_ui.nameLineEdit->isValid();
+    const bool newComplete = d->m_pathChooser->isValid() && d->m_nameLineEdit->isValid();
     if (newComplete != d->m_complete) {
         d->m_complete = newComplete;
         emit completeChanged();

src/libs/utils/filewizardpage.h
@@ -33,8 +33,6 @@ namespace Utils {
 
 class FilePath;
 
-class FileWizardPagePrivate;
-
 class QTCREATOR_UTILS_EXPORT FileWizardPage : public WizardPage
 {
     Q_OBJECT
@@ -76,7 +74,7 @@ private:
     void slotValidChanged();
     void slotActivated();
 
-    FileWizardPagePrivate *d;
+    class FileWizardPagePrivate *d;
 };
 
 } // namespace Utils

src/libs/utils/filewizardpage.ui
@@ -1,61 +1,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<ui version="4.0">
- <class>Utils::WizardPage</class>
- <widget class="QWizardPage" name="Utils::WizardPage">
-  <property name="geometry">
-   <rect>
-    <x>0</x>
-    <y>0</y>
-    <width>368</width>
-    <height>102</height>
-   </rect>
-  </property>
-  <property name="title">
-   <string>Choose the Location</string>
-  </property>
-  <layout class="QFormLayout" name="formLayout">
-   <item row="1" column="0">
-    <widget class="QLabel" name="nameLabel">
-     <property name="text">
-      <string>File name:</string>
-     </property>
-    </widget>
-   </item>
-   <item row="1" column="1">
-    <widget class="Utils::FileNameValidatingLineEdit" name="nameLineEdit"/>
-   </item>
-   <item row="3" column="0">
-    <widget class="QLabel" name="pathLabel">
-     <property name="text">
-      <string>Path:</string>
-     </property>
-    </widget>
-   </item>
-   <item row="3" column="1">
-    <widget class="Utils::PathChooser" name="pathChooser" native="true"/>
-   </item>
-   <item row="0" column="1">
-    <widget class="QLabel" name="defaultSuffixLabel">
-     <property name="text">
-      <string/>
-     </property>
-    </widget>
-   </item>
-  </layout>
- </widget>
- <customwidgets>
-  <customwidget>
-   <class>Utils::FileNameValidatingLineEdit</class>
-   <extends>QLineEdit</extends>
-   <header location="global">utils/filenamevalidatinglineedit.h</header>
-  </customwidget>
-  <customwidget>
-   <class>Utils::PathChooser</class>
-   <extends>QWidget</extends>
-   <header location="global">utils/pathchooser.h</header>
-   <container>1</container>
-  </customwidget>
- </customwidgets>
- <resources/>
- <connections/>
-</ui>

src/libs/utils/utils.qbs
@@ -128,7 +128,6 @@ Project {
             "fileutils.h",
             "filewizardpage.cpp",
             "filewizardpage.h",
-            "filewizardpage.ui",
             "fixedsizeclicklabel.cpp",
             "fixedsizeclicklabel.h",
             "flowlayout.cpp",

commit 21d0502909a7f0e510f499a12ec4938470d8ae72

GenericProjectManager: fix project creation crash
Mitigration for regression introduced by
1548eef10b8987142bf1cafa27278e5b816a11b7.

Labels does not created in the FileWizardPage ctor, but
accessed from the GenericProjectManager Project Creation Wizard.

Change-Id: I6c980fb9475c0c78be908ead6693c4c1dfa8b99c
Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
Reviewed-by: hjk <hjk@qt.io>

src/libs/utils/filewizardpage.cpp
@@ -69,15 +69,20 @@ FileWizardPage::FileWizardPage(QWidget *parent) :
     resize(368, 102);
 
     d->m_defaultSuffixLabel = new QLabel;
+    d->m_nameLabel = new QLabel;
     d->m_nameLineEdit = new FileNameValidatingLineEdit;
+    d->m_pathLabel = new QLabel;
     d->m_pathChooser = new PathChooser;
+
+    d->m_nameLabel->setText(tr("File name:"));
+    d->m_pathLabel->setText(tr("Path:"));
 
     using namespace Layouting;
 
     Form {
         empty, d->m_defaultSuffixLabel, br,
-        tr("File name:"), d->m_nameLineEdit, br,
-        tr("Path:"), d->m_pathChooser
+        d->m_nameLabel, d->m_nameLineEdit, br,
+        d->m_pathLabel, d->m_pathChooser
     }.attachTo(this);
 
     connect(d->m_pathChooser, &PathChooser::validChanged,