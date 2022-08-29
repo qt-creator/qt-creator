607900ae1702c8c15dea5ea1805a684bf4b4c65c
Christian Kandeler <christian.kandeler@qt.io>
2022-08-29 11:18:05 +0200

    ClangCodeModel: Fix tests
    Amends a7956df3ca45579c62b496621a7d3b55d73c98b.
    
    Change-Id: I7cbbd391ee015c5481e90ebdad356d9c6e916d5e
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/plugins/clangcodemodel/test/data/follow-symbol/header.h
+++ src/plugins/clangcodemodel/test/data/follow-symbol/header.h
@@ -1,5 +1,27 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0+ OR GPL-3.0 WITH Qt-GPL-exception-1.0
+
+
+
+
+
+
+
+
+// This space intentionally left empty
+
+
+
+
+
+
+
+
+
+
+
+
+
 #pragma once
 
 #define TEST_DEFINE 1
