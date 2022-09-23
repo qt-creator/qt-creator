qt-creator/qt-creator

commit d83f9d71cceab84659c7d9d4df381d571c82d0c2

Utils: use native path in FilePath::displayName
Change-Id: Idf82f963cbdee648c53038af478963044b3aa631
Reviewed-by: hjk <hjk@qt.io>

src/libs/utils/filepath.cpp
@@ -770,11 +770,11 @@ QString FilePath::displayName(const QString &args) const
 {
     QString deviceName;
     if (needsDevice()) {
-        QTC_ASSERT(s_deviceHooks.deviceDisplayName, return path());
+        QTC_ASSERT(s_deviceHooks.deviceDisplayName, return nativePath());
         deviceName = s_deviceHooks.deviceDisplayName(*this);
     }
 
-    const QString fullPath = path();
+    const QString fullPath = nativePath();
 
     if (args.isEmpty()) {
         if (deviceName.isEmpty())

commit 805e536df6ccf0d2ea7af2a3de3a136b560e714c

Utils: Fix result of fileNameWithPathComponents()
Combination of bf8bf1adc0784f8deb43 and d83f9d71cceab846 broke
the resulting string.
Fixes tst_fileutils file name tests.

Change-Id: I69c485e52bbf76093b903b407ab2ff2f58d28a0d
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/libs/utils/filepath.cpp
@@ -272,7 +272,7 @@ QString FilePath::fileNameWithPathComponents(int pathComponents) const
         return fullPath.mid(component);
 
     // If there are no more slashes before the found one, return the entire string
-    return displayName();
+    return toString();
 }
 
 /// \returns the base name of the file without the path.