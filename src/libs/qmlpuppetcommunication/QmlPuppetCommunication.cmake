63022d08ea8f9f04a75d301602e639b83b1b7396
Marco Bubke <marco.bubke@qt.io>
2022-11-02 13:58:34 +0100

    QmlDesigner: QmlPuppet as standalone tool
    The qmlpuppet is now a standalone tool. There is new a library too with
    the communication code. That is shared between the designer and the
    puppet. It's in a .cmake file so it can be included by the standalone
    tool if it is not part of a designer build.
    
    Task-number: QDS-5879
    Change-Id: I2bc2a0b463fbb3e0c8c23d182abfd368cf87e968
    Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

---
+++ src/libs/qmlpuppetcommunication/QmlPuppetCommunication.cmake
@@ -0,0 +1,87 @@
+add_qtc_library(QmlPuppetCommunication STATIC
+    PROPERTIES AUTOUIC OFF
+    DEPENDS Qt5::Core Qt5::CorePrivate Qt5::Gui
+)
+
+extend_qtc_library(QmlPuppetCommunication
+  CONDITION TARGET Nanotrace
+  PUBLIC_DEPENDS Nanotrace
+)
+
+extend_qtc_library(QmlPuppetCommunication
+  PUBLIC_INCLUDES ${CMAKE_CURRENT_LIST_DIR}/types
+  SOURCES_PREFIX ${CMAKE_CURRENT_LIST_DIR}/types
+  SOURCES
+    enumeration.h
+)
+
+extend_qtc_library(QmlPuppetCommunication
+  PUBLIC_INCLUDES ${CMAKE_CURRENT_LIST_DIR}/interfaces
+  SOURCES_PREFIX ${CMAKE_CURRENT_LIST_DIR}/interfaces
+  SOURCES
+    commondefines.h
+    nodeinstanceclientinterface.h
+    nodeinstanceserverinterface.cpp nodeinstanceserverinterface.h
+    nodeinstanceglobal.h
+)
+
+extend_qtc_library(QmlPuppetCommunication
+  PUBLIC_INCLUDES ${CMAKE_CURRENT_LIST_DIR}/container
+  SOURCES_PREFIX ${CMAKE_CURRENT_LIST_DIR}/container
+  SOURCES
+    addimportcontainer.cpp addimportcontainer.h
+    idcontainer.cpp idcontainer.h
+    imagecontainer.cpp imagecontainer.h
+    informationcontainer.cpp informationcontainer.h
+    instancecontainer.cpp instancecontainer.h
+    mockuptypecontainer.cpp mockuptypecontainer.h
+    propertyabstractcontainer.cpp propertyabstractcontainer.h
+    propertybindingcontainer.cpp propertybindingcontainer.h
+    propertyvaluecontainer.cpp propertyvaluecontainer.h
+    reparentcontainer.cpp reparentcontainer.h
+    sharedmemory.h
+    $<IF:$<BOOL:${UNIX}>,sharedmemory_unix.cpp,sharedmemory_qt.cpp>
+)
+
+extend_qtc_library(QmlPuppetCommunication
+  PUBLIC_INCLUDES ${CMAKE_CURRENT_LIST_DIR}/commands
+  SOURCES_PREFIX ${CMAKE_CURRENT_LIST_DIR}/commands
+  SOURCES
+    captureddatacommand.h
+    changeauxiliarycommand.cpp changeauxiliarycommand.h
+    changebindingscommand.cpp changebindingscommand.h
+    changefileurlcommand.cpp changefileurlcommand.h
+    changeidscommand.cpp  changeidscommand.h
+    changelanguagecommand.cpp changelanguagecommand.h
+    changenodesourcecommand.cpp changenodesourcecommand.h
+    changepreviewimagesizecommand.cpp changepreviewimagesizecommand.h
+    changeselectioncommand.cpp changeselectioncommand.h
+    changestatecommand.cpp changestatecommand.h
+    changevaluescommand.cpp changevaluescommand.h
+    childrenchangedcommand.cpp childrenchangedcommand.h
+    clearscenecommand.cpp clearscenecommand.h
+    completecomponentcommand.cpp completecomponentcommand.h
+    componentcompletedcommand.cpp componentcompletedcommand.h
+    createinstancescommand.cpp createinstancescommand.h
+    createscenecommand.cpp createscenecommand.h
+    debugoutputcommand.cpp debugoutputcommand.h
+    endpuppetcommand.cpp endpuppetcommand.h
+    informationchangedcommand.cpp informationchangedcommand.h
+    inputeventcommand.cpp inputeventcommand.h
+    nanotracecommand.cpp nanotracecommand.h
+    pixmapchangedcommand.cpp pixmapchangedcommand.h
+    puppetalivecommand.cpp puppetalivecommand.h
+    puppettocreatorcommand.cpp puppettocreatorcommand.h
+    removeinstancescommand.cpp removeinstancescommand.h
+    removepropertiescommand.cpp removepropertiescommand.h
+    removesharedmemorycommand.cpp removesharedmemorycommand.h
+    reparentinstancescommand.cpp reparentinstancescommand.h
+    requestmodelnodepreviewimagecommand.cpp requestmodelnodepreviewimagecommand.h
+    scenecreatedcommand.h
+    statepreviewimagechangedcommand.cpp statepreviewimagechangedcommand.h
+    synchronizecommand.h
+    tokencommand.cpp tokencommand.h
+    update3dviewstatecommand.cpp update3dviewstatecommand.h
+    valueschangedcommand.cpp valueschangedcommand.h
+    view3dactioncommand.cpp view3dactioncommand.h
+)
