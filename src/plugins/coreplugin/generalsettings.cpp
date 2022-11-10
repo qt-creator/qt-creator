7b02984088fb91861911af4b7a24ac354e7f3e2d
Christian Stenger <christian.stenger@qt.io>
2022-11-10 10:45:09 +0100

    Core: Fix handling of displaying shortcuts in context menus
    Change-Id: I19f93780bd2c21a3a54e0a4c1b9db87e2327d5ed
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/plugins/coreplugin/generalsettings.cpp
+++ src/plugins/coreplugin/generalsettings.cpp
@@ -274,7 +274,7 @@ void GeneralSettings::setShowShortcutsInContextMenu(bool show)
     ICore::settings()->setValueWithDefault(settingsKeyShortcutsInContextMenu,
                                            show,
                                            m_defaultShowShortcutsInContextMenu);
-    QGuiApplication::styleHints()->setShowShortcutsInContextMenus(show);
+    QCoreApplication::setAttribute(Qt::AA_DontShowShortcutsInContextMenus, !show);
 }
 
 GeneralSettings::GeneralSettings()
