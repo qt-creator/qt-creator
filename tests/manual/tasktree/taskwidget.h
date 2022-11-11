292e8f510e0b302377e6df43c39d7a214035b072
Jarek Kobus <jaroslaw.kobus@qt.io>
2022-11-11 10:11:22 +0100

    Add manual test for TaskTree
    Change-Id: I92ea4d949729f59d2d1e262ecd38fc3be4f463fe
    Reviewed-by: hjk <hjk@qt.io>
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>

---
+++ tests/manual/tasktree/taskwidget.h
@@ -0,0 +1,78 @@
+// Copyright (C) 2022 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0+ OR GPL-3.0 WITH Qt-GPL-exception-1.0
+
+#include <utils/layoutbuilder.h>
+#include <utils/tasktree.h>
+
+#include <QWidget>
+
+class StateIndicator;
+
+QT_BEGIN_NAMESPACE
+class QCheckBox;
+class QComboBox;
+class QLabel;
+class QSpinBox;
+QT_END_NAMESPACE
+
+enum class State {
+    Initial,
+    Running,
+    Done,
+    Error
+};
+
+class StateWidget : public QWidget
+{
+public:
+    StateWidget();
+
+    void setState(State state);
+
+protected:
+    StateIndicator *m_stateIndicator = nullptr;
+};
+
+class TaskWidget : public StateWidget
+{
+public:
+    TaskWidget();
+
+    void setBusyTime(int seconds);
+    int busyTime() const;
+    void setSuccess(bool success);
+    bool isSuccess() const;
+
+private:
+    QLabel *m_infoLabel = nullptr;
+    QSpinBox *m_spinBox = nullptr;
+    QCheckBox *m_checkBox = nullptr;
+};
+
+class GroupWidget : public StateWidget
+{
+public:
+    GroupWidget();
+
+    void setExecuteMode(Utils::Tasking::ExecuteMode mode);
+    Utils::Tasking::ExecuteMode executeMode() const;
+
+    void setWorkflowPolicy(Utils::Tasking::WorkflowPolicy policy);
+    Utils::Tasking::WorkflowPolicy workflowPolicy() const;
+
+private:
+    void updateExecuteMode();
+    void updateWorkflowPolicy();
+
+    QComboBox *m_executeCombo = nullptr;
+    QComboBox *m_workflowCombo = nullptr;
+
+    Utils::Tasking::ExecuteMode m_executeMode = Utils::Tasking::ExecuteMode::Sequential;
+    Utils::Tasking::WorkflowPolicy m_workflowPolicy = Utils::Tasking::WorkflowPolicy::StopOnError;
+};
+
+class TaskGroup : public Utils::Layouting::Row
+{
+public:
+    TaskGroup(QWidget *group, std::initializer_list<Utils::LayoutBuilder::LayoutItem> items);
+};
