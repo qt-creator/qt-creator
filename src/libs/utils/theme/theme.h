080c8af850cd4b6c93119d25558d55b4f73e375a
Thomas Hartmann <thomas.hartmann@qt.io>
2023-01-31 15:00:28 +0100

    Add DStoolbarBackground
    Change-Id: Ied42d086af2aba2ce54b2ad9ed411feea7817416
    Reviewed-by: Burak Hancerli <burak.hancerli@qt.io>
    Reviewed-by: Mahmoud Badri <mahmoud.badri@qt.io>

--- src/libs/utils/theme/theme.h
+++ src/libs/utils/theme/theme.h
@@ -305,6 +305,7 @@ public:
         DStextSelectionColor,
         DStextSelectedTextColor,
 
+        DStoolbarBackground,
         DSplaceholderTextColor,
         DSplaceholderTextColorInteraction,
 
