b6bfa5bd1f1e22ec392ed0520a20d7b514e04f3f
Ali Kianian <ali.kianian@qt.io>
2023-02-16 19:07:38 +0200

    QmlDesigner: Clean up curve editor toolbar
    - Slider size is adjusted
    - Zoom actions are available
    - Spinbox controls and labels are aligned to right
    - Control styles are modified
    
    find better style for the controls
    
    Task-number: QDS-9079
    Change-Id: I3fa376102d71ea6f904e72de1e6cc2f50ad1c8b6
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
    Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

---
+++ src/plugins/qmldesigner/components/curveeditor/curveeditorconstants.h
@@ -0,0 +1,14 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0+ OR GPL-3.0 WITH Qt-GPL-exception-1.0
+
+#pragma once
+
+namespace QmlDesigner {
+namespace CurveEditorConstants {
+
+constexpr char C_QMLCURVEEDITOR[] = "QmlDesigner::CurveEditor";
+
+constexpr char C_ZOOM_IN[] = "QmlDesigner.ZoomIn";
+constexpr char C_ZOOM_OUT[] = "QmlDesigner.ZoomOut";
+} // CurveEditorConstants
+} // QmlDesigner
