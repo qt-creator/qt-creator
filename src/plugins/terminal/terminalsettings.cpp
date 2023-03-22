d5b419dfa8851d57b42b9ffc417eeb8d03d04beb
Marcus Tillmanns <marcus.tillmanns@qt.io>
2023-03-22 13:36:06 +0100

    Terminal: Fix settings aspect registration
    Change-Id: I6f38660ba041dd745900746b16c7045d61a39e5b
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
    Reviewed-by: Cristian Adam <cristian.adam@qt.io>

--- src/plugins/terminal/terminalsettings.cpp
+++ src/plugins/terminal/terminalsettings.cpp
@@ -47,11 +47,16 @@ TerminalSettings &TerminalSettings::instance()
     return settings;
 }
 
-void setupColor(ColorAspect &color, const QString &label, const QColor &defaultColor)
+void setupColor(TerminalSettings *settings,
+                ColorAspect &color,
+                const QString &label,
+                const QColor &defaultColor)
 {
     color.setSettingsKey(label);
     color.setDefaultValue(defaultColor);
     color.setToolTip(Tr::tr("The color used for %1.").arg(label));
+
+    settings->registerAspect(&color);
 }
 
 TerminalSettings::TerminalSettings()
@@ -84,44 +89,38 @@ TerminalSettings::TerminalSettings()
     shell.setToolTip(Tr::tr("The shell executable to be started as terminal"));
     shell.setDefaultValue(defaultShell());
 
-    setupColor(foregroundColor, "Foreground", QColor::fromRgb(0xff, 0xff, 0xff));
-    setupColor(backgroundColor, "Background", QColor::fromRgb(0x0, 0x0, 0x0));
-    setupColor(selectionColor, "Selection", QColor::fromRgb(0xFF, 0xFF, 0xFF, 0x7F));
+    setupColor(this, foregroundColor, "Foreground", QColor::fromRgb(0xff, 0xff, 0xff));
+    setupColor(this, backgroundColor, "Background", QColor::fromRgb(0x0, 0x0, 0x0));
+    setupColor(this, selectionColor, "Selection", QColor::fromRgb(0xFF, 0xFF, 0xFF, 0x7F));
 
-    setupColor(colors[0], "0", QColor::fromRgb(0x00, 0x00, 0x00));
-    setupColor(colors[8], "8", QColor::fromRgb(102, 102, 102));
+    setupColor(this, colors[0], "0", QColor::fromRgb(0x00, 0x00, 0x00));
+    setupColor(this, colors[8], "8", QColor::fromRgb(102, 102, 102));
 
-    setupColor(colors[1], "1", QColor::fromRgb(139, 27, 16));
-    setupColor(colors[9], "9", QColor::fromRgb(210, 45, 31));
+    setupColor(this, colors[1], "1", QColor::fromRgb(139, 27, 16));
+    setupColor(this, colors[9], "9", QColor::fromRgb(210, 45, 31));
 
-    setupColor(colors[2], "2", QColor::fromRgb(74, 163, 46));
-    setupColor(colors[10], "10", QColor::fromRgb(98, 214, 63));
+    setupColor(this, colors[2], "2", QColor::fromRgb(74, 163, 46));
+    setupColor(this, colors[10], "10", QColor::fromRgb(98, 214, 63));
 
-    setupColor(colors[3], "3", QColor::fromRgb(154, 154, 47));
-    setupColor(colors[11], "11", QColor::fromRgb(229, 229, 75));
+    setupColor(this, colors[3], "3", QColor::fromRgb(154, 154, 47));
+    setupColor(this, colors[11], "11", QColor::fromRgb(229, 229, 75));
 
-    setupColor(colors[4], "4", QColor::fromRgb(0, 0, 171));
-    setupColor(colors[12], "12", QColor::fromRgb(0, 0, 254));
+    setupColor(this, colors[4], "4", QColor::fromRgb(0, 0, 171));
+    setupColor(this, colors[12], "12", QColor::fromRgb(0, 0, 254));
 
-    setupColor(colors[5], "5", QColor::fromRgb(163, 32, 172));
-    setupColor(colors[13], "13", QColor::fromRgb(210, 45, 222));
+    setupColor(this, colors[5], "5", QColor::fromRgb(163, 32, 172));
+    setupColor(this, colors[13], "13", QColor::fromRgb(210, 45, 222));
 
-    setupColor(colors[6], "6", QColor::fromRgb(73, 163, 176));
-    setupColor(colors[14], "14", QColor::fromRgb(105, 226, 228));
+    setupColor(this, colors[6], "6", QColor::fromRgb(73, 163, 176));
+    setupColor(this, colors[14], "14", QColor::fromRgb(105, 226, 228));
 
-    setupColor(colors[7], "7", QColor::fromRgb(191, 191, 191));
-    setupColor(colors[15], "15", QColor::fromRgb(229, 229, 230));
+    setupColor(this, colors[7], "7", QColor::fromRgb(191, 191, 191));
+    setupColor(this, colors[15], "15", QColor::fromRgb(229, 229, 230));
 
     registerAspect(&font);
     registerAspect(&fontSize);
     registerAspect(&shell);
     registerAspect(&allowBlinkingCursor);
-
-    registerAspect(&foregroundColor);
-    registerAspect(&backgroundColor);
-
-    for (auto &color : colors)
-        registerAspect(&color);
 }
 
 } // namespace Terminal
