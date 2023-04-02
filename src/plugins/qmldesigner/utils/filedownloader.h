9bdddd3df714bd73d4d0a08a83103b150e8fef27
Jarek Kobus <jaroslaw.kobus@qt.io>
2023-04-02 19:30:24 +0200

    Compile fix
    Change-Id: If0a12d9e2b7d8782ecc65855285f95e05c816f39
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/plugins/qmldesigner/utils/filedownloader.h
+++ src/plugins/qmldesigner/utils/filedownloader.h
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0+ OR GPL-3.0 WITH Qt-GPL-exception-1.0
 #pragma once
 
+#include <QDateTime>
 #include <QFile>
 #include <QNetworkReply>
 #include <QObject>
