a3f025063f7f11dbdd02db08afb05fa7eb176751
hjk <hjk@qt.io>
2023-04-20 11:00:48 +0200

    Help: Use IOptionPage::setWidgetCreator() for general settings
    Change-Id: I71a500cc349fed92c38368989fc258902c942e29
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/plugins/help/generalsettingspage.h
+++ src/plugins/help/generalsettingspage.h
@@ -5,51 +5,12 @@
 
 #include <coreplugin/dialogs/ioptionspage.h>
 
-#include <QFontDatabase>
-#include <QPointer>
-
-namespace Help {
-namespace Internal {
-
-class GeneralSettingsPageWidget;
+namespace Help::Internal {
 
 class GeneralSettingsPage : public Core::IOptionsPage
 {
-    Q_OBJECT
-
 public:
     GeneralSettingsPage();
-
-    QWidget *widget() override;
-    void apply() override;
-    void finish() override;
-
-private:
-    void setCurrentPage();
-    void setBlankPage();
-    void setDefaultPage();
-    void importBookmarks();
-    void exportBookmarks();
-
-    void updateFontSizeSelector();
-    void updateFontStyleSelector();
-    void updateFontFamilySelector();
-    void updateFont();
-    int closestPointSizeIndex(int desiredPointSize) const;
-
-    QFont m_font;
-    int m_fontZoom = 100;
-    QFontDatabase m_fontDatabase;
-
-    QString m_homePage;
-    int m_contextOption;
-
-    int m_startOption;
-    bool m_returnOnClose;
-    bool m_scrollWheelZoomingEnabled;
-
-    QPointer<GeneralSettingsPageWidget> m_widget;
 };
 
-    }   // Internal
-}   // Help
+} // Help::Internal
