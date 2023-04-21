3f32409748f6e52eb2268851b578edac31eaf82a
Knud Dollereder <knud.dollereder@qt.io>
2023-04-21 15:30:28 +0200

    QmlDesigner: Cleanup mcu deploy step
    Change-Id: I4f4fa4f9067613cb31c7408a89a25cf27f2093e3
    Reviewed-by: Aleksei German <aleksei.german@qt.io>

--- src/plugins/qmlprojectmanager/mcubuildstep.h
+++ src/plugins/qmlprojectmanager/mcubuildstep.h
@@ -2,10 +2,11 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0+ OR GPL-3.0 WITH Qt-GPL-exception-1.0
 #pragma once
 
-#include "projectexplorer/kit.h"
-#include "projectexplorer/project.h"
 #include <projectexplorer/abstractprocessstep.h>
 #include <projectexplorer/buildstep.h>
+#include <projectexplorer/kit.h>
+#include <projectexplorer/project.h>
+
 #include <utils/id.h>
 
 #include <QTemporaryDir>
@@ -16,18 +17,12 @@ class DeployMcuProcessStep : public ProjectExplorer::AbstractProcessStep
 {
 public:
     static const Utils::Id id;
-    static void showError(const QString& text);
+    static void showError(const QString &text);
 
     DeployMcuProcessStep(ProjectExplorer::BuildStepList *bc, Utils::Id id);
 
 private:
-    bool init() override;
-    void doRun() override;
-    QString findKitInformation(ProjectExplorer::Kit* kit, const QString& key);
-
-    static const QString processCommandKey;
-    static const QString processArgumentsKey;
-    static const QString processWorkingDirectoryKey;
+    QString findKitInformation(ProjectExplorer::Kit *kit, const QString &key);
     QTemporaryDir m_tmpDir;
 };
 
@@ -36,7 +31,7 @@ class MCUBuildStepFactory : public ProjectExplorer::BuildStepFactory
 public:
     MCUBuildStepFactory();
     static void attachToTarget(ProjectExplorer::Target *target);
-    static ProjectExplorer::Kit* findMostRecentQulKit( );
+    static ProjectExplorer::Kit *findMostRecentQulKit();
 };
 
 } // namespace QmlProjectManager
