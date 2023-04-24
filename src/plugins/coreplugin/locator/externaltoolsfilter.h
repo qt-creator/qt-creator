c8ae479a23de64b41bb85af30442555637ff1c0e
Jarek Kobus <jaroslaw.kobus@qt.io>
2023-04-24 21:19:49 +0200

    ExternalToolsFilter: Remove the old matchesFor() implementation
    Change-Id: I79e81103b0200ea9da56ecb84980e1814a01f2f1
    Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>

--- src/plugins/coreplugin/locator/externaltoolsfilter.h
+++ src/plugins/coreplugin/locator/externaltoolsfilter.h
@@ -5,23 +5,15 @@
 
 #include "ilocatorfilter.h"
 
-namespace Core {
-namespace Internal {
+namespace Core::Internal {
 
 class ExternalToolsFilter : public ILocatorFilter
 {
-    Q_OBJECT
 public:
     ExternalToolsFilter();
 
-    void prepareSearch(const QString &entry) override;
-    QList<LocatorFilterEntry> matchesFor(QFutureInterface<LocatorFilterEntry> &future,
-                                         const QString &entry) override;
 private:
     LocatorMatcherTasks matchers() final;
-
-    QList<LocatorFilterEntry> m_results;
 };
 
-} // namespace Internal
-} // namespace Core
+} // namespace Core::Internal
