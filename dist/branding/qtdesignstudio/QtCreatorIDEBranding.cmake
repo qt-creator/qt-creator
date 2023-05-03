e87f6722487e2ce02913d2c1f590e4489a861a23
Tim Jenssen <tim.jenssen@qt.io>
2023-05-03 06:24:49 +0200

    qds: move branding into opensource repository
    Task-number: QDS-8326
    Change-Id: I3484a48de30ef394071d1fc59ad8fdff20327af3
    Reviewed-by: Burak Hancerli <burak.hancerli@qt.io>
    Reviewed-by: Marco Bubke <marco.bubke@qt.io>
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>
    (cherry picked from commit 6eadb5d357bfab4967ad3224531712c9efa9f255)
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

---
+++ dist/branding/qtdesignstudio/QtCreatorIDEBranding.cmake
@@ -0,0 +1,18 @@
+set(IDE_VERSION "4.2.0")                              # The IDE version.
+set(IDE_VERSION_COMPAT "4.2.0")                       # The IDE Compatibility version.
+set(IDE_VERSION_DISPLAY "4.2.0")                      # The IDE display version.
+set(IDE_COPYRIGHT_YEAR "2023")                        # The IDE current copyright year.
+
+set(IDE_SETTINGSVARIANT "QtProject")                  # The IDE settings variation.
+set(IDE_COPY_SETTINGSVARIANT "Nokia")                 # The IDE settings to initially import.
+set(IDE_DISPLAY_NAME "Qt Design Studio")              # The IDE display name.
+set(IDE_ID "qtdesignstudio")                          # The IDE id (no spaces, lowercase!)
+set(IDE_CASED_ID "QtDesignStudio")                    # The cased IDE id (no spaces!)
+set(IDE_BUNDLE_IDENTIFIER "org.qt-project.${IDE_ID}") # The macOS application bundle identifier.
+
+set(PROJECT_USER_FILE_EXTENSION .qtds)
+set(IDE_DOC_FILE "qtdesignstudio/qtdesignstudio.qdocconf")
+set(IDE_DOC_FILE_ONLINE "qtdesignstudio/qtdesignstudio-online.qdocconf")
+
+set(IDE_ICON_PATH "${CMAKE_CURRENT_LIST_DIR}")
+set(IDE_LOGO_PATH "${CMAKE_CURRENT_LIST_DIR}")
