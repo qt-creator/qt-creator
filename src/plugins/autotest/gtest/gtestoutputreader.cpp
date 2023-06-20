17c8a6bfe2a184a3a2744d5d6180edda05571fca
Christian Stenger <christian.stenger@qt.io>
2023-06-20 13:54:30 +0200

    AutoTest: Fix special handling for gtest exit
    Similar to what has been done for boost test - there is special
    handling when the test applications process exits.
    This was no more executed since moving over to use the task tree.
    
    Change-Id: I982b7e4dfe6de4bbbe75c8d3ec0f62d0c3037f4a
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/plugins/autotest/gtest/gtestoutputreader.cpp
+++ src/plugins/autotest/gtest/gtestoutputreader.cpp
@@ -23,18 +23,6 @@ GTestOutputReader::GTestOutputReader(Process *testApplication,
     : TestOutputReader(testApplication, buildDirectory)
     , m_projectFile(projectFile)
 {
-    if (testApplication) {
-        connect(testApplication, &Process::done, this, [this, testApplication] {
-            const int exitCode = testApplication->exitCode();
-            if (exitCode == 1 && !m_description.isEmpty()) {
-                createAndReportResult(Tr::tr("Running tests failed.\n %1\nExecutable: %2")
-                                      .arg(m_description).arg(id()), ResultType::MessageFatal);
-            }
-            // on Windows abort() will result in normal termination, but exit code will be set to 3
-            if (HostOsInfo::isWindowsHost() && exitCode == 3)
-                reportCrash();
-        });
-    }
 }
 
 void GTestOutputReader::processOutputLine(const QByteArray &outputLine)
@@ -180,6 +168,17 @@ TestResult GTestOutputReader::createDefaultResult() const
         result.setLine(testItem->line());
     }
     return result;
+}
+
+void GTestOutputReader::onDone(int exitCode)
+{
+    if (exitCode == 1 && !m_description.isEmpty()) {
+        createAndReportResult(Tr::tr("Running tests failed.\n %1\nExecutable: %2")
+                              .arg(m_description).arg(id()), ResultType::MessageFatal);
+    }
+    // on Windows abort() will result in normal termination, but exit code will be set to 3
+    if (HostOsInfo::isWindowsHost() && exitCode == 3)
+        reportCrash();
 }
 
 void GTestOutputReader::setCurrentTestCase(const QString &testCase)
