3ff21fe3e15e9228c47e1bf248dd379cdb0e9d40
Eike Ziller <eike.ziller@qt.io>
2023-06-22 14:04:32 +0200

    Bump version to 11.0.0-rc1
    Change-Id: I7b6fa9105858996a82584a417520d8e241c9c1e8
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- cmake/QtCreatorIDEBranding.cmake
+++ cmake/QtCreatorIDEBranding.cmake
@@ -1,6 +1,6 @@
-set(IDE_VERSION "10.0.83")                            # The IDE version.
-set(IDE_VERSION_COMPAT "10.0.83")                     # The IDE Compatibility version.
-set(IDE_VERSION_DISPLAY "11.0.0-beta2")               # The IDE display version.
+set(IDE_VERSION "10.0.84")                            # The IDE version.
+set(IDE_VERSION_COMPAT "10.0.84")                     # The IDE Compatibility version.
+set(IDE_VERSION_DISPLAY "11.0.0-rc1")                 # The IDE display version.
 set(IDE_COPYRIGHT_YEAR "2023")                        # The IDE current copyright year.
 
 set(IDE_SETTINGSVARIANT "QtProject")                  # The IDE settings variation.
