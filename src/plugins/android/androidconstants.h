1e81f206b039802df3fa59702dca36b07b0c7f80
Alexey Edelev <alexey.edelev@qt.io>
2023-06-28 19:17:24 +0200

    Android: Pull android debugging related files to a subdirectory
    When building the project in QtCreator using the Debug configuration,
    android deployment procedure pulls app_process from the device to
    establish the debug environment. This operation litters the project
    build directory with the libraries and executables that don't belong
    to it. Instead of pulling the files to the root build directory, it's
    better to isolate these files using subdirectory.
    
    Fixes: QTCREATORBUG-29336
    Fixes: QTBUG-113851
    Fixes: QTBUG-111284
    Change-Id: I32c3403dc5f79c42b1ff2bd676c2cf3ae8d43ec3
    Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>
    Reviewed-by: Assam Boudjelthia <assam.boudjelthia@qt.io>

--- src/plugins/android/androidconstants.h
+++ src/plugins/android/androidconstants.h
@@ -27,6 +27,7 @@ const char ANDROID_KIT_NDK[] = "Android.NDK";
 const char ANDROID_KIT_SDK[] = "Android.SDK";
 
 const char ANDROID_BUILD_DIRECTORY[] = "android-build";
+const char ANDROID_APP_PROCESS_DIRECTORY[] = "android-app-process";
 const char JAVA_EDITOR_ID[] = "java.editor";
 const char JLS_SETTINGS_ID[] = "Java::JLSSettingsID";
 const char JAVA_MIMETYPE[] = "text/x-java";
