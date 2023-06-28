5867d986a8360dcf8d9d62678b3ce61de8bc4fbd
hjk <hjk@qt.io>
2023-06-28 10:48:37 +0200

    Core: Drop Q_OBJECT from OptionsPopup
    Not needed.
    
    Also slim down header a bit.
    
    Change-Id: I6ac828fdfc42173e0b5aeca5d92e920745b424c9
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/plugins/coreplugin/find/optionspopup.h
+++ src/plugins/coreplugin/find/optionspopup.h
@@ -9,25 +9,16 @@
 
 #include <QWidget>
 
-QT_BEGIN_NAMESPACE
-class QCheckBox;
-QT_END_NAMESPACE
-
 namespace Core {
 
 class CORE_EXPORT OptionsPopup : public QWidget
 {
-    Q_OBJECT
-
 public:
     OptionsPopup(QWidget *parent, const QVector<Utils::Id> &commands);
 
 protected:
     bool event(QEvent *ev) override;
     bool eventFilter(QObject *obj, QEvent *ev) override;
-
-private:
-    QCheckBox *createCheckboxForCommand(Utils::Id id);
 };
 
 } // namespace Core
