b1f1624b6ec5bdfaa3d27b2ec2f5f03888e78569
Jarek Kobus <jaroslaw.kobus@qt.io>
2023-07-13 00:11:04 +0200

    DeviceCheckBuildStep: Employ task tree for running
    Task-number: QTCREATORBUG-29168
    Change-Id: I3506663263d8ebdc922bd6ac1b11357f4a6dba8f
    Reviewed-by: hjk <hjk@qt.io>
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>

--- src/plugins/projectexplorer/devicesupport/devicecheckbuildstep.cpp
+++ src/plugins/projectexplorer/devicesupport/devicecheckbuildstep.cpp
@@ -8,8 +8,9 @@
 #include "../projectexplorertr.h"
 
 #include "devicemanager.h"
-#include "idevice.h"
 #include "idevicefactory.h"
+
+#include <solutions/tasking/tasktree.h>
 
 #include <QMessageBox>
 
@@ -26,40 +27,43 @@ public:
 
     bool init() override
     {
+        if (!BuildStep::init())
+            return false;
+
         IDevice::ConstPtr device = DeviceKitAspect::device(kit());
-        if (!device) {
-            Utils::Id deviceTypeId = DeviceTypeKitAspect::deviceTypeId(kit());
-            IDeviceFactory *factory = IDeviceFactory::find(deviceTypeId);
-            if (!factory || !factory->canCreate()) {
-                emit addOutput(Tr::tr("No device configured."), BuildStep::OutputFormat::ErrorMessage);
-                return false;
-            }
+        if (device)
+            return true;
 
-            QMessageBox msgBox(QMessageBox::Question, Tr::tr("Set Up Device"),
-                               Tr::tr("There is no device set up for this kit. Do you want to add a device?"),
-                               QMessageBox::Yes|QMessageBox::No);
-            msgBox.setDefaultButton(QMessageBox::Yes);
-            if (msgBox.exec() == QMessageBox::No) {
-                emit addOutput(Tr::tr("No device configured."), BuildStep::OutputFormat::ErrorMessage);
-                return false;
-            }
-
-            IDevice::Ptr newDevice = factory->create();
-            if (newDevice.isNull()) {
-                emit addOutput(Tr::tr("No device configured."), BuildStep::OutputFormat::ErrorMessage);
-                return false;
-            }
-
-            DeviceManager *dm = DeviceManager::instance();
-            dm->addDevice(newDevice);
-
-            DeviceKitAspect::setDevice(kit(), newDevice);
+        Utils::Id deviceTypeId = DeviceTypeKitAspect::deviceTypeId(kit());
+        IDeviceFactory *factory = IDeviceFactory::find(deviceTypeId);
+        if (!factory || !factory->canCreate()) {
+            emit addOutput(Tr::tr("No device configured."), OutputFormat::ErrorMessage);
+            return false;
         }
 
+        QMessageBox msgBox(QMessageBox::Question, Tr::tr("Set Up Device"),
+                           Tr::tr("There is no device set up for this kit. Do you want to add a device?"),
+                           QMessageBox::Yes | QMessageBox::No);
+        msgBox.setDefaultButton(QMessageBox::Yes);
+        if (msgBox.exec() == QMessageBox::No) {
+            emit addOutput(Tr::tr("No device configured."), OutputFormat::ErrorMessage);
+            return false;
+        }
+
+        IDevice::Ptr newDevice = factory->create();
+        if (newDevice.isNull()) {
+            emit addOutput(Tr::tr("No device configured."), OutputFormat::ErrorMessage);
+            return false;
+        }
+
+        DeviceManager *dm = DeviceManager::instance();
+        dm->addDevice(newDevice);
+        DeviceKitAspect::setDevice(kit(), newDevice);
         return true;
     }
 
-    void doRun() override { emit finished(true); }
+private:
+    Tasking::GroupItem runRecipe() final { return Tasking::Group{}; }
 };
 
 // Factory
