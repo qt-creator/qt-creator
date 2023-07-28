2c56fa8084f28d27d0183b6428b0ab9c1594de48
hjk <hjk@qt.io>
2023-07-28 18:24:24 +0200

    Autotest: Hide TestNavigationWidget implementation
    Also, de-Q_OBJECT-ify.
    
    Change-Id: I6e527bb36e9f38a702604f9c6c3923e62bf921c9
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/plugins/autotest/testnavigationwidget.h
+++ src/plugins/autotest/testnavigationwidget.h
@@ -3,78 +3,17 @@
 
 #pragma once
 
-#include "itemdatacache.h"
-
 #include <coreplugin/inavigationwidgetfactory.h>
 
-#include <utils/navigationtreeview.h>
-
-QT_BEGIN_NAMESPACE
-class QAction;
-class QMenu;
-class QTimer;
-class QToolButton;
-QT_END_NAMESPACE
-
-namespace Utils {
-class ProgressIndicator;
-}
-
-namespace Autotest {
-
-class TestTreeModel;
-
-namespace Internal {
-
-class TestRunner;
-class TestTreeSortFilterModel;
-class TestTreeView;
-
-class TestNavigationWidget : public QWidget
-{
-    Q_OBJECT
-
-public:
-    explicit TestNavigationWidget(QWidget *parent = nullptr);
-    void contextMenuEvent(QContextMenuEvent *event) override;
-    QList<QToolButton *> createToolButtons();
-
-    void updateExpandedStateCache();
-
-private:
-    void onItemActivated(const QModelIndex &index);
-    void onSortClicked();
-    void onFilterMenuTriggered(QAction *action);
-    void onParsingStarted();
-    void onParsingFinished();
-    void initializeFilterMenu();
-    void onRunThisTestTriggered(TestRunMode runMode);
-    void reapplyCachedExpandedState();
-
-    TestTreeModel *m_model;
-    TestTreeSortFilterModel *m_sortFilterModel;
-    TestTreeView *m_view;
-    QToolButton *m_sort;
-    QToolButton *m_filterButton;
-    QMenu *m_filterMenu;
-    bool m_sortAlphabetically;
-    Utils::ProgressIndicator *m_progressIndicator;
-    QTimer *m_progressTimer;
-    QFrame *m_missingFrameworksWidget;
-    ItemDataCache<bool> m_expandedStateCache;
-};
+namespace Autotest::Internal {
 
 class TestNavigationWidgetFactory : public Core::INavigationWidgetFactory
 {
-    Q_OBJECT
-
 public:
     TestNavigationWidgetFactory();
 
 private:
     Core::NavigationView createWidget() override;
-
 };
 
-} // namespace Internal
-} // namespace Autotest
+} // Autotest::Internal
