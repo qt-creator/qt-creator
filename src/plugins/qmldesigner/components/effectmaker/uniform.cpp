6b62b56903ade17a6e879a6cceb106a83f3f83a8
Amr Essam <amr.elsayed@qt.io>
2023-08-18 11:47:16 +0300

    QmlDesginer: Add Uniform getters and setters
    Change-Id: Id2e75db32bed9577cca061b2c5971171dca984ad
    Reviewed-by: Mahmoud Badri <mahmoud.badri@qt.io>

--- src/plugins/qmldesigner/components/effectmaker/uniform.cpp
+++ src/plugins/qmldesigner/components/effectmaker/uniform.cpp
@@ -12,4 +12,79 @@ Uniform::Uniform(const QJsonObject &props)
     Q_UNUSED(props)
 }
 
+Uniform::Type Uniform::type() const
+{
+    return m_type;
+}
+
+QVariant Uniform::value() const
+{
+    return m_value;
+}
+
+void Uniform::setValue(const QVariant &newValue)
+{
+    m_value = newValue;
+}
+
+QVariant Uniform::defaultValue() const
+{
+    return m_defaultValue;
+}
+
+QVariant Uniform::minValue() const
+{
+    return m_minValue;
+}
+
+QVariant Uniform::maxValue() const
+{
+    return m_maxValue;
+}
+
+QString Uniform::name() const
+{
+    return m_name;
+}
+
+void Uniform::setName(const QString &newName)
+{
+    m_name = newName;
+}
+
+QString Uniform::description() const
+{
+    return m_description;
+}
+
+QString Uniform::customValue() const
+{
+    return m_customValue;
+}
+
+void Uniform::setCustomValue(const QString &newCustomValue)
+{
+    m_customValue = newCustomValue;
+}
+
+bool Uniform::useCustomValue() const
+{
+    return m_useCustomValue;
+}
+
+bool Uniform::enabled() const
+{
+    return m_enabled;
+}
+
+void Uniform::setEnabled(bool newEnabled)
+{
+    m_enabled = newEnabled;
+}
+
+bool Uniform::enableMipmap() const
+{
+    return m_enableMipmap;
+}
+
 } // namespace QmlDesigner
--- src/plugins/qmldesigner/components/effectmaker/uniform.h
+++ src/plugins/qmldesigner/components/effectmaker/uniform.h
@@ -28,7 +28,31 @@ public:
 
     Uniform(const QJsonObject &props);
 
-    // TODO: setters & getters
+    Type type() const;
+
+    QVariant value() const;
+    void setValue(const QVariant &newValue);
+
+    QVariant defaultValue() const;
+
+    QVariant minValue() const;
+
+    QVariant maxValue() const;
+
+    QString name() const;
+    void setName(const QString &newName);
+
+    QString description() const;
+
+    QString customValue() const;
+    void setCustomValue(const QString &newCustomValue);
+
+    bool useCustomValue() const;
+
+    bool enabled() const;
+    void setEnabled(bool newEnabled);
+
+    bool enableMipmap() const;
 
 private:
     Type m_type;
@@ -40,7 +64,7 @@ private:
     QString m_description;
     QString m_customValue;
     bool m_useCustomValue = false;
-    bool m_visible = true;
+    bool m_enabled = true;
     bool m_enableMipmap = false;
 
     bool operator==(const Uniform& rhs) const noexcept
