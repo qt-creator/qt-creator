qt-creator/qt-creator

commit 5676e612982d3c87f403677bcb63e0b6343f9b7d

QmlDesigner: Add transient scroll bar to ComboBox
Change-Id: I118e4801fef9fab117e4d7355e56616c7d001858
Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>
Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
Reviewed-by: Qt CI Patch Build Bot <ci_patchbuild_bot@qt.io>
Reviewed-by: Brook Cronin <brook.cronin@qt.io>

share/qtcreator/qmldesigner/propertyEditorQmlSources/imports/HelperWidgets/ScrollView.qml
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 import QtQuick
-//import QtQuick.Controls as C
+import StudioControls as StudioControls
 import StudioTheme 1.0 as StudioTheme
 
 Flickable {
@@ -26,8 +26,9 @@ Flickable {
 
     HoverHandler { id: hoverHandler }
 
-    ScrollBar.horizontal: ScrollBar {
+    ScrollBar.horizontal: StudioControls.TransientScrollBar {
         id: horizontalScrollBar
+        style: StudioTheme.Values.viewStyle
         parent: flickable
         x: 0
         y: flickable.height - horizontalScrollBar.height
@@ -40,8 +41,9 @@ Flickable {
         otherInUse: verticalScrollBar.inUse
     }
 
-    ScrollBar.vertical: ScrollBar {
+    ScrollBar.vertical: StudioControls.TransientScrollBar {
         id: verticalScrollBar
+        style: StudioTheme.Values.viewStyle
         parent: flickable
         x: flickable.width - verticalScrollBar.width
         y: 0

share/qtcreator/qmldesigner/assetsLibraryQmlSources/AssetsView.qml
@@ -5,6 +5,7 @@ import QtQuick
 import QtQuick.Controls
 import HelperWidgets as HelperWidgets
 import StudioControls as StudioControls
+import StudioTheme as StudioTheme
 import AssetsLibraryBackend
 
 TreeView {
@@ -53,8 +54,9 @@ TreeView {
 
     HoverHandler { id: hoverHandler }
 
-    ScrollBar.vertical: HelperWidgets.ScrollBar {
+    ScrollBar.vertical: StudioControls.TransientScrollBar {
         id: verticalScrollBar
+        style: StudioTheme.Values.viewStyle
         parent: root
         x: root.width - verticalScrollBar.width
         y: 0

share/qtcreator/qmldesigner/connectionseditor/BindingsListView.qml
@@ -3,8 +3,8 @@
 
 import QtQuick
 import QtQuick.Controls
-import HelperWidgets 2.0 as HelperWidgets
-import StudioControls 1.0 as StudioControls
+import HelperWidgets as HelperWidgets
+import StudioControls as StudioControls
 import StudioTheme as StudioTheme
 import ConnectionsEditorEditorBackend
 
@@ -30,8 +30,9 @@ ListView {
 
     HoverHandler { id: hoverHandler }
 
-    ScrollBar.vertical: HelperWidgets.ScrollBar {
+    ScrollBar.vertical: StudioControls.TransientScrollBar {
         id: verticalScrollBar
+        style: StudioTheme.Values.viewStyle
         parent: root
         x: root.width - verticalScrollBar.width
         y: 0

share/qtcreator/qmldesigner/connectionseditor/ConnectionsListView.qml
@@ -3,8 +3,8 @@
 
 import QtQuick
 import QtQuick.Controls
-import HelperWidgets 2.0 as HelperWidgets
-import StudioControls 1.0 as StudioControls
+import HelperWidgets as HelperWidgets
+import StudioControls as StudioControls
 import StudioTheme as StudioTheme
 import ConnectionsEditorEditorBackend
 
@@ -30,8 +30,9 @@ ListView {
 
     HoverHandler { id: hoverHandler }
 
-    ScrollBar.vertical: HelperWidgets.ScrollBar {
+    ScrollBar.vertical: StudioControls.TransientScrollBar {
         id: verticalScrollBar
+        style: StudioTheme.Values.viewStyle
         parent: root
         x: root.width - verticalScrollBar.width
         y: 0

share/qtcreator/qmldesigner/connectionseditor/PropertiesListView.qml
@@ -3,8 +3,8 @@
 
 import QtQuick
 import QtQuick.Controls
-import HelperWidgets 2.0 as HelperWidgets
-import StudioControls 1.0 as StudioControls
+import HelperWidgets as HelperWidgets
+import StudioControls as StudioControls
 import StudioTheme as StudioTheme
 import ConnectionsEditorEditorBackend
 
@@ -30,8 +30,9 @@ ListView {
 
     HoverHandler { id: hoverHandler }
 
-    ScrollBar.vertical: HelperWidgets.ScrollBar {
+    ScrollBar.vertical: StudioControls.TransientScrollBar {
         id: verticalScrollBar
+        style: StudioTheme.Values.viewStyle
         parent: root
         x: root.width - verticalScrollBar.width
         y: 0

share/qtcreator/qmldesigner/connectionseditor/SuggestionPopup.qml
@@ -135,7 +135,7 @@ Controls.Popup {
                     boundsMovement: Flickable.StopAtBounds
                     boundsBehavior: Flickable.StopAtBounds
 
-                    Controls.ScrollBar.vertical: HelperWidgets.ScrollBar {
+                    Controls.ScrollBar.vertical: StudioControls.TransientScrollBar {
                         id: listScrollBar
                         parent: listView
                         x: listView.width - listScrollBar.width
@@ -195,7 +195,7 @@ Controls.Popup {
                     boundsMovement: Flickable.StopAtBounds
                     boundsBehavior: Flickable.StopAtBounds
 
-                    Controls.ScrollBar.vertical: HelperWidgets.ScrollBar {
+                    Controls.ScrollBar.vertical: StudioControls.TransientScrollBar {
                         id: treeScrollBar
                         parent: treeView
                         x: treeView.width - treeScrollBar.width

share/qtcreator/qmldesigner/newprojectdialog/imports/NewProjectDialog/Details.qml
@@ -6,7 +6,7 @@ import QtQuick.Controls
 
 import QtQuick
 import QtQuick.Layouts
-import StudioControls as SC
+import StudioControls as StudioControls
 import StudioTheme as StudioTheme
 
 import BackendApi
@@ -22,8 +22,8 @@ Item {
         anchors.fill: parent
 
         Item {
-            x: DialogValues.detailsPanePadding                               // left padding
-            width: parent.width - DialogValues.detailsPanePadding * 2        // right padding
+            x: DialogValues.detailsPanePadding * 2 // left padding
+            width: parent.width - DialogValues.detailsPanePadding * 3 // right padding
             height: parent.height
 
             Column {
@@ -44,6 +44,7 @@ Item {
                 }
 
                 Flickable {
+                    id: flickable
                     width: parent.width
                     height: parent.height - detailsHeading.height - DialogValues.defaultPadding
                             - savePresetButton.height
@@ -52,14 +53,27 @@ Item {
                     boundsBehavior: Flickable.StopAtBounds
                     clip: true
 
-                    ScrollBar.vertical: SC.VerticalScrollBar {}
+                    HoverHandler { id: hoverHandler }
+
+                    ScrollBar.vertical: StudioControls.TransientScrollBar {
+                        id: verticalScrollBar
+                        style: StudioTheme.Values.viewStyle
+                        parent: flickable
+                        x: flickable.width - verticalScrollBar.width
+                        y: 0
+                        height: flickable.availableHeight
+                        orientation: Qt.Vertical
+
+                        show: (hoverHandler.hovered || flickable.focus || verticalScrollBar.inUse)
+                              && verticalScrollBar.isNeeded
+                    }
 
                     Column {
                         id: scrollContent
                         width: parent.width - DialogValues.detailsPanePadding
                         spacing: DialogValues.defaultPadding
 
-                        SC.TextField {
+                        StudioControls.TextField {
                             id: projectNameTextField
                             actionIndicatorVisible: false
                             translationIndicatorVisible: false
@@ -85,7 +99,7 @@ Item {
                         RowLayout { // Project location
                             width: parent.width
 
-                            SC.TextField {
+                            StudioControls.TextField {
                                 Layout.fillWidth: true
                                 id: projectLocationTextField
                                 actionIndicatorVisible: false
@@ -102,7 +116,7 @@ Item {
                                 value: projectLocationTextField.text
                             }
 
-                            SC.AbstractButton {
+                            StudioControls.AbstractButton {
                                 implicitWidth: 30
                                 iconSize: 20
                                 visible: true
@@ -114,7 +128,7 @@ Item {
                                     if (newLocation)
                                         projectLocationTextField.text = newLocation
                                 }
-                            } // SC.AbstractButton
+                            }
                         } // Project location RowLayout
 
                         Item { width: parent.width; height: DialogValues.narrowSpacing(7) }
@@ -171,7 +185,7 @@ Item {
                             } // Text
                         } // RowLayout
 
-                        SC.CheckBox {
+                        StudioControls.CheckBox {
                             id: defaultLocationCheckbox
                             actionIndicatorVisible: false
                             text: qsTr("Use as default project location")
@@ -187,7 +201,7 @@ Item {
 
                         Rectangle { width: parent.width; height: 1; color: DialogValues.dividerlineColor }
 
-                        SC.ComboBox {   // Screen Size ComboBox
+                        StudioControls.ComboBox {   // Screen Size ComboBox
                             id: screenSizeComboBox
                             actionIndicatorVisible: false
                             currentIndex: -1
@@ -253,7 +267,7 @@ Item {
                             }
 
                             // content items
-                            SC.RealSpinBox {
+                            StudioControls.RealSpinBox {
                                 id: widthField
                                 actionIndicatorVisible: false
                                 implicitWidth: 70
@@ -274,7 +288,7 @@ Item {
                                 value: widthField.realValue
                             }
 
-                            SC.RealSpinBox {
+                            StudioControls.RealSpinBox {
                                 id: heightField
                                 actionIndicatorVisible: false
                                 implicitWidth: 70
@@ -368,7 +382,7 @@ Item {
                             color: DialogValues.dividerlineColor
                         }
 
-                        SC.CheckBox {
+                        StudioControls.CheckBox {
                             id: useQtVirtualKeyboard
                             actionIndicatorVisible: false
                             text: qsTr("Use Qt Virtual Keyboard")
@@ -389,7 +403,7 @@ Item {
                                 color: DialogValues.textColor
                             }
 
-                            SC.ComboBox {   // Target Qt Version ComboBox
+                            StudioControls.ComboBox {   // Target Qt Version ComboBox
                                 id: qtVersionComboBox
                                 actionIndicatorVisible: false
                                 implicitWidth: 82
@@ -421,7 +435,7 @@ Item {
                 } // ScrollView
             } // Column
 
-            SC.AbstractButton {
+            StudioControls.AbstractButton {
                 id: savePresetButton
                 width: StudioTheme.Values.singleControlColumnWidth
                 buttonIcon: qsTr("Save Custom Preset")
@@ -459,7 +473,7 @@ Item {
                         color: DialogValues.textColor
                     }
 
-                    SC.TextField {
+                    StudioControls.TextField {
                         id: presetNameTextField
                         actionIndicatorVisible: false
                         translationIndicatorVisible: false

share/qtcreator/qmldesigner/newprojectdialog/imports/NewProjectDialog/PresetView.qml
@@ -6,7 +6,7 @@ import QtQuick.Controls
 
 import QtQuick
 import QtQuick.Layouts
-import StudioControls as SC
+import StudioControls as StudioControls
 import StudioTheme as StudioTheme
 
 import BackendApi
@@ -23,12 +23,17 @@ ScrollView {
     property bool selectLast: false
 
     ScrollBar.horizontal.policy: ScrollBar.AlwaysOff
-    ScrollBar.vertical: SC.VerticalScrollBar {
+    ScrollBar.vertical: StudioControls.TransientScrollBar {
+        id: verticalScrollBar
+        style: StudioTheme.Values.viewStyle
         parent: scrollView
-        x: scrollView.width + (DialogValues.gridMargins
-                               - StudioTheme.Values.scrollBarThickness) * 0.5
+        x: scrollView.width + (DialogValues.gridMargins - verticalScrollBar.width) * 0.5
         y: scrollView.topPadding
         height: scrollView.availableHeight
+        orientation: Qt.Vertical
+
+        show: (scrollView.hovered || scrollView.focus || verticalScrollBar.inUse)
+              && verticalScrollBar.isNeeded
     }
 
     contentWidth: gridView.contentItem.childrenRect.width

commit 25b540d995b74ac786e4dfe7e700f7c6033b262a

QmlDesigner: Fix ScrollView not working on Windows
Caused by (5676e612982d3c87f403677bcb63e0b6343f9b7d)

Change-Id: I9dd45a31326e061e5ef6e3cddfcd069ce9132aee
Reviewed-by: Henning Gründl <henning.gruendl@qt.io>

share/qtcreator/qmldesigner/propertyEditorQmlSources/imports/HelperWidgets/ScrollView.qml
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 import QtQuick
+import QtQuick.Controls
 import StudioControls as StudioControls
 import StudioTheme 1.0 as StudioTheme
 