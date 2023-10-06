7ee0687ad58dd62d833ddd1802abf47ad15b15f4
Jarek Kobus <jaroslaw.kobus@qt.io>
2023-10-06 09:29:13 +0200

    SemanticInfoUpdater: Make doUpdate() static
    Don't operate on the same SemanticInfoUpdaterPrivate
    instance from 2 threads.
    
    Use future synchronizer for quick cancel.
    
    Drop using QMutex, as now the threads are separated.
    
    Change-Id: I33bb1485ff03751ec5a1481f5be5c21a48ac3fd4
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- src/plugins/cppeditor/cppsemanticinfoupdater.cpp
+++ src/plugins/cppeditor/cppsemanticinfoupdater.cpp
@@ -5,18 +5,22 @@
 
 #include "cppmodelmanager.h"
 
-#include <utils/async.h>
-#include <utils/qtcassert.h>
-
 #include <cplusplus/Control.h>
 #include <cplusplus/CppDocument.h>
 #include <cplusplus/TranslationUnit.h>
+
+#include <extensionsystem/pluginmanager.h>
+
+#include <utils/async.h>
+#include <utils/futuresynchronizer.h>
+#include <utils/qtcassert.h>
 
 #include <QLoggingCategory>
 
 enum { debug = 0 };
 
 using namespace CPlusPlus;
+using namespace Utils;
 
 static Q_LOGGING_CATEGORY(log, "qtc.cppeditor.semanticinfoupdater", QtWarningMsg)
 
@@ -25,89 +29,72 @@ namespace CppEditor {
 class SemanticInfoUpdaterPrivate
 {
 public:
-    class FuturizedTopLevelDeclarationProcessor: public TopLevelDeclarationProcessor
-    {
-    public:
-        explicit FuturizedTopLevelDeclarationProcessor(QPromise<void> &promise): m_promise(promise) {}
-        bool processDeclaration(DeclarationAST *) override { return !isCanceled(); }
-        bool isCanceled() { return m_promise.isCanceled(); }
-    private:
-        QPromise<void> &m_promise;
-    };
-
-public:
     explicit SemanticInfoUpdaterPrivate(SemanticInfoUpdater *q);
-    ~SemanticInfoUpdaterPrivate();
+    ~SemanticInfoUpdaterPrivate() { cancelFuture(); }
 
-    SemanticInfo semanticInfo() const;
+    SemanticInfo semanticInfo() const { return m_semanticInfo; }
     void setSemanticInfo(const SemanticInfo &semanticInfo, bool emitSignal);
-
-    SemanticInfo update(const SemanticInfo::Source &source,
-                        bool emitSignalWhenFinished,
-                        FuturizedTopLevelDeclarationProcessor *processor);
 
     bool reuseCurrentSemanticInfo(const SemanticInfo::Source &source, bool emitSignalWhenFinished);
 
-    void update_helper(QPromise<void> &promise, const SemanticInfo::Source &source);
-
+    void cancelFuture();
 public:
     SemanticInfoUpdater *q;
-    mutable QMutex m_lock;
     SemanticInfo m_semanticInfo;
-    QFuture<void> m_future;
+    std::unique_ptr<QFutureWatcher<SemanticInfo>> m_watcher;
 };
 
 SemanticInfoUpdaterPrivate::SemanticInfoUpdaterPrivate(SemanticInfoUpdater *q)
     : q(q)
-{
-}
-
-SemanticInfoUpdaterPrivate::~SemanticInfoUpdaterPrivate()
-{
-    m_future.cancel();
-    m_future.waitForFinished();
-}
-
-SemanticInfo SemanticInfoUpdaterPrivate::semanticInfo() const
-{
-    QMutexLocker locker(&m_lock);
-    return m_semanticInfo;
-}
+{}
 
 void SemanticInfoUpdaterPrivate::setSemanticInfo(const SemanticInfo &semanticInfo, bool emitSignal)
 {
-    {
-        QMutexLocker locker(&m_lock);
-        m_semanticInfo = semanticInfo;
-    }
+    m_semanticInfo = semanticInfo;
     if (emitSignal) {
         qCDebug(log) << "emiting new info";
         emit q->updated(semanticInfo);
     }
 }
 
-SemanticInfo SemanticInfoUpdaterPrivate::update(const SemanticInfo::Source &source,
-                                                bool emitSignalWhenFinished,
-                                                FuturizedTopLevelDeclarationProcessor *processor)
+void SemanticInfoUpdaterPrivate::cancelFuture()
+{
+    if (!m_watcher)
+        return;
+
+    m_watcher->cancel();
+    m_watcher.reset();
+}
+
+class FuturizedTopLevelDeclarationProcessor: public TopLevelDeclarationProcessor
+{
+public:
+    explicit FuturizedTopLevelDeclarationProcessor(const QFuture<void> &future): m_future(future) {}
+    bool processDeclaration(DeclarationAST *) override { return !m_future.isCanceled(); }
+private:
+    QFuture<void> m_future;
+};
+
+static void doUpdate(QPromise<SemanticInfo> &promise, const SemanticInfo::Source &source)
 {
     SemanticInfo newSemanticInfo;
     newSemanticInfo.revision = source.revision;
     newSemanticInfo.snapshot = source.snapshot;
 
-    Document::Ptr doc = newSemanticInfo.snapshot.preprocessedDocument(source.code,
-                                          Utils::FilePath::fromString(source.fileName));
-    if (processor)
-        doc->control()->setTopLevelDeclarationProcessor(processor);
+    Document::Ptr doc = newSemanticInfo.snapshot.preprocessedDocument(
+        source.code, FilePath::fromString(source.fileName));
+
+    FuturizedTopLevelDeclarationProcessor processor(QFuture<void>(promise.future()));
+    doc->control()->setTopLevelDeclarationProcessor(&processor);
     doc->check();
-    if (processor && processor->isCanceled())
+    if (promise.isCanceled())
         newSemanticInfo.complete = false;
     newSemanticInfo.doc = doc;
 
     qCDebug(log) << "update() for source revision:" << source.revision
                  << "canceled:" << !newSemanticInfo.complete;
 
-    setSemanticInfo(newSemanticInfo, emitSignalWhenFinished);
-    return newSemanticInfo;
+    promise.addResult(newSemanticInfo);
 }
 
 bool SemanticInfoUpdaterPrivate::reuseCurrentSemanticInfo(const SemanticInfo::Source &source,
@@ -135,51 +122,47 @@ bool SemanticInfoUpdaterPrivate::reuseCurrentSemanticInfo(const SemanticInfo::So
     return false;
 }
 
-void SemanticInfoUpdaterPrivate::update_helper(QPromise<void> &promise,
-                                               const SemanticInfo::Source &source)
-{
-    FuturizedTopLevelDeclarationProcessor processor(promise);
-    update(source, true, &processor);
-}
-
 SemanticInfoUpdater::SemanticInfoUpdater()
     : d(new SemanticInfoUpdaterPrivate(this))
 {
 }
 
-SemanticInfoUpdater::~SemanticInfoUpdater()
-{
-    d->m_future.cancel();
-    d->m_future.waitForFinished();
-}
+SemanticInfoUpdater::~SemanticInfoUpdater() = default;
 
 SemanticInfo SemanticInfoUpdater::update(const SemanticInfo::Source &source)
 {
     qCDebug(log) << "update() - synchronous";
-    d->m_future.cancel();
+    d->cancelFuture();
 
     const bool emitSignalWhenFinished = false;
-    if (d->reuseCurrentSemanticInfo(source, emitSignalWhenFinished)) {
-        d->m_future = QFuture<void>();
+    if (d->reuseCurrentSemanticInfo(source, emitSignalWhenFinished))
         return semanticInfo();
-    }
 
-    return d->update(source, emitSignalWhenFinished, nullptr);
+    QPromise<SemanticInfo> dummy;
+    dummy.start();
+    doUpdate(dummy, source);
+    const SemanticInfo result = dummy.future().result();
+    d->setSemanticInfo(result, emitSignalWhenFinished);
+    return result;
 }
 
 void SemanticInfoUpdater::updateDetached(const SemanticInfo::Source &source)
 {
     qCDebug(log) << "updateDetached() - asynchronous";
-    d->m_future.cancel();
+    d->cancelFuture();
 
     const bool emitSignalWhenFinished = true;
-    if (d->reuseCurrentSemanticInfo(source, emitSignalWhenFinished)) {
-        d->m_future = QFuture<void>();
+    if (d->reuseCurrentSemanticInfo(source, emitSignalWhenFinished))
         return;
-    }
 
-    d->m_future = Utils::asyncRun(CppModelManager::sharedThreadPool(),
-                                  &SemanticInfoUpdaterPrivate::update_helper, d.data(), source);
+    d->m_watcher.reset(new QFutureWatcher<SemanticInfo>);
+    connect(d->m_watcher.get(), &QFutureWatcherBase::finished, this, [this] {
+        d->setSemanticInfo(d->m_watcher->result(), true);
+        d->m_watcher.release()->deleteLater();
+    });
+    const auto future = Utils::asyncRun(CppModelManager::sharedThreadPool(), doUpdate, source);
+    d->m_watcher->setFuture(future);
+    ExtensionSystem::PluginManager::futureSynchronizer()->addFuture(future);
 }
 
 SemanticInfo SemanticInfoUpdater::semanticInfo() const
