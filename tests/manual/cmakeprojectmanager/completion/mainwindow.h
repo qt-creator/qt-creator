380ca70d6111e7dec9a3709ebad31854073810c1
Cristian Adam <cristian.adam@qt.io>
2023-10-09 16:12:47 +0200

    CMakePM: Add manual test for code completion / navigation
    Change-Id: Id7ad47babf4c6fff8e7b823805d6ccf858a3d513
    Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

---
+++ tests/manual/cmakeprojectmanager/completion/mainwindow.h
@@ -0,0 +1,25 @@
+// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
