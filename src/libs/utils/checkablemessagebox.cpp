3a5c22c3ce6edf6f39f4069a1a760f8bda11b220
Marcus Tillmanns <marcus.tillmanns@qt.io>
2023-11-06 09:24:39 +0100

    Utils: Disable workaround for 6.6.1
    Task-number: QTBUG-118241
    Change-Id: I419ec43db8f8a86e74262b36afc9a67e8eb324eb
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/libs/utils/checkablemessagebox.cpp
+++ src/libs/utils/checkablemessagebox.cpp
@@ -60,10 +60,12 @@ static QMessageBox::StandardButton exec(
         if (text.contains("<a "))
             msgBox.setOptions(QMessageBox::Option::DontUseNativeDialog);
 
-        // Workaround for QTBUG-118241
+        // Workaround for QTBUG-118241, fixed in Qt 6.6.1
+#if QT_VERSION < QT_VERSION_CHECK(6, 6, 1)
         if (!buttonTextOverrides.isEmpty())
             msgBox.setOptions(QMessageBox::Option::DontUseNativeDialog);
     }
+#endif
 #endif
 
     if (decider.shouldAskAgain) {
