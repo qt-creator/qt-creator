76dbc9e42bb8f499ccf2a8f988d28e3c4db724cd
Alessandro Portale <alessandro.portale@qt.io>
2023-11-08 14:19:37 +0100

    Utils: Remove handling of macOS < 10.14 in theme_mac.mm
    Qt Creator should only be built and be running on macOS 10.14 or later.
    
    This cleanup is in preparation for other following changes.
    
    Task-number: QTCREATORBUG-29783
    Change-Id: Idd549051b899726f2db8fed320a2ef15e948d2d7
    Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>

--- src/libs/utils/theme/theme_mac.mm
+++ src/libs/utils/theme/theme_mac.mm
@@ -4,47 +4,29 @@
 #include "theme_mac.h"
 
 #include <QMenu>
-#include <QOperatingSystemVersion>
 
 #include <AppKit/AppKit.h>
-
-#if !QT_MACOS_PLATFORM_SDK_EQUAL_OR_ABOVE(__MAC_10_14)
-@interface NSApplication (MojaveForwardDeclarations)
-@property (strong) NSAppearance *appearance NS_AVAILABLE_MAC(10_14);
-@end
-#endif
 
 namespace Utils {
 namespace Internal {
 
 bool currentAppearanceMatches(bool dark)
 {
-#if QT_MACOS_PLATFORM_SDK_EQUAL_OR_ABOVE(__MAC_10_14)
-    if (__builtin_available(macOS 10.14, *)) {
-        auto appearance = [NSApp.effectiveAppearance
-            bestMatchFromAppearancesWithNames:@[NSAppearanceNameAqua, NSAppearanceNameDarkAqua]];
-        return
-            [appearance isEqualToString:(dark ? NSAppearanceNameDarkAqua : NSAppearanceNameAqua)];
-    }
-#endif
-    return true;
+    auto appearance = [NSApp.effectiveAppearance
+        bestMatchFromAppearancesWithNames:@[NSAppearanceNameAqua, NSAppearanceNameDarkAqua]];
+    return
+        [appearance isEqualToString:(dark ? NSAppearanceNameDarkAqua : NSAppearanceNameAqua)];
 }
 
 void forceMacAppearance(bool dark)
 {
-#if __has_builtin(__builtin_available)
-    if (__builtin_available(macOS 10.14, *))
-#else // Xcode 8
-    if (QOperatingSystemVersion::current() >= QOperatingSystemVersion(QOperatingSystemVersion::MacOS, 10, 14, 0))
-#endif
-        NSApp.appearance = [NSAppearance
-            appearanceNamed:(dark ? NSAppearanceNameDarkAqua : NSAppearanceNameAqua)];
+    NSApp.appearance = [NSAppearance
+        appearanceNamed:(dark ? NSAppearanceNameDarkAqua : NSAppearanceNameAqua)];
 }
 
 bool currentAppearanceIsDark()
 {
-    // double negation, so we get "false" for macOS 10.13
-    return !currentAppearanceMatches(false /*==light*/);
+    return currentAppearanceMatches(true);
 }
 
 void setMacOSHelpMenu(QMenu *menu)
