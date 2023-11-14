1079204a3e45af2eae1df713f6732b486680f8b4
Marcus Tillmanns <marcus.tillmanns@qt.io>
2023-11-14 13:30:13 +0100

    Utils: Fix updating BoolAspect::action() checked state
    Task-number: QTCREATORBUG-29949
    Change-Id: I1c12bd59855ecf13f06fdb16bf8e0937768527b1
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/libs/utils/aspects.cpp
+++ src/libs/utils/aspects.cpp
@@ -1838,6 +1838,8 @@ QAction *BoolAspect::action()
     connect(act, &QAction::triggered, this, [this](bool newValue) {
         setValue(newValue);
     });
+    connect(this, &BoolAspect::changed, act, [act, this] { act->setChecked(m_internal); });
+
     return act;
 }
 
