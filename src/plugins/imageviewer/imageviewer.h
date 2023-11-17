11d5fbe04cada3b844699483b740cd9dc3dc8320
hjk <hjk@qt.io>
2023-11-17 17:01:21 +0100

    ImageViewer: Move Viewer class definition to .cpp
    And de-pimpl.
    
    Change-Id: I2c4acc95a7d583b4bfbc98bee3c989425d9306c1
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/plugins/imageviewer/imageviewer.h
+++ src/plugins/imageviewer/imageviewer.h
@@ -4,48 +4,9 @@
 
 #pragma once
 
-#include <coreplugin/editormanager/ieditor.h>
+#include <QObject>
 
 namespace ImageViewer::Internal {
-
-class ImageViewerFile;
-
-class ImageViewer : public Core::IEditor
-{
-    Q_OBJECT
-
-public:
-    ImageViewer();
-    ~ImageViewer() override;
-
-    Core::IDocument *document() const override;
-    QWidget *toolBar() override;
-
-    IEditor *duplicate() override;
-
-    void exportImage();
-    void exportMultiImages();
-    void copyDataUrl();
-    void imageSizeUpdated(const QSize &size);
-    void scaleFactorUpdate(qreal factor);
-
-    void switchViewBackground();
-    void switchViewOutline();
-    void zoomIn();
-    void zoomOut();
-    void resetToOriginalSize();
-    void fitToScreen();
-    void updateToolButtons();
-    void togglePlay();
-
-private:
-    ImageViewer(const QSharedPointer<ImageViewerFile> &document);
-    void ctor();
-    void playToggled();
-    void updatePauseAction();
-
-    struct ImageViewerPrivate *d;
-};
 
 void setupImageViewer(QObject *guard);
 
