423e6e8943eda15db462cd2f33b2e81607501f40
hjk <hjk@qt.io>
2023-11-24 15:43:32 +0100

    WebAssembly: Remove now empty webassemblyplugin.h
    Change-Id: Icbf59e53545a823a537461ad9986510f10f5c57d
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>
    Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

--- src/plugins/webassembly/webassemblyplugin.cpp
+++ src/plugins/webassembly/webassemblyplugin.cpp
@@ -1,11 +1,10 @@
 // Copyright (C) 2020 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
-#include "webassemblyplugin.h"
-
 #ifdef WITH_TESTS
 #include "webassembly_test.h"
 #endif // WITH_TESTS
+
 #include "webassemblydevice.h"
 #include "webassemblyqtversion.h"
 #include "webassemblyrunconfiguration.h"
