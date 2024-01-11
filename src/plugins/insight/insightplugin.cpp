7f419fdf6423ce1e166f6c2e5484598812b2a205
hjk <hjk@qt.io>
2024-01-11 17:58:55 +0100

    Insight: Inline plugin class definition into .cpp
    Change-Id: Ic242139085bad644b98bd2355f8bf4b6796114dd
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/plugins/insight/insightplugin.cpp
+++ src/plugins/insight/insightplugin.cpp
@@ -1,31 +1,31 @@
 // Copyright (C) 2022 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0+ OR GPL-3.0 WITH Qt-GPL-exception-1.0
 
-#include "insightplugin.h"
-
 #include "insightview.h"
+
+#include <extensionsystem/iplugin.h>
 
 #include <qmldesigner/dynamiclicensecheck.h>
 #include <qmldesignerplugin.h>
 
 namespace QmlDesigner {
 
-InsightPlugin::InsightPlugin() = default;
-InsightPlugin::~InsightPlugin() = default;
-
-bool InsightPlugin::initialize(const QStringList & /*arguments*/, QString * /*errorMessage*/)
+class InsightPlugin final : public ExtensionSystem::IPlugin
 {
-    return true;
-}
+    Q_OBJECT
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QtCreatorPlugin" FILE "Insight.json")
 
-bool InsightPlugin::delayedInitialize()
-{
-    auto *designerPlugin = QmlDesignerPlugin::instance();
-    auto &viewManager = designerPlugin->viewManager();
-    viewManager.registerView(std::make_unique<InsightView>(
-        QmlDesignerPlugin::externalDependenciesForPluginInitializationOnly()));
+    bool delayedInitialize() final
+    {
+        auto *designerPlugin = QmlDesignerPlugin::instance();
+        auto &viewManager = designerPlugin->viewManager();
+        viewManager.registerView(std::make_unique<InsightView>(
+            QmlDesignerPlugin::externalDependenciesForPluginInitializationOnly()));
 
-    return true;
-}
+        return true;
+    }
+};
 
-} // namespace QmlDesigner
+} // QmlDesigner
+
+#include "insightplugin.moc"
