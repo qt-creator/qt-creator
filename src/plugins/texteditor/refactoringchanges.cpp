qt-creator/qt-creator

commit e70b99c7da49c27b519f2fcacb4e06974ef1f6ad

TextEditor: avoid autoIndent when using default TextIndenter
Most programming languages use indentation to group stuff. So if we have
no special language support for a text file it is useful to start a
newline on the same indent depth as the previous line. But if we trigger
an explicit reindent (default ctrl+i) we usually don't want to change
the indentation if we don't know exactly where it should end up. This
also applies to refactoring changes, for example from the lsp.

Change-Id: I8268dc88cbceddfc7112757fd7d2fef940ab9a39
Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>

src/plugins/texteditor/textindenter.cpp
@@ -51,6 +51,15 @@ int TextIndenter::indentFor(const QTextBlock &block,
     return tabSettings.indentationColumn(previousText);
 }
 
+void TextIndenter::autoIndent(const QTextCursor &cursor,
+                              const TabSettings &tabSettings,
+                              int cursorPositionInEditor)
+{
+    Q_UNUSED(cursor);
+    Q_UNUSED(tabSettings);
+    Q_UNUSED(cursorPositionInEditor);
+}
+
 IndentationForBlock TextIndenter::indentationForBlocks(const QVector<QTextBlock> &blocks,
                                                        const TabSettings &tabSettings,
                                                        int /*cursorPositionInEditor*/)

src/plugins/texteditor/textindenter.h
@@ -26,6 +26,10 @@ public:
                   const TabSettings &tabSettings,
                   int cursorPositionInEditor = -1) override;
 
+    void autoIndent(const QTextCursor &cursor,
+                    const TabSettings &tabSettings,
+                    int cursorPositionInEditor = -1) override;
+
     IndentationForBlock indentationForBlocks(const QVector<QTextBlock> &blocks,
                                              const TabSettings &tabSettings,
                                              int cursorPositionInEditor = -1) override;

commit ec6732511f0a7737520a2bdde5d2d9833910b7a0

TextEditor: fix autoIndent
e70b99c7da49c27b519f2fcacb4e06974ef1f6ad broke the auto indent for all
indenters because those indenters are all based on the text indenter.

Add a fallback indenter that is used if no other indenter is configured
for a document, and avoid the auto indentation only if that fallback
indenter is used.

Change-Id: I00e04a07ab14c5fc433a71d1b9826f8e4cc5c0a0
Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>
Reviewed-by: <github-actions-qt-creator@cristianadam.eu>

src/plugins/texteditor/refactoringchanges.cpp
@@ -341,7 +341,7 @@ void RefactoringFile::doFormatting()
         ICodeStylePreferencesFactory * const factory
             = TextEditorSettings::codeStyleFactory(indenterId());
         indenterOwner.reset(factory ? factory->createIndenter(document)
-                                    : new TextIndenter(document));
+                                    : new PlainTextIndenter(document));
         indenter = indenterOwner.get();
         tabSettings = TabSettings::settingsForFile(filePath());
     }

src/plugins/texteditor/textdocument.cpp
@@ -58,7 +58,7 @@ class TextDocumentPrivate
 {
 public:
     TextDocumentPrivate()
-        : m_indenter(new TextIndenter(&m_document))
+        : m_indenter(new PlainTextIndenter(&m_document))
     {
     }
 

src/plugins/texteditor/textindenter.cpp
@@ -6,7 +6,7 @@
 #include <QTextDocument>
 #include <QTextCursor>
 
-using namespace TextEditor;
+namespace TextEditor {
 
 TextIndenter::TextIndenter(QTextDocument *doc)
     : Indenter(doc)
@@ -49,15 +49,6 @@ int TextIndenter::indentFor(const QTextBlock &block,
         return 0;
 
     return tabSettings.indentationColumn(previousText);
-}
-
-void TextIndenter::autoIndent(const QTextCursor &cursor,
-                              const TabSettings &tabSettings,
-                              int cursorPositionInEditor)
-{
-    Q_UNUSED(cursor);
-    Q_UNUSED(tabSettings);
-    Q_UNUSED(cursorPositionInEditor);
 }
 
 IndentationForBlock TextIndenter::indentationForBlocks(const QVector<QTextBlock> &blocks,
@@ -135,3 +126,14 @@ std::optional<TabSettings> TextIndenter::tabSettings() const
 {
     return std::optional<TabSettings>();
 }
+
+void PlainTextIndenter::autoIndent(const QTextCursor &cursor,
+                              const TabSettings &tabSettings,
+                              int cursorPositionInEditor)
+{
+    Q_UNUSED(cursor);
+    Q_UNUSED(tabSettings);
+    Q_UNUSED(cursorPositionInEditor);
+}
+
+} // namespace TextEditor

src/plugins/texteditor/textindenter.h
@@ -26,10 +26,6 @@ public:
                   const TabSettings &tabSettings,
                   int cursorPositionInEditor = -1) override;
 
-    void autoIndent(const QTextCursor &cursor,
-                    const TabSettings &tabSettings,
-                    int cursorPositionInEditor = -1) override;
-
     IndentationForBlock indentationForBlocks(const QVector<QTextBlock> &blocks,
                                              const TabSettings &tabSettings,
                                              int cursorPositionInEditor = -1) override;
@@ -47,6 +43,16 @@ public:
                   const TabSettings &tabSettings,
                   int cursorPositionInEditor = -1) override;
     std::optional<TabSettings> tabSettings() const override;
+};
+
+class TEXTEDITOR_EXPORT PlainTextIndenter : public TextIndenter
+{
+public:
+    using TextIndenter::TextIndenter;
+
+    void autoIndent(const QTextCursor &cursor,
+                    const TabSettings &tabSettings,
+                    int cursorPositionInEditor = -1) override;
 };
 
 } // namespace TextEditor