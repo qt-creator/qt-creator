cdfdb5cb9bb5328f51baf88e42dc845327ecb051
Christian Kandeler <christian.kandeler@qt.io>
2024-01-18 13:07:54 +0100

    QmlProjectManager: Fix include
    Change-Id: I68d45d49d55487585423793bb01c522bc08b9557
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/plugins/qmlprojectmanager/qmlprojectrunconfiguration.cpp
+++ src/plugins/qmlprojectmanager/qmlprojectrunconfiguration.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
-#include "qmlbuildsystem.h"
+#include "buildsystem/qmlbuildsystem.h"
 #include "qmlmainfileaspect.h"
 #include "qmlmultilanguageaspect.h"
 #include "qmlprojectconstants.h"
