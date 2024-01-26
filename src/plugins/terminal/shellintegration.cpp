56f42c319e51bb3ef7f52487d9977a2a20749d69
Marcus Tillmanns <marcus.tillmanns@qt.io>
2024-01-26 09:46:56 +0100

    Terminal: Fix env separator
    Change-Id: I738ff27d37df95170bbdc9f55fbaf6430ebb5a40
    Reviewed-by: hjk <hjk@qt.io>
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>

--- src/plugins/terminal/shellintegration.cpp
+++ src/plugins/terminal/shellintegration.cpp
@@ -184,13 +184,17 @@ void ShellIntegration::prepareProcess(Utils::Process &process)
         rcPath.copyFile(tmpRc);
 
         env.set("CLINK_HISTORY_LABEL", "QtCreator");
-        env.appendOrSet("CLINK_PATH", tmpRc.parentDir().nativePath(), ";");
+        env.appendOrSet("CLINK_PATH",
+                        tmpRc.parentDir().nativePath(),
+                        OsSpecificAspects::pathListSeparator(env.osType()));
     } else if (cmd.executable().baseName() == "fish") {
         FilePath xdgDir = FilePath::fromUserInput(m_tempDir.filePath("fish_xdg_data"));
         FilePath subDir = xdgDir.resolvePath(QString("fish/vendor_conf.d"));
         QTC_ASSERT(subDir.createDir(), return);
         filesToCopy.fish.script.copyFile(subDir.resolvePath(filesToCopy.fish.script.fileName()));
-        env.appendOrSet("XDG_DATA_DIRS", xdgDir.toUserOutput());
+        env.appendOrSet("XDG_DATA_DIRS",
+                        xdgDir.toUserOutput(),
+                        OsSpecificAspects::pathListSeparator(env.osType()));
     }
 
     process.setCommand(cmd);
