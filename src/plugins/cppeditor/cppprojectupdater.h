0459d3dde75b287ed4a8b06341ebc7efb03db3be
hjk <hjk@qt.io>
2024-02-02 14:55:06 +0100

    CppEditor: Un-export CppProjectUpdater
    Does not seem to be used outside.
    
    Change-Id: I9ed27e20ebb23ffd59b7f557b1ff56dbcadd6a2d
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- src/plugins/cppeditor/cppprojectupdater.h
+++ src/plugins/cppeditor/cppprojectupdater.h
@@ -3,34 +3,8 @@
 
 #pragma once
 
-#include "cppeditor_global.h"
-
-#include <projectexplorer/projectupdater.h>
-#include <projectexplorer/rawprojectpart.h>
-
-#include <solutions/tasking/tasktreerunner.h>
-
-#include <utils/futuresynchronizer.h>
-
-namespace ProjectExplorer { class ExtraCompiler; }
-
-namespace CppEditor {
-
-class CPPEDITOR_EXPORT CppProjectUpdater final
-    : public QObject, public ProjectExplorer::ProjectUpdater
-{
-    Q_OBJECT
-
-public:
-    void update(const ProjectExplorer::ProjectUpdateInfo &projectUpdateInfo,
-                const QList<ProjectExplorer::ExtraCompiler *> &extraCompilers) override;
-    void cancel() override;
-
-private:
-    Utils::FutureSynchronizer m_futureSynchronizer;
-    Tasking::TaskTreeRunner m_taskTreeRunner;
-};
+namespace CppEditor::Internal {
 
 void setupCppProjectUpdater();
 
-} // namespace CppEditor
+} // CppEditor::Internal
