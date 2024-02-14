035e9485367274fd0db0fbde55d1d8fdc0b86024
Jarek Kobus <jaroslaw.kobus@qt.io>
2024-02-14 08:18:49 +0100

    Axivion: Introduce postDtoRecipe()
    For posting dto data to the server.
    
    Change-Id: Id519c311f13e7c42c85b758f97fefd79fa2c41e9
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: hjk <hjk@qt.io>

--- src/plugins/axivion/axivionplugin.cpp
+++ src/plugins/axivion/axivionplugin.cpp
@@ -402,6 +402,94 @@ static Group getDtoRecipe(const Storage<GetDtoStorage<DtoType>> &dtoStorage)
     return recipe;
 };
 
+template <typename DtoType>
+struct PostDtoStorage
+{
+    QByteArray credential;
+    QUrl url;
+    QByteArray csrfToken;
+    QByteArray writeData;
+    std::optional<DtoType> dtoData;
+};
+
+template <typename DtoType>
+static Group postDtoRecipe(const Storage<PostDtoStorage<DtoType>> &dtoStorage)
+{
+    const Storage<QByteArray> storage;
+
+    const auto onNetworkQuerySetup = [dtoStorage](NetworkQuery &query) {
+        QNetworkRequest request(dtoStorage->url);
+        request.setRawHeader("Accept", s_jsonContentType);
+        request.setRawHeader("Authorization", dtoStorage->credential);
+        const QByteArray ua = "Axivion" + QCoreApplication::applicationName().toUtf8() +
+                              "Plugin/" + QCoreApplication::applicationVersion().toUtf8();
+        request.setRawHeader("X-Axivion-User-Agent", ua);
+        request.setRawHeader("AX-CSRF-Token", dtoStorage->csrfToken);
+        query.setRequest(request);
+        query.setWriteData(dtoStorage->writeData);
+        query.setOperation(NetworkOperation::Post);
+        query.setNetworkAccessManager(&dd->m_networkAccessManager);
+    };
+
+    const auto onNetworkQueryDone = [storage](const NetworkQuery &query, DoneWith doneWith) {
+        QNetworkReply *reply = query.reply();
+        const QNetworkReply::NetworkError error = reply->error();
+        const int statusCode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+        const QString contentType = reply->header(QNetworkRequest::ContentTypeHeader)
+                                        .toString()
+                                        .split(';')
+                                        .constFirst()
+                                        .trimmed()
+                                        .toLower();
+        if (doneWith == DoneWith::Success && statusCode == httpStatusCodeOk
+            && contentType == s_jsonContentType) {
+            *storage = reply->readAll();
+            return DoneResult::Success;
+        }
+
+        const auto getError = [&]() -> Error {
+            if (contentType == s_jsonContentType) {
+                try {
+                    return DashboardError(reply->url(), statusCode,
+                                          reply->attribute(QNetworkRequest::HttpReasonPhraseAttribute).toString(),
+                                          Dto::ErrorDto::deserialize(reply->readAll()));
+                } catch (const Dto::invalid_dto_exception &) {
+                    // ignore
+                }
+            }
+            if (statusCode != 0) {
+                return HttpError(reply->url(), statusCode,
+                                 reply->attribute(QNetworkRequest::HttpReasonPhraseAttribute).toString(),
+                                 QString::fromUtf8(reply->readAll())); // encoding?
+            }
+            return NetworkError(reply->url(), error, reply->errorString());
+        };
+
+        MessageManager::writeFlashing(QStringLiteral("Axivion: %1").arg(getError().message()));
+        return DoneResult::Error;
+    };
+
+    const auto onDeserializeSetup = [storage](Async<DtoType> &task) {
+        const auto deserialize = [](QPromise<DtoType> &promise, const QByteArray &input) {
+            promise.addResult(DtoType::deserialize(input));
+        };
+        task.setFutureSynchronizer(ExtensionSystem::PluginManager::futureSynchronizer());
+        task.setConcurrentCallData(deserialize, *storage);
+    };
+
+    const auto onDeserializeDone = [dtoStorage](const Async<DtoType> &task, DoneWith doneWith) {
+        if (doneWith == DoneWith::Success)
+            dtoStorage->dtoData = task.future().result();
+    };
+
+    const Group recipe {
+        storage,
+        NetworkQueryTask(onNetworkQuerySetup, onNetworkQueryDone),
+        AsyncTask<DtoType>(onDeserializeSetup, onDeserializeDone)
+    };
+    return recipe;
+};
+
 template<typename DtoType>
 static Group fetchDataRecipe(const QUrl &url, const std::function<void(const DtoType &)> &handler)
 {
