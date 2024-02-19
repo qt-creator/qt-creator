fe08adbf65d274e5752b1e0f5e5d790026957c1a
Tim Jenssen <tim.jenssen@qt.io>
2024-02-19 17:05:59 +0100

    qds: increase to version 4.5
    Change-Id: I119eef191fda8a358457a35b2f45b1dd7565b78b
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- dist/branding/qtdesignstudio/QtCreatorIDEBranding.cmake
+++ dist/branding/qtdesignstudio/QtCreatorIDEBranding.cmake
@@ -1,7 +1,7 @@
-set(IDE_VERSION "4.4.0")                              # The IDE version.
-set(IDE_VERSION_COMPAT "4.4.0")                       # The IDE Compatibility version.
-set(IDE_VERSION_DISPLAY "4.4.0")                      # The IDE display version.
-set(IDE_COPYRIGHT_YEAR "2023")                        # The IDE current copyright year.
+set(IDE_VERSION "4.5.0")                              # The IDE version.
+set(IDE_VERSION_COMPAT "4.5.0")                       # The IDE Compatibility version.
+set(IDE_VERSION_DISPLAY "4.5.0")                      # The IDE display version.
+set(IDE_COPYRIGHT_YEAR "2024")                        # The IDE current copyright year.
 
 set(IDE_SETTINGSVARIANT "QtProject")                  # The IDE settings variation.
 set(IDE_COPY_SETTINGSVARIANT "Nokia")                 # The IDE settings to initially import.
