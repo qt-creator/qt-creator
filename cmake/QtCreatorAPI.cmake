ff04dc53ef94ff424789c1ff9a71a32417d4160b
Eike Ziller <eike.ziller@qt.io>
2024-02-19 16:11:58 +0100

    Require Qt 6.4.3 for building Qt Creator
    And remove unneeded conditions from CMake files
    
    Change-Id: Ibb4646f83599c5a91fbd18ce3489103e4150c49f
    Reviewed-by: Qt CI Bot <qt_ci_bot@qt-project.org>
    Reviewed-by: hjk <hjk@qt.io>
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>

--- cmake/QtCreatorAPI.cmake
+++ cmake/QtCreatorAPI.cmake
@@ -3,7 +3,7 @@ if(QT_CREATOR_API_DEFINED)
 endif()
 set(QT_CREATOR_API_DEFINED TRUE)
 
-set(IDE_QT_VERSION_MIN "6.2.0")
+set(IDE_QT_VERSION_MIN "6.4.3")
 
 include(${CMAKE_CURRENT_LIST_DIR}/QtCreatorAPIInternal.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/QtcSeparateDebugInfo.cmake)
