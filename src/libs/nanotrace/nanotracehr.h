7448d44dc5e118d3930214874e0e6b4af2e3155a
Marco Bubke <marco.bubke@qt.io>
2024-03-14 15:29:38 +0100

    Nanotrace: Fix unsed arguments
    The arguments must be set because we otherwise access old data.
    
    Change-Id: I18e83297b80558b112af266bd50779bef8da9e94
    Reviewed-by: Qt CI Patch Build Bot <ci_patchbuild_bot@qt.io>
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>

--- src/libs/nanotrace/nanotracehr.h
+++ src/libs/nanotrace/nanotracehr.h
@@ -273,19 +273,19 @@ inline std::string_view toArguments(std::string_view arguments)
 }
 
 template<typename String>
-void appendArguments(String &eventArguments)
+void setArguments(String &eventArguments)
 {
     eventArguments = {};
 }
 
 template<typename String>
-void appendArguments(String &eventArguments, TracerLiteral arguments)
+void setArguments(String &eventArguments, TracerLiteral arguments)
 {
     eventArguments = arguments;
 }
 
 template<typename String, typename... Arguments>
-[[maybe_unused]] void appendArguments(String &eventArguments, Arguments &&...arguments)
+[[maybe_unused]] void setArguments(String &eventArguments, Arguments &&...arguments)
 {
     static_assert(
         !std::is_same_v<String, std::string_view>,
@@ -1286,7 +1286,7 @@ private:
         traceEvent.id = id;
         traceEvent.bindId = bindId;
         traceEvent.flow = flow;
-        Internal::appendArguments(traceEvent.arguments, std::forward<Arguments>(arguments)...);
+        Internal::setArguments(traceEvent.arguments, std::forward<Arguments>(arguments)...);
         traceEvent.time = Clock::now();
     }
 
@@ -1312,7 +1312,7 @@ private:
         traceEvent.id = id;
         traceEvent.bindId = bindId;
         traceEvent.flow = flow;
-        Internal::appendArguments(traceEvent.arguments, std::forward<Arguments>(arguments)...);
+        Internal::setArguments(traceEvent.arguments, std::forward<Arguments>(arguments)...);
     }
 
     template<typename... Arguments>
@@ -1332,7 +1332,7 @@ private:
         traceEvent.id = id;
         traceEvent.bindId = 0;
         traceEvent.flow = IsFlow::No;
-        Internal::appendArguments(traceEvent.arguments, std::forward<Arguments>(arguments)...);
+        Internal::setArguments(traceEvent.arguments, std::forward<Arguments>(arguments)...);
     }
 
     CategoryFunctionPointer self() { return m_self; }
@@ -1490,10 +1490,8 @@ private:
             traceEvent.bindId = m_bindId;
             traceEvent.flow = flow;
             traceEvent.type = 'B';
-            if (sizeof...(arguments)) {
-                Internal::appendArguments<ArgumentsStringType>(traceEvent.arguments,
-                                                               std::forward<Arguments>(arguments)...);
-            }
+            Internal::setArguments<ArgumentsStringType>(traceEvent.arguments,
+                                                        std::forward<Arguments>(arguments)...);
             traceEvent.time = Clock::now();
         }
     }
@@ -1512,11 +1510,8 @@ private:
                 traceEvent.bindId = m_bindId;
                 traceEvent.flow = flow;
                 traceEvent.type = 'E';
-                if (sizeof...(arguments)) {
-                    Internal::appendArguments<ArgumentsStringType>(traceEvent.arguments,
-                                                                   std::forward<Arguments>(
-                                                                       arguments)...);
-                }
+                Internal::setArguments<ArgumentsStringType>(traceEvent.arguments,
+                                                            std::forward<Arguments>(arguments)...);
             }
         }
     }
