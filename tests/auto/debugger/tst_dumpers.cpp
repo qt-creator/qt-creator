74a17e4521fd72ba93c0581903eda25e80d54435
Christian Stenger <christian.stenger@qt.io>
2024-03-20 09:29:24 +0100

    Tests: Fix expected values
    Amends 87ca7882c1da9879bf595f6272b4aec9fdf78c99.
    
    Change-Id: I922b1afd3b62584717a913a3fcc5c1c2d2223cc8
    Reviewed-by: hjk <hjk@qt.io>

--- tests/auto/debugger/tst_dumpers.cpp
+++ tests/auto/debugger/tst_dumpers.cpp
@@ -3351,16 +3351,20 @@ void tst_Dumpers::dumper_data()
 
                + CoreProfile()
 
-               + Check("pii", "(1, 2)", "@QPair")
+               + Check("pii", "(1, 2)", "@QPair<int,int>") % Qt5
+               + Check("pii", "(1, 2)", TypeDef("std::pair<int,int>", "@QPair")) % Qt6
                + Check("pii.first", "1", "int")
                + Check("pii.second", "2", "int")
-               + Check("pis", "(1, ...)", "@QPair")
+               + Check("pis", "(1, ...)", "@QPair<int,QString>") % Qt5
+               + Check("pis", "(1, ...)", TypeDef("std::pair<int,QString>", "@QPair")) % Qt6
                + Check("pis.first", "1", "int")
                + Check("pis.second", "\"ttt\"", "@QString")
-               + Check("psi", "(..., 2)", "@QPair")
+               + Check("psi", "(..., 2)", "@QPair<QString,int>") % Qt5
+               + Check("psi", "(..., 2)", TypeDef("std::pair<QString,int>", "@QPair")) % Qt6
                + Check("psi.first", "\"sss\"", "@QString")
                + Check("psi.second", "2", "int")
-               + Check("pss", "(..., ...)", "@QPair")
+               + Check("pss", "(..., ...)", "@QPair<QString,QString>") % Qt5
+               + Check("pss", "(..., ...)", TypeDef("std::pair<QString,QString>", "@QPair")) % Qt6
                + Check("pss.first", "\"sss\"", "@QString")
                + Check("pss.second", "\"ttt\"", "@QString");
 
