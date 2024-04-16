3e42b144badeb6cb1aedf9077489745c9363b20b
Knud Dollereder <knud.dollereder@qt.io>
2024-04-16 15:08:02 +0200

    QmlDesigner: Fix curveditor playhead update issues
    Change-Id: I04b3502395f0b0b25bca785acf220f69ba3428bf
    Reviewed-by: Qt CI Patch Build Bot <ci_patchbuild_bot@qt.io>
    Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

--- src/plugins/qmldesigner/components/curveeditor/curveeditor.cpp
+++ src/plugins/qmldesigner/components/curveeditor/curveeditor.cpp
@@ -77,8 +77,8 @@ CurveEditor::CurveEditor(CurveEditorModel *model, QWidget *parent)
 
     connect(m_toolbar, &CurveEditorToolBar::currentFrameChanged, [this, model](int frame) {
         model->setCurrentFrame(frame);
+        m_view->setCurrentFrame(frame, false);
         updateStatusLine();
-        m_view->viewport()->update();
     });
 
     connect(m_toolbar, &CurveEditorToolBar::zoomChanged, [this](double zoom) {
