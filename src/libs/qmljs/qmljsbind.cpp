f1fdd8a61903a5e2d1d5f5380f11f38c90aad367
Christian Stenger <christian.stenger@qt.io>
2024-04-17 06:55:51 +0200

    QmlJS: Fix completion of enum
    Keys of an enum value are accessible through the object
    wherein the enum is declared.
    
    Task-number: QTCREATORBUG-19226
    Change-Id: I3c4426973eaa13118af498bf750afcb86487610a
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>

--- src/libs/qmljs/qmljsbind.cpp
+++ src/libs/qmljs/qmljsbind.cpp
@@ -339,6 +339,13 @@ bool Bind::visit(UiEnumDeclaration *ast)
         UiEnumValue *value = new UiEnumValue(ast, &_valueOwner, _currentObjectValue->originId());
         _qmlObjects.insert(ast, value);
         _currentObjectValue->setMember(ast->name, value);
+        // add enum value's keys as member to its parent object
+        for (auto it = ast->members; it; it = it->next) {
+            const QString name = it->member.toString();
+            _currentObjectValue->setMember(name, _valueOwner.intValue());
+            _currentObjectValue->setPropertyInfo(
+                        name, PropertyInfo(PropertyInfo::Readable | PropertyInfo::ValueType));
+        }
     }
     return true;
 }
