ec88d279a8d52189685008c8fe46371d13d2505f
David Schulz <david.schulz@qt.io>
2024-04-22 10:18:09 +0200

    TextEditor: Optimize unindent backspace behavior
    Only remove more than one character when the cursor is inside the
    indentation in the beginning of the line. In all other cases always only
    remove one character.
    
    Fixes: QTCREATORBUG-30725
    Change-Id: I973101a95768cdd8b1a318972f53423eb72eb157
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>
    Reviewed-by: André Hartmann <aha_1980@gmx.de>
    Reviewed-by: <github-actions-qt-creator@cristianadam.eu>

--- src/plugins/texteditor/texteditor.cpp
+++ src/plugins/texteditor/texteditor.cpp
@@ -6876,8 +6876,7 @@ void TextEditorWidgetPrivate::handleBackspaceKey()
                 }
             }
         } else if (typingSettings.m_smartBackspaceBehavior == TypingSettings::BackspaceUnindents) {
-            const QChar previousChar = q->document()->characterAt(pos - 1);
-            if (!(previousChar == QLatin1Char(' ') || previousChar == QLatin1Char('\t'))) {
+            if (c.positionInBlock() > TabSettings::firstNonSpace(c.block().text())) {
                 if (cursorWithinSnippet)
                     c.beginEditBlock();
                 c.deletePreviousChar();
