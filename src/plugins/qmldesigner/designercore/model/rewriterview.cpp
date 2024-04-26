0c1009608722635a26ee0924b4cf5b5f3e8476c2
Miikka Heikkinen <miikka.heikkinen@qt.io>
2024-04-26 16:00:07 +0300

    QmlDesigner: Restore auxiliary properties after rewriter model attach
    Successfully restoring auxiliary properties requires scene model to be
    complete, so we must wait until model is attached.
    
    Fixes: QDS-12615
    Change-Id: I906e9b50df14fcdbc0cad8370c5466db5c1aab02
    Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>
    (cherry picked from commit 7ba7624bb6c778498f66963c4a210458a66d1df8)

--- src/plugins/qmldesigner/designercore/model/rewriterview.cpp
+++ src/plugins/qmldesigner/designercore/model/rewriterview.cpp
@@ -104,6 +104,7 @@ void RewriterView::modelAttached(Model *model)
         m_modelAttachPending = true;
         QTimer::singleShot(1000, this, [this, model](){
             modelAttached(model);
+            restoreAuxiliaryData();
         });
     }
 }
