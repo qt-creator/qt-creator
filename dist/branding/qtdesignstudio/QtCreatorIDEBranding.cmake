b7cab31fd0bdc8e39fe4ee2b1e01f75ea948a03a
Tim Jenssen <tim.jenssen@qt.io>
2024-05-24 15:11:05 +0200

    qds: prepare for a Qt Desgin Studio 4.5.1
    Change-Id: I9b46def5f98f9d16a285b41ec963b36e875cc70c
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- dist/branding/qtdesignstudio/QtCreatorIDEBranding.cmake
+++ dist/branding/qtdesignstudio/QtCreatorIDEBranding.cmake
@@ -1,6 +1,6 @@
-set(IDE_VERSION "4.5.0")                              # The IDE version.
-set(IDE_VERSION_COMPAT "4.5.0")                       # The IDE Compatibility version.
-set(IDE_VERSION_DISPLAY "4.5.0")                      # The IDE display version.
+set(IDE_VERSION "4.5.1")                              # The IDE version.
+set(IDE_VERSION_COMPAT "4.5.1")                       # The IDE Compatibility version.
+set(IDE_VERSION_DISPLAY "4.5.1")                      # The IDE display version.
 set(IDE_COPYRIGHT_YEAR "2024")                        # The IDE current copyright year.
 
 set(IDE_SETTINGSVARIANT "QtProject")                  # The IDE settings variation.
