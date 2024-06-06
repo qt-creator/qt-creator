qt-creator/qt-creator

commit 903d01b93459d0f1ee70e6ca74a8c01af5e0981b

Welcome: Implement new design
2024 redesign

Change-Id: I6629849921272d856f201693973a8e29c6465e94
Reviewed-by: hjk <hjk@qt.io>
Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

src/plugins/marketplace/qtmarketplacewelcomepage.cpp
@@ -9,9 +9,10 @@
 #include <coreplugin/welcomepagehelper.h>
 
 #include <utils/fancylineedit.h>
+#include <utils/layoutbuilder.h>
 #include <utils/progressindicator.h>
-#include <utils/theme/theme.h>
 #include <utils/qtcassert.h>
+#include <utils/theme/theme.h>
 
 #include <QDesktopServices>
 #include <QLabel>
@@ -44,38 +45,34 @@ class QtMarketplacePageWidget : public QWidget
 public:
     QtMarketplacePageWidget()
     {
-        auto searchBox = new Core::SearchBox(this);
-        m_searcher = searchBox->m_lineEdit;
+        m_searcher = new Core::SearchBox(this);
         m_searcher->setPlaceholderText(Tr::tr("Search in Marketplace..."));
 
-        auto vbox = new QVBoxLayout(this);
-        vbox->setContentsMargins(0, 0, 0, Core::WelcomePageHelpers::ItemGap);
-        vbox->setSpacing(Core::WelcomePageHelpers::ItemGap);
-
-        auto searchBar = Core::WelcomePageHelpers::panelBar();
-        auto hbox = new QHBoxLayout(searchBar);
-        hbox->setContentsMargins(Core::WelcomePageHelpers::HSpacing, 0,
-                                 Core::WelcomePageHelpers::HSpacing, 0);
-        hbox->addWidget(searchBox);
-        vbox->addWidget(searchBar);
         m_errorLabel = new QLabel(this);
         m_errorLabel->setVisible(false);
-        vbox->addWidget(m_errorLabel);
 
-        auto resultWidget = new QWidget(this);
-        auto resultHBox = new QHBoxLayout(resultWidget);
-        resultHBox->setContentsMargins(Core::WelcomePageHelpers::HSpacing, 0, 0, 0);
         m_sectionedProducts = new SectionedProducts(this);
         auto progressIndicator = new Utils::ProgressIndicator(ProgressIndicatorSize::Large, this);
         progressIndicator->attachToWidget(m_sectionedProducts);
         progressIndicator->hide();
-        resultHBox->addWidget(m_sectionedProducts);
-        vbox->addWidget(resultWidget);
+
+        using namespace StyleHelper::SpacingTokens;
+
+        using namespace Layouting;
+        Column {
+            Row {
+                m_searcher,
+                customMargin({0, 0, ExVPaddingGapXl, 0}),
+            },
+            m_sectionedProducts,
+            spacing(VPaddingL),
+            customMargin({ExVPaddingGapXl, ExVPaddingGapXl, 0, 0}),
+        }.attachTo(this);
 
         connect(m_sectionedProducts, &SectionedProducts::toggleProgressIndicator,
                 progressIndicator, &Utils::ProgressIndicator::setVisible);
         connect(m_sectionedProducts, &SectionedProducts::errorOccurred, this,
-                [this, progressIndicator, searchBox](int, const QString &message) {
+                [this, progressIndicator](int, const QString &message) {
             progressIndicator->hide();
             progressIndicator->deleteLater();
             m_errorLabel->setAlignment(Qt::AlignHCenter);
@@ -89,7 +86,7 @@ public:
                         "</p><br/><p><small><i>Error: %1</i></small></p>").arg(message);
             m_errorLabel->setText(txt);
             m_errorLabel->setVisible(true);
-            searchBox->setVisible(false);
+            m_searcher->setVisible(false);
             connect(m_errorLabel, &QLabel::linkActivated,
                     this, []() { QDesktopServices::openUrl(QUrl("https://marketplace.qt.io")); });
         });

share/qtcreator/themes/dark.creatortheme
@@ -1,4 +1,5 @@
 [General]
+Includes=dark.figmatokens
 ThemeName=Dark
 PreferredStyles=
 DefaultTextEditorColorScheme=dark.xml
@@ -405,39 +406,6 @@ Debugger_WatchItem_ValueInvalid=textDisabled
 Debugger_WatchItem_ValueChanged=ffff6666
 
 Debugger_Breakpoint_TextMarkColor=ffff4040
-
-; Qt Creator Color Tokens - dark mode
-Token_Basic_Black=ff131313
-Token_Basic_White=fff8f8f8
-Token_Accent_Default=ff23b26a
-Token_Accent_Muted=ff1f9b5d
-Token_Accent_Subtle=ff1a8550
-Token_Background_Default=ff1f1f1f
-Token_Background_Muted=ff262626
-Token_Background_Subtle=ff2e2e2e
-Token_Foreground_Default=ff5a5a5a
-Token_Foreground_Muted=ff3e3e3e
-Token_Foreground_Subtle=ff303030
-Token_Text_Default=fff8f8f8
-Token_Text_Muted=ffaeaeae
-Token_Text_Subtle=ff595959
-Token_Stroke_Strong=ffeeeeee
-Token_Stroke_Muted=ff727272
-Token_Stroke_Subtle=ff3a3a3a
-Token_Notification_Alert=ffc98014
-Token_Notification_Success=ff1f9b5d
-Token_Notification_Neutral=ff016876
-Token_Notification_Danger=ffb22245
-
-Welcome_TextColor=text
-Welcome_ForegroundPrimaryColor=ffa3a3a3
-Welcome_ForegroundSecondaryColor=ff808080
-Welcome_BackgroundPrimaryColor=normalBackground
-Welcome_BackgroundSecondaryColor=shadowBackground
-Welcome_HoverColor=ff404040
-Welcome_AccentColor=ff57d658
-Welcome_LinkColor=ff67e668
-Welcome_DisabledLinkColor=textDisabled
 
 Timeline_TextColor=text
 Timeline_BackgroundColor1=normalBackground

share/qtcreator/themes/dark.figmatokens
@@ -1,0 +1,31 @@
+; Qt Creator Color Tokens - dark mode
+
+[Colors]
+
+Token_Basic_Black=ff131313
+Token_Basic_White=fff8f8f8
+
+Token_Accent_Default=ff23b26a
+Token_Accent_Muted=ff1f9b5d
+Token_Accent_Subtle=ff1a8550
+
+Token_Background_Default=ff1f1f1f
+Token_Background_Muted=ff262626
+Token_Background_Subtle=ff2e2e2e
+
+Token_Foreground_Default=ff5a5a5a
+Token_Foreground_Muted=ff3e3e3e
+Token_Foreground_Subtle=ff303030
+
+Token_Text_Default=fff8f8f8
+Token_Text_Muted=ffaeaeae
+Token_Text_Subtle=ff595959
+
+Token_Stroke_Strong=ffeeeeee
+Token_Stroke_Muted=ff727272
+Token_Stroke_Subtle=ff3a3a3a
+
+Token_Notification_Alert=ffc98014
+Token_Notification_Success=ff1f9b5d
+Token_Notification_Neutral=ff016876
+Token_Notification_Danger=ffb22245

share/qtcreator/themes/default.creatortheme
@@ -1,4 +1,5 @@
 [General]
+Includes=light.figmatokens
 ThemeName=Classic
 PreferredStyles=
 
@@ -397,39 +398,6 @@ Debugger_WatchItem_ValueInvalid=ff8c8c8c
 Debugger_WatchItem_ValueChanged=ffc80000
 
 Debugger_Breakpoint_TextMarkColor=ffff4040
-
-; Qt Creator Color Tokens - light mode
-Token_Basic_Black=ff131313
-Token_Basic_White=fff2f2f2
-Token_Accent_Default=ff23b26a
-Token_Accent_Muted=ff1f9b5d
-Token_Accent_Subtle=ff1a8550
-Token_Background_Default=ffe3e3e3
-Token_Background_Muted=ffeeeeee
-Token_Background_Subtle=fffbfbfb
-Token_Foreground_Default=ffcdcdcd
-Token_Foreground_Muted=ffd5d5d5
-Token_Foreground_Subtle=ffdddddd
-Token_Text_Default=ff393939
-Token_Text_Muted=ff7c7c7c
-Token_Text_Subtle=ffbebebe
-Token_Stroke_Strong=ff464646
-Token_Stroke_Muted=ff727272
-Token_Stroke_Subtle=ffcdcdcd
-Token_Notification_Alert=ffeb991f
-Token_Notification_Success=ff23b26a
-Token_Notification_Neutral=ff0e7887
-Token_Notification_Danger=ffdc1343
-
-Welcome_TextColor=ff000000
-Welcome_ForegroundPrimaryColor=shadowBackground
-Welcome_ForegroundSecondaryColor=ff939393
-Welcome_BackgroundPrimaryColor=fffafafa
-Welcome_BackgroundSecondaryColor=ffffffff
-Welcome_HoverColor=ffefefef
-Welcome_AccentColor=ff45ce55
-Welcome_LinkColor=ff20a020
-Welcome_DisabledLinkColor=textDisabled
 
 Timeline_TextColor=darkText
 Timeline_BackgroundColor1=ffffffff

share/qtcreator/themes/design-light.creatortheme
@@ -1,4 +1,5 @@
 [General]
+Includes=light.figmatokens
 ThemeName=Design Light
 PreferredStyles=
 
@@ -409,39 +410,6 @@ Debugger_WatchItem_ValueInvalid=textDisabled
 Debugger_WatchItem_ValueChanged=ffbf0303
 
 Debugger_Breakpoint_TextMarkColor=ffff4040
-
-; Qt Creator Color Tokens - light mode
-Token_Basic_Black=ff131313
-Token_Basic_White=fff2f2f2
-Token_Accent_Default=ff23b26a
-Token_Accent_Muted=ff1f9b5d
-Token_Accent_Subtle=ff1a8550
-Token_Background_Default=ffe3e3e3
-Token_Background_Muted=ffeeeeee
-Token_Background_Subtle=fffbfbfb
-Token_Foreground_Default=ffcdcdcd
-Token_Foreground_Muted=ffd5d5d5
-Token_Foreground_Subtle=ffdddddd
-Token_Text_Default=ff393939
-Token_Text_Muted=ff7c7c7c
-Token_Text_Subtle=ffbebebe
-Token_Stroke_Strong=ff464646
-Token_Stroke_Muted=ff727272
-Token_Stroke_Subtle=ffcdcdcd
-Token_Notification_Alert=ffeb991f
-Token_Notification_Success=ff23b26a
-Token_Notification_Neutral=ff0e7887
-Token_Notification_Danger=ffdc1343
-
-Welcome_TextColor=ff000000
-Welcome_ForegroundPrimaryColor=ff404040
-Welcome_ForegroundSecondaryColor=ff727272
-Welcome_BackgroundPrimaryColor=ffeaeaea
-Welcome_BackgroundSecondaryColor=ffefefef
-Welcome_HoverColor=ffe1e1e1
-Welcome_AccentColor=ff25709a
-Welcome_LinkColor=ff104090
-Welcome_DisabledLinkColor=textDisabled
 
 Timeline_TextColor=text
 Timeline_BackgroundColor1=normalBackground

share/qtcreator/themes/design.creatortheme
@@ -1,4 +1,5 @@
 [General]
+Includes=dark.figmatokens
 ThemeName=Design Dark
 PreferredStyles=
 DefaultTextEditorColorScheme=creator-dark.xml
@@ -413,39 +414,6 @@ Debugger_WatchItem_ValueInvalid=textDisabled
 Debugger_WatchItem_ValueChanged=ffff6666
 
 Debugger_Breakpoint_TextMarkColor=ffff4040
-
-; Qt Creator Color Tokens - dark mode
-Token_Basic_Black=ff131313
-Token_Basic_White=fff8f8f8
-Token_Accent_Default=ff23b26a
-Token_Accent_Muted=ff1f9b5d
-Token_Accent_Subtle=ff1a8550
-Token_Background_Default=ff1f1f1f
-Token_Background_Muted=ff262626
-Token_Background_Subtle=ff2e2e2e
-Token_Foreground_Default=ff5a5a5a
-Token_Foreground_Muted=ff3e3e3e
-Token_Foreground_Subtle=ff303030
-Token_Text_Default=fff8f8f8
-Token_Text_Muted=ffaeaeae
-Token_Text_Subtle=ff595959
-Token_Stroke_Strong=ffeeeeee
-Token_Stroke_Muted=ff727272
-Token_Stroke_Subtle=ff3a3a3a
-Token_Notification_Alert=ffc98014
-Token_Notification_Success=ff1f9b5d
-Token_Notification_Neutral=ff016876
-Token_Notification_Danger=ffb22245
-
-Welcome_TextColor=text
-Welcome_ForegroundPrimaryColor=ffa3a3a3
-Welcome_ForegroundSecondaryColor=ff808080
-Welcome_BackgroundPrimaryColor=ff242424
-Welcome_BackgroundSecondaryColor=ff1c1c1c
-Welcome_HoverColor=ff2b2a2a
-Welcome_AccentColor=ff3f8ccc
-Welcome_LinkColor=ff5fafef
-Welcome_DisabledLinkColor=textDisabled
 
 Timeline_TextColor=text
 Timeline_BackgroundColor1=normalBackground

share/qtcreator/themes/flat-dark.creatortheme
@@ -1,4 +1,5 @@
 [General]
+Includes=dark.figmatokens
 ThemeName=Flat Dark
 PreferredStyles=
 DefaultTextEditorColorScheme=creator-dark.xml
@@ -409,39 +410,6 @@ Debugger_WatchItem_ValueInvalid=textDisabled
 Debugger_WatchItem_ValueChanged=ffff6666
 
 Debugger_Breakpoint_TextMarkColor=ffff4040
-
-; Qt Creator Color Tokens - dark mode
-Token_Basic_Black=ff131313
-Token_Basic_White=fff8f8f8
-Token_Accent_Default=ff23b26a
-Token_Accent_Muted=ff1f9b5d
-Token_Accent_Subtle=ff1a8550
-Token_Background_Default=ff1f1f1f
-Token_Background_Muted=ff262626
-Token_Background_Subtle=ff2e2e2e
-Token_Foreground_Default=ff5a5a5a
-Token_Foreground_Muted=ff3e3e3e
-Token_Foreground_Subtle=ff303030
-Token_Text_Default=fff8f8f8
-Token_Text_Muted=ffaeaeae
-Token_Text_Subtle=ff595959
-Token_Stroke_Strong=ffeeeeee
-Token_Stroke_Muted=ff727272
-Token_Stroke_Subtle=ff3a3a3a
-Token_Notification_Alert=ffc98014
-Token_Notification_Success=ff1f9b5d
-Token_Notification_Neutral=ff016876
-Token_Notification_Danger=ffb22245
-
-Welcome_TextColor=text
-Welcome_ForegroundPrimaryColor=ff999999
-Welcome_ForegroundSecondaryColor=ff808080
-Welcome_BackgroundPrimaryColor=normalBackground
-Welcome_BackgroundSecondaryColor=ff242628
-Welcome_HoverColor=ff404243
-Welcome_AccentColor=ff36c148
-Welcome_LinkColor=ff5fcf4f
-Welcome_DisabledLinkColor=textDisabled
 
 Timeline_TextColor=text
 Timeline_BackgroundColor1=normalBackground

share/qtcreator/themes/flat-light.creatortheme
@@ -1,4 +1,5 @@
 [General]
+Includes=light.figmatokens
 ThemeName=Flat Light
 PreferredStyles=
 
@@ -406,39 +407,6 @@ Debugger_WatchItem_ValueInvalid=textDisabled
 Debugger_WatchItem_ValueChanged=ffbf0303
 
 Debugger_Breakpoint_TextMarkColor=ffff4040
-
-; Qt Creator Color Tokens - light mode
-Token_Basic_Black=ff131313
-Token_Basic_White=fff2f2f2
-Token_Accent_Default=ff23b26a
-Token_Accent_Muted=ff1f9b5d
-Token_Accent_Subtle=ff1a8550
-Token_Background_Default=ffe3e3e3
-Token_Background_Muted=ffeeeeee
-Token_Background_Subtle=fffbfbfb
-Token_Foreground_Default=ffcdcdcd
-Token_Foreground_Muted=ffd5d5d5
-Token_Foreground_Subtle=ffdddddd
-Token_Text_Default=ff393939
-Token_Text_Muted=ff7c7c7c
-Token_Text_Subtle=ffbebebe
-Token_Stroke_Strong=ff464646
-Token_Stroke_Muted=ff727272
-Token_Stroke_Subtle=ffcdcdcd
-Token_Notification_Alert=ffeb991f
-Token_Notification_Success=ff23b26a
-Token_Notification_Neutral=ff0e7887
-Token_Notification_Danger=ffdc1343
-
-Welcome_TextColor=ff000000
-Welcome_ForegroundPrimaryColor=ff232323
-Welcome_ForegroundSecondaryColor=ff939393
-Welcome_BackgroundPrimaryColor=fffafafa
-Welcome_BackgroundSecondaryColor=ffffffff
-Welcome_HoverColor=ffefefef
-Welcome_AccentColor=ff45ce55
-Welcome_LinkColor=ff20a020
-Welcome_DisabledLinkColor=textDisabled
 
 Timeline_TextColor=text
 Timeline_BackgroundColor1=normalBackground

commit ac86e2328bf5ce698bd70f4be409b0ba8e71f015

Marketplace: Fix layout
Only noticeable with network issues.
Broke with 903d01b93459d0f1ee70e6ca74a8c01af5e0981b.

Change-Id: I85e35c951b4c28b9201362ed17d4dae529ccb041
Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

src/plugins/marketplace/qtmarketplacewelcomepage.cpp
@@ -62,6 +62,7 @@ public:
         Column {
             Row {
                 m_searcher,
+                m_errorLabel,
                 customMargins(0, 0, ExVPaddingGapXl, 0),
             },
             m_sectionedProducts,