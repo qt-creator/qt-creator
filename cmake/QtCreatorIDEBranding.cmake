f22660facbb8dd4cab83a21a092f7207df675e17
Eike Ziller <eike.ziller@qt.io>
2024-06-14 12:39:45 +0200

    Bump version to 14.0.0-beta2
    Change-Id: I1ca19c9cb6f7d8bfec665300c23389ba22e93c1e
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- cmake/QtCreatorIDEBranding.cmake
+++ cmake/QtCreatorIDEBranding.cmake
@@ -1,6 +1,6 @@
-set(IDE_VERSION "13.0.82")                            # The IDE version.
-set(IDE_VERSION_COMPAT "13.0.82")                     # The IDE Compatibility version.
-set(IDE_VERSION_DISPLAY "14.0.0-beta1")               # The IDE display version.
+set(IDE_VERSION "13.0.83")                            # The IDE version.
+set(IDE_VERSION_COMPAT "13.0.83")                     # The IDE Compatibility version.
+set(IDE_VERSION_DISPLAY "14.0.0-beta2")               # The IDE display version.
 set(IDE_COPYRIGHT_YEAR "2024")                        # The IDE current copyright year.
 
 set(IDE_SETTINGSVARIANT "QtProject")                  # The IDE settings variation.
