6ca917c123dd38b20056276922785793a99dcf9c
David Schulz <david.schulz@qt.io>
2024-06-25 08:26:23 +0200

    Core: remove unneeded template of mimeTypeFactoryLookup
    Change-Id: I2ca6defe1054b0ac06aa134c099a77f253d61a02
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/plugins/coreplugin/editormanager/ieditorfactory.cpp
+++ src/plugins/coreplugin/editormanager/ieditorfactory.cpp
@@ -16,15 +16,14 @@ namespace Core {
 
 /* Find the one best matching the mimetype passed in.
  * Recurse over the parent classes of the mimetype to find them. */
-template<class EditorTypeLike>
 static void mimeTypeFactoryLookup(const Utils::MimeType &mimeType,
-                                  const QList<EditorTypeLike *> &allFactories,
-                                  QList<EditorTypeLike *> *list)
+                                  const QList<IEditorFactory *> &allFactories,
+                                  QList<IEditorFactory *> *list)
 {
-    QSet<EditorTypeLike *> matches;
+    QSet<IEditorFactory *> matches;
     Utils::visitMimeParents(mimeType, [&](const Utils::MimeType &mt) -> bool {
         // check for matching factories
-        for (EditorTypeLike *factory : allFactories) {
+        for (IEditorFactory *factory : allFactories) {
             if (!matches.contains(factory)) {
                 const QStringList mimeTypes = factory->mimeTypes();
                 for (const QString &mimeName : mimeTypes) {
