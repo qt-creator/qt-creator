d309163c94a02c2d74800346878fc4309184356e
Marco Bubke <marco.bubke@qt.io>
2024-07-24 16:16:46 +0200

    QmlDesigner: Remove workaround code for PropertyName
    Since we now use PropertyNameView, we can remove this workaround.
    
    Change-Id: Id7c46857ee60579f8b27e8e4cbc4d189dc928bd0
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- src/plugins/qmldesigner/designercore/include/modelfwd.h
+++ src/plugins/qmldesigner/designercore/include/modelfwd.h
@@ -14,39 +14,6 @@ using PropertyName = QByteArray;
 using PropertyNameView = QByteArrayView;
 using PropertyNameList = QList<PropertyName>;
 
-template<std::size_t Size, const char Text[Size]>
-class ByteArrayViewLiteral : public QByteArrayView
-{
-public:
-    constexpr ByteArrayViewLiteral() noexcept
-        : QByteArrayView{Text, Size - 1}
-    {}
-
-    const QByteArray &toByteArray() const noexcept
-    {
-        static auto b = QByteArray::fromRawData(Text, Size - 1);
-
-        return b;
-    }
-
-    operator const QByteArray &() const noexcept { return toByteArray(); }
-};
-
-#define MakeCppPropertyViewLiteral(name, text) \
-    constexpr char name##InternalString[] = text; \
-    constexpr ByteArrayViewLiteral<sizeof(name##InternalString), name##InternalString> name{}; \
-    /* */
-
-#define MakeHeaderPropertyViewLiteral(name, text) \
-    inline constexpr char name##InternalString[] = text; \
-    inline constexpr ByteArrayViewLiteral<sizeof(name##InternalString), name##InternalString> name{}; \
-    /* */
-
-#define MakeFunctionPropertyViewLiteral(name, text) \
-    static constexpr char name##InternalString[] = text; \
-    static constexpr ByteArrayViewLiteral<sizeof(name##InternalString), name##InternalString> name{}; \
-    /* */
-
 using TypeName = QByteArray;
 using TypeNameView = QByteArrayView;
 using PropertyTypeList = QList<PropertyName>;
