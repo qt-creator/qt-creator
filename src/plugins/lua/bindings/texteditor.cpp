b7baf5a7362100161a5d54cefbdbf5e0b9e7070b
Christian Kandeler <christian.kandeler@qt.io>
2024-07-29 14:58:04 +0200

    Lua: Fix include
    Change-Id: I48cab5517d2f5358b88b1d97a8e3f5d92452ab85
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/plugins/lua/bindings/texteditor.cpp
+++ src/plugins/lua/bindings/texteditor.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 #include "../luaengine.h"
-#include "./luatr.h"
+#include "../luatr.h"
 
 #include <texteditor/basehoverhandler.h>
 #include <texteditor/textdocument.h>
