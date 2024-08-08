qt-creator/qt-creator

commit d079385d8147df8848898f6f7d8c688dd6599ab6

QmlDesigner: Fix build
Seems the CI don't catched it.

Change-Id: I0a68080162740cabc7c2ee63927abd185e02ce5a
Reviewed-by: Mahmoud Badri <mahmoud.badri@qt.io>

src/plugins/qmldesigner/components/contentlibrary/contentlibraryview.cpp
@@ -429,7 +429,8 @@ void ContentLibraryView::auxiliaryDataChanged(const ModelNode &,
         active3DSceneChanged(data.toInt());
 }
 
-void ContentLibraryView::modelNodePreviewPixmapChanged(const ModelNode &node, const QPixmap &pixmap,
+void ContentLibraryView::modelNodePreviewPixmapChanged(const ModelNode &,
+                                                       const QPixmap &pixmap,
                                                        const QByteArray &requestId)
 {
     if (requestId == ADD_ITEM_REQ_ID)
@@ -496,6 +497,19 @@ void ContentLibraryView::applyBundleMaterialToDropTarget(const ModelNode &bundle
     });
 }
 #endif
+
+namespace {
+Utils::FilePath componentPath([[maybe_unused]] const NodeMetaInfo &metaInfo)
+{
+#ifdef QDS_USE_PROJECTSTORAGE
+    // TODO
+    return {};
+#else
+    return Utils::FilePath::fromString(metaInfo.importDirectoryPath());
+#endif
+}
+
+} // namespace
 
 QPair<QString, QSet<AssetPath>> ContentLibraryView::modelNodeToQmlString(const ModelNode &node, int depth)
 {
@@ -590,7 +604,7 @@ QPair<QString, QSet<AssetPath>> ContentLibraryView::modelNodeToQmlString(const M
 
         if (depth > 0) {
             // add component file to the dependency assets
-            Utils::FilePath compFilePath = Utils::FilePath::fromString(node.metaInfo().componentFileName());
+            Utils::FilePath compFilePath = componentPath(node.metaInfo());
             assets.insert({compFilePath.parentDir().path(), compFilePath.fileName()});
         }
 
@@ -1194,7 +1208,7 @@ QSet<AssetPath> ContentLibraryView::getBundleComponentDependencies(const ModelNo
 {
     const QString compFileName = node.simplifiedTypeName() + ".qml";
 
-    Utils::FilePath compPath = Utils::FilePath::fromString(node.metaInfo().importDirectoryPath());
+    Utils::FilePath compPath = componentPath(node.metaInfo());
 
     QTC_ASSERT(compPath.exists(), return {});
 

commit 34ca4b9af75c5374eb6f0c6ed7493675c4a790fe

QmlDesigner: Fix resolving component path in content lib
Regression introduced by d079385d8147df8848898f6f7d8c688dd6599ab6

Change-Id: I1c60ac385e514ce8ff77503eedde52a97249b218
Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>

src/plugins/qmldesigner/components/contentlibrary/contentlibraryview.cpp
@@ -505,7 +505,7 @@ Utils::FilePath componentPath([[maybe_unused]] const NodeMetaInfo &metaInfo)
     // TODO
     return {};
 #else
-    return Utils::FilePath::fromString(metaInfo.importDirectoryPath());
+    return Utils::FilePath::fromString(metaInfo.componentFileName());
 #endif
 }
 