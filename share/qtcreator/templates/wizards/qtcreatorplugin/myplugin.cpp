29b8e613f4bc7483bfd554d2d1ee5c70759f4e02
hjk <hjk@qt.io>
2024-08-09 12:37:39 +0200

    Share: Modernize plugin template a bit
    Change-Id: I36158238a21c603bcd43d4e5ebbd6c581fa750e6
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- share/qtcreator/templates/wizards/qtcreatorplugin/myplugin.cpp
+++ share/qtcreator/templates/wizards/qtcreatorplugin/myplugin.cpp
@@ -18,31 +18,29 @@
 
 namespace %{PluginName}::Internal {
 
-class %{CN} : public ExtensionSystem::IPlugin
+class %{CN} final : public ExtensionSystem::IPlugin
 {
     Q_OBJECT
     Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QtCreatorPlugin" FILE "%{PluginName}.json")
 
 public:
-    %{CN}()
-    {
-        // Create your members
-    }
+    %{CN}() = default;
 
     ~%{CN}() final
     {
         // Unregister objects from the plugin manager's object pool
-        // Delete members
+        // Other cleanup, if needed.
     }
 
     void initialize() final
     {
-        // Register objects in the plugin manager's object pool
+        // Set up this plugin's factories, if needed.
+        // Register objects in the plugin manager's object pool, if needed. (rare)
         // Load settings
         // Add actions to menus
         // Connect to other plugins' signals
         // In the initialize function, a plugin can be sure that the plugins it
-        // depends on have initialized their members.
+        // depends on have passed their initialize() phase.
 
         // If you need access to command line arguments or to report errors, use the
         //    bool IPlugin::initialize(const QStringList &arguments, QString *errorString)
@@ -62,9 +60,10 @@ public:
 
     void extensionsInitialized() final
     {
-        // Retrieve objects from the plugin manager's object pool
+        // Retrieve objects from the plugin manager's object pool, if needed. (rare)
         // In the extensionsInitialized function, a plugin can be sure that all
-        // plugins that depend on it are completely initialized.
+        // plugins that depend on it have passed their initialize() and
+        // extensionsInitialized() phase.
     }
 
     ShutdownFlag aboutToShutdown() final
