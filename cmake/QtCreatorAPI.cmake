88323e429ff5d3affeae10b504be1f6d55431684
Thomas Hartmann <thomas.hartmann@qt.io>
2024-08-30 11:23:48 +0000

    Revert "Enforce that plugins can't be used as library dependencies"
    This reverts commit f5bb913c9da376fed78caff2639eb2f1f2f3765e.
    
    Reason for revert: This breaks snapshots build for the license plugin
    
    Change-Id: I52a9f0b40081d648de68a5bdc3d9b3962e19061a
    Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>

--- cmake/QtCreatorAPI.cmake
+++ cmake/QtCreatorAPI.cmake
@@ -129,9 +129,6 @@ function(add_qtc_library name)
     "CONDITION;DEPENDS;PUBLIC_DEPENDS;DEFINES;PUBLIC_DEFINES;INCLUDES;SYSTEM_INCLUDES;PUBLIC_INCLUDES;PUBLIC_SYSTEM_INCLUDES;SOURCES;EXPLICIT_MOC;SKIP_AUTOMOC;EXTRA_TRANSLATIONS;PROPERTIES;PRIVATE_COMPILE_OPTIONS;PUBLIC_COMPILE_OPTIONS" ${ARGN}
   )
 
-  check_library_dependencies(${_arg_DEPENDS})
-  check_library_dependencies(${_arg_PUBLIC_DEPENDS})
-
   get_default_defines(default_defines_copy ${_arg_ALLOW_ASCII_CASTS})
 
   if (${_arg_UNPARSED_ARGUMENTS})
@@ -338,9 +335,6 @@ function(add_qtc_plugin target_name)
     "CONDITION;DEPENDS;PUBLIC_DEPENDS;DEFINES;PUBLIC_DEFINES;INCLUDES;SYSTEM_INCLUDES;PUBLIC_INCLUDES;PUBLIC_SYSTEM_INCLUDES;SOURCES;EXPLICIT_MOC;SKIP_AUTOMOC;EXTRA_TRANSLATIONS;PLUGIN_DEPENDS;PLUGIN_RECOMMENDS;PLUGIN_TEST_DEPENDS;PLUGIN_MANUAL_DEPENDS;PROPERTIES;PRIVATE_COMPILE_OPTIONS;PUBLIC_COMPILE_OPTIONS"
     ${ARGN}
   )
-
-  check_library_dependencies(${_arg_DEPENDS})
-  check_library_dependencies(${_arg_PUBLIC_DEPENDS})
 
   if (${_arg_UNPARSED_ARGUMENTS})
     message(FATAL_ERROR "add_qtc_plugin had unparsed arguments")
@@ -621,9 +615,6 @@ function(extend_qtc_plugin target_name)
     return()
   endif()
 
-  check_library_dependencies(${_arg_DEPENDS})
-  check_library_dependencies(${_arg_PUBLIC_DEPENDS})
-
   extend_qtc_target(${target_name} ${ARGN})
 endfunction()
 
@@ -632,9 +623,6 @@ function(extend_qtc_library target_name)
   if (NOT _library_enabled)
     return()
   endif()
-
-  check_library_dependencies(${_arg_DEPENDS})
-  check_library_dependencies(${_arg_PUBLIC_DEPENDS})
 
   extend_qtc_target(${target_name} ${ARGN})
 endfunction()
