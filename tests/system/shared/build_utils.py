3dd8798d8481102f0a575c716bfdf6426765d78f
Robert Löhning <robert.loehning@qt.io>
2024-09-26 11:22:45 +0200

    SquishTests: Fix waiting for project parsing
    The run button isn't being disabled after changes to the project
    anymore. Replaced this condition with waiting for progress bars
    in the only place where that mattered.
    
    Change-Id: Ib73c2ec67479af9c7d45d30dcfdd4ed81c792b9b
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- tests/system/shared/build_utils.py
+++ tests/system/shared/build_utils.py
@@ -121,7 +121,7 @@ def selectBuildConfig(wantedKit, configName, afterSwitchTo=ViewConstants.EDIT):
     switchViewTo(ViewConstants.PROJECTS)
     if any((switchToBuildOrRunSettingsFor(wantedKit, ProjectSettings.BUILD),
             selectFromCombo(":scrollArea.Edit build configuration:_QComboBox", configName))):
-        waitForProjectParsing(5000, 30000, 0)
+        progressBarWait(30000)
     if afterSwitchTo:
         if ViewConstants.FIRST_AVAILABLE <= afterSwitchTo <= ViewConstants.LAST_AVAILABLE:
             switchViewTo(afterSwitchTo)
