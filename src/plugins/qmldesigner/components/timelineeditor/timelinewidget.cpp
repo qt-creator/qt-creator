c7491b6ba4fbda9b2a6bffc1926169436978a605
Tim Jenssen <tim.jenssen@qt.io>
2024-09-27 14:53:07 +0200

    fix GCC 10.2.1 arm std::ranges::dangling compile issues
    error was:
    src/plugins/qmldesigner/components/componentcore/zoomaction.cpp:97:14: error: no match for ‘operator!=’ (operand types are ‘std::ranges::dangling’ and ‘std::array<double, 27>::reverse_iterator’ {aka ‘std::reverse_iterator<double*>’})
    
    Change-Id: Ifefc4d17791fc5eb8a1446538b429410692b6d2e
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/plugins/qmldesigner/components/timelineeditor/timelinewidget.cpp
+++ src/plugins/qmldesigner/components/timelineeditor/timelinewidget.cpp
@@ -74,8 +74,8 @@ static qreal next(const QVector<qreal> &vector, qreal current)
 
 static qreal previous(const QVector<qreal> &vector, qreal current)
 {
-    auto iter = std::ranges::find_if(vector | std::views::reverse,
-                                     [&](qreal val) { return val < current; });
+    auto iter = std::find_if(vector.rbegin(), vector.rend(),
+                             [&](qreal val) { return val < current; });
     if (iter != vector.rend())
         return *iter;
     return current;
