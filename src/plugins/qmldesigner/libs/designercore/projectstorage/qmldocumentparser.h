173148995cd0b50cfa38cd3d7d8404d7d4b44331
Tim Jenssen <tim.jenssen@qt.io>
2024-10-04 17:57:55 +0200

    QmlDesigner: fix build for older Qt versions
    Change-Id: Ifca4aa284296718be359ec962408aba65d5463c1
    Reviewed-by: Marco Bubke <marco.bubke@qt.io>

--- src/plugins/qmldesigner/libs/designercore/projectstorage/qmldocumentparser.h
+++ src/plugins/qmldesigner/libs/designercore/projectstorage/qmldocumentparser.h
@@ -25,7 +25,7 @@ public:
         , m_pathCache{pathCache}
     {}
 #else
-    QmlDocumentParser(ProjectStorage &, PathCache &)
+    QmlDocumentParser(ProjectStorage &, PathCacheType &)
     {}
 #endif
 
