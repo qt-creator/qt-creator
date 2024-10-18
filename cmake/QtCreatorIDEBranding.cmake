4fd716ff09389d0c82e2b3a3f5a7335a7f9db12f
Eike Ziller <eike.ziller@qt.io>
2024-10-18 15:32:07 +0200

    Bump version to 15 beta2
    Change-Id: I293d173b377ff91d8f0889e11662d262cfa2cefa
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- cmake/QtCreatorIDEBranding.cmake
+++ cmake/QtCreatorIDEBranding.cmake
@@ -1,6 +1,6 @@
-set(IDE_VERSION "14.0.82")                            # The IDE version.
-set(IDE_VERSION_COMPAT "14.0.82")                     # The IDE Compatibility version.
-set(IDE_VERSION_DISPLAY "15.0.0-beta1")               # The IDE display version.
+set(IDE_VERSION "14.0.83")                            # The IDE version.
+set(IDE_VERSION_COMPAT "14.0.83")                     # The IDE Compatibility version.
+set(IDE_VERSION_DISPLAY "15.0.0-beta2")               # The IDE display version.
 
 set(IDE_SETTINGSVARIANT "QtProject")                  # The IDE settings variation.
 set(IDE_DISPLAY_NAME "Qt Creator")                    # The IDE display name.
