e88a841376ba7d843d7365de6b8f7e4916c1a002
hjk <hjk@qt.io>
2024-10-30 16:56:31 +0100

    ExtensionManager: Add a note about the (non-)security of extensions
    Change-Id: I4a173a2652a2e72bde09e37958a073f6572894dc
    Reviewed-by: Alessandro Portale <alessandro.portale@qt.io>

--- src/plugins/extensionmanager/extensionmanagersettings.cpp
+++ src/plugins/extensionmanager/extensionmanagersettings.cpp
@@ -6,12 +6,15 @@
 #include "extensionmanagerconstants.h"
 #include "extensionmanagertr.h"
 
+#include <app/app_version.h>
+
 #include <coreplugin/coreconstants.h>
 #include <coreplugin/dialogs/ioptionspage.h>
 #include <coreplugin/icore.h>
 #include <coreplugin/plugininstallwizard.h>
 
 #include <utils/layoutbuilder.h>
+#include <utils/stylehelper.h>
 
 namespace ExtensionManager::Internal {
 
@@ -39,6 +42,19 @@ ExtensionManagerSettings::ExtensionManagerSettings()
         using namespace Layouting;
         return Column {
             Group {
+                title(Tr::tr("Note")),
+                Column {
+                    Label {
+                        wordWrap(true),
+                        text(Tr::tr("%1 does not check extensions from external vendors for security "
+                                    "flaws or malicious intent, so be careful when installing them, "
+                                    "as it might leave your computer vulnerable to attacks such as "
+                                    "hacking, malware, and phishing.")
+                             .arg(QLatin1String(Core::Constants::IDE_DISPLAY_NAME)))
+                    }
+                }
+            },
+            Group {
                 title(Tr::tr("Use External Repository")),
                 groupChecker(useExternalRepo.groupChecker()),
                 Form {
@@ -56,7 +72,8 @@ ExtensionManagerSettings::ExtensionManagerSettings()
                 },
                 st,
             },
-            st
+            st,
+            spacing(Utils::StyleHelper::SpacingTokens::ExVPaddingGapXl),
         };
     });
 
