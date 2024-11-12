ae59ed0e4abd3e5f6504466cdae84d6e3ce2cd3a
Alessandro Portale <alessandro.portale@qt.io>
2024-11-12 15:40:54 +0100

    ExtensionsManager: Omit reloading for softloadable plugins
    This change avoids the "Reload now" info bar if a disabled and
    softloadable plugin gets enabled.
    
    Fixes: QTCREATORBUG-31123
    Change-Id: I4fb2a526aa9b04cb54867a57669cf073a021e733
    Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>

--- src/plugins/extensionmanager/extensionmanagerwidget.cpp
+++ src/plugins/extensionmanager/extensionmanagerwidget.cpp
@@ -9,6 +9,7 @@
 #include "extensionsmodel.h"
 
 #include <coreplugin/coreconstants.h>
+#include <coreplugin/coreplugin.h>
 #include <coreplugin/coreplugintr.h>
 #include <coreplugin/icontext.h>
 #include <coreplugin/icore.h>
@@ -266,17 +267,19 @@ public:
                 return;
             const bool doIt = m_pluginView.data().setPluginsEnabled({spec}, checked);
             if (doIt) {
-                if (!ICore::infoBar()->canInfoBeAdded(kRestartSetting))
-                    return;
-
-                Utils::InfoBarEntry info(
-                    kRestartSetting,
-                    Core::Tr::tr("Plugin changes will take effect after restart."));
-                info.addCustomButton(Tr::tr("Restart Now"), [] {
-                    ICore::infoBar()->removeInfo(kRestartSetting);
-                    QTimer::singleShot(0, ICore::instance(), &ICore::restart);
-                });
-                ICore::infoBar()->addInfo(info);
+                if (checked && spec->isEffectivelySoftloadable()) {
+                    ExtensionSystem::PluginManager::loadPluginsAtRuntime({spec});
+                    Core::Internal::CorePlugin::loadMimeFromPlugin(spec);
+                } else if (ICore::infoBar()->canInfoBeAdded(kRestartSetting)) {
+                    Utils::InfoBarEntry info(
+                        kRestartSetting,
+                        Core::Tr::tr("Plugin changes will take effect after restart."));
+                    info.addCustomButton(Tr::tr("Restart Now"), [] {
+                        ICore::infoBar()->removeInfo(kRestartSetting);
+                        QTimer::singleShot(0, ICore::instance(), &ICore::restart);
+                    });
+                    ICore::infoBar()->addInfo(info);
+                }
 
                 ExtensionSystem::PluginManager::writeSettings();
             } else {
