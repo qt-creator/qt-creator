9862de8e06e0026f3888170e22c98abfcfefdbcb
hjk <hjk@qt.io>
2024-11-20 13:37:27 +0100

    Core: Replace the FileUtils class by a namespace
    Change-Id: I954109e15f9ea48ccd4a436e7ae35bdb20f50320
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/plugins/coreplugin/fileutils.cpp
+++ src/plugins/coreplugin/fileutils.cpp
@@ -42,7 +42,7 @@
 
 using namespace Utils;
 
-namespace Core {
+namespace Core::FileUtils {
 
 static FilePath windowsDirectory()
 {
@@ -69,7 +69,7 @@ static void showGraphicalShellError(QWidget *parent, const QString &app, const Q
         ICore::showOptionsDialog(Constants::SETTINGS_ID_INTERFACE, parent);
 }
 
-void FileUtils::showInGraphicalShell(QWidget *parent, const FilePath &pathIn)
+void showInGraphicalShell(QWidget *parent, const FilePath &pathIn)
 {
     const QFileInfo fileInfo = pathIn.toFileInfo();
     // Mac, Windows support folder or file.
@@ -102,7 +102,7 @@ void FileUtils::showInGraphicalShell(QWidget *parent, const FilePath &pathIn)
     }
 }
 
-void FileUtils::showInFileSystemView(const FilePath &path)
+void showInFileSystemView(const FilePath &path)
 {
     QWidget *widget
         = NavigationWidget::activateSubWidget(FolderNavigationWidgetFactory::instance()->id(),
@@ -111,22 +111,22 @@ void FileUtils::showInFileSystemView(const FilePath &path)
         navWidget->syncWithFilePath(path);
 }
 
-void FileUtils::openTerminal(const FilePath &path, const Environment &env)
+void openTerminal(const FilePath &path, const Environment &env)
 {
     Terminal::Hooks::instance().openTerminal({path, env});
 }
 
-QString FileUtils::msgFindInDirectory()
+QString msgFindInDirectory()
 {
     return Tr::tr("Find in This Directory...");
 }
 
-QString FileUtils::msgFileSystemAction()
+QString msgFileSystemAction()
 {
     return Tr::tr("Show in File System View");
 }
 
-QString FileUtils::msgGraphicalShellAction()
+QString msgGraphicalShellAction()
 {
     if (HostOsInfo::isWindowsHost())
         return Tr::tr("Show in Explorer");
@@ -135,14 +135,14 @@ QString FileUtils::msgGraphicalShellAction()
     return Tr::tr("Show Containing Folder");
 }
 
-QString FileUtils::msgTerminalHereAction()
+QString msgTerminalHereAction()
 {
     if (HostOsInfo::isWindowsHost())
         return Tr::tr("Open Command Prompt Here");
     return Tr::tr("Open Terminal Here");
 }
 
-QString FileUtils::msgTerminalWithAction()
+QString msgTerminalWithAction()
 {
     if (HostOsInfo::isWindowsHost())
         return Tr::tr("Open Command Prompt With",
@@ -151,7 +151,7 @@ QString FileUtils::msgTerminalWithAction()
                   "Opens a submenu for choosing an environment, such as \"Run Environment\"");
 }
 
-void FileUtils::removeFiles(const FilePaths &filePaths, bool deleteFromFS)
+void removeFiles(const FilePaths &filePaths, bool deleteFromFS)
 {
     // remove from version control
     VcsManager::promptToDelete(filePaths);
@@ -171,47 +171,9 @@ void FileUtils::removeFiles(const FilePaths &filePaths, bool deleteFromFS)
     }
 }
 
-bool FileUtils::renameFile(const FilePath &orgFilePath, const FilePath &newFilePath,
-                           HandleIncludeGuards handleGuards)
-{
-    if (orgFilePath.equalsCaseSensitive(newFilePath))
-        return false;
-
-    const FilePath dir = orgFilePath.absolutePath();
-    IVersionControl *vc = VcsManager::findVersionControlForDirectory(dir);
-    const FilePath newDir = newFilePath.absolutePath();
-    if (newDir != dir && !newDir.ensureWritableDir())
-        return false;
-
-    bool result = false;
-    if (vc && vc->supportsOperation(IVersionControl::MoveOperation))
-        result = vc->vcsMove(orgFilePath, newFilePath);
-    if (!result) // The moving via vcs failed or the vcs does not support moving, fall back
-        result = bool(orgFilePath.renameFile(newFilePath));
-    if (result) {
-        DocumentManager::renamedFile(orgFilePath, newFilePath);
-        updateHeaderFileGuardIfApplicable(orgFilePath, newFilePath, handleGuards);
-    }
-    return result;
-}
-
-void FileUtils::updateHeaderFileGuardIfApplicable(const Utils::FilePath &oldFilePath,
-                                                  const Utils::FilePath &newFilePath,
-                                                  HandleIncludeGuards handleGuards)
-{
-    if (handleGuards == HandleIncludeGuards::No)
-        return;
-    const bool headerUpdateSuccess = updateHeaderFileGuardAfterRename(newFilePath.toString(),
-                                                                      oldFilePath.baseName());
-    if (headerUpdateSuccess)
-        return;
-    MessageManager::writeDisrupting(
-                Tr::tr("Failed to rename the include guard in file \"%1\".")
-                .arg(newFilePath.toUserOutput()));
-}
-
-bool FileUtils::updateHeaderFileGuardAfterRename(const QString &headerPath,
-                                                 const QString &oldHeaderBaseName)
+// This method is used to refactor the include guards in the renamed headers
+static bool updateHeaderFileGuardAfterRename(const QString &headerPath,
+                                             const QString &oldHeaderBaseName)
 {
     bool ret = true;
     QFile headerFile(headerPath);
@@ -228,7 +190,7 @@ bool FileUtils::updateHeaderFileGuardAfterRename(const QString &headerPath,
     const QByteArray data = headerFile.readAll();
     headerFile.close();
 
-    auto headerFileTextFormat = Utils::TextFileFormat::detect(data);
+    auto headerFileTextFormat = TextFileFormat::detect(data);
     if (!headerFileTextFormat.codec)
         headerFileTextFormat.codec = EditorManager::defaultTextCodec();
     QString stringContent;
@@ -315,8 +277,7 @@ bool FileUtils::updateHeaderFileGuardAfterRename(const QString &headerPath,
         QFile tmpHeader(headerPath + ".tmp");
         if (tmpHeader.open(QFile::WriteOnly)) {
             const auto lineEnd =
-                    headerFileTextFormat.lineTerminationMode
-                    == Utils::TextFileFormat::LFLineTerminator
+                    headerFileTextFormat.lineTerminationMode == TextFileFormat::LFLineTerminator
                     ? QStringLiteral("\n") : QStringLiteral("\r\n");
             // write into temporary string,
             // after that write with codec into file (QTextStream::setCodec is gone in Qt 6)
@@ -355,4 +316,43 @@ bool FileUtils::updateHeaderFileGuardAfterRename(const QString &headerPath,
     return ret;
 }
 
-} // namespace Core
+bool renameFile(const FilePath &orgFilePath, const FilePath &newFilePath,
+                HandleIncludeGuards handleGuards)
+{
+    if (orgFilePath.equalsCaseSensitive(newFilePath))
+        return false;
+
+    const FilePath dir = orgFilePath.absolutePath();
+    IVersionControl *vc = VcsManager::findVersionControlForDirectory(dir);
+    const FilePath newDir = newFilePath.absolutePath();
+    if (newDir != dir && !newDir.ensureWritableDir())
+        return false;
+
+    bool result = false;
+    if (vc && vc->supportsOperation(IVersionControl::MoveOperation))
+        result = vc->vcsMove(orgFilePath, newFilePath);
+    if (!result) // The moving via vcs failed or the vcs does not support moving, fall back
+        result = bool(orgFilePath.renameFile(newFilePath));
+    if (result) {
+        DocumentManager::renamedFile(orgFilePath, newFilePath);
+        updateHeaderFileGuardIfApplicable(orgFilePath, newFilePath, handleGuards);
+    }
+    return result;
+}
+
+void updateHeaderFileGuardIfApplicable(const FilePath &oldFilePath,
+                                       const FilePath &newFilePath,
+                                       HandleIncludeGuards handleGuards)
+{
+    if (handleGuards == HandleIncludeGuards::No)
+        return;
+    const bool headerUpdateSuccess = updateHeaderFileGuardAfterRename(newFilePath.toString(),
+                                                                      oldFilePath.baseName());
+    if (headerUpdateSuccess)
+        return;
+    MessageManager::writeDisrupting(
+                Tr::tr("Failed to rename the include guard in file \"%1\".")
+                .arg(newFilePath.toUserOutput()));
+}
+
+} // namespace Core::FileUtils
