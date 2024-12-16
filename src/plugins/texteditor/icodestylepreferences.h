e7aabf876218834af8b3003462913177806ddb19
Jarek Kobus <jaroslaw.kobus@qt.io>
2024-12-16 09:25:28 +0100

    ICodeStylePreferences: Introduce TypedCodeStylePreferences
    This is going to replace CppCodeStylePreferences and
    QmlJsCodeStylePreferences.
    
    Change-Id: Ica5c63555ffd1e00454ee804bf3237c66b6fb14f
    Reviewed-by: David Schulz <david.schulz@qt.io>

--- src/plugins/texteditor/icodestylepreferences.h
+++ src/plugins/texteditor/icodestylepreferences.h
@@ -97,5 +97,77 @@ private:
     Internal::ICodeStylePreferencesPrivate *d;
 };
 
+template <typename T>
+class TypedCodeStylePreferences : public ICodeStylePreferences
+{
+public:
+    TypedCodeStylePreferences(QObject *parent = nullptr)
+        : ICodeStylePreferences(parent)
+    {
+        setSettingsSuffix("CodeStyleSettings");
+        setGlobalSettingsCategory(T::settingsId());
+    }
+
+    QVariant value() const final
+    {
+        QVariant v;
+        v.setValue(codeStyleSettings());
+        return v;
+    }
+    void setValue(const QVariant &data) final
+    {
+        if (!data.canConvert<T>())
+            return;
+
+        setCodeStyleSettings(data.value<T>());
+    }
+
+    T codeStyleSettings() const { return m_data; }
+
+    // Tracks parent hierarchy until currentParentSettings is null. TODO: return optional?
+    T currentCodeStyleSettings() const
+    {
+        QVariant v = currentValue();
+        if (!v.canConvert<T>()) {
+            // warning
+            return {};
+        }
+        return v.value<T>();
+    }
+
+    Utils::Store toMap() const override
+    {
+        Utils::Store map = ICodeStylePreferences::toMap();
+        if (!currentDelegate()) {
+            const Utils::Store dataMap = m_data.toMap();
+            for (auto it = dataMap.begin(), end = dataMap.end(); it != end; ++it)
+                map.insert(it.key(), it.value());
+        }
+        return map;
+    }
+    void fromMap(const Utils::Store &map) override
+    {
+        ICodeStylePreferences::fromMap(map);
+        if (!currentDelegate())
+            m_data.fromMap(map);
+    }
+
+    void setCodeStyleSettings(const T &data)
+    {
+        if (m_data == data)
+            return;
+
+        m_data = data;
+
+        QVariant v;
+        v.setValue(data);
+        emit valueChanged(v);
+        if (!currentDelegate())
+            emit currentValueChanged(v);
+    }
+
+private:
+    T m_data;
+};
 
 } // namespace TextEditor
