7c2621de1e49b0666355faa33a5d28e60a4eb6c0
hjk <hjk@qt.io>
2025-01-10 16:15:50 +0100

    Baremetal: Inline baremetaldeviceconfigurationwizard.*
    ... into baremetaldevice.cpp.
    
    Change-Id: I4586c2ca252dbf26c319247cc3ddb6ced369a2ff
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/plugins/baremetal/baremetaldevice.cpp
+++ src/plugins/baremetal/baremetaldevice.cpp
@@ -5,15 +5,25 @@
 #include "baremetaldevice.h"
 
 #include "baremetalconstants.h"
+#include "baremetalconstants.h"
+#include "baremetaldevice.h"
 #include "baremetaldeviceconfigurationwidget.h"
-#include "baremetaldeviceconfigurationwizard.h"
 #include "baremetaltr.h"
+#include "debugserverproviderchooser.h"
 #include "debugserverprovidermanager.h"
 #include "idebugserverprovider.h"
 
+#include <projectexplorer/devicesupport/idevice.h>
 #include <projectexplorer/devicesupport/idevicefactory.h>
 
+#include <utils/fileutils.h>
 #include <utils/qtcassert.h>
+#include <utils/variablechooser.h>
+#include <utils/wizard.h>
+
+#include <QFormLayout>
+#include <QLineEdit>
+#include <QWizardPage>
 
 using namespace ProjectExplorer;
 using namespace Utils;
@@ -83,9 +93,84 @@ IDeviceWidget *BareMetalDevice::createWidget()
     return new BareMetalDeviceConfigurationWidget(shared_from_this());
 }
 
+//  BareMetalDeviceConfigurationWizardSetupPage
+
+class BareMetalDeviceConfigurationWizardSetupPage final : public QWizardPage
+{
+public:
+    explicit BareMetalDeviceConfigurationWizardSetupPage(QWidget *parent)
+        : QWizardPage(parent)
+    {
+        setTitle(Tr::tr("Set up Debug Server or Hardware Debugger"));
+
+        m_nameLineEdit = new QLineEdit(this);
+
+        m_providerChooser = new DebugServerProviderChooser(false, this);
+        m_providerChooser->populate();
+
+        const auto formLayout = new QFormLayout(this);
+        formLayout->setFieldGrowthPolicy(QFormLayout::AllNonFixedFieldsGrow);
+        formLayout->addRow(Tr::tr("Name:"), m_nameLineEdit);
+        formLayout->addRow(Tr::tr("Debug server provider:"), m_providerChooser);
+
+        connect(m_nameLineEdit, &QLineEdit::textChanged,
+                this, &BareMetalDeviceConfigurationWizardSetupPage::completeChanged);
+        connect(m_providerChooser, &DebugServerProviderChooser::providerChanged,
+                this, &QWizardPage::completeChanged);
+    }
+
+    void initializePage() final
+    {
+        m_nameLineEdit->setText(BareMetalDevice::defaultDisplayName());
+    }
+
+    bool isComplete() const final
+    {
+        return !configurationName().isEmpty();
+    }
+
+    QString configurationName() const { return m_nameLineEdit->text().trimmed(); }
+    QString debugServerProviderId() const { return m_providerChooser->currentProviderId(); }
+
+private:
+    QLineEdit *m_nameLineEdit = nullptr;
+    DebugServerProviderChooser *m_providerChooser = nullptr;
+};
+
+//  BareMetalDeviceConfigurationWizardSetupPage
+
+class BareMetalDeviceConfigurationWizard final : public Wizard
+{
+public:
+    BareMetalDeviceConfigurationWizard()
+        : m_setupPage(new BareMetalDeviceConfigurationWizardSetupPage(this))
+    {
+        enum PageId { SetupPageId };
+
+        setWindowTitle(Tr::tr("New Bare Metal Device Configuration Setup"));
+        setPage(SetupPageId, m_setupPage);
+        m_setupPage->setCommitPage(true);
+    }
+
+    IDevicePtr device() const
+    {
+        const auto dev = BareMetalDevice::create();
+        dev->setupId(IDevice::ManuallyAdded, Utils::Id());
+        dev->setDefaultDisplayName(m_setupPage->configurationName());
+        dev->setType(Constants::BareMetalOsType);
+        dev->setMachineType(IDevice::Hardware);
+        dev->setDebugServerProviderId(m_setupPage->debugServerProviderId());
+        return dev;
+    }
+
+private:
+    BareMetalDeviceConfigurationWizardSetupPage *m_setupPage = nullptr;
+};
+
+
 // Factory
 
-class BareMetalDeviceFactory final : public ProjectExplorer::IDeviceFactory
+class BareMetalDeviceFactory final : public IDeviceFactory
 {
 public:
     BareMetalDeviceFactory()
