qt-creator/qt-creator

commit ce9826ae6e727e344c13762eb92e1882768f4a2d

VcsBase: Fix enabling git actions after rebase
Delay the state changed signal for proper evaluation
(a file check for `rebase-apply` is run when the
signal is emitted)

This fixes that Git "Rebasing" stayed in the title
bar when rebasing was long finished and therefore
the rebase actions stayed disabled.

TODO: The [REBASING] message in the projects view is
not fixed, it disappears when clicking the title bar.

Fixes: QTCREATORBUG-31815
Change-Id: Ic83949dfdaf26df3af8149457e3f282ff1bcefe5
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/vcsbase/vcsbaseplugin.cpp
@@ -30,6 +30,7 @@
 #include <QSharedData>
 #include <QProcessEnvironment>
 #include <QTextCodec>
+#include <QTimer>
 
 using namespace Core;
 using namespace Utils;
@@ -308,8 +309,10 @@ void StateListener::slotStateChanged()
         state.clearPatchFile(); // Need a repository to patch
 
     qCDebug(stateLog).noquote() << "VC:" << (vc ? vc->displayName() : QString("None")) << state;
-    EditorManager::updateWindowTitles();
-    emit stateChanged(state, vc);
+    QTimer::singleShot(500, this, [this, state, vc] {
+        EditorManager::updateWindowTitles();
+        emit stateChanged(state, vc);
+    });
 }
 
 } // namespace Internal

commit 5b9f6d9c33071ca5ca3b65468233264d89d4182b

Git: Fix Instant Blame not working after first editor open
For the very first opened editor, we were unable to get a
workingDirectory with currentState().currentFileTopLevel()
within the lambda setupBlameForEditor(). The workingDirectory
was always empty and therefore Instant Blame only worked
after a force or editor change.

This and also some still failing ClearCase auto tests were
broken by ce9826ae6e727e344c13762eb92e1882768f4a2d.

Change-Id: I06239351274cf3aa01be66bc614ec8991274b4a6
Reviewed-by: Orgad Shaneh <orgads@gmail.com>
Reviewed-by: Eike Ziller <eike.ziller@qt.io>

src/plugins/vcsbase/vcsbaseplugin.cpp
@@ -309,10 +309,13 @@ void StateListener::slotStateChanged()
         state.clearPatchFile(); // Need a repository to patch
 
     qCDebug(stateLog).noquote() << "VC:" << (vc ? vc->displayName() : QString("None")) << state;
-    QTimer::singleShot(500, this, [this, state, vc] {
+
+    auto updateState = [this, state, vc] {
         EditorManager::updateWindowTitles();
         emit stateChanged(state, vc);
-    });
+    };
+    updateState();
+    QTimer::singleShot(500, this, updateState); // QTCREATORBUG-31815
 }
 
 } // namespace Internal