add2134ff159db3ccf099c9da3aa67a859eb1fd8
Marcus Tillmanns <marcus.tillmanns@qt.io>
2025-01-16 14:02:12 +0100

    ProjectExplorer: Fix crash when no configuration is set
    Amends 02d7e59a2e51e22026cee8eadd509e7508c008ec
    
    Change-Id: I5deb955e6fd9a0257e9aabf0edb19ed6f2cd3fde
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/plugins/projectexplorer/runcontrol.cpp
+++ src/plugins/projectexplorer/runcontrol.cpp
@@ -1586,8 +1586,13 @@ void SimpleTargetRunnerPrivate::start()
     m_stopRequested = false;
 
     QVariantHash extraData = m_extraData;
-    extraData[TERMINAL_SHELL_NAME]
-        = q->runControl()->target()->activeRunConfiguration()->displayName();
+    if (q->runControl() && q->runControl()->target()
+        && q->runControl()->target()->activeRunConfiguration()) {
+        extraData[TERMINAL_SHELL_NAME]
+            = q->runControl()->target()->activeRunConfiguration()->displayName();
+    } else {
+        extraData[TERMINAL_SHELL_NAME] = m_command.executable().fileName();
+    }
 
     m_process.setCommand(cmdLine);
     m_process.setEnvironment(env);
