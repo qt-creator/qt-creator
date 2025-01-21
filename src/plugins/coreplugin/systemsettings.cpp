4a4a8a85330e09b555422ba4b6294276b2793999
Tim Jenssen <tim.jenssen@qt.io>
2025-01-21 14:03:54 +0000

    Revert "Crashpad: add openCrashReportsLocationButton"
    This reverts commit 7ff1e29f251b55ddb464dafd93a5838570bf5862.
    
    Reason for revert: broken
    
    Change-Id: Ifa29d0ec12ea264d3966a3542e46f42d1f4124cc
    Reviewed-by: Tim Jenssen <tim.jenssen@qt.io>

--- src/plugins/coreplugin/systemsettings.cpp
+++ src/plugins/coreplugin/systemsettings.cpp
@@ -26,10 +26,8 @@
 #include <utils/unixutils.h>
 
 #include <QComboBox>
-#include <QDesktopServices>
 #include <QGuiApplication>
 #include <QLineEdit>
-#include <QMenu>
 #include <QMessageBox>
 #include <QPushButton>
 #include <QToolButton>
@@ -169,7 +167,7 @@ public:
         , m_terminalExecuteArgs(new QLineEdit)
         , m_environmentChangesLabel(new Utils::ElidingLabel)
 #ifdef ENABLE_CRASHPAD
-        , m_clearCrashReportsButton(new QPushButton(Tr::tr("Clear Crash Reports"), this))
+        , m_clearCrashReportsButton(new QPushButton(Tr::tr("Clear Local Crash Reports"), this))
         , m_crashReportsSizeText(new QLabel(this))
 #endif
 
@@ -240,8 +238,6 @@ public:
                                     .arg(appInfo().crashReports.toUserOutput());
         m_clearCrashReportsButton->setToolTip(toolTip);
         m_crashReportsSizeText->setToolTip(toolTip);
-        auto clearButtonMenu = new QMenu(m_clearCrashReportsButton);
-        m_clearCrashReportsButton->setMenu(clearButtonMenu);
         Row crashDetails
             = Row{m_clearCrashReportsButton, m_crashReportsSizeText, helpCrashReportingButton, st};
         if (qtcEnvironmentVariableIsSet("QTC_SHOW_CRASHBUTTON")) {
@@ -287,15 +283,6 @@ public:
         const FilePath reportsPath = ICore::crashReportsPath()
                                      / QLatin1String(
                                          HostOsInfo::isMacHost() ? "completed" : "reports");
-
-        auto openLocationAction = new QAction(Tr::tr("Open Location"), clearButtonMenu);
-        connect(openLocationAction, &QAction::triggered, this, [this, reportsPath] {
-            if (!QDesktopServices::openUrl(reportsPath.toUrl())) {
-                qWarning() << "Failed to open path:" << reportsPath;
-            }
-        });
-        clearButtonMenu->addAction(openLocationAction);
-
         const auto updateClearCrashWidgets = [this, reportsPath] {
             qint64 size = 0;
             const FilePaths crashFiles = reportsPath.dirEntries(QDir::Files);
