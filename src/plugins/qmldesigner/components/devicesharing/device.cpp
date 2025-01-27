a641364a1bfcd602d96a6d36a1e787502f632db8
Burak Hancerli <burak.hancerli@qt.io>
2025-01-27 16:19:06 +0100

    DeviceShare: Remove the OS version from OS string
    Change-Id: I33c49c1ee9e7c51e7878eb6ed52262bcd70b1cfb
    Reviewed-by: Henning Gründl <henning.gruendl@qt.io>

--- src/plugins/qmldesigner/components/devicesharing/device.cpp
+++ src/plugins/qmldesigner/components/devicesharing/device.cpp
@@ -267,6 +267,21 @@ void Device::processTextMessage(const QString &data)
     if (dataType == PackageFromDevice::deviceInfo) {
         QJsonObject deviceInfo = jsonObj.value("data").toObject();
         m_deviceInfo.setJsonObject(deviceInfo);
+
+        const QString os = m_deviceInfo.os();
+        if (os.contains("Android", Qt::CaseInsensitive))
+            m_deviceInfo.setOs("Android");
+        else if (os.contains("iOS", Qt::CaseInsensitive))
+            m_deviceInfo.setOs("iOS");
+        else if (os.contains("iPadOS", Qt::CaseInsensitive))
+            m_deviceInfo.setOs("iPadOS");
+        else if (os.contains("Windows", Qt::CaseInsensitive))
+            m_deviceInfo.setOs("Windows");
+        else if (os.contains("Linux", Qt::CaseInsensitive))
+            m_deviceInfo.setOs("Linux");
+        else if (os.contains("Mac", Qt::CaseInsensitive))
+            m_deviceInfo.setOs("macOS");
+
         emit deviceInfoReady(m_deviceSettings.deviceId());
     } else if (dataType == PackageFromDevice::projectRunning) {
         qCDebug(deviceSharePluginLog) << "Project started on device" << m_deviceSettings.deviceId();
