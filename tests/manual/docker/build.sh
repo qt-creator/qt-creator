5a29c2b93d5fb7ad7a9f3a85c883f00c5de2dc29
hjk <hjk@qt.io>
2025-02-14 13:53:04 +0100

    Docker: Add a manual Unbuntu 16.04 setup
    Task-number: QTCREATORBUG-32475
    Change-Id: Iaf79c899b7154bac6021ce75c62da87b780fdff0
    Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>

--- tests/manual/docker/build.sh
+++ tests/manual/docker/build.sh
@@ -1,6 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-docker build -t qt-5-ubuntu-20.04-build -f Dockerfile-qt-5-ubuntu-20.04-build .
-docker build -t qt-5-ubuntu-20.04-run -f Dockerfile-qt-5-ubuntu-20.04-run .
-docker build -t qt-5-ubuntu-20.04-clang-lldb-build -f Dockerfile-qt-5-ubuntu-20.04-clang-lldb-build .
-docker build -t qt-6-fedora-37-build -f Dockerfile-qt-6-fedora-37-build .
+function build {
+    export DOCKER_BUILDKIT=1
+    docker build -t "$1" -f Dockerfile-"$1" .
+}
+
+build qt-5-ubuntu-16.04-build
+build qt-5-ubuntu-20.04-build
+build qt-5-ubuntu-20.04-run
+build qt-5-ubuntu-20.04-clang-lldb-build
+build qt-6-fedora-37-build
