ad233d7c1bea902dd9c09a92044f00b0939c6885
Jarek Kobus <jaroslaw.kobus@qt.io>
2025-03-07 13:16:24 +0100

    QmlProfiler: Fix expected messages to be received
    It looks like the additional spaces were fixed in sources,
    but not in test.
    
    This makes LocalQmlProfilerRunnerTest::testRunner() to pass now.
    
    Change-Id: Ia8d6f35a440dea9034dba87fb3f7f8a3bcdca44f
    Reviewed-by: hjk <hjk@qt.io>

--- src/plugins/qmlprofiler/tests/localqmlprofilerrunner_test.cpp
+++ src/plugins/qmlprofiler/tests/localqmlprofilerrunner_test.cpp
@@ -66,8 +66,8 @@ void LocalQmlProfilerRunnerTest::testRunner()
     connectRunner();
 
     QTest::ignoreMessage(
-                QtDebugMsg, "Invalid run control state transition from  "
-                            "\"RunControlState::Starting\"  to  \"RunControlState::Stopped\"");
+                QtDebugMsg, "Invalid run control state transition from "
+                            "\"RunControlState::Starting\" to \"RunControlState::Stopped\"");
     runControl->initiateStart();
 
     QTRY_COMPARE_WITH_TIMEOUT(startCount, 1, 30000);
