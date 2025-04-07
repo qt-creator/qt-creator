qt-creator/qt-creator

commit a22e79f38c13b652ad9639ee810647fc0c0e96b0

QML Profiler: Fix server URL
QMLProfileRunner should use server URL from the ports gatherer
instead of trying to read it from the data record.

Amends: 94b63ad3b4b13dbad0277341f71c5d8f5ea1c6b2

Fixes: QTCREATORBUG-31847
Change-Id: Id54429f09cc10f17d763ede6db4df1fad8a323fe
Reviewed-by: hjk <hjk@qt.io>

src/plugins/qmlprofiler/qmlprofilerruncontrol.cpp
@@ -162,15 +162,9 @@ void QmlProfilerRunner::profilerStateChanged()
     }
 }
 
-void QmlProfilerRunner::setServerUrl(const QUrl &serverUrl)
-{
-    recordData(QmlServerUrl, serverUrl);
-}
-
 QUrl QmlProfilerRunner::serverUrl() const
 {
-    QVariant recordedServer = recordedData(QmlServerUrl);
-    return recordedServer.toUrl();
+    return runControl()->qmlChannel();
 }
 
 //
@@ -205,7 +199,6 @@ LocalQmlProfilerSupport::LocalQmlProfilerSupport(RunControl *runControl, const Q
     setId("LocalQmlProfilerSupport");
 
     auto profiler = new QmlProfilerRunner(runControl);
-    profiler->setServerUrl(serverUrl);
 
     addStopDependency(profiler);
     // We need to open the local server before the application tries to connect.

src/plugins/qmlprofiler/qmlprofilertool.cpp
@@ -603,8 +603,9 @@ ProjectExplorer::RunControl *QmlProfilerTool::attachToWaitingApplication()
 
     auto runControl = new RunControl(ProjectExplorer::Constants::QML_PROFILER_RUN_MODE);
     runControl->copyDataFromRunConfiguration(ProjectManager::startupRunConfiguration());
-    auto profiler = new QmlProfilerRunner(runControl);
-    profiler->setServerUrl(serverUrl);
+    // The object as such is needed, the RunWorker becomes part of the RunControl at construction time,
+    // similar to how QObject children are owned by their parents
+    [[maybe_unused]] auto profiler = new QmlProfilerRunner(runControl);
 
     connect(d->m_profilerConnections, &QmlProfilerClientManager::connectionClosed,
             runControl, &RunControl::initiateStop);

src/plugins/qmlprofiler/qmlprofilerruncontrol.h
@@ -24,7 +24,6 @@ public:
     QmlProfilerRunner(ProjectExplorer::RunControl *runControl);
     ~QmlProfilerRunner() override;
 
-    void setServerUrl(const QUrl &serverUrl);
     QUrl serverUrl() const;
 
     void registerProfilerStateManager( QmlProfilerStateManager *profilerState );

commit 21661db604437e9188f09d419c4d14a1ad5b14bd

Fix attaching QML Profiler to running application
The QML channel to use (the port that the user specified in the dialog
for attaching) was no longer set for the QmlProfilerRunner. That broke
with a22e79f38c13b652ad9639ee810647fc0c0e96b0
Avoid the usage of the ports gatherer with QmlProfilerRunner for the
attachToWaitingApplication case and set the qmlChannel manually.

Also avoid soft assert if there is no active run configuration.

Fixes: QTCREATORBUG-32617
Change-Id: I608cc736ed82284afbdd40277a54237f80f30306
Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

src/plugins/qmlprofiler/qmlprofilerruncontrol.cpp
@@ -38,7 +38,6 @@ QmlProfilerRunner::QmlProfilerRunner(RunControl *runControl)
     : RunWorker(runControl)
 {
     setId("QmlProfilerRunner");
-    runControl->requestQmlChannel();
     runControl->setIcon(ProjectExplorer::Icons::ANALYZER_START_SMALL_TOOLBAR);
 }
 
@@ -152,6 +151,7 @@ RunWorker *createLocalQmlProfilerWorker(RunControl *runControl)
     worker->setId("LocalQmlProfilerSupport");
 
     auto profiler = new QmlProfilerRunner(runControl);
+    runControl->requestQmlChannel();
 
     worker->addStopDependency(profiler);
     // We need to open the local server before the application tries to connect.

src/plugins/qmlprofiler/qmlprofilertool.cpp
@@ -555,7 +555,9 @@ ProjectExplorer::RunControl *QmlProfilerTool::attachToWaitingApplication()
     d->m_viewContainer->perspective()->select();
 
     auto runControl = new RunControl(ProjectExplorer::Constants::QML_PROFILER_RUN_MODE);
-    runControl->copyDataFromRunConfiguration(activeRunConfigForActiveProject());
+    if (RunConfiguration *runConfig = activeRunConfigForActiveProject())
+        runControl->copyDataFromRunConfiguration(runConfig);
+    runControl->setQmlChannel(serverUrl);
     // The object as such is needed, the RunWorker becomes part of the RunControl at construction time,
     // similar to how QObject children are owned by their parents
     [[maybe_unused]] auto profiler = new QmlProfilerRunner(runControl);