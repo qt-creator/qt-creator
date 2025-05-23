77ef58eb6f164c350801f9a2dcb90dea074251aa
hjk <hjk@qt.io>
2025-05-23 15:32:58 +0200

    Utils: Add deduction guide for 'overloaded'
    A comment earlier claimed this was not needed for c++20, but it
    apparently is.
    
    Amends 41d7b001dce6c5490b208cb.
    
    Change-Id: I310f4e6287d497686f8c23b706ed3a3dfaa59e35
    Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>

--- src/libs/utils/overloaded.h
+++ src/libs/utils/overloaded.h
@@ -3,12 +3,13 @@
 
 #pragma once
 
-namespace Utils
-{
+namespace Utils {
 
 // https://www.cppstories.com/2018/09/visit-variants/
 // https://en.cppreference.com/w/cpp/utility/variant/visit
 // https://en.cppreference.com/w/cpp/utility/variant/visit2
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 
-}
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
+
+} // Utils
