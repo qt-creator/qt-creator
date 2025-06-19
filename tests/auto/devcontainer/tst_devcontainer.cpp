cdaeb0871ac83bebe5360c53e50db1c0d9aa9d44
Marcus Tillmanns <marcus.tillmanns@qt.io>
2025-06-19 10:50:49 +0200

    Devcontainer: Add image container test
    Change-Id: Ie9d292ecca2fb392af0c1283c19388944a5eaadd
    Reviewed-by: hjk <hjk@qt.io>

--- tests/auto/devcontainer/tst_devcontainer.cpp
+++ tests/auto/devcontainer/tst_devcontainer.cpp
@@ -33,6 +33,7 @@ private slots:
 
     void readConfig();
     void testCommands();
+    void upImage();
     void upDockerfile();
 };
 
@@ -143,6 +144,37 @@ FROM alpine:latest
     };
     config.containerConfig = dockerFileConfig;
     config.common.name = "Test Dockerfile";
+
+    qDebug() << "DevContainer Config:" << config;
+
+    std::unique_ptr<DevContainer::Instance> instance = DevContainer::Instance::fromConfig(config);
+
+    DevContainer::InstanceConfig instanceConfig{
+        .dockerCli = "docker",
+        .dockerComposeCli = "docker-compose",
+        .workspaceFolder = Utils::FilePath::fromUserInput(QDir::tempPath()),
+        .configFilePath = Utils::FilePath::fromUserInput(QDir::tempPath()) / "devcontainer.json",
+    };
+
+    using namespace std::chrono_literals;
+
+    Utils::Result<Tasking::Group> recipe = instance->upRecipe(instanceConfig);
+    QVERIFY_RESULT(recipe);
+    QCOMPARE(Tasking::TaskTree::runBlocking((*recipe).withTimeout(5s)), Tasking::DoneWith::Success);
+
+    Utils::Result<Tasking::Group> downRecipe = instance->downRecipe(instanceConfig);
+    QVERIFY_RESULT(downRecipe);
+    QCOMPARE(Tasking::TaskTree::runBlocking(*downRecipe), Tasking::DoneWith::Success);
+}
+
+void tst_DevContainer::upImage()
+{
+    DevContainer::Config config;
+    DevContainer::ImageContainer imageConfig{
+        .image = "alpine:latest",
+    };
+    config.containerConfig = imageConfig;
+    config.common.name = "Test Image";
 
     qDebug() << "DevContainer Config:" << config;
 
