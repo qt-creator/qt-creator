qt-creator/qt-creator

commit a40e803503e4186823e2ca4585c8a4555e89d9c2

Debugger: Replace DiagnosticsLocation with Utils::Link
Change-Id: I41166d4f54468e0953fd3231af9fd52560b7c74c
Reviewed-by: David Schulz <david.schulz@qt.io>

src/plugins/clangtools/readexporteddiagnosticstest.cpp
@@ -6,6 +6,7 @@
 #include "clangtoolslogfilereader.h"
 
 #include <cppeditor/cpptoolstestcase.h>
+
 #include <utils/fileutils.h>
 #include <utils/hostosinfo.h>
 #include <utils/qtcassert.h>
@@ -13,7 +14,6 @@
 #include <QtTest>
 
 using namespace CppEditor::Tests;
-using namespace Debugger;
 using namespace Utils;
 
 namespace ClangTools::Internal {

src/libs/utils/link.cpp
@@ -32,4 +32,18 @@ Link Link::fromString(const QString &filePathWithNumbers, bool canContainLineNum
     return link;
 }
 
+bool operator<(const Link &first, const Link &second)
+{
+    return std::tie(first.targetFilePath, first.targetLine, first.targetColumn)
+         < std::tie(second.targetFilePath, second.targetLine, second.targetColumn);
+}
+
+QDebug operator<<(QDebug dbg, const Link &link)
+{
+    dbg.nospace() << "Link(" << link.targetFilePath << ", "
+                  << link.targetLine << ", "
+                  << link.targetColumn << ')';
+    return dbg.space();
+}
+
 } // namespace Utils

src/libs/utils/link.h
@@ -7,6 +7,7 @@
 
 #include "filepath.h"
 
+#include <QDebug>
 #include <QMetaType>
 #include <QString>
 
@@ -43,6 +44,9 @@ public:
                && linkTextEnd == other.linkTextEnd;
     }
     bool operator!=(const Link &other) const { return !(*this == other); }
+
+    QTCREATOR_UTILS_EXPORT friend bool operator<(const Link &first, const Link &second);
+    QTCREATOR_UTILS_EXPORT friend QDebug operator<<(QDebug dbg, const Link &link);
 
     bool hasSameLocation(const Link &other) const
     {

src/plugins/clangtools/clangtool.cpp
@@ -228,7 +228,7 @@ public:
     ApplyFixIts(const QVector<DiagnosticItem *> &diagnosticItems)
     {
         for (DiagnosticItem *diagnosticItem : diagnosticItems) {
-            const FilePath &filePath = diagnosticItem->diagnostic().location.filePath;
+            const FilePath &filePath = diagnosticItem->diagnostic().location.targetFilePath;
             QTC_ASSERT(!filePath.isEmpty(), continue);
 
             // Get or create refactoring file
@@ -262,16 +262,20 @@ public:
             if (!step.isFixIt)
                 continue;
 
-            const DiagnosticLocation start = step.ranges.first();
-            const DiagnosticLocation end = step.ranges.last();
-            const int startPos = file.position(start.filePath, start.line, start.column);
-            const int endPos = file.position(start.filePath, end.line, end.column);
+            const Link start = step.ranges.first();
+            const Link end = step.ranges.last();
+            const int startPos = file.position(start.targetFilePath,
+                                               start.targetLine,
+                                               start.targetColumn);
+            const int endPos = file.position(start.targetFilePath,
+                                             end.targetLine,
+                                             end.targetColumn);
 
             auto op = new ReplacementOperation;
             op->pos = startPos;
             op->length = endPos - startPos;
             op->text = step.message;
-            op->filePath = start.filePath;
+            op->filePath = start.targetFilePath;
             op->apply = apply;
 
             replacements += op;
@@ -1244,7 +1248,7 @@ void ClangTool::setState(State state)
 QSet<Diagnostic> ClangTool::diagnostics() const
 {
     return Utils::filtered(m_diagnosticModel->diagnostics(), [](const Diagnostic &diagnostic) {
-        return ProjectFile::isSource(ProjectFile::classify(diagnostic.location.filePath));
+        return ProjectFile::isSource(ProjectFile::classify(diagnostic.location.targetFilePath));
     });
 }
 

src/plugins/clangtools/clangtoolsdiagnostic.cpp
@@ -10,7 +10,7 @@ namespace Internal {
 
 bool ExplainingStep::isValid() const
 {
-    return location.isValid() && !ranges.isEmpty() && !message.isEmpty();
+    return location.hasValidTarget() && !ranges.isEmpty() && !message.isEmpty();
 }
 
 bool operator==(const ExplainingStep &lhs, const ExplainingStep &rhs)
@@ -44,9 +44,9 @@ size_t qHash(const Diagnostic &diagnostic)
 {
     return qHash(diagnostic.name)
          ^ qHash(diagnostic.description)
-         ^ qHash(diagnostic.location.filePath)
-         ^ diagnostic.location.line
-         ^ diagnostic.location.column;
+         ^ qHash(diagnostic.location.targetFilePath)
+         ^ diagnostic.location.targetLine
+         ^ diagnostic.location.targetColumn;
 }
 
 bool operator==(const Diagnostic &lhs, const Diagnostic &rhs)

src/plugins/clangtools/clangtoolsdiagnostic.h
@@ -3,7 +3,7 @@
 
 #pragma once
 
-#include <debugger/analyzer/diagnosticlocation.h>
+#include <utils/link.h>
 
 #include <QMetaType>
 #include <QString>
@@ -22,8 +22,8 @@ public:
     }
 
     QString message;
-    Debugger::DiagnosticLocation location;
-    QVector<Debugger::DiagnosticLocation> ranges;
+    Utils::Link location;
+    Utils::Links ranges;
     bool isFixIt = false;
 };
 
@@ -40,7 +40,7 @@ public:
     QString description;
     QString category;
     QString type;
-    Debugger::DiagnosticLocation location;
+    Utils::Link location;
     QVector<ExplainingStep> explainingSteps;
     bool hasFixits = false;
 };

src/plugins/clangtools/clangtoolsdiagnosticmodel.cpp
@@ -25,6 +25,8 @@
 
 static Q_LOGGING_CATEGORY(LOG, "qtc.clangtools.model", QtWarningMsg)
 
+using namespace Utils;
+
 namespace ClangTools {
 namespace Internal {
 
@@ -101,7 +103,7 @@ void ClangToolsDiagnosticModel::addDiagnostics(const Diagnostics &diagnostics, b
         }
 
         // Create file path item if necessary
-        const Utils::FilePath &filePath = d.location.filePath;
+        const FilePath &filePath = d.location.targetFilePath;
         FilePathItem *&filePathItem = m_filePathToItem[filePath];
         if (!filePathItem) {
             filePathItem = new FilePathItem(filePath);
@@ -166,7 +168,7 @@ void ClangToolsDiagnosticModel::clearAndSetupCache()
 void ClangToolsDiagnosticModel::onFileChanged(const QString &path)
 {
     forItemsAtLevel<2>([&](DiagnosticItem *item){
-        if (item->diagnostic().location.filePath == Utils::FilePath::fromString(path))
+        if (item->diagnostic().location.targetFilePath == FilePath::fromString(path))
             item->setFixItStatus(FixitStatus::Invalidated);
     });
     m_filesWatcher->removeFile(path);
@@ -193,9 +195,9 @@ std::unique_ptr<InlineSuppressedDiagnostics> ClangToolsDiagnosticModel::createIn
     QTC_ASSERT(false, return {});
 }
 
-static QString lineColumnString(const Debugger::DiagnosticLocation &location)
+static QString lineColumnString(const Link &location)
 {
-    return QString("%1:%2").arg(QString::number(location.line), QString::number(location.column));
+    return QString("%1:%2").arg(location.targetLine).arg(location.targetColumn);
 }
 
 static QString createExplainingStepToolTipString(const ExplainingStep &step)
@@ -227,10 +229,10 @@ static QString createExplainingStepToolTipString(const ExplainingStep &step)
     return html;
 }
 
-static QString createLocationString(const Debugger::DiagnosticLocation &location)
+static QString createLocationString(const Link &location)
 {
-    const QString filePath = location.filePath.toUserOutput();
-    const QString lineNumber = QString::number(location.line);
+    const QString filePath = location.targetFilePath.toUserOutput();
+    const QString lineNumber = QString::number(location.targetLine);
     const QString fileAndLine = filePath + QLatin1Char(':') + lineNumber;
     return QLatin1String("in ") + fileAndLine;
 }
@@ -253,7 +255,7 @@ static QString createExplainingStepString(const ExplainingStep &explainingStep, 
 
 static QString fullText(const Diagnostic &diagnostic)
 {
-    QString text = diagnostic.location.filePath.toUserOutput() + QLatin1Char(':');
+    QString text = diagnostic.location.targetFilePath.toUserOutput() + QLatin1Char(':');
     text += lineColumnString(diagnostic.location) + QLatin1String(": ");
     if (!diagnostic.category.isEmpty())
         text += diagnostic.category + QLatin1String(": ");
@@ -441,7 +443,7 @@ ExplainingStepItem::ExplainingStepItem(const ExplainingStep &step, int index)
     , m_index(index)
 {}
 
-static QString rangeString(const QVector<Debugger::DiagnosticLocation> &ranges)
+static QString rangeString(const Links &ranges)
 {
     return QString("%1-%2").arg(lineColumnString(ranges[0]), lineColumnString(ranges[1]));
 }
@@ -455,7 +457,7 @@ QVariant ExplainingStepItem::data(int column, int role) const
             return QVariant::fromValue(m_step.location);
         case Debugger::DetailedErrorView::FullTextRole: {
             return QString("%1:%2: %3")
-                .arg(m_step.location.filePath.toUserOutput(),
+                .arg(m_step.location.targetFilePath.toUserOutput(),
                      lineColumnString(m_step.location),
                      m_step.message);
         }
@@ -467,11 +469,11 @@ QVariant ExplainingStepItem::data(int column, int role) const
             return parent()->data(column, role);
         case Qt::DisplayRole: {
             const Utils::FilePath mainFilePath
-                = static_cast<DiagnosticItem *>(parent())->diagnostic().location.filePath;
+                = static_cast<DiagnosticItem *>(parent())->diagnostic().location.targetFilePath;
             const QString locationString
-                = m_step.location.filePath == mainFilePath
+                = m_step.location.targetFilePath == mainFilePath
                       ? lineColumnString(m_step.location)
-                      : QString("%1:%2").arg(m_step.location.filePath.fileName(),
+                      : QString("%1:%2").arg(m_step.location.targetFilePath.fileName(),
                                              lineColumnString(m_step.location));
 
             if (m_step.isFixIt) {
@@ -654,7 +656,7 @@ bool DiagnosticFilterModel::filterAcceptsRow(int sourceRow, const QModelIndex &s
             Utils::FilePath filePath = d.filePath;
             if (d.filePath.toFileInfo().isRelative())
                 filePath = m_lastProjectDirectory.resolvePath(filePath);
-            if (filePath == diag.location.filePath) {
+            if (filePath == diag.location.targetFilePath) {
                 diagnosticItem->setTextMarkVisible(false);
                 return false;
             }
@@ -676,19 +678,18 @@ bool DiagnosticFilterModel::lessThan(const QModelIndex &l, const QModelIndex &r)
     if (sortColumn() == Debugger::DetailedErrorView::DiagnosticColumn && isComparingDiagnostics) {
         bool result = false;
         if (itemLeft->level() == 2) {
-            using Debugger::DiagnosticLocation;
             const int role = Debugger::DetailedErrorView::LocationRole;
 
-            const auto leftLoc = sourceModel()->data(l, role).value<DiagnosticLocation>();
+            const auto leftLoc = sourceModel()->data(l, role).value<Link>();
             const auto leftText
                 = sourceModel()->data(l, ClangToolsDiagnosticModel::TextRole).toString();
 
-            const auto rightLoc = sourceModel()->data(r, role).value<DiagnosticLocation>();
+            const auto rightLoc = sourceModel()->data(r, role).value<Link>();
             const auto rightText
                 = sourceModel()->data(r, ClangToolsDiagnosticModel::TextRole).toString();
 
-            result = std::tie(leftLoc.line, leftLoc.column, leftText)
-                     < std::tie(rightLoc.line, rightLoc.column, rightText);
+            result = std::tie(leftLoc.targetLine, leftLoc.targetColumn, leftText)
+                     < std::tie(rightLoc.targetLine, rightLoc.targetColumn, rightText);
         } else if (itemLeft->level() == 3) {
             Utils::TreeItem *itemRight = model->itemForIndex(r);
             QTC_ASSERT(itemRight, QSortFilterProxyModel::lessThan(l, r));

src/plugins/clangtools/clangtoolsdiagnosticview.cpp
@@ -17,8 +17,6 @@
 #include <cppeditor/cppmodelmanager.h>
 #include <cppeditor/cpprefactoringchanges.h>
 
-#include <debugger/analyzer/diagnosticlocation.h>
-
 #include <utils/algorithm.h>
 #include <utils/fileutils.h>
 #include <utils/qtcassert.h>
@@ -36,7 +34,7 @@
 #include <set>
 
 using namespace CppEditor;
-using namespace Debugger;
+using namespace Utils;
 
 namespace ClangTools {
 namespace Internal {
@@ -210,9 +208,8 @@ void DiagnosticView::suppressCurrentDiagnostic()
             diags << diag;
             continue;
         }
-        Utils::FilePath filePath = diag.location.filePath;
-        const Utils::FilePath relativeFilePath
-                = filePath.relativeChildPath(project->projectDirectory());
+        FilePath filePath = diag.location.targetFilePath;
+        const FilePath relativeFilePath = filePath.relativeChildPath(project->projectDirectory());
         if (!relativeFilePath.isEmpty())
             filePath = relativeFilePath;
         const SuppressedDiagnostic supDiag(filePath, diag.description,
@@ -242,7 +239,9 @@ void DiagnosticView::suppressCurrentDiagnosticInline()
         if (!isApplicable)
             continue;
 
-        diagnosticsPerFileAndLine[diag.location.filePath][diag.location.line] << diag.name;
+        diagnosticsPerFileAndLine[diag.location.targetFilePath][diag.location.targetLine]
+                << diag.name;
+
         diags << diag;
     }
 
@@ -485,9 +484,11 @@ void DiagnosticView::mouseDoubleClickEvent(QMouseEvent *event)
 void DiagnosticView::openEditorForCurrentIndex()
 {
     const QVariant v = model()->data(currentIndex(), Debugger::DetailedErrorView::LocationRole);
-    const auto loc = v.value<Debugger::DiagnosticLocation>();
-    if (loc.isValid())
-        Core::EditorManager::openEditorAt(Utils::Link(loc.filePath, loc.line, loc.column - 1));
+    Link loc = v.value<Link>();
+    if (loc.hasValidTarget()) {
+        --loc.targetColumn; // FIXME: Move this to the model side.
+        Core::EditorManager::openEditorAt(loc);
+    }
 }
 
 } // namespace Internal

commit ebb63ca0d8f92708c7b3e9eac1073357c3643837

ClangTools: Fix off-by-one errors in test
Presumably introduced by a40e803503e4186823e2ca4585c8a4555e89d9c2.

Change-Id: Ic123aacf1a9f6ecb947d515321a67df1e2354d44
Reviewed-by: David Schulz <david.schulz@qt.io>

src/plugins/clangtools/readexporteddiagnosticstest.cpp
@@ -68,14 +68,14 @@ void ReadExportedDiagnosticsTest::testTidy()
         = createFile(filePath(appendYamlSuffix("tidy.modernize-use-nullptr")), sourceFile);
     Diagnostic expectedDiag;
     expectedDiag.name = "modernize-use-nullptr";
-    expectedDiag.location = {sourceFile, 2, 25};
+    expectedDiag.location = {sourceFile, 2, 24};
     expectedDiag.description = "use nullptr [modernize-use-nullptr]";
     expectedDiag.type = "warning";
     expectedDiag.hasFixits = true;
     expectedDiag.explainingSteps = {
         ExplainingStep{"nullptr",
                        expectedDiag.location,
-                       {expectedDiag.location, {sourceFile, 2, 26}},
+                       {expectedDiag.location, {sourceFile, 2, 25}},
                        true}};
     const Result<Diagnostics> diags = readExportedDiagnostics(exportedFile);
 
@@ -103,23 +103,23 @@ void ReadExportedDiagnosticsTest::testTidy_ClangAnalyzer()
         = createFile(filePath(appendYamlSuffix("clang-analyzer.dividezero")), sourceFile);
     Diagnostic expectedDiag;
     expectedDiag.name = "clang-analyzer-core.DivideZero";
-    expectedDiag.location = {sourceFile, 4, 15};
+    expectedDiag.location = {sourceFile, 4, 14};
     expectedDiag.description = "Division by zero [clang-analyzer-core.DivideZero]";
     expectedDiag.type = "warning";
     expectedDiag.hasFixits = false;
     expectedDiag.explainingSteps = {
         ExplainingStep{"Assuming 'z' is equal to 0",
-                       {sourceFile, 3, 7},
+                       {sourceFile, 3, 6},
                        {},
                        false,
         },
         ExplainingStep{"Taking true branch",
-                       {sourceFile, 3, 3},
+                       {sourceFile, 3, 2},
                        {},
                        false,
         },
         ExplainingStep{"Division by zero",
-                       {sourceFile, 4, 15},
+                       {sourceFile, 4, 14},
                        {},
                        false,
         },
@@ -136,19 +136,19 @@ void ReadExportedDiagnosticsTest::testClazy()
                                              sourceFile);
     Diagnostic expectedDiag;
     expectedDiag.name = "clazy-qgetenv";
-    expectedDiag.location = {sourceFile, 7, 5};
+    expectedDiag.location = {sourceFile, 7, 4};
     expectedDiag.description = "qgetenv().isEmpty() allocates. Use qEnvironmentVariableIsEmpty() instead [clazy-qgetenv]";
     expectedDiag.type = "warning";
     expectedDiag.hasFixits = true;
     expectedDiag.explainingSteps = {
         ExplainingStep{"qEnvironmentVariableIsEmpty",
                        expectedDiag.location,
-                       {expectedDiag.location, {sourceFile, 7, 12}},
+                       {expectedDiag.location, {sourceFile, 7, 11}},
                        true
         },
         ExplainingStep{")",
-                       {sourceFile, 7, 18},
-                       {{sourceFile, 7, 18}, {sourceFile, 7, 29}},
+                       {sourceFile, 7, 17},
+                       {{sourceFile, 7, 17}, {sourceFile, 7, 28}},
                        true},
     };
     const Result<Diagnostics> diags = readExportedDiagnostics(exportedFile);