d0badbbd91f769aff1b1cbc9ea1d3d0d9190da5e
Miikka Heikkinen <miikka.heikkinen@qt.io>
2025-08-11 11:26:01 +0300

    QmlDesigner: Fix crash when sending custom notification
    Some views such as 2D view detach when they are not visible, which can
    happen if newly shown view is on same dock tab as the previous view.
    Detached view cannot be directly used to send custom notification,
    so we now use generic view from view manager to do so.
    
    Fixes: QDS-15668
    Change-Id: I0bccfcf09f8f2b1f244d43f3e9d82ace79512be3
    Reviewed-by: Mahmoud Badri <mahmoud.badri@qt.io>

--- src/plugins/qmldesigner/components/componentcore/modelnodeoperations.cpp
+++ src/plugins/qmldesigner/components/componentcore/modelnodeoperations.cpp
@@ -991,10 +991,12 @@ void extractComponent(const SelectionContext &selectionContext)
 
 void addNodeToContentLibrary(const SelectionContext &selectionContext)
 {
-    QmlDesignerPlugin::instance()->mainWidget()->showDockWidget("ContentLibrary");
     ModelNode node = selectionContext.currentSingleSelectedNode();
 
-    selectionContext.view()->emitCustomNotification("add_node_to_content_lib", {node});
+    QmlDesignerPlugin::instance()->mainWidget()->showDockWidget("ContentLibrary");
+
+    QmlDesignerPlugin::viewManager().view()->emitCustomNotification("add_node_to_content_lib",
+                                                                    {node});
 }
 
 void goImplementation(const SelectionContext &selectionState)
