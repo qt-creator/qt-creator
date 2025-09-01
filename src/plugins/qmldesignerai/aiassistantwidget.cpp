qt-creator/qt-creator

commit 981a90bcc26bb7b501cdcee1df11ae2f71057ef7

AiAssistant: Change UI to Qml
Task-number: QDS-15697
Change-Id: I4a63afb103e5594f7af3b4e40b2d8a0d6c80e3eb
Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>
Reviewed-by: Mahmoud Badri <mahmoud.badri@qt.io>

src/plugins/qmldesignerai/aiassistantwidget.cpp
@@ -5,8 +5,10 @@
 
 #include <asset.h>
 #include <designersettings.h>
+#include <qmldesignerconstants.h>
 #include <qmldesignerplugin.h>
 
+#include <coreplugin/icore.h>
 #include <utils/filepath.h>
 
 #include <QApplication>
@@ -19,7 +21,10 @@
 #include <QNetworkReply>
 #include <QNetworkRequest>
 #include <QPushButton>
+#include <QQmlContext>
+#include <QQmlEngine>
 #include <QTextEdit>
+#include <QtQuick/QQuickItem>
 
 namespace QmlDesigner {
 
@@ -36,6 +41,24 @@ QStringList getImageAssetsPaths()
         imagePaths << Utils::FilePath::fromString(it.next()).relativePathFrom(resourePath).toFSPathString();
 
     return imagePaths;
+}
+
+QString propertyEditorResourcesPath()
+{
+#ifdef SHARE_QML_PATH
+    if (::Utils::qtcEnvironmentVariableIsSet("LOAD_QML_FROM_SOURCE"))
+        return QLatin1String(SHARE_QML_PATH) + "/propertyEditorQmlSources";
+#endif
+    return Core::ICore::resourcePath("qmldesigner/propertyEditorQmlSources").toUrlishString();
+}
+
+QString qmlSourcesPath()
+{
+#ifdef SHARE_QML_PATH
+    if (Utils::qtcEnvironmentVariableIsSet("LOAD_QML_FROM_SOURCE"))
+        return QLatin1String(SHARE_QML_PATH) + "/aiAssistantQmlSources";
+#endif
+    return Core::ICore::resourcePath("qmldesigner/aiAssistantQmlSources").toUrlishString();
 }
 
 QString getContent(const QJsonObject &responseObject)
@@ -68,37 +91,9 @@ QString getContent(const QJsonObject &responseObject)
 
 } // namespace
 
-bool AiAssistantWidget::eventFilter(QObject *obj, QEvent *event)
-{
-    if (event->type() == QEvent::EnabledChange) {
-        // Only focus m_textInput if no other view has taken focus through user interaction
-        if (m_textInput->isEnabled() && !QApplication::focusWidget())
-            m_textInput->setFocus();
-    } else if (event->type() == QEvent::KeyPress) {
-        QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
-        if (keyEvent->key() == Qt::Key_Return && !(keyEvent->modifiers() & Qt::ShiftModifier)) {
-            handleMessage();
-            return true;
-        } else if (keyEvent->key() == Qt::Key_Up) {
-            if (m_historyIndex > 0)
-                m_textInput->setText(m_inputHistory.at(--m_historyIndex));
-            return true;
-        } else if (keyEvent->key() == Qt::Key_Down) {
-            if (m_historyIndex < m_inputHistory.size() - 1) {
-                m_textInput->setText(m_inputHistory.at(++m_historyIndex));
-            } else {
-                m_historyIndex = m_inputHistory.size();
-                m_textInput->clear();
-            }
-            return true;
-        }
-    }
-
-    return QWidget::eventFilter(obj, event);
-}
-
 AiAssistantWidget::AiAssistantWidget()
     : m_manager(std::make_unique<QNetworkAccessManager>())
+    , m_quickWidget(Utils::makeUniqueObjectPtr<StudioQuickWidget>())
 {
     setWindowTitle(tr("AI Assistant", "Title of Ai Assistant widget"));
     setMinimumWidth(220);
@@ -107,65 +102,20 @@ AiAssistantWidget::AiAssistantWidget()
     auto vLayout = new QVBoxLayout(this);
     vLayout->setContentsMargins(5, 5, 5, 5);
 
-    QHBoxLayout *hLayout = new QHBoxLayout();
-    hLayout->setSpacing(5);
+    m_quickWidget->setContentsMargins({0, 0, 0, 0});
+    m_quickWidget->quickWidget()->setObjectName(Constants::OBJECT_NAME_AI_ASSISTANT);
+    m_quickWidget->setResizeMode(QQuickWidget::SizeRootObjectToView);
+    m_quickWidget->engine()->addImportPath(propertyEditorResourcesPath() + "/imports");
 
-    m_textInput = new QTextEdit(this);
-    m_textInput->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    m_textInput->setStyleSheet("padding: 4px; font-size: 16px;");
-    m_textInput->setPlaceholderText("Type your message...");
-    m_textInput->installEventFilter(this);
-
-    QPushButton *sendButton = new QPushButton(this);
-    sendButton->setStyleSheet("font-size: 12px;");
-    sendButton->setFixedWidth(32);
-    sendButton->setFixedHeight(32);
-
-    QIcon buttonIcon(":/AiAssistant/images/ai.png");
-    sendButton->setIcon(buttonIcon);
-    sendButton->setIconSize(QSize(20, 20));
-
-    hLayout->addWidget(m_textInput.data());
-    hLayout->addWidget(sendButton);
-    hLayout->setAlignment(sendButton, Qt::AlignTop);
-
-    hLayout->setStretch(0, 1);
-
-    QPushButton *selectButton = new QPushButton("Attach", this);
-    selectButton->setStyleSheet("padding: 4px; font-size: 14px;");
-
-    m_imageLabel = new QLabel("", this);
-    m_imageLabel->setStyleSheet("padding: 4px; font-size: 14px; color: gray;");
-    m_imageLabel->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
-
-    // Create a popup menu
-    QMenu *imageMenu = new QMenu(this);
-
-    connect(imageMenu, &QMenu::aboutToShow, this, [imageMenu, this]() {
-        imageMenu->clear();
-        m_imagePaths = QStringList("") << getImageAssetsPaths();
-        for (const QString &option : std::as_const(m_imagePaths)) {
-            QAction *action = imageMenu->addAction(option);
-            connect(action, &QAction::triggered, m_imageLabel.data(), [option, this]() {
-                m_imageLabel->setText(option);
-            });
-        }
+    auto map = m_quickWidget->registerPropertyMap("AiAssistantBackend");
+    map->setProperties({
+        {"rootView", QVariant::fromValue(this)},
     });
 
-    // Connect the button to show the popup
-    connect(selectButton, &QPushButton::clicked, this, [selectButton, imageMenu]() {
-        imageMenu->exec(selectButton->mapToGlobal(QPoint(0, selectButton->height())));
-    });
+    m_quickWidget->rootContext()->setContextProperty("rootView", QVariant::fromValue(this));
 
-    QHBoxLayout *selectLayout = new QHBoxLayout();
-    selectLayout->addWidget(selectButton);
-    selectLayout->addWidget(m_imageLabel.data());
-    selectLayout->addStretch();
-
-    vLayout->addLayout(hLayout);
-    vLayout->addLayout(selectLayout);
-
-    connect(sendButton, &QPushButton::clicked, this, &AiAssistantWidget::handleMessage);
+    vLayout->addWidget(m_quickWidget.get());
+    reloadQmlSource();
 }
 
 QSize AiAssistantWidget::sizeHint() const
@@ -173,15 +123,18 @@ QSize AiAssistantWidget::sizeHint() const
     return {420, 20};
 }
 
-void AiAssistantWidget::handleMessage()
+QStringList AiAssistantWidget::imageAssetsModel() const
 {
-    QString prompt = m_textInput->toPlainText();
+    return QStringList{""} << getImageAssetsPaths();
+}
+
+void AiAssistantWidget::handleMessage(const QString &prompt)
+{
     if (prompt.isEmpty())
         return;
 
     m_inputHistory.append(prompt);
     m_historyIndex = m_inputHistory.size();
-    m_textInput->clear();
 
     QString currentQml = QmlDesignerPlugin::instance()->currentDesignDocument()
                              ->plainTextEdit()->toPlainText();
@@ -228,8 +181,9 @@ Request: %2
 )";
 
     QJsonObject userJson;
-    if (!m_imageLabel->text().isEmpty()) {
-        Utils::FilePath imagePath = DocumentManager::currentResourcePath().pathAppended(m_imageLabel->text());
+    if (const QString &attachedImage = this->attachedImage(); !attachedImage.isEmpty()) {
+        Utils::FilePath imagePath = DocumentManager::currentResourcePath().pathAppended(
+            attachedImage);
         if (imagePath.exists()) {
             QImage image(imagePath.toFSPathString());
             QByteArray byteArray;
@@ -302,6 +256,40 @@ Request: %2
         }
         m_reply->deleteLater();
     });
+}
+
+QString AiAssistantWidget::getPreviousCommand()
+{
+    if (m_historyIndex > 0)
+        --m_historyIndex;
+
+    if (m_inputHistory.size() > m_historyIndex)
+        return m_inputHistory.at(m_historyIndex);
+
+    return {};
+}
+
+QString AiAssistantWidget::getNextCommand()
+{
+    if (int newIdx = m_historyIndex + 1; newIdx < m_inputHistory.size())
+        m_historyIndex = newIdx;
+
+    if (m_inputHistory.size() > m_historyIndex)
+        return m_inputHistory.at(m_historyIndex);
+
+    return {};
+}
+
+void AiAssistantWidget::reloadQmlSource()
+{
+    const QString itemLibraryQmlPath = qmlSourcesPath() + "/AiAssistantView.qml";
+    QTC_ASSERT(QFileInfo::exists(itemLibraryQmlPath), return);
+    m_quickWidget->setSource(QUrl::fromLocalFile(itemLibraryQmlPath));
+}
+
+QString AiAssistantWidget::attachedImage() const
+{
+    return m_quickWidget->rootObject()->property("attachedImage").toString();
 }
 
 } // namespace QmlDesigner

src/plugins/qmldesignerai/aiassistantwidget.h
@@ -3,6 +3,10 @@
 
 #pragma once
 
+#include <studioquickwidget.h>
+
+#include <utils/uniqueobjectptr.h>
+
 #include <QFrame>
 #include <QNetworkAccessManager>
 #include <QPointer>
@@ -26,25 +30,23 @@ public:
     AiAssistantWidget();
     ~AiAssistantWidget() = default;
 
-    QList<QToolButton *> createToolBarWidgets();
-
-    static QString qmlSourcesPath();
-    void clearSearchFilter();
-
     QSize sizeHint() const override;
 
-protected:
-    bool eventFilter(QObject *obj, QEvent *event) override;
+    Q_INVOKABLE QStringList imageAssetsModel() const;
+    Q_INVOKABLE void handleMessage(const QString &prompt);
+    Q_INVOKABLE QString getPreviousCommand();
+    Q_INVOKABLE QString getNextCommand();
 
-private:
-    void handleMessage();
+private: // functions
+    void reloadQmlSource();
 
-    QPointer<QTextEdit> m_textInput;
-    QPointer<QLabel> m_imageLabel;
-    QStringList m_imagePaths;
+    QString attachedImage() const;
 
+private: // variables
     std::unique_ptr<QNetworkAccessManager> m_manager;
     QPointer<QNetworkReply> m_reply;
+
+    Utils::UniqueObjectPtr<StudioQuickWidget> m_quickWidget;
 
     QStringList m_inputHistory;
     int m_historyIndex = -1;

share/qtcreator/qmldesigner/aiAssistantQmlSources/AiAssistantView.qml
@@ -1,0 +1,87 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+pragma ComponentBehavior: Bound
+
+import QtQuick
+import QtQuick.Layouts
+import HelperWidgets as HelperWidgets
+import StudioControls as StudioControls
+import StudioTheme as StudioTheme
+import AiAssistantBackend
+
+Rectangle {
+    id: root
+
+    property var rootView: AiAssistantBackend.rootView
+    property alias attachedImage: attachedImageText.text
+
+    color: StudioTheme.Values.themeBackgroundColorNormal
+
+    ColumnLayout {
+        anchors.fill: parent
+        anchors.margins: StudioTheme.Values.marginTopBottom
+
+        PromptTextBox {
+            rootView: root.rootView
+
+            Layout.fillHeight: true
+            Layout.fillWidth: true
+            Layout.minimumHeight: 100
+        }
+
+        Text {
+            id: attachedImageText
+
+            color: StudioTheme.Values.themeTextColor
+        }
+
+        HelperWidgets.AbstractButton {
+            id: attachImageButton
+            objectName: "AttachImageButton"
+
+            Layout.alignment: Qt.AlignRight
+
+            style: StudioTheme.Values.viewBarButtonStyle
+            buttonIcon: StudioTheme.Constants.attach_medium
+            tooltip: qsTr("Attach an image.\nThe attached image will be analyzed and integrated into the response by the AI.")
+
+            onClicked: attachMenu.show()
+        }
+    }
+
+    StudioControls.Menu {
+        id: attachMenu
+
+        closePolicy: StudioControls.Menu.CloseOnEscape | StudioControls.Menu.CloseOnPressOutside
+        implicitWidth: 250
+
+        onClosed: {
+            attachRepeater.model = {}
+        }
+
+        function show() {
+            updateModel()
+            attachMenu.popup(attachImageButton)
+        }
+
+        function updateModel() {
+            let imagesModel = root.rootView.imageAssetsModel()
+            attachRepeater.model = imagesModel
+            if (!imagesModel.includes(root.attachedImage))
+                root.attachedImage = ""
+        }
+
+        Repeater {
+            id: attachRepeater
+
+            delegate: StudioControls.MenuItem {
+                id: menuItem
+
+                required property string modelData
+                text: modelData
+                onTriggered: root.attachedImage = menuItem.text
+            }
+        }
+    }
+}

share/qtcreator/qmldesigner/aiAssistantQmlSources/PromptTextBox.qml
@@ -1,0 +1,100 @@
+// Copyright (C) 2025 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
+
+import QtQuick
+import QtQuick.Layouts
+import HelperWidgets as HelperWidgets
+import StudioTheme as StudioTheme
+import QtQuick.Controls.Basic as Basic
+
+Rectangle {
+    id: root
+
+    required property var rootView
+
+    property StudioTheme.ControlStyle style: StudioTheme.ControlStyle {
+        radius: StudioTheme.Values.smallRadius
+    }
+
+    color: root.style.background.idle
+    border.color: root.style.border.idle
+    border.width: root.style.borderWidth
+    radius: root.style.radius
+
+    function send() {
+        let trimmed = textEdit.text.trim()
+        if (trimmed !== "")
+            root.rootView.handleMessage(textEdit.text)
+        textEdit.text = ""
+    }
+
+    RowLayout {
+        anchors.fill: parent
+
+        Basic.TextArea {
+            id: textEdit
+
+            Layout.fillHeight: true
+            Layout.fillWidth: true
+
+            padding: root.style.inputHorizontalPadding
+            clip: true
+            font.pixelSize: root.style.baseFontSize
+            color: root.style.text.idle
+            wrapMode: TextEdit.WordWrap
+
+            placeholderText: qsTr("Type your message...")
+            placeholderTextColor: root.style.text.placeholder
+
+            Keys.onPressed: function(event) {
+                if (event.isAutoRepeat) {
+                    event.accepted = false
+                    return
+                }
+
+                switch (event.key) {
+                case Qt.Key_Return:
+                case Qt.Key_Enter: {
+                    if (!(event.modifiers & Qt.ShiftModifier)){
+                        root.send()
+                        event.accepted = true
+                    } else {
+                        event.accepted = false
+                    }
+                } break
+                case Qt.Key_Up: {
+                    textEdit.text = root.rootView.getPreviousCommand()
+                    event.accepted = true
+                } break
+                case Qt.Key_Down: {
+                    textEdit.text = root.rootView.getNextCommand()
+                    event.accepted = true
+                } break
+                default:
+                    event.accepted = false
+                }
+            }
+        }
+
+        HelperWidgets.IconButton {
+            id: sendButton
+            objectName: "SendButton"
+
+            Layout.alignment: Qt.AlignBottom | Qt.AlignHCenter
+            Layout.margins: StudioTheme.Values.marginTopBottom
+
+            icon: StudioTheme.Constants.selectFill_medium
+
+            iconColor: {
+                return sendButton.enabled
+                    ? root.style.interaction
+                    : root.style.icon.disabled
+            }
+
+            tooltip: qsTr("Send")
+            enabled: textEdit.text !== ""
+
+            onClicked: root.send()
+        }
+    }
+}

share/qtcreator/qmldesigner/aiAssistantQmlSources/qmldir
@@ -1,0 +1,2 @@
+AiAssistantView 1.0 AiAssistantView.qml
+PromptTextBox 1.0 PromptTextBox.qml

src/plugins/qmldesigner/qmldesignerconstants.h
@@ -195,6 +195,7 @@ inline constexpr char OBJECT_NAME_SPLASH_SCREEN[] = "QQuickWidgetSplashScreen";
 inline constexpr char OBJECT_NAME_WELCOME_PAGE[] = "QQuickWidgetQDSWelcomePage";
 inline constexpr char OBJECT_NAME_CONNECTION_EDITOR[] = "QQuickWidgetConnectionEditor";
 inline constexpr char OBJECT_NAME_DEVICE_MANAGER[] = "QQuickWidgetDeviceManager";
+inline constexpr char OBJECT_NAME_AI_ASSISTANT[] = "QQuickWidgetAiAssistant";
 
 inline constexpr char ENVIRONMENT_SHOW_QML_ERRORS[] = "QMLDESIGNER_SHOW_QML_ERRORS";
 

src/plugins/qmldesignerai/CMakeLists.txt
@@ -1,7 +1,9 @@
 add_qtc_plugin(QmlDesignerAi
   CONDITION TARGET QtCreator::QmlDesigner
   PLUGIN_DEPENDS
-    QtCreator::QmlDesigner
+    QtCreator::QmlDesigner QtCreator::QmlDesignerBase
+  DEPENDS
+    Qt6::Qml
   SOURCES
     qmldesigneraiplugin.cpp qmldesigneraiplugin.h
     aiassistantview.cpp aiassistantview.h

commit b06009ca6ab562059df966955e2e88afa107b91e

QmlDesignerAi: Maintain prompt box focus after sending a prompt
Was broken by 981a90bcc26bb7b501cdcee1df11ae2f71057ef7

Change-Id: Icd4360093587fd4f70a122bcdcaac3ad9e949a79
Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>

src/plugins/qmldesignerai/aiassistantwidget.cpp
@@ -78,6 +78,17 @@ QString getContent(const QJsonObject &responseObject)
 
 } // namespace
 
+bool AiAssistantWidget::eventFilter(QObject *obj, QEvent *event)
+{
+    if (event->type() == QEvent::EnabledChange) {
+        // Only focus the AI Assitant widget if no other view has taken focus through user interaction
+        if (m_quickWidget->quickWidget()->isEnabled() && !QApplication::focusWidget())
+            m_quickWidget->quickWidget()->setFocus();
+    }
+
+    return QWidget::eventFilter(obj, event);
+}
+
 AiAssistantWidget::AiAssistantWidget()
     : m_manager(std::make_unique<QNetworkAccessManager>())
     , m_quickWidget(Utils::makeUniqueObjectPtr<StudioQuickWidget>())
@@ -94,6 +105,7 @@ AiAssistantWidget::AiAssistantWidget()
     m_quickWidget->setResizeMode(QQuickWidget::SizeRootObjectToView);
     m_quickWidget->setMinimumHeight(minimumHeight() - 5);
     m_quickWidget->engine()->addImportPath(propertyEditorResourcesPath() + "/imports");
+    m_quickWidget->quickWidget()->installEventFilter(this);
 
     auto map = m_quickWidget->registerPropertyMap("AiAssistantBackend");
     map->setProperties({

src/plugins/qmldesignerai/aiassistantwidget.h
@@ -45,6 +45,9 @@ public:
 signals:
     void isGeneratingChanged();
 
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
+
 private: // functions
     void reloadQmlSource();
     void setIsGenerating(bool val);