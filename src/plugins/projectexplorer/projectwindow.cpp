9d775ef7d2a47375d0512fe9eade0ee60b7350ff
hjk <hjk@qt.io>
2025-10-21 11:44:51 +0200

    ProjectExplorer: Remove selection from "non-current" tree views
    ... in the Project view.
    
    Fixes: QTCREATORBUG-33653
    Change-Id: I8a20a9a9e4e43c6aefeeeea181f60d083a64044f
    Reviewed-by: Eike Ziller <eike.ziller@qt.io>

--- src/plugins/projectexplorer/projectwindow.cpp
+++ src/plugins/projectexplorer/projectwindow.cpp
@@ -1420,24 +1420,33 @@ public:
         m_targetsView = new SelectorTree;
         m_targetsView->setModel(&m_projectsModel);
         m_targetsView->setContextMenuPolicy(Qt::CustomContextMenu);
-        connect(m_targetsView, &QAbstractItemView::activated,
-                this, &ProjectWindowPrivate::itemActivated);
+        connect(m_targetsView, &QAbstractItemView::activated, this, [this](const QModelIndex &idx) {
+            m_vanishedTargetsView->clearSelection();
+            m_projectSettingsView->clearSelection();
+            itemActivated(idx);
+        });
         connect(m_targetsView, &QWidget::customContextMenuRequested,
                 this, &ProjectWindowPrivate::openContextMenu);
 
         m_vanishedTargetsView = new SelectorTree;
         m_vanishedTargetsView->setModel(&m_projectsModel);
         m_vanishedTargetsView->setContextMenuPolicy(Qt::CustomContextMenu);
-        connect(m_vanishedTargetsView, &QAbstractItemView::activated,
-                this, &ProjectWindowPrivate::itemActivated);
+        connect(m_vanishedTargetsView, &QAbstractItemView::activated, this, [this](const QModelIndex &idx) {
+            m_targetsView->clearSelection();
+            m_projectSettingsView->clearSelection();
+            itemActivated(idx);
+        });
         connect(m_vanishedTargetsView, &QWidget::customContextMenuRequested,
                 this, &ProjectWindowPrivate::openContextMenu);
 
         m_projectSettingsView = new SelectorTree;
         m_projectSettingsView->setModel(&m_projectsModel);
         m_projectSettingsView->setContextMenuPolicy(Qt::CustomContextMenu);
-        connect(m_projectSettingsView, &QAbstractItemView::activated,
-                this, &ProjectWindowPrivate::itemActivated);
+        connect(m_projectSettingsView, &QAbstractItemView::activated, this, [this](const QModelIndex &idx) {
+            m_targetsView->clearSelection();
+            m_vanishedTargetsView->clearSelection();
+            itemActivated(idx);
+        });
         connect(m_projectSettingsView, &QWidget::customContextMenuRequested,
                 this, &ProjectWindowPrivate::openContextMenu);
 
