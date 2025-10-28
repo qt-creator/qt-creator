d27a99bc11dc0b8131858dd28b2ab73fc1b1745c
Eike Ziller <eike.ziller@qt.io>
2025-10-28 11:04:03 +0100

    Sentry: Add some more data about the build
    Qt version, architecture, compiler, and QtC revision
    
    Change-Id: I6d7249a913f68a58faca87e63bc8e0b3e4423fb0
    Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>

--- src/libs/utils/appinfo.cpp
+++ src/libs/utils/appinfo.cpp
@@ -7,7 +7,7 @@ Q_GLOBAL_STATIC(Utils::AppInfo, sAppInfo)
 
 namespace Utils {
 
-const Utils::AppInfo &appInfo()
+const AppInfo &appInfo()
 {
     return *sAppInfo;
 }
@@ -15,6 +15,34 @@ const Utils::AppInfo &appInfo()
 void Internal::setAppInfo(const AppInfo &info)
 {
     *sAppInfo = info;
+}
+
+QString compilerString()
+{
+#if defined(Q_CC_CLANG) // must be before GNU, because clang claims to be GNU too
+    QString platformSpecific;
+#if defined(__apple_build_version__) // Apple clang has other version numbers
+    platformSpecific = QLatin1String(" (Apple)");
+#elif defined(Q_CC_MSVC)
+    platformSpecific = QLatin1String(" (clang-cl)");
+#endif
+    return QLatin1String("Clang ") + QString::number(__clang_major__) + QLatin1Char('.')
+           + QString::number(__clang_minor__) + platformSpecific;
+#elif defined(Q_CC_GNU)
+    return QLatin1String("GCC ") + QLatin1String(__VERSION__);
+#elif defined(Q_CC_MSVC)
+    if (_MSC_VER > 1999)
+        return QLatin1String("MSVC <unknown>");
+    if (_MSC_VER >= 1930)
+        return QLatin1String("MSVC 2022");
+    if (_MSC_VER >= 1920)
+        return QLatin1String("MSVC 2019");
+    if (_MSC_VER >= 1910)
+        return QLatin1String("MSVC 2017");
+    if (_MSC_VER >= 1900)
+        return QLatin1String("MSVC 2015");
+#endif
+    return QLatin1String("<unknown compiler>");
 }
 
 } // namespace Utils
