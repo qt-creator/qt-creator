834e7c3bf2d6a933cedef84312857232494d4830
hjk <hjk@qt.io>
2025-11-13 09:14:15 +0100

    ProParser: Remove unused classes ProString{Ro,Rw}User
    Change-Id: I96d9407f5cd5d3c6673e0db5e23107aa5312d9b9
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/shared/proparser/proitems.h
+++ src/shared/proparser/proitems.h
@@ -244,54 +244,6 @@ QTextStream &operator<<(QTextStream &t, const ProString &str);
 template<typename A, typename B>
 QTextStream &operator<<(QTextStream &t, const QStringBuilder<A, B> &str) { return t << QString(str); }
 
-// This class manages read-only access to a ProString via a raw data QString
-// temporary, ensuring that the latter is accessed exclusively.
-class ProStringRoUser
-{
-public:
-    ProStringRoUser(QString &rs)
-    {
-        m_rs = &rs;
-    }
-    ProStringRoUser(const ProString &ps, QString &rs)
-        : ProStringRoUser(rs)
-    {
-        ps.toQString(rs);
-    }
-    // No destructor, as a RAII pattern cannot be used: references to the
-    // temporary string can legitimately outlive instances of this class
-    // (if they are held by Qt, e.g. in QRegExp).
-    QString &set(const ProString &ps) { return ps.toQString(*m_rs); }
-    QString &str() { return *m_rs; }
-
-protected:
-    QString *m_rs;
-};
-
-// This class manages read-write access to a ProString via a raw data QString
-// temporary, ensuring that the latter is accessed exclusively, and that raw
-// data does not leak outside its source's refcounting.
-class ProStringRwUser : public ProStringRoUser
-{
-public:
-    ProStringRwUser(QString &rs)
-        : ProStringRoUser(rs), m_ps(nullptr) {}
-    ProStringRwUser(const ProString &ps, QString &rs)
-        : ProStringRoUser(ps, rs), m_ps(&ps) {}
-    QString &set(const ProString &ps) { m_ps = &ps; return ProStringRoUser::set(ps); }
-    ProString extract(const QString &s) const
-        { return s.isSharedWith(*m_rs) ? *m_ps : ProString(s).setSource(*m_ps); }
-    ProString extract(const QString &s, const ProStringRwUser &other) const
-    {
-        if (other.m_ps && s.isSharedWith(*other.m_rs))
-            return *other.m_ps;
-        return extract(s);
-    }
-
-private:
-    const ProString *m_ps;
-};
-
 class ProStringList : public QVector<ProString> {
 public:
     ProStringList() {}
