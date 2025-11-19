a5f8431f7a5e9644dd4fd948894b3e244d9d83f9
Mahmoud Badri <mahmoud.badri@qt.io>
2025-11-19 15:35:05 +0200

    QmlDesignerAi: Move llama 70b model to the top of the list
    As it is the best model from Groq, so to used it by default.
    
    Change-Id: Ic47d864dacf7ed14a5e753ebc75167e35307e912
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>
    (cherry picked from commit 5705fdc56b7ccb16098673369410792479502895)
    Reviewed-by: Mahmoud Badri <mahmoud.badri@qt.io>

--- src/plugins/qmldesignerai/settings/aiproviderdata.cpp
+++ src/plugins/qmldesignerai/settings/aiproviderdata.cpp
@@ -19,8 +19,8 @@ AiProviderData AiProviderData::defaultProviderData(const QString &providerName)
         {"Groq", AiProviderData{
             .url = QUrl{"https://api.groq.com/openai/v1/chat/completions"},
             .models = {
-                "llama-3.1-8b-instant",
                 "llama-3.3-70b-versatile",
+                "llama-3.1-8b-instant",
                 "openai/gpt-oss-120b",
                 "openai/gpt-oss-20b",
                 "groq/compound",
