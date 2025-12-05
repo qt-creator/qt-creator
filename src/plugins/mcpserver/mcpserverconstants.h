25c3bd04f5dc09f9a6854d410b8229b0b2a4b481
Cristian Adam <cristian.adam@qt.io>
2025-12-05 14:20:39 +0100

    mcpserver: Remove menu actions
    These were used for testing, and are not needed when a real MCP client
    is used.
    
    Change-Id: If4330db0bf71b9eaf12b29a68792e5b3ce6663b7
    Reviewed-by: hjk <hjk@qt.io>

--- src/plugins/mcpserver/mcpserverconstants.h
+++ src/plugins/mcpserver/mcpserverconstants.h
@@ -11,27 +11,4 @@ const char MENU_ID[] = "MCPServer.Menu";
 // About action
 const char ABOUT_ACTION_ID[] = "MCPServer.About";
 
-// MCP Command actions
-const char BUILD_ACTION_ID[] = "MCPServer.Build";
-const char DEBUG_ACTION_ID[] = "MCPServer.Debug";
-const char STOP_DEBUG_ACTION_ID[] = "MCPServer.StopDebug";
-const char LIST_PROJECTS_ACTION_ID[] = "MCPServer.ListProjects";
-const char LIST_BUILD_CONFIGS_ACTION_ID[] = "MCPServer.ListBuildConfigs";
-const char QUIT_ACTION_ID[] = "MCPServer.Quit";
-const char GET_CURRENT_PROJECT_ACTION_ID[] = "MCPServer.GetCurrentProject";
-const char GET_CURRENT_BUILD_CONFIG_ACTION_ID[] = "MCPServer.GetCurrentBuildConfig";
-const char RUN_PROJECT_ACTION_ID[] = "MCPServer.RunProject";
-const char CLEAN_PROJECT_ACTION_ID[] = "MCPServer.CleanProject";
-const char LIST_OPEN_FILES_ACTION_ID[] = "MCPServer.ListOpenFiles";
-const char LIST_SESSIONS_ACTION_ID[] = "MCPServer.ListSessions";
-const char GET_CURRENT_SESSION_ACTION_ID[] = "MCPServer.GetCurrentSession";
-const char SAVE_SESSION_ACTION_ID[] = "MCPServer.SaveSession";
-const char LIST_ISSUES_ACTION_ID[] = "MCPServer.ListIssues";
-const char GET_METHOD_METADATA_ACTION_ID[] = "MCPServer.GetMethodMetadata";
-const char SET_METHOD_METADATA_ACTION_ID[] = "MCPServer.SetMethodMetadata";
-
-// MCP Protocol Discovery actions
-const char MCP_INITIALIZE_ACTION_ID[] = "MCPServer.MCPInitialize";
-const char MCP_TOOLS_LIST_ACTION_ID[] = "MCPServer.MCPToolsList";
-
 } // namespace Mcp::Constants
