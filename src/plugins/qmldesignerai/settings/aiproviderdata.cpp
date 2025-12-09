ee0e059984ce93751bd502ebc2941cf7c12d2f70
Ali Kianian <ali.kianian@qt.io>
2025-12-09 14:19:00 +0200

    QmlDesignerAi: Enable models that require Responses API
    Task-number: QDS-16143
    Change-Id: I0412459faa52667ecd6f2391b96795c72053562f
    Reviewed-by: Miikka Heikkinen <miikka.heikkinen@qt.io>
    Reviewed-by: Mahmoud Badri <mahmoud.badri@qt.io>

--- src/plugins/qmldesignerai/settings/aiproviderdata.cpp
+++ src/plugins/qmldesignerai/settings/aiproviderdata.cpp
@@ -42,17 +42,18 @@ AiProviderData AiProviderData::defaultProviderData(const QString &providerName)
         {"OpenAI", AiProviderData{
             .url = QUrl{"https://api.openai.com/v1/chat/completions"},
             .models = {
-                // TODO: commented out models require Responses API which will be implemented soon
-                // https://platform.openai.com/docs/guides/migrate-to-responses
                 "gpt-5",
                 "gpt-5-mini",
                 "gpt-5-nano",
-                // "gpt-5-pro",
+                // "gpt-5-pro", Code execution is not supported https://platform.openai.com/docs/models/gpt-5-pro/?utm_source=chatgpt.com
+                "gpt-5-codex",
+                "gpt-5.1-codex",
+                "gpt-5.1-codex-mini",
                 "gpt-4.1",
                 "gpt-4.1-mini",
                 "gpt-4.1-nano",
-                // "gpt-4o",
-                // "gpt-4o-mini",
+                "gpt-4o",
+                "gpt-4o-mini",
             }
         }},
         {"Claude", AiProviderData{
