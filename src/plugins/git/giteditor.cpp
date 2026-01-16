qt-creator/qt-creator

commit 6f11521923617f4025fb50d281ad2f591c546935

Git: Blame: Add option to remove file path and author
Git does not provide a way to omit the file path (only an option
to force it). For files that have been moved the file path is
included in the annotation so we need to filter it out manually.

For more flexibility, replace the code with regular expressions.
This is a bit slower, but makes the code more maintainable.

For example, the filtering of gitclient.cpp (4070 lines) takes
15 ms on my machine and the worst I have seen so far was 20 ms.

Fixes: QTCREATORBUG-33636
Change-Id: Ib82e02f1bd0d27248d807c374a61286ad96be93b
Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/git/giteditor.cpp
@@ -122,59 +122,36 @@ VcsBase::BaseAnnotationHighlighterCreator GitEditorWidget::annotationHighlighter
     return VcsBase::getAnnotationHighlighterCreator<GitAnnotationHighlighter>();
 }
 
-/* Remove the date specification from annotation, which is tabular:
-\code
-8ca887aa (author               YYYY-MM-DD HH:MM:SS <offset>  <line>)<content>
-\endcode */
-
+/**
+ * Optionally remove path, author or date specification from annotation, which is tabular:
+ * \code
+ * 8ca887aa filepath (author YYYY-MM-DD HH:MM:SS <offset> <line>) <content>
+ * \endcode
+ */
 static QString sanitizeBlameOutput(const QString &b)
 {
+    static const char pattern[] =
+        R"(^(\S+)\s(.+?)\s\((.*)\s+(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\s\+\d{4}).*?\)(.*)$)";
+    static const QRegularExpression re(pattern, QRegularExpression::MultilineOption);
+
     if (b.isEmpty())
         return b;
 
+    const bool omitPath = settings().omitAnnotationPath();
+    const bool omitAuthor = settings().omitAnnotationAuthor();
     const bool omitDate = settings().omitAnnotationDate();
-    const QChar space(' ');
-    const int parenPos = b.indexOf(')');
-    if (parenPos == -1)
-        return b;
 
-    int i = parenPos;
-    while (i >= 0 && b.at(i) != space)
-        --i;
-    while (i >= 0 && b.at(i) == space)
-        --i;
-    int stripPos = i + 1;
-    if (omitDate) {
-        int spaceCount = 0;
-        // i is now on timezone. Go back 3 spaces: That is where the date starts.
-        while (i >= 0) {
-            if (b.at(i) == space)
-                ++spaceCount;
-            if (spaceCount == 3) {
-                stripPos = i;
-                break;
-            }
-            --i;
-        }
-    }
-
-    // Copy over the parts that have not changed into a new byte array
     QString result;
-    int prevPos = 0;
-    int pos = b.indexOf('\n', 0) + 1;
-    forever {
-        QTC_CHECK(prevPos < pos);
-        int afterParen = prevPos + parenPos;
-        result.append(b.mid(prevPos, stripPos));
-        result.append(b.mid(afterParen, pos - afterParen));
-        prevPos = pos;
-        QTC_CHECK(prevPos != 0);
-        if (pos == b.size())
-            break;
-
-        pos = b.indexOf('\n', pos) + 1;
-        if (pos == 0) // indexOf returned -1
-            pos = b.size();
+    QRegularExpressionMatchIterator i = re.globalMatch(b);
+    while (i.hasNext()) {
+        static const QString sep = "  ";
+        QRegularExpressionMatch match = i.next();
+        const QString hash   = match.captured(1) + sep;
+        const QString path   = omitPath   ? QString() : match.captured(2) + sep;
+        const QString author = omitAuthor ? QString() : match.captured(3) + sep;
+        const QString date   = omitDate   ? QString() : match.captured(4) + sep;
+        const QString code   = match.captured(5);
+        result.append(hash + path + author + date + code + "\n");
     }
     return result;
 }

src/plugins/git/gitclient.cpp
@@ -596,6 +596,12 @@ public:
     explicit GitBlameConfig(QToolBar *toolBar)
         : VcsBaseEditorConfig(toolBar)
     {
+        mapSetting(addToggleButton(QString(), Tr::tr("Omit Path"),
+                                   Tr::tr("Hide the file path of a change from the output.")),
+                   &settings().omitAnnotationPath);
+        mapSetting(addToggleButton(QString(), Tr::tr("Omit Author"),
+                                   Tr::tr("Hide the author of a change from the output.")),
+                   &settings().omitAnnotationAuthor);
         mapSetting(addToggleButton(QString(), Tr::tr("Omit Date"),
                                    Tr::tr("Hide the date of a change from the output.")),
                    &settings().omitAnnotationDate);
@@ -1401,7 +1407,7 @@ void GitClient::annotate(const Utils::FilePath &workingDir, const QString &file,
     }
 
     editor->setWorkingDirectory(workingDir);
-    QStringList arguments = {"blame", "--root"};
+    QStringList arguments = {"blame", "--root", "--show-name"};
     arguments << argWidget->arguments();
     if (!revision.isEmpty())
         arguments << revision;

src/plugins/git/gitsettings.cpp
@@ -55,6 +55,8 @@ GitSettings::GitSettings()
 
     showTags.setSettingsKey("ShowTags");
 
+    omitAnnotationPath.setSettingsKey("OmitAnnotationPath");
+    omitAnnotationAuthor.setSettingsKey("OmitAnnotationAuthor");
     omitAnnotationDate.setSettingsKey("OmitAnnotationDate");
 
     ignoreSpaceChangesInDiff.setSettingsKey("SpaceIgnorantDiff");

src/plugins/git/gitsettings.h
@@ -24,6 +24,8 @@ public:
     Utils::BoolAspect rebaseMerges{this};
     Utils::BoolAspect updateRefs{this};
     Utils::BoolAspect showTags{this};
+    Utils::BoolAspect omitAnnotationPath{this};
+    Utils::BoolAspect omitAnnotationAuthor{this};
     Utils::BoolAspect omitAnnotationDate{this};
     Utils::BoolAspect ignoreSpaceChangesInDiff{this};
     Utils::BoolAspect ignoreSpaceChangesInBlame{this};

commit d57719caa70fc7927687bb13bde7f59b85741167

Git: Blame: Fix blaming parent revision
The logic requires to have an opening brace after the
file path as indication for author and date.

Broken by 6f11521923617f4025fb50d281ad2f591c546935

Change-Id: Ifb22205b20dea6d29b7904c582fb9244b7d0efa0
Reviewed-by: Orgad Shaneh <orgads@gmail.com>

src/plugins/git/giteditor.cpp
@@ -147,11 +147,11 @@ static QString sanitizeBlameOutput(const QString &b)
         static const QString sep = "  ";
         QRegularExpressionMatch match = i.next();
         const QString hash   = match.captured(1) + sep;
-        const QString path   = omitPath   ? QString() : match.captured(2) + sep;
+        const QString path   = omitPath   ? QString() : match.captured(2);
         const QString author = omitAuthor ? QString() : match.captured(3) + sep;
-        const QString date   = omitDate   ? QString() : match.captured(4) + sep;
+        const QString date   = omitDate   ? QString() : match.captured(4);
         const QString code   = match.captured(5);
-        result.append(hash + path + author + date + code + "\n");
+        result.append(hash + path + "  (" + author + date + ")  " + code + "\n");
     }
     return result;
 }