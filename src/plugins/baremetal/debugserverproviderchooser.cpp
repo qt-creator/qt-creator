08a6876b05314c8e671c6aec030d462efcd247ad
hjk <hjk@qt.io>
2026-01-23 14:40:53 +0100

    BareMetal: Drop deprecated parent parameter in manageButtonClicked()
    Change-Id: Ifdcc94d5faf95fa805aca6bf4f4ed9f470e752d8
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/plugins/baremetal/debugserverproviderchooser.cpp
+++ src/plugins/baremetal/debugserverproviderchooser.cpp
@@ -59,7 +59,7 @@ void DebugServerProviderChooser::setCurrentProviderId(const QString &id)
 
 void DebugServerProviderChooser::manageButtonClicked()
 {
-    Core::ICore::showOptionsDialog(Constants::DEBUG_SERVER_PROVIDERS_SETTINGS_ID, this);
+    Core::ICore::showOptionsDialog(Constants::DEBUG_SERVER_PROVIDERS_SETTINGS_ID);
 }
 
 void DebugServerProviderChooser::currentIndexChanged(int index)
