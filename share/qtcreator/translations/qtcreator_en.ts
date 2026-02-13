a4d6c19d8474fecb914ede428f070920eb76c743
Eike Ziller <eike.ziller@qt.io>
2026-02-13 15:12:51 +0100

    Fix some UI text errors that sneaked past string freeze
    and were only found while actually translating.
    
    - it is Preferences mode
    - it is Show in File Manager
    - missing full stop
    
    Change-Id: Iabdbbb04fd0bdc4a5d8a6ad391fa430d29cacae2
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>

--- share/qtcreator/translations/qtcreator_en.ts
+++ share/qtcreator/translations/qtcreator_en.ts
@@ -14,7 +14,24 @@
 -->
 <!DOCTYPE TS>
 <TS version="2.1" language="en">
-
+<context>
+    <name>QtC::Core</name>
+    <message>
+        <source>Stay in Settings Mode</source>
+        <translation>Stay in Preferences Mode</translation>
+    </message>
+    <message>
+        <source>Uses the &lt;a href=&quot;%1&quot;&gt;freedesktop.org D-Bus interface&lt;/a&gt; for &lt;i&gt;Open in File Manager&lt;/i&gt;, if available. Otherwise falls back to the &quot;External file browser&quot; above.</source>
+        <translation>Uses the &lt;a href=&quot;%1&quot;&gt;freedesktop.org D-Bus interface&lt;/a&gt; for &lt;i&gt;Show in File Manager&lt;/i&gt;, if available. Otherwise falls back to the &quot;External file browser&quot; above.</translation>
+    </message>
+</context>
+<context>
+    <name>QtC::ProjectExplorer</name>
+    <message>
+        <source>The plugin &quot;%1&quot; is required to open projects of type &quot;%2&quot; Do you want to enable it now?</source>
+        <translation>The plugin &quot;%1&quot; is required to open projects of type &quot;%2&quot;. Do you want to enable it now?</translation>
+    </message>
+</context>
 <!-- test entry
 <context>
     <name>QtC::Core</name>
