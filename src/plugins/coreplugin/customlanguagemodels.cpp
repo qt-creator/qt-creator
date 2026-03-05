929f8a5902788eaebcb354d43946532cee2e10f8
Marcus Tillmanns <marcus.tillmanns@qt.io>
2026-03-05 07:38:09 +0100

    Core: Use built-in list display for custom llms
    Change-Id: I90c685d2d822b2ddeb4dff2d41c7a07e88276f08
    Reviewed-by: Christian Kandeler <christian.kandeler@qt.io>

--- src/plugins/coreplugin/customlanguagemodels.cpp
+++ src/plugins/coreplugin/customlanguagemodels.cpp
@@ -31,6 +31,7 @@ public:
     static const CustomLanguageModel &fromBaseAspect(const std::shared_ptr<BaseAspect> &aspect);
 
     CustomLanguageModel();
+
     CommandLine commandLine();
 
     StringAspect name{this};
@@ -38,46 +39,12 @@ public:
     StringAspect arguments{this};
 };
 
-class LanguageModelItem : public TreeItem
-{
-public:
-    LanguageModelItem(const std::shared_ptr<BaseAspect> &model);
-
-    bool hasModel(const std::shared_ptr<BaseAspect> &aspect) const { return aspect == m_model; }
-    const CustomLanguageModel &model() const { return CustomLanguageModel::fromBaseAspect(m_model); }
-
-private:
-    QVariant data(int column, int role) const override;
-
-    const std::shared_ptr<BaseAspect> m_model;
-    QObject guard;
-};
-
-class LanguageModelsListModel : public TreeModel<TreeItem>
-{
-public:
-    LanguageModelsListModel(AspectList &models);
-    void sync();
-
-private:
-    void appendChild(const std::shared_ptr<BaseAspect> &aspect);
-
-    AspectList &m_models;
-};
-
 class CustomLanguageModels : public AspectContainer
 {
 public:
     CustomLanguageModels();
 
-    void apply() override
-    {
-        AspectContainer::apply();
-        emit listModel.layoutChanged();
-    }
-
     AspectList models{this};
-    LanguageModelsListModel listModel{models};
 };
 
 CustomLanguageModels &customLanguageModels()
@@ -121,71 +88,20 @@ CustomLanguageModel::CustomLanguageModel()
     name.setDisplayName(Tr::tr("Name"));
     name.setDisplayStyle(StringAspect::LineEditDisplay);
     name.setLabelText(Tr::tr("Name:"));
+    name.setValue(Tr::tr("<New model>"));
+
     executable.setSettingsKey("Executable");
     executable.setDisplayName(Tr::tr("Executable"));
     executable.setLabelText(Tr::tr("Executable:"));
     executable.setExpectedKind(PathChooser::ExistingCommand);
+
     arguments.setSettingsKey("Arguments");
     arguments.setDisplayName(Tr::tr("Arguments"));
     arguments.setLabelText(Tr::tr("Arguments:"));
     arguments.setDisplayStyle(StringAspect::LineEditDisplay);
 
-    name.addOnVolatileValueChanged(this, markSettingsDirty);
-    executable.addOnVolatileValueChanged(this, markSettingsDirty);
-    arguments.addOnVolatileValueChanged(this, markSettingsDirty);
-
     using namespace Layouting;
-    setLayouter([this] { return Form{name, br, executable, br, arguments}; });
-}
-
-LanguageModelsListModel::LanguageModelsListModel(AspectList &models) : m_models(models)
-{
-    sync();
-    connect(&m_models, &BaseAspect::volatileValueChanged,
-            this, &LanguageModelsListModel::sync);
-    m_models.itemAddedCallback = [this](const std::shared_ptr<BaseAspect> &aspect) {
-        appendChild(aspect);
-    };
-    m_models.itemRemovedCallback = [this](const std::shared_ptr<BaseAspect> &aspect) {
-        for (int i = 0; i < rootItem()->childCount(); ++i) {
-            if (static_cast<LanguageModelItem *>(rootItem()->childAt(i))->hasModel(aspect)) {
-                rootItem()->removeChildAt(i);
-                break;
-            }
-        }
-    };
-}
-
-void LanguageModelsListModel::sync()
-{
-    clear();
-    for (const auto &aspect : m_models.volatileItems())
-        appendChild(aspect);
-}
-
-void LanguageModelsListModel::appendChild(const std::shared_ptr<BaseAspect> &aspect)
-{
-    rootItem()->appendChild(new LanguageModelItem(aspect));
-}
-
-LanguageModelItem::LanguageModelItem(const std::shared_ptr<BaseAspect> &model) : m_model(model)
-{
-    QObject::connect(model.get(), &BaseAspect::volatileValueChanged,
-                     &guard, [this] { update(); });
-}
-
-QVariant LanguageModelItem::data(int column, int role) const
-{
-    if (column != 0)
-        return {};
-    if (role == Qt::DisplayRole)
-        return CustomLanguageModel::fromBaseAspect(m_model).name.volatileValue();
-    if (role == Qt::FontRole) {
-        QFont f;
-        f.setBold(m_model->isDirty());
-        return f;
-    }
-    return {};
+    setLayouter([this] { return Form { name, br, executable, br, arguments }; });
 }
 
 CustomLanguageModels::CustomLanguageModels()
@@ -194,62 +110,17 @@ CustomLanguageModels::CustomLanguageModels()
     setSettingsGroup("CustomLanguageModels");
     models.setCreateItemFunction([] { return std::make_shared<CustomLanguageModel>(); });
     models.setSettingsKey("ModelList");
-    readSettings();
-    listModel.sync();
-    setLayouter([this] {
-        // Widgets
-        const auto view = new TreeView;
-        view->setModel(&listModel);
-        view->header()->hide();
-        const auto addButton = new QPushButton(Tr::tr("Add"));
-        const auto removeButton = new QPushButton(Tr::tr("Remove"));
-        const auto configWidget = new QWidget;
-        removeButton->setEnabled(false);
-        connect(addButton, &QPushButton::clicked, this, [this] {
-            const auto model = std::make_shared<CustomLanguageModel>();
-            models.addItem(model);
-            model->name.setVolatileValue(Tr::tr("<New model>"));
-        });
-        connect(removeButton, &QPushButton::clicked, this, [this, view] {
-            QTC_ASSERT(view->currentIndex().isValid(), return);
-            const auto item = static_cast<LanguageModelItem *>(
-                listModel.itemForIndex(view->currentIndex()));
-            QTC_ASSERT(item, return);
-            for (const auto &l = models.volatileItems(); const auto &i : l) {
-                if (item->hasModel(i)) {
-                    models.removeItem(i);
-                    break;
-                }
-            }
-        });
-        connect(view->selectionModel(), &QItemSelectionModel::currentChanged,
-                this, [this, configWidget, removeButton](const QModelIndex &current) {
-                    const bool hasCurrent = current.isValid();
-                    removeButton->setEnabled(hasCurrent);
-                    if (hasCurrent) {
-                        const auto item = static_cast<LanguageModelItem *>(
-                            listModel.itemForIndex(current));
-                        QTC_ASSERT(item, return);
-                        delete configWidget->layout();
-                        item->model().layouter()().attachTo(configWidget);
-                    }
-                    configWidget->setVisible(hasCurrent);
-                });
+    models.setDisplayStyle(AspectList::DisplayStyle::ListViewWithDetails);
+    models.listViewDisplayCallback = [](CustomLanguageModel *aspect) {
+        return aspect->name.volatileValue();
+    };
 
-        // Layouts
+    setLayouter([this] {
         using namespace Layouting;
-        return Column {
-            Row {
-                view,
-                Column {
-                    addButton,
-                    removeButton,
-                    st
-                }
-            },
-            configWidget
-        };
+        return Column { models };
     });
+
+    readSettings();
 }
 
 } // namespace
