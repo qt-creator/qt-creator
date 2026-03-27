f34e2917b4438ca7352ec741575cd8ff78144e9f
hjk <hjk@qt.io>
2026-03-27 13:04:16 +0100

    CMake: Compute some rarely used settings data on-the-fly
    Change-Id: Id88c55b3ffab6afaf352024a00acf6833d47f6be
    Reviewed-by: Cristian Adam <cristian.adam@qt.io>

--- src/plugins/cmakeprojectmanager/cmakesettingspage.cpp
+++ src/plugins/cmakeprojectmanager/cmakesettingspage.cpp
@@ -74,10 +74,8 @@ public:
     QString m_versionDisplay;
     DetectionSource m_detectionSource;
     bool m_isAutoRun = true;
-    bool m_pathExists = false;
-    bool m_pathIsFile = false;
-    bool m_pathIsExecutable = false;
     bool m_isSupported = false;
+    bool m_hasError = false;
 };
 
 } // namespace CMakeProjectManager::Internal
@@ -144,11 +142,6 @@ CMakeToolTreeItem::CMakeToolTreeItem(
 
 void CMakeToolTreeItem::updateErrorFlags()
 {
-    const FilePath filePath = CMakeTool::cmakeExecutable(m_executable);
-    m_pathExists = filePath.exists();
-    m_pathIsFile = filePath.isFile();
-    m_pathIsExecutable = filePath.isExecutableFile();
-
     std::unique_ptr<CMakeTool> temporaryTool;
     CMakeTool *cmake = CMakeToolManager::findById(m_id);
     if (!cmake) {
@@ -158,6 +151,7 @@ void CMakeToolTreeItem::updateErrorFlags()
     }
     cmake->setFilePath(m_executable);
     m_isSupported = cmake->hasFileApi();
+    m_hasError = !m_isSupported || !CMakeTool::cmakeExecutable(m_executable).isExecutableFile();
 
     m_tooltip = Tr::tr("Version: %1").arg(cmake->versionDisplay());
     m_tooltip += "<br>"
@@ -169,11 +163,6 @@ void CMakeToolTreeItem::updateErrorFlags()
     // Make sure to always have the right version in the name for Qt SDK CMake installations
     if (m_detectionSource.isAutoDetected() && m_name.startsWith("CMake") && m_name.endsWith("(Qt)"))
         m_name = QString("CMake %1 (Qt)").arg(m_versionDisplay);
-}
-
-bool CMakeToolTreeItem::hasError() const
-{
-    return !m_isSupported || !m_pathExists || !m_pathIsFile || !m_pathIsExecutable;
 }
 
 QVariant CMakeToolTreeItem::data(int column, int role) const
@@ -194,11 +183,12 @@ QVariant CMakeToolTreeItem::data(int column, int role) const
     case Qt::ToolTipRole: {
         QString result = m_tooltip;
         QString error;
-        if (!m_pathExists) {
+        const FilePath filePath = CMakeTool::cmakeExecutable(m_executable);
+        if (!filePath.exists()) {
             error = Tr::tr("CMake executable path does not exist.");
-        } else if (!m_pathIsFile) {
+        } else if (!filePath.isFile()) {
             error = Tr::tr("CMake executable path is not a file.");
-        } else if (!m_pathIsExecutable) {
+        } else if (!filePath.isExecutableFile()) {
             error = Tr::tr("CMake executable path is not executable.");
         } else if (!m_isSupported) {
             error = Tr::tr("CMake executable does not provide required IDE integration features.");
@@ -209,14 +199,14 @@ QVariant CMakeToolTreeItem::data(int column, int role) const
             return QString("%1<br><br><b>%2</b>").arg(result).arg(error);
     }
     case Qt::DecorationRole: {
-        if (column == 0 && hasError())
+        if (column == 0 && m_hasError)
             return Icons::CRITICAL.icon();
         return QVariant();
     }
     case ProjectExplorer::KitAspect::IdRole:
         return m_id.toSetting();
     case ProjectExplorer::KitAspect::QualityRole:
-        return int(!hasError());
+        return m_hasError ? 0 : 1;
     }
     return QVariant();
 }
