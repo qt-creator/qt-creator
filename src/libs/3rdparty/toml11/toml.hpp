4a23c54c689358090763168f3e55333451168acc
Marcus Tillmanns <marcus.tillmanns@qt.io>
2026-03-30 15:29:44 +0200

    TOML: Fix warnings
    Change-Id: I3011866abd5ca1b3fd1e3ce4ce4f2ccc583f364f
    Reviewed-by: Cristian Adam <cristian.adam@qt.io>

--- src/libs/3rdparty/toml11/toml.hpp
+++ src/libs/3rdparty/toml11/toml.hpp
@@ -16491,12 +16491,12 @@ inline namespace literals
 inline namespace toml_literals
 {
 
-::toml::value operator"" _toml(const char* str, std::size_t len);
+::toml::value operator""_toml(const char* str, std::size_t len);
 
 #if defined(TOML11_HAS_CHAR8_T)
 // value of u8"" literal has been changed from char to char8_t and char8_t is
 // NOT compatible to char
-::toml::value operator"" _toml(const char8_t* str, std::size_t len);
+::toml::value operator""_toml(const char8_t* str, std::size_t len);
 #endif
 
 } // toml_literals
@@ -16619,7 +16619,7 @@ inline namespace toml_literals
 {
 
 TOML11_INLINE ::toml::value
-operator"" _toml(const char* str, std::size_t len)
+operator""_toml(const char* str, std::size_t len)
 {
     if(len == 0)
     {
@@ -16650,7 +16650,7 @@ operator"" _toml(const char* str, std::size_t len)
 // value of u8"" literal has been changed from char to char8_t and char8_t is
 // NOT compatible to char
 TOML11_INLINE ::toml::value
-operator"" _toml(const char8_t* str, std::size_t len)
+operator""_toml(const char8_t* str, std::size_t len)
 {
     if(len == 0)
     {
