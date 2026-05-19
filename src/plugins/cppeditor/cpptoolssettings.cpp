qt-creator/qt-creator

commit 803a269047296e27d287cd0ff5b32e6c31054d33

TextEditor: Remove indirections on BehaviorSettingsPage
Since moving to the .cpp the pimpl serves no purpose anymode.
Also, make some pointer members real members.

Change-Id: Ib1ce8d9f0200207bde87eb82827ec3edb22ceff8
Reviewed-by: David Schulz <david.schulz@qt.io>

src/plugins/texteditor/behaviorsettings.cpp
@@ -132,68 +132,37 @@ class BehaviorSettingsPage : public Core::IOptionsPage
 {
 public:
     BehaviorSettingsPage();
-    ~BehaviorSettingsPage() override;
 
-    ICodeStylePreferences *codeStyle() const;
-    CodeStylePool *codeStylePool() const;
-
-private:
-    class BehaviorSettingsPagePrivate *d;
-};
-
-class BehaviorSettingsPagePrivate : public QObject
-{
-public:
-    BehaviorSettingsPagePrivate();
+    ICodeStylePreferences *codeStyle() { return &m_codeStyle; }
+    CodeStylePool *codeStylePool() { return &m_defaultCodeStylePool; }
 
     const Key m_settingsPrefix{"text"};
-    TextEditor::BehaviorSettingsWidget *m_behaviorWidget = nullptr;
-
-    CodeStylePool *m_defaultCodeStylePool = nullptr;
-    SimpleCodeStylePreferences *m_codeStyle = nullptr;
-    SimpleCodeStylePreferences *m_pageCodeStyle = nullptr;
+    SimpleCodeStylePreferences m_codeStyle;
+    CodeStylePool m_defaultCodeStylePool{nullptr};
 };
-
-BehaviorSettingsPagePrivate::BehaviorSettingsPagePrivate()
-{
-    // global tab preferences for all other languages
-    m_codeStyle = new SimpleCodeStylePreferences(this);
-    m_codeStyle->setDisplayName(Tr::tr("Global", "Settings"));
-    m_codeStyle->setId(Constants::GLOBAL_SETTINGS_ID);
-
-    // default pool for all other languages
-    m_defaultCodeStylePool = new CodeStylePool(nullptr, this); // Any language
-    m_defaultCodeStylePool->addCodeStyle(m_codeStyle);
-
-    m_codeStyle->fromSettings(m_settingsPrefix);
-}
 
 class BehaviorSettingsWidgetImpl : public Core::IOptionsPageWidget
 {
 public:
-    BehaviorSettingsWidgetImpl(BehaviorSettingsPagePrivate *d) : d(d)
+    BehaviorSettingsWidgetImpl(BehaviorSettingsPage *page)
+        : m_page(page)
+        , m_behaviorWidget(&globalTypingSettings(), &globalStorageSettings(),
+                           &globalBehaviorSettings(), &globalExtraEncodingSettings(), this)
     {
-        d->m_behaviorWidget = new BehaviorSettingsWidget(&globalTypingSettings(),
-                                                         &globalStorageSettings(),
-                                                         &globalBehaviorSettings(),
-                                                         &globalExtraEncodingSettings(),
-                                                         this);
-
         auto verticalSpacer = new QSpacerItem(20, 13, QSizePolicy::Minimum, QSizePolicy::Expanding);
 
         auto gridLayout = new QGridLayout(this);
         if (Utils::HostOsInfo::isMacHost())
             gridLayout->setContentsMargins(-1, 0, -1, 0); // don't ask.
-        gridLayout->addWidget(d->m_behaviorWidget, 0, 0, 1, 1);
+        gridLayout->addWidget(&m_behaviorWidget, 0, 0, 1, 1);
         gridLayout->addItem(verticalSpacer, 1, 0, 1, 1);
 
-        d->m_pageCodeStyle = new SimpleCodeStylePreferences(this);
-        d->m_pageCodeStyle->setDelegatingPool(d->m_codeStyle->delegatingPool());
-        d->m_pageCodeStyle->setTabSettings(d->m_codeStyle->tabSettings());
-        d->m_pageCodeStyle->setCurrentDelegate(d->m_codeStyle->currentDelegate());
-        d->m_behaviorWidget->setCodeStyle(d->m_pageCodeStyle);
+        m_pageCodeStyle.setDelegatingPool(page->m_codeStyle.delegatingPool());
+        m_pageCodeStyle.setTabSettings(page->m_codeStyle.tabSettings());
+        m_pageCodeStyle.setCurrentDelegate(page->m_codeStyle.currentDelegate());
+        m_behaviorWidget.setCodeStyle(&m_pageCodeStyle);
 
-        TabSettingsWidget *tabSettingsWidget = d->m_behaviorWidget->tabSettingsWidget();
+        TabSettingsWidget *tabSettingsWidget = m_behaviorWidget.tabSettingsWidget();
         tabSettingsWidget->setCodingStyleWarningVisible(true);
         connect(tabSettingsWidget, &TabSettingsWidget::codingStyleLinkClicked,
                 this, [] (TabSettingsWidget::CodingStyleLink link) {
@@ -207,7 +176,7 @@ public:
             }
         });
 
-        installMarkSettingsDirtyTriggerRecursively(d->m_pageCodeStyle);
+        installMarkSettingsDirtyTriggerRecursively(&m_pageCodeStyle);
         installCheckSettingsDirtyTrigger(&globalTypingSettings());
         installCheckSettingsDirtyTrigger(&globalStorageSettings());
         installCheckSettingsDirtyTrigger(&globalBehaviorSettings());
@@ -217,21 +186,23 @@ public:
     bool isDirty() const;
     void apply() final;
 
-    BehaviorSettingsPagePrivate *d;
+    BehaviorSettingsPage *m_page;
+    BehaviorSettingsWidget m_behaviorWidget;
+    SimpleCodeStylePreferences m_pageCodeStyle;
 };
 
 BehaviorSettingsPage::BehaviorSettingsPage()
-  : d(new BehaviorSettingsPagePrivate)
 {
     setId(Constants::TEXT_EDITOR_BEHAVIOR_SETTINGS);
     setDisplayName(Tr::tr("Behavior"));
     setCategory(TextEditor::Constants::TEXT_EDITOR_SETTINGS_CATEGORY);
-    setWidgetCreator([this] { return new BehaviorSettingsWidgetImpl(d); });
-}
+    setWidgetCreator([this] { return new BehaviorSettingsWidgetImpl(this); });
 
-BehaviorSettingsPage::~BehaviorSettingsPage()
-{
-    delete d;
+    m_codeStyle.setDisplayName(Tr::tr("Global", "Settings"));
+    m_codeStyle.setId(Constants::GLOBAL_SETTINGS_ID);
+    m_codeStyle.fromSettings(m_settingsPrefix);
+
+    m_defaultCodeStylePool.addCodeStyle(&m_codeStyle);
 }
 
 bool BehaviorSettingsWidgetImpl::isDirty() const
@@ -245,9 +216,9 @@ bool BehaviorSettingsWidgetImpl::isDirty() const
     if (globalExtraEncodingSettings().isDirty())
         return true;
 
-    if (d->m_codeStyle->tabSettings() != d->m_pageCodeStyle->tabSettings())
+    if (m_page->m_codeStyle.tabSettings() != m_pageCodeStyle.tabSettings())
         return true;
-    if (d->m_codeStyle->currentDelegate() != d->m_pageCodeStyle->currentDelegate())
+    if (m_page->m_codeStyle.currentDelegate() != m_pageCodeStyle.currentDelegate())
         return true;
 
     return false;
@@ -255,33 +226,20 @@ bool BehaviorSettingsWidgetImpl::isDirty() const
 
 void BehaviorSettingsWidgetImpl::apply()
 {
-    if (!d->m_behaviorWidget) // page was never shown
-        return;
-
     globalTypingSettings().apply();
     globalBehaviorSettings().apply();
     globalStorageSettings().apply();
     globalExtraEncodingSettings().apply();
 
-    if (d->m_codeStyle->tabSettings() != d->m_pageCodeStyle->tabSettings()) {
-        d->m_codeStyle->setTabSettings(d->m_pageCodeStyle->tabSettings());
-        d->m_codeStyle->toSettings(d->m_settingsPrefix);
+    if (m_page->m_codeStyle.tabSettings() != m_pageCodeStyle.tabSettings()) {
+        m_page->m_codeStyle.setTabSettings(m_pageCodeStyle.tabSettings());
+        m_page->m_codeStyle.toSettings(m_page->m_settingsPrefix);
     }
 
-    if (d->m_codeStyle->currentDelegate() != d->m_pageCodeStyle->currentDelegate()) {
-        d->m_codeStyle->setCurrentDelegate(d->m_pageCodeStyle->currentDelegate());
-        d->m_codeStyle->toSettings(d->m_settingsPrefix);
+    if (m_page->m_codeStyle.currentDelegate() != m_pageCodeStyle.currentDelegate()) {
+        m_page->m_codeStyle.setCurrentDelegate(m_pageCodeStyle.currentDelegate());
+        m_page->m_codeStyle.toSettings(m_page->m_settingsPrefix);
     }
-}
-
-ICodeStylePreferences *BehaviorSettingsPage::codeStyle() const
-{
-    return d->m_codeStyle;
-}
-
-CodeStylePool *BehaviorSettingsPage::codeStylePool() const
-{
-    return d->m_defaultCodeStylePool;
 }
 
 static BehaviorSettingsPage &behaviorSettingsPage()

commit 7496da564f5b4707aa1f0033403583d6810a3039

CppEditor, QmlJSTools: Re-work ownership of CodeStylePreferences
... and CodeStylePool.

Remove factory deletion from CodeStylePool destructor and reduce
setParent calls by making code style factory a proper members

Make the CodeStylePools proper members in their surrounding
settings page too, for simpler code and better reasonings about lifetimes.

This fixes the crash on shutdown introduced with  803a269047296.

Change-Id: I7b5f465a14ba4c9be30438604530171db1685d7c
Reviewed-by: David Schulz <david.schulz@qt.io>

src/plugins/cppeditor/cpptoolssettings.cpp
@@ -113,6 +113,9 @@ class CppToolsSettings final : public QObject
 public:
     CppToolsSettings();
     ~CppToolsSettings() final;
+
+    CppCodeStylePreferencesFactory m_factory;
+    CodeStylePool m_pool{&m_factory};
 };
 
 CppToolsSettings::CppToolsSettings()
@@ -120,19 +123,17 @@ CppToolsSettings::CppToolsSettings()
     qRegisterMetaType<CppCodeStyleSettings>("CppEditor::CppCodeStyleSettings");
 
     // code style factory
-    auto factory = new CppCodeStylePreferencesFactory;
-    TextEditorSettings::registerCodeStyleFactory(factory);
+    TextEditorSettings::registerCodeStyleFactory(&m_factory);
 
     // code style pool
-    auto pool = new CodeStylePool(factory, this);
-    TextEditorSettings::registerCodeStylePool(Constants::CPP_SETTINGS_ID, pool);
+    TextEditorSettings::registerCodeStylePool(Constants::CPP_SETTINGS_ID, &m_pool);
 
     // global code style settings
     g_globalCodeStyle = new CppCodeStylePreferences(this);
-    g_globalCodeStyle->setDelegatingPool(pool);
+    g_globalCodeStyle->setDelegatingPool(&m_pool);
     g_globalCodeStyle->setDisplayName(Tr::tr("Global", "Settings"));
     g_globalCodeStyle->setId(idKey);
-    pool->addCodeStyle(g_globalCodeStyle);
+    m_pool.addCodeStyle(g_globalCodeStyle);
     TextEditorSettings::registerCodeStyle(Constants::CPP_SETTINGS_ID, g_globalCodeStyle);
 
     /*
@@ -162,7 +163,7 @@ CppToolsSettings::CppToolsSettings()
 
     // built-in settings
     // Qt style
-    auto qtCodeStyle = new CppCodeStylePreferences;
+    auto qtCodeStyle = new CppCodeStylePreferences(this);
     qtCodeStyle->setId("qt");
     qtCodeStyle->setDisplayName(Tr::tr("Qt"));
     qtCodeStyle->setReadOnly(true);
@@ -173,10 +174,10 @@ CppToolsSettings::CppToolsSettings()
     qtTabSettings.m_continuationAlignBehavior = TabSettings::ContinuationAlignWithIndent;
     qtTabSettings.m_autoDetect = false;
     qtCodeStyle->setTabSettings(qtTabSettings);
-    pool->addCodeStyle(qtCodeStyle);
+    m_pool.addCodeStyle(qtCodeStyle);
 
     // GNU style
-    auto gnuCodeStyle = new CppCodeStylePreferences;
+    auto gnuCodeStyle = new CppCodeStylePreferences(this);
     gnuCodeStyle->setId("gnu");
     gnuCodeStyle->setDisplayName(Tr::tr("GNU"));
     gnuCodeStyle->setReadOnly(true);
@@ -193,12 +194,12 @@ CppToolsSettings::CppToolsSettings()
     gnuCodeStyleSettings.indentSwitchLabels = true;
     gnuCodeStyleSettings.indentBlocksRelativeToSwitchLabels = true;
     gnuCodeStyle->setCodeStyleSettings(gnuCodeStyleSettings);
-    pool->addCodeStyle(gnuCodeStyle);
+    m_pool.addCodeStyle(gnuCodeStyle);
 
     // default delegate for global preferences
     g_globalCodeStyle->setCurrentDelegate(qtCodeStyle);
 
-    pool->loadCustomCodeStyles();
+    m_pool.loadCustomCodeStyles();
 
     // load global settings (after built-in settings are added to the pool)
     g_globalCodeStyle->fromSettings(Constants::CPP_SETTINGS_ID);

src/plugins/qmljstools/qmljssettings.cpp
@@ -1362,25 +1362,23 @@ public:
     QmlJSToolsSettings();
     ~QmlJSToolsSettings() final;
 
+    QmlJSCodeStylePreferencesFactory m_factory;
+    CodeStylePool m_pool{&m_factory};
     QmlJSCodeStylePreferences m_globalCodeStyle;
 };
 
 QmlJSToolsSettings::QmlJSToolsSettings()
 {
-    ICodeStylePreferencesFactory *factory = new QmlJSCodeStylePreferencesFactory;
+    TextEditorSettings::registerCodeStyleFactory(&m_factory);
+    TextEditorSettings::registerCodeStylePool(Constants::QML_JS_SETTINGS_ID, &m_pool);
 
-    TextEditorSettings::registerCodeStyleFactory(factory);
-
-    auto pool = new CodeStylePool(factory, this);
-    TextEditorSettings::registerCodeStylePool(Constants::QML_JS_SETTINGS_ID, pool);
-
-    m_globalCodeStyle.setDelegatingPool(pool);
+    m_globalCodeStyle.setDelegatingPool(&m_pool);
     m_globalCodeStyle.setDisplayName(Tr::tr("Global", "Settings"));
     m_globalCodeStyle.setId(idKey);
-    pool->addCodeStyle(&m_globalCodeStyle);
+    m_pool.addCodeStyle(&m_globalCodeStyle);
     TextEditorSettings::registerCodeStyle(QmlJSTools::Constants::QML_JS_SETTINGS_ID, &m_globalCodeStyle);
 
-    auto qtCodeStyle = new QmlJSCodeStylePreferences;
+    auto qtCodeStyle = new QmlJSCodeStylePreferences(this);
     qtCodeStyle->setId("qt");
     qtCodeStyle->setDisplayName(Tr::tr("Qt"));
     qtCodeStyle->setReadOnly(true);
@@ -1407,9 +1405,9 @@ QmlJSToolsSettings::QmlJSToolsSettings()
         }
     });
 
-    pool->addCodeStyle(qtCodeStyle);
+    m_pool.addCodeStyle(qtCodeStyle);
     m_globalCodeStyle.setCurrentDelegate(qtCodeStyle);
-    pool->loadCustomCodeStyles();
+    m_pool.loadCustomCodeStyles();
     m_globalCodeStyle.fromSettings(QmlJSTools::Constants::QML_JS_SETTINGS_ID);
 
     using namespace Utils::Constants;

src/plugins/texteditor/behaviorsettings.cpp
@@ -155,8 +155,8 @@ public:
     CodeStylePool *codeStylePool() { return &m_defaultCodeStylePool; }
 
     const Key m_settingsPrefix{"text"};
-    SimpleCodeStylePreferences m_codeStyle;
     CodeStylePool m_defaultCodeStylePool{nullptr};
+    SimpleCodeStylePreferences m_codeStyle;
 };
 
 class BehaviorSettingsWidgetImpl : public Core::IOptionsPageWidget

src/plugins/texteditor/codestylepool.cpp
@@ -26,7 +26,6 @@ class CodeStylePoolPrivate
 {
 public:
     CodeStylePoolPrivate() = default;
-    ~CodeStylePoolPrivate();
 
     QByteArray generateUniqueId(const QByteArray &id) const;
 
@@ -37,11 +36,6 @@ public:
     QMap<QByteArray, ICodeStylePreferences *> m_idToCodeStyle;
     QString m_settingsPath;
 };
-
-CodeStylePoolPrivate::~CodeStylePoolPrivate()
-{
-    delete m_factory;
-}
 
 QByteArray CodeStylePoolPrivate::generateUniqueId(const QByteArray &id) const
 {
@@ -71,9 +65,8 @@ static FilePath customCodeStylesPath()
     return Core::ICore::userResourcePath("codestyles");
 }
 
-CodeStylePool::CodeStylePool(ICodeStylePreferencesFactory *factory, QObject *parent)
-    : QObject(parent),
-      d(new Internal::CodeStylePoolPrivate)
+CodeStylePool::CodeStylePool(ICodeStylePreferencesFactory *factory)
+    : d(new Internal::CodeStylePoolPrivate)
 {
     d->m_factory = factory;
 }
@@ -128,6 +121,7 @@ ICodeStylePreferences *CodeStylePool::createCodeStyle(const QByteArray &id, cons
     codeStyle->setDisplayName(displayName);
 
     addCodeStyle(codeStyle);
+    codeStyle->setParent(this);
 
     saveCodeStyle(codeStyle);
 
@@ -143,6 +137,7 @@ ICodeStylePreferences *CodeStylePool::createCodeStyle(const QString &displayName
     codeStyle->setDisplayName(displayName);
 
     addCodeStyle(codeStyle);
+    codeStyle->setParent(this);
     saveCodeStyle(codeStyle);
 
     return codeStyle;
@@ -159,8 +154,6 @@ void CodeStylePool::addCodeStyle(ICodeStylePreferences *codeStyle)
     else
         d->m_customPool.append(codeStyle);
     d->m_idToCodeStyle.insert(newId, codeStyle);
-    // take ownership
-    codeStyle->setParent(this);
 
     auto doSaveStyle = [this, codeStyle] { saveCodeStyle(codeStyle); };
     connect(codeStyle, &ICodeStylePreferences::valueChanged, this, doSaveStyle);
@@ -246,6 +239,7 @@ ICodeStylePreferences *CodeStylePool::loadCodeStyle(
             codeStyle->setProject(projectFile);
 
             addCodeStyle(codeStyle);
+            codeStyle->setParent(this);
         }
     }
     return codeStyle;

src/plugins/texteditor/codestylepool.h
@@ -21,8 +21,9 @@ namespace Internal { class CodeStylePoolPrivate; }
 class TEXTEDITOR_EXPORT CodeStylePool : public QObject
 {
     Q_OBJECT
+
 public:
-    explicit CodeStylePool(ICodeStylePreferencesFactory *factory, QObject *parent = nullptr);
+    explicit CodeStylePool(ICodeStylePreferencesFactory *factory);
     ~CodeStylePool() override;
 
     QList<ICodeStylePreferences *> codeStyles() const;