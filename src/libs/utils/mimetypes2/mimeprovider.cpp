c7747cf46a1a6302750738500db590c5f8181561
Eike Ziller <eike.ziller@qt.io>
2026-06-08 17:19:07 +0200

    Update mime database from Qt
    qtbase/7d163200f9d6f47bce941969869c54369fc26056
      Mark three files in corelib/mimetype as critical
    
    Change-Id: I63d31f48a7699ab3918a2f05d3a4336c9bd66688
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/libs/utils/mimetypes2/mimeprovider.cpp
+++ src/libs/utils/mimetypes2/mimeprovider.cpp
@@ -2,6 +2,7 @@
 // Copyright (C) 2018 Klaralvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author David Faure <david.faure@kdab.com>
 // Copyright (C) 2019 Intel Corporation.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:data-parser
 
 #include "mimeprovider_p.h"
 
