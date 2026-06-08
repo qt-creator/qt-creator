90bea117d7851bffd1ddfc49c1a4a321c2424048
Eike Ziller <eike.ziller@qt.io>
2026-06-08 17:22:42 +0200

    Update mime database from Qt
    qtbase/3e88d518eb3ef0c7225e5f551e154ac22a37f578
      Mark qmimedatabase.cpp security critical
    
    Change-Id: I4d21c2109ad3f067cfa4b33737872a3cd339bae2
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/libs/utils/mimetypes2/mimedatabase.cpp
+++ src/libs/utils/mimetypes2/mimedatabase.cpp
@@ -1,6 +1,7 @@
 // Copyright (C) 2016 The Qt Company Ltd.
 // Copyright (C) 2015 Klaralvdalens Datakonsult AB, a KDAB Group company, info@kdab.com, author David Faure <david.faure@kdab.com>
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
+// Qt-Security score:critical reason:data-parser
 
 #include <qplatformdefs.h> // always first
 
