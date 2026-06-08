99f096b62666226bbf8abca6c9e187c31f0d5508
Eike Ziller <eike.ziller@qt.io>
2026-06-08 14:34:35 +0200

    Update mime database from Qt
    qtbase/56fa23911c3a0681de39601489d51805e208dc03
      QFactoryLoader: fix the ability to load symlinks to plugins
    
    Change-Id: I515d99e2685f9b2df47faae551e35b7d65d06131
    Reviewed-by: Jarek Kobus <jaroslaw.kobus@qt.io>

--- src/libs/utils/mimetypes2/mimeprovider.cpp
+++ src/libs/utils/mimetypes2/mimeprovider.cpp
@@ -760,7 +760,8 @@ void MimeXMLProvider::ensureLoaded()
 {
     QStringList allFiles;
     const QString packageDir = m_directory + QStringView(u"/packages");
-    for (const auto &entry : QDirListing(packageDir, QDirListing::IteratorFlag::FilesOnly))
+    for (const auto &entry : QDirListing(packageDir, QDirListing::IteratorFlag::FilesOnly
+                                         | QDirListing::IteratorFlag::ResolveSymlinks))
         allFiles.emplace_back(packageDir + u'/' + entry.fileName());
 
     if (m_allFiles == allFiles)
