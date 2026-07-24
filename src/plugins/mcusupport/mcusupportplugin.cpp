e4bdb978ff9d24387268b1eb22bb43426bd9740f
Christian Kandeler <christian.kandeler@qt.io>
2026-07-24 13:58:01 +0200

    MCU: Add missing include
    Change-Id: I2e2145fbdb99ba8fab585ccc1ef64ab0af99d082
    Reviewed-by: Christian Stenger <christian.stenger@qt.io>

--- src/plugins/mcusupport/mcusupportplugin.cpp
+++ src/plugins/mcusupport/mcusupportplugin.cpp
@@ -1,6 +1,8 @@
 // Copyright (C) 2020 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
+#include "mcusupportplugin.h"
+
 #include "mcubuildstep.h"
 #include "mcukitmanager.h"
 #include "mculegacyconstants.h"
