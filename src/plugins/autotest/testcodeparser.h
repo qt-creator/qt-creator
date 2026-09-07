e479cf05ec7f0b13ef9ef00f0e89688647809f1d
Christian Stenger <christian.stenger@qt.io>
2026-09-07 10:28:27 +0200

    Revert "AutoTest: Speedup parsing for tests"
    Patch introduced a long ui freeze on Windows - not worth the
    trouble.
    This reverts commit 866d3777af3c71174d0bb7147e45ec9d739db532.
    
    Fixes: QTCREATORBUG-34915
    Change-Id: I3ffa99961fa2e40d305f7ce3100a1b2c7f19dde0
    Reviewed-by: Marcus Tillmanns <marcus.tillmanns@qt.io>

--- src/plugins/autotest/testcodeparser.h
+++ src/plugins/autotest/testcodeparser.h
@@ -94,7 +94,6 @@ private:
     bool m_singleShotScheduled = false;
     bool m_reparseTimerTimedOut = false;
     QSet<Utils::FilePath> m_postponedFiles;
-    QSet<Utils::FilePath> m_applicationTargetFiles; // refreshed on every full parse
     State m_parserState = Idle;
     QList<ITestParser *> m_testCodeParsers; // ptrs are still owned by TestFrameworkManager
     QTimer m_reparseTimer;
