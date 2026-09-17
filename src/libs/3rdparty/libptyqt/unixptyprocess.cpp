21d163e7d39f295c854937f779f4ad726be2e62e
SanthoshKumar Selvaraj <santhosh.kumar.selvaraj@qt.io>
2026-09-17 13:26:03 +0200

    Rename Q_OS_OHOS to Q_OS_HARMONY for HarmonyOS
    The HarmonyOS platform macro was renamed to Q_OS_HARMONY in qtbase
    commit f2a3e01ab1acae9b678c283823ec77b1e9b614d9, so the earlier
    Q_OS_OHOS guard no longer matched and the utmpx block was compiled,
    which broke the build.
    
    Rename the macro in the guard to Q_OS_HARMONY.
    
    Change-Id: Iac17c46bdab326abc919cda2392867c0bf96dfdc
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/libs/3rdparty/libptyqt/unixptyprocess.cpp
+++ src/libs/3rdparty/libptyqt/unixptyprocess.cpp
@@ -322,7 +322,7 @@ void ShellProcess::configChildProcess()
     ioctl(m_handleSlave, TIOCSCTTY, 0);
     tcsetpgrp(m_handleSlave, sid);
 
-#if !defined(Q_OS_ANDROID) && !defined(Q_OS_FREEBSD) && !defined(Q_OS_OHOS)
+#if !defined(Q_OS_ANDROID) && !defined(Q_OS_FREEBSD) && !defined(Q_OS_HARMONY)
     // on Android imposible to put record to the 'utmp' file
     struct utmpx utmpxInfo;
     memset(&utmpxInfo, 0, sizeof(utmpxInfo));
